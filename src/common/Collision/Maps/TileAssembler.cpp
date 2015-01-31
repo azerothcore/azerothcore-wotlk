4908029d278e0e77297eabd552bdead5596b60ca
jackpoz <giacomopoz@gmail.com>
2015-01-31 22:57:08 +0100

    Core/VMaps: Fix debug assert caused by invalid vmaps
    Fix debug assert thrown by G3D caused by models with empty bounding box being exported by vmap4assembler and imported by worldserver. This change will skip those models, proper investigation on the issue of why the bounding box is empty is still required.
    
    (cherry picked from commit a7ca78b2fe12c68e561782416bf5726410a3c01b)

--- src/common/Collision/Maps/TileAssembler.cpp
+++ src/common/Collision/Maps/TileAssembler.cpp
@@ -443,6 +443,18 @@ namespace VMAP
                 }
             }
 
+            if (bounds.isEmpty())
+            {
+                std::cout << "\nModel " << std::string(buff, name_length) << " has empty bounding box" << std::endl;
+                continue;
+            }
+
+            if (!bounds.isFinite())
+            {
+                std::cout << "\nModel " << std::string(buff, name_length) << " has invalid bounding box" << std::endl;
+                continue;
+            }
+
             fwrite(&displayId, sizeof(uint32), 1, model_list_copy);
             fwrite(&isWmo, sizeof(uint8), 1, model_list_copy);
             fwrite(&name_length, sizeof(uint32), 1, model_list_copy);
