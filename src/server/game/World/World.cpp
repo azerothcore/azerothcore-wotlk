ddf815caef865754cf6159ee6ff98a4f5f55ef2c
mik1893 <michele.roscelli@gmail.com>
2016-07-09 23:11:29 +0100

    Fix compile VS2015

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2927,6 +2927,7 @@ void World::LoadDBVersion()
     if (result)
     {
         Field* fields = result->Fetch();
+
         m_DBVersion              = fields[0].GetString();
 
         // will be overwrite by config values if different and non-0
