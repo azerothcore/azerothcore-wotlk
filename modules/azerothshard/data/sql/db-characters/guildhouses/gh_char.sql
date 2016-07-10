b835608b39a120733a7d9af4477485c596154a41
mik1893 <michele.roscelli@gmail.com>
2016-07-10 11:45:13 +0100

    Core/Guildhouses: missing SQL for char db

---
+++ modules/azerothshard/data/sql/db-characters/guildhouses/gh_char.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `gh_guildadd` (
+  `guildId` int(11) unsigned NOT NULL,
+  `GuildHouse_Add` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`guildId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
