7761e7e24e6b782383a4f68c6217261f18585aa0
mik1893 <michele.roscelli@gmail.com>
2016-07-16 11:35:33 +0100

    SQL Correction

--- modules/azerothshard/data/sql/db-extra/crossfaction_disable.sql
+++ modules/azerothshard/data/sql/db-extra/crossfaction_disable.sql
@@ -1,4 +1,4 @@
-DROP TABLE `crossfaction_disable`;
+DROP TABLE IF EXISTS `crossfaction_disable`;
 CREATE TABLE `crossfaction_disable` (
   `id` int(10) NOT NULL COMMENT 'could be Map, Zone, Area, depending on type field',
   `type` int(10) NOT NULL COMMENT '1 = MAP - 2 = ZONE - 3 = AREA'
