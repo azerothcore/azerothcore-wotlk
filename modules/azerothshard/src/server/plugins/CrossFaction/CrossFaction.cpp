5de76284cad7280ecc18f284fba20e4d71134ed4
mik1893 <michele.roscelli@gmail.com>
2016-07-26 07:29:43 +0100

    Core/Custom: Crossfaction - added offline group leader caching and corrected wrong reset behaviour

--- modules/azerothshard/src/server/plugins/CrossFaction/CrossFaction.cpp
+++ modules/azerothshard/src/server/plugins/CrossFaction/CrossFaction.cpp
@@ -16,20 +16,20 @@ void CrossFaction::UpdatePlayerTeam(Group* group, uint64 guid, bool reset /* = f
     {
         bool disable = true;
 
-        // Per prima cosa, controllo i disable
+        // Check disables
         if (isMapEnabled(player->GetMapId()) && isZoneEnabled(player->GetZoneId()) && isAreaEnabled(player->GetAreaId()))
             disable = false;
         else
         {
-            // messaggio al player in caso di crossfaction disabilitato in una specifica area, cosi lo sa prima di lamentarsi e scassarmi la minchia
+            // message player when crossfaction disabled in a specific area
             player->Whisper("Quest'area ha il crossfaction disabilitato", LANG_UNIVERSAL, player->GetGUID());
             sLog->outDebug(LOG_FILTER_CROSSFACTION, "Crossfaction: disabled for player %s", player->GetName().c_str());
         }
 
-        // se non e' un reset forzato oppure na zona disabilitata, eseguo la routine di cambio
+        // if not reset or disable, check and change
         if (!reset && !disable)
         {
-            // Se sono in battleground applico la fazione relativa al battleground
+            // battleground group
             if (player->InBattleground())
             {
                 if (Battleground * bg = player->GetBattleground())
@@ -41,26 +41,38 @@ void CrossFaction::UpdatePlayerTeam(Group* group, uint64 guid, bool reset /* = f
                 }
             }
 
-            // Se sono in un nomrale gruppo, applico la fazione relativa al leader
+            // standard group
             uint64 leaderGuid = group ? group->GetLeaderGUID() : player->GetGUID();
             if (leaderGuid != player->GetGUID())
+            {
                 if (Player* leader = ObjectAccessor::FindPlayer(leaderGuid))
                 {
                     player->setTeamId(leader->GetTeamId());
                     player->setFaction(leader->getFaction());
                     return;
                 }
+                else // if leader guid is offline, access is race from the leader map
+                {
+                    UNORDERED_MAP<uint64, uint8>::iterator itr = LeaderRaceMap.find(leaderGuid);
+                    if (itr != LeaderRaceMap.end())
+                    {
+                        uint8 raceid = itr->second;
+                        ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(raceid);
+                        player->setFaction(rEntry ? rEntry->FactionID : 0);
+                    }
+                }
+            }
         }
 
-        // TUTTI GLI ALTRI CASI: RESET DELLA FAZIONE ALL ORIGINALE
-        // Se reset forzato oppure zona disabilitata, va messa la fazione originale del player, qualunque cosa succeda.
-        // LA RAZZA NON VIENE MAI MODIFICATA UNA VOLTA LOGGATI - SI PUO USARE PER TORNARE AL PUNTO DI PARTENZA
+        // all the other cases: reset to the original faction
 
         player->setTeamId(player->GetTeamId(true));
         ChrRacesEntry const* rEntry = sChrRacesStore.LookupEntry(player->getRace());
         player->setFaction(rEntry ? rEntry->FactionID : 0);
         sLog->outDebug(LOG_FILTER_CROSSFACTION, "Crossfaction: reset done for player %s", player->GetName().c_str());
     }
+    else
+        sLog->outError("CrossFaction: tried to update faction of player %u but he's not online... ", GUID_LOPART(guid));
 }
 
 void CrossFaction::LoadConfig(bool reload)
@@ -73,7 +85,7 @@ void CrossFaction::LoadConfig(bool reload)
         areaDisable.clear();
     }
 
-    QueryResult mapResult = ExtraDatabase.PQuery("SELECT id FROM crossfaction_disable where type = 1");
+    QueryResult mapResult  = ExtraDatabase.PQuery("SELECT id FROM crossfaction_disable where type = 1");
     QueryResult zoneResult = ExtraDatabase.PQuery("SELECT id FROM crossfaction_disable where type = 2");
     QueryResult areaResult = ExtraDatabase.PQuery("SELECT id FROM crossfaction_disable where type = 3");
 
@@ -113,6 +125,38 @@ void CrossFaction::LoadConfig(bool reload)
     }
 
     sLog->outError("%u maps, %u zones, %u areas have been disabled for crossfaction ", mapcount,zonecount,areacount);
+
+    sLog->outError("Loading offline group leaders...");
+
+    LeaderRaceMap.clear();
+    QueryResult groupResult = CharacterDatabase.PQuery("SELECT c.guid, c.race from characters c, groups g where g.leaderguid = c.guid");
+
+    if (groupResult)
+    {
+        while (groupResult->NextRow());
+            LeaderRaceMap[(*groupResult)[0].GetUInt64()] = (*groupResult)[0].GetUInt8();
+    }
+}
+
+// This function is used to retrieve 
+uint8 CrossFaction::GetPlayerRace(uint64 playerGuid)
+{
+    if (Player* player = ObjectAccessor::FindPlayer(playerGuid))
+        return player->getRace();
+    else
+    {
+        // Query informations from the DB
+        PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_SEL_CHAR_PINFO);
+        stmt->setUInt32(0, GUID_LOPART(playerGuid));
+        PreparedQueryResult result = CharacterDatabase.Query(stmt);
+
+        ASSERT(result); //you cannot have a player non-existing in db that is playing... / a group leader...
+
+        Field* fields = result->Fetch();
+        uint8 raceid = fields[4].GetUInt8();
+
+        return raceid;
+    }
 }
 
 // ALL THE SCRIPT FUNCTIONS AFTER THIS POINT: GROUPSCRIPT, PLAYERSCRIPT, WORLDSCRIPT
@@ -136,6 +180,8 @@ public:
     // This script is called at the end of the leader change function - m_leader has already been set, we can use the group already (not the guids)
     void OnChangeLeader(Group* group, uint64 newLeaderGuid, uint64 oldLeaderGuid) override
     {
+        sCrossFaction->UpdateGroupLeaderMap(newLeaderGuid);
+
         std::list<Group::MemberSlot> memberSlots = group->GetMemberSlots();
         for (std::list<Group::MemberSlot>::iterator membersIterator = memberSlots.begin(); membersIterator != memberSlots.end(); membersIterator++)
             sCrossFaction->UpdatePlayerTeam(group, (*membersIterator).guid);
@@ -176,6 +222,12 @@ public:
     void OnUpdateFaction(Player *player) override
     {
         sCrossFaction->UpdatePlayerTeam(player->GetGroup(), player->GetGUID());
+    }
+
+    void OnLogout(Player* player) override
+    {
+        // force faction reset on logout to prevent issues with DB save
+        sCrossFaction->UpdatePlayerTeam(player->GetGroup(), player->GetGUID(),true);
     }
 };
 
