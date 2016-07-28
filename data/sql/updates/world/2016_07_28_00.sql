3917cf09654f9a2fb9124f4a7d1cd7bb513ef9f8
Gargarensis <gabririchi@yahoo.it>
2016-07-28 15:17:49 +0200

    fix loot for mindless servant

---
+++ data/sql/updates/world/2016_07_28_00.sql
@@ -0,0 +1,15 @@
+-- loot for mindless servant 26536 in utgarde pinnacle
+DELETE FROM `creature_loot_template` WHERE (Entry = 26536);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `ChanceOrQuestChance`, `LootMode`, `GroupId`, `mincountOrRef`, `maxcount`) VALUES
+(26536, 33470, 10.7, 1, 0, 1, 7),
+(26536, 26002, 3, 1, 1, -26002, 1),
+(26536, 26011, 1, 1, 1, -26011, 1),
+(26536, 26012, 1, 1, 1, -26012, 1),
+(26536, 26040, 21.4, 1, 0, -26040, 1),
+(26536, 33370, 3.6, 0, 1, 1, 1),
+(26536, 33399, 3.6, 0, 1, 1, 1),
+(26536, 33454, 35.5, 1, 0, 1, 1),
+(26536, 37068, 0.69, 0, 1, 1, 1),
+(26536, 37069, 0.69, 0, 1, 1, 1),
+(26536, 37070, 0.69, 0, 1, 1, 1),
+(26536, 45912, 0.1, 0, 1, 1, 1);
