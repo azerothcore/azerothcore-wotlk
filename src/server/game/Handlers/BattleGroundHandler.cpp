a1720db43b55c0ccb1fa651d636d7eb9ebdbe7f4
mik1893 <michele.roscelli@gmail.com>
2016-07-29 20:46:55 +0100

    Revert wrong logic - bg crossfaction

--- src/server/game/Handlers/BattleGroundHandler.cpp
+++ src/server/game/Handlers/BattleGroundHandler.cpp
@@ -439,14 +439,14 @@ void WorldSession::HandleBattleFieldPortOpcode(WorldPacket &recvData)
                 uint32 allyCount = bg->GetPlayersCountByTeam(TEAM_ALLIANCE);
                 uint32 hordeCount = bg->GetPlayersCountByTeam(TEAM_HORDE);
 
-                if (allyCount && hordeCount && allyCount == hordeCount)
+                if (allyCount == hordeCount)
                 {
                     if (roll_chance_i(50))
                         teamId = _player->GetTeamId(true) == TEAM_ALLIANCE ? TEAM_HORDE : TEAM_ALLIANCE;
                 }
                 else if (allyCount < hordeCount)
                     teamId = TEAM_ALLIANCE;
-                else if (hordeCount < allyCount)
+                else
                     teamId = TEAM_HORDE;
                 // [/AZTH]
 
