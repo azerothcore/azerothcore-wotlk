60a33973076c86d9602e191f544be27e0b763add
Yehonal <hw.2@hotmail.it>
2016-08-03 19:27:54 +0200

    [AZTH] usefull sql

--- modules/azerothshard/data/sql/tools/azth_auth_clean.sql
+++ modules/azerothshard/data/sql/tools/azth_auth_clean.sql
@@ -1,5 +1,7 @@
 # remove approved accounts from banlist
 
+-- deprecated
 DELETE FROM account_banned WHERE banreason = "New user waiting for approvement" AND active = 0;
 
+DELETE FROM account_banned WHERE banreason = "Failed login autoban" AND active = 0;
 
