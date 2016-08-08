3e0270d0eb64ca1a614010a372451b82997ae48e
Yehonal <yehonal.azeroth@gmail.com>
2016-08-08 15:25:04 +0200

    [AZTH] updated gitlogger

--- modules/azerothshard/bin/gitlogger/logger.sh
+++ modules/azerothshard/bin/gitlogger/logger.sh
@@ -11,7 +11,7 @@ cd  $DIR
 rm old.log
 mv new.log old.log
 cd /home/hw2/usr/workspace/projects/AzerothShard/server-live
-git log origin/master --no-merges --pretty=format:'<%an> %s' > $DIR/new.log
+git log origin/custom --no-merges --pretty=format:'<%an> %s' > $DIR/new.log
 cd  $DIR
 awk '!/DEV-OP/' new.log > temp && mv temp new.log
 comm -13 <(sort -u old.log) <(sort -u new.log) > shout.log
