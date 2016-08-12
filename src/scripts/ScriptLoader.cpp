e80aeedb9e2b2160f4693ceeaabcf33db963b2c0
Yehonal <yehonal.azeroth@gmail.com>
2016-08-12 02:37:42 +0200

    Another big repo refactoring, should be latest
    I've restored old paths for scripts and flattened the src structure

--- src/scripts/ScriptLoader.cpp
+++ src/scripts/ScriptLoader.cpp
@@ -18,13 +18,6 @@
 #include "ScriptLoader.h"
 #include "ScriptMgr.h"
 
-//examples
-void AddSC_example_creature();
-void AddSC_example_escort();
-void AddSC_example_gossip_codebox();
-void AddSC_example_misc();
-void AddSC_example_commandscript();
-
 // spells
 void AddSC_deathknight_spell_scripts();
 void AddSC_druid_spell_scripts();
