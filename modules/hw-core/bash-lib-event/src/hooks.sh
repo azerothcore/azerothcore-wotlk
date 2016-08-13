ff6201921c7da018ec200ca52ed023e92c83f747
Yehonal <yehonal.azeroth@gmail.com>
2016-08-13 20:25:34 +0200

    created hw-core bash libraries
    will be traited as a git subtree

--- modules/hw-core/bash-lib-event/src/hooks.sh
+++ modules/hw-core/bash-lib-event/src/hooks.sh
@@ -1,5 +1,5 @@
 # par 1: hook_name
-function runHooks() {
+function hwc_event_run_hooks() {
   hook_name="HOOKS_MAP_$1"
   read -r -a SRCS <<< ${!hook_name}
   echo "Running hooks: $hook_name"
@@ -9,7 +9,7 @@ function runHooks() {
   done
 }
 
-function registerHooks() {
+function hwc_event_register_hooks() {
   hook_name="HOOKS_MAP_$1"
   hooks=${@:2}
   declare -g "$hook_name+=$hooks "
