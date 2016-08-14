c41c478d2600c9994fa542282521dfeedbbd8aae
talamortis <littlelee_1991@live.co.uk>
2016-08-14 14:17:41 +0100

    Update and rename 16_08_14_02 to 16_08_14_02.sql

--- data/sql/updates/16_08_14_02.sql
+++ data/sql/updates/16_08_14_02.sql
@@ -1,5 +1,4 @@
 ALTER TABLE world_db_version CHANGE COLUMN 2016_08_14_00 2016_08_14_02 bit;
 
 UPDATE gameobject_template
-Set data10='29519'
-WHERE entry='181597'AND entry='181598'
+Set data10='29519' WHERE entry='181597'AND entry='181598'
