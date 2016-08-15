03f82acd46c3b875d29651e67d96c9aba1681806
talamortis <littlelee_1991@live.co.uk>
2016-08-15 17:47:58 +0100

    Update 16_08_14_02.sql

--- data/sql/updates/16_08_14_02.sql
+++ data/sql/updates/16_08_14_02.sql
@@ -1,3 +1,3 @@
 ALTER TABLE world_db_version CHANGE COLUMN 2016_08_14_00 2016_08_14_02 bit;
 
-UPDATE gameobject_template SET data10=29519 WHERE entry=181597 AND 181598
+UPDATE gameobject_template SET data10=29519 WHERE entry=181597 OR entry= 181598
