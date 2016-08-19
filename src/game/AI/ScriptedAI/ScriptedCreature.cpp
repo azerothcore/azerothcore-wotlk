0aba3ae2674118302e648af4e74d3e4003b1c87b
ShinDarth <borzifrancesco@gmail.com>
2016-08-19 23:22:03 +0200

    Corrected more copyrights

--- src/game/AI/ScriptedAI/ScriptedCreature.cpp
+++ src/game/AI/ScriptedAI/ScriptedCreature.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 
- *
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: http://github.com/azerothcore/azerothcore-wotlk/LICENSE-GPL2
  * 
  *
  * This program is free software licensed under GPL version 2
