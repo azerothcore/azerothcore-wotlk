ab17309f833b84e1fd3711b56344f265cca31d58
Yehonal <yehonal.azeroth@gmail.com>
2016-08-19 09:30:28 +0200

    fixed uwd/joiner installation

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 CUR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH_MODULES="$CUR_PATH/modules/"
-[ ! -d $PATH_MODULES/udw/joiner ] && git clone https://github.com/udw/joiner $PATH_MODULES/udw/joiner -b master
-source "$PATH_MODULES/uw-dev/joiner/joiner.sh"
-
+[ ! -d $PATH_MODULES/uwd/joiner ] && git clone https://github.com/uwd/joiner $PATH_MODULES/uwd/joiner -b master
+source "$PATH_MODULES/uwd/joiner/joiner.sh"
 
 
 if [[ $1 == "dev" ]]; then
