db3a863271caa0a6676baa699583c3a62f3721b7
DantestyleXD <Null-fox@hotmail.it>
2016-08-21 05:08:43 +0200

    Fix quest 11543 issue #93 rev.1

--- data/sql/updates/world/2016_08_19_01.sql
+++ data/sql/updates/world/2016_08_19_01.sql
@@ -13,3 +13,12 @@ INSERT INTO smart_scripts VALUES
 (25090,0,0,0,8,0,100,0,45115,0,0,0,33,25090,0,0,0,0,0,7,0,0,0,0,0,0,0,"Sin'Loren - On spellhit 45115 - Kill Credit"), 
 (25091,0,0,0,8,0,100,0,45115,0,0,0,33,25091,0,0,0,0,0,7,0,0,0,0,0,0,0,"Bloodoath - On spellhit 45115 - Kill Credit"), 
 (25092,0,0,0,8,0,100,0,45115,0,0,0,33,25092,0,0,0,0,0,7,0,0,0,0,0,0,0,"Dawnchaser - On spellhit 45115 - Kill Credit"); 
+
+/*
+Backup for next DELETE query
+INSERT INTO conditions VALUES
+(15,9143,0,0,0,9,0,11542,0,0,0,0,0,,"Show gossip option if player has quest 11542 but not complete"),
+(15,9143,0,0,1,9,0,11543,0,0,0,0,0,,"Show gossip option if player has quest 11543 but not complete");
+*/
+
+DELETE FROM conditions WHERE SourceTypeOrReferenceId=15 AND SourceGroup=9143;
