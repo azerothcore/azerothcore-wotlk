76b59632c0d226bb6a4bcdb34b3e4e3fe071b439
ShinDarth <borzifrancesco@gmail.com>
2016-08-26 16:42:23 +0200

    Misc: fix copyright header

--- src/scripts/Commands/cs_account.cpp
+++ src/scripts/Commands/cs_account.cpp
@@ -1,9 +1,8 @@
 /*
-* Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
-*
-* Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
-* Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
-*/
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: http://github.com/azerothcore/azerothcore-wotlk/LICENSE-GPL2
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ */
 
 /* ScriptData
 Name: account_commandscript
