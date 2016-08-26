d3a4ddaccbc868a3a0755a58ef1539f035c2e16b
Yehonal <yehonal.azeroth@gmail.com>
2016-08-26 02:05:09 +0200

    Fixed sql header

--- data/sql/updates/db_world/2016_08_14_00.sql
+++ data/sql/updates/db_world/2016_08_14_00.sql
@@ -1,4 +1,4 @@
-ALTER TABLE world_db_version CHANGE COLUMN 2016_08_13_00 2016_08_14_00 bit;
+ALTER TABLE world_db_version CHANGE COLUMN 2016_08_10_01 2016_08_14_00 bit;
 
 -- Dream Vision make it trigger
 UPDATE `creature_template` SET `flags_extra`='128' WHERE (`entry`='7863');
