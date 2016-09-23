ce8e914166e0fd1e634e0655ce4cf69d3d1fe1c6
talamortis <littlelee_1991@live.co.uk>
2016-09-23 09:22:21 +0100

    Removed Comment
    I take no credit for this fix im just doing the PR to close the issue.
    Winterfin oricle will now cast lightning bolt.
    the real credit goes to Chrisjiajun, this works
    
    Closes issue #119

--- data/sql/updates/pending_db_world/rev_1473152344430838100.sql
+++ data/sql/updates/pending_db_world/rev_1473152344430838100.sql
@@ -1,14 +1,5 @@
 INSERT INTO version_db_world(`sql_rev`) VALUES ('1473152344430838100');
 
-/* 
-I take no credit for this fix im just doing the PR to close the issue.
-Winterfin oricle will now cast lightning bolt.
-the real credit goes to Chrisjiajun, this works
-
-Closes issue #119
-
- */
-
 DELETE FROM smart_scripts WHERE entryorguid=25216;
 INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES
 (25216, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Set Phase 1 on Aggro'),
