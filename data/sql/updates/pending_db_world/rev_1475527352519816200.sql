ea8d2b8660c19043bd6caf2386c8a355177e9c2b
talamortis <littlelee_1991@live.co.uk>
2016-10-09 16:09:42 +0100

    Removed Comment
    Removed comment marker due to causing quary to crash

--- data/sql/updates/pending_db_world/rev_1475527352519816200.sql
+++ data/sql/updates/pending_db_world/rev_1475527352519816200.sql
@@ -1,6 +1,5 @@
 INSERT INTO version_db_world(`sql_rev`) VALUES ('1475527352519816200');
 
---Fix #116
 UPDATE `gameobject_template` SET Data1 = "5000" WHERE entry = 176248
 
 INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
