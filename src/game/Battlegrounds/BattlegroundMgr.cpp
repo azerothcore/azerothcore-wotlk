b625abe783ccf7bdeb9a4e71d9ef3c368257c3eb
Obitus1 <pistolnicusorin@yahoo.ro>
2016-10-18 22:56:06 +0300

    Update BattlegroundMgr.cpp

--- src/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/game/Battlegrounds/BattlegroundMgr.cpp
@@ -754,7 +754,10 @@ void BattlegroundMgr::SendToBattleground(Player* player, uint32 instanceId, Batt
     if (Battleground* bg = GetBattleground(instanceId))
     {
         float x, y, z, o;
-        bg->GetTeamStartLoc(player->GetBgTeamId(), x, y, z, o);
+        TeamId team = player->GetBgTeamId();
+		if (team != TEAM_ALLIANCE && team != TEAM_HORDE)
+			 team = player->GetTeamId();
+		bg->GetTeamStartLoc(team, x, y, z, o);
         player->TeleportTo(bg->GetMapId(), x, y, z, o);
     }
 }
