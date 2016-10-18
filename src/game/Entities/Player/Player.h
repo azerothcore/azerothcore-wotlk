3b573a491759aee8eb029d3bc1a3895ff78dc7a6
Obitus1 <pistolnicusorin@yahoo.ro>
2016-10-18 23:51:05 +0300

    Update Player.h

--- src/game/Entities/Player/Player.h
+++ src/game/Entities/Player/Player.h
@@ -1083,6 +1083,38 @@ class Player : public Unit, public GridObject<Player>
     public:
         explicit Player(WorldSession* session);
         ~Player();
+        
+    private:
+	bool m_ForgetBGPlayers;
+	bool m_ForgetInListPlayers;
+	uint8 m_FakeRace;
+	uint8 m_RealRace;
+	uint32 m_FakeMorph;
+
+	public:
+		typedef std::vector<uint64> FakePlayers;
+		void SendChatMessage(const char *format, ...);
+		void FitPlayerInTeam(bool action, Battleground* pBattleGround = NULL);          // void FitPlayerInTeam(bool action, Battleground* bg = NULL);
+		void DoForgetPlayersInList();
+		void DoForgetPlayersInBG(Battleground* pBattleGround);                                          // void DoForgetPlayersInBG(Battleground* bg);
+		uint8 getCFSRace() const { return m_RealRace; }
+		void SetCFSRace() { m_RealRace = GetByteValue(UNIT_FIELD_BYTES_0, 0); }; // SHOULD ONLY BE CALLED ON LOGIN
+		void SetFakeRace(); // SHOULD ONLY BE CALLED ON LOGIN
+		void SetFakeRaceAndMorph(); // SHOULD ONLY BE CALLED ON LOGIN
+		uint32 GetFakeMorph() { return m_FakeMorph; };
+		uint8 getFRace() const { return m_FakeRace; }
+		void SetForgetBGPlayers(bool value) { m_ForgetBGPlayers = value; }
+		bool ShouldForgetBGPlayers() { return m_ForgetBGPlayers; }
+		void SetForgetInListPlayers(bool value) { m_ForgetInListPlayers = value; }
+		bool ShouldForgetInListPlayers() { return m_ForgetInListPlayers; }
+		bool SendBattleGroundChat(uint32 msgtype, std::string message);
+		void MorphFit(bool value);
+		bool IsPlayingNative() const { return GetTeamId() == m_team; }
+		TeamId GetCFSTeamId() const { return m_team; }
+		TeamId GetTeamId() const { return m_bgData.bgTeamId && GetBattleground() ? m_bgData.bgTeamId : m_team; }
+		TeamId GetBgTeamId() const { return m_bgData.bgTeamId ? m_bgData.bgTeamId : GetTeamId(); }
+		bool SendRealNameQuery();
+		FakePlayers m_FakePlayers;
 
         void CleanupsBeforeDelete(bool finalCleanup = true);
 
@@ -1151,7 +1183,7 @@ class Player : public Unit, public GridObject<Player>
         PlayerSocial *GetSocial() { return m_social; }
 
         PlayerTaxi m_taxi;
-        void InitTaxiNodesForLevel() { m_taxi.InitTaxiNodesForLevel(getRace(), getClass(), getLevel()); }
+        void InitTaxiNodesForLevel() { m_taxi.InitTaxiNodesForLevel(getCFSRace(), getClass(), getLevel()); }
         bool ActivateTaxiPathTo(std::vector<uint32> const& nodes, Creature* npc = NULL, uint32 spellid = 1);
         bool ActivateTaxiPathTo(uint32 taxi_path_id, uint32 spellid = 1);
         void CleanupAfterTaxiFlight();
@@ -2063,7 +2095,6 @@ class Player : public Unit, public GridObject<Player>
         void CheckAreaExploreAndOutdoor(void);
 
         static TeamId TeamIdForRace(uint8 race);
-        TeamId GetTeamId(bool original = false) const { return original ? TeamIdForRace(getRace(true)) : m_team; };
         void setFactionForRace(uint8 race);
         void setTeamId(TeamId teamid) { m_team = teamid; };
 
@@ -2255,7 +2286,7 @@ class Player : public Unit, public GridObject<Player>
                 }
         }
 
-        TeamId GetBgTeamId() const { return m_bgData.bgTeamId != TEAM_NEUTRAL ? m_bgData.bgTeamId : GetTeamId(); }
+        void SetBGTeamId(TeamId team) { m_bgData.bgTeamId = team; }
 
         void LeaveBattleground(Battleground* bg = NULL);
         bool CanJoinToBattleground() const;
