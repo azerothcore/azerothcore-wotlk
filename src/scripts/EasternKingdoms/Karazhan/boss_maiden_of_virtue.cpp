98a6b4bc3bf39b661269c127acda79a1ae29273d
talamortis <littlelee_1991@live.co.uk>
2016-10-25 20:12:01 +0100

    Maiden change
    Changed from TYPE_MAIDEN to DATA_MAIDEN

--- src/scripts/EasternKingdoms/Karazhan/boss_maiden_of_virtue.cpp
+++ src/scripts/EasternKingdoms/Karazhan/boss_maiden_of_virtue.cpp
@@ -33,7 +33,7 @@ class boss_maiden_of_virtue : public CreatureScript
 
         struct boss_maiden_of_virtueAI : public BossAI
         {
-            boss_maiden_of_virtueAI(Creature* creature) : BossAI(creature, TYPE_MAIDEN) { }
+            boss_maiden_of_virtueAI(Creature* creature) : BossAI(creature, DATA_MAIDEN) { }
 
             void Reset()
             {
