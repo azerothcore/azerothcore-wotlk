96be391394a5732c3dc199c33aa7b7392a13368a
HeartWell <heartwell@list.ru>
2016-11-05 18:47:16 +0300

    Core/Text: Implemented locales_points_of_interest.

--- src/game/Globals/ObjectMgr.h
+++ src/game/Globals/ObjectMgr.h
@@ -1074,6 +1074,12 @@ class ObjectMgr
             if (itr == _pageTextLocaleStore.end()) return NULL;
             return &itr->second;
         }
+        PointOfInterestLocale const* GetPointOfInterestLocale(uint32 poi_id) const
+        {
+            PointOfInterestLocaleContainer::const_iterator itr = _pointOfInterestLocaleStore.find(poi_id);
+            if (itr == _pointOfInterestLocaleStore.end()) return NULL;
+            return &itr->second;
+        }
         GameObjectData& NewGOData(uint32 guid) { return _gameObjectDataStore[guid]; }
         void DeleteGOData(uint32 guid);
 
