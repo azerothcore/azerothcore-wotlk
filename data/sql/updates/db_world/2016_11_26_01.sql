cdb824546431a9cda728d21eb62aab0e9b5b3a9e
ShinDarth <borzifrancesco@gmail.com>
2016-11-26 21:08:10 +0100

    Import pending SQLs

--- data/sql/updates/db_world/2016_11_26_01.sql
+++ data/sql/updates/db_world/2016_11_26_01.sql
@@ -1,3 +1,14 @@
+-- DB update 2016_11_26_00 -> 2016_11_26_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2016_11_26_00 2016_11_26_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1479653240477084000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1479653240477084000');
 
 DROP TABLE IF EXISTS `creature_questitem`;
@@ -44,3 +55,12 @@ INSERT INTO creature_questitem (`CreatureEntry`, `Idx`, `ItemId`)
 
 ALTER TABLE `creature_template` DROP `questItem1`, DROP `questItem2`, DROP `questItem3`, DROP `questItem4`, DROP `questItem5`, DROP `questItem6`;
 ALTER TABLE `gameobject_template` DROP `questItem1`, DROP `questItem2`, DROP `questItem3`, DROP `questItem4`, DROP `questItem5`, DROP `questItem6`;
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END;
+//
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
