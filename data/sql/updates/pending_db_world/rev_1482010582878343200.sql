9354b802e6091b4fcb42770c3fd8070f0ee32f13
xml21 <grzegorzpiasecki2@gmail.com>
2016-12-17 23:14:09 +0100

    Added new line at the ned of sql UPDATE wrapper

--- data/sql/updates/pending_db_world/rev_1482010582878343200.sql
+++ data/sql/updates/pending_db_world/rev_1482010582878343200.sql
@@ -1,3 +1,4 @@
 UPDATE `spell_area` SET `area`='4169' WHERE (`spell`='58932') AND (`area`='4171') AND (`quest_start`='12499') AND (`aura_spell`='0') AND (`racemask`='0') AND (`gender`='2') LIMIT 1;
 
 UPDATE `spell_area` SET `area`='4170' WHERE (`spell`='58932') AND (`area`='4171') AND (`quest_start`='12500') AND (`aura_spell`='0') AND (`racemask`='0') AND (`gender`='2') LIMIT 1;
+
