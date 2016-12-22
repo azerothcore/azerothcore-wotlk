ae512d55c2e11c67ac2de3dcd135e9b955e7f8d3
Helias <stefanoborzi32@gmail.com>
2016-12-22 13:11:54 +0100

    DB/World: fix bad field rename introduced in previous commit

--- src/game/Globals/ObjectMgr.cpp
+++ src/game/Globals/ObjectMgr.cpp
@@ -3853,7 +3853,7 @@ void ObjectMgr::LoadQuests()
 
     QueryResult result = WorldDatabase.Query("SELECT "
         //0     1         2         3           4           5             6               7             8
-        "ID, Method, QuestLevel, MinLevel, QuestSortID, QuestType, SuggestedGroupNum, LimitTime, RequiredRaces,"
+        "ID, QuestType, QuestLevel, MinLevel, QuestSortID, QuestInfoID, SuggestedGroupNum, LimitTime, RequiredRaces,"
         //      9                     10                   11                    12
         "RequiredFactionId1, RequiredFactionId2, RequiredFactionValue1, RequiredFactionValue2, "
         //      13             14               15                    16               17             18             19               20
