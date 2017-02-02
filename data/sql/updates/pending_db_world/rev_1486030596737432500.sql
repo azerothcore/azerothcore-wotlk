67eeab01fc926be6728ed84884fbf38f4cfa561a
lee1991 <littlelee_1991@live.co.uk>
2017-02-02 15:26:38 +0000

    Fix creature error when loading creatures on start.

--- data/sql/updates/pending_db_world/rev_1486030596737432500.sql
+++ data/sql/updates/pending_db_world/rev_1486030596737432500.sql
@@ -5,10 +5,10 @@ DELETE FROM `creature_template` WHERE `entry` = "32341";
 DELETE FROM `creature_template` WHERE `entry` = "32321";
 DELETE FROM `creature_template` WHERE `entry` = "32324";
 
-UPDATE `creature` SET `spawndist`= "10" WHERE `guid` = "102313"; 
-UPDATE `creature` SET `spawndist`= "10" WHERE `guid` = "102123";
-UPDATE `creature` SET `spawndist`= "10" WHERE `guid` = "103579";
-UPDATE `creature` SET `spawndist`= "10" WHERE `guid` = "102063";
+UPDATE `creature` SET `spawndist`= "10", `MovementType` = "1" WHERE `guid` = "102313"; 
+UPDATE `creature` SET `spawndist`= "10", `MovementType` = "1" WHERE `guid` = "102123";
+UPDATE `creature` SET `spawndist`= "10",  `MovementType` = "1" WHERE `guid` = "103579";
+UPDATE `creature` SET `spawndist`= "10", `MovementType` = "1" WHERE `guid` = "102063";
 
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES 
 (32322, 0, 0, 0, 0, 0, 27916, 27917, 27918, 0, 'Gold Warrior', '', '', 0, 70, 70, 0, 11, 0, 1, 1.14286, 1, 0, 252, 357, 0, 304, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 0, 215, 320, 44, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 0, 0, 1, 0, 0, 'npc_dalaran_warrior', 12340),
