f6aded340d9130c85c60bce659f7da075884abc0
lee1991 <littlelee_1991@live.co.uk>
2017-02-03 11:04:54 +0000

    error was caused because of LOS, removed LOS for the spell.
    closes #213

---
+++ data/sql/updates/pending_db_world/rev_1486119784125250900.sql
@@ -0,0 +1,4 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1486119784125250900');
+
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(0, 54028, 64, '', '', 'Removed LOS for SPELL 54028');
