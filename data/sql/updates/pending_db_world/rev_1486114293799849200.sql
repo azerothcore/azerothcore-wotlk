c37b75ba10d08d52512dd1cdd8b65f80858480e4
lee1991 <littlelee_1991@live.co.uk>
2017-02-03 09:35:56 +0000

    Added Emote so when boss speaks she will do emote on that text.
    closes #364

---
+++ data/sql/updates/pending_db_world/rev_1486114293799849200.sql
@@ -0,0 +1,4 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1486114293799849200');
+
+UPDATE `creature_text` SET `emote` = "2" WHERE `entry` = "34928" AND `groupid` = "37";
+UPDATE `creature_text` SET `emote` = "398", `duration` = "3000" WHERE `entry` = "34928" AND `groupid` = "38";
