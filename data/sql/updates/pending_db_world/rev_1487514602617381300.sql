ddb15e478bd6425c79813d03645796f5091555f1
Lee <littlelee_1991@live.co.uk>
2017-02-19 14:54:34 +0000

    Update rev_1487514602617381300.sql

--- data/sql/updates/pending_db_world/rev_1487514602617381300.sql
+++ data/sql/updates/pending_db_world/rev_1487514602617381300.sql
@@ -1,3 +1,3 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1487514602617381300');
 -- Table `item_template`
-UPDATE `item_template` SET `spellcooldown_1` = 30000 WHERE `entry` IN (44010);+
+UPDATE `item_template` SET `spellcooldown_1` = 30000 WHERE `entry` IN (44010);
