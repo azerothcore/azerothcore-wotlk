a8e09d7c31d83ae59cdc6cd9cd69180115b1c110
starrheld <kevin@starrheld.co.uk>
2017-03-03 12:05:45 +0100

    [NPC] Lagoon Eel
    Currently swimming above water surface.

---
+++ data/sql/updates/pending_db_world/rev_1488539095792222900.sql
@@ -0,0 +1,5 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1488539095792222900');
+DELETE FROM `creature` WHERE `GUID` IN(72106);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES (72106, 20290, 530, 1, 1, 0, 0, -284.632, 6580.16, 15.1109, 4.78078, 300, 5, 0, 5158, 0, 1, 0, 0, 0);
+
+
