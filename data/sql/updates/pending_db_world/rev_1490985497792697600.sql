67cb6f02b1e262d2b0b769964d26c3ca9ac7d1df
Viste <viste02@gmail.com>
2017-03-31 22:04:33 +0300

    lines

--- data/sql/updates/pending_db_world/rev_1490985497792697600.sql
+++ data/sql/updates/pending_db_world/rev_1490985497792697600.sql
@@ -1,4 +1,5 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1490985497792697600');
+
 -- Crusader Lamoof SAI
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 28142);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
@@ -61,3 +62,4 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (28136, 0, 14, 15, 61, 0, 100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Crusader Jonathan - On Waypoint 5 Reached - Say Line 0 (No Repeat)'),
 (28136, 0, 15, 16, 61, 0, 100, 0, 0, 0, 0, 0, 53, 0, 28136, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Crusader Jonathan - On Waypoint 5 Reached - Start Waypoint (No Repeat)'),
 (28136, 0, 16, 0, 61, 0, 100, 0, 0, 0, 0, 0, 83, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Crusader Jonathan - On Waypoint 5 Reached - Remove Npc Flag Gossip (No Repeat)');
+
