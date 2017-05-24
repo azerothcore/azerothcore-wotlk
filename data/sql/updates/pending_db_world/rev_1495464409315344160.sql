a28785bccd2e15fe69dc6e2dc74e4552c3908fb3
PolarCookie <eilert009@gmail.com>
2017-05-24 23:28:09 +0200

    fixed `

--- data/sql/updates/pending_db_world/rev_1495464409315344160.sql
+++ data/sql/updates/pending_db_world/rev_1495464409315344160.sql
@@ -7,10 +7,10 @@ UPDATE `quest_template` SET `RequiredNpcOrGoCount1` = 14 where `ID` = 182;
 DELETE FROM `creature` WHERE `guid` IN (3137,3255,3261,3262,3548,3551,3554,3558,3583,3239,3243,3246,3257,3258,3552,3565,3566,3567,3570,3573,3574,3578);
 
 -- The Perfect Stout does not require Rejold's New Brew, it's the otherway around.
-UPDATE `quest_template_addon` SET `NextQuestId` = 0, `PrevQuestID` = 315 WHERE `ID` = 415;
+UPDATE `quest_template_addon` SET `NextQuestID` = 0, `PrevQuestID` = 315 WHERE `ID` = 415;
 
 -- All Tunnel Rats should be able to drop ears, not just 1 out of 6 types.
-INSERT INTO creature_loot_template VALUES
+INSERT INTO `creature_loot_template` VALUES
 (1172, 3110,-38,1,0,1,1),
 (1174, 3110,-80,1,0,1,1),
 (1175, 3110,-80,1,0,1,1),
@@ -18,37 +18,36 @@ INSERT INTO creature_loot_template VALUES
 (1177, 3110,-80,1,0,1,1)
 
 -- Red Silk Bandanas needs The Defias Brotherhood (6), not Red Leather Bandanas.
-UPDATE quest_template_addon SET PrevQuestID = 155 WHERE ID = 214;
+UPDATE `quest_template_addon` SET `PrevQuestID` = 155 WHERE `ID` = 214;
 
 -- Letter to Stormpike follows Encrypted Letter
-UPDATE quest_template_addon SET PrevQuestID = 511 WHERE ID = 514;
+UPDATE `quest_template_addon` SET `PrevQuestID` = 511 WHERE `ID` = 514;
 
 -- Kurzen Jungle Fighter also drops Jungle Remedy
-INSERT INTO creature_loot_template VALUES (937,2633,-33,1,0,1,1);
+INSERT INTO `creature_loot_template` VALUES (937,2633,-33,1,0,1,1);
 
 -- Inspecting the ruins requires either They Call Him Smilling Jim or James Hyal
-UPDATE quest_template_addon SET NextQuestID = 11123 WHERE ID = 1282;
-UPDATE quest_template_addon SET NextQuestID = 11123 WHERE ID = 1302;
-UPDATE quest_template_addon SET PrevQuestID =     0 WHERE ID = 11123;
+UPDATE `quest_template_addon` SET `NextQuestID` = 11123 WHERE `ID` = 1282;
+UPDATE `quest_template_addon` SET `NextQuestID` = 11123 WHERE `ID` = 1302;
+UPDATE `quest_template_addon` SET `PrevQuestID` =     0 WHERE `ID` = 11123;
 
 -- Raising the drop rate on Forked Mudrock Tongues
-UPDATE creature_loot_template SET ChanceOrQuestChance = -40 WHERE entry IN 4397 AND item = 5883;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -40 WHERE `entry` IN 4397 AND `item` = 5883;
 
 -- Trouble in Winterspring! is a breadcrumb quest, not prequest.
-UPDATE quest_template_addon SET PrevQuestID = 0 WHERE ID = 5082;
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` = 5082;
 
 -- Raising the drop rate of Thick Yeti Fur
-UPDATE creature_loot_template SET ChanceOrQuestChance = -71 WHERE entry = 7457 AND item = 12366;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -41 WHERE entry = 7458 AND item = 12366;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -43 WHERE entry = 7459 AND item = 12366;
-UPDATE creature_loot_template SET ChanceOrQuestChance = -45 WHERE entry = 7460 AND item = 12366;
-
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -71 WHERE `entry` = 7457 AND `item` = 12366;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -41 WHERE `entry` = 7458 AND `item` = 12366;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -43 WHERE `entry` = 7459 AND `item` = 12366;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -45 WHERE `entry` = 7460 AND `item` = 12366;
 
 -- Flesh Eating Worm adjustment
-UPDATE creature_template SET mindmg = 8, maxdmg = 12, scale=0.4 WHERE entry = 2462;
+UPDATE `creature_template` SET `mindmg` = 8, `maxdmg` = 12, `scale`=0.4 WHERE `entry` = 2462;
 
 -- Fool's stout follows Report Back to Fizzlebub
-UPDATE quest_template_addon SET PrevQuestID = 1122 WHERE ID = 1127;
+UPDATE `quest_template_addon` SET `PrevQuestID` = 1122 WHERE `ID` = 1127;
 
 -- Fixing Half-Buried Bottle loot table, it's not a raptor.
 DELETE FROM `gameobject_loot_template` WHERE `entry` = 2032;
@@ -106,37 +105,37 @@ INSERT INTO `gameobject_loot_template` VALUES
 (2032,11204,0.1,1,0,1,1);
 
 -- Atal'ai Artifacts (and 1 mithril deposit) are underground
-UPDATE gameobject SET position_z=-15.29  WHERE guid=30371;
-UPDATE gameobject SET position_z= 14.5   WHERE guid=30374;
-UPDATE gameobject SET position_z= 31.63  WHERE guid=30380;
-UPDATE gameobject SET position_z=-16.75  WHERE guid=30381;
-UPDATE gameobject SET position_z= 19.57  WHERE guid=30383;
-UPDATE gameobject SET position_z= 10.58  WHERE guid=30541;
-UPDATE gameobject SET position_z=-19     WHERE guid=30542;
-UPDATE gameobject SET position_z=-16.9   WHERE guid=30543;
-UPDATE gameobject SET position_z= 20.2   WHERE guid=30546;
-UPDATE gameobject SET position_z=-2.2    WHERE guid=30547;
-UPDATE gameobject SET position_z= 10.5   WHERE guid=30550;
-UPDATE gameobject SET position_z=-11.9   WHERE guid=30551;
-UPDATE gameobject SET position_z= 10.91  WHERE guid=30554;
-UPDATE gameobject SET position_z=-3.56   WHERE guid=30556;
-UPDATE gameobject SET position_z= 18.70  WHERE guid=30558;
-UPDATE gameobject SET position_z= 19.1   WHERE guid=30559;
-UPDATE gameobject SET position_z= 18.66  WHERE guid=30561;
-UPDATE gameobject SET position_z=-19     WHERE guid=30643;
-UPDATE gameobject SET position_z=-6      WHERE guid=30646;
-UPDATE gameobject SET position_z=-10.4   WHERE guid=30375;
-UPDATE gameobject SET position_z=-8.5    WHERE guid=30378;
-UPDATE gameobject SET position_z= 19     WHERE guid=31029;
+UPDATE `gameobject` SET `position_z`=-15.29  WHERE `guid`=30371;
+UPDATE `gameobject` SET `position_z`= 14.5   WHERE `guid`=30374;
+UPDATE `gameobject` SET `position_z`= 31.63  WHERE `guid`=30380;
+UPDATE `gameobject` SET `position_z`=-16.75  WHERE `guid`=30381;
+UPDATE `gameobject` SET `position_z`= 19.57  WHERE `guid`=30383;
+UPDATE `gameobject` SET `position_z`= 10.58  WHERE `guid`=30541;
+UPDATE `gameobject` SET `position_z`=-19     WHERE `guid`=30542;
+UPDATE `gameobject` SET `position_z`=-16.9   WHERE `guid`=30543;
+UPDATE `gameobject` SET `position_z`= 20.2   WHERE `guid`=30546;
+UPDATE `gameobject` SET `position_z`=-2.2    WHERE `guid`=30547;
+UPDATE `gameobject` SET `position_z`= 10.5   WHERE `guid`=30550;
+UPDATE `gameobject` SET `position_z`=-11.9   WHERE `guid`=30551;
+UPDATE `gameobject` SET `position_z`= 10.91  WHERE `guid`=30554;
+UPDATE `gameobject` SET `position_z`=-3.56   WHERE `guid`=30556;
+UPDATE `gameobject` SET `position_z`= 18.70  WHERE `guid`=30558;
+UPDATE `gameobject` SET `position_z`= 19.1   WHERE `guid`=30559;
+UPDATE `gameobject` SET `position_z`= 18.66  WHERE `guid`=30561;
+UPDATE `gameobject` SET `position_z`=-19     WHERE `guid`=30643;
+UPDATE `gameobject` SET `position_z`=-6      WHERE `guid`=30646;
+UPDATE `gameobject` SET `position_z`=-10.4   WHERE `guid`=30375;
+UPDATE `gameobject` SET `position_z`=-8.5    WHERE `guid`=30378;
+UPDATE `gameobject` SET `position_z`= 19     WHERE `guid`=31029;
 
 --Atal'ai Artifact twin spawn
-DELETE FROM gameobject WHERE guid=30593;
-DELETE FROM gameobject WHERE guid=30594;
-DELETE FROM gameobject WHERE guid=30587;
+DELETE FROM `gameobject` WHERE `guid`=30593;
+DELETE FROM `gameobject` WHERE `guid`=30594;
+DELETE FROM `gameobject` WHERE `guid`=30587;
 
 -- Blue Pearls do not drop from giant clams outside of the Vile Reef in STV.
-DELETE FROM gameobject_loot_template WHERE entry IN (2954,2959) and item = 4611;
+DELETE FROM `gameobject_loot_template` WHERE `entry` IN ( 2954, 2959) and `item` = 4611;
 -- Blue Pearls do not drop from Small Barnacle Clam or Thick Shell Clam items.
-DELETE FROM      spell_loot_template WHERE entry IN (58168,58172) and item = 4611;
+DELETE FROM      `spell_loot_template` WHERE `entry` IN (58168,58172) and `item` = 4611;
 
 
