34ed6aafa0d87c0d89d99c92e189e972a3f53cae
PolarCookie <eilert009@gmail.com>
2017-06-04 21:52:56 +0200

    fixes

--- data/sql/updates/pending_db_world/rev_1494246194005123844.sql
+++ data/sql/updates/pending_db_world/rev_1494246194005123844.sql
@@ -7,8 +7,7 @@ INSERT INTO `areatrigger_involvedrelation` VALUES (1667,1265);
 DELETE FROM `areatrigger_scripts` WHERE `id` = 302;
 INSERT INTO `areatrigger_scripts` VALUES (302,'at_sentry_point');
 
-UPDATE `creature_template` SET `ScriptName` = 'npc_archmage_tervosh' WHERE = 4967;
-
+UPDATE `creature_template` SET `ScriptName` = 'npc_archmage_tervosh' WHERE `entry` = 4967;
 
 DELETE FROM `creature_text` WHERE `entry` = 4967 AND `groupid` = 0 and `id` = 0;
-INSERT INTO `creature_text` VALUES (4967, 0, 0, 'Go with grace, and may the Lady\'s magic protect you.', 12, 0, 100, 0, 0, 0, 0, 0, 'SAY1');
+INSERT INTO `creature_text` VALUES (4967, 0, 0, 'Go with grace, and may the Lady\'s magic protect you.', 12, 0, 100, 0, 0, 0, 1751, 0, 'SAY1');
