06f42004e25813283594c34b6f9889f881498b3d
PolarCookie <eilert009@gmail.com>
2017-06-04 22:35:26 +0200

    fixes

--- data/sql/updates/pending_db_world/rev_1494246194005123844.sql
+++ data/sql/updates/pending_db_world/rev_1494246194005123844.sql
@@ -4,7 +4,7 @@ DELETE FROM `areatrigger_involvedrelation` WHERE `id` in (302,1667);
 INSERT INTO `areatrigger_involvedrelation` VALUES (1667,1265);
 
 
-DELETE FROM `areatrigger_scripts` WHERE `id` = 302;
+DELETE FROM `areatrigger_scripts` WHERE `entry` = 302;
 INSERT INTO `areatrigger_scripts` VALUES (302,'at_sentry_point');
 
 UPDATE `creature_template` SET `ScriptName` = 'npc_archmage_tervosh' WHERE `entry` = 4967;
