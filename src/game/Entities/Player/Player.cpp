bd799ecd5b064300cc198d3a7aa982a82c34e175
Inifield <Kaiosown@gmail.com>
2017-06-26 16:43:13 +0200

    Core/Player: Correct gender by modelid and avoid saving character with wrong gender
    Closes #396
    
    Author is MitchesD, Kittnz and Shauren

--- src/game/Entities/Player/Player.cpp
+++ src/game/Entities/Player/Player.cpp
@@ -25861,7 +25861,7 @@ void Player::_SaveCharacter(bool create, SQLTransaction& trans)
         stmt->setString(index++, GetName());
         stmt->setUInt8(index++, getRace(true));
         stmt->setUInt8(index++, getClass());
-        stmt->setUInt8(index++, getGender());
+        stmt->setUInt8(index++, GetByteValue(PLAYER_BYTES_3, 0));   // save gender from PLAYER_BYTES_3, UNIT_BYTES_0 changes with every transform effect
         stmt->setUInt8(index++, getLevel());
         stmt->setUInt32(index++, GetUInt32Value(PLAYER_XP));
         stmt->setUInt32(index++, GetMoney());
@@ -25966,7 +25966,7 @@ void Player::_SaveCharacter(bool create, SQLTransaction& trans)
         stmt->setString(index++, GetName());
         stmt->setUInt8(index++, getRace(true));
         stmt->setUInt8(index++, getClass());
-        stmt->setUInt8(index++, getGender());
+        stmt->setUInt8(index++, GetByteValue(PLAYER_BYTES_3, 0));   // save gender from PLAYER_BYTES_3, UNIT_BYTES_0 changes with every transform effect
         stmt->setUInt8(index++, getLevel());
         stmt->setUInt32(index++, GetUInt32Value(PLAYER_XP));
         stmt->setUInt32(index++, GetMoney());
