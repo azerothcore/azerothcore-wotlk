2bde13b3f7431f8d2b861e3d9c9097e912db0d67
Nefertumm <nefertumtg@gmail.com>
2017-06-30 19:03:36 -0300

    Codestyle

--- data/sql/updates/pending_db_auth/rev_1498796201292521600.sql
+++ data/sql/updates/pending_db_auth/rev_1498796201292521600.sql
@@ -2,22 +2,22 @@ INSERT INTO version_db_auth (`sql_rev`) VALUES ('1498796201292521600');
 
 ALTER TABLE `account` ADD COLUMN `lock_country` VARCHAR(2) NOT NULL DEFAULT '00' AFTER `locked`;
 
-DROP TABLE IF EXISTS ip2nation;
-CREATE TABLE ip2nation (
-  ip int(11) unsigned NOT NULL default '0',
-  country char(2) NOT NULL default '',
-  KEY ip (ip)
-);
+DROP TABLE IF EXISTS `ip2nation`;
+CREATE TABLE `ip2nation` (
+  `ip` int(11) unsigned NOT NULL default '0',
+  `country`char(2) NOT NULL default '',
+  KEY `ip` (`ip`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS ip2nationCountries;
-CREATE TABLE ip2nationCountries (
-  code varchar(4) NOT NULL default '',
-  iso_code_2 varchar(2) NOT NULL default '',
-  iso_code_3 varchar(3) default '',
-  iso_country varchar(255) NOT NULL default '',
-  country varchar(255) NOT NULL default '',
-  lat float NOT NULL default '0',
-  lon float NOT NULL default '0',  
-  PRIMARY KEY  (code),
-  KEY code (code)
-);
+DROP TABLE IF EXISTS `ip2nationCountries`;
+CREATE TABLE `ip2nationCountries` (
+  `code` varchar(4) NOT NULL default '',
+  `iso_code_2` varchar(2) NOT NULL default '',
+  `iso_code_3` varchar(3) default '',
+  `iso_country` varchar(255) NOT NULL default '',
+  `country` varchar(255) NOT NULL default '',
+  `lat` float NOT NULL default '0',
+  `lon` float NOT NULL default '0',  
+  PRIMARY KEY  (`code`),
+  KEY `code` (`code`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
