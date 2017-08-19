d388a36cf177a9936beb98151463e7b671ca6400
Kizarr <Kizarr@users.noreply.github.com>
2017-08-19 08:16:47 -0600

    [Database] Floating Corpse DK Starting/Scarlet Infantryman missing weapons (#539)
    * Scarlet Infantryman missing weapons.sql
    
    Fixes missing weapons from Scarlet Infantryman.
    
    *  DB: Instructor Razuvious - Event with Acherus Necromancer and Death Knight Initiate.sql
    
    *  DB: Instructor Razuvious - Event with Acherus Necromancer and Death Knight Initiate
    
    * Fixed path and added Header to file 1
    
    * Update path and add header to file 2
    
    * Update path and add header to file 3
    
    * Missing Header
    
    * Rename \data\sql\updates\pending_db_world\rev_1503139021197350000.sql to data/sql/updates/pending_db_world/rev_1503139021197350000.sql
    
    * Rename data\sql\updates\pending_db_world\rev_1503138647187080900.sql to data/sql/updates/pending_db_world/rev_1503138647187080900.sql
    
    * Rename data\sql\updates\pending_db_world\rev_1503138789369120900.sql to data/sql/updates/pending_db_world/rev_1503138789369120900.sql

---
+++ data/sql/updates/pending_db_world/rev_1503138789369120900.sql
@@ -0,0 +1,5 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1503138789369120900');
+
+-- Scarlet Infantryman
+UPDATE creature_equip_template SET ItemID1= 38721, ItemID2= 12932 WHERE CreatureID= 28609;
+UPDATE creature_equip_template SET ItemID1= 38723, ItemID2= 12932 WHERE CreatureID= 28896;
