14a8088f8624d92dae949193c6c1e31a455a0133
Yehonal <yehonal.azeroth@gmail.com>
2017-08-19 11:42:17 +0200

    import pending sql

--- data/sql/updates/db_world/2017_08_19_00.sql
+++ data/sql/updates/db_world/2017_08_19_00.sql
@@ -1,3 +1,18 @@
+-- DB update 2017_08_18_03 -> 2017_08_19_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2017_08_18_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2017_08_18_03 2017_08_19_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1503072683748672200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1503072683748672200');
 -- 'Clues in the Thicket' (Quest ID 9971) RewardText typo (Is it was/Is it what)
 UPDATE `quest_template`
@@ -11,4 +26,12 @@ UPDATE `quest_template`
 UPDATE `quest_template`
  SET `RequestItemsText` = "Has Teribus the Cursed been purged from the skies of Terokkar?"
  WHERE `ID` = 10923;
- 
+ --
+-- END UPDATING QUERIES
+--
+COMMIT;
+END;
+//
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
