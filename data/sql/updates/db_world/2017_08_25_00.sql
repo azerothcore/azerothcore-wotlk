7277e4f00697fd48cfc86b85bbed5607b803368c
Yehonal <yehonal.azeroth@gmail.com>
2017-08-25 11:49:51 +0200

    importing sql

--- data/sql/updates/db_world/2017_08_25_00.sql
+++ data/sql/updates/db_world/2017_08_25_00.sql
@@ -1,3 +1,18 @@
+-- DB update 2017_08_22_01 -> 2017_08_25_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2017_08_22_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2017_08_22_01 2017_08_25_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1503630648814585000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1503630648814585000');
 
 -- fix arrows and spawn script, must be started after talk
@@ -19,3 +34,12 @@ UPDATE `creature_text` SET `sound` = '10156' WHERE `entry` = '17461' AND `groupi
 UPDATE `creature_text` SET `sound` = '10157' WHERE `entry` = '17461' AND `groupid` = '1' AND `id` = '0'; 
 UPDATE `creature_text` SET `sound` = '10158' WHERE `entry` = '17461' AND `groupid` = '2' AND `id` = '0'; 
 UPDATE `creature_text` SET `sound` = '10159' WHERE `entry` = '17461' AND `groupid` = '3' AND `id` = '0'; 
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END;
+//
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
