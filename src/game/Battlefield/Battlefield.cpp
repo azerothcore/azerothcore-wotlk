317de4760fe8821489c55b6e1643e7ed9d850eb0
sucofog <4pdcvicente@gmail.com>
2017-09-10 15:30:10 +0200

    Fix warning under GCC Linux

--- src/game/Battlefield/Battlefield.cpp
+++ src/game/Battlefield/Battlefield.cpp
@@ -838,7 +838,7 @@ GameObject* Battlefield::SpawnGameObject(uint32 entry, float x, float y, float z
 // ******************* CapturePoint **********************
 // *******************************************************
 
-BfCapturePoint::BfCapturePoint(Battlefield* battlefield) : m_Bf(battlefield), m_capturePoint(NULL)
+BfCapturePoint::BfCapturePoint(Battlefield* battlefield) : m_Bf(battlefield), m_capturePoint(0)
 {
     m_team = TEAM_NEUTRAL;
     m_value = 0;
