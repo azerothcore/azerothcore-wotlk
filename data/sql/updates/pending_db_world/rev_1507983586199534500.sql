c3f97334ffe8102fb67ae3355a3e8f410b982ce2
Kevin Starrheld <kevin@starrheld.co.uk>
2017-10-15 17:07:10 +0100

    DB/Misc: Where Kings Walk & Warchief's Blessing
    SAI
    
    spaces
    
    spaces

--- data/sql/updates/pending_db_world/rev_1507983586199534500.sql
+++ data/sql/updates/pending_db_world/rev_1507983586199534500.sql
@@ -6,4 +6,4 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceEntry`,`ConditionType
 (19, 11585, 14, 10172, "Accept quest 11585 - Quest 10172 needs to be incomplete"),
 (19, 11586, 8, 10172, "Accept quest 11586 - Quest 10172 needs to be rewarded");
 
-UPDATE `quest_template_addon` SET `ExclusiveGroup`=11585 WHERE  `ID`IN(11585,11586);
+UPDATE `quest_template_addon` SET `ExclusiveGroup`=11585 WHERE `ID` IN (11585,11586);
