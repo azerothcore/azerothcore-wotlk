29141b9fdb4a7d665575e54ff324a58adc24fe65
Gargarensis <gabririchi@yahoo.it>
2017-11-01 18:32:00 +0100

    added scriptname sql

---
+++ data/sql/updates/pending_db_world/rev_1509557405527056200.sql
@@ -0,0 +1,17 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1509557405527056200');
+
+
+UPDATE creature_template SET ScriptName='npc_king_llane' WHERE entry=21684;
+UPDATE creature_template SET ScriptName='npc_warchief_blackhand' WHERE entry=21752;
+UPDATE creature_template SET ScriptName='npc_human_conjurer' WHERE entry=21683;
+UPDATE creature_template SET ScriptName='npc_orc_warlock' WHERE entry=21750;
+UPDATE creature_template SET ScriptName='npc_human_footman' WHERE entry=17211;
+UPDATE creature_template SET ScriptName='npc_orc_grunt' WHERE entry=17469;
+UPDATE creature_template SET ScriptName='npc_water_elemental' WHERE entry=21160;
+UPDATE creature_template SET ScriptName='npc_summoned_daemon' WHERE entry=21726;
+UPDATE creature_template SET ScriptName='npc_human_charger' WHERE entry=21664;
+UPDATE creature_template SET ScriptName='npc_orc_wolf' WHERE entry=21748;
+UPDATE creature_template SET ScriptName='npc_human_cleric' WHERE entry=21682;
+UPDATE creature_template SET ScriptName='npc_orc_necrolyte' WHERE entry=21747;
+UPDATE creature_template SET ScriptName='npc_chess_square' WHERE entry=17208;
+UPDATE creature_template SET ScriptName='npc_chess_square' WHERE entry=17316;
