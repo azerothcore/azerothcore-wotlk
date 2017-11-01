6fa236432e042faa1f61cb32cb58bfeb9012f356
Gargarensis <gabririchi@yahoo.it>
2017-11-01 16:24:48 +0100

    Initiali commit, full of bugs

--- src/scripts/EasternKingdoms/Karazhan/instance_karazhan.cpp
+++ src/scripts/EasternKingdoms/Karazhan/instance_karazhan.cpp
@@ -32,6 +32,21 @@ public:
             OperaEvent = urand(EVENT_OZ, EVENT_RAJ);
             OzDeathCount = 0;
             OptionalBossCount = 0;
+            m_uiTeam = 0;
+            m_uiChessResetTimer = 0;
+            m_uiAllianceStalkerCount = 0;
+            m_uiHordeStalkerCount = 0;
+        }
+
+        uint32 GetPlayerTeam() { return m_uiTeam; }
+        void GetChessPiecesByFaction(std::list<uint64>& lList, uint32 uiFaction) { lList = uiFaction == FACTION_ID_CHESS_ALLIANCE ? m_lChessPiecesAlliance : m_lChessPiecesHorde; };
+        Creature* GetCreatureByGUID(uint64 guid) { return instance->GetCreature(guid); };
+        
+        void OnPlayerEnter(Player* player)
+        {
+            // only once, taking the first player to enter
+            if (!m_uiTeam)
+                m_uiTeam = player->GetTeamId();
         }
 
         void OnCreatureCreate(Creature* creature) override
@@ -49,8 +64,44 @@ public:
                 break;
             case NPC_NIGHTBANE:
                 m_uiNightBaneGUID = creature->GetGUID();
+                break;
             case NPC_RELAY:
                 m_uiRelayGUID = creature->GetGUID();
+                break;
+            case NPC_ECHO_MEDIVH:
+                m_uiEchoOfMedivhGUID = creature->GetGUID();
+                break;
+            case NPC_CHESS_VICTORY_CONTROLLER:
+                m_uiChessVictoryControllerGUID = creature->GetGUID();
+                break;
+            case NPC_INVISIBLE_STALKER:
+                if (creature->GetPositionY() < -1870.0f)
+                    m_lChessHordeStalkerList.push_back(creature->GetGUID());
+                else
+                    m_lChessAllianceStalkerList.push_back(creature->GetGUID());
+                break;
+            case NPC_CHESS_STATUS_BAR:
+                if (creature->GetPositionY() < -1870.0f)
+                    m_HordeStatusGUID = creature->GetGUID();
+                else
+                    m_AllianceStatusGUID = creature->GetGUID();
+                break;
+            case NPC_HUMAN_CHARGER:
+            case NPC_HUMAN_CLERIC:
+            case NPC_HUMAN_CONJURER:
+            case NPC_HUMAN_FOOTMAN:
+            case NPC_CONJURED_WATER_ELEMENTAL:
+            case NPC_KING_LLANE:
+                m_lChessPiecesAlliance.push_back(creature->GetGUID());
+                break;
+            case NPC_ORC_GRUNT:
+            case NPC_ORC_NECROLYTE:
+            case NPC_ORC_WARLOCK:
+            case NPC_ORC_WOLF:
+            case NPC_SUMMONED_DAEMON:
+            case NPC_WARCHIEF_BLACKHAND:
+                m_lChessPiecesHorde.push_back(creature->GetGUID());
+                break;
             }
         }
 
@@ -105,6 +156,54 @@ public:
                 else if (data == IN_PROGRESS)
                     OzDeathCount = 0;
                 break;
+            case DATA_CHESS:
+                if (data == DONE)
+                {
+                    DoUseDoorOrButton(GO_GAMESMAN_HALL_EXIT_DOOR);
+                    DoRespawnGameObject(GO_DUST_COVERED_CHEST, DAY);
+                    instance->GetGameObject(GO_DUST_COVERED_CHEST)->SetFlag(GAMEOBJECT_DYNAMIC, GO_DYNFLAG_LO_NO_INTERACT);
+
+                    // cast game end spells
+                    if (Creature* medivh = instance->GetCreature(m_uiEchoOfMedivhGUID))
+                    {
+                        medivh->CastSpell(medivh, SPELL_FORCE_KILL_BUNNY, true);
+                        medivh->CastSpell(medivh, SPELL_GAME_OVER, true);
+                    }
+                    if (Creature* controller = instance->GetCreature(m_uiChessVictoryControllerGUID))
+                        controller->CastSpell(controller, SPELL_VICTORY_VISUAL, true);
+
+                    // remove silence debuff
+                    Map::PlayerList const& players = instance->GetPlayers();
+                    for (Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+                    {
+                        if (Player* pPlayer = itr->GetSource())
+                            pPlayer->RemoveAurasDueToSpell(SPELL_GAME_IN_SESSION);
+                    }
+
+                    /// TODO: reset board for friendly game!
+                }
+                else if (data == NOT_STARTED)
+                {
+                    // clean the board for reset
+                    if (Creature* pMedivh = instance->GetCreature(m_uiEchoOfMedivhGUID))
+                    {
+                        pMedivh->CastSpell(pMedivh, SPELL_GAME_OVER, true);
+                        pMedivh->CastSpell(pMedivh, SPELL_CLEAR_BOARD, true);
+                    }
+
+                    // remove silence debuff
+                    Map::PlayerList const& players = instance->GetPlayers();
+                    for (Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+                    {
+                        if (Player* player = itr->GetSource())
+                            player->RemoveAurasDueToSpell(SPELL_GAME_IN_SESSION);
+                    }
+
+                    m_uiChessResetTimer = 5000;
+                }
+                else if (data == IN_PROGRESS)
+                    DoPrepareChessEvent();
+                break;
             }
         }
 
@@ -122,7 +221,7 @@ public:
                     HandleGameObject(m_uiStageDoorRightGUID, true);
                     if (GameObject* sideEntrance = instance->GetGameObject(m_uiSideEntranceDoor))
                         sideEntrance->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_LOCKED);
-                  
+
                 }
                 break;
             case DATA_CHESS:
@@ -194,7 +293,7 @@ public:
                 DustCoveredChest = go->GetGUID();
                 break;
 
-              
+
             }
 
             switch (OperaEvent)
@@ -282,12 +381,202 @@ public:
             return 0;
         }
 
+        void DoPrepareChessEvent()
+        {
+            // Allow all the chess pieces to init start position
+            for (std::list<uint64>::const_iterator itr = m_lChessPiecesAlliance.begin(); itr != m_lChessPiecesAlliance.end(); ++itr)
+            {
+                if (Creature* chessPiece = instance->GetCreature(*itr))
+                {
+                    Creature* square = GetClosestCreatureWithEntry(chessPiece, NPC_SQUARE_BLACK, 2.0f);
+                    if (!square)
+                        square = GetClosestCreatureWithEntry(chessPiece, NPC_SQUARE_WHITE, 2.0f);
+                    if (!square)
+                    {
+                        sLog->outError("Instance Karazhan: ERROR Failed to properly load the Chess square for %llu.", chessPiece->GetGUID());
+                        return;
+                    }
+
+                    // send event which will prepare the current square
+                    //chessPiece->AI()->SendAIEvent(AI_EVENT_CUSTOM_B, square, chessPiece); NOT SURE! 
+                    /// TODO: CHECK IF WE MUST PASS SQUARE GUID OR CHESSPIECE GUID
+                    chessPiece->AI()->SetData(AI_EVENT_CUSTOM_B, square->GetGUID());
+                }
+            }
+
+            for (std::list<uint64>::const_iterator itr = m_lChessPiecesHorde.begin(); itr != m_lChessPiecesHorde.end(); ++itr)
+            {
+                if (Creature* chessPiece = instance->GetCreature(*itr))
+                {
+                    Creature* square = GetClosestCreatureWithEntry(chessPiece, NPC_SQUARE_BLACK, 2.0f);
+                    if (!square)
+                        square = GetClosestCreatureWithEntry(chessPiece, NPC_SQUARE_WHITE, 2.0f);
+                    if (!square)
+                    {
+                        sLog->outError("Instance Karazhan: ERROR Failed to properly load the Chess square for %llu.", chessPiece->GetGUID());
+                        return;
+                    }
+
+                    // send event which will prepare the current square
+                    //chessPiece->AI()->SendAIEvent(AI_EVENT_CUSTOM_B, square, chessPiece); NOT SURE! 
+                    chessPiece->AI()->SetData(AI_EVENT_CUSTOM_B, square->GetGUID());
+                }
+            }
+
+            // add silence debuff
+            Map::PlayerList const& players = instance->GetPlayers();
+            for (Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+            {
+                if (Player* player = itr->GetSource())
+                    player->CastSpell(player, SPELL_GAME_IN_SESSION, true);
+            }
+
+            m_uiAllianceStalkerCount = 0;
+            m_uiHordeStalkerCount = 0;
+            m_vHordeStalkers.clear();
+            m_vAllianceStalkers.clear();
+
+            // sort stalkers depending on side
+            std::list<Creature*> lStalkers;
+            for (std::list<uint64>::const_iterator itr = m_lChessHordeStalkerList.begin(); itr != m_lChessHordeStalkerList.end(); ++itr)
+            {
+                if (Creature* pTemp = instance->GetCreature(*itr))
+                    lStalkers.push_back(pTemp);
+            }
+
+            if (lStalkers.empty())
+            {
+                sLog->outError("Instance Karazhan: ERROR Failed to properly load the horde side stalkers for the Chess Event.");
+                return;
+            }
+
+            // get the proper statusBar npc
+            Creature* statusBar = instance->GetCreature(m_HordeStatusGUID);
+            if (!statusBar)
+                return;
+
+            /// TODO: could not find this sort function
+            //lStalkers.sort(ObjectDistanceOrder(statusBar));
+            for (std::list<Creature*>::const_iterator itr = lStalkers.begin(); itr != lStalkers.end(); ++itr)
+                m_vHordeStalkers.push_back((*itr)->GetGUID()); // getObjectGuid ? NOT SURE
+
+            lStalkers.clear();
+            for (std::list<uint64>::const_iterator itr = m_lChessAllianceStalkerList.begin(); itr != m_lChessAllianceStalkerList.end(); ++itr)
+            {
+                if (Creature* pTemp = instance->GetCreature(*itr))
+                    lStalkers.push_back(pTemp);
+            }
+
+            if (lStalkers.empty())
+            {
+                sLog->outError("Instance Karazhan: ERROR Failed to properly load the alliance side stalkers for the Chess Event.");
+                return;
+            }
+
+            // get the proper statusBar npc
+            statusBar = instance->GetCreature(m_AllianceStatusGUID);
+            if (!statusBar)
+                return;
+
+            /// TODO: could not find this sort function
+            //lStalkers.sort(ObjectDistanceOrder(statusBar));
+            for (std::list<Creature*>::const_iterator itr = lStalkers.begin(); itr != lStalkers.end(); ++itr)
+                m_vAllianceStalkers.push_back((*itr)->GetGUID()); // getObjectGuid ? NOT SURE
+        }
+
+        void DoMoveChessPieceToSides(uint32 uiSpellId, uint32 uiFaction, bool bGameEnd = false)
+        {
+            // assign proper faction variables
+            std::vector<uint64>& vStalkers = uiFaction == FACTION_ID_CHESS_ALLIANCE ? m_vAllianceStalkers : m_vHordeStalkers;
+            uint32 uiCount = uiFaction == FACTION_ID_CHESS_ALLIANCE ? m_uiAllianceStalkerCount : m_uiHordeStalkerCount;
+
+            // get the proper statusBar npc
+            Creature* pStatusBar = instance->GetCreature(uiFaction == FACTION_ID_CHESS_ALLIANCE ? m_AllianceStatusGUID : m_HordeStatusGUID);
+            if (!pStatusBar)
+                return;
+
+            if (vStalkers.size() < uiCount + 1)
+                return;
+
+            // handle stalker transformation
+            if (Creature* pStalker = instance->GetCreature(vStalkers[uiCount]))
+            {
+                // need to provide specific target, in order to ensure the logic of the event
+                pStatusBar->CastSpell(pStalker, uiSpellId, true);
+                uiFaction == FACTION_ID_CHESS_ALLIANCE ? ++m_uiAllianceStalkerCount : ++m_uiHordeStalkerCount;
+            }
+
+            // handle emote on end game
+            if (bGameEnd)
+            {
+                // inverse factions
+                vStalkers.clear();
+                vStalkers = uiFaction == FACTION_ID_CHESS_ALLIANCE ? m_vHordeStalkers : m_vAllianceStalkers;
+
+                for (std::vector<uint64>::const_iterator itr = vStalkers.begin(); itr != vStalkers.end(); ++itr)
+                {
+                    if (Creature* pStalker = instance->GetCreature(*itr))
+                        pStalker->HandleEmoteCommand(EMOTE_STATE_APPLAUD);
+                }
+            }
+        }
+
+        /// TODO: rework with events?
+        void Update(uint32 uiDiff) override
+        {
+            if (m_uiChessResetTimer)
+            {
+                // respawn all chess pieces and side stalkers on the original position
+                if (m_uiChessResetTimer <= uiDiff)
+                {
+                    for (std::list<uint64>::const_iterator itr = m_lChessPiecesAlliance.begin(); itr != m_lChessPiecesAlliance.end(); ++itr)
+                    {
+                        if (Creature* pTemp = instance->GetCreature(*itr))
+                            pTemp->Respawn();
+                    }
+                    for (std::list<uint64>::const_iterator itr = m_lChessPiecesHorde.begin(); itr != m_lChessPiecesHorde.end(); ++itr)
+                    {
+                        if (Creature* pTemp = instance->GetCreature(*itr))
+                            pTemp->Respawn();
+                    }
+
+                    for (std::list<uint64>::const_iterator itr = m_lChessAllianceStalkerList.begin(); itr != m_lChessAllianceStalkerList.end(); ++itr)
+                    {
+                        if (Creature* pTemp = instance->GetCreature(*itr))
+                        {
+                            pTemp->Respawn();
+                            pTemp->HandleEmoteCommand(EMOTE_STATE_NONE);
+                        }
+                    }
+                    for (std::list<uint64>::const_iterator itr = m_lChessHordeStalkerList.begin(); itr != m_lChessHordeStalkerList.end(); ++itr)
+                    {
+                        if (Creature* pTemp = instance->GetCreature(*itr))
+                        {
+                            pTemp->Respawn();
+                            pTemp->HandleEmoteCommand(EMOTE_STATE_NONE);
+                        }
+                    }
+
+                    m_uiChessResetTimer = 0;
+                }
+                else
+                    m_uiChessResetTimer -= uiDiff;
+            }
+        }
+
+
+
     private:
         uint32 OperaEvent;
         uint32 OzDeathCount;
         uint32 OptionalBossCount;
         //uint32 m_auiEncounter[MAX_ENCOUNTERS];
-        //uint32 m_uiTeam;
+        uint32 m_uiTeam;
+        uint32 m_uiChessResetTimer;
+        uint32 m_uiAllianceStalkerCount;
+        uint32 m_uiHordeStalkerCount;
+        uint64 m_HordeStatusGUID;
+        uint64 m_AllianceStatusGUID;
         uint64 m_uiCurtainGUID;
         uint64 m_uiStageDoorLeftGUID;
         uint64 m_uiStageDoorRightGUID;
@@ -295,18 +584,26 @@ public:
         uint64 m_uiTerestianGUID;
         uint64 m_uiMoroesGUID;
         uint64 m_uiNightBaneGUID;
-        //uint64 EchoOfMedivhGUID;
+        uint64 m_uiEchoOfMedivhGUID;
+        uint64 m_uiChessVictoryControllerGUID;
         uint64 m_uiLibraryDoor;                                     // Door at Shade of Aran
         uint64 m_uiMassiveDoor;                                     // Door at Netherspite
         uint64 m_uiSideEntranceDoor;                                // Side Entrance
         uint64 m_uiGamesmansDoor;                                   // Door before Chess
         uint64 m_uiGamesmansExitDoor;                               // Door after Chess
         uint64 m_uiNetherspaceDoor;                                // Door at Malchezaar
-        //uint64 m_uiServantsAccessDoor;                              // Door to Brocken Stair
+        //uint64 m_uiServantsAccessDoor;                           // Door to Broken Stair
         uint64 MastersTerraceDoor[2];
         uint64 ImageGUID;
         uint64 DustCoveredChest;
         uint64 m_uiRelayGUID;
+
+        std::list<uint64> m_lChessHordeStalkerList;
+        std::list<uint64> m_lChessAllianceStalkerList;
+        std::list<uint64> m_lChessPiecesAlliance;
+        std::list<uint64> m_lChessPiecesHorde;
+        std::vector<uint64> m_vHordeStalkers;
+        std::vector<uint64> m_vAllianceStalkers;
     };
 };
 
