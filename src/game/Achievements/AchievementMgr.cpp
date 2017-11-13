df10a1efe28827caf5f5e9e1b3ee2973fd686558
Yehonal <yehonal.azeroth@gmail.com>
2017-11-13 11:31:35 +0100

    Fixed other warnings

--- src/game/Achievements/AchievementMgr.cpp
+++ src/game/Achievements/AchievementMgr.cpp
@@ -1281,9 +1281,6 @@ void AchievementMgr::UpdateAchievementCriteria(AchievementCriteriaTypes type, ui
                     if (!area)
                         break;
 
-                    if (area->exploreFlag < 0)
-                        continue;
-
                     uint32 playerIndexOffset = uint32(area->exploreFlag) / 32;
                     if (playerIndexOffset >= PLAYER_EXPLORED_ZONES_SIZE)
                         continue;
