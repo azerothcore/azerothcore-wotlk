546c547e7bb8f1af3c36a372a8699e0e1b9a7159
talamortis <talamortis@gmail.com>
2017-11-20 13:51:48 +0000

    Bloodworms will now be defensive state when summoned.
    closes #641

--- src/game/Entities/Pet/Pet.cpp
+++ src/game/Entities/Pet/Pet.cpp
@@ -960,6 +960,7 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                     SetCreateHealth(4 * petlevel);
                     SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel - 30 - (petlevel / 4) + m_owner->GetTotalAttackPowerValue(BASE_ATTACK) * 0.006f));
                     SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel - 30 + (petlevel / 4) + m_owner->GetTotalAttackPowerValue(BASE_ATTACK) * 0.006f));
+                    SetReactState(REACT_DEFENSIVE);
                     break;
                 }
                 case NPC_ARMY_OF_THE_DEAD:
