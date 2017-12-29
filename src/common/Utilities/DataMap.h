b5d56761b2a36fbbfcb3b25d7f49df0a9ecc040c
Rochet2 <rochet2@post.com>
2017-12-29 23:51:32 +0200

    Remove TC header copy paste and dll code

--- src/common/Utilities/DataMap.h
+++ src/common/Utilities/DataMap.h
@@ -1,29 +1,15 @@
 /*
- * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: http://github.com/azerothcore/azerothcore-wotlk/LICENSE-GPL2
  */
 
 #ifndef _DATA_MAP_H_
 #define _DATA_MAP_H_
 
-#include "Define.h"
 #include <string>
 #include <unordered_map>
 #include <memory>
 
-class TC_COMMON_API DataMap
+class DataMap
 {
 public:
     class Base
