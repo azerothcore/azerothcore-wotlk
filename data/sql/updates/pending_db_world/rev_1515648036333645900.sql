5bf932aa1c24ceafd34d69a795740a74e98a5fef
Nefertumm <nefertumtg@gmail.com>
2018-01-11 05:25:14 -0300

    Added some Syntax.

--- data/sql/updates/pending_db_world/rev_1515648036333645900.sql
+++ data/sql/updates/pending_db_world/rev_1515648036333645900.sql
@@ -2,4 +2,4 @@ INSERT INTO version_db_world (`sql_rev`) VALUES ('1515648036333645900');
 
 /* Create the command. */
 DELETE FROM `command` WHERE `name` = "mutehistory";
-INSERT INTO `command` (`name`,`security`,`help`) VALUES ('mutehistory', 2, "");
+INSERT INTO `command` (`name`,`security`,`help`) VALUES ('mutehistory', 2, "Syntax: .mutehistory $accountName. Shows mute history for an account.");
