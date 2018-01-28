75ca1afe8564726d8fc5b81a60d872f6597eeeb3
Travis CI <travis@build.bot>
2018-01-28 18:58:23 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1516785669788049600.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1516785669788049600');
-
--- Correct Ashbringer show on Tirion during DK campaign
-UPDATE `creature_equip_template` SET `ItemID1` = 13262 WHERE `CreatureID` = 29175 AND `ID` = 1;
