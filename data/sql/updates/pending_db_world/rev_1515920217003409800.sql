7e079945da3ce3363bf6624a51cc8808ae729d28
Travis CI <travis@build.bot>
2018-01-28 19:28:55 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1515920217003409800.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1515920217003409800');
-
-DELETE FROM `command` WHERE `name` = 'guild info';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('guild info', 2, 'Shows information about the target''s guild or a given Guild Id or Name.');
