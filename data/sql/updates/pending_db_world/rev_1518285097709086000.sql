6c4911f8f27fd2618203cad8c783ab5048d8cc45
Evilpriest4 <evilpriest.lightwell@gmail.com>
2018-02-10 19:31:39 +0100

    Update rev_1518285097709086000.sql

--- data/sql/updates/pending_db_world/rev_1518285097709086000.sql
+++ data/sql/updates/pending_db_world/rev_1518285097709086000.sql
@@ -3,5 +3,5 @@ INSERT INTO version_db_world (`sql_rev`) VALUES ('1518285097709086000');
 -- Evilpriest #0001
 -- Removed MECHANIC_INTERRUPT flag from Lady Malande/High Nethermancer Zerevor (Black Temple) 
 
-UPDATE `creature_template` SET `mechanic_immune_mask` = '617299839' WHERE (`entry`='22951')
+UPDATE `creature_template` SET `mechanic_immune_mask` = '617299839' WHERE (`entry`='22951');
 UPDATE `creature_template` SET `mechanic_immune_mask` = '617299839' WHERE (`entry`='22950')
