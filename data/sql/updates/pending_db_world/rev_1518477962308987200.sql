b3888f07920e8d7a641208a4e8d2491f1cf6b8f3
Evilpriest4 <evilpriest.lightwell@gmail.com>
2018-02-13 01:04:40 +0100

    Heart-Shaped Box

---
+++ data/sql/updates/pending_db_world/rev_1518477962308987200.sql
@@ -0,0 +1,7 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1518477962308987200');
+
+--Evilpriest #0003
+--Added EoF from Heart-Shaped Box
+
+INSERT INTO `item_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES
+('54537','49426','100','1','0','2','2')
