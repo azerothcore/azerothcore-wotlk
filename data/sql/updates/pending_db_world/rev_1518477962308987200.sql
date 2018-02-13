9152c2c38bd9bd6abb53fc1281748b9dc287b665
Evilpriest4 <evilpriest.lightwell@gmail.com>
2018-02-13 14:37:14 +0100

    Update rev_1518477962308987200.sql
    Added the ; and the delete.

--- data/sql/updates/pending_db_world/rev_1518477962308987200.sql
+++ data/sql/updates/pending_db_world/rev_1518477962308987200.sql
@@ -3,5 +3,6 @@ INSERT INTO version_db_world (`sql_rev`) VALUES ('1518477962308987200');
 #--Evilpriest #0003
 #--Added EoF from Heart-Shaped Box
 
+DELETE FROM `item_loot_template` WHERE (`entry` = 54537 AND `item` = 49426);
 INSERT INTO `item_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES
-('54537','49426','100','1','0','2','2')
+('54537','49426','100','1','0','2','2');
