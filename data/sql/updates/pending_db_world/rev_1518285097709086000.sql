520936be60c59d2a06cee7afb361d702c436d088
Travis CI <travis@build.bot>
2018-02-23 11:56:33 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1518285097709086000.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1518285097709086000');
-
--- Evilpriest #0001
--- Removed MECHANIC_INTERRUPT flag from Lady Malande/High Nethermancer Zerevor (Black Temple) 
-
-UPDATE `creature_template` SET `mechanic_immune_mask` = '617299839' WHERE (`entry`='22951');
-UPDATE `creature_template` SET `mechanic_immune_mask` = '617299839' WHERE (`entry`='22950')
