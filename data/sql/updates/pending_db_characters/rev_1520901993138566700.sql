4ba4f7560f093d111eb186661dedff1eeb3399c1
zeb139 <casey.parshall@gmail.com>
2018-03-12 20:50:57 -0400

    Added Apprentice Riding (33388) spell to Death Knight playercreateinfo_spells

---
+++ data/sql/updates/pending_db_characters/rev_1520901993138566700.sql
@@ -0,0 +1,2 @@
+DELETE FROM `characters`.`playercreateinfo_spell` where `Note`='Apprentice Riding';
+INSERT INTO `characters`.`playercreateinfo_spell` (`racemask`, `classmask`, `Spell`, `Note`) VALUES (0, 32, 33388, 'Apprentice Riding');
---
+++ data/sql/updates/pending_db_world/rev_1520902066724046200.sql
@@ -0,0 +1,2 @@
+DELETE FROM `world`.`playercreateinfo_spell` where `Note`='Apprentice Riding';
+INSERT INTO `world`.`playercreateinfo_spell` (`racemask`, `classmask`, `Spell`, `Note`) VALUES (0, 32, 33388, 'Apprentice Riding');
