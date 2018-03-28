6d158f1e56efffc0e7e68daf53490d735be7fc11
BarbzYHOOL <yhool_server@gmx.com>
2018-03-28 21:50:24 +0200

    Wrong database chosen...

--- data/sql/updates/pending_db_world/rev_1522263653566045620.sql
+++ data/sql/updates/pending_db_world/rev_1522263653566045620.sql
@@ -1,3 +1,3 @@
-INSERT INTO version_db_auth(sql_rev) VALUES ('1522263653566045620');
+INSERT INTO version_db_world(sql_rev) VALUES ('1522263653566045620');
 
 DELETE FROM `areatrigger` WHERE map IN (24,28);
