d914be6e63b9cfb05471fed32ef9a3497a0452ba
Yehonal <yehonal.azeroth@gmail.com>
2018-04-09 08:40:05 +0000

    missing instance_template scripts

---
+++ data/sql/updates/pending_db_world/rev_1523263107129022148.sql
@@ -0,0 +1,6 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1523263107129022148');
+
+REPLACE INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (546, 530, 'instance_the_underbog', 0);
+REPLACE INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (547, 530, 'instance_the_slave_pens', 0);
+REPLACE INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (557, 530, 'instance_mana_tombs', 0);
+REPLACE INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (558, 530, 'instance_auchenai_crypts', 0);
