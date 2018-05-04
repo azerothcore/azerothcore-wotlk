b47f5a859acf33f045101e2d2062109b26509835
Ayase <137056643@qq.com>
2018-05-04 23:06:04 +0800

    some change

--- src/common/Database/QueryResult.cpp
+++ src/common/Database/QueryResult.cpp
@@ -185,11 +185,13 @@ bool PreparedResultSet::_NextRow()
     return retval;
 }
 
+#ifdef ELUNA
 char* ResultSet::GetFieldName(uint32 index) const
 {
     ASSERT(index < _fieldCount);
     return _fields[index].name;
 }
+#endif
 
 void ResultSet::CleanUp()
 {
