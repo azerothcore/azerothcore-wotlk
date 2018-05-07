86a9decf4c8b4d4d85b73c0e69041f4dfd0c090e
Travis CI <travis@build.bot>
2018-05-07 14:21:28 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1525438412721008951.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1525438412721008951');
-
-UPDATE `creature_template` SET `scriptname` = 'npc_the_etymidian' WHERE `entry` = 28092;
-UPDATE `creature_template` SET `scriptname` = '' WHERE `entry` = 28033;
