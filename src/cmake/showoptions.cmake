62f5e01b3025576b0448f050f16ccfde075358c7
Ayase <137056643@qq.com>
2018-05-15 23:10:36 +0800

    Remove the eluna switch.

--- src/cmake/showoptions.cmake
+++ src/cmake/showoptions.cmake
@@ -33,13 +33,6 @@ else()
   message("* Build with scripts     : No")
 endif()
 
-if( ELUNA )
-  message("* Build Eluna LuaEngine  : Yes")
-  add_definitions(-DELUNA)
-else()
-  message("* Build Eluna LuaEngine  : No (default)")
-endif()
-
 if( TOOLS )
   message("* Build map/vmap tools   : Yes")
   add_definitions(-DNO_CORE_FUNCS)
