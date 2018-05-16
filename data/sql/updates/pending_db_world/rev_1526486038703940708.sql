5c31088d03ba814c9271b1428b3886aec08d8243
Cillez <ole@dybing.no>
2018-05-16 19:03:21 +0200

    DB - Deadmines - Add missing waypathes for some patrols
    Cleaner SQL

--- data/sql/updates/pending_db_world/rev_1526486038703940708.sql
+++ data/sql/updates/pending_db_world/rev_1526486038703940708.sql
@@ -1,13 +1,15 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1526486038703940708');
 
--- Add missing waypoints on patrols before the Ogre boss
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 1, -158.903, -399.895, 56.3324, 0, 0, 0, 0, 100, 214525);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 2, -151.984, -402.043, 56.9471, 0, 0, 0, 0, 100, 214526);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 3, -144.297, -403.96, 57.6147, 0, 0, 0, 0, 100, 214527);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 4, -154.368, -401.496, 56.7647, 0, 0, 0, 0, 100, 214528);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 5, -161.632, -400.326, 56.6994, 0, 0, 0, 0, 100, 214529);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 6, -167.493, -401.874, 57.0191, 0, 0, 0, 0, 100, 214530);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 7, -170.658, -405.778, 57.1812, 0, 0, 0, 0, 100, 214531);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 8, -179.541, -417.187, 55.0673, 0, 0, 0, 0, 100, 214532);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 9, -172.693, -407.324, 56.4777, 0, 0, 0, 0, 100, 214533);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES (793730, 10, -169.554, -402.835, 57.1019, 0, 0, 0, 0, 100, 214534);
+-- Add missing waypoints on patrolling mobs before the Ogre boss
+INSERT INTO waypoint_data (id, point, position_x, position_y, position_z, orientation, delay, move_type, action, action_chance, wpguid) VALUES 
+(793730, 1, -158.903, -399.895, 56.3324, 0, 0, 0, 0, 100, 214525),
+(793730, 2, -151.984, -402.043, 56.9471, 0, 0, 0, 0, 100, 214526),
+(793730, 3, -144.297, -403.96, 57.6147, 0, 0, 0, 0, 100, 214527),
+(793730, 4, -154.368, -401.496, 56.7647, 0, 0, 0, 0, 100, 214528),
+(793730, 5, -161.632, -400.326, 56.6994, 0, 0, 0, 0, 100, 214529),
+(793730, 6, -167.493, -401.874, 57.0191, 0, 0, 0, 0, 100, 214530),
+(793730, 7, -170.658, -405.778, 57.1812, 0, 0, 0, 0, 100, 214531),
+(793730, 8, -179.541, -417.187, 55.0673, 0, 0, 0, 0, 100, 214532),
+(793730, 9, -172.693, -407.324, 56.4777, 0, 0, 0, 0, 100, 214533),
+(793730, 10, -169.554, -402.835, 57.1019, 0, 0, 0, 0, 100, 214534);
+
