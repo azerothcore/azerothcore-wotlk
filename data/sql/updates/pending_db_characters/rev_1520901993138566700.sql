16e5b8f34d3e664d460b2a74fcc7553dc2eff60d
Lee <littlelee_1991@live.co.uk>
2018-05-20 01:43:14 +0000

    Delete rev_1520901993138566700.sql
    Delete rev_1520901993138566700.sql  from character database due to causing travis to fail.

--- data/sql/updates/pending_db_characters/rev_1520901993138566700.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `playercreateinfo_spell` where `Note`='Apprentice Riding';
-INSERT INTO `playercreateinfo_spell` (`racemask`, `classmask`, `Spell`, `Note`) VALUES (0, 32, 33388, 'Apprentice Riding');
