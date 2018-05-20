c5101a6360bbe68f43cf1048ea022d3653fe08fe
Lee <littlelee_1991@live.co.uk>
2018-05-20 01:27:59 +0000

    Fixed Travis

--- data/sql/updates/pending_db_characters/rev_1520901993138566700.sql
+++ data/sql/updates/pending_db_characters/rev_1520901993138566700.sql
@@ -1,2 +1,2 @@
-DELETE FROM `characters`.`playercreateinfo_spell` where `Note`='Apprentice Riding';
-INSERT INTO `characters`.`playercreateinfo_spell` (`racemask`, `classmask`, `Spell`, `Note`) VALUES (0, 32, 33388, 'Apprentice Riding');
+DELETE FROM `playercreateinfo_spell` where `Note`='Apprentice Riding';
+INSERT INTO `playercreateinfo_spell` (`racemask`, `classmask`, `Spell`, `Note`) VALUES (0, 32, 33388, 'Apprentice Riding');
