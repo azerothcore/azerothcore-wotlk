968aaddd199956a9d07e3f8dfcc4b64c7fe45679
Travis CI <travis@build.bot>
2018-05-24 21:39:06 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1526717821898752673.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1526717821898752673');
-
-UPDATE `quest_template_addon` SET `RewardMailTemplateID` = 236 WHERE `id` = 12711;
-
-DELETE FROM `mail_loot_template` WHERE `entry` = 236;
-INSERT INTO `mail_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES (236,39317,100,1,0,1,1);
