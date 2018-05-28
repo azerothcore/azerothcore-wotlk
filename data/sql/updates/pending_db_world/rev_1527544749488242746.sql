63cd545d9e5849a61e8d78c617694da4448aed19
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2018-05-28 21:59:54 +0000

    Fix world server crash when using the Atal'ai Statue (ID 148833) in Sunken Temple

---
+++ data/sql/updates/pending_db_world/rev_1527544749488242746.sql
@@ -0,0 +1,3 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1527544749488242746');
+
+UPDATE `smart_scripts` SET `target_type` = 7 WHERE `entryorguid` IN (148830,148831,148832,148833,148834,148835) AND `id` = 3 AND `action_type` = 11;
