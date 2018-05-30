7755fc16d0f105effeff67aa20f6c66b23e160e0
Nefertumm <nefertumtg@gmail.com>
2018-05-30 16:34:02 -0300

    Fix creature_text stuff

--- data/sql/updates/pending_db_world/rev_1503249253226086300.sql
+++ data/sql/updates/pending_db_world/rev_1503249253226086300.sql
@@ -1,8 +1,8 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1503249253226086300');
 
 -- Auriaya emote and say
-DELETE FROM `creature_text` WHERE `CreatureID`=33515;
-INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Probability`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
+DELETE FROM `creature_text` WHERE `entry`=33515;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`probability`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
 (33515, 0, 0, 'Some things are better left alone!', 14, 100, 34341, 15473, 3, 'Auriaya SAY_AGGRO'),
 (33515, 1, 1, 'The secret dies with you.', 14, 100, 34354, 15474, 3, 'Auriaya SAY_SLAY_1'),
 (33515, 1, 2, 'There is no escape!', 14, 100, 34355, 15475, 3, 'Auriaya SAY_SLAY_2'),
@@ -12,6 +12,6 @@ INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Probabil
 (33515, 5, 0, '%s begins to activate the Feral Defender!', 41, 100, 0, 34162, 3, 'Auriaya EMOTE_DEFENDER');
 
 -- Locales text
-DELETE FROM `creature_text_locale` WHERE `CreatureID`=33515;
-INSERT INTO `creature_text_locale` (`CreatureID`,`GroupID`,`ID`,`Locale`,`Text`) VALUES
-(33515, 2, 0, 'ruRU','Ауриайя кричит в агонии.');
+DELETE FROM `locales_creature_text` WHERE `entry`=33515;
+INSERT INTO `locales_creature_text` (`entry`,`groupid`,`id`,`text_loc8`) VALUES
+(33515, 2, 0, 'Ауриайя кричит в агонии.');
