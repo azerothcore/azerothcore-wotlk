75f36675ce3c71e4b489dc99dae8648311f04694
José González <Deku@users.noreply.github.com>
2018-05-31 18:19:52 -0400

    Change to UPDATE command to be compliant with AC's guidelines

--- data/sql/updates/pending_db_world/rev_1527682153565076000.sql
+++ data/sql/updates/pending_db_world/rev_1527682153565076000.sql
@@ -1,6 +1,11 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1527682153565076000');
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 177385;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(177385,1,0,1,70,0,100,0,2,0,0,0,9,0,0,0,0,0,0,14,43178,175610,0,0,0,0,0,'Torch - On Gossip Hello - Set Gameobject State'),
-(177385,1,1,0,61,0,100,0,0,0,0,0,106,16,0,0,0,0,0,20,176944,200,0,0,0,0,0,'Torch - On Gossip Hello - Remove Gameobject Flag');
+UPDATE `smart_scripts` SET
+`event_type` = 70,
+`event_param1` = 2,
+`action_type` = 9,
+`action_param1` = 0,
+`target_type` = 14,
+`target_param1` = 43178,
+`target_param2` = 175610
+WHERE `entryorguid` = 177385 AND `id` = 0;
