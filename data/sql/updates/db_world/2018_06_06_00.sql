76ab13850e3dd1fb1bfdf132c54ca4de45053d16
Travis CI <travis@build.bot>
2018-06-06 18:38:51 +0000

    Importing pending sql

--- data/sql/updates/db_world/2018_06_06_00.sql
+++ data/sql/updates/db_world/2018_06_06_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2018_06_01_00 -> 2018_06_06_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2018_06_01_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2018_06_01_00 2018_06_06_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1527626760682403687'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1527626760682403687');
 
 --
@@ -167,3 +183,12 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comm
 (58513, 58514, 0, 'Rotten Banana - Rotten Banana'),
 (58520, 58519, 0, 'Spit - Spit');
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
