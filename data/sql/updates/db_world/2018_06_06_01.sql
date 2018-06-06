7320896ffdb48515622e4d64b23ac70e34028b7e
Travis CI <travis@build.bot>
2018-06-06 19:00:07 +0000

    Importing pending sql

--- data/sql/updates/db_world/2018_06_06_01.sql
+++ data/sql/updates/db_world/2018_06_06_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2018_06_06_00 -> 2018_06_06_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2018_06_06_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2018_06_06_00 2018_06_06_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1526938640533602197'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1526938640533602197');
 
 UPDATE `smart_scripts` SET `event_param1` = 2, `event_param2` = 30, `comment` = 'Scarlet Captain - Within 2-30 Range - Shoot' WHERE `entryorguid` = 28611 AND `id` = 0;
@@ -16,3 +32,12 @@ VALUES
 
 -- delete wrong text line "Light bless you, my child."
 DELETE FROM `creature_text` WHERE `entry` = 28611 AND `id` = 6;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
