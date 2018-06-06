4b898e545d1d4ce7fd666380db9c61aaa30234ec
Kargatum <dowlandtop@yandex.com>
2018-06-06 20:50:46 +0700

    Change encoding in banner (#911)

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
  * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
@@ -114,9 +114,9 @@ extern int main(int argc, char** argv)
     sLog->outString("  ███████║  ███╔╝ █████╗  ██████╔╝██║   ██║   ██║   ███████║");           
     sLog->outString("  ██╔══██║ ███╔╝  ██╔══╝  ██╔══██╗██║   ██║   ██║   ██╔══██║");           
     sLog->outString("  ██║  ██║███████╗███████╗██║  ██║╚██████╔╝   ██║   ██║  ██║");           
-    sLog->outString("  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝");                                                                
+    sLog->outString("  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝");
     sLog->outString("                                ██████╗ ██████╗ ██████╗ ███████╗");
-    sLog->outString("                                ██╔════╝██╔═══██╗██╔══██╗██╔════╝");
+    sLog->outString("                                ██╔════╝██╔═══██╗██╔══██╗██╔═══╝");
     sLog->outString("                                ██║     ██║   ██║██████╔╝█████╗");  
     sLog->outString("                                ██║     ██║   ██║██╔══██╗██╔══╝");  
     sLog->outString("                                ╚██████╗╚██████╔╝██║  ██║███████╗");
