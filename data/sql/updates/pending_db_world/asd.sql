3a508bec0991e2f57500b62f8e61b72548a756e5
Kartselyanski <emiliyan9604bg@gmail.com>
2018-06-13 00:11:44 +0300

    Maexxna portal rotation.

---
+++ data/sql/updates/pending_db_world/asd.sql
@@ -0,0 +1,4 @@
+-- maexxna portals
+REPLACE INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `VerifiedBuild`, `zoneId`, `areaId`) VALUES 
+(65857, 181233, 533, 3, 1, 3465.16, -3940.45, 308.788, 0.441179, -0.305481, 0.637715, 0.305481, 0.637716, 180, 0, 1, 0, 0, 0),
+(268045, 181575, 533, 3, 2, 3465.16, -3940.45, 308.788, 0.441179, -0.305481, 0.637715, 0.305481, 0.637716, 180, 0, 1, 0, 0, 0);
