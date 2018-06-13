ceb6f3d7e108d8c154722106ec0fde51a6692f1d
Kartselyanski <emiliyan9604bg@gmail.com>
2018-06-13 03:10:55 +0300

    Portal's visual effect should be in different phasemask and appear only when maexxna is killed.

--- data/sql/updates/pending_db_world/asd.sql
+++ data/sql/updates/pending_db_world/asd.sql
@@ -7,4 +7,10 @@ update `gameobject` set
     `spawnmask` = 3, `phasemask`= 1,
     `position_x` = 3465.16, `position_y` = -3940.45, `position_z` = 308.788,`orientation` = 0.441179, 
     `rotation0` = -0.305481, `rotation1` = 0.637715, `rotation2` = 0.305481, `rotation3` = 0.637716
-    where `guid` in(65857,268045); -- check if guid is valid
+    where `guid` in(65857); -- check if guid is valid
+
+update `gameobject` set
+    `spawnmask` = 3, `phasemask`= 2,
+    `position_x` = 3465.16, `position_y` = -3940.45, `position_z` = 308.788,`orientation` = 0.441179, 
+    `rotation0` = -0.305481, `rotation1` = 0.637715, `rotation2` = 0.305481, `rotation3` = 0.637716
+    where `guid` in(268045); -- check if guid is valid
