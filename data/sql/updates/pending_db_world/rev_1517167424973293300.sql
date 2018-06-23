9390304c90f60d0e39514ee588d4189b7b002315
Travis CI <travis@build.bot>
2018-06-23 23:00:39 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1517167424973293300.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1517167424973293300');
-
-DELETE FROM `areatrigger_scripts` WHERE `entry` = 5709;
-INSERT INTO `areatrigger_scripts` VALUES (5709, 'at_lady_deathwhisper_entrance');
