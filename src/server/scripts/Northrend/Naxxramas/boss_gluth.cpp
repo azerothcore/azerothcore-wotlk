78a97d49cf014192ef9cd13934194de9881a36fd
Barbz <BarbzYHOOL@users.noreply.github.com>
2018-07-05 00:27:33 +0200

    boss_gluth.cpp - Make comment more useful
    Describe how zombies must spawn

--- src/server/scripts/Northrend/Naxxramas/boss_gluth.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_gluth.cpp
@@ -177,8 +177,9 @@ public:
                         uint8 rand = urand(0,2);
                         for (int32 i = 0; i < RAID_MODE(1,2); ++i)
                         {
-                            // 10n - should spawn only from mid gate 25 from all 3
+                            // In 10 man raid, normal mode - should spawn only from mid gate
                             // \1 |0 /2 pos
+                            // In 25 man raid - should spawn from all 3 gates
                             if (me->GetMap()->GetDifficulty() == RAID_DIFFICULTY_10MAN_NORMAL)
                                 me->SummonCreature(NPC_ZOMBIE_CHOW, zombiePos[0]);
                             else
