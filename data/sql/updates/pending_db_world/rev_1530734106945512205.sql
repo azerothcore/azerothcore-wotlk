79d9453e8fa9d5f5d55fbb4c38be1f6c1bf5fd4a
Travis CI <travis@build.bot>
2018-07-12 13:43:52 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1530734106945512205.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1530734106945512205');
-
-UPDATE `creature_template` SET `unit_flags` = 0 WHERE `entry` = 7856;
-UPDATE `smart_scripts` SET `event_type` = 9, `event_param1` = 2, `event_param2` = 30, `comment` = 'Southsea Freebooter - Within 2-30 Range - Shoot' WHERE `entryorguid` = 7856 AND `id` = 0;
