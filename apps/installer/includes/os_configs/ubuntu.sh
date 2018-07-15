6e7e089b0b3606028f12b9a4b8ec0ef764072114
Yehonal <yehonal.azeroth@gmail.com>
2018-07-15 23:44:58 +0200

    Fixed libssl-dev for latest ubuntu revs on deps installer

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -12,7 +12,7 @@ else
       ;;
     *)
       sudo apt-get install -y git cmake make gcc g++ clang libmysqlclient-dev \
-      libssl-dev=1.0.2g-1ubuntu4.8 libbz2-dev libreadline-dev libncurses-dev \
+      libssl-dev libbz2-dev libreadline-dev libncurses-dev \
       mysql-server libace-6.* libace-dev
       ;;
   esac
