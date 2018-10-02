c7915036d7da73d6ce761141633f398b8fee1247
Travis CI <travis@build.bot>
2018-10-02 21:32:53 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1537654210905936070.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1537654210905936070');
-
-UPDATE `creature_text` SET `BroadcastTextID` = 37093 WHERE `entry` = 36794 AND `groupid` = 1;
-UPDATE `creature_text` SET `BroadcastTextID` = 37392 WHERE `entry` = 36990 AND `groupid` = 3;
-UPDATE `creature_text` SET `BroadcastTextID` = 37087 WHERE `entry` = 36993 AND `groupid` = 2;
