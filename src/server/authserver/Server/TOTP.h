5c4c857272a1b0573eed8579c75515a2686396e9
Barbz <BarbzYHOOL@users.noreply.github.com>
2018-10-14 18:52:22 +0200

    Fix typo and missing newline

--- src/server/authserver/Server/TOTP.h
+++ src/server/authserver/Server/TOTP.h
@@ -15,7 +15,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _TOTP_H
-#define _TOPT_H
+#define _TOTP_H
 #include "openssl/hmac.h"
 #include "openssl/evp.h"
 namespace TOTP
