57425fcf583cb143e371c8705cb9cadaf8b95cbc
Travis CI <travis@build.bot>
2018-10-17 22:08:56 +0000

    Importing pending sql

--- data/sql/updates/db_world/2018_10_17_00.sql
+++ data/sql/updates/db_world/2018_10_17_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2018_10_16_00 -> 2018_10_17_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2018_10_16_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2018_10_16_00 2018_10_17_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1537827596332623300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1537827596332623300');
 DELETE FROM `command` WHERE `name`='go gobject';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('go gobject', 1, 'Syntax: .go object #gameobject_guid\r\nTeleport your character to gameobject with guid #gameobject_guid');
@@ -15,3 +31,12 @@ WHERE `name` = 'lookup object' COLLATE utf8mb4_bin;
 
 UPDATE `command` SET `help` = '[DEPRECATED]: use ".list gobject" instead.\r\nSyntax: .go object #object_guid\r\nTeleport your character to gameobject with guid #object_guid'
 WHERE `name` = 'list object' COLLATE utf8mb4_bin;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
