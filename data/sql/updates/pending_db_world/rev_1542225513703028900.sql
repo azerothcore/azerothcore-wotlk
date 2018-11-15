fc4b52b036a33f1207acca8c93a4c9ef3f9baa10
Travis CI <travis@build.bot>
2018-11-15 23:18:33 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1542225513703028900.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1542225513703028900');
-REPLACE INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES (409, '%s is banned permanently. Reason: %s.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
