052f7e1083a2b2539d744597c9dfe4c17fb606f4
Poszer <41213210+poszer@users.noreply.github.com>
2018-11-22 15:22:21 +0100

    DB/Quest: fix OfferRewardText for The New Horde (#1087)
    Replacing wrong Offer Reward Text

---
+++ data/sql/updates/pending_db_world/rev_1542756898843297200.sql
@@ -0,0 +1,2 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1542756898843297200');
+UPDATE `quest_template` SET `OfferRewardText`='Another one of Eitrigg\'s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you\'re ready to leave the Valley, you\'ll be a proud warrior of the Horde.' WHERE  `ID`=787;
