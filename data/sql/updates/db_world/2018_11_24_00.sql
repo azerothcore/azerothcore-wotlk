d34e5329dfe6df44143219b3b9521610ee142ce2
Travis CI <travis@build.bot>
2018-11-24 17:44:29 +0000

    Importing pending sql

--- data/sql/updates/db_world/2018_11_24_00.sql
+++ data/sql/updates/db_world/2018_11_24_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2018_11_22_00 -> 2018_11_24_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2018_11_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2018_11_22_00 2018_11_24_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1542839405121938872'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1542839405121938872');
 
 SET @ENTRY := 18797;
@@ -7,3 +23,12 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,0,0,4,0,100,0,0,0,0,0,91,7,0,0,0,0,0,1,0,0,0,0,0,0,0,"Tortured Skeleton - On Aggro - Remove Flag Standstate Dead");
 
 UPDATE `creature` SET `spawndist`=0,`MovementType`=0 WHERE `guid` IN (67091,67096,67097,67098,67099,67133);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
