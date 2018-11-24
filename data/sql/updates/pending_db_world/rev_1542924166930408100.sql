d2254db7c722eae89ab62b0a422dab3006f2aba2
poszer <poszerinsane@gmail.com>
2018-11-24 20:26:08 +0100

    DB/NPC: fix Darnassus shield merchant missing equipment in hand
    Last Update.

--- data/sql/updates/pending_db_world/rev_1542924166930408100.sql
+++ data/sql/updates/pending_db_world/rev_1542924166930408100.sql
@@ -1,3 +1,2 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1542924166930408100');
-DELETE FROM `creature_equip_template` WHERE `CreatureID`=4240 AND `ID`=1;
-INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES (4240, 1, 35, 0, 0, 12340);
+UPDATE `creature_equip_template` SET `ItemID1`='35' WHERE `CreatureID`=4240 AND `ID`=1;
