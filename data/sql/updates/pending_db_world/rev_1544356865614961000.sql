8a22cf5a189336d893db655e8713776c3924055f
poszer <poszerinsane@gmail.com>
2018-12-09 13:03:26 +0100

    DB/Signs Correct road signs
    Updated road signs to correct positions

---
+++ data/sql/updates/pending_db_world/rev_1544356865614961000.sql
@@ -0,0 +1,4 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1544356865614961000');
+UPDATE `gameobject` SET `position_x`='342.79', `position_y`='1249.17', `position_z`='81.0146' WHERE `guid`=33326;
+UPDATE `gameobject` SET `position_x`='-3597', `position_y`='-2714.84', `position_z`='19.94' WHERE `guid`=13764;
+UPDATE `gameobject` SET `position_x`='-4970.3', `position_y`='-1712.6', `position_z`='-62.5' WHERE `guid`=16889;
