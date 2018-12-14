e56dabd78eab825c2137ed20e6c1e4b8642d5212
Poszer <41213210+poszer@users.noreply.github.com>
2018-12-14 16:55:00 +0100

    Updated

--- data/sql/updates/pending_db_world/rev_1544714673423311600.sql
+++ data/sql/updates/pending_db_world/rev_1544714673423311600.sql
@@ -4,5 +4,5 @@ UPDATE `creature_text` SET `text`='My freedom means nothing if we fail to save U
 -- NPC entry 40405 Kieupid, Pet Trainer in Silvermoon City
 SET @CGUID = 248570;
 DELETE FROM `creature` WHERE `id` = 40405;
-INSERT INTO `creature` (`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`VerifiedBuild`) VALUES
-(@CGUID,40405,530,0,0,1,1,0,0, 9924.067, -7400.503, 13.71723, 6.073746, 120,0,0,0,0,0,0,0,0,12340);
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+(@CGUID,40405,530,1,1,0,0, 9924.067, -7400.503, 13.71723, 6.073746, 120,0,0,0,0,0,0,0,0);
