aded79a00eb19ec5d7d5e5844ed444bde1084a93
Travis CI <travis@build.bot>
2018-12-16 15:07:57 +0000

    Importing pending sql

--- data/sql/updates/pending_db_world/rev_1544615446472927600.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO version_db_world (`sql_rev`) VALUES ('1544615446472927600');
-INSERT INTO `smart_scripts` (`entryorguid`, `id`, `event_type`, `action_type`, `action_param1`, `action_param2`, `target_type`, `comment`) VALUES ('20132', '5', '6', '11', '35762', '2', '16', 'Socrethar - On Death - Quest Credit');
