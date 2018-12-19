1cd498fbcd084bb908eb567c7b06fa3583a05a0a
Viste <viste02@gmail.com>
2018-12-19 04:38:54 +0300

    fix typo

--- data/sql/base/db_characters/character_queststatus.sql
+++ data/sql/base/db_characters/character_queststatus.sql
@@ -23,7 +23,7 @@ CREATE TABLE `character_queststatus`
   `itemcount3` smallint(5) unsigned NOT NULL DEFAULT '0',
   `itemcount4` smallint(5) unsigned NOT NULL DEFAULT '0',
   `itemcount5` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `itemcount6` smallint(5) unsigned NOT NULL DEFAULT '0'
+  `itemcount6` smallint(5) unsigned NOT NULL DEFAULT '0',
   `playercount` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`guid`,`quest`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Player System';
