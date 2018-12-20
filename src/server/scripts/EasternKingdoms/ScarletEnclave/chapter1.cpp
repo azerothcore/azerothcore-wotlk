ffa7b7a286d1f59b7a726e18110310add630701c
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2018-12-20 13:08:47 +0100

    DB/Quest: "The Endless Hunger" fix second talk text. (#867)
    Bugfix quest "The Endless Hunger": Wrong source for second talk text.

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -715,7 +715,7 @@ public:
                 me->CastSpell(me, SPELL_DK_INITIATE_VISUAL, true);
 
                 if (Player* starter = ObjectAccessor::GetPlayer(*me, playerGUID))
-                    sCreatureTextMgr->SendChat(me, SAY_EVENT_ATTACK, NULL, CHAT_MSG_ADDON, LANG_ADDON, TEXT_RANGE_NORMAL, 0, TEAM_NEUTRAL, false, starter);
+                    Talk(SAY_EVENT_ATTACK, starter);
 
                 phase = PHASE_TO_ATTACK;
             }
