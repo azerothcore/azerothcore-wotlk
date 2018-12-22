2455501b756d551617af16d5eb1471e6dd432492
Poszer <41213210+poszer@users.noreply.github.com>
2018-12-22 17:17:01 +0100

    DB/Creature Add missing pet trainer / NPC text (#1115)

---
+++ data/sql/updates/pending_db_world/rev_1544714673423311600.sql
@@ -0,0 +1,8 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1544714673423311600');
+-- NPC Urcos correct Creature_Text
+UPDATE `creature_text` SET `text`='My freedom means nothing if we fail to save Ursoc.  Make haste, $N.' WHERE `entry`= 27328 AND `groupid`=2 AND `id`= 0;
+-- NPC entry 40405 Kieupid, Pet Trainer in Silvermoon City
+SET @CGUID = 3548;
+DELETE FROM `creature` WHERE `id` = 40405;
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+(@CGUID,40405,530,1,1,0,0, 9924.067, -7400.503, 13.71723, 6.073746, 120,0,0,0,0,0,0,0,0);
