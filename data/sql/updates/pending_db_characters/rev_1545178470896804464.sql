2d575299d355e79cf8a1357cd88e707252b52e11
Travis CI <travis@build.bot>
2018-12-23 14:00:42 +0000

    Importing pending sql

--- data/sql/updates/pending_db_characters/rev_1545178470896804464.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO version_db_characters (`sql_rev`) VALUES ('1545178470896804464');
-
-ALTER TABLE `character_queststatus`
-  ADD `itemcount5` smallint(5) unsigned NOT NULL DEFAULT '0' AFTER `itemcount4`,
-  ADD `itemcount6` smallint(5) unsigned NOT NULL DEFAULT '0' AFTER `itemcount5`;
