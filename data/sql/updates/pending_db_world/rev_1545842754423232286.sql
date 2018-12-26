c4ecd1a039d54da672fbe68ee81198b5a546e955
FrancescoBorzi <borzifrancesco@gmail.com>
2018-12-26 18:53:00 +0100

    Solve conflicts

---
+++ data/sql/updates/pending_db_world/rev_1545842754423232286.sql
@@ -0,0 +1,4 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1545842754423232286');
+
+DELETE FROM `mail_loot_template` WHERE `entry` IN (90, 119, 124, 140, 180, 183, 216, 217, 218, 270);
+DELETE FROM `achievement_criteria_data` WHERE `entry` IN (4989, 4988, 5620, 5632, 5619, 4987);
