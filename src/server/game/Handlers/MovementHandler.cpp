d167890b3f1895cae41afe71cf537704888f5634
Barbz <BarbzYHOOL@users.noreply.github.com>
2018-12-26 01:42:27 +0100

    CORE/Movement: Fix a potential crash with a player out of bounds. (#970)

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -488,10 +488,12 @@ void WorldSession::HandleMovementOpcodes(WorldPacket & recvData)
                 else if (!plrMover->HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_IS_OUT_OF_BOUNDS))
                 {
                     WorldSafeLocsEntry const* grave = sObjectMgr->GetClosestGraveyard(plrMover->GetPositionX(), plrMover->GetPositionY(), plrMover->GetPositionZ(), plrMover->GetMapId(), plrMover->GetTeamId());
-
-                    if ( grave)
+                    
+                    if (grave)
+                    {
                         plrMover->TeleportTo(grave->map_id, grave->x, grave->y, grave->z, plrMover->GetOrientation());
-                    plrMover->Relocate(grave->x, grave->y, grave->z, plrMover->GetOrientation());
+                        plrMover->Relocate(grave->x, grave->y, grave->z, plrMover->GetOrientation());
+                    }
                 }
 
                 plrMover->StopMovingOnCurrentPos(); // pussywizard: moving corpse can't release spirit
