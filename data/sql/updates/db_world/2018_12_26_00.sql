aeadbce8d8f731ea0289305f2b8429693752e93a
FrancescoBorzi <borzifrancesco@gmail.com>
2018-12-26 18:06:37 +0100

    Fix bad copy paste

--- data/sql/updates/db_world/2018_12_26_00.sql
+++ data/sql/updates/db_world/2018_12_26_00.sql
@@ -17,7 +17,7 @@ SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '154584275442323228
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1545842754423232286');
 
 DELETE FROM `mail_loot_template` WHERE `entry` IN (90, 119, 124, 140, 180, 183, 216, 217, 218, 270);
-DELETE FROM `achievement_criteria_data` WHERE `entry` IN (4989, 4988, 5620, 5632, 5619, 4987);
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (4989, 4988, 5620, 5632, 5619, 4987);
 
 --
 -- END UPDATING QUERIES
