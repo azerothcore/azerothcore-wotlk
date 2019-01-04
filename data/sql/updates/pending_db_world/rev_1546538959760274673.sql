a62ea932eb260b37c7282b94515ab179e4f7befe
Viste <viste02@gmail.com>
2019-01-05 00:25:20 +0300

    fix

--- data/sql/updates/pending_db_world/rev_1546538959760274673.sql
+++ data/sql/updates/pending_db_world/rev_1546538959760274673.sql
@@ -1,7 +1,9 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1546538959760274673');
 
 ALTER TABLE `game_event`
-  ADD COLUMN `announce` tinyint(3) unsigned NOT NULL DEFAULT 2 COMMENT '0 dont announce, 1 announce, 2 value from config' AFTER `world_event`;
+ADD COLUMN `announce` tinyint(3) unsigned NOT NULL DEFAULT 2 COMMENT '0 dont announce, 1 announce, 2 value from config' AFTER `world_event`;
+
+ALTER TABLE `game_event`
   CHANGE `start_time` `start_time` TIMESTAMP NULL DEFAULT NULL COMMENT 'Absolute start date, the event will never start before',
   CHANGE `end_time` `end_time` TIMESTAMP NULL DEFAULT NULL COMMENT 'Absolute end date, the event will never start after';
 
