eb97640b4f268e9d0e30cbdad41ffe3e1e7a30c6
Viste <viste02@gmail.com>
2019-01-04 23:32:16 +0300

    miss

--- data/sql/updates/pending_db_world/rev_1546538959760274673.sql
+++ data/sql/updates/pending_db_world/rev_1546538959760274673.sql
@@ -1,4 +1,4 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1546538959760274673');
 
 ALTER TABLE `game_event`
-ADD COLUMN `announce` tinyint(3) unsigned NULL DEFAULT 2 COMMENT '0 dont announce, 1 announce, 2 value from config' AFTER `world_event`;
+ADD COLUMN `announce` tinyint(3) unsigned NOT NULL DEFAULT 2 COMMENT '0 dont announce, 1 announce, 2 value from config' AFTER `world_event`;
