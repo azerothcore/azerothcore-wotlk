4be81a4bdd0c18c762d21bd71e8d30663383f506
Travis CI <travis@build.bot>
2019-01-06 17:53:51 +0000

    Importing pending sql

--- data/sql/updates/db_world/2019_01_06_01.sql
+++ data/sql/updates/db_world/2019_01_06_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_01_06_00 -> 2019_01_06_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_01_06_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_01_06_00 2019_01_06_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1546694249586814731'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1546694249586814731');
 
 -- Moths:
@@ -110,3 +126,12 @@ DELETE FROM `creature_template_addon` WHERE `entry` IN (25062);
 INSERT INTO `creature_template_addon` (`entry`,`path_id`,`mount`,`bytes1`,`bytes2`,`emote`,`auras`)
 VALUES
 (25062,0,0,33554432,0,0,'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
