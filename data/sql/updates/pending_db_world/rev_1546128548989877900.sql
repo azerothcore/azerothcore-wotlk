9bf56aa12409f1d0557ac77a377b0938e1378730
Poszer <41213210+poszer@users.noreply.github.com>
2019-01-07 12:19:56 +0100

    Core/Quest: Mr.Floppy's Perilous Adventure Quest (#1158)

---
+++ data/sql/updates/pending_db_world/rev_1546128548989877900.sql
@@ -0,0 +1,4 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1546128548989877900');
+
+UPDATE `creature_template` SET `VehicleId` = 0 WHERE `entry` = 26586;
+UPDATE `creature_template` SET `ScriptName` = 'npc_ravenous_worg' WHERE `entry` = 26590;
