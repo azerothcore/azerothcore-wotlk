074cf0b324cbb43ebb764e68c1ad9e35d6156a08
Viste <viste02@gmail.com>
2019-01-09 14:12:38 +0300

    build: should make travis happy

--- src/server/scripts/EasternKingdoms/Karazhan/instance_karazhan.cpp
+++ src/server/scripts/EasternKingdoms/Karazhan/instance_karazhan.cpp
@@ -42,11 +42,11 @@ public:
         void GetChessPiecesByFaction(std::list<uint64>& lList, uint32 uiFaction) { lList = uiFaction == FACTION_ID_CHESS_ALLIANCE ? m_lChessPiecesAlliance : m_lChessPiecesHorde; };
         Creature* GetCreatureByGUID(uint64 guid) { return instance->GetCreature(guid); };
 
-        void OnPlayerEnter(Player* player) override
+        void OnPlayerEnter(Player* pPlayer) override
         {
             // only once, taking the first player to enter
             if (!m_uiTeam)
-                m_uiTeam = player->GetTeamId();
+                m_uiTeam = pPlayer->GetTeamId();
         }
 
         void OnCreatureCreate(Creature* creature) override
