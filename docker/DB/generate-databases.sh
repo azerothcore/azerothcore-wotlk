696a443c1a498d532bf8510cadcaaffb2f668d83
FrancescoBorzi <borzifrancesco@gmail.com>
2019-01-09 23:46:15 +0100

    Fix spaces

--- docker/DB/generate-databases.sh
+++ docker/DB/generate-databases.sh
@@ -1,10 +1,12 @@
 # TODO: remove this line after we fully support mysql 5.7
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "SET GLOBAL sql_mode = '';"
 
+
 echo "Creating DBs..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE acore_auth"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE acore_characters"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE acore_world"
+
 
 echo "Importing auth base..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD acore_auth < /azerothcore/env/dist/sql/auth_base.sql
@@ -25,6 +27,7 @@ mysql -u root -p$MYSQL_ROOT_PASSWORD acore_characters < /azerothcore/env/dist/sq
 echo "Importing world updates..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD acore_world < /azerothcore/env/dist/sql/world_update.sql
 
+
 echo "Importing auth custom (if any)..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD acore_auth < /azerothcore/env/dist/sql/auth_custom.sql
 
@@ -33,5 +36,6 @@ mysql -u root -p$MYSQL_ROOT_PASSWORD acore_characters < /azerothcore/env/dist/sq
 
 echo "Importing world custom (if any)..."
 mysql -u root -p$MYSQL_ROOT_PASSWORD acore_world < /azerothcore/env/dist/sql/world_custom.sql
+
 
 echo "Done!"
