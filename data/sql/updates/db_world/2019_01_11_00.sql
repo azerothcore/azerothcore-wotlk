222195b7d1a5c98dd8173274443bc5cca2314e21
José González <maangx@gmail.com>
2019-01-11 06:46:22 -0300

    Fix missing comma in SQL

--- data/sql/updates/db_world/2019_01_11_00.sql
+++ data/sql/updates/db_world/2019_01_11_00.sql
@@ -19,7 +19,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1547165890320901800');
 DELETE FROM `trinity_string` WHERE `entry` BETWEEN 5062 AND 5071;
 INSERT INTO `trinity_string` (`entry`, `content_default`) VALUES
 (5062, 'You will now read %s conversations.'),
-(5063, 'Stopped spying %s.')
+(5063, 'Stopped spying %s.'),
 (5064, '%s is already being followed by: %s'),
 (5065, 'Following %s\'s group, you will now read each player\'s conversations.'),
 (5066, 'Stopped spying %s\'s group.'),
