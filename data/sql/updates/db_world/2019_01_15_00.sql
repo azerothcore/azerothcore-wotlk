0b91bc0f2ae56acb74576526efa56143a777c8d9
Travis CI <travis@build.bot>
2019-01-15 17:32:51 +0000

    Importing pending sql

--- data/sql/updates/db_world/2019_01_15_00.sql
+++ data/sql/updates/db_world/2019_01_15_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_01_14_01 -> 2019_01_15_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_01_14_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_01_14_01 2019_01_15_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1547424818609511100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1547424818609511100');
 
 UPDATE `creature_template`
@@ -14,3 +30,12 @@ VALUES
 (20214,0,0,1,11,0,100,0,0,0,0,0,11,35788,0,0,0,0,0,1,0,0,0,0,0,0,0,'Arakkoa Egg - On Respawn - Cast Spell ''Arakkoa Egg Debuff'' (35788) on self'),
 (20214,0,1,2,10,0,100,0,0,6,20000,20000,11,35788,0,0,0,0,0,1,0,0,0,0,0,0,0,'Arakkoa Egg - Within 6 Range OOC LoS - Cast Spell ''Arakkoa Egg Debuff'' (35788) on self'),
 (20214,0,2,0,26,0,100,0,0,6,20000,20000,11,35788,0,0,0,0,0,1,0,0,0,0,0,0,0,'Arakkoa Egg - Within 6 Range IC LoS - Cast Spell ''Arakkoa Egg Debuff'' (35788) on self');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
