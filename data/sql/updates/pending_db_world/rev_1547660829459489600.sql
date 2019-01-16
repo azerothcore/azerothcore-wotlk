89c63df373229a41bbbfdcbd57c5c667ad7cfa35
Walter Pagani <paganiwalter@gmail.com>
2019-01-16 15:27:23 -0300

    creating an empty line

--- data/sql/updates/pending_db_world/rev_1547660829459489600.sql
+++ data/sql/updates/pending_db_world/rev_1547660829459489600.sql
@@ -1,2 +1,2 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1547660829459489600');
-UPDATE `item_template` SET `Flags` = `Flags` | 2048 WHERE `class` = 12 AND `subclass` = 0 AND `bonding` = 4;
+UPDATE `item_template` SET `Flags`=`Flags`|2048 WHERE `class`=12 AND `subclass`=0 AND `bonding`=4;
