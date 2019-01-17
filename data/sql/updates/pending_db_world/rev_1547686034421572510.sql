9472259d370c5b621c7c10073019fb319c3e6538
coldironwow <coldironwow@github.com>
2019-01-17 09:49:53 -0500

    Code review fixes

--- data/sql/updates/pending_db_world/rev_1547686034421572510.sql
+++ data/sql/updates/pending_db_world/rev_1547686034421572510.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1547686034421572510');
 
-UPDATE gameobject
-SET state = 1
-WHERE id = 181228 AND guid=150159
+UPDATE `gameobject`
+SET `state` = 1
+WHERE `id` = 181228 AND `guid`=150159;
