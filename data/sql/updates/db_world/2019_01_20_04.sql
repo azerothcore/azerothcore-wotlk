1f196f74309ccf8fe488aaa96997260bbcd45543
Travis CI <travis@build.bot>
2019-01-20 18:59:30 +0000

    Importing pending sql

--- data/sql/updates/db_world/2019_01_20_04.sql
+++ data/sql/updates/db_world/2019_01_20_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_01_20_03 -> 2019_01_20_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_01_20_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_01_20_03 2019_01_20_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1547990516486417003'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1547990516486417003');
 
 
@@ -45,3 +61,12 @@ UPDATE `smart_scripts` SET `action_type` = 214 WHERE `action_type` = 149;
 
 -- SMART_ACTION_LOAD_GRID
 UPDATE `smart_scripts` SET `action_type` = 215 WHERE `action_type` = 150;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
