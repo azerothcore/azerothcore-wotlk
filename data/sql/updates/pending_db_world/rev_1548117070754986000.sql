6b48fe4f163b3fc995a93956d36ddf684d9fab09
José González <Deku@users.noreply.github.com>
2019-01-22 09:30:40 -0300

    Remove brackets...
    ... to avoid parameters being interpreted as optional

--- data/sql/updates/pending_db_world/rev_1548117070754986000.sql
+++ data/sql/updates/pending_db_world/rev_1548117070754986000.sql
@@ -2,9 +2,9 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1548117070754986000');
 
 DELETE FROM `command` WHERE `name` IN ('spy follow', 'spy unfollow', 'spy groupfollow', 'spy groupunfollow', 'spy clear', 'spy status');
 INSERT INTO `command` (`name`,`security`,`help`) VALUES
-('spy follow',          2, 'Syntax: .spy follow [$name]\nStarts reading all the private conversations of that player.'),
-('spy unfollow',        2, 'Syntax: .spy unfollow [$name]\nStops reading private messages from that player.'),
-('spy groupfollow',     3, 'Syntax: .spy groupfollow [$name]\nStarts reading all the private conversations of that player and his current party.'),
-('spy groupunfollow',   3, 'Syntax: .spy groupunfollow [$name]\nStops reading private messages from that player\'s party.'),
+('spy follow',          2, 'Syntax: .spy follow $name\nStarts reading all the private conversations of that player.'),
+('spy unfollow',        2, 'Syntax: .spy unfollow $name\nStops reading private messages from that player.'),
+('spy groupfollow',     3, 'Syntax: .spy groupfollow $name\nStarts reading all the private conversations of that player and his current party.'),
+('spy groupunfollow',   3, 'Syntax: .spy groupunfollow $name\nStops reading private messages from that player\'s party.'),
 ('spy clear',           2, 'Syntax: .spy clear\nClears your current spying list, removing every player from it.'),
 ('spy status',          2, 'Syntax: .spy status\nShows your current spying list.');
