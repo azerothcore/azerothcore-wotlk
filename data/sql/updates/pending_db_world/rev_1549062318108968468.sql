af0653e94d9b013394ed1b68f981f877556752a1
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-02-01 23:05:56 +0000

    fix(DB): positioning of the control console on the zeppelin "Cloudkisser"

---
+++ data/sql/updates/pending_db_world/rev_1549062318108968468.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1549062318108968468');
+
+UPDATE `creature` SET `position_x` = 10.5731, `position_y` = -7.65137, `position_z` = -16.6839, `orientation` = 0 WHERE `guid` = 142823;
