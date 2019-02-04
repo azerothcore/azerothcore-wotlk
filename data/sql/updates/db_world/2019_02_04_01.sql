6299a412a5161affa0f4b40b8a3d70aba1df061d
AzerothCoreBot <azerothcorebot@gmail.com>
2019-02-04 21:50:36 +0000

    Import pending SQL update file

--- data/sql/updates/db_world/2019_02_04_01.sql
+++ data/sql/updates/db_world/2019_02_04_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_02_04_00 -> 2019_02_04_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_02_04_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_02_04_00 2019_02_04_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1548778743987887239'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1548778743987887239');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 32711;
@@ -19,3 +35,12 @@ VALUES
 (27047, 0, 0, '', 16, 0, 100, 0, 0, 0, 0, 0, 'Invisible Stalker (Floating Only)'),
 (32711, 0, 0, 'So peaceful...', 12, 0, 100, 0, 0, 0, 0, 0, 'Warp-Huntress Kula'),
 (29506, 0, 0, 'Welcome traveler!', 12, 0, 100, 0, 0, 0, 0, 0, 'Orland Schaeffer');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
