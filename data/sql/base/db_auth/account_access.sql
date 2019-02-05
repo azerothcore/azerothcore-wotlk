d11d35c861a317bf8431978ac934464f53b6aef3
Barbz <BarbzYHOOL@users.noreply.github.com>
2019-02-05 17:29:32 +0100

    Update account_access.sql

--- data/sql/base/db_auth/account_access.sql
+++ data/sql/base/db_auth/account_access.sql
@@ -19,16 +19,16 @@ CREATE TABLE `account_access`
 LOCK TABLES `account_access` WRITE;
 /*!40000 ALTER TABLE `account_access` DISABLE KEYS */;
 INSERT INTO `account_access` (`id`, `gmlevel`, `RealmID`) VALUES
-(1,  4,  -1),
-(2,  4,  -1),
-(3,  3,  -1),
-(4,  3,  -1),
-(5,  2,  -1),
-(6,  2,  -1),
-(7,  1,  -1),
-(8,  1,  -1),
-(9,  0,  -1),
-(10, 0,  -1);
+(1,  4, -1),
+(2,  4, -1),
+(3,  3, -1),
+(4,  3, -1),
+(5,  2, -1),
+(6,  2, -1),
+(7,  1, -1),
+(8,  1, -1),
+(9,  0, -1),
+(10, 0, -1);
 /*!40000 ALTER TABLE `account_access` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
