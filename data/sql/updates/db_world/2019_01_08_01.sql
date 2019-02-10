70472b747a4cc4fdb42855628aaf112672a85196
FrancescoBorzi <borzifrancesco@gmail.com>
2019-02-10 17:55:01 +0100

    fix(setup): MySQL 5.7

--- data/sql/updates/db_world/2019_01_08_01.sql
+++ data/sql/updates/db_world/2019_01_08_01.sql
@@ -23,8 +23,8 @@ ALTER TABLE `game_event`
   CHANGE `start_time` `start_time` TIMESTAMP NULL DEFAULT NULL COMMENT 'Absolute start date, the event will never start before',
   CHANGE `end_time` `end_time` TIMESTAMP NULL DEFAULT NULL COMMENT 'Absolute end date, the event will never start after';
 
-UPDATE `game_event` SET `start_time`=NULL WHERE `start_time`='0000-00-00 00:00:00';
-UPDATE `game_event` SET `end_time`=NULL WHERE `end_time`='0000-00-00 00:00:00';
+UPDATE IGNORE `game_event` SET `start_time`=NULL WHERE `start_time`='0000-00-00 00:00:00';
+UPDATE IGNORE `game_event` SET `end_time`=NULL WHERE `end_time`='0000-00-00 00:00:00';
 
 --
 -- END UPDATING QUERIES
