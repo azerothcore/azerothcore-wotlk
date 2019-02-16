f960d8e8486330d68a195d63eeda841223c03c11
Francesco Borzì <borzifrancesco@gmail.com>
2019-02-16 20:04:26 +0100

    fix(setup): MySQL 5.7 (#1445)

--- data/sql/updates/db_auth/2019_01_05_00.sql
+++ data/sql/updates/db_auth/2019_01_05_00.sql
@@ -19,7 +19,7 @@ INSERT INTO version_db_auth (`sql_rev`) VALUES ('1546540718864817294');
 ALTER TABLE `account` CHANGE `last_login` `last_login` TIMESTAMP NULL DEFAULT NULL;
 ALTER TABLE `account` CHANGE `email` `email` VARCHAR(255) NOT NULL DEFAULT '';
 
-UPDATE `account` SET `last_login`=NULL WHERE `last_login`='0000-00-00 00:00:00';
+UPDATE IGNORE `account` SET `last_login`=NULL WHERE `last_login`='0000-00-00 00:00:00';
 
 --
 -- END UPDATING QUERIES
