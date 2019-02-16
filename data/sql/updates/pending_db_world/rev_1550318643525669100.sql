3f66ae6ec708e0b080b964866db7ddb6616eeb6f
Poszer <41213210+poszer@users.noreply.github.com>
2019-02-16 13:32:23 +0100

    Update rev_1550318643525669100.sql

--- data/sql/updates/pending_db_world/rev_1550318643525669100.sql
+++ data/sql/updates/pending_db_world/rev_1550318643525669100.sql
@@ -1,7 +1,7 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1550318643525669100');
 
 -- Scattered Crate drop Lost Supplies should have 100 drop chance
-`UPDATE `gameobject_loot_template` SET `Chance`=100 WHERE `Item`=6172;`
+UPDATE `gameobject_loot_template` SET `Chance`='100' WHERE `Entry`=3597 AND `Item`=6172;
 
 -- Creature "Flanis Swiftwing" should be dead.
 UPDATE `creature_template_addon` SET `emote`=65 WHERE  `entry`=21727;
