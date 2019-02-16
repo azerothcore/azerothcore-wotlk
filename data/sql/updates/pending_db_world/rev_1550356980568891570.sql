9945f19a032dbd385e43ccc76ce80050ffcd5039
AzerothCoreBot <azerothcorebot@gmail.com>
2019-02-16 22:58:14 +0000

    Import pending SQL update file

--- data/sql/updates/pending_db_world/rev_1550356980568891570.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1550356980568891570');
-
--- Script for Living Inferno (Halion fight)
-UPDATE `creature_template` SET `ScriptName`='npc_living_inferno' WHERE `entry` = 40681;
