b86d52f8bff4ffa5bd8f6a24b312a254625fc522
AzerothCoreBot <azerothcorebot@gmail.com>
2019-02-24 17:37:19 +0000

    Import pending SQL update file

--- data/sql/updates/db_world/2019_02_24_00.sql
+++ data/sql/updates/db_world/2019_02_24_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_02_23_00 -> 2019_02_24_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_02_23_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_02_23_00 2019_02_24_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1549320792563626100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1549320792563626100');
 
 DELETE FROM `command` WHERE `name`='taxicheat';
@@ -12,3 +28,12 @@ INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('cheat power',     2, 'Syntax: .cheat power [on/off]\r\nRemove spells\' cost (mana, energy, rage...).'),
 ('cheat waterwalk', 2, 'Syntax: .cheat waterwalk on/off\r\nAllow to walk on water (self or selected character).'),
 ('cheat taxi',      2, 'Syntax: .cheat taxi on/off\r\nTemporary grant access to all taxi routes for the selected character.\r\n If no character is selected, hide or reveal all routes to you. Visited taxi nodes are still accessible after removing access.');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
