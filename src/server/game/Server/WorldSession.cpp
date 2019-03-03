be0b7b23f8ed30c0980e75db14b641ca4efae822
Francesco Borzì <borzifrancesco@gmail.com>
2019-03-03 15:26:07 +0100

    feat(core): allow to disable auto-kick for idle players (#1550)

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -254,7 +254,7 @@ bool WorldSession::Update(uint32 diff, PacketFilter& updater)
         
         /// If necessary, kick the player because the client didn't send anything for too long
         /// (or they've been idling in character select)
-        if (IsConnectionIdle())
+        if (sWorld->getBoolConfig(CONFIG_CLOSE_IDLE_CONNECTIONS) && IsConnectionIdle())
             m_Socket->CloseSocket();
     }
 
