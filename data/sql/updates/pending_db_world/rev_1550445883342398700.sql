824f304db7f9762db30b94fa2b9cda04a179b982
AzerothCoreBot <azerothcorebot@gmail.com>
2019-03-03 11:35:07 +0000

    Import pending SQL update file...
    Referenced commit(s): 65251a2b4e9eb5939a096e895a11e3eef5035723

--- data/sql/updates/pending_db_world/rev_1550445883342398700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1550445883342398700');
-
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (33701,3370100);
-UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 33701;
