92f91ad39868901cfff4ec87ca9e409058fdd4ed
milestorme <mic_24@live.com>
2019-03-04 17:38:43 +1100

    Eluna Update

--- modules/mod-eluna-lua-engine/cmake/after_load_conf.cmake
+++ modules/mod-eluna-lua-engine/cmake/after_load_conf.cmake
@@ -7,5 +7,7 @@ CU_ADD_HOOK(AFTER_GAME_LIBRARY "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/after_gs_ins
 CU_ADD_HOOK(BEFORE_SCRIPTS_LIBRARY "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/before_script_install.cmake")
 CU_ADD_HOOK(AFTER_WORLDSERVER_CMAKE "${CMAKE_MOD_ELUNA_ENGINE_DIR}/cmake/after_ws_install.cmake")
 
+AC_ADD_CONFIG_FILE("${CMAKE_MOD_ELUNA_ENGINE_DIR}/conf/mod_LuaEngine.conf.dist")
+
 message("** [Eluna Module] LuaEngine is enable!")
 
--- modules/mod-eluna-lua-engine/cmake/after_ws_install.cmake
+++ modules/mod-eluna-lua-engine/cmake/after_ws_install.cmake
@@ -4,19 +4,3 @@ include_directories(
   ${CMAKE_MOD_ELUNA_ENGINE_DIR}/LuaEngine
   ${CMAKE_MOD_ELUNA_ENGINE_DIR}/lualib
 )
-
-if( WIN32 )
-  if ( MSVC )
-    add_custom_command(TARGET worldserver
-      POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_ELUNA_ENGINE_DIR}/conf/mod_LuaEngine.conf.dist" ${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/
-    )
-  elseif ( MINGW )
-    add_custom_command(TARGET worldserver
-      POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_MOD_ELUNA_ENGINE_DIR}/conf/mod_LuaEngine.conf.dist" ${CMAKE_BINARY_DIR}/bin/
-    )
-  endif()
-endif()
-
-install(FILES "${CMAKE_MOD_ELUNA_ENGINE_DIR}/conf/mod_LuaEngine.conf.dist" DESTINATION ${CONF_DIR})
