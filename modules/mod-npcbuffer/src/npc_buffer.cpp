718a06bc2cf29cbb141f23cd17870cdfaf3f48ca
milestorme <mic_24@live.com>
2019-03-06 19:39:18 +1100

    updating buff_npc

--- modules/mod-npcbuffer/src/npc_buffer.cpp
+++ modules/mod-npcbuffer/src/npc_buffer.cpp
@@ -1,66 +1,77 @@
 /*
 
-# Buffer NPC #
+# Buffer NPC
 
-### Description ###
+_This module was created for [StygianCore](https://rebrand.ly/stygiancoreproject). A World of Warcraft 3.3.5a Solo/LAN repack by StygianTheBest | [GitHub](https://rebrand.ly/stygiangithub) | [Website](https://rebrand.ly/stygianthebest))_
+
+### Description
 ------------------------------------------------------------------------------------------------------------------
-This is a one-click buffing NPC that will buff the player with a specific set of spells. The NPC can also
-buff everyone the same or by player level. He speaks a random phrase after every use.
+This is a one-click buffing NPC that will buff the player with a specific set of spells. The NPC can also buff
+everyone the same or by player level. He speaks a configurable random phrase after every use and can also attract
+the player using configurable emote options.
 
 - Creates a Buff NPC with emotes
 - Buffs the player with no dialogue interaction
 - Buffs all the same or by player level
-- Speaks a random phrase after every use
-- Config: Spell ID(s) for buffs
-- Config: Enable/Disable cure resurrection sickness
+- Speaks configurable random whispers to the player after every use
+- Attracts the player using configurable emotes
+- Config:
+    - Buff by level
+    - Spell ID(s) for buffs
+    - Enable/Disable cure resurrection sickness
+    - Emote Options
 
-
-### Data ###
+### Data
 ------------------------------------------------------------------------------------------------------------------
 - Type: NPC (ID: 601016)
 - Script: buff_npc
 - Config: Yes
 - SQL: Yes
 
-
-### Version ###
+### Version
 ------------------------------------------------------------------------------------------------------------------
-- v2017.08.05
+- v2019.02.13 - Added phrases/emotes, config options, updated AI
 - v2017.08.06 - Removed dialogue options (Just buffs player on click)
+- v2017.08.05
 
-
-### Credits ###
+### CREDITS
 ------------------------------------------------------------------------------------------------------------------
-#### A module for AzerothCore by StygianTheBest ([stygianthebest.github.io](http://stygianthebest.github.io)) ####
+![Styx](https://stygianthebest.github.io/assets/img/avatar/avatar-128.jpg "Styx")
+![StygianCore](https://stygianthebest.github.io/assets/img/projects/stygiancore/StygianCore.png "StygianCore")
 
-###### Additional Credits include:
+##### This module was created for [StygianCore](https://rebrand.ly/stygiancoreproject). A World of Warcraft 3.3.5a Solo/LAN repack by StygianTheBest | [GitHub](https://rebrand.ly/stygiangithub) | [Website](https://rebrand.ly/stygianthebest))
+
+#### Additional Credits
+
 - [Blizzard Entertainment](http://blizzard.com)
 - [TrinityCore](https://github.com/TrinityCore/TrinityCore/blob/3.3.5/THANKS)
 - [SunwellCore](http://www.azerothcore.org/pages/sunwell.pl/)
 - [AzerothCore](https://github.com/AzerothCore/azerothcore-wotlk/graphs/contributors)
-- [AzerothCore Discord](https://discord.gg/gkt4y2x)
-- [EMUDevs](https://youtube.com/user/EmuDevs)
-- [AC-Web](http://ac-web.org/)
-- [ModCraft.io](http://modcraft.io/)
-- [OwnedCore](http://ownedcore.com/)
 - [OregonCore](https://wiki.oregon-core.net/)
 - [Wowhead.com](http://wowhead.com)
+- [OwnedCore](http://ownedcore.com/)
+- [ModCraft.io](http://modcraft.io/)
+- [MMO Society](https://www.mmo-society.com/)
 - [AoWoW](https://wotlk.evowow.com/)
+- [More credits are cited in the sources](https://github.com/StygianTheBest)
 
-
-### License ###
+### LICENSE
 ------------------------------------------------------------------------------------------------------------------
-- This code and content is released under the [GNU AGPL v3](https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3).
+This code and content is released under the [GNU AGPL v3](https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3).
 
 */
 
 #include "Config.h"
 #include "ScriptPCH.h"
 
-bool BufferAnnounceModule = 1;
-bool BuffByLevel = 1;
-bool BuffCureRes = 1;
-uint32 NumPhrases = 10;
+bool BFAnnounceModule;
+bool BuffByLevel;
+bool BuffCureRes;
+uint32 BuffNumPhrases;
+uint32 BuffNumWhispers;
+uint32 BuffMessageTimer;
+uint32 BuffEmoteSpell;
+uint32 BuffEmoteCommand;
 
 class BufferConfig : public WorldScript
 {
@@ -79,10 +90,23 @@ public:
             sConfigMgr->LoadMore(cfg_def_file.c_str());
             sConfigMgr->LoadMore(cfg_file.c_str());
 
-            BufferAnnounceModule = sConfigMgr->GetBoolDefault("BufferNPC.Announce", 1);
+            BFAnnounceModule = sConfigMgr->GetBoolDefault("Buff.Announce", 1);
             BuffByLevel = sConfigMgr->GetBoolDefault("Buff.ByLevel", 1);
             BuffCureRes = sConfigMgr->GetBoolDefault("Buff.CureRes", 1);
-            NumPhrases = sConfigMgr->GetIntDefault("Buff.NumPhrases", 10);
+            BuffNumPhrases = sConfigMgr->GetIntDefault("Buff.NumPhrases", 3);
+            BuffNumWhispers = sConfigMgr->GetIntDefault("Buff.NumWhispers", 3);
+            BuffMessageTimer = sConfigMgr->GetIntDefault("Buff.MessageTimer", 60000);
+            BuffEmoteSpell = sConfigMgr->GetIntDefault("Buff.EmoteSpell", 44940);
+            BuffEmoteCommand = sConfigMgr->GetIntDefault("Buff.EmoteCommand", 3);
+
+            // Enforce Min/Max Time
+            if (BuffMessageTimer != 0)
+            {
+                if (BuffMessageTimer < 60000 || BuffMessageTimer > 300000)
+                {
+                    BuffMessageTimer = 60000;
+                }
+            }
         }
     }
 };
@@ -97,9 +121,9 @@ public:
     void OnLogin(Player* player)
     {
         // Announce Module
-        if (BufferAnnounceModule)
+        if (BFAnnounceModule)
         {
-            ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00BufferNPC |rmodule.");
+            ChatHandler(player->GetSession()).SendSysMessage("");
         }
     }
 };
@@ -111,7 +135,7 @@ public:
 
     buff_npc() : CreatureScript("buff_npc") { }
 
-    bool replace(std::string& str, const std::string& from, const std::string& to)
+    static bool replace(std::string& str, const std::string& from, const std::string& to)
     {
         size_t start_pos = str.find(from);
         if (start_pos == std::string::npos)
@@ -120,9 +144,47 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player * player, Creature * creature, uint32 /*uiSender*/, uint32 uiAction)
+    static string PickWhisper(string Name)
+    {
+        // Choose and speak a random phrase to the player
+        // Phrases are stored in the config file
+        std::string whisper = "";
+        uint32 WhisperNum = urand(1, BuffNumWhispers); // How many phrases does the NPC speak? 
+        whisper = "BF.W" + std::to_string(WhisperNum);
+
+        // Sanitize
+        if (whisper == "")
+        {
+            whisper = "ERROR! NPC Emote Text Not Found! Check the npc_buffer.conf!";
+        }
+
+        std::string randMsg = sConfigMgr->GetStringDefault(whisper.c_str(), "");
+        replace(randMsg, "%s", Name);
+        return randMsg.c_str();
+    }
+
+    static string PickPhrase()
+    {
+        // Choose and speak a random phrase to the player
+        // Phrases are stored in the config file
+        std::string phrase = "";
+        uint32 PhraseNum = urand(1, BuffNumPhrases); // How many phrases does the NPC speak? 
+        phrase = "BF.P" + std::to_string(PhraseNum);
+
+        // Sanitize
+        if (phrase == "")
+        {
+            phrase = "ERROR! NPC Emote Text Not Found! Check the npc_buffer.conf!";
+        }
+
+        std::string randMsg = sConfigMgr->GetStringDefault(phrase.c_str(), "");
+        return randMsg.c_str();
+    }
+
+    bool OnGossipSelect(Player *player, Creature *creature, uint32 /*uiSender*/, uint32 uiAction)
     {
         // Who are we dealing with?
+        string CreatureWhisper = "Init";
         string PlayerName = player->GetName();
         uint32 PlayerLevel = player->getLevel();
 
@@ -138,7 +200,6 @@ public:
         if (BuffCureRes && player->HasAura(15007))
         {
             player->RemoveAura(15007, true);
-
             std::ostringstream res;
             res << "The aura of death has been lifted from you " << PlayerName << ". Watch yourself out there!";
             creature->MonsterWhisper(res.str().c_str(), player);
@@ -184,9 +245,9 @@ public:
             else if (PlayerLevel >= 40 && PlayerLevel < 50)
             {
                 player->CastSpell(player, 21562, true); // Prayer of Fortitude (Rank 1)
-                player->CastSpell(player, vecBuffs[3], true); //Mark of the Wild(48469)
+                player->CastSpell(player, 48469, true); //Mark of the Wild(48469)
                 player->CastSpell(player, 27681, true); // Prayer of Spirit (Rank 1)
-                player->CastSpell(player, vecBuffs[5], true); //Prayer of Shadow Protection(48170)
+                player->CastSpell(player, 48170, true); //Prayer of Shadow Protection(48170)
                 player->CastSpell(player, 13326, true);  // Arcane Intellect (Rank 1)
 
             } // 40-49
@@ -198,7 +259,7 @@ public:
                 player->CastSpell(player, vecBuffs[4], true); //Prayer of Spirit(48074)
                 player->CastSpell(player, vecBuffs[5], true); //Prayer of Shadow Protection(48170)
                 player->CastSpell(player, vecBuffs[6], true); //Arcane Intellect(36880)
-                                              
+
             } // 50-59
             else if (PlayerLevel >= 60 && PlayerLevel < 70)
             {
@@ -218,7 +279,7 @@ public:
                 player->CastSpell(player, vecBuffs[4], true); //Prayer of Spirit(48074)
                 player->CastSpell(player, vecBuffs[5], true); //Prayer of Shadow Protection(48170)
                 player->CastSpell(player, vecBuffs[6], true); //Arcane Intellect(36880)
-                                         
+
             } // 70-79
             else
             {
@@ -242,12 +303,7 @@ public:
 
         // Choose and speak a random phrase to the player
         // Phrases are stored in the config file
-        std::string phrase = "";
-        uint32 PhraseNum = urand(1, NumPhrases); // How many phrases does the NPC speak? 
-        phrase = "Buff.M" + std::to_string(PhraseNum);
-        std::string randMsg = sConfigMgr->GetStringDefault(phrase.c_str(), "");
-        replace(randMsg, "%s", PlayerName);
-        creature->MonsterWhisper(randMsg.c_str(), player, false);
+        creature->MonsterWhisper(PickWhisper(PlayerName).c_str(), player);
 
         // Emote and Close
         creature->HandleEmoteCommand(EMOTE_ONESHOT_FLEX);
@@ -256,58 +312,48 @@ public:
     }
 
     // Passive Emotes
-    struct buffer_passivesAI : public ScriptedAI
+    struct NPC_PassiveAI : public ScriptedAI
     {
-        buffer_passivesAI(Creature * creature) : ScriptedAI(creature) { }
+        NPC_PassiveAI(Creature * creature) : ScriptedAI(creature) { }
 
-        uint32 uiAdATimer;
-        uint32 uiAdBTimer;
-        uint32 uiAdCTimer;
+        uint32 MessageTimer;
 
+        // Called once when client is loaded
         void Reset()
         {
-            uiAdATimer = 25000;
-            uiAdBTimer = 50000;
-            uiAdCTimer = 75000;
+            MessageTimer = urand(BuffMessageTimer, 300000); // 1-5 minutes
         }
 
+        // Called at World update tick
         void UpdateAI(const uint32 diff)
         {
-
-            if (uiAdATimer <= diff)
+            if (MessageTimer <= diff)
             {
-                me->MonsterSay("You know.. I visited the Sunwell once. Long ago.", LANG_UNIVERSAL, NULL);
-                me->HandleEmoteCommand(EMOTE_ONESHOT_CRY);
-                me->CastSpell(me, 44940);
-                uiAdATimer = 150000;
-            }
-            else
-                uiAdATimer -= diff;
+                std::string Message = PickPhrase();
+                me->MonsterSay(Message.c_str(), LANG_UNIVERSAL, NULL);
 
-            if (uiAdBTimer <= diff)
-            {
-                me->MonsterSay("Good day to you traveller.", LANG_UNIVERSAL, NULL);
-                me->HandleEmoteCommand(EMOTE_ONESHOT_WAVE);
-                uiAdBTimer = 150000;
-            }
-            else
-                uiAdBTimer -= diff;
+                // Use gesture?
+                if (BuffEmoteCommand != 0)
+                {
+                    me->HandleEmoteCommand(BuffEmoteCommand);
+                }
 
-            if (uiAdCTimer <= diff)
-            {
-                me->MonsterSay("Mr. Grubbs is the toughest grub around. No doubt about it.", LANG_UNIVERSAL, NULL);
-                me->HandleEmoteCommand(EMOTE_ONESHOT_EXCLAMATION);
-                uiAdCTimer = 150000;
+                // Alert players?
+                if (BuffEmoteSpell != 0)
+                {
+                    me->CastSpell(me, BuffEmoteSpell);
+                }
+
+                MessageTimer = urand(BuffMessageTimer, 300000);
             }
-            else
-                uiAdCTimer -= diff;
+            else { MessageTimer -= diff; }
         }
     };
 
     // CREATURE AI
     CreatureAI * GetAI(Creature * creature) const
     {
-        return new buffer_passivesAI(creature);
+        return new NPC_PassiveAI(creature);
     }
 };
 
