002c004106d3d8c77c19de3831488aa14cfcb03d
Ercules76 <sgrittino@alice.it>
2019-03-07 22:20:02 +0100

    fix(core): prevent unsupported-by-client passwords (#1547)

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -876,8 +876,9 @@ enum TrinityStrings
     LANG_SQLDRIVER_QUERY_LOGGING_ENABLED  = 1027,
     LANG_SQLDRIVER_QUERY_LOGGING_DISABLED = 1028,
     // 1029-1030 used in other client versions
-
     // Room for more level 4              1031-1099 not used
+    LANG_ACCOUNT_PASS_TOO_LONG            = 1031,
+
 
     // Level 3 (continue)
     LANG_ACCOUNT_SETADDON               = 1100,
