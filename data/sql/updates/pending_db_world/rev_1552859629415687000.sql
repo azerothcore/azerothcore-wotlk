1f75a5571a20b9e2a36fd25b0a76133d199db637
Poszer <41213210+poszer@users.noreply.github.com>
2019-03-17 23:32:05 +0100

    Update rev_1552859629415687000.sql

--- data/sql/updates/pending_db_world/rev_1552859629415687000.sql
+++ data/sql/updates/pending_db_world/rev_1552859629415687000.sql
@@ -1,3 +1,4 @@
 INSERT INTO version_db_world (`sql_rev`) VALUES ('1552859629415687000');
 
+DELETE FROM `creature_loot_template` WHERE `Entry` = 31691 AND `Item` = 33470;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (31691, 33470, 0, 100, 0, 1, 0, 1, 7, NULL);
