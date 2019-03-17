2d748c9157dbde97dc4498d00577ae217dbd3e02
Poszer <41213210+poszer@users.noreply.github.com>
2019-03-17 22:57:11 +0100

    DB: Faceless Lurker drop
    The creature should drop Frostweave Cloth with drop chance 100

---
+++ data/sql/updates/pending_db_world/rev_1552859629415687000.sql
@@ -0,0 +1,3 @@
+INSERT INTO version_db_world (`sql_rev`) VALUES ('1552859629415687000');
+
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (31691, 33470, 0, 100, 0, 1, 0, 1, 7, NULL);
