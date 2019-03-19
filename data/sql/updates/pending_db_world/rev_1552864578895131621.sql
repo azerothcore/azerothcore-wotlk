758439084f1390873fe2724b0e2eaa2ec25d7a5e
AzerothCoreBot <azerothcorebot@gmail.com>
2019-03-19 21:19:57 +0000

    Import pending SQL update file...
    Referenced commit(s): a4fcf926feceb46f7ab949259eb6371abb346da2

--- data/sql/updates/pending_db_world/rev_1552864578895131621.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1552864578895131621');
-
-DELETE FROM `creature_template_addon` WHERE `entry` = 24747;
-DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` = 24747 AND `MovementType` = 0);
-INSERT INTO `creature_addon` (`guid`,`bytes1`) SELECT `guid`, 1 as `bytes1` FROM `creature` WHERE `id` = 24747 AND `MovementType` = 0;
-
-UPDATE `creature` SET `spawndist` = 5, `MovementType` = 1 WHERE `id` = 24746;
