feb6776928ec6cb65db4bcb943b9171a49e269a5
Poszer <41213210+poszer@users.noreply.github.com>
2019-03-20 18:37:41 +0100

    Update rev_1553042372568459200.sql
    Added charges to spell id 44875 too

--- data/sql/updates/pending_db_world/rev_1553042372568459200.sql
+++ data/sql/updates/pending_db_world/rev_1553042372568459200.sql
@@ -1,5 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553042372568459200');
 
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44875;
 UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44874;
 UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44872;
 UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44873;
