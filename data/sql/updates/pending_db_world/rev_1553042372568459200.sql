655934af38ada0e3acea99f3c0c62c3ae0d22a74
Poszer <41213210+poszer@users.noreply.github.com>
2019-03-20 01:42:34 +0100

    DB: Item Enhancements (Permanent)
    These enchancements should be available to use just one time.
    After used, they will be removed from bag.
    - Added spell charges

---
+++ data/sql/updates/pending_db_world/rev_1553042372568459200.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553042372568459200');
+
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44874;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44872;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44873;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44871;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44879;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44878;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44877;
+UPDATE `item_template` SET `spellcharges_1`='-1' WHERE `entry`=44876;
