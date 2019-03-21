c6401dff742e2e0da49dcd9cd916317da165f68c
Francesco Borzì <borzifrancesco@gmail.com>
2019-03-21 09:14:07 +0100

    Switch to C++14 to solve compilation issues (#1595)

--- src/cmake/compiler/gcc/settings.cmake
+++ src/cmake/compiler/gcc/settings.cmake
@@ -7,8 +7,8 @@ if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS GCC_EXPECTED_VERSION)
   message(FATAL_ERROR "GCC: This project requires version ${GCC_EXPECTED_VERSION} to build but found ${CMAKE_CXX_COMPILER_VERSION}")
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z")
-message(STATUS "GCC: Enabled c++17 support")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+message(STATUS "GCC: Enabled c++14 support")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99")
 message(STATUS "GCC: Enabled C99 support")
