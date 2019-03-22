2dcf7d6205b621c6f8e8ed823d1d2d7c52df0c65
WiZZy <bilucristian25@gmail.com>
2019-03-22 14:54:48 +0000

    Fixing copying mistakes *facepalm*

--- data/sql/create/create_mysql.sql
+++ data/sql/create/create_mysql.sql
@@ -2,12 +2,12 @@ GRANT USAGE ON * . * TO 'acore'@'localhost' IDENTIFIED BY 'acore' WITH MAX_QUERI
 
 CREATE DATABASE `acore_world` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE DATABASE `acore_worldcharacters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `acore_characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE DATABASE `acore_worldauth` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `acore_auth` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON `acore_worldworld` . * TO 'acore'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `acore_world` . * TO 'acore'@'localhost' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON `acore_worldcharacters` . * TO 'acore'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `acore_characters` . * TO 'acore'@'localhost' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON `acore_worldauth` . * TO 'acore'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `acore_auth` . * TO 'acore'@'localhost' WITH GRANT OPTION;
