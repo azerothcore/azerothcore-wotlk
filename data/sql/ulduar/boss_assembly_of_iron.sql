41c309d8b4214fbb137bde463c513b9cecd5b43f
Rooter <Slipknot500.slipknot@yandex.ru>
2019-03-24 16:05:03 +0300

    fix sql req

--- data/sql/ulduar/boss_assembly_of_iron.sql
+++
@@ -1,2 +0,0 @@
--- Fix Rune of Summoning
-UPDATE `creature_template` SET `entry`='33051', `difficulty_entry_1`='0' WHERE (`entry`='33051');
---
+++ data/sql/updates/pending_db_world/rev_1553432484507441600.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553432484507441600');
+
+-- Fix Rune of Summoning
+UPDATE `creature_template` SET `entry`='33051', `difficulty_entry_1`='0' WHERE (`entry`='33051');
