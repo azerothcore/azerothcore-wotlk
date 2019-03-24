938fb010a8e73e0aa6b192029c8df25848be8056
Viste <viste02@gmail.com>
2019-03-24 15:50:26 +0300

    delete before insert

--- data/sql/updates/pending_db_world/rev_1553390628412350005.sql
+++ data/sql/updates/pending_db_world/rev_1553390628412350005.sql
@@ -1,3 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553390628412350005');
 
+DELETE FROM `spell_script_names` WHERE spell_id=63414;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (63414, 'spell_mimiron_spinning_up');
