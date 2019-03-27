cc3b1f1f0de57912ef7bd39516639c626f1cc7b9
Poszer <41213210+poszer@users.noreply.github.com>
2019-03-27 22:23:07 +0100

    DB/Quest: Data Rescue update
    All in one query

--- data/sql/updates/pending_db_world/rev_1553712779275952300.sql
+++ data/sql/updates/pending_db_world/rev_1553712779275952300.sql
@@ -1,7 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553712779275952300');
 
 DELETE FROM conditions WHERE SourceTypeOrReferenceId=15 AND SourceGroup=1050;
-INSERT INTO conditions VALUES (15, 1050, 0, 0, 0, 2, 0, 9281, 1, 0, 0, 0, 0, '', 'Display option if player has red card');
-INSERT INTO conditions VALUES (15, 1050, 1, 0, 0, 2, 0, 9281, 1, 0, 0, 0, 0, '', 'Display option if player has red card');
-INSERT INTO conditions VALUES (15, 1050, 1, 0, 0, 2, 0, 9327, 1, 0, 0, 0, 0, '', 'Display option if player has delta card');
-INSERT INTO conditions VALUES (15, 1050, 1, 0, 0, 25, 0, 3959, 0, 0, 1, 0, 0, '', 'Display option if player has no Discombobulator Ray spell');
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(15, 1050, 0, 0, 0, 2, 0, 9281, 1, 0, 0, 0, 0, '', 'Display option if player has red card'),
+(15, 1050, 1, 0, 0, 2, 0, 9281, 1, 0, 0, 0, 0, '', 'Display option if player has red card'),
+(15, 1050, 1, 0, 0, 2, 0, 9327, 1, 0, 0, 0, 0, '', 'Display option if player has delta card'),
+(15, 1050, 1, 0, 0, 25, 0, 3959, 0, 0, 1, 0, 0, '', 'Display option if player has no Discombobulator Ray spell');
+
