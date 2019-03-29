ca682d2f17b579859fbd2467d2d1f6c40b3b1391
Alan <sr.alansantos@hotmail.com>
2019-03-29 10:27:16 -0300

    added npc Spirit of Udalo
     Spirit of Udalo also has the teleport to the Command Chamber.

--- data/sql/updates/pending_db_world/rev_1553769607243300300.sql
+++ data/sql/updates/pending_db_world/rev_1553769607243300300.sql
@@ -1,5 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553769607243300300');
 
+-- Spirit of Olum SAI
 DELETE FROM `gossip_menu` WHERE `MenuID`=8750 AND `TextID`=11082;
 INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES (8750, 11082);
 
@@ -25,3 +26,18 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 (15, 8750, 2, 0, 0, 13, 1, 7, 3, 2, 0, 0, 0, '', 'Show options for gossip only if COUNCIL done'),
 (14, 8750, 11082, 0, 0, 13, 1, 1, 3, 2, 1, 0, 0, '', 'Show gossip text only if SUPREMUS not done'),
 (14, 8750, 11081, 0, 0, 13, 1, 1, 3, 2, 0, 0, 0, '', 'Show gossip text only if SUPREMUS done');
+
+-- Spirit of Udalo SAI
+DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (8749) AND `OptionID`=1;
+INSERT INTO `gossip_menu_option` (`MenuID`,`OptionID`,`OptionIcon`,`OptionText`, `OptionBroadcastTextID`, `OptionType`,`OptionNpcFlag`,`ActionMenuID`) VALUES
+(8749,1,0,"I'm ready. Take me to the Chamber of Command.",21879,1,1,0); 
+
+uPDATE `creature_template` SET `ScriptName`='', `AIName`='SmartAI' WHERE `entry`=23410;
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (23410) AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(23410,0,0,1,62,0,100,0,8749,1,0,0,85,41570,0,0,0,0,0,7,0,0,0,0,0,0,0,"Spirit of Udalo - On gossip select - cast spell"),
+(23410,0,1,0,61,0,100,0,0,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,"Spirit of Udalo - On gossip select - Close gossip");
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` IN (15) AND `SourceGroup`=8749;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(15, 8749, 1, 0, 0, 13, 1, 7, 3, 2, 0, 0, 0, '', 'Show options for gossip only if COUNCIL done');
