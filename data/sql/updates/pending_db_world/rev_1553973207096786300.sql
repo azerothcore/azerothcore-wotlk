73a4b16a89a84dc7b31b9df56c578e82b552c83f
Poszer <41213210+poszer@users.noreply.github.com>
2019-03-30 21:07:02 +0100

    Update rev_1553973207096786300.sql

--- data/sql/updates/pending_db_world/rev_1553973207096786300.sql
+++ data/sql/updates/pending_db_world/rev_1553973207096786300.sql
@@ -1,13 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1553973207096786300');
 
-DELETE FROM `gameobject_loot_template` WHERE `Entry`=18092 AND `Item`=2835;
-INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (18092, 2835, 0, 88, 0, 1, 0, 1, 6, NULL);
-
-DELETE FROM `gameobject_loot_template` WHERE `Entry`=1502 AND `Item`=2835;
-INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1502, 2835, 0, 80, 0, 1, 0, 1, 11, NULL);
-
-DELETE FROM `gameobject_loot_template` WHERE `Entry`=1735 AND `Item`=2835;
-INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1735, 2835, 0, 80, 0, 1, 0, 1, 6, NULL);
-
-DELETE FROM `gameobject_loot_template` WHERE `Entry`=2626 AND `Item`=2835;
-INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (2626, 2835, 0, 80, 0, 1, 0, 1, 6, NULL);
+DELETE FROM gameobject_loot_template WHERE Item=2835 AND Entry IN (18092, 1502, 1735, 2626);
+INSERT INTO gameobject_loot_template (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment) VALUES
+(18092, 2835, 0, 88, 0, 1, 0, 1, 6, NULL);
+(1502, 2835, 0, 80, 0, 1, 0, 1, 11, NULL);
+(1735, 2835, 0, 80, 0, 1, 0, 1, 6, NULL);
+(2626, 2835, 0, 80, 0, 1, 0, 1, 6, NULL);
