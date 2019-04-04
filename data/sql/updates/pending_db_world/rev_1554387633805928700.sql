1629c4f48394877b357f083a53f220483022b786
WiZZy <bilucristian25@gmail.com>
2019-04-04 17:47:40 +0300

    Update rev_1554387633805928700.sql

--- data/sql/updates/pending_db_world/rev_1554387633805928700.sql
+++ data/sql/updates/pending_db_world/rev_1554387633805928700.sql
@@ -12,3 +12,10 @@ UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=23945;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=23945;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (23945, 0, 0, 0, 8, 0, 100, 0, 42767, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Fjord Crow - On Spellhit 'Sic'em' - Attack Invoker");
+
+-- Creature Pontius 23938 SAI
+SET @ENTRY := 23938;
+UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`= @ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 20, 0, 100, 0, 11227, 0, 0, 0, 41, 1500, 0, 0, 0, 0, 0, 9, 23943, 0, 10, 0, 0, 0, 0, "When player rewards quest 11227 - Creature Hungry Plaguehound (23943) in 0 - 10 yards: Despawn in 1500 ms // ");
