9105f6b5d6b7e7533d1887a2d07003ddca7aa116
Lee <talamortis@gmail.com>
2019-04-04 12:02:55 +0100

    update SQL

--- data/sql/updates/pending_db_world/rev_1554339238034040500.sql
+++ data/sql/updates/pending_db_world/rev_1554339238034040500.sql
@@ -2,8 +2,8 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554339238034040500');
 
 DELETE FROM `creature_text` WHERE `entry` = '22252';
 
-INSERT INTO `creature_text` VALUES (22252, 0, 0, 'Me so hungry! YUM!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon Say1');
-INSERT INTO `creature_text` VALUES (22252, 0, 1, 'It put the mutton in the stomach!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_2');
-INSERT INTO `creature_text` VALUES (22252, 0, 2, 'Mmmm! FOOD!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_3');
-INSERT INTO `creature_text` VALUES (22252, 0, 3, 'Time for eating?', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_4');
-INSERT INTO `creature_text` VALUES (22252, 1, 4, 'Hey...me not feel so good.', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_POISON');
+INSERT INTO `creature_text` VALUES (22252, 0, 0, 'Me so hungry! YUM!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon Say1'),
+(22252, 0, 1, 'It put the mutton in the stomach!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_2'),
+(22252, 0, 2, 'Mmmm! FOOD!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_3'),
+(22252, 0, 3, 'Time for eating?', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_4'),
+(22252, 1, 4, 'Hey...me not feel so good.', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_POISON');
