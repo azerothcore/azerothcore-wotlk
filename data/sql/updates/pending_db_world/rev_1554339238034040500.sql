0fd83ad4fbcc18e628dc14fb313d2e3653e2351d
Lee <talamortis@gmail.com>
2019-04-04 11:19:15 +0100

    Update SQL
    Updated SQL delete if rows are found

--- data/sql/updates/pending_db_world/rev_1554339238034040500.sql
+++ data/sql/updates/pending_db_world/rev_1554339238034040500.sql
@@ -1,5 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554339238034040500');
 
+DELETE FROM `creature_text` WHERE `entry` = '22252';
+
 INSERT INTO `creature_text` VALUES (22252, 0, 0, 'Me so hungry! YUM!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon Say1');
 INSERT INTO `creature_text` VALUES (22252, 0, 1, 'It put the mutton in the stomach!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_2');
 INSERT INTO `creature_text` VALUES (22252, 0, 2, 'Mmmm! FOOD!', 12, 0, 100, 0, 0, 0, 0, 0, 'Dragonmaw Peon SAY_3');
