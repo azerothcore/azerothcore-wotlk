5105333968e2f3b1e421ba739c4aac703523f873
Kargatum <dowlandtop@yandex.com>
2019-04-04 16:47:17 +0700

    feat(Core/Library): Rename shared library to common (#1675)

--- src/tools/mmaps_generator/VMapExtensions.cpp
+++ src/tools/mmaps_generator/VMapExtensions.cpp
@@ -19,26 +19,26 @@ namespace VMAP
     // Need direct access to encapsulated VMAP data, so we add functions for MMAP generator
     // maybe add MapBuilder as friend to all of the below classes would be better?
 
-    // declared in src/shared/vmap/MapTree.h
+    // declared in src/common/vmap/MapTree.h
     void StaticMapTree::getModelInstances(ModelInstance* &models, uint32 &count)
     {
         models = iTreeValues;
         count = iNTreeValues;
     }
 
-    // declared in src/shared/vmap/VMapManager2.h
+    // declared in src/common/vmap/VMapManager2.h
     void VMapManager2::getInstanceMapTree(InstanceTreeMap &instanceMapTree)
     {
         instanceMapTree = iInstanceMapTrees;
     }
 
-    // declared in src/shared/vmap/WorldModel.h
+    // declared in src/common/vmap/WorldModel.h
     void WorldModel::getGroupModels(std::vector<GroupModel> &groupModels)
     {
         groupModels = this->groupModels;
     }
 
-    // declared in src/shared/vmap/WorldModel.h
+    // declared in src/common/vmap/WorldModel.h
     void GroupModel::getMeshData(std::vector<G3D::Vector3> &vertices, std::vector<MeshTriangle> &triangles, WmoLiquid* &liquid)
     {
         vertices = this->vertices;
@@ -46,13 +46,13 @@ namespace VMAP
         liquid = iLiquid;
     }
 
-    // declared in src/shared/vmap/ModelInstance.h
+    // declared in src/common/vmap/ModelInstance.h
     WorldModel* ModelInstance::getWorldModel()
     {
         return iModel;
     }
 
-    // declared in src/shared/vmap/WorldModel.h
+    // declared in src/common/vmap/WorldModel.h
     void WmoLiquid::getPosInfo(uint32 &tilesX, uint32 &tilesY, G3D::Vector3 &corner) const
     {
         tilesX = iTilesX;
