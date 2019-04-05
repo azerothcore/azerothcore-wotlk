7d1fce235195a70432b4f2d51c83226f48cae7c3
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-05 11:53:55 +0100

    Confused min and max on creature 4648

--- data/sql/updates/pending_db_world/rev_1554420147566038300.sql
+++ data/sql/updates/pending_db_world/rev_1554420147566038300.sql
@@ -14,7 +14,7 @@ SET @ENTRY := 4648;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,9,0,100,0,10,1,3500,5000,11,5568,0,0,0,0,0,2,0,0,0,0,0,0,0,'Gelkis Stamper - Within 10-1 Range - Cast \'Trample\''),
+(@ENTRY,0,0,0,9,0,100,0,1,10,3500,5000,11,5568,0,0,0,0,0,2,0,0,0,0,0,0,0,'Gelkis Stamper - Within 1-10 Range - Cast \'Trample\''),
 (@ENTRY,0,1,0,2,0,100,1,1,10,0,0,25,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Gelkis Stamper - Between 1-10% Health - Flee For Assist (No Repeat)');
 
 -- Gelkis Scout SAI
