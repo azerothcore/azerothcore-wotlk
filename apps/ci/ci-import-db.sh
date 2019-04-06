2014610723d2c553f3e5d1cc7503584f7cf9645b
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-04-06 23:13:30 +0200

    feat(CI): Travis check modules (#1663)

--- apps/ci/ci-import-db.sh
+++ apps/ci/ci-import-db.sh
@@ -6,4 +6,10 @@ if [ "$TRAVIS_BUILD_ID" = "1" ]
 then
   echo "import DB"
   bash ./acore.sh "db-assembler" "import-all"
+  
+  if [ -s modules/mod-premium/sql/example_item_9017.sql ]
+  then
+    # if the premium module is available insert the example item or else the worldserver dry run will fail
+    mysql -u root world_$DB_RND_NAME <modules/mod-premium/sql/example_item_9017.sql
+  fi
 fi
--- apps/ci/ci-worldserver-dry-run.sh
+++ apps/ci/ci-worldserver-dry-run.sh
@@ -5,8 +5,12 @@ set -e
 if [ "$TRAVIS_BUILD_ID" = "1" ]
 then
   echo "start worldserver dry-run"
-  git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git /home/travis/build/azerothcore/azerothcore-wotlk/env/dist/data
+  git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git "$TRAVIS_BUILD_DIR/env/dist/data"
   sed -e "s/;;acore_auth/;$DB_RND_NAME;auth_$DB_RND_NAME/" -e "s/;;acore_world/;$DB_RND_NAME;world_$DB_RND_NAME/" -e "s/;;acore_characters/;$DB_RND_NAME;characters_$DB_RND_NAME/" ./data/travis/worldserver.conf >./env/dist/etc/worldserver.conf
+  cat >>./env/dist/etc/worldserver.conf <<WORLD_CONF
+DataDir = "$TRAVIS_BUILD_DIR/env/dist/data"
+LogsDir = "$TRAVIS_BUILD_DIR/env/dist/bin"
+WORLD_CONF
   ./env/dist/bin/worldserver --dry-run
   ./apps/ci/ci-error-check.sh
 fi
