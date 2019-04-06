9506d50bd6e63e451a8f98d51a9d47389276ea38
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-06 05:27:09 +0100

    missing spell scripts on db

---
+++ data/sql/updates/pending_db_world/rev_1554524766481187700.sql
@@ -0,0 +1,13 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554524766481187700');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_red_sky_effect';
+INSERT INTO `spell_script_names` VALUES
+(32111, 'spell_red_sky_effect');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_finger_of_death';
+INSERT INTO `spell_script_names` VALUES
+(31984, 'spell_finger_of_death');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hand_of_death';
+INSERT INTO `spell_script_names` VALUES
+(35354, 'spell_finger_of_death');
