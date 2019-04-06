88ddf5d29d44ed6a02f2ea41f90119d1bcdcd2c0
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-06 20:41:41 +0100

    Update rev_1554524766481187700.sql

--- data/sql/updates/pending_db_world/rev_1554524766481187700.sql
+++ data/sql/updates/pending_db_world/rev_1554524766481187700.sql
@@ -10,4 +10,4 @@ INSERT INTO `spell_script_names` VALUES
 
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hand_of_death';
 INSERT INTO `spell_script_names` VALUES
-(35354, 'spell_finger_of_death');
+(35354, 'spell_hand_of_death');
