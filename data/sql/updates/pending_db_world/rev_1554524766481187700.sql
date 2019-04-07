dc7fd282181a5d62dcbe8a61872a30006d28103e
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-07 13:36:42 +0100

    compact sql

--- data/sql/updates/pending_db_world/rev_1554524766481187700.sql
+++ data/sql/updates/pending_db_world/rev_1554524766481187700.sql
@@ -1,13 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554524766481187700');
 
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_red_sky_effect';
-INSERT INTO `spell_script_names` VALUES
-(32111, 'spell_red_sky_effect');
+DELETE FROM `spell_script_names` WHERE `entry` IN ('spell_red_sky_effect', 'spell_finger_of_death', 'spell_hand_of_death');
 
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_finger_of_death';
 INSERT INTO `spell_script_names` VALUES
-(31984, 'spell_finger_of_death');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hand_of_death';
-INSERT INTO `spell_script_names` VALUES
+(32111, 'spell_red_sky_effect'),
+(31984, 'spell_finger_of_death'),
 (35354, 'spell_hand_of_death');
