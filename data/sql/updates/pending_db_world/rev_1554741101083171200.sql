e70bec3ef4e1474f0b0b95c33750db8f238f6640
Alan <sr.alansantos@hotmail.com>
2019-04-08 14:05:56 -0300

    remove white space

--- data/sql/updates/pending_db_world/rev_1554741101083171200.sql
+++ data/sql/updates/pending_db_world/rev_1554741101083171200.sql
@@ -6,6 +6,3 @@ DELETE FROM `creature` WHERE `id` =22443;
 INSERT INTO `creature` (`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`ScriptName`,`VerifiedBuild`) VALUES
 (78793, 22443, 530, 0, 0, 1, 1, 0, 0, 2227.4599609375, 5484.31982421875, 153.772994995117, 3.3173999786377, 60, 0, 0, 7850, 0, 0, 0, 0, 0, '', 0),
 (78794, 22443, 530, 0, 0, 1, 1, 0, 0, 1931.43994140625, 5330.759765625, 154.175994873047, 5.86600017547607, 60, 0, 0, 7850, 0, 0, 0, 0, 0, '', 0);
-
-
-
