35c895bdf0d8f4b7dd5f3727e3436ed09703ff18
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-10 00:02:56 +0100

    Update rev_1554842995091120500.sql

--- data/sql/updates/pending_db_world/rev_1554842995091120500.sql
+++ data/sql/updates/pending_db_world/rev_1554842995091120500.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554842995091120500');
 
-UPDATE `world`.`creature_template` SET `ScriptName`='chromatic_elite_guard', `AIName`='' WHERE  `entry`=10814;
-DELETE FROM `world`.`smart_scripts` WHERE  `entryorguid`=10814;
+UPDATE `creature_template` SET `ScriptName`='chromatic_elite_guard', `AIName`='' WHERE  `entry`= 10814;
+DELETE FROM `smart_scripts` WHERE  `entryorguid`= 10814;
