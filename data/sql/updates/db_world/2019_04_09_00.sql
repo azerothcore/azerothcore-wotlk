1d8b3ec1ff15c127bafbd2c27580fe42ecf6cf04
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-09 17:19:03 +0000

    Import pending SQL update file...
    Referenced commit(s): edec48f1797f94b41a92d1138c11f9865dfd52a8

--- data/sql/updates/db_world/2019_04_09_00.sql
+++ data/sql/updates/db_world/2019_04_09_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_04_07_00 -> 2019_04_09_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_04_07_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_04_07_00 2019_04_09_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1554420147566038300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554420147566038300');
 
 -- Gelkis Windchaser SAI
@@ -120,3 +136,12 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,1000,4500,15000,20000,11,6533,32,0,0,0,0,2,0,0,0,0,0,0,0,'Magram Wrangler - In Combat - Cast \'Net\''),
 (@ENTRY,0,1,0,2,0,100,1,1,10,0,0,25,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Magram Wrangler - Between 1-10% Health - Flee For Assist (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
