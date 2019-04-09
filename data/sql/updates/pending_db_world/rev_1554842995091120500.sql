be96c693a461e7acf84606e4313b293c1acb2957
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-09 21:52:24 +0100

    Create rev_1554842995091120500.sql

---
+++ data/sql/updates/pending_db_world/rev_1554842995091120500.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554842995091120500');
+
+UPDATE `world`.`creature_template` SET `ScriptName`='chromatic_elite_guard', `AIName`='' WHERE  `entry`=10814;
