7679aed952e8df6f27385a27df54259519ada8bd
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-10 01:30:20 +0100

    Update rev_1554524766481187700.sql

--- data/sql/updates/pending_db_world/rev_1554524766481187700.sql
+++ data/sql/updates/pending_db_world/rev_1554524766481187700.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554524766481187700');
 
-DELETE FROM `spell_script_names` WHERE `spell_id` IN ('spell_red_sky_effect', 'spell_finger_of_death', 'spell_hand_of_death');
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (32111, 31984, 35354);
 
 INSERT INTO `spell_script_names` VALUES
 (32111, 'spell_red_sky_effect'),
