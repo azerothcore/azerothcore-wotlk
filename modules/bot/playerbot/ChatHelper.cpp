707ef428f5ae5e89746d2ca49176e9001dd0eb02
Psilovybin <me@chrisfaulkner.org>
2019-04-12 15:34:38 -0500

    removed GetTalentTabPages

--- modules/bot/playerbot/ChatHelper.cpp
+++ modules/bot/playerbot/ChatHelper.cpp
@@ -425,10 +425,10 @@ string ChatHelper::formatClass(Player* player, int spec)
         if (!talentInfo)
             continue;
 
-        uint32 const* talentTabIds = GetTalentTabPages(player->getClass());
-        if (talentInfo->TalentTab == talentTabIds[0]) c0++;
-        if (talentInfo->TalentTab == talentTabIds[1]) c1++;
-        if (talentInfo->TalentTab == talentTabIds[2]) c2++;
+        //uint32 const* talentTabIds = GetTalentTabPages(player->getClass());
+        //if (talentInfo->TalentTab == talentTabIds[0]) c0++;
+        //if (talentInfo->TalentTab == talentTabIds[1]) c1++;
+        //if (talentInfo->TalentTab == talentTabIds[2]) c2++;
     }
 
     out << (c0 ? "|h|cff00ff00" : "") << c0 << "|h|cffffffff/";
