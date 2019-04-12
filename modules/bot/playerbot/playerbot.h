71feb873db00760eb8c295ebcd0a4c0b563db103
Psilovybin <me@chrisfaulkner.org>
2019-04-12 14:30:35 -0500

    put Common.h at the top to satisfy warnings

--- modules/bot/playerbot/playerbot.h
+++ modules/bot/playerbot/playerbot.h
@@ -1,6 +1,6 @@
 #pragma once
-
-//std::vector<std::string> split(const std::string &s, char delim);
+#include "Common.h"
+std::vector<std::string> split(const std::string &s, char delim);
 #ifndef WIN32
 int strcmpi(std::string s1, std::string s2);
 #endif
@@ -10,7 +10,6 @@ int strcmpi(std::string s1, std::string s2);
 #include "../Loot/LootMgr.h"
 #include "../Entities/Creature/GossipDef.h"
 #include "../Chat/Chat.h"
-#include "Common.h"
 #include "../World/World.h"
 #include "../Spells/SpellMgr.h"
 #include "../Globals/ObjectMgr.h"
