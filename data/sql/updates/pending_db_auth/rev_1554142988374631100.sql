a0cb2c0b7c665069e3afb3565e049e8be9093b09
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-13 16:09:19 +0000

    Import pending SQL update file...
    Referenced commit(s): c1ebc70b58454a651629327ea9f30373348b1a3c

--- data/sql/updates/pending_db_auth/rev_1554142988374631100.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1554142988374631100');
-
-ALTER TABLE `account` CHANGE COLUMN `online` `online` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `last_login`;
