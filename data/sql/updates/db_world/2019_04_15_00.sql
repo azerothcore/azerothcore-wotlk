4ded62ba653f9f24392eb12fc5f899e9d8652373
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-15 21:46:47 +0000

    Import pending SQL update file...
    Referenced commit(s): 5480c1a11273cb4242b108e5bef613b5f7f0442e

--- data/sql/updates/db_world/2019_04_15_00.sql
+++ data/sql/updates/db_world/2019_04_15_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_04_14_00 -> 2019_04_15_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_04_14_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_04_14_00 2019_04_15_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1555024514010587300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555024514010587300');
 
 SET @ENTRY := 21291;
@@ -7,3 +23,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@ENTRY, 0, 0, 0, 64, 0, 100, 0, 0, 0, 0, 0, 54, 20000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Grom''tor, Son of Oronok - When player opened dialog - Give Pause path for 20000 ms'),
 (@ENTRY, 0, 1, 0, 11, 0, 100, 0, 0, 0, 0, 0, 53, 0, 21291, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Grom''tor, Son of Oronok - On respawn - Start path, walk, repeat, Passive'),
 (@ENTRY, 0, 2, 0, 1, 0, 100, 0, 10000, 30000, 240000, 240000, 80, 2129100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Grom''tor, Son of Oronok - When out of combat and timer at the beginning - Start timed action list id #2129100');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
