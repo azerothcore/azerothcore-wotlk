6895b37a83abf788eea95c43204f6b5d4e6a3e1e
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-04-15 11:03:22 +0000

    implement SCRIPT_COMMAND_MOVEMENT (35); remove SQL script

--- data/sql/updates/pending_db_world/rev_1555246241741494428.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555246241741494428');
-
-DELETE FROM `waypoint_scripts` WHERE `id` IN (8014900,8025100);
-UPDATE `waypoint_data` SET `action` = 0 WHERE `action` IN (8014900,8025100);
