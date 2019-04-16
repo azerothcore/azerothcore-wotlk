1a586072950909382951ee11ec0195c07aa2d3a9
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-16 10:37:57 +0000

    Import pending SQL update file...
    Referenced commit(s): 28bd4ce9ac35be9ff58e2a529e7bf5e4ee4eeab8

--- data/sql/updates/db_world/2019_04_16_00.sql
+++ data/sql/updates/db_world/2019_04_16_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_04_15_00 -> 2019_04_16_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_04_15_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_04_15_00 2019_04_16_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1555017745656363300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555017745656363300');
 
 -- Item should only drop from Riverpaw Mongrel (123)
@@ -81,3 +97,12 @@ INSERT INTO `creature_loot_template` (`Entry`,`Item`,`Reference`,`Chance`,`Quest
 (103, 159, 0, 5, 0, 1, 0, 1, 1, NULL), -- Refreshing Spring Water
 (103, 24073, 24073, 10, 0, 1, 0, 1, 1, NULL), -- Junk
 (103, 11111, 11111, 0.5, 0, 1, 0, 1, 1, NULL); -- Bags
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
