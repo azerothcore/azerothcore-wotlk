df9aa4acf5123d5f3c5751fef3725002448f6535
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-19 05:37:30 +0100

    Create rev_1555648621681705600.sql

---
+++ data/sql/updates/pending_db_world/rev_1555648621681705600.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555648621681705600');
+
+
+INSERT INTO spell_script_names VALUES
+(63644 , 'spell_activate_talent'),
+(63645 , 'spell_activate_talent');
