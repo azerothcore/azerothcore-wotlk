6628d6b7b29c6715054ef886b178ed2c69eb659b
Poszer <41213210+poszer@users.noreply.github.com>
2019-04-19 21:45:13 +0200

    DB/Quest: Head of Nefarian (A&H)
    - Link

--- data/sql/updates/pending_db_world/rev_1555597548140620700.sql
+++ data/sql/updates/pending_db_world/rev_1555597548140620700.sql
@@ -31,7 +31,7 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=9;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=14392 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (14392,0,0,1,20,0,100,0,7784,0,0,0,80,1439200,2,0,0,0,0,1,0,0,0,0,0,0,0,'Overlord Runthak - On Quest The Lord of Blackrock Finished - Run Script'),
-(14392,0,1,0,61,0,100,0,7784,0,0,0,64,1,0,0,0,0,0,7,0,0,0,0,0,0,0,'Overlord Runthak - On Quest The Lord of Blackrock Finished - Store Targetlist'),
+(14392,0,1,2,61,0,100,0,7784,0,0,0,64,1,0,0,0,0,0,7,0,0,0,0,0,0,0,'Overlord Runthak - On Quest The Lord of Blackrock Finished - Store Targetlist'),
 (14392,0,2,3,61,0,100,0,0,0,0,0,1,0,3000,0,0,0,0,1,0,0,0,0,0,0,0,'Overlord Runthak - On Quest Completed - Say Line 0'),
 (14392,0,3,4,61,0,100,0,0,0,0,0,1,1,17000,0,0,0,0,1,0,0,0,0,0,0,0,'Overlord Runthak - On Quest Completed - Say Line 1'),
 (14392,0,4,5,61,0,100,0,0,0,0,0,50,179881,5000,0,0,0,0,8,0,0,0,1540.28,-4422.19,7.0051,5.22833,'Overlord Runthak - On Quest Completed - Summon Gameobject The Severed Head of Nefarian'),
