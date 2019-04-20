2ae9c9078dcc3cd3140e59779214f7bf56f79d1e
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-20 06:53:36 +0000

    Import pending SQL update file...
    Referenced commit(s): 6b6083e07778673655856e977c58faa5aee8ba0b

--- data/sql/updates/pending_db_world/rev_1554842995091120500.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1554842995091120500');
-
-UPDATE `creature_template` SET `ScriptName`='chromatic_elite_guard', `AIName`='' WHERE  `entry`= 10814;
-DELETE FROM `smart_scripts` WHERE  `entryorguid`= 10814;
