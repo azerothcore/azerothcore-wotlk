e5085f4331039bb579696b0fa7e6c77070d109aa
Poszer <41213210+poszer@users.noreply.github.com>
2019-04-25 12:02:19 +0200

    fix(DB/SAI): Alterac Valley mobs movement
    - Added one more action to SAI (Use random movement in the radious of 10) for the next creatures:
    - Snowblind Windcaller

--- data/sql/updates/pending_db_world/rev_1556112726467897600.sql
+++ data/sql/updates/pending_db_world/rev_1556112726467897600.sql
@@ -24,3 +24,12 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,3000,5000,12000,15000,11,3589,0,0,0,0,0,2,0,0,0,0,0,0,0,'Snowblind Harpy - In Combat - Cast Deafening Screech'),
 (@ENTRY,0,1,0,1,0,100,0,0,0,0,0,89,10,0,0,0,0,0,1,0,0,0,0,0,0,0,'Snowblind Harpy - Out of Combat - Use Random Movement');
+
+-- Snowblind Windcaller SAI
+SET @ENTRY := 11675;
+UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,0,0,100,2,0,0,3400,4800,11,9532,64,0,0,0,0,2,0,0,0,0,0,0,0,'Snowblind Windcaller - Combat CMC - Cast Lightning Bolt (Normal Dungeon)'),
+(@ENTRY,0,1,0,0,0,100,2,5000,8000,12000,16000,11,9532,1,0,0,0,0,6,0,0,0,0,0,0,0,'Snowblind Windcaller - Combat - Cast Lightning Bolt (Normal Dungeon)'),
+(@ENTRY,0,2,0,1,0,100,0,0,0,0,0,89,10,0,0,0,0,0,1,0,0,0,0,0,0,0,'Snowblind Windcaled - Out of Combat - Use random movement');
