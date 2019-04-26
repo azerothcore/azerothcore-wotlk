26e67c5a1410409dfc49375d6b969b558d621361
Poszer <41213210+poszer@users.noreply.github.com>
2019-04-26 17:00:34 +0200

    fix(DB/Creature): Alterac Valley creatures factions
    - Correct faction for creatures Alterac Ram (7 - Creature / Neutral for Alliance, Hostile for Horde).
    - Correct faction for creatures Frostwolfs (7 - Creature / Neutral for Horde, Hostile for Alliance).

--- data/sql/updates/pending_db_world/rev_1556184690803184100.sql
+++ data/sql/updates/pending_db_world/rev_1556184690803184100.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556184690803184100');
 
 -- Alterac Ram faction Stormpike Guard 
-UPDATE `creature_template` SET `faction`='1216' WHERE `entry` IN (10990, 22726, 31921, 37237);
+UPDATE `creature_template` SET `faction`='7' WHERE `entry` IN (10990, 22726, 31921, 37237);
 
 -- Frostwolf faction Frostwolf Clan
-UPDATE `creature_template` SET `faction`='1214' WHERE `entry` IN (10981, 22737, 31975, 37294);
+UPDATE `creature_template` SET `faction`='7' WHERE `entry` IN (10981, 22737, 31975, 37294);
