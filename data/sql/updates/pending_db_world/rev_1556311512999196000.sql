ae7313a4d690d92235aeded85db37269c829ea5f
Vitoco <44324742+xDevICCI@users.noreply.github.com>
2019-04-26 17:04:26 -0400

    Update rev_1556311512999196000.sql

--- data/sql/updates/pending_db_world/rev_1556311512999196000.sql
+++ data/sql/updates/pending_db_world/rev_1556311512999196000.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556311512999196000');
 
-UPDATE quest_template SET `RewardFactionOverride1` =2500 WHERE `ID` = 13662;
-UPDATE quest_template SET `flags` = 1 WHERE ID =13662;
+UPDATE `quest_template` SET `RewardFactionOverride1`=2500 WHERE `ID`=13662;
+UPDATE `quest_template` SET `flags`=1 WHERE `ID`=13662;
