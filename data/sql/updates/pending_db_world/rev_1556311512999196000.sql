0c4b98e3c6024b3ab9c35b467f18dd097c1f615b
Poszer <41213210+poszer@users.noreply.github.com>
2019-04-27 15:25:41 +0200

    Update rev_1556311512999196000.sql

--- data/sql/updates/pending_db_world/rev_1556311512999196000.sql
+++ data/sql/updates/pending_db_world/rev_1556311512999196000.sql
@@ -1,5 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556311512999196000');
 
-UPDATE `quest_template` SET `RewardFactionOverride1`=2500 WHERE `ID`=13662;
-
-UPDATE `quest_template` SET `flags`=1 WHERE `ID`=13662;
+UPDATE `quest_template` SET `RewardFactionOverride1`=2500, `flags`=1 WHERE `ID`=13662;
