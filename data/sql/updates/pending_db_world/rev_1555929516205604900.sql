7480037584385be45c24e7486f1ae8ce0e5eab7d
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-27 08:22:48 +0000

    Import pending SQL update file...
    Referenced commit(s): 0af062a3643d10e38d61f0ebde9871e51bbf2316

--- data/sql/updates/pending_db_world/rev_1555929516205604900.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555929516205604900');
-
--- Coilskar defender updated flag to Only Swim and remove extra flag
-UPDATE `creature_template` SET `unit_flags`=32768, `flags_extra`=0 WHERE `entry`=19762;
