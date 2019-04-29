7faaf4b59a9e5a442a7a5c03aa373a2c3224c98f
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-29 15:51:21 +0100

    another travis

--- data/sql/updates/pending_db_world/rev_1555723394137575000.sql
+++ data/sql/updates/pending_db_world/rev_1555723394137575000.sql
@@ -1,11 +1,11 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555723394137575000');
 
 -- Update razorgore movement
-UPDATE `world`.`creature_template` SET `MovementType`='0' WHERE  `entry`=12435;
+UPDATE `creature_template` SET `MovementType`='0' WHERE  `entry`=12435;
 
 -- Remove Grethok script from SAI
 DELETE FROM `smart_scripts` WHERE `entryorguid`=12557;
-UPDATE `world`.`creature_template` SET `AIName`='', `ScriptName`='boss_grethok' WHERE  `entry`=12557;
+UPDATE `creature_template` SET `AIName`='', `ScriptName`='boss_grethok' WHERE  `entry`=12557;
 
 -- Change Razorgore's faction
-UPDATE `world`.`creature_template` SET `faction`='103' WHERE  `entry`=12435;
+UPDATE `creature_template` SET `faction`='103' WHERE  `entry`=12435;
