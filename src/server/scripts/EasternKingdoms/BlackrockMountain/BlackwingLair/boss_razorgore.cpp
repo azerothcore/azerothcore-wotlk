e57ddf32164c4d8ab3aa4d882699efd02c322bb1
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-04-29 12:15:08 +0100

    Update src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_razorgore.cpp

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_razorgore.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_razorgore.cpp
@@ -85,7 +85,7 @@ public:
 
     struct boss_razorgoreAI : public BossAI
     {
-        boss_razorgoreAI(Creature* creature) : BossAI(creature, BOSS_RAZORGORE) , summons(me), creaturesSummoned(NULL) { }
+        boss_razorgoreAI(Creature* creature) : BossAI(creature, BOSS_RAZORGORE) , summons(me), creaturesSummoned(NULL), secondPhase(NULL) { }
 
         void Reset() override
         {
