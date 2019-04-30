bb362af9269a34ac75ebab77ef65e95ac6e00bea
AzerothCoreBot <azerothcorebot@gmail.com>
2019-04-30 07:40:52 +0000

    Import pending SQL update file...
    Referenced commit(s): 7e7662b6fdc1172d4a5980c06476863f638e4ef3

--- data/sql/updates/db_world/2019_04_30_00.sql
+++ data/sql/updates/db_world/2019_04_30_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_04_28_00 -> 2019_04_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_04_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_04_28_00 2019_04_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1555843331078984800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555843331078984800');
 
 -- Leviroth SmartAI
@@ -15,3 +31,12 @@ UPDATE `creature_template` SET `InhabitType`=2 WHERE `entry`=26452;
 DELETE FROM `conditions` WHERE `SourceEntry`=47170 AND `SourceTypeOrReferenceId`=17;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `comment`) VALUES
 (17, 0, 47170, 0, 0, 29, 0, 26452, 8, 0, 0, 0, 0, "", 'Spell Impale Leviroth can only be used within 8 yards of NPC Leviroth');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
