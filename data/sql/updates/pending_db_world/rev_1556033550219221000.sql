6b92e0f50fc914481d55d2fa094b3ce667f16c6a
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-04-30 19:15:40 +0200

    Update data/sql/updates/pending_db_world/rev_1556033550219221000.sql

--- data/sql/updates/pending_db_world/rev_1556033550219221000.sql
+++ data/sql/updates/pending_db_world/rev_1556033550219221000.sql
@@ -50,6 +50,6 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 2001;
 -- Table smart_scripts
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 2001);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(2001, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 6751, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Giant Webwood Spider - On Aggro - Cast \'6751\''),
+(2001, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 11, 6752, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Giant Webwood Spider - On Respawn - Cast \'6752\''),
 (2001, 0, 1, 0, 0, 0, 100, 0, 6000, 6000, 12800, 12800, 11, 744, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Giant Webwood Spider - In Combat - Cast \'744\''),
 (2001, 0, 2, 0, 0, 0, 100, 0, 7500, 7500, 15000, 15000, 11, 745, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Giant Webwood Spider - In Combat - Cast \'745\'');
