ad742e349200d63fe83a6614796ed92bc3527dc1
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-01 23:09:46 +0200

    Bloodcursed Naga: Cast "Frost Armor" on reset instead of respawn; remove "Chilled"

--- data/sql/updates/pending_db_world/rev_1556033308686302400.sql
+++ data/sql/updates/pending_db_world/rev_1556033308686302400.sql
@@ -202,6 +202,5 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17713;
 -- Table smart_scripts
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17713);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(17713, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 11, 12544, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - On Respawn - Cast \'12544\''),
-(17713, 0, 1, 0, 0, 0, 100, 0, 1500, 1500, 28500, 28500, 11, 6136, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - In Combat - Cast \'6136\''),
-(17713, 0, 2, 0, 0, 0, 100, 0, 2500, 2500, 6700, 6700, 11, 20792, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - In Combat - Cast \'20792\'');
+(17713, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 11, 12544, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - On Reset - Cast \'12544\''),
+(17713, 0, 1, 0, 0, 0, 100, 0, 2500, 2500, 6700, 6700, 11, 20792, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - In Combat - Cast \'20792\'');
