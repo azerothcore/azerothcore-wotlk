045e1ea92376c764c5495ccc510a2d762a7cdddd
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-01 19:39:47 +0200

    Sunhawk Agent: Cast "Stealth" on reset instead of respawn

--- data/sql/updates/pending_db_world/rev_1556033308686302400.sql
+++ data/sql/updates/pending_db_world/rev_1556033308686302400.sql
@@ -173,7 +173,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17610;
 -- Table smart_scripts
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17610);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(17610, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 11, 5916, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Sunhawk Agent - On Respawn - Cast \'5916\''),
+(17610, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 11, 5916, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Sunhawk Agent - On Reset - Cast \'5916\''),
 (17610, 0, 1, 0, 0, 0, 100, 0, 2500, 2500, 8900, 8900, 11, 14873, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Sunhawk Agent - In Combat - Cast \'14873\''),
 (17610, 0, 2, 0, 0, 0, 100, 0, 12850, 12850, 35700, 35700, 11, 7159, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Sunhawk Agent - In Combat - Cast \'7159\''),
 (17610, 0, 3, 0, 0, 0, 100, 0, 15000, 15000, 87000, 87000, 11, 15691, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Sunhawk Agent - In Combat - Cast \'15691\'');
