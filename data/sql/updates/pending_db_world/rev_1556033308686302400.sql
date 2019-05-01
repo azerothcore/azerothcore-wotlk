be88b5b6011082746d25a60079b2f54dbb406156
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-01 16:35:34 +0200

    Use correct "Lash" spell for Thistle Lasher

--- data/sql/updates/pending_db_world/rev_1556033308686302400.sql
+++ data/sql/updates/pending_db_world/rev_1556033308686302400.sql
@@ -55,7 +55,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17343;
 -- Table smart_scripts
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17343);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(17343, 0, 0, 0, 0, 0, 100, 0, 1800, 1800, 8600, 8600, 11, 34644, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Thistle Lasher - In Combat - Cast \'31286\'');
+(17343, 0, 0, 0, 0, 0, 100, 0, 1800, 1800, 8600, 8600, 11, 31286, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Thistle Lasher - In Combat - Cast \'31286\'');
 
 -- Creating a new SmartAI script for [Creature] ENTRY 17352 (name: Corrupted Treant)
 
