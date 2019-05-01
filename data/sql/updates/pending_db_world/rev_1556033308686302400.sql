7a8e2485ee8cc088ea71954cb8bcb5d71ed7b526
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-01 18:15:46 +0200

    Enraged Ravager: Fix Enrage cast

--- data/sql/updates/pending_db_world/rev_1556033308686302400.sql
+++ data/sql/updates/pending_db_world/rev_1556033308686302400.sql
@@ -81,7 +81,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17527;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17527);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17527, 0, 0, 0, 0, 0, 100, 0, 5000, 5000, 25000, 25000, 11, 3242, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Enraged Ravager - In Combat - Cast \'3242\''),
-(17527, 0, 1, 0, 2, 0, 100, 0, 30, 30, 0, 0, 11, 15716, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Enraged Ravager - Between 30-30% Health - Cast \'15716\'');
+(17527, 0, 1, 0, 2, 0, 100, 0, 0, 30, 0, 0, 11, 15716, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Enraged Ravager - Between 0-30% Health - Cast \'15716\'');
 
 
 -- Creating a new SmartAI script for [Creature] ENTRY 17346 (name: Mutated Tangler)
