370d54d76ea120eb0910202fd54b8668f7f055cd
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-05-02 14:33:08 +0100

    Update rev_1555648621681705600.sql

--- data/sql/updates/pending_db_world/rev_1555648621681705600.sql
+++ data/sql/updates/pending_db_world/rev_1555648621681705600.sql
@@ -1,6 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555648621681705600');
 
-INSERT INTO spell_script_names VALUES
-(63644 , 'spell_activate_talent'),
-(63645 , 'spell_activate_talent'),
-('1515', 'spell_tame_beast');
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (63644, 63645, 1515);
+INSERT INTO `spell_script_names` VALUES
+(63644, 'spell_activate_talent'),
+(63645, 'spell_activate_talent'),
+(1515, 'spell_tame_beast');
