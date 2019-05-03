4794695674b8622180d4a8155f3adbf2b1b31d8e
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-03 01:42:24 +0000

    Import pending SQL update file...
    Referenced commit(s): 1ea4eca4a4a47af4d0d89fb71c26c135587a1ace

--- data/sql/updates/pending_db_world/rev_1555926066020418100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1555926066020418100');
-
--- Remove UNIT_FLAG_IMMUNE_TO_PC from NPC 'Ethereum guardian'
-UPDATE `creature_template` SET `unit_flags`='0' WHERE `entry`=20854;
