7188fa0f68f469666f3673a115640245cfdacfef
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-04 06:58:25 +0000

    Import pending SQL update file...
    Referenced commit(s): d46777ac982b4e4eb8aee3299714b7ac1bf82f53

--- data/sql/updates/db_world/2019_05_04_00.sql
+++ data/sql/updates/db_world/2019_05_04_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_03_00 -> 2019_05_04_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_03_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_03_00 2019_05_04_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1556033308686302400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556033308686302400');
 
 DELETE FROM `creature_equip_template` WHERE `CreatureID`=17701 AND `ID`=1;
@@ -204,3 +220,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17713);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17713, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 11, 12544, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - On Reset - Cast \'12544\''),
 (17713, 0, 1, 0, 0, 0, 100, 0, 2500, 2500, 6700, 6700, 11, 20792, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Bloodcursed Naga - In Combat - Cast \'20792\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
