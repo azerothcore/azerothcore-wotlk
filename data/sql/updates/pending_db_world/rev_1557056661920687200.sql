3573fcb160e7103eb383c73a689ad00bfaad6aa8
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-06 00:20:56 +0200

    fix(DB/SAI): Void Spawner - Quest - Warp Rifts
    - Comments

--- data/sql/updates/pending_db_world/rev_1557056661920687200.sql
+++ data/sql/updates/pending_db_world/rev_1557056661920687200.sql
@@ -5,6 +5,6 @@ SET @ENTRY := 20143;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,1,1,0,100,0,2000,5000,15000,23000,11,34943,0,0,0,0,0,1,0,0,0,0,0,0,0,'When out of combat and timer at the begining between - Self: Cast spell Summon Unstable Voidwalker (34943) on Self - Void Spawner - Quest - Warp Rifts - Out of Combat - Cast Summon Unstable Voidwalker (No Repeat)'),
-(@ENTRY,0,1,0,61,0,100,0,0,0,0,0,23,1,0,0,0,0,0,0,0,0,0,0,0,0,0,'Linked - Self: Increment phase by 1 and decrement by 0'),
-(@ENTRY,0,2,0,1,4,100,0,0,0,0,0,41,1000,0,0,0,0,0,1,0,0,0,0,0,0,0,'When out of combat and timer at the begining between - Self: Despawn in 1000 ms');
+(@ENTRY,0,0,1,1,0,100,0,2000,5000,15000,23000,11,34943,0,0,0,0,0,1,0,0,0,0,0,0,0,'Void Spawner - Quest - Warp Rifts - Out of Combat - Cast ''Summon Unstable Voidwalker'''),
+(@ENTRY,0,1,0,61,0,100,0,0,0,0,0,23,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Void Spawner - Linked - Self: Increment Phase by 1'),
+(@ENTRY,0,2,0,1,4,100,0,0,0,0,0,41,1000,0,0,0,0,0,1,0,0,0,0,0,0,0,'Void Spawner - Out of Combat (Phase 3) - Self: Despawn in 1000 ms');
