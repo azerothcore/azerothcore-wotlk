1898ca1cb1ce1f246342f481e66512403651cc43
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-05 22:45:31 +0000

    Import pending SQL update file...
    Referenced commit(s): 190be9cbd0591aa2bc8861f6de945c9d89877096

--- data/sql/updates/db_world/2019_05_05_00.sql
+++ data/sql/updates/db_world/2019_05_05_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_04_01 -> 2019_05_05_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_04_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_04_01 2019_05_05_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1556112726467897600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556112726467897600');
 
 -- Frostwolf SAI
@@ -41,3 +57,12 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,25,0,100,0,0,0,0,0,11,8876,0,0,0,0,0,1,0,0,0,0,0,0,0,'Frostwolf Bloodhound - In Combat - Cast Thrash'),
 (@ENTRY,0,1,0,1,0,100,0,0,0,0,0,89,15,0,0,0,0,0,1,0,0,0,0,0,0,0,'Frostwolf Bloodhound - Out of Combat - Use random movement');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
