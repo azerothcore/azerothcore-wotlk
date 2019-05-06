59295986ec855ad6f17c66130acaa3b07757453d
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-07 01:04:40 +0200

    fix(DB/Quest): Healthy Dragon Scale
    - Updated drop chance to 6 (Source: wowhead and some other DB's for patch 3.3.5)

--- data/sql/updates/pending_db_world/rev_1557180801797784400.sql
+++ data/sql/updates/pending_db_world/rev_1557180801797784400.sql
@@ -5,4 +5,4 @@ DELETE FROM `conditions` WHERE  `SourceGroup`=10678 AND `SourceEntry` IN (13920,
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
 (1, 10678, 13920, 0, 0, 8, 0, 5529, 0, 0, 0, 0, 0, '', 'Healthy Dragon Scale drop if Quest 5529 rewarded');
 
-UPDATE `creature_loot_template` SET `Chance`='100' WHERE `Entry`=10678 AND `Item`=13920;
+UPDATE `creature_loot_template` SET `Chance`='6' WHERE `Entry`=10678 AND `Item`=13920;
