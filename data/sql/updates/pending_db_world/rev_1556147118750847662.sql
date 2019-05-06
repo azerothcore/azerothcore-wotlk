c74dde7f991182bd9e3c53a02bdd47b68438b62b
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-06 10:46:17 +0000

    Import pending SQL update file...
    Referenced commit(s): 53bd1504a13bedd65ff89ea569147864accd004e

--- data/sql/updates/pending_db_world/rev_1556147118750847662.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556147118750847662');
-
-UPDATE `smart_scripts` SET `action_param1` = 38556, `comment` = REPLACE(`comment`,'Shoot','Throw') WHERE `entryorguid` = 27560 AND `action_param1` = 6660;
