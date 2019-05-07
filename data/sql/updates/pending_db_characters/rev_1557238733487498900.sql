fab7ca08ab6001efe96045b2ebab29c10e97845d
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-05-07 15:49:33 +0100

    Update rev_1557238733487498900.sql

--- data/sql/updates/pending_db_characters/rev_1557238733487498900.sql
+++ data/sql/updates/pending_db_characters/rev_1557238733487498900.sql
@@ -1,1 +1,6 @@
 INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1557238733487498900');
+
+UPDATE `auctionhouse`
+SET `time` = 0, `auctioneerguid` = 7;
+
+ALTER TABLE `auctionhouse` CHANGE COLUMN `auctioneerguid` `houseid` TINYINT(3) UNSIGNED NOT NULL DEFAULT '7' AFTER `id`;
