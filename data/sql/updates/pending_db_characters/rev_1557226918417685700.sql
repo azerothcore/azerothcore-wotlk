739da2bf2d5bb232acdaa4d4912220a710fa753b
Pondaveia <43385840+Pondaveia@users.noreply.github.com>
2019-05-07 12:16:44 +0100

    Update rev_1557226918417685700.sql

--- data/sql/updates/pending_db_characters/rev_1557226918417685700.sql
+++ data/sql/updates/pending_db_characters/rev_1557226918417685700.sql
@@ -1,1 +1,6 @@
 INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1557226918417685700');
+
+ALTER TABLE `character_arena_stats`
+MODIFY `guid` int(10) unsigned NOT NULL DEFAULT '0',
+MODIFY `slot` tinyint(3) unsigned NOT NULL DEFAULT '0',
+MODIFY `matchMakerRating` smallint(5) unsigned NOT NULL DEFAULT '0';
