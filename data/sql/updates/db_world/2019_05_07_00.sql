e5a55a5855d3a8db6e81b6a19618ce178fec209f
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-07 05:53:51 +0000

    Import pending SQL update file...
    Referenced commit(s): fa32450176a241a494fef76dadb1d390deb28874

--- data/sql/updates/db_world/2019_05_07_00.sql
+++ data/sql/updates/db_world/2019_05_07_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_06_00 -> 2019_05_07_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_06_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_06_00 2019_05_07_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1556522385066705884'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1556522385066705884');
 
 -- Winterskorn Woodsman: Use emote 'STATE_WORK_CHOPWOOD' (234) instead of 'STATE_WORK_MINING' (233)
@@ -137,3 +153,12 @@ DELETE FROM `spell_script_names` WHERE `spell_id` = 43102;
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`)
 VALUES
 (43102,'spell_item_summon_or_dismiss');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
