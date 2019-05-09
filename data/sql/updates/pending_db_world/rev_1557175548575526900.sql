bcf4e883d7319c88ca44397249e5e63a90ffefab
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-09 18:23:10 +0200

    fix(DB/Creature): Posible wrong spawned
    - Deleting two more wrong spawned creatures
    * GUID 101638, ID 25660 (Festering Ghoul)
    * GUID 101626, ID 25660 (Festering Ghoul)

--- data/sql/updates/pending_db_world/rev_1557175548575526900.sql
+++ data/sql/updates/pending_db_world/rev_1557175548575526900.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557175548575526900');
 
-DELETE FROM `creature` WHERE `guid`=97670;
+DELETE FROM `creature` WHERE `guid` IN(97670, 101638, 101626);
