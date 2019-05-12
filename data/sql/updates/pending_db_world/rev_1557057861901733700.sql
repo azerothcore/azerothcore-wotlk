dd4d89294f26382398c35948290e9a9cfc78594a
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-12 07:11:14 +0000

    Import pending SQL update file...
    Referenced commit(s): 9f040c626d5956acc9953134407c89ab41709c4c

--- data/sql/updates/pending_db_world/rev_1557057861901733700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557057861901733700');
-
--- Returning the Favor (9931)
-UPDATE `conditions` SET `ConditionValue1` = 18064 WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 32314 AND `ConditionTypeOrReference` = 29 AND `ElseGroup` = 1;
