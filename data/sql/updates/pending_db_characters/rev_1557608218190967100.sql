b7863dc4f1c1d61cb932429dda240943b96c37bd
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-12 21:12:05 +0000

    Import pending SQL update file...
    Referenced commit(s): 2b4fb29c08fbe9c30ce8422a45e4ab80248e63ed

--- data/sql/updates/pending_db_characters/rev_1557608218190967100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1557608218190967100');
-
-ALTER TABLE `characters`
-	ADD COLUMN `creation_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `grantableLevels`;
