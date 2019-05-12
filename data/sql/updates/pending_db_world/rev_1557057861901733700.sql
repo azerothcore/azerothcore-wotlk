3544bc2afa3700673f2a150b50a16e582046a61e
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-12 09:09:24 +0200

    fix(DB/Quest): Returning the Favor (#1797)
    * Shamans and Reavers are creatures for this quest.

---
+++ data/sql/updates/pending_db_world/rev_1557057861901733700.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557057861901733700');
+
+-- Returning the Favor (9931)
+UPDATE `conditions` SET `ConditionValue1` = 18064 WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 32314 AND `ConditionTypeOrReference` = 29 AND `ElseGroup` = 1;
