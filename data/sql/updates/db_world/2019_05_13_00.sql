bd749961868c0c4eb74f58c516319f35ab1cd84b
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-13 06:19:11 +0000

    Import pending SQL update file...
    Referenced commit(s): 9a7084e42d1929d0fbdcdcda1a17b3e38982b778

--- data/sql/updates/db_world/2019_05_13_00.sql
+++ data/sql/updates/db_world/2019_05_13_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_12_00 -> 2019_05_13_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_12_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_12_00 2019_05_13_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1557081152867649198'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557081152867649198');
 
 DELETE FROM `command` WHERE `name` IN ('arena create', 'arena disband', 'arena rename', 'arena captain', 'arena info', 'arena lookup');
@@ -8,3 +24,12 @@ INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('arena captain', 3, 'Syntax: .arena captain #TeamID $name\n\nA command to set new captain to the team $name must be in the team'),
 ('arena info', 2, 'Syntax: .arena info #TeamID\n\nA command that show info about arena team'),
 ('arena lookup', 2, 'Syntax: .arena lookup $name\n\nA command that give a list of arenateam with the given $name');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
