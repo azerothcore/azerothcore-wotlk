42a333a7f6b06c972f6f2b2b48b90ba893a9a6e2
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-16 09:52:47 +0200

    Update ObjectMgr.cpp

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -3449,7 +3449,7 @@ void ObjectMgr::LoadPlayerInfo()
 
         if (!result)
         {
-            sLog->outErrorDb(">> Loaded 0 player create spells. DB table `playercreateinfo_spell_custom` is empty.");
+            sLog->outString(">> Loaded 0 player create spells. DB table `playercreateinfo_spell_custom` is empty.");
         }
         else
         {
