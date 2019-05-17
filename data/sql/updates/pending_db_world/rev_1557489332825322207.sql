e32ff17058c42aa5afd8aea6204c345f4fed5155
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-17 00:51:05 +0000

    Import pending SQL update file...
    Referenced commit(s): 55ef766b19f1d65f1073615b8da390605989ff83

--- data/sql/updates/pending_db_world/rev_1557489332825322207.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557489332825322207');
-
-UPDATE `smart_scripts` SET `target_type` = 1 WHERE `entryorguid` = 25758 AND `source_type` = 0 AND `id` = 0;
