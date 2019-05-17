24d1f64c71e3ed2667e9f6bce8c0da211a762f59
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-17 20:52:45 +0200

    fix(DB/Gameobject): Missing in DB (Giving conflicts on quest) (#1829)
    - Adding missing gameobject_template in DB.
    - Required for Quest https://wotlk.evowow.com/?quest=10629

---
+++ data/sql/updates/pending_db_world/rev_1557741075472498600.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557741075472498600');
+
+DELETE FROM `gameobject_template` WHERE `entry`=184981;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES 
+(184981, 6, 7244, 'Poodad Trap', '', '', '', 0.5, 1716, 0, 0, 37695, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', -18019);
