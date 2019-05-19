d839651bcb811bd558d12e304d555d0ff9c7a738
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-19 12:13:20 +0000

    Import pending SQL update file...
    Referenced commit(s): b75e5c2d5d49316e5b0a7f344e070f0383de81d3

--- data/sql/updates/db_world/2019_05_19_00.sql
+++ data/sql/updates/db_world/2019_05_19_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_18_01 -> 2019_05_19_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_18_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_18_01 2019_05_19_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1557766899660556000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1557766899660556000');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 27122;
@@ -7,3 +23,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (27122, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 12739, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Overseer Deathgaze - On Aggro - Cast \'12739\''),
 (27122, 0, 1, 0, 0, 0, 100, 0, 3300, 3300, 4600, 4600, 11, 12739, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Overseer Deathgaze - In Combat - Cast \'12739\''),
 (27122, 0, 2, 0, 0, 0, 100, 0, 10000, 10000, 45000, 45000, 11, 50659, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Overseer Deathgaze - In Combat - Cast \'50659\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
