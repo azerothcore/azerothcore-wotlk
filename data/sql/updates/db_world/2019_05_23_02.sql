3e852c7eb967df6da95248fb0d238aed0e6f7ff2
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-23 15:26:51 +0000

    Import pending SQL update file...
    Referenced commit(s): 988d95ff2cedb27ee47b6d2d63511c3fb1fcad67

--- data/sql/updates/db_world/2019_05_23_02.sql
+++ data/sql/updates/db_world/2019_05_23_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_23_01 -> 2019_05_23_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_23_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_23_01 2019_05_23_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1558565210531241306'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1558565210531241306');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 8905;
@@ -8,3 +24,12 @@ VALUES
 (9476,0,4,5,4,0,100,1,0,0,0,0,0,19,768,0,0,0,0,0,11,8905,50,0,0,0,0,0,0,'Watchman Doomgrip - On Aggro - Remove Unit Flags from ''Warbringer Construct'' (8905)'),
 (9476,0,5,6,61,0,100,0,0,0,0,0,0,8,2,0,0,0,0,0,11,8905,50,0,0,0,0,0,0,'Watchman Doomgrip - Linked - Set React State ''Aggressive'' for ''Warbringer Construct'' (8905)'),
 (9476,0,6,0,61,0,100,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Watchman Doomgrip - Linked - Say Line 0');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
