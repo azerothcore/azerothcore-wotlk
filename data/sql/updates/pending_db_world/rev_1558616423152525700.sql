2b47218c477cf1c90f9109f8c76a76006abafe8a
Poszer <poszerinsane@gmail.com>
2019-05-23 15:05:10 +0200

    fix(DB/Gameobject): Spire Spider Egg Trap
    - Adding Spire Spider Egg Trap gameobject template, missing in our DB.
    - Related to this quest: https://wotlk.evowow.com/?quest=4862

---
+++ data/sql/updates/pending_db_world/rev_1558616423152525700.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1558616423152525700');
+
+-- Using free entry (175590)
+DELETE FROM `gameobject_template` WHERE `entry`=175590;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES
+(175590, 6, 0, 'Spire Spider Egg Trap', '', '', '', 1, 0, 0, 0, 16453, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', -18019);
