23abb1e006306ec80aab529b67ecf2f003e43dee
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-23 22:53:41 +0200

    Update rev_1558616423152525700.sql
    - Adding SAI to Spide Spiderling

--- data/sql/updates/pending_db_world/rev_1558616423152525700.sql
+++ data/sql/updates/pending_db_world/rev_1558616423152525700.sql
@@ -9,3 +9,14 @@ INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconNa
 DELETE FROM `gameobject` WHERE `guid`=267000;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
 (267000, 175590, 229, 0, 0, 1, 1, -139.772, -526.945, 6.41529, 1.5574, -0, -0, -0.702354, -0.711828, 300, 0, 1, '', 0);
+
+-- Spire Spiderling SAI
+SET @ENTRY := 10375;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,1,0,100,0,0,0,0,0,89,15,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Spire Spiderling - Out of Combat - Random Move"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,0,0,0,0,8,0,0,0,0,0,0,17,5,10,0,0.0,0.0,0.0,0.0,"Spire Spiderling - In combat - Set React state "),
+(@ENTRY,@SOURCETYPE,2,0,7,0,100,0,0,0,0,0,41,150,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Spide Spiderling - On Evade - Despawn");
