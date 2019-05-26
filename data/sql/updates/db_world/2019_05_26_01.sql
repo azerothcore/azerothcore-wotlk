68a998c79e9c19de973a9551fecdff2f711e299a
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-26 13:09:40 +0000

    Import pending SQL update file...
    Referenced commit(s): d7ac0771289fd72f23423643d5559ea99a1084b3 337820db0fb4207ee07b6dae4e2712a39b714d9e 052d3364d606354839f3548fba390c4e738935e1 bdd33f1f7e8fc955b53309210212bcd69f6d54a9

--- data/sql/updates/db_world/2019_05_26_01.sql
+++ data/sql/updates/db_world/2019_05_26_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_26_00 -> 2019_05_26_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_26_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_26_00 2019_05_26_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1558471696386408000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1558471696386408000');
 
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `Entry` IN (29351, 29358);
@@ -6,3 +22,12 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (29351,0,0,0,0,0,100,0,2000,5000,7000,9000,11,38621,0,0,0,0,0,2,0,0,0,0,0,0,0,'Niffelem Frost Giant - IC - Cast Debilitating Strike'),
 (29358,0,0,0,0,0,100,0,2000,5000,7000,9000,11,61572,0,0,0,0,0,2,0,0,0,0,0,0,0,'Frostworg - IC - Cast Frostbite'),
 (29358,0,1,0,0,0,100,0,2000,6000,6000,9000,11,50075,0,0,0,0,0,2,0,0,0,0,0,0,0,'Frostworg - IC - Cast Maim Flesh');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
