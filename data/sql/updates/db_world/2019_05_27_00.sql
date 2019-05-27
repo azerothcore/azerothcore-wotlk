fc66a5c8a040d9250494ec7fe1d71df1de4803e6
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-27 12:09:40 +0000

    Import pending SQL update file...
    Referenced commit(s): 7b7f6e6559ecdb0abcef5e7396a1b49fc7b259e5

--- data/sql/updates/db_world/2019_05_27_00.sql
+++ data/sql/updates/db_world/2019_05_27_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_26_03 -> 2019_05_27_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_26_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_26_03 2019_05_27_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1558616423152525700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1558616423152525700');
 
 -- Cast "Summon Spire Spiderling" with a 10% chance if opening "Spire Spider Egg" (IDs 175606 & 175588) 
@@ -25,3 +41,12 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (10374,0,0,0,6,0,100,2,0,0,0,0,11,16103,3,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Spire Spider - On Death - Cast 'Summon Spire Spiderling'"),
 (10374,0,1,0,0,0,100,2,15000,15000,15000,15000,11,16104,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"Spire Spider - In Combat - Cast 'Crystallize'"),
 (10374,0,2,0,1,0,100,0,0,0,0,0,89,20,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Spire Spider - Out of Combat - Random move");
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
