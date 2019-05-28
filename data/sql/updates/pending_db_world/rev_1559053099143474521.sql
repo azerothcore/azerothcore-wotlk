804e90e4c27d8e2bda8bcbae0d4e393a3ff8d8b4
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-28 14:36:52 +0000

    condensed update statements

--- data/sql/updates/pending_db_world/rev_1559053099143474521.sql
+++ data/sql/updates/pending_db_world/rev_1559053099143474521.sql
@@ -1,8 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559053099143474521');
 
-UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847100 AND `source_type` = 9 AND `id` = 1;
-UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847101 AND `source_type` = 9 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `source_type` = 9 AND `id` = 1 AND `entryorguid` IN (1847100,1847101,1847102,1847103,1847104);
 UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847101 AND `source_type` = 9 AND `id` = 2;
-UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847102 AND `source_type` = 9 AND `id` = 1;
-UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847103 AND `source_type` = 9 AND `id` = 1;
-UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847104 AND `source_type` = 9 AND `id` = 1;
