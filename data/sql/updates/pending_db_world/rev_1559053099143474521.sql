2ad8105423e0396dbb378f2da076d5a6c05ca324
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-28 14:18:42 +0000

    fix(DB/SAI): Ring of Blood creature despawn

---
+++ data/sql/updates/pending_db_world/rev_1559053099143474521.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559053099143474521');
+
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847100 AND `source_type` = 9 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847101 AND `source_type` = 9 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847101 AND `source_type` = 9 AND `id` = 2;
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847102 AND `source_type` = 9 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847103 AND `source_type` = 9 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_param2` = 4, `action_param3` = 10000 WHERE `entryorguid` = 1847104 AND `source_type` = 9 AND `id` = 1;
