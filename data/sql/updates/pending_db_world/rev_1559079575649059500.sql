8db1401b86ddfa5c0784ce0ecd2d4b8daa4324ad
Poszer <41213210+poszer@users.noreply.github.com>
2019-05-28 23:48:57 +0200

    Update rev_1559079575649059500.sql

--- data/sql/updates/pending_db_world/rev_1559079575649059500.sql
+++ data/sql/updates/pending_db_world/rev_1559079575649059500.sql
@@ -6,7 +6,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559079575649059500');
 SET @ENTRY := 28902;
 SET @SOURCETYPE := 0;
 
- DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,0,38,0,100,0,0,1,0,0,1,1,7000,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Warlord Zol'Maz - On Data Set 0 1 - Say Line 1"),
