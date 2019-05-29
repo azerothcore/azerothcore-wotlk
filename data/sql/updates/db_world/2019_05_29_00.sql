7cc25a63a7cee430170d1f87ab14d6a52c79eae7
AzerothCoreBot <azerothcorebot@gmail.com>
2019-05-29 10:18:06 +0000

    Import pending SQL update file...
    Referenced commit(s): b97e6f36c6ae21c9b0539e68086863a6e6e74892

--- data/sql/updates/db_world/2019_05_29_00.sql
+++ data/sql/updates/db_world/2019_05_29_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_05_27_00 -> 2019_05_29_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_05_27_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_05_27_00 2019_05_29_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1558651761752003200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1558651761752003200');
 
 -- Olga's gossip menu needs to be corrected in smart_scripts to register user selection
@@ -39,3 +55,12 @@ UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 24788 AND `id` = 5; 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 2478800 AND `source_type` = 9 AND `id` = 9;
 INSERT INTO `smart_scripts`(`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (2478800, 9, 9, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 45, 3, 3, 0, 0, 0, 0, 19, 24639, 0, 0, 0, 0, 0, 0, 0, 'Jack Adams - On Script - Set Data 3 3');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
