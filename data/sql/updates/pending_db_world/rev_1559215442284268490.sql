8620c54f59cc1ce8d41149a955c03400dd6d76f0
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-31 18:09:44 +0200

    update script

--- data/sql/updates/pending_db_world/rev_1559215442284268490.sql
+++ data/sql/updates/pending_db_world/rev_1559215442284268490.sql
@@ -41,4 +41,5 @@ DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 29927;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES
 (29927,0,0,0,54,0,100,0,0,0,0,0,0,49,0,0,0,0,0,0,19,29375,30,0,0,0,0,0,0,'Earthen Ironbane - Is Summoned - Attack Start (Stormforged Iron Giant)'),
-(29927,0,1,0,54,0,100,0,0,0,0,0,0,49,0,0,0,0,0,0,19,29503,30,0,0,0,0,0,0,'Earthen Ironbane - Is Summoned - Attack Start (Fjorn)');
+(29927,0,1,0,54,0,100,0,0,0,0,0,0,49,0,0,0,0,0,0,19,29503,30,0,0,0,0,0,0,'Earthen Ironbane - Is Summoned - Attack Start (Fjorn)'),
+(29927,0,2,0,1,0,100,0,0,0,0,0,0,41,10000,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Earthen Ironbane - Out of Combat - Despawn After 10 Seconds');
