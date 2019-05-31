aeda72a8440d604acff8db3eda94c79b4354fc81
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-31 15:56:58 +0000

    update script

--- data/sql/updates/pending_db_world/rev_1559220494373180355.sql
+++ data/sql/updates/pending_db_world/rev_1559220494373180355.sql
@@ -5,6 +5,9 @@ UPDATE `creature_template` SET `spell1` = 0 WHERE `entry` = 12141;
 DELETE FROM `creature_template_addon` WHERE `entry` = 12141;
 INSERT INTO `creature_template_addon` (`entry`,`path_id`,`mount`,`bytes1`,`bytes2`,`emote`,`auras`) VALUES (12141,0,0,0,0,0,'18978');
 
+-- Mogor should be uninterruptible
+UPDATE `creature_template` SET `mechanic_immune_mask` = 650854271 WHERE `entry` = 18069;
+
 -- Base script
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 18069 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
