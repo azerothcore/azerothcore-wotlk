53f3f59bfd0ea6c39005b193c93744439890e48d
Selyria <F.plump@gmx.de>
2019-06-01 15:59:10 +0200

    Update data/sql/updates/pending_db_world/rev_1559394390864887200.sql
    Co-Authored-By: Barbz <BarbzYHOOL@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1559394390864887200.sql
+++ data/sql/updates/pending_db_world/rev_1559394390864887200.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559394390864887200');
 
-UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 128 WHERE (entry = 20308);
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 128 WHERE `entry` = 20308;
