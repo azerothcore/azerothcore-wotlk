d0642f0a4d0d99bf406db83ed287547e589273c0
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-02 12:02:46 +0000

    Import pending SQL update file...
    Referenced commit(s): 78f4e7189467096ee2daebffa5f5bf7876db447b

--- data/sql/updates/pending_db_world/rev_1558943884011471260.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1558943884011471260');
-
--- Despawn "Gearmaster Mechazod" after 300 seconds (was 3000 seconds before)
-UPDATE `event_scripts` SET `datalong2` = 300000 WHERE `id` = 17209 AND `command` = 10;
-
--- Ensure that "The Gearmaster's Manual" is consumable (both Horde and Alliance version)
-UPDATE `gameobject_template` SET `Data5` = 1 WHERE `entry` IN (190334,190335);
