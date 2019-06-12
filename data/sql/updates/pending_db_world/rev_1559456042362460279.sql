6a26d8161bf865fe94b8e7205c8cd36ff354cd24
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-06-12 23:14:43 +0200

    Update rev_1559456042362460279.sql

--- data/sql/updates/pending_db_world/rev_1559456042362460279.sql
+++ data/sql/updates/pending_db_world/rev_1559456042362460279.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559456042362460279');
 
--- use "SMART_ACTION_MUSIC" instead of "SMART_ACTION_SOUND" to play "Lament of the Highborne" as music for the players in the area
-UPDATE `smart_scripts` SET `action_type` = 216, `action_param2` = 1, `action_param3` = 2, `target_type` = 1, `target_param1` = 0, `comment` = 'Sylvanas Lamenter - On Update - Play Area Music' WHERE `entryorguid` = 39048 AND `source_type` = 0 AND `id` = 1;
+-- use "SMART_ACTION_MUSIC" instead of "SMART_ACTION_SOUND" to play "Lament of the Highborne" as music for all players within 50 yards distance
+UPDATE `smart_scripts` SET `action_type` = 216, `action_param2` = 1, `action_param3` = 0, `target_type` = 18, `target_param1` = 50, `comment` = 'Sylvanas Lamenter - On Update - Play Area Music' WHERE `entryorguid` = 39048 AND `source_type` = 0 AND `id` = 1;
 
 -- use "onlySelf" 1 for Thrall event as the music needs to be played only for the players directly
 UPDATE `smart_scripts` SET `action_param2` = 1 WHERE `entryorguid` = 19556 AND `source_type` = 0 AND `action_type` = 216;
