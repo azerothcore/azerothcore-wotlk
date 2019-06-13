8e1f32ddd553dd2874478dca3b2fea3f626e4cdb
Askh <andreim_2005@yahoo.com>
2019-06-13 06:37:36 +0300

    Delete rev_1560252813861928400.sql

--- data/sql/updates/pending_db_world/rev_1560252813861928400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1560252813861928400');
-
--- Increases the health pool of Malygos's platform to 1500000 from 100
-UPDATE `gameobject_template` SET `Data0`='1500000' WHERE  `entry`=193070; -- Malygos Platform no longer collapses upon taking siege damage.
