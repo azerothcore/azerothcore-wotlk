ae2cbbb610b1cd633ff1c648cc029ca6426462c6
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-14 05:53:21 +0000

    Import pending SQL update file...
    Referenced commit(s): 5da61f1167e8c0e4f592a78979e8358680edf033

--- data/sql/updates/db_world/2019_06_14_00.sql
+++ data/sql/updates/db_world/2019_06_14_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_06_13_00 -> 2019_06_14_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_06_13_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_06_13_00 2019_06_14_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1559456042362460279'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559456042362460279');
 
 -- use "SMART_ACTION_MUSIC" instead of "SMART_ACTION_SOUND" to play "Lament of the Highborne" as music for all players within 50 yards distance
@@ -12,3 +28,12 @@ VALUES
 
 -- use "onlySelf" 1 for Thrall event as the music needs to be played only for the players directly
 UPDATE `smart_scripts` SET `action_param2` = 1 WHERE `entryorguid` = 19556 AND `source_type` = 0 AND `action_type` = 216;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
