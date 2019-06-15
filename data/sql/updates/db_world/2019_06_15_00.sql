68c98d5f0c8456484430a43742f6641e1f539a5a
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-15 18:40:52 +0000

    Import pending SQL update file...
    Referenced commit(s): 4c1ef0a67e2951355fc3c795df3cc80751f721b4

--- data/sql/updates/db_world/2019_06_15_00.sql
+++ data/sql/updates/db_world/2019_06_15_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_06_14_00 -> 2019_06_15_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_06_14_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_06_14_00 2019_06_15_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1560496359236834500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1560496359236834500');
 
 -- Remorseful Highborne
@@ -26,3 +42,12 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=7523 AND `source_type`=0 AND `id
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (7523,0,2,0,38,0,100,0,1,1,0,0,2,124,0,0,0,0,0,1,0,0,0,0,0,0,0,'Suffering Highborne - On Data Set 1 1 - Set Faction 124'),
 (7523,0,3,0,38,0,100,0,2,2,0,0,2,21,0,0,0,0,0,1,0,0,0,0,0,0,0,'Suffering Highborne - On Data Set 2 2 - Set Faction 21');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
