258b4e583c7481da5ad99f251fa094e0338dbbed
Winfidonarleyan <dowlandtop@yandex.ru>
2019-06-20 02:30:21 +0700

    Try fix travis

--- apps/ci/ci-worldserver-dry-run.sh
+++ apps/ci/ci-worldserver-dry-run.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BUILD_ID" = "1" ]
 then
   echo "start worldserver dry-run"
   git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git /home/travis/build/azerothcore/azerothcore-wotlk/env/dist/data
-  sed -e "s/;;acore_auth/;$DB_RND_NAME;auth_$DB_RND_NAME/" -e "s/;;acore_world/;$DB_RND_NAME;world_$DB_RND_NAME/" -e "s/;;acore_characters/;$DB_RND_NAME;characters_$DB_RND_NAME/" ./data/travis/worldserver.conf >./env/dist/etc/worldserver.conf
+  sed -e "s/;;acore_auth/;$DB_RND_NAME;auth_$DB_RND_NAME/" -e "s/;;acore_world/;$DB_RND_NAME;world_$DB_RND_NAME/" -e "s/;;acore_characters/;$DB_RND_NAME;characters_$DB_RND_NAME/" ./env/dist/etc/worldserver.conf.dist >./env/dist/etc/worldserver.conf
   ./env/dist/bin/worldserver --dry-run
   ./apps/ci/ci-error-check.sh
 fi
