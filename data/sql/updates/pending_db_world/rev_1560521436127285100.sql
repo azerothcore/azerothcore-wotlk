32aee8a9f39587c2733b538bc83263dd1e938475
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-20 14:12:55 +0000

    Import pending SQL update file...
    Referenced commit(s): 54ddf1950147d49053f1dc70a5d20fbfeb3488de

--- data/sql/updates/pending_db_world/rev_1560521436127285100.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1560521436127285100');
-
-UPDATE `smart_scripts` SET `event_param1` = 0, `event_param2` = 0, `action_param2` = 1, `comment` = 'Venture Co. Strip Miner - In Combat - Cast Throw Dynamite' WHERE `entryorguid` = 674 AND `source_type` = 0 and `id` = 0;
