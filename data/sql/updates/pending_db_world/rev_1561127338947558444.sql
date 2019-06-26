261fc797dd38a534c82f918399d8eb1eb216ce1d
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-26 21:45:58 +0000

    Import pending SQL update file...
    Referenced commit(s): 9f3c0267bcb738a274e5f9eed90ce4bd568a3489

--- data/sql/updates/pending_db_world/rev_1561127338947558444.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1561127338947558444');
-
-DELETE FROM `spell_script_names` WHERE `spell_id` = 53768;
-INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES (53768,'spell_gen_haunted');
