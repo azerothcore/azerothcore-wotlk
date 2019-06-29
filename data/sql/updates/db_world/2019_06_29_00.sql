391b4ba95413c7055b33c18cdfd7feaf67c7511b
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-29 00:16:49 +0000

    Import pending SQL update file...
    Referenced commit(s): 44b83a3d7c64bd374e2f0e9a5b3ebff9fe6a7c33

--- data/sql/updates/db_world/2019_06_29_00.sql
+++ data/sql/updates/db_world/2019_06_29_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_06_28_00 -> 2019_06_29_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_06_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_06_28_00 2019_06_29_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1561242305351726648'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1561242305351726648');
 
 DELETE FROM `creature_template_addon` WHERE `entry` = 11327;
@@ -27,3 +43,12 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = 34694 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES
 (34694,0,0,0,1,0,100,0,5500,5500,5500,5500,0,87,1132700,1132701,0,0,0,0,19,11327,15,0,0,0,0,0,0,'Grunty - OOC - Call Random Timed Action List For ''Zergling''');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
