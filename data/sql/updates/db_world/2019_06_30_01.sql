3fe5eed6e933a9f875adfe846a50520bb6427b07
AzerothCoreBot <azerothcorebot@gmail.com>
2019-06-30 23:07:29 +0000

    Import pending SQL update file...
    Referenced commit(s): ea2ecdbcc8eb2c47d420d0fb29dcb04897228d84

--- data/sql/updates/db_world/2019_06_30_01.sql
+++ data/sql/updates/db_world/2019_06_30_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_06_30_00 -> 2019_06_30_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_06_30_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_06_30_00 2019_06_30_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1561274158622782000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1561274158622782000');
 
 -- Adding two missing creatures Drolig and Drelig Blastpipe in Shattrah, They re located in the Scryer's Tier and Aldor Rise
@@ -14,3 +30,12 @@ INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
 (56, 79018),
 (56, 75475),
 (56, 88156);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
