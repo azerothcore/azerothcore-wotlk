b933436e29b0224e00547938fa084fa547151013
Poszer <41213210+poszer@users.noreply.github.com>
2019-07-01 10:08:12 +0200

    fix(DB/Quests): For Great Honor & Concerted Efforts
    - Also adding missing rows for Alliance quest related to these reports https://github.com/azerothcore/azerothcore-wotlk/issues/1133

--- data/sql/updates/pending_db_world/rev_1561966083359604300.sql
+++ data/sql/updates/pending_db_world/rev_1561966083359604300.sql
@@ -4,3 +4,8 @@ DELETE FROM `creature_questender` WHERE `id`=15350 AND `quest` IN (8367,13476);
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES 
 ('15350', '8367'),
 ('15350', '13476');
+
+DELETE FROM `creature_queststarter` WHERE `id`=15351 AND `quest` IN (8371,13478);
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES 
+('15351', '8371'),
+('15351', '13478');
