52160ef5c90acd26e5a84e0d4728ca7a67a9000f
Poszer <41213210+poszer@users.noreply.github.com>
2019-07-01 19:21:47 +0200

    Update rev_1561966083359604300.sql
    Removing strings

--- data/sql/updates/pending_db_world/rev_1561966083359604300.sql
+++ data/sql/updates/pending_db_world/rev_1561966083359604300.sql
@@ -2,10 +2,10 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1561966083359604300');
 
 DELETE FROM `creature_questender` WHERE `id`=15350 AND `quest` IN (8367,13476);
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES 
-('15350', '8367'),
-('15350', '13476');
+(15350, 8367),
+(15350, 13476);
 
 DELETE FROM `creature_queststarter` WHERE `id`=15351 AND `quest` IN (8371,13478);
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES 
-('15351', '8371'),
-('15351', '13478');
+(15351, 8371),
+(15351, 13478);
