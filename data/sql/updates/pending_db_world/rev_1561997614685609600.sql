e642fbd7311949104bf06cbe485bd3bf30d90c8d
Francesco Borzì <borzifrancesco@gmail.com>
2019-07-01 22:08:56 +0200

    Update rev_1561997614685609600.sql

--- data/sql/updates/pending_db_world/rev_1561997614685609600.sql
+++ data/sql/updates/pending_db_world/rev_1561997614685609600.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1561997614685609600');
 
 -- Razorscale Harpoon Fire State creature is trigger-NPC (invisible to players only)
-UPDATE `creature_template` SET `flags_extra`='130' WHERE `entry`=33282;
+UPDATE `creature_template` SET `flags_extra` = 130 WHERE `entry` = 33282;
