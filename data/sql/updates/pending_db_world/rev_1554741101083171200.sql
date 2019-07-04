6295c22a3aae735cb10d05d94a8790b82d993a0f
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-07-04 23:07:16 +0200

    Added Unstable Fel-Imp Explosion

--- data/sql/updates/pending_db_world/rev_1554741101083171200.sql
+++ data/sql/updates/pending_db_world/rev_1554741101083171200.sql
@@ -20,3 +20,11 @@ DELETE FROM `creature` WHERE `id`=22495;
 INSERT INTO `creature` (`guid`,`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`ScriptName`,`VerifiedBuild`) VALUES
 (78947, 22495, 530, 0, 0, 1, 1, 0, 0, 2191.68725585938, 5478.57275390625, 160.826309204102, 0.456801176071167, 1, 0, 0, 6986, 0, 0, 0, 0, 0, '', 0),
 (78948, 22495, 530, 0, 0, 1, 1, 0, 0, 1978.12377929688, 5319.36865234375, 162.545059204102, 2.30369400978088, 300, 0, 0, 6986, 0, 0, 0, 0, 0, '', 0);
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 22474;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22474 AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
+VALUES
+(22474,0,0,1,9,0,100,0,0,1,0,0,0,11,60081,2,0,0,0,0,2,0,0,0,0,0,0,0,0,'Unstable Fel-Imp - On Victim In Range (0 Yards) - Cast ''Cosmetic - Explosion'''),
+(22474,0,1,0,61,0,100,0,0,0,0,0,0,67,1,500,500,0,0,0,1,0,0,0,0,0,0,0,0,'Unstable Fel-Imp - Linked - Create Timed Event ID 1'),
+(22474,0,2,0,59,0,100,0,1,0,0,0,0,11,39266,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Unstable Fel-Imp - On Timed Event ID 1 - Cast ''Unstable Explosion''');
