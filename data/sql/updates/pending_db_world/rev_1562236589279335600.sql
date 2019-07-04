12fff271d462cb67180d7583eb7ce11e0f82cefc
Poszer <41213210+poszer@users.noreply.github.com>
2019-07-04 13:28:29 +0200

    Update rev_1562236589279335600.sql
    - Added to other event

--- data/sql/updates/pending_db_world/rev_1562236589279335600.sql
+++ data/sql/updates/pending_db_world/rev_1562236589279335600.sql
@@ -1,3 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1562236589279335600');
 
-DELETE FROM `gameobject` WHERE `guid`=78033;
+DELETE FROM `game_event_gameobject` WHERE `guid` IN (78033,78106,78117);
+INSERT INTO `game_event_gameobject` (`eventEntry`, `guid`) VALUES
+(12,78033),
+(12,78106),
+(12,78117);
