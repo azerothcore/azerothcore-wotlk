5ae5de7e5b78062db73d3c38eec072b4c57d357e
Francesco Borzì <borzifrancesco@gmail.com>
2019-07-13 17:18:41 +0200

    Update rev_1563029982815095900.sql

--- data/sql/updates/pending_db_world/rev_1563029982815095900.sql
+++ data/sql/updates/pending_db_world/rev_1563029982815095900.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1563029982815095900');
 
 -- Enslaved Netherwing Drake (Correctly fly animation)
-UPDATE `creature_template` SET `InhabitType`='5' WHERE `entry`=21722;
+UPDATE `creature_template` SET `InhabitType`=5 WHERE `entry`=21722;
