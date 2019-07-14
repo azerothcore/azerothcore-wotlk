97bcb2f54ba045757cc2f9dcd0aac886edea6b9b
AzerothCoreBot <azerothcorebot@gmail.com>
2019-07-14 17:06:53 +0000

    Import pending SQL update file...
    Referenced commit(s): 5aa9266cf3cc49e8dc0852c19ec161b4c6df71d8

--- data/sql/updates/db_world/2019_07_14_00.sql
+++ data/sql/updates/db_world/2019_07_14_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_07_13_00 -> 2019_07_14_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_07_13_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_07_13_00 2019_07_14_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1562110333242304237'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1562110333242304237');
 
 -- Random movement for Bile Golem, Crypt Fiend, Devouring Ghoul, Patchwork Construct
@@ -18,3 +34,12 @@ UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 24077 AND `source_ty
 -- Fix Ymirjar Flesh Hunter SAI link error in Normal difficulty; fix using "Aimed Shot" in Heroic difficulty
 UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 26670 AND `source_type` = 0 AND `id` = 18;
 UPDATE `smart_scripts` SET `event_type` = 0 WHERE `entryorguid` = 26670 AND `source_type` = 0 AND `id` = 19;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
