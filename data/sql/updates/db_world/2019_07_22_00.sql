e55b7e582099920724718dcc7070033362110464
AzerothCoreBot <azerothcorebot@gmail.com>
2019-07-22 22:23:55 +0000

    Import pending SQL update file...
    Referenced commit(s): d025a90a69a22cc20817adda5351739567a89f47

--- data/sql/updates/db_world/2019_07_22_00.sql
+++ data/sql/updates/db_world/2019_07_22_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_07_21_00 -> 2019_07_22_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_07_21_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_07_21_00 2019_07_22_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1562779174835710547'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1562779174835710547');
 
 -- Remove Socrethar SAI Script for credit
@@ -155,3 +171,12 @@ DELETE FROM `creature_text` WHERE `CreatureID` = 18538;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextID`, `TextRange`, `comment`) VALUES
 (18538, 0, 0, 'The Light wants its lost son back, Socrethar.', 12, 0, 100, 0, 0, 0, 18400, 0, ''),
 (18538, 1, 0, 'I offer myself in exchange.  You will have Ishanah, the High Priestess of the Aldor, as your prisoner if you release Kaylaan from your dark grasp.', 12, 0, 100, 0, 0, 0, 18401, 0, '');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
