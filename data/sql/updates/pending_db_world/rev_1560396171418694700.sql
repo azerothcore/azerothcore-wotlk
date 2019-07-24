d9d84c4eb9c670182cd52596e5f336d1d58241b1
AzerothCoreBot <azerothcorebot@gmail.com>
2019-07-24 22:02:43 +0000

    Import pending SQL update file...
    Referenced commit(s): 2b490ea4871196351e426d07f34fb982c502ed42

--- data/sql/updates/pending_db_world/rev_1560396171418694700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1560396171418694700');
-
-UPDATE `gameobject_template_addon` SET `faction`=35 WHERE `entry`=193070; -- Nexus Raid Platform now has faction 35, instead of 0 and is friendly to players.
-UPDATE `gameobject_template` SET `Data0`=6000000 WHERE `entry`=193070;    -- Nexus Raid Platform has a lot more hp in order not to break in case any siege damage is taken, initial value was 100. (double safety)
