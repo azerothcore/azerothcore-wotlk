0aee8ed4d87d637d1530ec3d17622838724444d7
AzerothCoreBot <azerothcorebot@gmail.com>
2019-07-25 18:09:25 +0000

    Import pending SQL update file...
    Referenced commit(s): 7f599f8c3539a177284b7cd1aaac5b7b01c5674f

--- data/sql/updates/db_world/2019_07_25_00.sql
+++ data/sql/updates/db_world/2019_07_25_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_07_24_01 -> 2019_07_25_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_07_24_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_07_24_01 2019_07_25_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1563271511100419000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1563271511100419000');
 
 -- Exarch Admetius SAI
@@ -148,3 +164,12 @@ UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,54,0,100,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Prophet Velen - On Just Summoned - Set Active On'); -- Ensure that all summoned creatures near Velen disappear even when no player is near
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
