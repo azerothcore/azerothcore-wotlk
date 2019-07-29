32c1b38e71ad65965ed57856f5da569fcb8e87e6
AzerothCoreBot <azerothcorebot@gmail.com>
2019-07-29 22:28:08 +0000

    Import pending SQL update file...
    Referenced commit(s): c7156c787fe8156f094090cb87a575151b31561d

--- data/sql/updates/pending_db_world/rev_1563566703983662300.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1563566703983662300');
-
-DELETE FROM `waypoint_data` WHERE `id`=1142920 AND `point` IN (33,34);
-INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `move_type`, `action`, `action_chance`, `wpguid`) VALUES
-(1142920, 33, 5761.59, 597.858, 649.707, 0, 0, 0, 0, 100, 0),
-(1142920, 34, 5722.22, 624.876, 646.633, 0, 0, 0, 0, 100, 0);
