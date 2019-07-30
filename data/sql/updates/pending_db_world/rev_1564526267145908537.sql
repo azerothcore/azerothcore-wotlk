cbd70156de6c9b933b3b546c29ee824282eed3d9
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-07-31 00:38:16 +0200

    fix(DB/creature): Thorim position (Temple of Storms)

---
+++ data/sql/updates/pending_db_world/rev_1564526267145908537.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1564526267145908537');
+
+UPDATE `creature` SET `position_x` = 7448.98, `position_y` = -534.783, `position_z` = 1896.85, `orientation` = 2.99243 WHERE `guid` = 97128;
