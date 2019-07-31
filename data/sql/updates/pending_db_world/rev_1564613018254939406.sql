ad972a3011ab903c8c5163693307d761076250f2
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-08-01 01:08:30 +0200

    Update rev_1564613018254939406.sql

--- data/sql/updates/pending_db_world/rev_1564613018254939406.sql
+++ data/sql/updates/pending_db_world/rev_1564613018254939406.sql
@@ -13,7 +13,7 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = 29796 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES
 (29796,0,0,1,19,0,100,0,12886,0,0,0,0,12,29679,3,600000,0,0,0,8,0,0,0,0,7090.47,-1765.8,821.052,0,'Gretta the Arbiter - On Accepted Quest ''The Drakkensryd'' - Summon ''Hyldsmeet Proto-Drake'''),
-(29796,0,1,0,61,0,100,0,0,0,0,0,0,86,46598,2,7,0,0,0,19,29679,10,0,0,0,0,0,0,'');
+(29796,0,1,0,61,0,100,0,0,0,0,0,0,86,46598,2,7,0,0,0,19,29679,10,0,0,0,0,0,0,'Gretta the Arbiter - Linked - Cross Cast ''Ride Vehicle Hardcoded'' (Invoker - Hyldsmeet Proto-Drake)');
 
 -- Hyldsmeet Proto-Drake
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 29679 AND `source_type` = 0 AND `id` = 2;
