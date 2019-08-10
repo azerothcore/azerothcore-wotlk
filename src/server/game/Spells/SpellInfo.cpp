82d53bf316b82c4ba0aef2162ef097979fd6f506
Shard <30301841+Shard-MW@users.noreply.github.com>
2019-08-10 13:06:52 +0400

    fix(Core/SpellInfo): Magmadar's Enrage dispel fix (#2146)
    Putting the Magmadar's Enrage spell as a Buff to make it dispellable.

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2569,6 +2569,7 @@ bool SpellInfo::_IsPositiveEffect(uint8 effIndex, bool deep) const
                 case 62344: // Fists of Stone
                 case 61819: // Manabonked! (item)
                 case 61834: // Manabonked! (minigob)
+                case 19451: // Enrage (Magmadar)
                     return true;
                 default:
                     break;
