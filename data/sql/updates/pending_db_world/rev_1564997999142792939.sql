a69c59c0fcc6946cef02461899707b1ba4faf6e5
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-08-28 01:05:04 +0200

    fix(DB/creature): Thrall position (Orgrimmar) (#2181)

---
+++ data/sql/updates/pending_db_world/rev_1564997999142792939.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1564997999142792939');
+
+UPDATE `creature` SET `position_x` = 1919.9, `position_y` = -4123.8, `position_z` = 43.62 WHERE `guid` = 4770;
