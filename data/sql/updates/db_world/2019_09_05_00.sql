f876cdda718f2e41fb7ae41215c824ca2ce2e8d7
AzerothCoreBot <azerothcorebot@gmail.com>
2019-09-05 05:53:07 +0000

    Import pending SQL update file...
    Referenced commit(s): c5e187fed6246361f98e786638781f0768134b6d

--- data/sql/updates/db_world/2019_09_05_00.sql
+++ data/sql/updates/db_world/2019_09_05_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_09_04_00 -> 2019_09_05_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_09_04_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_09_04_00 2019_09_05_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1566945787997948952'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1566945787997948952');
 
 DELETE FROM `spell_script_names` WHERE `spell_id` IN
@@ -41,3 +57,12 @@ VALUES
 (30102,'spell_hun_tame_beast'),
 (30104,'spell_hun_taming_the_beast'), -- Tame Mistbat               (NPC 16353, Quest 9485)
 (30105,'spell_hun_tame_beast');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
