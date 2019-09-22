e510e24505dc7d9bb39498135adfc94a746bb670
Francesco Borzì <borzifrancesco@gmail.com>
2019-09-22 13:25:19 +0200

    Update rev_1569111515040203886.sql

--- data/sql/updates/pending_db_world/rev_1569111515040203886.sql
+++ data/sql/updates/pending_db_world/rev_1569111515040203886.sql
@@ -1,7 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1569111515040203886');
 
-
-DELETE FROM gameobject_template_addon WHERE entry = 192081;
-
-INSERT INTO gameobject_template_addon (entry, faction, flags, mingold, maxgold) VALUES
+DELETE FROM `gameobject_template_addon` WHERE `entry` = 192081;
+INSERT INTO `gameobject_template_addon` (`entry`, `faction`, `flags`, `mingold`, `maxgold`) VALUES
 (192081, 35, 4, 0, 0);
