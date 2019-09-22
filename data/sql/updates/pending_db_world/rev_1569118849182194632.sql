45cadbad4dff12d50c7425876602982d5ca4fe5f
Francesco Borzì <borzifrancesco@gmail.com>
2019-09-22 13:22:12 +0200

    Update rev_1569118849182194632.sql

--- data/sql/updates/pending_db_world/rev_1569118849182194632.sql
+++ data/sql/updates/pending_db_world/rev_1569118849182194632.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1569118849182194632');
 
-UPDATE quest_template set Flags = 12424 where ID = 10110; /* Horde Side */
-UPDATE quest_template set Flags = 12424 where ID = 10106; /* Alliance Side*/
+UPDATE `quest_template` SET `Flags` = 12424 WHERE `ID` IN (10110, 10106);
