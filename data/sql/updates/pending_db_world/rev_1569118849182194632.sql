a6ec1f37db1c1e7e037244afa3c95e257489a90d
WiZZy <bilucristian25@gmail.com>
2019-09-22 05:23:18 +0300

    Hellfire Fortifications
    Fixes PVP Flag

---
+++ data/sql/updates/pending_db_world/rev_1569118849182194632.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1569118849182194632');
+
+UPDATE quest_template set Flags = 12424 where ID = 10110; /* Horde Side */
+UPDATE quest_template set Flags = 12424 where ID = 10106; /* Alliance Side*/
