e0b170f221ffed1c85441fc1241032d3d3cced1c
AzerothCoreBot <azerothcorebot@gmail.com>
2019-10-03 12:39:50 +0000

    Import pending SQL update file...
    Referenced commit(s): afe7937396b33c581ffb1e7de091f2dd36a19083

--- data/sql/updates/db_world/2019_10_03_00.sql
+++ data/sql/updates/db_world/2019_10_03_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_10_02_00 -> 2019_10_03_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_10_02_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_10_02_00 2019_10_03_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1568383989969278689'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1568383989969278689');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 20071 AND `source_type` = 0;
@@ -18,3 +34,12 @@ VALUES
 (20071,0,12,13,61,0,100,0,0,0,0,0,0,12,20101,4,10000,0,0,0,8,0,0,0,0,4328.97,2140.08,124.66,2.88,'Wind Trader Marid - Linked - Summon Creature'),
 (20071,0,13,14,61,0,100,0,0,0,0,0,0,19,768,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Wind Trader Marid - Linked - Remove Unit Flags'),
 (20071,0,14,0,61,0,100,0,0,0,0,0,0,49,0,0,0,0,0,0,21,20,0,0,0,0,0,0,0,'Wind Trader Marid - Linked - Attack Start');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
