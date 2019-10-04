60680d1f881da69dff634dca4deecfbc89ff0e5e
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-10-04 13:31:11 +0000

    also set default spell IDs for the auras in World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1331,8 +1331,8 @@ void World::LoadConfigSettings(bool reload)
     m_bool_configs[CONFIG_PRELOAD_ALL_NON_INSTANCED_MAP_GRIDS] = sConfigMgr->GetBoolDefault("PreloadAllNonInstancedMapGrids", false);
 
     // ICC buff override
-    m_int_configs[CONFIG_ICC_BUFF_HORDE] = sConfigMgr->GetIntDefault("ICC.Buff.Horde", 0);
-    m_int_configs[CONFIG_ICC_BUFF_ALLIANCE] = sConfigMgr->GetIntDefault("ICC.Buff.Alliance", 0);
+    m_int_configs[CONFIG_ICC_BUFF_HORDE] = sConfigMgr->GetIntDefault("ICC.Buff.Horde", 73822);
+    m_int_configs[CONFIG_ICC_BUFF_ALLIANCE] = sConfigMgr->GetIntDefault("ICC.Buff.Alliance", 73828);
 
     // call ScriptMgr if we're reloading the configuration
     sScriptMgr->OnAfterConfigLoad(reload);
