e4073816fb16bb446f8d9db0e786b83b271e5b4c
AzerothCoreBot <azerothcorebot@gmail.com>
2019-10-12 06:37:33 +0000

    Import pending SQL update file...
    Referenced commit(s): 4e4c6195a017e3d096b1721b4487f56e6cb8fbe8

--- data/sql/updates/db_world/2019_10_12_00.sql
+++ data/sql/updates/db_world/2019_10_12_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_10_11_00 -> 2019_10_12_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_10_11_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_10_11_00 2019_10_12_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1569001965174107468'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1569001965174107468');
 
 UPDATE `creature_template` SET `modelid1` = 29189 WHERE `entry` = 33810;
@@ -8,3 +24,12 @@ VALUES
 (33810,0,0,1,54,0,100,0,0,0,0,0,0,3,0,28880,0,0,0,0,1,0,0,0,0,0,0,0,0,'Sen''Jin Fetish - Just Summoned - Morph Model'),
 (33810,0,1,2,61,0,100,0,0,0,0,0,0,11,52619,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Sen''Jin Fetish - Linked - Cast Spell ''Enchanted Tiki Warrior: Enchanted Tiki Warrior Glow Visual'' (52619) on self'),
 (33810,0,2,0,61,0,100,0,0,0,0,0,0,87,3381000,3381001,3381002,0,0,0,1,0,0,0,0,0,0,0,0,'Sen''Jin Fetish - Linked - Call Random Script');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
