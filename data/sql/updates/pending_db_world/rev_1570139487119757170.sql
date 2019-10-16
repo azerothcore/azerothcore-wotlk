700c5d9bc1aaee602edfc06a4d049a8d52c9eb78
AzerothCoreBot <azerothcorebot@gmail.com>
2019-10-16 05:55:57 +0000

    Import pending SQL update file...
    Referenced commit(s): 3baa3c7534288a19c014afdc3c6a1fe06cd691e2

--- data/sql/updates/pending_db_world/rev_1570139487119757170.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1570139487119757170');
-
-DELETE FROM `spell_area` WHERE `area` = 4812 AND `quest_start` = 0 AND `aura_spell` = 0 AND (`racemask` = 690 OR `racemask` = 1101) AND `gender` = 2;
