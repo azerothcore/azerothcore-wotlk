3e3de57e0a1cdceae18de5ecced403013ff572a8
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-10-18 22:31:39 +0200

    Update rev_1571346137981297600.sql

--- data/sql/updates/pending_db_world/rev_1571346137981297600.sql
+++ data/sql/updates/pending_db_world/rev_1571346137981297600.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1571346137981297600');
 
-UPDATE `creature_template` SET `unit_flags` = 256 WHERE (`entry` = 35984);
+UPDATE `creature_template` SET `unit_flags` = 256 WHERE `entry` = 35984;
