aad2b9680970c48aec7639479d41050e09f79146
GMKyle <kyle@SO-06628>
2019-10-25 15:43:10 -0400

    Cleanning

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4362,23 +4362,10 @@ void SpellMgr::LoadDbcDataCorrections()
         case 50990: // Flame Breath (Catapult)
             spellInfo->EffectRadiusIndex[EFFECT_0] = 19; // 18yd
             break;
-<<<<<<< HEAD
-<<<<<<< HEAD
-         //This spells should interrupts npc spell casting(SPELL_ATTR7_INTERRUPT_ONLY_NONPLAYER)
-         case 47476: // Deathknight - Strangulate
-         case 15487: // Priest - Silence
-         case 5211:  // Druid - Bash  - R1
-         case 6798:  // Druid - Bash  - R2
-         case 8983:  // Druid - Bash  - R3
-            spellInfo->AttributesEx7 |= SPELL_ATTR7_INTERRUPT_ONLY_NONPLAYER;
-=======
         case 56103: // Jormungar Bite
             spellInfo->EffectImplicitTargetA[EFFECT_0] = TARGET_UNIT_TARGET_ENEMY;
             spellInfo->EffectImplicitTargetB[EFFECT_0] = 0;
->>>>>>> upstream/master
             break;
-=======
->>>>>>> parent of 5e21ad54... Core/Spells: Implement SPELL_ATTR7_INTERRUPT_ONLY_NONPLAYER
 
         /////////////////////////////////
         ///// Generic NPC Spells
