7fe25ea1775c3c114927a01a2f09e55c3f82e23c
Helias <stefanoborzi32@gmail.com>
2019-10-25 21:03:00 +0200

    feat(DB/characters): added comments to worldstates

---
+++ data/sql/updates/pending_db_characters/rev_1572030074009407852.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1572030074009407852');
+
+UPDATE `worldstates` SET `comment`='NextArenaPointDistributionTime' WHERE `entry`='20001';
+UPDATE `worldstates` SET `comment`='NextWeeklyQuestResetTime' WHERE `entry`='20002';
+UPDATE `worldstates` SET `comment`='NextBGRandomDailyResetTime' WHERE `entry`='20003';
+UPDATE `worldstates` SET `comment`='cleaning_flags' WHERE `entry`='20005';
+UPDATE `worldstates` SET `comment`='NextGuildDailyResetTime' WHERE `entry`='20006';
+UPDATE `worldstates` SET `comment`='NextMonthlyQuestResetTime' WHERE `entry`='20007';
