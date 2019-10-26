78e37c31165d129e68c6164e9a59402ee25b8e40
AzerothCoreBot <azerothcorebot@gmail.com>
2019-10-26 06:22:02 +0000

    Import pending SQL update file...
    Referenced commit(s): 8d1d3b217d0ea6de2373029a0339de6ba6e662d9

--- data/sql/updates/pending_db_world/rev_1571179939629745569.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1571179939629745569');
-
--- Quest "The Exorcism of Colonel Jules": Remove duplicate gossip entry
-DELETE FROM `gossip_menu_option` WHERE `MenuID` = 8539;
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`)
-VALUES
-(8539,0,0,'I am ready, Anchorite.  Let us begin the exorcism.',20396,1,3,0,0,0,0,NULL,0,0);
