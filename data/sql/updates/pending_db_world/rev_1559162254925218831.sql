5da7342648c03819bf0d949f4adcac9ea8053731
AzerothCoreBot <azerothcorebot@gmail.com>
2019-11-05 06:56:46 +0000

    Import pending SQL update file...
    Referenced commit(s): 26998d36bb5ab5703bd445d169eaeea8f519ca3d

--- data/sql/updates/pending_db_world/rev_1559162254925218831.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1559162254925218831');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_pal_item_healing_discount' AND `spell_id`=37705;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(37705, 'spell_item_eye_of_gruul_healing_discount');
