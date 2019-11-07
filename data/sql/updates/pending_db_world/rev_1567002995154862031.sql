15771aa963ddc2d9b8ffaefd3e7269dcf1c58554
GMKyle <delvalmorell860604@gmail.com>
2019-11-07 02:25:03 -0500

    fix(Core/Dungeon): Elder Nadox hardcore texts + Ahn'kahar Spell Flinger spells. (#2244)

---
+++ data/sql/updates/pending_db_world/rev_1567002995154862031.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1567002995154862031');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (56698, 59102, 56702, 59103);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(56698, 'spell_gen_default_count_pct_from_max_hp'),
+(59102, 'spell_gen_default_count_pct_from_max_hp'),
+(56702, 'spell_shadow_sickle_periodic_damage'),
+(59103, 'spell_shadow_sickle_periodic_damage');
