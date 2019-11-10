2cfe1b608816415634b19e90da0a516a6a90b1d2
Winfidonarleyan <dowlandtop@yandex.com>
2019-11-11 01:41:12 +0700

    feat(License): delete address to license file

--- src/server/game/AI/ScriptedAI/ScriptedGossip.h
+++ src/server/game/AI/ScriptedAI/ScriptedGossip.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
+/* Copyright (C) 2016+     AzerothCore <www.azerothcore.org>
  *
  *
  * This program is free software licensed under GPL version 2
