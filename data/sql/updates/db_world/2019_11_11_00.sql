e1498b83ea531f62e7bc248edf18c0c8735352d5
AzerothCoreBot <azerothcorebot@gmail.com>
2019-11-11 08:21:13 +0000

    Import pending SQL update file...
    Referenced commit(s): 4e6a8a27568c00a842b396429a545dfadd7aad1f

--- data/sql/updates/db_world/2019_11_11_00.sql
+++ data/sql/updates/db_world/2019_11_11_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_11_09_00 -> 2019_11_11_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_11_09_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_11_09_00 2019_11_11_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1571737953110374673'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1571737953110374673');
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = 17179;
@@ -14,3 +30,12 @@ DELETE FROM `creature_text` WHERE `CreatureID` = 10508;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`)
 VALUES
 (10508,0,0,'THIS CANNOT BE!!',14,0,100,0,0,0,6371,0,'Ras Frostwhisper');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
