247fe6a785ba3923c89dcbe9335b3ef5f1c9a6d4
Winfidonarleyan <dowlandtop@yandex.com>
2019-11-15 18:16:32 +0700

    Fixed build for win

--- deps/poco/Foundation/include/Poco/DirectoryIteratorStrategy.h
+++ deps/poco/Foundation/include/Poco/DirectoryIteratorStrategy.h
@@ -27,12 +27,33 @@
 
 namespace Poco {
 
+// STRUCT TEMPLATE unary_function
+template <class _Arg, class _Result>
+struct unary_function { // base class for unary functions
+    using argument_type = _Arg;
+    using result_type = _Result;
+};
+
+// CLASS TEMPLATE pointer_to_unary_function
+template <class _Arg, class _Result, class _Fn = _Result(*)(_Arg)>
+class pointer_to_unary_function : public unary_function<_Arg, _Result> { // functor adapter (*pfunc)(left)
+public:
+    explicit pointer_to_unary_function(_Fn _Left) : _Pfun(_Left) { // construct from pointer
+    }
+
+    _Result operator()(_Arg _Left) const { // call function with operand
+        return _Pfun(_Left);
+    }
+
+protected:
+    _Fn _Pfun; // the function pointer
+};
 
 class Foundation_API TraverseBase
 {
 public:
 	typedef std::stack<DirectoryIterator> Stack;
-	typedef std::pointer_to_unary_function<const Stack&, UInt16> DepthFunPtr;
+	typedef pointer_to_unary_function<const Stack&, UInt16> DepthFunPtr;
 
 	enum
 	{
