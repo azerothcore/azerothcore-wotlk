30afca4d5b12b8fa0930f84e92d3dc422e914e61
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2019-11-17 13:33:04 +0100

    Update rev_1573992675866478300.sql

--- data/sql/updates/pending_db_world/rev_1573992675866478300.sql
+++ data/sql/updates/pending_db_world/rev_1573992675866478300.sql
@@ -1,4 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1573992675866478300');
 
 DELETE FROM `creature_queststarter` WHERE `id` = 13278 AND `quest` = 6804;
+DELETE FROM `creature_questender` WHERE `id` = 13278 AND `quest` = 6804;
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (13278, 6804);
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES (13278, 6804);
