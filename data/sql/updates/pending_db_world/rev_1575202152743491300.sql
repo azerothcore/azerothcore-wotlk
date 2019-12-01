43758a0fa86a02c58aca55b702494684c0c1e994
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2019-12-01 13:12:31 +0100

    fix(DB): Missing Titles for Weapon Masters
    The titles for weapon master in creature_template_locales for all
    locales were missing.
    Addes deDE titles.
    Compared with creature_template were the titles are existing.

---
+++ data/sql/updates/pending_db_world/rev_1575202152743491300.sql
@@ -0,0 +1,13 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575202152743491300');
+
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 17005 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 2704 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11869 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 16773 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11870 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11868 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11867 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11865 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeisterin' WHERE entry = 13084 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeisterin' WHERE entry = 11866 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET Title = 'Waffenmeisterin' WHERE entry = 16621 AND locale = 'deDE';
