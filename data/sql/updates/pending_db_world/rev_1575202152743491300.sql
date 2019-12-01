c41600c52ebc5b920dd0a629a91e355381ae84d3
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2019-12-01 13:30:11 +0100

    Update rev_1575202152743491300.sql
    code compacted

--- data/sql/updates/pending_db_world/rev_1575202152743491300.sql
+++ data/sql/updates/pending_db_world/rev_1575202152743491300.sql
@@ -1,13 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575202152743491300');
 
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 17005 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 2704 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11869 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 16773 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11870 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11868 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11867 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeister' WHERE entry = 11865 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeisterin' WHERE entry = 13084 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeisterin' WHERE entry = 11866 AND locale = 'deDE';
-UPDATE `creature_template_locale` SET Title = 'Waffenmeisterin' WHERE entry = 16621 AND locale = 'deDE';
+UPDATE `creature_template_locale` SET `Title` = 'Waffenmeister' WHERE `locale` = 'deDE' AND `entry` IN (17005,  2704, 11869, 16773, 11870, 11868, 11867, 11865);
+UPDATE `creature_template_locale` SET `Title` = 'Waffenmeisterin' WHERE `locale` = 'deDE' AND `entry` IN (13084,  11866, 16621);
