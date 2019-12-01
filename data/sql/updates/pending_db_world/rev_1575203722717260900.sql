403b03084be4b3eb050d5b92317ebf1acdc89a6b
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2019-12-01 13:42:30 +0100

    fix(DB): Missing Titles for Auctioneers in locales
    The titles for auctioneers in Silvermoon and The Exodar in creature_template_locales for all
    locales were missing.

---
+++ data/sql/updates/pending_db_world/rev_1575203722717260900.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575203722717260900');
+
+UPDATE `creature_template_locale` SET `Title` = 'Auktionator' WHERE `locale` = 'deDE' AND `entry` IN (16627, 16629, 17627, 17628, 16707, 18348);
+UPDATE `creature_template_locale` SET `Title` = 'Auktionatorin' WHERE `locale` = 'deDE' AND `entry` IN (17629, 18761, 16628, 18349);
