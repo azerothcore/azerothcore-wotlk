8755dad1fadd4cc87f8d8a8597b17daabb0beb99
vhiperdev <vhipercode@gmail.com>
2019-12-02 08:21:18 -0300

    update

--- data/sql/updates/pending_db_world/rev_1575285367471524200.sql
+++ data/sql/updates/pending_db_world/rev_1575285367471524200.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575285367471524200');
 
-UPDATE `command` SET `name`='reload acore_string', `help`='Syntax: .reload acore_stringReload trinity_string table.' WHERE `name`='reload trinity_string';
+UPDATE `command` SET `name`='reload acore_string', `help`='Syntax: .reload acore_stringReload acore_string table.' WHERE `name`='reload trinity_string';
