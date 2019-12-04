a211c85a247042267adc066c6fedb4a317d60b5b
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-12-04 22:58:49 +0000

    use a new CommandScript

--- data/sql/updates/pending_db_world/rev_1575409612123583009.sql
+++ data/sql/updates/pending_db_world/rev_1575409612123583009.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575409612123583009');
 
-DELETE FROM `command` WHERE `name` IN ('playerlearnspell','playerunlearnspell');
+DELETE FROM `command` WHERE `name` IN ('player learn','player unlearn');
 INSERT INTO `command` (`name`, `security`, `help`)
 VALUES
-('playerlearnspell',2,'Syntax: .playerlearnspell #playername #spell.'),
-('playerunlearnspell',2,'Syntax: .playerunlearnspell #playername #spell.');
+('player learn',2,'Syntax: .player learn #playername #spell.'),
+('player unlearn',2,'Syntax: .player unlearn #playername #spell.');
---
+++ src/server/scripts/Commands/cs_player.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ */
+
+#include "Chat.h"
+#include "ScriptMgr.h"
+#include "Language.h"
+#include "Player.h"
+
+class player_commandscript : public CommandScript
+{
+public:
+    player_commandscript() : CommandScript("player_commandscript") { }
+
+    std::vector<ChatCommand> GetCommands() const override
+    {
+        static std::vector<ChatCommand> playerCommandTable =
+        {
+            { "learn",               SEC_GAMEMASTER,  true, &HandlePlayerLearnCommand,           "" },
+            { "unlearn",             SEC_GAMEMASTER,  true, &HandlePlayerUnLearnCommand,         "" }
+        };
+
+        static std::vector<ChatCommand> commandTable =
+        {
+            { "player",              SEC_GAMEMASTER,  true, nullptr,                             "", playerCommandTable }
+        };
+        return commandTable;
+    }
+
+    static bool HandlePlayerLearnCommand(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* playerName = strtok((char*)args, " ");
+        char* spellid = strtok(nullptr, " ");
+        char* all = strtok(nullptr, " ");
+        Player* targetPlayer = FindPlayer(handler, playerName);
+        if (!spellid || !targetPlayer)
+            return false;
+
+        uint32 spell = handler->extractSpellIdFromLink(spellid);
+        if (!spell)
+            return false;
+
+        SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(spell);
+        if (!spellInfo)
+        {
+            handler->PSendSysMessage(LANG_COMMAND_NOSPELLFOUND);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        if (!SpellMgr::IsSpellValid(spellInfo))
+        {
+            handler->PSendSysMessage(LANG_COMMAND_SPELL_BROKEN, spell);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        bool allRanks = all ? (strncmp(all, "all", strlen(all)) == 0) : false;
+
+        if (!allRanks && targetPlayer->HasSpell(spell))
+        {
+            handler->PSendSysMessage(LANG_TARGET_KNOWN_SPELL, handler->GetNameLink(targetPlayer).c_str());
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        if (allRanks)
+            targetPlayer->learnSpellHighRank(spell);
+        else
+            targetPlayer->learnSpell(spell);
+
+        uint32 firstSpell = sSpellMgr->GetFirstSpellInChain(spell);
+        if (GetTalentSpellCost(firstSpell))
+            targetPlayer->SendTalentsInfoData(false);
+
+        return true;
+    }
+
+    static bool HandlePlayerUnLearnCommand(ChatHandler* handler, char const* args)
+    {
+        if (!*args)
+            return false;
+
+        char* playerName = strtok((char*)args, " ");
+        char* spellid = strtok(nullptr, " ");
+        char* all = strtok(nullptr, " ");
+        Player* targetPlayer = FindPlayer(handler, playerName);
+        if (!spellid || !targetPlayer)
+            return false;
+
+        uint32 spell = handler->extractSpellIdFromLink(spellid);
+        if (!spell)
+            return false;
+
+        bool allRanks = all ? (strncmp(all, "all", strlen(all)) == 0) : false;
+
+        if (allRanks)
+            spell = sSpellMgr->GetFirstSpellInChain (spell);
+
+        if (targetPlayer->HasSpell(spell))
+            targetPlayer->removeSpell(spell, SPEC_MASK_ALL, false);
+        else
+            handler->SendSysMessage(LANG_FORGET_SPELL);
+
+        if (GetTalentSpellCost(spell))
+            targetPlayer->SendTalentsInfoData(false);
+
+        return true;
+    }
+
+private:
+    static Player* FindPlayer(ChatHandler* handler, char* playerName)
+    {
+        if (!playerName)
+            return nullptr;
+
+        Player* targetPlayer;
+        if (!handler->extractPlayerTarget(playerName, &targetPlayer))
+            return nullptr;
+
+        if (!targetPlayer)
+        {
+            handler->SendSysMessage(LANG_PLAYER_NOT_FOUND);
+            handler->SetSentErrorMessage(true);
+            return nullptr;
+        }
+
+        return targetPlayer;
+    }
+};
+
+void AddSC_player_commandscript()
+{
+    new player_commandscript();
+}
