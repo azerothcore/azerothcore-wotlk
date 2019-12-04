a5a3897a7c617b0d7571ed93c579ed26d120d403
VhiperDEV <39068627+vhiperdev@users.noreply.github.com>
2019-12-04 03:21:55 -0300

    fix(DB): Command reload acore_string (#2484)

---
+++ data/sql/updates/pending_db_world/rev_1575285367471524200.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575285367471524200');
+
+UPDATE `command` SET `name`='reload acore_string', `help`='Syntax: .reload acore_string\nReload acore_string table.' WHERE `name`='reload trinity_string';
