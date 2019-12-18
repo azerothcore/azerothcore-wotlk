dadfccb7be9f4b8aa391f681e51b1294f90efeca
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2019-12-18 14:21:43 +0330

    Check Bag, Work

---
+++ data/sql/updates/pending_db_world/rev_1576659911412868500.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1576659911412868500');
+
+INSERT INTO `command` VALUES
+('character check bag', 2, 'Syntax: .character check bag [$target_player]\r #bagSlot 1 - 4'),
+('character check work', 2, 'Syntax: .character check work [$target_player]\r\nShow known professions list for selected player');
