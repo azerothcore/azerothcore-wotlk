16b45bf3340ee11d7f4f50daeadaf7b752ce7579
AzerothCoreBot <azerothcorebot@gmail.com>
2019-12-20 08:14:35 +0000

    Import pending SQL update file...
    Referenced commit(s): 4e8c3fd3440e6f773c1f8aadd70d8b86a7b20e55

--- data/sql/updates/pending_db_world/rev_1575409612123583009.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1575409612123583009');
-
-DELETE FROM `command` WHERE `name` IN ('player learn','player unlearn');
-INSERT INTO `command` (`name`, `security`, `help`)
-VALUES
-('player learn',2,'Syntax: .player learn #playername #spell [all].'),
-('player unlearn',2,'Syntax: .player unlearn #playername #spell [all].');
