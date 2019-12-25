1cc4b54b3fad9146720723d9b213019514ddb7d3
AzerothCoreBot <azerothcorebot@gmail.com>
2019-12-25 08:36:01 +0000

    Import pending SQL update file...
    Referenced commit(s): 03c791e127d90033e1ac40acd3da82680e7c3a99

--- data/sql/updates/db_world/2019_12_25_00.sql
+++ data/sql/updates/db_world/2019_12_25_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2019_12_24_00 -> 2019_12_25_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2019_12_24_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2019_12_24_00 2019_12_25_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1576450864355101200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1576450864355101200');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 17083 AND `source_type` = 0;
@@ -12,3 +28,12 @@ DELETE FROM `spell_script_names` WHERE `spell_id` = 30505;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`)
 VALUES
 (30505, 'spell_tsh_shadow_bolt');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
