dd0f02953904ae4a9ca7a34fb3c3646969ceb28a
AzerothCoreBot <azerothcorebot@gmail.com>
2019-12-26 08:12:06 +0000

    Import pending SQL update file...
    Referenced commit(s): b776e68665def38af797b02e6a4ed68651bd0f7e

--- data/sql/updates/pending_db_world/rev_1576535250226124911.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1576535250226124911');
-
-DELETE FROM `spell_script_names` WHERE `spell_id` = 70937;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`)
-VALUES
-(70937, 'spell_mage_glyph_of_eternal_water');
