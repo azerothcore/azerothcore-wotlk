03be665009d4fc57cad985b3c8b610fba4828683
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-12-29 23:46:04 +0100

    fix(DB/creature): Great Wavethrasher position

---
+++ data/sql/updates/pending_db_world/rev_1577659546451711787.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1577659546451711787');
+
+UPDATE `creature` SET `position_x` = 2347.43, `position_y` = -6767.51, `position_z` = 17.2098 WHERE `guid` = 35968;
