57cd14491c7eb3eb0e8fe238bb0f7ebb6b3ebbaf
AzerothCoreBot <azerothcorebot@gmail.com>
2020-01-04 07:49:17 +0000

    Import pending SQL update file...
    Referenced commit(s): 811233974a63a2d726cb53fb7fc86bbcad6a4923

--- data/sql/updates/pending_db_characters/rev_1572815191193825836.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1572815191193825836');
-
-UPDATE `worldstates` SET `comment`='NextDailyQuestResetTime' WHERE `entry`=20005;
-DELETE FROM `worldstates` WHERE `entry`=20004;
-INSERT INTO `worldstates` (`entry`, `value`, `comment`) VALUES(20004, 0, 'cleaning_flags');
