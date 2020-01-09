0a66799a98e79d4ac653b7e2af85de2d3eb05465
Giacomo Pozzoni <giacomopoz@gmail.com>
2020-01-09 19:38:45 +0100

    Core/PathGenerator: Fix path generator returning shortcuts when start and end are on the same polygon (#24036)
    * Core/PathGenerator: Fix path generator returning shortcuts when start and end are on the same polygon
    
    Fix path generator returning shortcuts when start and end are on the same polygon by handling this case as if start and end were on 2 different polygons. This will ensure BuildPointPath() gets called which calls FindSmoothPath(), making sure each step is not longer than SMOOTH_PATH_STEP_SIZE (4 yards)
    
    * Change ingame cast error message to SPELL_FAILED_NOPATH from SPELL_FAILED_OUT_OF_RANGE if the generated path is too long

--- src/tools/mmaps_generator/MapBuilder.cpp
+++ src/tools/mmaps_generator/MapBuilder.cpp
@@ -872,16 +872,8 @@ namespace MMAP
             return static_cast<uint32>(m_mapid) != mapID;
 
         if (m_skipContinents)
-            switch (mapID)
-            {
-                case 0:
-                case 1:
-                case 530:
-                case 571:
-                    return true;
-                default:
-                    break;
-            }
+            if (isContinentMap(mapID))
+                return true;
 
         if (m_skipJunkMaps)
             switch (mapID)
@@ -955,6 +947,20 @@ namespace MMAP
             case 712:
             case 713:
             case 718:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    bool MapBuilder::isContinentMap(uint32 mapID)
+    {
+        switch (mapID)
+        {
+            case 0:
+            case 1:
+            case 530:
+            case 571:
                 return true;
             default:
                 return false;
