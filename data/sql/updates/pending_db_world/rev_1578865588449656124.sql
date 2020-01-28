f6232315c521b837feed735b57becc242147e130
AzerothCoreBot <azerothcorebot@gmail.com>
2020-01-28 07:20:20 +0000

    Import pending SQL update file...
    Referenced commit(s): 7efd0a7e9a923c8a3b31f0a708a70363a13189eb

--- data/sql/updates/pending_db_world/rev_1578865588449656124.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1578865588449656124');
-
-UPDATE `creature` SET `spawndist` = 5, `MovementType` = 1 WHERE `guid` IN (96069,96100);
-UPDATE `creature_addon` SET `bytes1` = 0 WHERE `guid` IN (96069,96100);
-
-UPDATE `creature` SET `spawndist` = 0, `MovementType` = 0 WHERE `id` = 26369 AND `guid` IN (SELECT `guid` FROM `creature_addon` WHERE `bytes1` = 1);
