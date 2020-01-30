b1bcd21d51a77f9f8796e93b7dbe66ccbae2418e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-01-30 08:36:08 +0000

    Import pending SQL update file...
    Referenced commit(s): 14598dbee93e7850701722b944dbc35e98c4b616

--- data/sql/updates/db_world/2020_01_30_00.sql
+++ data/sql/updates/db_world/2020_01_30_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_01_28_00 -> 2020_01_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_01_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_01_28_00 2020_01_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1579041492812648634'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1579041492812648634');
 
 -- Ironhide Devilsaur: Use SAI
@@ -20,3 +36,12 @@ VALUES
 (6499,0,0,0,11,0,100,0,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Ironhide Devilsaur - On Respawn - Set Active On'),
 (6500,0,1,0,11,0,100,0,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Tyrant Devilsaur - On Respawn - Set Active On'),
 (6584,0,3,0,11,0,100,0,0,0,0,0,0,48,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'King Mosh - On Respawn - Set Active On');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
