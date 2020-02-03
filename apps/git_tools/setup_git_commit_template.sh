23322eb619488e05799eb0953a20e77b72e09cce
Barbz <BarbzYHOOL@users.noreply.github.com>
2020-02-03 21:47:39 +0100

    fix(apps): Script for default git commit template returns output
    Before that, it didn't return anything and could confuse people

--- apps/git_tools/setup_git_commit_template.sh
+++ apps/git_tools/setup_git_commit_template.sh
@@ -2,3 +2,4 @@
 
 ## Set a local git commit template
 git config --local commit.template ".git_commit_template.txt" ;
+echo "--- Successfully set the default commit template for this AzerothCore repository only. Verify with: git config -e"
