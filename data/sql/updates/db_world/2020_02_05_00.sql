82ca8874595bbf2bff86e0caa34394d3be9ab56d
AzerothCoreBot <azerothcorebot@gmail.com>
2020-02-05 09:13:23 +0000

    Import pending SQL update file...
    Referenced commit(s): 2fe2909f90083304588396ac8f0e545cf4de0ed3

--- data/sql/updates/db_world/2020_02_05_00.sql
+++ data/sql/updates/db_world/2020_02_05_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_01_31_00 -> 2020_02_05_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_01_31_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_01_31_00 2020_02_05_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1579215466676030435'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1579215466676030435');
 
 UPDATE `creature` SET `spawndist` = 5, `MovementType` = 1 WHERE `id` = 27131;
@@ -11,3 +27,12 @@ VALUES
 (2713100,9,0,0,0,0,100,0,0,0,0,0,0,103,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Grizzly Bear - On Script - Set Root On'),
 (2713100,9,1,0,0,0,100,0,0,0,0,0,0,5,7,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Grizzly Bear - On Script - Play Emote ''ONESHOT_EAT'''),
 (2713100,9,2,0,0,0,100,0,2000,2000,0,0,0,103,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Grizzly Bear - On Script - Set Root Off');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
