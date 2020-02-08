5413cf2ce32d87656513a7e790c04c2c43d7a6c8
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-02-09 00:08:46 +0100

    fix flags_extra set 0
    Set flags_extra from 1 to 0 because creature needs no instance binds

--- data/sql/updates/pending_db_world/rev_1581196474128672400.sql
+++ data/sql/updates/pending_db_world/rev_1581196474128672400.sql
@@ -1,4 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1581196474128672400');
 
+UPDATE `creature_template` SET `flags_extra` = 0 WHERE `entry` = 15625;
 DELETE FROM `creature` WHERE `guid`=3110362;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES (3110362, 15625, 0, 0, 0, 1, 1, 0, 0, -10325.9, -490.507, 50.3208, 5.67792, 300, 0, 0, 832750, 0, 0, 0, 0, 0, '', 0);
