d4264c296992b17c828468a9de1161577a4b8d82
AzerothCoreBot <azerothcorebot@gmail.com>
2020-02-10 07:06:34 +0000

    Import pending SQL update file...
    Referenced commit(s): 8a83faad9f071dad22f93c00624648683e324ee2

--- data/sql/updates/db_world/2020_02_10_00.sql
+++ data/sql/updates/db_world/2020_02_10_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_02_05_00 -> 2020_02_10_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_02_05_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_02_05_00 2020_02_10_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1579389922264853693'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1579389922264853693');
 
 -- Sasha: Fix talk targets; fix shooting Anatoly
@@ -27,3 +43,12 @@ VALUES
 (2762600,9,0,0,0,0,100,0,500,500,0,0,0,53,1,27626,0,0,0,0,1,0,0,0,0,0,0,0,0,'Tatjana''s Horse - On Script - Start WP movement'),
 (2762600,9,1,0,0,0,100,0,0,0,0,0,0,2,35,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Tatjana''s Horse - On Script - Set Faction'),
 (2762600,9,2,0,0,0,100,0,0,0,0,0,0,64,1,0,0,0,0,0,7,0,0,0,0,0,0,0,0,'Tatjana''s Horse - On Script - Store Target List 1');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
