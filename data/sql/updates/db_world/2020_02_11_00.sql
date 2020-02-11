1646e3001ecd3120d479975f5f27bcb4bc2a8fa6
AzerothCoreBot <azerothcorebot@gmail.com>
2020-02-11 07:12:46 +0000

    Import pending SQL update file...
    Referenced commit(s): fea38aa939ba67f5fad14bd34ef5c7369cb72a04

--- data/sql/updates/db_world/2020_02_11_00.sql
+++ data/sql/updates/db_world/2020_02_11_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_02_10_00 -> 2020_02_11_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_02_10_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_02_10_00 2020_02_11_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1579458178491145006'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1579458178491145006');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 11897 AND `source_type` = 0;
@@ -5,3 +21,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 VALUES
 (11897,0,0,0,0,0,100,0,2000,4000,8000,11000,0,11,18663,0,0,0,0,0,2,0,0,0,0,0,0,0,0,'Duskwing - In Combat - Cast ''Shadow Shock'''),
 (11897,0,1,0,0,0,100,0,6000,9000,14000,17000,0,11,8281,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Duskwing - In Combat - Cast ''Sonic Burst''');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
