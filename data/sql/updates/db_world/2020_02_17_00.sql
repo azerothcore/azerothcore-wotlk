3b5b7a506f4eb4e6c0ebcdcf4378eaa587949751
AzerothCoreBot <azerothcorebot@gmail.com>
2020-02-17 07:11:04 +0000

    Import pending SQL update file...
    Referenced commit(s): 2debf7b15384d8215fb0054f0c7529c6d94757cf

--- data/sql/updates/db_world/2020_02_17_00.sql
+++ data/sql/updates/db_world/2020_02_17_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_02_15_00 -> 2020_02_17_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_02_15_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_02_15_00 2020_02_17_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1579770074985495361'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1579770074985495361');
 
 -- Spark Nilminer: Fix wrong gossip options by removing the obsolete gossip menu options for Ragged John
@@ -41,3 +57,12 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`)
 VALUES
 (15,2081,0,0,0,9,0,4244,0,0,0,0,0,'','Show gossip option if quest ''Chasing A-Me 01 (Part 2)'' is taken');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
