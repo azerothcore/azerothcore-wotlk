d185c9052e5851bc7893dd4c353fe6449da44930
gengarshadowball <60325009+gengarshadowball@users.noreply.github.com>
2020-02-20 12:34:21 +0000

    Update data/sql/updates/pending_db_world/rev_1580747652284896800.sql
    Co-Authored-By: Barbz <BarbzYHOOL@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1580747652284896800.sql
+++ data/sql/updates/pending_db_world/rev_1580747652284896800.sql
@@ -2,4 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1580747652284896800');
 DELETE FROM `command` WHERE  `name`='demorph';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('morph target', '1', 'Syntax: .morph target #displayid - Change the target''s current model id to #displayid.');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('morph reset', '1', 'Syntax: .morph reset - Doesn''t use any parameters to reset the target''s model');
-UPDATE `command` SET `help`='Syntax: .morph $subcommand\r\nType .morph to see the list of possible subcommands or .help modify $subcommand to see info on subcommands' WHERE  `name`='morph';
+UPDATE `command` SET `help`='Syntax: .morph $subcommand\r\nType .morph to see the list of possible subcommands or ".help morph" to see info on subcommands' WHERE  `name`='morph';
