1c505670a30e2ff4ad399b8ffd52e4ecfb98d6b5
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-02-25 21:48:46 +0100

    fix(DB): Moon Priestess Amara pathing
    Use creature_formations instead of pathing for each npc.
    
    Closes #2700

---
+++ data/sql/updates/pending_db_world/rev_1582663580844407800.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1582663580844407800');
+
+UPDATE `creature_addon` SET `path_id`=0 WHERE `guid` IN (46394, 46416); 
+
+DELETE FROM `creature_formations` WHERE `leaderGUID` = 46414; 
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES 
+(46414,46414,0,0,515,0,0), 
+(46414,46394,2,135,515,0,0), 
+(46414,46416,3,225,515,0,0);  
