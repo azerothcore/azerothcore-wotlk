71590410fd2532cba48167813a80437915171f6c
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-02-25 22:44:11 +0100

    delete waypoint data

--- data/sql/updates/pending_db_world/rev_1582663580844407800.sql
+++ data/sql/updates/pending_db_world/rev_1582663580844407800.sql
@@ -1,6 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1582663580844407800');
 
 UPDATE `creature_addon` SET `path_id`=0 WHERE `guid` IN (46394, 46416); 
+DELETE FROM `waypoint_data` WHERE `id` IN (463940,464160);
 
 DELETE FROM `creature_formations` WHERE `leaderGUID` = 46414; 
 INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES 
