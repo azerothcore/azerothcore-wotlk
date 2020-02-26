16334b3fe271eb69f42350186787584dedff3b5d
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2020-02-26 12:10:29 +0100

    Update rev_1582472809971331600.sql

--- data/sql/updates/pending_db_world/rev_1582472809971331600.sql
+++ data/sql/updates/pending_db_world/rev_1582472809971331600.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1582472809971331600');
 
-UPDATE `reputation_reward_rate` SET `quest_repeatable_rate`='1' WHERE  `faction`=970;
+UPDATE `reputation_reward_rate` SET `quest_repeatable_rate` = 1 WHERE  `faction` = 970;
