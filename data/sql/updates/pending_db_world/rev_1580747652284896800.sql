3241135d625b95880cc8a61ec1ca3505a8490378
AzerothCoreBot <azerothcorebot@gmail.com>
2020-03-01 09:37:43 +0000

    Import pending SQL update file...
    Referenced commit(s): 857e799298c06f328d4b0f189148cbcbb51595e3

--- data/sql/updates/pending_db_world/rev_1580747652284896800.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1580747652284896800');
-DELETE FROM `command` WHERE  `name`='demorph';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES ('morph target', '1', 'Syntax: .morph target #displayid - Change the selected target''s current model id to #displayid.');
-INSERT INTO `command` (`name`, `security`, `help`) VALUES ('morph reset', '1', 'Syntax: .morph reset - Doesn''t use any parameters to reset the selected target''s model');
-UPDATE `command` SET `help`='Syntax: .morph $subcommand\r\nType .morph to see the list of possible subcommands or ".help morph" to see info on subcommands' WHERE  `name`='morph';
