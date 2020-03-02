831dfb4d71a6e4f05e5f344282775d315acf953c
AzerothCoreBot <azerothcorebot@gmail.com>
2020-03-02 22:36:48 +0000

    Import pending SQL update file...
    Referenced commit(s): e285ac4a2c924e9ab3b1ac5cb89a3d91431e09a4

--- data/sql/updates/pending_db_world/rev_1581974554600178400.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1581974554600178400');
-UPDATE `creature_template` SET `faction` = 35 WHERE `entry` = 4787;
-UPDATE `creature_template_locale` SET `Name` = 'Argentumwache Thaelrid', `Title` = 'Die Argentumdämmerung' WHERE `entry` = 4787 AND `locale` = 'deDE';
-UPDATE `creature_template_locale` SET `Name` = 'Guardia Argenta Thaelrid', `Title` = 'El Alba Argenta' WHERE `entry` = 4787 AND `locale` = 'esES';
-UPDATE `creature_template_locale` SET `Name` = 'Guardia Argenta Thaelrid', `Title` = 'El Alba Argenta' WHERE `entry` = 4787 AND `locale` = 'esMX';
-UPDATE `creature_template_locale` SET `Name` = 'Garde d’argent Thaelrid', `Title` = 'L''Aube d''argent' WHERE `entry` = 4787 AND `locale` = 'frFR';
