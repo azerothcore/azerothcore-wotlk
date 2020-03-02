e684483004cd8d6f2e76cf5e96377ad6c01bdcde
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-03-02 22:34:43 +0100

    ppdate to correct research links

--- data/sql/updates/pending_db_world/rev_1583183801743154900.sql
+++ data/sql/updates/pending_db_world/rev_1583183801743154900.sql
@@ -11,13 +11,13 @@ UPDATE `quest_template_locale` SET `Objectives`='Bringt die Klaue von Scharfkral
 UPDATE `quest_template_locale` SET `Details`='Der mächtige Hippogryph Scharfkralle wurde getötet und die Klaue der erschlagenen Bestie dient als Beweis für Euren Sieg.$b$bSenani Donnerherz im Splitterholzposten wird zum Beweis Eurer Tat sicher gern diese Trophäe sehen wollen.' WHERE `ID`=2 AND `locale`='deDE';
 
 -- Quest obejctives lead to Captain Tarkan in Silverwind Refuge which was added in Cataclysm. - Text in quest_template is already correct, cannot proof the correctness of other locales!
--- https://wowwiki.fandom.com/wiki/Quest:Ursangous%27s_Paw
+-- https://wow.gamepedia.com/Ursangous%27s_Paw_(quest)
 -- Quest 23
 UPDATE `quest_template_locale` SET `Objectives`='Bringt Ursangus'' Tatze zu Senani Donnerherz im Splitterholzposten im Eschental.' WHERE `ID`=23 AND `locale`='deDE';
 UPDATE `quest_template_locale` SET `Details`='Die Tatze des mächtigen Bären Ursangus stellt die Trophäe für diesen Teil der Jagd im Eschental dar. Auf der Tatze befindet sich ein kleines Halbmond-Emblem; die Bedeutung dieses Emblems bleibt jedoch ein Geheimnis.$B$BDie Tatze soll zu Senani Donnerherz im Splitterholzposten gebracht werden, um zu beweisen, dass Ihr das mächtige Tier besiegt und diesen Teil der Jagd abgeschlossen habt.' WHERE `ID`=23 AND `locale`='deDE';
 
 -- Quest details & obejctives lead to Captain Tarkan in Silverwind Refuge which was added in Cataclysm. - Text in quest_template is already correct, cannot proof the correctness of other locales!
--- https://wowwiki.fandom.com/wiki/Quest:Shadumbra%27s_Head
+-- https://wow.gamepedia.com/Shadumbra%27s_Head_(old)
 -- Quest 24
 UPDATE `quest_template_locale` SET `Objectives`='Bringt Schattumbras Kopf zu Senani Donnerherz im Splitterholzposten im Eschental.' WHERE `ID`=24 AND `locale`='deDE';
 UPDATE `quest_template_locale` SET `Details`='Der einstmals so flinke Schattumbra liegt jetzt regungslos vor Euch und der Sieg ist Euer.$B$BDer Kopf des erschlagenen Nachtsäblers ist eine passende Trophäe für diesen Kampf. Senani Donnerherz im Splitterholzposten wird eine solche Trophäe sehen wollen, um Euch den Erfolg in diesem Teil der Jagd im Eschental anzurechnen.' WHERE `ID`=24 AND `locale`='deDE';
