671727bb0cdf6f044f5168510621e7a227f31210
AzerothCoreBot <azerothcorebot@gmail.com>
2020-03-04 21:05:36 +0000

    Import pending SQL update file...
    Referenced commit(s): 1c23bf4024c7f1555742e7272385cadfb00894e5

--- data/sql/updates/db_world/2020_03_04_00.sql
+++ data/sql/updates/db_world/2020_03_04_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_03_02_00 -> 2020_03_04_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_03_02_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_03_02_00 2020_03_04_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1583183801743154900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1583183801743154900');
 
 -- Quest texts changed to patch stand 3.3.5a from 4.0.3a
@@ -21,3 +37,12 @@ UPDATE `quest_template_locale` SET `Details`='Die Tatze des mächtigen Bären Ur
 -- Quest 24
 UPDATE `quest_template_locale` SET `Objectives`='Bringt Schattumbras Kopf zu Senani Donnerherz im Splitterholzposten im Eschental.' WHERE `ID`=24 AND `locale`='deDE';
 UPDATE `quest_template_locale` SET `Details`='Der einstmals so flinke Schattumbra liegt jetzt regungslos vor Euch und der Sieg ist Euer.$B$BDer Kopf des erschlagenen Nachtsäblers ist eine passende Trophäe für diesen Kampf. Senani Donnerherz im Splitterholzposten wird eine solche Trophäe sehen wollen, um Euch den Erfolg in diesem Teil der Jagd im Eschental anzurechnen.' WHERE `ID`=24 AND `locale`='deDE';
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
