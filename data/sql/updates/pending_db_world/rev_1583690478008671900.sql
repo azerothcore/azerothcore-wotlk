37da7d36590427723ca59921c80d9fd4db4eeafc
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-03-08 19:02:53 +0100

    Fix rev and group

--- data/sql/updates/pending_db_world/rev_1583690478008671900.sql
+++ data/sql/updates/pending_db_world/rev_1583690478008671900.sql
@@ -1,4 +1,4 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('XXXXXXXXXXXX');
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1583690478008671900');
 
 DELETE FROM `gameobject_loot_template` WHERE `entry` IN (11104); -- Secret Safe
 INSERT INTO `gameobject_loot_template` (`entry`, `item`, `chance`, `questrequired`,  `groupid`, `mincount`, `maxcount`) VALUES
@@ -6,4 +6,4 @@ INSERT INTO `gameobject_loot_template` (`entry`, `item`, `chance`, `questrequire
 (11104, 22255, 25, 0, 1, 1, 1),
 (11104, 22256, 25, 0, 1, 1, 1),
 (11104, 22254, 25, 0, 1, 1, 1),
-(11104, 11309, 100, 1, 1, 1, 1);
+(11104, 11309, 100, 1, 0, 1, 1);
