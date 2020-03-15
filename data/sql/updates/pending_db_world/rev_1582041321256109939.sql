72bd51dee96d783a7bedae6872eebd709311e16d
AzerothCoreBot <azerothcorebot@gmail.com>
2020-03-15 09:11:55 +0000

    Import pending SQL update file...
    Referenced commit(s): 8231a7cc1a11b7aee139e20698ee171e08fe3d29

--- data/sql/updates/pending_db_world/rev_1582041321256109939.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1582041321256109939');
-
-UPDATE `gameobject_template_addon` SET `flags` = `flags` | 4 WHERE `entry` IN (SELECT `entry` FROM `gameobject_template` WHERE `type` = 2 AND `data3` = 0);
