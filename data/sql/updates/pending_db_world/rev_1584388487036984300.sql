66717c7ded2cb2f70dd1359d8313a88a8dc71aba
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-03-16 21:06:28 +0100

    try to make travis happy

--- data/sql/updates/pending_db_world/rev_1584388487036984300.sql
+++ data/sql/updates/pending_db_world/rev_1584388487036984300.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1584388487036984300');
 
 UPDATE `creature_template` SET `npcflag`=`npcflag`&~2 WHERE `entry` IN (15350,15351);
-DELETE FROM `creature_queststarter` WHERE `quset` IN (8388,13475,8371,8385,13477,13478,13476,8367);
+DELETE FROM `creature_queststarter` WHERE `quest` IN (8388,13475,8371,8385,13477,13478,13476,8367);
 DELETE FROM `creature_questender` WHERE `quest` IN (8388,13475,8371,8385,13477,13478,13476,8367);
 DELETE FROM `disables` WHERE `sourceType`=1 AND `entry` IN (8388,13475,8371,8385,13477,13478,13476,8367);
 INSERT INTO `disables` (`sourceType`,`entry`,`flags`,`params_0`,`params_1`,`comment`) VALUES
