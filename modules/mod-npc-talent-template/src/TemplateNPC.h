7528f0e2d6ba7d7ed692ab6df5af6f5ca35fa0ac
ArenaCraft <arenacraftwow@gmail.com>
2020-03-18 19:30:14 +0100

    ArenaCraft: Add TemplateNPC save command.

--- modules/mod-npc-talent-template/src/TemplateNPC.h
+++ modules/mod-npc-talent-template/src/TemplateNPC.h
@@ -19,77 +19,83 @@
 #define SPELL_Artisan_Riding 34091
 #define SPELL_Cold_Weather_Flying 54197
 
-
 struct TalentTemplate
 {
-    std::string    playerClass;
-    std::string    playerSpec;
-    uint32         talentId;
+    std::string playerClass;
+    std::string playerSpec;
+    uint32 talentId;
 };
 
 struct GlyphTemplate
 {
-    std::string    playerClass;
-    std::string    playerSpec;
-    uint8          slot;
-    uint32         glyph;
+    std::string playerClass;
+    std::string playerSpec;
+    uint8 slot;
+    uint32 glyph;
 };
 
 struct HumanGearTemplate
 {
-    std::string    playerClass;
-    std::string    playerSpec;
-    uint8          pos;
-    uint32         itemEntry;
-    uint32         enchant;
-    uint32         socket1;
-    uint32         socket2;
-    uint32         socket3;
-    uint32         bonusEnchant;
-    uint32         prismaticEnchant;
+    std::string playerClass;
+    std::string playerSpec;
+    uint8 pos;
+    uint32 itemEntry;
+    uint32 enchant;
+    uint32 socket1;
+    uint32 socket2;
+    uint32 socket3;
+    uint32 bonusEnchant;
+    uint32 prismaticEnchant;
 };
 
 struct AllianceGearTemplate
 {
-    std::string    playerClass;
-    std::string    playerSpec;
-    uint8          pos;
-    uint32         itemEntry;
-    uint32         enchant;
-    uint32         socket1;
-    uint32         socket2;
-    uint32         socket3;
-    uint32         bonusEnchant;
-    uint32         prismaticEnchant;
+    std::string playerClass;
+    std::string playerSpec;
+    uint8 pos;
+    uint32 itemEntry;
+    uint32 enchant;
+    uint32 socket1;
+    uint32 socket2;
+    uint32 socket3;
+    uint32 bonusEnchant;
+    uint32 prismaticEnchant;
 };
 
 struct HordeGearTemplate
 {
-    std::string    playerClass;
-    std::string    playerSpec;
-    uint8          pos;
-    uint32         itemEntry;
-    uint32         enchant;
-    uint32         socket1;
-    uint32         socket2;
-    uint32         socket3;
-    uint32         bonusEnchant;
-    uint32         prismaticEnchant;
+    std::string playerClass;
+    std::string playerSpec;
+    uint8 pos;
+    uint32 itemEntry;
+    uint32 enchant;
+    uint32 socket1;
+    uint32 socket2;
+    uint32 socket3;
+    uint32 bonusEnchant;
+    uint32 prismaticEnchant;
 };
 
-typedef std::vector<HumanGearTemplate*> HumanGearContainer;
+enum TemplateType
+{
+    TYPE_HUMAN,
+    TYPE_ALLIANCE,
+    TYPE_HORDE
+};
+
 typedef std::vector<AllianceGearTemplate*> AllianceGearContainer;
 typedef std::vector<HordeGearTemplate*> HordeGearContainer;
+typedef std::vector<HumanGearTemplate*> HumanGearContainer;
 
-typedef std::vector<TalentTemplate*> TalentContainer;
-typedef std::vector<GlyphTemplate*> GlyphContainer;
+typedef std::vector<TalentTemplate *> TalentContainer;
+typedef std::vector<GlyphTemplate *> GlyphContainer;
 
 class sTemplateNPC
 {
 public:
-    static sTemplateNPC* instance()
+    static sTemplateNPC *instance()
     {
-        static sTemplateNPC* instance = new sTemplateNPC();
+        static sTemplateNPC *instance = new sTemplateNPC();
         return instance;
     }
     void LoadTalentsContainer();
@@ -99,22 +105,17 @@ public:
     void LoadAllianceGearContainer();
     void LoadHordeGearContainer();
 
-    void ApplyGlyph(Player* player, uint8 slot, uint32 glyphID);
-    void RemoveAllGlyphs(Player* player);
-    void ApplyBonus(Player* player, Item* item, EnchantmentSlot slot, uint32 bonusEntry);
+    void ApplyGlyph(Player *player, uint8 slot, uint32 glyphID);
+    void RemoveAllGlyphs(Player *player);
+    void ApplyBonus(Player *player, Item *item, EnchantmentSlot slot, uint32 bonusEntry);
+    void PurgeTemplate(Player *player, std::string &playerSpecStr, TemplateType type);
+    void ExtractGearTemplateToDB(Player * /*player*/, std::string & /*playerSpecStr*/, TemplateType /*templateType*/);
+    bool CanEquipTemplate(Player * /*player*/, std::string & /*playerSpecStr*/);
 
-    bool OverwriteTemplate(Player* /*player*/, std::string& /*playerSpecStr*/);
-    void ExtractGearTemplateToDB(Player* /*player*/, std::string& /*playerSpecStr*/);
-    void ExtractTalentTemplateToDB(Player* /*player*/, std::string& /*playerSpecStr*/);
-    void ExtractGlyphsTemplateToDB(Player* /*player*/, std::string& /*playerSpecStr*/);
-    bool CanEquipTemplate(Player* /*player*/, std::string& /*playerSpecStr*/);
-
-    std::string GetClassString(Player* /*player*/);
+    std::string GetClassString(Player * /*player*/);
     std::string sTalentsSpec;
 
-    void LearnTemplateTalents(Player* /*player*/);
-    void LearnTemplateGlyphs(Player* /*player*/);
-    void EquipTemplateGear(Player* /*player*/);
+    void EquipTemplateGear(Player * /*player*/);
 
     GlyphContainer m_GlyphContainer;
     TalentContainer m_TalentContainer;
