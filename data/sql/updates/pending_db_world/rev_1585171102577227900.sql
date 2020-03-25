9a98373626432e51d41c675f8887d4c095b7d4f3
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-03-25 22:20:00 +0100

    fix(DB/lang): TBC heroic keys german description

---
+++ data/sql/updates/pending_db_world/rev_1585171102577227900.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585171102577227900');
+
+UPDATE `item_template_locale` SET `Description`='Eröffnet den Schwierigkeitsgrad ''Heroisch'' für Dungeons der Höllenfeuerzitadelle.' WHERE  `ID`=30622 AND `locale`='deDE';
+UPDATE `item_template_locale` SET `Description`='Eröffnet den Schwierigkeitsgrad ''Heroisch'' für Dungeons des Echsenkessels.' WHERE  `ID`=30623 AND `locale`='deDE';
+UPDATE `item_template_locale` SET `Description`='Eröffnet den Schwierigkeitsgrad ''Heroisch'' für Dungeons von Auchindoun.' WHERE  `ID`=30633 AND `locale`='deDE';
+UPDATE `item_template_locale` SET `Description`='Eröffnet den Schwierigkeitsgrad ''Heroisch'' für Dungeons der Festung der Stürme.' WHERE  `ID`=30634 AND `locale`='deDE';
+UPDATE `item_template_locale` SET `Description`='Eröffnet den Schwierigkeitsgrad ''Heroisch'' für Dungeons der Höhlen der Zeit.' WHERE  `ID`=30635 AND `locale`='deDE';
+UPDATE `item_template_locale` SET `Description`='Eröffnet den Schwierigkeitsgrad ''Heroisch'' für Dungeons der Höllenfeuerzitadelle.' WHERE  `ID`=30637 AND `locale`='deDE';
