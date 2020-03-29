88178e9bfb62a080a3ec0876770cb2f097fe876a
Maximilian Daumlechner <maximilian.daumlechner@stud.h-da.de>
2020-03-30 01:18:04 +0200

    fix(DB/creature_text): Roleplay in ToC, same text twice

---
+++ data/sql/updates/pending_db_world/rev_1585523678966764200.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585523678966764200');
+
+UPDATE `creature_text` SET `Text`="You will have your challenge, Fordring.", `Sound`=16321, `BroadcastTextId`=35747, `comment`="The Lich King SAY CHALLENGE" WHERE `CreatureID`=35877 AND `GroupID`=0;
