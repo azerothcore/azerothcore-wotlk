f9f3efe0d37ac044aaa19c0f367731db8b5ab6a0
AzerothCoreBot <azerothcorebot@gmail.com>
2020-03-29 01:13:01 +0000

    Import pending SQL update file...
    Referenced commit(s): ccb1d813f606f1d6dc6261c94e5b5aa109844289

--- data/sql/updates/db_world/2020_03_29_00.sql
+++ data/sql/updates/db_world/2020_03_29_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_03_28_00 -> 2020_03_29_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_03_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_03_28_00 2020_03_29_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1583866008487985900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1583866008487985900');
 
 UPDATE `creature_template` SET `type_flags`=`type_flags`|134217728, `gossip_menu_id` = 10649 WHERE `entry` = 35477;
@@ -19,3 +35,12 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` IN (35477) AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (35477, 0, 0, 1, 62, 0, 100, 0, 10649, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Little Adeline - On gossip select - Close gossip'),
 (35477, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 11, 67554, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Little Adeline - On gossip select - Cast Spell Cascade of Petals');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
