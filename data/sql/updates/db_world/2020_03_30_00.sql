633ef83b47851c5d78176f16525c623879f0dd39
AzerothCoreBot <azerothcorebot@gmail.com>
2020-03-30 00:27:37 +0000

    Import pending SQL update file...
    Referenced commit(s): 71f98abc2c1653d9f91c34811bc3f9735fe1fba6

--- data/sql/updates/db_world/2020_03_30_00.sql
+++ data/sql/updates/db_world/2020_03_30_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_03_29_01 -> 2020_03_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_03_29_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_03_29_01 2020_03_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1582823894137331500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1582823894137331500');
 
 -- Set movement type for formation members
@@ -17,3 +33,12 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 
 -- Delete unused waypoints
 DELETE FROM `waypoint_data` WHERE `id` IN (68860, 68830, 68800, 68770);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
