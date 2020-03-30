def1748a8224534c6871017b01ae5409d0df954f
LukasVolgger <55922592+LukasVolgger@users.noreply.github.com>
2020-03-30 02:08:14 +0200

    fix(DB/creature_formations): Thal'trak Proudtusk pathing (#2724)
    Use creature_formations instead of pathing for each npc. This avoids that they get out of sync after a while or get stuck.

---
+++ data/sql/updates/pending_db_world/rev_1582823894137331500.sql
@@ -0,0 +1,19 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1582823894137331500');
+
+-- Set movement type for formation members
+UPDATE `creature` SET `MovementType` = 0 WHERE `guid` IN (6886, 6883, 6880, 6877);
+
+-- Deactivate paths for formation members
+UPDATE `creature_addon` SET `path_id` = 0 WHERE `guid` IN (6886, 6883, 6880, 6877);
+
+-- Create formation
+DELETE FROM `creature_formations` WHERE `leaderGUID` = 6885;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(6885, 6885, 0, 0, 515, 0, 0),
+(6885, 6886, 3, 0, 515, 0, 0),
+(6885, 6883, 6, 0, 515, 0, 0),
+(6885, 6880, 9, 0, 515, 0, 0),
+(6885, 6877, 12, 0, 515, 0, 0);
+
+-- Delete unused waypoints
+DELETE FROM `waypoint_data` WHERE `id` IN (68860, 68830, 68800, 68770);
