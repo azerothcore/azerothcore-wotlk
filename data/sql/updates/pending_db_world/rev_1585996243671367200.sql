a2b979169156b57c53a8e57eac42dc6168eb48d3
aleigood@gmail.com <aleigood@gmail.com>
2020-04-04 18:32:08 +0800

    update  rev value

--- data/sql/updates/pending_db_world/rev_1585996243671367200.sql
+++ data/sql/updates/pending_db_world/rev_1585996243671367200.sql
@@ -1,4 +1,4 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585888945510697800');
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585996243671367200');
 
 -- fix Julianne's first death sound
 UPDATE `creature_text` SET `Sound`=9198 WHERE `CreatureID`=17534 and `GroupID`=2 and `ID`=0;
