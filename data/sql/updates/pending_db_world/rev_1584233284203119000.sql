44c3bef14ea78bf6c33e56f4c361fd83749c6152
AzerothCoreBot <azerothcorebot@gmail.com>
2020-04-05 17:09:02 +0000

    Import pending SQL update file...
    Referenced commit(s): f821b0c7aae9b2968a4c30da33e54cbc7000805c

--- data/sql/updates/pending_db_world/rev_1584233284203119000.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1584233284203119000');
-
-DELETE FROM `gossip_menu` WHERE `MenuID`=55002 AND `TextID`=3550;
-INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES 
-(55002, 3550);
