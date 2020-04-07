b0948362c5444a2c464e34944ad4b5e807034699
Francesco Borzì <borzifrancesco@gmail.com>
2020-04-07 17:57:28 +0200

    chore: revert unwanted changes

--- apps/db_assembler/includes/functions.sh
+++ apps/db_assembler/includes/functions.sh
@@ -140,7 +140,9 @@ function dbasm_assemble() {
 
     curTime=`date +%Y_%m_%d_%H_%M_%S`
 
-    if [ $with_base = true ]; then
+    # ALLOW FOR RECURSION WITH "**"
+    shopt -s globstar
+
         echo "" > $OUTPUT_FOLDER$database$suffix_base".sql"
 
 
