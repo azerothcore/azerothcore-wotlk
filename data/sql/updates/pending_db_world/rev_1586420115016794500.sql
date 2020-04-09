7158a3dbf4c2be642a2da7bd0355a009967cb813
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-09 10:30:11 +0200

    fix sql error
    Co-Authored-By: ratkosrb <ratkosrb@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1586420115016794500.sql
+++ data/sql/updates/pending_db_world/rev_1586420115016794500.sql
@@ -3,7 +3,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1586420115016794500');
 ALTER TABLE `creature`
 	CHANGE COLUMN `spawndist` `wander_distance` FLOAT NOT NULL DEFAULT '0' AFTER `spawntimesecs`;
 	
-DELETE FROM `command` WHERE `name` = 'npc set spawndist';
+DELETE FROM `command` WHERE `name` IN ('npc set spawndist', 'npc set wanderdistance');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('npc set wanderdistance', 3, 'Syntax: .npc set wanderdistance #dist\r\n\r\nAdjust wander distance of selected creature to dist.');
 
