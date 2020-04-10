0f2504b2030d348b1c10e61d1cd3e3bd848d9290
Rochet2 <rochet2@post.com>
2020-04-10 14:49:37 +0300

    Update ci-install-modules.sh

--- apps/ci/ci-install-modules.sh
+++ apps/ci/ci-install-modules.sh
@@ -3,8 +3,6 @@
 set -e
 
 echo "install modules"
-git clone --depth=1 --branch=master --recursive https://github.com/azerothcore/mod-eluna-lua-engine.git modules/mod-eluna-lua-engine
-
 git clone --depth=1 --branch=master https://github.com/azerothcore/mod-autobalance.git modules/mod-autobalance
 git clone --depth=1 --branch=master https://github.com/azerothcore/mod-transmog.git modules/mod-transmog
 git clone --depth=1 --branch=master https://github.com/azerothcore/mod-npc-beastmaster.git modules/mod-npc-beastmaster
