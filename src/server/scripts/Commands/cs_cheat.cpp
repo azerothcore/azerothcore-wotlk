316122a3c0c531f05f6dbc908b691642c48f6139
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-10 03:19:22 +0200

    fix .cheat explore 0

--- src/server/scripts/Commands/cs_cheat.cpp
+++ src/server/scripts/Commands/cs_cheat.cpp
@@ -257,7 +257,7 @@ public:
             if (flag != 0)
                 handler->GetSession()->GetPlayer()->SetFlag(PLAYER_EXPLORED_ZONES_1 + i, 0xFFFFFFFF);
             else
-                handler->GetSession()->GetPlayer()->SetFlag(PLAYER_EXPLORED_ZONES_1 + i, 0);
+                handler->GetSession()->GetPlayer()->RemoveFlag(PLAYER_EXPLORED_ZONES_1 + i, 0xFFFFFFFF);
         }
 
         return true;
