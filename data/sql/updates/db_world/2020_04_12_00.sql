f7562f8d961de02e8b56fd112b7ca0a5b9c8c5a6
AzerothCoreBot <azerothcorebot@gmail.com>
2020-04-12 21:59:00 +0000

    Import pending SQL update file...
    Referenced commit(s): 7661eb2a537b984a58f5780d018dfdea1037ed01

--- data/sql/updates/db_world/2020_04_12_00.sql
+++ data/sql/updates/db_world/2020_04_12_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_04_11_00 -> 2020_04_12_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_04_11_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_04_11_00 2020_04_12_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1583347738773107900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1583347738773107900');
 
 -- Quest texts changed to patch stand 3.3.5a from 4.0.3a
@@ -68,3 +84,12 @@ UPDATE `quest_template_locale` SET `Objectives`='Hemet Nesingwary Jr. möchte, d
 -- Quest 208
 -- https://wowwiki.fandom.com/wiki/Quest:Big_Game_Hunter_(old)
 UPDATE `quest_template_locale` SET `Objectives`='Hemet Nesingwary möchte, dass Ihr ihm den Kopf von König Bangalash, dem großen weißen Tiger, bringt.' WHERE  `ID`=208 AND `locale`='deDE';
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
