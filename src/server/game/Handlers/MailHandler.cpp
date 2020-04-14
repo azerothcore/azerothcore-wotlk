f132ab630a5b3a173191fac331e040699398cbd7
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-14 20:08:14 +0200

    Update MailHandler.cpp

--- src/server/game/Handlers/MailHandler.cpp
+++ src/server/game/Handlers/MailHandler.cpp
@@ -24,7 +24,7 @@ bool WorldSession::CanOpenMailBox(uint64 guid)
 {
     if (guid == _player->GetGUID())
     {
-        if (!SEC_MODERATOR)
+        if (_player->GetSession()->GetSecurity() < SEC_MODERATOR)
         {
             sLog->outError("%s attempt open mailbox in cheating way.", _player->GetName().c_str());
             return false;
