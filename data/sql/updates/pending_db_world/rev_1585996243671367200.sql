5f2dbd996ac8877319da5d3daa49db9424222d64
Barbz <BarbzYHOOL@users.noreply.github.com>
2020-04-15 17:05:39 +0200

    style: uppercase

--- data/sql/updates/pending_db_world/rev_1585996243671367200.sql
+++ data/sql/updates/pending_db_world/rev_1585996243671367200.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585996243671367200');
 
 -- fix Julianne's first death sound
-UPDATE `creature_text` SET `Sound`=9198 WHERE `CreatureID`=17534 and `GroupID`=2 and `ID`=0;
+UPDATE `creature_text` SET `Sound` = 9198 WHERE `CreatureID` = 17534 AND `GroupID` = 2 AND `ID` = 0;
