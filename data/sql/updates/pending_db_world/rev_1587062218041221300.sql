d2d7a166a33d1a9375ec14a1b9b8e96105e7e8d1
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-16 21:49:50 +0200

    Update help to make it more understandable

--- data/sql/updates/pending_db_world/rev_1587062218041221300.sql
+++ data/sql/updates/pending_db_world/rev_1587062218041221300.sql
@@ -4,3 +4,5 @@ DELETE FROM `acore_string` WHERE `entry` IN (30081,30082);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
 (30081, "%s does not have itemID = %i, thus cannot be removed."),
 (30082, "%s does not have that many of itemID = %i.");
+
+UPDATE `command` SET `help` = "Syntax: .additem #itemID/[#itemName]/#itemLink #itemCount\r\nAdds the specified item to you or the selected character.\nIf #itemCount is negative, you will remove #itemID." WHERE `name` = "additem";
