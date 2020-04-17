c8085b87bac9636d2414f61b8f3e38d62e9cfc75
AzerothCoreBot <azerothcorebot@gmail.com>
2020-04-17 21:02:41 +0000

    Import pending SQL update file...
    Referenced commit(s): 85cbf146f548a184dd67e840b078c2dd0d6a5d2d 495f362263c2ee1db79f69a95af3d504f6acd499

--- data/sql/updates/pending_db_world/rev_1584052298613344800.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1584052298613344800');
-
-UPDATE `quest_offer_reward` SET `RewardText` = 'Listen well, $c. In accepting this weapon your fate is sealed.$B$BOvercome or succumb.$B$BI have placed your feet upon this path. You are therefore my personal responsibility. Should you failter, I am duty-bound to deliver you from this life.$B$BRemember my words, $r, and do not fail.' WHERE `id` = 24743;
