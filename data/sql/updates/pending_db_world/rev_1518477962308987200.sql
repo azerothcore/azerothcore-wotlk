3a7fd6ca6e54c7fd853dbc5584f6e47da7cbc547
LukasVolgger <55922592+LukasVolgger@users.noreply.github.com>
2020-04-19 18:30:22 +0200

    Update data/sql/updates/pending_db_world/rev_1518477962308987200.sql
    Co-Authored-By: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1518477962308987200.sql
+++ data/sql/updates/pending_db_world/rev_1518477962308987200.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1518477962308987200');
 
-DELETE FROM `item_loot_template` WHERE (`entry` = 54537 AND `item` = 49426);
-INSERT INTO `item_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES
-('54537','49426','100','1','0','2','2');
+DELETE FROM `item_loot_template` WHERE `Entry` = 54537 AND `Item` = 49426;
+INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
+(54537, 49426, 0, 100, 0, 1, 0, 2, 2, '');
