66dc38d617e6351e336bb9609f1dc60570c5de50
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-21 19:52:39 +0200

    Create rev_1587491025762555300.sql

---
+++ data/sql/updates/pending_db_world/rev_1587491025762555300.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587491025762555300');
+
+UPDATE `creature` SET `curhealth` = 126000 WHERE `id` = 30330;
+
+UPDATE `conditions` SET `SourceEntry` = 56570 WHERE `SourceTypeOrReferenceId` = 13 AND `ElseGroup` = 1 AND `SourceEntry` = 56578;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 30330 AND `id` = 1;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(30330, 0, 1, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Jotunheim Proto-Drake - On Reset - Set Reactstate Passive');
