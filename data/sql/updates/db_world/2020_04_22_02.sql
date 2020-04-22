af602e0bb04e2d25287eb99d764553f5bf44358e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-04-22 22:38:30 +0000

    Import pending SQL update file...
    Referenced commit(s):9cc5bcc1a447113ed77ab983e9596d89be3fbedd

--- data/sql/updates/db_world/2020_04_22_02.sql
+++ data/sql/updates/db_world/2020_04_22_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_04_22_01 -> 2020_04_22_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_04_22_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_04_22_01 2020_04_22_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1584114505260057800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1584114505260057800');
 
 -- Based on https://wowwiki.fandom.com/wiki/Clam?oldid=2076011
@@ -18,3 +34,12 @@ INSERT INTO `spell_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Ques
 (58160, 24478, 0, 15, 0, 1, 1, 1, 1, ''),
 (58160, 13926, 0, 5, 0, 1, 1, 1, 1, ''),
 (58160, 24479, 0, 1, 0, 1, 1, 1, 1, '');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
