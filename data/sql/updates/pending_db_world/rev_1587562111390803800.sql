8125521f18d9c2deb33ea2bb811d7cb4468ca377
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-24 00:19:00 +0200

    I changed my mind of string

--- data/sql/updates/pending_db_world/rev_1587562111390803800.sql
+++ data/sql/updates/pending_db_world/rev_1587562111390803800.sql
@@ -2,5 +2,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587562111390803800');
 
 DELETE FROM `acore_string` WHERE entry IN (30081,30082);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(30081, 'You cannot share quests while in a Battleground'),
-(30082, 'You cannot start a Ready Check while in a Battlground');
+(30081, 'You cannot share quests while in a Battleground.'),
+(30082, 'You cannot start a Ready Check while in a Battlground.');
