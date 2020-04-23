d96766f3a5197b2ef741d580296cc15c613494ed
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-23 11:10:54 +0200

    fix build

--- data/sql/updates/pending_db_world/rev_1587630388241612100.sql
+++ data/sql/updates/pending_db_world/rev_1587630388241612100.sql
@@ -2,10 +2,17 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587630388241612100');
 
 ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc1` TO `locale_koKR`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc2` TO `locale_frFR`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc3` TO `locale_deDE`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc4` TO `locale_zhCN`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc5` TO `locale_zhTW`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc6` TO `locale_esES`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc7` TO `locale_esMX`,
+ALTER TABLE `acore_string`
 	RENAME COLUMN `content_loc8` TO `locale_ruRU`;
