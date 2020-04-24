33b38101d991e968aac969f5c46c8e288260129c
merdlin <60373652+merdlin@users.noreply.github.com>
2020-04-24 11:49:38 -0300

    Create rev_1587739742290144300.sql

---
+++ data/sql/updates/pending_db_world/rev_1587739742290144300.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587739742290144300');
+
+UPDATE `creature_template` SET `DamageModifier`=13 WHERE `entry`=34035;
+UPDATE `creature_template` SET `DamageModifier`=25 WHERE `entry`=34171;
