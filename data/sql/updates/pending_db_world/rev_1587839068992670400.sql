d5eda7a6b738c7a1a5feac8bf707bd317085e9d3
VhiperDEV <39068627+vhiperdev@users.noreply.github.com>
2020-04-25 20:47:06 -0300

    Update data/sql/updates/pending_db_world/rev_1587839068992670400.sql
    Co-Authored-By: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1587839068992670400.sql
+++ data/sql/updates/pending_db_world/rev_1587839068992670400.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587839068992670400');
 
-DELETE FROM `spell_script_names` WHERE `ScriptName`="spell_item_eggnog";
+DELETE FROM `spell_script_names` WHERE `spell_id`=21149 AND `ScriptName`="spell_item_eggnog";
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (21149,'spell_item_eggnog');
