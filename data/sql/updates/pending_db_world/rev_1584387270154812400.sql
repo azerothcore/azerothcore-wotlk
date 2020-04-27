e4b0a54b8225e48e76833e653cd47340473b9818
AzerothCoreBot <azerothcorebot@gmail.com>
2020-04-27 11:18:51 +0000

    Import pending SQL update file...
    Referenced commit(s): e37c11458d49c3eeaea260661dc1652021107c5f

--- data/sql/updates/pending_db_world/rev_1584387270154812400.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1584387270154812400');
-
-UPDATE `item_template` SET `flags`=`flags`|16 WHERE `entry` = 16665;
-
-DELETE FROM `creature_loot_template` WHERE `Item`=16665;
