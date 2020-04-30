c0e083f592514f0878bbf985f951fc87c2eac119
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-04-30 10:08:11 +0200

    fix build

--- data/sql/updates/pending_db_world/rev_1588233350653637100.sql
+++ data/sql/updates/pending_db_world/rev_1588233350653637100.sql
@@ -3,9 +3,9 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588233350653637100');
 ALTER TABLE `broadcast_text`
 	CHANGE `unk1` `EmotesID` MEDIUMINT,
 	CHANGE `unk2` `Flags` MEDIUMINT,
-	CHANGE `EmoteID2`, `EmoteID3`, MEDIUMINT,
-	CHANGE `EmoteID1`, `EmoteID2` MEDIUMINT,
+	CHANGE `EmoteID2` `EmoteID3` MEDIUMINT,
+	CHANGE `EmoteID1` `EmoteID2` MEDIUMINT,
 	CHANGE `EmoteID0` `EmoteID1` MEDIUMINT,
-	CHANGE `EmoteDelay2`, `EmoteDelay3`, MEDIUMINT,
-	CHANGE `EmoteDelay1`, `EmoteDelay2` MEDIUMINT,
+	CHANGE `EmoteDelay2` `EmoteDelay3` MEDIUMINT,
+	CHANGE `EmoteDelay1` `EmoteDelay2` MEDIUMINT,
 	CHANGE `EmoteDelay0` `EmoteDelay1` MEDIUMINT;
