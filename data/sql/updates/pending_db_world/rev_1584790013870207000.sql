909b05bbf988f854c103f5694f1fec3bef36802e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-04-30 20:47:34 +0000

    Import pending SQL update file...
    Referenced commit(s): 75d4fb3dff42ad79f727fde86dfae5da62ba414e

--- data/sql/updates/pending_db_world/rev_1584790013870207000.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1584790013870207000');
-
--- Set rates to 1 for faction "The Mag'har" & "Kurenai" to be blizzlike
-UPDATE `reputation_reward_rate` SET `quest_rate`=1, `quest_daily_rate`=1, `quest_repeatable_rate`=1 WHERE `faction`=941;
-UPDATE `reputation_reward_rate` SET `quest_daily_rate`=1, `quest_repeatable_rate`=1, `spell_rate`=1 WHERE `faction`=978;
