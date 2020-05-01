affa30c140fa915f029f9cb80a7ec400a76f94ab
Knindza <knindzagxg@gmail.com>
2020-05-01 15:49:56 +0200

    Update rev_1588339146103973500.sql

--- data/sql/updates/pending_db_world/rev_1588339146103973500.sql
+++ data/sql/updates/pending_db_world/rev_1588339146103973500.sql
@@ -18,10 +18,12 @@ UPDATE `creature_loot_template` SET `Chance` = 30 WHERE `Item` = 39266;
 
 
 /* SMARTSCRIPTS */
+
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 29126;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 29126);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(29126, 0, 0, 0, 0, 0, 100, 0, 1000, 1000, 15000, 15000, 0, 11, 53229, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Crystal of the Violent Storm - In Combat - Cast \'53229\'');
+(29126, 0, 0, 0, 0, 0, 100, 0, 1000, 1000, 15000, 15000, 11, 53229, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Crystal of the Violent Storm - In Combat - Cast \'53229\'');
+
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 29125;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 29125);
