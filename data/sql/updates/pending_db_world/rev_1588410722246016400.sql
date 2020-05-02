6a682e633fcb94c94723cfea714a25646328602c
Flashaholic88 <60391292+Flashaholic88@users.noreply.github.com>
2020-05-02 15:52:38 +0200

    Update rev_1588410722246016400.sql

--- data/sql/updates/pending_db_world/rev_1588410722246016400.sql
+++ data/sql/updates/pending_db_world/rev_1588410722246016400.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588410722246016400');
 
-UPDATE `creature_template` SET `lootid` = '0' WHERE (`entry` = '2992, 7484 3680, 17079, 17080');
+UPDATE `creature_template` SET `lootid` = '0' WHERE (`entry` = '2992', '7484', '3680', '17079', '17080');
