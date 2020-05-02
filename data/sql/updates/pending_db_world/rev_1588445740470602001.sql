aecbe3e4387cccf47c7cd5465f756a7de1d70f11
h0lysp4nk <liamjosephkeenan@gmail.com>
2020-05-02 20:25:48 +0100

    Updated SQL

--- data/sql/updates/pending_db_world/rev_1588445740470602001.sql
+++ data/sql/updates/pending_db_world/rev_1588445740470602001.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588445740470602001');
 
 -- Ouro Spawner
-UPDATE `creature_template` SET `unit_flags` = 33685504, `ScriptName` = 'npc_ouro_spawner' WHERE (`entry` = 15957);
+UPDATE `creature_template` SET `unit_flags` = `unit_flags`| 33685504, `ScriptName` = 'npc_ouro_spawner' WHERE (`entry` = 15957);
