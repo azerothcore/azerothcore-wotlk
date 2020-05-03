5454a864c3b2dd482cf39cd176205f3fda4878af
Flashaholic88 <Happymeal1887@gmail.com>
2020-05-03 18:06:01 +0200

    Thanks for your help. now i understand these errors

--- data/sql/updates/pending_db_world/rev_1588410722246016400.sql
+++ data/sql/updates/pending_db_world/rev_1588410722246016400.sql
@@ -1,3 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588410722246016400');
 
 UPDATE `creature_template` SET `lootid` = 0 WHERE `entry` IN (2992,7484,3680,17079,17080); 
+
+DELETE FROM `creature_loot_template` WHERE `entry` IN (2992, 7484, 3680, 17079, 17080); 
