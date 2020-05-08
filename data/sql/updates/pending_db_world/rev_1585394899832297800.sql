307d4e9aa83604f3a43b68feba2d403d42aeaa92
LukasVolgger <55922592+LukasVolgger@users.noreply.github.com>
2020-05-08 09:33:26 +0200

    fix(DB/Creature): Bloodmaul Skirmisher Spawn Point (#2812)
    * GUID 71130 has spawnpoint and movementtype fixed.

---
+++ data/sql/updates/pending_db_world/rev_1585394899832297800.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585394899832297800');
+
+UPDATE `creature` SET `position_x` = 1488.602661, `position_y` = 6376.107910, `position_z` = 35.148029, `wander_distance` = 0, `MovementType` = 0 WHERE `guid` = 71130; 
