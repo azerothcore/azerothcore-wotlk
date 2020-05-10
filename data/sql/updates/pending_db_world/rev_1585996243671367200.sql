566ead8934ef8062f629528ac0660c11739ddf2d
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-10 23:07:07 +0000

    Import pending SQL update file...
    Referenced commit(s): e27078ab67c02b7c70019f97e1191b21b94b1f22

--- data/sql/updates/pending_db_world/rev_1585996243671367200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1585996243671367200');
-
--- fix Julianne's first death sound
-UPDATE `creature_text` SET `Sound` = 9198 WHERE `CreatureID` = 17534 AND `GroupID` = 2 AND `ID` = 0;
