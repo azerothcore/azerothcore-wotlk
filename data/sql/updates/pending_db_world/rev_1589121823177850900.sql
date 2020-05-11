26a841f91fc4db617c68183cd0b149e9a36cc210
Knindza <knindzagxg@gmail.com>
2020-05-11 17:11:26 +0200

    Update rev_1589121823177850900.sql

--- data/sql/updates/pending_db_world/rev_1589121823177850900.sql
+++ data/sql/updates/pending_db_world/rev_1589121823177850900.sql
@@ -207,6 +207,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 7105);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (7105, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 13578, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Jadefire Satyr - On Aggro - Cast \'13578\'');
 
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 7106;
+DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 7106);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(7106, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 13578, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Jadefire Rogue - On Aggro - Cast \'13578\''),
+(7106, 0, 1, 0, 0, 0, 100, 0, 2800, 3200, 9600, 10400, 11, 7159, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Jadefire Rogue - In Combat - Cast \'7159\'');
+
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 9464;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 9464);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
