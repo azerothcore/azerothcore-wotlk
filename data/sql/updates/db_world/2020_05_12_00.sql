f301d817e7d542929cb54a9c3b61e7a401c1a23a
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-12 06:08:54 +0000

    Import pending SQL update file...
    Referenced commit(s): f35780c286865133d839ef5ac5d97d86c382cb20

--- data/sql/updates/db_world/2020_05_12_00.sql
+++ data/sql/updates/db_world/2020_05_12_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_05_10_01 -> 2020_05_12_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_05_10_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_05_10_01 2020_05_12_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1586023897838741800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1586023897838741800');
 
 -- Delete queststarter for creature 20482
@@ -33,3 +49,12 @@ Image of Commander Ameer - Blade's Edge Mountains Queststarter
 
 */
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
