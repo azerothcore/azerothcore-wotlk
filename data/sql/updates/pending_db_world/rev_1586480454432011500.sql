4a402d6dcdc5351f68368fb618de7a4c9e8eaaec
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-14 23:04:36 +0000

    Import pending SQL update file...
    Referenced commit(s): 807005ae255b8f4a91f7e2c786c8a6bd31324bd9

--- data/sql/updates/pending_db_world/rev_1586480454432011500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1586480454432011500');
-
-DELETE FROM `command` WHERE `name`='cheat explore';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES 
-('cheat explore', 2, 'Syntax: .cheat explore #flag\r\nReveal or hide all maps for the selected player. If no player is selected, hide or reveal maps to you.\r\nUse a #flag of value 1 to reveal, use a #flag value of 0 to hide all maps.');
