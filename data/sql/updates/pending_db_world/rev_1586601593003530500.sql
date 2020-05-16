c399c05f2e02092c2d3d6ce36fac6efb2e0310c1
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-16 14:02:21 +0000

    Import pending SQL update file...
    Referenced commit(s): ccaf946ebd36163f009aed0ae7e6a46aebb04a26

--- data/sql/updates/pending_db_world/rev_1586601593003530500.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1586601593003530500');
-
-UPDATE `quest_template_addon` SET `PrevQuestID` = 12294, `NextQuestID` = 12225, `ExclusiveGroup` = -12222 WHERE `id` IN (12222, 12223);
-UPDATE `quest_template_addon` SET `PrevQuestID` = 12222 WHERE `id` = 12255;
