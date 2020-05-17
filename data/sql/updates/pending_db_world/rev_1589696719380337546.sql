6b9c5ddb23d5ea8a0a9eeae8e19c38f4d8c67712
Tyrelis <admin@worselis.com>
2020-05-17 09:54:19 -0600

    Added more proposed changes

--- data/sql/updates/pending_db_world/rev_1589696719380337546.sql
+++ data/sql/updates/pending_db_world/rev_1589696719380337546.sql
@@ -1,3 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1589696719380337546');
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`&~256 WHERE `entry` = 5269;
 UPDATE `creature` SET `unit_flags`=`unit_flags`|256 WHERE `guid` = 39745;
+UPDATE smart_scripts SET id=id|&~ 2 WHERE entryorguid = 5269; -- Atal'ai Priest
