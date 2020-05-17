965caf613dead8645ed2ab15cb434b546053fcad
Tyrelis <admin@worselis.com>
2020-05-17 09:57:57 -0600

    Removed/updated mistake.

--- data/sql/updates/pending_db_world/rev_1589696719380337546.sql
+++ data/sql/updates/pending_db_world/rev_1589696719380337546.sql
@@ -1,4 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1589696719380337546');
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`&~256 WHERE `entry` = 5269;
 UPDATE `creature` SET `unit_flags`=`unit_flags`|256 WHERE `guid` = 39745;
-UPDATE smart_scripts SET id=id|&~ 2 WHERE entryorguid = 5269; -- Atal'ai Priest
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 5269 AND `id` = 2;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = -39745;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(-39745, 0, 0, 0, 14, 0, 100, 0, 1000, 40, 4000, 6000, 0, 11, 11642, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Atal\'ai Priest - Friendly Missing Health - Cast Heal'),
+(-39745, 0, 1, 0, 0, 0, 100, 0, 0, 3000, 3000, 5000, 0, 11, 9613, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Atal\'ai Priest - In Combat - Cast Shadow Bolt'),
+(-39745, 0, 2, 0, 1, 0, 100, 0, 5000, 5000, 5000, 5000, 0, 19, 256, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Atal\'ai Priest - Out of Combat - Remove Unit Flags');
