691ccb0430a36892655046dcf0c970144d947e5e
Tyrelis <admin@worselis.com>
2020-05-17 09:26:21 -0600

    Added proposed pr changes

--- data/sql/updates/pending_db_world/rev_1589696719380337546.sql
+++ data/sql/updates/pending_db_world/rev_1589696719380337546.sql
@@ -1,2 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1589696719380337546');
-UPDATE `creature_template` SET `unit_flags` = 32768 WHERE `entry` = 5269; -- Atal'ai Priest
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`&~256 WHERE `entry` = 5269;
+UPDATE `creature` SET `unit_flags`=`unit_flags`|256 WHERE `guid` = 39745;
