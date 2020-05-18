9097cf26ab9fcbaa01b7f613ccc8baaa539d8e08
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-18 06:49:29 +0000

    Import pending SQL update file...
    Referenced commit(s): c065f37418995014beeebf36bbcbb5bd2637a7e5

--- data/sql/updates/pending_db_world/rev_1586994404926935400.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1586994404926935400');
-
-UPDATE `quest_template_locale` SET `Objectives`='Sammelt 12 Bäumlingssprosse.' WHERE `ID`=919 AND `locale`='deDE';
-UPDATE `quest_template_locale` SET `Objectives`='Reúne 12 brotes de Brezomadera.' WHERE `ID`=919 AND `locale`='esES';
-UPDATE `quest_template_locale` SET `Objectives`='Reúne 12 brotes de Brezomadera.' WHERE `ID`=919 AND `locale`='esMX';
-UPDATE `quest_template_locale` SET `Objectives`='Collectez 12 Pousses de sylvain.' WHERE `ID`=919 AND `locale`='frFR';
-UPDATE `quest_template_locale` SET `Objectives`='Принесите 12 ростков древесника.' WHERE `ID`=919 AND `locale`='ruRU';
