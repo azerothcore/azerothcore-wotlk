74ea7c8e49d8ceb11f51f3c12b0884a0a1b6c05b
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-18 19:14:33 +0000

    Import pending SQL update file...
    Referenced commit(s): 7f3945e13ed872204ca71b44a7dce02c8648a557

--- data/sql/updates/pending_db_world/rev_1587062218041221300.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587062218041221300');
-
-DELETE FROM `acore_string` WHERE `entry` IN (30081,30082);
-INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(30081, "%s does not have itemID = %i, thus cannot be removed."),
-(30082, "%s does not have that many of itemID = %i, thus none were removed.");
-
-UPDATE `command` SET `help` = "Syntax: .additem #itemID/[#itemName]/#itemLink #itemCount\r\nAdds the specified item to you or the selected character.\nIf #itemCount is negative, you will remove #itemID." WHERE `name` = "additem";
