814d84d6ce7927273910bf2ae85f3dd30173d73f
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-05-18 21:17:36 +0200

    fix merge conflict

--- data/sql/updates/pending_db_world/rev_1587562111390803800.sql
+++ data/sql/updates/pending_db_world/rev_1587562111390803800.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587562111390803800');
 
-DELETE FROM `acore_string` WHERE entry IN (30081,30082);
+DELETE FROM `acore_string` WHERE entry IN (30083,30084);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(30081, 'You cannot share quests while in a Battleground.'),
-(30082, 'You cannot start a Ready Check while in a Battlground.');
+(30083, 'You cannot share quests while in a Battleground.'),
+(30084, 'You cannot start a Ready Check while in a Battlground.');
