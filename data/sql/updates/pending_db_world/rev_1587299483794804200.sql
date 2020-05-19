0a5f8cbe0daed93439f2f24283b03fdbf58cea2e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-05-19 19:55:58 +0000

    Import pending SQL update file...
    Referenced commit(s): 36be8c3f8a0fcc2fe4dbec704ec86aa1f5b988c3

--- data/sql/updates/pending_db_world/rev_1587299483794804200.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1587299483794804200');
-
-UPDATE `gameobject` SET `position_x` = 792.891, `position_y` = -5149.93, `position_z` = -82.3544, `orientation` = 2.11791, `spawntimesecs` = 5 WHERE `guid` = 56732 AND `guid` = 56732;
