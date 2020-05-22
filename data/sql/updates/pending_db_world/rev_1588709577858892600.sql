075c0c7010c69c67e6ecb5e75228b031102e1ae7
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-05-22 22:58:23 +0200

    fix merge conflict

--- data/sql/updates/pending_db_world/rev_1588709577858892600.sql
+++ data/sql/updates/pending_db_world/rev_1588709577858892600.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588709577858892600');
 
-DELETE FROM `acore_string` WHERE `entry` IN (30081,30082);
+DELETE FROM `acore_string` WHERE `entry` IN (30085,30086);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(30081, 'Battleground Debugging is already enabled in the config, thus you are unable to enable/disable it with command.'),
-(30082, 'Arena Debugging is already enabled in the config, thus you are unable to enable/disable it with command.');
+(30085, 'Battleground Debugging is already enabled in the config, thus you are unable to enable/disable it with command.'),
+(30086, 'Arena Debugging is already enabled in the config, thus you are unable to enable/disable it with command.');
