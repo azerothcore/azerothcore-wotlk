02b01443779b01fbc6ae6443d5fb3e87d293ca39
Barbz <BarbzYHOOL@users.noreply.github.com>
2020-05-24 01:17:14 +0200

    Update LFGMgr.h

--- src/server/game/DungeonFinding/LFGMgr.h
+++ src/server/game/DungeonFinding/LFGMgr.h
@@ -38,6 +38,7 @@ enum LFGMgrEnum
     LFG_SPELL_DUNGEON_DESERTER                   = 71041,
     LFG_SPELL_LUCK_OF_THE_DRAW                   = 72221,
     LFG_GROUP_KICK_VOTES_NEEDED                  = 3
+    LFG_MIN_REQUIRED_PLAYERS_TO_FINISH_DUNGEON   = 3
 };
 
 enum LfgFlags
