fff54d7bdc68c6e998b70767fb81b7b8936f0196
gengarshadowball <60325009+gengarshadowball@users.noreply.github.com>
2020-05-29 18:39:42 +0100

    Update data/sql/updates/pending_db_world/rev_1590769251197902700.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1590769251197902700.sql
+++ data/sql/updates/pending_db_world/rev_1590769251197902700.sql
@@ -2,3 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1590769251197902700');
 
 -- Change maraudon portal faction to 0 (usable by both factions)
 UPDATE `gameobject_template_addon` SET `faction` = 0 WHERE `entry` = 178404;
+DELETE FROM `gameobject` WHERE `guid` = 265127 AND `id` = 178404;
