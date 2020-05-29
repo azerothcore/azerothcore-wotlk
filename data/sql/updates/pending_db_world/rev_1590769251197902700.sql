78d654afc7cda9e1a25b16dfa48cad51b9cfcb0d
gengarshadowball <shadowballgengar69@gmail.com>
2020-05-29 17:21:47 +0100

    fix(DB): Maraudon portal clickable

---
+++ data/sql/updates/pending_db_world/rev_1590769251197902700.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1590769251197902700');
+
+-- Change maraudon portal faction to 0 (usable by both factions)
+UPDATE `gameobject_template_addon` SET `faction` = 0 WHERE `entry` = 178404;
