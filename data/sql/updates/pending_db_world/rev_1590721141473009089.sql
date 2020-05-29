66168dfc050e70598f25ee4c46b5bc26cfc9e746
wackinwallaby <stevensvelund@gmail.com>
2020-05-29 09:01:46 -0400

    removed quotes from integers

--- data/sql/updates/pending_db_world/rev_1590721141473009089.sql
+++ data/sql/updates/pending_db_world/rev_1590721141473009089.sql
@@ -5,7 +5,7 @@ DELETE FROM `creature_formations` WHERE `leaderGUID` = '79333';
 
 /*add captain greenskin as leader of the formation and give him followers.*/
 INSERT INTO  `creature_formations` (`leaderGUID`,`memberGUID`,`dist`,`angle`,`groupAI`) VALUES 
-('79333','79333','0','0','0'),
-('79333','79334','2','135','0'),
-('79333','79335','2','225','0');
+(79333,79333,0,0,0),
+(79333,79334,2,135,0),
+(79333,79335,2,225,0);
 
