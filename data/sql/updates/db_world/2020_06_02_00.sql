33968b76bc141c20307c8b08cfa462677212a123
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-02 11:15:30 +0000

    Import pending SQL update file...
    Referenced commit(s): 46123b3f9e36d0c0158834157544e18249ad4278

--- data/sql/updates/db_world/2020_06_02_00.sql
+++ data/sql/updates/db_world/2020_06_02_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_01_00 -> 2020_06_02_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_01_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_01_00 2020_06_02_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1588332453846633800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588332453846633800');
 
 UPDATE `quest_template` SET `RequiredNPCorGO1` = 20058 WHERE `id` = 10506;
@@ -15,3 +31,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (20058, 0, 8, 0, 61, 0, 100, 0, 36310, 0, 0, 0, 0, 27, 20058, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodmaul Dire Wolf - On Spellhit \'Rina`s Diminution Powder\' - Stop Combat');
 
 DELETE FROM `creature` WHERE `id` = 21176;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
