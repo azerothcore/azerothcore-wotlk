d883fe20fd01523111ed5f88926cc8fc3fdd79f8
FrancescoBorzi <borzifrancesco@gmail.com>
2020-06-02 18:43:22 +0200

    feat(CI): CLang 10

--- apps/ci/ci-install.sh
+++ apps/ci/ci-install.sh
@@ -30,10 +30,10 @@ case $COMPILER in
     echo "CCOMPILERCXX=\"clang++-6.0\"" >> ./conf/config.sh
     ;;
 
-  "clang7" )
+  "clang10" )
     time sudo apt-get install -y clang-7
-    echo "CCOMPILERC=\"clang-7\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-7\"" >> ./conf/config.sh
+    echo "CCOMPILERC=\"clang-10\"" >> ./conf/config.sh
+    echo "CCOMPILERCXX=\"clang++-10\"" >> ./conf/config.sh
     ;;
 
   * )
