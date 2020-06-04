c98c6452559c4e7d8c15c72395ddab75dca7e0a3
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-04 19:42:28 +0000

    Import pending SQL update file...
    Referenced commit(s): 9bd47bd5cb5cb501d4e9f44dacc0016178e86379

--- data/sql/updates/db_world/2020_06_04_01.sql
+++ data/sql/updates/db_world/2020_06_04_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_04_00 -> 2020_06_04_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_04_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_04_00 2020_06_04_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1588337177136468800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588337177136468800');
 
 /*
@@ -33,3 +49,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 6233;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 6233);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (6233, 0, 0, 0, 0, 0, 100, 0, 3000, 5000, 8000, 9000, 11, 1604, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Mechanized Sentry - In Combat - Cast \'Dazed\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
