0d386df80bb1724f59e1da8ddb65e47fa7b62d0c
gengarshadowball <60325009+gengarshadowball@users.noreply.github.com>
2020-06-04 13:33:04 +0100

    Update data/sql/updates/pending_db_world/rev_1591204377400264600.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1591204377400264600.sql
+++ data/sql/updates/pending_db_world/rev_1591204377400264600.sql
@@ -32,8 +32,8 @@ INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `BroadcastTextID0`, `lang0`,
 
 DELETE FROM `conditions` WHERE `SourceGroup`=50000 AND `SourceTypeOrReferenceId`=15;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
-(15, 50000, 0, 0, 0, 14, 0, 3181, 0, 0, 0, 0, 0, '', NULL),
-(15, 50000, 1, 0, 0, 8, 0, 3201, 0, 0, 0, 0, 0, '', NULL);
+(15, 50000, 0, 0, 0, 14, 0, 3181, 0, 0, 0, 0, 0, '', "Show Gossip 50000 if 'The Horn of the Beast' is rewarded AND"),
+(15, 50000, 1, 0, 0, 8, 0, 3201, 0, 0, 0, 0, 0, '', "Show Gossip 50000  if 'At Last!' is not taken");
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=3836 AND `source_type`=0 AND `id`=0 AND `link`=0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
