3c806b0b2eb673e9137b91b14355c3d8a632f199
Knindza <knindzagxg@gmail.com>
2020-06-06 16:07:27 +0200

    Update rev_1590935951123059600.sql

--- data/sql/updates/pending_db_world/rev_1590935951123059600.sql
+++ data/sql/updates/pending_db_world/rev_1590935951123059600.sql
@@ -218,7 +218,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 3278;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 3278);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(3278, 0, 0, 0, 0, 0, 100, 1, 4000, 6500, 0, 0, 11, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Witchwing Slayer - In Combat - Cast \'0\' (No Repeat)'),
+(3278, 0, 0, 0, 0, 0, 100, 1, 4000, 6500, 0, 0, 11, 13730, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Witchwing Slayer - In Combat - Cast \'13730\' (No Repeat)'),
 (3278, 0, 1, 0, 12, 0, 100, 1, 5, 30, 0, 0, 11, 7160, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Witchwing Slayer - Target Between 5-30% Health - Cast \'7160\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 3276;
