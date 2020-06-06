a67dd97494e6b6662934ac51deabe8ab2a7e8fc8
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-06 10:16:12 +0000

    Import pending SQL update file...
    Referenced commit(s): 3ed778dffbb8b5ce6ee99471897adc5cf614c728

--- data/sql/updates/db_world/2020_06_06_00.sql
+++ data/sql/updates/db_world/2020_06_06_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_05_00 -> 2020_06_06_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_05_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_05_00 2020_06_06_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1588337666335849000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588337666335849000');
 
 /*
@@ -37,3 +53,12 @@ UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 269, `max
 
 /* QUEST */
 UPDATE `creature_template` SET `speed_run` = 1 WHERE `entry` = 8516;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
