5a2726a36aebb56828e12376a3f7bf327723ea99
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-06-12 22:31:42 +0200

    make it actually unsigned

--- data/sql/updates/pending_db_world/rev_1591992052509403400.sql
+++ data/sql/updates/pending_db_world/rev_1591992052509403400.sql
@@ -1,4 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1591992052509403400');
 
+DELETE FROM `playercreateinfo_item` WHERE `itemid` = 40582 AND `amount` = -1;
+
 ALTER TABLE `playercreateinfo_item`
-	CHANGE `amount` `amount` SMALLINT SIGNED NOT NULL DEFAULT 1;
+	CHANGE `amount` `amount` SMALLINT UNSIGNED NOT NULL DEFAULT 1;
