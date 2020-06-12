742471836b95a6432d5ceeeb6fb79b5e791ecc99
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-12 22:58:52 +0000

    Import pending SQL update file...
    Referenced commit(s): d3e5b5bba352db9de58caa845722e14ffb60c3a9

--- data/sql/updates/db_world/2020_06_12_02.sql
+++ data/sql/updates/db_world/2020_06_12_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_12_01 -> 2020_06_12_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_12_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_12_01 2020_06_12_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1588339406799887900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588339406799887900');
 
 /*
@@ -54,3 +70,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 11685);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (11685, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 11639, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Maraudine Priest - On Aggro - Cast \'11639\''),
 (11685, 0, 1, 0, 0, 0, 100, 0, 3400, 4100, 9200, 10100, 11, 16568, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Maraudine Priest - In Combat - Cast \'16568\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
