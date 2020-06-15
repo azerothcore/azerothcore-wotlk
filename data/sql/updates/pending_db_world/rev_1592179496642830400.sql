50c447c127cb9762c93dca15a1c6e46be8f0d0bd
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-06-15 15:16:43 +0200

    Update rev_1592179496642830400.sql

--- data/sql/updates/pending_db_world/rev_1592179496642830400.sql
+++ data/sql/updates/pending_db_world/rev_1592179496642830400.sql
@@ -8,8 +8,32 @@ CREATE TABLE IF NOT EXISTS `world_config` (
    PRIMARY KEY (`type`, `name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=UTF8MB3 COMMENT='Holds configs for the World Server';
 
+/*
+Type: 
+1 - PERFORMANCE SETTINGS
+2 - SERVER LOGGING
+3 - SERVER SETTINGS
+4 - WARDEN SETTINGS
+5 - PLAYER SETTINGS
+6 - CREATURE SETTINGS
+7 - CHAT SETTINGS
+8 - GM SETTINGS
+9 - VISIBILITY AND DISTANCES
+10 - SERVER RATES
+11 - AUTO BROADCAST
+12 - BATTLEGROUND SETTINGS
+13 - BATTLEFIELD SETTINGS
+14 - ARENA SETTINGS
+15 - CHARACTER DELETE SETTINGS
+16 - ITEM DELETE SETTINGS
+17 - CUSTOM SERVER SETTINGS (TODO: Can probably fit in other categories or be re-done)
+18 - PACKET SPOOF PROTECTION SETTINGS
+19 - DEBUG
+*/
+
+
 REPLACE INTO `world_config` (`name`, `value`, `comment`) VALUES
-('ICC.Buff.Horde', 73822, 'Specify ICC buff\r\n
+(17, 'ICC.Buff.Horde', 73822, 'Specify ICC buff\r\n
 It is necessary to restart the server after changing the values!\r\n
 Default: ICC.Buff.Horde = 73822\r\n
 Spell IDs for the auras:\r\n
