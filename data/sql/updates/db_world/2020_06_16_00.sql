7f27e3061a35f8a717a5069bd0a4b53458e586d8
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-16 15:13:19 +0000

    Import pending SQL update file...
    Referenced commit(s): 82dab24a46757a82cc59e59902c83e1e6e4767d3

--- data/sql/updates/db_world/2020_06_16_00.sql
+++ data/sql/updates/db_world/2020_06_16_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_14_00 -> 2020_06_16_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_14_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_14_00 2020_06_16_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1588339999314521100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1588339999314521100');
 
 /*
@@ -90,3 +106,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 18679);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (18679, 0, 0, 0, 0, 0, 100, 0, 1700, 2400, 5200, 6300, 11, 9080, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Vorakem Doomspeaker - In Combat - Cast \'9080\''),
 (18679, 0, 1, 0, 2, 0, 100, 1, 0, 25, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Vorakem Doomspeaker - Between 0-25% Health - Cast \'8599\' (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
