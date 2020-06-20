2913b5f12e21e9c53daf7ca12011897064d0f6ea
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-06-20 14:55:57 +0200

    Make it one query

--- data/sql/updates/pending_db_world/rev_1592650543169399600.sql
+++ data/sql/updates/pending_db_world/rev_1592650543169399600.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1592650543169399600');
 
-UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` IN (16398,8338,178908,32347,24077) AND `id` = 0;
-UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 30895 AND `id` = 1;
+UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` IN (16398,8338,178908,32347,24077) AND `id` = 0 OR `entryorguid` = 30895 AND `id` = 1;
