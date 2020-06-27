6332fd958eb7fd6eddb62574be7fe2945b5a3c25
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-27 22:17:04 +0000

    Import pending SQL update file...
    Referenced commit(s): 7d54d1918b99e98e761d97c1562fbab9d2bc2dcc

--- data/sql/updates/db_world/2020_06_27_01.sql
+++ data/sql/updates/db_world/2020_06_27_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_27_00 -> 2020_06_27_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_27_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_27_00 2020_06_27_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1589122636951229900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1589122636951229900');
 /*
  * Shadow Labyrinth
@@ -50,3 +66,12 @@ UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 1119, `ma
 UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 1214, `maxdmg` = 1647, `DamageModifier` = 1.01 WHERE `entry` = 20653;
 UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 1207, `maxdmg` = 1711, `DamageModifier` = 1.01 WHERE `entry` = 18708;
 UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 1756, `maxdmg` = 2490, `DamageModifier` = 1.01 WHERE `entry` = 20657;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
