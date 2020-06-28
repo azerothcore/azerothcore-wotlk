c159fa310af2e8059a26f63e84255188bb8252d5
Knindza <knindzagxg@gmail.com>
2020-06-28 13:22:52 +0200

    Update rev_1592668725846266000.sql
    Updated to correct version of Fel Lighting, made mistake when created.

--- data/sql/updates/pending_db_world/rev_1592668725846266000.sql
+++ data/sql/updates/pending_db_world/rev_1592668725846266000.sql
@@ -68,7 +68,7 @@ UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 1586, `ma
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 24777;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 24777);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(24777, 0, 0, 0, 0, 0, 100, 0, 4700, 5200, 14700, 15200, 11, 32337, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Sunblade Sentinel - In Combat - Cast \'32337\'');
+(24777, 0, 0, 0, 0, 0, 100, 0, 4700, 5200, 14700, 15200, 11, 46479, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Sunblade Sentinel - In Combat - Cast \'46479\'');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 24552;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 24552);
