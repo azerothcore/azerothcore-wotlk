8968039a78c3002bb8ca437bbaa1088e915f0e94
AzerothCoreBot <azerothcorebot@gmail.com>
2020-06-30 18:14:16 +0000

    Import pending SQL update file...
    Referenced commit(s): 849edc241d09358041f0122a2135932ba2217bab

--- data/sql/updates/db_world/2020_06_30_00.sql
+++ data/sql/updates/db_world/2020_06_30_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_06_29_00 -> 2020_06_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_06_29_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_06_29_00 2020_06_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1586071351793266500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1586071351793266500');
 
 -- Texts
@@ -157,3 +173,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (16134,0,0,0,1,0,100,0,1000,15000,150000,180000,80,1613400,0,0,0,0,0,1,0,0,0,0,0,0,0,"Rimblat Earthshatter - Out of Combat - Run Script"),
 (1613400,9,0,0,0,0,100,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Rimblat Earthshatter - On Script - Say Line 0"),
 (1613400,9,1,0,0,0,100,0,6000,6000,0,0,1,0,0,0,0,0,0,19,16135,0,0,0,0,0,0,"Rimblat Earthshatter - On Script - Say Line 0 (Rayne)");
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
