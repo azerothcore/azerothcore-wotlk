aeab506fbfe5bea07580c51399672ff70d000c56
AzerothCoreBot <azerothcorebot@gmail.com>
2020-07-03 18:38:21 +0000

    Import pending SQL update file...
    Referenced commit(s): 3dbd0181bcae04c5698a2b40e4b0c6416e317495

--- data/sql/updates/db_world/2020_07_03_00.sql
+++ data/sql/updates/db_world/2020_07_03_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_07_02_00 -> 2020_07_03_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_07_02_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_07_02_00 2020_07_03_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1589130360356001400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1589130360356001400');
 /*
  * Dungeon: Auchenai Crypts
@@ -48,3 +64,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (18478, 0, 0, 0, 0, 0, 100, 0, 3500, 5500, 7500, 8500, 11, 16856, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Avatar of the Martyred - In Combat - Cast \'16856\''),
 (18478, 0, 1, 0, 0, 0, 100, 0, 1500, 3000, 9500, 12000, 11, 16145, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Avatar of the Martyred - In Combat - Cast \'16145\''),
 (18478, 0, 2, 0, 7, 0, 100, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Avatar of the Martyred - On Evade - Despawn In 5000 ms');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
