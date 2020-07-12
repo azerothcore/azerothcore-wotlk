30ec6f69c85bce93e0a7a45dc9abc3a4f06f25c5
Kargatum <dowlandtop@yandex.com>
2020-07-12 22:47:12 +0700

    feat(Core/Config): rework configs load system (#2566)

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -703,7 +703,6 @@ class World
 
         void SetInitialWorldSettings();
         void LoadConfigSettings(bool reload = false);
-        void LoadModuleConfigSettings();
 
         void SendWorldText(uint32 string_id, ...);
         void SendGlobalText(const char* text, WorldSession* self);
@@ -837,9 +836,6 @@ class World
         std::string const& GetRealmName() const { return _realmName; } // pussywizard
         void SetRealmName(std::string name) { _realmName = name; } // pussywizard
 
-        std::string GetConfigFileList() { return m_configFileList; }
-        void SetConfigFileList(std::string list) { m_configFileList = list; }
-
     protected:
         void _UpdateGameTime();
         // callback for UpdateRealmCharacters
@@ -939,8 +935,6 @@ class World
 
         void ProcessQueryCallbacks();
         ACE_Future_Set<PreparedQueryResult> m_realmCharCallbacks;
-
-        std::string m_configFileList;
 };
 
 #define sWorld World::instance()
