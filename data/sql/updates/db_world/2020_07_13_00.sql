bba42a5ddd0df0d289ae108219bc4bb88b8573fe
AzerothCoreBot <azerothcorebot@gmail.com>
2020-07-13 08:53:04 +0000

    Import pending SQL update file...
    Referenced commit(s): 26dcedd79dea4ff06d26464144ca4d0b718468d1

--- data/sql/updates/db_world/2020_07_13_00.sql
+++ data/sql/updates/db_world/2020_07_13_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_07_12_00 -> 2020_07_13_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_07_12_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_07_12_00 2020_07_13_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1589718566727079200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1589718566727079200');
 /*
  * Zone: Ashenvale
@@ -74,3 +90,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 3809;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 3809);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (3809, 0, 0, 0, 0, 0, 100, 0, 2100, 2400, 9400, 10800, 11, 15793, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Ashenvale Bear - In Combat - Cast \'15793\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
