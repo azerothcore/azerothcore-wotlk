57c27d840c2dff84df8f6de0ed17200c398b79b4
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-07-17 13:59:25 +0200

    I am a noob

--- data/sql/updates/pending_db_world/rev_1594986175990195100.sql
+++ data/sql/updates/pending_db_world/rev_1594986175990195100.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1594986175990195100');
 
-DELETE * FROM `smart_scripts` WHERE `entryorguid` = 124371 AND `id` = 4;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 124371 AND `id` = 4;
 UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 124371 AND `id` = 3;
