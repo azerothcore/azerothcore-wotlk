0d476bf09f19bc22fdafa6803c42ad3a338fad12
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-07-17 16:52:45 +0200

    Update rev_1591646826613634700.sql

--- data/sql/updates/pending_db_world/rev_1591646826613634700.sql
+++ data/sql/updates/pending_db_world/rev_1591646826613634700.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1591646826613634700');
 
-UPDATE `command` SET `help` = "Syntax: .reload page_text\r\nReload page_text table.\nYou will need to delete the client cache to see the changes." WHERE `name` = "reload page_text";
-UPDATE `command` SET `help` = "Syntax: .reload page_text_locale\r\nReload page_text_locale table.\nYou will need to delete the client cache to see the changes." WHERE `name` = "reload page_text_locale";
+UPDATE `command` SET `help` = "Syntax: .reload page_text\r\nReload page_text table.\nYou need to delete your client cache in order to see the changes." WHERE `name` = "reload page_text";
+UPDATE `command` SET `help` = "Syntax: .reload page_text_locale\r\nReload page_text_locale table.\nYou need to delete your client cache in order to see the changes." WHERE `name` = "reload page_text_locale";
