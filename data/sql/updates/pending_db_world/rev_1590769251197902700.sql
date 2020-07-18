e1bf18374124ff05ab41f8014f001d27af1ab865
AzerothCoreBot <azerothcorebot@gmail.com>
2020-07-18 14:43:14 +0000

    Import pending SQL update file...
    Referenced commit(s): 8de2b063b281f9d916d390bc7bdb5e659324b148

--- data/sql/updates/pending_db_world/rev_1590769251197902700.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1590769251197902700');
-
--- Change maraudon portal faction to 0 (usable by both factions)
-UPDATE `gameobject_template_addon` SET `faction` = 0 WHERE `entry` = 178404;
-DELETE FROM `gameobject` WHERE `guid` = 265127 AND `id` = 178404;
