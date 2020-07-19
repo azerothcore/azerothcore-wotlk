cfa9b1e8b57381ed5c61356bb2e19fe4ae1fe691
Stefano Borzì <stefanoborzi32@gmail.com>
2020-07-19 11:04:16 +0200

    chore(sql): remove quotes from number

--- data/sql/updates/pending_db_world/rev_1595118810076078406.sql
+++ data/sql/updates/pending_db_world/rev_1595118810076078406.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1595118810076078406');
 
-UPDATE `creature_loot_template` SET `Chance` = '40' WHERE `Entry` = '3672' AND `Item` = '5423';
-UPDATE `creature_loot_template` SET `Chance` = '60', `GroupId` = '1' WHERE `Entry` = '3672' AND `Item` = '5422';
+UPDATE `creature_loot_template` SET `Chance` = 40 WHERE `Entry` = 3672 AND `Item` = 5423;
+UPDATE `creature_loot_template` SET `Chance` = 60, `GroupId` = 1 WHERE `Entry` = 3672 AND `Item` = 5422;
 
