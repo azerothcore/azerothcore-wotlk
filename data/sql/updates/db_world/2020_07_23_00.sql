16c20c967537cd47803f712b9e5d8fffaf94f607
AzerothCoreBot <azerothcorebot@gmail.com>
2020-07-23 12:57:05 +0000

    Import pending SQL update file...
    Referenced commit(s): e8fa3720a9ab1f671b49a64d31e554d19e7ea999

--- data/sql/updates/db_world/2020_07_23_00.sql
+++ data/sql/updates/db_world/2020_07_23_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_07_22_00 -> 2020_07_23_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_07_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_07_22_00 2020_07_23_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1590936036679871300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1590936036679871300');
 /*
  * Zone: Dustwallow Marsh
@@ -196,3 +212,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 4348);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (4348, 0, 0, 0, 0, 0, 100, 0, 2100, 2300, 32100, 32300, 11, 744, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Noxious Shredder - In Combat - Cast \'744\''),
 (4348, 0, 1, 0, 0, 0, 100, 0, 4500, 5000, 15000, 15500, 11, 3391, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Noxious Shredder - In Combat - Cast \'3391\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
