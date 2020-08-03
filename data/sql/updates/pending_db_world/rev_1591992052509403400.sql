83d5d9dcefb5c19ccb527fb0d45e1b4836bd6db6
AzerothCoreBot <azerothcorebot@gmail.com>
2020-08-03 13:02:30 +0000

    Import pending SQL update file...
    Referenced commit(s): 4e7648503795e9bb9a4945f8a7a454fd2326d6f1

--- data/sql/updates/pending_db_world/rev_1591992052509403400.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1591992052509403400');
-
-DELETE FROM `playercreateinfo_item` WHERE `itemid` = 40582 AND `amount` = -1;
-
-ALTER TABLE `playercreateinfo_item`
-	CHANGE `amount` `amount` SMALLINT UNSIGNED NOT NULL DEFAULT 1,
-	ADD `Note` VARCHAR(255) AFTER `amount`;
