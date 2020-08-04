960faef2cdc6212945547144a4ff7c4003e80c6f
Knindza <knindzagxg@gmail.com>
2020-08-04 23:03:39 +0200

    Update rev_1596557661388670700.sql

--- data/sql/updates/pending_db_world/rev_1596557661388670700.sql
+++ data/sql/updates/pending_db_world/rev_1596557661388670700.sql
@@ -14,7 +14,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 15949;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 15949);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(15949, 0, 0, 0, 0, 0, 100, 0, 2500, 3000, 9500, 1000, 11, 6818, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Thaelis the Hungerer - In Combat - Cast \'6818\'');
+(15949, 0, 0, 0, 0, 0, 100, 0, 2500, 3000, 9500, 10000, 11, 6818, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Thaelis the Hungerer - In Combat - Cast \'6818\'');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 15655;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 15655);
