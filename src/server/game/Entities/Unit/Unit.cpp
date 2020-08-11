3812bf15a936dc4341eebbf1fe24707a5eca119c
FrancescoBorzi <borzifrancesco@gmail.com>
2020-08-11 10:29:38 +0200

    fix: more indentation issues

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -15094,7 +15094,7 @@ void Unit::ProcDamageAndSpellFor(bool isVictim, Unit* target, uint32 procFlag, u
                     continue;
                 // Some spells must always trigger
                 //if (isAlwaysTriggeredAura[aurEff->GetAuraType()])
-                    triggerData.effMask |= 1<<i;
+                triggerData.effMask |= 1<<i;
             }
         }
         if (triggerData.effMask)
