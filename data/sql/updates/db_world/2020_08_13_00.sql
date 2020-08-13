2052491a5a67d6b7c5ce56d0756324303e7005f0
AzerothCoreBot <azerothcorebot@gmail.com>
2020-08-13 07:47:38 +0000

    Import pending SQL update file...
    Referenced commit(s): 8b57a3e25abf9d7e970b3d5a3061757004fa0f60

--- data/sql/updates/db_world/2020_08_13_00.sql
+++ data/sql/updates/db_world/2020_08_13_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_08_12_00 -> 2020_08_13_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_08_12_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_08_12_00 2020_08_13_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1592668418033075700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1592668418033075700');
 /*
  * Zone: Mulgore
@@ -143,3 +159,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 2965);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (2965, 0, 0, 0, 0, 0, 100, 0, 2800, 3200, 10800, 13200, 11, 9532, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Windfury Matriarch - In Combat - Cast \'9532\''),
 (2965, 0, 1, 0, 2, 0, 100, 1, 10, 40, 0, 0, 11, 332, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Windfury Matriarch - Between 10-40% Health - Cast \'332\' (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
