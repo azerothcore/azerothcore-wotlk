538740b83e202d10b493f942cdf92e2e86a65fb5
peti446 <peti446@users.noreply.github.com>
2020-08-15 14:58:41 +0100

    fix(CORE/Commands): Renamed .character deleted old to .character deleted purge Closes https://github.com/azerothcore/azerothcore-wotlk/issues/2925

---
+++ data/sql/updates/pending_db_world/rev_1597499028423474100.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1597499028423474100');
+
+UPDATE `command` SET `name` = 'character deleted purge', `help` = 'Syntax: .character deleted purge [#keepDays]\r \r Completely removes all characters from the database that where deleted more than #keepDays ago. If #keepDays not provided the used value from worldserver.conf option \'CharDelete.KeepDays\'. If \'CharDelete.KeepDays\' option is disabled (set to value 0) then this command can\'t be used without the specifying #keepDays parameter.' WHERE (`name` = 'character deleted old');
