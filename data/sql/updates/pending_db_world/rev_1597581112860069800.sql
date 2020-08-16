d2d8e08b1ae4b5cc43bb8075a2ff4e5d39fba96b
Francesco Borzì <borzifrancesco@gmail.com>
2020-08-16 15:06:02 +0200

    Update rev_1597581112860069800.sql

--- data/sql/updates/pending_db_world/rev_1597581112860069800.sql
+++ data/sql/updates/pending_db_world/rev_1597581112860069800.sql
@@ -2,3 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1597581112860069800');
 
 -- Nightfall
 UPDATE `item_template` SET `spellppmRate_1`=2.5, `ScriptName`='item_generic_limit_chance_above_60' WHERE `entry`=19169;
+
