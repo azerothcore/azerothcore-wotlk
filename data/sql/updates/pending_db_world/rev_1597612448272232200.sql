c13ac8031eaaf462d4ea625ff83f7f6ce72c38b5
peti446 <peti446@users.noreply.github.com>
2020-08-17 17:49:06 +0100

    fix(DB/GameObject): Added missing music box to shattrath

--- data/sql/updates/pending_db_world/rev_1597612448272232200.sql
+++ data/sql/updates/pending_db_world/rev_1597612448272232200.sql
@@ -4,3 +4,9 @@ UPDATE `gameobject_template` SET `ScriptName` = 'go_brewfest_music' WHERE `entry
 UPDATE `gameobject_template` SET `ScriptName` = 'go_pirate_day_music' WHERE `entry` = 190363;
 UPDATE `gameobject_template` SET `ScriptName` = 'go_darkmoon_faire_music' WHERE `entry` = 180335;
 UPDATE `gameobject_template` SET `ScriptName` = 'go_midsummer_music' WHERE `entry` = 188174;
+
+SET @LASTGUID = 2134513; 
+DELETE FROM `gameobject` WHERE `guid` BETWEEN @LASTGUID+1 AND @LASTGUID+2;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
+(@LASTGUID+1 , 186221, 530, 1, 1, -1897.498, 5560.614, -12.34483, 4.363323, 0, 0, 0, 0, 120, 255, 1),
+(@LASTGUID+2 , 186221, 530, 1, 1, 9325.442, -7276.318, 13.34217, 4.363323, 0, 0, 0, 0, 120, 255, 1);
