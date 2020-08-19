b72e35b9050d2753aacd86f1b9e77e3d4466685e
Petric <peti446@users.noreply.github.com>
2020-08-19 17:19:51 +0100

    fix(DB/GameObject): Added the music boxed to event 24

--- data/sql/updates/pending_db_world/rev_1597612448272232200.sql
+++ data/sql/updates/pending_db_world/rev_1597612448272232200.sql
@@ -10,3 +10,7 @@ DELETE FROM `gameobject` WHERE `guid` BETWEEN @LASTGUID+1 AND @LASTGUID+2;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
 (@LASTGUID+1 , 186221, 530, 1, 1, -1897.498, 5560.614, -12.34483, 4.363323, 0, 0, 0, 0, 120, 255, 1),
 (@LASTGUID+2 , 186221, 530, 1, 1, 9325.442, -7276.318, 13.34217, 4.363323, 0, 0, 0, 0, 120, 255, 1);
+
+DELETE FROM `game_event_gameobject` WHERE `guid` IN (@LASTGUID+1,@LASTGUID+2);
+INSERT INTO `game_event_gameobject` VALUES (24, @LASTGUID+1);
+INSERT INTO `game_event_gameobject` VALUES (24, @LASTGUID+2);
