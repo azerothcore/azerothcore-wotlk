ac9fc6c6ed5a3c9e3ef0fd783bae1695d34129b8
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2020-08-19 16:17:48 +0430

    feat(Core/Commands): Character check bag && work (#3164)

---
+++ data/sql/updates/pending_db_world/rev_1576659911412868500.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1576659911412868500');
+
+DELETE FROM `command` WHERE `name` IN ('bank', 'character check bank', 'character check bag', 'character check profession');
+
+INSERT INTO `command` VALUES
+('character check bank', 2, 'Syntax: .character check bank \r\n\r\nShow your bank inventory.'),
+('character check bag', 2, 'Syntax: .character check bag [$target_player]\r #bagSlot 1 - 4'),
+('character check profession', 2, 'Syntax: .character check profession [$target_player]\r\nShow known professions list for selected player');
