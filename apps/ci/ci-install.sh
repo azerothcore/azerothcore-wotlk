efc3508c996a53ab45d8f56af39c5be7aa13b30e
Erdem Meydanli <emeydanli@gmail.com>
2020-08-19 11:48:10 +0200

    feat(CI): Enable GCC support on the CI
    Add compiler support for GCC and GCC 10.

--- apps/ci/ci-install.sh
+++ apps/ci/ci-install.sh
@@ -2,7 +2,61 @@
 
 set -e
 
-cat >>conf/config.sh <<CONFIG_SH
+COMPILATION_OPTIONS='-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache'
+# These flags below are disabled for gcc, gcc 10 and clang 10.
+ADDITIONAL_OPTIONS='-DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror"'
+C_COMP=Unknown
+CPP_COMP=Unknown
+
+case $COMPILER in
+  # this is in order to use the "default" gcc version of the OS, without forcing a specific version
+  "gcc" )
+    time sudo apt-get install -y gcc g++
+    C_COMP=gcc
+    CPP_COMP=g++
+    ;;
+
+  "gcc10" )
+    time sudo apt-get install -y gcc-10 g++-10
+    C_COMP=gcc-10
+    CPP_COMP=g++-10
+    ;;
+
+  # this is in order to use the "default" clang version of the OS, without forcing a specific version
+  "clang" )
+    time sudo apt-get install -y clang
+    C_COMP=clang
+    CPP_COMP=clang++
+    COMPILATION_OPTIONS="${COMPILATION_OPTIONS} ${ADDITIONAL_OPTIONS}"
+    ;;
+
+  "clang6" )
+    time sudo apt-get install -y clang-6.0
+    C_COMP=clang-6
+    CPP_COMP=clang++-6
+    COMPILATION_OPTIONS="${COMPILATION_OPTIONS} ${ADDITIONAL_OPTIONS}"
+    ;;
+
+  "clang9" )
+    time sudo apt-get install -y clang-9
+    C_COMP=clang-9
+    CPP_COMP=clang++-9
+    COMPILATION_OPTIONS="${COMPILATION_OPTIONS} ${ADDITIONAL_OPTIONS}"
+    ;;
+
+  "clang10" )
+    time sudo apt-get install -y clang-10
+    C_COMP=clang-10
+    CPP_COMP=clang++-10
+    ;;
+
+  * )
+    echo "Unknown compiler $COMPILER"
+    exit 1
+    ;;
+esac
+
+cat > conf/config.sh <<CONFIG_SH
 MTHREADS=$(expr $(grep -c ^processor /proc/cpuinfo) + 2)
 CWARNINGS=ON
 CDEBUG=OFF
@@ -13,10 +67,12 @@ CSERVERS=ON
 CTOOLS=ON
 CSCRIPTPCH=OFF
 CCOREPCH=OFF
-CCUSTOMOPTIONS='-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror"'
 DB_CHARACTERS_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 DB_AUTH_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
 DB_WORLD_CONF="MYSQL_USER='root'; MYSQL_PASS='root'; MYSQL_HOST='localhost';"
+CCOMPILERC="${C_COMP}"
+CCOMPILERCXX="${CPP_COMP}"
+CCUSTOMOPTIONS="${COMPILATION_OPTIONS}"
 CONFIG_SH
 
 time sudo apt-get update -y
@@ -24,37 +80,4 @@ time sudo apt-get update -y
 time sudo apt-get install -y git lsb-release sudo ccache
 time ./acore.sh install-deps
 
-case $COMPILER in
 
-  # this is in order to use the "default" clang version of the OS, without forcing a specific version
-  "clang" )
-    time sudo apt-get install -y clang
-    echo "CCOMPILERC=\"clang\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++\"" >> ./conf/config.sh
-    ;;
-
-  "clang6" )
-    time sudo apt-get install -y clang-6.0
-    echo "CCOMPILERC=\"clang-6.0\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-6.0\"" >> ./conf/config.sh
-    ;;
-
-  "clang9" )
-    time sudo apt-get install -y clang-9
-    echo "CCOMPILERC=\"clang-9\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-9\"" >> ./conf/config.sh
-    ;;
-
-  "clang10" )
-    time sudo apt-get install -y clang-10
-    echo "CCOMPILERC=\"clang-10\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-10\"" >> ./conf/config.sh
-    # disable -Werror for clang-10 TODO: remove when this is fixed https://github.com/azerothcore/azerothcore-wotlk/issues/3108
-    echo "CCUSTOMOPTIONS='-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache'" >> ./conf/config.sh
-    ;;
-
-  * )
-    echo "Unknown compiler $COMPILER"
-    exit 1
-    ;;
-esac
