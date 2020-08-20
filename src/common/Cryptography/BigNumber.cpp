a43f88a7c62ec2cbf5cad6acef5b66f97cb94b2c
FrancescoBorzi <borzifrancesco@gmail.com>
2020-08-20 17:21:47 +0200

    chore: remove crap

--- src/common/Cryptography/BigNumber.cpp
+++ src/common/Cryptography/BigNumber.cpp
@@ -4,10 +4,6 @@
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  */
 
-#ifndef ACE_HAS_CPP17
-#define ACE_HAS_CPP17
-#endif
-
 #include <ace/Guard_T.h>
 
 #include "Cryptography/BigNumber.h"
