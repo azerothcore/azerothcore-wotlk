a5c301a265eb79aceeb21107b1e7066a1aa5e88a
Stefano Borzì <stefanoborzi32@gmail.com>
2020-08-22 19:11:29 +0200

    fix(DB/GO): improve gameobject instances respawn timesecs (#3201)

---
+++ data/sql/updates/pending_db_world/rev_1595112699414325989.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1595112699414325989');
+
+UPDATE `gameobject` SET `spawntimesecs`=2 WHERE guid IN (36738, 182011, 142088, 176484, 176485, 176486, 176487);
+UPDATE `gameobject` SET `spawntimesecs`=180 WHERE guid IN (182199, 176249, 177287);
+UPDATE `gameobject` SET `spawntimesecs`=300 WHERE guid IN (20726, 19284, 19283, 103664, 142477, 13949, 177964, 91138, 113757, 125477);
+UPDATE `gameobject` SET `spawntimesecs`=7200 WHERE guid = 179545;
+
