806f5d453392f5c550de7babf7306e1afb98caa1
Helias <stefanoborzi32@gmail.com>
2020-08-24 23:02:22 +0200

    fix(DB/Quest): gameobject loot and related responsible spell script

---
+++ data/sql/updates/pending_db_world/rev_1598302868929248922.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598302868929248922');
+UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE `item` = 43059;
+INSERT INTO `spell_target_position` VALUES
+(57553, 0, 571, 5875.43, -1981.37, 234.671, 0, 0); -- Escape Voltarus
