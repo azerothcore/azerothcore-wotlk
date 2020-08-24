261d90c1e9163b9e30aeaa10c298773ac6c8fc15
Viste <viste02@gmail.com>
2020-08-24 23:07:37 +0300

    Core/Misc: remove ACE_Auto_Ptr from core

--- src/common/Cryptography/HMACSHA1.cpp
+++ src/common/Cryptography/HMACSHA1.cpp
@@ -1,12 +1,24 @@
 /*
- * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ * This file is part of the WarheadCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "HMACSHA1.h"
 #include "BigNumber.h"
-#include "Common.h"
+#include "Errors.h"
+#include <cstring>
 
 #if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L
 HMAC_CTX* HMAC_CTX_new()
@@ -23,8 +35,7 @@ void HMAC_CTX_free(HMAC_CTX* ctx)
 }
 #endif
 
-
-HmacHash::HmacHash(uint32 len, uint8 *seed)
+HmacHash::HmacHash(uint32 len, uint8* seed)
 {
     m_ctx = HMAC_CTX_new();
     HMAC_Init_ex(m_ctx, seed, len, EVP_sha1(), nullptr);
@@ -36,14 +47,14 @@ HmacHash::~HmacHash()
     HMAC_CTX_free(m_ctx);
 }
 
-void HmacHash::UpdateData(const std::string &str)
+void HmacHash::UpdateData(std::string const& str)
 {
     HMAC_Update(m_ctx, reinterpret_cast<uint8 const*>(str.c_str()), str.length());
 }
 
-void HmacHash::UpdateData(const uint8* data, size_t len)
+void HmacHash::UpdateData(uint8 const* data, size_t len)
 {
-   HMAC_Update(m_ctx, data, len);
+    HMAC_Update(m_ctx, data, len);
 }
 
 void HmacHash::Finalize()
