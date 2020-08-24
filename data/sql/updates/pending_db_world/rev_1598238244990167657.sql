e9ddfa1ecfbe7ea33a601b3b29ebf8c32a42c93c
locus313 <pat@lo5t.com>
2020-08-24 09:09:56 -0700

    update smart_scripts insert

--- data/sql/updates/pending_db_world/rev_1598238244990167657.sql
+++ data/sql/updates/pending_db_world/rev_1598238244990167657.sql
@@ -2,7 +2,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598238244990167657');
 
 UPDATE `creature_template` SET `faction` = 21 WHERE `entry` = 14688;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 14688;
-INSERT INTO `smart_scripts` VALUES
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 	(14688, 0, 0, 0, 0, 0, 80, 0, 0, 0, 10000, 20000, 0, 11, 61162, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Prince Sandoval - In Combat - Cast \'Engulfing Strike\''),
 	(14688, 0, 1, 0, 0, 0, 80, 0, 15000, 20000, 30000, 30000, 0, 11, 61163, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Prince Sandoval - In Combat - Cast \'Fire Nova\''),
 	(14688, 0, 2, 0, 0, 0, 100, 0, 30000, 30000, 30000, 30000, 0, 11, 61144, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Prince Sandoval - In Combat - Cast \'Fire Shield\''),
