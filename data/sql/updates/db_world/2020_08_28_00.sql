fae7f58b2d42c31d54169f95c90dcb3c903d93c7
AzerothCoreBot <azerothcorebot@gmail.com>
2020-08-28 14:10:15 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): fd51741406af99be178e6b12c4105ebd7b94a908

--- data/sql/updates/db_world/2020_08_28_00.sql
+++ data/sql/updates/db_world/2020_08_28_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_08_27_01 -> 2020_08_28_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_08_27_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_08_27_01 2020_08_28_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1596557661388670700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1596557661388670700');
 /*
  * Zone: Eversong Woods
@@ -55,3 +71,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 16347;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 16347);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (16347, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 32918, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Starving Ghostclaw - On Aggro - Cast \'32918\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
