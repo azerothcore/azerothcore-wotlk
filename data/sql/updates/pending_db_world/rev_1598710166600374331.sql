1b1f11105d8de1d4a143ab07c8a0f2deb07bde29
Stefano Borzì <stefanoborzi32@gmail.com>
2020-08-29 16:14:16 +0200

    Update rev_1598710166600374331.sql

--- data/sql/updates/pending_db_world/rev_1598710166600374331.sql
+++ data/sql/updates/pending_db_world/rev_1598710166600374331.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598710166600374331');
-UPDATE acore_world.creature SET position_x=-7249.024, position_y=-937.411, position_z=168.396, orientation=4.995 WHERE guid=5231;
-UPDATE acore_world.creature SET position_x=-7249.391, position_y=-939.451, position_z=168.207, orientation=0.638 WHERE guid=5848;
-UPDATE acore_world.creature SET position_x=-7276.807, position_y=-877.667, position_z=169.758, orientation=3.428 WHERE guid=5747;
-UPDATE acore_world.creature SET position_x=-7279.149, position_y=-877.149, position_z=169.645, orientation=6.164 WHERE guid=3756;
+UPDATE `creature` SET `position_x`=-7249.024, `position_y`=-937.411, `position_z`=168.396, `orientation`=4.995 WHERE `guid`=5231;
+UPDATE `creature` SET `position_x`=-7249.391, `position_y`=-939.451, `position_z`=168.207, `orientation`=0.638 WHERE `guid`=5848;
+UPDATE `creature` SET `position_x`=-7276.807, `position_y`=-877.667, `position_z`=169.758, `orientation`=3.428 WHERE `guid`=5747;
+UPDATE `creature` SET `position_x`=-7279.149, `position_y`=-877.149, `position_z`=169.645, `orientation`=6.164 WHERE `guid`=3756;
