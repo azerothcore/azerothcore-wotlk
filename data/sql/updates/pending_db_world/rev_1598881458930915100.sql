0405db3c7e9b242d4e0db08d59602c618ea1ee18
Knindza <knindzagxg@gmail.com>
2020-08-31 20:02:13 +0200

    Update rev_1598881458930915100.sql

--- data/sql/updates/pending_db_world/rev_1598881458930915100.sql
+++ data/sql/updates/pending_db_world/rev_1598881458930915100.sql
@@ -7,4 +7,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598881458930915100');
 
 /* Content 3.3.5 */
 
+DELETE FROM `instance_template` WHERE `map`=169;
 INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (169, 0, '', 1);
