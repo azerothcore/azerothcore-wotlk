05ed75949fd6a89abdf1f16c952b611f0033976e
Knindza <knindzagxg@gmail.com>
2020-08-31 20:20:18 +0200

    Update rev_1598881657136430300.sql

--- data/sql/updates/pending_db_auth/rev_1598881657136430300.sql
+++ data/sql/updates/pending_db_auth/rev_1598881657136430300.sql
@@ -5,7 +5,7 @@ INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1598881657136430300');
  * Copyright (C) <www.shadowburn.net> & <www.lichbane.com>
 */
 
-/* Content 3.3.5 */
+/* Content 3.3.5  */
 
 ALTER TABLE `account`
 	CHANGE COLUMN `active` `active` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `recruiter`,
