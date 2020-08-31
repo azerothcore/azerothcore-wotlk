8570dff515720479cd30013eb8507964a8c59c6c
Knindza <knindzagxg@gmail.com>
2020-08-31 15:44:52 +0200

    [Zone] Emerald Dream

---
+++ data/sql/updates/pending_db_world/rev_1598881458930915100.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598881458930915100');
+/*
+ * General: Emerald Dream Update
+ * Update by Knindza | <www.azerothcore.org>
+ * Copyright (C) <www.shadowburn.net> & <www.lichbane.com>
+*/
+
+/* Content 3.3.5 */
+
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (169, 0, '', 1);
