bda7ff50037e407cb64d9eeb689628990bfacb2f
Knindza <knindzagxg@gmail.com>
2020-08-31 15:43:29 +0200

    [NPC] Silvermoon Pet Trainer

---
+++ data/sql/updates/pending_db_world/rev_1598881374545560300.sql
@@ -0,0 +1,15 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598881374545560300');
+/*
+ * General: NPC Update
+ * Update by Knindza | <www.azerothcore.org>
+ * Copyright (C) <www.shadowburn.net> & <www.lichbane.com>
+*/
+
+/* Content 3.3.5 */
+
+-- NPC entry 40405 Kieupid, Pet Trainer in Silvermoon City (map: 530, zone ID 0, area ID 0)
+SET @CGUID = 248570;
+
+DELETE FROM `creature` WHERE `id` = 40405;
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
+(@CGUID,40405,530,1,1,0,0, 9924.067, -7400.503, 13.71723, 6.073746, 120,0,0,0,0,0,0,0,0);
