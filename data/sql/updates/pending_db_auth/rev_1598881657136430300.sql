2be0a90c20e9414517dcc57318149152af7f8146
Knindza <knindzagxg@gmail.com>
2020-08-31 15:48:06 +0200

    [Auth] Frozen Feature

---
+++ data/sql/updates/pending_db_auth/rev_1598881657136430300.sql
@@ -0,0 +1,13 @@
+INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1598881657136430300');
+/*
+ * General: Auth Update
+ * Update by Knindza | <www.azerothcore.org>
+ * Copyright (C) <www.shadowburn.net> & <www.lichbane.com>
+*/
+
+/* Content 3.3.5 */
+
+ALTER TABLE `account`
+	CHANGE COLUMN `active` `active` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `recruiter`,
+	ADD COLUMN `frozen` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `active`;
+	
