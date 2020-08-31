cfb4a7e1e0c3d8632ae6f851754fe84d533ae996
Knindza <knindzagxg@gmail.com>
2020-08-31 20:28:01 +0200

    Update data/sql/updates/pending_db_world/rev_1598881374545560300.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1598881374545560300.sql
+++ data/sql/updates/pending_db_world/rev_1598881374545560300.sql
@@ -10,6 +10,6 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598881374545560300');
 -- NPC entry 40405 Kieupid, Pet Trainer in Silvermoon City (map: 530, zone ID 0, area ID 0)
 SET @CGUID = 248600;
 
-DELETE FROM `creature` WHERE `id` = 40405;
+DELETE FROM `creature` WHERE `id` = 40405 AND `guid` = @CGUID;
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
 (@CGUID,40405,530,1,1,0,0, 9924.067, -7400.503, 13.71723, 6.073746, 120,0,0,0,0,0,0,0,0);
