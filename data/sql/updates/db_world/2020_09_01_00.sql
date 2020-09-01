eb952ecb3e89d94835f1f58d2614b0f05a87f82c
AzerothCoreBot <azerothcorebot@gmail.com>
2020-09-01 09:49:22 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): a07c2b3989e913c7f096f334ad5a70cc72a826ed

--- data/sql/updates/db_world/2020_09_01_00.sql
+++ data/sql/updates/db_world/2020_09_01_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_08_31_00 -> 2020_09_01_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_08_31_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_08_31_00 2020_09_01_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1596728845705471100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1596728845705471100');
 /*
  * Raid: Ulduar
@@ -22,3 +38,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (33806, 0, 0, 1, 6, 0, 100, 0, 0, 0, 0, 0, 28, 63710, 0, 0, 0, 0, 0, 19, 33772, 100, 0, 0, 0, 0, 0, 'Void Beast - On Just Died - Remove Aura \'63710\''),
 (33806, 0, 1, 0, 7, 0, 100, 0, 0, 0, 0, 0, 41, 10000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Void Beast - On Evade - Despawn In 10000 ms'),
 (33806, 0, 2, 0, 0, 0, 100, 0, 10000, 15000, 20000, 40000, 11, 63723, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Void Beast - In Combat - Cast \'63723\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
