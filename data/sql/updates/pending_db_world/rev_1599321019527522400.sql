865d7865b9316214b37bcd26c8473a72c1331eba
Akimbo <66260220+Akimboss@users.noreply.github.com>
2020-09-05 19:27:21 +0200

    Update rev_1599321019527522400.sql

--- data/sql/updates/pending_db_world/rev_1599321019527522400.sql
+++ data/sql/updates/pending_db_world/rev_1599321019527522400.sql
@@ -1,2 +1,2 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1599321019527522400');
-UPDATE creature_template SET modelid1='30583' WHERE (entry='32315');
+UPDATE creatureSET modelid1='30583' WHERE (id='32315');
