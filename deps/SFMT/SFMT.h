b401451882d21801449ff35943b7be7e6050f19b
FrancescoBorzi <borzifrancesco@gmail.com>
2020-09-08 17:41:47 +0200

    chore(wip): wip

--- deps/SFMT/SFMT.h
+++ deps/SFMT/SFMT.h
@@ -168,16 +168,18 @@ public:
         uint32_t y = seed;                  // Temporary
         uint32_t statesize = SFMT_N*4;      // Size of state vector
 
+// TODO: manually silencing this warning, but it should properly fixed
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
         // Fill state vector with random numbers from seed
-        #pragma GCC diagnostic ignored "-Wstrict-aliasing"
         ((uint32_t*)state)[0] = y;
         const uint32_t factor = 1812433253U;// Multiplication factor
 
         for (i = 1; i < statesize; i++) {
             y = factor * (y ^ (y >> 30)) + i;
-            #pragma GCC diagnostic ignored "-Wstrict-aliasing"
             ((uint32_t*)state)[i] = y;
         }
+#pragma GCC diagnostic pop
 
         // Further initialization and period certification
         Init2();
