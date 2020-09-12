6a4b5ad1dbbdd142b4968b58ffd885980d0ffa9f
Kargatum <dowlandtop@yandex.com>
2020-09-13 05:45:52 +0700

    chore(Core/Scripting): cleanup unused code (#3473)

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -787,10 +787,6 @@ class AchievementCriteriaScript : public ScriptObject
 
         bool IsDatabaseBound() const { return true; }
 
-        // Called when an additional criteria is checked.
-        virtual bool OnCheck(Player* source, Unit* target, uint32 /*criteria_id*/) {
-            return OnCheck(source, target);
-        }
         // deprecated/legacy
         virtual bool OnCheck(Player* /*source*/, Unit* /*target*/) { return true; };
 };
@@ -1403,7 +1399,7 @@ class ScriptMgr
 
     public: /* AchievementCriteriaScript */
 
-        bool OnCriteriaCheck(uint32 scriptId, Player* source, Unit* target, uint32 criteria_id);
+        bool OnCriteriaCheck(uint32 scriptId, Player* source, Unit* target);
 
     public: /* PlayerScript */
 
