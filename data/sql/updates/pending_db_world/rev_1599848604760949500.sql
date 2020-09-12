0f0878cf6f0245b60eb78ef2de8d883ceaa469ab
Craftorix <70746948+craftorix@users.noreply.github.com>
2020-09-12 11:36:33 +0200

    Update rev_1599848604760949500.sql

--- data/sql/updates/pending_db_world/rev_1599848604760949500.sql
+++ data/sql/updates/pending_db_world/rev_1599848604760949500.sql
@@ -6,7 +6,7 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 
 DELETE FROM `creature_addon` WHERE guid IN (9204,9205);
 DELETE FROM `waypoint_data` WHERE id IN (92040,92050);
-UPDATE `creature` SET `MovementType`=0, `SpawnDist`=0 WHERE `id` IN (2477,7170);
+UPDATE `creature` SET `MovementType`=0, `wander_distance`=0 WHERE `id` IN (2477,7170);
 UPDATE `creature_template` SET `MovementType`=0 WHERE `entry` IN (2477,7170);
 UPDATE `creature_template` SET `Speed_Walk`=1.47, `Speed_Run`=1.14286 WHERE `entry` IN (2478);
 UPDATE `creature_template` SET `Speed_Walk`=1.48, `Speed_Run`=1.14286 WHERE `entry` IN (2477,7170);
