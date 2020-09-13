854dbefe640424fb1c4a626b25629f59383247f0
AzerothCoreBot <azerothcorebot@gmail.com>
2020-09-13 15:50:35 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d327641dd90acbdb2c348b057758bdc9926994e4

--- data/sql/updates/pending_db_world/rev_1598302868929248922.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598302868929248922');
-UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE `item` = 43059;
-DELETE FROM `spell_target_position` WHERE `ID` = 57553;
-INSERT INTO `spell_target_position` VALUES
-(57553, 0, 571, 5875.43, -1981.37, 234.671, 0, 0); -- Escape Voltarus
