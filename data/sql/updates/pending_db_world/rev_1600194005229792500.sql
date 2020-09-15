35a68a8b9e5721108d49899469f383000f4a374c
HoustonCore <houston.feedback@gmail.com>
2020-09-15 21:30:49 +0300

    Fix build

--- data/sql/updates/pending_db_world/rev_1600194005229792500.sql
+++ data/sql/updates/pending_db_world/rev_1600194005229792500.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1600194005229792500');
 
 -- Delete LANG_FLEE and LANG_CALL_FOR_HELP
-DELETE FROM `trinity_string` WHERE `entry` IN (5035,5030);
+DELETE FROM `acore_string` WHERE `entry` IN (5035,5030);
