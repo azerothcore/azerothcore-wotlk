32e5d978325cd2d85aaaeff764445183b65f8949
locus313 <pat@lo5t.com>
2020-09-21 19:00:17 -0700

    fix sql import

--- data/sql/updates/pending_db_world/rev_1600739072121853409.sql
+++ data/sql/updates/pending_db_world/rev_1600739072121853409.sql
@@ -1,8 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1600739072121853409');
 
---- brewfest camp hostile mobs ---
+/* brewfest camp hostile mobs */
 DELETE FROM `creature` WHERE `guid` IN (7370, 22181, 22188, 22473);
 
---- brewfest camp non-hostile mobs ---
+/* brewfest camp non-hostile mobs */
 DELETE FROM `creature` WHERE `guid` IN (12369, 21020, 21022, 21026);
 
