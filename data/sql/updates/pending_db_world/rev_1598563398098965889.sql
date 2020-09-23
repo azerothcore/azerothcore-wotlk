bdf59de17a8b8650a6a51652a52b21f5824aee81
AzerothCoreBot <azerothcorebot@gmail.com>
2020-09-23 08:23:11 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d20554d45d4cfe9b2bcc75390144367a0aaa840e

--- data/sql/updates/pending_db_world/rev_1598563398098965889.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598563398098965889');
-UPDATE `waypoint_data` SET `position_x`=-6130.679199, `position_y`=393.473969 WHERE `id`=10210 and `point`=19;
