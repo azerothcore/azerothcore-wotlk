c20c8ad3e5433218ec298dad9d7769b3f4da8b12
AzerothCoreBot <azerothcorebot@gmail.com>
2020-09-24 12:09:38 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): a2a71240d912747e96e0fa20e5f86a1a250ea4d4

--- data/sql/updates/db_world/2020_09_24_00.sql
+++ data/sql/updates/db_world/2020_09_24_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_09_23_00 -> 2020_09_24_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_09_23_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_09_23_00 2020_09_24_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1598878347481363100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598878347481363100');
 /*
  * Dungeon: Dire Maul (North)
@@ -25,3 +41,12 @@ UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 652, `max
 UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 661, `maxdmg` = 798, `DamageModifier` = 1.01 WHERE `entry` = 14325;
 UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 463, `maxdmg` = 571, `DamageModifier` = 1.01 WHERE `entry` = 14324;
 UPDATE `creature_template` SET `type_flags`=`type_flags`|4, `mindmg` = 635, `maxdmg` = 810, `DamageModifier` = 1.01 WHERE `entry` = 11501;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
