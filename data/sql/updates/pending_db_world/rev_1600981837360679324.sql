8ff5f68733679a0fff5230ddf37589d2bfdc6088
locus313 <pat@lo5t.com>
2020-09-24 17:08:14 -0700

    update from @genion18

--- data/sql/updates/pending_db_world/rev_1600981837360679324.sql
+++ data/sql/updates/pending_db_world/rev_1600981837360679324.sql
@@ -12,8 +12,8 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 and `SourceGroup` in (10603,10604) AND `SourceEntry`= 1 ;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`)VALUES
-(15, 10603, 1, 0, 0, 2, 0, 46735, 1, 0, 1, 0, 0, "", "Gossip Option when player has no item Synthebrew Goggles"),
-(15, 10604, 1, 0, 0, 2, 0, 46735, 1, 0, 1, 0, 0, "", "Gossip Option when player has no item Synthebrew Goggles");
+(15, 10603, 1, 0, 0, 2, 0, 46735, 1, 1, 1, 0, 0, "", "Gossip Option when player has no item Synthebrew Goggles"),
+(15, 10604, 1, 0, 0, 2, 0, 46735, 1, 1, 1, 0, 0, "", "Gossip Option when player has no item Synthebrew Goggles");
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`= 24657 and `source_type` = 0 and `id` in (0,1,2,3,4,5,6);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
