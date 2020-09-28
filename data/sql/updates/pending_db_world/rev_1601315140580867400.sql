286901a8ad13f309ecb419813c246f0bbb77e046
Knindza <knindzagxg@gmail.com>
2020-09-28 19:54:17 +0200

    Update rev_1601315140580867400.sql
    Updated Bloodling to be despawned on Evade.
    Just in case.

--- data/sql/updates/pending_db_world/rev_1601315140580867400.sql
+++ data/sql/updates/pending_db_world/rev_1601315140580867400.sql
@@ -118,3 +118,9 @@ DELETE FROM `creature_addon` WHERE `guid` IN (36898, 36904, 36907, 36924, 36938,
 UPDATE `creature` SET `position_x`=6594.56, `position_y`=313.645, `position_z`=28.3839 WHERE `guid`=36643;
 UPDATE `creature` SET `position_x`=4424.85, `position_y`=422.488, `position_z`=56.4554 WHERE `guid`=36794;
 UPDATE `creature` SET `position_x`=7742.69, `position_y`=-1067.14, `position_z`=38.1206 WHERE `guid`=36959;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 7768;
+
+DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 7768);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(7768, 0, 0, 0, 7, 0, 100, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Witherbark Bloodling - On Evade - Despawn In 5000 ms');
