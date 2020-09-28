e9e5f32ea7b700ec78c55d1b69149ec73579a06e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-09-28 12:15:51 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 3e81beac5874e541495b2658b8ff3ec6d613d05f

--- data/sql/updates/db_world/2020_09_28_00.sql
+++ data/sql/updates/db_world/2020_09_28_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_09_25_00 -> 2020_09_28_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_09_25_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_09_25_00 2020_09_28_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1598878078501049600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598878078501049600');
 /*
  * Dungeon: Dire Maul (East)
@@ -37,3 +53,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 13276;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 13276);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (13276, 0, 0, 0, 9, 0, 100, 2, 0, 20, 6100, 15700, 11, 13340, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Wildspawn Imp - Within 0-20 Range - Cast \'13340\' (Normal Dungeon)');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
