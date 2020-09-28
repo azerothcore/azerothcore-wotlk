fbb479b0b34f5e04a599083b6c4c82e0a630e5e0
Si1ker <55638679+Sombranator@users.noreply.github.com>
2020-09-27 23:12:44 -0600

    fix(DB/Loot): Naxxramas 25
    This will fix the Naxxarmas 25 loot on Thaddius, Four Horsemen, Gluth and Loatheb as their current loot values are wrong
    
    References:
    
    End wing bosses like Thaddius
    (at 2:55)  https://www.youtube.com/watch?v=WMVGZNe-cbo
    
    Non-end wing bosses like Gluth:
    (at 3:35)  https://www.youtube.com/watch?v=6_jUTBGQJew
    (at 4:00)  https://www.youtube.com/watch?v=Yi85pjYGg0c
    
    Full reference: https://www.mmo-champion.com/threads/625542-Simple-Question-How-many-items-per-Heoric-Naxx-Boss

---
+++ data/sql/updates/pending_db_world/rev_1601269291015416700.sql
@@ -0,0 +1,92 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601269291015416700');
+
+-- = Thaddius 25 = 29448 -------------------------------------------------------------------------------
+
+-- Create referenced tokens into the reference table
+DELETE FROM `reference_loot_template` WHERE `Entry`=34380;
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('34380', '40634', '0', '0', '0', '1', '1', '1', '1'),
+('34380', '40635', '0', '0', '0', '1', '1', '1', '1'),
+('34380', '40636', '0', '0', '0', '1', '1', '1', '1');
+
+-- Insert reference loot into the creature loot temlate
+DELETE FROM `creature_loot_template` WHERE `Entry`=29448 AND `Reference`=34380;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('29448', '2', '34380', '100', '0', '1', '0', '1', '2');
+
+-- Delete old tokens which are separetely placed
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29448 AND `Item`=40634;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29448 AND `Item`=40635;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29448 AND `Item`=40636;
+
+
+-- = Loatheb 25 = 29718 -------------------------------------------------------------------------------
+
+-- Create referenced tokens into the reference table
+DELETE FROM `reference_loot_template` WHERE `Entry`=34381;
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('34381', '40637', '0', '0', '0', '1', '1', '1', '1'),
+('34381', '40638', '0', '0', '0', '1', '1', '1', '1'),
+('34381', '40639', '0', '0', '0', '1', '1', '1', '1');
+
+-- Insert reference loot into the creature loot temlate
+DELETE FROM `creature_loot_template` WHERE `Entry`=29718 AND `Reference`=34381;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('29718', '2', '34381', '100', '0', '1', '0', '1', '2');
+
+-- Delete old tokens which are separetely placed
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29718 AND `Item`=40637;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29718 AND `Item`=40638;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29718 AND `Item`=40639;
+
+
+-- = Four horsemen 25 = 25193 (data1)
+
+-- Create referenced tokens into the reference table
+DELETE FROM `reference_loot_template` WHERE `Entry`=34382;
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('34382', '40625', '0', '0', '0', '1', '1', '1', '1'),
+('34382', '40626', '0', '0', '0', '1', '1', '1', '1'),
+('34382', '40627', '0', '0', '0', '1', '1', '1', '1');
+
+-- Insert reference loot into the creature loot temlate
+DELETE FROM `gameobject_loot_template` WHERE `Entry`=25193 AND `Reference`=34382;
+INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('25193', '2', '34382', '100', '0', '1', '0', '1', '2');
+
+-- Delete tokens which are separetely placed
+DELETE FROM `gameobject_loot_template` WHERE  `Entry`=25193 AND `Item`=40625;
+DELETE FROM `gameobject_loot_template` WHERE  `Entry`=25193 AND `Item`=40626;
+DELETE FROM `gameobject_loot_template` WHERE  `Entry`=25193 AND `Item`=40627;
+
+
+-- = Gluth 25 = 29417
+
+-- Create referenced tokens into the reference table
+DELETE FROM `reference_loot_template` WHERE `Entry`=34383;
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('34383', '40625', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40626', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40627', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40634', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40635', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40636', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40637', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40638', '0', '0', '0', '1', '1', '1', '1'),
+('34383', '40639', '0', '0', '0', '1', '1', '1', '1');
+
+-- Insert reference loot into the creature loot temlate
+DELETE FROM `creature_loot_template` WHERE `Entry`=29417 AND `Reference`=34383;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+('29417', '2', '34383', '100', '0', '1', '0', '1', '2');
+
+-- Delete tokens which are separetely placed
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40625;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40626;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40627;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40634;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40635;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40636;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40637;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40638;
+DELETE FROM `creature_loot_template` WHERE  `Entry`=29417 AND `Item`=40639;
