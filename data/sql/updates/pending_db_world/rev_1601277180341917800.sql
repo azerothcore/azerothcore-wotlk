ac7b64b1ad161f710fb27979a554c5fd136e22ef
Silker <61223313+Si1ker@users.noreply.github.com>
2020-09-28 18:08:06 -0600

    Update rev_1601277180341917800.sql
    Removed strings from numeric values

--- data/sql/updates/pending_db_world/rev_1601277180341917800.sql
+++ data/sql/updates/pending_db_world/rev_1601277180341917800.sql
@@ -1,8 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601277180341917800');
 
--- Immune to Spell IDs: 9484 / 3355 / 20066 / 57546 / 60192 / 339 / 33786
-UPDATE `creature_template` SET mechanic_immune_mask='550187859' WHERE  entry=34607; -- ToC 10-man Normal Raid
-UPDATE `creature_template` SET mechanic_immune_mask='550187859' WHERE  entry=34648; -- ToC 25-man Normal Raid 
-UPDATE `creature_template` SET mechanic_immune_mask='550187859' WHERE  entry=35655; -- ToC 10-man Heroic Raid
-UPDATE `creature_template` SET mechanic_immune_mask='550187859' WHERE  entry=35656; -- ToC 25-man Heroic Raid
+-- Immune to Spell IDs: 9484 / 3355 / 20066 / 10326 / 60192 / 339 / 33786
+UPDATE `creature_template` SET mechanic_immune_mask=550187859 WHERE  entry=34607; -- ToC 10-man Normal Raid
+UPDATE `creature_template` SET mechanic_immune_mask=550187859 WHERE  entry=34648; -- ToC 25-man Normal Raid 
+UPDATE `creature_template` SET mechanic_immune_mask=550187859 WHERE  entry=35655; -- ToC 10-man Heroic Raid
+UPDATE `creature_template` SET mechanic_immune_mask=550187859 WHERE  entry=35656; -- ToC 25-man Heroic Raid
 
