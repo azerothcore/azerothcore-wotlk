d090a832c651b03b583b58e0d5498ac80dc61e22
Silker <61223313+Si1ker@users.noreply.github.com>
2020-09-29 02:42:29 -0600

    Update rev_1601361978416709800.sql
    Add space at the end

--- data/sql/updates/pending_db_world/rev_1601361978416709800.sql
+++ data/sql/updates/pending_db_world/rev_1601361978416709800.sql
@@ -3,3 +3,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601361978416709800');
 DELETE FROM `creature_loot_template` WHERE `entry` = 32297 AND `item` = 44563;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
 (32297, 44563, 0, 1, 0, 1, 0, 1, 1); -- reference: https://db.rising-gods.de/?item=44563#dropped-by
+
