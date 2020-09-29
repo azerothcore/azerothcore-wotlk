8a9cecc71d383f085f24c9f39941af5798bbc552
Silker <61223313+Si1ker@users.noreply.github.com>
2020-09-29 13:40:10 -0600

    Update data/sql/updates/pending_db_world/rev_1601361978416709800.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1601361978416709800.sql
+++ data/sql/updates/pending_db_world/rev_1601361978416709800.sql
@@ -1,8 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601361978416709800');
 
 DELETE FROM `creature_loot_template` WHERE `entry` = 32297 AND `item` = 44563;
-DELETE FROM `creature_loot_template` WHERE `entry` = 31702 AND `item` = 44563;
+DELETE FROM `creature_loot_template` WHERE `entry` IN (31702, 32297) AND `item` = 44563;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
 (32297, 44563, 0, 1, 0, 1, 0, 1, 1); -- Cult Researcher: https://db.rising-gods.de/?item=44563#dropped-by
 (31702, 44563, 0, 1, 0, 1, 0, 1, 1); -- Frostbrood Spawn: https://www.wowhead.com/item=44563/pattern-fur-lining-arcane-resist
-
