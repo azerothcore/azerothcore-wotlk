6bbba24b62e71db07ada1df0b54816a9e8897ee0
Si1ker <55638679+Sombranator@users.noreply.github.com>
2020-09-29 00:53:00 -0600

    fix(DB/creature_loot_template): Pattern: Fur Lining - Arcane Resist
    This change will add the item to the NPC Cult Researcher
    
    Reference: https://db.rising-gods.de/?item=44563#dropped-by

---
+++ data/sql/updates/pending_db_world/rev_1601361978416709800.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601361978416709800');
+
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
+(32297, 44563, 0, 1, 0, 1, 0, 1, 1); -- reference: https://db.rising-gods.de/?item=44563#dropped-by
+
