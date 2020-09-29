1ac4116da9ae4c587f0a6e595c0f3401935ab7cb
Knindza <knindzagxg@gmail.com>
2020-09-29 17:02:37 +0200

    Update rev_1601315140580867400.sql

--- data/sql/updates/pending_db_world/rev_1601315140580867400.sql
+++ data/sql/updates/pending_db_world/rev_1601315140580867400.sql
@@ -86,7 +86,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (11912,0,0,0,4,0,100,0,0,0,0,0,11,6268,0,0,0,0,0,2,0,0,0,0,0,0,0,'Grimtotem Brute - On Aggro - Cast RushIng Charge'),
 (11912,0,1,0,2,0,100,1,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Grimtotem Brute - Between 0-15% Health - Flee For Assist (No Repeat)');
 
-UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=17236; CHECk
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=17236;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=17236 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17236,0,0,0,6,0,100,0,0,0,0,0,11,11018,0,0,0,0,0,7,0,0,0,0,0,0,0,'Tcha\'kaz - On Death - Summon Witherbark Bloodlings');
