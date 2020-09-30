4c2eae0f487debe288a056ff2efeec135c03d09f
Silker <61223313+Si1ker@users.noreply.github.com>
2020-09-30 00:03:55 -0600

    Fix query

--- data/sql/updates/pending_db_world/rev_1601277180341917800.sql
+++ data/sql/updates/pending_db_world/rev_1601277180341917800.sql
@@ -1,8 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601277180341917800');
 
--- Immune to Spell IDs: 9484 / 3355 / 20066 / 10326 / 60192 / 339 / 33786
-UPDATE `creature_template` SET `mechanic_immune_mask`=550187859 WHERE `entry`=34607; -- ToC 10-man Normal Raid
-UPDATE `creature_template` SET `mechanic_immune_mask`=550187859 WHERE `entry`=34648; -- ToC 25-man Normal Raid 
-UPDATE `creature_template` SET `mechanic_immune_mask`=550187859 WHERE `entry`=35655; -- ToC 10-man Heroic Raid
-UPDATE `creature_template` SET `mechanic_immune_mask`=550187859 WHERE `entry`=35656; -- ToC 25-man Heroic Raid
+UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|2|64|4096|8192|131072|524288 WHERE `entry` IN 
+(34607, 34648, 35655, 35656);
 
