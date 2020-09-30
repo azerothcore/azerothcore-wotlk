ad07f9adafeafea53428124eca94b07a5d0d678c
Si1ker <55638679+Sombranator@users.noreply.github.com>
2020-09-30 02:08:19 -0600

    fix(DB/creature_temlate): Adjust Anub'arak minions level
    This change adjust the level of the Anub'arak (ToC version) minions to their correct values
    
    Nerubian Burrower | https://www.wowhead.com/npc=34607/nerubian-burrower
    Swarm Scarab | https://www.wowhead.com/npc=34605/swarm-scarab

---
+++ data/sql/updates/pending_db_world/rev_1601451772137449700.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601451772137449700');
+
+-- https://www.wowhead.com/npc=34607/nerubian-burrower
+UPDATE `creature_template` SET `minlevel`=82, `maxlevel`=82 WHERE `entry` IN 
+(34607, 34648, 35655, 35656);
+
+-- https://www.wowhead.com/npc=34605/swarm-scarab  (maxlevel is already 80)
+UPDATE `creature_template` SET `minlevel`=80 WHERE `entry` IN 
+(34605, 34650, 35658, 35659);
+
