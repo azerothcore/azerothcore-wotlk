fd58860b40811044c3a587e269d06e410d722c2d
Knindza <knindzagxg@gmail.com>
2020-10-02 16:39:50 +0200

    Update rev_1601396210900255400.sql

--- data/sql/updates/pending_db_world/rev_1601396210900255400.sql
+++ data/sql/updates/pending_db_world/rev_1601396210900255400.sql
@@ -7,7 +7,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601396210900255400');
 
 /* SMARTSCRIPT */
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 15720;
-UPDATE `creature_template` SET `unit_class` = '8' WHERE `entry` = 15720;
+UPDATE `creature_template` SET `unit_class` = 8 WHERE `entry` = 15720;
 
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 15720);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
