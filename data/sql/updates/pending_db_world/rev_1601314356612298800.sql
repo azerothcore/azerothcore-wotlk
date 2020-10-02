7fdb88159fd5f084ae34f82d3f84d43b51721e92
Knindza <knindzagxg@gmail.com>
2020-10-02 16:39:06 +0200

    Update rev_1601314356612298800.sql

--- data/sql/updates/pending_db_world/rev_1601314356612298800.sql
+++ data/sql/updates/pending_db_world/rev_1601314356612298800.sql
@@ -12,7 +12,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (17878, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 7978, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Scourge Siege Engineer - On Aggro - Cast \'7978\'');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 15162;
-UPDATE `creature_template` SET `unit_class`= '2' WHERE `entry`=15162;
+UPDATE `creature_template` SET `unit_class`= 2 WHERE `entry`= 15162;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 15162);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (15162, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 20294, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Inquisitor - On Aggro - Cast \'20294\'');
