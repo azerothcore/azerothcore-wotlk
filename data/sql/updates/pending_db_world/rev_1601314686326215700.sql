c5a11f01b7e8640d9af46e399cebbd3f8328bb94
Knindza <knindzagxg@gmail.com>
2020-10-03 18:52:06 +0200

    Update rev_1601314686326215700.sql

--- data/sql/updates/pending_db_world/rev_1601314686326215700.sql
+++ data/sql/updates/pending_db_world/rev_1601314686326215700.sql
@@ -40,7 +40,7 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 11613);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (11613, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 12, 11614, 1, 30000, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Huntsman Radley - On Aggro - Summon Creature \'Bloodshot\''),
 (11613, 0, 1, 0, 0, 0, 100, 0, 2700, 3900, 9700, 12400, 11, 11976, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Huntsman Radley - In Combat - Cast \'11976\''),
-(11613, 0, 2, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 14443, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Huntsman Radley - Between 20-80% Health - Cast \'14443\' (No Repeat)');
+(11613, 0, 2, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 14443, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Huntsman Radley - Between 20-80% Health - Cast \'14443\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 11614;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 11614);
@@ -58,17 +58,17 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10608;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 10608);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(10608, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 11, 13864, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Priest - On Respawn - Cast \'13864\''),
+(10608, 0, 0, 0, 1, 0, 100, 0, 0, 0, 0, 0, 11, 13864, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Priest - Out of Combat - Cast \'13864\''),
 (10608, 0, 1, 0, 0, 0, 100, 0, 2300, 5200, 8600, 9300, 11, 15498, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Priest - In Combat - Cast \'15498\''),
 (10608, 0, 2, 0, 2, 0, 100, 1, 60, 90, 0, 0, 11, 8362, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Priest - Between 60-90% Health - Cast \'8362\' (No Repeat)'),
 (10608, 0, 3, 0, 2, 0, 100, 1, 5, 25, 0, 0, 11, 12039, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Priest - Between 5-25% Health - Cast \'12039\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 11611;
+UPDATE `creature_addon` SET `auras`= 13008 WHERE `guid`=45432;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 11611);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(11611, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 13008, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Cavalier Durgen - On Aggro - Cast \'13008\''),
-(11611, 0, 1, 0, 0, 0, 100, 0, 2300, 3100, 8900, 9600, 11, 13953, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Cavalier Durgen - In Combat - Cast \'13953\''),
-(11611, 0, 2, 0, 2, 0, 100, 1, 10, 30, 0, 0, 11, 13005, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Cavalier Durgen - Between 10-30% Health - Cast \'13005\' (No Repeat)');
+(11611, 0, 0, 0, 0, 0, 100, 0, 2300, 3100, 8900, 9600, 11, 13953, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Cavalier Durgen - In Combat - Cast \'13953\''),
+(11611, 0, 1, 0, 2, 0, 100, 1, 10, 30, 0, 0, 11, 13005, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Cavalier Durgen - Between 10-30% Health - Cast \'13005\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 1884;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 1884);
@@ -85,7 +85,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10605;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 10605);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(10605, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 11, 17175, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Medic - On Respawn - Cast \'17175\''),
+(10605, 0, 0, 0, 1, 0, 100, 0, 0, 0, 0, 0, 11, 17175, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Medic - Out of Combat - Cast \'17175\''),
 (10605, 0, 1, 0, 2, 0, 100, 1, 5, 95, 0, 0, 11, 11640, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Medic - Between 5-95% Health - Cast \'11640\' (No Repeat)'),
 (10605, 0, 2, 0, 2, 0, 100, 1, 5, 20, 0, 0, 11, 17137, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Medic - Between 5-20% Health - Cast \'17137\' (No Repeat)');
 
