2092adfd03c7fc35e39ce14ac5c464838ba9eb51
Silker <61223313+Si1ker@users.noreply.github.com>
2020-10-04 22:46:17 -0600

    Add AC credits

--- data/sql/updates/pending_db_world/rev_1601361978416709800.sql
+++ data/sql/updates/pending_db_world/rev_1601361978416709800.sql
@@ -1,5 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601361978416709800');
 
+/*
+ * Update by Silker | <www.azerothcore.org> | Copyright (C)
+*/
+
 DELETE FROM `creature_loot_template` WHERE `entry` IN (31702, 32297) AND `item` = 44563;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
 (32297, 44563, 0, 1, 0, 1, 0, 1, 1), -- Cult Researcher: https://db.rising-gods.de/?item=44563#dropped-by
