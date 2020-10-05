9033ca0c0b2559a0d36e4301d1ce6aeeadc622d1
Si1ker <55638679+Sombranator@users.noreply.github.com>
2020-10-04 23:11:22 -0600

    fix(DB/creature): Expedition base camp - NPC locations
    This change will fix the spawn locations for the three npcs around the Dalaran portal at the expedition base camp at Ulduar

---
+++ data/sql/updates/pending_db_world/rev_1601874501115309800.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601874501115309800');
+
+/*
+ * Update by Silker | <www.azerothcore.org> | Copyright (C)
+*/
+
+UPDATE `creature` SET `position_x`=-813.208, `position_y`=-203.94, `position_z`=429.84 WHERE  `guid`=136552; -- Kirin Tor Mage
+UPDATE `creature` SET `position_x`=-813.208, `position_z`=429.842 WHERE  `guid`=136523; -- Kirin Tor Battle-Mage
+UPDATE `creature` SET `position_x`=-813.208, `position_z`=429.842 WHERE  `guid`=136553;  -- Kirin Tor Mage
+
