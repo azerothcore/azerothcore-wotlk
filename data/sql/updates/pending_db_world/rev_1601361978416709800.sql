86e74a98063234dcb926d21444ea97f7e40dd380
Silker <61223313+Si1ker@users.noreply.github.com>
2020-10-05 07:24:48 -0600

    Change rate values and add new reference link

--- data/sql/updates/pending_db_world/rev_1601361978416709800.sql
+++ data/sql/updates/pending_db_world/rev_1601361978416709800.sql
@@ -4,8 +4,9 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601361978416709800');
  * Update by Silker | <www.azerothcore.org> | Copyright (C)
 */
 
+-- Reference: http://web.archive.org/web/20150630004928/https://www.wowhead.com/item=44563/pattern-fur-lining-arcane-resist
 DELETE FROM `creature_loot_template` WHERE `entry` IN (31702, 32297) AND `item` = 44563;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES 
-(32297, 44563, 0, 1, 0, 1, 0, 1, 1), -- Cult Researcher: https://db.rising-gods.de/?item=44563#dropped-by
-(31702, 44563, 0, 1, 0, 1, 0, 1, 1); -- Frostbrood Spawn: https://www.wowhead.com/item=44563/pattern-fur-lining-arcane-resist
+(32297, 44563, 0, 0.19, 0, 1, 0, 1, 1),
+(31702, 44563, 0, 0.06, 0, 1, 0, 1, 1);
 
