261002c5fab30798005283c23c01195e4c270a13
Knindza <knindzagxg@gmail.com>
2020-10-12 21:09:21 +0200

    Update rev_1601827407065319600.sql

--- data/sql/updates/pending_db_world/rev_1601827407065319600.sql
+++ data/sql/updates/pending_db_world/rev_1601827407065319600.sql
@@ -8,7 +8,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601827407065319600');
 /* REGULAR */
 UPDATE `creature_template` SET `mindmg` = 4293, `maxdmg` = 6560, `DamageModifier` = 1.03 WHERE `entry` = 12129;
 UPDATE `creature_template` SET `mindmg` = 7708, `maxdmg` = 9905, `DamageModifier` = 1.03 WHERE `entry` = 36572;
-UPDATE `creature_template` SET `mindmg` = 776, `maxdmg` = 912, `DamageModifier` = 1.03 WHERE `entry` = 12262;
+UPDATE `creature_template` SET `mindmg` = 776, `maxdmg` = 912, `DamageModifier` = 1.03 WHERE `entry` = 11262;
 UPDATE `creature_template` SET `mindmg` = 824, `maxdmg` = 1286, `DamageModifier` = 1.03 WHERE `entry` = 36566;
 
 /* BOSS */ 
