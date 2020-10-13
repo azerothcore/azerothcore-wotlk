f1f5b207935ed82fd17288623b6c41ed616f5baa
Knindza <knindzagxg@gmail.com>
2020-10-13 17:42:04 +0200

    Update rev_1601826746153871500.sql

--- data/sql/updates/pending_db_world/rev_1601826746153871500.sql
+++ data/sql/updates/pending_db_world/rev_1601826746153871500.sql
@@ -186,7 +186,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (1662, 0, 0, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 3391, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Captain Perrine - Between 20-80% Health - Cast \'3391\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 1536;
-UPDATE `creature_template` SET `unit_class`= 2 WHERE `entry`=1536;
+UPDATE `creature_template` SET `unit_class`= 2 WHERE `entry`= 1536;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 1536);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (1536, 0, 0, 0, 0, 0, 100, 0, 1700, 2100, 8400, 8800, 11, 9053, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Missionary - In Combat - Cast \'9053\'');
