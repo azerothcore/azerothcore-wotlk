bd09cea7a359ff50cf1683f7f6279e9b56c018c4
Pagani Walter <paganiwalter@gmail.com>
2020-10-14 10:17:18 -0300

    (DB/Gossip) Gossip Spanish corrected

---
+++ data/sql/updates/pending_db_world/rev_1602681411978701400.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1602681411978701400');
+
+UPDATE `broadcast_text_locale` SET `MaleText`='Cazador', `FemaleText`='Cazadora' WHERE `ID`=50546 AND `locale` IN ('esES', 'esMX');
