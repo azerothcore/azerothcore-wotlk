c5df114454dac6d38931f44d5a2f6d9001082e89
AzerothCoreBot <azerothcorebot@gmail.com>
2020-10-15 08:08:25 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): f25e61c03dfa5394ab75ac654b99979b5ae8458a

--- data/sql/updates/pending_db_world/rev_1599859218307990300.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1599859218307990300');
-
-DELETE FROM `spell_custom_attr` WHERE `spell_id` = 38318;
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
-(38318, 33554432|67108864);
