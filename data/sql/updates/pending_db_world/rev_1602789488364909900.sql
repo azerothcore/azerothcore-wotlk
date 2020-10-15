a741fb6ec707380459923b9c9a053c86458de2dc
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-10-15 21:18:25 +0200

    fix(DB/Creature): Shandy Glossgleam

---
+++ data/sql/updates/pending_db_world/rev_1602789488364909900.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1602789488364909900');
+
+UPDATE `creature_text` SET `text` = 'Nice. I don\'t want to know what that stain was.' WHERE `CreatureID` = 36856 AND `groupid` = 1 AND `id` = 3;
