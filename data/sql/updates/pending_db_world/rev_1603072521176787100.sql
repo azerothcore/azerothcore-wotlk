88e026d637449515bb83996c28835423f955244d
Si1ker <55638679+Sombranator@users.noreply.github.com>
2020-10-18 19:59:59 -0600

    fix(DB/creature_template): Ulduar - Overload
    This change will fix the visual overload npc at the Iron assembly encounter

---
+++ data/sql/updates/pending_db_world/rev_1603072521176787100.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603072521176787100');
+
+/*
+ * Update by Silker | <www.azerothcore.org> | Copyright (C)
+*/
+
+UPDATE `creature_template` SET `unit_flags`=33554432, `AIName`='PassiveAI' WHERE  `entry` IN
+(32866, 33690)
+
