be95fb8a9956c40966e176b4afb278aa245a4ed2
Silker <61223313+Si1ker@users.noreply.github.com>
2020-10-19 17:35:35 -0600

    remove override

--- data/sql/updates/pending_db_world/rev_1603072521176787100.sql
+++ data/sql/updates/pending_db_world/rev_1603072521176787100.sql
@@ -4,6 +4,6 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603072521176787100');
  * Update by Silker | <www.azerothcore.org> | Copyright (C)
 */
 
-UPDATE `creature_template` SET `unit_flags`=33554432, `AIName`='PassiveAI' WHERE  `entry` IN
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`|33554432, `AIName`='PassiveAI' WHERE  `entry` IN
 (32866, 33690)
 
