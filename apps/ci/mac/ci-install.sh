d941f00b64c0928503ab8f825a550603368a58c9
locus313 <pat@lo5t.com>
2020-10-20 22:47:45 -0700

    wip

--- apps/ci/mac/ci-install.sh
+++ apps/ci/mac/ci-install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+time brew update
+
 # workaround to fix mac build from failing
 time brew uninstall openssl@1.0.2t
 time brew uninstall python@2.7.17
 time brew untap local/openssl
 time brew untap local/python2
 
-time brew update
 time brew install openssl readline ace coreutils bash bash-completion mysql ccache
 
 # workaround to fix mac build from failing
