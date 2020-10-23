abf46c4fe796ea05c5203c59047caa34b9eb5274
Degen <61268368+aradep@users.noreply.github.com>
2020-10-23 08:30:51 -0300

    Update rev_1603401587698987500.sql

--- data/sql/updates/pending_db_world/rev_1603401587698987500.sql
+++ data/sql/updates/pending_db_world/rev_1603401587698987500.sql
@@ -1,7 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603401587698987500');
 
 -- Ulduar Colossus bridge pathing fix --
-REPLACE INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`)  VALUES 
+DELETE FROM `waypoint_data` WHERE `id` = '1374780';
+INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`)  VALUES 
 ('1374780', '1', '71.3266', '-170.786', '410.72'),
 ('1374780', '2', '71.3266', '-212.300', '418.99'),
 ('1374780', '3', '71.3266', '-232.054', '418.99'),
