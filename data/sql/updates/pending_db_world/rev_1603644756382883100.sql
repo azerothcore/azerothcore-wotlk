a43aee0ab99d7e24d07aee421393779557c576ba
Knindza <knindzagxg@gmail.com>
2020-10-25 19:47:46 +0100

    Update rev_1603644756382883100.sql

--- data/sql/updates/pending_db_world/rev_1603644756382883100.sql
+++ data/sql/updates/pending_db_world/rev_1603644756382883100.sql
@@ -16,7 +16,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 6412;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 6412);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(6412, 0, 0, 0, 7, 0, 100, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Skeleton - On Evade - Despawn In 500 ms');
+(6412, 0, 0, 0, 1, 0, 100, 1, 1000, 1500, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Skeleton - Out of Combat - Despawn In 5000 ms (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 1850;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 1850);
