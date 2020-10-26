b1ce4ed85e38638dab2a2d70a04e0c8783c2213c
Knindza <knindzagxg@gmail.com>
2020-10-26 16:55:46 +0100

    Update rev_1603643078632991300.sql

--- data/sql/updates/pending_db_world/rev_1603643078632991300.sql
+++ data/sql/updates/pending_db_world/rev_1603643078632991300.sql
@@ -49,7 +49,7 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 5974);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (5974, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 11960, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Dreadmaul Ogre - On Aggro - Cast \'11960\' (No Repeat)'),
 (5974, 0, 1, 0, 0, 0, 100, 0, 2700, 3200, 8700, 11200, 11, 20793, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Dreadmaul Ogre - In Combat - Cast \'20793\''),
-(5974, 0, 2, 0, 2, 0, 100, 1, 40, 10, 0, 0, 11, 6742, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Dreadmaul Ogre - Between 40-10% Health - Cast \'6742\' (No Repeat)');
+(5974, 0, 2, 0, 2, 0, 100, 1, 10, 40, 0, 0, 11, 6742, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Dreadmaul Ogre - Between 10-40% Health - Cast \'6742\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 751;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 751);
