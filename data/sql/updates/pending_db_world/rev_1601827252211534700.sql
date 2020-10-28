35c55df08902fcaa7195ae51e6a27bd90d879de7
Knindza <knindzagxg@gmail.com>
2020-10-28 18:15:59 +0100

    Update rev_1601827252211534700.sql

--- data/sql/updates/pending_db_world/rev_1601827252211534700.sql
+++ data/sql/updates/pending_db_world/rev_1601827252211534700.sql
@@ -6,11 +6,11 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601827252211534700');
 */
 
 /* REGULAR */ 
-UPDATE `creature_template` SET `mindmg` = 4125, `maxdmg` = 6362, `DamageModifier` = 1.03 WHERE `entry` = 30245;
-UPDATE `creature_template` SET `mindmg` = 8250, `maxdmg` = 12724, `DamageModifier` = 1.03 WHERE `entry` = 31750;
-UPDATE `creature_template` SET `mindmg` = 3502, `maxdmg` = 5614, `DamageModifier` = 1.03 WHERE `entry` = 30249;
-UPDATE `creature_template` SET `mindmg` = 7004, `maxdmg` = 11228, `DamageModifier` = 1.03 WHERE `entry` = 31751;
+UPDATE `creature_template` SET `mindmg` = 591, `maxdmg` = 908, `DamageModifier` = 7 WHERE `entry` = 30245;
+UPDATE `creature_template` SET `mindmg` = 591, `maxdmg` = 908, `DamageModifier` = 14 WHERE `entry` = 31750;
+UPDATE `creature_template` SET `mindmg` = 502, `maxdmg` = 802, `DamageModifier` = 7 WHERE `entry` = 30249;
+UPDATE `creature_template` SET `mindmg` = 502, `maxdmg` = 802, `DamageModifier` = 14 WHERE `entry` = 31751;
 
 /* BOSS */ 
-UPDATE `creature_template` SET `mindmg` = 18815, `maxdmg` = 24905, `DamageModifier` = 1.01 WHERE `entry` = 28859;
-UPDATE `creature_template` SET `mindmg` = 37630, `maxdmg` = 49810, `DamageModifier` = 1.01 WHERE `entry` = 31734;
+UPDATE `creature_template` SET `mindmg` = 537, `maxdmg` = 712, `DamageModifier` = 35 WHERE `entry` = 28859;
+UPDATE `creature_template` SET `mindmg` = 537, `maxdmg` = 712, `DamageModifier` = 70 WHERE `entry` = 31734;
