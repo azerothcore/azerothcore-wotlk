6ce39703a00f82b02659c4999898fb9363e8a8dd
AzerothCoreBot <azerothcorebot@gmail.com>
2020-10-28 15:10:43 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 9dbfd3bcba352ab4b532d59e4324be35475538e4

--- data/sql/updates/pending_db_world/rev_1601277180341917800.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601277180341917800');
-
-/*
- * Update by Silker | <www.azerothcore.org> | Copyright (C)
-*/
-
-UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|2|64|4096|8192|131072|524288 WHERE `entry` IN 
-(34607, 34648, 35655, 35656);
-
