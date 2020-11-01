30c12bd614e6389a94e99fef3a4c37f62c961e0d
Silker <61223313+Si1ker@users.noreply.github.com>
2020-11-01 12:23:01 -0600

    Replace " " for ' ' on trigger_periodic

--- data/sql/updates/pending_db_world/rev_1604182926047452600.sql
+++ data/sql/updates/pending_db_world/rev_1604182926047452600.sql
@@ -6,5 +6,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604182926047452600');
 
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`|33554432 WHERE  `entry` IN
 (30641, 31521);
-UPDATE `creature_template` SET `ScriptName` = "trigger_periodic" WHERE `entry`= 30641;
+UPDATE `creature_template` SET `ScriptName` = 'trigger_periodic' WHERE `entry`= 30641;
 
