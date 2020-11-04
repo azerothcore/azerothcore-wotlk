9a9308afd16a291e4c88b53c1bc2852223682c19
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-04 16:30:40 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 473013d21fa1f316b56bbd9eb0cd12601d0a7fd3

--- data/sql/updates/pending_db_world/rev_1603072521176787100.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603072521176787100');
-
-/*
- * Update by Silker | <www.azerothcore.org> | Copyright (C)
-*/
-
-UPDATE `creature_template` SET `AIName`='PassiveAI' WHERE `entry`=32866;
-UPDATE `creature_template` SET `unit_flags`=`unit_flags`|33554432 WHERE  `entry` IN
-(32866, 33690);
