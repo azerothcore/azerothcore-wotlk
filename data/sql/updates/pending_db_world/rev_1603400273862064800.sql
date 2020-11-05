6973a66716eaac4ebcbc0a99345a24c4e9c83b1a
Akimbo <66260220+Akimboss@users.noreply.github.com>
2020-11-05 18:58:00 +0100

    fix(DB/creature) Light's Hope Muradin/Saurfang Wrong Models (#3600)

---
+++ data/sql/updates/pending_db_world/rev_1603400273862064800.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603400273862064800');
+UPDATE `creature` SET `id` = 32315, `modelid` = 0 WHERE `guid` = 200984;
+UPDATE `creature_template` SET `modelid1` = 30583, `modelid2` = 27905 WHERE `entry` = 32315;
