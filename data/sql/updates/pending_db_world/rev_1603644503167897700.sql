879777685b1d114ca688041d65afcc84f1bc0b9c
Knindza <knindzagxg@gmail.com>
2020-11-06 16:53:08 +0100

    Update rev_1603644503167897700.sql

--- data/sql/updates/pending_db_world/rev_1603644503167897700.sql
+++ data/sql/updates/pending_db_world/rev_1603644503167897700.sql
@@ -14,7 +14,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 2929;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 2929);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(2929, 0, 0, 0, 0, 0, 100, 0, 3200, 3900, 9200, 9800, 11, 7938, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Savage Owlbeast - In Combat - Cast \'7938\'');
+(2929, 0, 0, 0, 0, 0, 100, 0, 3200, 3900, 9200, 9800, 11, 54663, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Savage Owlbeast - In Combat - Cast \'54663\'');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 2505;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 2505);
