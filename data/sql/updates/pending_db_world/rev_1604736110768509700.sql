0ec676f5c936becf18a50ccc65451ae9dac08d4b
aradep <61268368+aradep@users.noreply.github.com>
2020-11-07 05:40:21 -0400

    Update rev_1604736110768509700.sql

--- data/sql/updates/pending_db_world/rev_1604736110768509700.sql
+++ data/sql/updates/pending_db_world/rev_1604736110768509700.sql
@@ -2,8 +2,9 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604736110768509700');
 
 /* Raise MinCount to match MaxCount for [Emblem of Triumph] in boss loot. */
 
-UPDATE `creature_loot_template` SET `MinCount`=2 WHERE `Item`=47241 AND `Entry`= IN
+UPDATE `creature_loot_template` SET `MinCount`=2 WHERE `MinCount`=1 AND `MaxCount`=2 AND `Item`=47241 AND `Entry` IN
 (
+15989, /* Sapphiron 10-man */
 15990, /* Kel'thuzad 10-man */
 33885, /* XT-002 Deconstructor 25-man */
 34175, /* Auriaya 25-man */
