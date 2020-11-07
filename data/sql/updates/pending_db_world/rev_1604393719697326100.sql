adb13e2176a15d54574fd86da8e7922fbe18f7bb
Silker <61223313+Si1ker@users.noreply.github.com>
2020-11-07 10:27:26 -0600

    fix(DB/spelldifficulty_dbc): Fix Disrupting shout [10/25] (#3639)

---
+++ data/sql/updates/pending_db_world/rev_1604393719697326100.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604393719697326100');
+
+/*
+ * Update by Silker | <www.azerothcore.org> | Copyright (C)
+*/
+
+UPDATE `spelldifficulty_dbc` SET `DifficultySpellID_1`=55543, `DifficultySpellID_2`=29107 WHERE `ID`=29107;
+
