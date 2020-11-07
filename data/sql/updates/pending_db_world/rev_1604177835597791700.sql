6e7fac81cf1dfc71d3ed0dcf93c303ce2ef8d646
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-07 18:50:11 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 393fb2cf78594644d167ae00e3356ef79ed211c2

--- data/sql/updates/pending_db_world/rev_1604177835597791700.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604177835597791700');
-
-/*
- * Update by Silker | <www.azerothcore.org> | Copyright (C)
-*/
-
-UPDATE `creature_template` SET `minlevel`=80, `maxlevel`=80 WHERE `entry`=16697;
-
