e62de0706e3b4c80b5de45151f2c9835d56cc636
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-11-07 10:20:50 +0100

    fix(DB/creature) Rachelle Gothena Position

---
+++ data/sql/updates/pending_db_world/rev_1604740702289748200.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604740702289748200');
+UPDATE `creature`SET `position_x` = 1656.397705, `position_y` = -4434.079102, `position_z` = 17.744991 WHERE `guid` = 86234;
