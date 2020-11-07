393fb2cf78594644d167ae00e3356ef79ed211c2
Silker <61223313+Si1ker@users.noreply.github.com>
2020-11-07 12:49:08 -0600

    fix(DB/creature_template): Void Zone from Lady Blaumeux (#3637)

---
+++ data/sql/updates/pending_db_world/rev_1604177835597791700.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604177835597791700');
+
+/*
+ * Update by Silker | <www.azerothcore.org> | Copyright (C)
+*/
+
+UPDATE `creature_template` SET `minlevel`=80, `maxlevel`=80 WHERE `entry`=16697;
+
