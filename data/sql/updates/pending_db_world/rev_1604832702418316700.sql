4d6052c54925a0e5a27dc56605459e88dac7b5a4
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-11-08 11:52:55 +0100

    fix(DB/creature) Frosthorn Ram Position

---
+++ data/sql/updates/pending_db_world/rev_1604832702418316700.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604832702418316700');
+UPDATE `creature` SET `position_x` = 2972.916748, `position_y` = -4553.171875, `position_z` = 292.601624 WHERE `guid` = 203163;
