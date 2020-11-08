fde79361741a8a9f3c5036cf85099c294389a5fa
Endeffect93 <55922592+Endeffect93@users.noreply.github.com>
2020-11-08 13:01:55 +0100

    Update rev_1604834556044808500.sql

--- data/sql/updates/pending_db_world/rev_1604834556044808500.sql
+++ data/sql/updates/pending_db_world/rev_1604834556044808500.sql
@@ -1,2 +1,2 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604834556044808500');
-UPDATE `quest_poi_points` SET X = 2023, Y = -4514 WHERE `QuestID` = 11314 AND `Idx1` = 0;
+UPDATE `quest_poi_points` SET X = 2025, Y = -4519 WHERE `QuestID` = 11314 AND `Idx1` = 0;
