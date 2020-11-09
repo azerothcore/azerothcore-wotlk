01ee50d50e93a62cdbaf84ff4217c2c970d69e44
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-09 16:06:03 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2204678c8fbb85a2cae1ff44d4e6c908e6c4f6e1

--- data/sql/updates/pending_db_world/rev_1601091401144068714.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601091401144068714');
-
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|0x00000080 WHERE `entry` IN (24202, 24203, 24204, 24205);
