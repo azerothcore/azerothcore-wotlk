b1042044c5795fbfddf61cb91378d6f95edb6e09
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-09 00:15:19 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 74f2d5f0f6ce90d4e19753cbba54b0ffe66dc248

--- data/sql/updates/pending_db_world/rev_1601869322546655200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601869322546655200');
-
-UPDATE `creature` SET `spawnMask` = 3 WHERE `id` = 18956;
-UPDATE `gameobject` SET `spawnMask` = 3 WHERE `id` = 183051;
