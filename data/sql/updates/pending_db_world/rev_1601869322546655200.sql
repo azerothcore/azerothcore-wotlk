62f244d933fa7ff4399b3d1adeb6ae07b9881e56
WiZZy <bilucristian25@gmail.com>
2020-11-09 02:14:19 +0200

    fix(DB/creature/gameobject): Brother against brother (#3547)

---
+++ data/sql/updates/pending_db_world/rev_1601869322546655200.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601869322546655200');
+
+UPDATE `creature` SET `spawnMask` = 3 WHERE `id` = 18956;
+UPDATE `gameobject` SET `spawnMask` = 3 WHERE `id` = 183051;
