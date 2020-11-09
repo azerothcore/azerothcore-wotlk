df33ea78f482754c40118e02f4f39b399cfcd9b3
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-09 20:38:42 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 1cccc8d4b5ca005adcadcc12142cb8fb5d658348

--- data/sql/updates/db_world/2020_11_09_04.sql
+++ data/sql/updates/db_world/2020_11_09_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_11_09_03 -> 2020_11_09_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_11_09_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_11_09_03 2020_11_09_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1603642724794965100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603642724794965100');
 /*
  * Zone: Duskwood
@@ -35,3 +51,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 923;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 923);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (923, 0, 0, 0, 2, 0, 100, 1, 1, 5, 0, 0, 11, 3149, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Young Black Ravager - Between 1-5% Health - Cast \'3149\' (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
