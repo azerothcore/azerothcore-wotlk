e79dd26412576538c0174fc52810e4e5be621c07
FALL1N1 <emiliyan9604bg@gmail.com>
2020-11-11 03:58:04 +0200

    add copyright, backport sql.. let the fun begin

--- src/server/scripts/EasternKingdoms/OperationGnomeregan/operation_gnomeregan.h
+++ src/server/scripts/EasternKingdoms/OperationGnomeregan/operation_gnomeregan.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
  * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2011 gmlt.A
  *
