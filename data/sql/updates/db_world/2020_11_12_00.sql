f6a926d3122306d1e0b307ce891c2efb07e06df7
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-12 01:27:07 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d9b24bf1963ba80908563cbe32d9d67f32827bc1

--- data/sql/updates/db_world/2020_11_12_00.sql
+++ data/sql/updates/db_world/2020_11_12_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_11_11_02 -> 2020_11_12_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_11_11_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_11_11_02 2020_11_12_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1603643161575152300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1603643161575152300');
 /*
  * Zone: Stranglethorn Vale II
@@ -187,3 +203,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 684;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 684);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (684, 0, 0, 0, 1, 0, 100, 1, 1000, 1500, 0, 0, 11, 22766, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Shadowmaw Panther - Out of Combat - Cast \'22766\' (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
