b8fe69a0b945490ba4b1b57354ef429c830593e3
peti446 <peti446@users.noreply.github.com>
2020-11-15 14:27:25 +0000

    feat(CORE/Config): Added Pet experience rate to config

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -502,6 +502,8 @@ void World::LoadConfigSettings(bool reload)
     rate_values[RATE_XP_BG_KILL]                  = sConfigMgr->GetFloatDefault("Rate.XP.BattlegroundKill", 1.0f);
     rate_values[RATE_XP_QUEST]                    = sConfigMgr->GetFloatDefault("Rate.XP.Quest", 1.0f);
     rate_values[RATE_XP_EXPLORE]                  = sConfigMgr->GetFloatDefault("Rate.XP.Explore", 1.0f);
+    rate_values[RATE_XP_PET]                      = sConfigMgr->GetFloatDefault("Rate.XP.Pet", 1.0f);
+    rate_values[RATE_XP_PET_NEXT_LEVEL]           = sConfigMgr->GetFloatDefault("Rate.Pet.LevelXP", 0.05f);
     rate_values[RATE_REPAIRCOST]                  = sConfigMgr->GetFloatDefault("Rate.RepairCost", 1.0f);
 
     rate_values[RATE_SELLVALUE_ITEM_POOR]         = sConfigMgr->GetFloatDefault("Rate.SellValue.Item.Poor", 1.0f);
