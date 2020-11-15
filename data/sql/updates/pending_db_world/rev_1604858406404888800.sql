0fae729553c70ca7e15b1f8e7c69b4dcceee3a98
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2020-11-15 17:10:39 +0100

    fix(DB/creature): Rabid Brown Bear Position (#3667)

---
+++ data/sql/updates/pending_db_world/rev_1604858406404888800.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604858406404888800');
+UPDATE `creature` SET `position_x` = -14.866208, `position_y` = -3560.556641, `position_z` = 34.651653 WHERE `guid` = 101032;
