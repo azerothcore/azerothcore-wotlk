f5be2eb688334d6db7efa14832f80be13177e971
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-16 15:21:58 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 9a8aa6152657fde5f297f6251ff9237f6d399e15

--- data/sql/updates/db_world/2020_11_16_01.sql
+++ data/sql/updates/db_world/2020_11_16_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_11_16_00 -> 2020_11_16_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_11_16_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_11_16_00 2020_11_16_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1605209372908686400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605209372908686400');
 
 -- ---------------- --
@@ -78,3 +94,12 @@ INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `positio
 (847236, 9, 5737.76, 3273.59, 299.117, 0, 0, 0, 0, 100, 0),
 (847236, 10, 5723.77, 3278.65, 299.138, 0, 0, 0, 0, 100, 0);
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
