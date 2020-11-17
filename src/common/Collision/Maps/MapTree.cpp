7f26c9ddaddb99fb47c6738c997e2fd2dbd876e4
Kargatum <dowlandtop@yandex.com>
2020-11-17 16:59:58 +0700

    refactor(Core/Common): restyle tools with astyle (#3706)

--- src/common/Collision/Maps/MapTree.cpp
+++ src/common/Collision/Maps/MapTree.cpp
@@ -220,7 +220,7 @@ namespace VMAP
         {
             height = pPos.z - maxDist;
         }
-        return(height);
+        return (height);
     }
 
     //=========================================================
