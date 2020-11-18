ed451d5c23375932a19da6b86613e0fd7f152297
aradep <61268368+aradep@users.noreply.github.com>
2020-11-18 03:52:27 -0400

    locale

--- data/sql/updates/pending_db_world/rev_1605661689213920900.sql
+++ data/sql/updates/pending_db_world/rev_1605661689213920900.sql
@@ -3,3 +3,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605661689213920900');
 /* Copy FemaleText into missing MaleText for Computer NPC in Mimiron fight, Ulduar. */
 
 UPDATE `broadcast_text` SET `MaleText`=`FemaleText` WHERE `ID` IN (34284,34283,34282,34281,34280,34279,34278,34277,34276,34275,34274,34273,34268);
+UPDATE `broadcast_text_locale` SET `MaleText`=`FemaleText` WHERE `ID` IN (34284,34283,34282,34281,34280,34279,34278,34277,34276,34275,34274,34273,34268);
