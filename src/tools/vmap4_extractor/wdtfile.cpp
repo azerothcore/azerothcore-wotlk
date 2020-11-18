85927685f2a06e4dc743176f3b912d6c060e2eb2
Kargatum <dowlandtop@yandex.com>
2020-11-18 09:49:56 +0700

    refactor(Tools): restyle tools with astyle (#3705)

--- src/tools/vmap4_extractor/wdtfile.cpp
+++ src/tools/vmap4_extractor/wdtfile.cpp
@@ -13,7 +13,7 @@ char* wdtGetPlainName(char* FileName)
 {
     char* szTemp;
 
-    if((szTemp = strrchr(FileName, '\\')) != nullptr)
+    if ((szTemp = strrchr(FileName, '\\')) != nullptr)
         FileName = szTemp + 1;
     return FileName;
 }
@@ -37,7 +37,7 @@ bool WDTFile::init(char* /*map_id*/, unsigned int mapID)
     std::string dirname = std::string(szWorkDirWmo) + "/dir_bin";
     FILE* dirfile;
     dirfile = fopen(dirname.c_str(), "ab");
-    if(!dirfile)
+    if (!dirfile)
     {
         printf("Can't open dirfile!'%s'\n", dirname.c_str());
         return false;
@@ -108,7 +108,7 @@ WDTFile::~WDTFile(void)
 
 ADTFile* WDTFile::GetMap(int x, int z)
 {
-    if(!(x >= 0 && z >= 0 && x < 64 && z < 64))
+    if (!(x >= 0 && z >= 0 && x < 64 && z < 64))
         return nullptr;
 
     char name[512];
