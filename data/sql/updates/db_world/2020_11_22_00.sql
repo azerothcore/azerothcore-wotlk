5714058dd10ca8edded8dde0447ebd3e2253213e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-22 05:26:07 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 8d59610cd76bb77698f8da41b8198eaddd7e5e91

--- data/sql/updates/db_world/2020_11_22_00.sql
+++ data/sql/updates/db_world/2020_11_22_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_11_21_00 -> 2020_11_22_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_11_21_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_11_21_00 2020_11_22_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1592648244203077800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1592648244203077800');
 
 ALTER TABLE `player_factionchange_titles`
@@ -28,3 +44,12 @@ INSERT INTO `player_factionchange_titles` (`alliance_id`, `alliance_comment`, `h
 (147, "<name> of Darnassus", 154, "<name> of the Undercity"),
 (148, "<name> of Ironforge", 151, "<name> of Sen'jin"),
 (149, "<name> of Stormwind", 150, "<name> of Orgrimmar");
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
