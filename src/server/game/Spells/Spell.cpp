65114e7968ae9400c8bea050c2394f0e302c56b5
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-11-22 11:36:16 +0100

    Revert "Update SpellMgr.cpp"
    This reverts commit 61bc4d1f24a460f1ceb7ee96cc74e96a948d626a.

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -896,12 +896,11 @@ void Spell::SelectEffectImplicitTargets(SpellEffIndex effIndex, SpellImplicitTar
             for (uint32 j = effIndex + 1; j < MAX_SPELL_EFFECTS; ++j)
             {
                 SpellEffectInfo const* effects = GetSpellInfo()->Effects;
-                if (effects[j].IsEffect() &&
-                    effects[effIndex].TargetA.GetTarget() == effects[j].TargetA.GetTarget() &&
-                    effects[effIndex].TargetB.GetTarget() == effects[j].TargetB.GetTarget() &&
-                    effects[effIndex].ImplicitTargetConditions == effects[j].ImplicitTargetConditions &&
-                    effects[effIndex].CalcRadius(m_caster) == effects[j].CalcRadius(m_caster) &&
-                    CheckScriptEffectImplicitTargets(effIndex, j))
+                if (effects[effIndex].TargetA.GetTarget() == effects[j].TargetA.GetTarget() &&
+                        effects[effIndex].TargetB.GetTarget() == effects[j].TargetB.GetTarget() &&
+                        effects[effIndex].ImplicitTargetConditions == effects[j].ImplicitTargetConditions &&
+                        effects[effIndex].CalcRadius(m_caster) == effects[j].CalcRadius(m_caster) &&
+                        CheckScriptEffectImplicitTargets(effIndex, j))
                 {
                     effectMask |= 1 << j;
                 }
@@ -927,8 +926,9 @@ void Spell::SelectEffectImplicitTargets(SpellEffIndex effIndex, SpellImplicitTar
             SelectImplicitAreaTargets(effIndex, targetType, effectMask);
             break;
         case TARGET_SELECT_CATEGORY_TRAJ:
-            // just in case there is no dest, explanation in SelectImplicitDestDestTargets
-            CheckDst();
+            // xinef: just in case there is no dest, explanation in SelectImplicitDestDestTargets
+            if (!m_targets.HasDst())
+                m_targets.SetDst(*m_caster);
 
             SelectImplicitTrajTargets(effIndex, targetType);
             break;
@@ -949,18 +949,18 @@ void Spell::SelectEffectImplicitTargets(SpellEffIndex effIndex, SpellImplicitTar
                 case TARGET_OBJECT_TYPE_DEST:
                     switch (targetType.GetReferenceType())
                     {
-                    case TARGET_REFERENCE_TYPE_CASTER:
-                        SelectImplicitCasterDestTargets(effIndex, targetType);
-                        break;
-                    case TARGET_REFERENCE_TYPE_TARGET:
-                        SelectImplicitTargetDestTargets(effIndex, targetType);
-                        break;
-                    case TARGET_REFERENCE_TYPE_DEST:
-                        SelectImplicitDestDestTargets(effIndex, targetType);
-                        break;
-                    default:
-                        ASSERT(false && "Spell::SelectEffectImplicitTargets: received not implemented select target reference type for TARGET_TYPE_OBJECT_DEST");
-                        break;
+                        case TARGET_REFERENCE_TYPE_CASTER:
+                            SelectImplicitCasterDestTargets(effIndex, targetType);
+                            break;
+                        case TARGET_REFERENCE_TYPE_TARGET:
+                            SelectImplicitTargetDestTargets(effIndex, targetType);
+                            break;
+                        case TARGET_REFERENCE_TYPE_DEST:
+                            SelectImplicitDestDestTargets(effIndex, targetType);
+                            break;
+                        default:
+                            ASSERT(false && "Spell::SelectEffectImplicitTargets: received not implemented select target reference type for TARGET_TYPE_OBJECT_DEST");
+                            break;
                     }
                     break;
                 default:
