2e3c98df94842d3f28530a7321c4db66aa94a10c
Francesco Borzì <borzifrancesco@gmail.com>
2020-11-22 18:46:35 +0100

    fix(DB): failure due to missing semicolumn (#3746)

--- data/sql/updates/db_world/2020_11_22_01.sql
+++ data/sql/updates/db_world/2020_11_22_01.sql
@@ -22,7 +22,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605911267657207700');
 
 -- fix models
 UPDATE `creature_template` SET `modelid1`=23258, `modelid2`=0 WHERE `entry` IN
-(33395, 33402)
+(33395, 33402);
 
 
 --
