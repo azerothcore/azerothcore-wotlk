00f1cccd4e92e43e185c243b39df344edf57bbaa
Quickson <ilya.v.golubev@gmail.com>
2020-11-24 00:25:16 +0300

    code-review

--- data/sql/updates/pending_db_world/rev_1606162431492361200.sql
+++ data/sql/updates/pending_db_world/rev_1606162431492361200.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606162431492361200');
-DELETE creature, creature_addon, waypoint_data
+DELETE `creature`, `creature_addon`, `waypoint_data`
 FROM
-    creature, creature_addon, waypoint_data
+    `creature`, `creature_addon`, `waypoint_data`
 WHERE
-    creature.guid = 203340 AND creature.guid = creature_addon.guid AND creature_addon.path_id = waypoint_data.id;
+    `creature`.`guid` = 203340 AND `creature`.`guid` = `creature_addon`.`guid` AND `creature_addon`.`path_id` = `waypoint_data`.`id`;
