1da051571cecce2092445745dfd6c1a9a6c9652a
Quickson <ilya.v.golubev@gmail.com>
2020-11-24 00:03:31 +0300

    fix(DB/creature): delete duplicate Big Roy and his waypoints

--- data/sql/updates/pending_db_world/rev_1606162431492361200.sql
+++ data/sql/updates/pending_db_world/rev_1606162431492361200.sql
@@ -1,2 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606162431492361200');
-DELETE FROM creature WHERE guid = 203340;
+DELETE creature, creature_addon, waypoint_data
+FROM
+    creature, creature_addon, waypoint_data
+WHERE
+    creature.guid = 203340 AND creature.guid = creature_addon.guid AND creature_addon.path_id = waypoint_data.id;
