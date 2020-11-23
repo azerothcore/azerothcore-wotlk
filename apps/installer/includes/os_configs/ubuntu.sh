5703644bbba194a174c727648a65dbae87846b7f
FrancescoBorzi <borzifrancesco@gmail.com>
2020-11-23 23:01:46 +0100

    fix: ubuntu 18.04

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 if [[ $CONTINUOUS_INTEGRATION ]]; then
   sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl libgoogle-perftools-dev \
   libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev libace-dev mysql-client \
-  libncurses5-dev libboost-system1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev
+  libncurses5-dev libboost-all-dev
 else
   case $UBUNTU_VERSION in
     "14.04")
