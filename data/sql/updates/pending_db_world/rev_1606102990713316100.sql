40146c2527bb4079a150e80180866ee21fb83d43
Vox <ndinosaur1213@Live.com>
2020-11-23 17:37:13 -0500

    comment change

--- data/sql/updates/pending_db_world/rev_1606102990713316100.sql
+++ data/sql/updates/pending_db_world/rev_1606102990713316100.sql
@@ -12,7 +12,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 -- The Lich King Timed Actionlist Trigger
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 25462);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(25462, 0, 0, 0, 19, 0, 100, 0, 12593, 0, 0, 0, 80, 2546200, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'In Service of the Lich King - Quest Accept - Play Sound');
+(25462, 0, 0, 0, 19, 0, 100, 0, 12593, 0, 0, 0, 80, 2546200, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'In Service of the Lich King - Quest Accept - Timed Actionlist');
 
 -- 'In Service of The Lich King' quest details
 DELETE FROM `quest_details` WHERE `ID` = 12593;
