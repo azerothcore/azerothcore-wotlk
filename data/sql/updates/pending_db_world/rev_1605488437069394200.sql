3f7f41089c500bc5fed79dfeb5e2a2b3f97e7d32
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-24 16:19:36 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 46f4b9ed1341e687d04e7fd16e35481a74e44b89

--- data/sql/updates/pending_db_world/rev_1605488437069394200.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605488437069394200');
-
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 15998;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
-(17, 0, 15998, 0, 0, 29, 0, 10221, 3, 0, 0, 0, 0, '', 'Empty Worg Pup Cage requires Bloodaxe Worg Pup (3y)');
