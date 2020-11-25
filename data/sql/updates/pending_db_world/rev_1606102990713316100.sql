00e811b576b3da10a59a3512f30d11abf6c9e686
Vox <ndinosaur1213@Live.com>
2020-11-25 16:05:51 -0500

    improvements

--- data/sql/updates/pending_db_world/rev_1606102990713316100.sql
+++ data/sql/updates/pending_db_world/rev_1606102990713316100.sql
@@ -1,16 +1,14 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606102990713316100');
 
--- Imported from TrinityCore by Voxstrasza
-
 -- The Lich King Timed Actionlist
-DELETE FROM `smart_scripts` WHERE (source_type = 9 AND entryorguid = 25462 * 100);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 25462 * 100);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (25462 * 100, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 4, 14734, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'In Service of the Lich King - Quest Accept - Play Sound 14734'),
 (25462 * 100, 9, 1, 0, 0, 0, 100, 0, 21000, 21000, 0, 0, 4, 14735, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'In Service of the Lich King - Quest Accept - Play Sound 14735'),
 (25462 * 100, 9, 2, 0, 0, 0, 100, 0, 26000, 26000, 0, 0, 4, 14736, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'In Service of the Lich King - Quest Accept - Play Sound 14736');
 
 -- The Lich King Timed Actionlist Trigger
-DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 25462);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 25462);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (25462, 0, 0, 0, 19, 0, 100, 0, 12593, 0, 0, 0, 80, 2546200, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'In Service of the Lich King - Quest Accept - Timed Actionlist');
 
