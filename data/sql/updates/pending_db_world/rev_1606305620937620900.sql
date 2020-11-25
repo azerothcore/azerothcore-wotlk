b501e1690e3006dbc45ff8766563fcad7033d63b
Si1ker <55638679+Sombranator@users.noreply.github.com>
2020-11-25 06:41:19 -0600

    fix(DB/creature): Starting zones
    This change will update the guid position for human and night-elf initial NPCs
    
    Reference - Human: https://www.youtube.com/watch?v=nvPSKTsvObk
    Reference - Night elf: https://www.youtube.com/watch?v=0YhY4g-JEj4

---
+++ data/sql/updates/pending_db_world/rev_1606305620937620900.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606305620937620900');
+
+/*
+ * Update by Silker | <www.azerothcore.org> | Copyright (C)
+*/
+
+UPDATE `creature` SET `position_x`=10329, `position_y`=826.056, `position_z`=1326.37, `orientation`=2.72516 WHERE `guid`=46205; -- Ilthalaine
+UPDATE `creature` SET `position_x`=-8933.93, `position_y`=-137.472, `position_z`=83.2960, `orientation`=2.2803 WHERE  `guid`=79942; -- Deputy Willem
