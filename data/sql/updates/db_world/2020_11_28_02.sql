d8e210229a37d02bd1a852dcf099347bae653cc7
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-28 21:37:10 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 01fa6257c89745458dc1fbbd60009bc8e5e8883f

--- data/sql/updates/db_world/2020_11_28_02.sql
+++ data/sql/updates/db_world/2020_11_28_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_11_28_01 -> 2020_11_28_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_11_28_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_11_28_01 2020_11_28_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1604614793984918900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1604614793984918900');
 -- Pyrobuffet (57557)
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceGroup`=7 AND `SourceEntry`=57557 AND `SourceId`=0 AND `ElseGroup`=0 AND `ConditionTypeOrReference`=26 AND `ConditionTarget`=0 AND `ConditionValue1`=1 AND `ConditionValue2`=0 AND `ConditionValue3`=0;
@@ -14,3 +30,12 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=57620 AND `spell_effect`=-57935 AND `type`=0;
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 (57620, -57935, 0, 'Twilight Shift aura removes Twilight Torment');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
