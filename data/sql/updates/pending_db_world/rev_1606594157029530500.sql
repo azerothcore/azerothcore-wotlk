d7473e402061fd1c9f97c64d7c1e6e846137706c
Vox <ndinosaur1213@Live.com>
2020-11-28 16:26:04 -0500

    fix

--- data/sql/updates/pending_db_world/rev_1606594157029530500.sql
+++ data/sql/updates/pending_db_world/rev_1606594157029530500.sql
@@ -1,10 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606594157029530500');
 
--- Correct buffs incorrectly placed as 'Positive'
+-- Correct spell Algalon the Observer, 25 player 'Phase Punch' to be applied as negative buff
 
-DELETE FROM spell_custom_attr WHERE spell_id = 64412; -- Algalon the Observer, 25 player 'Phase Punch'
-DELETE FROM spell_custom_attr WHERE spell_id = 54836; -- Noth the Plaguebringer, 25 player 'Wrath of the Plaguebringer'
-DELETE FROM spell_custom_attr WHERE spell_id = 29214; -- Noth the Plaguebringer, 10 player 'Wrath of the Plaguebringer'
-DELETE FROM spell_custom_attr WHERE spell_id = 72998; -- Blood-Prince Council, Heroic mode, 'Shadow Prison'
-DELETE FROM spell_custom_attr WHERE spell_id = 11196; -- First Aid, 'Recently Bandaged'
+DELETE FROM spell_custom_attr WHERE spell_id = 64412;
 
