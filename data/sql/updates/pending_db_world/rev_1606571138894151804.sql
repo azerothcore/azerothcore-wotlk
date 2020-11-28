fdf626d98504d3b18d81d7c7d351e8e73cbdb53b
Helias <stefanoborzi32@gmail.com>
2020-11-28 14:46:18 +0100

    fix(db/npc): respawntime secs

---
+++ data/sql/updates/pending_db_world/rev_1606571138894151804.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606571138894151804');
+UPDATE `creature` SET `spawntimesecs` = 0 WHERE `id` IN (8901);
+UPDATE `creature` SET `spawntimesecs` = 300 WHERE `id` IN (9956);
+
