69b74109c894a6316ee0b914ca84ab9c7465c75b
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-28 14:23:48 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d4612c7ac6cbb80f972c1a46c29a088e44ef2c08

--- data/sql/updates/pending_db_world/rev_1605203808017925200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605203808017925200');
-UPDATE `creature_text` SET `Text`='This is our final stand. What happens here will echo through the ages. Regardless of outcome, they will know that we fought with honor. That we fought for the freedom and safety of our people!' WHERE  `CreatureID`=37119 AND `GroupID`=0 AND `ID`=0;
-UPDATE `broadcast_text` SET `MaleText`='This is our final stand. What happens here will echo through the ages. Regardless of outcome, they will know that we fought with honor. That we fought for the freedom and safety of our people!' WHERE  `ID`=36923;
-
