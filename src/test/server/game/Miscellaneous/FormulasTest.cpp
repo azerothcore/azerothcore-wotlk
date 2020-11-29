6ffe02bb014f7fa3deff3db5ce827b53b20c9c3a
FrancescoBorzi <borzifrancesco@gmail.com>
2020-11-29 11:04:30 +0100

    chore: work in progress

--- src/test/server/game/Miscellaneous/FormulasTest.cpp
+++ src/test/server/game/Miscellaneous/FormulasTest.cpp
@@ -1,13 +1,24 @@
 #include "gtest/gtest.h"
 #include "Formulas.h"
+#include "Log.h"
+#include "../../../mocks/LogMock.h"
+
+using namespace acore::Honor;
+using namespace acore::XP;
 
 TEST(FormulasTest, hk_honor_at_level)
 {
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(80), 124);
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(80, 2), 248);
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(80, 0.5), 62);
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(1), 2);
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(1, 10), 16);
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(2), 4);
-    EXPECT_EQ(acore::Honor::hk_honor_at_level(3), 5);
+    EXPECT_EQ(hk_honor_at_level(80), 124);
+    EXPECT_EQ(hk_honor_at_level(80, 2), 248);
+    EXPECT_EQ(hk_honor_at_level(80, 0.5), 62);
+    EXPECT_EQ(hk_honor_at_level(1), 2);
+    EXPECT_EQ(hk_honor_at_level(1, 10), 16);
+    EXPECT_EQ(hk_honor_at_level(2), 4);
+    EXPECT_EQ(hk_honor_at_level(3), 5);
+}
+
+TEST(FormulasTest, BaseGain)
+{
+    sLog.reset(new LogMock());
+    EXPECT_EQ(BaseGain(60, 1, CONTENT_1_60), 0);
 }
