4ffe22a6ac45949e2c7d4691cd0d48725ae202fa
Vox <ndinosaur1213@Live.com>
2020-11-28 19:22:48 -0500

    add lightning staff

--- data/sql/updates/pending_db_world/rev_1606604290694357400.sql
+++ data/sql/updates/pending_db_world/rev_1606604290694357400.sql
@@ -18,7 +18,7 @@ DELETE FROM `creature_addon` WHERE (`guid` IN (136610));
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `isLarge`, `auras`) VALUES
 (136610, 0, 0, 0, 1, 333, 0, NULL);
 
--- Update position, facing for Iron Mender (10 & 25 Ulduar)
+-- Update position, facing, and add item 'NPC Equip 39750' (lightning staff) for Iron Mender (10 & 25 Ulduar)
 
 DELETE FROM `creature` WHERE (`id` = 34198) AND (`guid` IN (136603));
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
@@ -27,6 +27,13 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `p
 DELETE FROM `creature` WHERE (`id` = 34198) AND (`guid` IN (136772));
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (136772, 34198, 603, 0, 0, 2, 1, 26218, 1, 1551.93571, -16.073572, 420.66736, 1.945428, 604800, 0, 0, 337025, 62535, 0, 0, 0, 0, '', 0);
+
+UPDATE `creature_template_addon` SET `bytes2` = 1 WHERE (`entry` = 34198);
+UPDATE `creature_equip_template` SET `ItemID1` = 39750 WHERE (`CreatureID` = 34198);
+UPDATE `creature_template_addon` SET `bytes2` = 1 WHERE (`entry` = 34236);
+DELETE FROM `creature_equip_template` WHERE (`CreatureID` = 34236);
+INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES
+(34236, 1, 39750, 0, 0, 0);
 
 -- Update position, facing for Hardened Iron Golem (10 & 25 Ulduar)
 
