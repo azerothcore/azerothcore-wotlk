19acb696783607f6ca32034f4e3882ecea4b6900
AzerothCoreBot <azerothcorebot@gmail.com>
2020-11-30 08:08:40 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): a4d0a90d903c66806ad129ac38f1505503432147

--- data/sql/updates/db_world/2020_11_30_00.sql
+++ data/sql/updates/db_world/2020_11_30_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_11_29_00 -> 2020_11_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_11_29_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_11_29_00 2020_11_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1605959888351409700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605959888351409700');
 
 -- item_set_names_locale
@@ -182,3 +198,12 @@ INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `Ver
 (11085, 'deDE', 'Ja, $N?', 18019),
 (12327, 'deDE', 'Ja, $N?', 18019),
 (12671, 'deDE', 'Ja, $N?', 18019);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
