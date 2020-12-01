81c544320bf606425c58db2eb8385c578185f4f9
Vox <ndinosaur1213@Live.com>
2020-11-30 20:51:51 -0500

    update

--- data/sql/updates/pending_db_world/rev_1606636935711383900.sql
+++ data/sql/updates/pending_db_world/rev_1606636935711383900.sql
@@ -2,6 +2,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606636935711383900');
 
 -- Apply 'Shadowform' Aura to Saronite Animus
 
-UPDATE `creature_template_addon` SET `auras` = '63359' WHERE (`entry` = 33524);
-UPDATE `creature_template_addon` SET `auras` = '63359' WHERE (`entry` = 34152);
+UPDATE `creature_template_addon` SET `auras` = '63359' WHERE `entry` IN (33524,34152);
 
