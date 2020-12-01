7692894324b905c8fcb3b52da9aa0344ab5499b6
FrancescoBorzi <borzifrancesco@gmail.com>
2020-12-01 18:10:17 +0100

    refactor(Core): apply clang-tidy modernize-raw-string-literal

--- src/tools/vmap4_extractor/vmapexport.cpp
+++ src/tools/vmap4_extractor/vmapexport.cpp
@@ -227,7 +227,7 @@ void ParsMapFiles()
     for (unsigned int i = 0; i < map_count; ++i)
     {
         sprintf(id, "%03u", map_ids[i].id);
-        sprintf(fn, "World\\Maps\\%s\\%s.wdt", map_ids[i].name, map_ids[i].name);
+        sprintf(fn, R"(World\Maps\%s\%s.wdt)", map_ids[i].name, map_ids[i].name);
         WDTFile WDT(fn, map_ids[i].name);
         if (WDT.init(id, map_ids[i].id))
         {
