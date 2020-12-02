920f19faa9e3cdc590ede9a7b244d517f1aa5321
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-02 07:01:30 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ba036ac19d0994fee653370865a562ba72363815

--- data/sql/updates/db_world/2020_12_02_00.sql
+++ data/sql/updates/db_world/2020_12_02_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_12_01_00 -> 2020_12_02_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_12_01_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_12_01_00 2020_12_02_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1606240668432550200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606240668432550200');
 
 UPDATE `gameobject_template_locale` SET `castBarCaption` = 'Öffnen' WHERE `entry` = 201922 AND `locale` = 'deDE';
@@ -367,3 +383,12 @@ UPDATE `gameobject_template_locale` SET `castBarCaption` = 'In Brand stecken' WH
 UPDATE `gameobject_template_locale` SET `castBarCaption` = 'In Brand stecken' WHERE `entry` = 3190 AND `locale` = 'deDE';
 UPDATE `gameobject_template_locale` SET `castBarCaption` = 'In Brand stecken' WHERE `entry` = 3189 AND `locale` = 'deDE';
 UPDATE `gameobject_template_locale` SET `castBarCaption` = 'Lese' WHERE `entry` = 3076 AND `locale` = 'deDE';
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
