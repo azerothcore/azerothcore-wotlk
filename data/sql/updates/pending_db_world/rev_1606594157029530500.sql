f55f4fdc61457f27c06f808fcf925f204d9364fb
Vox <ndinosaur1213@Live.com>
2020-12-01 22:38:10 -0500

    tick marks

--- data/sql/updates/pending_db_world/rev_1606594157029530500.sql
+++ data/sql/updates/pending_db_world/rev_1606594157029530500.sql
@@ -2,7 +2,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606594157029530500');
 
 -- Correct spell Algalon the Observer, 10 & 25 player 'Phase Punch' to be applied as negative buff
 
-DELETE FROM spell_custom_attr WHERE spell_id = 64412;
+DELETE FROM `spell_custom_attr` WHERE `spell_id` = 64412;
 
 -- Correct spell High King Maulgar, 'Death Coil' to be applied as negative buff
 
