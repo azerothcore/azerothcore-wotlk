590c81429e0079ac913cda739ab88b94b67b7fe4
Vox <ndinosaur1213@Live.com>
2020-12-02 17:20:01 -0500

    aa

--- data/sql/updates/pending_db_world/rev_1606594157029530500.sql
+++ data/sql/updates/pending_db_world/rev_1606594157029530500.sql
@@ -1,10 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606594157029530500');
 
--- Correct spell High King Maulgar, 'Death Coil' to be applied as negative buff
-
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (33130,12288);
-
 -- Correct spell Algalon the Observer, 10 & 25 player 'Phase Punch' to be applied as negative buff
 
 DELETE FROM `spell_custom_attr` WHERE `spell_id` = 64412;
+
+-- Correct spell High King Maulgar, 'Death Coil' to be applied as negative buff
+
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (33130,12288);
 
