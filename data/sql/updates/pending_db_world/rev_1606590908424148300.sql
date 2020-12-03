a55e493b5fddb55522d4ab2f09d4670c65e95da0
Silker <61223313+Si1ker@users.noreply.github.com>
2020-12-03 01:29:05 -0600

    translations already in broadcast_text_locale

--- data/sql/updates/pending_db_world/rev_1606590908424148300.sql
+++ data/sql/updates/pending_db_world/rev_1606590908424148300.sql
@@ -53,14 +53,6 @@ UPDATE `creature` SET `position_x`=2415.735, `position_y`=-5629.859, `position_z
 DELETE FROM `creature_text` WHERE `CreatureID`=31099 AND `GroupID`=0;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
 (31099, 0, 0, '%s goes into a frenzy!', 41, 0, 100, 0, 0, 0, 38630, 0, 'Patchwerk emote Frenzy - Ebon Hold');
-/* Add translations */
-DELETE FROM `creature_text_locale` WHERE `CreatureID`=31099 AND `GroupID`=0;
-INSERT INTO `creature_text_locale` (`CreatureID`, `GroupID`, `ID`, `Locale`, `Text`) VALUES
-(31099, 0, 0, 'deDE', '%s gerät in Raserei!'),
-(31099, 0, 0, 'frFR', '%s entre en frénésie!'),
-(31099, 0, 0, 'esES', '¡%s entra en frenesí!'),
-(31099, 0, 0, 'esMX', '¡%s entra en frenesí!'),
-(31099, 0, 0, 'ruRU', '%s впадает в бешенство!');
 
 -- Update AIName to Patchwerk and build SAI
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=31099;
