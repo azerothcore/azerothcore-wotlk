4cdbf14ba04504621ff1f489bf7529811a3bb3c0
Vox <ndinosaur1213@Live.com>
2020-12-03 22:22:34 -0500

    add delete

--- data/sql/updates/pending_db_world/rev_1606594157029530500.sql
+++ data/sql/updates/pending_db_world/rev_1606594157029530500.sql
@@ -6,5 +6,6 @@ DELETE FROM `spell_custom_attr` WHERE `spell_id` = 64412;
 
 -- Correct spell High King Maulgar, 'Death Coil' to be applied as negative buff
 
+DELETE FROM `spell_custom_attr` WHERE  `spell_id`=33130;
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (33130,12288);
 
