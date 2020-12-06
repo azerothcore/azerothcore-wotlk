2610486ccc7ec71761f645a43856075314bcef33
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-06 14:21:15 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 23a3d6ae3a20ca252eef3d8262ec476ef28f439e

--- data/sql/updates/db_world/2020_12_06_00.sql
+++ data/sql/updates/db_world/2020_12_06_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_12_05_02 -> 2020_12_06_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_12_05_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_12_05_02 2020_12_06_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1601827252211534700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601827252211534700');
 /*
  * Raid: The Eye of Eternity 
@@ -14,3 +30,12 @@ UPDATE `creature_template` SET `mindmg` = 7028, `maxdmg` = 11228, `DamageModifie
 /* BOSS */ 
 UPDATE `creature_template` SET `mindmg` = 18815, `maxdmg` = 24905, `DamageModifier` = 1.01 WHERE `entry` = 28859;
 UPDATE `creature_template` SET `mindmg` = 37630, `maxdmg` = 49810, `DamageModifier` = 1.01 WHERE `entry` = 31734;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
