0b8ec1f6eeef15be9798e4ebc038e44d7cd64feb
Francesco Borzì <borzifrancesco@gmail.com>
2020-12-07 19:04:19 +0100

    refactor(Core): apply clang-tidy modernize-loop-convert (#3822)

--- src/common/Collision/BoundingIntervalHierarchy.h
+++ src/common/Collision/BoundingIntervalHierarchy.h
@@ -369,7 +369,7 @@ protected:
             maxObjects(0xFFFFFFFF), sumDepth(0), minDepth(0x0FFFFFFF),
             maxDepth(0xFFFFFFFF), numBVH2(0)
         {
-            for (int i = 0; i < 6; ++i) numLeavesN[i] = 0;
+            for (int & i : numLeavesN) i = 0;
         }
 
         void updateInner() { numNodes++; }
