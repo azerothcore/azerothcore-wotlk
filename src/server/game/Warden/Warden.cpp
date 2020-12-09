ee33d0d5ec35f7dffd20440bee762d7a90b29ad5
sanctum32 <andriuspel@gmail.com>
2020-12-09 22:12:59 +0200

    Core/Warden: minor adjustment for previous commit

--- src/server/game/Warden/Warden.cpp
+++ src/server/game/Warden/Warden.cpp
@@ -240,7 +240,9 @@ void Warden::ApplyPenalty(uint16 checkId, std::string const& reason)
         if (Player const* plr = _session->GetPlayer())
         {
             std::string const reportFormat = "Player %s (guid %u, account id: %u) failed warden %u check (%s). Action: %s";
-            reportMsg = acore::StringFormat(reportFormat, plr->GetName().c_str(), plr->GetGUIDLow(), _session->GetAccountId(), checkId, checkData && (checkData->Comment.empty()) ? checkData->Comment.c_str() : "Unknown", GetWardenActionStr(action).c_str());
+            reportMsg = acore::StringFormat(reportFormat, plr->GetName().c_str(), plr->GetGUIDLow(), _session->GetAccountId(),
+                                           checkId, ((checkData && checkData->Comment.empty()) ? checkData->Comment.c_str() : "<warden comment is not set>"),
+                                           GetWardenActionStr(action).c_str());
         }
         else
         {
