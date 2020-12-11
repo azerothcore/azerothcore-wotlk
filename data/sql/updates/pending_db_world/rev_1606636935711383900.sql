e168ef3d2449eacdcbc65bc4d4f0a88a62caa4b7
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-11 17:04:55 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 1fb71189c79d14683b6393cb40ac7684055adadc

--- data/sql/updates/pending_db_world/rev_1606636935711383900.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606636935711383900');
-
--- Apply 'Shadowform' Aura to Saronite Animus
-
-UPDATE `creature_template_addon` SET `auras` = '63359' WHERE `entry` IN (33524,34152);
-
