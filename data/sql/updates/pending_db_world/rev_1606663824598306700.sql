32087e0d0dc15c26c3c04bee63a1b29c0ba33250
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-11 19:04:03 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ed85ddafefd4aa43d96c60f5b7064dd7f87ff243

--- data/sql/updates/pending_db_world/rev_1606663824598306700.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1606663824598306700');
-
-UPDATE `item_template_locale` SET `Description` = 'Möge Unglück über deinen neuen Besitzer kommen. - Anias' WHERE `ID`= 42811 AND `locale` = 'deDE';
