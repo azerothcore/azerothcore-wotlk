54809a75e4da849bb1d8782f5db81894cd43800f
FALL1N1 <1922208+FALL1N1@users.noreply.github.com>
2020-12-14 17:08:14 +0200

    Update data/sql/updates/pending_db_world/rev_1607432767438370200.sql
    ty Si1ker
    
    Co-authored-by: Silker <61223313+Si1ker@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1607432767438370200.sql
+++ data/sql/updates/pending_db_world/rev_1607432767438370200.sql
@@ -51,8 +51,7 @@ INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `positi
 DELETE FROM  `creature` WHERE `guid` IN (108008,108007,108006,108005,108004,108003,108002,108001,108000);
 
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` IN (24959,25220,25307);
-DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid` IN (-107575,-107574);
-DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid` IN (25220,25307);
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid` IN (-107575 ,-107574, 25220, 25307);
 DELETE FROM `smart_scripts` WHERE `source_type`=9 AND `entryorguid` IN (2522000,2522001);
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
