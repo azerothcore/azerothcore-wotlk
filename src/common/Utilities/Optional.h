e0e7ee6636768326a884ba345794c9340518bd01
FALL1N1 <1922208+FALL1N1@users.noreply.github.com>
2020-12-15 13:49:22 +0200

    update copyright header

--- src/common/Utilities/Optional.h
+++ src/common/Utilities/Optional.h
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-GPL2
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/> 
  */
 
 #ifndef acore_optional_h__
