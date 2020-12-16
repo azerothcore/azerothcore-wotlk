a19febc54e93244cac2a6ad1522083e2e34b5e7e
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-16 10:03:09 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ed305b03f03f361682ac6125e787e569f0e6403d

--- data/sql/updates/db_world/2020_12_16_00.sql
+++ data/sql/updates/db_world/2020_12_16_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_12_15_00 -> 2020_12_16_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_12_15_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_12_15_00 2020_12_16_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1607085889307075600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1607085889307075600');
 
 -- What The Dragons Know - Horde
@@ -33,3 +49,12 @@ UPDATE `quest_poi_points` SET `X`=5652, `Y`=2106 WHERE  `QuestID`=24461 AND `Idx
 -- The Halls Of Reflection - Alliance
 UPDATE `quest_poi_points` SET `X`=5652, `Y`=2106 WHERE  `QuestID`=24480 AND `Idx1`=2 AND `Idx2`=0;
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
