0a76aef5a0b5a6365eae7adeba72ce31d2f77f12
Silker <61223313+Si1ker@users.noreply.github.com>
2020-12-18 17:06:53 -0600

    fix typo

--- data/sql/updates/pending_db_world/rev_1608331608234968600.sql
+++ data/sql/updates/pending_db_world/rev_1608331608234968600.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1608331608234968600');
 
--- Update inicial NPCs positions
+-- Update initial NPCs positions
 UPDATE `creature` SET `position_x`=-6214.805, `position_y`=328.701, `position_z`=383.514, `orientation`=2.686 WHERE `guid`=351; -- Sten Stoutarm
 UPDATE `creature` SET `position_x`=-3987.618, `position_y`=-13874.763, `position_z`=91.132, `orientation`=4.785 WHERE `guid`=57173; -- Megelon
 UPDATE `creature` SET `position_x`=-606.627, `position_y`=-4251.340, `position_z`=38.957, `orientation`=3.175 WHERE `guid`=3442; -- Kaltunk
