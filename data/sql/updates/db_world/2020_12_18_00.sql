3741743b8a1425f993bdd2b0c69cef07c55883ca
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-18 07:23:39 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 07588a170e0b273f9ea1ce8ec290ade213efdba5

--- data/sql/updates/db_world/2020_12_18_00.sql
+++ data/sql/updates/db_world/2020_12_18_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_12_16_00 -> 2020_12_18_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_12_16_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_12_16_00 2020_12_18_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1607086882277070500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1607086882277070500');
 
 UPDATE `item_template_locale` SET `Description`='Lehrt Euch, wie man ein Fischmahl zubereitet.' WHERE `ID`=43017 AND `locale`='deDE';
@@ -11,3 +27,12 @@ UPDATE `item_template_locale` SET `Description`='Lehrt Euch, wie man einen würz
 UPDATE `item_template_locale` SET `Description`='Tun wir mal so, als hättet Ihr ein komplettes Ulduar-10er-Set' WHERE `ID`=46103 AND `locale`='deDE';
 UPDATE `item_template_locale` SET `Description`='Tun wir mal so, als hättet Ihr ein komplettes Ulduar-25er-Set' WHERE `ID`=46104 AND `locale`='deDE';
 UPDATE `item_template_locale` SET `Description`='Tun wir mal so, als hättet Ihr ein komplettes Ulduar-Hardmode-Set' WHERE `ID`=46105 AND `locale`='deDE';
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
