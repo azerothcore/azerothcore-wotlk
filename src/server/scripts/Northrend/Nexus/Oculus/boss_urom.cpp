9bba1c80299480464834d4508bb87e3867078216
Silker <61223313+Si1ker@users.noreply.github.com>
2020-12-18 18:31:11 -0600

    wtf that space lol

--- src/server/scripts/Northrend/Nexus/Oculus/boss_urom.cpp
+++ src/server/scripts/Northrend/Nexus/Oculus/boss_urom.cpp
@@ -343,7 +343,6 @@ public:
                     me->SetControlled(false, UNIT_STATE_ROOT);
                     me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                     me->GetMotionMaster()->MoveChase(me->GetVictim());
-
                     break;
             }
         }
