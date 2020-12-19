798de57ce3581a6e1090d9e0f41babfd5c4c891e
locus313 <pat@lo5t.com>
2020-12-19 08:45:42 -0800

    wip

--- apps/bash_shared/defines.sh
+++ apps/bash_shared/defines.sh
@@ -3,7 +3,7 @@ if [[ "$unamestr" == 'Darwin' ]]; then
    if ! command -v brew &>/dev/null ; then
        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
    fi
-   if ! [ "${BASH_VERSINFO}" == 5 ]; then
+   if ! [ "${BASH_VERSINFO}" -ge 4 ]; then
        brew install bash
    fi
    if ! command -v greadlink &>/dev/null ; then
@@ -19,4 +19,3 @@ AC_PATH_CONF="$AC_PATH_ROOT/conf"
 AC_PATH_MODULES="$AC_PATH_ROOT/modules"
 
 AC_PATH_DEPS="$AC_PATH_ROOT/deps"
-
--- apps/installer/includes/os_configs/osx.sh
+++ apps/installer/includes/os_configs/osx.sh
@@ -7,5 +7,10 @@ brew reinstall openssl@1.1
 
 brew update
 
-# brew install openssl readline cmake ace coreutils bash bash-completion md5sha1sum curl unzip
-brew install openssl readline cmake ace bash-completion curl unzip mysql ccache
+# brew install openssl readline cmake ace coreutils bash bash-completion md5sha1sum curl unzip mysql ccache
+
+# workaround for cmake already being installed in the github runners
+if ! command -v cmake &>/dev/null ; then
+       brew install cmake
+   fi
+brew install openssl readline ace bash-completion curl unzip mysql ccache
