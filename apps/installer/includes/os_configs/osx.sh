84850bfa1c240b51bf1d1d73df0f20da3efbb93d
locus313 <pat@lo5t.com>
2020-12-19 10:39:42 -0800

    wip

--- apps/installer/includes/os_configs/osx.sh
+++ apps/installer/includes/os_configs/osx.sh
@@ -1,5 +1,3 @@
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 ##########################################
 ## workaround to fix macos-10.15 configure os from failing
 brew reinstall openssl@1.1
@@ -7,10 +5,11 @@ brew reinstall openssl@1.1
 
 brew update
 
-# brew install openssl readline cmake ace coreutils bash bash-completion md5sha1sum curl unzip mysql ccache
-
-# workaround for cmake already being installed in the github runners
+##########################################
+## workaround for cmake already being installed in the github runners
 if ! command -v cmake &>/dev/null ; then
        brew install cmake
 fi
+##########################################
+
 brew install openssl readline ace bash-completion curl unzip mysql ccache
