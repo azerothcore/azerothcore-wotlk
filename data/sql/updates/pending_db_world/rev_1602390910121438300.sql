845ee3242a91b341629cddb64bd09c89122a36b9
Pagani Walter <paganiwalter@gmail.com>
2020-12-24 10:55:32 -0300

    Creating a single SQL file.

--- data/sql/updates/pending_db_world/rev_1602390910121438300.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1602390910121438300');
-
-UPDATE `broadcast_text` SET `MaleText`='Have mercy, $gsir:madam;!', `FemaleText`='Have mercy, $gsir:madam;!' WHERE `ID`=28767;
