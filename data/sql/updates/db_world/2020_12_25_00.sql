19050706af229e9c88101f53f02bbdbb0fa0f8aa
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-25 15:57:40 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d710057a3187a6f8540a24a8908709c285c1e32f

--- data/sql/updates/db_world/2020_12_25_00.sql
+++ data/sql/updates/db_world/2020_12_25_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2020_12_24_01 -> 2020_12_25_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2020_12_24_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2020_12_24_01 2020_12_25_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1601870525065182700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1601981411342865100');
 
 -- ULDUAR SHIELD BUNNY
@@ -602,3 +618,12 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 -- Add the script to the 48310 id spell
 DELETE FROM `spell_script_names` WHERE `spell_id`=48310;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (48310, 'spell_transitus_shield_beam');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
