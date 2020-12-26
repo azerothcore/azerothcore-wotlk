3b3f97707c2e649da97d0d9629097f0b44c30d84
Silker <61223313+Si1ker@users.noreply.github.com>
2020-12-26 15:52:02 -0600

    replace guid for ID in Tallonkai's Dresser

--- data/sql/updates/pending_db_world/rev_1609009225243254400.sql
+++ data/sql/updates/pending_db_world/rev_1609009225243254400.sql
@@ -2,7 +2,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609009225243254400');
 
 UPDATE `gameobject` SET `spawntimesecs` = 2 WHERE `id` IN
 (20691,  -- Cozzle's Footlocker
- 20925,  -- Captain's Footlocker
+20925,   -- Captain's Footlocker
 181110,  -- Soaked Tome
 181133,  -- Rathis Tomber's Supplies
 181238,  -- Dented Chest
@@ -10,7 +10,7 @@ UPDATE `gameobject` SET `spawntimesecs` = 2 WHERE `id` IN
 182011,  -- Crate of Ingots
 182804,  -- Mysteries of the Light
 183050,  -- The Saga of Terokk
-49828,   -- Tallonkai's Dresser
+126158,  -- Tallonkai's Dresser
 187980); -- First Aid Supplies
 
 -- Night Elf Plans: An'daroth, An'owyn, Scrying on the Sin'dorei
