4d80961fa9a8a5105d35ab547908b930ad09f552
Silker <61223313+Si1ker@users.noreply.github.com>
2020-12-26 13:16:57 -0600

    hehe

--- data/sql/updates/pending_db_world/rev_1608949912835926000.sql
+++ data/sql/updates/pending_db_world/rev_1608949912835926000.sql
@@ -3,7 +3,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1608949912835926000');
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (19257, 19258);
 
 -- Arcanist Torseldori
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 19257 AND `source_type`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 19257 AND `source_type`= 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)VALUES 
 (19257, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcanist Torseldori - On aggro - say text'),
 (19257, 0, 1, 0, 0, 0, 100, 0, 1000, 1000, 3000, 4000, 0, 11, 15530, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcanist Torseldori - IC - Cast spell Frostbolt'),
@@ -11,7 +11,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (19257, 0, 3, 0, 0, 0, 100, 0, 10000, 15000, 13000, 24000, 0, 11, 33634, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Arcanist Torseldori - IC - Cast spell Frost Blizzard');
 
 -- Bloodmage
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 19258 AND `source_type`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 19258 AND `source_type`= 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (19258, 0, 0, 0, 0, 0, 100, 0, 1000, 1000, 3000, 4000, 0, 11, 15530, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodmage - IC - Cast spell Frostbolt'),
 (19258, 0, 1, 0, 0, 0, 100, 0, 4000, 10000, 10000, 14000, 0, 11, 12674, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodmage - IC - Cast spell Frost Nova'),
