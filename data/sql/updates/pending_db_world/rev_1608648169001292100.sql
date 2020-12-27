207c8a50abf887267556a1e6df07e7a965e0763d
AzerothCoreBot <azerothcorebot@gmail.com>
2020-12-27 17:28:00 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 3c96a7df103978fcd5d536e642ed6d1bbb7f75d4

--- data/sql/updates/pending_db_world/rev_1608648169001292100.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1608648169001292100');
-
-UPDATE `acore_string` SET `content_default` = 'Unit Flags: %u.\nUnit Flags 2: %u.\nDynamic Flags: %u.\nFaction Template: %u.' WHERE `entry` = 542;
