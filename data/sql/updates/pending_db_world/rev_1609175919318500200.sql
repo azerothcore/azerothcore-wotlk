0fda2c7229796fad85d1d378e975fced24516aa0
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2020-12-28 18:39:22 +0100

    Update rev_1609175919318500200.sql

--- data/sql/updates/pending_db_world/rev_1609175919318500200.sql
+++ data/sql/updates/pending_db_world/rev_1609175919318500200.sql
@@ -11,6 +11,6 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI', `unit_flags`=`unit_flags`|5
 
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 25270;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(25270, 0, 0, 0, 1, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Say Line 0'),
-(25270, 0, 1, 2, 1, 0, 100, 0, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Despawn In 5000 ms'),
+(25270, 0, 0, 1, 1, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Say Line 0'),
+(25270, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Despawn In 5000 ms'),
 (25270, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 122, 2500, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Flee For 2500 ms');
