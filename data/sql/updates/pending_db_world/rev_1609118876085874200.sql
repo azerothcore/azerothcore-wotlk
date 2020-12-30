9d4584150aed7eb7c05e961113d1fcc30e5d89bb
WiZZy <bilucristian25@gmail.com>
2020-12-30 22:25:59 +0200

    Actually, i screwed this up, good thing we have the checks

--- data/sql/updates/pending_db_world/rev_1609118876085874200.sql
+++ data/sql/updates/pending_db_world/rev_1609118876085874200.sql
@@ -5,5 +5,5 @@ INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, 
 (99732, 192011, 571, 0, 0, 1, 1, 8426.94, 2921.13, 606.259, 1.61609, 0, 0, -0.722938, -0.690913, 25, 0, 1, '', 0),
 (2134518, 192011, 571, 0, 0, 1, 1, 8427.42, 2910.6, 606.259, 1.61609, 0, 0, -0.722938, -0.690913, 300, 0, 1, '', 0);
 
-UPDATE `gameobject_template` SET `Data0` = 15715, `Data1` = 17 WHERE (`entry` = 192011);
+UPDATE `gameobject_template` SET `Data1` = 17 WHERE (`entry` = 192011);
 
