5dc1330472144464cb4e8c600181d5235e061ad7
r0m1ntik <r.badanin94@yandex.ru>
2020-12-31 17:29:03 +0100

    errors

--- data/sql/updates/pending_db_world/rev_1609431351512296000.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609431351512296000');
-UPDATE `quest_poi` SET `MapID` = 543, `WorldMapAreaId` = 0 WHERE `QuestID` IN (9572, 9575) AND `id` IN (0, 1, 2);
