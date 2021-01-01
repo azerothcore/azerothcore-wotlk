e6050f80c26d39aecc0bbd661acba23fa30a3c31
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-02 00:41:03 +0100

    Actually commit the SQL changes this time

--- data/sql/updates/pending_db_world/rev_1609543457839849700.sql
+++ data/sql/updates/pending_db_world/rev_1609543457839849700.sql
@@ -1,1 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609543457839849700');
+
+UPDATE `creature_template` SET `npcflag`=`npcflag`&~32768, `flags_extra`=`flags_extra`|1024 WHERE `entry` IN (8888, 9299);
