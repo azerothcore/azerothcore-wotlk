d02b177f4b76b2a1b9b7131630a0f7700e8e13fb
r0m1ntik <r.badanin94@yandex.ru>
2021-01-01 20:49:25 +0100

    update

--- data/sql/updates/pending_db_world/rev_1609431870979648000.sql
+++ data/sql/updates/pending_db_world/rev_1609431870979648000.sql
@@ -1,12 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609431870979648000');
 UPDATE `gameobject_template` SET `Data2` = 0, `AIName` = 'SmartGameObjectAI' WHERE `entry` = 181964;
 DELETE FROM `event_scripts` WHERE `id` = 11027;
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (181964, 17715, 1771500);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (181964, 17715, 1771500) AND `source_type` IN (0, 1, 9);
 INSERT INTO `smart_scripts` VALUES
-	(181964, 1, 0, 0, 70, 0, 100, 0, 2, 0, 0, 0, 0, 12, 17715, 4, 300000, 0, 0, 0, 8, 0, 0, 0, 0, -1896.13, -12861.1, 87.1412, 3.5412, 'Statue of Queen Azshara - On Gameobject State Change - Summon Creature \'Atoph the Bloodcursed\'');
-INSERT INTO `smart_scripts` VALUES
-	(17715, 0, 0, 0, 63, 0, 100, 0, 0, 0, 0, 0, 0, 80, 1771500, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Atoph the Bloodcursed - Just Summoned - Run Script');
-INSERT INTO `smart_scripts` VALUES
+	(181964, 1, 0, 0, 70, 0, 100, 0, 2, 0, 0, 0, 0, 12, 17715, 4, 300000, 0, 0, 0, 8, 0, 0, 0, 0, -1896.13, -12861.1, 87.1412, 3.5412, 'Statue of Queen Azshara - On Gameobject State Change - Summon Creature \'Atoph the Bloodcursed\''),
+	(17715, 0, 0, 0, 63, 0, 100, 0, 0, 0, 0, 0, 0, 80, 1771500, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Atoph the Bloodcursed - Just Summoned - Run Script'),
 	(1771500, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Atoph the Bloodcursed - On Script - Say Line 0'),
 	(1771500, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, -1936.44, -12878.4, 85.7825, 3.5632, 'Atoph the Bloodcursed - On Script - Move To Pos'),
 	(1771500, 9, 2, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 0, 50, 181965, 10, 0, 0, 0, 0, 8, 0, 0, 0, 0, -1943.62, -12878.9, 88.3187, 3.8366, 'Atoph the Bloodcursed - On Script - Summon Gameobject \'Statue Fire\''),
