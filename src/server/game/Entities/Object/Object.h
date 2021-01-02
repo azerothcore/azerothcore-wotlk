52f0e060e3e5e9bdb26c9b987dcac768fc2a6de7
wowhungarian <szepe2324@gmail.com>
2021-01-02 04:56:28 +0100

    WG general visibility, and Cannon range update

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -28,11 +28,11 @@ class ElunaEventProcessor;
 #define INTERACTION_DISTANCE        5.5f
 #define ATTACK_DISTANCE             5.0f
 #define MAX_SEARCHER_DISTANCE       150.0f // pussywizard: replace the use of MAX_VISIBILITY_DISTANCE in searchers, because MAX_VISIBILITY_DISTANCE is quite too big for this purpose
-#define MAX_VISIBILITY_DISTANCE     250.0f // max distance for visible objects, experimental
+#define MAX_VISIBILITY_DISTANCE     300.0f // max distance for visible objects, experimental
 #define VISIBILITY_INC_FOR_GOBJECTS 30.0f // pussywizard
 #define VISIBILITY_COMPENSATION     15.0f // increase searchers
 #define SPELL_SEARCHER_COMPENSATION 30.0f // increase searchers size in case we have large npc near cell border
-#define VISIBILITY_DIST_WINTERGRASP 175.0f
+#define VISIBILITY_DIST_WINTERGRASP 280.0f
 #define SIGHT_RANGE_UNIT            50.0f
 #define DEFAULT_VISIBILITY_DISTANCE 90.0f // default visible distance, 90 yards on continents
 #define DEFAULT_VISIBILITY_INSTANCE 120.0f // default visible distance in instances, 120 yards
