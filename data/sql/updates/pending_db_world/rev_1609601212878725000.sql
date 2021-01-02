cbbe5f381e8fbba0b6b123e5804dab3726d295bd
r0m1ntik <r.badanin94@yandex.ru>
2021-01-02 16:37:25 +0100

    fix (DB/Creature): Laris Geardawdle (9616) missing text

--- data/sql/updates/pending_db_world/rev_1609601212878725000.sql
+++ data/sql/updates/pending_db_world/rev_1609601212878725000.sql
@@ -10,8 +10,8 @@ INSERT INTO `gossip_menu` VALUES
     (2408, 3102);
 DELETE FROM `conditions` WHERE `SourceGroup` = 2405;
 INSERT INTO `conditions` VALUES
-	(14, 2405, 3097, 0, 0, 8, 0, 4512, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 1)\' is rewarded'),
-	(14, 2405, 3097, 0, 0, 9, 0, 4513, 0, 0, 1, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is not taken'),
-	(14, 2405, 3099, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken'),
-	(14, 2405, 3098, 0, 0, 8, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3098 if quest \'A Little Slime Goes a Long Way (Part 2)\' is rewarded'),
-	(15, 2405, 0, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip option if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken');
+    (14, 2405, 3097, 0, 0, 8, 0, 4512, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 1)\' is rewarded'),
+    (14, 2405, 3097, 0, 0, 9, 0, 4513, 0, 0, 1, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is not taken'),
+    (14, 2405, 3099, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken'),
+    (14, 2405, 3098, 0, 0, 8, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3098 if quest \'A Little Slime Goes a Long Way (Part 2)\' is rewarded'),
+    (15, 2405, 0, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip option if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken');
