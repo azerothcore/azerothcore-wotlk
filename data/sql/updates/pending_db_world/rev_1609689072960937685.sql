f6eae422eb80926582794319b70ee97abf9fc90a
WiZZy <bilucristian25@gmail.com>
2021-01-03 17:53:24 +0200

    Initial commit.

---
+++ data/sql/updates/pending_db_world/rev_1609689072960937685.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609689072960937685');
+
+UPDATE `acore_world`.`game_event` SET `start_time` = '2014-03-23 14:00:00' WHERE (`eventEntry` IN (15, 62));
