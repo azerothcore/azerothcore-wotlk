83b21cca0ddb8705de8136278983f5f56d98295a
r0m1ntik <r.badanin94@yandex.ru>
2021-01-04 17:00:07 +0100

    fix(DB/Quest): quest 9589 &9590 wrong location

---
+++ data/sql/updates/pending_db_world/rev_1609775864907442000.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609775864907442000');
+UPDATE `quest_poi` SET `MapID` = 542, `WorldMapAreaId` = 0 WHERE (`QuestID` IN (9589, 9590)) AND (`id` = 0));
