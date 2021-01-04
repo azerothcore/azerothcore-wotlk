1461fac1e5b3c095c094c5d9bdb1ae13ba50b6cf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-04 00:19:44 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 92881ad1c9c38f15660e6c87669c07867efc8f40

--- data/sql/updates/db_world/2021_01_04_00.sql
+++ data/sql/updates/db_world/2021_01_04_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_01_03_01 -> 2021_01_04_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_01_03_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_01_03_01 2021_01_04_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1608949912835926000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1608949912835926000');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (19257, 19258);
@@ -17,3 +33,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (19258, 0, 1, 0, 0, 0, 100, 0, 4000, 10000, 10000, 14000, 0, 11, 12674, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodmage - IC - Cast spell Frost Nova'),
 (19258, 0, 2, 0, 0, 0, 100, 0, 10000, 15000, 13000, 24000, 0, 11, 33634, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodmage - IC - Cast spell Frost Blizzard');
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
