228ee9a547f5f3ef501945b132f0db97cc7a3d6b
r0m1ntik <r.badanin94@yandex.ru>
2021-01-05 19:13:39 +0100

    fix(DB/Spel) and (Core/Spell) [Weapon][Val'anyr] It is not activating with periodic healings

--- data/sql/updates/pending_db_auth/rev_1609867708436603000.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1609867708436603000');
-ALTER TABLE `account_muted`
-CHARSET = 'utf8',
-MODIFY `mutedby` varchar(50) NOT NULL,
-MODIFY `mutereason` varchar(255) NOT NULL;
---
+++ data/sql/updates/pending_db_world/rev_1609870134525124000.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609870134525124000');
+UPDATE `spell_proc_event` SET `procFlags` = 279552, `procEx` = 786432 WHERE `entry` = 64411;
