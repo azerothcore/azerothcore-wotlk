93cda6767eee0c66155159defa9fbdcf88968721
Francesco Borzì <borzifrancesco@gmail.com>
2021-01-05 10:18:27 +0100

    Update rev_1609803806703890000.sql

--- data/sql/updates/pending_db_world/rev_1609803806703890000.sql
+++ data/sql/updates/pending_db_world/rev_1609803806703890000.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609803806703890000');
 
 -- add initial delay of half a second to In Combat - Cast 'Lightning Shield' + add comments to all rows
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 17982) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17982);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17982, 0, 0, 0, 19, 0, 100, 0, 9759, 0, 0, 0, 0, 53, 1, 17982, 0, 0, 0, 2, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Demolitionist Legoso - On Quest \'Ending Their World\' Taken - Start Waypoint'),
 (17982, 0, 1, 0, 0, 0, 100, 0, 5000, 6000, 8000, 8000, 0, 11, 8056, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Demolitionist Legoso - In Combat - Cast \'Frost Shock\''),
