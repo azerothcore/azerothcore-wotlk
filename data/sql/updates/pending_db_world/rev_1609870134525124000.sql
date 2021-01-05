30082a88f9c9910b3025c0a60d31ba4402515101
r0m1ntik <r.badanin94@yandex.ru>
2021-01-05 19:29:39 +0100

    update

--- data/sql/updates/pending_db_world/rev_1609870134525124000.sql
+++ data/sql/updates/pending_db_world/rev_1609870134525124000.sql
@@ -1,2 +1,2 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609870134525124000');
-UPDATE `spell_proc_event` SET `procFlags` = 279552, `procEx` = 786432 WHERE `entry` = 64411;
+UPDATE `spell_proc_event` SET `procFlags` = 279552|1024|16384|262144, `procEx` = 786432|262144|524288 WHERE `entry` = 64411;
