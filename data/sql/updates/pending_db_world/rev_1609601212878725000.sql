10fee60ced747a3b836347bd47329cd7ee0264eb
Francesco Borzì <borzifrancesco@gmail.com>
2021-01-06 23:42:34 +0100

    Update data/sql/updates/pending_db_world/rev_1609601212878725000.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1609601212878725000.sql
+++ data/sql/updates/pending_db_world/rev_1609601212878725000.sql
@@ -1,17 +1,17 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609601212878725000');
 DELETE FROM `gossip_menu` WHERE `MenuID` IN (2405, 2406, 2407, 2408);
-INSERT INTO `gossip_menu` VALUES
-    (2405, 3077),
-    (2405, 3097),
-    (2405, 3098),
-    (2405, 3099),
-    (2406, 3100),
-    (2407, 3101),
-    (2408, 3102);
-DELETE FROM `conditions` WHERE `SourceGroup` = 2405;
-INSERT INTO `conditions` VALUES
-    (14, 2405, 3097, 0, 0, 8, 0, 4512, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 1)\' is rewarded'),
-    (14, 2405, 3097, 0, 0, 9, 0, 4513, 0, 0, 1, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is not taken'),
-    (14, 2405, 3099, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken'),
-    (14, 2405, 3098, 0, 0, 8, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3098 if quest \'A Little Slime Goes a Long Way (Part 2)\' is rewarded'),
-    (15, 2405, 0, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip option if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken');
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+(2405, 3077),
+(2405, 3097),
+(2405, 3098),
+(2405, 3099),
+(2406, 3100),
+(2407, 3101),
+(2408, 3102);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 2405;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(14, 2405, 3097, 0, 0, 8, 0, 4512, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 1)\' is rewarded'),
+(14, 2405, 3097, 0, 0, 9, 0, 4513, 0, 0, 1, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is not taken'),
+(14, 2405, 3099, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3099 if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken'),
+(14, 2405, 3098, 0, 0, 8, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip text 3098 if quest \'A Little Slime Goes a Long Way (Part 2)\' is rewarded'),
+(15, 2405, 0, 0, 0, 9, 0, 4513, 0, 0, 0, 0, 0, '', 'Show gossip option if quest \'A Little Slime Goes a Long Way (Part 2)\' is taken');
