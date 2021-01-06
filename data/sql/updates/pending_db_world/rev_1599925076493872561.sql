5d064199a8ed84eb118cf61083723e63bc1ce2af
sanctum32 <andriuspel@gmail.com>
2021-01-06 08:36:10 -0800

    DB/Warden: addition to previous commit

--- data/sql/updates/pending_db_world/rev_1599925076493872561.sql
+++ data/sql/updates/pending_db_world/rev_1599925076493872561.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1599925076493872561');
 ALTER TABLE `warden_checks` MODIFY `str` VARCHAR(170);
-DELETE FROM `warden_checks` WHERE `id` IN (788,789,790);
+DELETE FROM `warden_checks` WHERE `id` IN (788,789,790,791,792);
 INSERT INTO `warden_checks` (`id`,`type`,`str`,`address`,`length`,`result`, `comment`) VALUES
 (788, 139, 'forceinsecure() return issecure()', NULL, NULL, NULL, 'Detects naive Lua unlockers'),
 (789, 139, 'return not not PQR_IsMoving', NULL, NULL, NULL, 'Detects PQR'),
