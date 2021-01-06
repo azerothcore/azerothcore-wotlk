1bd25facd449073e8b118a0eaee2183899db0128
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-06 22:44:24 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 9427da06ad0668946b5796e156b93f46a60f7bfe

--- data/sql/updates/pending_db_world/rev_1609618111014466000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609618111014466000');
-DELETE FROM `npc_trainer` WHERE (`ID` = 17482) AND (`SpellID` = -200012);
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) VALUES
-(17482, -200012, 0, 0, 0, 0);
