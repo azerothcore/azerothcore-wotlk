a2c40466497fcbdc96d7b6da81d38fb67e7051f8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-09 00:22:36 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 9bffd569934d8dede9a72d81a91b826949a71968

--- data/sql/updates/pending_db_world/rev_1609978307795332000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609978307795332000');
-DELETE FROM `graveyard_zone` WHERE (`ID` = 925) AND (`GhostZone` = 3479);
-INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES
-(925, 3479, 0, 'Bloodmyst, Blood Watch GY - Bloodmyst Isle');
