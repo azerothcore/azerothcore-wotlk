72ee5f4972b2327a03a70c0b06dee6ca20e2d63a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-09 11:02:09 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): b1452b5b171b6f1f748362bafe41f7c4c8fb9e37

--- data/sql/updates/db_world/2021_01_09_01.sql
+++ data/sql/updates/db_world/2021_01_09_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_01_09_00 -> 2021_01_09_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_01_09_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_01_09_00 2021_01_09_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1595505810975581600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1595505810975581600');
 
 DELETE FROM `spell_script_names` WHERE  `spell_id`=45980 AND `ScriptName`='spell_gen_despawn_self';
@@ -16,3 +32,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (25814, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 1, 1, 10000, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Fizzcrank Mechagnome - On Aggro - Say Line 1 (Phase 1) (No Repeat)'),
 (25814, 0, 1, 2, 8, 0, 100, 0, 46485, 0, 0, 0, 0, 33, 26096, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Fizzcrank Mechagnome - On Spellhit \'The Greatmother\'s Soulcatcher\' - Quest Credit \'Souls of the Decursed\''),
 (25814, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 41, 10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Fizzcrank Mechagnome - On Spellhit \'The Greatmother\'s Soulcatcher\' - Despawn In 10 ms');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
