cf267337754817bbe562af153243bc23e7067061
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-11 19:23:30 +0100

    fix(Core/Item): Implement mount speed mod items

---
+++ data/sql/updates/pending_db_world/rev_1610385898427311800.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610385898427311800');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (48777,7215,48776,25653);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(48777,'spell_item_with_mount_speed'),
+(7215,'spell_item_with_mount_speed'),
+(48776,'spell_item_with_mount_speed'),
+(25653, 'spell_item_with_mount_speed');
