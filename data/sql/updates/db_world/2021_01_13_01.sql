cde6a1bed65763ad55ea54d90aae6d11e615839e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-13 20:51:03 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d54a860a47f7d0b971f6d3be4e3c5165c68d93ec

--- data/sql/updates/db_world/2021_01_13_01.sql
+++ data/sql/updates/db_world/2021_01_13_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_01_13_00 -> 2021_01_13_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_01_13_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_01_13_00 2021_01_13_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1609106243604105800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609106243604105800');
 
 -- DELETE OLD WAYPOINTS
@@ -88,3 +104,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (2522001, 9, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 71, 0, 0, 2178, 143, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Civilian Recruit - Actionlist - Change Equipment'),
 (2522001, 9, 4, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 0, 53, 0, 2522001, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Civilian Recruit - Actionlist - Start Waypoint'),
 (2522001, 9, 5, 0, 0, 0, 100, 0, 4000, 5000, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 19, 25222, 0, 0, 0, 0, 0, 0, 0, 'Civilian Recruit - Actionlist - Say Line 3');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
