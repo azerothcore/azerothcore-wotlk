546262adbffc975f0a294308732d23017ba6c30c
lineagedr <lordlo0kk@gmail.com>
2021-01-14 20:15:55 +0200

    fix(Scripts/DB/Creature): Set PvP flag for some creatures (#4270)

---
+++ data/sql/updates/pending_db_world/rev_1610598175630537700.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610598175630537700');
+
+UPDATE `creature_template` SET `faction` = '1639', `unit_flags` = '4096', `flags_extra` = '0' WHERE `entry` = '17551';
