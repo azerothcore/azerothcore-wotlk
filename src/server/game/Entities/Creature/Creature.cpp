838c0081c01bc2dae00a9eb088c66b71c53399a2
Stefano Borzì <stefanoborzi32@gmail.com>
2021-01-16 15:09:26 +0100

    chore(Core/Creature): dismount npc if dead @footman

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1627,7 +1627,7 @@ void Creature::setDeathState(DeathState s, bool despawn)
         SetTarget(0);                // remove target selection in any cases (can be set at aura remove in Unit::setDeathState)
         SetUInt32Value(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_NONE);
 
-        SetUInt32Value(UNIT_FIELD_MOUNTDISPLAYID, 0); // if creature is mounted on a virtual mount, remove it at death
+        Dismount(); // if creature is mounted on a virtual mount, remove it at death
 
         setActive(false);
 
