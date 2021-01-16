e7052d15f845d5e7abcfd0c6e28cf59bcc15398b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-16 12:44:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d5c41aa5d76bfeb4a71d5d7ca6c06849663016ce

--- data/sql/updates/db_world/2021_01_16_01.sql
+++ data/sql/updates/db_world/2021_01_16_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_01_16_00 -> 2021_01_16_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_01_16_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_01_16_00 2021_01_16_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1609175919318500200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609175919318500200');
 
 DELETE FROM `creature_text` WHERE `CreatureID` = 25270 AND `GroupID` = 0;
@@ -15,3 +31,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (25270, 0, 0, 1, 1, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Say Line 0'),
 (25270, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Despawn In 5000 ms'),
 (25270, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 122, 2500, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warsong Peon - Out of Combat - Flee For 2500 ms');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
