cf5ae65298d1d50f9269c649b3d54c3797df0e78
Francesco Borzì <borzifrancesco@gmail.com>
2021-01-17 01:52:04 +0100

    Update Guild.cpp

--- src/server/game/Guilds/Guild.cpp
+++ src/server/game/Guilds/Guild.cpp
@@ -1203,7 +1203,6 @@ bool Guild::Create(Player* pLeader, std::string const& name)
     _CreateDefaultGuildRanks(pLeaderSession->GetSessionDbLocaleIndex()); // Create default ranks
     bool ret = AddMember(m_leaderGuid, GR_GUILDMASTER);                  // Add guildmaster
 
-
     for (short i = 0; i < static_cast<short>(sWorld->getIntConfig(CONFIG_GUILD_BANK_INITIAL_TABS)); i++)
     {
         _CreateNewBankTab();
