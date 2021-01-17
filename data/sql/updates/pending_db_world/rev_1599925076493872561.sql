2794842553d99d64982dc4877152b44b4cde1a5e
Andrius Peleckas <32540208+sanctum32@users.noreply.github.com>
2021-01-17 19:36:35 +0200

    feat(Core/Warden): optimization + PQR detection (#3875)
    * Update AuctionHouseHandler.cpp
    
    * feat(warden): block PQR
    
    * wip
    
    * Update Warden.cpp
    
    * Core/Warden: minor preperation
    
    * Core/Misc: implemented some needed container wrapper functions
    
    * Core/Warden: more preperations
    
    * Core/Warden: more rework
    * LUA checks now are splitted to seperate config
    * LUA checks are always in front of queue
    * Fixed "Other" checks
    * Fixed PQR detection
    
    * Core/Warden: adjusted and optimized
    
    * Core/Warden: optimization
    
    * Core/Warden: more optimization
    
    * Core/Warden: use warden comments in ban reason
    
    * Core/Warden: more warden work
    
    * Core/Warden: more optimizations
    
    * Core/Warden: more refactors
    
    * Core/Warden: some more refactors + use default SHA definitions
    
    * Core/Warden: more refactoring
    
    * Core/Warden: fixed PQR detection again
    
    * Core/Warden: improved detection logging
    
    * Core/Misc: removed SmartEnums - we do not need it here + maybe should go as seperate commit
    
    * Core/Warden: fixed some warnings
    
    * Core/Warden: codestyle
    
    * Core/Warden: include warden check comment to console logs
    
    * Core/Warden: minor adjustment for previous commit
    
    * Core/Warden: fixed static analysis warning
    
    * Core/Warden: compilers, you're drunk this time... function is not unused...
    
    * Core/Warden: minor correction for logs
    
    * Core/Warden: one more improvement for logs
    
    * Core/Warden: added missing overrides
    
    * Core/Warden: some codestyle
    
    * Core/Misc: more codestyle
    
    * Core/Misc: more codestyle!
    
    * Core/Warden: restored default config options... (if you change it, change in core source too)
    
    * Core/Warden: addition to previous commit
    
    * DB/Warden: added EWT and WOWPlus checks (provided by lineagedr)
    
    * DB/Warden: addition to previous commit
    
    * fix: codestyle
    
    * Core/Warden: fixed build when extra logs are enabled in cmake
    
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>
    Co-authored-by: Francesco Borzì <borzifrancesco@gmail.com>

---
+++ data/sql/updates/pending_db_world/rev_1599925076493872561.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1599925076493872561');
+ALTER TABLE `warden_checks` MODIFY `str` VARCHAR(170);
+DELETE FROM `warden_checks` WHERE `id` IN (788,789,790,791,792);
+INSERT INTO `warden_checks` (`id`,`type`,`str`,`address`,`length`,`result`, `comment`) VALUES
+(788, 139, 'forceinsecure() return issecure()', NULL, NULL, NULL, 'Detects naive Lua unlockers'),
+(789, 139, 'return not not PQR_IsMoving', NULL, NULL, NULL, 'Detects PQR'),
+(790, 139, 'local f=DEFAULT_CHAT_FRAME for i=1,f:GetNumMessages() do if (f:GetMessageInfo(i)):find("|cffffd200PQR|r") then return true end end', NULL, NULL, NULL, 'Detects PQR'),
+(791, 139, 'local f=DEFAULT_CHAT_FRAME for i=1,f:GetNumMessages() do if (f:GetMessageInfo(i)):find("|cFF32CD32EWT|r") then return true end end',NULL,NULL,NULL,'Detects EWT'),
+(792, 139, 'local f=DEFAULT_CHAT_FRAME for i=1,f:GetNumMessages() do if (f:GetMessageInfo(i)):find("|cFFFF4400WoWPlus|r") then return true end end',NULL,NULL,NULL,'Detects WoWPlus');
