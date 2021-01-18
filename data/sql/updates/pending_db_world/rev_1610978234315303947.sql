91aa6ab7e1b6cee4e8eee97488620404bb79f7d8
Zoidwaffle <zoidwaffle@gmail.com>
2021-01-18 14:59:01 +0100

    fix(DB/Object): Core Fragment in BRD should not despawn

---
+++ data/sql/updates/pending_db_world/rev_1610978234315303947.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610978234315303947');
+
+--- Core Fragment in Blackrock Depths shouldn't despawn after being looted once
+
+UPDATE `gameobject` SET `spawntimesecs`=0 WHERE `guid`=43133;
