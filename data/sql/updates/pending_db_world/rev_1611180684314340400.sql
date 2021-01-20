0fefb821d0c4b0d9dcf1b5b2db21fd40fd42275c
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-20 23:45:04 +0100

    scriptName

--- data/sql/updates/pending_db_world/rev_1611180684314340400.sql
+++ data/sql/updates/pending_db_world/rev_1611180684314340400.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611180684314340400');
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = 75731;
-INSERT INTO `spell_script_names` (`spell_id`, `scriptName`) VALUES
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (75731, 'spell_item_instant_statue');
 UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 40246;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (74890, -75731);
