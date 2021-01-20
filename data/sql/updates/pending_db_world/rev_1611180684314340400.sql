e2d9bb880a042ee768861ff81116ea58c3643754
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-20 23:14:05 +0100

    fix(Core/Spell): Instant Statue

---
+++ data/sql/updates/pending_db_world/rev_1611180684314340400.sql
@@ -0,0 +1,11 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611180684314340400');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = 75731;
+INSERT INTO `spell_script_names` (`spell_id`, `scriptName`) VALUE
+(75731, 'spell_item_instant_statue');
+UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 40246;
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (74890, -75731);
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(74890, 75055, 0, 'Instant Statue'),
+(-75731, -74890, 0, 'Instant Statue'),
+(-75731, -75055, 0, 'Instant Statue');
