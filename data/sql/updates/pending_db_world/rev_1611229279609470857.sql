063faf7dace012ec08f2625504b9461fe5155f04
Zoidwaffle <zoidwaffle@gmail.com>
2021-01-23 14:13:39 +0100

    Share the Darkshore GY's between Horde and Alliance

--- data/sql/updates/pending_db_world/rev_1611229279609470857.sql
+++ data/sql/updates/pending_db_world/rev_1611229279609470857.sql
@@ -1,7 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611229279609470857');
 
 -- Fix issue #4304, a cemetary for Horde in Darkshore
--- The graveyard is shared between Horde and Alliance, so remove the Horde specific one first
+-- The graveyards are shared between Horde and Alliance, so remove the Horde specific one first
 
 DELETE FROM `graveyard_zone` WHERE `ID`=512 AND `GhostZone`=148 AND `Faction`=67;
 UPDATE `graveyard_zone` SET `Faction`=0 WHERE `ID`=469 AND `GhostZone`=148;
+UPDATE `graveyard_zone` SET `Faction`=0 WHERE `ID`=35 AND `GhostZone`=148;
