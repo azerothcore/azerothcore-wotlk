fd202e9d03f62e25ba2e8ad2acab31cf222e9959
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-24 01:07:31 +0100

    untargetable

--- data/sql/updates/pending_db_world/rev_1611180684314340400.sql
+++ data/sql/updates/pending_db_world/rev_1611180684314340400.sql
@@ -3,7 +3,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611180684314340400');
 DELETE FROM `spell_script_names` WHERE `spell_id` = 75731;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (75731, 'spell_item_instant_statue');
-UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 40246;
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`|33554432, `AIName` = '' WHERE `entry` = 40246;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (74890, -75731);
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 (74890, 75055, 0, 'Instant Statue'),
