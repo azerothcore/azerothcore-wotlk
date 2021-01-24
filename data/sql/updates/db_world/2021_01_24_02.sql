2e6f78bdddbc84a1950aaec0865a689efa9fcde4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-01-24 13:44:48 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d0c5bee59d404429bc91e839ceec1f1aba0eab0a

--- data/sql/updates/db_world/2021_01_24_02.sql
+++ data/sql/updates/db_world/2021_01_24_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_01_24_01 -> 2021_01_24_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_01_24_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_01_24_01 2021_01_24_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1610768378723362000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610768378723362000');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 2784;
@@ -8,3 +24,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (2784, 0, 1, 0, 0, 0, 100, 0, 5000, 7000, 25000, 30000, 0, 11, 19135, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'King Magni Bronzebeard - In Combat - Cast Avatar'),
 (2784, 0, 2, 0, 0, 0, 100, 0, 8000, 10000, 20000, 30000, 0, 11, 20686, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'King Magni Bronzebeard - In Combat - Cast Knock Away'),
 (2784, 0, 3, 0, 0, 0, 100, 0, 12000, 15000, 15000, 20000, 0, 11, 20685, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'King Magni Bronzebeard - In Combat - Cast Storm Bolt');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
