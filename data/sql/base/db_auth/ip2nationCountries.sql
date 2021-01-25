bfcc05f09bbc030ad8f6cb93e05dd8d77036f07d
FrancescoBorzi <borzifrancesco@gmail.com>
2021-01-25 19:04:06 +0100

    fix: ip2nationCountries

--- data/sql/base/db_auth/ip2nationCountries.sql
+++ data/sql/base/db_auth/ip2nationCountries.sql
@@ -4,10 +4,10 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-DROP TABLE IF EXISTS `ip2nationcountries`;
+DROP TABLE IF EXISTS `ip2nationCountries`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ip2nationcountries` 
+CREATE TABLE `ip2nationCountries`
 (
   `code` varchar(4) NOT NULL DEFAULT '',
   `iso_code_2` varchar(2) NOT NULL DEFAULT '',
@@ -21,9 +21,9 @@ CREATE TABLE `ip2nationcountries`
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-LOCK TABLES `ip2nationcountries` WRITE;
-/*!40000 ALTER TABLE `ip2nationcountries` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ip2nationcountries` ENABLE KEYS */;
+LOCK TABLES `ip2nationCountries` WRITE;
+/*!40000 ALTER TABLE `ip2nationCountries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ip2nationCountries` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
