36ca450b4504259cd5cf7f43aa8c9c2a28b36416
55Honey <71938210+55Honey@users.noreply.github.com>
2021-01-28 21:55:24 +0100

    Cleanup

--- data/sql/updates/pending_db_world/rev_1611865917828980400.sql
+++ data/sql/updates/pending_db_world/rev_1611865917828980400.sql
@@ -1,3 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611865917828980400');
-INSERT INTO item_template_locale (ID, locale, Name, Description, VerifiedBuild) VALUES ('30622', 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', '15050'); 
-INSERT INTO item_template_locale (ID, locale, Name, Description, VerifiedBuild) VALUES ('30637', 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', '15050');
+DELETE FROM `item_template_locale` WHERE `ID` = 30622 and locale = 'zhCN'
+DELETE FROM `item_template_locale` WHERE `ID` = 30637 and locale = 'zhCN'
+INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES
+('30622', 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', '15050'),
+('30637', 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', '15050');
