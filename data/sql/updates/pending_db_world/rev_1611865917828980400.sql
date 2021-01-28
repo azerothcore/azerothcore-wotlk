9dc76f825b89e8d4785346f5a54eed7c90a25d48
55Honey <71938210+55Honey@users.noreply.github.com>
2021-01-28 21:39:39 +0100

    zhCN translation for Flamewrought Key
    Created a pending_db_world sql file to add zhCN translations for Flamewrought Key

---
+++ data/sql/updates/pending_db_world/rev_1611865917828980400.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611865917828980400');
+INSERT INTO item_template_locale (ID, locale, Name, Description, VerifiedBuild) VALUES ('30622', 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', '15050'); 
+INSERT INTO item_template_locale (ID, locale, Name, Description, VerifiedBuild) VALUES ('30637', 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', '15050');
