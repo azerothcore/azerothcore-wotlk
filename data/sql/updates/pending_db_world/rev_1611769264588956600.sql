b5f9a5bf585731b24990d4e435ab475d58c110e9
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-28 13:16:52 +0100

    just capitalize sql keywords

--- data/sql/updates/pending_db_world/rev_1611769264588956600.sql
+++ data/sql/updates/pending_db_world/rev_1611769264588956600.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611769264588956600');
 
-ALTER TABLE `creature_template` ADD COLUMN `spell_school_immune_mask` int(3) unsigned NOT NULL DEFAULT '0' AFTER `mechanic_immune_mask`;
+ALTER TABLE `creature_template` ADD COLUMN `spell_school_immune_mask` INT(3) UNSIGNED NOT NULL DEFAULT '0' AFTER `mechanic_immune_mask`;
 
 -- Imported from TrinityCore
 -- SPELL_SCHOOL_NORMAL
