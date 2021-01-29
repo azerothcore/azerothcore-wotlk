4d76ed670297bfe664aed7b0fb78f2c0b93b572f
Winfidonarleyan <dowlandtop@yandex.com>
2021-01-29 16:18:09 +0700

    +

--- src/server/game/Quests/QuestTracker.cpp
+++ src/server/game/Quests/QuestTracker.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016+  AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
- * Copyright (C) 2021   WarheadCore <https://github.com/WarheadCore/WarheadCore_AC_Arhive>. See AUTHORS file for Copyright information
+ * Copyright (C) 2021   WarheadCore <https://github.com/WarheadCore/WarheadCore_AC_Arhive>
  */
 
 #include "QuestTracker.h"
