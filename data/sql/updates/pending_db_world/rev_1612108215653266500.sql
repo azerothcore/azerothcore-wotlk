87e3f2112db9820f4d4cfaf03eaf470ca705bcc1
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-01-31 16:52:14 +0100

    fix(DB/Quest): Journey to Undercity outro
    * https://github.com/TrinityCore/TrinityCore/commit/a9d493c4564cdaa7efe400408f3582f1cdbb8cde
    
    Co-Authored-By: Trond B. Krokli <38162891+illfated@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1612108215653266500.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612108215653266500');
+
+UPDATE `creature_text` SET `Text`= "Sighing, %s kneels down and picks up the amulet.", `BroadcastTextId`= 19244 WHERE `CreatureID`= 10181 AND `GroupID`= 0;
