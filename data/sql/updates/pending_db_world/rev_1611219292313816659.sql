58c1d0025efb713b413fbd04b60f82b459f4f185
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-01 22:51:25 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 50a4b44c7636f0acaf5f43d785db0aad5a5eeb8d

--- data/sql/updates/pending_db_world/rev_1611219292313816659.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611219292313816659');
-
--- The Damaged Chest from the warlock quest Tome of the Cabal should respawn right away
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=12699;
