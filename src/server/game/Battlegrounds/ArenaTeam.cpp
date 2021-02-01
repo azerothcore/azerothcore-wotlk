8ac72330269752deab170536f409906efca03d86
Winfidonarleyan <dowlandtop@yandex.com>
2021-02-01 17:41:30 +0700

    include correct

--- src/server/game/Battlegrounds/ArenaTeam.cpp
+++ src/server/game/Battlegrounds/ArenaTeam.cpp
@@ -13,8 +13,8 @@
 #include "Player.h"
 #include "WorldSession.h"
 #include "BattlegroundMgr.h"
-#include "ScriptMgr.h"
 #include "Opcodes.h"
+#include "ScriptMgr.h"
 #include "BattlegroundMgr.h"
 
 ArenaTeam::ArenaTeam()
