adcf3f7abb1c0634b24626c65aac03870024f723
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-01 17:02:36 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 68f7cf3fabd00506d5f2c89d5fb31b1cd4b1792e

--- data/sql/updates/pending_db_world/rev_1610978234315303947.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610978234315303947');
-
--- Core Fragment in Blackrock Depths shouldn't despawn after being looted once
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=43133;
