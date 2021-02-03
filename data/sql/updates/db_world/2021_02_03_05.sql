557f940bba512db97e57bade58db432cffcb051c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-03 09:48:01 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 68532913385f417b64da058e167e579aa811fff1

--- data/sql/updates/db_world/2021_02_03_05.sql
+++ data/sql/updates/db_world/2021_02_03_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_02_03_04 -> 2021_02_03_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_02_03_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_02_03_04 2021_02_03_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1610314669347459800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1610314669347459800');
 
 -- Increase drop chance of 'Tender Strider Meat' for quest Kyle's Gone Missing! (ID: 11129) to 50% based on videos that show that this quest is completed with one or two kills of a mob - currently on AC it's a 10% droprate which is considerable for a low level entry quest
@@ -11,3 +27,12 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 -- Remove 'Get In' Icon on Refurbished Steam Tank
 
 UPDATE `creature_template` SET `IconName` = '', `VehicleId` = 0 WHERE (`entry` = 29144);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
