64016e234eddec735bb4f154bc53e10774ef1c94
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-03 17:44:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ae74eb0713fb7f17a2a03919b6888cf6f4f1ac99

--- data/sql/updates/pending_db_world/rev_1611779118831382003.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611779118831382003');
-
--- Lower respawn rate of Compendium of the Fallen
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=32236;
