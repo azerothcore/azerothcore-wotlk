18e3ece24f20ae1680ab8c7b808fca3a8a9a9f5b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-03 04:34:15 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 5c994d2d6d7ecec0b6f2254b3f6d777f2aaee9c9

--- data/sql/updates/db_world/2021_02_03_03.sql
+++ data/sql/updates/db_world/2021_02_03_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_02_03_02 -> 2021_02_03_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_02_03_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_02_03_02 2021_02_03_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1612115546205486724'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612115546205486724');
 
 -- Add new script for flee event, also flee for assist
@@ -28,3 +44,12 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,2,0,2,0,100,0,0,50,12000,15000,11,12491,1,0,0,0,0,1,0,0,0,0,0,0,0,"Mmmrrrggglll - Between 0-50% Health - Cast 'Healing Wave'"),
 (@ENTRY,0,3,4,6,0,100,0,0,0,0,0,45,0,1,0,0,0,0,9,15668,0,60,0,0,0,0,"Mmmrrrggglll - On Just Died - Set Data 0 1"),
 (@ENTRY,0,4,0,61,0,100,0,0,0,0,0,45,0,1,0,0,0,0,9,15669,0,60,0,0,0,0,"Mmmrrrggglll - On Just Died - Set Data 0 1");
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
