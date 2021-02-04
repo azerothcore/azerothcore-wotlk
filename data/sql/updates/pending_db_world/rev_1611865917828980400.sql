b0d0378e40e4e26aed69911e03bdcb796a4bd8ae
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-04 09:46:24 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 8cce6687d78317a1f66af143073d3fa63939d71b

--- data/sql/updates/pending_db_world/rev_1611865917828980400.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1611865917828980400');
-DELETE FROM `item_template_locale` WHERE `ID` IN (30622,30637) and `locale` = 'zhCN';
-INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES
-(30622, 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', 15050),
-(30637, 'zhCN', '焰铸钥匙', '允许你进入英雄难度的地狱火堡垒地下城。', 15050);
