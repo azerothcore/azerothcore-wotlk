7757fd6bb08240c11bda66f106251241f52d10a0
Zoidwaffle <zoidwaffle@gmail.com>
2021-02-05 11:43:56 +0100

    Add Conspicuous Urn

--- data/sql/updates/pending_db_world/rev_1612520952274785900.sql
+++ data/sql/updates/pending_db_world/rev_1612520952274785900.sql
@@ -1,5 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612520952274785900');
 
--- Lower respawn of Shadowforge Cache
+-- Lower respawns to improve quests Find the Gems and Power Source & Find the Gems
 
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=40694;
+UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid` IN
+(
+40694, -- Shadowforge Cache
+40695  -- Conspicuous Urn
+);
