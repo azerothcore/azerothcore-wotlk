158b1bcf7328a08ec2450bda1a1ab315cfb25382
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-05 22:37:27 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 852b17117b945ca4639dce4ba09d3f96e2129de7

--- data/sql/updates/pending_db_world/rev_1612360387230427830.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612360387230427830');
-
--- Fix DK quest: The Plaguebringer's Request
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid` IN
-(
-66377,  -- Empty Cauldron
-66378   -- Iron Chain
-);
