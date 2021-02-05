8961a2406d4e7ab9281ad577f63c550c584e34d0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-05 00:08:37 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2dba53425b8735ed996d02ffcfdd4056c8be6184

--- data/sql/updates/db_world/2021_02_05_00.sql
+++ data/sql/updates/db_world/2021_02_05_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_02_04_03 -> 2021_02_05_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_02_04_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_02_04_03 2021_02_05_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1612095806740149900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612095806740149900');
 
 DELETE FROM `creature_loot_template` WHERE `entry` IN (
@@ -552,3 +568,12 @@ DELETE FROM `creature_loot_template` WHERE `entry` IN (
 ) AND `Item` IN (24477,5504,5503,15924) AND `Reference` = 0;
 
 UPDATE `creature_template` SET `lootid` = 0 WHERE `entry` IN (3388,240,246);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
