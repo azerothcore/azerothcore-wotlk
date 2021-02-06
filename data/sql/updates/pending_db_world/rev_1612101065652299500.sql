58254f6611d0ad9adc2c867d624359fae3a769ef
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-06 11:25:16 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2f35eed50944efc0a9e7ec6b1fe9c2722d4c29b8

--- data/sql/updates/pending_db_world/rev_1612101065652299500.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612101065652299500');
-
-UPDATE `page_text` SET `Text` = "L. F. has ordered increased aggression against the fools in the Alliance.  Below is a list of materials needed for an important mission against Southshore:$B$B1 week travel rations for 20 men$B20 black night suits$B20 pairs new shoes$B25 new swords$B30 throwing knives$B20 black masks$B10 vials inflammatory oil$B50 doses paralytic poison$B5 stolen Southshore guard uniforms, if available$B100 silver coins$B$B--Baron V.$BQuartermaster" WHERE `ID` = 371;
