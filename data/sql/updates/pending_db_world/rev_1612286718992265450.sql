990e4943bad54c5c64e7f6bf0b71c44b43c05211
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-06 17:28:09 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 61aa62bbbc0fb961813178c0feccb666094dd6c0

--- data/sql/updates/pending_db_world/rev_1612286718992265450.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612286718992265450');
-
--- Faster spawn rates for quest Secondhand Diving Gear
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid` IN
-(
-40774, -- Tool Kit
-40775 -- Damaged Diving Gear
-);
