c3a83c3618c4ccc1af19547a024c5c87c5beb145
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-06 21:32:50 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d8afa849e31d179116d2fdb412761792cb2028ed

--- data/sql/updates/pending_db_world/rev_1612528921944990905.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612528921944990905');
-
--- Lower respawn of Tablet of Will from 24h to 2s
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=40688;
