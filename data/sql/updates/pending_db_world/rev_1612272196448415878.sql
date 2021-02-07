8015de0c4be92b9cd6be5ae6688ee47222cfc5d1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-07 21:38:10 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): fd024e4661864476f504a3215985c63d2979eeb4

--- data/sql/updates/pending_db_world/rev_1612272196448415878.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612272196448415878');
-
--- Deserter Propaganda should respawn way faster
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=9175;
