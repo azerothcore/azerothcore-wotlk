4c558fd670dc1f8b3bde15d5bcf581876dfb14b9
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-08 23:13:47 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d0290d25fbd09af2b675a6a26632cd149a07d65d

--- data/sql/updates/pending_db_world/rev_1612441103905701673.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612441103905701673');
-
--- Faster respawn of Beginnings of the Undead Threat
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=11901;
