0f1633c9c506e8049ba759ca32a21bd70389b652
Stefano Borzì <stefanoborzi32@gmail.com>
2021-02-08 02:50:54 +0100

    fix(DB/Spawn): move outside a tree a rabid bear (#4480)

---
+++ data/sql/updates/pending_db_world/rev_1612709699286458572.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612709699286458572');
+
+DELETE FROM `creature` WHERE (`id` = 2164) AND (`guid` IN (37191));
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(37191, 2164, 1, 0, 0, 1, 1, 8840, 0, 6916.	, 9.62545, 24.915615, 0.547067, 275, 5, 0, 341, 0, 1, 0, 0, 0, '', 0);
+
