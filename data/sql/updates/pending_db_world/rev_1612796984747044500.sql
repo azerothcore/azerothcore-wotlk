9e7c23d8bd5ee9e3eadf8b4429468600dea2695c
Vox <ndinosaur1213@Live.com>
2021-02-08 11:12:21 -0500

    Update data/sql/updates/pending_db_world/rev_1612796984747044500.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1612796984747044500.sql
+++ data/sql/updates/pending_db_world/rev_1612796984747044500.sql
@@ -1,4 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612796984747044500');'
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612796984747044500');
 
 UPDATE `command` SET `help`='Syntax: .achievement checkall\r\nCheck all achievement criteria of the selected player.' WHERE `name`='achievement checkall';
-
