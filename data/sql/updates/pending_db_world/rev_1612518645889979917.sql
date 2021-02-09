c5400b301259e048cfb89d3e591bef565d51cec3
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-09 03:27:38 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 45f012a52bcc2e0dde2f141b2073a1d9c934e3ff

--- data/sql/updates/pending_db_world/rev_1612518645889979917.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612518645889979917');
-
--- Lower respawn of Rituals of Power from 24h to 2s
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=32237;
