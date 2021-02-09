5b64035d0f390f81c6d8137d5621ce704c631b30
Zoidwaffle <zoidwaffle@gmail.com>
2021-02-09 12:18:37 +0100

    Increase droprate for item Healthy Dragon Scale

---
+++ data/sql/updates/pending_db_world/rev_1612869459845259064.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612869459845259064');
+
+-- Increase drop rate of Healthy Dragon Scale
+
+UPDATE `creature_loot_template` SET `Chance`=20 WHERE `Item` = 13920;
