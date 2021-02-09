98a61b71e553cf8b9db3910656dd007449e750d5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-09 21:33:15 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 55c1ab8fbaa3a6aedd55a5bffbc23e86955909df

--- data/sql/updates/pending_db_world/rev_1612530048757741422.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612530048757741422');
-
--- Lower respawn of Thermaplugg's Safe from 24h to 2s
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=32387;
