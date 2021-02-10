7dd00dc0ae92b17257e42021960b979daca7e002
Zoidwaffle <zoidwaffle@gmail.com>
2021-02-10 13:56:37 +0100

    Remove invisible character

--- data/sql/updates/pending_db_world/rev_1612957477367642592.sql
+++ data/sql/updates/pending_db_world/rev_1612957477367642592.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612957477367642592');
 
 -- Lower respawn of and add more spawn locations for Tirisfal Pumpkin
-￼
+
 DELETE FROM `gameobject` WHERE `id`=375;
 
 INSERT INTO `gameobject` (`id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
