fba1504652ee99808992bfcbc188df29dbea54d1
Vox <ndinosaur1213@Live.com>
2021-02-11 10:30:07 -0500

    deletetext

---
+++ data/sql/updates/pending_db_world/rev_1613057319260756500.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613057319260756500');
+
+*/ Delete Cataclysm text from quest The Nordrassil Summit https://wow.gamepedia.com/The_Nordrassil_Summit
+*/
+
+DELETE FROM `npc_text` WHERE  `ID`=18268;
+DELETE FROM `broadcast_text` WHERE  `ID`=52965;
