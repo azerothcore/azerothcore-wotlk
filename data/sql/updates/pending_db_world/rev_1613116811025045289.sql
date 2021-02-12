1f4e317f518fdc96b964ecd9b2f65cca9619577a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-12 15:22:05 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): a63e41e5b39f53b1a00f83aaaf8b11da84f8976a

--- data/sql/updates/pending_db_world/rev_1613116811025045289.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613116811025045289');
-
--- Fix prequest requirement for Encrypted Scroll
-
-UPDATE `quest_template_addon` SET `PrevQuestID`=364 WHERE `ID`=3096;
