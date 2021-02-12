4d102787705fa5e1322fa023577dd5cdd08c6c53
Peter <kirkemann@gmail.com>
2021-02-12 16:13:09 +0100

    fix(DB/creature): Move Undertaker Mordo (#4548)

---
+++ data/sql/updates/pending_db_world/rev_1613071309294888253.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613071309294888253');
+
+-- Move Undertaker Mordo
+
+UPDATE `creature` SET `position_x`=1671.25, `position_y`=1678.31, `position_z`=120.72,`orientation`=0.051 WHERE `guid`=29803;
