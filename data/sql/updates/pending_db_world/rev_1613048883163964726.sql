61c0e5abb09a293beed6390b84d88113773a5342
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-12 14:21:29 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 0ce7f8df2e58f680b37122d6d33316cc4c84ffcf

--- data/sql/updates/pending_db_world/rev_1613048883163964726.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613048883163964726');
-
--- Move Megelon the Draenei starting NPC
-
-UPDATE `creature` SET `position_x`=-3962, `position_y`=-13926.32, `position_z`=101.13,`orientation`=4.1889 WHERE `guid`=57173;
