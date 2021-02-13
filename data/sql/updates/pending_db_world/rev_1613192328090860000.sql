7cd74793a1ac6af4c9bbb9aa74a45e713cf2dc87
Vox <ndinosaur1213@Live.com>
2021-02-13 00:21:08 -0500

    cleanup

--- data/sql/updates/pending_db_world/rev_1613192328090860000.sql
+++ data/sql/updates/pending_db_world/rev_1613192328090860000.sql
@@ -4,14 +4,12 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613192328090860000');
    Source: https://wow.gamepedia.com/Master_Wood, https://www.wowhead.com/npc=8383/master-wood
 */
 
-DELETE FROM `creature_text` WHERE `CreatureID`=8383 AND `GroupID`=211 AND `ID`=0;
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES (8383, 211, 0, 'Master Wood is not impressed.', 16, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Emote on Receive Emote');
-DELETE FROM `creature_text` WHERE `CreatureID`=8383 AND `GroupID`=210 AND `ID`=2;
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES (8383, 210, 2, 'Don\'t make me go medieval on you.', 12, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Say on Receive Emote');
-DELETE FROM `creature_text` WHERE `CreatureID`=8383 AND `GroupID`=210 AND `ID`=1;
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES (8383, 210, 1, 'Keep it up, $N, and I\'ll beat some manners into you.', 12, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Say on Receive Emote 2');
-DELETE FROM `creature_text` WHERE `CreatureID`=8383 AND `GroupID`=210 AND `ID`=0;
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES (8383, 210, 0, 'You\'re pushing it $N.', 12, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Say on Receive Emote 3');
+DELETE FROM `creature_text` WHERE `CreatureID`=8383;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
+(8383, 211, 0, 'Master Wood is not impressed.', 16, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Emote on Receive Emote'), 
+(8383, 210, 2, 'Don\'t make me go medieval on you.', 12, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Say on Receive Emote'), 
+(8383, 210, 1, 'Keep it up, $N, and I\'ll beat some manners into you.', 12, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Say on Receive Emote 2'), 
+(8383, 210, 0, 'You\'re pushing it $N.', 12, 0, 100, 0, 0, 0, 0, 0, 'Master Wood - Say on Receive Emote 3');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 8383;
 
