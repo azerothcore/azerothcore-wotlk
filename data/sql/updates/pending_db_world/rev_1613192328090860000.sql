9a0f76de367e37637cced3f95cec7b752cf829ce
Vox <ndinosaur1213@Live.com>
2021-02-13 00:42:32 -0500

    cooldown

--- data/sql/updates/pending_db_world/rev_1613192328090860000.sql
+++ data/sql/updates/pending_db_world/rev_1613192328090860000.sql
@@ -13,10 +13,10 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 8383;
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 8383) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4, 5));
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 8383) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(8383, 0, 0, 0, 22, 0, 100, 0, 77, 0, 0, 0, 0, 1, 210, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 77 - Say Randomized Line 210'),
-(8383, 0, 1, 0, 22, 0, 100, 0, 41, 0, 0, 0, 0, 1, 211, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 41 - Emote Line 211'),
-(8383, 0, 2, 0, 22, 0, 100, 0, 17, 0, 0, 0, 0, 5, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 17 - Play Emote 2'),
-(8383, 0, 3, 0, 22, 0, 100, 0, 101, 0, 0, 0, 0, 5, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 101 - Play Emote 3'),
-(8383, 0, 4, 0, 22, 0, 100, 0, 78, 0, 0, 0, 0, 5, 66, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 78 - Play Emote 66');
+(8383, 0, 0, 0, 22, 0, 100, 0, 77, 5000, 10000, 0, 0, 1, 210, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 77 - Say Randomized Line 210'),
+(8383, 0, 1, 0, 22, 0, 100, 0, 41, 5000, 10000, 0, 0, 1, 211, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 41 - Emote Line 211'),
+(8383, 0, 2, 0, 22, 0, 100, 0, 17, 5000, 10000, 0, 0, 5, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 17 - Play Emote 2'),
+(8383, 0, 3, 0, 22, 0, 100, 0, 101, 5000, 10000, 0, 0, 5, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 101 - Play Emote 3'),
+(8383, 0, 4, 0, 22, 0, 100, 0, 78, 5000, 10000, 0, 0, 5, 66, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Master Wood - Received Emote 78 - Play Emote 66');
