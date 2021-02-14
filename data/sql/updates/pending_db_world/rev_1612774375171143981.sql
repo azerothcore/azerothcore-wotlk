a06f183db5fb3060699aaa4f4d707ff91f2c6cf6
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-14 00:27:27 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 3d7663d310311580e86c4b46483b66847f73214d

--- data/sql/updates/pending_db_world/rev_1612774375171143981.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612774375171143981');
-
--- Lower respawn of Unfinished Painting
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=28477;
