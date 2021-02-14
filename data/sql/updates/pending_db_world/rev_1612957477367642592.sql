03e77ce66f2ccf55a4958faa4f36991ac4e95e2d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-14 01:39:17 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 5709fc4bec31ce1995216f3c2b9453021d8bd343

--- data/sql/updates/pending_db_world/rev_1612957477367642592.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612957477367642592');
-
--- Lower respawn rate for Tirisfal Pumpkin
-
-UPDATE `gameobject` SET `spawntimesecs`=300 WHERE `guid` IN (45042, 45043, 45157, 45194, 45195, 45196, 45197, 45198, 45200, 45201);
