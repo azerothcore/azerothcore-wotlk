f1c08f940c542250c780a11abeaf334fe7f21c5c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-14 00:05:47 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 25ef224515ce310b7b46da495871b69872959be3

--- data/sql/updates/db_world/2021_02_14_00.sql
+++ data/sql/updates/db_world/2021_02_14_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_02_13_03 -> 2021_02_14_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_02_13_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_02_13_03 2021_02_14_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1613146762649566200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613146762649566200');
 
 /* Add missing npc text for Verna Furlbrow  npc=238
@@ -17,3 +33,12 @@ INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
 */
 
 UPDATE `creature_template` SET `faction` = 35 WHERE (`entry` = 582);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
