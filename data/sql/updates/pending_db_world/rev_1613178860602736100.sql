97ea384cbe8ff38db60f6430eaf8ba35aa6a7503
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-14 05:10:18 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 98a0ff82979577c8c7488c6d8cfa1189d8534ce6

--- data/sql/updates/pending_db_world/rev_1613178860602736100.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613178860602736100');
-
-/* Add missing emotes for Maybell Maclure when offering quest Young Lovers=id106
-   Source: https://www.youtube.com/watch?v=Y6XYsgc2epI
-*/
-
-DELETE FROM `quest_details` WHERE `ID` = 106;
-INSERT INTO `quest_details` (`ID`, `Emote1`, `Emote2`, `EmoteDelay2`, `VerifiedBuild`) VALUES (106, 18, 20, 1, 12340);
