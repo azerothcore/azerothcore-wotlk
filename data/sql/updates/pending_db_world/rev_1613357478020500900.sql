0c0ec452f714d1b7fe8103bcf554104df747b019
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-15 14:54:32 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 60ee3500a650e4a3968f68f388e2619de0d23dd8

--- data/sql/updates/pending_db_world/rev_1613357478020500900.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613357478020500900');
-
-/* Move Copper Vein GUID 4788 to avoid being spawned inside of a map object
-*/
-
-DELETE FROM `gameobject` WHERE (`id` = 1731) AND (`guid` IN (4788));
-INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
-(4788, 1731, 1, 0, 0, 1, 1, 1125.928345, -4498.002441, 20.325047, 4.777278, 0, 0, 0.704938, 0.709268, 900, 100, 1, '', 0);
