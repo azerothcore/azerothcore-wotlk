f4cf2689fa40cf69bdc110ec221c40a18e07bb75
Vox <ndinosaur1213@Live.com>
2021-02-16 16:37:45 -0500

    build error

--- data/sql/updates/pending_db_world/rev_1613510617294494000.sql
+++ data/sql/updates/pending_db_world/rev_1613510617294494000.sql
@@ -5,3 +5,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613510617294494000');
 
 UPDATE `creature_template` SET `gossip_menu_id` = 5381 WHERE (`entry` = 12920);
 UPDATE `npc_text` SET `ID`='999999' WHERE  `ID`=6573;
+DELETE FROM `gossip_menu` WHERE  `MenuID`=5381 AND `TextID`=6573;
