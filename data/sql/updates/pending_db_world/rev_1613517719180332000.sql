54fa30a962cc0b2540e776511bbab7f11a7d4ad8
r0m1ntik <r.badanin94@yandex.ru>
2021-02-17 00:38:46 +0100

    Update for guards

--- data/sql/updates/pending_db_world/rev_1613517719180332000.sql
+++ data/sql/updates/pending_db_world/rev_1613517719180332000.sql
@@ -1,2 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613517719180332000');
 UPDATE `creature` SET `position_x` = -1201.63, `position_y` = -3099.24, `position_z` = 94.8781 WHERE (`guid` = 20720);
+-- his guards https://www.youtube.com/watch?v=pJm-Ja82IJM --
+UPDATE `creature` SET `position_x` = -1198.8, `position_y` = -3096.75, `position_z` = 94.5592 WHERE (`guid` = 20588);
+UPDATE `creature` SET `position_x` = -1198.92, `position_y` = -3101.53, `position_z` = 94.8262 WHERE (`guid` = 14007);
