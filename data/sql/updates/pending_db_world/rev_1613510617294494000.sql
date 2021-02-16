8774fca6ac7b3da2fbc463beea675a9ef26cd051
Vox <ndinosaur1213@Live.com>
2021-02-16 16:24:56 -0500

    gregvic

---
+++ data/sql/updates/pending_db_world/rev_1613510617294494000.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613510617294494000');
+
+/* Update Doctor Gregory Victor's gossip to be blizzlike.  Change entry of unknown gossip to avoid conflict.
+*/
+
+UPDATE `creature_template` SET `gossip_menu_id` = 5381 WHERE (`entry` = 12920);
+UPDATE `npc_text` SET `ID`='999999' WHERE  `ID`=6573;
