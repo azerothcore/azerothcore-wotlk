fe51e57b8969e8d90827b9e452836761d0810f00
Patrick Lewis <pat@lo5t.com>
2021-02-15 22:50:32 -0800

    fix(CORE): add include

--- src/server/scripts/Northrend/ChamberOfAspects/ObsidianSanctum/instance_obsidian_sanctum.cpp
+++ src/server/scripts/Northrend/ChamberOfAspects/ObsidianSanctum/instance_obsidian_sanctum.cpp
@@ -2,6 +2,7 @@
  * Originally written by Xinef - Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
 */
 
+#include "CreatureAIImpl.h"
 #include "obsidian_sanctum.h"
 #include "Player.h"
 #include "ScriptedCreature.h"
