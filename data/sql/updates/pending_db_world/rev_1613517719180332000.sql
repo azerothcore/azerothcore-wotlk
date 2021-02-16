96e4dd73263ad8086a46cd8818303e22fc9c54e9
r0m1ntik <r.badanin94@yandex.ru>
2021-02-17 00:23:18 +0100

    fix(DB/Creature): Position NPC Rocklance

---
+++ data/sql/updates/pending_db_world/rev_1613517719180332000.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613517719180332000');
+UPDATE `creature` SET `position_x` = -1201.63, `position_y` = -3099.24, `position_z` = 94.8781 WHERE (`guid` = 20720);
