e843425c4ac30ffd7f02845efbd80ddf1dc7100b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-17 03:18:52 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): a209eaf00ef16570ff318bdc9aa9476a5c24be68

--- data/sql/updates/db_world/2021_02_17_01.sql
+++ data/sql/updates/db_world/2021_02_17_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_02_17_00 -> 2021_02_17_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_02_17_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_02_17_00 2021_02_17_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1613512073209092300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613512073209092300');
 
 /* Commander Althea Ebonlocke Gossip
@@ -24,3 +40,12 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `p
 
 UPDATE `creature_text` SET `Text`='The news from Stormwind does not bode well....' WHERE  `CreatureID`=271 AND `GroupID`=0 AND `ID`=1;
 UPDATE `broadcast_text` SET `MaleText`='The news from Stormwind does not bode well....' WHERE  `ID`=71;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
