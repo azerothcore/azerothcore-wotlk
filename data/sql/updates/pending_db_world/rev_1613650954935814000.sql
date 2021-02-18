f680ecf8a6e04b785f5f721082943b0fb766156f
r0m1ntik <r.badanin94@yandex.ru>
2021-02-18 13:25:01 +0100

    fix(DB/Gameobject): 'Chemical Wagon' zhCN translation

---
+++ data/sql/updates/pending_db_world/rev_1613650954935814000.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613650954935814000');
+UPDATE `gameobject_template_locale` SET `name` = '药剂车' WHERE (`entry` = '201716') AND (`locale` = 'zhCN');
