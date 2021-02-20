1a74f020a7114c8d29e215b9f60135cc8dbd5b63
Zoidwaffle <zoidwaffle@gmail.com>
2021-02-20 23:42:52 +0100

    Rewrite comment a bit and add a co-author
    Co-authored-by: @adeutscher

--- data/sql/updates/pending_db_world/rev_1613853959150972140.sql
+++ data/sql/updates/pending_db_world/rev_1613853959150972140.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613853959150972140');
 
--- Captured Scarlet Zealot should despawn himself
+-- Captured Scarlet Zealot should die when event is triggered
 
 UPDATE `smart_scripts` SET `action_type`=51, `action_param1`=0 WHERE `entryorguid`=193100 AND `id`=7;
