e01a2cc7c374ead79e696ea0cee556dc36aeb0ae
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-21 18:54:30 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): f910d635b7337a78253db0286df145f65853c94d

--- data/sql/updates/db_world/2021_02_21_05.sql
+++ data/sql/updates/db_world/2021_02_21_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_02_21_04 -> 2021_02_21_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_02_21_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_02_21_04 2021_02_21_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1613843733957350900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613843733957350900');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10216;
@@ -14,3 +30,12 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGroup` = 2562) AND (`SourceEntry` = 1) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 7) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 356) AND (`ConditionValue2` = 1) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (15, 2562, 1, 0, 0, 7, 0, 356, 1, 0, 1, 0, 0, '', 'Gubber Blump - Quest: The Family and the Fishing Pole - Gossip Condition Hide Gossip');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
