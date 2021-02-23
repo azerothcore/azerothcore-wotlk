913d6f2803bdd8c335bc186a88288d7ee587125c
Zoidwaffle <zoidwaffle@gmail.com>
2021-02-23 10:48:51 +0100

    Targeting the wrong pool...

--- data/sql/updates/pending_db_world/rev_1614071599270991320.sql
+++ data/sql/updates/pending_db_world/rev_1614071599270991320.sql
@@ -10,9 +10,9 @@ INSERT INTO `pool_template` (`entry`,`max_limit`,`description`) VALUES
 DELETE FROM `pool_gameobject` WHERE `guid` IN (120324,120325,120363);
 
 INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUES
-(120324,11645,0,"Bloodmyst Isle, Copper Vein, spawn 1"),
-(120325,11645,0,"Bloodmyst Isle, Copper Vein, spawn 2"),
-(120363,11645,0,"Bloodmyst Isle, Copper Vein, spawn 3");
+(120324,11646,0,"Bloodmyst Isle, Copper Vein, spawn 1"),
+(120325,11646,0,"Bloodmyst Isle, Copper Vein, spawn 2"),
+(120363,11646,0,"Bloodmyst Isle, Copper Vein, spawn 3");
 
 -- Lower respawn of veins to 1 minute
 UPDATE `gameobject` SET `spawntimesecs`=60 WHERE `guid` IN (120324,120325,120363);
