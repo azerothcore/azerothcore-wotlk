a0e76c42dda7092009290681b6f37ce15f4a9dd4
BuildTools <unconfigured@null.spigotmc.org>
2021-02-24 08:16:54 -0700

    fix(DB): Fix Two More Troggs Texts and Missing Dark Iron Agent Text
    Fix accidental deletion of Dark Iron Agent texts
    Fix Caverndeep Burrower and Ambusher SAI to use correct GroupID

--- data/sql/updates/pending_db_world/rev_1613848536903926000.sql
+++ data/sql/updates/pending_db_world/rev_1613848536903926000.sql
@@ -28,6 +28,28 @@ UPDATE `creature_loot_template` SET `QuestRequired` = 0 WHERE `Item` = 9308;
 
 
 -- -------------------------------------------Creature Fixes------------------------------------------
+-- Caverndeep Burrower SAI
+SET @ENTRY := 6206;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @ENTRY AND `source_type` = 0 AND `id` = 2;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(6206,0,2,0,8,0,100,0,9798,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Caverndeep Burrower - On Spell Hit - Say Line 0');
+
+DELETE FROM `creature_text` WHERE `CreatureID` = @ENTRY;
+INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES 
+(@ENTRY,0,0,'%s is splashed by the blood and becomes irradiated!',16,0,100,0,0,0,3367,0,'Caverndeep Burrower ');
+
+
+-- Caverndeep Ambusher SAI
+SET @ENTRY := 6207;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @ENTRY AND `source_type` = 0 AND `id` = 2;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,0,2,0,8,0,100,0,9798,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Caverndeep Ambusher - On Spell Hit - Say Line 0');
+
+DELETE FROM `creature_text` WHERE `CreatureID` = @ENTRY;
+INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES 
+(@ENTRY,0,0,'%s is splashed by the blood and becomes irradiated!',16,0,100,0,0,0,3367,0,'Caverndeep Ambusher');
+
+
 -- Caverndeep Invader SAI
 SET @ENTRY := 6208;
 UPDATE `creature_template` SET `AIName` = "SmartAI" WHERE `entry` = @ENTRY;
@@ -56,14 +78,21 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,3,0,2,0,100,1,0,15,0,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,"Caverndeep Pillager - Between 0-15% Health - Flee For Assist (No Repeat)");
 
 
+-- Caverndeep Reaver SAI
+SET @ENTRY := 6211;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @ENTRY AND `source_type` = 0 AND `id` = 3;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,0,3,0,8,0,100,0,9798,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Caverndeep Reaver - On Spell Hit - Say Line 0');
+
+
 -- Dark Iron Agent SAI
 SET @ENTRY := 6212;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @ENTRY AND `source_type` = 0 AND `id` >= 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,1,0,4,0,20,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,"Dark Iron Agent - On Aggro - Say Line 1"),
+(@ENTRY,0,1,0,4,0,40,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,"Dark Iron Agent - On Aggro - Say Line 1"),
 (@ENTRY,0,2,0,8,0,100,0,9798,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,"Dark Iron Agent - On Spellhit 'Radiation' - Say Line 0");
 
-DELETE FROM `creature_text` WHERE `CreatureID` = @ENTRY;
+DELETE FROM `creature_text` WHERE `CreatureID` = @ENTRY AND `GroupID` = 0 AND `id` = 0;
 INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES 
 (@ENTRY,0,0,'%s is splashed by the blood and becomes irradiated!',16,0,100,0,0,0,3367,0,'Dark Iron Agent');
 
@@ -249,10 +278,3 @@ UPDATE `creature_template` SET `AIName` = "SmartAI" WHERE `entry` = @ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @ENTRY AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,38,0,100,0,1,1,0,0,0,41,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Dupe Bug - On Data Set - Despawn');
-
-
--- Gnomeregan Misc Creature Text updates
-DELETE FROM `creature_text` WHERE `CreatureID` IN (6206,6207);
-INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES 
-(6206,0,0,'%s is splashed by the blood and becomes irradiated!',16,0,100,0,0,0,3367,0,'Caverndeep Burrower '),
-(6207,0,0,'%s is splashed by the blood and becomes irradiated!',16,0,100,0,0,0,3367,0,'Caverndeep Ambusher');
