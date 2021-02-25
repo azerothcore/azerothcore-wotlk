ec038c3dfce259fc6e3cb0ea19e37c0eb1da9f07
Stifler82 <xspidey@hotmail.it>
2021-02-25 00:30:50 +0000

    fix(DB/Creature): Patroling-Deathguards-chain-aggro-range (#4632)

---
+++ data/sql/updates/pending_db_world/rev_1614022899539097500.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614022899539097500');
+
+UPDATE `creature_template` SET `unit_flags` = 4608, `type` = 6 WHERE `entry` IN (1735,5725,3547,2314,1743,1515,2114,1518,10055,1745,2309,4773,4731,2311,2137,33996,1746,1652,2135,2136,2118,2134,1738,1742,10665,10666);
+UPDATE `creature_template` SET `unit_flags` = 4608, `type` = 7 WHERE `entry` = 727;
