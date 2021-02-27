3d0a4288e0ce9579d90b86437a49c648eee16e87
AzerothCoreBot <azerothcorebot@gmail.com>
2021-02-27 11:44:54 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 39ece8d518891197decb10974dc9af6482f467d0

--- data/sql/updates/pending_db_world/rev_1614328114555323168.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614328114555323168');
-
--- Stop Young Moonkin from having Moonfire
-
-DELETE FROM `smart_scripts` WHERE `entryOrGuid`=10159 AND `source_type`=0;
-UPDATE `creature_template` SET `AIName`='' WHERE  `entry`=10159;
