6cb182cc7d94db9ff904bbd82c01ccfffb69b3e5
Francesco Borzì <borzifrancesco@gmail.com>
2021-03-02 01:34:54 +0100

    Update rev_1614625832426604950.sql

--- data/sql/updates/pending_db_world/rev_1614625832426604950.sql
+++ data/sql/updates/pending_db_world/rev_1614625832426604950.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614625832426604950');
 
-DELETE FROM reference_loot_template WHERE Item IN (10400, 10401, 10402);
+DELETE FROM `reference_loot_template` WHERE `Item` IN (10400, 10401, 10402);
 
