32c17f03a01b17f62ce1939b62228c18d2b5b4e0
Helias <stefanoborzi32@gmail.com>
2021-03-02 23:00:22 +0100

    fix(DB/Page): update legacy aspects

---
+++ data/sql/updates/pending_db_world/rev_1614722186740264425.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614722186740264425');
+
+UPDATE `acore_world`.`page_text` SET `Text` = 'The black dragon underwent a complete transformation; and in the end, Neltharion no longer existed. He became Deathwing, a creature of malice and hatred. No longer the Earth-Warder in the eyes of races like the high elves, he was renamed the Death Aspect and Xaxas in place of his titan-given name. Deathwing is the embodiment of chaos and evil, and is always hungry for destruction. He is arguably the first evil of Azeroth that appeared after the banishment of the Old Gods.' WHERE `ID` = 459;
+
