b5a13c2e76fb55d2a8fe542c961d4ab424030cbf
Cláudio Costa <claudio.daniel.f.c@gmail.com>
2021-03-05 04:29:37 +0000

    Draft ready

--- data/sql/updates/pending_db_world/rev_1614870494129873100.sql
+++ data/sql/updates/pending_db_world/rev_1614870494129873100.sql
@@ -3,5 +3,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614870494129873100');
 DELETE FROM `spell_target_position` WHERE `ID`=62501 AND `EffectIndex`=0;
 INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES (62501, 0, 603, 2035.95, -202.085, 432.687, 3.16408, 0);
 
-DELETE FROM `spell_script_names` WHERE spell_id = 62501;
+DELETE FROM `spell_script_names` WHERE spell_id = 62502;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('62501', 'spell_hodir_shatter_chest');
