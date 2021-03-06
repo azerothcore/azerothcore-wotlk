99d64d5647a3f481121ed0163924fbdf0f2144e3
Si1ker <55638679+Sombranator@users.noreply.github.com>
2021-03-06 01:02:38 -0600

    fix(DB/smart_scripts): Pyrewood Elder - Healing SAI

---
+++ data/sql/updates/pending_db_world/rev_1615014079681051900.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615014079681051900');
+
+UPDATE `smart_scripts` SET `action_param1`=2053 WHERE `entryorguid`=1895 AND `source_type`=0 AND `id`=3 AND `link`=0;
+
