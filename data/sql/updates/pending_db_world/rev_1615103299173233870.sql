2b9108880ff8a99e464212a29a3639181058b165
Peter <kirkemann@gmail.com>
2021-03-07 10:55:34 +0100

    Update data/sql/updates/pending_db_world/rev_1615103299173233870.sql
    Co-authored-by: Trond B. Krokli <38162891+illfated@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1615103299173233870.sql
+++ data/sql/updates/pending_db_world/rev_1615103299173233870.sql
@@ -1,5 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615103299173233870');
 
 -- Kolkar Drudge and Kolkar Outrunner spell resistances
-
 UPDATE `creature_template` SET `resistance2`=0, `resistance5`=0 WHERE `entry` IN (3119,3120);
