6a7155eb7e82309ac34a38eec65eb7663b0605a0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-07 12:37:07 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 1e90722a157b6e6ffedb1231b9a51e1a0563af8c

--- data/sql/updates/db_world/2021_03_07_13.sql
+++ data/sql/updates/db_world/2021_03_07_13.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_07_12 -> 2021_03_07_13
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_07_12';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_07_12 2021_03_07_13 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1614921496929991300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614921496929991300');
 -- ----------------------------------------Missing Diplomat Part 11 ---------------------------------------------------
 
@@ -365,3 +381,12 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 -- Orientation fixes
 UPDATE `creature` SET `orientation` = 2.44 WHERE `guid` = 31323;
 UPDATE `creature` SET `orientation` = 0.181 WHERE `guid` = 31324;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
