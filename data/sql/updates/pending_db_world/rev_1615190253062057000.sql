17f406509b1ccb0fb132a500e037928f87e92730
Stifler82 <xspidey@hotmail.it>
2021-03-08 08:38:30 +0000

    Update rev_1615190253062057000.sql

--- data/sql/updates/pending_db_world/rev_1615190253062057000.sql
+++ data/sql/updates/pending_db_world/rev_1615190253062057000.sql
@@ -11,5 +11,7 @@ INSERT INTO `smart_scripts` VALUES
 (6224, 0, 4, 0, 2, 0, 100, 1, 0, 90, 0, 0, 0, 11, 10348, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Leprous Machinesmith - Between 0-90% Health - Cast \'Tune Up\' (No Repeat)'),
 (6224, 0, 5, 0, 8, 0, 100, 0, 9798, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Leprous Machinesmith - On Spellhit \'Radiation\' - Say Line 0');
 
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceGroup`=7 AND `SourceEntry`=10732;
+
 DELETE FROM `spell_dbc` WHERE `ID` =10732;
 INSERT INTO `spell_dbc` VALUES (10732, 0, 0, 0, 65536, 16388, 2048, 67108864, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 15, 0, 31756, 0, 101, 0, 0, 26, 26, 1, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 6, 6, 6, 1, 1, 1, 2, 0, 0, 49, 24, 24, 0, 0, 0, 21, 21, 21, 0, 0, 0, 0, 0, 0, 13, 138, 61, 3000, 3000, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12656, 0, 320, 0, 0, 'Supercharge','','','','','','','','', 0, 0, 0, 0, 0, 0, 0, 16712190,'','','','','','','','','', 0, 0, 0, 0, 0, 0, 0, 16712190, 'Speeds the attack of nearby mechanical allies by $s2% and increases the Physical damage they deal by $s1 for $d.','','','','','','','','', 0, 0, 0, 0, 0, 0, 0, 16712190, 'Physical damage dealt is increased by $s1.', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
