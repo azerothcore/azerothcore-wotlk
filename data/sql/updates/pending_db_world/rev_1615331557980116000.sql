b22d159e7c30788b724127af19a8ffe5f870ce78
Si1ker <Si1ker@users.noreply.github.com>
2021-03-09 17:20:27 -0600

    fix(DB/creature_addon): Add "Double Attack" back on Anub'Rekhan and Gluth

---
+++ data/sql/updates/pending_db_world/rev_1615331557980116000.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615331557980116000');
+
+UPDATE `creature_addon` SET `auras`='19818' WHERE `guid`=127782; -- fix Gluth
+UPDATE `creature_addon` SET `auras`='18943' WHERE  `guid`=127814; -- fix Anub'Rekhan
+
