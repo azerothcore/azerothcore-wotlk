5c713ed6bc4dcf4decebadb4ec8ed739f7e9f6e7
Daniel Hackmann <daniel.hackmann@hs-bochum.de>
2021-03-09 20:39:38 +0100

    Forgot to update SQL

--- data/sql/updates/pending_db_world/rev_1615287196510790727.sql
+++ data/sql/updates/pending_db_world/rev_1615287196510790727.sql
@@ -6,4 +6,3 @@ DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (58984);
 -- Fix Shadowmeld
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gen_shadowmeld';
 INSERT INTO `spell_script_names` VALUES (58984, 'spell_gen_shadowmeld');
-INSERT INTO `spell_script_names` VALUES (59646, 'spell_gen_shadowmeld');
