69a0211fcab78776e423b915edcf6265b1b2d795
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-09 21:00:10 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 9d096233546e26c6aa9355e99c8f7cd04d31bc0f

--- data/sql/updates/pending_db_world/rev_1615313723387834900.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615313723387834900');
-
-UPDATE `creature_addon` SET `auras`='' WHERE `guid` IN (
-/* Anub'Rekhan */ 127814,
-/* Gluth */ 127782,
-/* Venom Stalkers */ 127868, 127869, 127870, 127871);
-
