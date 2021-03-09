64e5dd6cb50e56ebebb51beb5e3e9a7213aee1c0
Si1ker <Si1ker@users.noreply.github.com>
2021-03-09 12:26:19 -0600

    update instead delete

--- data/sql/updates/pending_db_world/rev_1615313723387834900.sql
+++ data/sql/updates/pending_db_world/rev_1615313723387834900.sql
@@ -1,4 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615313723387834900');
 
-DELETE FROM `creature_addon` WHERE  `guid`=127814;
+-- Anub'Rekhan
+UPDATE `creature_addon` SET `auras`='' WHERE `guid`=127814;
 
