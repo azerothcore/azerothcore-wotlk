722e3aa967f1a0ba2a0b60927ee9bce873c33890
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-09 23:18:18 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 0f3a1815611df901140bbfa1a4aee30d757bcaaf

--- data/sql/updates/pending_db_world/rev_1615103299173233870.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615103299173233870');
-
--- Kolkar Drudge and Kolkar Outrunner spell resistances
-UPDATE `creature_template` SET `resistance2`=0, `resistance5`=0 WHERE `entry` IN (3119,3120);
