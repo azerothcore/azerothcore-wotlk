d9c7f42c301686d81b34e8373d4b412cd15b6fa0
Si1ker <Si1ker@users.noreply.github.com>
2021-03-09 12:29:58 -0600

    gluth fixed

--- data/sql/updates/pending_db_world/rev_1615313723387834900.sql
+++ data/sql/updates/pending_db_world/rev_1615313723387834900.sql
@@ -3,3 +3,6 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615313723387834900');
 -- Anub'Rekhan
 UPDATE `creature_addon` SET `auras`='' WHERE `guid`=127814;
 
+-- Gluth
+UPDATE `creature_addon` SET `auras`='' WHERE `guid`=127782;
+
