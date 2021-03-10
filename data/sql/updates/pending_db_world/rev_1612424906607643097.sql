86ac12236136488123717017a0af9b7fba12f48a
Zoidwaffle <zoidwaffle@gmail.com>
2021-03-10 09:05:58 +0100

    Time to make a new checkout...

--- data/sql/updates/pending_db_world/rev_1612424906607643097.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1612424906607643097');
-
--- Lower respawn rate of Pitted Iron Chest
-
-UPDATE `gameobject` SET `spawntimesecs`=2 WHERE `guid`=32614;
