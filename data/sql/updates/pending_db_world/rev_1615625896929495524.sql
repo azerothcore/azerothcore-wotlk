e5b72f0b7e2e3339a408193847e1775fb65a8ec2
Zoidwaffle <zoidwaffle@gmail.com>
2021-03-13 10:19:54 +0100

    Add movement as well

--- data/sql/updates/pending_db_world/rev_1615625896929495524.sql
+++ data/sql/updates/pending_db_world/rev_1615625896929495524.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615625896929495524');
 
--- Decrease respawn rate to 10m
-UPDATE `creature` SET `spawntimesecs`=600 WHERE `guid`=2169;
+-- Decrease respawn rate and make him move
+UPDATE `creature` SET `spawntimesecs`=600,`MovementType`=1, `wander_distance`=60 WHERE `guid`=2169;
