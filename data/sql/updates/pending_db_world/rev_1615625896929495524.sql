04495ad4bc9d95e07311fa6857e4fbfee78a2de3
Zoidwaffle <zoidwaffle@gmail.com>
2021-03-13 12:15:17 +0100

    Make him stay closer to his spawn location

--- data/sql/updates/pending_db_world/rev_1615625896929495524.sql
+++ data/sql/updates/pending_db_world/rev_1615625896929495524.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615625896929495524');
 
 -- Decrease respawn rate and make him move
-UPDATE `creature` SET `spawntimesecs`=300,`MovementType`=1, `wander_distance`=60 WHERE `guid`=2169;
+UPDATE `creature` SET `spawntimesecs`=300,`MovementType`=1, `wander_distance`=10 WHERE `guid`=2169;
