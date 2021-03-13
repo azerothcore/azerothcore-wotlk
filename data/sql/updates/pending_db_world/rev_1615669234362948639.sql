a3bd91a885defca836067f5667f58e07b75207a3
otanikotani <otanikotani@gmail.com>
2021-03-13 16:08:32 -0500

    fix(DB/GameObject-Spawn): Solid Chests respawn time in Azshara

---
+++ data/sql/updates/pending_db_world/rev_1615669234362948639.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615669234362948639');
+
+UPDATE `gameobject` SET `spawntimesecs` = 7200 WHERE `guid` IN (48362, 48363,48364, 48365, 48366, 48367, 48368, 48369, 48370, 48371);
