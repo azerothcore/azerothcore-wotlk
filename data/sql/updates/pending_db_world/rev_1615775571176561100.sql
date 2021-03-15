668202a0ac9eec53752d813c46a8cc1775ec768c
Wojtek Nosal <w.nosal22@hotmail.com>
2021-03-15 03:42:22 +0100

    fix(DB/object) Archbishop Alonsus Faol misplaced plaque in Stormwind
    Closes AzerothCore issue #4642

---
+++ data/sql/updates/pending_db_world/rev_1615775571176561100.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615775571176561100');
+
+UPDATE `gameobject` SET `position_x` = '-8640.98', `position_y` = '759.85', `position_z` = '97.7' WHERE `guid` = 26628;
