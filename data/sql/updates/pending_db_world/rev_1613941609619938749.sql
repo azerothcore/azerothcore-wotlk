6b3f6ae9b4b33346eb509ea03f66b1721e4199a4
Zoidwaffle <zoidwaffle@gmail.com>
2021-03-16 15:49:03 +0100

    Remove blank line

--- data/sql/updates/pending_db_world/rev_1613941609619938749.sql
+++ data/sql/updates/pending_db_world/rev_1613941609619938749.sql
@@ -1,7 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613941609619938749');
 
 -- Fix party credit for burning The Scarlet Archive
-
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 176245) AND (`source_type` = 1) AND (`id` IN (0, 1, 2, 3, 4, 5));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (176245, 1, 0, 1, 64, 0, 100, 0, 1, 0, 0, 0, 0, 9, 176245, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 'Scarlet Archive - On Gossip Hello - Activate Gameobject'),
