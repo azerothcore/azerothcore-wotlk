1d7a26ddb4100a1ee4378823157607cf9d04d5cf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-16 23:26:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 215455784177453ba419a885927a9695398fed74

--- data/sql/updates/db_world/2021_03_16_03.sql
+++ data/sql/updates/db_world/2021_03_16_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_16_02 -> 2021_03_16_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_16_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_16_02 2021_03_16_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1614791799466997404'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614791799466997404');
 
 -- Rewrite positions for herbs for zone: Bloodmyst Isle
@@ -591,3 +607,12 @@ INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUE
 
 -- Respawn rates of gameobjects is 5 minutes
 UPDATE `gameobject` SET `spawntimesecs`=300 WHERE FIND_IN_SET (`guid`,@GUID);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
