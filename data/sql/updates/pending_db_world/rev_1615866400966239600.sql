e47d65c1230d6b9ad4d140830cf695020c7e83d5
Stifler82 <xspidey@hotmail.it>
2021-03-16 04:18:35 +0000

    Update rev_1615866400966239600.sql

--- data/sql/updates/pending_db_world/rev_1615866400966239600.sql
+++ data/sql/updates/pending_db_world/rev_1615866400966239600.sql
@@ -1,5 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615866400966239600');
 
-DELETE FROM `factiontemplate_dbc` WHERE (`ID` = 714);
-INSERT INTO `factiontemplate_dbc` VALUES
-(714,66,33,4,4,10,0,0,0,0,0,0,0,0);
+DELETE FROM `factiontemplate_dbc` WHERE ID = 714;
+INSERT INTO `factiontemplate_dbc` VALUES (714,66,33,4,4,10,0,0,0,0,0,0,0,0);
