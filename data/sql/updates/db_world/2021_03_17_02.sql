9c6ede651e48b1ee4b07ff12ba8178a104c24d77
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-17 01:18:37 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 78f027309f56fc98304106427e1bfe1d881d5b0b

--- data/sql/updates/db_world/2021_03_17_02.sql
+++ data/sql/updates/db_world/2021_03_17_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_17_01 -> 2021_03_17_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_17_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_17_01 2021_03_17_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1615134713336559993'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615134713336559993');
 
 -- Align resistances with vmangos for creatures level 1-20
@@ -29,3 +45,12 @@ UPDATE `creature_template` SET `resistance2`=0  WHERE `entry`=9524;  -- 'Kolkar 
 UPDATE `creature_template` SET `resistance5`=0  WHERE `entry`=9524;  -- 'Kolkar Invader' level 17, resistance5 was 30 (Shadow)
 UPDATE `creature_template` SET `resistance3`=15 WHERE `entry`=11321; -- 'Molten Elemental' level 13, resistance3 was 22 (Nature)
 UPDATE `creature_template` SET `resistance1`=0  WHERE `entry`=12431; -- 'Gorefang'' level 13, resistance1 was 70 (Holy)
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
