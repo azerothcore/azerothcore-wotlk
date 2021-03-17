907f6fbf98883bf862d8659375e4cc5c1a2a7250
Wojtek Nosal <w.nosal22@hotmail.com>
2021-03-17 22:23:50 +0100

    fix(DB) add gameobject addon for Archbishop Alonsus Faol plaque to enable rotation, position correction

--- data/sql/updates/pending_db_world/rev_1615775571176561100.sql
+++ data/sql/updates/pending_db_world/rev_1615775571176561100.sql
@@ -1,3 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615775571176561100');
 
-UPDATE `gameobject` SET `position_x` = '-8640.98', `position_y` = '759.85', `position_z` = '97.7' WHERE `guid` = 26628;
+DELETE FROM `gameobject_addon` WHERE `guid` = 26628;
+INSERT INTO `gameobject_addon` VALUES (26628, 0, 0);
+UPDATE `gameobject` SET `position_x` = '-8640.98', `position_y` = '760', `position_z` = '98.38' WHERE `guid` = 26628;
