933b0aa3852450e43ee9a2815154a8d2b8cfac37
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-19 23:27:58 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2031e55ec7025c25ac0d9ebba6f2e3efba809fec

--- data/sql/updates/db_world/2021_03_19_02.sql
+++ data/sql/updates/db_world/2021_03_19_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_19_01 -> 2021_03_19_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_19_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_19_01 2021_03_19_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1615125177017902050'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615125177017902050');
 
 UPDATE creature_classlevelstats SET attackpower = 9, damage_base = 0.1321, damage_exp1 = 0.1321, damage_exp2 = 0.1321 WHERE level = 1 AND class = 1;
@@ -3157,3 +3173,11 @@ ALTER TABLE creature_template DROP attackpower;
 ALTER TABLE creature_template DROP rangedattackpower;
 ALTER TABLE creature_template ADD COLUMN BaseVariance FLOAT DEFAULT 1 NOT NULL AFTER RangeAttackTime;
 ALTER TABLE creature_template ADD COLUMN RangeVariance FLOAT DEFAULT 1 NOT NULL AFTER BaseVariance;
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
