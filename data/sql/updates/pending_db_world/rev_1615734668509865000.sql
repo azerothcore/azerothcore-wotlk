6fc6d7ac4e96d84964c63ee15eed37378f4f9d5c
Cláudio Costa <claudio.daniel.f.c@gmail.com>
2021-03-20 15:34:22 +0000

    npc flags overriden

--- data/sql/updates/pending_db_world/rev_1615734668509865000.sql
+++ data/sql/updates/pending_db_world/rev_1615734668509865000.sql
@@ -8,7 +8,11 @@ DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGrou
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (15, 1802, 1, 0, 0, 47, 0, 3909, 8, 0, 0, 0, 0, '', '');
 
+-- Brewspewer has a White ballon icon when you over him regardless of his vendor flags
 UPDATE `creature_template` SET `IconName` = 'Speak' WHERE (`entry` = 7775);
 
 -- Removing his script name
 UPDATE `creature_template` SET `ScriptName` = '' WHERE (`entry` = 7775);
+
+-- This NPC has his npc flags overriden
+UPDATE `creature` SET `npcflag`='131' WHERE  `guid`=50145;
