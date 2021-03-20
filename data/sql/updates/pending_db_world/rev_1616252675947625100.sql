6f3d0c423119fa2831bbe6b47e70f66447414b5f
Knindza <knindzagxg@gmail.com>
2021-03-20 16:30:35 +0100

    Update rev_1616252675947625100.sql

--- data/sql/updates/pending_db_world/rev_1616252675947625100.sql
+++ data/sql/updates/pending_db_world/rev_1616252675947625100.sql
@@ -490,7 +490,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19943;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19943);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(19943, 0, 0, 0, 0, 0, 100, 0, 2600, 4000, 8600, 1000, 11, 37685, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Lashh\'an Talonite - In Combat - Cast \'37685\'');
+(19943, 0, 0, 0, 0, 0, 100, 0, 2600, 4000, 8600, 10000, 11, 37685, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Lashh\'an Talonite - In Combat - Cast \'37685\'');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19945;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19945);
