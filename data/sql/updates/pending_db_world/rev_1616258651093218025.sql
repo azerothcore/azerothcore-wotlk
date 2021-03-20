ffae0a696f60332109bb853819af48e647cab185
markvetx <78725204+markvetx@users.noreply.github.com>
2021-03-20 20:36:44 +0300

    Changed typo

--- data/sql/updates/pending_db_world/rev_1616258651093218025.sql
+++ data/sql/updates/pending_db_world/rev_1616258651093218025.sql
@@ -1,11 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616258651093218025');
 
 DELETE FROM `gameobject_addon` WHERE `guid` IN (5141, 5205, 5382, 5398, 5405, 5425);
-
-INSERT INTO `gameobject_addon` VALUES (5141, 0, 0);
-INSERT INTO `gameobject_addon` VALUES (5205, 0, 0);
-INSERT INTO `gameobject_addon` VALUES (5382, 0, 0);
-INSERT INTO `gameobject_addon` VALUES (5398, 0, 0);
-INSERT INTO `gameobject_addon` VALUES (5405, 0, 0);
-INSERT INTO `gameobject_addon` VALUES (5425, 0, 0);
-
+INSERT INTO `gameobject_addon` (`guid`, `invisibilityType`, `invisibilityValue`) VALUES 
+(5141, 0, 0),
+(5205, 0, 0),
+(5382, 0, 0),
+(5398, 0, 0),
+(5405, 0, 0),
+(5425, 0, 0);
