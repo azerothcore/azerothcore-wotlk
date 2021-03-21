3cbbc8d6477ec8cfc9c662267b7a98175693e6fc
Si1ker <Si1ker@users.noreply.github.com>
2021-03-20 23:07:53 -0600

    fix spawns, channeling, movement, enrage, etc

--- src/server/scripts/Northrend/Naxxramas/boss_kelthuzad.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_kelthuzad.cpp
@@ -39,6 +39,7 @@ enum Spells
     SPELL_FROST_BLAST                       = 27808,
     SPELL_CHAINS_OF_KELTHUZAD               = 28410, //28408 script effect
     SPELL_BERSERK                           = 28498,
+    SPELL_KELTHUZAD_CHANNEL                 = 29423,
 
     // Minions
     SPELL_FRENZY                            = 28468,
@@ -75,22 +76,24 @@ enum Event
     EVENT_SPELL_CHAINS                      = 12,
     EVENT_SUMMON_GUARDIAN_OF_ICECROWN       = 13,
     EVENT_FLOOR_CHANGE                      = 14,
+    EVENT_ENRAGE                            = 15,
+    EVENT_SPAWN_POOL                        = 16,
 
     // Minions
-    EVENT_MINION_SPELL_FRENZY               = 15,
-    EVENT_MINION_SPELL_MORTAL_WOUND         = 16,
-    EVENT_MINION_SPELL_BLOOD_TAP            = 17
+    EVENT_MINION_SPELL_FRENZY               = 16,
+    EVENT_MINION_SPELL_MORTAL_WOUND         = 17,
+    EVENT_MINION_SPELL_BLOOD_TAP            = 18
 };
 
-const Position SummonPositions[12] =
+const Position SummonGroups[12] =
 {
     // Portals
-    {3783.272705f, -5062.697266f, 143.711203f, 3.617599f}, //LEFT_FAR
-    {3730.291260f, -5027.239258f, 143.956909f, 4.461900f}, //LEFT_MIDDLE
-    {3683.868652f, -5057.281250f, 143.183884f, 5.237086f}, //LEFT_NEAR
-    {3759.355225f, -5174.128418f, 143.802383f, 2.170104f}, //RIGHT_FAR
-    {3700.724365f, -5185.123047f, 143.928024f, 1.309310f}, //RIGHT_MIDDLE
-    {3665.121094f, -5138.679199f, 143.183212f, 0.604023f}, //RIGHT_NEAR
+    {3783.272705f, -5062.697266f, 143.711203f, 3.617599f}, // LEFT_FAR
+    {3730.291260f, -5027.239258f, 143.956909f, 4.461900f}, // LEFT_MIDDLE
+    {3683.868652f, -5057.281250f, 143.183884f, 5.237086f}, // LEFT_NEAR
+    {3759.355225f, -5174.128418f, 143.802383f, 2.170104f}, // RIGHT_FAR
+    {3700.724365f, -5185.123047f, 143.928024f, 1.309310f}, // RIGHT_MIDDLE
+    {3665.121094f, -5138.679199f, 143.183212f, 0.604023f}, // RIGHT_NEAR
 
     // Middle
     {3769.34f, -5071.80f, 143.2082f, 3.658f},
@@ -99,6 +102,18 @@ const Position SummonPositions[12] =
     {3752.58f, -5161.82f, 143.2944f, 2.126f},
     {3702.83f, -5171.70f, 143.4356f, 1.305f},
     {3665.30f, -5141.55f, 143.1846f, 0.566f}
+};
+
+const Position SpawnPool[7] =
+{
+    // Portals
+    {3783.272705f, -5062.697266f, 143.711203f, 3.617599f}, // LEFT_FAR
+    {3730.291260f, -5027.239258f, 143.956909f, 4.461900f}, // LEFT_MIDDLE
+    {3683.868652f, -5057.281250f, 143.183884f, 5.237086f}, // LEFT_NEAR
+    {3759.355225f, -5174.128418f, 143.802383f, 2.170104f}, // RIGHT_FAR
+    {3700.724365f, -5185.123047f, 143.928024f, 1.309310f}, // RIGHT_MIDDLE
+    {3665.121094f, -5138.679199f, 143.183212f, 0.604023f}, // RIGHT_NEAR
+    {3651.729980f, -5092.620117f, 143.380005f, 6.050000f} // GATE
 };
 
 class boss_kelthuzad : public CreatureScript
@@ -131,14 +146,25 @@ public:
 
         void SpawnHelpers()
         {
-            // 6 rooms, 8 soldiers, 3 abominations and 1 weaver in each room
-            // middle positions in table starts from 6
+            // spawn at gate
+            me->SummonCreature(NPC_UNSTOPPABLE_ABOMINATION, 3656.19f, -5093.78f, 143.33f, 6.08, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000);// abo center
+            me->SummonCreature(NPC_UNSTOPPABLE_ABOMINATION, 3657.94f, -5087.68f, 143.60f, 6.08, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000);// abo left
+            me->SummonCreature(NPC_UNSTOPPABLE_ABOMINATION, 3655.48f, -5100.05f, 143.53f, 6.08, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000);// abo right
+            me->SummonCreature(NPC_SOUL_WEAVER, 3651.73f, -5092.62f, 143.38f, 6.05, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // soul behind
+            me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 3660.17f, -5092.45f, 143.37f, 6.07, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // ske front left
+            me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 3659.39f, -5096.21f, 143.29f, 6.07, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // ske front right
+            me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 3659.29f, -5090.19f, 143.48f, 6.07, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // ske left left
+            me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 3657.43f, -5098.03f, 143.41f, 6.07, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // ske right right
+            me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 3654.36f, -5090.51f, 143.48f, 6.09, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // ske behind left
+            me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 3653.35f, -5095.91f, 143.41f, 6.09, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 2000); // ske right right
+
+            // 6 rooms, 8 soldiers, 3 abominations and 1 weaver in each room | middle positions in table starts from 6
             for (uint8 i = 6; i < 12; ++i)
             {
                 for (uint8 j = 0; j < 8; ++j)
                 {
                     float angle = M_PI * 2 / 8 * j;
-                    me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, SummonPositions[i].GetPositionX() + 6 * cos(angle), SummonPositions[i].GetPositionY() + 6 * sin(angle), SummonPositions[i].GetPositionZ(), SummonPositions[i].GetOrientation(), TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000);
+                    me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, SummonGroups[i].GetPositionX() + 6 * cos(angle), SummonGroups[i].GetPositionY() + 6 * sin(angle), SummonGroups[i].GetPositionZ(), SummonGroups[i].GetOrientation(), TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000);
                 }
             }
             for (uint8 i = 6; i < 12; ++i)
@@ -146,16 +172,31 @@ public:
                 for (uint8 j = 1; j < 4; ++j)
                 {
                     float dist = j == 2 ? 0.0f : 8.0f; // second in middle
-                    float angle = SummonPositions[i].GetOrientation() + M_PI * 2 / 4 * j;
-                    me->SummonCreature(NPC_UNSTOPPABLE_ABOMINATION, SummonPositions[i].GetPositionX() + dist * cos(angle), SummonPositions[i].GetPositionY() + dist * sin(angle), SummonPositions[i].GetPositionZ(), SummonPositions[i].GetOrientation(), TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000);
+                    float angle = SummonGroups[i].GetOrientation() + M_PI * 2 / 4 * j;
+                    me->SummonCreature(NPC_UNSTOPPABLE_ABOMINATION, SummonGroups[i].GetPositionX() + dist * cos(angle), SummonGroups[i].GetPositionY() + dist * sin(angle), SummonGroups[i].GetPositionZ(), SummonGroups[i].GetOrientation(), TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000);
                 }
             }
             for (uint8 i = 6; i < 12; ++i)
             {
                 for (uint8 j = 0; j < 1; ++j)
                 {
-                    float angle = SummonPositions[i].GetOrientation() + M_PI;
-                    me->SummonCreature(NPC_SOUL_WEAVER, SummonPositions[i].GetPositionX() + 6 * cos(angle), SummonPositions[i].GetPositionY() + 6 * sin(angle), SummonPositions[i].GetPositionZ() + 0.5f, SummonPositions[i].GetOrientation(), TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000);
+                    float angle = SummonGroups[i].GetOrientation() + M_PI;
+                    me->SummonCreature(NPC_SOUL_WEAVER, SummonGroups[i].GetPositionX() + 6 * cos(angle), SummonGroups[i].GetPositionY() + 6 * sin(angle), SummonGroups[i].GetPositionZ() + 0.5f, SummonGroups[i].GetOrientation(), TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000);
+                }
+            }
+        }
+
+        void SummonHelper(uint32 entry, uint32 count)
+        {
+            for (uint8 i = 0; i < count; ++i)
+            {
+                if (Creature* cr = me->SummonCreature(entry, SpawnPool[urand(0, 6)], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000))
+                {
+                    if (Unit* target = SelectTargetFromPlayerList(100.0f))
+                    {
+                        cr->AI()->DoAction(ACTION_CALL_HELP_OFF);
+                        cr->AI()->AttackStart(target);
+                    }
                 }
             }
         }
@@ -232,11 +273,13 @@ public:
             me->RemoveAllAttackers();
             me->SetTarget(0);
             me->SetReactState(REACT_PASSIVE);
-            SpawnHelpers();
-            events.ScheduleEvent(EVENT_SUMMON_SOLDIER, 3200);
+            me->CastSpell(me, SPELL_KELTHUZAD_CHANNEL, false);
+            events.ScheduleEvent(EVENT_SPAWN_POOL, 5000);
+            events.ScheduleEvent(EVENT_SUMMON_SOLDIER, 6400);
             events.ScheduleEvent(EVENT_SUMMON_UNSTOPPABLE_ABOMINATION, 10000);
             events.ScheduleEvent(EVENT_SUMMON_SOUL_WEAVER, 24000);
             events.ScheduleEvent(EVENT_START_SECOND_PHASE, 228000);
+            events.ScheduleEvent(EVENT_ENRAGE, 900000);
             if (pInstance)
             {
                 if (GameObject* go = me->GetMap()->GetGameObject(pInstance->GetData64(DATA_KELTHUZAD_FLOOR)))
@@ -254,6 +297,10 @@ public:
         void JustSummoned(Creature* cr) override
         {
             summons.Summon(cr);
+            if (!cr->IsInCombat())
+            {
+                cr->GetMotionMaster()->MoveRandom(5);
+            }
         }
 
         void UpdateAI(uint32 diff) override
@@ -262,8 +309,11 @@ public:
                 return;
 
             events.Update(diff);
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
+            if (!me->HasAura(SPELL_KELTHUZAD_CHANNEL))
+            {
+                if (me->HasUnitState(UNIT_STATE_CASTING))
+                    return;
+            }
 
             switch (events.ExecuteEvent())
             {
@@ -278,37 +328,19 @@ public:
                         }
                     }
                     break;
+                case EVENT_SPAWN_POOL:
+                    SpawnHelpers();
+                    break;
                 case EVENT_SUMMON_SOLDIER:
-                    if (Creature* cr = me->SummonCreature(NPC_SOLDIER_OF_THE_FROZEN_WASTES, SummonPositions[urand(0, 5)], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000))
-                    {
-                        if (Unit* target = SelectTargetFromPlayerList(100.0f))
-                        {
-                            cr->AI()->DoAction(ACTION_CALL_HELP_OFF);
-                            cr->AI()->AttackStart(target);
-                        }
-                    }
+                    SummonHelper(NPC_SOLDIER_OF_THE_FROZEN_WASTES, 1);
                     events.RepeatEvent(3200);
                     break;
                 case EVENT_SUMMON_UNSTOPPABLE_ABOMINATION:
-                    if (Creature* cr = me->SummonCreature(NPC_UNSTOPPABLE_ABOMINATION, SummonPositions[urand(0, 5)], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000))
-                    {
-                        if (Unit* target = SelectTargetFromPlayerList(100.0f))
-                        {
-                            cr->AI()->DoAction(ACTION_CALL_HELP_OFF);
-                            cr->AI()->AttackStart(target);
-                        }
-                    }
+                    SummonHelper(NPC_UNSTOPPABLE_ABOMINATION, 1);
                     events.RepeatEvent(30000);
                     break;
                 case EVENT_SUMMON_SOUL_WEAVER:
-                    if (Creature* cr = me->SummonCreature(NPC_SOUL_WEAVER, SummonPositions[urand(0, 5)], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 20000))
-                    {
-                        if (Unit* target = SelectTargetFromPlayerList(100.0f))
-                        {
-                            cr->AI()->DoAction(ACTION_CALL_HELP_OFF);
-                            cr->AI()->AttackStart(target);
-                        }
-                    }
+                    SummonHelper(NPC_SOUL_WEAVER, 1);
                     events.RepeatEvent(30000);
                     break;
                 case EVENT_START_SECOND_PHASE:
@@ -328,6 +360,9 @@ public:
                     {
                         events.ScheduleEvent(EVENT_SPELL_CHAINS, 50000);
                     }
+                    break;
+                case EVENT_ENRAGE:
+                    me->CastSpell(me, SPELL_BERSERK, true);
                     break;
                 case EVENT_SPELL_FROST_BOLT_SINGLE:
                     me->CastSpell(me->GetVictim(), RAID_MODE(SPELL_FROST_BOLT_SINGLE_10, SPELL_FROST_BOLT_SINGLE_25), false);
@@ -408,7 +443,7 @@ public:
                     }
                     break;
                 case EVENT_SUMMON_GUARDIAN_OF_ICECROWN:
-                    if (Creature* cr = me->SummonCreature(NPC_GUARDIAN_OF_ICECROWN, SummonPositions[RAND(0, 1, 3, 4)]))
+                    if (Creature* cr = me->SummonCreature(NPC_GUARDIAN_OF_ICECROWN, SpawnPool[RAND(0, 1, 3, 4)]))
                     {
                         cr->AI()->Talk(EMOTE_GUARDIAN_APPEAR);
                         cr->AI()->AttackStart(me->GetVictim());
