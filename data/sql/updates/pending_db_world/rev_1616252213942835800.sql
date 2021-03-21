6954c0e8c366cd826be5864e11450e3f6bbb6593
Knindza <knindzagxg@gmail.com>
2021-03-21 19:52:39 +0100

    Update rev_1616252213942835800.sql

--- data/sql/updates/pending_db_world/rev_1616252213942835800.sql
+++ data/sql/updates/pending_db_world/rev_1616252213942835800.sql
@@ -20,7 +20,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 16967;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 16967);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(16967, 0, 0, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 32720, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Haal\'eshi Talonguard - Between 20-80% Health - Cast \'32720\' (No Repeat)');
+(16967, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 29651, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Haal\'eshi Talonguard - On Aggro - Cast \'29651\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19461;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19461);
