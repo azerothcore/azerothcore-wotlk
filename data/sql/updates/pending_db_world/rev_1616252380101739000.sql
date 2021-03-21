617e702ee21e17b73ba6be8fd9787f94e2db2fd6
Knindza <knindzagxg@gmail.com>
2021-03-21 11:43:10 +0100

    Update rev_1616252380101739000.sql

--- data/sql/updates/pending_db_world/rev_1616252380101739000.sql
+++ data/sql/updates/pending_db_world/rev_1616252380101739000.sql
@@ -57,7 +57,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 18122;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 18122);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(18122, 0, 0, 1, 4, 0, 100, 1, 0, 0, 0, 0, 11, 41363, 0, 0, 0, 0, 0, 19, 18122, 5, 0, 0, 0, 0, 0, 'Dreghood Drudge - On Aggro - Cast \'41363\' (No Repeat)');
+(18122, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 41363, 0, 0, 0, 0, 0, 19, 18122, 5, 0, 0, 0, 0, 0, 'Dreghood Drudge - On Aggro - Cast \'41363\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 20291;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 20291);
