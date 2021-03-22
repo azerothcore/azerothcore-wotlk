7503c7853664b35283fa48c4ae1e1320631ddcb5
Knindza <knindzagxg@gmail.com>
2021-03-22 17:03:46 +0100

    Update rev_1616252213942835800.sql

--- data/sql/updates/pending_db_world/rev_1616252213942835800.sql
+++ data/sql/updates/pending_db_world/rev_1616252213942835800.sql
@@ -20,7 +20,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 16967;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 16967);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(16967, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 29651, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Haal\'eshi Talonguard - On Aggro - Cast \'29651\' (No Repeat)');
+(16967, 0, 0, 0, 11, 0, 100, 1, 0, 0, 0, 0, 11, 29651, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Haal\'eshi Talonguard - On Respawn - Cast \'29651\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19461;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19461);
@@ -62,7 +62,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19408;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19408);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (19408, 0, 0, 0, 0, 0, 100, 0, 2500, 5000, 10000, 15000, 11, 15968, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Maiden of Pain - In Combat - Cast \'15968\''),
-(19408, 0, 1, 0, 2, 0, 100, 1, 10, 30, 0, 0, 11, 34086, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Maiden of Pain - Between 10-30% Health - Cast \'34086\' (No Repeat)');
+(19408, 0, 1, 0, 2, 0, 100, 1, 10, 30, 0, 0, 11, 40743, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Maiden of Pain - Between 10-30% Health - Cast \'40743\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 16879;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 16879);
@@ -87,10 +87,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19282;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19282);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(19282, 0, 0, 0, 0, 0, 100, 0, 5000, 10000, 15000, 20000, 11, 32026, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Shi\'aziv - In Combat - Cast \'32026\'');
+(19282, 0, 0, 0, 11, 0, 100, 1, 0, 0, 0, 0, 11, 29651, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Shi\'aziv - On Respawn - Cast \'29651\' (No Repeat)'),
+(19282, 0, 1, 0, 0, 0, 100, 0, 5000, 10000, 15000, 20000, 11, 32026, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Shi\'aziv - In Combat - Cast \'32026\''),
+(19282, 0, 2, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 32000, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Shi\'aziv - Between 20-80% Health - Cast \'32000\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19335;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19335);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(19335, 0, 0, 0, 0, 0, 100, 0, 5000, 10000, 15000, 20000, 11, 32026, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Yalqiz - In Combat - Cast \'32026\''),
-(19335, 0, 1, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 32000, 33, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Yalqiz - Between 20-80% Health - Cast \'32000\' (No Repeat)');
+(19335, 0, 0, 0, 11, 0, 100, 1, 0, 0, 0, 0, 11, 29651, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Yalqiz - On Respawn - Cast \'29651\' (No Repeat)'),
+(19335, 0, 1, 0, 0, 0, 100, 0, 5000, 10000, 15000, 20000, 11, 32026, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Yalqiz - In Combat - Cast \'32026\''),
+(19335, 0, 2, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 32000, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Subjugator Yalqiz - Between 20-80% Health - Cast \'32000\' (No Repeat)');
