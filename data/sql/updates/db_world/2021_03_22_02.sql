b8c2c2311d08b2b6332ce013ecc69d4ce7e33500
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-22 15:20:02 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 3c3f1a79a92bf76e0b4f90cb34d6afbea16ea01d

--- data/sql/updates/db_world/2021_03_22_02.sql
+++ data/sql/updates/db_world/2021_03_22_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_22_01 -> 2021_03_22_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_22_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_22_01 2021_03_22_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1616369823354830255'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616369823354830255');
 
 -- Update Incorrect spawn of Tin Vein in the Barrens
@@ -21,3 +37,12 @@ DELETE FROM `gameobject` WHERE `guid` in (13216, 64839, 56790, 64843, 65291);
 
 -- Delete nonexisting pool_objects in Western Plaguelands underwater
 DELETE FROM `pool_gameobject` where `guid` in (70262, 70263, 70264, 70265, 70266, 70267);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
