dc35d0ba50e0858a7df25dcae5c5447fd5f5e618
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-22 02:42:15 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 66ca61025d932f93291522cacfc2934f7d61a506

--- data/sql/updates/pending_db_world/rev_1609870134525124000.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1609870134525124000');
-UPDATE `spell_proc_event` SET `procFlags` = 279552|1024|16384|262144, `procEx` = 786432|262144|524288 WHERE `entry` = 64411;
