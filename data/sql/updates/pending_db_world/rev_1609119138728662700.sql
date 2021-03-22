58ea307980e653399c52ba12570bfdfdafbcfc61
Si1ker <Si1ker@users.noreply.github.com>
2021-03-21 22:52:39 -0600

    fix spanish translation for EMOTE_PHASE_TWO

--- data/sql/updates/pending_db_world/rev_1609119138728662700.sql
+++ data/sql/updates/pending_db_world/rev_1609119138728662700.sql
@@ -117,4 +117,6 @@ UPDATE `creature_text` SET `comment`='Sapphiron EMOTE_ENRAGE' WHERE `CreatureID`
 -- Guardians of Icecrown should be immune to all CC
 UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|1|2|4|8|32|64|256|512|1024|2048|4096|8192|65536|131072|524288|4194304|67108864|536870912 WHERE `entry` IN 
 (16441, 30057);
+-- Fix translation for Spanish
+UPDATE `broadcast_text_locale` SET `MaleText`='¡Kel\'Thuzad ataca!' WHERE `ID`=32803 AND `locale` IN ('esES', 'esMX');
 
