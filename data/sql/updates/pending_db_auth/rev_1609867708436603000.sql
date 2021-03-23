7dced8456f67862abee23fe88085fd1e27f993ec
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-23 22:37:42 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 850aaddf20bc7c566809700243f47173480f6ac7

--- data/sql/updates/pending_db_auth/rev_1609867708436603000.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1609867708436603000');
-ALTER TABLE `account_muted`
-CHARSET = 'utf8',
-MODIFY `mutedby` varchar(50) NOT NULL,
-MODIFY `mutereason` varchar(255) NOT NULL;
-ALTER TABLE `logs`
-CHARSET = 'utf8',
-MODIFY `string` text;
