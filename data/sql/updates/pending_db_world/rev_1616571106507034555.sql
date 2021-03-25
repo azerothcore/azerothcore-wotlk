c2237de5ae749338865049f4d2dc5c521491d24b
Zoidwaffle <zoidwaffle@gmail.com>
2021-03-25 08:31:26 +0100

    Remove bitmask in a fancy way

--- data/sql/updates/pending_db_world/rev_1616571106507034555.sql
+++ data/sql/updates/pending_db_world/rev_1616571106507034555.sql
@@ -1,10 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616571106507034555');
 
 -- Remove MECHANIC_SLOW_ATTACK mask
-UPDATE `creature_template` SET `mechanic_immune_mask`=608908883 WHERE `entry` IN
+UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask` &~128 WHERE `entry` IN
 (639, -- Edwin VanCleef
 643,  -- Sneed
 645,  -- Cookie
 646,  -- Mr. Smite
-647); -- Captain Greenskin
-UPDATE `creature_template` SET `mechanic_immune_mask`=608925267 WHERE `entry`=642; -- Sneed's Shredder
+647,  -- Captain Greenskin
+642); -- Sneed's Shredder
