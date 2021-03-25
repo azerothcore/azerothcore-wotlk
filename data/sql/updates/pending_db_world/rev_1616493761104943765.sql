983a9edcfc7dafb2b87e602b13c68a37b00c089c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-25 03:03:24 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2cf294fe5a1512e3788c27a26ebe5bfd9cd37c35

--- data/sql/updates/pending_db_world/rev_1616493761104943765.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616493761104943765');
-
--- Less blues from Mottled Razormaw/Scytheclaw
-UPDATE `creature_loot_template` SET `Chance`=0.5 WHERE `Entry`=1022 AND `Reference` IN (24065,24067);
