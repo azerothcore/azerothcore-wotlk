669a9459e14b24063f08d0bb4e16b74531432b61
r0m1ntik <r.badanin94@yandex.ru>
2021-03-26 17:13:36 +0100

    forgot old

--- data/sql/updates/pending_db_world/rev_1613508281301607000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1613508281301607000');
-DELETE FROM `spell_script_names` WHERE `spell_id` = -20335;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(-20335, 'spell_pal_heart_of_the_crusader');
