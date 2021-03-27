3e29ce2e50e969d7d4ca1cd8a86745e74301f3b7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-27 03:29:25 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 01f0020e147a4c061be3587a6c7d2d8f0537ddfe

--- data/sql/updates/db_world/2021_03_27_00.sql
+++ data/sql/updates/db_world/2021_03_27_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_25_00 -> 2021_03_27_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_25_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_25_00 2021_03_27_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1616747334374476238'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616747334374476238');
 
 -- Edwin VanCleef encounter
@@ -10,3 +26,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (639, 0, 6, 0, 2, 0, 100, 1, 0, 33, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Edwin VanCleef - Between 0-33% Health - Say Line 3 (No Repeat)'),
 (639, 0, 7, 0, 5, 0, 100, 0, 0, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Edwin VanCleef - On Killed Unit - Say Line 4'),
 (639, 0, 8, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 1, 5, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Edwin VanCleef - On Just Died - Say Line 5');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
