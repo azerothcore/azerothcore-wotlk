79c2335bedf941e62f8c3d17c4750a5c1efe6a99
AzerothCoreBot <azerothcorebot@gmail.com>
2021-03-28 22:07:42 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2f4b80bee051d581dfe2141e1ed7c4d99271bb8e

--- data/sql/updates/db_world/2021_03_28_03.sql
+++ data/sql/updates/db_world/2021_03_28_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_03_28_02 -> 2021_03_28_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_03_28_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_03_28_02 2021_03_28_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1616345174221959057'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616345174221959057');
 
 SET
@@ -32,3 +48,12 @@ INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUE
 
 -- Respawn rates of gameobjects
 UPDATE `gameobject` SET `spawntimesecs`=@RESPAWN WHERE FIND_IN_SET (`guid`,@GUID);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
