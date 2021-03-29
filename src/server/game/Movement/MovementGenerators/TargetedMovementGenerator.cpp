7b618c4ca393862fe8f0d1a3af82d53a4d29b2fb
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-03-29 18:24:13 +0200

    Fix Pet being slowed when owner is slowed

--- src/server/game/Movement/MovementGenerators/TargetedMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/TargetedMovementGenerator.cpp
@@ -365,7 +365,7 @@ void FollowMovementGenerator<Creature>::_updateSpeed(Creature* owner)
 {
     // pet only sync speed with owner
     /// Make sure we are not in the process of a map change (IsInWorld)
-    if (!IS_PLAYER_GUID(owner->GetOwnerGUID()) || !owner->IsInWorld() || !i_target.isValid() || i_target->GetGUID() != owner->GetOwnerGUID())
+    if (!IS_PLAYER_GUID(owner->GetOwnerGUID()) || !owner->IsInWorld() || !i_target.isValid() || i_target->GetGUID() != owner->GetOwnerGUID() || !(owner->GetOwner()->GetAuraEffectsByType(SPELL_AURA_MOD_DECREASE_SPEED).empty()))
         return;
 
     owner->UpdateSpeed(MOVE_RUN, true);
