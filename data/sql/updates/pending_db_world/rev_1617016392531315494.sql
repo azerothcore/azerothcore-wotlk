eed4d9274c4a3eda1c1266827620eabddb0e630f
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-03-29 13:29:51 +0200

    fix #5036 NPC's don't attack the player if he is in water
    Set the inhabitType of the NPCs around the waterhole to 3

---
+++ data/sql/updates/pending_db_world/rev_1617016392531315494.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617016392531315494');
+
+UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3282); -- Venture Co. Mercenary
+UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3283); -- Venture Co. Enforcer
+UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3284); -- Venture Co. Drudger
+UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3445); -- Supervisor Lugwizzle
+UPDATE creature_template SET InhabitType = 3 WHERE (entry = 5836); -- Engineer Whirleygig
+
