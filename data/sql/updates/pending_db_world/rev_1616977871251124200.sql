f1d004c831d13e8e5419897599cc59d0ef5fea28
Si1ker <Si1ker@users.noreply.github.com>
2021-03-28 20:59:31 -0600

    add/rework conditions

--- data/sql/updates/pending_db_world/rev_1616977871251124200.sql
+++ data/sql/updates/pending_db_world/rev_1616977871251124200.sql
@@ -8,4 +8,9 @@ UPDATE `creature_template` SET `DamageModifier`=8 WHERE `entry` IN (33989, 36067
 -- Add script into DB
 DELETE FROM `spell_script_names` WHERE  `spell_id`=64465 AND `ScriptName`='spell_yogg_saron_shadow_beacon';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (64465, 'spell_yogg_saron_shadow_beacon');
+-- Add/rework conditions for effect 'Titanic Storm'
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=64172;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(13,1,64172,0,0,31,0,3,33988,0,0,0,0,'','Titanic Storm'),
+(13,1,64172,0,1,31,0,3,36064,0,0,0,0,'','Titanic Storm');
 
