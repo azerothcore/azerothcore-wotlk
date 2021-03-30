3fe635b1051ba1e02faf64242fa85d66fb022407
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-03-30 22:30:13 +0200

    Update query

--- data/sql/updates/pending_db_world/rev_1617016392531315494.sql
+++ data/sql/updates/pending_db_world/rev_1617016392531315494.sql
@@ -1,8 +1,11 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617016392531315494');
 
-UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3282); -- Venture Co. Mercenary
-UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3283); -- Venture Co. Enforcer
-UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3284); -- Venture Co. Drudger
-UPDATE creature_template SET InhabitType = 3 WHERE (entry = 3445); -- Supervisor Lugwizzle
-UPDATE creature_template SET InhabitType = 3 WHERE (entry = 5836); -- Engineer Whirleygig
+UPDATE creature_template SET InhabitType = 3 WHERE entry IN (
+  3282, -- Venture Co. Mercenary
+  3283, -- Venture Co. Enforcer
+  3284, -- Venture Co. Drudger
+  3445, -- Supervisor Lugwizzle
+  5836  -- Engineer Whirleygig
+);
+
 
