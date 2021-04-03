599310a8d1907f8960084a7d4774a19c70d685ef
Knindza <knindzagxg@gmail.com>
2021-04-03 13:47:41 +0200

    Update rev_1616252834972147900.sql

--- data/sql/updates/pending_db_world/rev_1616252834972147900.sql
+++ data/sql/updates/pending_db_world/rev_1616252834972147900.sql
@@ -125,7 +125,7 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 21164);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (21164, 0, 0, 0, 0, 0, 100, 0, 5000, 15000, 25000, 35000, 11, 39262, 32, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Netharel - In Combat - Cast \'39262\''), 
 (21164, 0, 1, 0, 2, 0, 100, 1, 20, 80, 0, 0, 11, 37683, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Netharel - Between 20-80% Health - Cast \'37683\' (No Repeat)'),
-(21164, 0, 2, 0, 0, 0, 100, 0, 10000, 20000, 30000, 40000, 11, 39135, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Netharel - In Combat - Cast \'39135\'')
+(21164, 0, 2, 0, 0, 0, 100, 0, 10000, 20000, 30000, 40000, 11, 39135, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Netharel - In Combat - Cast \'39135\''),
 (21164, 0, 3, 0, 2, 0, 100, 1, 45, 50, 0, 0, 11, 36298, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Netharel - Between 45-50% Health - Cast \'36298\' (No Repeat)'); 
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 21168;
