d8c4795ec6ad100aaaffe032f3ee90650001de45
Nolt <nolt@users.noreply.github.com>
2021-04-04 23:37:37 +0200

    fix(DB) Spawn placement of mineral veins - Redridge Mountains (#5100)

---
+++ data/sql/updates/pending_db_world/rev_1617306326870988315.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617306326870988315');
+
+-- Redridge Mountains
+UPDATE `gameobject` SET `position_x`=-9200.57, `position_y`=-2441.51, `position_z`=60.5342 WHERE `guid`=5236;
+UPDATE `gameobject` SET `position_x`=-8999.95, `position_y`=-3221.55, `position_z`=112.057 WHERE `guid`=4755;
+UPDATE `gameobject` SET `position_x`=-9001.20, `position_y`=-3207.21, `position_z`=111.948 WHERE `guid`=5746;
+UPDATE `gameobject` SET `position_x`=-9041.66, `position_y`=-3307.45, `position_z`=107.945 WHERE `guid`=5199;
+UPDATE `gameobject` SET `position_x`=-8923.28, `position_y`=-2651.38, `position_z`=134.326 WHERE `guid`=5637;
