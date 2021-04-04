c2b8d78f3ebfb0d4cbf60c3636f26a9f1b841733
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-04 22:01:06 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 96fbc0eb7543d737b39ee0c9559b1ba54d85da2c

--- data/sql/updates/db_world/2021_04_04_02.sql
+++ data/sql/updates/db_world/2021_04_04_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_04_04_01 -> 2021_04_04_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_04_04_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_04_04_01 2021_04_04_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1615980645222325242'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1615980645222325242');
 
 -- Mass change respawns of various fishing pools
@@ -28,3 +44,12 @@ UPDATE `gameobject` SET `spawntimesecs`=3600 WHERE `id`=182956; -- School of Dar
 UPDATE `gameobject` SET `spawntimesecs`=3600 WHERE `id`=182953; -- Sporefish School
 UPDATE `gameobject` SET `spawntimesecs`=3600 WHERE `id`=182952; -- Steam Pump Flotsam
 -- UPDATE `gameobject` SET `spawntimesecs`=300 WHERE `id`=180248; -- School of Tastyfish
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
