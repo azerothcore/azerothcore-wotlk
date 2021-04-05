ae4c85412767add48727c257b538c7628f3fd688
Silker <61223313+Si1ker@users.noreply.github.com>
2021-04-04 19:34:24 -0600

    fix(DB/gameobject): Adjust position for Cooper Vein 18679 (#5132)

---
+++ data/sql/updates/pending_db_world/rev_1617559127512335600.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617559127512335600');
+
+UPDATE `gameobject` SET `position_x`=-8747.79, `position_y`=-2256.47, `position_z`=153.350, `orientation`=0.855903 WHERE `guid`=18679;
+
