ca73c3011457b5594bb66a0c8c95b7e6f340a339
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-05 12:52:58 +0200

    Update Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -5568,9 +5568,9 @@ void Player::DurabilityPointsLossAll(int32 points, bool inventory)
 void Player::DurabilityPointsLoss(Item* item, int32 points)
 {
     if (HasAuraType(SPELL_AURA_PREVENT_DURABILITY_LOSS))
-        {
-            return;
-        }
+    {
+        return;
+    }
 
     int32 pMaxDurability = item->GetUInt32Value(ITEM_FIELD_MAXDURABILITY);
     int32 pOldDurability = item->GetUInt32Value(ITEM_FIELD_DURABILITY);
