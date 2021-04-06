c2b8e478dfffb969f689959878949e2e859a270a
Silker <61223313+Si1ker@users.noreply.github.com>
2021-04-06 12:39:31 -0600

    fix(DB/gameobject): Adjust position for Copper Vein 5242 (#5159)

---
+++ data/sql/updates/pending_db_world/rev_1617674243069210400.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617674243069210400');
+
+UPDATE `gameobject` SET `position_x`=-9668.717, `position_y`=-2031.464, `position_z`=60.258, `orientation`=0.480889 WHERE `guid`=5242;
+
