d93c6969f2771e16066310feae389ab8aeac0d68
Silker <61223313+Si1ker@users.noreply.github.com>
2021-04-06 17:30:25 -0600

    fix(DB/smart_scripts): Adjust SAI for Elder Timberling (#5170)

---
+++ data/sql/updates/pending_db_world/rev_1617743115631291700.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617743115631291700');
+
+UPDATE `smart_scripts` SET `event_chance`=100, `event_param2`=50, `event_param3`=9000, `event_param4`=15000, `comment`='Elder Timberling - Between 0-50% Health - Cast Healing Wave' WHERE `entryorguid`=2030 AND `source_type`=0 AND `id`=2 AND `link`=0;
+
