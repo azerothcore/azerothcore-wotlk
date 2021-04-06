cae69421e205a1b9ac5f2abfc5c79dc888d9c2ef
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-06 21:04:13 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): deae802302b39c21e706a66959446be1257adaaa

--- data/sql/updates/pending_db_world/rev_1617688058640897400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617688058640897400');
-
-UPDATE `smart_scripts` SET `event_param1`=5000, `event_param2`=9000, `event_param3`=26000, `event_param4`=30000, `action_param1`=6016, `comment`='Dabyrie Laborer - In Combat - Cast Pierce Armor' WHERE  `entryorguid`=2582 AND `source_type`=0 AND `id`=0 AND `link`=0;
-
