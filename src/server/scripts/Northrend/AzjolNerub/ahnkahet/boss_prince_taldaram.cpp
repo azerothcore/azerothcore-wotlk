6efa2d21a8cb1d33a2aadbcc7ece3d0bb52a3164
sanctum32 <andriuspel@gmail.com>
2021-04-06 05:31:44 +0300

    Scripts/AhnKahet: reordered included headers...

--- src/server/scripts/Northrend/AzjolNerub/ahnkahet/boss_prince_taldaram.cpp
+++ src/server/scripts/Northrend/AzjolNerub/ahnkahet/boss_prince_taldaram.cpp
@@ -2,8 +2,8 @@
  * Originally written by Xinef - Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
 */
 
-#include "ahnkahet.h"
 #include "ScriptMgr.h"
+#include "ahnkahet.h"
 #include "ScriptedCreature.h"
 #include "SpellInfo.h"
 #include "SpellScript.h"
