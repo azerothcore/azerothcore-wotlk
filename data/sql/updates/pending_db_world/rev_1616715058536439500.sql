a298803d7205bacbd1fbb9a923adc0d9f88472a0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-07 11:50:05 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 07f2087683e11065277e87cf784fe5e2ba074bf6

--- data/sql/updates/pending_db_world/rev_1616715058536439500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616715058536439500');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gen_clear_debuffs';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
-(34098, 'spell_gen_clear_debuffs');
