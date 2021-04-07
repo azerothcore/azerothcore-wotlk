fb49b9710eb7d816c0609ad6369b1fd2ef6115cb
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-07 19:58:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 4765ab3d590c6755f8bed6ed319b0581e60ab7ef

--- data/sql/updates/pending_db_world/rev_1617791300086571700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617791300086571700');
-
-UPDATE `instance_template` SET `script` = 'instance_hellfire_ramparts' WHERE `map` = 543;
-UPDATE `instance_template` SET `script` = 'instance_utgarde_pinnacle' WHERE `map` = 575;
