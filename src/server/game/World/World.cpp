54e371e0e7fa2911eb02ff8a3c042d51e7d4ef5b
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-04-08 13:00:45 +0200

    feat(Core/PvP): Set 30 sec timer before turn off FFA PvP flag. (#5090)

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1240,6 +1240,8 @@ void World::LoadConfigSettings(bool reload)
     m_int_configs[CONFIG_ITEMDELETE_QUALITY]    = sConfigMgr->GetOption<int32>("ItemDelete.Quality", 3);
     m_int_configs[CONFIG_ITEMDELETE_ITEM_LEVEL] = sConfigMgr->GetOption<int32>("ItemDelete.ItemLevel", 80);
 
+    m_int_configs[CONFIG_FFA_PVP_TIMER] = sConfigMgr->GetOption<int32>("FFAPvPTimer", 30);
+
     ///- Read the "Data" directory from the config file
     std::string dataPath = sConfigMgr->GetOption<std::string>("DataDir", "./");
     if (dataPath.empty() || (dataPath.at(dataPath.length() - 1) != '/' && dataPath.at(dataPath.length() - 1) != '\\'))
