7cec0c29bca1a157d208a7956094da8a1296981b
nolt <nolt@wizzi.pl>
2021-04-08 23:14:05 +0200

    fixed in durotar from another report

--- data/sql/updates/pending_db_world/rev_1617913282545327642.sql
+++ data/sql/updates/pending_db_world/rev_1617913282545327642.sql
@@ -5,3 +5,6 @@ DELETE FROM  `gameobject` WHERE `guid`=35525;
 
 -- Darkshore
 DELETE FROM  `gameobject` WHERE `guid` in (4729, 5079, 5055, 5078, 5079, 5392, 5351);
+
+-- Durotar
+DELETE FROM  `gameobject` WHERE `guid`=12383;
