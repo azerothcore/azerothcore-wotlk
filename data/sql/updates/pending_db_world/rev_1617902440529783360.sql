627208b4289c0227261a0ebcd0b6b265896008eb
Braden <braden.beer@gmail.com>
2021-04-08 10:39:33 -0700

    Fixed a typo in both queries

--- data/sql/updates/pending_db_world/rev_1617902440529783360.sql
+++ data/sql/updates/pending_db_world/rev_1617902440529783360.sql
@@ -1,9 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617902440529783360');
 
 # Remove Flesh Piercer from Reference Table
-DELETE FROM `reference_loot_table` WHERE `Entry` = 24068 AND `Item` = 3336;
+DELETE FROM `reference_loot_template` WHERE `Entry` = 24068 AND `Item` = 3336;
 
 # Adds Flesh Piercer to Daggerspine Shorestalkers and Screamers loot tables
-INSERT INTO `creature_loot_table` VALUES
+INSERT INTO `creature_loot_template` VALUES
 (2368, 3336, 0, 1.5, 0, 1, 0, 1, 1, "Daggerspine Shorestalker - Flesh Piercer"),
 (2370, 3336, 0, 1.1, 0, 1, 0, 1, 1, "Daggerspine Screamer - Flesh Piercer");
