99302447534fa37a170cc918dc6b8f5a05cc0104
Delcidx <fdelcid83@gmail.com>
2021-04-08 22:04:23 -0600

    Delete comment

--- data/sql/updates/pending_db_world/rev_1617936479961800600.sql
+++ data/sql/updates/pending_db_world/rev_1617936479961800600.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617936479961800600');
 
---Durotar Copper Veins--
+
 UPDATE `gameobject` SET `position_x`=1234.406, `position_y`=-4135.120, `position_z`=26.184, `orientation`=0.517706 WHERE `guid`=12381;
 
 UPDATE `gameobject` SET `position_x`=857.312, `position_y`=-4118.451, `position_z`=26.625, `orientation`=2.861797 WHERE `guid`=5361;
