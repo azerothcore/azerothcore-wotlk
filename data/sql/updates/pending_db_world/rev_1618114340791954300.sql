e9e169849e2dca8723bdce37d937a4d888ff9ca1
unknown <vadderandur@gmail.com>
2021-04-10 23:25:18 -0500

    hairs Fixes

---
+++ data/sql/updates/pending_db_world/rev_1618114340791954300.sql
@@ -0,0 +1,55 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618114340791954300');
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11051;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11040;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11043;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11045;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11046;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11047;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11048;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11049;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11050;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11051;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11052;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11062;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11063;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11061;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11060;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11132;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11053;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11057;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11056;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11055;
+
+UPDATE `gameobject` SET `id` = 24681 WHERE `guid` = 11054;
+
+DELETE FROM `gameobject` WHERE `guid` = 11042;
+
+DELETE FROM `gameobject` WHERE `guid` = 11041;
+
+DELETE FROM `gameobject` WHERE `guid` = 11044;
+
+DELETE FROM `gameobject` WHERE `guid` = 11039;
+
+DELETE FROM `gameobject` WHERE `guid` = 11058;
+
+DELETE FROM `gameobject` WHERE `guid` = 11059;
