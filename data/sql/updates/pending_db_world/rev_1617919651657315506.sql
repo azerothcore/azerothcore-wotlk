40b951fbf58119a9c63281e8a690a309566e6435
Nolt <nolt@users.noreply.github.com>
2021-04-11 12:02:49 +0200

    fix(DB/Creature): Timberstrider Fledgling undermap (#5201)

---
+++ data/sql/updates/pending_db_world/rev_1617919651657315506.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617919651657315506');
+
+-- Azuremyst Isle - Timberstrider Fledgling spawn
+UPDATE `creature` SET `position_x`=-4660.85, `position_y`=-12176.9, `position_z`=31.6266, `orientation`=0.746134 WHERE `guid`=62767 and `id`=17372;
