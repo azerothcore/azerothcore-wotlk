f712ecd5dca51383c60261ed22efb99dec4d0ebd
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-11 23:31:54 +0200

    Update whitespace_remover.sh

--- apps/whitespace_remover/whitespace_remover.sh
+++ apps/whitespace_remover/whitespace_remover.sh
@@ -2,4 +2,5 @@
 cd "E:\Azerothcore-wotlk" 
 
 # Change '*.cpp' and '*.h' to the extension you want to remove whitespaces in.
-find -name '*.cpp' -or -name '*.h' -print0 | xargs -r0 sed -e 's/[[:blank:]]\+$//' -i
+find -name '*.cpp' -print0 | xargs -r0 sed -e 's/[[:blank:]]\+$//' -i
+find -name '*.h' -print0 | xargs -r0 sed -e 's/[[:blank:]]\+$//' -i
