7d5188033684a13b2c5e3d0e39ba7150963902fb
v.tihomirov <vladislav@mindsys.org>
2021-04-12 22:14:03 +0300

    Fix query param typo

--- data/sql/updates/pending_db_world/rev_1617974280334682621.sql
+++ data/sql/updates/pending_db_world/rev_1617974280334682621.sql
@@ -13,7 +13,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @ENTRY;
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = @ENTRY);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(@ENTRY, 0, 0, 0, 0, 0, 25, 0, 18000, 20000g, 20000, 20000, 0, 11, 11838, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Splintered Skeleton - In Combat - Cast \'Serverside - Hate to Zero\'');
+(@ENTRY, 0, 0, 0, 0, 0, 25, 0, 18000, 20000, 20000, 20000, 0, 11, 11838, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Splintered Skeleton - In Combat - Cast \'Serverside - Hate to Zero\'');
 
 -- Set Creature spawn locations
 DELETE FROM `creature` WHERE (`id` = @ENTRY);
