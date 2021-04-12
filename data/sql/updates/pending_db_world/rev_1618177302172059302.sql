b9025d9149b21e81c5cee48d3ac6fdaa4e17eb0f
Stefano Borzì <stefanoborzi32@gmail.com>
2021-04-13 00:01:40 +0200

    Update rev_1618177302172059302.sql

--- data/sql/updates/pending_db_world/rev_1618177302172059302.sql
+++ data/sql/updates/pending_db_world/rev_1618177302172059302.sql
@@ -1,6 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618177302172059302');
 
 DELETE FROM `gameobject` WHERE `id`=2046 AND `guid`=8871;
+DELETE FROM `pool_gameobject` WHERE `guid` IN (8871);
+
 
 DELETE FROM `gameobject` WHERE (`id` = 1623) AND (`guid` IN (4214));
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
