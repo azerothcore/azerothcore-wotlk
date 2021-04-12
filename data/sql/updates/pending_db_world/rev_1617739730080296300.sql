b48dfb66e013ee548afcf1df7c5072743059163d
sanctum32 <andriuspel@gmail.com>
2021-04-12 12:33:11 +0300

    DB/MoltenCore: adjusted "Flamewaker Protector"(12119) timers

--- data/sql/updates/pending_db_world/rev_1617739730080296300.sql
+++ data/sql/updates/pending_db_world/rev_1617739730080296300.sql
@@ -13,3 +13,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceGroup`=3 AND `SourceEntry`=20482 AND `SourceId`=0 AND `ElseGroup`=0 AND `ConditionTypeOrReference`=31 AND `ConditionTarget`=0 AND `ConditionValue1`=3 AND `ConditionValue2`=12099 AND `ConditionValue3`=0;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 3, 20482, 0, 0, 31, 0, 3, 12099, 0, 0, 0, 0, '', 'Firesworn Eruption Trigger - should target only Fireworn');
+
+-- Timers update for NPC "Flamewaker Protector"
+UPDATE `smart_scripts` SET `event_param1`='5000', `event_param2`='5000', `event_param3`='6500', `event_param4`='6500' WHERE  `entryorguid`=12119 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `event_param1`='5000', `event_param2`='10000', `event_param3`='7000', `event_param4`='7000' WHERE  `entryorguid`=12119 AND `source_type`=0 AND `id`=1 AND `link`=0;
