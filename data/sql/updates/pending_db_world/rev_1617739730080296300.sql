9de6b45072767b1e88f222d83e76d6a41934696c
sanctum32 <andriuspel@gmail.com>
2021-04-12 16:06:54 +0300

    DB/MoltenCore: Timers update for NPC "Flamewaker"

--- data/sql/updates/pending_db_world/rev_1617739730080296300.sql
+++ data/sql/updates/pending_db_world/rev_1617739730080296300.sql
@@ -22,3 +22,8 @@ UPDATE `smart_scripts` SET `event_param1`='5000', `event_param2`='10000', `event
 DELETE FROM `creature_text` WHERE `CreatureID`=12057 AND `GroupID`=0 AND `ID`=0;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (12057, 0, 0, '%s forces one of his Firesworn minions to erupt!', 41, 0, 100, 0, 0, 0, 8254, 0, 'Garr EMOTE_MASS_ERRUPTION');
+
+-- Timers update for NPC "Flamewaker"
+UPDATE `smart_scripts` SET `event_param1`='3000', `event_param2`='6000', `event_param3`='10000', `event_param4`='13000' WHERE  `entryorguid`=11661 AND `source_type`=0 AND `id`=1 AND `link`=0;
+UPDATE `smart_scripts` SET `event_param1`='3000', `event_param3`='4000', `event_param4`='6000' WHERE  `entryorguid`=11661 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `event_param1`='4000', `event_param2`='9000', `event_param3`='5000', `event_param4`='8000' WHERE  `entryorguid`=11661 AND `source_type`=0 AND `id`=2 AND `link`=0;
