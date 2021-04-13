93871e72dac73bf60cd5681fa5c690616855093f
Francesco Borzì <borzifrancesco@gmail.com>
2021-04-13 10:57:07 +0200

    fix(DB/Quest): Noblegarden RewardMoney (#5268)

---
+++ data/sql/updates/pending_db_world/rev_1618275651812886000.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618275651812886000');
+
+UPDATE `quest_template` SET `RewardMoney` = 900 WHERE `ID` IN (13479, 13480, 13483, 13484, 13502, 13503);
