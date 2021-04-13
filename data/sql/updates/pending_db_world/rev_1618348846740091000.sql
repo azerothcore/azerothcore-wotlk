2d9ebef1a33ad51bdee79e88bcaecf752500a51d
Patrick Lewis <Patrick.Lewis@modusbox.com>
2021-04-13 15:29:12 -0700

    fix(SQL): WIP

--- data/sql/updates/pending_db_world/rev_1618348846740091000.sql
+++ data/sql/updates/pending_db_world/rev_1618348846740091000.sql
@@ -1,10 +1,20 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618348846740091000');
 
 -- Shadowfang Keep
-UPDATE `dungeon_access_template` SET `max_level` = 26  WHERE `map_id` = 33;
+UPDATE `dungeon_access_template` SET `max_level` = 26, `min_level` = 16 WHERE `map_id` = 33;
 -- Stormwind Stockade
-UPDATE `dungeon_access_template` SET `max_level` = 30  WHERE `map_id` = 34;
+UPDATE `dungeon_access_template` SET `max_level` = 30, `min_level` = 20 WHERE `map_id` = 34;
 -- Deadmines
-UPDATE `dungeon_access_template` SET `max_level` = 25  WHERE `map_id` = 36;
+UPDATE `dungeon_access_template` SET `max_level` = 25, `min_level` = 15 WHERE `map_id` = 36;
 -- Wailing Caverns
-UPDATE `dungeon_access_template` SET `max_level` = 25  WHERE `map_id` = 43;
+UPDATE `dungeon_access_template` SET `max_level` = 25, `min_level` = 15 WHERE `map_id` = 43;
+-- Razorfen Kraul
+UPDATE `dungeon_access_template` SET `max_level` = 32, `min_level` = 22 WHERE `map_id` = 47;
+-- Blackfathom Deeps
+UPDATE `dungeon_access_template` SET `max_level` = 29, `min_level` = 19 WHERE `map_id` = 48;
+-- Uldaman
+UPDATE `dungeon_access_template` SET `max_level` = 45, `min_level` = 35 WHERE `map_id` = 70;
+-- Gnomeregan
+UPDATE `dungeon_access_template` SET `max_level` = 33, `min_level` = 23 WHERE `map_id` = 90;
+-- Sunken Temple
+UPDATE `dungeon_access_template` SET `max_level` = 55, `min_level` = 45 WHERE `map_id` = 109;
