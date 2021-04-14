28bbe433037c401cb8687197e59c4fcf2974f42a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-14 15:16:03 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 37793bd23813f24160988678877cfac2be399250

--- data/sql/updates/pending_db_world/rev_1618370524737999000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618370524737999000');
-
-UPDATE `smart_scripts` SET `action_type`=39, `action_param1`=40, `comment`='Deviate Guardian - Between 0-15% Health - Call for help' WHERE `entryorguid`=3637 AND `source_type`=0 AND `id`=0 AND `link`=1;
-
