0ad36bd6c4e0d79dc5f0c0d9071ef666ad2a677b
Chaouki Dhib <chaodhib@gmail.com>
2021-04-16 00:48:15 +0200

    fix codestyle

--- src/common/Utilities/CircularBuffer.h
+++ src/common/Utilities/CircularBuffer.h
@@ -1,9 +1,7 @@
 /*
- * This file was taken from
+ * This file was based on
  * https://embeddedartistry.com/blog/2017/05/17/creating-a-circular-buffer-in-c-and-c/
  * https://github.com/embeddedartistry/embedded-resources/blob/master/examples/cpp/circular_buffer.cpp
- * and adapted
- * 
  * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
  * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
