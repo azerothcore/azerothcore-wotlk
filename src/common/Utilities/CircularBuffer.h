a90f33a53f6b95672f5b31876e3bc67b4b488ec6
Kargatum <dowlandtop@yandex.com>
2021-04-16 06:11:14 +0700

    Update CircularBuffer.h

--- src/common/Utilities/CircularBuffer.h
+++ src/common/Utilities/CircularBuffer.h
@@ -1,10 +1,9 @@
 /*
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
+ *
  * This file was based on
  * https://embeddedartistry.com/blog/2017/05/17/creating-a-circular-buffer-in-c-and-c/
  * https://github.com/embeddedartistry/embedded-resources/blob/master/examples/cpp/circular_buffer.cpp
- * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  */
 
 #ifndef AZEROTHCORE_CIRCULAR_BUFFER_H
