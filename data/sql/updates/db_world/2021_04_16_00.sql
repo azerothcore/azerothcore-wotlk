d069d19df0353ea77b4626dcb9d92a3d0f07f92c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-16 07:54:18 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 42dde8c74af315ec650247c6da6bc2dba51c55b2

--- data/sql/updates/db_world/2021_04_16_00.sql
+++ data/sql/updates/db_world/2021_04_16_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_04_14_02 -> 2021_04_16_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_04_14_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_04_14_02 2021_04_16_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1605009324701425500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1605009324701425500');
 
 SET @NPC_JONATHAN := 28136;
@@ -116,3 +132,12 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment
 (50712,-50693,1,'Strip Aura Josephine remove aura Josephine Dying from player'),
 (50722,-50679,1,'Strip Aura Lamoof remove aura Lamoof Dying from player'),
 (50723,-50681,1,'Strip Aura Lamoof 01 remove aura Bleeding Out');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
