cb121bb54e00c8b853fba8b21946593abf4bd94d
kik0 <kik0swd@gmail.com>
2021-04-16 14:50:48 -0700

    update

--- data/sql/updates/pending_db_world/rev_1618600341183458700.sql
+++ data/sql/updates/pending_db_world/rev_1618600341183458700.sql
@@ -1,16 +1,23 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618600341183458700');
 
+DELETE FROM `spell_dbc` WHERE `ID`=20253;
 INSERT INTO `spell_dbc` (`ID`, `DefenseType`, `Attributes`, `SpellLevel`) VALUES
-('20253','2','8192','0');
-
+(20253,2,8192,0);
+DELETE FROM `spell_dbc` WHERE `ID`=58620;
+DELETE FROM `spell_dbc` WHERE `ID`=58621;
 INSERT INTO `spell_dbc` (`ID`, `DefenseType`, `SpellLevel`) VALUES
-('58620','1','0'),
-('58621','1','0');
-
+(58620,1,0),
+(58621,1,0);
+DELETE FROM `spell_dbc` WHERE `ID`=64382;
+DELETE FROM `spell_dbc` WHERE `ID`=20252;
+DELETE FROM `spell_dbc` WHERE `ID`=20616;
+DELETE FROM `spell_dbc` WHERE `ID`=20617;
+DELETE FROM `spell_dbc` WHERE `ID`=25272;
+DELETE FROM `spell_dbc` WHERE `ID`=25275;
 INSERT INTO `spell_dbc` (`ID`, `Attributes`) VALUES
-('64382','8192'),
-('20252','8192'),
-('20616','8192'),
-('20617','8192'),
-('25272','8192'),
-('25275','8192');
+(64382,8192),
+(20252,8192),
+(20616,8192),
+(20617,8192),
+(25272,8192),
+(25275,8192);
