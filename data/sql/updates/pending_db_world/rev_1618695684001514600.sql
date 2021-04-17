2f94adc72a57f957b96a0ced484fcc1b67ee2579
Si1ker <Si1ker@users.noreply.github.com>
2021-04-17 15:53:38 -0600

    update - this is more blizzlike

--- data/sql/updates/pending_db_world/rev_1618695684001514600.sql
+++ data/sql/updates/pending_db_world/rev_1618695684001514600.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618695684001514600');
 
-UPDATE `gameobject` SET `position_x`=2684.41, `position_y`=1486.15, `position_z`=234.502, `zoneId`=406, `areaId`=467 WHERE `guid`=3716;
+UPDATE `gameobject` SET `position_x`=2660.25, `position_y`=1490.65, `position_z`=234.041, `zoneId`=406, `areaId`=467 WHERE `guid`=3716;
 
