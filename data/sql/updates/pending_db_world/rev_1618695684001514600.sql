c91f705ce120e7af1b0553832730156291912614
Si1ker <Si1ker@users.noreply.github.com>
2021-04-17 15:42:25 -0600

    fix

---
+++ data/sql/updates/pending_db_world/rev_1618695684001514600.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618695684001514600');
+
+UPDATE `gameobject` SET `position_x`=2684.41, `position_y`=1486.15, `position_z`=234.502, `zoneId`=406, `areaId`=467 WHERE `guid`=3716;
+
