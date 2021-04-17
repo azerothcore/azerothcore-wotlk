575361cb47fa85534d6d32ab54f609f80baf3fea
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-17 14:20:44 +0200

    int

--- data/sql/updates/pending_db_characters/rev_1618660143408049500.sql
+++ data/sql/updates/pending_db_characters/rev_1618660143408049500.sql
@@ -2,4 +2,4 @@ INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1618660143408049500');
 
 ALTER TABLE `gm_ticket`
 	CHANGE COLUMN `closedBy` `closedBy` INT NOT NULL DEFAULT 0 COMMENT '-1 Closed by Console, >0 GUID of GM' AFTER `lastModifiedTime`,
-	CHANGE COLUMN `resolvedBy` `resolvedBy` IN NOT NULL DEFAULT 0 COMMENT '-1 Resolved by Console, >0 GUID of GM' AFTER `needMoreHelp`;
+	CHANGE COLUMN `resolvedBy` `resolvedBy` INT NOT NULL DEFAULT 0 COMMENT '-1 Resolved by Console, >0 GUID of GM' AFTER `needMoreHelp`;
