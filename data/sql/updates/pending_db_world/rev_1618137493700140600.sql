8c8bc61b40b3dae847261a0e4021160daa90deb8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-17 12:45:25 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 57490af8dd000ca4e0e55a98050a4cf2c3ddc500

--- data/sql/updates/pending_db_world/rev_1618137493700140600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618137493700140600');
-
--- Mulgore copper veins
-UPDATE `gameobject` SET `position_x` = -3050.334229, `position_y` = 316.995148, `position_z` = 151.141052 WHERE `guid` = 4696;
-UPDATE `gameobject` SET `position_x` = -3002.635986, `position_y` = 280.656830, `position_z` = 110.877350 WHERE `guid` = 20650;
