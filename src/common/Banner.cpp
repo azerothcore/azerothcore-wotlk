51ed9c4cc75ba174dedd28be42722d7b4b6c9e99
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-17 02:42:16 +0200

    log

--- src/common/Banner.cpp
+++ src/common/Banner.cpp
@@ -1,32 +1,28 @@
 /*
  * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  */
 
 #include "Banner.h"
 #include "GitRevision.h"
-#include "Log.h"
 #include "StringFormat.h"
 
-void acore::Banner::Show(char const* applicationName, void(char const* text), void(*logExtraInfo)())
+void acore::Banner::Show(char const* applicationName, void(*log)(char const* text), void(*logExtraInfo)())
 {
-    sLog->outString(acore::StringFormat("%s (%s)", GitRevision::GetFullVersion(), applicationName).c_str());
-    sLog->outString("<Ctrl-C> to stop.\n");
-
-    sLog->outString("   █████╗ ███████╗███████╗██████╗  ██████╗ ████████╗██╗  ██╗");
-    sLog->outString("  ██╔══██╗╚══███╔╝██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝██║  ██║");
-    sLog->outString("  ███████║  ███╔╝ █████╗  ██████╔╝██║   ██║   ██║   ███████║");
-    sLog->outString("  ██╔══██║ ███╔╝  ██╔══╝  ██╔══██╗██║   ██║   ██║   ██╔══██║");
-    sLog->outString("  ██║  ██║███████╗███████╗██║  ██║╚██████╔╝   ██║   ██║  ██║");
-    sLog->outString("  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝");
-    sLog->outString("                                ██████╗ ██████╗ ██████╗ ███████╗");
-    sLog->outString("                                ██╔════╝██╔═══██╗██╔══██╗██╔═══╝");
-    sLog->outString("                                ██║     ██║   ██║██████╔╝█████╗");
-    sLog->outString("                                ██║     ██║   ██║██╔══██╗██╔══╝");
-    sLog->outString("                                ╚██████╗╚██████╔╝██║  ██║███████╗");
-    sLog->outString("                                 ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝\n");
-
-    sLog->outString("     AzerothCore 3.3.5a  -  www.azerothcore.org\n");
+    log(acore::StringFormat("%s (%s)", GitRevision::GetFullVersion(), applicationName).c_str());
+    log("<Ctrl-C> to stop.\n");
+    log("   █████╗ ███████╗███████╗██████╗  ██████╗ ████████╗██╗  ██╗");
+    log("  ██╔══██╗╚══███╔╝██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝██║  ██║");
+    log("  ███████║  ███╔╝ █████╗  ██████╔╝██║   ██║   ██║   ███████║");
+    log("  ██╔══██║ ███╔╝  ██╔══╝  ██╔══██╗██║   ██║   ██║   ██╔══██║");
+    log("  ██║  ██║███████╗███████╗██║  ██║╚██████╔╝   ██║   ██║  ██║");
+    log("  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝");
+    log("                                ██████╗ ██████╗ ██████╗ ███████╗");
+    log("                                ██╔════╝██╔═══██╗██╔══██╗██╔═══╝");
+    log("                                ██║     ██║   ██║██████╔╝█████╗");
+    log("                                ██║     ██║   ██║██╔══██╗██╔══╝");
+    log("                                ╚██████╗╚██████╔╝██║  ██║███████╗");
+    log("                                 ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝\n");
+    log("     AzerothCore 3.3.5a  -  www.azerothcore.org\n");
 
     if (logExtraInfo)
     {
--- src/common/Banner.h
+++ src/common/Banner.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
- * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  */
 
 #ifndef AZEROTHCORE_BANNER_H
@@ -12,7 +11,7 @@ namespace acore
 {
     namespace Banner
     {
-        void Show(char const* applicationName, void(char const* text), void(*logExtraInfo)());
+        void Show(char const* applicationName, void(*log)(char const* text), void(*logExtraInfo)());
     }
 }
 
