fb3004ca2699fda542e053d6ee129bd8089fd458
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-18 20:26:00 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): acce0d488d1f7b4199423b6f74ac75772408b22d

--- data/sql/updates/db_world/2021_04_18_02.sql
+++ data/sql/updates/db_world/2021_04_18_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_04_18_01 -> 2021_04_18_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_04_18_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_04_18_01 2021_04_18_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1614130170345582600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1614130170345582600');
 
 DELETE FROM `creature` WHERE `id` IN (2603, 2604, 2605, 2606, 14231, 14233, 14235, 14236, 14237, 14429);
@@ -141,3 +157,12 @@ DELETE FROM smart_scripts WHERE entryOrGuid = 14429 AND source_type = 0;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry= @ENTRY;
 INSERT INTO smart_scripts (entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES
 (@ENTRY, 0, 0, 0, 0, 0, 100, 0, 4000, 4000, 6000, 8000, 11, 19319, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Every 6 - 8 seconds (4 - 4s initially)  - Self: Cast spell Vicious Bite (19319) on Victim");
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
