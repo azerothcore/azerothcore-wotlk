167951848d0c07e7648b2784f7fea59babfdc179
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-18 15:47:20 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d624baba2a98cf754053002a720087ea1f9000f5

--- data/sql/updates/pending_db_world/rev_1617964644615239534.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617964644615239534');
-
-SET @ENTRY := 11598;
-DELETE FROM `creature_onkill_reputation` WHERE (`creature_id` = @ENTRY);
-INSERT INTO `creature_onkill_reputation` (`creature_id`, `RewOnKillRepFaction1`, `RewOnKillRepFaction2`, `MaxStanding1`, `IsTeamAward1`, `RewOnKillRepValue1`, `MaxStanding2`, `IsTeamAward2`, `RewOnKillRepValue2`, `TeamDependent`) VALUES
-(@ENTRY, 529, 0, 6, 0, 0, 0, 0, 0, 0);
