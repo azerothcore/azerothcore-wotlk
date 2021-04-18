a8129e8e788d9155afe00b410bb23544a8f56245
Branel <emirpod02@gmail.com>
2021-04-18 19:15:15 +0200

    update to add another node

--- data/sql/updates/pending_db_world/rev_1618759286165193600.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618750411311383100');
-UPDATE `gameobject` SET `position_x`='-7406', `position_y`='-2756', `position_z`='12.064' WHERE  `guid`=7327;
