eb562b47f32a87822858c504999dad64d2ed9931
Si1ker <Si1ker@users.noreply.github.com>
2021-04-19 10:13:02 -0600

    fix pos

---
+++ data/sql/updates/pending_db_world/rev_1618848739269957700.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618848739269957700');
+
+UPDATE `gameobject` SET `position_x`=-2076.86, `position_y`=-3631.41, `position_z`=60.910 WHERE `guid`=3538;
+
