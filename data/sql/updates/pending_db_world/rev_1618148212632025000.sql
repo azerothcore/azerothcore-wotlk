aa42a13de725a0dc05252628ec46fb02e1e2aa83
Raelorasz <ryil2016@gmail.com>
2021-04-19 10:21:42 +0200

    Fix DB startup error

--- data/sql/updates/pending_db_world/rev_1618148212632025000.sql
+++ data/sql/updates/pending_db_world/rev_1618148212632025000.sql
@@ -1,3 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618148212632025000');
 
 DELETE FROM `creature` WHERE (`id` = 1787) AND (`guid` IN (52542));
+
+DELETE FROM `creature_addon` WHERE  `guid`=52542;
