0148160659220b8f15e49710898b769a4c702a0a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-19 00:51:08 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 4e6fdecaa003fa77dd53b27f1a45b9c4311cc999

--- data/sql/updates/pending_db_world/rev_1618170334855143471.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618170334855143471');
-
-DELETE FROM `creature` WHERE (`id` = 6491) AND (`guid` IN (950));
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(950, 6491, 0, 0, 0, 1, 1, 0, 0, 2053.1, -5019.08, 75.2506, 0.0315629, 300, 0, 0, 4121, 0, 0, 0, 0, 0, '', 0);
-
