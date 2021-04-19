f8777b645a00cda9e9f312fd00abdb80eaef0995
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-19 12:21:04 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 085be43e1d1bb971445dca344c7aea71dae0026b

--- data/sql/updates/db_world/2021_04_19_10.sql
+++ data/sql/updates/db_world/2021_04_19_10.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_04_19_09 -> 2021_04_19_10
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_04_19_09';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_04_19_09 2021_04_19_10 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1616252834972147900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616252834972147900');
 /*
  * Zone: Shadowmoon Valley
@@ -307,3 +323,12 @@ DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 21408);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (21408, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 37941, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Felfire Diemetradon - On Aggro - Cast \'37941\' (No Repeat)'),
 (21408, 0, 1, 0, 0, 0, 100, 0, 2500, 5000, 10000, 12500, 11, 36247, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Felfire Diemetradon - In Combat - Cast \'36247\'');
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
