347ea9f00bac4324df9dcd4e32487db10bd7288e
Si1ker <Si1ker@users.noreply.github.com>
2021-04-21 02:09:59 -0600

    update

--- data/sql/updates/pending_db_world/rev_1618990113391357800.sql
+++ data/sql/updates/pending_db_world/rev_1618990113391357800.sql
@@ -53,3 +53,16 @@ DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 32492;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (32492, 0, 0, 0, 0, 0, 100, 0, 4000, 7000, 10000, 20000, 0, 11, 60667, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, "Frostbrood Matriarch - In Combat - Cast 'Frost Breath'");
 
+-- Vargul Wanderer
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 32505;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 32505;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(32505, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 11, 36788, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, "Vargul Wanderer - On aggro - Cast 'Diminish Soul'");
+
+-- Ravaged Ghoul
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 32502;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 32502;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(32502, 0, 0, 0, 0, 0, 100, 0, 2000, 5000, 7000, 11000, 0, 11, 60873, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, "Ravaged Ghoul - In Combat - Cast 'Festering Bite'"),
+(32502, 0, 1, 0, 0, 0, 100, 0, 6300, 15200, 16700, 22300, 0, 11, 60872, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, "Ravaged Ghoul - In Combat - Cast 'Ravenous Claw'");
+
