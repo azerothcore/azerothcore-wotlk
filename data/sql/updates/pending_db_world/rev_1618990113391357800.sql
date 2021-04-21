ff131d9a79b34972aef5e54c9cffdd1be47047ac
Si1ker <Si1ker@users.noreply.github.com>
2021-04-21 01:46:43 -0600

    update

--- data/sql/updates/pending_db_world/rev_1618990113391357800.sql
+++ data/sql/updates/pending_db_world/rev_1618990113391357800.sql
@@ -47,3 +47,9 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (31747, 0, 0, 0, 0, 0, 100, 0, 2000, 6000, 12000, 16000, 0, 11, 744, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, "Necrotic Webspinner - In Combat - Cast 'Poison'"),
 (31747, 0, 1, 0, 0, 0, 100, 0, 1000, 3000, 10000, 15000, 0, 11, 745, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, "Necrotic Webspinner - In Combat - Cast 'Web'");
 
+-- Frostbrood Matriarch
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 32492;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 32492;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(32492, 0, 0, 0, 0, 0, 100, 0, 4000, 7000, 10000, 20000, 0, 11, 60667, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, "Frostbrood Matriarch - In Combat - Cast 'Frost Breath'");
+
