4a8faafaff2753349ea15aa602cc3816f4e42de6
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-22 02:42:46 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 14e62b5eaeafccacf0251f80fca13d3e418fe005

--- data/sql/updates/db_world/2021_04_22_00.sql
+++ data/sql/updates/db_world/2021_04_22_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_04_21_00 -> 2021_04_22_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_04_21_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_04_21_00 2021_04_22_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1619032787077995400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619032787077995400');
 
 -- Despawn minions on death
@@ -8,3 +24,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 -- Use correct spell for Knock Away
 UPDATE `smart_scripts` SET `action_param1`=10101 WHERE `entryorguid`=12203 AND `source_type`=0 AND `id`=1 AND `link`=0;
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
