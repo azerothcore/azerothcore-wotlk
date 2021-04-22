b385878e4a5be963f974a6434d692ddb14e7a47e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-22 22:25:19 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 434f0c7d3b0c334e61b7eda44b5a100e8711ca49

--- data/sql/updates/pending_db_world/rev_1619115153766529700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619115153766529700');
-
-UPDATE `npc_text` SET `text0_0`='Welcome to Camp Mojache, brave $c. How may I assist you this day?' WHERE `ID`=2368;
-
