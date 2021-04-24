818d64916f2fd13bba927250d08e81ae5f758f3d
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-24 06:22:33 +0200

    Update rev_1619236551391059700.sql

--- data/sql/updates/pending_db_world/rev_1619236551391059700.sql
+++ data/sql/updates/pending_db_world/rev_1619236551391059700.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619236551391059700');
 
 ALTER TABLE `npc_trainer`
-	CHANGE COLUMN `ReqSpell` `ReqSpell` INT NOT NULL DEFAULT 0 AFTER `ReqSkillRank`;
+	ADD COLUMN `ReqSpell` INT NOT NULL DEFAULT 0 AFTER `ReqSkillRank`;
 
 -- 	Dragonscale Leatherworking
 UPDATE `npc_trainer` SET `ReqSpell` = 10656 WHERE `SpellID` IN (35575, 35576, 35577, 35580, 35582, 35584, 36076, 36079, 10619, 10650, 24654, 24655);
