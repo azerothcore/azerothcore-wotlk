b487ce90caac39047043e53979e06057c991a618
Branel <emirpod02@gmail.com>
2021-04-27 19:38:06 +0200

    this might do it

--- src/server/scripts/EasternKingdoms/Deadmines/boss_mr_smite.cpp
+++ src/server/scripts/EasternKingdoms/Deadmines/boss_mr_smite.cpp
@@ -110,6 +110,8 @@ public:
                         events.ScheduleEvent(EVENT_SMITE_SLAM, 6000);
                         break;
                     }
+                    events.ScheduleEvent(EVENT_SMITE_SLAM, 500)
+                    break;
                 case EVENT_SWAP_WEAPON1:
                     me->LoadEquipment(EQUIP_TWO_SWORDS);
                     me->SetCanDualWield(true);
