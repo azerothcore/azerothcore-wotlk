0372ddc15e6b08794fce34b174e492dda80a8479
Branel <emirpod02@gmail.com>
2021-04-27 14:17:08 +0200

    fix(DB/Gameobject) Mining nodes inside of eachother (#5345)

---
+++ data/sql/updates/pending_db_world/rev_1618750411311383100.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618750411311383100');
+UPDATE `gameobject`SET `position_x`='-7406', `position_y`='-2756', `position_z`='12.064' WHERE `guid`=7327;
+UPDATE `gameobject`SET `position_x`='-520', `position_y`='-4762', `position_z`='30.064' WHERE `guid`=12487;
