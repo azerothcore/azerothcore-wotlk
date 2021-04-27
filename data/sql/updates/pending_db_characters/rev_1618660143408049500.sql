28242ca061ccc02fb6a94f5f710a540e7cbb49f5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-27 13:46:06 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 21f61b56410592ff4746dd7f51347f0dfe25f54a

--- data/sql/updates/pending_db_characters/rev_1618660143408049500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1618660143408049500');
-
-ALTER TABLE `gm_ticket`
-	CHANGE COLUMN `closedBy` `closedBy` INT NOT NULL DEFAULT 0 COMMENT '-1 Closed by Console, >0 GUID of GM' AFTER `lastModifiedTime`,
-	CHANGE COLUMN `resolvedBy` `resolvedBy` INT NOT NULL DEFAULT 0 COMMENT '-1 Resolved by Console, >0 GUID of GM' AFTER `needMoreHelp`;
