05b356a6b1ca4ba2728905fc96271a3659e259d9
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-04-28 23:02:51 +0200

    Add better descriptions

--- data/sql/updates/pending_db_world/rev_1619547017987840005.sql
+++ data/sql/updates/pending_db_world/rev_1619547017987840005.sql
@@ -14,21 +14,21 @@ INSERT INTO `pool_template` (`entry`,`max_limit`,`description`) VALUES (@POOL,@P
 -- Add gameobjects to pools
 DELETE FROM `pool_gameobject` WHERE FIND_IN_SET (`guid`,@GUID);
 INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUES
-(30169,@POOL,0,@POOLDESC),
-(85697,@POOL,0,@POOLDESC),
-(30170,@POOL,0,@POOLDESC),
-(85853,@POOL,0,@POOLDESC),
-(30171,@POOL,0,@POOLDESC),
-(85852,@POOL,0,@POOLDESC),
-(85712,@POOL,0,@POOLDESC),
-(30178,@POOL,0,@POOLDESC),
-(85863,@POOL,0,@POOLDESC),
-(30183,@POOL,0,@POOLDESC),
-(85713,@POOL,0,@POOLDESC),
-(85862,@POOL,0,@POOLDESC),
-(30182,@POOL,0,@POOLDESC),
-(30184,@POOL,0,@POOLDESC),
-(85861,@POOL,0,@POOLDESC);
+(30169,@POOL,0,'Solid Chest, Desolace, node 1'),
+(85697,@POOL,0,'Solid Chest, Desolace, node 2'),
+(30170,@POOL,0,'Solid Chest, Desolace, node 3'),
+(85853,@POOL,0,'Solid Chest, Desolace, node 4'),
+(30171,@POOL,0,'Solid Chest, Desolace, node 5'),
+(85852,@POOL,0,'Solid Chest, Desolace, node 6'),
+(85712,@POOL,0,'Solid Chest, Desolace, node 7'),
+(30178,@POOL,0,'Solid Chest, Desolace, node 8'),
+(85863,@POOL,0,'Solid Chest, Desolace, node 9'),
+(30183,@POOL,0,'Solid Chest, Desolace, node 10'),
+(85713,@POOL,0,'Solid Chest, Desolace, node 11'),
+(85862,@POOL,0,'Solid Chest, Desolace, node 12'),
+(30182,@POOL,0,'Solid Chest, Desolace, node 13'),
+(30184,@POOL,0,'Solid Chest, Desolace, node 14'),
+(85861,@POOL,0,'Solid Chest, Desolace, node 15');
 
 -- Respawn rates of gameobjects
 UPDATE `gameobject` SET `spawntimesecs`=@RESPAWN WHERE FIND_IN_SET (`guid`,@GUID);
