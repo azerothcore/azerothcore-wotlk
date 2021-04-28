27d7aa746d35ded4e2dea422c6f7152e913e1770
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-04-28 23:30:08 +0200

    Add ZoneId

--- data/sql/updates/pending_db_world/rev_1619641851979377067.sql
+++ data/sql/updates/pending_db_world/rev_1619641851979377067.sql
@@ -40,4 +40,5 @@ INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUE
 
 -- Respawn rates of gameobjects
 UPDATE `gameobject` SET `spawntimesecs`=@RESPAWN WHERE FIND_IN_SET (`guid`,@GUID);
+UPDATE `gameobject` SET `zoneId`= '440' WHERE FIND_IN_SET (`guid`,@GUID);
 
