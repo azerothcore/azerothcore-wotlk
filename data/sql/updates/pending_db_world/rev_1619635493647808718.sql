a079a88dfa3df23fdee34c306400ca96a7d7fe09
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-04-28 20:59:20 +0200

    Fix: Chest Pooling in Dustwallow Marsh

---
+++ data/sql/updates/pending_db_world/rev_1619635493647808718.sql
@@ -0,0 +1,40 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619635493647808718');
+
+SET
+@POOL            = 11658,
+@POOLSIZE        = 6,
+@POOLDESC        = 'Treasures - Dustwallow Marsh',
+@RESPAWN         = 900,
+@GUID = '87386,11755,14618,85709,85714,13632,85864,85706,9096,14619,14931,40758,40772,40796,85718,85721,85722,85734,85735';
+
+-- Create pool(s)
+DELETE FROM `pool_template` WHERE `entry`=@POOL;
+INSERT INTO `pool_template` (`entry`,`max_limit`,`description`) VALUES (@POOL,@POOLSIZE,@POOLDESC);
+
+-- Add gameobjects to pools
+DELETE FROM `pool_gameobject` WHERE FIND_IN_SET (`guid`,@GUID);
+INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUES
+(87386,@POOL,0,@POOLDESC),
+(11755,@POOL,0,@POOLDESC),
+(14618,@POOL,0,@POOLDESC),
+(85709,@POOL,0,@POOLDESC),
+(85714,@POOL,0,@POOLDESC),
+(13632,@POOL,0,@POOLDESC),
+(85864,@POOL,0,@POOLDESC),
+(85706,@POOL,0,@POOLDESC),
+(9096,@POOL,0,@POOLDESC),
+(14619,@POOL,0,@POOLDESC),
+(14931,@POOL,0,@POOLDESC),
+(40758,@POOL,0,@POOLDESC),
+(40772,@POOL,0,@POOLDESC),
+(40796,@POOL,0,@POOLDESC),
+(85718,@POOL,0,@POOLDESC),
+(85721,@POOL,0,@POOLDESC),
+(85722,@POOL,0,@POOLDESC),
+(85734,@POOL,0,@POOLDESC),
+(85735,@POOL,0,@POOLDESC);
+
+
+-- Respawn rates of gameobjects
+UPDATE `gameobject` SET `spawntimesecs`=@RESPAWN WHERE FIND_IN_SET (`guid`,@GUID);
+
