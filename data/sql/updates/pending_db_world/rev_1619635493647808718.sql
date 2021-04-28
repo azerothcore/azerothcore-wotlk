c94dadfc873eb79c60873238f613a95da7492199
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-04-28 22:56:38 +0200

    Add better description

--- data/sql/updates/pending_db_world/rev_1619635493647808718.sql
+++ data/sql/updates/pending_db_world/rev_1619635493647808718.sql
@@ -14,25 +14,25 @@ INSERT INTO `pool_template` (`entry`,`max_limit`,`description`) VALUES (@POOL,@P
 -- Add gameobjects to pools
 DELETE FROM `pool_gameobject` WHERE FIND_IN_SET (`guid`,@GUID);
 INSERT INTO `pool_gameobject` (`guid`,`pool_entry`,`chance`,`description`) VALUES
-(87386,@POOL,0,@POOLDESC),
-(11755,@POOL,0,@POOLDESC),
-(14618,@POOL,0,@POOLDESC),
-(85709,@POOL,0,@POOLDESC),
-(85714,@POOL,0,@POOLDESC),
-(13632,@POOL,0,@POOLDESC),
-(85864,@POOL,0,@POOLDESC),
-(85706,@POOL,0,@POOLDESC),
-(9096,@POOL,0,@POOLDESC),
-(14619,@POOL,0,@POOLDESC),
-(14931,@POOL,0,@POOLDESC),
-(40758,@POOL,0,@POOLDESC),
-(40772,@POOL,0,@POOLDESC),
-(40796,@POOL,0,@POOLDESC),
-(85718,@POOL,0,@POOLDESC),
-(85721,@POOL,0,@POOLDESC),
-(85722,@POOL,0,@POOLDESC),
-(85734,@POOL,0,@POOLDESC),
-(85735,@POOL,0,@POOLDESC);
+(87386,@POOL,0,'Solid Chest, Dustwallow Marsh, node 1'),
+(11755,@POOL,0,'Solid Chest, Dustwallow Marsh, node 2'),
+(14618,@POOL,0,'Solid Chest, Dustwallow Marsh, node 3'),
+(85709,@POOL,0,'Solid Chest, Dustwallow Marsh, node 4'),
+(85714,@POOL,0,'Solid Chest, Dustwallow Marsh, node 5'),
+(13632,@POOL,0,'Solid Chest, Dustwallow Marsh, node 6'),
+(85864,@POOL,0,'Solid Chest, Dustwallow Marsh, node 7'),
+(85706,@POOL,0,'Solid Chest, Dustwallow Marsh, node 8'),
+(9096,@POOL,0,'Solid Chest, Dustwallow Marsh, node 9'),
+(14619,@POOL,0,'Solid Chest, Dustwallow Marsh, node 10'),
+(14931,@POOL,0,'Solid Chest, Dustwallow Marsh, node 11'),
+(40758,@POOL,0,'Solid Chest, Dustwallow Marsh, node 12'),
+(40772,@POOL,0,'Solid Chest, Dustwallow Marsh, node 13'),
+(40796,@POOL,0,'Solid Chest, Dustwallow Marsh, node 14'),
+(85718,@POOL,0,'Solid Chest, Dustwallow Marsh, node 15'),
+(85721,@POOL,0,'Solid Chest, Dustwallow Marsh, node 16'),
+(85722,@POOL,0,'Solid Chest, Dustwallow Marsh, node 17'),
+(85734,@POOL,0,'Solid Chest, Dustwallow Marsh, node 18'),
+(85735,@POOL,0,'Solid Chest, Dustwallow Marsh, node 19');
 
 
 -- Respawn rates of gameobjects
