a34e923d68d1722c76941bb1cb21348a4486f342
Axel Cocat <ax.cocat@gmail.com>
2021-04-29 15:06:37 +0200

    remove BroadcastTextID instead of changing broadcast_text

--- data/sql/updates/pending_db_world/rev_1619681545150953400.sql
+++ data/sql/updates/pending_db_world/rev_1619681545150953400.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619681545150953400');
 
-SET @STORMWIND_GUARD_SHAMAN_TRAINER_TEXT:='I don''t know what''s possessed you to look for a shaman... but I have seen someone who matches the profile. There''s a woman who calls herself a Farseer on the shore in the Valley of Heroes.';
-UPDATE `broadcast_text` SET `MaleText` = @STORMWIND_GUARD_SHAMAN_TRAINER_TEXT, `FemaleText` = @STORMWIND_GUARD_SHAMAN_TRAINER_TEXT WHERE `ID` = 18360;
+UPDATE `npc_text` SET `BroadcastTextID0` = 0 WHERE `ID` = 10106 AND `BroadcastTextID0` = 18360; -- Unlink incorrect broadcast text from Stormwind guard (Class Trainer > Shaman)
