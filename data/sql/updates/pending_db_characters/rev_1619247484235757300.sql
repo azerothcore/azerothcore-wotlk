a8248cb9e96052769965c826beee69ec1e9d5c75
AzerothCoreBot <azerothcorebot@gmail.com>
2021-04-29 14:20:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): e8742d40549ddbe544c292e0011de8ffb9637418

--- data/sql/updates/pending_db_characters/rev_1619247484235757300.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1619247484235757300');
-
-ALTER TABLE `pet_aura`
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`guid`, `casterGuid`, `spell`, `effectMask`);
