8bfe1a0945ec06ae9da4b9fb97d40993889f6b78
Chaouki Dhib <chaodhib@gmail.com>
2021-04-30 22:03:19 +0200

    fix MSG_MOVE_SET_RUN_SPEED

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13704,6 +13704,7 @@ void Unit::SetSpeed(UnitMoveType mtype, float rate, bool forced)
                 return;
         }
 
+        data << GetPackGUID();
         BuildMovementPacket(&data);
         data << float(GetSpeed(mtype));
         SendMessageToSet(&data, true);
