445131ee3884cc7972528d81acbdec77324c1354
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-02 22:45:44 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 36c82f4c6150f29c4bf3754ee0d28779b7fe0199

--- data/sql/updates/pending_db_world/rev_1619976848278136900.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619976848278136900');
-
--- Durotar | GUID 4815 | issue #5571
-UPDATE `gameobject` SET `position_x`=-246.69, `position_y`=-5237.25, `position_z`=2.6372 WHERE `guid`=4815;
-
--- Thousand Needles | GUID 5365 | issue #5584
-UPDATE `gameobject` SET `position_x`=-4859.25, `position_y`=-1798.27, `position_z`=-41.880 WHERE `guid`=5365;
-
