b5ba7ba9a1d9e7a5b64148d88ed9d7cea01156a2
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-03 14:54:50 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 24ed28ff934418cc36c3679fa3b7cacb4e1a31af

--- data/sql/updates/db_world/2021_05_03_02.sql
+++ data/sql/updates/db_world/2021_05_03_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_03_01 -> 2021_05_03_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_03_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_03_01 2021_05_03_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1619363905724595186'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1619363905724595186');
 
 -- select max(entry) from reference_loot_template; 526760, using 526760 + 30
@@ -60,3 +76,12 @@ DELETE
 FROM `creature_loot_template`
 WHERE `Reference` IN (24068, 24077, 24060)
   AND `Entry` = 2744;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
