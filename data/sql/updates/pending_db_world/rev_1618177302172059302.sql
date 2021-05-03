1d919f8448e185a34b4f7451fc4b0350722a5a93
Stefano Borzì <stefanoborzi32@gmail.com>
2021-05-04 00:18:04 +0200

    fix(DB/Gameobject): duplicate or wrong spawns (#5258)
    * fix(DB/Gameobject): duplicate or wrong spawns
    
    * Update rev_1618177302172059302.sql

---
+++ data/sql/updates/pending_db_world/rev_1618177302172059302.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1618177302172059302');
+
+DELETE FROM `gameobject` WHERE `id`=2046 AND `guid`=8871;
+DELETE FROM `pool_gameobject` WHERE `guid` IN (8871);
+
+
+DELETE FROM `gameobject` WHERE (`id` = 1623) AND (`guid` IN (4214));
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
+(4214, 1623, 0, 0, 0, 1, 1, -581.555237, -2028.706299, 69.566879, 4.984, 0, 0, 0, 0, 60, 100, 1, '', 0);
