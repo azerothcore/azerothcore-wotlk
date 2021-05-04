f4b5c28fd4379f839dc95605bef45dd3fbdf1a73
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-04 01:10:34 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 91e90dd4dab87f3d692f161d8e1972defba9fa82

--- data/sql/updates/pending_db_world/rev_1620068494045251800.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620068494045251800');
-
-UPDATE `gameobject` SET `position_x`=-11422.49, `position_y`=11.69, `position_z`=45.235 WHERE `guid`=34036;
-UPDATE `gameobject` SET `position_x`=-11547.95, `position_y`=-178.38, `position_z`=15.896 WHERE `guid`=65255;
-UPDATE `gameobject` SET `position_x`=-12039.91, `position_y`=-289.09, `position_z`=25.124 WHERE `guid`=11969;
-
