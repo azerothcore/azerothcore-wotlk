1c448280cec7c484d8345f0fec8332b5d194e0af
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-05-04 12:07:54 +0200

    Correct drop chances of all classic ores
    Co-Author: @necropola

---
+++ data/sql/updates/pending_db_world/rev_1620115611701757811.sql
@@ -0,0 +1,59 @@
+-- INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620115611701757811');
+
+-- Copper Vein
+UPDATE `gameobject_loot_template` SET `MaxCount`=7 WHERE FIND_IN_SET (`Entry`,'1502,1735,2626,18092') and Item = 2835; -- Rough Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE FIND_IN_SET (`Entry`,'1502,1735,2626,18092') and Item = 2835; -- Rough Stone
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE FIND_IN_SET (`Entry`,'1502,1735,2626,18092') and Item = 2835; -- Rough Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE FIND_IN_SET (`Entry`,'1502,1735,2626,18092') and Item = 2770; -- Copper Ore
+UPDATE `gameobject_loot_template` SET `MaxCount`=4 WHERE FIND_IN_SET (`Entry`,'1502,1735,2626,18092') and Item = 2770; -- Copper Ore
+
+-- Tin Vein
+UPDATE `gameobject_loot_template` SET `MaxCount`=7 WHERE FIND_IN_SET (`Entry`,'1503,1736,2627,18093') and Item = 2836; -- Coarse Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE FIND_IN_SET (`Entry`,'1503,1736,2627,18093') and Item = 2836; -- Coarse Stone
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE FIND_IN_SET (`Entry`,'1503,1736,2627,18093') and Item = 2836; -- Coarse Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE FIND_IN_SET (`Entry`,'1503,1736,2627,18093') and Item = 2771; -- Tin Ore
+UPDATE `gameobject_loot_template` SET `MaxCount`=4 WHERE FIND_IN_SET (`Entry`,'1503,1736,2627,18093') and Item = 2771; -- Tin Ore
+
+-- Iron Deposit
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE Entry = 1505 and Item = 2838; -- Heavy Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=3 WHERE Entry = 1505 and Item = 2838; -- Heavy Stone
+UPDATE `gameobject_loot_template` SET `MaxCount`=9 WHERE Entry = 1505 and Item = 2838; -- Heavy Stone
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE Entry = 1505 and Item = 2838; -- Heavy Stone
+
+-- Mithril Deposit
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE FIND_IN_SET (`Entry`,'1742,13961') and Item = 3858; -- Mithril Ore
+UPDATE `gameobject_loot_template` SET `MaxCount`=4 WHERE FIND_IN_SET (`Entry`,'1742,13961') and Item = 3858; -- Mithril Ore
+UPDATE `gameobject_loot_template` SET `MinCount`=3 WHERE FIND_IN_SET (`Entry`,'1742,13961') and Item = 7912; -- Solid Stone
+UPDATE `gameobject_loot_template` SET `MaxCount`=12 WHERE FIND_IN_SET (`Entry`,'1742,13961') and Item = 7912; -- Solid Stone
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE FIND_IN_SET (`Entry`,'1742,13961') and Item = 7912; -- Solid Stone
+
+-- Hakkari Thorium Vein
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE Entry = 17241 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE Entry = 17241 and Item = 1; -- Reference Tables
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE Entry = 17241 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=3 WHERE Entry = 17241 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MaxCount`=9 WHERE Entry = 17241 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE Entry = 17241 and Item = 19774; -- Souldarite
+UPDATE `gameobject_loot_template` SET `MaxCount`=4 WHERE Entry = 17241 and Item = 19774; -- Souldarite
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE Entry = 17241 and Item = 10620; -- Thorium Ore
+UPDATE `gameobject_loot_template` SET `MaxCount`=4 WHERE Entry = 17241 and Item = 10620; -- Thorium Ore
+
+-- Small Thorium Veins
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE Entry = 13960 and Item = 11513; -- Tainted Vitriol
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 1; -- Reference Tables
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=3 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MaxCount`=9 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=2 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 10620; -- Thorium Ore
+UPDATE `gameobject_loot_template` SET `MaxCount`=4 WHERE FIND_IN_SET (`Entry`,'9597,13960') and Item = 10620; -- Thorium Ore
+
+-- Rich Thorium Vein
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE Entry = 12883 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `GroupId`=1 WHERE Entry = 12883 and Item = 1; -- Reference Tables
+UPDATE `gameobject_loot_template` SET `Chance`=40 WHERE Entry = 12883 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=4 WHERE Entry = 12883 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MaxCount`=12 WHERE Entry = 12883 and Item = 12365; -- Dense Stone
+UPDATE `gameobject_loot_template` SET `MinCount`=3 WHERE Entry = 12883 and Item = 19774; -- Thorium Ore
+UPDATE `gameobject_loot_template` SET `MaxCount`=5 WHERE Entry = 12883 and Item = 19774; -- Thorium Ore
+
