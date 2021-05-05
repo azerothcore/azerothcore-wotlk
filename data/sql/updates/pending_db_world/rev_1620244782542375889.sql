fb13189232e5b87e54d19115bc9131b5391d6113
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-05-05 22:00:57 +0200

    Fix: Molten Core: Magmadar
    Co-Author: @sanctum32 Co-Author: @t1ti

---
+++ data/sql/updates/pending_db_world/rev_1620244782542375889.sql
@@ -0,0 +1,11 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620244782542375889');
+
+-- Added missing aura for Magmadar
+UPDATE `creature_template_addon` SET `auras`='19449' WHERE  `entry`=11982;
+
+-- 19411 Lava Bomb (used by Magmadar)
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (19411, 20474) AND `ScriptName`='spell_magmadar_lava_bomb';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(19411, 'spell_magmadar_lava_bomb'),
+(20474, 'spell_magmadar_lava_bomb');
+
