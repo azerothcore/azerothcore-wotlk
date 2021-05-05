d0ea033f3d787043cec5067b5fc196e7863770a8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-05 06:10:37 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): cc0192ad028309d9e65bf71eed07e4cf8cff8049

--- data/sql/updates/pending_db_world/rev_1620067514646031100.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620067514646031100');
-
-UPDATE `creature` SET `spawntimesecs`=300 WHERE `id` IN (
-877, -- Saltscale Forager
-879, -- Saltscale Hunter
-871, -- Saltscale Warrior
-873, -- Saltscale Oracle
-875); -- Saltscale Tide Lord
-
