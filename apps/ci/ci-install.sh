17083a9231b26c07cac44a7ff115faeca3918d57
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-07 01:59:33 +0700

    chore(Cmake/Compiler): deprecated clang6 and gcc 7

--- apps/ci/ci-install.sh
+++ apps/ci/ci-install.sh
@@ -52,12 +52,6 @@ case $COMPILER in
     echo "CCOMPILERCXX=\"clang++\"" >> ./conf/config.sh
     ;;
 
-  "clang6" )
-    time sudo apt-get install -y clang-6.0
-    echo "CCOMPILERC=\"clang-6.0\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-6.0\"" >> ./conf/config.sh
-    ;;
-
   "clang9" )
     time sudo apt-get install -y clang-9
     echo "CCOMPILERC=\"clang-9\"" >> ./conf/config.sh
