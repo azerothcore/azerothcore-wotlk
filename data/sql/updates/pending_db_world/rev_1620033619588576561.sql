d7ddd6f8d13dc7f85cf9a28487607bdd151341c1
Necropola <necropola@rain-fighters.net>
2021-05-06 15:35:59 +0200

    fix(DB/go_loot_template): Silver Piffeny Band should not be lootable from chests (#5608)

---
+++ data/sql/updates/pending_db_world/rev_1620033619588576561.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620033619588576561');
+
+DELETE FROM `gameobject_loot_template` WHERE `item`=7338; -- Mood Ring
+DELETE FROM `gameobject_loot_template` WHERE `item`=7339; -- Miniscule Diamond Ring
+DELETE FROM `gameobject_loot_template` WHERE `item`=7341; -- Cubic Zirconia Ring
+DELETE FROM `gameobject_loot_template` WHERE `item`=7342; -- Silver Piffeny Band
+
