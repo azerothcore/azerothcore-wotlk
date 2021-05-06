6cb242115a978cc5c42af2448b19465cbbf17d8e
Si1ker <Si1ker@users.noreply.github.com>
2021-05-06 13:52:42 -0600

    .

--- data/sql/updates/pending_db_world/rev_1620330327979257800.sql
+++ data/sql/updates/pending_db_world/rev_1620330327979257800.sql
@@ -2,3 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620330327979257800');
 
 DELETE FROM `creature_addon` WHERE `guid`=3596;
 UPDATE `creature` SET `wander_distance`=10, `MovementType`=1 WHERE `id`=1199;
+
