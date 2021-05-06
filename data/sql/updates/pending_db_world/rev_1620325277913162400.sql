0e27b000e31449af1dfce61a720255035a3d28fd
Si1ker <Si1ker@users.noreply.github.com>
2021-05-06 12:30:59 -0600

    update

---
+++ data/sql/updates/pending_db_world/rev_1620325277913162400.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620325277913162400');
+
+UPDATE `gameobject` SET `position_x`=1366.00, `position_y`=-318.00, `position_z`=3.727 WHERE `guid`=3440;
+
