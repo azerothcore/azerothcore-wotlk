10179f38d00b3826f6b9e97ddddc1d1ae937b831
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-07 23:12:24 +0700

    add clang 7

--- apps/ci/ci-install.sh
+++ apps/ci/ci-install.sh
@@ -52,6 +52,12 @@ case $COMPILER in
     echo "CCOMPILERCXX=\"clang++\"" >> ./conf/config.sh
     ;;
 
+  "clang7" )
+    time sudo apt-get install -y clang-7
+    echo "CCOMPILERC=\"clang-7\"" >> ./conf/config.sh
+    echo "CCOMPILERCXX=\"clang++-7\"" >> ./conf/config.sh
+    ;;
+
   "clang9" )
     time sudo apt-get install -y clang-9
     echo "CCOMPILERC=\"clang-9\"" >> ./conf/config.sh
