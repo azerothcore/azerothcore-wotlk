4eb9e6d082b8280830abb455eb20813425cdc954
Si1ker <Si1ker@users.noreply.github.com>
2021-05-07 03:16:40 -0600

    remove SAI crashing server

--- data/sql/updates/pending_db_world/rev_1620365613702531200.sql
+++ data/sql/updates/pending_db_world/rev_1620365613702531200.sql
@@ -35,14 +35,6 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ID*100+0,9,6,0,0,0,100,0,3000,3000,0,0,0,9,0,0,0,0,0,0,20,37118,0,0,0,0,0,0,0,"Galen Goodward - On Script - Activate Closest Gameobject 'Galen's Cage'"),
 (@ID*100+0,9,7,0,0,0,100,0,0,0,0,0,0,53,0,5391,0,0,0,2,1,0,0,0,0,0,0,0,0,"Galen Goodward - On Script - Start Waypoint");
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 2387 AND `source_type` = 2;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(2387,2,0,0,46,0,100,0,2387,0,0,0,0,45,0,1,0,0,0,0,19,5391,20,0,0,0,0,0,0,"Areatrigger (Swamp of Sorrows) - On Trigger - Set Data 0 1 (Galen Goodward)");
-
-DELETE FROM `areatrigger_scripts` WHERE `entry` = 2387;
-INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES
-(2387,"SmartTrigger");
-
 DELETE FROM `creature_text` WHERE `CreatureID` = @ID;
 INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
 (@ID,0,0,"Look out!  The $n attacks!",12,0,100,0,0,0,1628,0,"Galen Goodward"),
