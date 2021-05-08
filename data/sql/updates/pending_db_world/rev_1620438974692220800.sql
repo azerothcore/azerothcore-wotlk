66c2e98b9d063e62da9a32c5c4c490db11591a98
Si1ker <Si1ker@users.noreply.github.com>
2021-05-07 19:58:51 -0600

    update

---
+++ data/sql/updates/pending_db_world/rev_1620438974692220800.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620438974692220800');
+
+-- 2 steps above of current pos
+UPDATE `creature` SET `position_x`=-4788.24, `position_y`=-11589.40, `position_z`=32.176 WHERE `guid`=61450;
+
