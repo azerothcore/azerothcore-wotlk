4c8de753aeebc958fa6f2926bf1b6e9fbd94cdca
Hacki95 <daniel.hackmann@stud.hs-bochum.de>
2021-05-08 21:26:12 +0200

    Fix: Molten Core: Sulfuron Harbinger
    Co-Author: @sanctum32 Co-Author: @T1ti

--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_sulfuron_harbinger.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_sulfuron_harbinger.cpp
@@ -11,9 +11,9 @@ SDComment: Adds NYI
 SDCategory: Molten Core
 EndScriptData */
 
-#include "molten_core.h"
-#include "ObjectMgr.h"
+#include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "molten_core.h"
 #include "ScriptMgr.h"
 
 enum Spells
@@ -34,20 +34,22 @@ enum Spells
 enum Events
 {
     EVENT_DARK_STRIKE           = 1,
-    EVENT_DEMORALIZING_SHOUT    = 2,
-    EVENT_INSPIRE               = 3,
-    EVENT_KNOCKDOWN             = 4,
-    EVENT_FLAMESPEAR            = 5,
+    EVENT_DEMORALIZING_SHOUT,
+    EVENT_INSPIRE,
+    EVENT_KNOCKDOWN,
+    EVENT_FLAMESPEAR,
 
-    EVENT_HEAL                  = 6,
-    EVENT_SHADOW_WORD_PAIN      = 7,
-    EVENT_IMMOLATE              = 8,
+    EVENT_HEAL,
+    EVENT_SHADOW_WORD_PAIN,
+    EVENT_IMMOLATE,
 };
 
 class boss_sulfuron : public CreatureScript
 {
 public:
-    boss_sulfuron() : CreatureScript("boss_sulfuron") { }
+    boss_sulfuron() : CreatureScript("boss_sulfuron")
+    {
+    }
 
     struct boss_sulfuronAI : public BossAI
     {
@@ -55,12 +57,12 @@ public:
         {
         }
 
-        void EnterCombat(Unit* victim) override
+        void EnterCombat(Unit* /*victim*/) override
         {
-            BossAI::EnterCombat(victim);
-            events.ScheduleEvent(EVENT_DARK_STRIKE, 10000);
-            events.ScheduleEvent(EVENT_DEMORALIZING_SHOUT, 15000);
-            events.ScheduleEvent(EVENT_INSPIRE, 13000);
+            _EnterCombat();
+            events.ScheduleEvent(EVENT_DARK_STRIKE, urand(4000, 7000));
+            events.ScheduleEvent(EVENT_DEMORALIZING_SHOUT, urand(6000, 20000));
+            events.ScheduleEvent(EVENT_INSPIRE, urand(7000, 10000));
             events.ScheduleEvent(EVENT_KNOCKDOWN, 6000);
             events.ScheduleEvent(EVENT_FLAMESPEAR, 2000);
         }
@@ -68,46 +70,60 @@ public:
         void UpdateAI(uint32 diff) override
         {
             if (!UpdateVictim())
+            {
                 return;
+            }
 
             events.Update(diff);
 
             if (me->HasUnitState(UNIT_STATE_CASTING))
+            {
                 return;
+            }
 
-            while (uint32 eventId = events.ExecuteEvent())
+            while (uint32 const eventId = events.ExecuteEvent())
             {
                 switch (eventId)
                 {
                     case EVENT_DARK_STRIKE:
-                        DoCast(me, SPELL_DARK_STRIKE);
-                        events.ScheduleEvent(EVENT_DARK_STRIKE, urand(15000, 18000));
+                    {
+                        DoCastSelf(SPELL_DARK_STRIKE);
+                        events.RepeatEvent(urand(4000, 7000));
                         break;
+                    }
                     case EVENT_DEMORALIZING_SHOUT:
+                    {
                         DoCastVictim(SPELL_DEMORALIZING_SHOUT);
-                        events.ScheduleEvent(EVENT_DEMORALIZING_SHOUT, urand(15000, 20000));
+                        events.RepeatEvent(urand(12000, 18000));
                         break;
+                    }
                     case EVENT_INSPIRE:
+                    {
+                        std::list<Creature*> healers = DoFindFriendlyMissingBuff(45.0f, SPELL_INSPIRE);
+                        if (!healers.empty())
                         {
-                            std::list<Creature*> healers = DoFindFriendlyMissingBuff(45.0f, SPELL_INSPIRE);
-                            if (!healers.empty())
-                                DoCast(acore::Containers::SelectRandomContainerElement(healers), SPELL_INSPIRE);
-
-                            DoCast(me, SPELL_INSPIRE);
-                            events.ScheduleEvent(EVENT_INSPIRE, urand(20000, 26000));
-                            break;
+                            DoCast(acore::Containers::SelectRandomContainerElement(healers), SPELL_INSPIRE);
                         }
+
+                        DoCastSelf(SPELL_INSPIRE);
+                        events.RepeatEvent(urand(13000, 20000));
+                        break;
+                    }
                     case EVENT_KNOCKDOWN:
+                    {
                         DoCastVictim(SPELL_KNOCKDOWN);
-                        events.ScheduleEvent(EVENT_KNOCKDOWN, urand(12000, 15000));
+                        events.RepeatEvent(urand(10000, 20000));
                         break;
+                    }
                     case EVENT_FLAMESPEAR:
+                    {
                         if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
+                        {
                             DoCast(target, SPELL_FLAMESPEAR);
-                        events.ScheduleEvent(EVENT_FLAMESPEAR, urand(12000, 16000));
+                        }
+                        events.RepeatEvent(urand(12000, 16000));
                         break;
-                    default:
-                        break;
+                    }
                 }
             }
 
@@ -142,45 +158,58 @@ public:
             events.Reset();
         }
 
-        void EnterCombat(Unit* victim) override
+        void EnterCombat(Unit* /*victim*/) override
         {
-            ScriptedAI::EnterCombat(victim);
             events.ScheduleEvent(EVENT_HEAL, urand(15000, 30000));
-            events.ScheduleEvent(EVENT_SHADOW_WORD_PAIN, 2000);
-            events.ScheduleEvent(EVENT_IMMOLATE, 8000);
+            events.ScheduleEvent(EVENT_SHADOW_WORD_PAIN, urand(2000, 4000));
+            events.ScheduleEvent(EVENT_IMMOLATE, urand(3500, 6000));
         }
 
         void UpdateAI(uint32 diff) override
         {
             if (!UpdateVictim())
+            {
                 return;
+            }
 
             events.Update(diff);
 
             if (me->HasUnitState(UNIT_STATE_CASTING))
+            {
                 return;
+            }
 
-            while (uint32 eventId = events.ExecuteEvent())
+            while (uint32 const eventId = events.ExecuteEvent())
             {
                 switch (eventId)
                 {
                     case EVENT_HEAL:
+                    {
                         if (Unit* target = DoSelectLowestHpFriendly(60.0f, 1))
+                        {
                             DoCast(target, SPELL_HEAL);
-                        events.ScheduleEvent(EVENT_HEAL, urand(15000, 20000));
+                        }
+                        events.RepeatEvent(urand(15000, 20000));
                         break;
+                    }
                     case EVENT_SHADOW_WORD_PAIN:
+                    {
                         if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -SPELL_SHADOWWORDPAIN))
+                        {
                             DoCast(target, SPELL_SHADOWWORDPAIN);
-                        events.ScheduleEvent(EVENT_SHADOW_WORD_PAIN, urand(18000, 26000));
+                        }
+                        events.RepeatEvent(urand(2500, 5000));
                         break;
+                    }
                     case EVENT_IMMOLATE:
+                    {
                         if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -SPELL_IMMOLATE))
+                        {
                             DoCast(target, SPELL_IMMOLATE);
-                        events.ScheduleEvent(EVENT_IMMOLATE, urand(15000, 25000));
+                        }
+                        events.RepeatEvent(urand(5000, 7000));
                         break;
-                    default:
-                        break;
+                    }
                 }
             }
 
