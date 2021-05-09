e5dea7a1a3a53863786ae77e4fceb9d4a5dfb4d2
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-09 04:16:51 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ea6c23218334b86535e42e097df1ad542b873a3e

--- data/sql/updates/db_world/2021_05_09_00.sql
+++ data/sql/updates/db_world/2021_05_09_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_08_00 -> 2021_05_09_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_08_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_08_00 2021_05_09_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1620115611701757811'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620115611701757811');
 
 SET 
@@ -72,3 +88,12 @@ UPDATE `gameobject_loot_template` SET `MaxCount`= 7 WHERE `Entry` = 12883 AND `I
 UPDATE `gameobject_loot_template` SET `MinCount`= 1 WHERE `Entry` = 12883 AND `Item` = @THORIUMORE; -- Thorium Ore
 UPDATE `gameobject_loot_template` SET `MaxCount`= 5 WHERE `Entry` = 12883 AND `Item` = @THORIUMORE; -- Thorium Ore
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
