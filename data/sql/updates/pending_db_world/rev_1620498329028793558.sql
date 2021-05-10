af3b2494231c4f72c4a0c490557279e3e31f6cd5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-10 04:16:10 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 5bd26262b052a9761c8f21cc99a200825d21b246

--- data/sql/updates/pending_db_world/rev_1620498329028793558.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620498329028793558');
-
--- Missing Reference Tables in STV
-UPDATE `creature_loot_template` SET `Reference` = 24723, `Comment` = 'Ana''thek the Cruel - (ReferenceTable)' WHERE `Entry`= 1059 AND `Item` = 24723;
-UPDATE `creature_loot_template` SET `Reference` = 24722, `Comment` = 'Gan''zulah - (ReferenceTable)' WHERE `Entry`= 1061 AND `Item` = 24722;
-UPDATE `creature_loot_template` SET `Reference` = 24723, `Comment` = 'Nezzliok the Dire - (ReferenceTable)' WHERE `Entry`= 1062 AND `Item` = 24723;
-UPDATE `creature_loot_template` SET `Reference` = 24736, `Comment` = 'Nezzliok the Dire - (ReferenceTable)' WHERE `Entry`= 1062 AND `Item` = 24736;
-
