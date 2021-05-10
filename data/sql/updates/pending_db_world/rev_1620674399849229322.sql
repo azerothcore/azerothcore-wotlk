33f9e3ad410e3cfaa672ad380e8ba23c5bfc8225
Necropola <necropola@rain-fighters.net>
2021-05-10 21:21:40 +0200

    fix(DB/item_template): Add coin loot to fishing trunks Closes AzerothCore #5759 Closes https://github.com/chromiecraft/chromiecraft/issues/582

---
+++ data/sql/updates/pending_db_world/rev_1620674399849229322.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620674399849229322');
+UPDATE `item_template` SET `minMoneyLoot`=50, `maxMoneyLoot`=100 WHERE `entry`=20708; -- Tightly Sealed Trunk
+UPDATE `item_template` SET `minMoneyLoot`=100, `maxMoneyLoot`=200 WHERE `entry`=21113; -- Watertight Trunk
+UPDATE `item_template` SET `minMoneyLoot`=100, `maxMoneyLoot`=200 WHERE `entry`=21150; -- Iron Bound Trunk
+UPDATE `item_template` SET `minMoneyLoot`=100, `maxMoneyLoot`=200 WHERE `entry`=21228; -- Mithril Bound Trunk
