f6f638b46bd3a578338f948f5ad76a96cad31a06
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-10 06:21:26 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ca74d3081e7a4172a7fc2aad3e487fb129971cb2

--- data/sql/updates/db_world/2021_05_10_05.sql
+++ data/sql/updates/db_world/2021_05_10_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_10_04 -> 2021_05_10_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_10_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_10_04 2021_05_10_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1620531160911239800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620531160911239800');
 
 -- Mosh'Ogg Warmonger Entry 709 --
@@ -14,3 +30,12 @@ UPDATE `creature` SET `spawntimesecs` = 300 WHERE `guid` IN (521, 522, 526, 761)
 
 -- Mosh'Ogg Spellcrafter Entry 710 --
 UPDATE `creature` SET `spawntimesecs` = 300 WHERE `guid` IN (527, 528, 623, 626, 628, 762, 1093);
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
