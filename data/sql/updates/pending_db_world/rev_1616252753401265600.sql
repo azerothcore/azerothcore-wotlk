197f6656680f9c0fd5b965f4b3ea5c8b999fd208
Knindza <knindzagxg@gmail.com>
2021-05-11 20:07:24 +0200

    Update rev_1616252753401265600.sql

--- data/sql/updates/pending_db_world/rev_1616252753401265600.sql
+++ data/sql/updates/pending_db_world/rev_1616252753401265600.sql
@@ -35,7 +35,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19731;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 19731);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(19731, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 11, 33731, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Nether Beast - On Aggro - Cast \'33731\' (No Repeat)');
+(19731, 0, 0, 0, 0, 0, 100, 1, 1500, 2000, 0, 0, 75, 33731, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Nether Beast - In Combat - Add Aura \'33731\' (No Repeat)');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 20404;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 20404);
