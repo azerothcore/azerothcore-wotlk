1b6c2004ce05ee6934bf1fa1562f7eae4cc74d07
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-14 08:15:03 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): c3242caf74d0fd3896e5c8bfeda2b07d9020a051

--- data/sql/updates/db_world/2021_05_14_03.sql
+++ data/sql/updates/db_world/2021_05_14_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_14_02 -> 2021_05_14_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_14_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_14_02 2021_05_14_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1620477325004735900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620477325004735900');
 
 SET @ENTRY := 4362;
@@ -7,3 +23,12 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,0,0,2,0,100,1,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0,"Mirefin Coastrunner - Between 0-15% Health - Flee For Assist (No Repeat)"),
 (@ENTRY,0,1,0,0,0,100,0,2000,4500,12000,13000,11,37998,0,0,0,0,0,2,0,0,0,0,0,0,0,"Mirefin Coastrunner - In Combat - Cast 'Piercing Strike'");
 
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
--- data/sql/updates/pending_db_world/rev_1620586666463013100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620586666463013100');
-
--- Imp - Felhunter - Voidwalker - Succubus --
-UPDATE `creature_template` SET `InhabitType` = 3 WHERE `entry` IN (416, 417, 1860, 1863);
