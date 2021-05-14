cf2212ca5ed551684e4223deda00f680e5bb549c
anguaive <angurg98@gmail.com>
2021-05-14 11:07:44 +0200

    fix(DB/gameobject): Remove Redridge Peaceblooms
    Peacebloom nodes shouldn't spawn in Redridge Mountains zone according
    to sources, so the ones I found are being removed.
    
    Closes #5836

--- data/sql/updates/pending_db_world/rev_1620978339651948745.sql
+++ data/sql/updates/pending_db_world/rev_1620978339651948745.sql
@@ -1,5 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620978339651948745');
 
-UPDATE `gameobject`
-SET `position_x` = -9858, `position_y` = -1773, `position_z` = 23.508
-WHERE `guid` = 1396;
+DELETE FROM `gameobject`
+WHERE `guid` in (1396, 1374, 1686, 1740);
