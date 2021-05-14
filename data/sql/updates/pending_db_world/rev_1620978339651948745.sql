7ce303da1f0d3fc5b0af485f9c5e1859063178c5
anguaive <angurg98@gmail.com>
2021-05-14 10:06:51 +0200

    fix(DB/gameobject): Peacebloom spawn location
    Peacebloom node (id: 1396) was positioned on a road. It has been moved
    to a nearby location next to the road, where there are no other herb
    nodes close by.
    
    Closes #5836

---
+++ data/sql/updates/pending_db_world/rev_1620978339651948745.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620978339651948745');
+
+UPDATE `gameobject`
+SET `position_x` = -9858, `position_y` = -1773, `position_z` = 23.508
+WHERE `guid` = 1396;
