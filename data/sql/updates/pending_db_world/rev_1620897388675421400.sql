d16c0c7ae3aa9cbaa2e8e479aa39440bf54b9448
Emir <emirpod02@gmail.com>
2021-05-14 17:12:05 +0200

    Delete rev_1620897388675421400.sql

--- data/sql/updates/pending_db_world/rev_1620897388675421400.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620897388675421400');
-
-DELETE FROM `skinning_loot_template` WHERE `Entry`=60042;
-INSERT INTO `skinning_loot_template` (`Entry`,`Item`,`Reference`,`Chance`,`QuestRequired`,`LootMode`,`GroupId`,`MinCount`,`MaxCount`,`Comment`) VALUES
-(60042,4234,0,35,0,1,1,1,2,"Heavy Leather"),
-(60042,4235,0,5,0,1,1,1,1,"Heavy Hide"),
-(60042,4304,0,50,0,1,1,1,2,"Thick Leather"),
-(60042,8165,0,5,0,1,1,1,2,"Worn Dragonscale"),
-(60042,8169,0,5,0,1,1,1,1,"Thick Hide");
