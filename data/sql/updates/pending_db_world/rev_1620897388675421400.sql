6dd7e45e70c0656a514a7300a87df7a8eff11e1f
Emir <emirpod02@gmail.com>
2021-05-14 09:44:05 +0200

    Update rev_1620897388675421400.sql

--- data/sql/updates/pending_db_world/rev_1620897388675421400.sql
+++ data/sql/updates/pending_db_world/rev_1620897388675421400.sql
@@ -1,7 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620897388675421400');
 
-DELETE FROM `skinning_loot_template` WHERE `Entry` BETWEEN 60041 AND 60048;
-DELETE FROM `skinning_loot_template` WHERE `Entry` IN (2725,4323,4324,5278,2447,2726,4328,4329,4331,4334,4339,10678,14398,12129,5276,8196,8197,8198);
+DELETE FROM `skinning_loot_template` WHERE `Entry`=60042;
 INSERT INTO `skinning_loot_template` (`Entry`,`Item`,`Reference`,`Chance`,`QuestRequired`,`LootMode`,`GroupId`,`MinCount`,`MaxCount`,`Comment`) VALUES
 (60042,4234,0,35,0,1,1,1,2,"Heavy Leather"),
 (60042,4235,0,5,0,1,1,1,1,"Heavy Hide"),
