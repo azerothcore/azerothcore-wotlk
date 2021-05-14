520a44aace860ba06c0f749e1f5b5586c55ec46c
Silker <61223313+Si1ker@users.noreply.github.com>
2021-05-14 02:09:21 -0600

    fix(DB/creature_addon): Remove path info from Blackwater Deckhand (#5758)

---
+++ data/sql/updates/pending_db_world/rev_1620616751102617000.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620616751102617000');
+
+-- Remove path
+UPDATE `creature_addon` SET `path_id`=0 WHERE `guid`=11198;
+-- Make npc walk around
+UPDATE `creature` SET `MovementType`=1 WHERE `guid`=11198;
+
