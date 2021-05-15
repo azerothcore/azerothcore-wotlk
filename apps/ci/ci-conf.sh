fd6e879d99bfd213ba67217f5fbe5be021e49a67
FrancescoBorzi <borzifrancesco@gmail.com>
2021-05-16 00:49:26 +0200

    chore(wip): wip

--- apps/ci/ci-conf.sh
+++ apps/ci/ci-conf.sh
@@ -53,16 +53,16 @@ case $COMPILER in
     echo "CCOMPILERCXX=\"clang++-6.0\"" >> ./conf/config.sh
     ;;
 
-  "clang9" )
-    time sudo apt-get install -y clang-9
-    echo "CCOMPILERC=\"clang-9\"" >> ./conf/config.sh
-    echo "CCOMPILERCXX=\"clang++-9\"" >> ./conf/config.sh
-    ;;
-
   "clang8" )
     time sudo apt-get install -y clang-8
     echo "CCOMPILERC=\"clang-8\"" >> ./conf/config.sh
     echo "CCOMPILERCXX=\"clang++-8\"" >> ./conf/config.sh
+    ;;
+
+  "clang9" )
+    time sudo apt-get install -y clang-9
+    echo "CCOMPILERC=\"clang-9\"" >> ./conf/config.sh
+    echo "CCOMPILERCXX=\"clang++-9\"" >> ./conf/config.sh
     ;;
 
   "clang10" )
