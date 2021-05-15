7afeb4b4325513f4fbe7df50bb544cc11e45d373
Branel <emirpod02@gmail.com>
2021-05-15 09:50:57 +0200

    Update rev_1621064404059904900.sql

--- data/sql/updates/pending_db_world/rev_1621064404059904900.sql
+++ data/sql/updates/pending_db_world/rev_1621064404059904900.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621064404059904900');
-UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter of Undercity.' WHERE (`ID` = 1899);
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter.' WHERE (`ID` = 1899);
 UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter' WHERE (`ID` = 1886);
 UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Andron Gant at Undercity in the Apothecarium.' WHERE (`ID` = 14419);
 UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter.' WHERE (`ID` = 14421);
