819c261d8a0b7d35f0e0e5cbe0be5c5b28de1ed2
Necropola <necropola@rain-fighters.net>
2021-05-15 22:56:07 +0200

    Remove space at end of line

--- data/sql/updates/pending_db_world/rev_1621105426491747047.sql
+++ data/sql/updates/pending_db_world/rev_1621105426491747047.sql
@@ -4,5 +4,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621105426491747047');
 DELETE FROM `smart_scripts` WHERE `entryorguid`=2926 AND `id`=1;
 
 -- The `creature_template` table does already have the updated (black) modelid (9562),
--- but the `creature` table (spawns) does not. Change modelid: 11418 --> 9562 
+-- but the `creature` table (spawns) does not. Change modelid: 11418 --> 9562
 UPDATE `creature` SET `modelid`=9562 WHERE `id`=2926;
