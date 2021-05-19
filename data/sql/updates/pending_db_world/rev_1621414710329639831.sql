16b28a3cf1d9beac49539f71026d1eb10d1152ce
anguaive <angurg98@gmail.com>
2021-05-19 11:50:55 +0200

    Delete before insert

--- data/sql/updates/pending_db_world/rev_1621414710329639831.sql
+++ data/sql/updates/pending_db_world/rev_1621414710329639831.sql
@@ -5,5 +5,6 @@ SET @GESHARAHAN := 3398;
 
 DELETE FROM `reference_loot_template` where `Item` = @SHIVER_BLADE;
 
+DELETE FROM `creature_loot_template` WHERE `Entry` = @GESHARAHAN AND `Item` = @SHIVER_BLADE;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (@GESHARAHAN, @SHIVER_BLADE, 0, 72.12, 0, 1, 0, 1, 1, "Gesharahan - Shiver Blade");
