3916cdf377758f29cce7ca3eb0092da8cc928947
tkn963 <tkn963@gmail.com>
2021-05-19 17:11:37 +0200

    fix(DB/GOSSIP) Duplicate gossip option
    Sergeant Bly (id: 7604) had duplicate gossip menu options

---
+++ data/sql/updates/pending_db_world/rev_1621437037316104400.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621437037316104400');
+
+DELETE FROM `gossip_menu_option` WHERE `MenuID`=941 AND `OptionID`=1;
+DELETE FROM `broadcast_text` WHERE `ID`=4165;
