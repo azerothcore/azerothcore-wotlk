0107a87510a4fd6eab5a3904ab69f7b92de06eb1
tkn963 <tkn963@gmail.com>
2021-05-20 00:45:32 +0200

    fix(DB/GOSSIP): Do not delete broadcast_text
    Reverted the deleting from broadcast_text

--- data/sql/updates/pending_db_world/rev_1621437037316104400.sql
+++ data/sql/updates/pending_db_world/rev_1621437037316104400.sql
@@ -2,4 +2,3 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621437037316104400');
 
 DELETE FROM `gossip_menu_option` WHERE `MenuID`=941 AND `OptionID`=1;
 DELETE FROM `gossip_menu_option_locale` WHERE `MenuID`=941 AND `OptionID`=1;
-DELETE FROM `broadcast_text` WHERE `ID`=4165;
