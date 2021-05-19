6d3901cf0531344bc7b1ffee360be27118228122
anguaive <angurg98@gmail.com>
2021-05-19 11:31:31 +0200

    fix(DB/loot): Fix Shiver Blade drop
    Only Gesharahan (id: 3398) should be able to drop Shiver Blade
    (id: 5182), with a ~72% drop chance.
    
    Closes #5943
    Closes https://github.com/chromiecraft/chromiecraft/issues/672

---
+++ data/sql/updates/pending_db_world/rev_1621414710329639831.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621414710329639831');
+
+SET @SHIVER_BLADE := 5182;
+SET @GESHARAHAN := 3398;
+
+DELETE FROM `reference_loot_template` where `Item` = @SHIVER_BLADE;
+
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(@GESHARAHAN, @SHIVER_BLADE, 0, 72.12, 0, 1, 0, 1, 1, "Gesharahan - Shiver Blade");
