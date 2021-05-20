e197d72102e29ca7eae76f20ae3f7648f2b6c623
anguaive <angurg98@gmail.com>
2021-05-20 11:04:45 +0200

    fix(DB/creature): Fix Sunscale Lashtails movement
    Sunscale Lashtails (id: 40489 - 40496) were standing still and their
    respawn time was significantly lower compared to their wandering
    brethren (id: 19809 - 19817). I modified the immobile creatures'
    attributes to match with the moving ones.
    
    Closes #5940
    Closes chromiecraft/chromiecraft#673

---
+++ data/sql/updates/pending_db_world/rev_1621501368347745883.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621501368347745883');
+
+UPDATE `creature`
+SET `modelid` = 1744, `spawntimesecs` = 275, `wander_distance` = 5, `curhealth` = 178, `movementtype` = 1
+WHERE `guid` BETWEEN 40489 AND 40496;
