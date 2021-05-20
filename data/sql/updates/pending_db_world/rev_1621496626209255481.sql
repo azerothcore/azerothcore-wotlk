3b332d975c75baea0921005170139690e03ae5ef
anguaive <angurg98@gmail.com>
2021-05-20 09:51:47 +0200

    fix(DB/gameobject): Fix Andorhal sign location
    Andorhal sign (id: 30079) was floating in midair next to a signpost, but
    its orientation was correct. It has been attached to the signpost, below
    the sign pointing to Strahnbrad.
    
    Closes #5945
    Closes https://github.com/chromiecraft/chromiecraft/issues/674

---
+++ data/sql/updates/pending_db_world/rev_1621496626209255481.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621496626209255481');
+
+UPDATE `gameobject` 
+SET `position_x` = 455.827, `position_y` = -628.813, `position_z` = 167
+WHERE `guid` = 30079;
