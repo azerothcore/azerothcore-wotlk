56ed3dbd2548fbdf3a285b2ef87554332edf579f
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-05-22 22:40:19 +0200

    refactor(DB/Version): Add date on update

---
+++ data/sql/updates/pending_db_auth/rev_1621715473238990700.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1621715473238990700');
+
+ALTER TABLE `version_db_auth`
+	ADD COLUMN `date` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci' AFTER `required_rev`;
