0cb7843bb0914b4176063aaeee20f6fa3f62ec8f
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-05-22 23:00:42 +0200

    utf8mb4_general_ci

--- data/sql/updates/pending_db_characters/rev_1621715444570678000.sql
+++ data/sql/updates/pending_db_characters/rev_1621715444570678000.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1621715444570678000');
 
 ALTER TABLE `version_db_characters`
-	ADD COLUMN `date` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci' AFTER `required_rev`;
+	ADD COLUMN `date` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci' AFTER `required_rev`;
