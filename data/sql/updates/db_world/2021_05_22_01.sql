5a32ecc8cecd48bbe9a8f196a4ea378e38748d57
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-22 19:55:17 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 0527c17d5f92504bbec8634cfe81321ff441ff2a

--- data/sql/updates/db_world/2021_05_22_01.sql
+++ data/sql/updates/db_world/2021_05_22_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_22_00 -> 2021_05_22_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_22_00 2021_05_22_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1620469549972464900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620469549972464900');
 UPDATE `creature_text` SET `language`=7, `BroadcastTextid`=13770 WHERE `creatureid`=17312 AND `groupid`=1;
 UPDATE `creature_text` SET `emote`=5 WHERE `creatureid`=17312 AND `groupid`=2;
@@ -10,3 +26,12 @@ INSERT INTO `creature_text` (`creatureid`, `groupid`, `id`, `text`, `type`, `lan
 
 UPDATE `script_waypoint` SET `waittime`=3000 WHERE `entry`=17312 AND `pointid`=28;
 UPDATE `script_waypoint` SET `waittime`=5000 WHERE `entry`=17312 AND `pointid`=29;
+
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
