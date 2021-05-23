af28aeebde8f8d9dec96259a594af595e1308d33
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-23 05:15:30 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 3bc7880d0a3df3af6bdf34a0a9da0b46e3b51548

--- data/sql/updates/pending_db_world/rev_1621246317595402000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621246317595402000');
-
-UPDATE `gossip_menu_option` SET `OptionBroadcastTextID` = 23112 WHERE `MenuID` = 9546 AND `OptionID` = 1; 
-UPDATE `gossip_menu_option` SET `OptionBroadcastTextID` = 26697 WHERE `MenuID` = 9546 AND `OptionID` = 2; 
