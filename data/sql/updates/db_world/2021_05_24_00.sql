5c70a7b9e128aa73a579d2379c3e434db6ba12c0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-24 21:29:53 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 34e7bc24e80c5f34906ee6aaa179c6ed92d6e3c7

--- data/sql/updates/db_world/2021_05_24_00.sql
+++ data/sql/updates/db_world/2021_05_24_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_23_03 -> 2021_05_24_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_23_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_23_03 2021_05_24_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1620427214886749400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620427214886749400');
 
 -- Redridge Mountains 
@@ -15,3 +31,11 @@ UPDATE `gameobject` SET `position_x`=-1437.727, `position_y`=-3085.670, `positio
 
 -- Tanaris 
 UPDATE `gameobject` SET `position_x`=-8096.516, `position_y`=-4314.403, `position_z`=17.563, `orientation`=2.512113 WHERE `guid`=6494;
+--
+-- END UPDATING QUERIES
+--
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
