6ab8a7c7b2dfd4c564ed8238d6e329799fdf7c8d
Patrick Lewis <Patrick.Lewis@modusbox.com>
2021-05-24 23:27:04 -0700

    fix(Deps/Boost) fix for ci

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -8,14 +8,14 @@
 sudo apt update
 
 # shared deps
-sudo apt-get -y install make cmake clang curl unzip libboost-all-dev libmysqlclient-dev libace-dev ccache google-perftools
+sudo apt-get -y install make cmake clang curl unzip libmysqlclient-dev libace-dev ccache google-perftools
 
 if [[ $CONTINUOUS_INTEGRATION || $DOCKER ]]; then
-  sudo apt-get -y install build-essential libtool cmake-data openssl libgoogle-perftools-dev \
-  libssl-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev mysql-client \
+  sudo add-apt-repository -y ppa:mhier/libboost-latest && apt update && apt-get -y install build-essential libtool cmake-data openssl libgoogle-perftools-dev \
+  libboost1.74-dev libssl-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev mysql-client \
   libncurses5-dev
 else
   sudo apt-get install -y git gcc g++ gdb gdbserver \
-  libssl-dev libbz2-dev libreadline-dev libncurses-dev \
+  libboost-all-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev \
   mysql-server libace-6.*
 fi
