fcca12afbb9ca698c2e70449e510f367e19ed1e9
Stifler82 <xspidey@hotmail.it>
2021-05-28 13:58:44 +0100

    fix(DB/spell_dbc): Imp's Phase Shift

--- src/server/scripts/Pet/pet_warlock.cpp
+++ src/server/scripts/Pet/pet_warlock.cpp
@@ -7,37 +7,40 @@
  * Scriptnames of files in this file should be prefixed with "npc_pet_warlock_".
  */
 
-#include "ScriptedCreature.h"
-#include "ScriptMgr.h"
+/*#include "ScriptedCreature.h"
+/*#include "ScriptMgr.h"
 
-enum WarlockSpells
-{
-    SPELL_WARLOCK_PHASE_SHIFT = 4511, // Imp spell
-};
+/*
+ * enum WarlockSpells
+ * {
+ * 	SPELL_WARLOCK_PHASE_SHIFT = 4511, // Imp spell
+ * };
+ * 
+ * class npc_pet_warlock_imp : public CreatureScript
+ * {
+ * public:
+ * 	npc_pet_warlock_imp() : CreatureScript("npc_pet_warlock_imp") { }
+ * 
+ * 	struct npc_pet_warlock_impAI : public ScriptedAI
+ * 	{
+ * 		npc_pet_warlock_impAI(Creature* creature) : ScriptedAI(creature) { }
+ * 
+ * 		void EnterCombat(Unit* /*who*//*) override
+ * 		{
+ * 			me->RemoveAurasDueToSpell(SPELL_WARLOCK_PHASE_SHIFT);
+ * 		}
+ * 	};
+ * 
+ * 	CreatureAI* GetAI(Creature* creature) const override
+ * 	{
+ * 		return new npc_pet_warlock_impAI(creature);
+ * 	}
+ * };
+ * 
+ * 
+ * void AddSC_warlock_pet_scripts()
+ * {
+ * 	new npc_pet_warlock_imp();
+ * }
 
-class npc_pet_warlock_imp : public CreatureScript
-{
-public:
-    npc_pet_warlock_imp() : CreatureScript("npc_pet_warlock_imp") { }
-
-    struct npc_pet_warlock_impAI : public ScriptedAI
-    {
-        npc_pet_warlock_impAI(Creature* creature) : ScriptedAI(creature) { }
-
-        void EnterCombat(Unit* /*who*/) override
-        {
-            me->RemoveAurasDueToSpell(SPELL_WARLOCK_PHASE_SHIFT);
-        }
-    };
-
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return new npc_pet_warlock_impAI(creature);
-    }
-};
-
-
-void AddSC_warlock_pet_scripts()
-{
-    new npc_pet_warlock_imp();
-}
+ */
