a1119ecac2701ef2f97bbbed8a369f17c7c9dcc4
Azcobu <quux.gitazco@mailnull.com>
2021-05-29 15:47:18 +0930

    fix(DB/loot): Removes various overlevelled gear drops This PR removes a group of items that are between 7 and 13 levels higher than the mobs that drop them.

---
+++ data/sql/updates/pending_db_world/rev_1622268692168409196.sql
@@ -0,0 +1,24 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622268692168409196');
+
+-- Silicate Feeder -> removes Silithid Husked Launcher, Antenna of Invigoration, The Lost Kris of Zedd,
+-- Coif of Elemental Fury, Polished Obsidian Pauldrons
+DELETE FROM `creature_loot_template` WHERE `Entry` = 15333 AND `Item` IN (21800, 21801, 21802, 21804, 21805);
+-- Cave Creeper -> removes Lefty's Brass Knuckle
+DELETE FROM `creature_loot_template` WHERE `Entry` = 8933 AND `Item` = 22317;
+-- Borer Beetle -> removes Shroud of Arcane Mastery 
+DELETE FROM `creature_loot_template` WHERE `Entry` = 8932 AND `Item` = 22330;
+-- Dark Screecher -> removes Lefty's Brass Knuckle, Malgen's Long Bow
+DELETE FROM `creature_loot_template` WHERE `Entry` = 8927 AND `Item` IN (22317, 22318); 
+-- Deep Stinger -> removes Ironweave Mantle
+DELETE FROM `creature_loot_template` WHERE `Entry` = 8926  AND `Item` = 22305;
+-- Dredge Worm -> removes Malgen's Long Bow
+DELETE FROM `creature_loot_template` WHERE `Entry` = 8925 AND `Item` = 22318;
+-- Wailing Death -> removes Idol of Brutality 
+DELETE FROM `creature_loot_template` WHERE `Entry` = 1804 AND `Item` = 23198;
+-- Unfettered Spirit -> removes Steelclaw Reaver 
+DELETE FROM `creature_loot_template` WHERE `Entry` = 4308 AND `Item` = 7761;
+-- Tink Sprocketwhistle -> removes Gnomeregan Amulet 
+DELETE FROM `creature_loot_template` WHERE `Entry` = 9676 AND `Item` = 10299;
+-- Scarlet Spellbinder, Stitched Horror, Eyeless Watcher, Chromatic Whelp, Risen Aberration,
+-- Stonelash Flayer, Desert Rumbler, Carrion Swarmer -> removes Idol of Brutality
+DELETE FROM `creature_loot_template` WHERE `Entry` IN (4494, 8543, 8539, 10442, 10485, 11737, 11746, 13160) AND `Item` = 23198;
