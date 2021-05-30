fbe3b458f882ffe82d08fa1ec18fe9d879fb5480
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-30 17:33:28 +0700

    3

--- data/sql/updates/pending_db_world/rev_1622302528765274600.sql
+++ data/sql/updates/pending_db_world/rev_1622302528765274600.sql
@@ -1,6 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622302528765274600');
 
---
 DELETE FROM `acore_string` WHERE `entry` IN (713, 726);
 INSERT INTO `acore_string`(`entry`, `content_default`) VALUES
 (713, 'Queue status for %s (skirmish %s) (Lvl: %u to %u)\nQueued: %u (Need at least %u more)'),
