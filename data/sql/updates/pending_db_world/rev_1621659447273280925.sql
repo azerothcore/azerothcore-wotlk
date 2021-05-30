e93159b408f2ce2162b98c9a631fa7b932a97593
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-30 18:13:24 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): bc9b33a1a3ab6051d0c78d567dc4cc573a46a8ec

--- data/sql/updates/pending_db_world/rev_1621659447273280925.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621659447273280925');
-
-DELETE FROM `reference_loot_template` WHERE `Entry` = 24062 AND `Item` = 880;
-
-DELETE FROM `creature_loot_template` WHERE `Entry` = 202 AND `Item` = 880;
-
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(202, 880, 0, 1.6, 0, 1, 0, 1, 1, "Skeletal Horror - Staff of Horrors");
-
