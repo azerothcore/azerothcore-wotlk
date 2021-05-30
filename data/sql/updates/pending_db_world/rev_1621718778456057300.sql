75c1e6132095245b87c1acaa0c4272a917aa6ad3
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-30 19:13:27 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ae665f7ec32c5da3877b4acb0d0dd2534e9cdd06

--- data/sql/updates/pending_db_world/rev_1621718778456057300.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621718778456057300');
-
-DELETE FROM `command` WHERE `name` = 'server debug';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('server debug', 3, 'Syntax: .server debug\r\nShows detailed information about the server setup, useful when reporting a bug.');
