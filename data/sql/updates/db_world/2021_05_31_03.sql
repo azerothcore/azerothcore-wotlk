579dbe1e5ffc3ba6240ab99a8794e0760e4dfdb3
AzerothCoreBot <azerothcorebot@gmail.com>
2021-05-31 17:05:09 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): d46a55d9a01085aaf84a4ab69355d856fedbf62a

--- data/sql/updates/db_world/2021_05_31_03.sql
+++ data/sql/updates/db_world/2021_05_31_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_05_31_02 -> 2021_05_31_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_05_31_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_05_31_02 2021_05_31_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1621746341007519100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621746341007519100');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 36551;
@@ -13,3 +29,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 -- Not optimal but right now no way around it
 UPDATE `creature_template` SET `type_flags`=`type_flags`|1048576 WHERE `entry` = 36551;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_05_31_03' WHERE sql_rev = '1621746341007519100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
