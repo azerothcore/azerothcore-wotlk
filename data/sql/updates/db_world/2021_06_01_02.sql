780ff33c23b032bebebcb19b441264c8fcfa4751
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-01 12:41:21 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ee7d54b7c39ff6d3efc1ef58dca09593350775e8

--- data/sql/updates/db_world/2021_06_01_02.sql
+++ data/sql/updates/db_world/2021_06_01_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_01_01 -> 2021_06_01_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_01_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_01_01 2021_06_01_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1621933074448445073'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621933074448445073');
 
 -- Lord Baurles K. Wishock
@@ -15,3 +31,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@ENTRY, 0, 5, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 67, 2, 5000, 5000, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Lord Baurles K. Wishock - On Text 0 Over - Create Timed Event'),
 (@ENTRY, 0, 6, 0, 59, 0, 100, 0, 2, 0, 0, 0, 0, 37, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Lord Baurles K. Wishock - On Timed Event 2 Triggered - Kill Self'),
 (@ENTRY, 0, 7, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 17, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Lord Baurles K. Wishock - On Respawn - Set Emote State 0');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_01_02' WHERE sql_rev = '1621933074448445073';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
