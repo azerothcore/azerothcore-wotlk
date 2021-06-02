4c55e9c3bfd3a552931eab595ec270123703ec99
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-02 15:48:54 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 3205eca428d6cca392fd02db4261e55d14207dd0

--- data/sql/updates/pending_db_world/rev_1622117565567403115.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622117565567403115');
-
--- Remove Shriveled Heart from Farmer Ray
-DELETE FROM `creature_loot_template` WHERE `Entry` = 232 AND `Item` = 9243;
--- Remove Sunroc Mask from Sawtooth Crocolisk
-DELETE FROM `creature_loot_template` WHERE `Entry` = 1082 AND `Item` = 24737;
--- Remove Sunroc Gloves from Sawtooth Snapper
-DELETE FROM `creature_loot_template` WHERE `Entry` = 1087 AND `Item` = 24736;
