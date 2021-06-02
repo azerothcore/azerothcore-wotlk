ab073e9677f13c146137a448161ae649fbc27e44
anguaive <angurg98@gmail.com>
2021-06-02 09:14:01 +0200

    ffs

--- data/sql/updates/pending_db_world/rev_1622617341739892363.sql
+++ data/sql/updates/pending_db_world/rev_1622617341739892363.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622617341739892363');
 
 UPDATE `creature` SET `wander_distance` = 10, `MovementType` = 1 WHERE `guid` = 18424 AND `id` = 2529;
-
