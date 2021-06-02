fb9ff74ecb0b0d97e3c1c102d0b89512c8cb9551
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-02 12:15:21 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 95455e61a29bc8d9fd34b3f627200cbcae523032

--- data/sql/updates/pending_db_world/rev_1621968262932056500.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621968262932056500');
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 3928) AND (`Item` IN (2592, 4306, 5519, 6712, 16747, 16748));
