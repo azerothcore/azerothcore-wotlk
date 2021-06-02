1f0479b5936620b9581789c1e18b8ae587dd5031
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-02 14:06:11 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 8747f965c16c0fe2ca5ab22ee884efa17bb1b919

--- data/sql/updates/db_world/2021_06_02_06.sql
+++ data/sql/updates/db_world/2021_06_02_06.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_02_05 -> 2021_06_02_06
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_02_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_02_05 2021_06_02_06 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1622046680993341100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622046680993341100');
 
 -- Have Lunaclaw set data to the Moonkin Stone when it is summoned + add movement to Darkshore spawn
@@ -16,3 +32,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 -- Update Lunaclaw spawn position in Darkshore
 UPDATE `event_scripts` SET `x` = 6359.26, `y` = 143.38, `z` = 23.61, `o` = 4.124 WHERE  `id`= 5991 AND `delay` = 0 AND `command` = 10;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_02_06' WHERE sql_rev = '1622046680993341100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
