84c9d1c46201372fcc2c7632f3da93aa724d758c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-02 15:10:52 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 2e6b33289b10c6afde29f2c9de27bf13810b0a13

--- data/sql/updates/db_world/2021_06_02_09.sql
+++ data/sql/updates/db_world/2021_06_02_09.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_02_08 -> 2021_06_02_09
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_02_08';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_02_08 2021_06_02_09 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1622106798992540218'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622106798992540218');
 
 -- Noblegarden items:
@@ -27,3 +43,13 @@ INSERT INTO `creature_loot_template` VALUES
 
 -- Add missing lootids
 UPDATE `creature_template` SET `lootid` = `entry` WHERE `entry` IN (16112, 2389, 15500, 16134, 16133, 2302, 1573);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_02_09' WHERE sql_rev = '1622106798992540218';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
