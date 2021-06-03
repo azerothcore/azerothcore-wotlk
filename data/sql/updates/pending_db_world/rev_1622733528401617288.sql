142fd75295e9324a8d62162313b91ae41b60f5a0
anguaive <angurg98@gmail.com>
2021-06-03 18:36:41 +0200

    Replace subquery with its results

--- data/sql/updates/pending_db_world/rev_1622733528401617288.sql
+++ data/sql/updates/pending_db_world/rev_1622733528401617288.sql
@@ -1,12 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622733528401617288');
 
 UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1
-WHERE `guid` IN (
-    SELECT c.guid 
-    FROM 
-	`creature` c 
-	JOIN `creature_template` ct ON c.id = ct.entry
-    WHERE c.wander_distance = 0 
-    AND c.MovementType = 0 
-    AND (ct.name LIKE "%Razormaw%" OR ct.name LIKE "%Bloodfen%" OR ct.name = "Goreclaw the Ravenous")
-);
+WHERE `guid` IN (9720, 9761, 9981, 10226, 31701, 31706, 31708, 31710);
