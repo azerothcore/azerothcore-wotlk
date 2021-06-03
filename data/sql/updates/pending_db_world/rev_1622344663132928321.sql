40cf7b46d221e562a20264316d660318af213690
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-03 13:01:26 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 31a13d32bbd8f0edfe5682c111675322bdb61588

--- data/sql/updates/pending_db_world/rev_1622344663132928321.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622344663132928321');
-
-DELETE FROM `pool_gameobject` WHERE `pool_entry` = 994 AND `guid` = 1962;
-DELETE FROM `gameobject` WHERE `id` = 1619 AND `guid` = 1962;
