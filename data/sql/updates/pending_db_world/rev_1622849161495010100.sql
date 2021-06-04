3cd4b2c52493cd4f0d3426186044317c99b77915
dsegna <dsegna@users.noreply.github.com>
2021-06-04 19:46:12 -0400

    fix(DB/Creature): Some Nethergarde Foremans missing patrol pattern
    Set MovementType to 1

--- data/sql/updates/pending_db_world/rev_1622849161495010100.sql
+++ data/sql/updates/pending_db_world/rev_1622849161495010100.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622849161495010100');
 
-UPDATE `creature` SET `wander_distance` = 5 WHERE `guid` IN (3713, 3863, 3878);
+UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1 WHERE `guid` IN (3713, 3863, 3878);
