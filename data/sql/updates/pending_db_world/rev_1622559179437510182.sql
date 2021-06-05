102d82d40324a51c79f7a62eb09ae39a62685f82
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-05 23:51:46 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ef2f073df45acce511fc70a13fa613ba42d9b46f

--- data/sql/updates/pending_db_world/rev_1622559179437510182.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622559179437510182');
-
-SET @KURDROS := 16025;
-SET @GRANISTAD := 16027;
-UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1 WHERE `guid` IN (@KURDROS, @GRANISTAD);
