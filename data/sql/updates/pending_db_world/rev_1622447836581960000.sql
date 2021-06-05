f819cf86fd7f0eb5cfd30ede0b28781a3e957388
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-05 12:39:42 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 752038e25bbe86d9bfa30a7058a67285b938bb0e

--- data/sql/updates/pending_db_world/rev_1622447836581960000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622447836581960000');
-
-UPDATE `item_template_locale` SET `Name` = '完美反光茶晶石' , `Description` = '对应红色或黄色插槽。' WHERE `ID` = '41491' AND `locale` = 'zhCN';
-UPDATE `item_template_locale` SET `Name` = '完美閃爍的巨黃晶' , `Description` = '與紅色或黃色插槽相容。' WHERE `ID` = '41491' AND `locale` = 'zhTW';
