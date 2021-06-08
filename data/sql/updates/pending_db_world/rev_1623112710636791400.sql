3b3145e622577a2134bee404e54dd3d55db57c79
Christopher Höllriegl <c.hoellriegl@hotmail.de>
2021-06-08 10:59:42 +0200

    feat(CORE/Commands): GM command to display strings - Implements a new GM command to display strings from the acore_string table - Syntax: .string #id [#locale] - Closes https://github.com/azerothcore/azerothcore-wotlk/issues/1052

---
+++ data/sql/updates/pending_db_world/rev_1623112710636791400.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623112710636791400');
+
+INSERT INTO `acore_string` VALUES ('6617', 'No acore_string for id: %i found.', null, null, 'Es wurde kein acore_string mit der id: %i gefunden.', null, null, null, null, null);
+
+INSERT INTO `command` VALUES ('string', '2', 'Syntax: .string #id [#locale]');
