4c9f1e36d9d868a82f66406e165fe432237f7685
Christopher Höllriegl <c.hoellriegl@hotmail.de>
2021-06-08 23:22:17 +0200

    Updated sql update script to include proper delete statements

--- data/sql/updates/pending_db_world/rev_1623112710636791400.sql
+++ data/sql/updates/pending_db_world/rev_1623112710636791400.sql
@@ -1,5 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623112710636791400');
 
+DELETE FROM `acore_string` WHERE `entry`=6617;
 INSERT INTO `acore_string` VALUES ('6617', 'No acore_string for id: %i found.', null, null, 'Es wurde kein acore_string mit der id: %i gefunden.', null, null, null, null, null);
 
+DELETE FROM `command` WHERE `name`='string';
 INSERT INTO `command` VALUES ('string', '2', 'Syntax: .string #id [#locale]');
