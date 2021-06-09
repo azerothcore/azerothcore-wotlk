361fd86416c5267dacc33701196afc7ce95140dd
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-09 08:22:46 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 0062a81d2ff3b354dff2132b6987423922b17304

--- data/sql/updates/pending_db_world/rev_1622599423158947800.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622599423158947800');
-
-SET @DINOSAUR_BONE = 11114;
-UPDATE `creature_loot_template` SET `QuestRequired` = 1 WHERE `ITEM` = @DINOSAUR_BONE;
