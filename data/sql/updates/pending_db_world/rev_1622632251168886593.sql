ec4a34e4bd79af61cf22a68fddfe62ef4daba902
anguaive <39011611+anguaive@users.noreply.github.com>
2021-06-10 16:35:37 +0200

    fix(DB/spell): Fix Hex of Ravenclaw Removal spell (#6196)
    Drinking Bethor's Potion (spell id: 7669) triggered the Hex of Ravenclaw
    Removal (spell id: 8320), which was supposed to remove the Hex of
    Ravenclaw debuff from the caster (spell id: 7656), but it didn't.
    
    - Closes #5906
    - Closes chromiecraft/chromiecraft#661

---
+++ data/sql/updates/pending_db_world/rev_1622632251168886593.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622632251168886593');
+
+UPDATE `spell_dbc` SET `Effect_1` = 164, `ImplicitTargetA_1`= 0, `EffectTriggerSpell_1` = 7656 WHERE `ID` = 8320;
