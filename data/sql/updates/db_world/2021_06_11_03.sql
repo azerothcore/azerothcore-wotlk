2559e6123fe6405796538deee362c73ba71580c8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-11 17:26:07 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 85103fd2afd81be320a9255e271840ac20fd59b6

--- data/sql/updates/db_world/2021_06_11_03.sql
+++ data/sql/updates/db_world/2021_06_11_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_11_02 -> 2021_06_11_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_11_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_11_02 2021_06_11_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1622719475133710519'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622719475133710519');
 
 -- unused GUIDs
@@ -30,3 +46,13 @@ INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VA
 (64839, @POOL1, 0, '4 - Patch of Elemental Water'),
 (64843, @POOL1, 0, '5 - Patch of Elemental Water'),
 (64859, @POOL1, 0, '6 - Patch of Elemental Water');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_11_03' WHERE sql_rev = '1622719475133710519';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
