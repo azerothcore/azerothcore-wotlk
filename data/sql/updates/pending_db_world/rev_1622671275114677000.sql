77d50876efb2174764862caf62ecdf095784f1e4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-11 15:28:50 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 8442c5c2aec2d455363d7dd687c4ef8912aadf2f

--- data/sql/updates/pending_db_world/rev_1622671275114677000.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622671275114677000');
-
-SET @ANNOUNCE = 14;
-SET @FISHING_POOLS = 15;
-SET @TURN_INS = 62;
-UPDATE `game_event` SET `start_time` = '2016-10-29 00:00:00' WHERE `eventEntry` = @ANNOUNCE;
-UPDATE `game_event` SET `start_time` = '2016-10-30 14:00:00' WHERE `eventEntry` IN (@FISHING_POOLS, @TURN_INS);
