3f00da73c08cc2a52b953d8e84faa1e3797c39b7
Raelorasz <ryil2016@gmail.com>
2021-06-11 19:36:27 +0200

    Update SQL query

--- data/sql/updates/pending_db_world/rev_1623407321415010500.sql
+++ data/sql/updates/pending_db_world/rev_1623407321415010500.sql
@@ -17,7 +17,7 @@ UPDATE `creature_text` SET `Emote` = 0 WHERE `CreatureID` = 10637 AND `GroupID` 
 -- Add script on gossip end
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10637;
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (10637, 1063700));
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (10637, 1063700)) AND `source_type` IN (0, 9);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (10637, 0, 0, 0, 62, 0, 100, 0, 2993, 0, 0, 0, 0, 80, 1063700, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Malyfous Darkhammer - On Gossip Option 0 Selected - Run Script'),
 (1063700, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Malyfous Darkhammer - Script - Close Gossip'),
