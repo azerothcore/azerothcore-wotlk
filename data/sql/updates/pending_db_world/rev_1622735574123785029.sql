d0a52896283a9bfed36ca897f0c2ebe870a25800
anguaive <39011611+anguaive@users.noreply.github.com>
2021-06-12 10:57:44 +0200

    fix(DB/smart_scripts): The Plains Vision speed (#6213)
    During the Rite of Vision quest (id: 772), The Plains Vision creature
    (entry: 2983) guides the player to a location. The creature moves too
    fast and outruns the player, making it impossible to follow all the way.
    
    - Closes #5982
    - Closes https://github.com/chromiecraft/chromiecraft/issues/680

---
+++ data/sql/updates/pending_db_world/rev_1622735574123785029.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622735574123785029');
+
+UPDATE `smart_scripts` SET `action_param1` = 0 WHERE `entryorguid` = 2983 AND `source_type` = 0 AND `id` = 0 AND `link` = 0;
