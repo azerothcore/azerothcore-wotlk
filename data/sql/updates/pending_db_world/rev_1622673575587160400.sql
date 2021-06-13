806dc4052f25351b47b695a1ee2ef7757a063048
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-13 08:11:30 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): ebd70d544b1d33dc7ce4c274da5a1b5e9feb6ab4

--- data/sql/updates/pending_db_world/rev_1622673575587160400.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622673575587160400');
-
-SET @PART1 = 14352;
-SET @PART2 = 14353;
-UPDATE `quest_template_addon` SET `NextQuestID` = @PART2 WHERE `ID` = @PART1;
-UPDATE `quest_template_addon` SET `PrevQuestID` = @PART1 WHERE `ID` = @PART2;
