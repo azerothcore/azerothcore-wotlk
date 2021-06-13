95b6494d55b0479841e25c3b62d29c026cce743b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-13 09:27:18 +0000

    chore(DB): import pending SQL update file
    Referenced commit(s): 4734475b9347a25f61f4392623034894d5ff986c

--- data/sql/updates/pending_db_world/rev_1622718211042013969.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622718211042013969');
-
-UPDATE `graveyard_zone` SET `GhostZone` = 28, `Comment` = 'Western Plaguelands, Hearthglen - Western Plaguelands' WHERE `ID` = 1451;
