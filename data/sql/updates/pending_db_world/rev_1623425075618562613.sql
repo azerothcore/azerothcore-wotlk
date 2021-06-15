b9d501d26ba2eab47a45fc798775c9ec3b5f0e2b
anguaive <angurg98@gmail.com>
2021-06-15 21:36:32 +0200

    Correction

--- data/sql/updates/pending_db_world/rev_1623425075618562613.sql
+++ data/sql/updates/pending_db_world/rev_1623425075618562613.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623425075618562613');
 
-UPDATE `creature_template` SET `faction` = 29, `dynamicflags` = 0 WHERE `entry` = 10668;
+UPDATE `creature_template` SET `dynamicflags` = 0 WHERE `entry` = 10668;
