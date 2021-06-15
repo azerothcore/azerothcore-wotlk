d0e5f9b372728c28595d235d9ac946b99d7aafbe
Bogir <m-000@bk.ru>
2021-06-15 14:22:19 +0500

    Possibility to set the date format familiar to the region

--- src/common/Logging/LogMessage.cpp
+++ src/common/Logging/LogMessage.cpp
@@ -6,6 +6,7 @@
 #include "LogMessage.h"
 #include "StringFormat.h"
 #include "Util.h"
+#include "Config.h"
 
 LogMessage::LogMessage(LogLevel _level, std::string const& _type, std::string&& _text)
     : level(_level), type(_type), text(std::forward<std::string>(_text)), mtime(time(nullptr))
@@ -21,7 +22,11 @@ std::string LogMessage::getTimeStr(time_t time)
 {
     tm aTm;
     localtime_r(&time, &aTm);
-    return Acore::StringFormat("%04d-%02d-%02d_%02d:%02d:%02d", aTm.tm_year + 1900, aTm.tm_mon + 1, aTm.tm_mday, aTm.tm_hour, aTm.tm_min, aTm.tm_sec);
+    if (sConfigMgr->GetOption<int32>("Logger.Timestamp.Format", 1) == 1)
+    {
+        return Acore::StringFormat("%04d-%02d-%02d_%02d:%02d:%02d", aTm.tm_year + 1900, aTm.tm_mon + 1, aTm.tm_mday, aTm.tm_hour, aTm.tm_min, aTm.tm_sec);
+    }
+    return Acore::StringFormat("%02d-%02d-%04d %02d:%02d:%02d", aTm.tm_mday, aTm.tm_mon + 1, aTm.tm_year + 1900, aTm.tm_hour, aTm.tm_min, aTm.tm_sec);
 }
 
 std::string LogMessage::getTimeStr() const
