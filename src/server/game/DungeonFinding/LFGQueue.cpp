e3a572722245de55880f07b77565e7ceee2c532c
Bogir[rus] <m-1@bk.ru>
2021-06-16 14:28:00 +0500

    chore(Core): typo fixes (#6272)
    Co-authored-by: Bogir <m-000@bk.ru>

--- src/server/game/DungeonFinding/LFGQueue.cpp
+++ src/server/game/DungeonFinding/LFGQueue.cpp
@@ -274,7 +274,6 @@ namespace lfg
             // Store group so we don't need to call Mgr to get it later (if it's player group will be 0 otherwise would have joined as group)
             for (LfgRolesMap::const_iterator it2 = itQueue->second.roles.begin(); it2 != itQueue->second.roles.end(); ++it2)
                 proposalGroups[it2->first] = itQueue->first.IsGroup() ? itQueue->first : ObjectGuid::Empty;
-;
 
             numPlayers += itQueue->second.roles.size();
 
