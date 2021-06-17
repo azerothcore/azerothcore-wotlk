c789ec0ebc711543a098c83db43829adf4aca827
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-17 11:27:16 +0000

    chore(DB): import pending files
    Referenced commit(s): 19f94e0211804a9806eecbb02c25556ee110f7da

--- data/sql/updates/pending_db_world/rev_1623254092348182246.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623254092348182246');
-
--- Delete despawn event from SmartAI of Lorgus Jett
-DELETE FROM `smart_scripts` WHERE `entryorguid`=12902 AND `id`=0;
