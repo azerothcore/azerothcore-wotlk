016e7427d506f4f207e300a6f19c2f852e7a8c82
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-18 07:37:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 6dab2a3bafde090466e53be8e46a42cd3ffb4db4

--- data/sql/updates/db_world/2021_06_18_00.sql
+++ data/sql/updates/db_world/2021_06_18_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_17_06 -> 2021_06_18_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_17_06';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_17_06 2021_06_18_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1616275490460233200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616275490460233200');
 
 -- raid bosses
@@ -283,3 +299,13 @@ UPDATE `creature_template` SET `DamageModifier`='2.4' WHERE  `entry`=11518;
 UPDATE `creature_template` SET `DamageModifier`='2.2' WHERE  `entry`=11519;
 -- trash, was 1.7
 UPDATE `creature_template` SET `DamageModifier`='1.5' WHERE  `entry`=11320;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_18_00' WHERE sql_rev = '1616275490460233200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
