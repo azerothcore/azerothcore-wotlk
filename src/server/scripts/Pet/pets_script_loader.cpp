c9c02f8dd7f28fce1744f00ac44279116c4e43df
Winfidonarleyan <dowlandtop@yandex.com>
2021-06-18 23:13:29 +0700

    039f0e0faa081352d19f34b6f0abaa4e74414988

--- src/server/scripts/Pet/pets_script_loader.cpp
+++ src/server/scripts/Pet/pets_script_loader.cpp
@@ -11,7 +11,6 @@ void AddSC_hunter_pet_scripts();
 void AddSC_mage_pet_scripts();
 void AddSC_priest_pet_scripts();
 void AddSC_shaman_pet_scripts();
-void AddSC_warlock_pet_scripts();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -23,5 +22,4 @@ void AddPetScripts()
     AddSC_mage_pet_scripts();
     AddSC_priest_pet_scripts();
     AddSC_shaman_pet_scripts();
-    AddSC_warlock_pet_scripts();
 }
