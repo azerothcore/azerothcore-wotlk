acf624214e6cac269a9fcb99dcad578f3fa182ef
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-18 10:19:49 +0000

    chore(DB): import pending files
    Referenced commit(s): efd575fefad7e7379c75a05a249572187be98220

--- data/sql/updates/db_world/2021_06_18_04.sql
+++ data/sql/updates/db_world/2021_06_18_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_18_03 -> 2021_06_18_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_18_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_18_03 2021_06_18_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1617975941808751069'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1617975941808751069');
 
 SET @ENTRY := 10901;
@@ -15,3 +31,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@ENTRY, 0, 1, 0, 0, 0, 100, 0, 3000, 12000, 17000, 28000, 0, 11, 8245, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Lorekeeper Polkelt - In Combat - Cast \'Corrosive Acid\''),
 (@ENTRY, 0, 2, 0, 0, 0, 100, 0, 11000, 22000, 30000, 30000, 0, 11, 18151, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Lorekeeper Polkelt - In Combat - Cast \'Noxious Catalyst\''),
 (@ENTRY, 0, 3, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 34, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Lorekeeper Polkelt - On Just Died - Set Instance Data 1 to 1');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_18_04' WHERE sql_rev = '1617975941808751069';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
