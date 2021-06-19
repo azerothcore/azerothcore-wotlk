6e6d37ae569b6374a688573131bc0006cbde6309
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-19 16:50:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 8f47b3d5b0fc68f3a7c56f65e60a62030977ddbe

--- data/sql/updates/db_world/2021_06_19_03.sql
+++ data/sql/updates/db_world/2021_06_19_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_19_02 -> 2021_06_19_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_19_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_19_02 2021_06_19_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1623600456041056800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623600456041056800');
 
 DELETE FROM `creature_template_locale` WHERE `entry` IN (37942,37941,35495,35494,33964,33963,31582,31581,31580,31579);
@@ -82,3 +98,13 @@ INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `Ver
 (31579, 'ruRU', 'Чародей Адурин', 'Эмблема почетного квартирмейстера', 18019),
 (31579, 'zhCN', '奥术师埃杜林', '荣誉军需官徽章', 18019),
 (31579, 'zhTW', '秘法師阿度靈', '榮譽軍需官徽章', 18019);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_19_03' WHERE sql_rev = '1623600456041056800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
