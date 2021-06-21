b0e23df3fe101628f19f312822dc8617954fd811
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-21 19:24:57 +0000

    chore(DB): import pending files
    Referenced commit(s): 1be561e03b56dc396270335886e59eddad9fa0c6

--- data/sql/updates/db_world/2021_06_21_00.sql
+++ data/sql/updates/db_world/2021_06_21_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_20_04 -> 2021_06_21_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_20_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_20_04 2021_06_21_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1621752432246342200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621752432246342200');
 
 DROP TABLE IF EXISTS `skillraceclassinfo_dbc`;
@@ -104,3 +120,13 @@ INSERT INTO `playercreateinfo_skills` (`raceMask`, `classMask`, `skill`, `rank`,
 (1024, 4, 226, 0, 'Crossbows'),
 (1061, 3, 160, 0, 'Two-Handed Maces'),
 (1101, 0, 98, 0, 'Language: Common');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_21_00' WHERE sql_rev = '1621752432246342200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
