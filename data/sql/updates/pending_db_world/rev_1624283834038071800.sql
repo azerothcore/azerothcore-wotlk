7ae981a70fc917586d1a7c2aa7fe4eb650b6d89a
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-06-21 15:31:12 +0100

    better mysql bit update

--- data/sql/updates/pending_db_world/rev_1624283834038071800.sql
+++ data/sql/updates/pending_db_world/rev_1624283834038071800.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624283834038071800');
 
-UPDATE `creature_template` SET `unit_flags` = 131076, `flags_extra` = 8388866 WHERE (`entry` = 17680);
+UPDATE `creature_template` SET `unit_flags` = `unit_flags`|4|131072, `flags_extra` = `flags_extra`|2|256|8388608 WHERE (`entry` = 17680);
