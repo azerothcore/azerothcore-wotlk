77ad79c9288e65c8d32bbc8cd2225e80523a8dba
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-22 23:23:32 +0000

    chore(DB): import pending files
    Referenced commit(s): b1c5a6c047aaf6e499adc5e344d03b919a7b533f

--- data/sql/updates/db_world/2021_06_22_01.sql
+++ data/sql/updates/db_world/2021_06_22_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_22_00 -> 2021_06_22_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_22_00 2021_06_22_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1623407321415010500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623407321415010500');
 
 -- Add correct gossip text
@@ -30,3 +46,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGroup` = 2984) AND (`SourceEntry` = 0) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 8) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 5047) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (15, 2984, 0, 0, 0, 8, 0, 5047, 0, 0, 0, 0, 0, '', 'Only show gossip option if quest 5047 is completed');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_22_01' WHERE sql_rev = '1623407321415010500';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
