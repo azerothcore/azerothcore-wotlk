b2472caeedf394b7c193be4df33dd88babdcbee4
Emir <emirpod02@gmail.com>
2021-06-23 00:45:09 +0200

    fix(DB/Quest): The Deathstalkers (#5854)

---
+++ data/sql/updates/pending_db_world/rev_1621064404059904900.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621064404059904900');
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter.' WHERE (`ID` = 1899);
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter.' WHERE (`ID` = 1886);
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Andron Gant at Undercity in the Apothecarium.' WHERE (`ID` = 14419);
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter.' WHERE (`ID` = 14421);
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mennet Carkad at Undercity in the Rogues\' Quarter.' WHERE (`ID` = 14420);
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Aleric Hawkins at Undercity in the Hall of the Dark Lady.' WHERE (`ID` = 14418);
