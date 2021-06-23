4eaa4229e6cb38a1693958e5ffb82e2cfb2b7fce
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-23 10:49:00 +0000

    chore(DB): import pending files
    Referenced commit(s): 32b7ba55003ea087c6493501e85d6d5be3697880

--- data/sql/updates/pending_db_characters/rev_1621780938723425400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1621780938723425400');
-
-ALTER TABLE `mail` 
-ADD COLUMN `auctionId` INT DEFAULT 0 NOT NULL AFTER `checked`; 
--- data/sql/updates/pending_db_world/rev_1623851652869107640.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623851652869107640');
-
--- Reduce Plans: Copper Chain Vest drop rate for Siltfin Murloc and Mistbat
-UPDATE `creature_loot_template` SET `Chance` = 0.3 WHERE `entry` IN (17190, 16353) AND `item` = 3609;
-
-
