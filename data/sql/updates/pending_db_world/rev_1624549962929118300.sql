c0101bf757646c61e9fb36dca2c19380c082001e
Efymer <loscojones999@gmail.com>
2021-06-24 17:59:22 +0200

    fix(DB/quest_template_addon): Fix prerequisites for Convert Ops quests

---
+++ data/sql/updates/pending_db_world/rev_1624549962929118300.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624549962929118300');
+
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = -1074 WHERE (`ID` = 1074); -- Ineptitude + Chemicals = Fun [1074]
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = -1074 WHERE (`ID` = 1077); -- Special Delivery for Gaxim [1077]
