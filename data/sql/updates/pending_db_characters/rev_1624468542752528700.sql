1b63dbc0404fcd1b7fac747f97696d6c38dc2586
Axel Cocat <ax.cocat@gmail.com>
2021-06-24 22:55:40 +0200

    fix sql file

--- data/sql/updates/pending_db_characters/rev_1624468542752528700.sql
+++ data/sql/updates/pending_db_characters/rev_1624468542752528700.sql
@@ -1,4 +1,19 @@
 INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1624468542752528700');
 
-ALTER TABLE `characters` DROP IF EXISTS `order`;
-ALTER TABLE `characters` ADD `order` TINYINT(1) NULL DEFAULT NULL AFTER `grantableLevels`;
+SET @dbname = DATABASE();
+SET @tablename = "characters";
+SET @columnname = "order";
+SET @preparedStatement = (SELECT IF(
+    (
+        SELECT COUNT(*) FROM information_schema.COLUMNS
+        WHERE
+            (TABLE_NAME = @tablename)
+            AND (TABLE_SCHEMA = @dbname)
+            AND (COLUMN_NAME = @columnname)
+    ) > 0,
+    "SELECT 1 WHERE false",
+    CONCAT("ALTER TABLE `", @tablename, "` ADD `", @columnname, "` TINYINT NULL DEFAULT NULL AFTER `grantableLevels`;")
+));
+PREPARE alterIfNotExists FROM @preparedStatement;
+EXECUTE alterIfNotExists;
+DEALLOCATE PREPARE alterIfNotExists;
