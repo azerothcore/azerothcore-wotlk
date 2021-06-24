849b235ca1fda393ef97f2f8982ceb3bfb7a0d5a
Efymer <79163132+Efymer@users.noreply.github.com>
2021-06-24 18:22:59 +0200

    Update data/sql/updates/pending_db_world/rev_1624549962929118300.sql
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- data/sql/updates/pending_db_world/rev_1624549962929118300.sql
+++ data/sql/updates/pending_db_world/rev_1624549962929118300.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624549962929118300');
 
-UPDATE `quest_template_addon` SET `ExclusiveGroup` = -1074 WHERE (`ID` = 1074); -- Ineptitude + Chemicals = Fun [1074]
-UPDATE `quest_template_addon` SET `ExclusiveGroup` = -1074 WHERE (`ID` = 1077); -- Special Delivery for Gaxim [1077]
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = -1074 WHERE `ID` IN (1074, 1077); -- Ineptitude + Chemicals = Fun [1074], Special Delivery for Gaxim [1077]
