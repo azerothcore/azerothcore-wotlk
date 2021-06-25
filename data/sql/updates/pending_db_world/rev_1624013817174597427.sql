2701ab447191310211639d79cf89fedc8914a6e4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-25 09:53:03 +0000

    chore(DB): import pending files
    Referenced commit(s): a2a1657ce43110777782fdc750bf4e8f4a1a7f2c

--- data/sql/updates/pending_db_world/rev_1624013817174597427.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624013817174597427');
-
--- Slow Baron Charr down to reasonable speed
-UPDATE `creature_template` SET `speed_walk` = 1 WHERE `entry` = 14461;
