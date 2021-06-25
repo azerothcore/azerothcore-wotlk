152b9f4d8984ddee987892fa6ea76977b5bb1f7f
Efymer <loscojones999@gmail.com>
2021-06-25 15:59:31 +0200

    fix(DB/gameobject_loot_template): Indurium Mineral Vein should contain always ores

---
+++ data/sql/updates/pending_db_world/rev_1624629015111761400.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624629015111761400');
+
+UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE (`Entry` = 3266) AND (`Item` IN (5833)); -- Indurium Mineral Vein - Indurium Ore
