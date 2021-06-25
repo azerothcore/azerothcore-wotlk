edf3df5cbfe33d16341923048bcd93841340abaf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-25 03:21:24 +0000

    chore(DB): import pending files
    Referenced commit(s): 0b8d679fd1073cd924430952d70fcc58af463a49

--- data/sql/updates/db_world/2021_06_25_00.sql
+++ data/sql/updates/db_world/2021_06_25_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_24_02 -> 2021_06_25_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_24_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_24_02 2021_06_25_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1623926977654930100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1623926977654930100');
 
 SET @WRETCHED_CAPTIVE = 16916;
@@ -27,3 +43,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@SERGEANT_KANREN, 0, 1, 0, 52, 0, 100, 0, 0, @SERGEANT_KANREN, 0, 0, 0, 45, 1, 1, 0, 0, 0, 0, 19, 16916, 15, 0, 0, 0, 0, 0, 0, 'Sergeant Kan\'ren - On Text 0 Over - Set Data 1 1'),
 (@SERGEANT_KANREN, 0, 2, 0, 38, 0, 100, 0, 1, 1, 0, 0, 0, 1, 1, 6000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Sergeant Kan\'ren - On Data Set 1 1 - Say Line 1'),
 (@SERGEANT_KANREN, 0, 3, 0, 52, 0, 100, 0, 1, @SERGEANT_KANREN, 0, 0, 0, 45, 2, 2, 0, 0, 0, 0, 19, 16916, 15, 0, 0, 0, 0, 0, 0, 'Sergeant Kan\'ren - On Text 1 Over - Set Data 2 2');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_25_00' WHERE sql_rev = '1623926977654930100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
