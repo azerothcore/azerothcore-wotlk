76ccd9e7a9a3217c73f79b8ab937a31867427c81
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-25 16:12:53 +0000

    chore(DB): import pending files
    Referenced commit(s): 6a37b62a1c60e57b83f92f4c414d19a312a19146

--- data/sql/updates/db_world/2021_06_25_04.sql
+++ data/sql/updates/db_world/2021_06_25_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_25_03 -> 2021_06_25_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_25_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_25_03 2021_06_25_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1624022594966979301'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624022594966979301');
 
 SET @POOL1 = 60005,
@@ -41,3 +57,13 @@ INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VA
 (@FOODCRATE3, @POOL3, 0, 'Food Crate (30839) - Lions Pride Inn'),
 (@FOODCRATE4, @POOL4, 0, 'Food Crate (30687) - Elwynn Forest'),
 (@FOODCRATE5, @POOL5, 0, 'Food Crate (29306) - Steelgrills Depot');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_06_25_04' WHERE sql_rev = '1624022594966979301';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
