631257234ce3261750a8edb2ce0b8943bfcacff7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-26 01:30:01 +0000

    chore(DB): import pending files
    Referenced commit(s): 0b7e74b0956ee0bf1d6c7d1d46150458a4a765c4

--- data/sql/updates/pending_db_world/rev_1624093373812039200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624093373812039200');
-
--- Set spell school immunity to FIRE
-UPDATE `creature_template` SET `spell_school_immune_mask` = 4 WHERE (`entry` = 4339);
