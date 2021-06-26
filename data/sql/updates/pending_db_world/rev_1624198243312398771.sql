21d7182ba83fe8f11173cdad93035ae4de84a42d
Necropola <necropola@rain-fighters.net>
2021-06-26 18:14:03 +0200

    fix(DB/quest_poi): Waters of Xavian (1944) and Laughing Sisters (1945) (#6459)
    - Closes #6456
    - Closes https://github.com/chromiecraft/chromiecraft/issues/924

---
+++ data/sql/updates/pending_db_world/rev_1624198243312398771.sql
@@ -0,0 +1,16 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624198243312398771');
+
+-- Add POI for the quest objective of Waters of Xavian (1944)
+DELETE FROM `quest_poi` WHERE `QuestId`=1944 AND `id`=1;
+INSERT INTO `quest_poi` (`QuestId`, `id`, `ObjectiveIndex`, `MapId`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES (1944, 1, 4, 1, 43, 0, 0, 1, 0);
+DELETE FROM `quest_poi_points` WHERE `QuestId`=1944 AND `Idx1`=1;
+INSERT INTO `quest_poi_points` (`QuestId`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (1944, 1, 0, 3079, -2701, 0);
+
+-- Add POIs for the quest objective of Laughing Sisters (1945)
+DELETE FROM `quest_poi` WHERE `QuestId`=1945 AND `id`=1;
+INSERT INTO `quest_poi` (`QuestId`, `id`, `ObjectiveIndex`, `MapId`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES (1945, 1, 4, 1, 43, 0, 0, 1, 0);
+DELETE FROM `quest_poi_points` WHERE `QuestId`=1945 AND `Idx1`=1;
+INSERT INTO `quest_poi_points` (`QuestId`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES
+(1945, 1, 0, 2797, -1812, 0),
+(1945, 1, 1, 2524, -1801, 0),
+(1945, 1, 2, 2519, -1618, 0);
