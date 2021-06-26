1221134e54f5c0aa04f85b46e871c9a2e4df4d0d
Necropola <necropola@rain-fighters.net>
2021-06-26 09:48:34 +0200

    Squeeze whitespace

--- data/sql/updates/pending_db_world/rev_1624669484085082860.sql
+++ data/sql/updates/pending_db_world/rev_1624669484085082860.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624669484085082860');
 
 -- Despawn the summoned creatures/corpses 5 minutes after being killed instead of 1 minute after not being in combat.
-UPDATE  `creature_summon_groups` SET `summonType`=6, `summonTime`=300000 WHERE `summonerId` IN (21118, 21119, 21120, 21121);
+UPDATE `creature_summon_groups` SET `summonType`=6, `summonTime`=300000 WHERE `summonerId` IN (21118, 21119, 21120, 21121);
