a5904f63de4622acb9cc06394e58e07da217bf70
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-06-26 16:40:54 +0930

    Update rev_1624689676324157623.sql

--- data/sql/updates/pending_db_world/rev_1624689676324157623.sql
+++ data/sql/updates/pending_db_world/rev_1624689676324157623.sql
@@ -6,4 +6,4 @@ UPDATE `creature_template` SET `flags_extra` = `flags_extra`&~(2) WHERE `entry` 
 -- Set MovementType to 1 for spawn 86538 to match all other Nazzivus Summoners
 DELETE FROM `creature` WHERE `id` = 17524 AND `guid` = 86538;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(86538, 17524, 530, 0, 0, 1, 1, 0, 1, -2481.5, -11259.1, 31.07, 5.3, 534, 0, 0, 273, 0, 1, 0, 0, 0, '', 0);
+(86538, 17524, 530, 0, 0, 1, 1, 0, 1, -2481.5, -11259.1, 31.07, 5.3, 534, 5, 0, 273, 0, 1, 0, 0, 0, '', 0);
