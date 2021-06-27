634fceabe1698071a42b1c0e5da349e5b03b254b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-06-27 00:32:32 +0000

    chore(DB): import pending files
    Referenced commit(s): 22c4da88ad234cf64cf10f424fa75ee7a01d1bbd

--- data/sql/updates/pending_db_world/rev_1624165153573726801.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624165153573726801');
-
--- Removes Green Linen Shirt from Skeletal Warrior
-DELETE FROM `creature_loot_template` WHERE `Entry` = 48 AND `Item` = 2579;
