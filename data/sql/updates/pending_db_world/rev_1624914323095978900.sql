dfa672871eaf3d1a87bdc72724031ec4627faccf
Efymer <loscojones999@gmail.com>
2021-06-30 14:16:54 +0200

    Update rev_1624914323095978900.sql

--- data/sql/updates/pending_db_world/rev_1624914323095978900.sql
+++ data/sql/updates/pending_db_world/rev_1624914323095978900.sql
@@ -1,8 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624914323095978900');
 
+DELETE FROM `acore_string` WHERE `entry` IN (30096, 30097);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
   (30096, 'LFG is set to 1 player queue for debugging.'),
   (30097, 'LFG is set to normal queue.');
 
+DELETE FROM `command` WHERE `name` = 'debug lfg';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
   ('debug lfg', 3, 'Syntax: .debug lfg\r\nToggle debug mode for lfg. In debug mode GM can start lfg queue with one player.');
