a8b858ec92831b6bde4fb1c48638ed62fd0ac5d9
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-01 14:58:34 +0000

    chore(DB): import pending files
    Referenced commit(s): c4185a511da1de569175f5300d4090a743fd8043

--- data/sql/updates/db_world/2021_07_01_01.sql
+++ data/sql/updates/db_world/2021_07_01_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_01_00 -> 2021_07_01_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_01_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_01_00 2021_07_01_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1625066362619921400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625066362619921400');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 5763;
@@ -73,3 +89,13 @@ DELETE FROM `smart_scripts` WHERE (`entryorguid` = 5762) AND (`source_type` = 0)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (5762, 0, 0, 0, 1, 0, 100, 257, 3000, 3000, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Deviate Moccasin - Out of Combat - Set In Combat With Zone (No Repeat)'),
 (5762, 0, 1, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 63, 1, 1, 0, 0, 0, 0, 11, 3678, 500, 1, 0, 0, 0, 0, 0, 'Deviate Moccasin - On Just Died - Missing comment for action_type 63');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_01_01' WHERE sql_rev = '1625066362619921400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
