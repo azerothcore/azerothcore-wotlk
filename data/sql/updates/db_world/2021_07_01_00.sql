734ac6cdd394d992bb7b392d6d241e16fa025dff
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-01 11:39:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 7a7d3d6999fcefa0492fdf43d5acee865c606788

--- data/sql/updates/db_world/2021_07_01_00.sql
+++ data/sql/updates/db_world/2021_07_01_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_06_30_04 -> 2021_07_01_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_06_30_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_06_30_04 2021_07_01_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1624513366178294000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624513366178294000');
 
 /*Just in case*/
@@ -2478,3 +2494,13 @@ INSERT  INTO `item_set_names_locale` (`ID`, `locale`, `Name`, `VerifiedBuild`) V
 (51775,'esMX','Musleras de señor Ymirjar',15050),
 (51776,'esMX','Espaldares de señor Ymirjar',15050);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_01_00' WHERE sql_rev = '1624513366178294000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
