aac4b5f0bba9f1b29098dfcbdc746cf1627bbf16
Francesco Borzì <borzifrancesco@gmail.com>
2021-07-02 13:20:31 +0200

    Update rev_1624629015111761400.sql

--- data/sql/updates/pending_db_world/rev_1624629015111761400.sql
+++ data/sql/updates/pending_db_world/rev_1624629015111761400.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624629015111761400');
 
-UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE (`Entry` = 3266) AND (`Item` IN (5833)); -- Indurium Mineral Vein - Indurium Ore
+UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE `Entry` = 3266 AND `Item` = 5833; -- Indurium Mineral Vein - Indurium Ore
