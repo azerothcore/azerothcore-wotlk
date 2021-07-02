175017acd68ce480d8c49e87ea0835b4d58102fa
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-02 12:26:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 30013c8ee9680faa480bbbbf8df5fd49dd86c013

--- data/sql/updates/pending_db_world/rev_1624629015111761400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624629015111761400');
-
--- Indurium Mineral Vein - Indurium Ore
-UPDATE `gameobject_loot_template` SET `QuestRequired` = 0 WHERE `Entry` = 3266 AND `Item` = 5833;
