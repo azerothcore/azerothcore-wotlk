32d1f044ea1cd6dd1fe393f46ac6b22f73687a04
Raelorasz <ryil2016@gmail.com>
2021-07-02 21:39:25 +0200

    Update SQL query

--- data/sql/updates/pending_db_world/rev_1625248824338191500.sql
+++ data/sql/updates/pending_db_world/rev_1625248824338191500.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625248824338191500');
 
 -- Update Kroshius' SAI and make it not selectable
-UPDATE `creature_template` SET `unit_flags` = 33555200, `AIName` = 'SmartAI' WHERE (`entry` = 14467);
+UPDATE `creature_template` SET `unit_flags` = `unit_flags`|33554432, `AIName` = 'SmartAI' WHERE (`entry` = 14467);
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (14467,1446700)) AND (`source_type` IN (0,9)) AND (`id` IN (0, 1, 2, 3, 4, 5, 6));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
