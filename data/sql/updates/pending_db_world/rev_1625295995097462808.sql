90bb4aa81f99a2fcbe45c7f8ed8d375a015bb0d3
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-07-03 17:48:02 +0930

    Update rev_1625295995097462808.sql

--- data/sql/updates/pending_db_world/rev_1625295995097462808.sql
+++ data/sql/updates/pending_db_world/rev_1625295995097462808.sql
@@ -3,9 +3,6 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625295995097462808');
 -- Warmaster Laggond
 DELETE FROM `creature_loot_template` WHERE `Entry` = 13840 AND `Item` IN (17422, 17423, 17503, 17504);
 
--- Sergeant Durgen Stormpike
-DELETE FROM `creature_loot_template` WHERE `Entry` = 13777 AND `Item` = 17306;
-
 -- Corporal Teeka Bloodsnarl
 DELETE FROM `creature_loot_template` WHERE `Entry` = 13776 AND `Item` IN (17422, 17423, 17502, 17503);
 
@@ -14,9 +11,6 @@ DELETE FROM `creature_loot_template` WHERE `Entry` = 14285 AND `Item` IN (17422,
 
 -- Lieutenant Haggerdin 
 DELETE FROM `creature_loot_template` WHERE `Entry` = 13841 AND `Item` IN (17306, 17327, 17328, 17422);
-
--- Prospector Stonehewer
-DELETE FROM `creature_loot_template` WHERE `Entry` = 13816 AND `Item` IN (17306, 17326, 17327, 17422, 18206, 18225, 18231);
 
 -- Stormpike Mountaineer
 DELETE FROM `creature_loot_template` WHERE `Entry` = 12047 AND `Item` IN (17306, 17326, 17327, 17328, 17422, 17423, 17502, 17503, 17504, 18143, 18144, 18145, 18146, 18147, 18206, 18207, 18225, 18231, 18233);
