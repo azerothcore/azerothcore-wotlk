a0fafa8f52e832bd45ef9c4db7c52cef0acdaceb
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-04 21:14:33 +0000

    chore(DB): import pending files
    Referenced commit(s): 681e7139a1629420eee04728a8ca53cc987007cc

--- data/sql/updates/db_world/2021_07_04_02.sql
+++ data/sql/updates/db_world/2021_07_04_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_04_01 -> 2021_07_04_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_04_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_04_01 2021_07_04_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1624282955303279000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624282955303279000');
 
 SET @STATUE_QUEEN_AZSHARA = 181964;
@@ -32,3 +48,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `creature_text` WHERE `CreatureID` = @ENTRY_ATOPH_THE_BLOODCURSED;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (@ENTRY_ATOPH_THE_BLOODCURSED, 0, 0, 'Who dares defile the statue of our beloved?', 14, 0, 100, 0, 0, 0, 14366, 0, 'Atoph the Bloodcursed');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_04_02' WHERE sql_rev = '1624282955303279000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
