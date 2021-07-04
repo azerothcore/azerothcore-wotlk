9fe79f368ed411ce1b2ec685c828467041ad2c52
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-07-04 15:04:25 +0930

    Update rev_1625375927776772391.sql

--- data/sql/updates/pending_db_world/rev_1625375927776772391.sql
+++ data/sql/updates/pending_db_world/rev_1625375927776772391.sql
@@ -11,6 +11,6 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 -- Blackwater Cutlass added to Defias Squallshaper loot table with 6% drop rate
 DELETE FROM `creature_loot_template` WHERE `Entry` = 1732 AND `Item` = 1951;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1732, 1951, 0, 6, 0, 1, 1, 1, 1, 'Blackwater Cutlass')
+(1732, 1951, 0, 6, 0, 1, 1, 1, 1, 'Blackwater Cutlass');
 
 
