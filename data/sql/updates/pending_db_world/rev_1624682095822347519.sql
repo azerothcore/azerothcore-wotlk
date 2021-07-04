5cf5ff4d6b46f5b85edfbdb45e8e27cddbe4678d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-04 10:18:04 +0000

    chore(DB): import pending files
    Referenced commit(s): 8f25f64c5b71b33f6bffa28d4c18b698152c11e7

--- data/sql/updates/pending_db_world/rev_1624682095822347519.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624682095822347519');
-
--- Slows Gnarl Leafbrother to normal speed
-UPDATE `creature_template` SET `speed_walk` = 1 WHERE `entry` = 5354;
