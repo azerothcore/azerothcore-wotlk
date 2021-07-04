30124d0acd73098711f276e986030c1472f495c7
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2021-07-04 14:31:18 +0200

    fix_tannaria_npc_dede_title

---
+++ data/sql/updates/pending_db_world/rev_1625401809536592600.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625401809536592600');
+
+UPDATE `creature_template_locale` SET `Title`='Schurkenlehrer' WHERE  `entry`=16279 AND `locale`='deDE';
