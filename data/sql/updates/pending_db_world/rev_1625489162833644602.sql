61862347fb328bd8e9d51a030494200de49feb49
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-07-05 23:50:52 +0930

    Update rev_1625489162833644602.sql
    Just saw the info in the wiki about using a number > 50000 for gossip IDs that are not sniffed. I was using 11860, which is also the creature_templaye ID for this creature as it was free, so changing this to the first free number > 50K. Re-tested with new ID, still working.

--- data/sql/updates/pending_db_world/rev_1625489162833644602.sql
+++ data/sql/updates/pending_db_world/rev_1625489162833644602.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625489162833644602');
 
 -- Add gossip to Maggran Earthbinder
-DELETE FROM `gossip_menu` WHERE `MenuID` = 11860;
+DELETE FROM `gossip_menu` WHERE `MenuID` = 50008;
 INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
-(11860, 5443);
-UPDATE `creature_template` SET `gossip_menu_id` = 11860 WHERE `entry` = 11860;
+(50008, 5443);
+UPDATE `creature_template` SET `gossip_menu_id` = 50008 WHERE `entry` = 11860;
