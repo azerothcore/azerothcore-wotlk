961c0e8c646309f21415de28018faf1e2268606a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-05 15:14:21 +0000

    chore(DB): import pending files
    Referenced commit(s): 45f35e8acc88793f89f675c2c57bd4b994b96b75

--- data/sql/updates/pending_db_world/rev_1624770439358526029.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624770439358526029');
-
--- Remove Crisp Spider Meat from Deviate Lurker and Deviate Creeper
-DELETE FROM `creature_loot_template` WHERE `Entry` IN (3632, 3641) AND `Item` = 1081;
-
-
