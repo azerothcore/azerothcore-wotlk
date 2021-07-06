9b6f9339a2472f84514b32ecf8015f8b0a5a0ac7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-06 14:07:43 +0000

    chore(DB): import pending files
    Referenced commit(s): 5f4d0af6cc6be2f38798892c013a666a0bc9ede2

--- data/sql/updates/pending_db_world/rev_1625138742497401267.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625138742497401267');
-
--- Siege Golem - set correct speed
-UPDATE `creature_template` SET `speed_walk` = 1 WHERE `entry` = 2749;
