4cb1524a6096aa817ebae072284f13d79588a4e5
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-07-08 01:23:20 +0930

    fix(DB/Quest): Remove respawn timers from "Broken Alliances" pillars (#6726)

---
+++ data/sql/updates/pending_db_world/rev_1625410362418489198.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625410362418489198');
+
+UPDATE `gameobject` SET `spawntimesecs` = 0 WHERE `id` = 2842 AND `guid` = 10643;
+UPDATE `gameobject` SET `spawntimesecs` = 0 WHERE `id` = 2848 AND `guid` = 10644;
+UPDATE `gameobject` SET `spawntimesecs` = 0 WHERE `id` = 2858 AND `guid` = 10830;
