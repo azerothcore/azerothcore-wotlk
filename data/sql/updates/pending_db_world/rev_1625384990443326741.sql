c87b92fbfa3123c9ac8de321b5fe6dfe20bbde54
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-07 15:16:48 +0000

    chore(DB): import pending files
    Referenced commit(s): 0fa3348bcd34ba081e6f86b65c867eaf0d8cb555

--- data/sql/updates/pending_db_world/rev_1625384990443326741.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625384990443326741');
-
-UPDATE `smart_scripts` SET `action_param1` = 8599, `comment` = 'Amani Berserker - Between 0-30% Health - Cast \'Enrage\' (No Repeat)' WHERE `entryorguid` = 15643 AND `source_type` = 0 AND `id` = 0;
-
