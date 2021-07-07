a2dc3939fc0d70a18051b363e4e284e704fb0d53
Christopher Höllriegl <c.hoellriegl@hotmail.de>
2021-07-07 22:24:35 +0200

    Removed unnecessary lines from sql script

--- data/sql/updates/pending_db_world/rev_1625481187426686000.sql
+++ data/sql/updates/pending_db_world/rev_1625481187426686000.sql
@@ -5,7 +5,6 @@ DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 5851) AND (`OptionID` IN (0))
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
 (5851, 0, 0, 'I know this is rather silly but a young ward who is a bit shy would like your hoofprint.', 0, 1, 1, 0, 0, 0, 0, '', 0, 0);
 
-
 -- Fix https://github.com/azerothcore/azerothcore-wotlk/issues/4147
 DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 7582) AND (`OptionID` IN (1));
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
@@ -22,6 +21,3 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 7559) AND (`OptionID` IN (0));
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
 (7559, 0, 0, 'Grant me your mark, wise ancient.', 0, 1, 1, 0, 0, 0, 0, '', 0, 0);
-
-
-
