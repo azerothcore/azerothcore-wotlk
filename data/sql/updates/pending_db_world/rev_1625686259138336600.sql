8bd733de4dc06c266fc178377ff786fa0cb221d3
Efymer <loscojones999@gmail.com>
2021-07-07 22:01:13 +0200

    Update rev_1625686259138336600.sql

--- data/sql/updates/pending_db_world/rev_1625686259138336600.sql
+++ data/sql/updates/pending_db_world/rev_1625686259138336600.sql
@@ -5,7 +5,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625686259138336600');
 DELETE FROM `event_scripts` WHERE `id` = 8605;
 
 -- Spawntime changed to 10 secs to prevent abuse
-UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI' WHERE `entry` = 179910;
+UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI', `Data2` = 0, `Data3` = 10000 WHERE `entry` = 179910;
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 1 AND `entryorguid` = 179910);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
