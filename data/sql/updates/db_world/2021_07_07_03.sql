672241e82b08de38e83b8cfa9cc96e2ae81cc143
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-07 12:10:14 +0000

    chore(DB): import pending files
    Referenced commit(s): bad784e5ae79385137d5166b27a85a3136f58b55

--- data/sql/updates/db_world/2021_07_07_03.sql
+++ data/sql/updates/db_world/2021_07_07_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_07_02 -> 2021_07_07_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_07_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_07_02 2021_07_07_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1625314084249000751'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625314084249000751');
 
 -- Verifonix
@@ -26,3 +42,13 @@ UPDATE `creature_template` SET `MovementType` = 1 WHERE `entry` = 8218;
 DELETE FROM `creature` WHERE `id` = 8218 AND `guid` = 85478;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (85478, 8218, 0, 0, 0, 1, 1, 0, 1, -337.534, -2849.99, 77.1062, 1.57174, 72000, 3, 0, 2218, 0, 1, 0, 0, 0, '', 0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_07_03' WHERE sql_rev = '1625314084249000751';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
