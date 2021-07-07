a6f6a3ca7916e03764026abcc3648bd62da6b423
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-07 11:22:47 +0000

    chore(DB): import pending files
    Referenced commit(s): 8876e26d54af61f085988a9c49dce37b7c1c9b79

--- data/sql/updates/pending_db_world/rev_1625291253501808914.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625291253501808914');
-
-DELETE FROM `reference_loot_template` WHERE `Entry` = 11105 AND `Item` = 13888;
-INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(11105, 13888, 0, 0, 0, 1, 1, 1, 1, 'Darkclaw Lobster');
