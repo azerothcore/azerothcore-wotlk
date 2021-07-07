458469ec01f1653b1c5b3b238afaf24da67366b8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-07 09:31:43 +0000

    chore(DB): import pending files
    Referenced commit(s): 39457b283754179476d369a47c2b2e8689675389

--- data/sql/updates/pending_db_world/rev_1625248286388193100.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625248286388193100');
-
-DELETE FROM `reference_loot_template` WHERE (`Entry` = 24060) AND (`Item` IN (2021));
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 569) AND (`Item` IN (2021));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(569, 2021, 0, 1.2, 0, 1, 0, 1, 1, '');
