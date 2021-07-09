db2c451fb8dcc59847d2a61d5e2a10dc285487da
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-09 16:47:29 +0000

    chore(DB): import pending files
    Referenced commit(s): ba2949ed171dd7beb7321c5c82c541914bc7e8bf

--- data/sql/updates/pending_db_world/rev_1625666226723560650.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625666226723560650');
-
-UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Zangen Stonehoof in Thunder Bluff.' WHERE `ID` = 1195;
-
