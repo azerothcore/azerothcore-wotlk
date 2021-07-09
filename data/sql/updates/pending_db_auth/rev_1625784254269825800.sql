e66a3b48973ad2106c5ff30c394cfc520aad8f99
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-07-09 04:28:13 +0200

    Update rev_1625784254269825800.sql

--- data/sql/updates/pending_db_auth/rev_1625784254269825800.sql
+++ data/sql/updates/pending_db_auth/rev_1625784254269825800.sql
@@ -1,17 +1,83 @@
--- INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1625784254269825800');
+INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1625784254269825800');
 
-DROP TABLE IF EXISTS `rbac_permissions`;
-CREATE TABLE `rbac_permissions`
-(
-  `id` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Permission id',
-  `name` VARCHAR(100) NOT NULL COMMENT 'Permission name',
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumpar struktur för tabell acore_auth.rbac_account_permissions
+CREATE TABLE IF NOT EXISTS `rbac_account_permissions` (
+  `accountId` int unsigned NOT NULL COMMENT 'Account id',
+  `permissionId` int unsigned NOT NULL COMMENT 'Permission id',
+  `granted` tinyint NOT NULL DEFAULT 1 COMMENT 'Granted = 1, Denied = 0',
+  `realmId` int NOT NULL DEFAULT -1 COMMENT 'Realm Id, -1 means all',
+  PRIMARY KEY (`accountId`,`permissionId`,`realmId`),
+  KEY `fk__rbac_account_roles__rbac_permissions` (`permissionId`),
+  CONSTRAINT `fk__rbac_account_permissions__account` FOREIGN KEY (`accountId`) REFERENCES `account` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__rbac_account_roles__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Account-Permission relation';
+
+-- Dumpar data för tabell acore_auth.rbac_account_permissions: ~0 rows (ungefär)
+DELETE FROM `rbac_account_permissions`;
+/*!40000 ALTER TABLE `rbac_account_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rbac_account_permissions` ENABLE KEYS */;
+
+-- Dumpar struktur för tabell acore_auth.rbac_default_permissions
+CREATE TABLE IF NOT EXISTS `rbac_default_permissions` (
+  `secId` int unsigned NOT NULL COMMENT 'Security Level id',
+  `permissionId` int unsigned NOT NULL COMMENT 'permission id',
+  `realmId` int NOT NULL DEFAULT -1 COMMENT 'Realm Id, -1 means all',
+  PRIMARY KEY (`secId`,`permissionId`,`realmId`),
+  KEY `fk__rbac_default_permissions__rbac_permissions` (`permissionId`),
+  CONSTRAINT `fk__rbac_default_permissions__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Default permission to assign to different account security levels';
+
+-- Dumpar data för tabell acore_auth.rbac_default_permissions: ~4 rows (ungefär)
+DELETE FROM `rbac_default_permissions`;
+/*!40000 ALTER TABLE `rbac_default_permissions` DISABLE KEYS */;
+INSERT INTO `rbac_default_permissions` (`secId`, `permissionId`, `realmId`) VALUES
+(3, 192, -1),
+(2, 193, -1),
+(1, 194, -1),
+(0, 195, -1);
+/*!40000 ALTER TABLE `rbac_default_permissions` ENABLE KEYS */;
+
+-- Dumpar struktur för tabell acore_auth.rbac_linked_permissions
+CREATE TABLE IF NOT EXISTS `rbac_linked_permissions` (
+  `id` int unsigned NOT NULL COMMENT 'Permission id',
+  `linkedId` int unsigned NOT NULL COMMENT 'Linked Permission id',
+  PRIMARY KEY (`id`,`linkedId`),
+  KEY `fk__rbac_linked_permissions__rbac_permissions1` (`id`),
+  KEY `fk__rbac_linked_permissions__rbac_permissions2` (`linkedId`),
+  CONSTRAINT `fk__rbac_linked_permissions__rbac_permissions1` FOREIGN KEY (`id`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__rbac_linked_permissions__rbac_permissions2` FOREIGN KEY (`linkedId`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Permission - Linked Permission relation';
+
+-- Dumpar data för tabell acore_auth.rbac_linked_permissions: ~0 rows (ungefär)
+DELETE FROM `rbac_linked_permissions`;
+/*!40000 ALTER TABLE `rbac_linked_permissions` DISABLE KEYS */;
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES
+(192, 196),
+(193, 197),
+(194, 198),
+(195, 199),
+(196, 200),
+(196, 201);
+/*!40000 ALTER TABLE `rbac_linked_permissions` ENABLE KEYS */;
+
+-- Dumpar struktur för tabell acore_auth.rbac_permissions
+CREATE TABLE IF NOT EXISTS `rbac_permissions` (
+  `id` int unsigned NOT NULL DEFAULT '0' COMMENT 'Permission id',
+  `name` varchar(100) NOT NULL COMMENT 'Permission name',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Permission List';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Permission List';
 
-LOCK TABLES `rbac_permissions` WRITE;
+-- Dumpar data för tabell acore_auth.rbac_permissions: ~7 rows (ungefär)
+DELETE FROM `rbac_permissions`;
 /*!40000 ALTER TABLE `rbac_permissions` DISABLE KEYS */;
-DELETE FROM rbac_permissions;
-INSERT INTO `rbac_permissions` VALUES
+INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
 (200, 'Command: rbac'),
 (201, 'Command: rbac account'),
 (202, 'Command: rbac account list'),
@@ -20,67 +86,11 @@ INSERT INTO `rbac_permissions` VALUES
 (205, 'Command: rbac account revoke'),
 (206, 'Command: rbac list');
 /*!40000 ALTER TABLE `rbac_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `rbac_account_permissions`;
-CREATE TABLE `rbac_account_permissions`
-(
-  `accountId` INT UNSIGNED NOT NULL COMMENT 'Account id',
-  `permissionId` INT UNSIGNED NOT NULL COMMENT 'Permission id',
-  `granted` TINYINT NOT NULL DEFAULT 1 COMMENT 'Granted = 1, Denied = 0',
-  `realmId` INT NOT NULL DEFAULT -1 COMMENT 'Realm Id, -1 means all',
-  PRIMARY KEY (`accountId`,`permissionId`,`realmId`),
-  KEY `fk__rbac_account_roles__rbac_permissions` (`permissionId`),
-  CONSTRAINT `fk__rbac_account_permissions__account` FOREIGN KEY (`accountId`) REFERENCES `account` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk__rbac_account_roles__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Account-Permission relation';
-
-DROP TABLE IF EXISTS `rbac_default_permissions`;
-CREATE TABLE `rbac_default_permissions`
-(
-  `secId` INT UNSIGNED NOT NULL COMMENT 'Security Level id',
-  `permissionId` INT UNSIGNED NOT NULL COMMENT 'permission id',
-  `realmId` INT NOT NULL DEFAULT -1 COMMENT 'Realm Id, -1 means all',
-  PRIMARY KEY (`secId`,`permissionId`,`realmId`),
-  KEY `fk__rbac_default_permissions__rbac_permissions` (`permissionId`),
-  CONSTRAINT `fk__rbac_default_permissions__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Default permission to assign to different account security levels';
-
-LOCK TABLES `rbac_default_permissions` WRITE;
-/*!40000 ALTER TABLE `rbac_default_permissions` DISABLE KEYS */;
-DELETE FROM `rbac_default_permissions`;
-INSERT INTO `rbac_default_permissions` VALUES
-(3,192,-1), -- Administrator
-(2,193,-1), -- GameMaster
-(1,194,-1), -- Moderator
-(0,195,-1); -- Player
-/*!40000 ALTER TABLE `rbac_default_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
-
-DROP TABLE IF EXISTS `rbac_linked_permissions`;
-CREATE TABLE `rbac_linked_permissions`
-(
-  `id` INT UNSIGNED NOT NULL COMMENT 'Permission id',
-  `linkedId` INT UNSIGNED NOT NULL COMMENT 'Linked Permission id',
-  PRIMARY KEY (`id`,`linkedId`),
-  KEY `fk__rbac_linked_permissions__rbac_permissions1` (`id`),
-  KEY `fk__rbac_linked_permissions__rbac_permissions2` (`linkedId`),
-  CONSTRAINT `fk__rbac_linked_permissions__rbac_permissions1` FOREIGN KEY (`id`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk__rbac_linked_permissions__rbac_permissions2` FOREIGN KEY (`linkedId`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Permission - Linked Permission relation';
-
-LOCK TABLES `rbac_linked_permissions` WRITE;
-/*!40000 ALTER TABLE `rbac_linked_permissions` DISABLE KEYS */;
-INSERT INTO `rbac_linked_permissions` VALUES
-(192, 196),
-(193, 197),
-(194, 198),
-(195, 199),
-(196, 200),
-(196, 201);
-/*!40000 ALTER TABLE `rbac_linked_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
 DROP VIEW IF EXISTS `vw_rbac`;
 CREATE SQL SECURITY INVOKER VIEW `vw_rbac` AS
 (
