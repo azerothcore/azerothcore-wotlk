2fcafa5f3915577dd921349ef5c2dcf56aa9f896
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-10 12:51:12 +0000

    chore(DB): import pending files
    Referenced commit(s): 5020056bfa3779b49464a3764976fda402987b59

--- data/sql/updates/pending_db_world/rev_1625774675099271452.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625774675099271452');
-
-UPDATE `creature_loot_template` SET `GroupId` = 1, `Comment` = 'Chief Ukorz Sandscalp - Spellshock Leggings' WHERE `Entry` = 7267 AND `Item` = 9484; 
-UPDATE `creature_loot_template` SET `GroupId` = 1 WHERE `Entry` = 8127 AND `Item` = 9484;
-
