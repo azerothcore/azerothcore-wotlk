2e829144c36d4060dc1fc806ffa8a14879ecc8ce
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-10 09:02:08 +0000

    chore(DB): import pending files
    Referenced commit(s): e5c78f5a9581259913cfaeacc02f66aa3adddda4

--- data/sql/updates/pending_db_world/rev_1625736069367927700.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625736069367927700');
-
--- ID: 11874 - Masat T'andr
--- faction was 1638, speed_run was 1
-UPDATE `creature_template` SET `faction` = 35, `speed_run` = 1.14286 WHERE (`entry` = 11874);
-
--- ID: 11198 - Broken Exile
--- faction was 1638, speed_run was 1
-UPDATE `creature_template` SET `faction` = 15, `speed_run` = 1.14286 WHERE (`entry` = 11198);
