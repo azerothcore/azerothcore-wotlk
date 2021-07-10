229e3d68c871417788670298c2591c1dba18d3b0
Loron gröbner <raizo@riseup.net>
2021-07-10 13:50:09 +0200

    Add copyright notice

--- src/server/game/Entities/Player/PlayerUpdates.cpp
+++ src/server/game/Entities/Player/PlayerUpdates.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ */
+ 
 #include "BattlefieldMgr.h"
 #include "Channel.h"
 #include "ChannelMgr.h"
