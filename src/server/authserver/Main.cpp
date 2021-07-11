8c0a107b2cd2a06e91aafc83828281e992135572
Winfidonarleyan <dowlandtop@yandex.com>
2021-07-11 22:53:29 +0700

    feat(Core/Logging): add support fmt::format logging

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -51,8 +51,7 @@ bool stopEvent = false;                                     // Setting it to tru
 void usage(const char* prog)
 {
     LOG_INFO("server.authserver", "Usage: \n %s [<options>]\n"
-                    "    -c config_file           use config_file as configuration file\n\r",
-                    prog);
+                    "    -c config_file           use config_file as configuration file\n", prog);
 }
 
 /// Launch the auth server
@@ -90,18 +89,17 @@ extern int main(int argc, char** argv)
     sLog->Initialize();
 
     Acore::Banner::Show("authserver",
-        [](char const* text)
+        [](std::string_view text)
         {
-            LOG_INFO("server.authserver", "%s", text);
+            FMT_LOG_INFO("server.authserver", text);
         },
         []()
         {
-            LOG_INFO("server.authserver", "> Using configuration file       %s.", sConfigMgr->GetFilename().c_str());
-            LOG_INFO("server.authserver", "> Using SSL version:             %s (library: %s)", OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
-            LOG_INFO("server.authserver", "> Using Boost version:           %i.%i.%i", BOOST_VERSION / 100000, BOOST_VERSION / 100 % 1000, BOOST_VERSION % 100);
-            LOG_INFO("server.authserver", "> Using ACE version:             %s\n", ACE_VERSION);
-        }
-    );
+            FMT_LOG_INFO("server.authserver", "> Using configuration file       {}", sConfigMgr->GetFilename());
+            FMT_LOG_INFO("server.authserver", "> Using SSL version:             {} (library: {})", OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
+            FMT_LOG_INFO("server.authserver", "> Using Boost version:           {}.{}.{}", BOOST_VERSION / 100000, BOOST_VERSION / 100 % 1000, BOOST_VERSION % 100);
+            FMT_LOG_INFO("server.authserver", "> Using ACE version:             {}", ACE_VERSION);
+        });
 
 #if defined (ACE_HAS_EVENT_POLL) || defined (ACE_HAS_DEV_POLL)
     ACE_Reactor::instance(new ACE_Reactor(new ACE_Dev_Poll_Reactor(ACE::max_handles(), 1), 1), true);
