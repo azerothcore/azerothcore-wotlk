752c45917bdf08f34a65476f3494960ad2f38010
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-07-11 20:37:17 +0930

    fix(DB/Creature): various static NPCs in Darkshore/Tanaris (#6869)

---
+++ data/sql/updates/pending_db_world/rev_1625925059779148449.sql
@@ -0,0 +1,15 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625925059779148449');
+
+-- Kregg Keelhaul
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 10 WHERE `id` = 8203 AND `guid` = 51834;
+-- Licillin
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 10 WHERE `id` = 2191 AND `guid` = 52009;
+-- Carnivous the Breaker
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5  WHERE `id` = 2186 AND `guid` = 51900;
+-- Flagglemurk the Cruel
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 10 WHERE `id` = 7015 AND `guid` = 51899;
+-- Lord Sinslayer
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 3  WHERE `id` = 7017 AND `guid` = 37089;
+
+-- Correct speeds for Kregg and Licillin
+UPDATE `creature_template` SET `speed_walk` = 1 WHERE `entry`IN (8203, 2191);
