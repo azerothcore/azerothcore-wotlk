5f16085c2e914c8cb71ff5d84816b06af33cc9a3
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-07-12 22:22:40 +0200

    fix(DB): Moved herb from rocky surface to grass

--- data/sql/updates/pending_db_world/rev_1626119631085426000.sql
+++ data/sql/updates/pending_db_world/rev_1626119631085426000.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626119631085426000');
 
 UPDATE `gameobject` SET `position_x` = -3821.93, `position_y` = -1474.1, `position_z` = 133.19 WHERE `id` = 1623 AND `guid` = 4198;
-UPDATE `gameobject` SET `position_x` = -3467.42, `position_y` = -1806.8, `position_z` = 95.39 WHERE `id` = 1623 AND `guid` = 4252;
+UPDATE `gameobject` SET `position_x` = -3472.18, `position_y` = -1814.58, `position_z` = 91.94 WHERE `id` = 1623 AND `guid` = 4252;
