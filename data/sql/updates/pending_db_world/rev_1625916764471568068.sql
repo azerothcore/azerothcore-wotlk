05bbf4a8589c46fdebd5da75d61847526b2d220a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-12 08:23:43 +0000

    chore(DB): import pending files
    Referenced commit(s): 7cf5fbc458e70aa1ed4e1091e060bf21891e4a69

--- data/sql/updates/pending_db_world/rev_1625916764471568068.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625916764471568068');
-
--- Deletes lvl (min:10/avg:10.68/max:11) RLT 24074 from lvl 44 Blisterpaw Hyena (ID 5426), lvl 43 Fire Roc (ID 5429)
-DELETE FROM `creature_loot_template` WHERE `Entry` IN (5426, 5429) AND `Reference` = 24074;
