22467c6ff0d870947d1c92bbfd0c0ae1f42d1136
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-07-12 23:06:44 +0200

    fix(DB): Moved groomsblood
     - This is done to align more with vanilla TBC

--- data/sql/updates/pending_db_world/rev_1626116879969851100.sql
+++ data/sql/updates/pending_db_world/rev_1626116879969851100.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626116879969851100');
 
 -- move the Groomsblood down on the ground
-UPDATE `gameobject` SET `position_x` = -11745, `position_y` = -3272, `position_z` = 4.467 WHERE `id` = 142145 AND `guid` = 16527;
-UPDATE `gameobject` SET `position_x` = -11865.76, `position_y` = -3065.08, `position_z` = 14.45 WHERE `id` = 142145 AND `guid` = 16504;
+UPDATE `gameobject` SET `position_x` = -11810.63, `position_y` = -3047.32, `position_z` = 9.84 WHERE `id` = 142145 AND `guid` = 16527;
+UPDATE `gameobject` SET `position_x` = -11685.57, `position_y` = -3211.47, `position_z` = 12.53 WHERE `id` = 142145 AND `guid` = 16504;
