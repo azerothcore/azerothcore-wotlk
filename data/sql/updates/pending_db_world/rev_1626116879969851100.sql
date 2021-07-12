5f3dbb202fa4750af1380c2bd0758f66995a9147
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-07-12 21:38:04 +0200

    fix(DB): move another herb down to the ground

--- data/sql/updates/pending_db_world/rev_1626116879969851100.sql
+++ data/sql/updates/pending_db_world/rev_1626116879969851100.sql
@@ -1,4 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626116879969851100');
 
 -- move the Groomsblood down on the ground
-UPDATE `gameobject` SET `position_x` = -11745, `position_y` = -3272, `position_z` = 4.467 WHERE `id` = 142145 AND `guid` = 16527
+UPDATE `gameobject` SET `position_x` = -11745, `position_y` = -3272, `position_z` = 4.467 WHERE `id` = 142145 AND `guid` = 16527;
+UPDATE `gameobject` SET `position_x` = -11865.76, `position_y` = -3065.08, `position_z` = 14.45 WHERE `id` = 142145 AND `guid` = 16504;
