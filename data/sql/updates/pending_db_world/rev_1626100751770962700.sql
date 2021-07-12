5d326bcb8e42a0d3a593600dc0affe85745906b9
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-07-12 16:47:10 +0200

    fix(DB/Creature): Fix static Ironjaw Basilisks
     - The Ironjaw Basilisks (GUID 373, 612, 756, 981 and 1062) were the only non moving in the cave, whereas they wander.
     - Closes #6899

---
+++ data/sql/updates/pending_db_world/rev_1626100751770962700.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626100751770962700');
+
+-- Add wandering to the NPCs 373, 612, 756, 921 and 1062
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 2 WHERE `id` = 1551 AND `guid` = 373;
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 2 WHERE `id` = 1551 AND `guid` = 612;
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 1551 AND `guid` = 756;
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 1551 AND `guid` = 981;
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 1551 AND `guid` = 1062;
