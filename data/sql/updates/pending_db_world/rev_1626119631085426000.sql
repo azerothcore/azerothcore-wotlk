26769bc2a58a5d4b425a9dfbbd841b25fe66b4fa
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-07-12 22:50:39 +0200

    fix(DB): moved herbs
     - This is to better align with data from wow TBC

--- data/sql/updates/pending_db_world/rev_1626119631085426000.sql
+++ data/sql/updates/pending_db_world/rev_1626119631085426000.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626119631085426000');
 
-UPDATE `gameobject` SET `position_x` = -3821.93, `position_y` = -1474.1, `position_z` = 133.19 WHERE `id` = 1623 AND `guid` = 4198;
-UPDATE `gameobject` SET `position_x` = -3472.18, `position_y` = -1814.58, `position_z` = 91.94 WHERE `id` = 1623 AND `guid` = 4252;
+UPDATE `gameobject` SET `position_x` = -3333.07, `position_y` = -1774.95, `position_z` = 115.58 WHERE `id` = 1623 AND `guid` = 4198;
+UPDATE `gameobject` SET `position_x` = -4292.46, `position_y` = -2271.48, `position_z` = 109.59 WHERE `id` = 1623 AND `guid` = 4252;
