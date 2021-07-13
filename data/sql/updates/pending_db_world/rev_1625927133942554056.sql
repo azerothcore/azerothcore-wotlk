114d55d41a3192916426d1c9111b708d3ef14534
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-13 12:01:10 +0000

    chore(DB): import pending files
    Referenced commit(s): ffd0d7fbecaa345c9b112d4dc07a9388eb83652e

--- data/sql/updates/pending_db_world/rev_1625927133942554056.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625927133942554056');
-
--- Stop Wrathscale Siren spawning underground
-UPDATE `creature` SET `position_z` = -21.14 WHERE `id` = 17195 AND `guid` = 60996;
