4b95f08e74b0540763e106984f9da68555bdd6a1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-13 14:43:09 +0000

    chore(DB): import pending files
    Referenced commit(s): 020fcaf5f721f1552af569f5159e761465d5338b

--- data/sql/updates/pending_db_world/rev_1626007477539439387.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626007477539439387');
-
--- Delete Arathi PVP supplies from Warpwood Stomper, Desert Rumbler, Vekniss Soldier
-DELETE FROM `creature_loot_template` WHERE `entry` IN (11465, 11746, 15229) AND `item` IN (20062, 20066);
-
--- Delete loot table for Field Marshal Oslight 
-UPDATE `creature_template` SET `lootid` = 0 WHERE `entry` = 14983;
-DELETE FROM `creature_loot_template` WHERE `entry` = 14983;
-
