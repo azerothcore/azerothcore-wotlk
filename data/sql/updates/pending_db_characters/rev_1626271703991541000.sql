06b2869bee7413e6247628b6f4aadb440ef2b082
robinsch <hello@robinsch.com>
2021-07-14 16:19:45 +0200

    SQL: Changed column name

--- data/sql/updates/pending_db_characters/rev_1626271703991541000.sql
+++ data/sql/updates/pending_db_characters/rev_1626271703991541000.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1626271703991541000');
 ALTER TABLE `item_loot_storage` 
-ADD COLUMN `item_index` int(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `item_count`;
+ADD COLUMN `item_index` int(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `count`;
