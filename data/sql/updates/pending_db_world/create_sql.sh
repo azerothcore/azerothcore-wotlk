5b5a772bb4a0bd19fa1493caa2c0a7bf7533416f
Melonio2 <roachs2606@gmail.com>
2021-07-14 18:16:43 +0200

    Fix DB RewardText for quest 1507
    DESCRIPTION
    Change hard coded name to adapt to the player's name.
    I put SQL in the good file this time !
    
    Closes AzerothCore issue #6887

--- data/sql/updates/pending_db_world/create_sql.sh
+++ data/sql/updates/pending_db_world/create_sql.sh
@@ -13,7 +13,3 @@ rev=$( $date +%s%N );
 filename=rev_"$rev".sql
 
 echo "INSERT INTO \`version_db_world\` (\`sql_rev\`) VALUES ('"$rev"');" > "$CUR_PATH/$filename" && echo "File created: $filename";
-
-DELETE FROM `quest_offer_reward` WHERE (`ID` = 1507);
-INSERT INTO `quest_offer_reward` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `RewardText`, `VerifiedBuild`) VALUES
-(1507, 0, 0, 0, 0, 0, 0, 0, 0, 'Hm... $N. You are still new to your path, but I sense the possibility for greatness in you.$B$BYou were born with gifts, $N. See that they do not go to waste.', 12340);
