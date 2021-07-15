42f27af83c4f4f4f8e46b742a24c9c11fff8561a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-15 09:42:32 +0000

    chore(DB): import pending files
    Referenced commit(s): d0512d8bcd9b546965b8c07819043ad6ff3c01c7

--- data/sql/updates/pending_db_world/rev_1626123639599949300.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626123639599949300');
-
-DELETE FROM `quest_offer_reward` WHERE (`ID` = 1507);
-INSERT INTO `quest_offer_reward` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `RewardText`, `VerifiedBuild`) VALUES
-(1507, 0, 0, 0, 0, 0, 0, 0, 0, 'Hm... $N. You are still new to your path, but I sense the possibility for greatness in you.$B$BYou were born with gifts, $N. See that they do not go to waste.', 12340);
