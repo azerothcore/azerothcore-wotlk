f4972a8f96609ec1eb62b951db181bd0e659ebc7
Winfidonarleyan <dowlandtop@yandex.com>
2021-07-16 18:29:36 +0700

    feat(Deps/Ace): remove ace library. Part 1

--- src/server/worldserver/PrecompiledHeaders/worldPCH.h
+++ src/server/worldserver/PrecompiledHeaders/worldPCH.h
@@ -1,8 +1,11 @@
-#include "WorldSocket.h"        // must be first to make ACE happy with ACE includes in it
+/*
+ * Copyright (C) 2016+     AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
+ */
 
 #include "Common.h"
-#include "Configuration/Config.h"
-#include "Database/DatabaseEnv.h"
+#include "Config.h"
+#include "DatabaseEnv.h"
 #include "Log.h"
 #include "Util.h"
 #include "World.h"
+#include "WorldSocket.h"
