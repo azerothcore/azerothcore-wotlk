a57c4fd3b82ef2a59e58d172ad6df5b01552b2e1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-18 10:40:38 +0000

    chore(DB): import pending files
    Referenced commit(s): 58b0bca04abad7bb6776d07db58368acf53e5e9e

--- data/sql/updates/db_world/2021_07_18_00.sql
+++ data/sql/updates/db_world/2021_07_18_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_16_02 -> 2021_07_18_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_16_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_16_02 2021_07_18_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1626186297807070700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626186297807070700');
 
 DELETE FROM `gossip_menu` WHERE (`MenuID` = 7403) AND (`TextID` IN (8869));
@@ -16,3 +32,13 @@ DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 14) AND (`SourceGrou
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (14, 7403, 8870, 0, 0, 16, 0, 1024, 0, 0, 0, 0, 0, '', 'Enable GOSSIP_MENU only for Draenei');
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_18_00' WHERE sql_rev = '1626186297807070700';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
