be6262f197d679f37459cf5e987fa91a1ef8ff4e
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-07-18 22:20:33 +0200

    Re-add sql_rev insert

--- data/sql/updates/pending_db_world/rev_1626444381466806000.sql
+++ data/sql/updates/pending_db_world/rev_1626444381466806000.sql
@@ -1,5 +1,6 @@
-DELETE FROM `creature_loot_template` WHERE `Entry` IN (193, 6130, 6129, 6131) AND `Item` = 21949;
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626444381466806000');
 
+DELETE FROM `creature_loot_template` WHERE `Entry` IN (193, 6130, 6129, 6131) AND `Item` = 21949;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (193, 21949, 0, 0.5, 0, 1, 1, 1, 1, 'Blue Dragonspawn - Design: Ruby Serpent'),
 (6130, 21949, 0, 0.5, 0, 1, 1, 1, 1, 'Blue Scalebane - Design: Ruby Serpent'),
