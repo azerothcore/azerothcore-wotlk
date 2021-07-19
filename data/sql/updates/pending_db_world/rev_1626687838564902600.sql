8b51bd4558c5551c879af6e9d9e40580b731cf18
Asthalor <mac99108@gmail.com>
2021-07-19 12:12:43 +0200

    Fixed SQL query
    Improved the sql query

--- data/sql/updates/pending_db_world/rev_1626687838564902600.sql
+++ data/sql/updates/pending_db_world/rev_1626687838564902600.sql
@@ -1,14 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626687838564902600');
 
--- Pyrewood leatherworker
-UPDATE `creature_template` SET `ArmorModifier` = 1.5 WHERE (`entry` = 3532);
+-- Pyrewood leatherworker, Pyrewood Armorer,Moonrage armorer, Moonrage Leatherworker 
+UPDATE `creature_template` SET `ArmorModifier` = 1.5 WHERE `entry` IN (3532, 3528, 3529, 3533);
 
--- Pyrewood Armorer
-UPDATE `creature_template` SET `ArmorModifier` = 1.5 WHERE (`entry` = 3528);
-
--- Moonrage armorer
-UPDATE `creature_template` SET `ArmorModifier` = 1.5 WHERE (`entry` = 3529);
-
--- Moonrage Leatherworker 
-UPDATE `creature_template` SET `ArmorModifier` = 1.5 WHERE (`entry` = 3533);
 
