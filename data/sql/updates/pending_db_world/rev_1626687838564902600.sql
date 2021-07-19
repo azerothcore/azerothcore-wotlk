b78319b06e2cdbb7105c657d2ca8b91fb43e028b
Asthalor <mac99108@gmail.com>
2021-07-19 12:14:05 +0200

    Deleted end line duplicated

--- data/sql/updates/pending_db_world/rev_1626687838564902600.sql
+++ data/sql/updates/pending_db_world/rev_1626687838564902600.sql
@@ -3,4 +3,3 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626687838564902600');
 -- Pyrewood leatherworker, Pyrewood Armorer,Moonrage armorer, Moonrage Leatherworker 
 UPDATE `creature_template` SET `ArmorModifier` = 1.5 WHERE `entry` IN (3532, 3528, 3529, 3533);
 
-
