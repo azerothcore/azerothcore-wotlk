b6c9c4d242a0dae19ab30c264f492103ca158170
Asthalor <mac99108@gmail.com>
2021-07-20 15:27:44 +0200

    Improved the SQL query

--- data/sql/updates/pending_db_world/rev_1626782944130753700.sql
+++ data/sql/updates/pending_db_world/rev_1626782944130753700.sql
@@ -1,7 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626782944130753700');
 
 -- Changed the spawn point of Spellmaw closer to the ground and added patrolling around
-DELETE FROM `creature` WHERE (`id` = 10662) AND (`guid` IN (42265));
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(42265, 10662, 1, 0, 0, 1, 1, 9995, 0, 6247.134, -4412.64, 687.228, 4.67879, 600, 20, 0, 16194, 0, 1, 0, 0, 0, '', 0);
+UPDATE `creature` SET `position_x` = 6247.134, `position_y` = -4412.64, `position_z` = 687.228, `orientation` = 4.67879, `wander_distance` = 20,`MovementType` = 1  WHERE (`id` = 10662) AND (`guid` = 42265);
 
