4332a7a43a1212b0ac3091ef0457c27c43728c71
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-20 12:29:31 +0000

    chore(DB): import pending files
    Referenced commit(s): b226df1ece50ed0efec7d87bbe4384f731352e78

--- data/sql/updates/db_world/2021_07_20_00.sql
+++ data/sql/updates/db_world/2021_07_20_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_19_00 -> 2021_07_20_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_19_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_19_00 2021_07_20_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1626206610803393000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626206610803393000');
 
 -- Remove the nodes from any existing nodes then add them to their distinct pool
@@ -64,3 +80,13 @@ UPDATE `gameobject` SET `spawntimesecs` = @RESPAWN WHERE `guid` IN (SELECT `guid
 
 -- Drop temp table
 DROP TABLE TEMP_POOL_ENTRIES;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_20_00' WHERE sql_rev = '1626206610803393000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
