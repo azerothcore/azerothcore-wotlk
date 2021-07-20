578f59c169619dc9c27081cfdf787d77a756ac50
Asthalor <mac99108@gmail.com>
2021-07-20 16:28:58 +0200

    Added delete clause before the insert into

--- data/sql/updates/pending_db_world/rev_1626780409112630500.sql
+++ data/sql/updates/pending_db_world/rev_1626780409112630500.sql
@@ -4,6 +4,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626780409112630500');
 UPDATE `creature` SET `wander_distance` = 20, `MovementType` = 1 WHERE (`id` = 14343) AND (`guid` = 51897);
 
 -- Added 2 more spawn points and movement on them
+DELETE FROM `creature` WHERE (`id` = 14343) AND (`guid` IN (301303, 301304));
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (301303, 14343, 1, 0, 0, 1, 1, 0, 0, 6091.22, -1485.77, 438.13, 0.18, 9900, 20, 0, 1, 0, 1, 0, 0, 0, '', 0),
 (301304, 14343, 1, 0, 0, 1, 1, 0, 0, 6307.9, -1637.18, 474.611, 0, 9900, 20, 0, 1, 0, 1, 0, 0, 0, '', 0);
