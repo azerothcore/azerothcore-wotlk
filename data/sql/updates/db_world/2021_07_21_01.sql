dfa890fde4608d41735894a3e6dc3632d3a7dd71
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-21 18:47:24 +0000

    chore(DB): import pending files
    Referenced commit(s): 38f7afeaa9e6d627e28ce41998f4717f3f28da80

--- data/sql/updates/db_world/2021_07_21_01.sql
+++ data/sql/updates/db_world/2021_07_21_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_21_00 -> 2021_07_21_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_21_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_21_00 2021_07_21_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1626390963148739000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626390963148739000');
 
 -- fixed Chinese translation errors
@@ -19,3 +35,13 @@ INSERT INTO `gameobject_template_locale` (`entry`, `locale`, `name`, `castBarCap
 -- fixed name error
 UPDATE `creature_template_locale` SET `Title` = '勇气纹章军需官' WHERE `entry` = '31581' AND `locale` = 'zhCN';
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_21_01' WHERE sql_rev = '1626390963148739000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
