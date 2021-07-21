e954e3e32a5ccccfb2ffa28e11451db2efd29d72
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-21 14:54:23 +0000

    chore(DB): import pending files
    Referenced commit(s): 40796fa3ddcb994a06b4f2087b073b5d3581aa9e

--- data/sql/updates/db_world/2021_07_21_00.sql
+++ data/sql/updates/db_world/2021_07_21_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_20_00 -> 2021_07_21_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_20_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_20_00 2021_07_21_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1626317015822404300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626317015822404300');
 
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 7158) AND (`Item` IN (21377));
@@ -35,3 +51,13 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 9462) AND (`Item` IN (21377));
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (9462, 21377, 0, 39, 0, 1, 0, 1, 1, 'Chieftain Bloodmaw - Deadwood Headdress Feather');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_21_00' WHERE sql_rev = '1626317015822404300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
