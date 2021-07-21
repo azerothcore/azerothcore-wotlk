b7332b93050cbd69e2122fbe03e930bb1d907309
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-21 10:06:40 +0000

    chore(DB): import pending files
    Referenced commit(s): a3e3fd029fa01c26b12e7789c9e149b798c7e14a

--- data/sql/updates/pending_db_characters/rev_1626271703991541000.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1626271703991541000');
-ALTER TABLE `item_loot_storage` 
-  ADD COLUMN `item_index` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `count`;
