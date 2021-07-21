383946af6cda4205f0d427fb7c1d2b4a11b0d21f
Francesco Borzì <borzifrancesco@gmail.com>
2021-07-21 14:12:40 +0200

    refactor(Scripts/OutdoorPvP): code cleanup (part 7) (#6927)

--- src/server/scripts/OutdoorPvP/OutdoorPvPEP.cpp
+++ src/server/scripts/OutdoorPvP/OutdoorPvPEP.cpp
@@ -4,10 +4,8 @@
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  */
 
-#include "Creature.h"
 #include "GameGraveyard.h"
 #include "GameObject.h"
-#include "GossipDef.h"
 #include "Language.h"
 #include "MapManager.h"
 #include "ObjectAccessor.h"
@@ -140,11 +138,7 @@ void OPvPCapturePointEP_EWT::SummonSupportUnitAtNorthpassTower(TeamId teamId)
     if (m_UnitsSummonedSideId != teamId)
     {
         m_UnitsSummonedSideId = teamId;
-        const creature_type* ct = nullptr;
-        if (teamId == TEAM_ALLIANCE)
-            ct = EP_EWT_Summons_A;
-        else
-            ct = EP_EWT_Summons_H;
+        const creature_type* ct = teamId == TEAM_ALLIANCE ? EP_EWT_Summons_A : EP_EWT_Summons_H;
 
         for (uint8 i = 0; i < EP_EWT_NUM_CREATURES; ++i)
         {
