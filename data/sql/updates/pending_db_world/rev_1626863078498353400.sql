0233648ee6f35388287616c32718db3f77f77e6c
Asthalor <mac99108@gmail.com>
2021-07-21 12:28:06 +0200

    fix(DB/Creature): Changed the spell Sneak(11013) to Sneak(22766)
    They were using a wrong sneak spell, they should go into stealth when used

---
+++ data/sql/updates/pending_db_world/rev_1626863078498353400.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626863078498353400');
+
+-- Changed the id of the spell so they use the Sneak that puts them on stealth. Changed the comments to know what spell are they using
+UPDATE `smart_scripts` SET `action_param1` = 22766, `comment` = 'Jadefire Shadowstalker - On Respawn - Cast Sneak (Stealth mode)' WHERE (`entryorguid` = 7110) AND (`source_type` = 0) AND (`id` = 0);
+UPDATE `smart_scripts` SET `comment` = 'Jadefire Shadowstalker - On Aggro - Cast Jadefire' WHERE (`entryorguid` = 7110) AND (`source_type` = 0) AND (`id` = 1);
+UPDATE `smart_scripts` SET `comment` = 'Jadefire Shadowstalker - On Respawn - Cast Slowing Poison' WHERE (`entryorguid` = 7110) AND (`source_type` = 0) AND (`id` = 2);
+
