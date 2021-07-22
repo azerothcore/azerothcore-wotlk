833aa25a881b34794bffa9af1d924df66641764e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-22 06:41:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 586175ed4c52e22abb9451d178b768bc6357e761

--- data/sql/updates/db_world/2021_07_22_01.sql
+++ data/sql/updates/db_world/2021_07_22_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_22_00 -> 2021_07_22_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_22_00 2021_07_22_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1626462660514898600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626462660514898600');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 8978;
@@ -8,3 +24,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (8978, 0, 1, 0, 0, 0, 100, 0, 7000, 11000, 21700, 35200, 0, 11, 6533, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Thauris Balgarr - In Combat - Cast \'Net\''),
 (8978, 0, 2, 0, 0, 0, 100, 0, 1000, 3000, 19200, 32700, 0, 11, 11802, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Thauris Balgarr - In Combat - Cast \'Dark Iron Land Mine\'');
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_22_01' WHERE sql_rev = '1626462660514898600';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
