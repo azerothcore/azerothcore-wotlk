1cf7f5c8a00c5dfb8f19d26b98cb6d5b79076b08
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-07-22 02:34:35 +0200

    fix(DB/Gossip): Champion Cyssa Dawnrose gossip in Undercity (#7007)
    Champion Cyssa had a draenei gossip. That happened because the gossip menu id had 2 gossip lines.
    One the draenei and other the correct one.
    I deleted the correct one and added to a new gossip id.
    After that i updated the character to use the right one.

---
+++ data/sql/updates/pending_db_world/rev_1626532264671526700.sql
@@ -0,0 +1,11 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626532264671526700');
+
+-- We had 2 gossips with the id, and it was choosing the wrong one. So we insert into a new one id.
+DELETE FROM `gossip_menu` WHERE `menuid` = 61025 and `textid` = 10041;
+
+-- The line 10041 is the one needed for this character.
+INSERT INTO `gossip_menu` (`menuid`, `textid` ) VALUES (61025 ,10041);
+
+-- We update the character to use the new gossip id.
+UPDATE `creature_template` SET `gossip_menu_id` = 61025, `faction` = 1604 WHERE (`entry` = 20406);
+
