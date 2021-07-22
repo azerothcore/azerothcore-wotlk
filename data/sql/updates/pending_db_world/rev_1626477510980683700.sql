5368da64e08e780d2cd28b1d58e06b726411fcc4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-22 09:03:54 +0000

    chore(DB): import pending files
    Referenced commit(s): 7830ff5b2864429af12e26a35675bd281dc8ce47

--- data/sql/updates/pending_db_world/rev_1626477510980683700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626477510980683700');
-
--- Removes the four objects
-DELETE FROM `gameobject` WHERE `id` = 177272 AND `guid` IN (42907, 42908, 42909, 42910);
