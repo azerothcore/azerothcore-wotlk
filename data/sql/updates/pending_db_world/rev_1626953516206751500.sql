05694a43ce484d5d17ff73cca743713e64128475
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-23 11:15:27 +0000

    chore(DB): import pending files
    Referenced commit(s): ba85988d31639f83b9bad6fb41377bcf843bb4a6

--- data/sql/updates/pending_db_world/rev_1626953516206751500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626953516206751500');
-
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 13278 AND `spell_effect` = 13493;
-INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment`) VALUES
-(13278, 13493, 0, 'Gnomish Death Ray');
