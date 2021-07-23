3d4befd7043fbbceb32f7c2a6e0a4f9f638f8b0a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-23 13:15:27 +0000

    chore(DB): import pending files
    Referenced commit(s): f4e5a0ef98981a8a65bb539a4666ced1f023bfce

--- data/sql/updates/pending_db_world/rev_1626884022511079500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626884022511079500');
-
--- Slowed the movement speed of Dragonmaw Battlemaster from 1.54 to 1 as the rest of the orcs there
-UPDATE `creature_template` SET `speed_walk` = 1 WHERE (`entry` = 1037);
-
