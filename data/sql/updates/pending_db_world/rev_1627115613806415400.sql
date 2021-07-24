b8cde4689f272c0b38ab3a738ac12690a42da9a4
syssneck <ron_sommerfeld@web.de>
2021-07-24 15:44:40 +0200

    Fix whitespace and newline

--- data/sql/updates/pending_db_world/rev_1627115613806415400.sql
+++ data/sql/updates/pending_db_world/rev_1627115613806415400.sql
@@ -1,6 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627115613806415400');
 
---Changed the position of Peacebloom (guid=26813) and deleted the dependency on the Darkmoon Fare event
+-- Relocate Peacebloom gameobject (guid=26813) to avoid collision with Darkmoon Faire and remove it from game_event_gameobject (de-)spawns
 UPDATE `gameobject` SET `position_x` = -9570.9, `position_y` = 120.2, `position_z` = 59.594 WHERE `guid` = 26813;
-DELETE FROM `game_event_gameobject` WHERE `guid`=26813 AND `eventEntry`=-4;
-
+DELETE FROM `game_event_gameobject` WHERE `guid` = 26813 AND `eventEntry` = -4;
