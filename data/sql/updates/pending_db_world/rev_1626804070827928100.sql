fe901e3ede267297b9c478ee22bd42eaed667076
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-24 01:16:55 +0000

    chore(DB): import pending files
    Referenced commit(s): 9343cb513717f9df9a485fe9e5db76a0ba637542

--- data/sql/updates/pending_db_world/rev_1626804070827928100.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626804070827928100');
-
--- Changed the param of the shadow bolt so it will stop any spells being casted at the moment
-UPDATE `smart_scripts` SET `action_param2` = 1 WHERE (`entryorguid` = 2590) AND (`source_type` = 0) AND (`id` IN (2));
-
