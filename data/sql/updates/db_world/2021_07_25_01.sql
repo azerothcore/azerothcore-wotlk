b344fd7f6a1d79e4d43cbe28908a9c9be40edc5e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-25 08:24:19 +0000

    chore(DB): import pending files
    Referenced commit(s): 6a5a901a93594b4d0b19e0864ac4f84c32aef0f9

--- data/sql/updates/db_world/2021_07_25_01.sql
+++ data/sql/updates/db_world/2021_07_25_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_25_00 -> 2021_07_25_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_25_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_25_00 2021_07_25_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1626956389818224000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1626956389818224000');
 
 -- Set the Creature Shadowforge Commander a patrol route movement
@@ -76,3 +92,13 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 (1344550,3,-6349.87,-3065.44,310.83,0,0,0,0,100,0),
 (1344550,4,-6347.683,-3067.48,310.83,0,0,0,0,100,0);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_25_01' WHERE sql_rev = '1626956389818224000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
