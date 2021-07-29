18a8fdec368ffb962319ff551cf204e1bfce6f64
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-29 11:20:56 +0000

    chore(DB): import pending files
    Referenced commit(s): 1e36a644b2a4837fdec52fda4a77d9a6e97b292e

--- data/sql/updates/db_world/2021_07_29_03.sql
+++ data/sql/updates/db_world/2021_07_29_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_29_02 -> 2021_07_29_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_29_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_29_02 2021_07_29_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627113920593737013'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627113920593737013');
 
 -- Jordan Stilwell
@@ -28,3 +44,13 @@ INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `positi
 (6181,1,-5094.12,-786.116,495.226,""),
 (6181,2,-5089.28,-782.95,495.279,"");
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_29_03' WHERE sql_rev = '1627113920593737013';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
