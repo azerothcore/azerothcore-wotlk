8bd15f418a6e41856ee05d255f7f5c56bd17ce2b
Stefano Borzì <stefanoborzi32@gmail.com>
2021-07-29 10:16:38 +0200

    chore(Core/zone_teldrassil): remove file

--- src/server/scripts/Kalimdor/zone_teldrassil.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
- */
-
-//void AddSC_teldrassil()
-//{
-//}
