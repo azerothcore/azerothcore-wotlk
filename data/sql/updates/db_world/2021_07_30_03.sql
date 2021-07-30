df7230748d534839887d36e4f2b080118bc5d7d5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-30 17:56:02 +0000

    chore(DB): import pending files
    Referenced commit(s): 8f5100f358b65688a9b1ca8bbf3b3063b0cab552

--- data/sql/updates/db_world/2021_07_30_03.sql
+++ data/sql/updates/db_world/2021_07_30_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_30_02 -> 2021_07_30_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_30_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_30_02 2021_07_30_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627247672807799200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627247672807799200');
 
 -- Set the Creature Alshirr Banebreath a patrol route movement
@@ -40,3 +56,13 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 (518940,20,3866.84,-672.534,328.888,0,0,0,0,100,0),
 (518940,21,3866.84,-672.53,328.888,0,0,0,0,100,0);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_30_03' WHERE sql_rev = '1627247672807799200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
