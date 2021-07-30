c392c4b5777bfe0741f88d227428284ad1a941de
AzerothCoreBot <azerothcorebot@gmail.com>
2021-07-30 17:51:45 +0000

    chore(DB): import pending files
    Referenced commit(s): 2e8301b706db1a9c166ec817e4d0d4ecc970004b

--- data/sql/updates/db_world/2021_07_30_02.sql
+++ data/sql/updates/db_world/2021_07_30_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_07_30_01 -> 2021_07_30_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_07_30_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_07_30_01 2021_07_30_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627244221682670400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627244221682670400');
 
 -- Updated his movement from 1 to 2
@@ -29,3 +45,13 @@ UPDATE `waypoint_data` SET `position_z` = 135.192 WHERE `id` = 33020 AND `point`
 UPDATE `waypoint_data` SET `position_z` = 134.327 WHERE `id` = 33020 AND `point` = 27;
 UPDATE `waypoint_data` SET `position_z` = 135.192 WHERE `id` = 33020 AND `point` = 28;
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_07_30_02' WHERE sql_rev = '1627244221682670400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
