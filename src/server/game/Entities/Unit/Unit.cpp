7e8fcadc6f96b4f13b5b50a723bf340896b15b83
kik0 <kik0swd@gmail.com>
2021-07-31 01:54:34 -0700

    Fix Glyph of Freezing Trap

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7204,12 +7204,6 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                             victim->CastSpell(victim, 57894, true, nullptr, nullptr, GetGUID());
                             return true;
                         }
-                    // Glyph of Freezing Trap
-                    case 56845:
-                        {
-                            victim->CastSpell(this, 61394, true, nullptr, nullptr, victim->GetGUID());
-                            return true;
-                        }
                 }
                 break;
             }
