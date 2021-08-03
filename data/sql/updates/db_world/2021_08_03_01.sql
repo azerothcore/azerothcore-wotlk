543a8257c0cfe352589f01b51b1e230f04b6056f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-03 22:06:15 +0000

    chore(DB): import pending files
    Referenced commit(s): b8f6c2a14bb8e56166a88f0b5a04e04a741f1b62

--- data/sql/updates/db_world/2021_08_03_01.sql
+++ data/sql/updates/db_world/2021_08_03_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_03_00 -> 2021_08_03_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_03_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_03_00 2021_08_03_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627307634943513800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627307634943513800');
 
 -- Updated movement of Dart to patrol
@@ -25,3 +41,13 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 (2001450,6,-2694.37,-3460.22,34.09,0,0,0,0,100,0),
 (2001450,7,-2714.64,-3489.85,34.85,0,0,0,0,100,0);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_03_01' WHERE sql_rev = '1627307634943513800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
