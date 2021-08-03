075185e961bff08cbab213f2178c92ede9e4d21a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-03 22:14:05 +0000

    chore(DB): import pending files
    Referenced commit(s): d56f27edea52d5c6cc890c5ed09ed92436439724

--- data/sql/updates/pending_db_world/rev_1627304134519639700.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627304134519639700');
-
--- Changed the coords just a bit to assure that no dwarf spawns in the same place as other
-UPDATE `creature` SET `position_x` = -7183.29, `position_y` = -881.80, `position_z` = 163.93 WHERE (`id` = 24818) AND (`guid` = 4566);
-UPDATE `creature` SET `position_x` = -7339.15, `position_y` = -1020.25, `position_z` = 178.65  WHERE (`id` = 24818) AND (`guid` = 5727);
-
