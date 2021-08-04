39f00cd883a107a0c8c66b1fa0b23b2e7d6c08ad
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-04 12:55:29 +0000

    chore(DB): import pending files
    Referenced commit(s): 02f5ec4e6d656110c92e0eb8a9543a6d0dc013d0

--- data/sql/updates/pending_db_world/rev_1627383748783756100.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627383748783756100');
-
--- Changed the spawn point of Scarlet High Clerist to the tower south of hearthglen
-UPDATE `creature` SET  `position_x` = 2703.07, `position_y` = -1951.61, `position_z` = 107.23 WHERE (`id` = 1839) AND (`guid` = 49764);
-
