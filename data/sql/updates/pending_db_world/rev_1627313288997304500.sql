49cc3777e91e1456e9cc21dad471b6ced43015b8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-04 14:06:58 +0000

    chore(DB): import pending files
    Referenced commit(s): 16a57e57d996953e929df463bde3e3f69ffbbd0e

--- data/sql/updates/pending_db_world/rev_1627313288997304500.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627313288997304500');
-
-UPDATE `spell_dbc` SET `Attributes`=`Attributes`|0x20000000 WHERE `ID` IN (20252,20616,20617,25272,25275);
