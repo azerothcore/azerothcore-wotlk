6e1fe7b3c75cc1c9dcc1dbbb8fded60066d4338d
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-08-04 21:43:17 +0200

    Deleted aggresorai

--- data/sql/updates/pending_db_world/rev_1628101702822210800.sql
+++ data/sql/updates/pending_db_world/rev_1628101702822210800.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628101702822210800');
 
 -- Removed script where Mosh'Ogg Brute (1142) used Trash. Because it was the only entry it was changed to AggressorAI
-UPDATE `creature_template` SET `AIName` = 'AggressorAI' WHERE (`entry` = 1142);
+UPDATE `creature_template` SET `AIName` = '' WHERE (`entry` = 1142);
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 1142) AND (`source_type` = 0) AND (`id` IN (0));
 
