14b3655f3376af5763b5a12732f7bb655d8b67b8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-05 14:40:27 +0000

    chore(DB): import pending files
    Referenced commit(s): 8fe5a22cf228f35bfee56d1385fa8b16be35c6bd

--- data/sql/updates/pending_db_world/rev_1627581490592719000.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627581490592719000');
-
--- Moved the node outside of the tree
-UPDATE `gameobject` SET `position_x` = 6629.067 , `position_y` = -7382.726, `position_z` = 54.81 WHERE (`id` = 1621) AND (`guid` = 65024);
-
