8ca9da6421d1777f5f7ae911a385c7d99fe5d0e1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 17:43:23 +0000

    chore(DB): import pending files
    Referenced commit(s): 774abca7ca1c7422e9a66e448724199daed81fbd

--- data/sql/updates/pending_db_world/rev_1627818799536182900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627818799536182900');
-
--- Added movement to Mok'rash (1493)
-UPDATE `creature` SET  `MovementType` = 1, `wander_distance`= 5 WHERE (`id` = 1493) AND (`guid` IN (1672));
-
