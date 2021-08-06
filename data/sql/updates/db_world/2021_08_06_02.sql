42b3acb659eaf90754c993aaf9509465553bc4a2
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 09:52:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 19601c0e028576b26fac6b19e718783bc4e81bd2

--- data/sql/updates/db_world/2021_08_06_02.sql
+++ data/sql/updates/db_world/2021_08_06_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_06_01 -> 2021_08_06_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_06_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_06_01 2021_08_06_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627741035221642000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627741035221642000');
 
 -- Set the Creature Azurous a patrol route movement
@@ -28,3 +44,13 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 (518900,8,6431.722,-4518.271,737.40,0,0,0,0,100,0),
 (518900,9,6487.26,-4542.47,717.283,0,0,0,0,100,0);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_06_02' WHERE sql_rev = '1627741035221642000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
