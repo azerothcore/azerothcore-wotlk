8fd27908a1b096235a801f19331ea920c6ef777c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 14:32:47 +0000

    chore(DB): import pending files
    Referenced commit(s): be4377f5406339e77179c8353493b4469e461c7a

--- data/sql/updates/pending_db_world/rev_1627743560834389300.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627743560834389300');
-
-DELETE FROM `spell_script_names` WHERE `spell_id`=23134;
-INSERT INTO `spell_script_names` VALUES
-(23134, 'spell_item_goblin_bomb');
