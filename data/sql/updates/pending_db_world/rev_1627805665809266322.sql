ea76a0c2bb8f7fe7c577d3a19f13c3ded5e701d0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 15:41:52 +0000

    chore(DB): import pending files
    Referenced commit(s): dfe71ff211078534b88f17ad012f5c2c7cac9390

--- data/sql/updates/pending_db_world/rev_1627805665809266322.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627805665809266322');
-
--- Delete 'Recipe: Savory Deviate Delight' from RLT 24701
-DELETE FROM `reference_loot_template` WHERE `Entry` = 24701 AND `Item` = 6661;
-
