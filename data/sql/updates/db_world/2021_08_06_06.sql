cc02724200e92e8acf00e557487db5b5e1b0bb93
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 12:34:11 +0000

    chore(DB): import pending files
    Referenced commit(s): 4aed38d7879a2d7eeee8ad24b5be17163fe56e54

--- data/sql/updates/db_world/2021_08_06_06.sql
+++ data/sql/updates/db_world/2021_08_06_06.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_06_05 -> 2021_08_06_06
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_06_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_06_05 2021_08_06_06 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627733875042202000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627733875042202000');
 
 -- Added movement for Lord Maldazzar spawns
@@ -18,3 +34,13 @@ INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALU
 (52725, 368, 0, "Lord Maldazzar Spawn Crypt"),
 (152292, 368, 0, "Lord Maldazzar Spawn Inn Sorrow hill");
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_06_06' WHERE sql_rev = '1627733875042202000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
