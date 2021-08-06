cce76d1a18235009b6b08478a68b59e9f3fb6b34
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 10:58:21 +0000

    chore(DB): import pending files
    Referenced commit(s): 3facf2e113047b19df6ae4fec7093874857b38c4

--- data/sql/updates/pending_db_world/rev_1627727878498349900.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627727878498349900');
-UPDATE `quest_request_items` SET `CompletionText` = 'This is going to be a tough battle...' WHERE (`ID` = 3382);
