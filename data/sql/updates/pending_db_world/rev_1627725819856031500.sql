1cd3af96b37b4168369d85114c6afcc553993cf7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 09:15:28 +0000

    chore(DB): import pending files
    Referenced commit(s): 7618e7706edd949f0b2fc54535811cf07d49404f

--- data/sql/updates/pending_db_world/rev_1627725819856031500.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627725819856031500');
-
-DELETE FROM `spell_script_names` WHERE `scriptname`= "spell_mage_combustion_proc";
-INSERT INTO `spell_script_names` (`spell_id`,`scriptname`) VALUES (28682, "spell_mage_combustion_proc");
