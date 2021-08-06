57ac049e15f8a3d4e3b60d05ed37c11d961872a3
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 22:29:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 5831c336df5ea9013fefbc314dc74478bb575c84

--- data/sql/updates/pending_db_world/rev_1627403306832649400.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627403306832649400');
-
-DELETE FROM `spell_script_names` WHERE `spell_id`=-16257;
-INSERT INTO `spell_script_names` VALUES
-(-16257,'spell_sha_flurry_proc');
