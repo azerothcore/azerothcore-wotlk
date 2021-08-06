49f64ce2f23da21207b46d93d2078fbfaa229aeb
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-06 11:32:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 1dc96266f1e0901abfe764d1c4361e49354329f8

--- data/sql/updates/pending_db_world/rev_1627735957987999500.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627735957987999500');
-
--- Change the spawn points of Heavy War Golem (5854) so they spawn in the bridge
-UPDATE `creature` SET `position_x` = -6681.53, `position_y` = -1352.86, `position_z` = 210.77  WHERE (`id` = 5854) AND (`guid` = 5608);
-UPDATE `creature` SET `position_x` = -6663.55, `position_y` = -1317.16, `position_z` = 208.48  WHERE (`id` = 5854) AND (`guid` = 5687);
-
