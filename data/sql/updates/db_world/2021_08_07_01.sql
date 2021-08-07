418a3814fd1a9f03709d4ddde011026ce387218c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-07 12:19:09 +0000

    chore(DB): import pending files
    Referenced commit(s): 49fcccb25e9697b5cc4a1af2dd122e8b336f15a7

--- data/sql/updates/db_world/2021_08_07_01.sql
+++ data/sql/updates/db_world/2021_08_07_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_07_00 -> 2021_08_07_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_07_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_07_00 2021_08_07_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1627896958983750100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627896958983750100');
 
 -- Change the spawn to 1 second from 15 minutes
@@ -11,3 +27,13 @@ INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, 
 (100508, 177604, 0, 0, 0, 1, 1,-12705.1, -472.833, 30.2692, 3.14159, 0, 0, 0, 0, 0, 0, 0, '', 0), -- Ziata'jai Trophy Skulls
 (100509, 177604, 0, 0, 0, 1, 1,-12850.8, -819.736, 54.8824, 3.14159, 0, 0, 0, 0, 0, 0, 0, '', 0); -- Zul'Mamwe Trophy Skulls
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_07_01' WHERE sql_rev = '1627896958983750100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
