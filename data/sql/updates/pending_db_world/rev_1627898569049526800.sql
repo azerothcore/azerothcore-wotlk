a9de389b30b0b64db94b56e26d9a4ef76fa35ec8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-07 15:50:47 +0000

    chore(DB): import pending files
    Referenced commit(s): 9f62a9bca746f8228d04afa6719aa5369d63915b

--- data/sql/updates/pending_db_world/rev_1627898569049526800.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627898569049526800');
-
--- Spawn time changed to 38h, added movement to the spawn of Mongress (14344). Reduced her movement speed from 2.45 to 1
-UPDATE `creature_template` SET `MovementType` = 1, `speed_walk` = 1 WHERE (`entry` = 14344);
-UPDATE `creature` SET `spawntimesecs` = 136800, `wander_distance` = 10, `MovementType` = 1 WHERE (`id` = 14344) AND (`guid` IN (51895));
