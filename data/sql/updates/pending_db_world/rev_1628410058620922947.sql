d37f8391c83ed318a05e2dce3a6bf6a8e065ee1a
Azcobu <quux.gitazco@mailnull.com>
2021-08-08 17:39:16 +0930

    fix(DB/Creature): Reorient Dorin Songblade

---
+++ data/sql/updates/pending_db_world/rev_1628410058620922947.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628410058620922947');
+
+-- Reorient Dorin Songblade
+UPDATE `creature` SET `orientation` = 2.3 WHERE `id` = 956 AND `guid` = 6717;
+
