90c30a919918f299a2b07ba14258e1a18be5ea3f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-08 00:05:58 +0000

    chore(DB): import pending files
    Referenced commit(s): 80da8284c8043653888d541dc33a6cb0cee46b47

--- data/sql/updates/pending_db_world/rev_1627916502657250695.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1627916502657250695');
-
--- Moves Wildthorn Stalker spawn from inside tree object
-UPDATE `creature` SET `position_x` = 1591.68, `position_y` = -2539.28, `position_z` = 101.81  WHERE `id` = 3819 AND `guid` = 34832;
-
