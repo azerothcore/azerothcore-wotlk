cf55ab5ce30e9398cf2f55d59cb8086a341bddc8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-09 13:12:33 +0000

    chore(DB): import pending files
    Referenced commit(s): ab5aeeb536ad7897d2975edb6cf26c11d2819335

--- data/sql/updates/pending_db_world/rev_1628079878307473993.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628079878307473993');
-
--- Delete lvl 75 plans from Galak Windchaser
-DELETE FROM `creature_loot_template` WHERE `Entry` = 4096 AND `Item` IN (30302, 30322);
--- Deletes drop condition skill requirement
-DELETE FROM `conditions` WHERE `SourceGroup` = 4096 AND `SourceEntry` IN (30302, 30322);
