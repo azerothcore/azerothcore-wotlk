2ca265e96007234bf11f68e07d79f37924805c7e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-10 13:55:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 50ed776947a1846a5378f8a4b7c1c281a4857b06

--- data/sql/updates/pending_db_world/rev_1628170702879843880.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628170702879843880');
-
--- removes skinning loot from Ranger Jaela and Vile'rel
-UPDATE `creature_template` SET `skinloot` = 0 WHERE `entry` IN (15416, 9036);
-
