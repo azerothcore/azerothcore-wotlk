444383a585438cc68ae86e16713c1b3fa082aef3
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-11 08:36:38 +0000

    chore(DB): import pending files
    Referenced commit(s): 856a9d980c9381c25c57a994dd4b2612cc09bb87

--- data/sql/updates/pending_db_world/rev_1628410058620922947.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628410058620922947');
-
--- Reorient Dorin Songblade
-UPDATE `creature` SET `orientation` = 2.3 WHERE `id` = 956 AND `guid` = 6717;
-
