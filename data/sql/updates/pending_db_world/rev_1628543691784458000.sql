8ccf7c808e3a474f1323e1c2e718f8beba317438
Fewmade <iskakovartem2001@gmail.com>
2021-08-12 12:07:45 +0300

    Fix

--- data/sql/updates/pending_db_world/rev_1628543691784458000.sql
+++ data/sql/updates/pending_db_world/rev_1628543691784458000.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628543691784458000');
 
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 42325) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 4351) AND (`ConditionValue3` = 0);
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 42325) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 4352) AND (`ConditionValue3` = 0);
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 42325) AND (`SourceId` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 4351) AND (`ConditionValue3` = 0);
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 42325) AND (`SourceId` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 4352) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(17, 0, 42325, 0, 0, 31, 1, 3, 4351, 0, 0, 0, 0, '', 'Casting a spell only on a Bloodfen Raptor '),
-(17, 0, 42325, 0, 0, 31, 1, 3, 4352, 0, 0, 0, 0, '', 'Casting a spell only on a Bloodfen Screecher');
+(17, 0, 42325, 0, 1, 31, 1, 3, 4351, 0, 0, 0, 0, '', 'Casting a spell only on a Bloodfen Raptor '),
+(17, 0, 42325, 0, 2, 31, 1, 3, 4352, 0, 0, 0, 0, '', 'Casting a spell only on a Bloodfen Screecher');
