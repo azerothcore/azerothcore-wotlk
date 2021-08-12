2d77fe9274005a99f8445b8dac9dd0fcd3faf3c5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-12 17:11:54 +0000

    chore(DB): import pending files
    Referenced commit(s): a98ac8c8532debb7293f1241b368b8993d14610e

--- data/sql/updates/pending_db_world/rev_1628521621840404043.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628521621840404043');
-
--- Delete skinning table 100008 from lvl 1 Serpentbloom Snake
-UPDATE `creature_template` SET `skinloot` = 0 WHERE `entry` = 3680;
-
