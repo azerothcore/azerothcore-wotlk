6ff9b21b736c5bbf506a1ffa439caadb42c5dbdf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-13 15:49:13 +0000

    chore(DB): import pending files
    Referenced commit(s): 2ef25c63c63fb65a466c4b85d63d1f150d90e1f3

--- data/sql/updates/db_world/2021_08_13_01.sql
+++ data/sql/updates/db_world/2021_08_13_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_13_00 -> 2021_08_13_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_13_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_13_00 2021_08_13_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1628606203210970248'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628606203210970248');
 
 -- Add motion to Dunemaul Ogres
@@ -15,3 +31,13 @@ UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 547
 -- Add motion to Dunemaul Warlocks
 UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 10 WHERE `id` = 5475 AND `guid` IN (23197, 23209, 23213);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_13_01' WHERE sql_rev = '1628606203210970248';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
