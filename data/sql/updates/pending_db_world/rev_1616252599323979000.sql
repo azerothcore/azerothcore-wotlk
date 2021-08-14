52620889e44e0d30f921a5fe4d0887c61b6b536f
Knindza <knindzagxg@gmail.com>
2021-08-14 11:23:01 +0200

    Update rev_1616252599323979000.sql

--- data/sql/updates/pending_db_world/rev_1616252599323979000.sql
+++ data/sql/updates/pending_db_world/rev_1616252599323979000.sql
@@ -61,3 +61,8 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 18449;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 18449);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (18449, 0, 0, 0, 11, 0, 100, 1, 0, 0, 0, 0, 11, 29651, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Shienor Talonite - On Respawn - Cast \'29651\' (No Repeat)');
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 22265;
+DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 22265);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(17128, 0, 0, 0, 0, 0, 100, 0, 2500, 5000, 20000, 25000, 11, 30285, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Shadowwing Owl - In Combat - Cast \'30285\'');
