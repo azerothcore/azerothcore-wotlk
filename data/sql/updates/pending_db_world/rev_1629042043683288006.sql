dab12986e8e9d85be9e269bce57512ecba295fe8
JamesGent <61194190+JamesGent@users.noreply.github.com>
2021-08-15 16:49:33 +0100

    Add files via upload

---
+++ data/sql/updates/pending_db_world/rev_1629042043683288006.sql
@@ -0,0 +1,2 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629042043683288006');
+UPDATE `quest_template` SET `RewardFactionValue1` = 625, `RewardFactionOverride1` = 0 WHERE (`ID` = 7737);
