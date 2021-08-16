390c6bc27738cff06fc5619006ca55fb452019b6
lineagedr <lordlo0kk@gmail.com>
2021-08-16 21:13:04 +0300

    [DB/Item]: Fix Wolfshead Helm not giving Energy when going into cat form

---
+++ data/sql/updates/pending_db_world/rev_1629137505579314700.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629137505579314700');
+
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES ('17770','29940','0','Wolfshead Helm Energy');
