c7ee70ea75ff4d6729d8cfc0cbb4ac4d01ac67ee
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-16 15:48:48 +0000

    chore(DB): import pending files
    Referenced commit(s): 001096503bbc596445a6615250122e005c69e214

--- data/sql/updates/pending_db_world/rev_1628777209086733059.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628777209086733059');
-
--- Make Aku'mai Servant cast Frostbolt Volley on victims and not itself
-UPDATE `smart_scripts` SET `target_type` = 2 WHERE `source_type` = 0 AND `id` = 0 AND `entryorguid` = 4978;
-
