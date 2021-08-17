c88e93fb5079ddf17fccec75c2c96acc8d95ac62
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-17 22:49:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 6c8d76be30c9fd15d50168ad5d5b1a84a72d9838

--- data/sql/updates/pending_db_world/rev_1628955615714597262.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628955615714597262');
-
--- Shift Silvermane Stalker spawn slightly to avoid tree
-UPDATE `creature` SET `position_x` = -108.08, `position_y` = -3529.83, `position_z` = 118.49  WHERE `id` = 2926 AND `guid` = 93058;
-
