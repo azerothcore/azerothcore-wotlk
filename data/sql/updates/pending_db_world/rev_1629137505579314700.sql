d0ebde6c84b098f1ab6305518f8372eb517b77d8
lineagedr <lordlo0kk@gmail.com>
2021-08-17 22:24:12 +0300

    Update rev_1629137505579314700.sql

--- data/sql/updates/pending_db_world/rev_1629137505579314700.sql
+++ data/sql/updates/pending_db_world/rev_1629137505579314700.sql
@@ -1,3 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629137505579314700');
 
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 17770 AND `spell_effect` = 29940;
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (17770,29940,0,'Wolfshead Helm Energy');
