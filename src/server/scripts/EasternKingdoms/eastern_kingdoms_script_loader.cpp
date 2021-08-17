1918b31a67174ca798665247c613f9659baffab2
Ouizzy <25827787+wizzymore@users.noreply.github.com>
2021-08-18 01:34:03 +0300

    Fix Gizrul

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -33,7 +33,6 @@ void AddSC_boss_pyroguard_emberseer();
 void AddSC_boss_gyth();
 void AddSC_boss_rend_blackhand();
 void AddSC_boss_urok_doomhowl();
-void AddSC_boss_gizrul_the_slavener();
 void AddSC_instance_blackrock_spire();
 void AddSC_boss_razorgore();                 //Blackwing lair
 void AddSC_boss_vaelastrasz();
@@ -170,7 +169,6 @@ void AddEasternKingdomsScripts()
     AddSC_boss_gyth();
     AddSC_boss_rend_blackhand();
     AddSC_boss_urok_doomhowl();
-    AddSC_boss_gizrul_the_slavener();
     AddSC_instance_blackrock_spire();
     AddSC_boss_razorgore();                 //Blackwing lair
     AddSC_boss_vaelastrasz();
