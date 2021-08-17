f16f8798292df65220c6f83ac114086cc55c3455
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-08-18 08:14:08 +0930

    fix(DB/creature): Add movement to 2 NPC spawns in Azshara (#7336)
    * fix(DB/creature): Add movement to 2 NPC spawns in Azshara
    
    * Comment typo

---
+++ data/sql/updates/pending_db_world/rev_1628938293777910785.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628938293777910785');
+
+-- Add movement to Draconic Mageweaver
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `guid` = 36427 AND `id` = 6131;
+-- Add movement to Makrinni Razorclaw
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 10 WHERE `guid` = 36013 AND `id` = 6350;
+
