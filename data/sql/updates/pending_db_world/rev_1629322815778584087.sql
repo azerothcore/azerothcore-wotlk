e1d51319f4a43d07b1851717298f83eb50fc1fa1
jestermaniac <88903576+jestermaniac@users.noreply.github.com>
2021-08-18 23:42:33 +0200

    fix(DB/Gameobject): fixed position of ores in bfd

---
+++ data/sql/updates/pending_db_world/rev_1629322815778584087.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629322815778584087');
+
+UPDATE gameobject SET position_x = 4217.04, position_y = 906.04, position_z = -8.08 WHERE GUID = 48120;
+UPDATE gameobject SET position_x = 4222.18, position_y = 914.03, position_z = -7.44 WHERE GUID = 63507;
