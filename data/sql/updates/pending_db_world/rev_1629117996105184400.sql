a43d76ede4d13a904c7416d1bda698b8dc1d0fe8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-18 10:01:24 +0000

    chore(DB): import pending files
    Referenced commit(s): 211f0756f412ff24bc9bdb84bc77495fa925c995

--- data/sql/updates/pending_db_world/rev_1629117996105184400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629117996105184400');
-
-DELETE FROM `quest_poi_points` WHERE `QuestID`=2518 AND `Idx1`=0 AND `Idx2`=0;
-INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (2518, 0, 0, 10979, 1366, 0);
