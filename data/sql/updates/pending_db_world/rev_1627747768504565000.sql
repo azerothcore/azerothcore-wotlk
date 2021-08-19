3d02f96c56f33b664e1c917a733b6d6486a50d77
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-08-19 10:16:30 +0200

    Fix typo

--- data/sql/updates/pending_db_world/rev_1627747768504565000.sql
+++ data/sql/updates/pending_db_world/rev_1627747768504565000.sql
@@ -12,7 +12,7 @@ UPDATE `creature_template` SET `unit_class`= '2' WHERE `entry` IN (14533,14538);
 UPDATE `creature_template` SET `unit_class`= '1' WHERE `entry` IN (14530,14535,14534);
 
 -- The Cleaner
-UPDATE `creature_template` SET `speed_run` = 2.6,` Mechanic_Immune_Mask` = 650870623, `MovementType` = 0 WHERE entry = 14503;
+UPDATE `creature_template` SET `speed_run` = 2.6, `Mechanic_Immune_Mask` = 650870623, `MovementType` = 0 WHERE entry = 14503;
 
 -- Creeping Doom
 UPDATE `creature_template` SET `speed_run` = 0.33, `speed_walk` = 0.33 WHERE entry = 14761;
