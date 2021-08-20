7b343d0088a74a051640faee9b2a0926f9d023e0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-20 22:44:34 +0000

    chore(DB): import pending files
    Referenced commit(s): 4cc00f548890d1e6c2ecd2abebfc48fafd14eda9

--- data/sql/updates/db_world/2021_08_20_04.sql
+++ data/sql/updates/db_world/2021_08_20_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_20_03 -> 2021_08_20_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_20_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_20_03 2021_08_20_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1629208512724054400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629208512724054400');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE (`entry` = 9736);
@@ -9,3 +25,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (9736, 0, 2, 3, 9, 0, 100, 0, 10, 30, 2000, 2000, 0, 11, 16496, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Quartermaster Zigris - Within 10-30 Range - Cast \'Shoot\''),
 (9736, 0, 3, 0, 61, 0, 100, 0, 10, 30, 2000, 2000, 0, 21, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Quartermaster Zigris - Within 10-30 Range - Disable Combat Movement'),
 (9736, 0, 4, 0, 9, 0, 100, 0, 0, 10, 300, 300, 0, 21, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Quartermaster Zigris - Within 0-10 Range - Enable Combat Movement');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_20_04' WHERE sql_rev = '1629208512724054400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
