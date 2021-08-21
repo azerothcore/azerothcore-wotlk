e8d80503300a765b9d6052649509541a17c80479
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-21 22:57:40 +0000

    chore(DB): import pending files
    Referenced commit(s): 26cc8c4f274c8a52251aa4c282c424dd33ccf476

--- data/sql/updates/pending_db_world/rev_1629217819213526612.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629217819213526612');
-
--- Slightly changes position_z so that the mob doesn't fall through the floor on respawn
-DELETE FROM `creature` WHERE (`id` = 7448) AND (`guid` IN (41253));
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(41253, 7448, 1, 0, 0, 1, 1, 10810, 0, 6034.82, -5179.02, 867.5, 1.72181, 333, 15, 0, 3529, 0, 1, 0, 0, 0, '', 0);
