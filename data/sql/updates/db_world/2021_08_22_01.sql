0818e44e675ccef324e9da5a0bc5dba7f82f6327
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-22 11:45:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 690187cb3f48cc6ea52db2132c0d4c676012558c

--- data/sql/updates/db_world/2021_08_22_01.sql
+++ data/sql/updates/db_world/2021_08_22_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_22_00 -> 2021_08_22_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_22_00 2021_08_22_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1629238721796812800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629238721796812800');
 
 -- Set the waypoint to the NPC
@@ -11,3 +27,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (10268, 0, 0, 0, 0, 0, 100, 0, 17000, 20000, 8000, 10000, 0, 11, 16495, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Gizrul the Slavener - In Combat - Cast \'Fatal Bite\''),
 (10268, 0, 1, 0, 0, 0, 100, 0, 10000, 12000, 8000, 10000, 0, 11, 16128, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Gizrul the Slavener - In Combat - Cast \'Infected Bite\''),
 (10268, 0, 2, 0, 2, 0, 100, 1, 0, 40, 300, 300, 0, 11, 8269, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Gizrul the Slavener - Between 0-40% Health - Cast \'Frenzy\' (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_22_01' WHERE sql_rev = '1629238721796812800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
