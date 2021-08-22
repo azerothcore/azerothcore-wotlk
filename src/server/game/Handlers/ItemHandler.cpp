d4b035a416b7d626b70ebe0d2a4409a37b29da80
sanctum32 <andriuspel@gmail.com>
2021-08-22 22:39:46 +0300

    fix(Core/Vendors): Fixed incorrect check which allowed/disabled alliance only items in vendors

--- src/server/game/Handlers/ItemHandler.cpp
+++ src/server/game/Handlers/ItemHandler.cpp
@@ -924,7 +924,7 @@ void WorldSession::SendListInventory(ObjectGuid vendorGuid, uint32 vendorEntry)
                     continue;
                 // Only display items in vendor lists for the team the
                 // player is on. If GM on, display all items.
-                if (!_player->IsGameMaster() && ((itemTemplate->Flags2 & ITEM_FLAGS_EXTRA_HORDE_ONLY && _player->GetTeamId() == TEAM_ALLIANCE) || (itemTemplate->Flags2 == ITEM_FLAGS_EXTRA_ALLIANCE_ONLY && _player->GetTeamId() == TEAM_HORDE)))
+                if (!_player->IsGameMaster() && ((itemTemplate->Flags2 & ITEM_FLAGS_EXTRA_HORDE_ONLY && _player->GetTeamId() == TEAM_ALLIANCE) || (itemTemplate->Flags2 & ITEM_FLAGS_EXTRA_ALLIANCE_ONLY && _player->GetTeamId() == TEAM_HORDE)))
                     continue;
 
                 // Items sold out are not displayed in list
