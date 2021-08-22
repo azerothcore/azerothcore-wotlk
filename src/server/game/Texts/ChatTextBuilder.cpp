788f92aecb3afd4eb7d5881428302c76d5b67539
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-08-22 18:32:29 +0200

    Update ChatTextBuilder.cpp

--- src/server/game/Texts/ChatTextBuilder.cpp
+++ src/server/game/Texts/ChatTextBuilder.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license, you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license, you may redistribute it and/or modify it under version 3 of the License, or (at your option), any later version.
  * Copyright (C) 2021+ Trinitycore <https://TrinityCore.org/>
  */
 
