b5f00bd54c3a91582700680ff650bc027ed1d843
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-08-23 15:35:52 +0200

    Changed the timing of the shadopw channeling
    Changed from 100 ms min and 1 sec max to 15 seconds min and max so she cast is less often

--- data/sql/updates/pending_db_world/rev_1629721384880200437.sql
+++ data/sql/updates/pending_db_world/rev_1629721384880200437.sql
@@ -20,6 +20,6 @@ INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALU
 -- Change the spell for one more suited for her and added script so che can roam and channle it in a loop
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 2783) AND (`source_type` = 0) AND (`id` IN (0, 2));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(2783, 0, 0, 0, 1, 0, 100, 0, 100, 1000, 20000, 25000, 0, 11, 43897, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marez Cowl - Out of Combat - Cast \'Shadow Channeling\''),
+(2783, 0, 0, 0, 1, 0, 100, 0, 15000, 15000, 20000, 25000, 0, 11, 43897, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marez Cowl - Out of Combat - Cast \'Shadow Channeling\''),
 (2783, 0, 2, 0, 1, 0, 100, 0, 5000, 10000, 10000, 15000, 0, 92, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marez Cowl - Out of Combat - Interrupt Spell');
 
