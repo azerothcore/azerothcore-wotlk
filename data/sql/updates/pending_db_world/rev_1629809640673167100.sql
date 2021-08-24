534be32929ee7652c8489b081c1fd2cee227e1ba
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-08-24 15:23:38 +0200

    fix(DB/Quest): restricts quests 9425 & 9189 to only blood elves

--- data/sql/updates/pending_db_world/rev_1629809640673167100.sql
+++ data/sql/updates/pending_db_world/rev_1629809640673167100.sql
@@ -2,3 +2,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629809640673167100');
 
 # Restrict quest Report to Splintertree Post(ID 9428) to only allow blood elfs
 UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` = 9428);
+# Restrict quest Delivery to the Sepulcher(ID 9189) to only allow blood elfs
+UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` = 9189);
+# Restrict quest Report to Tarren Mill(ID 9425) to only allow blood elfs
+UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` = 9425);
