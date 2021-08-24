014f76c60ad4a5bf7aff34160d651c3e1b59b396
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-08-24 15:30:00 +0200

    Update rev_1629809640673167100.sql

--- data/sql/updates/pending_db_world/rev_1629809640673167100.sql
+++ data/sql/updates/pending_db_world/rev_1629809640673167100.sql
@@ -1,8 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629809640673167100');
 
-# Restrict quest Report to Splintertree Post(ID 9428) to only allow blood elfs
-UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` = 9428);
-# Restrict quest Delivery to the Sepulcher(ID 9189) to only allow blood elfs
-UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` = 9189);
-# Restrict quest Report to Tarren Mill(ID 9425) to only allow blood elfs
-UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` = 9425);
+# Restrict quest Report to Splintertree Post(ID 9428), Delivery to the Sepulcher(ID 9189) and Report to Tarren Mill(ID 9425) to only allow blood elfs
+UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE `ID` IN (9189, 9425, 9428);
