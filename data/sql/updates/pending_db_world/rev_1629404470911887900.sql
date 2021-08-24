c28d6ee992377bab0b76ae679fd92d751764e5cd
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-24 23:15:23 +0000

    chore(DB): import pending files
    Referenced commit(s): 940c30a17b3cb41f5418ca153cf1f53763accb88

--- data/sql/updates/pending_db_world/rev_1629404470911887900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629404470911887900');
-
-DELETE FROM `spell_script_names` WHERE `spell_id`=-42243;
-INSERT INTO `spell_script_names` VALUES
-(-42243,'spell_hun_volley_trigger');
