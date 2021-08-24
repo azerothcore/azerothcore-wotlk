f503a124188f820b769fd6e1c597288f2740caad
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-24 14:56:03 +0000

    chore(DB): import pending files
    Referenced commit(s): b4da3a021bd9e14c876715d6d8378336b3d351e6

--- data/sql/updates/pending_db_world/rev_1629293337424377430.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629293337424377430');
-
-UPDATE `creature_template` SET `skinloot` = 0 WHERE `entry` = 9198;
-DELETE FROM `skinning_loot_template` WHERE `entry` = 9198;
-
