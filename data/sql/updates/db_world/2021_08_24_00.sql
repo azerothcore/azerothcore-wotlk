92acf6feb88a68e24d08ec0b0f57ce54e2e279e0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-24 09:38:01 +0000

    chore(DB): import pending files
    Referenced commit(s): 1e50a9027bba8085bc52c7a157b54cd3a7146e04

--- data/sql/updates/db_world/2021_08_24_00.sql
+++ data/sql/updates/db_world/2021_08_24_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_23_01 -> 2021_08_24_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_23_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_23_01 2021_08_24_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1629290838385347720'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629290838385347720');
 
 -- Delete Chimaera Leather
@@ -54,3 +70,13 @@ INSERT INTO `skinning_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (11497, 4304, 0, 5.413, 0, 1, 0, 1, 1, 'The Razza - Thick Leather'),
 (11497, 8170, 0, 82.823, 0, 1, 0, 1, 1, 'The Razza - Rugged Leather'),
 (11497, 8171, 0, 11.764, 0, 1, 0, 1, 1, 'The Razza - Rugged Hide');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_24_00' WHERE sql_rev = '1629290838385347720';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
