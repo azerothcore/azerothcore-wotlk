c286537c85e6c6663162afd453fcb252280a96a1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-25 14:03:20 +0000

    chore(DB): import pending files
    Referenced commit(s): e18c21947da06156e77457370f2ea8b8a713c950

--- data/sql/updates/pending_db_world/rev_1629391445774422568.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629391445774422568');
-
--- Change the respawntime of the Ammo Crate to 15 s
-UPDATE `gameobject` SET `spawntimesecs` = 15 WHERE (`id` = 176785) AND (`guid` IN (10663));
