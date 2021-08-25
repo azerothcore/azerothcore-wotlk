42750494e54d40343b12b1c58fd03dae2efd3c1d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-25 08:23:48 +0000

    chore(DB): import pending files
    Referenced commit(s): 2b9817f0c0e971e815e97403a3dd8cbe32ae93e8

--- data/sql/updates/pending_db_world/rev_1629485842027723200.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629485842027723200');
-
-ALTER TABLE `playercreateinfo_item`
-    CHANGE `amount` `amount` INT SIGNED NOT NULL DEFAULT 1;
-
-DELETE FROM `playercreateinfo_item` WHERE `itemid` = 40582;
-INSERT INTO `playercreateinfo_item` (`race`, `class`, `itemid`, `amount`, `Note`) VALUES (0, 6, 40582, -1, "[TDB PH] - unsused Scourgestone");
