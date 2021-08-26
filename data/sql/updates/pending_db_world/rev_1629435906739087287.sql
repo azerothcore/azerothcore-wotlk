9854a1fb91c3e7a5b843103e5a0c0ad55961017e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-26 12:32:48 +0000

    chore(DB): import pending files
    Referenced commit(s): 907171bd3ddc30e04515b650d59e764f1fec01cb

--- data/sql/updates/pending_db_world/rev_1629435906739087287.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629435906739087287');
-
--- Add movement to Withered Green Keepers
-UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 15637 AND `guid` IN (55527, 55528, 55529, 55532, 55533, 55534, 55535, 55569, 55570);
-
--- Shift one spawn slightly to avoid overlapping
-UPDATE `creature` SET `position_x` = 8258, `position_y` = -6136.1, `position_z` = 34.2 WHERE `id` = 15637 AND `guid` = 55534;
-
