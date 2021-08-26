2d3d52b32d6e0b21e79941e5c64423e8e49404be
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-08-26 20:30:21 +0200

    Stardarized and fixed typos

--- data/sql/updates/pending_db_world/rev_1629919057423789312.sql
+++ data/sql/updates/pending_db_world/rev_1629919057423789312.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629919057423789312');
 
--- Change the race to blood elf for this quests on Sunstrider isle
-UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE (`ID` IN (8325, 9393, 9676, 8328, 8563, 8564, 9392, 8326, 8327, 8334, 8335, 8347, 8338, 8336, 8330, 8345, 0069, 10070, 10071, 10072, 10073));
+-- Change the race to blood elf for this quests on Sunstrider isle (Flags: Orc 2, Undead 16, Tauren 32, Troll 128)
+UPDATE `quest_template` SET `AllowableRaces` = `AllowableRaces`&~(2|16|32|128) WHERE (`ID` IN (8325, 9393, 9676, 8328, 8563, 8564, 9392, 8326, 8327, 8334, 8335, 8347, 8338, 8336, 8330, 8345, 10068, 10069, 10070, 10071, 10072, 10073));
 
