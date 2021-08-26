0afc189d4d3739e6afbacce13fe31278adcb826a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-26 00:17:02 +0000

    chore(DB): import pending files
    Referenced commit(s): 16c5b1bdcb6b95adf58bb80544e1c7e2da4f2876

--- data/sql/updates/pending_db_world/rev_1629418018641557053.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629418018641557053');
-
-# Update Scarlet Friar preventing evade / floor falling
-UPDATE `creature` SET `position_z` = 81 WHERE (`id` = 1538) AND (`guid` IN (44770));
