197de3ffc32250847ce6e5ce6540339665cc2744
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-26 16:14:42 +0000

    chore(DB): import pending files
    Referenced commit(s): 96addc671c811e0f5e50d184c932bb67965633a8

--- data/sql/updates/db_world/2021_08_26_06.sql
+++ data/sql/updates/db_world/2021_08_26_06.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_26_05 -> 2021_08_26_06
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_26_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_26_05 2021_08_26_06 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1629721384880200437'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629721384880200437');
 
 -- Add new spawn and add roaming around
@@ -23,3 +39,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (2783, 0, 0, 0, 1, 0, 100, 0, 15000, 15000, 20000, 25000, 0, 11, 43897, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marez Cowl - Out of Combat - Cast \'Shadow Channeling\''),
 (2783, 0, 2, 0, 1, 0, 100, 0, 5000, 10000, 10000, 15000, 0, 92, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marez Cowl - Out of Combat - Interrupt Spell');
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_26_06' WHERE sql_rev = '1629721384880200437';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
