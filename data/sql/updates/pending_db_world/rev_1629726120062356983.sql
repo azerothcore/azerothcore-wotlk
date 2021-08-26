d4154b9747bdf7a89c8b5404b52c3b528b8ac204
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-26 23:17:52 +0000

    chore(DB): import pending files
    Referenced commit(s): 93466e58599ac9b58432f4f723d250c0f4ecb859

--- data/sql/updates/pending_db_world/rev_1629726120062356983.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629726120062356983');
-
--- adds movement to a few Mummified Headhunters
-UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 16342 AND `guid` IN (82050, 82052, 82053, 82054, 82055, 82056, 82048, 82084);
