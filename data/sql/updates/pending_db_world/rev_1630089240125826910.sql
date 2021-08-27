245829406db476d9760b27f972f81e3887aa44bc
syssneck <68868567+syssneck@users.noreply.github.com>
2021-08-27 20:51:15 +0200

    Delete rev_1630089240125826910.sql

--- data/sql/updates/pending_db_world/rev_1630089240125826910.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630089240125826910');
-
--- Decreases the respawntime for Garrick Padfoot to 90 s
-UPDATE `creature` SET `spawntimesecs` = 90 WHERE `id` = 103 AND `guid` IN (80247);
