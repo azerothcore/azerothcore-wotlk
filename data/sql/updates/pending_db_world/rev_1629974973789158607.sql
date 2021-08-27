4e6540d2c1e27bd3876c3f777ceb3e0558715cf6
raizo <35413558+iraizo@users.noreply.github.com>
2021-08-27 09:11:22 +0200

    Update rev_1629974973789158607.sql

--- data/sql/updates/pending_db_world/rev_1629974973789158607.sql
+++ data/sql/updates/pending_db_world/rev_1629974973789158607.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629974973789158607');
 
-UPDATE `quest_template_addon` SET `RequiredMaxRepValue` = 6000 WHERE (`ID` = 13662);
+UPDATE `quest_template_addon` SET `RequiredMaxRepValue` = 6000, `RequiredMaxRepFaction` = 59 WHERE (`ID` = 13662);
