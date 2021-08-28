5d1544215e1408708e7b116aea0f4b8f9bf4fb6c
AV <44707108+zxcnxc@users.noreply.github.com>
2021-08-28 13:06:16 +0700

    updated
    rewrite SQL

--- data/sql/updates/pending_db_world/rev_1630085433143335851.sql
+++ data/sql/updates/pending_db_world/rev_1630085433143335851.sql
@@ -1,33 +1,20 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630085433143335851');
 
--- Add lootid for Ur'dan
+SET @ITEM := 13140;
 
-DELETE FROM `creature_template` WHERE (`entry` = 14522);
-INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(14522, 0, 0, 0, 0, 0, 14564, 0, 0, 0, 'Ur\'dan', NULL, NULL, 0, 54, 54, 0, 1434, 129, 1, 1.14286, 1, 0, 0, 1, 2000, 2000, 1, 1, 2, 32768, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 14522, 0, 0, 0, 0, 100, 136, '', 0, 3, 1, 1.3, 1, 1, 0, 0, 1, 0, 0, 0, '', 12340);
-
--- Change drop rate
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 7118) AND (`Item` IN (13140));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(7118, 13140, 0, 5.6, 0, 1, 0, 1, 1, 'Jaedenar Darkweaver - Blood Red Key');
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 7120) AND (`Item` IN (13140));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(7120, 13140, 0, 5.7, 0, 1, 0, 1, 1, 'Jaedenar Warlock - Blood Red Key');
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 7114) AND (`Item` IN (13140));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(7114, 13140, 0, 6.2, 0, 1, 0, 1, 1, 'Jaedenar Enforcer - Blood Red Key');
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 14523) AND (`Item` IN (13140));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(14523, 13140, 0, 4, 0, 1, 0, 1, 1, 'Ulathek - Blood Red Key');
- 
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 14522) AND (`Item` IN (13140));
+-- Add Ur'dan to loot table
+UPDATE `creature_template` SET `lootid`= 14522 WHERE `entry` = 14522;
+-- Ur'dan drop rate
+DELETE FROM `creature_loot_template` WHERE `Entry` = 14522 AND `Item`= 13140;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (14522, 13140, 0, 3.7, 0, 1, 0, 1, 1, 'Ur\'dan - Blood Red Key');
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 9862) AND (`Item` IN (13140));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(9862, 13140, 0, 3.1, 0, 1, 0, 1, 1, 'Jaedenar Legionnaire - Blood Red Key');
+-- Jaednar Darkweaver drop rate
+UPDATE `creature_loot_template` SET `Chance`= 5.6 WHERE `Entry`=7118  AND `Item`=@ITEM;
+-- Jaednar Enforcer drop rate
+UPDATE `creature_loot_template` SET `Chance`= 5.7 WHERE `Entry`=7120  AND `Item`=@ITEM;
+--  Jaednar Warlock drop rate
+UPDATE `creature_loot_template` SET `Chance`= 6.2 WHERE `Entry`=7114  AND `Item`=@ITEM;
+-- Ulathek drop rate
+UPDATE `creature_loot_template` SET `Chance`= 4 WHERE `Entry`=14523 AND `Item`=@ITEM;
+-- Jaedenar Legionaire drop rate
+UPDATE `creature_loot_template` SET `Chance`=3.1 WHERE `Entry`=9862  AND `Item`=@ITEM;
