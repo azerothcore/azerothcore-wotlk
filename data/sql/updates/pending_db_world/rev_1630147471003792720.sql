ee739907a50cd262affabeac0f317b8b115f2e06
AV <44707108+zxcnxc@users.noreply.github.com>
2021-08-28 17:59:20 +0700

    fix(DB/quest_template): Correct objective for the Blackfathom Villainy Horde

---
+++ data/sql/updates/pending_db_world/rev_1630147471003792720.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630147471003792720');
+
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Bashana Runetotem at Elder Rise in Thunder Bluff.' WHERE `ID` = 6561;
