46e684dc3b35a87b7891c35e730a0541ec860e61
AV <44707108+zxcnxc@users.noreply.github.com>
2021-08-28 16:51:43 +0700

    final sql
    little fix for the comment

--- data/sql/updates/pending_db_world/rev_1630085433143335851.sql
+++ data/sql/updates/pending_db_world/rev_1630085433143335851.sql
@@ -10,9 +10,9 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (14522, 13140, 0, 3.7, 0, 1, 0, 1, 1, 'Ur\'dan - Blood Red Key');
 -- Jaednar Darkweaver drop rate
 UPDATE `creature_loot_template` SET `Chance`= 5.6 WHERE `Entry`=7118  AND `Item`=@ITEM;
--- Jaednar Enforcer drop rate
+-- Jaednar Warlock drop rate
 UPDATE `creature_loot_template` SET `Chance`= 5.7 WHERE `Entry`=7120  AND `Item`=@ITEM;
---  Jaednar Warlock drop rate
+-- Jaednar Enforcer drop rate
 UPDATE `creature_loot_template` SET `Chance`= 6.2 WHERE `Entry`=7114  AND `Item`=@ITEM;
 -- Ulathek drop rate
 UPDATE `creature_loot_template` SET `Chance`= 4 WHERE `Entry`=14523 AND `Item`=@ITEM;
