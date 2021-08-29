292e15d023dc63decadbb6cc28a6e172321a6dbc
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-29 08:47:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 6978d624826407eab041f3264c5297a51a2ef3b6

--- data/sql/updates/pending_db_world/rev_1629233438552386300.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629233438552386300');
-
-UPDATE `command` SET `help` = 'Syntax: .modify mount #id #speed\nSet CreatureDisplayID as #id and set speed to #speed value between 0.1 - 50.0' WHERE `name` = 'modify mount';
