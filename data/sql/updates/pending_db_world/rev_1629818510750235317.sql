10983b118d599a934b2167f06b87636313fc2e1b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-30 17:08:31 +0000

    chore(DB): import pending files
    Referenced commit(s): 3466e82c3b9269ccbc4ad09de017afd3bc369632

--- data/sql/updates/pending_db_world/rev_1629818510750235317.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629818510750235317');
-
--- Removed Manual: Heavy Silk Bandage and Manual: Mageweave Bandage from Deneb Walker (2805)
-DELETE FROM `npc_vendor` WHERE (`entry` = 2805) AND (`item` IN (16112, 16113));
-
--- Removed Manual: Heavy Silk Bandage and Manual: Mageweave Bandage from Balai Lok'Wein (13476)
-DELETE FROM `npc_vendor` WHERE (`entry` = 13476) AND (`item` IN (16112, 16113));
-
