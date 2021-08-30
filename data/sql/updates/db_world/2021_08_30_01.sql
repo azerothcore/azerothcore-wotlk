98f0c0bf35794f118497d169671117761d7694ee
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-30 09:08:47 +0000

    chore(DB): import pending files
    Referenced commit(s): 259012469dd3f6a1a9365fd4258b79fd606bff3d

--- data/sql/updates/db_world/2021_08_30_01.sql
+++ data/sql/updates/db_world/2021_08_30_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_08_30_00 -> 2021_08_30_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_08_30_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_08_30_00 2021_08_30_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1629793641237819618'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629793641237819618');
 
 -- Set the Creature Swiftmane (5831) a patrol route movement
@@ -51,3 +67,13 @@ DELETE FROM `smart_scripts` WHERE (`entryorguid` = 5831) AND (`source_type` = 0)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (5831, 0, 1, 0, 1, 0, 100, 0, 0, 0, 0, 0, 0, 53, 1, 5831, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Swiftmane - Out of Combat - Start Waypoint');
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_08_30_01' WHERE sql_rev = '1629793641237819618';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
