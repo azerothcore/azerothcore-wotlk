eddf9e6909abfd2fec709d1f1335c14e04cb84f3
AzerothCoreBot <azerothcorebot@gmail.com>
2021-08-30 12:51:54 +0000

    chore(DB): import pending files
    Referenced commit(s): e36b4395c97212d19237bd67868975b6ad7e493c

--- data/sql/updates/pending_db_world/rev_1629809640673167100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629809640673167100');
-
-# Restrict quest Report to Splintertree Post(ID 9428), Delivery to the Sepulcher(ID 9189) and Report to Tarren Mill(ID 9425) to only allow blood elfs
-UPDATE `quest_template` SET `AllowableRaces` = 512 WHERE `ID` IN (9189, 9425, 9428);
