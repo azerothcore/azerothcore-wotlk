1a4dd43695c093609e3f202d4b9a14d0d499d995
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-08-30 16:21:58 +0200

    Removed the civilian flags

--- data/sql/updates/pending_db_world/rev_1630318500363217970.sql
+++ data/sql/updates/pending_db_world/rev_1630318500363217970.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630318500363217970');
 
 -- Set Webwood spiders(1986) and Githyiss the Vile(1994) neutral to the player instead of aggresive
-UPDATE `creature_template` SET `flags_extra` = 2, `faction` = 7 WHERE (`entry` IN (1986, 1994));
+UPDATE `creature_template` SET `faction` = 7 WHERE (`entry` IN (1986, 1994));
 
