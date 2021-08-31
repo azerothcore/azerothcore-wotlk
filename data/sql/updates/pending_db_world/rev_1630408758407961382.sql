c36dbeed85ca7ef8462d1ec5bd87578946b42b64
mpfans <mpfans@vip.qq.com>
2021-08-31 23:33:57 +0800

    Update data/sql/updates/pending_db_world/rev_1630408758407961382.sql
    Co-authored-by: Francesco Borzì <borzifrancesco@gmail.com>

--- data/sql/updates/pending_db_world/rev_1630408758407961382.sql
+++ data/sql/updates/pending_db_world/rev_1630408758407961382.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630408758407961382');
 
-UPDATE `creature_template` SET `type_flags` = '34816' WHERE `entry` = '32358';
+UPDATE `creature_template` SET `type_flags` = 34816 WHERE `entry` = 32358;
