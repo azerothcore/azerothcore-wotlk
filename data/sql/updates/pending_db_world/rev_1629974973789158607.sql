aa1c744584d8077e009c10c9d08f0d411dd52f83
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-01 10:56:09 +0000

    chore(DB): import pending files
    Referenced commit(s): 37c594941425c9fb16fa999cd3d973509e68f472

--- data/sql/updates/pending_db_world/rev_1629974973789158607.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629974973789158607');
-
-UPDATE `quest_template_addon` SET `RequiredMaxRepValue` = 9000, `RequiredMaxRepFaction` = 59 WHERE (`ID` = 13662);
