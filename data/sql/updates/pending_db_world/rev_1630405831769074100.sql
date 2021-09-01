d36b054f2ae256a41659c8f4189dca13d9bc68f4
Efymer <loscojones999@gmail.com>
2021-09-01 10:54:21 +0200

    Update rev_1630405831769074100.sql

--- data/sql/updates/pending_db_world/rev_1630405831769074100.sql
+++ data/sql/updates/pending_db_world/rev_1630405831769074100.sql
@@ -2,3 +2,6 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630405831769074100');
 
 -- Corrected faction for Southshore Guard
 UPDATE `creature_template` SET `faction` = 11 WHERE (`entry` = 2386);
+
+-- Corrected faction for Shadowy Assassins
+UPDATE `creature_template` SET `faction` = 97 WHERE (`entry` = 2434);
