de7686f930310b07f7c0ea22f7f8d27c7a6f2d29
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-01 12:24:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 1faca44120cc9f376fa8001f26e37a01e7e773d7

--- data/sql/updates/pending_db_world/rev_1629983088640886216.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629983088640886216');
-
--- Added roaming movement to Risen Creeper
-UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1 WHERE (`id` = 16300) AND (`guid` IN (82122, 82123, 82124, 82130, 82713));
--- Added roaming movement to Dreadbone Skeleton
-UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1 WHERE (`id` = 16303) AND (`guid` IN (82125, 82127, 82131, 82450, 82822, 82823, 82826, 82827, 82841, 82864, 82899, 82932));
--- Added roaming movement to Deathcage Scryer
-UPDATE `creature` SET `wander_distance` = 5, `MovementType` = 1 WHERE (`id` = 16307) AND (`guid` IN (82126, 82129));
-
