499d0800085c2291adab21639aec1194ce62fc03
Tr1v1an <89851300+Tr1v1an@users.noreply.github.com>
2021-09-01 10:52:30 +0200

    Updating change after comments
    Updating the PR based on comments from Azcobu.

--- data/sql/updates/pending_db_world/rev_1630423020825292612.sql
+++ data/sql/updates/pending_db_world/rev_1630423020825292612.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630423020825292612');
 
-UPDATE `acore_world`.`creature` SET `spawntimesecs`='37800' WHERE `guid`=36519;
+UPDATE `creature` SET `spawntimesecs` = 37800 WHERE `guid` = 36519 and `id`= 6651;
