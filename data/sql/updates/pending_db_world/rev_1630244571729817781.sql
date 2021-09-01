d059788aaafd7c310441436b346911559a5d2f93
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-01 17:47:52 +0000

    chore(DB): import pending files
    Referenced commit(s): 475fa3eaa251a3518838d4f6324ad05cac54987d

--- data/sql/updates/pending_db_world/rev_1630244571729817781.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630244571729817781');
-
---  Anubisath Defender, Anubisath Sentinel, Obsidian Eradicator, Qiraji Lasher, Vekniss Hive Crawler, Vekniss Soldier, Vekniss Stinger, Vekniss Warrior, Vekniss Wasp kill rep to 100
-UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 100 WHERE `creature_id` IN (15277, 15264, 15262, 15249, 15240, 15229, 15235, 15230, 15236);
