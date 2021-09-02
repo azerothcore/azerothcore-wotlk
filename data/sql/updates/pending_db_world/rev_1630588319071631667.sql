337e12cdc7456b56bc33747dc76182d35c84d049
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-09-02 15:41:11 +0200

    Changed spell fade 6408 for sneak 22766

--- data/sql/updates/pending_db_world/rev_1630588319071631667.sql
+++ data/sql/updates/pending_db_world/rev_1630588319071631667.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630588319071631667');
 
 -- Add Slowing poison and fade by aura
-UPDATE `creature_template_addon` SET `auras` = '6408 8601' WHERE (`entry` = 7110);
+UPDATE `creature_template_addon` SET `auras` = '22766 8601' WHERE (`entry` = 7110);
 
 -- Remove the casting of the slowing posion and sneak from smartAi as they now works on auras
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 7110) AND (`source_type` = 0) AND (`id` IN (0, 2));
