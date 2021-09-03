4d4c902cdad2d7decda91a869feaa28f4a74addf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-03 22:02:44 +0000

    chore(DB): import pending files
    Referenced commit(s): 767f2ab5ec499a5e2d2a2ee5940c782f9195b2fb

--- data/sql/updates/db_world/2021_09_03_05.sql
+++ data/sql/updates/db_world/2021_09_03_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_03_04 -> 2021_09_03_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_03_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_03_04 2021_09_03_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1630437845134253537'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630437845134253537');
 
 -- Set 5 rep until honored for Desert Rumbler, Dust Stormer, Greater Obsidian Elemental, Cyclone Warrior
@@ -26,3 +42,13 @@ UPDATE `creature_onkill_reputation` SET `MaxStanding1` = 7, `RewOnKillRepValue1`
 
 -- Set 200 rep until end exalted for Ragnaros
 UPDATE `creature_onkill_reputation` SET `MaxStanding1` = 7, `RewOnKillRepValue1` = 200 WHERE (`creature_id` = 11502);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_03_05' WHERE sql_rev = '1630437845134253537';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
