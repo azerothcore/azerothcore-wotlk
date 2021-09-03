aca8e5697177ba510f55d5a00f45d4e468fbebcb
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-03 17:54:32 +0000

    chore(DB): import pending files
    Referenced commit(s): 0763188e62eb52a646c86f2b0fe3ac542c5de4aa

--- data/sql/updates/pending_db_world/rev_1630690395841002600.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630690395841002600');
-
--- Fix merge of #7707
--- Adds movements for Stonewind Trackers
-UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 16316 AND `guid` IN (82666, 82668);
-
