fd4b515657473923f9d3d2501f232d25db92a722
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-03 13:38:47 +0000

    chore(DB): import pending files
    Referenced commit(s): 392319ab79ae772339281352c2d0d12cd5bd4602

--- data/sql/updates/pending_db_world/rev_1630263728286533056.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630263728286533056');
-
--- insert quest "Return to Krog" after "The Black Shield (1276)" 
-UPDATE `quest_template_addon` SET `NextQuestID` = 11204 WHERE (`ID` = 1276);
