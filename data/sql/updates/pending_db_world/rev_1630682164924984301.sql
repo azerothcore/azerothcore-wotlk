3826573821610f69843d4832402ffa7e8ca651be
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-09-03 18:21:45 +0200

    Added to the pool table

--- data/sql/updates/pending_db_world/rev_1630682164924984301.sql
+++ data/sql/updates/pending_db_world/rev_1630682164924984301.sql
@@ -1,5 +1,15 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630682164924984301');
 
---Removed one of the spawns of Razormaw Matriach's Nest because its on the same place as the guid 6245
+-- Removed one of the spawns of Razormaw Matriach's Nest because its on the same place as the guid 6245
 DELETE FROM `gameobject` WHERE (`id` = 202083) AND (`guid` IN (14999));
 
+-- Added the spawns to the same pool so only 1 can be spawned at the same time
+DELETE FROM `pool_template` WHERE `entry` = 374;
+INSERT INTO `pool_template` (`entry`, `max_limit`, `description`)  VALUES (374, 1, 'Razormaw Matriach''s Nest spawns');
+
+DELETE FROM `pool_gameobject` WHERE `guid` IN (6245, 6246, 6247, 6248);
+INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VALUES 
+(6245, 374, 0, 'Razormaw Matriach''s Nest spawn 1'),
+(6246, 374, 0, 'Razormaw Matriach''s Nest spawn 2'),
+(6247, 374, 0, 'Razormaw Matriach''s Nest spawn 3'),
+(6248, 374, 0, 'Razormaw Matriach''s Nest spawns 4');
