2f510298d832dd0f08e569930cb814b8b5f2e150
Si1ker <Si1ker@users.noreply.github.com>
2021-09-03 11:46:54 -0600

    lol

--- data/sql/updates/pending_db_world/rev_1630690395841002600.sql
+++ data/sql/updates/pending_db_world/rev_1630690395841002600.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630690395841002600');
 
 -- Fix merge of #7707
--- Set exclamation mark on Quest Items - Elegant Letters
-UPDATE `item_template` SET `bonding` = 1 WHERE (`entry` = 17126);
+-- Adds movements for Stonewind Trackers
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `id` = 16316 AND `guid` IN (82666, 82668);
 
