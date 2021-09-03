8a501fa6455378104cba09f340a3ff19ec56c301
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-09-03 11:40:43 +0200

    Update rev_1629826555036082700.sql

--- data/sql/updates/pending_db_world/rev_1629826555036082700.sql
+++ data/sql/updates/pending_db_world/rev_1629826555036082700.sql
@@ -1,3 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1629826555036082700');
 
 ALTER TABLE `creature_loot_template` CHANGE `Reference` `Reference` MEDIUMINT DEFAULT 0 NOT NULL;
+ALTER TABLE `prospecting_loot_template` CHANGE `Reference` `Reference` MEDIUMINT DEFAULT 0 NOT NULL;
