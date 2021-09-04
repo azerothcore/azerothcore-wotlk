5c7d07ff82e804cde63cf6d7e1a065cc4265b4a7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-04 09:41:00 +0000

    chore(DB): import pending files
    Referenced commit(s): d8b3df09d9b54efb7f76fc9e9b46bfca45b27eae

--- data/sql/updates/db_world/2021_09_04_00.sql
+++ data/sql/updates/db_world/2021_09_04_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_03_05 -> 2021_09_04_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_03_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_03_05 2021_09_04_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1630456715748683765'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630456715748683765');
 
 -- add `Rusty Prison Key`(id 43650) Chinese translation
@@ -15,3 +31,13 @@ INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `Veri
 -- add `Key to the Focusing Iris`(id 44582) Chinese translation
 DELETE from `item_template_locale` where `ID` = 44582 AND `locale` = 'zhCN';
 INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES ('44582', 'zhCN', '聚焦之虹的钥匙', '', '15050'); 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_04_00' WHERE sql_rev = '1630456715748683765';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
