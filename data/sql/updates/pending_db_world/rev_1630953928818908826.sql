f62012c954f65acd89d4a36d437231c997963757
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-09-06 20:46:22 +0200

    fix(DB/Creature): Fixed Mist Howl

---
+++ data/sql/updates/pending_db_world/rev_1630953928818908826.sql
@@ -0,0 +1,21 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630953928818908826');
+
+-- Added 2 new spawns and changed the coords according to sniff
+DELETE FROM `creature` WHERE (`id` = 10644);
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(32884, 10644, 1, 331, 0, 1, 1, 165, 0, 3528.78, 628.189, 9.04502, 5.20659, 19990, 32.707, 0, 573, 0, 1, 0, 0, 0, '', 0),
+(51885, 10644, 1, 331, 0, 1, 1, 0, 0, 3330.55, 397.033, 3.7174, 0.0612236, 9900, 1.9268, 0, 573, 0, 1, 0, 0, 0, '', 0),
+(945802, 10644, 1, 331, 0, 1, 1, 0, 0, 3823.67, 52.756, 10.97, 5.73397, 120, 5, 0, 1, 0, 1, 0, 0, 0, '', 0),
+(945803, 10644, 1, 331, 0, 1, 1, 0, 0, 3498.51, 416.076, -0.199577, 0.926322, 120, 7.68181, 0, 1, 0, 1, 0, 0, 0, '', 0);
+
+-- Add the new spawns to the same spawn pool so he can only be spawned once at a time
+DELETE FROM `pool_template` WHERE `entry` = 375;
+INSERT INTO `pool_template` (`entry`, `max_limit`, `description`)  VALUES (375, 1, "Mist Howler Spawns");
+
+DELETE FROM `pool_creature` WHERE `guid` IN (32884, 51885, 945802, 945803);
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES 
+(32884, 375, 0, "Mist Howler Spawn 1"),
+(51885, 375, 0, "Mist Howler Spawn 2"),
+(945802, 375, 0, "Mist Howler Spawn 3"),
+(945803, 375, 0, "Mist Howler Spawn 4");
+
