a97e73e5925aabed06dc3b599e46e01ea319240f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-07 15:57:09 +0000

    chore(DB): import pending files
    Referenced commit(s): 8f47a6da212189fe0c53851cd2920f6ac28ba8c3

--- data/sql/updates/db_world/2021_09_07_11.sql
+++ data/sql/updates/db_world/2021_09_07_11.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_07_10 -> 2021_09_07_11
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_07_10';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_07_10 2021_09_07_11 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1630832577462717951'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630832577462717951');
 
 -- Set 10 rep until end of exalted for Vile Scarab, Silicate Feeder, Qiraji (Swarmguard, Warrior), Swarmguard Needler, Scarab (Shrieker, Spitting), Vyral the Vile, Templar (Azure Hoary, Earthen).
@@ -17,3 +33,13 @@ UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue2` = -600 WHERE `creat
 
 -- Set -10 rep until end of exalted for Vekniss (Soldier, Warrior, Stinger, Wasp, Hive Crawler), Qiraji Lasher, Obsidian Eradicator, Anubisath (Sentinel, Defender).
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue2` = -10 WHERE `creature_id` IN (15229, 15230, 15235, 15236, 15240, 15249, 15262, 15264, 15277);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_07_11' WHERE sql_rev = '1630832577462717951';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
