2a6bc1dec0e8a029f3316b00ac3669e84f422c92
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-07 19:51:47 +0000

    chore(DB): import pending files
    Referenced commit(s): 77f118951a6a42e715824463e4cd378883a3d86c

--- data/sql/updates/pending_db_world/rev_1630927565231100667.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630927565231100667');
-
--- Remove Frost Oil from Vilebranch Witch Doctor, Tar Creeper, and Deadwood Gardener
-DELETE FROM `creature_loot_template` WHERE `Entry` IN (2640, 6527, 7154) AND `Item` = 3829;
-
