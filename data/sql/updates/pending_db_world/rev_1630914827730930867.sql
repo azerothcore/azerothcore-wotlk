ce97155cc28d063cfa8e5d539e27a720e46fb2c9
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2021-09-09 08:46:15 +0430

    Update rev_1630914827730930867.sql

--- data/sql/updates/pending_db_world/rev_1630914827730930867.sql
+++ data/sql/updates/pending_db_world/rev_1630914827730930867.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630914827730930867');
 
-UPDATE `spell_dbc` SET `RangeIndex` = '5' WHERE `ID` = 61698;
+UPDATE `spell_dbc` SET `RangeIndex` = 5 WHERE `ID` = 61698;
