1510752f64b488b69ffd1d15ffc52f1fcc8781d1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-09 14:20:13 +0000

    chore(DB): import pending files
    Referenced commit(s): 06a99d6c654215323ead9546938cf35c7571dd3a

--- data/sql/updates/pending_db_world/rev_1631038913388302500.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631038913388302500');
-
-DELETE FROM `creature_loot_template` WHERE `Entry`=193 AND `Item`=34535;
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
-(193, 34535, 0, 0.1, 0, 1, 0, 1, 1, 'Blue Dragonspawn - Azure Whelpling');
-
