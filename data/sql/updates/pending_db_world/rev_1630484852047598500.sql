10f82f48b16a1c677792cba43e358ae2c10cb424
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-09 09:51:03 +0000

    chore(DB): import pending files
    Referenced commit(s): 3de6ee6c8b0b1af7b78af2c9f26ce6e103be3d68

--- data/sql/updates/pending_db_world/rev_1630484852047598500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630484852047598500');
-
-SET @THORIUM_ORE := 10620; 
-UPDATE `prospecting_loot_template` SET `GroupId`=1 WHERE `entry` = @THORIUM_ORE;
-UPDATE `prospecting_loot_template` SET `Reference`=-13001 WHERE `entry` = @THORIUM_ORE AND `item`=1;
