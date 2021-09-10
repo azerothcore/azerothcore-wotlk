b461b46fb5418c1dfef943c5bd5d63e9513f178d
JamesGent <61194190+JamesGent@users.noreply.github.com>
2021-09-10 10:52:58 +0100

    Add files via upload

---
+++ data/sql/updates/pending_db_world/rev_1631267482542863767.sql
@@ -0,0 +1,14 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631267482542863767');
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5874);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5876);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5878);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5879);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5883);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5899);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5901);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5902);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5904);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5905);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5906);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5907);
+UPDATE `creature` SET `Time` = 60 WHERE (`guid` = 5911);
