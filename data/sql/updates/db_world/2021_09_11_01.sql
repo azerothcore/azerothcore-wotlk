1465a659ecd899a22a7b1c0f9c377626c9c8584e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-11 15:35:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 9305216015c4aa584e3141f627bf5ec720b8e329

--- data/sql/updates/db_world/2021_09_11_01.sql
+++ data/sql/updates/db_world/2021_09_11_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_11_00 -> 2021_09_11_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_11_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_11_00 2021_09_11_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631031703375947799'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631031703375947799');
 
 -- Adds Emperor Dagran Thaurissan to a formation as leader of the whole throne, so when pulled all will come to the aid
@@ -58,3 +74,13 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 (47613, 90619, 300, 0, 1, 0, 0),
 (47613, 90620, 300, 0, 1, 0, 0),
 (47613, 90621, 300, 0, 1, 0, 0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_11_01' WHERE sql_rev = '1631031703375947799';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
