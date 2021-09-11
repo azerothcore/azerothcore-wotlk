e5b8dbeb2936dcf35f52e2cbd65fc26b54310b52
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-09-11 11:02:38 +0200

    fix(Core/Achievements): new achievement criteria type ACHIEVEMENT_CRITERIA_DATA_TYPE_BG_TEAMS_SCORES (#7756)
    - Closes #7613

---
+++ data/sql/updates/pending_db_world/rev_1630947176293030300.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1630947176293030300');
+
+UPDATE `achievement_criteria_data` SET `type`=24, `value1`=1600 WHERE `criteria_id` IN (611,1235,1238);
+UPDATE `achievement_criteria_data` SET `type`=24, `value1`=3 WHERE `criteria_id`=1236;
