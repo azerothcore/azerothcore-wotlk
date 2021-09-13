177cc213f256ad889ee58c3d1384fe5bcc9a5d0e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-13 14:08:34 +0000

    chore(DB): import pending files
    Referenced commit(s): bb343b482eca7e76f998e83da4c19c9fa64ae473

--- data/sql/updates/pending_db_world/rev_1631088531210049294.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631088531210049294');
-
-UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`&~(33554432) WHERE `entry` = 3917;
--- data/sql/updates/pending_db_world/rev_1631169209646445982.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631169209646445982');
-
--- Removed the skinning loot of Rock Worm (NPC 11788), Rock Borer (NPC 11787) and Vile Larva (NPC 12218)
-UPDATE `creature_template` SET `skinloot` = 0 WHERE (`entry` IN (11788, 11787, 12218));
-
