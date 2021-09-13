6ea2fc18ab16502e70c160d5cd3b6bcdc1c3eb2f
jestermaniac <88903576+jestermaniac@users.noreply.github.com>
2021-09-13 20:44:56 +0200

    Update rev_1631557649018772603.sql

--- data/sql/updates/pending_db_world/rev_1631557649018772603.sql
+++ data/sql/updates/pending_db_world/rev_1631557649018772603.sql
@@ -2,3 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631557649018772603');
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = 23134;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (23134, 'spell_item_poweful_anti_venom');
+
