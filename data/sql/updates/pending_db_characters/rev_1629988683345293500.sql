bd956b5a5750014bd4168dd82df3438dcc5df43f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-13 18:59:16 +0000

    chore(DB): import pending files
    Referenced commit(s): 7406a01ac303ef1ee68b698b9c4ebf2c6b1666a2

--- data/sql/updates/pending_db_characters/rev_1629988683345293500.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1629988683345293500');
-
-ALTER TABLE `character_spell_cooldown` ADD COLUMN `category` MEDIUMINT UNSIGNED DEFAULT 0 NOT NULL AFTER `spell`;
-ALTER TABLE `pet_spell_cooldown` ADD COLUMN `category` MEDIUMINT UNSIGNED DEFAULT 0 NOT NULL AFTER `spell`;
