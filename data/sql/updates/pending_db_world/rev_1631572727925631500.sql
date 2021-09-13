d46d51974bf489c481df60a729310f778046b62b
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-09-14 00:40:51 +0200

    Update rev_1631572727925631500.sql

--- data/sql/updates/pending_db_world/rev_1631572727925631500.sql
+++ data/sql/updates/pending_db_world/rev_1631572727925631500.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631572727925631500');
 
-UPDATE `game_tele` SET `name` = "CrusadersOutpostScarletMonastery" WHERE `id` = 230;
+UPDATE `game_tele` SET `name` = 'CrusadersOutpostScarletMonastery' WHERE `id` = 230;
