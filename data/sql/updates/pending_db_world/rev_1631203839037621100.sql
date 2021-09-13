f45cc859ca55860b0bbc9045a255f17d5b077fe6
UltraNix <ultranix@gmail.com>
2021-09-13 19:00:00 +0200

    Update.

--- data/sql/updates/pending_db_world/rev_1631203839037621100.sql
+++ data/sql/updates/pending_db_world/rev_1631203839037621100.sql
@@ -17,6 +17,7 @@ DELETE FROM `gossip_menu_option` WHERE `MenuID`=1470 AND `OptionID`=0;
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
 (1470, 0, 0, 'Yes.  Give me the spell to summon the avatar.', 4567, 1, 1, 0, 0, 0, 0, NULL, 0, 0);
 
+UPDATE `creature_template` SET `AiName`='SmartAI' WHERE `entry`=8579;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=8579 AND `source_type`=0 AND `id` IN(3,4);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (8579, 0, 3, 4, 62, 0, 100, 0, 1470, 0, 0, 0, 0, 11, 12998, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, "Yeh'kinya - On Gossip Option 0 Selected - Cast 'Create Yeh'kinya's Scroll'"),
