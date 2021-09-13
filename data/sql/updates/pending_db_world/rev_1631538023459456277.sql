ed4961ee0e540a4d1866c73a78cd6056cc9aba93
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-09-13 16:07:44 +0200

    Update rev_1631538023459456277.sql

--- data/sql/updates/pending_db_world/rev_1631538023459456277.sql
+++ data/sql/updates/pending_db_world/rev_1631538023459456277.sql
@@ -2,3 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631538023459456277');
 
 -- Removed the cast of Thrash for Tamra Stormpike
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 14275) AND (`source_type` = 0);
+
