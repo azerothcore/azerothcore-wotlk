6630a97e43ab7e3be93f2c4ca5a99da3ec8fbd92
Cold-Martijn <90683398+Cold-Martijn@users.noreply.github.com>
2021-09-14 12:48:20 +0200

    fix(DB/Creature): dead scar mobs wandering
    https://github.com/azerothcore/azerothcore-wotlk/issues/6585

---
+++ data/sql/updates/pending_db_world/rev_1631616272860535120.sql
@@ -0,0 +1,12 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631616272860535120');
+
+UPDATE creature SET wander_distance = 5 WHERE guid = 56058;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56059;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56086;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56127;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56160;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56162;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56125;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56123;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56166;
+UPDATE creature SET wander_distance = 5 WHERE guid = 56150;
