f768b7cd8fa1ec358f1e9e54ff60c1154ccfaafa
jestermaniac <88903576+jestermaniac@users.noreply.github.com>
2021-09-14 19:02:24 +0200

    Update data/sql/updates/pending_db_world/rev_1631557649018772603.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1631557649018772603.sql
+++ data/sql/updates/pending_db_world/rev_1631557649018772603.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631557649018772603');
 
-DELETE FROM `spell_script_names` WHERE `spell_id` = 23786;
+DELETE FROM `spell_script_names` WHERE `spell_id` = 23786 AND `ScriptName` = 'spell_item_poweful_anti_venom';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (23786, 'spell_item_poweful_anti_venom');
 
