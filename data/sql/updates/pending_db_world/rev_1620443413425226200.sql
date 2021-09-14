a4a0976d5e9ffcf1a52ad4478c161875ee27a41d
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-09-15 01:38:10 +0200

    fix(Core/Spell): Remove ErrorCube visual (#5713)
    * fix(Core/Spell): Remove ErrorCube visual
    
    * cherry-pick https://github.com/trinitycore/trinitycore/commit/3b5014fdae
    
    * cherry-pick small part of https://github.com/TrinityCore/TrinityCore/commit/f4f7e6324d6c99335bf3479f212edea1e5572f88
    
    Co-Authored-By: Shauren <shauren.trinity@gmail.com>
    Co-Authored-By: Treeston <14020072+treeston@users.noreply.github.com>
    
    * brackets
    
    * Update rev_1620443413425226200.sql
    
    * Update rev_1620443413425226200.sql
    
    * It was the S, AN S
    
    * Update SpellMgr.cpp
    
    * Update SpellMgr.cpp
    
    * Revert "Merge branch 'master' into fix-visual"
    
    This reverts commit f0b629a2835c2a2d9aa5696bb7539c1ae9b14d57, reversing
    changes made to c06f4465d15e9f71426d796e2fcfe714592c3443.
    
    * Revert "Revert "Merge branch 'master' into fix-visual""
    
    This reverts commit bfdf75bd6f8f06944463249de5d398a9e77c3a79.
    
    * fix build
    
    Co-authored-by: Shauren <shauren.trinity@gmail.com>
    Co-authored-by: Treeston <14020072+treeston@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1620443413425226200.sql
@@ -0,0 +1,38 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620443413425226200');
+
+DROP TABLE IF EXISTS `spellvisual_dbc`; 
+CREATE TABLE IF NOT EXISTS `spellvisual_dbc` (
+  `ID` INT NOT NULL DEFAULT 0,
+  `PrecastKit` INT NOT NULL DEFAULT 0,
+  `CastKit` INT NOT NULL DEFAULT 0,
+  `ImpactKit` INT NOT NULL DEFAULT 0,
+  `StateKit` INT NOT NULL DEFAULT 0,
+  `StateDoneKit` INT NOT NULL DEFAULT 0,
+  `ChannelKit` INT NOT NULL DEFAULT 0,
+  `HasMissile` INT NOT NULL DEFAULT 0,
+  `MissileModel` INT NOT NULL DEFAULT 0,
+  `MissilePathType` INT NOT NULL DEFAULT 0,
+  `MissileDestinationAttachment` INT NOT NULL DEFAULT 0,
+  `MissileSound` INT NOT NULL DEFAULT 0,
+  `AnimEventSoundID` INT NOT NULL DEFAULT 0,
+  `Flags` INT NOT NULL DEFAULT 0,
+  `CasterImpactKit` INT NOT NULL DEFAULT 0,
+  `TargetImpactKit` INT NOT NULL DEFAULT 0,
+  `MissileAttachment` INT NOT NULL DEFAULT 0,
+  `MissileFollowGroundHeight` INT NOT NULL DEFAULT 0,
+  `MissileFollowGroundDropSpeed` INT NOT NULL DEFAULT 0,
+  `MissileFollowGroundApproach` INT NOT NULL DEFAULT 0,
+  `MissileFollowGroundFlags` INT NOT NULL DEFAULT 0,
+  `MissileMotion` INT NOT NULL DEFAULT 0,
+  `MissileTargetingKit` INT NOT NULL DEFAULT 0,
+  `InstantAreaKit` INT NOT NULL DEFAULT 0,
+  `ImpactAreaKit` INT NOT NULL DEFAULT 0,
+  `PersistentAreaKit` INT NOT NULL DEFAULT 0,
+  `MissileCastOffsetX` FLOAT NOT NULL DEFAULT 0,
+  `MissileCastOffsetY` FLOAT NOT NULL DEFAULT 0,
+  `MissileCastOffsetZ` FLOAT NOT NULL DEFAULT 0,
+  `MissileImpactOffsetX` FLOAT NOT NULL DEFAULT 0,
+  `MissileImpactOffsetY` FLOAT NOT NULL DEFAULT 0,
+  `MissileImpactOffsetZ` FLOAT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_general_ci;
