84c66d61918fc61346d4fd325115b3b4626bb7ee
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-09-14 15:08:10 +0200

    fix(DB/Quest): Fix undead starting quests allowed races

---
+++ data/sql/updates/pending_db_world/rev_1631624817554390205.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631624817554390205');
+
+-- Flags: Orc 2, Undead 16, Tauren 32, Troll 128, Blood Elf 512
+
+-- The Mindless Ones and Rattling the Rattlecages are now Horde only from undead only
+UPDATE `quest_template` SET `AllowableRaces` = `AllowableRaces`|2|16|32|128|512 WHERE (`ID` IN (364, 3901));
+
+-- Simple scroll, Encrypted Scroll, Hallowed Scroll, Glyphic Scroll and Tainted Scroll are now undead only from Horde only
+UPDATE `quest_template` SET `AllowableRaces` = `AllowableRaces`&~(2|16|32|128|512) WHERE (`ID` IN (3095, 3096, 3097, 3098, 3099));
+
