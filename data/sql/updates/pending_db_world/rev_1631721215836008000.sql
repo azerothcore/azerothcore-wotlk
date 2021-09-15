a258ab743bbbb101d43fb781c7f849fcdc68fcb3
Si1ker <Si1ker@users.noreply.github.com>
2021-09-15 14:34:18 -0600

    update

--- data/sql/updates/pending_db_world/rev_1631721215836008000.sql
+++ data/sql/updates/pending_db_world/rev_1631721215836008000.sql
@@ -1,9 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631721215836008000');
 
--- Add UNIT_FLAG_NON_ATTACKABLE and UNIT_FLAG_NOT_SELECTABLE to Ironaya
+-- Add UNIT_FLAG_NON_ATTACKABLE (2) and UNIT_FLAG_NOT_SELECTABLE (33554432) to Ironaya
 UPDATE `creature_template` SET `unit_flags`=`unit_flags`|2|33554432 WHERE `entry` = 7228;
 
--- Text & remov unit flags from Ironaya based on gob "keystone" activation
+-- Text & remove unit flags from Ironaya based on gob "keystone" activation
 DELETE FROM `smart_scripts` WHERE `entryorguid`=124371 AND `source_type`=1 AND `id` IN (4,5,6);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (124371, 1, 4, 0, 59, 0, 100, 0, 1, 0, 0, 0, 0, 19, 33554434, 0, 0, 0, 0, 0, 11, 7228, 100, 1, 0, 0, 0, 0, 0, 'Keystone - On Gossip Hello - Remove unit flags of Ironaya'),
