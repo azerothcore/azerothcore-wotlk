9fb1d155f987e4be7e56eab657bd59924994668b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-16 16:56:12 +0000

    chore(DB): import pending files
    Referenced commit(s): 5a92947e03f0d63cf2e3b9440e4d44ca1f9b87a3

--- data/sql/updates/db_world/2021_09_16_04.sql
+++ data/sql/updates/db_world/2021_09_16_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_16_03 -> 2021_09_16_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_16_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_16_03 2021_09_16_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631721215836008000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631721215836008000');
 
 -- Add UNIT_FLAG_NON_ATTACKABLE (2) and UNIT_FLAG_NOT_SELECTABLE (33554432) to Ironaya
@@ -16,3 +32,13 @@ UPDATE `smart_scripts` SET `id`=0 WHERE `entryorguid`=7228 AND `source_type`=0 A
 UPDATE `smart_scripts` SET `id`=1 WHERE `entryorguid`=7228 AND `source_type`=0 AND `id`=2 AND `link`=0;
 UPDATE `smart_scripts` SET `id`=2 WHERE `entryorguid`=7228 AND `source_type`=0 AND `id`=3 AND `link`=0;
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_16_04' WHERE sql_rev = '1631721215836008000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
