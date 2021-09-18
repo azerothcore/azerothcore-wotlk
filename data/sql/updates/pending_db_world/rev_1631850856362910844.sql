1dcf5c5cb909afd71f6f288246ed2bca82166aba
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2021-09-18 09:49:43 +0430

    Update rev_1631850856362910844.sql

--- data/sql/updates/pending_db_world/rev_1631850856362910844.sql
+++ data/sql/updates/pending_db_world/rev_1631850856362910844.sql
@@ -4,7 +4,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631850856362910844');
 SET @id := 17093;
 
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@id;
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@id, @id*100);
+DELETE FROM `smart_scripts` WHERE `source_type` IN (0, 9) AND `entryorguid` IN (@id, @id*100);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (@id , 0, 0, 0, 20, 0, 100, 0, 9434, 0, 0, 0, 80, @id*100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Magistrix Elosai - On Quest \"Testing the Tonic\" rewarded - Run script"),
 (@id*100, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 83, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Magistrix Elosai - On Script - Remove questgiver flag"),
