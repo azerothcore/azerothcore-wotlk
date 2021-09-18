c17f509ac69757f3f4b09996f75d3b7d61a2cd31
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-18 18:58:19 +0000

    chore(DB): import pending files
    Referenced commit(s): f2abf6b5e2c5d9e93b462d06ce4966dcef81b14c

--- data/sql/updates/db_world/2021_09_18_02.sql
+++ data/sql/updates/db_world/2021_09_18_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_18_01 -> 2021_09_18_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_18_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_18_01 2021_09_18_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1616252519696041200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1616252519696041200');
 /*
  * Zone: Nagrand
@@ -255,3 +271,13 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17130;
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 17130);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17130, 0, 0, 0, 0, 0, 100, 0, 2500, 5000, 20000, 25000, 11, 32019, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Talbuk Stag - In Combat - Cast \'32019\'');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_18_02' WHERE sql_rev = '1616252519696041200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
