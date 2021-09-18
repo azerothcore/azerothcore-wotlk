aa22a3a2692f4c9e1923300b0e196745b57f1ca4
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-09-18 16:03:44 +0930

    Update rev_1631944525344217322.sql

--- data/sql/updates/pending_db_world/rev_1631944525344217322.sql
+++ data/sql/updates/pending_db_world/rev_1631944525344217322.sql
@@ -3,3 +3,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631944525344217322');
 -- Deletes Plaguebloom from all NPC loot tables 
 DELETE FROM `creature_loot_template` WHERE `item` = 13466 AND `comment` LIKE '%Plaguebloom%';
 
+-- Remove loot from Crimson Bodyguard
+UPDATE `creature_template` SET `lootid` = 0 WHERE `Entry` = 13118 AND `Name` = 'Crimson Bodyguard';
