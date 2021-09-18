16a68f76db84444a19b2fc631b6ef56fe8d1e4a7
baranzadeoglu <84640316+baranzadeoglu@users.noreply.github.com>
2021-09-18 21:37:29 +0200

    fix(DB/Quest):Corruption of Earth and Seed repeatable after faction change
    Puts quest ids 7064 and 7065 into same exclusive group as suggested by the relevant documentation.
    Fixes #7273
    Note that the reputation issue in #7273 is a replication of a visual bug on Keira3 and should be ignored.

---
+++ data/sql/updates/pending_db_world/rev_1631992050760579041.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631992050760579041');
+
+UPDATE quest_template_addon SET ExclusiveGroup = 7064 WHERE (ID = 7064);
+UPDATE quest_template_addon SET ExclusiveGroup = 7064 WHERE (ID = 7065);
