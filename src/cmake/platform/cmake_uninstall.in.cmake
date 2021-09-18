46225c3ac160e0fdc7e0856ac3c46ac051fc7f48
Winfidonarleyan <dowlandtop@yandex.com>
2021-09-18 18:20:54 +0700

    correct

--- src/cmake/platform/cmake_uninstall.in.cmake
+++ src/cmake/platform/cmake_uninstall.in.cmake
@@ -10,7 +10,7 @@
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 
- from cmake wiki
+# from cmake wiki
 IF(NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
   MESSAGE(FATAL_ERROR "Cannot find install manifest: \"@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt\"")
 ENDIF(NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
