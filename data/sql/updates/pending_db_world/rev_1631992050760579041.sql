484a9c049665b7f883569ae3728df0b810c6a71e
baranzadeoglu <84640316+baranzadeoglu@users.noreply.github.com>
2021-09-19 19:50:53 +0200

    Update data/sql/updates/pending_db_world/rev_1631992050760579041.sql
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- data/sql/updates/pending_db_world/rev_1631992050760579041.sql
+++ data/sql/updates/pending_db_world/rev_1631992050760579041.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631992050760579041');
 
-UPDATE quest_template_addon SET ExclusiveGroup = 7064 WHERE (ID = 7064);
-UPDATE quest_template_addon SET ExclusiveGroup = 7064 WHERE (ID = 7065);
+UPDATE quest_template_addon SET ExclusiveGroup = 7064 WHERE (ID IN (7064,7065));
