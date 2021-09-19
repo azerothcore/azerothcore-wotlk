b39c55460d13ebcd89e385d8eb0d98eabfc5bcb3
baranzadeoglu <84640316+baranzadeoglu@users.noreply.github.com>
2021-09-19 19:51:06 +0200

    Update data/sql/updates/pending_db_world/rev_1631994698247378987.sql
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- data/sql/updates/pending_db_world/rev_1631994698247378987.sql
+++ data/sql/updates/pending_db_world/rev_1631994698247378987.sql
@@ -1,5 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631994698247378987');
 
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 7153) AND (`Item` IN (9155));
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 8598) AND (`Item` IN (9155));
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 11462) AND (`Item` IN (9155));
+DELETE FROM `creature_loot_template` WHERE (`Entry` IN (7153,8598,11462)) AND (`Item` IN (9155));
