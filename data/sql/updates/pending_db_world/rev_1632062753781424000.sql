98b39888106a5a3da614d0d818887283e4c91c84
Ragelezz <raduneculaulostone@gmail.com>
2021-09-19 17:51:42 +0300

    fix(DB\smart_scripts):fixed NPC Greymist Coastrunner Fleeing at low hp and removed battle stance

---
+++ data/sql/updates/pending_db_world/rev_1632062753781424000.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632062753781424000');
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 2202);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(2202, 0, 0, 0, 2, 0, 101, 0, 10, 15, 500, 1500, 0, 25, 1, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '');
