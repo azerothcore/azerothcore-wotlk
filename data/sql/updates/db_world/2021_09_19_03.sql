9febed8fa844dad66583db8675f13a056f6cfc01
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-19 16:47:13 +0000

    chore(DB): import pending files
    Referenced commit(s): 059ef9abacfd33697d71dd16b904ba0211cff17a

--- data/sql/updates/db_world/2021_09_19_03.sql
+++ data/sql/updates/db_world/2021_09_19_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_19_02 -> 2021_09_19_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_19_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_19_02 2021_09_19_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631634961417748000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631634961417748000');
 
 -- sets for all, generic 20/100 and maxrep
@@ -15,3 +31,13 @@ INSERT INTO `creature_onkill_reputation` (`creature_id`, `RewOnKillRepFaction1`,
 
 -- khan jehn
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 25, `RewOnKillRepValue2` = -125 WHERE (`creature_id` = 5601);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_19_03' WHERE sql_rev = '1631634961417748000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
