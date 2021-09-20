99529627fa68cc31a355fefeedebe7e43b9ebbd7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-20 11:02:42 +0000

    chore(DB): import pending files
    Referenced commit(s): 1018f061090b142a2bd2d3e611800973477f7824

--- data/sql/updates/db_world/2021_09_20_01.sql
+++ data/sql/updates/db_world/2021_09_20_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_20_00 -> 2021_09_20_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_20_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_20_00 2021_09_20_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631640758221414000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631640758221414000');
 
 UPDATE `gameobject` SET `position_x`=-8337.72, `position_y`=519.845, `position_z`=122.939, `orientation`=-1.12724, `rotation0`=-0.667571,
@@ -14,3 +30,13 @@ INSERT INTO `gameobject_addon` VALUES
 (20458,0,0),
 (20459,0,0),
 (31619,0,0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_20_01' WHERE sql_rev = '1631640758221414000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
