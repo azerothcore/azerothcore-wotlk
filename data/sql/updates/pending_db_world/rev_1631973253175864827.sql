a03125baeef4936bacd51a45cf6aec20f75940de
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-20 23:34:19 +0000

    chore(DB): import pending files
    Referenced commit(s): 75b99f2626313a83a9e2b83ba9836e49877ba70a

--- data/sql/updates/pending_db_world/rev_1631973253175864827.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631973253175864827');
-
--- Slightly changes the position of two mining Nodes, so that the whole Node is visible
-UPDATE `gameobject` SET `position_z` = -277 WHERE `id` = 123848 AND `guid` = 15462;
-UPDATE `gameobject` SET `position_z` = -233 WHERE `id` = 123309 AND `guid` = 15423;
