05330231a1fd84bb344220a6f30fcb42ef9a7c8b
Silker <61223313+Si1ker@users.noreply.github.com>
2021-09-20 03:54:12 -0600

    fix(DB/broadcast_text_locale): Typo on Lore Keeper of Norgannon's Greetings (#7903)

---
+++ data/sql/updates/pending_db_world/rev_1631739945979964800.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631739945979964800');
+
+UPDATE `broadcast_text_locale` SET `MaleText`='Saludos. Mi función consiste en proporcionar acceso y, si es admisible, explicaciones sobre la síntesis de terráneos por parte de los Creadores del lugar conocido como Azeroth. Estoy listo para compartir esta información contigo cuando lo desees.' WHERE `ID`=3172 AND `locale` IN ('esES','esMX');
+
