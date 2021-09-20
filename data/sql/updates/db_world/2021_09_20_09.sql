5295e23c122da7da60dfa1b66c2a697901e2a860
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-20 16:04:10 +0000

    chore(DB): import pending files
    Referenced commit(s): 251a2c8743e40b4da3bc13720274a9594b7ede07

--- data/sql/updates/db_world/2021_09_20_09.sql
+++ data/sql/updates/db_world/2021_09_20_09.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_20_08 -> 2021_09_20_09
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_20_08';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_20_08 2021_09_20_09 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631756219313456610'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631756219313456610');
 
 -- Servant of Razelikh
@@ -81,3 +97,13 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 (7671,1,4,"It was not his fault. Tell him that...",12,0,100,0,0,0,3684,0,"Servant of Sevine"),
 (7671,1,5,"Finally, released from his cursed grasp. Father, I come... ",12,0,100,0,0,0,3685,0,"Servant of Sevine"),
 (7671,2,0,"Stone of Binding shatters, releasing the soul which it held.",16,0,100,0,0,0,0,0,"Servant of Sevine");
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_20_09' WHERE sql_rev = '1631756219313456610';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
