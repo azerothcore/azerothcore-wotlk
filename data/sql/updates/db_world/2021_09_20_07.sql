2b58463622d982e3becbd06b9e77964dce48772a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-20 15:32:52 +0000

    chore(DB): import pending files
    Referenced commit(s): e000448f08c614560bd7816fd070ffdfc8003d47

--- data/sql/updates/db_world/2021_09_20_07.sql
+++ data/sql/updates/db_world/2021_09_20_07.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_20_06 -> 2021_09_20_07
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_20_06';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_20_06 2021_09_20_07 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631691102244326583'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631691102244326583');
 
 UPDATE `dungeon_access_requirements` SET `leader_only` = 0 WHERE `dungeon_access_id` = 17 AND `requirement_type` = 1 AND `requirement_id` = 10285;
@@ -22,3 +38,13 @@ UPDATE `dungeon_access_requirements` SET `leader_only` = 0 WHERE `dungeon_access
 UPDATE `dungeon_access_requirements` SET `leader_only` = 0 WHERE `dungeon_access_id` = 116 AND `requirement_type` = 1 AND `requirement_id` = 24712; 
 UPDATE `dungeon_access_requirements` SET `leader_only` = 0 WHERE `dungeon_access_id` = 117 AND `requirement_type` = 1 AND `requirement_id` = 24710; 
 UPDATE `dungeon_access_requirements` SET `leader_only` = 0 WHERE `dungeon_access_id` = 117 AND `requirement_type` = 1 AND `requirement_id` = 24712;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_20_07' WHERE sql_rev = '1631691102244326583';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
