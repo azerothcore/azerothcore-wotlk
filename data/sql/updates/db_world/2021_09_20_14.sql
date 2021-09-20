1e27defbbdf97a86e01e8eb236f86b8c3931572e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-20 18:02:14 +0000

    chore(DB): import pending files
    Referenced commit(s): 480a528949cc28fee1460da8a479ae90fa3d4eda

--- data/sql/updates/db_world/2021_09_20_14.sql
+++ data/sql/updates/db_world/2021_09_20_14.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_20_13 -> 2021_09_20_14
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_20_13';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_20_13 2021_09_20_14 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631804806947961857'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631804806947961857');
 
 -- Completion event for quest "Jin'Zil's Forest Magic"
@@ -23,3 +39,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (399500,9,6,0,0,0,100,0,6000,6000,0,0,51,0,0,0,0,0,0,9,4072,0,30,0,0,0,0,"Witch Doctor Jin\'Zil - On Script - Kill Target"),
 (399500,9,7,0,0,0,100,0,2000,2000,0,0,1,2,0,0,0,0,0,1,0,0,0,0,0,0,0,"Witch Doctor Jin\'Zil - On Script - Say Line 2"),
 (399500,9,8,0,0,0,100,0,3000,3000,0,0,82,2,0,0,0,0,0,1,0,0,0,0,0,0,0,"Witch Doctor Jin\'Zil - On Script - Add Npc Flag Questgiver");
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_20_14' WHERE sql_rev = '1631804806947961857';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
