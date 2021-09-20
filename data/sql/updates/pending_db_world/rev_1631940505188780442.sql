0710be78657f97996c0aa8c68ba6e18595fbebc0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-20 18:53:07 +0000

    chore(DB): import pending files
    Referenced commit(s): 19c35718bed78a3ce43310d8e44ec2d9480048bf

--- data/sql/updates/pending_db_world/rev_1631940505188780442.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631940505188780442');
-
--- Deletes Black Lotus from all NPC loot tables
-DELETE FROM `creature_loot_template` WHERE `item` = 13468 AND `comment` LIKE '%Black Lotus%';
-
