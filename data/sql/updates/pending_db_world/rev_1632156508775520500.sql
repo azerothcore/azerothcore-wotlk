2df09d42ef63dd978ec0a7f48fce182a9e434391
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-09-21 20:06:17 +0200

    Update rev_1632156508775520500.sql

--- data/sql/updates/pending_db_world/rev_1632156508775520500.sql
+++ data/sql/updates/pending_db_world/rev_1632156508775520500.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632156508775520500');
 
-UPDATE `creature_template` SET `maxlevel` = 21 WHERE `entry` = 1039 AND `name` LIKE '%Fen Dweller%';
+UPDATE `creature_template` SET `maxlevel` = 21 WHERE `entry` = 1039;
