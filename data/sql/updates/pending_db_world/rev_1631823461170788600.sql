51aac38f5993064a015e548b6968a7b973158aee
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-21 12:03:47 +0000

    chore(DB): import pending files
    Referenced commit(s): c3259dabcfbee5bb721772826a340460ab674cbc

--- data/sql/updates/pending_db_world/rev_1631823461170788600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631823461170788600');
-
-UPDATE `spell_proc_event` SET `procEx`=0x0200003 WHERE `entry`=-11213;
-UPDATE `spell_proc_event` SET `procEx`=0x0200002 WHERE `entry`=16164;
-UPDATE `spell_proc_event` SET `procEx`=0x0210000 WHERE `entry`=16864;
