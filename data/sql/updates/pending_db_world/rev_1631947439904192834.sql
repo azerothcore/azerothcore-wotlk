3570c0b16c57d412d5b1321e1781dd076046ad35
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-21 10:57:56 +0000

    chore(DB): import pending files
    Referenced commit(s): bacba76ab7f6c3fcc4034bb36977b9e2b718d9f6

--- data/sql/updates/pending_db_world/rev_1631947439904192834.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631947439904192834');
-
--- Correct handin location for Twisted Hatred
-UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Tallonkai Swiftroot at Dolanaar in Teldrassil.' WHERE `ID` = 932;
-
