4cf8e7a1f36e3fcbe195bab790a7c1129e2ce89f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-21 10:49:51 +0000

    chore(DB): import pending files
    Referenced commit(s): f4ebe78c39979e08a493d9b8d6ef1c49e90cb65c

--- data/sql/updates/pending_db_world/rev_1631945631125292040.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631945631125292040');
-
--- Deletes Hide of Lupos from RLT 24062
-DELETE FROM `reference_loot_template` WHERE `Entry` = 24062 AND `Item` = 3018;
-
