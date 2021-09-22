5498fd6382c04b5b51068bc3ebe4a517dc135c48
All <44707108+zxcnxc@users.noreply.github.com>
2021-09-23 00:53:22 +0700

    Update rev_1632332436826853639.sql

--- data/sql/updates/pending_db_world/rev_1632332436826853639.sql
+++ data/sql/updates/pending_db_world/rev_1632332436826853639.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632332436826853639');
 
---Delete incorrect DisplayID Other Gender for captain Halyndor
+-- Delete incorrect DisplayID Other Gender for captain Halyndor
 UPDATE `creature_model_info` SET `DisplayID_Other_Gender`= 0 WHERE `DisplayID`= 3494;
 
