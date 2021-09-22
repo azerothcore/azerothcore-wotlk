fe119b7374c05198606d5e6817d2db9484283a6a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-22 07:46:47 +0000

    chore(DB): import pending files
    Referenced commit(s): a460d21f3c9150f691bdcb79d3f2177474a7e76a

--- data/sql/updates/db_world/2021_09_22_00.sql
+++ data/sql/updates/db_world/2021_09_22_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_21_06 -> 2021_09_22_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_21_06';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_21_06 2021_09_22_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1631850856362910844'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631850856362910844');
 
 -- 
@@ -27,3 +43,13 @@ INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language
 (@id, 2, 0, (SELECT `FemaleText` FROM `broadcast_text` WHERE `ID`=13510), 12, 0, 100, 0, 0, 0, 13510, 0, ""),
 (@id, 3, 0, (SELECT `FemaleText` FROM `broadcast_text` WHERE `ID`=13511), 12, 0, 100, 0, 0, 0, 13511, 0, ""),
 (@id, 4, 0, (SELECT `FemaleText` FROM `broadcast_text` WHERE `ID`=14318), 12, 0, 100, 0, 0, 0, 14318, 0, "");
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_22_00' WHERE sql_rev = '1631850856362910844';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
