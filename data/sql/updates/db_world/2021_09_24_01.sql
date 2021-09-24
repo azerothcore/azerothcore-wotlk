5d08c6c64b50e46cf843b7cc24098b4e2b62ed5e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-24 17:29:22 +0000

    chore(DB): import pending files
    Referenced commit(s): 0c2dbfeac609f0a5208e7ce478b749c514c4daeb

--- data/sql/updates/db_world/2021_09_24_01.sql
+++ data/sql/updates/db_world/2021_09_24_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_24_00 -> 2021_09_24_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_24_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_24_00 2021_09_24_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1632228638474923220'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632228638474923220');
 
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=12803;
@@ -13,3 +29,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `creature_text` WHERE `CreatureID`=12803;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (12803,0,0,"%s goes into a frenzy!",16,0,100,0,0,0,10645,0,"Lord Lakmaeran");
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_24_01' WHERE sql_rev = '1632228638474923220';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
