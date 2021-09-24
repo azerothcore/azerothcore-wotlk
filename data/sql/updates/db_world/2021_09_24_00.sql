75f6afc516f85ac6d2ddb81334f4deab844ef673
AzerothCoreBot <azerothcorebot@gmail.com>
2021-09-24 17:00:21 +0000

    chore(DB): import pending files
    Referenced commit(s): 115f8ce17e8c2674f4fd4a8f976e1d3825895ca6

--- data/sql/updates/db_world/2021_09_24_00.sql
+++ data/sql/updates/db_world/2021_09_24_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_09_23_02 -> 2021_09_24_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_09_23_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_09_23_02 2021_09_24_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1632254602577940300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632254602577940300');
 
 SET @ENTRY := 24484;
@@ -128,3 +144,13 @@ UPDATE `creature_addon` SET `auras`='44003' WHERE `guid`=88921;
 UPDATE `creature_addon` SET `auras`='44003' WHERE `guid`=88920;
 UPDATE `creature_addon` SET `auras`='44003' WHERE `guid`=88919;
 UPDATE `creature_addon` SET `auras`='44003' WHERE `guid`=88914;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_09_24_00' WHERE sql_rev = '1632254602577940300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
