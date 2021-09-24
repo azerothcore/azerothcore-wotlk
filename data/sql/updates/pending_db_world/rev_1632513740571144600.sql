86b69d09e87bc00823dc7d825c55b48fc48e0f49
neifion-00000000 <vbdudl@gmail.com>
2021-09-24 15:11:36 -0500

    fix(DB): Bael'dun Digsite mob flags fixed
    Issue: Mobs at Bael'dun Digsite were incorrectly flagging players for pvp.
    Faction now matches other ally friendly horde quest NPCs.

---
+++ data/sql/updates/pending_db_world/rev_1632513740571144600.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632513740571144600');
+UPDATE `acore_world`.`creature_template` SET `faction`='122' WHERE  `entry`=2990;
+UPDATE `acore_world`.`creature_template` SET `faction`='122' WHERE  `entry`=2989;
