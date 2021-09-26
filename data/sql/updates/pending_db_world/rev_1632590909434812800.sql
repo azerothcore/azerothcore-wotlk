be91895c79eea4eff0ba162f627e8b6d49a77fce
Ragelezz <69433539+Ragelezz@users.noreply.github.com>
2021-09-26 12:15:27 +0300

    Update data/sql/updates/pending_db_world/rev_1632590909434812800.sql
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- data/sql/updates/pending_db_world/rev_1632590909434812800.sql
+++ data/sql/updates/pending_db_world/rev_1632590909434812800.sql
@@ -1,5 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632590909434812800');
 
-UPDATE `creature` SET `position_x`=2424.75 WHERE `guid`=34193;
-UPDATE `creature` SET `position_y`=-1669.55 WHERE `guid`=34193;
-UPDATE `creature` SET `position_z`=139.06 WHERE `guid`=34193;
+UPDATE `creature` SET `position_x`=2424.75, `position_y`=-1669.55, `position_z`=139.06 WHERE `guid`=34193;
