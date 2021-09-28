07a14fb4975458097116039e1dacf2282fa296af
baranzadeoglu <84640316+baranzadeoglu@users.noreply.github.com>
2021-09-28 17:40:20 +0200

    Update data/sql/updates/pending_db_world/rev_1631992050760579041.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1631992050760579041.sql
+++ data/sql/updates/pending_db_world/rev_1631992050760579041.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1631992050760579041');
 
-UPDATE 'quest_template_addon' SET 'ExclusiveGroup' = '7064' WHERE ('ID' IN (7064,7065));
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 7064 WHERE (`ID` IN (7064,7065));
