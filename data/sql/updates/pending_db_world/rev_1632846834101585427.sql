1d0bdcf7f2e89f7b55f2616b94c4fa7ab86ed42e
All <44707108+zxcnxc@users.noreply.github.com>
2021-09-28 23:56:23 +0700

    update to 1 query for esES and esMX

--- data/sql/updates/pending_db_world/rev_1632846834101585427.sql
+++ data/sql/updates/pending_db_world/rev_1632846834101585427.sql
@@ -3,11 +3,8 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632846834101585427');
 -- Correct Quest marker text
 UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Keeper Bel\'dugur at Apothecarium in the Undercity.' WHERE `ID` = 1013;
 
--- es ES
-UPDATE `quest_template_locale` SET `CompletedText` = 'Vuelve con: Guardián Bel\'dugur. Zona: Apothecarium de Entrañas.' WHERE `ID` = 1013 AND `locale` = 'esES';
-
--- esMX
-UPDATE `quest_template_locale` SET `CompletedText` = 'Vuelve con: Guardián Bel\'dugur. Zona: Apothecarium de Entrañas.' WHERE `ID` = 1013 AND `locale` = 'esMX';
+-- esES & esMX
+UPDATE `quest_template_locale` SET `CompletedText` = 'Vuelve con: Guardián Bel\'dugur. Zona: Apothecarium de Entrañas.' WHERE `ID` = 1013 AND `locale` IN ('esES', 'esMX');
 
 -- frFR
 UPDATE `quest_template_locale` SET `CompletedText` = 'Retournez voir le Gardien Bel\'dugur au  à l\'Apothicarium à Undercity' WHERE `ID` = 1013 AND `locale` = 'frFR';
