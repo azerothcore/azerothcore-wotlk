e46eb221a3756f571bbb87adbe7d0801a92f997a
Patrick Lewis <pat@lo5t.com>
2021-09-28 23:41:59 -0700

    update

--- apps/installer/includes/os_configs/osx.sh
+++ apps/installer/includes/os_configs/osx.sh
@@ -1,9 +1,3 @@
-##########################################
-## workaround to fix macos-10.15 configure os from failing
-#brew reinstall openssl@1.1
-#rm '/usr/local/bin/2to3'
-##########################################
-
 brew update
 
 ##########################################
