e3dd04f32a285a5cf939989aeee8fb4c6b2adc61
jestermaniac <88903576+jestermaniac@users.noreply.github.com>
2021-09-30 20:53:05 +0200

    Update rev_1633027575141830731.sql

--- data/sql/updates/pending_db_world/rev_1633027575141830731.sql
+++ data/sql/updates/pending_db_world/rev_1633027575141830731.sql
@@ -2,3 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633027575141830731');
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = 15712 AND `ScriptName` = "spell_item_linken_boomerang";
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (15712, "spell_item_linken_boomerang");
+
