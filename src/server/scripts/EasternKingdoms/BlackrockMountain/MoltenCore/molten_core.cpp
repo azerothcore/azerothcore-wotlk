5f0d28aceeaa08c1af50316b2c5fbe1d34e2f062
sanctum32 <andriuspel@gmail.com>
2021-10-01 18:25:14 +0300

    Scripts/MoltenCore: moved npc "Core Hound" script from C++ to SAI

--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/molten_core.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/molten_core.cpp
@@ -3,113 +3,8 @@
  */
 
 #include "ScriptMgr.h"
-#include "ScriptedCreature.h"
 #include "molten_core.h"
-
-enum Texts
-{
-    EMOTE_SMOLDERING        = 0,
-    EMOTE_IGNITE            = 1,
-};
-
-enum Spells
-{
-    SPELL_SERRATED_BITE     = 19771,
-    SPELL_PLAY_DEAD         = 19822,
-};
-
-enum Events
-{
-    EVENT_SERRATED_BITE     = 1,
-    EVENT_IGNITE,
-};
-
-enum Actions
-{
-    ACTION_HOUND_IGNITE     = 1,
-};
-
-// Serrated Bites timer may be wrong
-class npc_mc_core_hound : public CreatureScript
-{
-public:
-    npc_mc_core_hound() : CreatureScript("npc_mc_core_hound") {}
-
-    struct npc_mc_core_houndAI : public CreatureAI
-    {
-        npc_mc_core_houndAI(Creature* creature) : CreatureAI(creature) {}
-
-        void Reset() override
-        {
-            killerGUID.Clear();
-        }
-
-        void EnterCombat(Unit* /*victim*/) override
-        {
-            events.ScheduleEvent(EVENT_SERRATED_BITE, 3000);
-        }
-
-        void DamageTaken(Unit* attacker, uint32& damage, DamageEffectType /*damagetype*/, SpellSchoolMask /*damageSchoolMask*/) override
-        {
-            // Prevent receiving any extra damage if Hound is playing dead
-            if (me->HasAura(SPELL_PLAY_DEAD))
-            {
-                damage = 0;
-                return;
-            }
-
-            if (me->HealthBelowPctDamaged(0, damage) && !me->HasAura(SPELL_PLAY_DEAD))
-            {
-                damage = 0;
-                killerGUID = attacker->GetGUID();
-                Talk(EMOTE_SMOLDERING);
-            }
-        }
-
-        void DoAction(int32 action) override
-        {
-            if (action == ACTION_HOUND_IGNITE && me->IsInCombat())
-            {
-
-
-                killerGUID.Clear();
-            }
-        }
-
-        void UpdateAI(uint32 diff) override
-        {
-            if (!UpdateVictim() && !smoldering)
-            {
-                return;
-            }
-
-            events.Update(diff);
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-            {
-                return;
-            }
-
-            if (events.ExecuteEvent() == EVENT_SERRATED_BITE)
-            {
-                DoCastVictim(SPELL_SERRATED_BITE);
-                events.RepeatEvent(urand(5000, 6000));
-            }
-
-            DoMeleeAttackIfReady();
-        }
-
-    private:
-        EventMap events;
-        ObjectGuid killerGUID;
-        bool smoldering;
-    };
-
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return GetMoltenCoreAI<npc_mc_core_houndAI>(creature);
-    }
-};
+#include "SpellScript.h"
 
 // 19822 Play Dead
 class spell_mc_play_dead : public SpellScriptLoader
@@ -155,8 +50,8 @@ public:
 
                     if (!shouldDie)
                     {
-                        target->AI()->DoAction(target->GetEntry() * 10);
                         target->SetFullHealth();
+                        target->AI()->SetData(target->GetEntry(), 1);
                     }
                     else
                     {
