6c953e9333d8ff23639c488b77107aa2f77525bd
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-01 14:59:53 +0000

    chore(DB): import pending files
    Referenced commit(s): ecb92f9d669f8f888eedf5feb229a0e350e02976

--- data/sql/updates/pending_db_world/rev_1632327857472629400.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632327857472629400');
-
-DELETE FROM `creature_loot_template` WHERE `Entry` = 8891 AND `Item` IN (24086, 774, 818, 1206, 1210, 1529, 1705, 2449, 3356, 3357, 3820, 5498, 5500);
-
-# Note: This reference_loot_template entry has multiple (1000+) different mobs referencing it.
-# Checked a couple of the mobs (On Wowhead) and found that it varied whether they dropped the item (Citrine)
-# Those that did had it in their individual table
-DELETE FROM `reference_loot_template` WHERE `Entry` = 24036 AND `Item` = 3864;
