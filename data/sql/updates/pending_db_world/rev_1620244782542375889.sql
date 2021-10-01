f08314bdacbc64649565da61e257d79e788ceeea
sanctum32 <andriuspel@gmail.com>
2021-10-01 19:17:52 +0300

    DB/MoltenCore: one more minor correction

--- data/sql/updates/pending_db_world/rev_1620244782542375889.sql
+++ data/sql/updates/pending_db_world/rev_1620244782542375889.sql
@@ -88,7 +88,7 @@ UPDATE `creature_template_addon` SET `auras`='18950' WHERE  `entry`=11658;
 update creature_addon set `auras`='18950' where `guid` in (select `guid` from `creature` where `id`=11658);
 
 -- Fire Imp
-update `creature_template` set `AIName`='SmartAI' where `entryorguid`=11669;
+update `creature_template` set `AIName`='SmartAI' where `entry`=11669;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=11669 AND `source_type`=0 AND `id`=0 AND `link`=0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (11669, 0, 0, 0, 0, 0, 100, 0, 5000, 15000, 4000, 7000, 0, 11, 20602, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Flame Imp - IC - Cast Fire Nova');
