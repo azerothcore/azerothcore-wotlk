6e2c87b35eb6feb41f7587a8e57ec586f473622c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-03 02:52:28 +0000

    chore(DB): import pending files
    Referenced commit(s): 391bd70c8d20e13745c1c66eb3a7f1463bcd481b

--- data/sql/updates/pending_db_world/rev_1632479021658969480.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632479021658969480');
-
--- Decreases the respawn time for Colonel Kurzen to 5 min
-UPDATE `creature` SET `spawntimesecs` = 300 WHERE `id` = 813 AND `guid` = 1481;
