396070ee4d8e88de2527c4715f0470afcda36cb7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-03 14:51:41 +0000

    chore(DB): import pending files
    Referenced commit(s): 9550d219d97ae87831939687ca9da52ff76463d0

--- data/sql/updates/pending_db_world/rev_1632542867559951404.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632542867559951404');
-
--- Add 2 day time delay to Gubber Blump's letter
-UPDATE `quest_template_addon` SET `RewardMailDelay` = 172800 WHERE `ID` = 1141;
-
