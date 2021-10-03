3d8f2ccbbed509d0862a5e95af09bfb9154dd027
Pagani Walter <paganiwalter@gmail.com>
2021-10-03 06:10:37 -0300

    Modifying the text.

--- data/sql/updates/pending_db_world/rev_1632966261848137000.sql
+++ data/sql/updates/pending_db_world/rev_1632966261848137000.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632966261848137000');
 
-UPDATE `acore_string` SET `locale_esES`="¡%s intenta huir con miedo!", `locale_esMX`="¡%s intenta huir con miedo!" WHERE `entry`=5030;
+UPDATE `acore_string` SET `locale_esES`="¡%s intenta huir despavorido!", `locale_esMX`="¡%s intenta huir despavorido!" WHERE `entry`=5030;
