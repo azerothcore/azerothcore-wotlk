679ee83862378050e15b5a669d3efcbad45b9bc7
Ragelezz <raduneculaulostone@gmail.com>
2021-10-04 19:45:36 +0300

    fix(DB\Quest):Argent Dawn Commission faction-locked

---
+++ data/sql/updates/pending_db_world/rev_1633365891402740400.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633365891402740400');
+
+UPDATE `quest_template` SET `AllowableRaces`=690 WHERE `ID`=5405;
+UPDATE `quest_template` SET `AllowableRaces`=1101 WHERE `ID`=5401;
