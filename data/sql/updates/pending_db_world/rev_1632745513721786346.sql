b9c98dffa015e7b43b3cbed7ae7b21ab4e1b8d79
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-04 10:56:23 +0000

    chore(DB): import pending files
    Referenced commit(s): f181a895114940a7bfcb9ca920c450f72c676668

--- data/sql/updates/pending_db_world/rev_1632745513721786346.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632745513721786346');
-
--- Enable swimming for Tooga
-UPDATE `creature_template` SET `InhabitType` = `InhabitType`|1|2 WHERE `entry` = 5955;
-
