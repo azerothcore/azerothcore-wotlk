05abd5d357a4d54a055914e17387f4a5b7a4774c
Ragelezz <raduneculaulostone@gmail.com>
2021-10-04 19:53:52 +0300

    fix(DB\Quest):Updated missing lines from Argent Dawn Commission

--- data/sql/updates/pending_db_world/rev_1633365891402740400.sql
+++ data/sql/updates/pending_db_world/rev_1633365891402740400.sql
@@ -2,3 +2,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633365891402740400');
 
 UPDATE `quest_template` SET `AllowableRaces`=690 WHERE `ID`=5405;
 UPDATE `quest_template` SET `AllowableRaces`=1101 WHERE `ID`=5401;
+UPDATE `creature_queststarter` SET `id`=11039 WHERE `quest`=5503;
+UPDATE `creature_questender` SET `id`=11039 WHERE `quest`=5503;
