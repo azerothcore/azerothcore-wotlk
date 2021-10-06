a7c2066150ab6891d5cc2401acde34405b2f8368
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-06 08:22:38 +0000

    chore(DB): import pending files
    Referenced commit(s): 13aaeb25f6f28a8c711b057662ac0acf3a164ca6

--- data/sql/updates/pending_db_world/rev_1633501534792643595.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633501534792643595');
-
--- Change the spawn time of Incendosaur from 500 seconds (9 minutes) to 60 seconds (1 minute)
-UPDATE `creature` SET `spawntimesecs` = 60 WHERE (`id` = 9318) AND (`guid` IN (5874 , 5876, 5878, 5879, 5883, 5899 ,5901, 5902, 5904, 5905, 5906, 5907, 5911));
-
