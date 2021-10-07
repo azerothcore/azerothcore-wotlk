59abc17df5325a8c9b5e00be3dd0f6b8f8883792
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-07 16:22:40 +0000

    chore(DB): import pending files
    Referenced commit(s): a85c263f2fc4a5f9c34986605b66d40384d8d060

--- data/sql/updates/pending_db_world/rev_1632966261848137000.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632966261848137000');
-
-UPDATE `acore_string` SET `locale_esES`="¡%s intenta huir despavorido!", `locale_esMX`="¡%s intenta huir despavorido!" WHERE `entry`=5030;
