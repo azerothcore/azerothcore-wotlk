0edee4ab9c258ad43770979a14d4f8a5184aa58e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-07 16:18:11 +0000

    chore(DB): import pending files
    Referenced commit(s): 261d25e97f203489d2f66bdb361eb8cebbb137dd

--- data/sql/updates/db_world/2021_10_07_06.sql
+++ data/sql/updates/db_world/2021_10_07_06.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_07_05 -> 2021_10_07_06
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_07_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_07_05 2021_10_07_06 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1632792974502538900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632792974502538900');
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 15702;
@@ -35,3 +51,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (6559, 0, 0, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 11, 14147, 6, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Glutinous Ooze - On Just Died - Cast \'Acid Slime\' (Phase 2)'),
 (6559, 0, 1, 0, 8, 0, 100, 1, 15702, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Glutinous Ooze - On Spell Hit (Filling Empty Jar) - Despawn'),
 (6559, 0, 2, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Glutinous Ooze - On Just Died - Say Line 0');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_07_06' WHERE sql_rev = '1632792974502538900';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
