14da707c6e23816cae4b0f019d1490e9cd1866ed
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-07 11:05:28 +0000

    chore(DB): import pending files
    Referenced commit(s): d74c961d93bcab6fc8f73928c14d12d0bcb746b3

--- data/sql/updates/db_world/2021_10_07_01.sql
+++ data/sql/updates/db_world/2021_10_07_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_07_00 -> 2021_10_07_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_07_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_07_00 2021_10_07_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1624399628559846000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1624399628559846000');
 
 -- adding 85% chance to despawn to new dungeon rares
@@ -54,3 +70,13 @@ DELETE FROM `smart_scripts` WHERE `entryOrGuid` = @ENTRY AND `source_type` = 0;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`= @ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (@ENTRY, 0, 0, 0, 0, 0, 100, 2, 2000, 2000, 20000, 30000, 11, 10869, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Every 20 - 30 seconds (2 - 2s initially) - Self: Cast spell Summon Embers (10869) on Self');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_07_01' WHERE sql_rev = '1624399628559846000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
