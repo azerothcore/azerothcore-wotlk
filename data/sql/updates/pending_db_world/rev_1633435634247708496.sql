6a9b73e94613e16e24c6db7773fbae5e92eb385d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-08 22:10:53 +0000

    chore(DB): import pending files
    Referenced commit(s): 2c7df906f3f7b96b158753cc655b08563041ad05

--- data/sql/updates/pending_db_world/rev_1633435634247708496.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633435634247708496');
-
--- Giant Clam(2264) Vile reef loot fixes
-
--- Increased the loot chance for Blue Pearl(4611) to 100%
-UPDATE `gameobject_loot_template` SET `Chance` = 100 WHERE (`Entry` = 2264) AND (`Item` = 4611);
--- Increased the loot chance for Giant Clam Meat(4655) to 35% and moved to another group id as the groupid 2 is always Blue Pearl
-UPDATE `gameobject_loot_template` SET `Chance` = 35, `GroupId` = 1 WHERE (`Entry` = 2264) AND (`Item` = 4655);
-
