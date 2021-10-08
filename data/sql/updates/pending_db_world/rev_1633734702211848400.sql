dadbfe5789a440813b4f297d10574325419d01c8
Nyeriah <sarah.trysan@live.com>
2021-10-08 20:13:54 -0300

    fix(DB/Formations): Correct GroupAI for Tendris Warpwood's formation

---
+++ data/sql/updates/pending_db_world/rev_1633734702211848400.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633734702211848400');
+
+-- Tendris Warpwood - Dire Maul
+UPDATE `creature_formations` SET `groupAI` = 1 WHERE `leaderGUID` = 248035;
