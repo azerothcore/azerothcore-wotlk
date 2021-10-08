eaa8fc6df084de25ac75769fba30b27f1b65744d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-08 19:05:08 +0000

    chore(DB): import pending files
    Referenced commit(s): 4947887644f8e51b3718f584d8e3e854b4757d30

--- data/sql/updates/db_world/2021_10_08_11.sql
+++ data/sql/updates/db_world/2021_10_08_11.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_08_10 -> 2021_10_08_11
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_08_10';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_08_10 2021_10_08_11 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1632342398795739900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632342398795739900');
 
 -- Coren Direbrew Implementation
@@ -48,3 +64,13 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (47370,'spell_send_mug_target_picker'),
 (47344,'spell_request_second_mug'),
 (50278,'spell_barreled_control_aura');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_08_11' WHERE sql_rev = '1632342398795739900';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
