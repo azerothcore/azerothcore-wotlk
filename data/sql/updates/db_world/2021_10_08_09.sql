a35dbbd1750d8f728ed9787f42461bd2b526481f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-08 18:24:53 +0000

    chore(DB): import pending files
    Referenced commit(s): 668f519fc6893aa4b07f5da66a250f1d133abc46

--- data/sql/updates/db_world/2021_10_08_09.sql
+++ data/sql/updates/db_world/2021_10_08_09.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_08_08 -> 2021_10_08_09
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_08_08';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_08_08 2021_10_08_09 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633447081046893687'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633447081046893687');
 
 -- Cyrus Therepentous
@@ -34,3 +50,13 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 (19,0,4023,0,0,2,0,10575,1,1,1,0,0,'','Quest A Taste of Flame (2) can only be taken if player does not have item Black Dragonflight Molt');
 
 UPDATE `quest_template_addon` SET `SpecialFlags`=2 WHERE `ID`=4022;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_08_09' WHERE sql_rev = '1633447081046893687';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
