369c9bac94d29f7417ab1099fd23b71a50d492fe
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-08 09:43:48 +0000

    chore(DB): import pending files
    Referenced commit(s): 49817f222d2c71fbaead1cf73abb9eadc06936f0

--- data/sql/updates/db_world/2021_10_08_04.sql
+++ data/sql/updates/db_world/2021_10_08_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_08_03 -> 2021_10_08_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_08_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_08_03 2021_10_08_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1632847156889213161'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632847156889213161');
 
 -- Adds 8 Giant Webwood Spider spawns
@@ -11,3 +27,13 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `p
 (209194, 2001, 1, 0, 0, 1, 1, 6808, 0, 10970, 1824, 1330.3, 3.8, 300, 5, 0, 198, 0, 1, 0, 0, 0, '', 0),
 (209195, 2001, 1, 0, 0, 1, 1, 6808, 0, 10834, 1824, 1341.3, 1.3, 300, 5, 0, 198, 0, 1, 0, 0, 0, '', 0),
 (209196, 2001, 1, 0, 0, 1, 1, 6808, 0, 10972, 1386, 1321.6, 2.6, 300, 5, 0, 198, 0, 1, 0, 0, 0, '', 0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_08_04' WHERE sql_rev = '1632847156889213161';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
