6c0fde37c24d0e8cb28f0ac0ecc5fe21da72974b
sanctum32 <andriuspel@gmail.com>
2021-10-09 17:41:28 +0300

    Core/Spells: applied some more code style in Aura::CanBeSaved + moved Raise "Ally control aura" handling to DB

--- data/sql/updates/pending_db_world/rev_1633787767742446400.sql
+++ data/sql/updates/pending_db_world/rev_1633787767742446400.sql
@@ -2,3 +2,8 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633787767742446400');
 
 ALTER TABLE `spell_custom_attr`
 	ADD COLUMN `attributes1` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'SpellCustomAttributes1' AFTER `attributes`;
+
+-- Raise Ally control aura
+DELETE FROM `spell_custom_attr` WHERE `spell_id`=46619;
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`, `attributes1`) VALUES
+(46619, 0, 2);
