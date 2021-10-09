94e21182862d2f42964c7c379ceeba66fa11be7a
sanctum32 <andriuspel@gmail.com>
2021-10-09 18:04:36 +0300

    DB/Spells: removed INT width

--- data/sql/updates/pending_db_world/rev_1633787767742446400.sql
+++ data/sql/updates/pending_db_world/rev_1633787767742446400.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633787767742446400');
 
 ALTER TABLE `spell_custom_attr`
-	ADD COLUMN `attributes1` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'SpellCustomAttributes1' AFTER `attributes`;
+	ADD COLUMN `attributes1` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'SpellCustomAttributes1' AFTER `attributes`;
 
 -- Raise Ally control aura
 DELETE FROM `spell_custom_attr` WHERE `spell_id`=46619;
