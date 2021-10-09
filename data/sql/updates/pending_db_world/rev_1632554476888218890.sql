75bb82e3fb95b572d8e555736f91191a322da6f1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-09 10:48:28 +0000

    chore(DB): import pending files
    Referenced commit(s): 7cf0ed0abe0ddae665e11c1965bab3ee04018c49

--- data/sql/updates/pending_db_world/rev_1632554476888218890.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632554476888218890');
-
--- Add vendor option to Innkeeper Shaussiy
-DELETE FROM `gossip_menu_option` WHERE `MenuID` = 347 AND `OptionID` = 3;
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
-(347, 3, 1, 'Let me browse your goods.', 2823, 3, 128, 0, 0, 0, 0, '', 0, 0);
-
