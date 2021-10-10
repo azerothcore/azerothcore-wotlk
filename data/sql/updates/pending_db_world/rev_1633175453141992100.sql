1b4e838d606ae6fbdcde34326a9ea5a5fef02d54
Ragelezz <raduneculaulostone@gmail.com>
2021-10-10 17:34:38 +0300

    typo fields added to query

--- data/sql/updates/pending_db_world/rev_1633175453141992100.sql
+++ data/sql/updates/pending_db_world/rev_1633175453141992100.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633175453141992100');
 
 DELETE FROM `creature` WHERE `id`=119;
-INSERT INTO `creature` VALUES
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (3110417, 119, 0, 0, 0, 1, 1, 0, 0, -9768.05, 615.642, 35.228, 0.933077, 300, 0, 0, 198, 0, 0, 0, 0, 0, '', 0),
 (3110419, 119, 0, 0, 0, 1, 1, 0, 0, -9862.54, 715.2, 30.3827, 2.47246, 300, 0, 0, 198, 0, 0, 0, 0, 0, '', 0),
 (3110416, 119, 0, 0, 0, 1, 1, 0, 0, -9803.8, 655.994, 34.7701, 5.34544, 300, 0, 0, 198, 0, 0, 0, 0, 0, '', 0),
