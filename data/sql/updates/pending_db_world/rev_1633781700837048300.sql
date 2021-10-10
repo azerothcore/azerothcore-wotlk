6ab3c103bec57573d81c127dae92c9b93812110b
Ragelezz <raduneculaulostone@gmail.com>
2021-10-10 17:21:05 +0300

    fixing local merge issue

---
+++ data/sql/updates/pending_db_world/rev_1633781700837048300.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633781700837048300');
+
+-- Target: Felstone Field
+UPDATE `quest_template` SET `RewardFactionOverride2`=50000 WHERE `ID`=5229;
+-- Dead Man's Plea
+UPDATE `quest_template` SET `RewardFactionOverride1`=100000 WHERE `ID`=8945;
+-- Above and Beyond
+UPDATE `quest_template` SET `RewardFactionOverride1`=100000 WHERE  `ID`=5263;
