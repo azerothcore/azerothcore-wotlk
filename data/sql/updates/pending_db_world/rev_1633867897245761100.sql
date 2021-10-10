a05cdbdf3990e3f4687e2d28aab4eb4348b797c2
Nyeriah <sarah.trysan@live.com>
2021-10-10 09:14:36 -0300

    fix(DB/Spells): fix the spell proc from Haunted incorrectly phasing players out

---
+++ data/sql/updates/pending_db_world/rev_1633867897245761100.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633867897245761100');
+
+-- Server-side spell 'Summon Darrowshire Poltergeinst (DND)'
+UPDATE `spell_dbc` SET `Effect_1` = 28, `EffectMiscValueB_1` = 64 WHERE `id` = 17694;
