c63f6aa2afb3bb162d5fb58dafff2e44a6bcc294
Nyeriah <sarah.trysan@live.com>
2021-10-11 15:52:04 -0300

    c.c

--- data/sql/updates/pending_db_world/rev_1633972846166794700.sql
+++ data/sql/updates/pending_db_world/rev_1633972846166794700.sql
@@ -1,8 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633972846166794700');
 
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 1282 AND `SourceEntry` = 0;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 1282 AND `SourceEntry` = 0 AND  `ConditionTypeOrReference` = 47;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(15, 1282, 0, 0, 0, 47, 0, 3566, 8, 0, 0, 0, 0, '', 'Alter of Suntara - Show gossip option only if player has quest in progress \'Rise, Obsidion\'');
+(15, 1282, 0, 0, 0, 47, 0, 3566, 8, 0, 0, 0, 0, '', 'Alter of Suntara - Show gossip option only if player has quest in progress \'Rise, Obsidion!\'');
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 148498) AND (`source_type` = 1) AND (`id` IN (0, 1, 2, 3, 4));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
