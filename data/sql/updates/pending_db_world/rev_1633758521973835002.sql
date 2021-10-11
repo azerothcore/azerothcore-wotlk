ec336612ef4dd50c9de82bc591cbb70690ae37e4
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2021-10-11 13:15:44 +0330

    Update rev_1633758521973835002.sql

--- data/sql/updates/pending_db_world/rev_1633758521973835002.sql
+++ data/sql/updates/pending_db_world/rev_1633758521973835002.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633758521973835002');
 
-DELETE FROM conditions WHERE SourceTypeOrReferenceId = 13 AND SourceEntry = 29866;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` = 29866;
