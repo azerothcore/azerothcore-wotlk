ac544592a9fdaf0e5e10d960d4880d074f32f15f
Nyeriah <sarah.trysan@live.com>
2021-10-11 08:12:55 -0300

    err

--- data/sql/updates/pending_db_world/rev_1633950727186349900.sql
+++ data/sql/updates/pending_db_world/rev_1633950727186349900.sql
@@ -1,4 +1,4 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633867897245761100');
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633950727186349900');
 
 -- Server-side spell 'Summon Darrowshire Poltergeinst (DND)'
 UPDATE `spell_dbc` SET `Effect_1` = 28, `EffectMiscValueB_1` = 64 WHERE `id` = 17694;
