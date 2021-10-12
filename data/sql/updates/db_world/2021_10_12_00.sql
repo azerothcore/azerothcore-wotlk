805826735b1c26a804ae5867666555dfd0009331
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-12 09:08:08 +0000

    chore(DB): import pending files
    Referenced commit(s): 0f5c8887ec1e8477bca50cdb82335e8372e849f9

--- data/sql/updates/db_world/2021_10_12_00.sql
+++ data/sql/updates/db_world/2021_10_12_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_10_38 -> 2021_10_12_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_10_38';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_10_38 2021_10_12_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1628024682672891800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1628024682672891800');
 
 -- spell id 9205, 50% chance to reset threat on melee attack done
@@ -20,3 +36,13 @@ INSERT INTO `creature_template_addon` VALUES (9680, 0, 0, 0, 0, 0, 0, '13767 341
 
 -- 10% chance to reset threat  on melee attack done
 UPDATE `creature_template_addon` SET `auras`='25592' WHERE  `entry`=15323;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_12_00' WHERE sql_rev = '1628024682672891800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
