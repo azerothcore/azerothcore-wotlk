1bfac713f9066dced62bc94a7e49ea38b3321fcc
syssneck <68868567+syssneck@users.noreply.github.com>
2021-10-12 17:22:38 +0200

    now it's correct

--- data/sql/updates/pending_db_world/rev_1634048043586591327.sql
+++ data/sql/updates/pending_db_world/rev_1634048043586591327.sql
@@ -1,6 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634048043586591327');
 
 -- Fixes disenchanting loot for Dusty Mail Boots
-DELETE FROM `disenchant_loot_template` WHERE (`Entry` = 7) AND (`Item` IN (11137, 11174, 11177));
-INSERT INTO `disenchant_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(7, 11177, 0, 100, 0, 1, 1, 1, 1, 'Small Radiant Shard');
+UPDATE `item_template` SET `DisenchantID` = 45 WHERE `entry` = 19509;
