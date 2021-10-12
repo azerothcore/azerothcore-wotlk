4eea7a162759be765c94a0cafc9eb9c69d0aaeba
Banehammer <92228379+Banehammer@users.noreply.github.com>
2021-10-12 20:36:08 +0200

    Update data/sql/updates/pending_db_world/rev_1634061953963631484.sql
    Co-authored-by: Nefertumm <nefertum.dev@protonmail.com>

--- data/sql/updates/pending_db_world/rev_1634061953963631484.sql
+++ data/sql/updates/pending_db_world/rev_1634061953963631484.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634061953963631484');
 
-UPDATE `acore_world`.`quest_template_addon` SET `PrevQuestID`='0' WHERE  `ID`=5041;
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE  `ID` = 5041;
