d92c9d5551f7c8b80df29ec3dbb4f62899de8b21
Ragelezz <raduneculaulostone@gmail.com>
2021-10-12 20:12:07 +0300

    fix typo for Argent Dawn Commission sql file

--- data/sql/updates/pending_db_world/rev_1633781441009417000.sql
+++ data/sql/updates/pending_db_world/rev_1633781441009417000.sql
@@ -1,8 +1,15 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633781441009417000');
 
+
+
 -- Target: Felstone Field
-UPDATE `quest_template` SET `RewardFactionOverride2`='50000' WHERE `ID`=5229;
+
+UPDATE `quest_template` SET `RewardFactionOverride2`=50000 WHERE `ID`=5229;
+
 -- Dead Man's Plea
-UPDATE `quest_template` SET `RewardFactionOverride1`='100000' WHERE `ID`=8945;
+
+UPDATE `quest_template` SET `RewardFactionOverride1`=100000 WHERE `ID`=8945;
+
 -- Above and Beyond
-UPDATE `quest_template` SET `RewardFactionOverride1`='100000' WHERE  `ID`=5263;
+
+UPDATE `quest_template` SET `RewardFactionOverride1`=100000 WHERE `ID`=5263;
