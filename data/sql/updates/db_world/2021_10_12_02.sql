a4216a6a3f0e4730b5489eec7806866a95c8df5d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-12 12:52:18 +0000

    chore(DB): import pending files
    Referenced commit(s): cec360b3b374255fb1326a6d183db822c78ac468

--- data/sql/updates/db_world/2021_10_12_02.sql
+++ data/sql/updates/db_world/2021_10_12_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_12_01 -> 2021_10_12_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_12_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_12_01 2021_10_12_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633365891402740400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633365891402740400');
 
 -- Alliance only(5401)
@@ -23,3 +39,13 @@ INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
 DELETE FROM `creature_questender` WHERE (`quest` = 5503) AND (`id` IN (10839, 11039));
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES
 (11039, 5503);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_12_02' WHERE sql_rev = '1633365891402740400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
