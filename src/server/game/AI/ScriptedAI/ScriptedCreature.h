14b4e101f1dca5f1cb04a191fc64afaa362dcf59
acidmanifesto <joshua.lee.betts@gmail.com>
2021-10-12 09:01:18 -0400

    Update src/server/game/AI/ScriptedAI/ScriptedCreature.h
    Co-authored-by: Kargatum <dowlandtop@yandex.com>

--- src/server/game/AI/ScriptedAI/ScriptedCreature.h
+++ src/server/game/AI/ScriptedAI/ScriptedCreature.h
@@ -506,4 +506,5 @@ GameObject* GetClosestGameObjectWithEntry(WorldObject* source, uint32 entry, flo
 void GetCreatureListWithEntryInGrid(std::list<Creature*>& list, WorldObject* source, uint32 entry, float maxSearchRange);
 void GetGameObjectListWithEntryInGrid(std::list<GameObject*>& list, WorldObject* source, uint32 entry, float maxSearchRange);
 void GetDeadCreatureListInGrid(std::list<Creature*>& list, WorldObject* source, float maxSearchRange, bool alive = false);
+
 #endif // SCRIPTEDCREATURE_H_
