3cfb02fcae238496719578ff0e84e7c66904dbd2
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-13 22:59:27 +0000

    chore(DB): import pending files
    Referenced commit(s): 21e67bae0ccf800adba02e3110ce9df0a6706a64

--- data/sql/updates/db_world/2021_10_13_04.sql
+++ data/sql/updates/db_world/2021_10_13_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_13_03 -> 2021_10_13_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_13_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_13_03 2021_10_13_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1625055681072662039'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1625055681072662039');
 
 -- Old Grizzlegut ID 5352, speed_walk to 1, MovementType to 1, wander_distance to 15, 
@@ -35,3 +51,13 @@ DELETE FROM `creature` WHERE `id` = 1492 AND `guid` = 2634;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (2634, 1492, 0, 0, 0, 1, 1, 10039, 0, -14135.3, -137.275, -8.33448, 3.85509, 360, 15, 0, 6604, 0, 1, 0, 0, 0, '', 0);
 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_13_04' WHERE sql_rev = '1625055681072662039';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
