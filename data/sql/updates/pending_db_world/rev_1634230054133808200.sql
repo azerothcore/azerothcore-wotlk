cc9c4647d41cd72df00cc635a4fe60cd0b0df8ec
Ragelezz <raduneculaulostone@gmail.com>
2021-10-14 19:49:25 +0300

    fix(DB\Quest):Make 2 Hallow's End quests not repeatable

---
+++ data/sql/updates/pending_db_world/rev_1634230054133808200.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634230054133808200');
+
+
+UPDATE `quest_template_addon` SET `SpecialFlags`=0 WHERE `ID`=8312;
+UPDATE `quest_template_addon` SET `SpecialFlags`=0 WHERE `ID`=1657;
