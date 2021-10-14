5d36c94a2d24aa819c7f3c54e3e8cd83b686c7c7
jestermaniac <88903576+jestermaniac@users.noreply.github.com>
2021-10-14 16:48:37 +0200

    fix(CORE/Unit): Fix controllable guardians evading when not posed to be (#7959)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9713,8 +9713,7 @@ bool Unit::Attack(Unit* victim, bool meleeAttack)
     // set position before any AI calls/assistance
     //if (GetTypeId() == TYPEID_UNIT)
     //    ToCreature()->SetCombatStartPosition(GetPositionX(), GetPositionY(), GetPositionZ());
-
-    if (creature && !IsControllableGuardian())
+    if (creature && !IsControlledByPlayer())
     {
         // should not let player enter combat by right clicking target - doesn't helps
         SetInCombatWith(victim);
