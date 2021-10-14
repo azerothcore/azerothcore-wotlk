13db23200f62cfacee93279455b108223c482d6b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-14 12:10:44 +0000

    chore(DB): import pending files
    Referenced commit(s): 72e3ac60f50998a0f57cdf8d63717e1a70f424ee

--- data/sql/updates/db_world/2021_10_14_05.sql
+++ data/sql/updates/db_world/2021_10_14_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_14_04 -> 2021_10_14_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_14_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_14_04 2021_10_14_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633953386512414400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633953386512414400');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10040;
@@ -13,3 +29,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (10041, 0, 0, 0, 0, 0, 100, 0, 6000, 6000, 24000, 24000, 0, 11, 8137, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Gorishi Hive Queen - In Combat - Cast \'Silithid Pox\''),
 (10041, 0, 1, 0, 0, 0, 100, 0, 8000, 12000, 15000, 15000, 0, 11, 7992, 32, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Gorishi Hive Queen - In Combat - Cast \'Slowing Poison\''),
 (10041, 0, 2, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 21, 30, 0, 0, 0, 0, 0, 0, 0, 'Gorishi Hive Queen - On Just Summoned - Start Attacking');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_14_05' WHERE sql_rev = '1633953386512414400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
--- data/sql/updates/pending_db_world/rev_1633980128389704900.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633980128389704900');
-
-DELETE FROM `smart_scripts` WHERE `action_type`=11 AND `action_param1` IN (1604,13496);
