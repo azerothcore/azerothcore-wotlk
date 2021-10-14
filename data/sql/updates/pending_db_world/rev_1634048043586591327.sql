3986a019ba9bc720c784aaada50bf56f3fc76a55
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-14 22:59:35 +0000

    chore(DB): import pending files
    Referenced commit(s): b8f67ae58f4c30a26c4157bfd896060f6a708392

--- data/sql/updates/pending_db_world/rev_1634048043586591327.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634048043586591327');
-
--- Fixes disenchanting loot for Dusty Mail Boots
-UPDATE `item_template` SET `DisenchantID` = 45 WHERE `entry` = 19509;
--- data/sql/updates/pending_db_world/rev_1634130719161646149.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634130719161646149');
-
--- Delete Plans: Corruption from various NPCs
-DELETE FROM `creature_loot_template` WHERE `Entry` IN (10398, 10399, 10400, 10406, 10407, 10408, 10409, 10412, 10413, 10463, 10464) AND `Item` = 12830;
-
--- Delete Plans: Corruption from RLT 24709
-DELETE FROM `reference_loot_template` WHERE `Entry` = 24709 AND `Item` = 12830;
-
