a458f224911b8492e90a3f612d4caffb9d287ff5
Ragelezz <69433539+Ragelezz@users.noreply.github.com>
2021-10-15 11:15:48 +0300

    Update data/sql/updates/pending_db_world/rev_1634230054133808200.sql
    Co-authored-by: IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>

--- data/sql/updates/pending_db_world/rev_1634230054133808200.sql
+++ data/sql/updates/pending_db_world/rev_1634230054133808200.sql
@@ -1,5 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634230054133808200');
 
 
-UPDATE `quest_template_addon` SET `SpecialFlags`=0 WHERE `ID`=8312;
-UPDATE `quest_template_addon` SET `SpecialFlags`=0 WHERE `ID`=1657;
+UPDATE `quest_template_addon` SET `SpecialFlags`=0 WHERE `ID` IN(8312,1657);
