491d3237ce682e292d2cda213f9f5121e49d1e15
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-16 00:40:27 +0000

    chore(DB): import pending files
    Referenced commit(s): a56f4fe17c72cdfbaa1c8d499a3a39ab940e8bfd

--- data/sql/updates/pending_db_world/rev_1634042303141509213.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634042303141509213');
-
--- Removes invalid Firebloom drops from various NPCs
-DELETE FROM `creature_loot_template` WHERE `item` = 4625 AND `entry` NOT IN (1812, 1851, 5481, 5485, 5490, 5881, 6509, 6510, 6511, 6512, 6517, 6518, 
-6519, 6527, 7100, 7101, 7139, 7584, 8384, 11462, 12219, 12220, 12223, 12224, 13022, 13141, 13142);
