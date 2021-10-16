c4cbd6161d25701c28ac03c19275782f0c72ffa8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-16 14:26:24 +0000

    chore(DB): import pending files
    Referenced commit(s): ba719f0feb70827beaebb40ff5095f75e2fd4302

--- data/sql/updates/db_world/2021_10_16_05.sql
+++ data/sql/updates/db_world/2021_10_16_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_16_04 -> 2021_10_16_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_16_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_16_04 2021_10_16_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633773844859273100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633773844859273100');
 
 UPDATE `creature_template` SET `faction`=1998, `InhabitType`=3 WHERE `entry`=23543;
@@ -12,3 +28,13 @@ INSERT INTO `creature_text` VALUES
 (23543,4,0,'Fire consumes! You\'ve tried and failed. Let there be no doubt, justice prevailed!',14,0,100,0,0,11967,22026,0,'Shade of Horseman Talk 4'),
 (23543,5,0,'My flames have died, left not a spark! I shall send you now to the lifeless dark!',14,0,100,0,0,11968,22027,0,'Shade of Horseman Talk 5'),
 (23543,6,0,'So eager you are, for my blood to spill. Yet to vanquish me, \'tis my head you must kill!',14,0,100,0,0,11969,22757,0,'Shade of Horseman Talk 6');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_16_05' WHERE sql_rev = '1633773844859273100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
