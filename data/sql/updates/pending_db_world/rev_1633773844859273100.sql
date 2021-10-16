8bcc5786796a786dca368d8c97716e696ce1a527
Francesco Borzì <borzifrancesco@gmail.com>
2021-10-16 11:26:44 +0200

    Update rev_1633773844859273100.sql

--- data/sql/updates/pending_db_world/rev_1633773844859273100.sql
+++ data/sql/updates/pending_db_world/rev_1633773844859273100.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633773844859273100');
 
-UPDATE `creature_template` SET `faction`=1998, `InhabitType`=3, WHERE `entry`=23543;
+UPDATE `creature_template` SET `faction`=1998, `InhabitType`=3 WHERE `entry`=23543;
 UPDATE `creature_template_addon` SET `Mount`=0 WHERE `entry`=23543;
 
 DELETE FROM `creature_text` WHERE `CreatureID`=23543;
