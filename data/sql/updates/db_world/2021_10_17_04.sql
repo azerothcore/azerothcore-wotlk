c83aaab257f20af1a9bb4b43f03a902bb22888c5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-17 23:51:15 +0000

    chore(DB): import pending files
    Referenced commit(s): 9aab7e8fd3f755888ba476117c9ff7f411b572ba

--- data/sql/updates/db_world/2021_10_17_04.sql
+++ data/sql/updates/db_world/2021_10_17_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_17_03 -> 2021_10_17_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_17_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_17_03 2021_10_17_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1634256857773109500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634256857773109500');
 
 -- Quest: You Survived! - ID: 9279
@@ -150,3 +166,13 @@ DELETE FROM `quest_offer_reward_locale` WHERE `ID`=9799 AND `locale` IN ('esES',
 INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES
 (9799, 'esES', 'Estos especímenes tienen buen aspecto. Gracias por ocuparte del trabajo de campo. Cuando los haya analizado, podré determinar qué hay que hacer para restaurar los campos.', 18019),
 (9799, 'esMX', 'Estos especímenes tienen buen aspecto. Gracias por ocuparte del trabajo de campo. Cuando los haya analizado, podré determinar qué hay que hacer para restaurar los campos.', 18019);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_17_04' WHERE sql_rev = '1634256857773109500';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
