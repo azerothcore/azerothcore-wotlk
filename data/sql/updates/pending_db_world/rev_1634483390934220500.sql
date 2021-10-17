67ebf7b0923caa986ff4b154ac58779e9b2597f7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-17 23:56:14 +0000

    chore(DB): import pending files
    Referenced commit(s): 429823a76491f4479318db6f0a522e45f3a542eb

--- data/sql/updates/pending_db_world/rev_1634483390934220500.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634483390934220500');
-DELETE FROM `creature` WHERE  `guid` = 247693;
--- data/sql/updates/pending_db_world/rev_1634485959723104200.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634485959723104200');
-UPDATE `gossip_menu_option_locale` SET `OptionText`='Danke, Eisenborke. Wir öffnen Euch die Tür.' WHERE  `MenuID`=5602 AND `OptionID`=0 AND `Locale`='deDE';
