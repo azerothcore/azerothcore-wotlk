751d382972ca5dbee8ea076f64b9cf667c3c04d4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-19 15:10:32 +0000

    chore(DB): import pending files
    Referenced commit(s): cc417df03e31d7b9eb1bf94898bbbe03c68ee974

--- data/sql/updates/pending_db_world/rev_1634394523910362900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634394523910362900');
-
- -- Forsaken Thug
-UPDATE `creature_template_addon` SET `mount` = 0, `bytes2` = 4097 WHERE `entry` = 3734;
-UPDATE `creature_addon` SET `mount` = 0 WHERE `guid` IN (32905, 32906, 32907, 32908, 32909, 32910);
