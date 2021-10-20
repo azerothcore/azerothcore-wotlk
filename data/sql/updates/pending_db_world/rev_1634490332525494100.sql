39bfe880f49fd0a4b62fb7a361df8362a44db555
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-20 16:46:49 +0000

    chore(DB): import pending files
    Referenced commit(s): aab110c1422621b5e707de384aa7781d1fa456df

--- data/sql/updates/pending_db_world/rev_1634490332525494100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634490332525494100');
-
-UPDATE `smart_scripts` SET `action_param2` = 0 WHERE `entryorguid` IN (5319, 5317) AND `source_type` = 0 AND `id` IN (0, 1);
-
