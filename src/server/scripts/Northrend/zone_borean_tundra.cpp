a0200795b1ca5a8fbacbabad9bfdb2843cb961ae
Malcrom <malcromdev@gmail.com>
2021-10-20 13:55:36 -0300

    Update zone_borean_tundra.cpp

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -1158,7 +1158,7 @@ public:
             DoMeleeAttackIfReady();
         }
 
-        void sGossipSelect(Player* player, uint32 menuId, uint32 gossipListId) override
+        void sGossipSelect(Player* player, uint32 /*menuId*/, uint32 gossipListId) override
         {
             if (gossipListId == 0)
             {
