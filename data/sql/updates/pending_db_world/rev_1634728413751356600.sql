f99b86ea26c868478c8dee5d1719d759ebdc8401
Nyeriah <sarah.trysan@live.com>
2021-10-20 08:14:14 -0300

    1

--- data/sql/updates/pending_db_world/rev_1634728413751356600.sql
+++ data/sql/updates/pending_db_world/rev_1634728413751356600.sql
@@ -1,4 +1,4 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634723511838368600');
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634728413751356600');
 
 -- Condition for source Gossip menu condition type Quest taken
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=14 AND `SourceGroup`=10204 AND `SourceEntry` IN (14169,14170) AND `SourceId`=0;
@@ -14,7 +14,7 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 
 -- Corastrasza SAI
 SET @ENTRY := 32548;
-UPDATE `creature_template` SET `ScriptName`='' WHERE `entry`=@ENTRY;
+UPDATE `creature_template` SET `AIName`='SmartAI', `ScriptName`='' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,2,62,0,100,0,10204,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,'Corastrasza - Gossip option select 0 - Close Gossip for player'),
