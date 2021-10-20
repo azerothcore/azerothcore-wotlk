07484d04390efc853e96603241d7c942b18ab7e2
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-20 08:35:08 +0000

    chore(DB): import pending files
    Referenced commit(s): a1126c18c4c8cd85f06ae04fe7170c4a64a6ba2d

--- data/sql/updates/pending_db_world/rev_1634540496312439970.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634540496312439970');
-
--- Updates the quest completion text for the Art of the Armorsmith (Alliance) to Ironforge
-UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Grumnus Steelshaper in Ironforge.' WHERE `ID` = 5283;
