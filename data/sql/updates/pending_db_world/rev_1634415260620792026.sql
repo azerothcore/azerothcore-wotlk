fd0314b873b5be1c0d5cae665eb8814d5a8aa00b
Banehammer <92228379+Banehammer@users.noreply.github.com>
2021-10-21 00:06:31 +0200

    fix(DB/Gossip) Fix Tirion's gossip order for to the Redemption quest (#8568)

---
+++ data/sql/updates/pending_db_world/rev_1634415260620792026.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634415260620792026');
+
+UPDATE `gossip_menu` SET `TextID` = 4493 WHERE `MenuID` = 3681 AND `TextID` = 4495;
+UPDATE `gossip_menu` SET `TextID` = 4495 WHERE `MenuID` = 3683 AND `TextID` = 4493;
+
