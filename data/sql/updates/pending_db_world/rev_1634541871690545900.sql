6be6bcc5784ac6bf445744905295cb04f4127137
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-21 08:46:11 +0000

    chore(DB): import pending files
    Referenced commit(s): e75cc1058d2ac1dbbf2bda3c95b8ea02d3505acf

--- data/sql/updates/pending_db_world/rev_1634541871690545900.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634541871690545900');
-
-UPDATE `smart_scripts` SET `target_type` = 7, `comment` = 'Innkeeper Allison - On gossip option 0 select - Player cast Trick or Treat on Invoker' WHERE `entryorguid` = 6740 AND `source_type` = 0 AND `id` = 1;
