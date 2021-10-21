a009e935ac42163113f8ca3a7918f336e5c1bb44
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-21 19:40:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 6798b907b9c451dec48abafc3403de4b35644769

--- data/sql/updates/db_world/2021_10_21_10.sql
+++ data/sql/updates/db_world/2021_10_21_10.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_21_09 -> 2021_10_21_10
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_21_09';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_21_09 2021_10_21_10 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1634143017202196500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634143017202196500');
 
 -- add doZOneInCombat for the bosses
@@ -28,3 +44,13 @@ UPDATE `creature_template` SET  `unit_flags` = `unit_flags`&~(256) WHERE `entry`
 DELETE FROM `creature_text` WHERE `CreatureID` = 9039;
 INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`COMMENT`) VALUES
 (9039,0,0,'You have challenged the Seven, and now you will die!',12,0,100,0,0,0,4894,0,'start event');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_21_10' WHERE sql_rev = '1634143017202196500';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
