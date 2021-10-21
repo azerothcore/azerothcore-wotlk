b4f535dd1bea9728bfc3f7c49d4bfd6019ed2bdf
Nefertumm <nefertum.dev@protonmail.com>
2021-10-21 05:36:55 -0300

    Update rev_1634802877481310300.sql

--- data/sql/updates/pending_db_world/rev_1634802877481310300.sql
+++ data/sql/updates/pending_db_world/rev_1634802877481310300.sql
@@ -1,6 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634802877481310300');
 
-
-
 UPDATE `gameobject` SET `orientation`=0.27, `position_z`=131.54, `position_y`=-905.33, `position_x`=-7872.48 WHERE `guid`=72780;
-
