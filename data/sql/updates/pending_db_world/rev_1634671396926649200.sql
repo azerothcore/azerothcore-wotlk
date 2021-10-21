8c20978449d1bc92a65ab04d02d71e381f2e0691
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-21 18:57:23 +0000

    chore(DB): import pending files
    Referenced commit(s): ad73b564bc6f24c14dd3077fcea9a19c6ce06051

--- data/sql/updates/pending_db_world/rev_1634671396926649200.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634671396926649200');
-
-DELETE FROM `spell_script_names` WHERE `spell_id`=58984;
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=58984;
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
-(58984,59646,0,'Shadowmeld: Sanctuary'),
-(58984,62196,0,'Shadowmeld: Force deselect');
