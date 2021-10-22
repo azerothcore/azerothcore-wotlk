1101f9dd2a8f7c049b728ddb5302997e00dc3038
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-22 23:20:45 +0000

    chore(DB): import pending files
    Referenced commit(s): 73a55e02516f67ddc4ed0d956507a8e11e3dc007

--- data/sql/updates/pending_db_world/rev_1634407932513021400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634407932513021400');
-
-UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'boss_baroness_anastari' WHERE `entry` = 10436;
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 10436 AND `source_type` = 0;
