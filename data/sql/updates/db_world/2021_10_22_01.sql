97d13d7ff30ed8c3abe65681cbb868b9f98b5557
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-22 11:38:34 +0000

    chore(DB): import pending files
    Referenced commit(s): dad24b2d655480271bf9b353a17953f6069196a4

--- data/sql/updates/db_world/2021_10_22_01.sql
+++ data/sql/updates/db_world/2021_10_22_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_22_00 -> 2021_10_22_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_22_00 2021_10_22_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1634699890992468941'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634699890992468941');
 
 -- fixed reference_loot_template id 44011 and 44012
@@ -129,3 +145,13 @@ insert into `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 ('435','24067','24067','0.5','0','1','1','1','1','Blackrock Champion - (ReferenceTable)'),
 ('435','24068','24068','1','0','1','1','1','1','Blackrock Champion - (ReferenceTable)'),
 ('435','24069','24069','0.5','0','1','1','1','1','Blackrock Champion - (ReferenceTable)');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_22_01' WHERE sql_rev = '1634699890992468941';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
