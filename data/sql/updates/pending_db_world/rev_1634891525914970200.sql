d66d50d32aa6b9e184d71c7a3070a1c47d5147cc
Nefertumm <nefertum.dev@protonmail.com>
2021-10-22 05:54:28 -0300

    Dumb

--- data/sql/updates/pending_db_world/rev_1634891525914970200.sql
+++ data/sql/updates/pending_db_world/rev_1634891525914970200.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634891525914970200');
 
-DELETE `spell_script_names` WHERE `spell_id` IN (22563, 22564) AND `ScriptName` = 'spell_item_recall';
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (22563, 22564) AND `ScriptName` = 'spell_item_recall';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (22563, 'spell_item_recall'),
 (22564, 'spell_item_recall');
