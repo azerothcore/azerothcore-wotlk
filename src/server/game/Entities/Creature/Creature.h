6ec03f204b04e9b8fbbba5025b64d529adba800f
Nyeriah <sarah.trysan@live.com>
2021-10-23 18:38:59 -0300

    feat(Core/SAI): Allow forcing creatures/gameobjects's respawn timers when using SMART_ACTION_FORCE_DESPAWN

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -267,7 +267,8 @@ public:
 
     void RemoveCorpse(bool setSpawnTime = true, bool skipVisibility = false);
 
-    void DespawnOrUnsummon(uint32 msTimeToDespawn = 0);
+    void DespawnOrUnsummon(Milliseconds msTimeToDespawn, Seconds forcedRespawnTimer);
+    void DespawnOrUnsummon(uint32 msTimeToDespawn = 0) { DespawnOrUnsummon(Milliseconds(msTimeToDespawn), 0s); };
     void DespawnOnEvade();
     void RespawnOnEvade();
 
@@ -438,7 +439,7 @@ protected:
     bool CanAlwaysSee(WorldObject const* obj) const override;
 
 private:
-    void ForcedDespawn(uint32 timeMSToDespawn = 0);
+    void ForcedDespawn(uint32 timeMSToDespawn = 0, Seconds forcedRespawnTimer = 0s);
 
     [[nodiscard]] bool CanPeriodicallyCallForAssistance() const;
 
@@ -483,11 +484,12 @@ private:
 class ForcedDespawnDelayEvent : public BasicEvent
 {
 public:
-    ForcedDespawnDelayEvent(Creature& owner) : BasicEvent(), m_owner(owner) { }
+    ForcedDespawnDelayEvent(Creature& owner, Seconds respawnTimer) : BasicEvent(), m_owner(owner), m_respawnTimer(respawnTimer) { }
     bool Execute(uint64 e_time, uint32 p_time) override;
 
 private:
     Creature& m_owner;
+    Seconds const m_respawnTimer;
 };
 
 #endif
