6838bcb70073ac5f0ccaf56cc95def2bc7ce22b8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-23 08:17:20 +0000

    chore(DB): import pending files
    Referenced commit(s): bc9473482e94d0fe3766fa42eeca4c796c93a8b1

--- data/sql/updates/db_world/2021_10_23_00.sql
+++ data/sql/updates/db_world/2021_10_23_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_22_05 -> 2021_10_23_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_22_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_22_05 2021_10_23_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1622929985554698000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1622929985554698000');
 
 DELETE FROM `acore_string` WHERE `entry` BETWEEN 1500 AND 1515;
@@ -35,3 +51,13 @@ UPDATE `command` SET `security`='3' WHERE `name` = 'debug anim';
 UPDATE `command` SET `security`='4' WHERE `name` = 'modify spell';
 UPDATE `command` SET `security`='3' WHERE `name` = 'npc set allowmove';
 UPDATE `command` SET `security`='3' WHERE `name` LIKE '%unban%';
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_23_00' WHERE sql_rev = '1622929985554698000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
