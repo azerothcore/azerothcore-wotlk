d2c742a2ec017b44bdd0c5a286b852c9594fe339
Nyeriah <sarah.trysan@live.com>
2021-10-22 23:11:46 -0300

    fix(DB/SAI): Correct Primal Ooze's invisibility target, so they no longer turn players invisible

---
+++ data/sql/updates/pending_db_world/rev_1634955021318894500.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634955021318894500');
+
+UPDATE `smart_scripts` SET `target_type` = 1 WHERE `entryorguid` = 6557 AND `source_type` = 0 AND `id` = 3;
