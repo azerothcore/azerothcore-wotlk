86ed73c63dc83cb93142c30196d8d6a4a55fd8f4
Ragelezz <raduneculaulostone@gmail.com>
2021-10-24 17:45:12 +0300

    faction adjusted accordingly to sniffs

--- data/sql/updates/pending_db_world/rev_1633782254647082100.sql
+++ data/sql/updates/pending_db_world/rev_1633782254647082100.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633782254647082100');
 
-UPDATE `creature_template` SET `faction`='11' WHERE `entry`=10824;
+UPDATE `creature_template` SET `faction`=1576 WHERE `entry`=10824;
