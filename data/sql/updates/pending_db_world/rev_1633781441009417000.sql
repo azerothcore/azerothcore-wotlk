affb6126abe7eac131ca4d95b61cc92abe792fb7
Ragelezz <raduneculaulostone@gmail.com>
2021-10-24 17:43:49 +0300

    remove non-pr related sql files

--- data/sql/updates/pending_db_world/rev_1633781441009417000.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633781441009417000');
-
--- Target: Felstone Field
-UPDATE `quest_template` SET `RewardFactionOverride2`='50000' WHERE `ID`=5229;
--- Dead Man's Plea
-UPDATE `quest_template` SET `RewardFactionOverride1`='100000' WHERE `ID`=8945;
--- Above and Beyond
-UPDATE `quest_template` SET `RewardFactionOverride1`='100000' WHERE  `ID`=5263;
