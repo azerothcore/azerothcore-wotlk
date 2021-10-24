a98c412296a3a1639cdf59e1e11edf5a3b71b864
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-10-24 04:19:03 +0200

    fix(Scripts/Quests): "Let the fires come" and "Stop the fires" should… (#8565)
    Fixes #8527

---
+++ data/sql/updates/pending_db_world/rev_1634412327773100900.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634412327773100900');
+
+UPDATE `quest_template_addon` SET `ExclusiveGroup`=12135 WHERE `id`=12135;
+UPDATE `quest_template_addon` SET `ExclusiveGroup`=12135 WHERE `id`=11131;
+UPDATE `quest_template_addon` SET `ExclusiveGroup`=12139 WHERE `id`=12139;
+UPDATE `quest_template_addon` SET `ExclusiveGroup`=12139 WHERE `id`=11219;
