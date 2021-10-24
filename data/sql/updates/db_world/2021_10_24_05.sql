993570fa8b7ecfcede22e98eb7f13e3f67c6157a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-24 03:28:16 +0000

    chore(DB): import pending files
    Referenced commit(s): c0b52da8901793933c78104d459a81f3c6883f2c

--- data/sql/updates/db_world/2021_10_24_05.sql
+++ data/sql/updates/db_world/2021_10_24_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_24_04 -> 2021_10_24_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_24_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_24_04 2021_10_24_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1634824422202732820'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634824422202732820');
 
 -- *** Quest "Abduction" ***
@@ -18,3 +34,13 @@ UPDATE `creature_template` SET `minlevel`=69, `maxlevel`=70, `unit_flags`=32768,
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceGroup`=1 AND `SourceEntry`=45735 AND `SourceId`=0;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 1, 45735, 0, 0, 31, 0, 3, 25474, 0, 0, 0, 0, '', 'Spell Arcane Chains: Chain Channel II (effect 0) will hit the target of the spell if target is unit Captured Beryl Sorcerer.');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_24_05' WHERE sql_rev = '1634824422202732820';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
