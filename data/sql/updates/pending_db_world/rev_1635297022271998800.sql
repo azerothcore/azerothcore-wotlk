166d0adf06c707862882c43621870c47b718417b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-27 01:17:01 +0000

    chore(DB): import pending files
    Referenced commit(s): 041b327c800ae59e66a0f61dec132edc4248f9d8

--- data/sql/updates/pending_db_world/rev_1635297022271998800.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635297022271998800');
-
-DELETE FROM `gameobject_template` WHERE `entry` = 195641;
-INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES
-(195641, 6, 0, 'Brazier', '', '', '',  1, 0, 1, 1, 7897, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', -18019);
