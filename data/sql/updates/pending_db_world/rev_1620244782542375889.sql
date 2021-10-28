ce8a014e0051bff20f70bf62e27dbd12c59db0ad
sanctum32 <andriuspel@gmail.com>
2021-10-28 17:42:05 +0300

    DB/MoltenCore: Added linked formation for Core Hounds

--- data/sql/updates/pending_db_world/rev_1620244782542375889.sql
+++ data/sql/updates/pending_db_world/rev_1620244782542375889.sql
@@ -121,3 +121,60 @@ UPDATE `creature_template_addon` SET `auras`='13879 18943 20556' WHERE  `entry`=
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceGroup`=7 AND `SourceEntry`=20544 AND `SourceId`=0 AND `ElseGroup`=0 AND `ConditionTypeOrReference`=31 AND `ConditionTarget`=0 AND `ConditionValue1`=3 AND `ConditionValue2`=11672 AND `ConditionValue3`=0;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 7, 20544, 0, 0, 31, 0, 3, 11672, 0, 0, 0, 0, '', 'Spell Attract Rager should target only Core Rager');
+
+-- Core Hound formations
+-- GROUP 1
+-- 56638, 56635, 56637, 56634, 56636
+set @leader := 56638;
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (@leader, 56635, 56637, 56634, 56636);
+DELETE FROM `creature_formations` WHERE `leaderGUID` IN (@leader, 56635, 56637, 56634, 56636);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@leader, @leader, 0, 0, 2, 0, 0),
+(@leader, 56635, 0, 0, 3, 0, 0),
+(@leader, 56637, 0, 0, 3, 0, 0),
+(@leader, 56634, 0, 0, 3, 0, 0),
+(@leader, 56636, 0, 0, 3, 0, 0);
+-- GROUP 2
+-- 56629, 56630, 56632, 56633, 56631
+set @leader := 56629;
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (@leader, 56630, 56632, 56633, 56631);
+DELETE FROM `creature_formations` WHERE `leaderGUID` IN (@leader, 56630, 56632, 56633, 56631);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@leader, @leader, 0, 0, 2, 0, 0),
+(@leader, 56630, 0, 0, 3, 0, 0),
+(@leader, 56632, 0, 0, 3, 0, 0),
+(@leader, 56633, 0, 0, 3, 0, 0),
+(@leader, 56631, 0, 0, 3, 0, 0);
+-- GROUP 3
+-- 56639, 56641, 56642, 56643, 56640
+set @leader := 56639;
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (@leader, 56641, 56642, 56643, 56640);
+DELETE FROM `creature_formations` WHERE `leaderGUID` IN (@leader, 56641, 56642, 56643, 56640);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@leader, @leader, 0, 0, 2, 0, 0),
+(@leader, 56641, 0, 0, 3, 0, 0),
+(@leader, 56642, 0, 0, 3, 0, 0),
+(@leader, 56643, 0, 0, 3, 0, 0),
+(@leader, 56640, 0, 0, 3, 0, 0);
+-- GROUP 4
+-- 56651, 56650, 56652, 56653, 56654
+set @leader := 56651;
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (@leader, 56650, 56652, 56653, 56654);
+DELETE FROM `creature_formations` WHERE `leaderGUID` IN (@leader, 56650, 56652, 56653, 56654);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@leader, @leader, 0, 0, 2, 0, 0),
+(@leader, 56650, 0, 0, 3, 0, 0),
+(@leader, 56652, 0, 0, 3, 0, 0),
+(@leader, 56653, 0, 0, 3, 0, 0),
+(@leader, 56654, 0, 0, 3, 0, 0);
+-- GROUP 5
+-- 56648, 56644, 56646, 56649, 56647
+set @leader := 56648;
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (@leader, 56644, 56646, 56649, 56647);
+DELETE FROM `creature_formations` WHERE `leaderGUID` IN (@leader, 56644, 56646, 56649, 56647);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@leader, @leader, 0, 0, 2, 0, 0),
+(@leader, 56644, 0, 0, 3, 0, 0),
+(@leader, 56646, 0, 0, 3, 0, 0),
+(@leader, 56649, 0, 0, 3, 0, 0),
+(@leader, 56647, 0, 0, 3, 0, 0);
