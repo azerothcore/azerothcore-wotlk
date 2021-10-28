9f6a1a721bfd050253c22eb0a7ec4e7f17ae600f
Ragelezz <raduneculaulostone@gmail.com>
2021-10-28 21:51:44 +0300

    set VerifiedBuild to 0

--- data/sql/updates/pending_db_world/rev_1635418346534500600.sql
+++ data/sql/updates/pending_db_world/rev_1635418346534500600.sql
@@ -1,6 +1,13 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635418346534500600');
 
+
+
 UPDATE `broadcast_text` SET `MaleText`='prepares a Great Feast!', `FemaleText`='prepares a Great Feast!' WHERE `ID`=31843;
+
 UPDATE `broadcast_text` SET `MaleText`='prepares a Small Feast!', `FemaleText`='prepares a Small Feast!' WHERE `ID`=31845;
+
 UPDATE `broadcast_text` SET `MaleText`='prepares a Fish Feast!', `FemaleText`='prepares a Fish Feast!' WHERE `ID`=31844;
+
 UPDATE `broadcast_text` SET `FemaleText`='prepares a Gigantic Feast!', `MaleText`='prepares a Gigantic Feast!' WHERE `ID`=31846;
+
+UPDATE `broadcast_text` SET `VerifiedBuild`='0' WHERE `ID` IN (31843,31845,31844,31846);
