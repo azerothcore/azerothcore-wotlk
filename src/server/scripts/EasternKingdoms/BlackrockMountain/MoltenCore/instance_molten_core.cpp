6629a642577ce417c065cce24a270aa108eb6a5d
sanctum32 <andriuspel@gmail.com>
2021-10-29 04:29:04 +0100

    Scripts/MoltenCore: attempt to fix another build issue

--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/instance_molten_core.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/instance_molten_core.cpp
@@ -4,14 +4,14 @@
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  */
 
+#include "ScriptMgr.h"
 #include "InstanceScript.h"
-#include "molten_core.h"
 #include "ObjectMgr.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
-#include "molten_core.h"
 #include "InstanceScript.h"
 #include "TemporarySummon.h"
+#include "molten_core.h"
 
 Position const MajordomoSummonPos = {759.542f, -1173.43f, -118.974f, 3.3048f};
 
