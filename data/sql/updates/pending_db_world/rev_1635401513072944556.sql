a84e8e479614e4f5f24f6fa37e3a4cfc1a11d90e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-30 14:54:46 +0000

    chore(DB): import pending files
    Referenced commit(s): 8f3c197f15ad42ce9e90e4418e5cdd6f2e9f48cf

--- data/sql/updates/pending_db_world/rev_1635401513072944556.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635401513072944556');
-
--- Updates quest 5057 (Past Endeavors) to be available to both factions
-UPDATE `quest_template` SET `AllowableRaces` = 0 WHERE (`ID` = 5057);
