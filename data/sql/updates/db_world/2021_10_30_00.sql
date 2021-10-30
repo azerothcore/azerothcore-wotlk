109672b7c505ec6bffc21b5ab0a5f105e01fb0a0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-30 01:01:24 +0000

    chore(DB): import pending files
    Referenced commit(s): 721601009768398c4462e51f6b7fa3f776c5158c

--- data/sql/updates/db_world/2021_10_30_00.sql
+++ data/sql/updates/db_world/2021_10_30_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_29_01 -> 2021_10_30_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_29_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_29_01 2021_10_30_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1635282917364687995'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635282917364687995');
 
 -- Condition for source Quest available condition type Quest taken
@@ -22,3 +38,13 @@ UPDATE `quest_template_addon` SET `PrevQuestID`=0 WHERE `ID`=1789;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=19 AND `SourceGroup`=0 AND `SourceEntry`=1647 AND `SourceId`=0;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (19, 0, 1647, 0, 0, 47, 0, 1646, 64, 0, 0, 0, 0, '', 'Quest The Symbol of Life available if quest The Tome of Divinity has been rewarded.');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_30_00' WHERE sql_rev = '1635282917364687995';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
