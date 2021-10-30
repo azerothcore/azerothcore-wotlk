308b26e76c7a98f7f5d389ae4587e76b861823a9
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-30 20:04:14 +0000

    chore(DB): import pending files
    Referenced commit(s): c08ad3e3c9c6fba4ec1f672d61227f993c36bfe6

--- data/sql/updates/pending_db_world/rev_1635493022954794719.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635493022954794719');
-
-UPDATE `command` SET `help`='Syntax: .npc whisper #playername #text\r\nMake the selected npc whisper #text to  #playername.' WHERE `name`='npc whisper';
