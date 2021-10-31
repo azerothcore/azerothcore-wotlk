9e2ec7591a12797bc518eb4549766717a5a1868c
Patou01 <youdontneed@this.com>
2021-10-31 18:08:40 +0100

    removes aura applied to waypoints

---
+++ data/sql/updates/pending_db_world/rev_1635700076222913000.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635700076222913000');
+
+DELETE FROM `creature_addon` WHERE (`guid` IN (137990));
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `isLarge`, `auras`) VALUES
+(137990, 1379900, 0, 0, 1, 0, 0, '0');
