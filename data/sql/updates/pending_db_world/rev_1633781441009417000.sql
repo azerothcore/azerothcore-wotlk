be7c4c9e4cc6e9235bf03c45a4eda12faee1749e
Ragelezz <69433539+Ragelezz@users.noreply.github.com>
2021-11-01 00:49:17 +0200

    fix(DB\Quest): Argent Dawn reward recalculated (#8370)

---
+++ data/sql/updates/pending_db_world/rev_1633781441009417000.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633781441009417000');
+
+-- Target: Felstone Field
+UPDATE `quest_template` SET `RewardFactionOverride2`=50000 WHERE `ID`=5229;
+
+-- Dead Man's Plea
+UPDATE `quest_template` SET `RewardFactionOverride1`=100000 WHERE `ID`=8945;
+
+-- Above and Beyond
+UPDATE `quest_template` SET `RewardFactionOverride1`=100000 WHERE `ID`=5263;
