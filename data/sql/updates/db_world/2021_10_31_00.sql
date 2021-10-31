8a139a073f564db445650c9bb90061d7a4e544ba
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-31 00:38:17 +0000

    chore(DB): import pending files
    Referenced commit(s): b7665c45c6d9df464e24e5e9226a88c2f3bcb6f1

--- data/sql/updates/db_world/2021_10_31_00.sql
+++ data/sql/updates/db_world/2021_10_31_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_10_30_08 -> 2021_10_31_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_10_30_08';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_10_30_08 2021_10_31_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1635441257434103531'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635441257434103531');
 
 -- Add SAI to Rousch
@@ -9,3 +25,13 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (1609000,9,0,0,0,0,100,0,0,0,0,0,0,90,8,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Rousch - OOC - Kneel'),
 (1609000,9,1,0,0,0,100,0,20000,20000,0,0,0,91,8,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Rousch - OOC - Stand'),
 (1609000,9,2,0,0,0,100,0,2000,2000,0,0,0,5,66,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Rousch - OOC - Salute');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_10_31_00' WHERE sql_rev = '1635441257434103531';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
