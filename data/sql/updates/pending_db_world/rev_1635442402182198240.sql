933eb5c4c1e264c0cdc3fe6e81f34cac6a32879c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-10-31 12:52:01 +0000

    chore(DB): import pending files
    Referenced commit(s): 334b3c5b92d03659720688655b6d330ad108a108

--- data/sql/updates/pending_db_world/rev_1635442402182198240.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635442402182198240');
-
--- Fix location of Darcy
-UPDATE `creature` SET `position_x`=-9218.02, `position_y`=-2148.62, `position_z`=64.3548, `orientation`=4.47915 WHERE `id`=379;
