3396a9da875a6b65aad035c47f562248ab79b11a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-01 13:36:58 +0000

    chore(DB): import pending files
    Referenced commit(s): 9fe49d8f1851785e40706618ac85a974cea69a7b

--- data/sql/updates/db_world/2021_11_01_01.sql
+++ data/sql/updates/db_world/2021_11_01_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_01_00 -> 2021_11_01_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_01_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_01_00 2021_11_01_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1634598950367001300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634598950367001300');
 
 -- Quest: The Missing Scout - ID: 9309
@@ -137,3 +153,13 @@ DELETE FROM `quest_offer_reward_locale` WHERE `ID`=10324 AND `locale` IN ('esES'
 INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES
 (10324, 'esES', '¡Maravilloso! Te estás convirtiendo en un gran cazador, $n.$B$B¿Te gustaría parecerte más a mí? ¡Pues puedes hacerlo! Cuando estabas cazando, hice estos objetos con algunas pieles que tenía por aquí. ¡Elige uno!', 18019),
 (10324, 'esMX', '¡Maravilloso! Te estás convirtiendo en un gran cazador, $n.$B$B¿Te gustaría parecerte más a mí? ¡Pues puedes hacerlo! Cuando estabas cazando, hice estos objetos con algunas pieles que tenía por aquí. ¡Elige uno!', 18019);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_01_01' WHERE sql_rev = '1634598950367001300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
