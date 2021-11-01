1adf6acd132ef47e8107c14bbd06b0042f2ad680
Nyeriah <sarah.trysan@live.com>
2021-11-01 12:47:18 -0300

    refactor(Scripts/UBRS): Use DoCastRandomVictim on The Beast's Immolate spell

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackrockSpire/boss_the_beast.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackrockSpire/boss_the_beast.cpp
@@ -84,8 +84,7 @@ public:
                         events.ScheduleEvent(EVENT_FLAME_BREAK, 10 * IN_MILLISECONDS);
                         break;
                     case EVENT_IMMOLATE:
-                        if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 100, true))
-                            DoCast(target, SPELL_IMMOLATE);
+                        DoCastRandomVictim(SPELL_IMMOLATE, 0, 100.0f);
                         events.ScheduleEvent(EVENT_IMMOLATE, 8 * IN_MILLISECONDS);
                         break;
                     case EVENT_TERRIFYING_ROAR:
