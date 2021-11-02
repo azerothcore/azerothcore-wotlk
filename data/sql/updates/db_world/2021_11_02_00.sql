58a04d12c7e0f694f6ad4cb00dd465f41f647471
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-02 12:00:42 +0000

    chore(DB): import pending files
    Referenced commit(s): 2da923e761b1aad30e5337153560823bc26db833

--- data/sql/updates/db_world/2021_11_02_00.sql
+++ data/sql/updates/db_world/2021_11_02_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_01_01 -> 2021_11_02_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_01_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_01_01 2021_11_02_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1620244782542375889'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620244782542375889');
 
 -- Added missing aura for Magmadar
@@ -309,3 +325,13 @@ INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `positi
 DELETE FROM `spell_script_names` WHERE `spell_id`=19774 AND `ScriptName`='spell_summon_ragnaros';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (19774, 'spell_summon_ragnaros');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_02_00' WHERE sql_rev = '1620244782542375889';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
