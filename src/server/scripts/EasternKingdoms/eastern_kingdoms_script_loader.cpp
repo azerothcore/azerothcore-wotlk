6f9535064eab846fb19506476e31e1428c3934a5
Malcrom <malcromdev@gmail.com>
2021-11-02 09:49:59 -0300

    fix(Script/Gossip) Mathredis Firestar

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -147,6 +147,7 @@ void AddSC_undercity();
 void AddSC_western_plaguelands();
 void AddSC_westfall();
 //void AddSC_wetlands();
+void AddSC_burning_steppes();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -283,4 +284,5 @@ void AddEasternKingdomsScripts()
     AddSC_western_plaguelands();
     AddSC_westfall();
     //AddSC_wetlands();
+    AddSC_burning_steppes();
 }
