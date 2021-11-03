f30725144dbc1b25be08591568b82076c19e60a5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-03 23:42:35 +0000

    chore(DB): import pending files
    Referenced commit(s): b2ce49135e474285eda9f806a0eea038ac0e2ee5

--- data/sql/updates/pending_db_world/rev_1635649346761519900.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635649346761519900');
-
-DELETE FROM `areatrigger_scripts` WHERE `entry` = 3957;
-INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
-(3957, 'at_zulgurub_entrance_speech');
-
-UPDATE `creature_text` SET `TextRange` = 3 WHERE `CreatureID` = 14834 AND `GroupID` = 3 AND `ID` = 0;
