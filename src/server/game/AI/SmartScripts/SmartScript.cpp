dddf765a44f2de5a0c768f45af64bd9301a760fc
acidmanifesto <joshua.lee.betts@gmail.com>
2021-11-03 19:01:12 -0400

    Nefertumm Request
    Co-Authored-By: Malcrom <malcromdev@gmail.com>

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -1746,7 +1746,9 @@ void SmartScript::ProcessAction(SmartScriptHolder& e, Unit* unit, uint32 var0, u
                         delete targets;
                 }
                 if (e.action.wpStart.reactState <= REACT_AGGRESSIVE)
-                    me->SetReactState((ReactStates)e.action.wpStart.reactState);
+                {
+                    me->SetReactState((ReactStates) e.action.wpStart.reactState);
+                }
                 CAST_AI(SmartAI, me->AI())->StartPath(run, entry, repeat, unit);
 
                 uint32 quest = e.action.wpStart.quest;
