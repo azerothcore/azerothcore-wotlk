da424267830b57c17f6804dc3bdb9dce248e513f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-04 12:12:07 +0000

    chore(DB): import pending files
    Referenced commit(s): 8af051d6d6919b5f2c8c6cebf95879252128de39

--- data/sql/updates/pending_db_world/rev_1634327540840591400.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634327540840591400');
-
-DELETE FROM `command` WHERE `name` IN ('bags','bags clear');
-INSERT INTO `command` VALUES
-('bags',2,'Syntax: .bags $subcommand \nType .bags to see the list of possible subcommands or .help bags $subcommand to see info on subcommands'),
-('bags clear',2,'Syntax: .bags clear $itemQuality \nClear from players\' bags all items including and below $itemQuality (or all items if used .bags clear all).');
