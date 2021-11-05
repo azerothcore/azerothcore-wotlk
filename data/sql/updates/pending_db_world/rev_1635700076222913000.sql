9704a04fe2455523ec3ad262ad653d0335c2c269
patou01 <2592673+patou01@users.noreply.github.com>
2021-11-05 17:02:16 +0100

    Update data/sql/updates/pending_db_world/rev_1635700076222913000.sql
    Co-authored-by: Nefertumm <nefertum.dev@protonmail.com>

--- data/sql/updates/pending_db_world/rev_1635700076222913000.sql
+++ data/sql/updates/pending_db_world/rev_1635700076222913000.sql
@@ -5,7 +5,6 @@ DELETE FROM `creature_addon` WHERE `guid` = 137990;
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `isLarge`, `auras`) VALUES
 (137990, 1379900, 0, 0, 1, 0, 0, '');
 
-
 -- remove "remove sneak" from assassin, this way they drop the sneak on attacking
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10318;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 10318 AND `source_type` = 0 AND `id` = 1;
