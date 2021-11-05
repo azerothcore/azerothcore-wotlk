6d2ffb06d1d3417ac94d3483aad246579d3e39c8
acidmanifesto <joshua.lee.betts@gmail.com>
2021-11-05 13:34:41 -0400

    remove int width per kitzuna's recommendation

--- data/sql/updates/db_world/rev_1635951187866923861.sql
+++ data/sql/updates/db_world/rev_1635951187866923861.sql
@@ -1,4 +1,4 @@
 -- add revision
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635951187866923861');
 -- add orientation` and delay into existing wp table
-ALTER TABLE `waypoints` ADD COLUMN `orientation` FLOAT DEFAULT 0 NOT NULL AFTER `position_z`, ADD COLUMN `delay` INT(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `orientation`;
+ALTER TABLE `waypoints` ADD COLUMN `orientation` FLOAT DEFAULT 0 NOT NULL AFTER `position_z`, ADD COLUMN `delay` INT UNSIGNED DEFAULT 0 NOT NULL AFTER `orientation`;
