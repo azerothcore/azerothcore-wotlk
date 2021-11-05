6cac04d990c7f938a37907520b0ed941e7886baf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-05 14:58:56 +0000

    chore(DB): import pending files
    Referenced commit(s): c24fcc7efda937b6cb6b91db04e2ddbabc1de717

--- data/sql/updates/pending_db_world/rev_1633787767742446400.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633787767742446400');
-
--- Raise Ally control aura should not be saved
-DELETE FROM `spell_custom_attr` WHERE `spell_id` = '46619';
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
-('46619', '268435456');
--- data/sql/updates/pending_db_world/rev_1635875025536424855.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1635875025536424855');
-
--- Add missing gosip for Witch Doctor Uzer'i
-DELETE FROM `gossip_menu` WHERE `MenuID` IN (1289);
-INSERT INTO `gossip_menu` (`MenuID`,`TextID`) VALUES (1289,1924);
