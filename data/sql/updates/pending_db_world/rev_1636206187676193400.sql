9177f23559594d0a9c3336d06e2521d0b1939375
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-06 15:04:26 +0000

    chore(DB): import pending files
    Referenced commit(s): 9f334c8c0af1cfda84b3a96cfe331a50c62f4c5b

--- data/sql/updates/pending_db_world/rev_1636206187676193400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636206187676193400');
-
--- Fix equipment for Southsea Swashbuckler
-UPDATE `creature_equip_template` SET `ItemID1`=1897, `ItemID2`=0 WHERE `CreatureID`=7858;
--- data/sql/updates/pending_db_world/rev_1636206943540376100.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636206943540376100');
-
-ALTER TABLE `updates` CHANGE `state` `state` ENUM('RELEASED','CUSTOM','MODULE','ARCHIVED') CHARSET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'RELEASED' NOT NULL COMMENT 'defines if an update is released or archived.';
