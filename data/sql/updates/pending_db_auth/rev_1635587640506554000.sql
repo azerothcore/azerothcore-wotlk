97effd83d141419c00227833c7887c58e6598113
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-06 13:07:47 +0000

    chore(DB): import pending files
    Referenced commit(s): 70d8b88f3be6da17fe65e34c19e6e8ac8977a3d4

--- data/sql/updates/pending_db_auth/rev_1635587640506554000.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1635587640506554000');
-
-ALTER TABLE `updates` CHANGE `state` `state` ENUM('RELEASED','CUSTOM','MODULE','ARCHIVED') CHARSET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'RELEASED' NOT NULL COMMENT 'defines if an update is released or archived.';
