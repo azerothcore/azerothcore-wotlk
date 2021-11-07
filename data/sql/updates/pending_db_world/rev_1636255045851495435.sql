1dc1352ea9c4be31a3da70dd1d0063f5c1be41d6
Azcobu <81782124+Azcobu@users.noreply.github.com>
2021-11-07 15:07:47 +1030

    Update rev_1636255045851495435.sql

--- data/sql/updates/pending_db_world/rev_1636255045851495435.sql
+++ data/sql/updates/pending_db_world/rev_1636255045851495435.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636255045851495435');
 
---Removes deprecated breadcrumb quests as prerequistes for 2 AV quests
-UPDATE `quest_template_addon` SET `NextQuestID`= 0 WHERE `ID` IN (7221, 7222);
+-- Removes deprecated breadcrumb quests as prerequistes for 2 AV quests
+UPDATE `quest_template_addon` SET `NextQuestID` = 0 WHERE `ID` IN (7221, 7222);
 
