8087dbaa49dedf23be4cbb2010f464c397cf0142
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-07 19:47:22 +0000

    chore(DB): import pending files
    Referenced commit(s): d05b9d45ac3e982eb3166b9e7c4d000b93a2ea17

--- data/sql/updates/pending_db_world/rev_1636144391799645100.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636144391799645100');
-
-UPDATE `creature_template` SET `AiName`='' WHERE `entry`=23090;
-UPDATE `creature_template_addon` SET `auras`='18950 32199' WHERE `entry`=23090;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=23090;
-
-UPDATE `creature_template_addon` SET `auras`='20540' WHERE `entry`=12856;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=12856 AND `id`=3;
