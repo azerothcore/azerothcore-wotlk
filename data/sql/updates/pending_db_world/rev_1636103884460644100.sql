0eeaf34c8c2d3b6a1abeeaccbaf625383e4f0197
schell244 <>
2021-11-07 20:09:05 +0100

    remove drop conditions

--- data/sql/updates/pending_db_world/rev_1636103884460644100.sql
+++ data/sql/updates/pending_db_world/rev_1636103884460644100.sql
@@ -1,12 +1,14 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636103884460644100');
 
--- Westfall Deed
+-- Correct drop chance for Westfall Deed
 UPDATE `creature_loot_template` SET `Chance`=4 WHERE `Item`=1972 AND `Entry` IN (6866,6846);
 UPDATE `creature_loot_template` SET `Chance`=3 WHERE `Item`=1972 AND `Entry` IN (116,474,880,6927);
-DELETE FROM `creature_loot_template` WHERE `Item`=1972 AND `Entry`=583;
-DELETE FROM `creature_loot_template` WHERE `Item`=1972 AND `Entry`=881;
 
--- Gold Pickup Schedule
+-- Defias Ambusher and Surena Caledon should not drop Westfall Deed
+DELETE FROM `creature_loot_template` WHERE `Item`=1972 AND `Entry` IN (583,881);
+DELETE FROM `conditions` WHERE `SourceEntry`=1972 AND `SourceGroup` IN (583,881);
+
+-- Correct drop chance for Gold Pickup Schedule
 UPDATE `creature_loot_template` SET `Chance`=86 WHERE `Item`=1307 AND `Entry`=100;
 UPDATE `creature_loot_template` SET `Chance`=3 WHERE `Item`=1307 AND `Entry` IN (478,97);
 UPDATE `creature_loot_template` SET `Chance`=1.9 WHERE `Item`=1307 AND `Entry`=448;
