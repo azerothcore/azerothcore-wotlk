b1d3f8f839330a83edd5e69d6194babc54a67342
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-07 09:12:50 +0000

    chore(DB): import pending files
    Referenced commit(s): 7cd03f14293257def99920741ac7e1c66f996eb9

--- data/sql/updates/db_world/2021_11_07_00.sql
+++ data/sql/updates/db_world/2021_11_07_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_06_07 -> 2021_11_07_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_06_07';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_06_07 2021_11_07_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633268017505597200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633268017505597200');
 
 -- rookery guardians
@@ -25,3 +41,13 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = 175245 AND `source_type` = 1 A
 DELETE FROM `gameobject_template` WHERE `entry` = 175622;
 DELETE FROM `gameobject_template_addon` WHERE `entry` = 175622;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 175622;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_07_00' WHERE sql_rev = '1633268017505597200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
