8b4442b0bb7b05b7669b1a140f30b00c937f9506
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-07 23:51:07 +0000

    chore(DB): import pending files
    Referenced commit(s): a9e5b35a5df9cf3f40de4215352fcd000a4b5d6f

--- data/sql/updates/pending_db_world/rev_1636091637254613269.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636091637254613269');
-
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('debug play music', 3, 'Syntax: .debug play music <musicId>\nPlay music with <musicId>.\nMusic will be played only for you. Other players will not hear this.');
