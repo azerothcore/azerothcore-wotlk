6a7fcca7e32f20100dda92af1829afb3db1ecbfc
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-11-08 19:04:15 +0100

    fix(DB/Quests): Set Hallow's End Treats quests as repeatable. (#8954)

---
+++ data/sql/updates/pending_db_world/rev_1636056541021794400.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636056541021794400');
+
+UPDATE `quest_poi` SET `Flags`=3 WHERE `questId`=8312 AND `id`=1;
+UPDATE `quest_poi` SET `Flags`=1 WHERE `questId`=8312 AND `id`=0;
+
+UPDATE `quest_template_addon` SET `SpecialFlags`=1 WHERE `id` IN (8353,8355,8356,8357,8358,8359,8360);
+UPDATE `quest_template_addon` SET `SpecialFlags`=3 WHERE `id`=8354;
