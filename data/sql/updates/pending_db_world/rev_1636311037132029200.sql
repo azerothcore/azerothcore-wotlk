8511ac1285bbb48511dc441defbf26b531da9142
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-09 02:59:32 +0000

    chore(DB): import pending files
    Referenced commit(s): 6dab5a32c02974961f6300f688deac41dae1b2c9

--- data/sql/updates/pending_db_world/rev_1636311037132029200.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636311037132029200');
-
-DELETE FROM `creature_text` WHERE `CreatureID` = 9568;
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`) VALUES
-(9568, 0, 0, '%s calls for help!' , 16);
