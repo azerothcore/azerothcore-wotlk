b92e9a703613ad3a9db7802896cc1038aeece663
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-09 10:49:35 +0000

    chore(DB): import pending files
    Referenced commit(s): 834cfecc4f42fb68c7e46cd431060d1fdc57335c

--- data/sql/updates/pending_db_world/rev_1636371600934152600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636371600934152600');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_mage_polymorph_visual';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(32826,'spell_mage_polymorph_cast_visual');
