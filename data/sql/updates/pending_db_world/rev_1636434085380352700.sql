5c18ed307c5a208f6fcf83a15392da8aae325ba8
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-09 20:42:19 +0000

    chore(DB): import pending files
    Referenced commit(s): c8cd2c597a1e7a255062a627f794300dd16d7150

--- data/sql/updates/pending_db_world/rev_1636434085380352700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636434085380352700');
-
-UPDATE `creature_template` SET `ScriptName` = 'boss_quartermaster_zigris', `AIName` = '' WHERE `entry` = 9736;
-DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 9736;
