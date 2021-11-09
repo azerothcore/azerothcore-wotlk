f77b1c98b5013c405c284593039cff124fadadbe
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-09 09:52:29 +0000

    chore(DB): import pending files
    Referenced commit(s): 4db3dc629b2dca76b51ebf2c18f00277bf2336f8

--- data/sql/updates/pending_db_world/rev_1636427747651853200.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636427747651853200');
-
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 15958;
-INSERT INTO `conditions`(`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(17, 0, 15958, 0, 0, 30, 0, 175124, 5, 0, 0, 0, 0, '', 'Collect Rookery Egg require near GO Rookery Egg');
