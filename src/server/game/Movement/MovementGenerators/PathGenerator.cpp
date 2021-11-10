31c303439134e218928dacfb0d429d62eb3ebf04
sschepens <sebastian.schepens@mercadolibre.com>
2021-11-10 15:15:54 -0300

    Revert "fix top of water pathing"
    This reverts commit 49d6744abd8203a04bb8fbc7805f9480eaaac5c8.

--- src/server/game/Movement/MovementGenerators/PathGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/PathGenerator.cpp
@@ -211,21 +211,20 @@ void PathGenerator::BuildPolyPath(G3D::Vector3 const& startPos, G3D::Vector3 con
     {
         bool buildShotrcut = false;
 
-        bool isWaterStart = _source->GetMap()->IsInWater(_source->GetPhaseMask(), startPos.x, startPos.y, startPos.z, _source->GetCollisionHeight());
-        bool isWaterEnd   = _source->GetMap()->IsInWater(_source->GetPhaseMask(), endPos.x, endPos.y, endPos.z, _source->GetCollisionHeight());
-        bool isFarUnderWater = startFarFromPoly ? isWaterStart : isWaterEnd;
+        bool isUnderWaterStart = _source->GetMap()->IsUnderWater(_source->GetPhaseMask(), startPos.x, startPos.y, startPos.z, _source->GetCollisionHeight());
+        bool isUnderWaterEnd = _source->GetMap()->IsUnderWater(_source->GetPhaseMask(), endPos.x, endPos.y, endPos.z, _source->GetCollisionHeight());
+        bool isFarUnderWater = startFarFromPoly ? isUnderWaterStart : isUnderWaterEnd;
 
         Unit const* _sourceUnit = _source->ToUnit();
 
         if (_sourceUnit)
         {
-            bool isInWater = (_sourceUnit->CanSwim() && isWaterStart && isWaterEnd) || (isFarUnderWater && _useRaycast);
+            bool isUnderWater = (_sourceUnit->CanSwim() && isUnderWaterStart && isUnderWaterEnd) || (_sourceUnit->IsInWater() && isUnderWaterEnd) || (isFarUnderWater && _useRaycast);
 
-            if (isInWater || _sourceUnit->CanFly() || (_sourceUnit->IsFalling() && endPos.z < startPos.z))
+            if (isUnderWater || _sourceUnit->CanFly() || (_sourceUnit->IsFalling() && endPos.z < startPos.z))
             {
                 buildShotrcut = true;
             }
-
         }
 
         if (buildShotrcut)
