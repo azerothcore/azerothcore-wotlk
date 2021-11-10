f9189af8740a27838b57cc35c0ec5953e1efacae
Winfidonarleyan <dowlandtop@yandex.com>
2021-11-10 20:55:12 +0700

    send 3/4

--- src/server/scripts/Commands/cs_send.cpp
+++ src/server/scripts/Commands/cs_send.cpp
@@ -25,10 +25,7 @@
 #include "Pet.h"
 #include "Player.h"
 #include "WorldSession.h"
-
-#if AC_COMPILER == AC_COMPILER_GNU
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
+#include "Tokenize.h"
 
 using namespace Acore::ChatCommands;
 
@@ -37,9 +34,9 @@ class send_commandscript : public CommandScript
 public:
     send_commandscript() : CommandScript("send_commandscript") { }
 
-    std::vector<ChatCommand> GetCommands() const override
+    ChatCommandTable GetCommands() const override
     {
-        static std::vector<ChatCommand> sendCommandTable =
+        static ChatCommandTable sendCommandTable =
         {
             { "items",   HandleSendItemsCommand,   SEC_GAMEMASTER,    Console::Yes },
             { "mail",    HandleSendMailCommand,    SEC_GAMEMASTER,    Console::Yes },
@@ -47,89 +44,66 @@ public:
             { "money",   HandleSendMoneyCommand,   SEC_GAMEMASTER,    Console::Yes }
         };
 
-        static std::vector<ChatCommand> commandTable =
+        static ChatCommandTable commandTable =
         {
             { "send", sendCommandTable }
         };
+
         return commandTable;
     }
 
-    static bool HandleSendItemsCommand(ChatHandler* handler, char const* args)
+    static bool HandleSendItemsCommand(ChatHandler* handler, Optional<PlayerIdentifier> target, QuotedString subject, QuotedString text, Tail items)
     {
-        // format: name "subject text" "mail text" item1[:count1] item2[:count2] ... item12[:count12]
-        Player* receiver;
-        ObjectGuid receiverGuid;
-        std::string receiverName;
-        if (!handler->extractPlayerTarget((char*)args, &receiver, &receiverGuid, &receiverName))
-            return false;
+        if (!target)
+        {
+            target = PlayerIdentifier::FromTargetOrSelf(handler);
+        }
 
-        char* tail1 = strtok(nullptr, "");
-        if (!tail1)
+        if (!target)
+        {
             return false;
-
-        char const* msgSubject = handler->extractQuotedArg(tail1);
-        if (!msgSubject)
-            return false;
-
-        char* tail2 = strtok(nullptr, "");
-        if (!tail2)
-            return false;
-
-        char const* msgText = handler->extractQuotedArg(tail2);
-        if (!msgText)
-            return false;
-
-        // msgSubject, msgText isn't NUL after prev. check
-        std::string subject = msgSubject;
-        std::string text    = msgText;
+        }
 
         // extract items
-        typedef std::pair<uint32, uint32> ItemPair;
-        typedef std::list< ItemPair > ItemPairs;
-        ItemPairs items;
+        std::vector<std::pair<uint32, uint32>> itemList;
 
-        // get all tail string
-        char* tail = strtok(nullptr, "");
-
-        // get from tail next item str
-        while (char* itemStr = strtok(tail, " "))
+        for (auto const& itemString : Acore::Tokenize(items, ' ', true))
         {
-            // and get new tail
-            tail = strtok(nullptr, "");
+            auto itemTokens = Acore::Tokenize(itemString, ':', false);
 
-            // parse item str
-            char const* itemIdStr = strtok(itemStr, ":");
-            char const* itemCountStr = strtok(nullptr, " ");
-
-            uint32 itemId = atoi(itemIdStr);
-            if (!itemId)
-                return false;
-
-            ItemTemplate const* item_proto = sObjectMgr->GetItemTemplate(itemId);
-            if (!item_proto)
+            if (itemTokens.size() != 2)
             {
-                handler->PSendSysMessage(LANG_COMMAND_ITEMIDINVALID, itemId);
+                handler->SendSysMessage(Acore::StringFormatFmt("> Incorrect item list format for '{}'", itemString));
+                continue;
+            }
+
+            uint32 itemID = *Acore::StringTo<uint32>(itemTokens.at(0));
+            uint32 itemCount = *Acore::StringTo<uint32>(itemTokens.at(1));
+
+            ItemTemplate const* itemTemplate = sObjectMgr->GetItemTemplate(itemID);
+            if (!itemTemplate)
+            {
+                handler->PSendSysMessage(LANG_COMMAND_ITEMIDINVALID, itemID);
                 handler->SetSentErrorMessage(true);
                 return false;
             }
 
-            uint32 itemCount = itemCountStr ? atoi(itemCountStr) : 1;
-            if (itemCount < 1 || (item_proto->MaxCount > 0 && itemCount > uint32(item_proto->MaxCount)))
+            if (!itemCount || (itemTemplate->MaxCount > 0 && itemCount > uint32(itemTemplate->MaxCount)))
             {
-                handler->PSendSysMessage(LANG_COMMAND_INVALID_ITEM_COUNT, itemCount, itemId);
+                handler->PSendSysMessage(LANG_COMMAND_INVALID_ITEM_COUNT, itemCount, itemID);
                 handler->SetSentErrorMessage(true);
                 return false;
             }
 
-            while (itemCount > item_proto->GetMaxStackSize())
+            while (itemCount > itemTemplate->GetMaxStackSize())
             {
-                items.push_back(ItemPair(itemId, item_proto->GetMaxStackSize()));
-                itemCount -= item_proto->GetMaxStackSize();
+                itemList.emplace_back(itemID, itemTemplate->GetMaxStackSize());
+                itemCount -= itemTemplate->GetMaxStackSize();
             }
 
-            items.push_back(ItemPair(itemId, itemCount));
+            itemList.emplace_back(itemID, itemCount);
 
-            if (items.size() > MAX_MAIL_ITEMS)
+            if (itemList.size() > MAX_MAIL_ITEMS)
             {
                 handler->PSendSysMessage(LANG_COMMAND_MAIL_ITEMS_LIMIT, MAX_MAIL_ITEMS);
                 handler->SetSentErrorMessage(true);
@@ -137,101 +111,77 @@ public:
             }
         }
 
-        ObjectGuid::LowType senderGuid;
         // If the message is sent from console, set it as sent by the target itself, like the other Customer Support mails.
-        senderGuid = handler->GetSession() ? handler->GetSession()->GetPlayer()->GetGUID().GetCounter() : receiverGuid.GetCounter();
-        MailSender sender(MAIL_NORMAL, senderGuid, MAIL_STATIONERY_GM);
+        ObjectGuid::LowType senderGuid = handler->GetSession() ? handler->GetSession()->GetPlayer()->GetGUID().GetCounter() : target->GetGUID().GetCounter();
 
         // fill mail
         MailDraft draft(subject, text);
-
+        MailSender sender(MAIL_NORMAL, senderGuid, MAIL_STATIONERY_GM);
         CharacterDatabaseTransaction trans = CharacterDatabase.BeginTransaction();
 
-        for (ItemPairs::const_iterator itr = items.begin(); itr != items.end(); ++itr)
+        for (auto const& [itemID, itemCount] : itemList)
         {
-            if (Item* item = Item::CreateItem(itr->first, itr->second, handler->GetSession() ? handler->GetSession()->GetPlayer() : 0))
+            if (Item* item = Item::CreateItem(itemID, itemCount, handler->GetSession() ? handler->GetSession()->GetPlayer() : 0))
             {
-                item->SaveToDB(trans);                               // save for prevent lost at next mail load, if send fail then item will deleted
+                item->SaveToDB(trans); // save for prevent lost at next mail load, if send fail then item will deleted
                 draft.AddItem(item);
             }
         }
 
-        draft.SendMailTo(trans, MailReceiver(receiver, receiverGuid.GetCounter()), sender);
+        draft.SendMailTo(trans, MailReceiver(target->GetConnectedPlayer(), target->GetGUID().GetCounter()), sender);
         CharacterDatabase.CommitTransaction(trans);
 
-        std::string nameLink = handler->playerLink(receiverName);
-        handler->PSendSysMessage(LANG_MAIL_SENT, nameLink.c_str());
+        handler->PSendSysMessage(LANG_MAIL_SENT, handler->playerLink(target->GetName()).c_str());
         return true;
     }
 
-    static bool HandleSendMailCommand(ChatHandler* handler, char const* args)
+    static bool HandleSendMailCommand(ChatHandler* handler, Optional<PlayerIdentifier> target, QuotedString subject, QuotedString text)
     {
-        // format: name "subject text" "mail text"
-        Player* target;
-        ObjectGuid targetGuid;
-        std::string targetName;
-        if (!handler->extractPlayerTarget((char*)args, &target, &targetGuid, &targetName))
+        if (!target)
+        {
+            target = PlayerIdentifier::FromTargetOrSelf(handler);
+        }
+
+        if (!target)
+        {
             return false;
+        }
 
-        char* tail1 = strtok(nullptr, "");
-        if (!tail1)
-            return false;
+        ObjectGuid::LowType senderGuid = handler->GetSession() ? handler->GetSession()->GetPlayer()->GetGUID().GetCounter() : target->GetGUID().GetCounter();
 
-        char const* msgSubject = handler->extractQuotedArg(tail1);
-        if (!msgSubject)
-            return false;
-
-        char* tail2 = strtok(nullptr, "");
-        if (!tail2)
-            return false;
-
-        char const* msgText = handler->extractQuotedArg(tail2);
-        if (!msgText)
-            return false;
-
-        // msgSubject, msgText isn't NUL after prev. check
-        std::string subject = msgSubject;
-        std::string text    = msgText;
-
-        ObjectGuid::LowType senderGuid;
         // If the message is sent from console, set it as sent by the target itself, like the other Customer Support mails.
-        senderGuid = handler->GetSession() ? handler->GetSession()->GetPlayer()->GetGUID().GetCounter() : targetGuid.GetCounter();
         MailSender sender(MAIL_NORMAL, senderGuid, MAIL_STATIONERY_GM);
-
-        //- TODO: Fix poor design
-        CharacterDatabaseTransaction trans = CharacterDatabase.BeginTransaction();
-        MailDraft(subject, text)
-                .SendMailTo(trans, MailReceiver(target, targetGuid.GetCounter()), sender);
-
+        MailDraft draft(subject, text);
+        CharacterDatabaseTransaction trans = CharacterDatabase.BeginTransaction();        
+        draft.SendMailTo(trans, MailReceiver(target->GetConnectedPlayer(), target->GetGUID().GetCounter()), sender);
         CharacterDatabase.CommitTransaction(trans);
 
-        std::string nameLink = handler->playerLink(targetName);
-        handler->PSendSysMessage(LANG_MAIL_SENT, nameLink.c_str());
+        handler->PSendSysMessage(LANG_MAIL_SENT, handler->playerLink(target->GetName()).c_str());
         return true;
     }
 
-    static bool HandleSendMessageCommand(ChatHandler* handler, char const* args)
+    static bool HandleSendMessageCommand(ChatHandler* handler, Optional<PlayerIdentifier> target, Tail message)
     {
-        /// - Find the player
-        Player* player = nullptr;
-        if (!handler->extractPlayerTarget((char*)args, &player))
-            return false;
+        if (!target)
+        {
+            target = PlayerIdentifier::FromTargetOrSelf(handler);
+        }
 
-        if (!player)
+        if (!target || !target->IsConnected())
+        {
             return false;
+        }
 
-        char* msgStr = strtok(nullptr, "");
-        if (!msgStr)
-            return false;
+        Player* player = target->GetConnectedPlayer();
+        auto msg = std::string{ message }.c_str();
 
         /// - Send the message
         // Use SendAreaTriggerMessage for fastest delivery.
-        player->GetSession()->SendAreaTriggerMessage("%s", msgStr);
+        player->GetSession()->SendAreaTriggerMessage("%s", msg);
         player->GetSession()->SendAreaTriggerMessage("|cffff0000[Message from administrator]:|r");
 
         // Confirmation message
-        std::string nameLink = handler->GetNameLink(player);
-        handler->PSendSysMessage(LANG_SENDMESSAGE, nameLink.c_str(), msgStr);
+        handler->PSendSysMessage(LANG_SENDMESSAGE, handler->playerLink(target->GetName()).c_str(), msg);
 
         return true;
     }
