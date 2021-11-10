dd60e661b26f97c2140b9519cbb98adaf810106f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-10 05:19:40 +0000

    chore(DB): import pending files
    Referenced commit(s): c3a0214bce5cb3e5109865a70b4fa8899e64c443

--- data/sql/updates/pending_db_world/rev_1636491400432791100.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636491400432791100');
-
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 1) AND (`SourceGroup` IN (2764, 2765, 4063)) AND (`SourceEntry` = 21525);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(1, 2764, 21525, 0, 0, 12, 0, 2, 0, 0, 0, 0, 0, '', 'Green Winter Hat only drops when event 2 is active'),
-(1, 2765, 21525, 0, 0, 12, 0, 2, 0, 0, 0, 0, 0, '', 'Green Winter Hat only drops when event 2 is active'),
-(1, 4063, 21525, 0, 0, 12, 0, 2, 0, 0, 0, 0, 0, '', 'Green Winter Hat only drops when event 2 is active');
