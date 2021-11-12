63ca3305313913b9533100ddffc0cd9d212cb9dd
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-12 14:15:46 +0000

    chore(DB): import pending files
    Referenced commit(s): 3240d27d6fe9b937086845f5c72f55e1fb1404e1

--- data/sql/updates/pending_db_world/rev_1634947992074110300.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1634947992074110300');
-
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = -24869;
-
-DELETE FROM `spell_script_names` WHERE `spell_id` = -24869 AND `ScriptName` = 'spell_gen_holiday_buff_food';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(-24869, 'spell_gen_holiday_buff_food');
