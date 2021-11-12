b1f038336dbe146190702743990b6b17864eeded
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-12 11:09:07 +0000

    chore(DB): import pending files
    Referenced commit(s): 5f2d86cece59c5ebd25d18b9721ae690e158f8d6

--- data/sql/updates/pending_db_world/rev_1636652299107244900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636652299107244900');
-
-DELETE FROM `creature_text` WHERE `CreatureID` = 10316;
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
-(10316, 0, 0, '%s attempts to run away in fear!', 16, 0, 100, 0, 0, 0, 1150, 0, 'Blackhand Incarcerator - EMOTE_FLEE');
