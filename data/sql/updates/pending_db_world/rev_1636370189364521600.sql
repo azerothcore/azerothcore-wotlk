65274a21a0968cbb17bc3ffcbe3c9d08270d492e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-13 12:04:50 +0000

    chore(DB): import pending files
    Referenced commit(s): 0850dea4df4fab3e870fe84cb7c3ba1146bed07d

--- data/sql/updates/pending_db_world/rev_1636370189364521600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636370189364521600');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_item_poweful_anti_venom';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(23786,'spell_item_powerful_anti_venom');
