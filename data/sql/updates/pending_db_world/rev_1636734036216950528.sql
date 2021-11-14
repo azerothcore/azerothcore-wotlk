5499b94eff71cd07f3bb9ae1f178c6a0c112c465
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-14 00:23:59 +0000

    chore(DB): import pending files
    Referenced commit(s): 9ea9bb02599488a588c749d4c4adff9d725b56f6

--- data/sql/updates/pending_db_world/rev_1636734036216950528.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636734036216950528');
-
--- Add missing gossip text for Finkle Einhorn
-DELETE FROM `gossip_menu` WHERE `MenuID` IN (2994,2995,2996,2997,2998,2999);
-INSERT INTO `gossip_menu` (`MenuID`,`TextID`) VALUES
-(2994,3660),(2995,3661),(2996,3662),(2997,3663),(2998,3664),(2999,3665);
