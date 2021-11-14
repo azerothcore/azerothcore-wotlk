27839c2e6452f89ffdbab6184bde81222ab7a466
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-14 18:24:34 +0000

    chore(DB): import pending files
    Referenced commit(s): 3b89a7e077bcb328d49e3d34ac95ca991b2aa6e4

--- data/sql/updates/db_world/2021_11_14_01.sql
+++ data/sql/updates/db_world/2021_11_14_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_14_00 -> 2021_11_14_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_14_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_14_00 2021_11_14_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1636718487600176900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636718487600176900');
 
 UPDATE `gameobject_template` SET `ScriptName`='go_bells' WHERE `entry` IN (175885, 176573, 182064);
@@ -57,3 +73,12 @@ INSERT INTO `game_event_gameobject` (`eventEntry`, `guid`) VALUES
 -- karazhan bell
 INSERT INTO `game_event_gameobject` (`eventEntry`, `guid`) VALUES
 (73, 24539);
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_14_01' WHERE sql_rev = '1636718487600176900';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
