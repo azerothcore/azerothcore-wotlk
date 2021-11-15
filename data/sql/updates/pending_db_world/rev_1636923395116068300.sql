e5b77efd63c3bad161ded7fa2bd17d98f7f9c2ce
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-15 09:44:27 +0000

    chore(DB): import pending files
    Referenced commit(s): 56d17e9e04c14fcd335b8a86bb51bed2f4b55ec4

--- data/sql/updates/pending_db_world/rev_1636923395116068300.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636923395116068300');
-
--- Remove Fifth Mosharu Tablet from Smolderthorn Shadow Priest
-DELETE FROM `creature_loot_template` WHERE `item` = 12740;
