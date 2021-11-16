a86f1b8251d7a4a122cb0d47d483270bbe6da4bf
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-11-16 21:24:49 +0100

    fix(Scripts/BlackrockSpire): Urok's summoned mobs should move towards the pile. (#9215)
    Fixes #9197

---
+++ data/sql/updates/pending_db_world/rev_1637088212473831800.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637088212473831800');
+
+UPDATE `smart_scripts` SET `target_param1`=175584 WHERE `entryorguid` IN (10601,10602) AND `target_param1`=175621;
