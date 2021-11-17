1773756f24a2587616688ed2ba96038b4df72870
schell244 <>
2021-11-17 09:35:29 +0100

    merge lines

--- data/sql/updates/pending_db_world/rev_1637094877418094500.sql
+++ data/sql/updates/pending_db_world/rev_1637094877418094500.sql
@@ -9,26 +9,19 @@ UPDATE `creature_loot_template` SET `Chance`=4 WHERE `Item`=2839 AND `Entry`=152
 UPDATE `creature_loot_template` SET `Chance`=3 WHERE `Item`=2839 AND `Entry` IN (1520,1523);
 
 -- Captain Vachon
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1664,2839,0,0.12,0,1,0,1,1,'Captain Vachon - A Letter to Yvette');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1664,2839,0,0.12,0,1,0,1,1,'Captain Vachon - A Letter to Yvette');
 
 -- Moonrage Darkrunner
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1770,2839,0,0.05,0,1,0,1,1,'Moonrage Darkrunner - A Letter to Yvette');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1770,2839,0,0.05,0,1,0,1,1,'Moonrage Darkrunner - A Letter to Yvette');
 
 -- Cursed Darkhound
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1548,2839,0,0.01,0,1,0,1,1,'Cursed Darkhound - A Letter to Yvette');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1548,2839,0,0.01,0,1,0,1,1,'Cursed Darkhound - A Letter to Yvette');
 
 -- Scarlet Zealot
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1537,2839,0,0.01,0,1,0,1,1,'Scarlet Zealot - A Letter to Yvette');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1537,2839,0,0.01,0,1,0,1,1,'Scarlet Zealot - A Letter to Yvette');
 
 -- Rotting Ancestor
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1530,2839,0,0.01,0,1,0,1,1,'Rotting Ancestor - A Letter to Yvette');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1530,2839,0,0.01,0,1,0,1,1,'Rotting Ancestor - A Letter to Yvette');
 
 -- Wandering Spirit
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(1532,2839,0,0.01,0,1,0,1,1,'Wandering Spirit - A Letter to Yvette');
-
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (1532,2839,0,0.01,0,1,0,1,1,'Wandering Spirit - A Letter to Yvette');
