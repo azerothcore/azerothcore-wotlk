2bcd78041eef081c1a60b929af88b3f3d94665c5
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-17 19:01:40 +0000

    chore(DB): import pending files
    Referenced commit(s): de6af87bd37ce063e08633eca28bf7d644d83bd2

--- data/sql/updates/db_world/2021_11_17_02.sql
+++ data/sql/updates/db_world/2021_11_17_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_17_01 -> 2021_11_17_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_17_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_17_01 2021_11_17_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1636844514010159481'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636844514010159481');
 
  -- Creating visibilityDistanceType COLUMN 
@@ -3965,3 +3981,13 @@ UPDATE `creature_template_addon` SET `visibilityDistanceType`='3'WHERE `entry` I
 27947,
 13996,
 14401);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_17_02' WHERE sql_rev = '1636844514010159481';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
