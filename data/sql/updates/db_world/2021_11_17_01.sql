865a0aaa661a9feb51d6107e691d7dcb0852f410
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-17 11:09:15 +0000

    chore(DB): import pending files
    Referenced commit(s): 39425e0c1ff9145871cd9e16445eb5b5e7401475

--- data/sql/updates/db_world/2021_11_17_01.sql
+++ data/sql/updates/db_world/2021_11_17_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_17_00 -> 2021_11_17_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_17_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_17_00 2021_11_17_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1636980654557070200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636980654557070200');
 
 DROP TABLE IF EXISTS `player_loot_template`;
@@ -48,3 +64,13 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 DELETE FROM `command` WHERE `name` IN ('reload player_loot_template');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('reload player_loot_template', 3, 'Syntax: .reload player_loot_template\nReload player_loot_template table.'); 
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_17_01' WHERE sql_rev = '1636980654557070200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
