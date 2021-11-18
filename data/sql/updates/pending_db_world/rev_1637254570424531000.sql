6269b0d9498f37b0285acd8aa3dec350817ee303
Patou01 <youdontneed@this.com>
2021-11-18 18:01:23 +0100

    adding names

---
+++ data/sql/updates/pending_db_world/rev_1637254570424531000.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637254570424531000');
+
+DELETE FROM `creature_template_locale` WHERE entry in (8673, 9856, 8724) AND locale in ('esES', 'esMX');
+INSERT INTO `creature_template_locale` (entry, locale, NAME, Title, VerifiedBuild) VALUES 
+(8673, 'esES', 'Subastador Thathung', '', 18019),
+(8673, 'esMX', 'Subastador Thathung', '', 18019),
+(9856, 'esES', 'Subastador Grimful', '', 18019),
+(9856, 'esMX', 'Subastador Grimful', '', 18019),
+(8724, 'esES', 'Subastador Wabang', '', 18019),
+(8724, 'esMX', 'Subastador Wabang', '', 18019);
