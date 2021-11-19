98cd81fa18089e789675bf35299e8ee6a737bcd4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-19 09:05:39 +0000

    chore(DB): import pending files
    Referenced commit(s): 8a570f483cf50e2d48462d9b037c3043acdd6b98

--- data/sql/updates/db_world/2021_11_19_01.sql
+++ data/sql/updates/db_world/2021_11_19_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_19_00 -> 2021_11_19_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_19_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_19_00 2021_11_19_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1633861396739213700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1633861396739213700');
 
  -- Brazier of the Herald
@@ -14,3 +30,13 @@ UPDATE `creature_template` SET `InhabitType` = 1 WHERE `entry` = 10506;
 
 -- add sound to emote
 UPDATE `creature_text` SET `Sound` = 557 WHERE `CreatureID` = 10506;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_19_01' WHERE sql_rev = '1633861396739213700';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
