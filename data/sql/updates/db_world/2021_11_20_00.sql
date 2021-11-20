00c5fbd5bae3169e682e60556966b018d2b9f44f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-20 13:14:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 6a430cd2f9e141a57221bb1a9e2635616d683a17

--- data/sql/updates/db_world/2021_11_20_00.sql
+++ data/sql/updates/db_world/2021_11_20_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_19_02 -> 2021_11_20_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_19_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_19_02 2021_11_20_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637191609234345200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637191609234345200');
 
 UPDATE `spell_dbc` SET `effect_1` = 24, `ImplicitTargetA_1` = 25, `EffectItemType_1` = 32688 WHERE `id` = 40804;
@@ -20,3 +36,13 @@ UPDATE `spell_dbc` SET `effect_1` = 24, `ImplicitTargetA_1` = 25, `EffectItemTyp
 UPDATE `spell_dbc` SET `effect_1` = 24, `ImplicitTargetA_1` = 25, `EffectItemType_1` = 32711 WHERE `id` = 40921;
 UPDATE `spell_dbc` SET `effect_1` = 24, `ImplicitTargetA_1` = 25, `EffectItemType_1` = 32712 WHERE `id` = 40922;
 UPDATE `spell_dbc` SET `effect_1` = 24, `ImplicitTargetA_1` = 25, `EffectItemType_1` = 32713 WHERE `id` = 40923;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_20_00' WHERE sql_rev = '1637191609234345200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
