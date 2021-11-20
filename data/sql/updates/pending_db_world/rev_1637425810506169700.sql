8361726099e00fce5254537fb54ddaf43e85f362
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-11-20 13:30:59 -0300

    set offer book quest as repeatable, condition it to only show if player doesn't have the book

---
+++ data/sql/updates/pending_db_world/rev_1637425810506169700.sql
@@ -0,0 +1,16 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637425810506169700');
+
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE `ID` IN (1642, 1646);
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=19 AND `SourceGroup`=0 AND `SourceEntry`=1643 AND `SourceId`=0;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(19, 0, 1643, 0, 0, 47, 0, 1642, 64, 0, 0, 0, 0, '', 'Quest The Tome of Divinity (Human) available if quest The Tome of Divinity (book) has been rewarded.');
+
+UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE `ID` IN (1641, 1645);
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=19 AND `SourceGroup`=0 AND `SourceEntry` IN (1641, 1645) AND `SourceId`=0;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(19, 0, 1641, 0, 0, 47, 0, 1642, 1, 0, 0, 0, 0, '', 'Quest The Tome of Divinity (Human, offer book) available if quest The Tome of Divinity (Human, book) has not been taken.'),
+(19, 0, 1641, 0, 0, 2,  0, 6775, 1,  0, 1, 0, 0, '', 'Quest The Tome of Divinity (Human, offer book) available if player does not already have item Tome of Divinity (Human)'),
+(19, 0, 1645, 0, 0, 47, 0, 1646, 1, 0, 0, 0, 0, '', 'Quest The Tome of Divinity (Dwarf, offer book) available if quest The Tome of Divinity (Dwarf, book) has not been taken.'),
+(19, 0, 1645, 0, 0, 2,  0, 6916, 1,  0, 1, 0, 0, '', 'Quest The Tome of Divinity (Dwarf, offer book) available if player does not already have item Tome of Divinity (human)');
