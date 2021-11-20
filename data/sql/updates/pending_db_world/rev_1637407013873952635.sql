3b803969930d90e74fa9cd6c8b03faaf6b7a4b85
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-20 18:48:11 +0000

    chore(DB): import pending files
    Referenced commit(s): 6d1318e971d98d924b353e1da682e1da1c561dbf

--- data/sql/updates/pending_db_world/rev_1637407013873952635.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637407013873952635');
-
--- Reduce Emberstrife's respawn to 5 minutes
-UPDATE `creature` SET `spawntimesecs` = 300 WHERE `id` = 10321 AND `guid` = 31041;
-
