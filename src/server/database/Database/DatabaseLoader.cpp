5d129e5a9296a94877a306ca458425c60a8b250e
Winfidonarleyan <dowlandtop@yandex.com>
2021-11-22 21:09:54 +0700

    1

--- src/server/database/Database/DatabaseLoader.cpp
+++ src/server/database/Database/DatabaseLoader.cpp
@@ -28,8 +28,8 @@
 DatabaseLoader::DatabaseLoader(std::string const& logger, uint32 const defaultUpdateMask, std::string_view modulesList)
     : _logger(logger),
     _autoSetup(sConfigMgr->GetOption<bool>("Updates.AutoSetup", true)),
-    _updateFlags(sConfigMgr->GetOption<uint32>("Updates.EnableDatabases", defaultUpdateMask)),
-    _modulesList(modulesList) { }
+    _modulesList(modulesList),
+    _updateFlags(sConfigMgr->GetOption<uint32>("Updates.EnableDatabases", defaultUpdateMask)) { }
 
 template <class T>
 DatabaseLoader& DatabaseLoader::AddDatabase(DatabaseWorkerPool<T>& pool, std::string const& name)
