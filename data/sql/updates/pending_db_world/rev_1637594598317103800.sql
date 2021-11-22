11f46791351bd4ef53d15341ee67336e38ee5ea9
Valens14 <dylansingh61@aol.com>
2021-11-22 11:11:37 -0500

    fix(Raid/AQ40): Ouro not spawning
    -Currently Ouro does not spawn when approched by players
    -The Ouro Spawner NPC is also currently visible/attackable for players
    -These changes fix Ouro not spawning and mark the spawner NPC as a trigger
    
    -Closes Azerothcore issue 1689
    -https://github.com/azerothcore/azerothcore-wotlk/issues/1689

---
+++ data/sql/updates/pending_db_world/rev_1637594598317103800.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637594598317103800');
+UPDATE `acore_world`.`creature_template` SET `ScriptName`='npc_ouro_spawner' WHERE  `entry`=15957;
+UPDATE `acore_world`.`creature_template` SET `flags_extra`='128' WHERE  `entry`=15957;
