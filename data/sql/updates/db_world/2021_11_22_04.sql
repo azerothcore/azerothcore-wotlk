d0d4563c26b8aa800cd0b51939dbff53ec1685c1
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-22 23:35:52 +0000

    chore(DB): import pending files
    Referenced commit(s): 0f4268bea7bbc29ff2f4461b1e35916b820777ac

--- data/sql/updates/db_world/2021_11_22_04.sql
+++ data/sql/updates/db_world/2021_11_22_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_22_03 -> 2021_11_22_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_22_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_22_03 2021_11_22_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1636799867064749300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1636799867064749300');
 
 -- shield
@@ -19,3 +35,13 @@ INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, 
 DELETE FROM `gameobject` WHERE `id` = 187273;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
 (10167, 187273, 1, 0, 0, 1, 1, -3700.58, -2534.09, 68.8, 3.05433, 0, 0, -0.999048, -0.0436174, 0, 100, 1, '', 0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_22_04' WHERE sql_rev = '1636799867064749300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
