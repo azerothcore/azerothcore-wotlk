05552d9d850173698d18e6707b4fa70e7a9cddc0
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-23 09:31:51 +0000

    chore(DB): import pending files
    Referenced commit(s): dbe6c1324e96b5c7d4150b44a3901fea717624d6

--- data/sql/updates/db_world/2021_11_23_01.sql
+++ data/sql/updates/db_world/2021_11_23_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_23_00 -> 2021_11_23_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_23_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_23_00 2021_11_23_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637094877418094500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637094877418094500');
 
 -- Correct drop chance: [A Letter to Yvette] https://classic.wowhead.com/item=2839/a-letter-to-yvette
@@ -17,3 +33,13 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (1537,2839,0,0.01,0,1,0,1,1,'Scarlet Zealot - A Letter to Yvette'),
 (1530,2839,0,0.01,0,1,0,1,1,'Rotting Ancestor - A Letter to Yvette'),
 (1532,2839,0,0.01,0,1,0,1,1,'Wandering Spirit - A Letter to Yvette');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_23_01' WHERE sql_rev = '1637094877418094500';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
