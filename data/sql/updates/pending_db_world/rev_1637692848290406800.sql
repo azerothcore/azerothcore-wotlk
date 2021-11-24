7f994b6353aa60c128fa7bdcc847768ea5760820
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-11-24 01:14:46 +0100

    Update rev_1637692848290406800.sql

--- data/sql/updates/pending_db_world/rev_1637692848290406800.sql
+++ data/sql/updates/pending_db_world/rev_1637692848290406800.sql
@@ -28,7 +28,7 @@ CREATE TABLE `creature_template_movement` (
 )
 COLLATE='utf8mb4_general_ci' ENGINE=MyISAM;
 
-INSERT INTO `creature_template_movement` SELECT `entry`,0,0,0,0 FROM `creature_template` WHERE `InhabitType`!=3;
+INSERT INTO `creature_template_movement` SELECT `entry`,0,0,0,0,0,0,0 FROM `creature_template` WHERE `InhabitType`!=3;
 UPDATE `creature_template_movement` SET `Ground`=1 WHERE `CreatureId` IN (SELECT `entry` FROM `creature_template` WHERE `InhabitType` & 1);
 UPDATE `creature_template_movement` SET `Swim`=1 WHERE `CreatureId` IN (SELECT `entry` FROM `creature_template` WHERE `InhabitType` & 2);
 UPDATE `creature_template_movement` SET `Flight`=1 WHERE `CreatureId` IN (SELECT `entry` FROM `creature_template` WHERE (`InhabitType` & 5) = 4);
