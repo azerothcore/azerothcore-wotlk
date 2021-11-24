d2913d30a5aa8730a6e3f5b9e40ae507799e314d
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-11-24 22:04:06 +0000

    fix(DB): Lazy peons commited workers
    - Removed ACTION_PLAY_RANDOM_SOUND
    - Added Sounds played to the creature_text instead

---
+++ data/sql/updates/pending_db_world/rev_1637791407206032100.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637791407206032100');
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (334500, 334501, 334601, 334701, 334801, 652301, 652401, 652501, 652601, 652701, 737201, 737301, 737401, 737501)) AND (`source_type` = 9) AND (`id` IN (2, 12, 21));
+DELETE FROM `creature_text` WHERE `CreatureID`=10556 AND `GroupID`=0 AND (`ID` IN (0,1,2));
+
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
+(10556, 0, 0, 'Ow!  OK, I\'ll get back to work, $n!', 12, 1, 100, 0, 0, 6292, 5774, 0, 'lazy peon SAY_WP_0'),
+(10556, 0, 1, 'Ow!  OK, I\'ll get back to work, $n!', 12, 1, 100, 0, 0, 6294, 5774, 0, 'lazy peon SAY_WP_0'),
+(10556, 0, 2, 'Ow!  OK, I\'ll get back to work, $n!', 12, 1, 100, 0, 0, 6197, 5774, 0, 'lazy peon SAY_WP_0');
