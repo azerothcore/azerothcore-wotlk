928d9706dab8702fdae33cdfcefa5cd77e2dbc00
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-11-25 18:20:39 +0100

    fix(Core/Auras): Set proper displayId for mounts that changes depending on your Riding skill and location with more than one modelId
    * cherry-pick commit (https://github.com/TrinityCore/TrinityCore/commit/a5b8e167dda0697f88b16ce0892912afda4c3e4b)
    
    Co-Authored-By: Gildor <521036+Jildor@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1637860809744969300.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637860809744969300');
+
+-- Set proper modelid
+UPDATE `creature_template` SET `modelid1`=31248, `modelid2`=31007 WHERE `entry`=38545; -- Invincible
+UPDATE `creature_template` SET `modelid1`=31957, `modelid2`=31958 WHERE `entry`=40625; -- Celestial Steed
+-- Invincible wrong otherGender displayID
+UPDATE `creature_model_info` SET `DisplayID_Other_Gender`=0 WHERE `DisplayID` IN (31007,31248);
