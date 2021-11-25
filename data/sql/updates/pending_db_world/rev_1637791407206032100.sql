162c3ee9084ac2bee83c0cf852df01d5ae3ae7f9
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-11-25 11:49:04 +0000

    Missing some action_lists

--- data/sql/updates/pending_db_world/rev_1637791407206032100.sql
+++ data/sql/updates/pending_db_world/rev_1637791407206032100.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637791407206032100');
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (334500, 334501, 334601, 334701, 334801, 652301, 652401, 652501, 652601, 652701, 737201, 737301, 737401, 737501)) AND (`source_type` = 9) AND (`id` IN (2, 12, 21));
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (334500, 334501, 334600, 334601, 334700, 334701, 334800, 334801, 652300, 652301, 652400, 652401, 652500, 652501, 652600, 652601, 652700, 652701, 737200, 737201, 737300, 737301, 737400, 737401, 737500, 737501)) AND (`source_type` = 9) AND (`id` IN (2, 12, 21));
 DELETE FROM `creature_text` WHERE `CreatureID`=10556 AND `GroupID`=0 AND (`ID` IN (0,1,2));
 
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
