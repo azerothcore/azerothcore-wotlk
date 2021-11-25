b78a7609b7859a9cdc21716c458812c728d9317e
acidmanifesto <joshua.lee.betts@gmail.com>
2021-11-25 20:24:34 +0100

    Update src/server/game/Entities/Creature/Creature.cpp
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -64,9 +64,9 @@ CreatureMovementData::CreatureMovementData() : Ground(CreatureGroundMovementType
 std::string CreatureMovementData::ToString() const
 {
     constexpr std::array<char const*, 3> GroundStates = {"None", "Run", "Hover"};
-    constexpr std::array<char[15], 3> FlightStates = {"None", "DisableGravity", "CanFly"};
-    constexpr std::array<char[15], 3> ChaseStates  = {"Run", "CanWalk", "AlwaysWalk"};
-    constexpr std::array<char[15], 3> RandomStates = {"Walk", "CanRun", "AlwaysRun"};
+    constexpr std::array<char const*, 3> FlightStates = {"None", "DisableGravity", "CanFly"};
+    constexpr std::array<char const*, 3> ChaseStates  = {"Run", "CanWalk", "AlwaysWalk"};
+    constexpr std::array<char const*, 3> RandomStates = {"Walk", "CanRun", "AlwaysRun"};
 
     std::ostringstream str;
     str << std::boolalpha << "Ground: " << GroundStates[AsUnderlyingType(Ground)]
