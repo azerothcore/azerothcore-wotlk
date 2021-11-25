26f1e3ffb822512704be0b9a99a1a3c30de50b54
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-25 10:15:27 +0000

    chore(DB): import pending files
    Referenced commit(s): a220b54aa412734a8369cea3138b54f8542222e1

--- data/sql/updates/db_world/2021_11_25_00.sql
+++ data/sql/updates/db_world/2021_11_25_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_24_02 -> 2021_11_25_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_24_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_24_02 2021_11_25_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637770124917773500'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637770124917773500');
 
 SET @Guid := 14218;
@@ -74,3 +90,13 @@ UPDATE `creature_template` SET `AIName`= 'SmartAI' WHERE  `entry`=16048;
 DELETE FROM `smart_scripts` WHERE `entryorguid` =16048;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (16048, 0, 0, 0, 38,  0, 100, 1, 1, 1, 0, 0, 12, 16042, 8, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lord Valthalak Trigger - On Data Set - Spawn Lord Valthalak');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_25_00' WHERE sql_rev = '1637770124917773500';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
