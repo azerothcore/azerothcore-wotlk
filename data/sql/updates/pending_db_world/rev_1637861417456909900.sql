4ad16bd3013a5735e7fb892ee7b681f6c667b219
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-11-26 12:58:45 +0100

    add flag

--- data/sql/updates/pending_db_world/rev_1637861417456909900.sql
+++ data/sql/updates/pending_db_world/rev_1637861417456909900.sql
@@ -2,4 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637861417456909900');
 
 SET @flag := 268435456;
 
-UPDATE `spell_custom_attr` SET `attributes` = 16777216 WHERE `spell_id` = (SELECT `spell_id` WHERE `attributes` & @flag = @flag);
+UPDATE `spell_custom_attr` SET `attributes`=`attributes`|16777216 WHERE `spell_id` = (SELECT `spell_id` WHERE `attributes` & @flag = @flag);
