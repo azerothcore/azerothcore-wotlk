ef1fa8bb8c39a9c1495ebea9d7c9542c6e82dba3
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-11-26 14:52:21 -0300

    Update rev_1637948987614040500.sql

--- data/sql/updates/pending_db_world/rev_1637948987614040500.sql
+++ data/sql/updates/pending_db_world/rev_1637948987614040500.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637948987614040500');
 
-UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 128, `ScriptName` = '' WHERE `entry` = 12758;
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` |128, `ScriptName` = '' WHERE `entry` = 12758;
 UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 11262;
