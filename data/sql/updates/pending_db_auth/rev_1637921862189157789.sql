08c5b6c807f29ccdc592acfb6884cfabe414f08f
baranzadeoglu <84640316+baranzadeoglu@users.noreply.github.com>
2021-11-26 11:23:06 +0100

    Frigid ring fix
    Frigid ring removed from RLT
    Added to CLT

---
+++ data/sql/updates/pending_db_auth/rev_1637921862189157789.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637921862189157789');
+
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 24016) AND (`Item` IN (18679));
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 14457) AND (`Item` IN (18679));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(14457, 18679, 0, 40, 0, 1, 0, 1, 1, '');
+
