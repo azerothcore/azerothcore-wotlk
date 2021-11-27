68e322301af74c79a2e66f4085e8bd88f575f6a2
Beta.Yi <beta.yi@qq.com>
2021-11-27 10:07:26 +0800

    fix(DB/GameObject): ZAM hostage chest unlootble
    make the chest unlock and lootable

---
+++ data/sql/updates/pending_db_world/rev_1637978286218341866.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637978286218341866');
+
+update gameobject_template_addon set faction='95',flags='1' where entry in (186648,187021,186672,186667);
