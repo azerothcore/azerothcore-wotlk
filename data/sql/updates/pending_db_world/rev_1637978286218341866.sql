bc3d14c29a8f268c8535ba37178d45a0da3a2a93
Beta.Yi <beta.yi@qq.com>
2021-11-27 12:55:13 +0800

    Update rev_1637978286218341866.sql
    replace update with delete and insert;

--- data/sql/updates/pending_db_world/rev_1637978286218341866.sql
+++ data/sql/updates/pending_db_world/rev_1637978286218341866.sql
@@ -1,3 +1,8 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637978286218341866');
 
-update gameobject_template_addon set faction='94',flags='0' where entry in (186648,187021,186672,186667);
+delete from gameobject_template_addon where entry in (186648,187021,186672,186667);
+insert into gameobject_template_addon values
+(186648,94,0,0,0),
+(187021,94,0,0,0),
+(186672,94,0,0,0),
+(186667,94,0,0,0);
