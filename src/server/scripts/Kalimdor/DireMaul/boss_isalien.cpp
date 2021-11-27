d6baddb5ded6bebf9314b7e134299d853298fbda
Nefertumm <nefertum.dev@protonmail.com>
2021-11-27 12:48:13 -0300

    Fix indentation

--- src/server/scripts/Kalimdor/DireMaul/boss_isalien.cpp
+++ src/server/scripts/Kalimdor/DireMaul/boss_isalien.cpp
@@ -45,96 +45,95 @@ enum Points
     POINT_RANDOM = 1
 };
 
-    struct boss_isalien : public BossAI
+struct boss_isalien : public BossAI
+{
+    boss_isalien(Creature* creature) : BossAI(creature, DATA_ISALIEN) { }
+
+    void Reset() override
     {
-        boss_isalien(Creature* creature) : BossAI(creature, DATA_ISALIEN) { }
+        _Reset();
+        _scheduler.CancelAll();
+        summons.DespawnAll();
+        _phase = PHASE_NONE;
 
-        void Reset() override
+        _scheduler.SetValidator([this]
+            {
+                return !me->HasUnitState(UNIT_STATE_CASTING);
+            });
+    }
+
+    void DamageTaken(Unit* /*attacker*/, uint32& /*damage*/, DamageEffectType /*type*/, SpellSchoolMask /*school*/) override
+    {
+        if (_phase != PHASE_REGROWTH && me->HealthBelowPct(80.f))
         {
-            _Reset();
-            _scheduler.CancelAll();
-            summons.DespawnAll();
-            _phase = PHASE_NONE;
-
-            _scheduler.SetValidator([this]
+            _phase = PHASE_REGROWTH;
+            _scheduler.Schedule(25s, 30s, [this](TaskContext context)
                 {
-                    return !me->HasUnitState(UNIT_STATE_CASTING);
+                    Position randomPos = me->GetRandomPoint(me->GetPosition(), 15.f);
+                    me->GetMotionMaster()->MovePoint(POINT_RANDOM, randomPos);
+                    context.Schedule(3s, [this](TaskContext /*context*/)
+                        {
+                            me->GetMotionMaster()->Clear();
+                            me->GetMotionMaster()->MoveChase(me->GetVictim());
+                            DoCastSelf(SPELL_REGROWTH);
+                        });
+                    context.Repeat(25s, 30s);
                 });
         }
+    }
 
-        void DamageTaken(Unit* /*attacker*/, uint32& /*damage*/, DamageEffectType /*type*/, SpellSchoolMask /*school*/) override
-        {
-            if (_phase != PHASE_REGROWTH && me->HealthBelowPct(80.f))
+    void JustSummoned(Creature* summon) override
+    {
+        summons.Summon(summon);
+    }
+
+    void EnterCombat(Unit* /*who*/) override
+    {
+        _EnterCombat();
+        _scheduler.Schedule(4s, 5s, [this](TaskContext context)
             {
-                _phase = PHASE_REGROWTH;
-                _scheduler.Schedule(25s, 30s, [this](TaskContext context)
-                    {
-                        Position randomPos = me->GetRandomPoint(me->GetPosition(), 15.f);
-                        me->GetMotionMaster()->MovePoint(POINT_RANDOM, randomPos);
-                        context.Schedule(3s, [this](TaskContext /*context*/)
-                            {
-                                me->GetMotionMaster()->Clear();
-                                me->GetMotionMaster()->MoveChase(me->GetVictim());
-                                DoCastSelf(SPELL_REGROWTH);
-                            });
-                        context.Repeat(25s, 30s);
-                    });
-            }
-        }
-
-        void JustSummoned(Creature* summon) override
-        {
-            summons.Summon(summon);
-        }
-
-        void EnterCombat(Unit* /*who*/) override
-        {
-            _EnterCombat();
-            _scheduler.Schedule(4s, 5s, [this](TaskContext context)
-                {
-                    DoCastRandomTarget(SPELL_NET);
-                    context.Repeat(12s, 15s);
-                })
-            .Schedule(7s, 12s, [this](TaskContext context)
-                {
-                    DoCastRandomTarget(SPELL_MULTI_SHOT);
-                    context.Repeat(7s, 12s);
-                })
-            .Schedule(20s, 30s, [this](TaskContext context)
-                {
-                    DoCastAOE(SPELL_STARSHARDS);
-                    context.Repeat(20s, 30s);
-                })
-            .Schedule(8s, 10s, [this](TaskContext /*context*/)
-                {
-                    DoCastAOE(SPELL_CALL_EMPYREAN);
-                }
-            );
-        }
-
-        void UpdateAI(uint32 diff) override
-        {
-            if (!UpdateVictim())
+                DoCastRandomTarget(SPELL_NET);
+                context.Repeat(12s, 15s);
+            })
+        .Schedule(7s, 12s, [this](TaskContext context)
             {
-                return;
-            }
+                DoCastRandomTarget(SPELL_MULTI_SHOT);
+                context.Repeat(7s, 12s);
+            })
+        .Schedule(20s, 30s, [this](TaskContext context)
+            {
+                DoCastAOE(SPELL_STARSHARDS);
+                context.Repeat(20s, 30s);
+            })
+        .Schedule(8s, 10s, [this](TaskContext /*context*/)
+            {
+                DoCastAOE(SPELL_CALL_EMPYREAN);
+            });
+    }
 
-            _scheduler.Update(diff, [this]
-                {
-                    DoMeleeAttackIfReady();
-                });
-        }
-
-        void JustDied(Unit* /*killer*/) override
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
         {
-            _JustDied();
-            Talk(SAY_DEATH);
+            return;
         }
 
-        protected:
-            TaskScheduler _scheduler;
-            uint8 _phase;
-    };
+        _scheduler.Update(diff, [this]
+            {
+                DoMeleeAttackIfReady();
+            });
+    }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        _JustDied();
+        Talk(SAY_DEATH);
+    }
+
+protected:
+    TaskScheduler _scheduler;
+    uint8 _phase;
+};
 
 void AddSC_boss_isalien()
 {
