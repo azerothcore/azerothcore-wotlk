08580defd0c3a1699590cbad1aa4e981f46b04c4
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-11-27 13:33:50 -0300

    refactor(Scripts/Onyxia): Update Onyxia's script (#9371)

---
+++ data/sql/updates/pending_db_world/rev_1637948987614040500.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637948987614040500');
+
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` |128, `ScriptName` = '' WHERE `entry` = 12758;
+UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 11262;
+
+-- Remove taunt immunity
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` &~ 256 WHERE `entry` = 10184;
