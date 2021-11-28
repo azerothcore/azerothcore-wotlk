cd50ee1c8efed0aaa2142c25baba52bebbe4ad49
Malcrom <malcromdev@gmail.com>
2021-11-28 14:49:21 -0400

    fix(DB/SAI) Clean up event_type in Timed Actionlists

---
+++ data/sql/updates/pending_db_world/rev_1638125279373569035.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638125279373569035');
+
+-- Fix event_type in Timed Actionlists
+UPDATE `smart_scripts` SET `event_type`=0 WHERE `source_type`=9 AND event_type>0;
