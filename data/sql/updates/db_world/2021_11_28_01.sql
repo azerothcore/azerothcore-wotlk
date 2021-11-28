413f316b6c6fce05099e1330e57194a4d2f5334b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-28 12:04:14 +0000

    chore(DB): import pending files
    Referenced commit(s): e49da799965b6a3e63559e61b295d11bd686ba32

--- data/sql/updates/db_world/2021_11_28_01.sql
+++ data/sql/updates/db_world/2021_11_28_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_28_00 -> 2021_11_28_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_28_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_28_00 2021_11_28_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637109262496158800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637109262496158800');
 
 -- Alliance part
@@ -40,3 +56,13 @@ UPDATE `quest_template_addon` SET `RequiredMinRepFaction` = @FACTIONID, `Require
 
 -- Remove wrong repeatable flag
 UPDATE `quest_template_addon` SET `SpecialFlags` = 0 WHERE `ID` IN (7164, 7166, 7167, 7170, 7171, 7172);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_28_01' WHERE sql_rev = '1637109262496158800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
