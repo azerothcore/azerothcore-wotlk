456b1a086290be07d84ef74ca1bf3d70ccf9ea0e
valens14 <dylansingh61@aol.com>
2021-11-29 06:14:02 -0500

    fix(Raid/AQ40): Ouro not spawning (#9285)

---
+++ data/sql/updates/pending_db_world/rev_1637594598317103800.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637594598317103800');
+UPDATE `creature_template` SET `ScriptName`='npc_ouro_spawner' WHERE  `entry`=15957;
+UPDATE `creature_template` SET `flags_extra`='128' WHERE  `entry`=15957;
+UPDATE `creature_template` SET `InhabitType`='9' WHERE  `entry`=15957;
