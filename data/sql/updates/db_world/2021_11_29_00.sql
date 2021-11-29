5c3831f4da0f30124d1e89ae1d6ad354aa89ace4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-29 06:17:01 +0000

    chore(DB): import pending files
    Referenced commit(s): 32846657c7cca6e24d5d7983a6d9ec6e42e52a52

--- data/sql/updates/db_world/2021_11_29_00.sql
+++ data/sql/updates/db_world/2021_11_29_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_11_28_04 -> 2021_11_29_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_11_28_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_11_28_04 2021_11_29_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637426939576443600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637426939576443600');
 
 DELETE FROM `gameobject_loot_template` WHERE `Entry`=2265;
@@ -32,3 +48,13 @@ INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, 
 (2265, 2650, 0, 4.16, 0, 1, 1, 1, 1,'Battered Chest - Flimsy Chain Boots'),
 (2265, 2651, 0, 4.16, 0, 1, 1, 1, 1,'Battered Chest - Flimsy Chain Bracers'),
 (2265, 2652, 0, 4.16, 0, 1, 1, 1, 1,'Battered Chest - Flimsy Chain Cloak');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_11_29_00' WHERE sql_rev = '1637426939576443600';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
