e6b755d467f969a3f6cb2f70e9c11a25d4241aaf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-11-30 16:04:57 +0000

    chore(DB): import pending files
    Referenced commit(s): f9337497d87437245469e46f4a76e5b904cc08ac

--- data/sql/updates/pending_db_world/rev_1637861417456909900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637861417456909900');
-
-SET @flag := 268435456;
-
-UPDATE `spell_custom_attr` SET `attributes` = 16777216 WHERE `spell_id` = (SELECT `spell_id` WHERE `attributes` & @flag = @flag);
