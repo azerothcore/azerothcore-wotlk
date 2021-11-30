697a771038c0354267a4f97ae6f1fa47c9574682
Beta.Yi <beta.yi@qq.com>
2021-11-30 12:06:53 +0800

    Update data/sql/updates/pending_db_world/rev_1637978286218341866.sql
    thanks
    
    Co-authored-by: Nefertumm <nefertum.dev@protonmail.com>

--- data/sql/updates/pending_db_world/rev_1637978286218341866.sql
+++ data/sql/updates/pending_db_world/rev_1637978286218341866.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637978286218341866');
 
-delete from gameobject_template_addon where entry in (186648,187021,186672,186667);
+DELETE FROM `gameobject_template_addon` WHERE `entry` IN (186648,187021,186672,186667);
 insert into gameobject_template_addon values
 (186648,94,0,0,0),
 (187021,94,0,0,0),
