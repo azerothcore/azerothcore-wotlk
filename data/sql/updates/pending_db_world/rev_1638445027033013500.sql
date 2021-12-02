d84b6b19f321b3e54cdfa81f3ef95bf956645888
Cláudio Costa <54484196+claudiodfc@users.noreply.github.com>
2021-12-02 12:56:19 +0000

    Update rev_1638445027033013500.sql

--- data/sql/updates/pending_db_world/rev_1638445027033013500.sql
+++ data/sql/updates/pending_db_world/rev_1638445027033013500.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638445027033013500');
 
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 9456;
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 9456) AND (`source_type` = 0) AND (`id` IN (0));
