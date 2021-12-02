11a0b48c5981a25ec1cb71ec9e92ee356b778fcf
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-02 22:17:22 +0000

    chore(DB): import pending files
    Referenced commit(s): f0a10fd3c10a9063713dd60a943be59cf7b6e757

--- data/sql/updates/pending_db_world/rev_1638101787793400851.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638101787793400851');
-
--- Remove underlevel RLT 24075 from Blackhand Veteran
-DELETE FROM `creature_loot_template` WHERE `Entry` = 9819 AND `Reference` = 24075;
-
