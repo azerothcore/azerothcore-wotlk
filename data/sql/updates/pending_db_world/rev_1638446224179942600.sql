d8003c0f203606f93b5a2cfa5962be7c355d1f34
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-12-02 12:03:40 +0000

    fix(DB): Adding Mor'ogal's gossip

---
+++ data/sql/updates/pending_db_world/rev_1638446224179942600.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638446224179942600');
+
+UPDATE `creature_template` SET `gossip_menu_id`='1' WHERE  `entry`=11861;
+
+DELETE FROM `gossip_menu`WHERE `MenuID` = 1 AND `TextID` = 8;
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES (1, 8);
+
+DELETE FROM `npc_text`WHERE `ID` = 8;
+INSERT INTO `npc_text` (`text0_0`, `ID`) VALUES ('Stonetalon Mountains has many perilous caves through out this region. Exploring these caves can hold many adventures for those willing to risk life for glory.', 8);
