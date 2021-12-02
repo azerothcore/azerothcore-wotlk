0339fcd33862040ef3e4520624b8ceea71b950b2
Cláudio Costa <54484196+claudiodfc@users.noreply.github.com>
2021-12-02 12:58:47 +0000

    Update rev_1638444071475524100.sql

--- data/sql/updates/pending_db_world/rev_1638444071475524100.sql
+++ data/sql/updates/pending_db_world/rev_1638444071475524100.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638444071475524100');
 
-UPDATE `npc_text` SET `Probability0`='100' WHERE  `ID`=1875;
+UPDATE `npc_text` SET `Probability0`=100 WHERE  `ID`=1875;
