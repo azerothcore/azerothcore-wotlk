e329d956171ca78b7939a5016782f0bb49d7ddd4
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-12-02 08:35:17 +0000

    missing semicolon

--- data/sql/updates/pending_db_world/rev_1638433789445189200.sql
+++ data/sql/updates/pending_db_world/rev_1638433789445189200.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638433789445189200');
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 3284;
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 3284) AND (`source_type` = 0)
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 3284) AND (`source_type` = 0);
