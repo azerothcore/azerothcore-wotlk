2789d5b87784f2fb87eaa6c9ecdd79e92568faec
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-03 13:28:53 +0000

    chore(DB): import pending files
    Referenced commit(s): 6c3e54b8cdaeda2648535dc90d7ced86ff2a7b20

--- data/sql/updates/db_world/2021_12_03_05.sql
+++ data/sql/updates/db_world/2021_12_03_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_03_04 -> 2021_12_03_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_03_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_03_04 2021_12_03_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638107879444151149'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638107879444151149');
 
 -- The Duke of Cynders
@@ -64,3 +80,13 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 (15220,0,1,"You will pay the ultimate price for this insolence, little vermin.  Your soul is mine!",12,0,100,0,0,0,10801,0,"The Duke of Zephyrs"),
 (15220,0,2,"This act of defiance will not go unpunished.  You, and your world, will die!",12,0,100,0,0,0,10802,0,"The Duke of Zephyrs"),
 (15220,0,3,"Imposter!  It is a dishonor without equal to be summoned by a whelp such as you!  DIE!",12,0,100,0,0,0,10804,0,"The Duke of Zephyrs");
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_03_05' WHERE sql_rev = '1638107879444151149';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
