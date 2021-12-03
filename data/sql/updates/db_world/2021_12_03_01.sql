18762d940c44ff2a47947ac7e8b45f12f552b2a4
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-03 08:20:16 +0000

    chore(DB): import pending files
    Referenced commit(s): 7df43c36a5238edf252e7bec209fda0e08ede052

--- data/sql/updates/db_world/2021_12_03_01.sql
+++ data/sql/updates/db_world/2021_12_03_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_03_00 -> 2021_12_03_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_03_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_03_00 2021_12_03_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637946798791977700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637946798791977700');
 
 DELETE FROM `creature_text` WHERE `CreatureID` = 16080;
@@ -8,3 +24,13 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'boss_mor_grayhoof' WHERE `entry` = 16080;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 16080 AND `source_type` = 0;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_03_01' WHERE sql_rev = '1637946798791977700';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
