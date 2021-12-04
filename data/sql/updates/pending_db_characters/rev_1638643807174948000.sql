6a7f9891805593f29f4b2b70cee82c68030507b4
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-04 15:52:05 -0300

    Create rev_1638643807174948000.sql

---
+++ data/sql/updates/pending_db_characters/rev_1638643807174948000.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1638643807174948000');
+
+DROP TABLE IF EXISTS`character_settings`;
+CREATE TABLE IF NOT EXISTS `character_settings` (
+  `guid` INT UNSIGNED NOT NULL,
+  `source` INT UNSIGNED NOT NULL,
+  `data` TEXT NULL,
+  PRIMARY KEY (`guid`, `source`)
+) ENGINE=MYISAM DEFAULT CHARSET=utf8mb4 COMMENT='Player Settings'
