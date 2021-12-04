67126b6863ed732cb5856c0c39908b2d0f6d038e
Vox <ndinosaur1213@Live.com>
2021-12-04 13:29:42 -0700

    linebreak

--- data/sql/updates/pending_db_world/rev_1638646536763994700.sql
+++ data/sql/updates/pending_db_world/rev_1638646536763994700.sql
@@ -6,4 +6,3 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638646536763994700');
 DELETE FROM `creature` WHERE (`id` = 29110);
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (130896, 29110, 609, 0, 0, 1, 192, 0, 0, 2310.64, -5742.08, 161.3, 3.85123, 360, 0, 0, 27890000, 0, 0, 0, 0, 0, '', 0);
-
