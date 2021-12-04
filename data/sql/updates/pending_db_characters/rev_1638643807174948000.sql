55e3d9f4eadf05f1eb0b3b747fcf7e0a79b47c27
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-04 16:14:14 -0300

    fix sql

--- data/sql/updates/pending_db_characters/rev_1638643807174948000.sql
+++ data/sql/updates/pending_db_characters/rev_1638643807174948000.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1638643807174948000');
 
-DROP TABLE IF EXISTS`character_settings`;
+DROP TABLE IF EXISTS `character_settings`;
 CREATE TABLE IF NOT EXISTS `character_settings` (
   `guid` INT UNSIGNED NOT NULL,
   `source` INT UNSIGNED NOT NULL,
