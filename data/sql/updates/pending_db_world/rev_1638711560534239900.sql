559f6be3a30e11c26e40385f40c6cd4914069bdb
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-05 10:40:35 -0300

    fix(DB/SAI): Correct target for some Dalaran vendor welcome texts

---
+++ data/sql/updates/pending_db_world/rev_1638711560534239900.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638711560534239900');
+
+-- Ildine Sorrowspear / Edward Egan
+UPDATE `smart_scripts` SET `action_param3` = 1 WHERE `entryorguid` IN (28714, 28727) AND `source_type` = 0;
