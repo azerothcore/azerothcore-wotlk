95950104022408e444c649707907c95012709d04
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-05 19:16:02 +0000

    chore(DB): import pending files
    Referenced commit(s): c92721df3f02b2f3e62574c04aa05ffd26184dff

--- data/sql/updates/db_world/2021_12_05_05.sql
+++ data/sql/updates/db_world/2021_12_05_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_05_04 -> 2021_12_05_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_05_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_05_04 2021_12_05_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638112959691757857'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638112959691757857');
 
 -- Fix up npc Elder Torntusk
@@ -14,3 +30,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (1475700,9,2,0,0,0,100,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0, 'Elder Torntusk - On Script - Say Line 0'),
 (1475700,9,3,0,0,0,100,0,60000,60000,0,0,0,90,7,0,0,0,0,0,1,0,0,0,0,0,0,0,0, 'Elder Torntusk - On Script - Set Flag Standstate Dead'),
 (1475700,9,4,0,0,0,100,0,0,0,0,0,0,82,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0, 'Elder Torntusk - On Script - Add Npc Flag Gossip');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_05_05' WHERE sql_rev = '1638112959691757857';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
