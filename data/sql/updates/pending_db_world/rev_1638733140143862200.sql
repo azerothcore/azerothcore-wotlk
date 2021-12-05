3acd0c236247e57afc1260b67c91b9acf2ffe9fc
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-12-05 21:05:47 +0100

    Update rev_1638733140143862200.sql

--- data/sql/updates/pending_db_world/rev_1638733140143862200.sql
+++ data/sql/updates/pending_db_world/rev_1638733140143862200.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638733140143862200');
 
-DELETE FROM `game_event_creature` WHERE `eventEntry` IN (2, 24) AND `guid` = 2;
+DELETE FROM `game_event_creature` WHERE `eventEntry` IN (2, 24) AND `guid` = 24;
 INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
 (2, 24);
