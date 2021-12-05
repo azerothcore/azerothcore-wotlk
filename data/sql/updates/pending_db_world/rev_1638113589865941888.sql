141e81a181c9a4e0cde25b4f043b107a0c4bf218
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-05 19:25:12 +0000

    chore(DB): import pending files
    Referenced commit(s): 6778faf381f88226946e0862f331947a048ed1b8

--- data/sql/updates/pending_db_world/rev_1638113589865941888.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638113589865941888');
-
--- Add SAI to Deathstalker Vincent
-UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=4444;
