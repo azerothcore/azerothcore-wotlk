0adbbb588b5abbdf186b2435607d8f96706d2731
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-05 09:36:46 +0000

    chore(DB): import pending files
    Referenced commit(s): d6fc2a1d52a338afb5a894354eafe335025e12f4

--- data/sql/updates/db_world/2021_12_05_00.sql
+++ data/sql/updates/db_world/2021_12_05_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_04_03 -> 2021_12_05_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_04_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_04_03 2021_12_05_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638657340'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638657340');
 
 /* Dr Weavil - Mental Domination and Mind Shatter SAI */
@@ -10,3 +26,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (15552, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dr Weavil - MovementInform - Despawn'),
 (15552, 0, 5, 0, 0, 0, 100, 0, 5000, 8000, 55000, 60000, 0, 11, 25772, 32, 1, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Dr Weavil - In Combat - Cast Mental Domination'),
 (15552, 0, 6, 0, 0, 0, 100, 0, 4000, 7000, 14000, 16500, 0, 11, 25774, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dr Weavil - In Combat - Cast Mind Shatter');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_05_00' WHERE sql_rev = '1638657340';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
