7604c5e0df8ef2427ec41b83840f7964bb2fe6a7
eronox <eronoxsmail@gmail.com>
2021-12-05 14:58:44 +0100

    fix(DB/SAI): Make all generically spawned traps invisible

--- data/sql/updates/pending_db_world/rev_1638658608.sql
+++ data/sql/updates/pending_db_world/rev_1638658608.sql
@@ -1,15 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638658608');
 
-/* Zul'Farrak Shallow Grave TRAP - invisible */
-UPDATE `gameobject_template` SET `Data11` = 1 WHERE (`entry` = 128972);
-
 /* Zul'Farrak Shallow Grave - On Gossip Hello - Despawn after 5min */
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 128308 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 128308 AND `id` = 1 AND `source_type` = 1;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (128308, 1, 1, 0, 64, 0, 100, 0, 0, 0, 0, 0, 0, 41, 300000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shallow Grave - On Gossip Hello - Despawn');
 
-/* Shellfish Trap - Invisible */
-UPDATE gameobject_template SET Data11 = 1 WHERE (entry = 176592);
-
-/* Resonite Crystal Trap - Invisible */
-UPDATE `gameobject_template` SET `Data11` = 1 WHERE (`entry` = 178124);
+/* Make generically spawned traps invisible - They share displayId */
+/* For example, fixes: ZF Grave traps, shellfish traps, resonite crystal traps and more..*/
+UPDATE gameobject_template SET `data11` = 1 WHERE displayid = 327 AND TYPE = 6;
