ddd6fd0984ca3b0446e00c58e9578058ceaf8e54
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-06 14:12:22 +0000

    chore(DB): import pending files
    Referenced commit(s): c6a1c2c985d8cb97e47078f94fbfc484b08a3d2f

--- data/sql/updates/db_world/2021_12_06_02.sql
+++ data/sql/updates/db_world/2021_12_06_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_06_01 -> 2021_12_06_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_06_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_06_01 2021_12_06_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638445356401170100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638445356401170100');
 
 UPDATE `creature` SET `spawntimesecs`=60 WHERE `id`=15664;
@@ -18,3 +34,13 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 DELETE FROM `spell_scripts` WHERE `id`=25952;
 INSERT INTO `spell_scripts` (`id`,`effIndex`,`delay`,`command`,`datalong`,`datalong2`,`dataint`,`x`,`y`,`z`,`o`) VALUES
 (25952,0,0,18,0,0,0,0,0,0,0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_06_02' WHERE sql_rev = '1638445356401170100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
