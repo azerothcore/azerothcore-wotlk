1448b53c3fe6515f67988b1e788b87593a436073
acidmanifesto <joshua.lee.betts@gmail.com>
2021-12-06 20:11:55 +0100

    Update data/sql/updates/pending_db_world/rev_1638737032167850775.sql
    Co-authored-by: Stefano Borzì <stefanoborzi32@gmail.com>

--- data/sql/updates/pending_db_world/rev_1638737032167850775.sql
+++ data/sql/updates/pending_db_world/rev_1638737032167850775.sql
@@ -2,4 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638737032167850775');
 
 -- Makes quest Lazy Peon broadcast work work only from themselves and not the player (action_para2)
 -- Sets it up where the target_type is the player and only triggers with in 20 yards
-UPDATE `smart_scripts` SET `action_param2`=1, `target_type`=18, `target_param1`=20 WHERE  `comment`="Lazy Peon - On Script - Play Sound 6197";
+UPDATE `smart_scripts` SET `action_param2`=1, `target_type`=18, `target_param1`=20 WHERE  `comment`='Lazy Peon - On Script - Play Sound 6197';
