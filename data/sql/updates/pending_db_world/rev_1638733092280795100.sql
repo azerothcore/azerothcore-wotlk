b196bf8d351c4d8daac3734b746c28a2e1586363
Nefertumm <nefertum.dev@protonmail.com>
2021-12-06 00:30:54 -0300

    Sniff

--- data/sql/updates/pending_db_world/rev_1638733092280795100.sql
+++ data/sql/updates/pending_db_world/rev_1638733092280795100.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638733092280795100');
 
-UPDATE `creature_template` SET `faction` = 22 WHERE `entry` = 4263;
+UPDATE `creature_template` SET `faction` = 312 WHERE `entry` = 4263;
