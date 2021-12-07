62ffdc000a6a032cf25586c7fa214e52de3e1194
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-07 02:34:35 +0000

    chore(DB): import pending files
    Referenced commit(s): 3e6f0e588db7773ede981912a0ffc35f8a803d91

--- data/sql/updates/db_world/2021_12_07_03.sql
+++ data/sql/updates/db_world/2021_12_07_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_07_02 -> 2021_12_07_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_07_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_07_02 2021_12_07_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638437762578877300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638437762578877300');
 
 -- Water seekers cast a poison spell that didn't exist on the SmartAI.
@@ -36,3 +52,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (3258, 0, 0, 0, 0, 0, 100, 0, 1000, 5000, 3000, 4000, 0, 11, 6660, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Bristleback Hunter - In Combat - Cast \'Shoot\''),
 (3258, 0, 1, 0, 0, 0, 100, 0, 1000, 5000, 6000, 8000, 0, 11, 8806, 32, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Bristleback Hunter - In Combat - Cast \'Poisoned Shot\''),
 (3258, 0, 2, 0, 2, 0, 100, 1, 1, 15, 0, 0, 0, 25, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Bristleback Hunter - Between 1-15% Health - Flee For Assist (No Repeat)');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_07_03' WHERE sql_rev = '1638437762578877300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
