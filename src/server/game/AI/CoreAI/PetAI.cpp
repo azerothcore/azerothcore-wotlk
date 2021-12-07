dbaf1167c8b8faf6e6776b01ae31ab95421e0905
UltraNix <ultranix@gmail.com>
2021-12-07 19:08:12 +0100

    fix(Core/Pets): Properly cast spells on friendly target if not in LoS or out of range.
    Fixes #7790

--- src/server/game/AI/CoreAI/PetAI.cpp
+++ src/server/game/AI/CoreAI/PetAI.cpp
@@ -174,7 +174,7 @@ void PetAI::UpdateAI(uint32 diff)
         else
             _doMeleeAttack();
     }
-    else if (!me->GetCharmInfo() || (!me->GetCharmInfo()->GetForcedSpell() && !me->HasUnitState(UNIT_STATE_CASTING)))
+    else if (!me->GetCharmInfo() || (!me->GetCharmInfo()->GetForcedSpell() && !(me->IsPet() && me->ToPet()->HasTempSpell()) && !me->HasUnitState(UNIT_STATE_CASTING)))
     {
         if (me->HasReactState(REACT_AGGRESSIVE) || me->GetCharmInfo()->IsAtStay())
         {
