733b575e440efc22c796b65b0669f8216f30c368
Vox <ndinosaur1213@Live.com>
2021-12-07 18:27:13 -0700

    loottemplate

---
+++ data/sql/updates/pending_db_world/rev_1638926789289565100.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638926789289565100');
+
+DELETE FROM `creature_loot_template` WHERE (`Item` IN (8956, 9061, 9030, 4623, 9144, 9154, 9179, 23427, 2772));
