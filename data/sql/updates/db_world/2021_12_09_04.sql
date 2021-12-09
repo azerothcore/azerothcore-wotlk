d007fe1ca49312078362a152e9a9a8a4bb802a9a
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-09 12:24:33 +0000

    chore(DB): import pending files
    Referenced commit(s): 438fce9326fd2b9790af5896a58ae5eb4fa7281d

--- data/sql/updates/db_world/2021_12_09_04.sql
+++ data/sql/updates/db_world/2021_12_09_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_09_03 -> 2021_12_09_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_09_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_09_03 2021_12_09_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638374497455068200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638374497455068200');
 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` IN (15)) AND (`SourceGroup` = 7083);
@@ -19,3 +35,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (16014, 0, 1, 0, 62, 0, 100, 0, 7083, 1, 0, 0, 0, 56, 21946, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Mux Manascrambler - On Gossip Option 1 Selected - Add Item \'Ectoplasmic Distiller\' 1 Time'),
 (16014, 0, 2, 0, 62, 0, 100, 0, 7083, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Mux Manascrambler - On Gossip Option 0 Selected - Close Gossip'),
 (16014, 0, 3, 0, 62, 0, 100, 0, 7083, 1, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Mux Manascrambler - On Gossip Option 1 Selected - Close Gossip');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_09_04' WHERE sql_rev = '1638374497455068200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
