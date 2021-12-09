bfffa709822b9ff67992c33b74548060339254a2
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-09 09:57:42 +0000

    chore(DB): import pending files
    Referenced commit(s): 2ba68d65b5fa365f0513243c94a82200f10f0dfa

--- data/sql/updates/db_world/2021_12_09_03.sql
+++ data/sql/updates/db_world/2021_12_09_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_09_02 -> 2021_12_09_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_09_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_09_02 2021_12_09_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637975402275290929'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637975402275290929');
 
 -- Update Primary Keys for creature_loot_template
@@ -842,3 +858,13 @@ UPDATE `creature_template` SET `mingold`=1, `maxgold`=7 WHERE `entry` IN (16522)
 
 -- *** Sunstrider Isle ***
 UPDATE `creature_template` SET `mingold`=1, `maxgold`=7 WHERE `entry` IN (15367);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_09_03' WHERE sql_rev = '1637975402275290929';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
