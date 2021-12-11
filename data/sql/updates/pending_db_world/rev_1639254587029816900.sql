a5f9400f3526700c17356aab6730bcc0fcbff52e
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-12-11 21:45:11 +0100

    Freya - Touch of Eonar

--- data/sql/updates/pending_db_world/rev_1639254587029816900.sql
+++ data/sql/updates/pending_db_world/rev_1639254587029816900.sql
@@ -6,4 +6,5 @@ INSERT INTO `spelldifficulty_dbc`(`id`, `DifficultySpellID_1`, `DifficultySpellI
 (63821, 63821, 64001), -- Mimiron - Falling Rubble
 (63666, 63666, 65026), -- Mimiron - Napalm Shell
 (62478, 62478, 63512), -- Hodir - Frozen Blows
+(62528, 62528, 62892), -- Freya - Touch of Eonar
 (62411, 62411, 62413 /* Ancient Rune Giant (Thorim) - Stomp */);
