c69f1050be73b5ecbb237ddba18149cb8b07887e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-11 12:51:18 +0000

    chore(DB): import pending files
    Referenced commit(s): ad86b1ef14a8313567ba478109f748b691f5e400

--- data/sql/updates/pending_db_world/rev_1638733183561519300.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638733183561519300');
-
-UPDATE `gameobject_template` SET `AIName`='' WHERE `entry`=190351;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=190351 AND `source_type`=1;
-DELETE FROM `spell_scripts` WHERE `id`=50499;
-INSERT INTO `spell_scripts` (`id`,`effIndex`,`delay`,`command`,`datalong`,`datalong2`,`dataint`,`x`,`y`,`z`,`o`) VALUES
-(50499,0,0,15,50493,0,0,0,0,0,0);
