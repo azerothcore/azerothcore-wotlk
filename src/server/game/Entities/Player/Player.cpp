859c28dc715fc84681a693c5033f874e0595fc90
Ayahuasca <arthur.fegne@hotmail.fr>
2021-12-12 00:18:44 +0100

    Move the removal under the 100yards condition
    This removes the specified auras
    only if teleports more than 100 yards away
    and avoids spells such as blink or shadowstep
    removing such auras

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1357,9 +1357,6 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
     SetUnitMovementFlags(GetUnitMovementFlags() & MOVEMENTFLAG_MASK_HAS_PLAYER_STATUS_OPCODE);
     DisableSpline();
 
-    // remove auras that should be removed whenever being teleported
-    RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_TELEPORTED);
-
     // Xinef: Remove all movement imparing effects auras, skip small teleport like blink
     if (mapid != GetMapId() || GetDistance2d(x, y) > 100)
     {
@@ -1367,6 +1364,8 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
         RemoveAurasByType(SPELL_AURA_MOD_FEAR);
         RemoveAurasByType(SPELL_AURA_MOD_CONFUSE);
         RemoveAurasByType(SPELL_AURA_MOD_ROOT);
+        // remove auras that should be removed when being teleported
+        RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_TELEPORTED);
     }
 
     if (m_transport)
