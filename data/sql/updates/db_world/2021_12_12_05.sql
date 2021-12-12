48c7aa4936356303126d57a297cb2875e4fd930e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-12 19:44:18 +0000

    chore(DB): import pending files
    Referenced commit(s): 4f3df3d2dedbb8ea86682df014b3cc21a955877b

--- data/sql/updates/db_world/2021_12_12_05.sql
+++ data/sql/updates/db_world/2021_12_12_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_12_04 -> 2021_12_12_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_12_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_12_04 2021_12_12_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638933646064544700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638933646064544700');
 
 /* Fix incorrect quest tracking status.
@@ -13,3 +29,13 @@ UPDATE `quest_template` SET `QuestCompletionLog`='Return to Shen\'dralar Ancient
 */
 
 UPDATE `quest_poi` SET `ObjectiveIndex`=0 WHERE  `QuestID`=7461 AND `id`=2;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_12_05' WHERE sql_rev = '1638933646064544700';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
