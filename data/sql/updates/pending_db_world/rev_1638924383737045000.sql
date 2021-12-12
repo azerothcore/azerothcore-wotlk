e9d45feb9a8a13890dbecc283e1ccf973d34af92
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-12 17:52:32 +0000

    chore(DB): import pending files
    Referenced commit(s): ce1516103f1e44cc0931b4f05a9e7e3a5a172478

--- data/sql/updates/pending_db_world/rev_1638924383737045000.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638924383737045000');
-
-/* Mining type_flag for NPC Tavarok
-*/
-
-UPDATE `creature_template` SET `type_flags` = 512 WHERE (`entry` = 18343);
-UPDATE `creature_template` SET `type_flags` = 512 WHERE (`entry` = 20268);
