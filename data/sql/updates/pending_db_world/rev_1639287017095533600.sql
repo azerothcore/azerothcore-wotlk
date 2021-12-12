88eb8d82776a3677cc4331ff13c5b8e0b8046409
Vox <ndinosaur1213@Live.com>
2021-12-11 23:33:11 -0700

    Update rev_1639287017095533600.sql

--- data/sql/updates/pending_db_world/rev_1639287017095533600.sql
+++ data/sql/updates/pending_db_world/rev_1639287017095533600.sql
@@ -2,12 +2,14 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES
 
 /* Eskhandar Ghostly Aura
 */
+
 DELETE FROM `creature_template_addon` WHERE (`entry` = 14306);
-INSERT INTO `creature_template_addon` (`entry`, `visibilityDistanceType`, `auras`) VALUES 
-(14306, 0, 22650);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(14306, 0, 0, 0, 0, 0, 0, 22650);
 
 /* Eskhandar Combat Script
 */
+
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 14306;
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 14306) AND (`source_type` = 0) AND (`id` IN (0, 1));
