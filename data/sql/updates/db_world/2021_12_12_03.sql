109de44bb81e5be5462e8464d941140329c986b7
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-12 16:59:32 +0000

    chore(DB): import pending files
    Referenced commit(s): 8c8ac2bef2515709e171ffbcc0f4a2a9ff5bac97

--- data/sql/updates/db_world/2021_12_12_03.sql
+++ data/sql/updates/db_world/2021_12_12_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_12_02 -> 2021_12_12_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_12_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_12_02 2021_12_12_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638926789289565100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638926789289565100');
 
 /* Remove Remove Oil of Immolation, Restorative Potion, Lesser Stoneshield Potion, Elixir of Detect Undead, 
@@ -14,3 +30,13 @@ DELETE FROM `creature_loot_template` WHERE (`Entry` = 12178) AND (`Item` IN (906
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 13136) AND (`Item` IN (9061));
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 13416) AND (`Item` IN (9061));
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 22576) AND (`Item` IN (9061));
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_12_03' WHERE sql_rev = '1638926789289565100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
