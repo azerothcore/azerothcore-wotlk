ad09e22fa0096dee87c1f0ca37e38b05e7ea2759
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-12-13 21:10:23 +0000

    codestyle

--- data/sql/updates/pending_db_world/rev_1639428645054557700.sql
+++ data/sql/updates/pending_db_world/rev_1639428645054557700.sql
@@ -4,4 +4,4 @@ UPDATE `quest_template` SET `RewardNextQuest` = 1518 WHERE `ID` = 1517;
 UPDATE `quest_template` SET `RewardNextQuest` = 1521 WHERE `ID` = 1520;
 UPDATE `quest_template` SET `RewardNextQuest` = 1517 WHERE `ID` = 1516;
 UPDATE `quest_template` SET `RewardNextQuest` = 1520 WHERE `ID` = 1519;
-UPDATE `quest_template` SET `Flags`='65536' WHERE `ID` IN (1463, 1462);
+UPDATE `quest_template` SET `Flags`= 65536 WHERE `ID` IN (1463, 1462);
