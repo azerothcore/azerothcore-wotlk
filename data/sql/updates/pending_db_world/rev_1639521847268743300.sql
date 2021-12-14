2d708465e427aa97f7eafa8bd7b28832966dbad8
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-12-14 23:46:44 +0000

    codestyle

--- data/sql/updates/pending_db_world/rev_1639521847268743300.sql
+++ data/sql/updates/pending_db_world/rev_1639521847268743300.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639521847268743300');
 
-UPDATE `creature_template` SET `mechanic_immune_mask` = 646135647 WHERE (`entry` IN (11502, 12057));
-UPDATE `creature_template` SET `mechanic_immune_mask` = 646135583 WHERE (`entry` = 12056);
+UPDATE `creature_template` SET `mechanic_immune_mask`= `mechanic_immune_mask`&~ 16384 WHERE (`entry` IN (11502, 12057, 12056));
