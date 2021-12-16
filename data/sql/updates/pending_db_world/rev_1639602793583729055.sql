7a6058e2b1f4b0584a4b3baa6f5a45c4619ce917
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-16 18:30:42 +0000

    chore(DB): import pending files
    Referenced commit(s): e05b78b9223d66a2a604186cbe84caab3ae9f4bd

--- data/sql/updates/pending_db_world/rev_1639602793583729055.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639602793583729055');
-
--- remove item from loot table
-DELETE FROM `creature_loot_template` WHERE  `Entry`=14277 AND `Item`=3820 AND `Reference`=0 AND `GroupId`=0;
