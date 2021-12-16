1331fd03e5a6943906004c50bff6082e27ae8a09
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-16 11:16:32 +0000

    chore(DB): import pending files
    Referenced commit(s): 9b3b61139a497ef801522b0274356c619512a8bc

--- data/sql/updates/db_world/2021_12_16_03.sql
+++ data/sql/updates/db_world/2021_12_16_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_16_02 -> 2021_12_16_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_16_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_16_02 2021_12_16_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1632675335691344600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1632675335691344600');
 
 -- add yells emperor
@@ -41,3 +57,13 @@ UPDATE `creature_template` SET `unit_class` = 8 WHERE `entry` IN (8929, 10076);
 UPDATE `creature_template` SET `ScriptName` = 'boss_eviscerator' WHERE (`entry` = 9029);
 UPDATE `creature_template` SET `ScriptName` = 'boss_okthor' WHERE (`entry` = 9030);
 UPDATE `creature_template` SET `ScriptName` = 'boss_hedrum' WHERE (`entry` = 9032);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_16_03' WHERE sql_rev = '1632675335691344600';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
