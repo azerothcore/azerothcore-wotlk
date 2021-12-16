d912a90fa3bda19ca01adce497705ff07f8a6889
Asthalor <87535580+Asthalor@users.noreply.github.com>
2021-12-16 10:06:21 +0100

    Disabled quest A little luck (6606)

--- data/sql/updates/pending_db_world/rev_1637918089302728408.sql
+++ data/sql/updates/pending_db_world/rev_1637918089302728408.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637918089302728408');
 
--- Add quest A Little Luck [6606] as a requisite to get Luck Be With You [969] quest
-UPDATE `quest_template_addon` SET `PrevQuestID` = 6606 WHERE (`ID` = 969);
+-- Disabled quest A little luck (6606)
+UPDATE `quest_template` SET `QuestType` = 1 WHERE (`ID` = 6606);
 
