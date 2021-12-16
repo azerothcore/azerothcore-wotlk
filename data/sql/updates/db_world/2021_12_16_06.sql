b7cb73fd78ef6796e36827d5401b5805e99f497d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-16 16:48:56 +0000

    chore(DB): import pending files
    Referenced commit(s): fdbf5ab75aff61cde61f007060a3ec210dea2246

--- data/sql/updates/db_world/2021_12_16_06.sql
+++ data/sql/updates/db_world/2021_12_16_06.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_16_05 -> 2021_12_16_06
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_16_05';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_16_05 2021_12_16_06 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1639063426788133858'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639063426788133858');
 
 -- Remove some overspawns
@@ -387,3 +403,13 @@ UPDATE `creature` SET `position_x`=7849.493, `position_y`=-7654.489, `position_z
 UPDATE `creature` SET `position_x`=7917.6772, `position_y`=-7651.6997, `position_z`=118.304405, `orientation`=5.524006366729736328 WHERE `guid`=82021;
 UPDATE `creature` SET `position_x`=7681.146, `position_y`=-7685.6973, `position_z`=121.68097, `orientation`=4.341725349426269531 WHERE `guid`=82022;
 UPDATE `creature` SET `wander_distance`=20, `MovementType`=1 WHERE `id` IN (16327,16328);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_16_06' WHERE sql_rev = '1639063426788133858';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
