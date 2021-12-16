c822ef969bae976a753cc559c4ed351243ec89bc
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-16 09:36:51 +0000

    chore(DB): import pending files
    Referenced commit(s): 8545257cc808bdcba923ae136320c08beaa6aee7

--- data/sql/updates/db_world/2021_12_16_00.sql
+++ data/sql/updates/db_world/2021_12_16_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_15_00 -> 2021_12_16_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_15_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_15_00 2021_12_16_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638832657384544715'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638832657384544715');
 
 UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_tharnarian' WHERE `entry`=3701;
@@ -14,3 +30,13 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=370100 AND `source_type`=9;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceGroup`=1 AND `SourceEntry`=9439 AND `SourceId`=0;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 1, 9439, 0, 0, 31, 0, 3, 2164, 0, 0, 0, 0, '', 'Bear Captured in Trap (effect 0) will hit the potential target of the spell if target is Rabid Thistle Bear.');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_16_00' WHERE sql_rev = '1638832657384544715';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
