0f83d52f4345e07f97a95fc48382a9715b99d962
UltraNix <80540499+UltraNix@users.noreply.github.com>
2021-12-16 11:18:21 +0100

    fix(Core/Trainers): add new column `ReqSpell` to `npc_trainer` table (#9490)
    Prevent learning gnomish and goblin engineering recipes at same time.
    Fixes #5390
    
    Co-authored-by: Skjalf <47818697+Nyeriah@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1638617008671223200.sql
@@ -0,0 +1,48 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638617008671223200');
+
+-- Gnomish Engineering
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12897;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=30575;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=30574;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12907;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=56473;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12905;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=30570;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12903;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=23096;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=30568;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=15633;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=23129;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=11454;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12906;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12759;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12902;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=30569;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=12899;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=23489;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=36955;
+UPDATE `npc_trainer` SET `ReqSpell`=20219 WHERE `SpellID`=11454;
+
+-- Goblin Engineering
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=23486;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=36954;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=30565;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=30566;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12755;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12718;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12908;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=9273;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=23078;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12717;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12716;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=8895;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12758;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=30563;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12760;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=15628;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=30560;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12754;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=30558;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=12715;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=13240;
+UPDATE `npc_trainer` SET `ReqSpell`=20222 WHERE `SpellID`=56514;
