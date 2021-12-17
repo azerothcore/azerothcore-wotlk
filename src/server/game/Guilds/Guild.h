9eab6d373dcefa22ea260f9f000603e118931bea
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-12-17 18:06:32 +0100

    fix(Core/Guilds): Implement guild bank gold limit matching client (#9666)
    Co-authored-by: Shauren <shauren.trinity@gmail.com>

--- src/server/game/Guilds/Guild.h
+++ src/server/game/Guilds/Guild.h
@@ -54,6 +54,8 @@ enum GuildMisc
     TAB_UNDEFINED                       = 0xFF,
 };
 
+constexpr uint64 GUILD_BANK_MONEY_LIMIT = UI64LIT(0x7FFFFFFFFFFFF);
+
 enum GuildMemberData
 {
     GUILD_MEMBER_DATA_ZONEID,
