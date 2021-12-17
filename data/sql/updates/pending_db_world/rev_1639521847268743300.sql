45a9695b5d0706c079b9e37aee38a4679f5a329f
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-17 17:48:54 +0000

    chore(DB): import pending files
    Referenced commit(s): 3402806c502a679401528fd8061715b490051485

--- data/sql/updates/pending_db_world/rev_1639521847268743300.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639521847268743300');
-
-UPDATE `creature_template` SET `mechanic_immune_mask`= `mechanic_immune_mask`&~ 16384 WHERE (`entry` IN (11502, 12057, 12056));
