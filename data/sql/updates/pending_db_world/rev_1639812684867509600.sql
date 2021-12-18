caae4c7a68d0252d2d166b1984a2253e03c05bc4
neifion-00000000 <vbdudl@gmail.com>
2021-12-18 01:37:35 -0600

    fix(db): 4 NPCS in sw shadowglen with "wait" movetype location fixed
    2 Grell and 2 Grellkin were incorrectly placed.
    
    Uploaded sniff that was taken while working/exploring to submissions.

---
+++ data/sql/updates/pending_db_world/rev_1639812684867509600.sql
@@ -0,0 +1,12 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639812684867509600');
+
+-- Source: TBC Classic 2.5.2.41446
+/* Grell Camp stay mobs placement */
+REPLACE INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `MovementType`) VALUES (47286, 1988, 10265.5, 967.318, 1340.81, 6.18836, 0, 0);
+
+REPLACE INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `MovementType`) VALUES (47284, 1988, 10267.1, 964.477, 1340.85, 0.844044, 0, 0);
+
+/* Grellkin Camp stay mobs placement */
+REPLACE INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `MovementType`) VALUES (47334, 1989, 10331.1, 1036.3, 1339.32, 5.85673, 0, 0);
+
+REPLACE INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `MovementType`) VALUES (47344, 1989, 10339.1, 1040.53, 1339.32, 4.86607, 0, 0);
