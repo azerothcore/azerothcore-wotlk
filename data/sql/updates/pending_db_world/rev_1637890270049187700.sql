ce9c8e3ba383c065e0541a683c87dcb572a40a92
claudiodfc <claudio.daniel.f.c@gmail.com>
2021-12-20 17:08:33 +0000

    adding Baron to elemental invasions

--- data/sql/updates/pending_db_world/rev_1637890270049187700.sql
+++ data/sql/updates/pending_db_world/rev_1637890270049187700.sql
@@ -1,5 +1,9 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637890270049187700');
 
+-- Adding Boss to Game event 13: Elemental Invasions
+DELETE FROM `game_event_creature` WHERE `eventEntry` = 13 AND `guid` = 14461;
+INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES ('13', '14461');
+
 UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI' WHERE `entry` = 179666;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 1 AND `entryorguid` = 179666);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
