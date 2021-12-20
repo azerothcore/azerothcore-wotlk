35a347881678b002e844955a1b370a28cf863ae3
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2021-12-19 18:54:38 -0600

    space after IN

--- data/sql/updates/pending_db_world/rev_1639812684867509600.sql
+++ data/sql/updates/pending_db_world/rev_1639812684867509600.sql
@@ -2,7 +2,7 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639812684867509600');
 
 -- Source: TBC Classic 2.5.2.41446
 
-DELETE FROM creature WHERE guid IN(47284, 47286, 47334, 47344);
+DELETE FROM creature WHERE guid IN (47284, 47286, 47334, 47344);
 
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 /* Grell Camp stay mobs placement */
