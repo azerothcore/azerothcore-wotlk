953c6c3eae250602ed096388520f4dd918a9e11d
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-22 23:33:35 +0000

    chore(DB): import pending files
    Referenced commit(s): a8ca9228738774799b5f62992d0d2df65ec1a3d0

--- data/sql/updates/db_world/2021_12_22_01.sql
+++ data/sql/updates/db_world/2021_12_22_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_22_00 -> 2021_12_22_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_22_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_22_00 2021_12_22_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1640207292899393200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640207292899393200');
 
 -- Flamewalker Priest - Remove items not reflected to tbc.wowhead
@@ -61,3 +77,13 @@ DELETE FROM `creature_loot_template` WHERE `Entry` = 11664 AND `Item` IN (22390,
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (11664, 22390, 0, 0.1, 0, 1, 0, 1, 1, 'Flamewaker Elite - Plans: Persuader'),
 (11664, 3475, 0, 0.1, 0, 1, 0, 1, 1, 'Flamewaker Elite - Cloak of Flames');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_22_01' WHERE sql_rev = '1640207292899393200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
