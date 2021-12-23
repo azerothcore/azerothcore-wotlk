9465bd2f13d16b6cbee4fdbac0808632c123f051
acidmanifesto <joshua.lee.betts@gmail.com>
2021-12-23 14:54:54 -0500

    Update rev_1640281609265649594.sql
    adjustment to standards

--- data/sql/updates/pending_db_world/rev_1640281609265649594.sql
+++ data/sql/updates/pending_db_world/rev_1640281609265649594.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640281609265649594');
 
 -- High Warlord Whirlaxis is not skinnable fix. Never was skinnable. Flags Corrected.
-UPDATE `creature_template` SET `unit_flags`='0', `skinloot`='0' WHERE  `entry`=15204;
+UPDATE `creature_template` SET `unit_flags`=0, `skinloot`=0 WHERE  `entry`=15204;
