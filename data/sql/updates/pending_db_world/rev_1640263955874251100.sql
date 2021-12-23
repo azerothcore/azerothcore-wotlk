fc4892357ac4f2dfe3e96f2b9115bc55d3c199ec
eronox <eronoxsmail@gmail.com>
2021-12-23 13:54:58 +0100

    abyssal crest for you and you and you and you

---
+++ data/sql/updates/pending_db_world/rev_1640263955874251100.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640263955874251100');
+
+-- Set 100% drop chance for Abyssal crest in Crimson Templar / Azure Templar / Hoary Templar / Earthen Templar
+UPDATE `creature_loot_template` SET `Chance`='100' WHERE  9 AND `Item`=20513 AND `Reference`=0 AND `GroupId`=0;
