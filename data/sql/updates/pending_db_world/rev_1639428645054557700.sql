60e8a4ebd8e8114f32126ebe96ed28ef600b480f
Cláudio Costa <54484196+claudiodfc@users.noreply.github.com>
2021-12-23 08:25:40 +0000

    fix(DB/Quest): Updated Durotar shaman quests (#9691)
    * fix(db): Updated Durotar shaman quests
    
    - Missing RewardNextQuest for 'Call of Earth'
    - Made 'Earth Sapta' a repeatable quest to mimic the blue question marks
      from retail (Classic/Vanilla).

---
+++ data/sql/updates/pending_db_world/rev_1639428645054557700.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639428645054557700');
+
+UPDATE `quest_template` SET `RewardNextQuest` = 1518 WHERE `ID` = 1517;
+UPDATE `quest_template` SET `RewardNextQuest` = 1521 WHERE `ID` = 1520;
+UPDATE `quest_template` SET `RewardNextQuest` = 1517 WHERE `ID` = 1516;
+UPDATE `quest_template` SET `RewardNextQuest` = 1520 WHERE `ID` = 1519;
+UPDATE `quest_template` SET `Flags`= 65536 WHERE `ID` IN (1463, 1462);
