12527775e4a5c751f0f1742087634dcafcaf6072
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-23 15:50:13 +0000

    chore(DB): import pending files
    Referenced commit(s): d2ee50a005b6d0d3be10d4451c42e21fca0b7724

--- data/sql/updates/pending_db_world/rev_1640263955874251100.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640263955874251100');
-
--- Set 100% drop chance for Abyssal crest in Crimson Templar / Azure Templar / Hoary Templar / Earthen Templar
-UPDATE `creature_loot_template` SET `Chance`='100' WHERE `Item`=20513 AND `Reference`=0 AND `GroupId`=0;
