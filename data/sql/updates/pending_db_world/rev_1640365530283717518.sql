1659332129806898b9b389a0030fb3040927d387
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-25 11:53:50 +0000

    chore(DB): import pending files
    Referenced commit(s): 5abfe27365959b24f6a078d106d0b906f905cd12

--- data/sql/updates/pending_db_world/rev_1640365530283717518.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640365530283717518');
-
-UPDATE `gameobject_template` SET `ScriptName` = "go_duskwither_spire_power_source" WHERE `entry` IN (180916, 180919, 180920);
