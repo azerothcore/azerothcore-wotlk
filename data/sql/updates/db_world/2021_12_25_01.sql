d4ddfed87e9a83e7ca44b2604976c118f3e0b33e
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-25 15:34:10 +0000

    chore(DB): import pending files
    Referenced commit(s): a3c58d915ec08c153968d251de3eb91f318141ab

--- data/sql/updates/db_world/2021_12_25_01.sql
+++ data/sql/updates/db_world/2021_12_25_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_25_00 -> 2021_12_25_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_25_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_25_00 2021_12_25_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1638655418843943400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1638655418843943400');
 
 /* LK Path of Frost Effect
@@ -35,3 +51,13 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 29110;
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29110) AND (`source_type` = 0) AND (`id` IN (0));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (29110, 0, 0, 0, 1, 0, 100, 0, 40000, 50000, 60000, 60000, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Lich King - Ebon Hold - Phase 3 Voice Lines');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_25_01' WHERE sql_rev = '1638655418843943400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
