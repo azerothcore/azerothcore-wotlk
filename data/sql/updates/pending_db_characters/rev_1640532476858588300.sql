03c218b31e64da22d8b7401c37ca226ac9791d6c
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-26 16:31:35 +0000

    chore(DB): import pending files
    Referenced commit(s): d4963f3b83c8a327904addb129d5fd5a15b30d25

--- data/sql/updates/pending_db_characters/rev_1640532476858588300.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1640532476858588300');
-
-DROP TABLE IF EXISTS `character_settings`;
-CREATE TABLE `character_settings` (
-  `guid` INT UNSIGNED NOT NULL,
-  `source` VARCHAR(40) NOT NULL,
-  `data` TEXT NULL,
-  PRIMARY KEY (`guid`, `source`)
-) ENGINE=MYISAM DEFAULT CHARSET=utf8mb4 COMMENT='Player Settings';
