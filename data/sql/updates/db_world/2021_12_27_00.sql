d5e2f74ef5157f8964bb2c1e7b1da37b31248351
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-27 05:08:32 +0000

    chore(DB): import pending files
    Referenced commit(s): 288e91d5efad16109cf35560ca897bb946ac8b55

--- data/sql/updates/db_world/2021_12_27_00.sql
+++ data/sql/updates/db_world/2021_12_27_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_26_00 -> 2021_12_27_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_26_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_26_00 2021_12_27_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1640038807964241732'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640038807964241732');
 
 -- Deatholme
@@ -422,3 +438,13 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (1631700,9,1,0,0,0,100,0,0,0,0,0,0,11,21157,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Deatholme Necromancer - Script - Cast ''Dark Channeling'''),
 (1631700,9,2,0,0,0,100,0,7000,10000,0,0,0,92,0,21157,0,0,0,0,1,0,0,0,0,0,0,0,0,'Deatholme Necromancer - Script - Stop casting ''Dark Channeling'''),
 (1631700,9,3,0,0,0,100,0,3000,3000,0,0,0,90,8,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Deatholme Necromancer - Script - Kneel');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_27_00' WHERE sql_rev = '1640038807964241732';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
