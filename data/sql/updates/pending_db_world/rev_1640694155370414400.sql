a47607e0638f990c73eac58c839dae76b7cb575b
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-28 13:48:18 +0000

    chore(DB): import pending files
    Referenced commit(s): 3412c3e2191d6a683502c82da8f725982afeff00

--- data/sql/updates/pending_db_world/rev_1640694155370414400.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640694155370414400');
-
--- High Marshal Whirlaxis unit flags: UNK_6 & IMMUNE_TO_PC
-UPDATE `acore_world`.`creature_template` SET `unit_flags`='320' WHERE  `entry` = 15204;
