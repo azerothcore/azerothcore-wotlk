992816bfd2d3197c27d649cfc021c494111fa11d
eronox <eronoxsmail@gmail.com>
2021-12-28 20:13:01 +0100

    comment text

--- data/sql/updates/pending_db_world/rev_1640718198383728700.sql
+++ data/sql/updates/pending_db_world/rev_1640718198383728700.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640718198383728700');
 
--- Remove 'Jingling Bell' from being looted by a creature.
+-- Remove 'Jingling Bell' should not drop from any creature
 DELETE FROM `creature_loot_template` WHERE `Item`=21308;
