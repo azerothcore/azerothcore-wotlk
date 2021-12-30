9c9230941baf58ca25f17490a7d70021bc34b6b2
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-30 08:19:01 -0300

    Update data/sql/updates/pending_db_world/rev_1640818723105671700.sql

--- data/sql/updates/pending_db_world/rev_1640818723105671700.sql
+++ data/sql/updates/pending_db_world/rev_1640818723105671700.sql
@@ -44,7 +44,7 @@ INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language
 (@TIRION,8,0,'This I promise... This I vow...',12,0,100,0,0,0,7430,0,'Lord Tirion Fordring');
 
 -- Grand Inquisitor Isillien
-UPDATE `creature_template` SET `faction`=16, `AIName`='SmartAI', `ScriptName`='' WHERE entry=@ISILLIEN;
+UPDATE `creature_template` SET `faction`=16, `AIName`='SmartAI', `ScriptName`='' WHERE `entry`=@ISILLIEN;
 
 DELETE FROM `smart_scripts` WHERE `entryOrGuid` = @ISILLIEN AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
