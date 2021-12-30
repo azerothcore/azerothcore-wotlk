eb1a69117f5d7c7f8d42dab35a20ef895fddb1de
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2021-12-30 07:39:16 -0300

    feat(Core/Conditions): Implement CONDITION_TYPE_CREATURE_SPAWN

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -577,7 +577,16 @@ void Creature::Update(uint32 diff)
                 time_t now = time(nullptr);
                 if (m_respawnTime <= now)
                 {
-                    bool allowed = !IsAIEnabled || AI()->CanRespawn();     // First check if there are any scripts that object to us respawning
+
+                    ConditionList conditions = sConditionMgr->GetConditionsForNotGroupedEntry(CONDITION_SOURCE_TYPE_CREATURE_SPAWN, GetEntry());
+
+                    if (!sConditionMgr->IsObjectMeetToConditions(this, conditions))
+                    {
+                        // Creature should not respawn, reset respawn timer. Conditions will be checked again the new time it tries to respawn.
+                        m_respawnTime = time(nullptr) + m_respawnDelay;
+                        break;
+                    }
+
                     if (!allowed)                                               // Will be rechecked on next Update call
                         break;
 
