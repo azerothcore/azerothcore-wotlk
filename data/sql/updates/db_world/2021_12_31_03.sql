8dba60b2aa5b6dddbbbc63d06bb50732c233cfec
AzerothCoreBot <azerothcorebot@gmail.com>
2021-12-31 05:31:51 +0000

    chore(DB): import pending files
    Referenced commit(s): a3b226540efb9b6eca3f3472bf01c5d18f05cefa

--- data/sql/updates/db_world/2021_12_31_03.sql
+++ data/sql/updates/db_world/2021_12_31_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2021_12_31_02 -> 2021_12_31_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2021_12_31_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2021_12_31_02 2021_12_31_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1640823255701543300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640823255701543300');
 
 -- "Patches", correcting SAI
@@ -26,3 +42,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 -- Update Doctor Sabnok final waypoint orientation to not look in wall
 UPDATE `waypoints` SET `orientation`='6.16' WHERE `entry`=30992 AND `pointid`=14;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2021_12_31_03' WHERE sql_rev = '1640823255701543300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
