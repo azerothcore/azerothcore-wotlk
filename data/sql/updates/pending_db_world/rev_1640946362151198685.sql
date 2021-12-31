547db606f01cea0d89105041f1dcce2c452370fc
baranzadeoglu <84640316+baranzadeoglu@users.noreply.github.com>
2021-12-31 16:40:43 +0100

    Update rev_1640946362151198685.sql

--- data/sql/updates/pending_db_world/rev_1640946362151198685.sql
+++ data/sql/updates/pending_db_world/rev_1640946362151198685.sql
@@ -1,4 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640946362151198685');
 
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 13036) AND (`Item` BETWEEN 18255 AND 18533);
+DELETE FROM `creature_loot_template` WHERE (`Item` IN (18255, 18266, 18297, 18365)) AND (`Entry` != 14354);
+DELETE FROM `creature_loot_template` WHERE (`Item` IN (18266, 18297, 18365)) AND (`Entry` = 14354);
 
