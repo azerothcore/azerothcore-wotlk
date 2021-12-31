b868b6798dfc7a852c4d21b69371da55323b2078
Cláudio Costa <54484196+claudiodfc@users.noreply.github.com>
2021-12-31 14:12:35 +0000

    Update rev_1640706977785890600.sql

--- data/sql/updates/pending_db_world/rev_1640706977785890600.sql
+++ data/sql/updates/pending_db_world/rev_1640706977785890600.sql
@@ -2,4 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640706977785890600');
 
 -- Setting probability to 25% for each of the texts. 5*25 = 100%
 SET @PROBABILITY = 25;
-UPDATE `npc_text` SET Probability0 = @PROBABILITY, Probability1 = @PROBABILITY, Probability2 = @PROBABILITY, Probability3 = @PROBABILITY, Probability4 = @PROBABILITY WHERE `ID` IN (778);
+UPDATE `npc_text` SET `Probability0` = @PROBABILITY, `Probability1` = @PROBABILITY, `Probability2` = @PROBABILITY, `Probability3` = @PROBABILITY, `Probability4` = @PROBABILITY WHERE `ID` IN (778);
