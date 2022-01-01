8e471afcfc6cd9e9be94d797ebc5ba5422fc3486
eronox <eronoxsmail@gmail.com>
2022-01-02 00:33:28 +0100

    fix(DB): Missing gameobjects causing world errors

---
+++ data/sql/updates/pending_db_world/rev_1641079540649663500.sql
@@ -0,0 +1,11 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641079540649663500');
+
+-- Midsummer Bonfire Spawn Trap 2 (181375)
+DELETE FROM `gameobject_template` WHERE `entry` = 181375;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`)
+VALUES (181375, 6, 0, 'Midsummer Bonfire Spawn Trap 2', '', '', '', 1, 0, 0, 0, 29114, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', -18019);
+
+-- Rookery Egg Spawner (175622)
+DELETE FROM `gameobject_template` WHERE `entry` = 175622;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`)
+VALUES (175622, 6, 477, 'Rookery Egg Spawner', '', '', '', 1, 0, 0, 0, 15745, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'SmartGameObjectAI', '', -18019);
