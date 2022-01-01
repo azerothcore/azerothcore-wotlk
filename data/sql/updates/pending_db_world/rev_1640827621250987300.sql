46fbb817953de351fc927832de761115acf40d9d
Noxies <eronoxsmail@gmail.com>
2022-01-02 00:49:57 +0100

    Update data/sql/updates/pending_db_world/rev_1640827621250987300.sql
    Co-authored-by: Skjalf <47818697+Nyeriah@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1640827621250987300.sql
+++ data/sql/updates/pending_db_world/rev_1640827621250987300.sql
@@ -1,3 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640827621250987300');
+
 -- Golorn Frostbeard Waypoints
 SET @NPC := 219;
 UPDATE `creature` SET `position_x`=-5633.440, `position_y`=-496.4770, `position_z`=396.8753, `orientation`=2.539195, `MovementType`=2 WHERE `guid`=@NPC;
