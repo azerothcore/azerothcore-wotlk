39ad876f3d4572c47559750b036bad01e5e7d4e0
Francesco Borzì <borzifrancesco@gmail.com>
2022-01-02 20:28:19 +0100

    Update PCQueue.h

--- src/common/Threading/PCQueue.h
+++ src/common/Threading/PCQueue.h
@@ -34,7 +34,7 @@ private:
     std::atomic<bool> _shutdown;
 
 public:
-    ProducerConsumerQueue<T>() : _shutdown(false) = default;
+    ProducerConsumerQueue<T>() : _shutdown(false) { }
 
     void Push(const T& value)
     {
