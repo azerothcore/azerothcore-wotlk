4a3816c5668dfc1e706f580588b91ad35674293a
eronox <eronoxsmail@gmail.com>
2022-01-02 21:34:43 +0100

    Update `gm on/off` to SEC_MODERATOR fallback

--- src/server/scripts/Commands/cs_gm.cpp
+++ src/server/scripts/Commands/cs_gm.cpp
@@ -50,8 +50,8 @@ public:
             { "ingame",  HandleGMListIngameCommand, SEC_PLAYER,         Console::Yes },
             { "list",    HandleGMListFullCommand,   SEC_ADMINISTRATOR,  Console::Yes },
             { "visible", HandleGMVisibleCommand,    SEC_GAMEMASTER,     Console::No },
-            { "on",      HandleGMOnCommand,         SEC_GAMEMASTER,     Console::No },
-            { "off",     HandleGMOffCommand,        SEC_GAMEMASTER,     Console::No }
+            { "on",      HandleGMOnCommand,         SEC_MODERATOR,      Console::No },
+            { "off",     HandleGMOffCommand,        SEC_MODERATOR,      Console::No }
         };
         static ChatCommandTable commandTable =
         {
