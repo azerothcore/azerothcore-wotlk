7d25a0fb68e6ac78ba455a23e7232394f5f01725
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-02 22:32:44 +0000

    chore(DB): import pending files
    Referenced commit(s): e71d902027cb588880b94325ef8fb370406cc864

--- data/sql/updates/pending_db_world/rev_1641155240423609800.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641155240423609800');
-
--- Adjust ".gm list" to security 3 as default
-UPDATE `command` SET `security`='3' WHERE  `name`='gm list';
