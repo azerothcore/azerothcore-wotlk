268065f5f9a4aadca63bb4db83ba3788dbfbe63c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-03 20:11:26 +0000

    chore(DB): import pending files
    Referenced commit(s): d810ddf0424bca2ff6402a9abf3309f332ec2c55

--- data/sql/updates/pending_db_world/rev_1640656702788330300.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640656702788330300');
-
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(16414, 'spell_item_wraith_scythe_drain_life');
