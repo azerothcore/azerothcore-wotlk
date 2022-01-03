7ae83964dd63eabbfc5f58edb078b26f5cd0dff7
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-03 16:58:18 +0000

    chore(DB): import pending files
    Referenced commit(s): 7c2086d067587cb9cc45ff2a6ec6793c6b70b1ed

--- data/sql/updates/pending_db_world/rev_1639255994521340200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639255994521340200');
-
-ALTER TABLE `creature_addon` CHANGE `visibilityDistanceType` `visibilityDistanceType` TINYINT UNSIGNED DEFAULT 0 NOT NULL;
-ALTER TABLE `creature_template_addon` CHANGE `visibilityDistanceType` `visibilityDistanceType` TINYINT UNSIGNED DEFAULT 0 NOT NULL;
