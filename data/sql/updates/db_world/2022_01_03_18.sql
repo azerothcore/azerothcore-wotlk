7b3e99f290cdc93f342e19c933c46264a1eb92e4
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-03 19:47:07 +0000

    chore(DB): import pending files
    Referenced commit(s): c1a734337248555fc1a59fba103dd739b1b42080

--- data/sql/updates/db_world/2022_01_03_18.sql
+++ data/sql/updates/db_world/2022_01_03_18.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_03_17 -> 2022_01_03_18
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_03_17';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_03_17 2022_01_03_18 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1640704117328778600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640704117328778600');
 
 -- The 'Serpent Statue' is supposed to be despawned but currently not supported by the core.
@@ -19,3 +35,13 @@ UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI' WHERE `entry` = 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 1 AND `entryorguid` = 177705);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (177705, 1, 0, 1, 60, 0, 100, 0, 3500, 3500, 0, 0, 0, 12, 12369, 4, 30000, 0, 0, 0, 8, 0, 0, 0, 0, 252.513, 2963.7, 1.64204, 0.90757, 'Naga Beam - On Update - Summon Creature \'Lord Kragaru\'');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_03_18' WHERE sql_rev = '1640704117328778600';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
--- data/sql/updates/pending_db_world/rev_1640815190779401500.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640815190779401500');
-
-UPDATE `quest_template_addon` SET `PrevQuestID` = 9578 WHERE `ID` = 9706;
