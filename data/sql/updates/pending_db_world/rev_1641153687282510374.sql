5c0b5ccab9a708c0232ff2252d383aa993c4bbb3
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-03 21:02:12 +0000

    chore(DB): import pending files
    Referenced commit(s): 1e53ada71fea06ac860faa69a1051902160f47af

--- data/sql/updates/pending_db_world/rev_1641153687282510374.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641153687282510374');
-
--- Removes Ez-Thro Dynamite II  from mob. Item is not a Mod Drop Item per UDB verification
-DELETE FROM `creature_loot_template` WHERE  `Entry`=12178 AND `Item`=18588 AND `Reference`=0 AND `GroupId`=0;
-DELETE FROM `creature_loot_template` WHERE  `Entry`=13136 AND `Item`=18588 AND `Reference`=0 AND `GroupId`=0;
