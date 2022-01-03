106f8a66539dd925df072e450996f32474f28728
eronox <eronoxsmail@gmail.com>
2022-01-03 14:24:45 +0100

    Lower wander_distance, prevent walk in walls

--- data/sql/updates/pending_db_world/rev_1641215954764868200.sql
+++ data/sql/updates/pending_db_world/rev_1641215954764868200.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641215954764868200');
 
 -- Fix error in worldserver (creature random movement without wanderer distance)
-UPDATE `creature` SET `wander_distance`='4' WHERE `guid`= 121049;
+UPDATE `creature` SET `wander_distance`='2' WHERE `guid`= 121049;
