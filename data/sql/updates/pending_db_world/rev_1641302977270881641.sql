6ed71ad3b2d5051c561d4a9200bf069264e0fa64
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-04 18:40:13 +0000

    chore(DB): import pending files
    Referenced commit(s): f92c072d7942f5d76520b8db7db59bba112d4f66

--- data/sql/updates/pending_db_world/rev_1641302977270881641.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641302977270881641');
-
--- Removes improper loot Broken Silithid Chitin from Silithus Rare Zora
-DELETE FROM `creature_loot_template` WHERE  `Entry`=14474 AND `Item`=20499 AND `Reference`=0 AND `GroupId`=0;
