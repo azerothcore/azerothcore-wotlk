3ee9ea0105f8dbc1dea6627927e0616e23715839
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-05 18:56:13 +0000

    chore(DB): import pending files
    Referenced commit(s): 6f98d32790dadb760c42583045ee541323acedbb

--- data/sql/updates/db_world/2022_01_05_02.sql
+++ data/sql/updates/db_world/2022_01_05_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_05_01 -> 2022_01_05_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_05_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_05_01 2022_01_05_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641388762527715027'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641388762527715027');
 
 UPDATE `creature_template` SET `ScriptName`='npc_cameron' WHERE `entry`=805;
@@ -191,3 +207,13 @@ INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `positio
 (@ENTRY, 2,-9155.719,-132.458,75.17039, 0, 0, 1, 0, 100, 0), -- 07:32:15
 (@ENTRY, 3,-9340.689,-89.09771,66.49249, 0, 0, 1, 0, 100, 0), -- 07:32:27
 (@ENTRY, 4,-9353.076,-86.58789,65.68958, 0, 0, 0, 0, 100, 0); -- 07:33:47
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_05_02' WHERE sql_rev = '1641388762527715027';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
