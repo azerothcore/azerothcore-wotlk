6e910def070c0f9bf824ab0deca157a85facff48
TheDdraig <theddraig@gmail.com>
2022-01-05 23:25:07 +0000

    Update rev_1641421027611299094.sql

--- data/sql/updates/pending_db_world/rev_1641421027611299094.sql
+++ data/sql/updates/pending_db_world/rev_1641421027611299094.sql
@@ -90,6 +90,11 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 SET @ENTRY := 17392;
 SET @SOURCETYPE := 0;
 
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,54,0,100,0,0,1,0,0,80,1739200,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"Stillpine Ancestor Tikti - On Just Summoned - Run Script");
+
 -- Totem of Yor
 SET @ENTRY := 17363;
 SET @SOURCETYPE := 0;
