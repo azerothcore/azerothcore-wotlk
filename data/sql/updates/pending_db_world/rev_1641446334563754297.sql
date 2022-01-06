8695a93fbbf1fb3f3155d392cd3f68b487dbf201
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-01-06 08:50:58 +0330

    .

---
+++ data/sql/updates/pending_db_world/rev_1641446334563754297.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641446334563754297');
+
+--
+UPDATE `smart_scripts` SET `event_param1`=2 WHERE `entryorguid`=23760 AND `source_type`=0 AND `id`=3 AND `link`=0;
