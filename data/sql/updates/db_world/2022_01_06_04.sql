7fb6b7e15f277935ddabd1654b70cd6c6fcd0dca
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-06 01:52:46 +0000

    chore(DB): import pending files
    Referenced commit(s): 89aa2a00c69e8f5864b530047987ebedb36123a0

--- data/sql/updates/db_world/2022_01_06_04.sql
+++ data/sql/updates/db_world/2022_01_06_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_06_03 -> 2022_01_06_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_06_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_06_03 2022_01_06_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641384460010602200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641384460010602200');
 
 -- Reputation level
@@ -20,3 +36,13 @@ UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 30 WHERE `creatur
 
 -- Araj the Summoner should reward 50 reputation <= Revered (6)
 UPDATE `creature_onkill_reputation` SET `MaxStanding1` = @REVERED, `RewOnKillRepValue1` = 50 WHERE `creature_id` IN (@ARAJ);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_06_04' WHERE sql_rev = '1641384460010602200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
--- data/sql/updates/pending_db_world/rev_1641340369241454365.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641340369241454365');
-
-#closes AC issue #9996
-DELETE FROM `creature` WHERE (`guid`=62817);
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(62817,17374,530,0,0,1,1,0,0,-3525.93,-12480.8,15.5967,4.46185,300,5,0,137,0,1,0,0,0,'',0);
