43ea4aa2df896d9c468617accc6addf8f5dfff3a
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-06 10:58:57 +0000

    chore(DB): import pending files
    Referenced commit(s): b2a4dda478bc75fd89b860b71de4f5e991722a7a

--- data/sql/updates/pending_db_world/rev_1640706977785890600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1640706977785890600');
-
--- Setting probability to 25% for each of the texts. 5*25 = 100%
-SET @PROBABILITY = 25;
-UPDATE `npc_text` SET `Probability0` = @PROBABILITY, `Probability1` = @PROBABILITY, `Probability2` = @PROBABILITY, `Probability3` = @PROBABILITY, `Probability4` = @PROBABILITY WHERE `ID` IN (778);
