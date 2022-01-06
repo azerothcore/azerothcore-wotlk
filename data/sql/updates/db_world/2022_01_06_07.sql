3bb09310f6dae28c1f0894eeb23bbc8e274e3320
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-06 13:35:27 +0000

    chore(DB): import pending files
    Referenced commit(s): 2d4e17fd16326b27f100977ac8a542ad56d57934

--- data/sql/updates/db_world/2022_01_06_07.sql
+++ data/sql/updates/db_world/2022_01_06_07.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_06_06 -> 2022_01_06_07
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_06_06';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_06_06 2022_01_06_07 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1637692848290406800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1637692848290406800');
 
 ALTER TABLE `creature_template` DROP `InhabitType`;
@@ -4421,3 +4437,13 @@ INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Fligh
 (26607, 2, 0, 1, 0, 0, 0, NULL),
 (4844, 0, 0, 0, 0, 0, 0, NULL),
 (25817, 2, 0, 1, 0, 0, 0, NULL);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_06_07' WHERE sql_rev = '1637692848290406800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
