d6cbc4a7d9f97ef43fe7611ae98cfd1891dffbd7
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-01-06 08:52:39 +0330

    .

--- data/sql/updates/pending_db_world/rev_1641446334563754297.sql
+++ data/sql/updates/pending_db_world/rev_1641446334563754297.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641446334563754297');
 
 --
-UPDATE `smart_scripts` SET `event_param1`=2 WHERE `entryorguid`=23760 AND `source_type`=0 AND `id`=3 AND `link`=0;
+UPDATE `smart_scripts` SET `event_type` = 1, `event_param1` = 6000, `event_param2` = 11000, `target_type` = 19, `target_param1` = 24042, `target_param2` = 40 WHERE `entryorguid` = 23760 AND `source_type` = 0 AND `id` = 3;
