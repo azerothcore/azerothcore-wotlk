f5a0cb36ef0a01f11a81fe0baeee9cefefe1ba9d
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-07 13:57:15 -0500

    Update rev_1641574598839293356.sql

--- data/sql/updates/pending_db_world/rev_1641574598839293356.sql
+++ data/sql/updates/pending_db_world/rev_1641574598839293356.sql
@@ -2,5 +2,4 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641574598839293356');
 
 -- remove ctm for creatures dalaran visitor (two fo them) by default they are just walkers but smart ai set them as runnings
 -- this corrects the issue of them flying when they land in dalaran
-DELETE FROM `acore_world`.`creature_template_movement` WHERE  `CreatureId`=32596;
-DELETE FROM `acore_world`.`creature_template_movement` WHERE  `CreatureId`=32597;
+DELETE FROM `creature_template_movement` WHERE  CreatureId IN(32596, 32597);
