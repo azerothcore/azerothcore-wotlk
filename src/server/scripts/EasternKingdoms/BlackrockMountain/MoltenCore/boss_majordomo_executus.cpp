0c3a57c2641bf82021947019a4bd99fe6452b9ec
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-01-08 16:16:37 -0300

    Revert "Update boss_majordomo_executus.cpp"
    This reverts commit 66f7c09b0ab51fd01acee6c889b8c1dc0c9a6363.

--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_majordomo_executus.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_majordomo_executus.cpp
@@ -190,10 +190,7 @@ public:
         {
             me->ResetLootMode();
             events.Reset();
-<<<<<<< Updated upstream
             aliveMinionsGUIDS.clear();
-=======
->>>>>>> Stashed changes
 
             if (instance->GetBossState(DATA_MAJORDOMO_EXECUTUS) != DONE)
             {
@@ -202,11 +199,7 @@ public:
 
                 for (auto summon : majordomoSummonsData)
                 {
-<<<<<<< Updated upstream
                     if (ObjectAccessor::GetCreature(*me, summon.guid))
-=======
-                    if (aliveMinionsGUIDS.find(summon.guid) != aliveMinionsGUIDS.end())
->>>>>>> Stashed changes
                     {
                         continue;
                     }
@@ -225,8 +218,6 @@ public:
                 majordomoSummonsData.clear();
                 summons.DespawnAll();
             }
-
-            aliveMinionsGUIDS.clear();
         }
 
         bool CanAIAttack(Unit const* /*target*/) const override
