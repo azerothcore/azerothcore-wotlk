9d6c879271d98793b1cee3490e9c48365566b4e7
UltraNix <ultranix@gmail.com>
2022-01-09 18:20:32 +0100

    fix(Core/Spells): Remove vanish immunity aura on stealth removal.
    Fixes #2668

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1783,7 +1783,10 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                     }
                     // Remove Vanish on stealth remove
                     if (GetId() == 1784)
+                    {
                         target->RemoveAurasWithFamily(SPELLFAMILY_ROGUE, 0x800, 0, 0, ObjectGuid::Empty);
+                        target->RemoveAurasDueToSpell(18461);
+                    }
                     break;
                 }
 
