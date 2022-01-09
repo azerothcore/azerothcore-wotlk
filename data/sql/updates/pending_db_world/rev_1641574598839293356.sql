2947c929f3afbb4b2f946e1390392acbecef8f8f
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-09 16:52:31 +0100

    fix (DB/NPC): Dalaran Visitor CTM fix (#10058)
    * fix (DB/NPC): Dalaran CTM fix
    
    Fixes the air swimmers when they land.
    
    * Update rev_1641574598839293356.sql
    
    * and replace of in
    
    * Update data/sql/updates/pending_db_world/rev_1641574598839293356.sql
    
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>
    
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1641574598839293356.sql
@@ -0,0 +1,5 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641574598839293356');
+
+-- remove ctm for creatures dalaran visitor (two fo them) by default they are just walkers but smart ai set them as runnings
+-- this corrects the issue of them flying when they land in dalaran
+DELETE FROM `creature_template_movement` WHERE  `CreatureID` IN (32596, 32597);
