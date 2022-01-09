a331698c5bcfeeaa8f77ccb783a746754be604f8
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-09 00:41:15 +0000

    chore(DB): import pending files
    Referenced commit(s): dbc35e34955b4ae3a1e9a96e2fab18929ec309f2

--- data/sql/updates/pending_db_world/rev_1641650285026899700.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641650285026899700');
-
-ALTER TABLE `creature_template`
-    ADD COLUMN `speed_swim` FLOAT NOT NULL DEFAULT '1' AFTER `speed_run`,
-    ADD COLUMN `speed_flight` FLOAT NOT NULL DEFAULT '1' AFTER `speed_swim`;
