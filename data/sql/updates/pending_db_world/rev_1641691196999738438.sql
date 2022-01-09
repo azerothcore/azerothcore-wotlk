3d807c9488fc201f57a635cdb02e9a39893d38ee
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-09 10:37:36 -0500

    Included iron forge npc as well

--- data/sql/updates/pending_db_world/rev_1641691196999738438.sql
+++ data/sql/updates/pending_db_world/rev_1641691196999738438.sql
@@ -1,5 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641691196999738438');
 
 -- relocks npc Great-father Winter's Helper back to winter veil.
-DELETE FROM `game_event_creature` WHERE `guid`=3566 AND `eventEntry`=2;
-INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES (2, 3566);
+DELETE FROM `game_event_creature` WHERE `guid` IN  (3566, 3565) AND `eventEntry`=2;
+INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES 
+(2, 3566),
+(2, 3565);
