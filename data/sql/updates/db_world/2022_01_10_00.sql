da7f4da51639e1bd3b4c6b76fda5ba78b822d203
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-10 17:04:47 +0000

    chore(DB): import pending files
    Referenced commit(s): e482cce164891eb9a83f454cdf3b6d2d89633a6e

--- data/sql/updates/db_world/2022_01_10_00.sql
+++ data/sql/updates/db_world/2022_01_10_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_09_01 -> 2022_01_10_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_09_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_09_01 2022_01_10_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641556717798866910'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641556717798866910');
 
 UPDATE `smart_scripts` SET `target_param2`=0 WHERE `entryorguid`=12939100 AND `source_type`=9 AND `id`=11 AND `link`=0;
@@ -32,3 +48,13 @@ UPDATE `smart_scripts` SET `action_param2`=1 WHERE `entryorguid`=2067 AND `sourc
 UPDATE `smart_scripts` SET `action_param2`=1 WHERE `entryorguid` IN(2068, 2067, 2066, 2065, 2064, 2063, 2062, 2061, 2060) AND `source_type`=0 AND `id`=0 AND `link`=1;
 UPDATE `smart_scripts` SET `action_param3`=1 WHERE `entryorguid`=253 AND `source_type`=0 AND `id`=0 AND `link`=0;
 UPDATE `smart_scripts` SET `action_param3`=1 WHERE `entryorguid`=15491 AND `source_type`=0 AND `id`=9 AND `link`=0;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_10_00' WHERE sql_rev = '1641556717798866910';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
