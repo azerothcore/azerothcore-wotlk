58b86814ea36a727d40e53a8be8aca20c470d643
Malcrom <malcromdev@gmail.com>
2022-01-10 14:17:14 -0400

    Update rev_1641837958335217980.sql

--- data/sql/updates/pending_db_world/rev_1641837958335217980.sql
+++ data/sql/updates/pending_db_world/rev_1641837958335217980.sql
@@ -4,4 +4,3 @@ ALTER TABLE `creature`
     CHANGE COLUMN `id` `creature_id1` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Creature Identifier' AFTER `guid`,
     ADD COLUMN `creature_id2` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Creature Identifier' AFTER `creature_id1`,
     ADD COLUMN `chance_id1` FLOAT UNSIGNED NOT NULL DEFAULT 100 COMMENT 'Chance id1 spawns' AFTER `creature_id2`;
-	
