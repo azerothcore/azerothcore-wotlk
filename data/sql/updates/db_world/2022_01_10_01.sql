cbf0c345f6d55dbac0f291a52f2e48dd1bb707ed
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-10 19:06:55 +0000

    chore(DB): import pending files
    Referenced commit(s): ae795fbcaa49eef18d11daf614c636eaf05b5a8f

--- data/sql/updates/db_world/2022_01_10_01.sql
+++ data/sql/updates/db_world/2022_01_10_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_10_00 -> 2022_01_10_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_10_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_10_00 2022_01_10_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641677125615651909'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641677125615651909');
 
 -- Pathing for Meggi Peppinrocker Entry: 11754
@@ -78,3 +94,13 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 -- Add missing random movement
 UPDATE `creature` SET `wander_distance`=1,`MovementType`=1 WHERE `guid` IN (42284);
 UPDATE `creature` SET `wander_distance`=2,`MovementType`=1 WHERE `guid` IN (42278,42280,42281,42285,42286,42287,42288,42290,42291);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_10_01' WHERE sql_rev = '1641677125615651909';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
