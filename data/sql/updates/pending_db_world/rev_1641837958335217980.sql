fa752e42db0769d3152f36da4d2cc063f24f80c5
Malcrom <malcromdev@gmail.com>
2022-01-10 14:06:50 -0400

    Add Sql

---
+++ data/sql/updates/pending_db_world/rev_1641837958335217980.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641837958335217980');
+
+ALTER TABLE `creature`
+    CHANGE COLUMN `id` `creature_id1` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Creature Identifier' AFTER `guid`;
+ALTER TABLE `creature` 
+    ADD COLUMN `creature_id2` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Creature Identifier' AFTER `creature_id1`;
+ALTER TABLE `creature` 
+    ADD COLUMN `chance_id1` FLOAT UNSIGNED NOT NULL DEFAULT 100 COMMENT 'Chance id1 spawns' AFTER `creature_id2`;
+	
