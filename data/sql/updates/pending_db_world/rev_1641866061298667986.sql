9065303aeabdb5a9a6537e1d6951f126ef349584
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-11 18:20:11 -0500

    Update rev_1641866061298667986.sql

--- data/sql/updates/pending_db_world/rev_1641866061298667986.sql
+++ data/sql/updates/pending_db_world/rev_1641866061298667986.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641866061298667986');
 
 -- Maraudon CTM
-DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (12223, 12207, 13323, 12216, 12217, 11790, 11791, 11792, 13141, 12236, 13142, 11345, 11685, 11785, 12222, 12221);
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (12223, 12207, 13323, 12216, 12217, 11790, 11791, 11792, 13141, 12236, 13142, 11345, 11685, 11785, 12222, 12221, 13282);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES 
 (12223, 1, 1, 0, 0, 0, 0, NULL),
 (12207, 1, 1, 0, 0, 0, 0, NULL),
@@ -18,4 +18,5 @@ INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Fligh
 (11685, 1, 1, 0, 0, 0, 0, NULL),
 (11785, 1, 1, 0, 0, 0, 0, NULL),
 (12222, 1, 1, 0, 0, 0, 0, NULL),
+(13282, 1, 1, 0, 0, 0, 0, NULL),
 (12221, 1, 1, 0, 0, 0, 0, NULL);
