0cd602d9ad4d7a54f8847710c11600ad3cab5eed
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-11 22:18:17 +0000

    chore(DB): import pending files
    Referenced commit(s): dd89bdfb2a95f67417434ee4a0d905ed3a72f5da

--- data/sql/updates/db_world/2022_01_11_05.sql
+++ data/sql/updates/db_world/2022_01_11_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_11_04 -> 2022_01_11_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_11_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_11_04 2022_01_11_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641924478819152116'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641924478819152116');
 
 -- Winterfall, Tmbermaw Post, & Frostfire Hot Springs, Winterspring
@@ -201,3 +217,13 @@ UPDATE `creature_template` SET `unit_flags`=0 WHERE entry IN (7440);
 UPDATE `creature_template` SET `MovementType`=1 WHERE entry IN (7441);
 DELETE FROM `creature_template_addon` WHERE entry IN (7438,7439,7440,7441,7442,10738);
 DELETE FROM `waypoint_data` WHERE `id` IN (410730,410790,410330);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_11_05' WHERE sql_rev = '1641924478819152116';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
