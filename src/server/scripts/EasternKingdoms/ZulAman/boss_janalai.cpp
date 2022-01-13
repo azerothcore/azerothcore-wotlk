4330e44ad0dc17caf96c3035d50a45027ac56935
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-13 14:39:54 -0500

    fix (Core/CodeReduction) TC_ removal (#10162)
    TC_ remove from any commit out TC_LOG in the event when we do need to run debug logging, we can just remove the // instead of // TC_

--- src/server/scripts/EasternKingdoms/ZulAman/boss_janalai.cpp
+++ src/server/scripts/EasternKingdoms/ZulAman/boss_janalai.cpp
@@ -237,7 +237,7 @@ public:
             Acore::CreatureListSearcher<Acore::AllCreaturesOfEntryInRange> searcher(me, templist, check);
             Cell::VisitGridObjects(me, searcher, me->GetGridActivationRange());
 
-            //TC_LOG_ERROR("scripts", "Eggs %d at middle", templist.size());
+            //LOG_ERROR("scripts", "Eggs %d at middle", templist.size());
             if (templist.empty())
                 return false;
 
@@ -514,7 +514,7 @@ public:
             Acore::CreatureListSearcher<Acore::AllCreaturesOfEntryInRange> searcher(me, templist, check);
             Cell::VisitGridObjects(me, searcher, me->GetGridActivationRange());
 
-            //TC_LOG_ERROR("scripts", "Eggs %d at %d", templist.size(), side);
+            //LOG_ERROR("scripts", "Eggs %d at %d", templist.size(), side);
 
             for (std::list<Creature*>::const_iterator i = templist.begin(); i != templist.end() && num > 0; ++i)
                 if ((*i)->GetDisplayId() != 11686)
