2781c9810f5db0136c51af3e279f91597c856e96
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-15 17:46:56 +0000

    chore(DB): import pending files
    Referenced commit(s): 83bde5b3320dff2af8963c621ac870b69f2db204

--- data/sql/updates/db_world/2022_01_15_02.sql
+++ data/sql/updates/db_world/2022_01_15_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_15_01 -> 2022_01_15_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_15_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_15_01 2022_01_15_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1642127857406476700'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642127857406476700');
 /* Delete the Old Pools */
 DELETE FROM `pool_template` WHERE  `entry`=957;
@@ -646,3 +662,13 @@ INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VA
 (10462, 432, 0, 'Teldrassil Earthroot'),
 (10471, 432, 0, 'Teldrassil Earthroot'),
 (10472, 432, 0, 'Teldrassil Earthroot');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_15_02' WHERE sql_rev = '1642127857406476700';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
