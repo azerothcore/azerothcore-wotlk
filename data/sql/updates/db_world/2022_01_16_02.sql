e1cb6b4b68b4122eec7a512664c0c30fe6c2bcce
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-16 14:36:11 +0000

    chore(DB): import pending files
    Referenced commit(s): 3cc3afbd830fff0a2e73869066f63907b9d5875a

--- data/sql/updates/db_world/2022_01_16_02.sql
+++ data/sql/updates/db_world/2022_01_16_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_16_01 -> 2022_01_16_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_16_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_16_01 2022_01_16_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641841935074090300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641841935074090300');
 
 -- Adding formations to Molten core Giants to prevent separate pulling
@@ -25,3 +41,13 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 (56714, 56715, 0, 0, 3),
 (56705, 56705, 0, 0, 3),
 (56705, 56704, 0, 0, 3);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_16_02' WHERE sql_rev = '1641841935074090300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
