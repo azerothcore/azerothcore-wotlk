b18c72bb5fc68f436675923b5de3ee933ec54842
Kargatum <dowlandtop@yandex.ru>
2022-01-17 10:23:01 +0700

    Update data/sql/updates/pending_db_world/rev_1641914952623130600.sql

--- data/sql/updates/pending_db_world/rev_1641914952623130600.sql
+++ data/sql/updates/pending_db_world/rev_1641914952623130600.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641914952623130600');
 
-UPDATE `creature_template_addon` SET auras = REPLACE(auras, ',', ' ');
+UPDATE `creature_template_addon` SET `auras` = REPLACE(auras, ',', ' ');
