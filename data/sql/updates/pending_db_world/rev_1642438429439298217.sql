72a99fada10a191bc69ebafdf5b692289639f3fc
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-17 20:41:18 +0000

    chore(DB): import pending files
    Referenced commit(s): 7862e3845f0fa1620d57930b99fc2e5a915fe671

--- data/sql/updates/pending_db_world/rev_1642438429439298217.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642438429439298217');
-
--- add mapID 44 as valid instance
-DELETE FROM `instance_template` WHERE `map`=44;
-INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES (44, 0, '', 0);
-
--- add teleport location OldScarletMonastery
-DELETE FROM `game_tele` WHERE `id`=1491;
-INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (1491, 79, -1, 18.6778, 0, 44, 'OldScarletMonastery');
