442d7c89225d1f0f310664a6d4a02b56750eb828
天鹿 <pkl_long@163.com>
2022-01-21 01:30:14 +0800

    Delete rev_1642434238867581705.sql

--- data/sql/updates/pending_db_world/rev_1642434238867581705.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642434238867581705');
-
-UPDATE `acore_string` SET `content_default`='%d - |c%s|Hitem:%d:0:0:0:0:0:0:0:0|h[%s]|h|r' WHERE `entry`=512;
