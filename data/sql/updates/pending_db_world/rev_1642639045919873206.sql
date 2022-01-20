8e082d4cb24492312d1d4714e101565779558833
nolt <nolt@wizzi.pl>
2022-01-20 20:39:41 +0000

    little modification

--- data/sql/updates/pending_db_world/rev_1642639045919873206.sql
+++ data/sql/updates/pending_db_world/rev_1642639045919873206.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642639045919873206');
 
 #closes #10227
-UPDATE creature SET `position_x`=-5026.9, `position_y`=-949.037, `position_z`=76.7268, `orientation`=3.77385 WHERE `guid`=21706;
-UPDATE creature SET `position_x`=-5196.11, `position_y`=-1217.24, `position_z`=121.521, `orientation`=1.45691, `MovementType`=0 WHERE `guid`=21708;
+UPDATE creature SET `position_x`=-5026.72, `position_y`=-949.15, `position_z`=61.9471, `orientation`=4.07623 WHERE `guid`=21706;
+UPDATE creature SET `position_x`=-5195.02, `position_y`=-1213.47, `position_z`=116.165, `orientation`=1.10743, `MovementType`=0 WHERE `guid`=21708;
