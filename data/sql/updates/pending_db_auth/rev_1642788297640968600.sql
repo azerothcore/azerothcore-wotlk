71f7023e1917ab7123118d4d65933dadcbe66d19
Winfidonarleyan <dowlandtop@yandex.com>
2022-01-22 01:07:40 +0700

    new files

---
+++ data/sql/updates/pending_db_auth/rev_1642788297640968600.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1642788297640968600');
+
+-- ACDB 335.6-dev auth
+UPDATE `updates` SET `state`='ARCHIVED';
---
+++ data/sql/updates/pending_db_characters/rev_1642788316128179200.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1642788316128179200');
+
+-- ACDB 335.6-dev characters
+UPDATE `updates` SET `state`='ARCHIVED';
