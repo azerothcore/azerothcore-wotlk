cff15e1df7c568512b4ff8f5f4bc178d9466fbf4
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-21 12:52:11 -0500

    Update rev_1642724246151292535.sql

--- data/sql/updates/pending_db_world/rev_1642724246151292535.sql
+++ data/sql/updates/pending_db_world/rev_1642724246151292535.sql
@@ -1,5 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642724246151292535');
 
 -- adds missing quest item objective marker for CC ticket 2838
-DELETE FROM `creature_questitem` WHERE `CreatureEntry`=17200 AND `Idx`=0;
-INSERT INTO `creature_questitem` (`CreatureEntry`, `Idx`, `ItemId`, `VerifiedBuild`) VALUES (17200, 0, 23676, 0);
+DELETE FROM `creature_questitem` WHERE `CreatureEntry` IN (17200, 17201);
+INSERT INTO `creature_questitem` (`CreatureEntry`, `Idx`, `ItemId`, `VerifiedBuild`) VALUES 
+(17200, 0, 23676, 0),
+(17201, 0, 23676, 0);
