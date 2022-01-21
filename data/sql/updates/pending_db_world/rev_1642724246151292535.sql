cd6d58ee4107269142eac15a9342f57306eaafce
acidmanifesto <joshua.lee.betts@gmail.com>
2022-01-21 13:46:34 -0500

    Update rev_1642724246151292535.sql

--- data/sql/updates/pending_db_world/rev_1642724246151292535.sql
+++ data/sql/updates/pending_db_world/rev_1642724246151292535.sql
@@ -4,4 +4,5 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642724246151292535');
 DELETE FROM `creature_questitem` WHERE `CreatureEntry` IN (17200, 17201);
 INSERT INTO `creature_questitem` (`CreatureEntry`, `Idx`, `ItemId`, `VerifiedBuild`) VALUES 
 (17200, 0, 23676, 0),
-(17201, 0, 23676, 0);
+(17201, 0, 23676, 0),
+(17201, 1, 23677, 0);
