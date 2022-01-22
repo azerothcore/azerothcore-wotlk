9d4bda0c26adf183e3496c54c58ad835b32faa5c
Bogir[rus] <m-1@bk.ru>
2022-01-22 09:56:16 +0500

    Update rev_1642821969869259834.sql

--- data/sql/updates/pending_db_characters/rev_1642821969869259834.sql
+++ data/sql/updates/pending_db_characters/rev_1642821969869259834.sql
@@ -1,4 +1,4 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642821969869259834');
+INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1642821969869259834');
 
 -- column `type` contains type of logged action
 ALTER TABLE `log_money` ADD COLUMN `type` char(255) NOT NULL AFTER `date`;
