2e09cb7138d28d15be4b8fca32c94fa2fcf39854
Nefertumm <nefertum.dev@protonmail.com>
2022-01-22 14:39:01 -0300

    codestyle + removeflag

--- src/server/scripts/EasternKingdoms/Stratholme/boss_jarien_and_sothos.cpp
+++ src/server/scripts/EasternKingdoms/Stratholme/boss_jarien_and_sothos.cpp
@@ -111,7 +111,7 @@ struct boss_jarien : public BossAI
             });
 
         me->SetReactState(REACT_PASSIVE);
-        me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_IMMUNE_TO_NPC);
+        me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_IMMUNE_TO_NPC);
         _talked = false;
         _phase = PHASE_TALK;
     }
@@ -145,8 +145,7 @@ struct boss_jarien : public BossAI
                 _talked = true;
                 _phase = PHASE_FIGHT;
                 me->SetReactState(REACT_AGGRESSIVE);
-                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_NPC);
+                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_IMMUNE_TO_NPC);
             });
     }
 
