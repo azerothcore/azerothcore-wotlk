85532ecce5484d946f593c9e4b767af62f3da22d
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-01-23 15:31:20 +0100

    feat(Guild/Commands): guild rename
    * cherry-pick commit (https://github.com/TrinityCore/TrinityCore/commit/678cade1163080263bdea9e349fbb67b7af15ea7#)

--- src/server/game/Guilds/Guild.h
+++ src/server/game/Guilds/Guild.h
@@ -686,6 +686,8 @@ public:
     std::string const& GetMOTD() const { return m_motd; }
     std::string const& GetInfo() const { return m_info; }
 
+    bool SetName(std::string const& name);
+
     // Handle client commands
     void HandleRoster(WorldSession* session);
     void HandleQuery(WorldSession* session);
