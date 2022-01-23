d97c95c3fae692acc0cad6a56ebcfa807d35d3b2
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-23 17:56:40 +0000

    chore(DB): import pending files
    Referenced commit(s): 46316bfdaa2f84edf3b0058122fb9be5e66019ef

--- data/sql/updates/pending_db_world/rev_1642322992276769800.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642322992276769800');
-
-UPDATE `gameobject` SET `position_z`=458.51 WHERE `guid`=230;
-UPDATE `gameobject` SET `position_z`=460.87 WHERE `guid`=231;
