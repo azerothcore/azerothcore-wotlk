7267f643350204180ff9de1cc691ccaaaa67126a
Winfidonarleyan <dowlandtop@yandex.com>
2022-01-23 15:13:30 +0700

    pendinig to normal

--- data/sql/updates/pending_db_characters/rev_1642788316128179200.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_characters` (`sql_rev`) VALUES ('1642788316128179200');
-
--- ACDB 335.6-dev characters
-UPDATE `updates` SET `state`='ARCHIVED';
--- data/sql/updates/pending_db_world/rev_1642788337196877700.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642788337196877700');
-
--- ACDB 335.5-dev world
-UPDATE `version` SET `db_version`='ACDB 335.6-dev', `cache_id`=6 LIMIT 1;
-UPDATE `updates` SET `state`='ARCHIVED';
