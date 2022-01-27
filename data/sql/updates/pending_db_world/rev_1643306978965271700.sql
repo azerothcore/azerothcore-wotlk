aa6aa8da81ed13837c32de94b122eb3caadb8f57
Nefertumm <nefertum.dev@protonmail.com>
2022-01-27 15:11:03 -0300

    Fix(DB/Gossip): Angela Dosantos

---
+++ data/sql/updates/pending_db_world/rev_1643306978965271700.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643306978965271700');
+
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID` = 12033 WHERE `MenuID` = 7130 AND `OptionID` = 0;
