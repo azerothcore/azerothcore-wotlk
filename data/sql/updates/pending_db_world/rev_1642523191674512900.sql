9726bf2407633b2ec8f3102a8f9aecc46ee0202f
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-27 21:44:42 +0000

    chore(DB): import pending files
    Referenced commit(s): 79d4efd1b4e3891a36f8cc7545d1f3f5a4703778

--- data/sql/updates/pending_db_world/rev_1642523191674512900.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642523191674512900');
-
-DELETE FROM `creature_formations` WHERE `memberGUID` IN (42921, 44397, 44396);
-INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
-(42921, 42921, 0, 0, 3),
-(42921, 44397, 0, 0, 3),
-(42921, 44396, 0, 0, 3);
