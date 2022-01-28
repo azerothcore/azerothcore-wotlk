8fb050b8b1a50680570b63e8aa930052671be012
claudiodfc <claudio.daniel.f.c@gmail.com>
2022-01-28 18:48:52 +0000

    database structure

---
+++ data/sql/updates/pending_db_world/rev_1643395587559675400.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643395587559675400');
+
+CREATE TABLE IF NOT EXISTS `instance_saved_data` (
+  `instance_id` int unsigned NOT NULL,
+  `gameobject_entry` int unsigned NOT NULL,
+  `gameobject_guid` int unsigned NOT NULL,
+  `gameobject_state` int unsigned DEFAULT '0',
+  PRIMARY KEY (`instance_id`,`gameobject_entry`,`gameobject_guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
