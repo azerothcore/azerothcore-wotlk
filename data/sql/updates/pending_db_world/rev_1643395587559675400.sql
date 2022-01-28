81b1845e597fa4080c31335f5cb27bd9536e31c2
claudiodfc <claudio.daniel.f.c@gmail.com>
2022-01-28 19:43:14 +0000

    table changes for columns

--- data/sql/updates/pending_db_world/rev_1643395587559675400.sql
+++ data/sql/updates/pending_db_world/rev_1643395587559675400.sql
@@ -2,9 +2,9 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643395587559675400');
 
 DROP TABLE IF EXISTS `instance_saved_data`;
 CREATE TABLE IF NOT EXISTS `instance_saved_data` (
-  `id` int unsigned NOT NULL,
-  `entry` int unsigned NOT NULL,
-  `guid` int unsigned NOT NULL,
-  `state` int unsigned DEFAULT '0',
+  `id` int unsigned NOT NULL COMMENT 'id = instance.id',
+  `entry` int unsigned NOT NULL COMMENT 'entry = gameobject.entry',
+  `guid` int unsigned NOT NULL COMMENT 'guid = gameobject.guid',
+  `state` int unsigned DEFAULT '0' COMMENT 'state = gameobject.state',
   PRIMARY KEY (`id`,`entry`,`guid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='id = instance.id\r\nentry = gameobject.entry\r\nguid = gameobject.guid\r\nstate = gameobject''s state';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
