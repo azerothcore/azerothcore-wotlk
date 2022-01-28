46cdd3e2fd3b4010eaa9f4c3ea3bfff27c0d9262
claudiodfc <claudio.daniel.f.c@gmail.com>
2022-01-28 19:25:29 +0000

    table changes

--- data/sql/updates/pending_db_world/rev_1643395587559675400.sql
+++ data/sql/updates/pending_db_world/rev_1643395587559675400.sql
@@ -1,9 +1,10 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643395587559675400');
 
+DROP TABLE IF EXISTS `instance_saved_data`;
 CREATE TABLE IF NOT EXISTS `instance_saved_data` (
   `instance_id` int unsigned NOT NULL,
   `gameobject_entry` int unsigned NOT NULL,
   `gameobject_guid` int unsigned NOT NULL,
   `gameobject_state` int unsigned DEFAULT '0',
   PRIMARY KEY (`instance_id`,`gameobject_entry`,`gameobject_guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
