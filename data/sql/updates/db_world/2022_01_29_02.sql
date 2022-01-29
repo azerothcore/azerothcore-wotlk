d3dedee7273e6a4266888f537cfbb27b5d909b01
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-29 19:10:09 +0000

    chore(DB): import pending files
    Referenced commit(s): 9036e3b98f569c673ed3a64e8cf4d04f0cdff3a0

--- data/sql/updates/db_world/2022_01_29_02.sql
+++ data/sql/updates/db_world/2022_01_29_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_01_29_01 -> 2022_01_29_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_01_29_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_01_29_01 2022_01_29_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1642779550112004300'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642779550112004300');
 
 DELETE FROM `gossip_menu` WHERE `MenuID` in (4821, 4824, 4825, 4827) AND `TextID` in (5874, 5880, 5882, 5886);
@@ -19,3 +35,13 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 (14, 4825, 5882, 0, 0, 15, 0, 128, 0, 0, 1, 0, 0, '', 'Portal Trainer - Show gossip menu if player is not a mage'),
 (14, 4827, 5885, 0, 0, 15, 0, 128, 0, 0, 1, 0, 0, '', 'Portal Trainer - Show gossip menu if player is not a mage'),
 (14, 4827, 5886, 0, 0, 15, 0, 128, 0, 0, 0, 0, 0, '', 'Portal Trainer - Show gossip menu if player is a mage');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_01_29_02' WHERE sql_rev = '1642779550112004300';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
