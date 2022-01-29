effbefb493dea2d36be199ce999202bdbf9dff7e
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-01-30 00:45:57 +0100

    Revert "just so CI also runs on C++20 just incase, revert later"
    This reverts commit 142275ad0cea68fa65929f7e3db29d89c49bf4ad.

--- src/cmake/showoptions.cmake
+++ src/cmake/showoptions.cmake
@@ -182,6 +182,12 @@ if(BUILD_SHARED_LIBS)
   WarnAboutSpacesInBuildPath()
 endif()
 
+if (USE_CPP_20)
+  message("")
+  message(" *** Enabled C++20 standart")
+  message(" *** Please note that this is an experimental feature!")
+endif()
+
 if (CONFIG_ABORT_INCORRECT_OPTIONS)
   message("")
   message(" WARNING !")
