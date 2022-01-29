e36f2b9d597fd8eb1ef926cb20cc4a05dd72f140
Claudiodfc <54484196+claudiodfc@users.noreply.github.com>
2022-01-29 21:14:19 +0000

    Update data/sql/updates/pending_db_world/rev_1643395587559675400.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1643395587559675400.sql
+++ data/sql/updates/pending_db_world/rev_1643395587559675400.sql
@@ -2,9 +2,9 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643395587559675400');
 
 DROP TABLE IF EXISTS `instance_saved_go_state_data`;
 CREATE TABLE IF NOT EXISTS `instance_saved_go_state_data` (
-  `id` int unsigned NOT NULL COMMENT 'id = instance.id',
-  `entry` int unsigned NOT NULL COMMENT 'entry = gameobject.entry',
-  `guid` int unsigned NOT NULL COMMENT 'guid = gameobject.guid',
-  `state` int unsigned DEFAULT '0' COMMENT 'state = gameobject.state',
+  `id` INT UNSIGNED NOT NULL COMMENT 'instance.id',
+  `entry` INT UNSIGNED NOT NULL COMMENT 'gameobject_template.entry',
+  `guid` INT UNSIGNED NOT NULL COMMENT 'gameobject.guid',
+  `state` INT UNSIGNED DEFAULT '0' COMMENT 'gameobject.state',
   PRIMARY KEY (`id`, `entry`, `guid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
