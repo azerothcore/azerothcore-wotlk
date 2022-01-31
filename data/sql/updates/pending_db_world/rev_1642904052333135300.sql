809e11d1a9bb284e0ceb08395645c107d62cb9d3
AzerothCoreBot <azerothcorebot@gmail.com>
2022-01-31 11:38:08 +0000

    chore(DB): import pending files
    Referenced commit(s): b80d2b727fdb6bbe2993a10aee598490f93d6760

--- data/sql/updates/pending_db_world/rev_1642904052333135300.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642904052333135300');
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 3281) AND (`Item` IN (4905));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(3281, 4905, 0, 100, 1, 1, 0, 1, 1, 'Sarkoth - Sarkoth\'s Mangled Claw');
