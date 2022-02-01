0b0df4dda81b222df24daaeead0da689017e7e30
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2022-02-01 14:56:42 -0700

    Update rev_1643745379505594100.sql
    Change SQL comments to use /* */ instead of #

--- data/sql/updates/pending_db_world/rev_1643745379505594100.sql
+++ data/sql/updates/pending_db_world/rev_1643745379505594100.sql
@@ -1,12 +1,12 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643745379505594100');
-DELETE FROM `creature` WHERE  `guid` = 247680; # Duplicate of 247673
-DELETE FROM `creature` WHERE  `guid` = 247684; # Duplicate of 247676
-DELETE FROM `creature` WHERE  `guid` = 247688; # Duplicate of 247678
-DELETE FROM `creature` WHERE  `guid` = 247687; # Duplicate of 247669
-DELETE FROM `creature` WHERE  `guid` = 247692; # Duplicate of 247668
-DELETE FROM `creature` WHERE  `guid` = 247689; # Duplicate of 247667
-DELETE FROM `creature` WHERE  `guid` = 247681; # Duplicate of 247670
-DELETE FROM `creature` WHERE  `guid` = 247922; # Duplicate of 247911
-DELETE FROM `creature` WHERE  `guid` = 247921; # Duplicate of 247912
-DELETE FROM `creature` WHERE  `guid` = 247916; # Duplicate of 247910
-DELETE FROM `creature` WHERE  `guid` = 247914; # Duplicate of 247918
+DELETE FROM `creature` WHERE  `guid` = 247680; /* Duplicate of 247673 */
+DELETE FROM `creature` WHERE  `guid` = 247684; /* Duplicate of 247676 */
+DELETE FROM `creature` WHERE  `guid` = 247688; /* Duplicate of 247678 */
+DELETE FROM `creature` WHERE  `guid` = 247687; /* Duplicate of 247669 */
+DELETE FROM `creature` WHERE  `guid` = 247692; /* Duplicate of 247668 */
+DELETE FROM `creature` WHERE  `guid` = 247689; /* Duplicate of 247667 */
+DELETE FROM `creature` WHERE  `guid` = 247681; /* Duplicate of 247670 */
+DELETE FROM `creature` WHERE  `guid` = 247922; /* Duplicate of 247911 */
+DELETE FROM `creature` WHERE  `guid` = 247921; /* Duplicate of 247912 */
+DELETE FROM `creature` WHERE  `guid` = 247916; /* Duplicate of 247910 */
+DELETE FROM `creature` WHERE  `guid` = 247914; /* Duplicate of 247918 */
