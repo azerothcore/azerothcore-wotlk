9e5dcb167118b80e33b78b49fecec2e222cdcac0
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-01 09:54:46 +0000

    chore(DB): import pending files
    Referenced commit(s): 642d243665b365be3a979f625b05f45698ab8255

--- data/sql/updates/pending_db_world/rev_1642953762715459200.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642953762715459200');
-
-UPDATE `smart_scripts` SET `id`=19 WHERE `entryorguid`=12818 AND `source_type`=0 AND `id`=18 AND `event_type`=11;
-UPDATE `smart_scripts` SET `link`=18 WHERE `entryorguid`=12818 AND `source_type`=0 AND `id`=17;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=12818 AND `source_type`=0 AND `id`=18;
-INSERT INTO `smart_scripts` VALUES
-(12818,0,18,0,61,0,100,0,0,0,0,0,0,26,6482,0,0,0,0,0,7,0,0,0,0,0,0,0,0,'Ruul Snowhoof - On Waypoint Finished - Quest Complete');
