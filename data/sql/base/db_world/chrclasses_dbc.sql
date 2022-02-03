e813fba5a12741fd6767ed241754b2086829ed3c
tgdbm <ccuster2008@gmail.com>
2022-02-03 12:05:11 -0500

    DB update

--- data/sql/base/db_world/chrclasses_dbc.sql
+++ data/sql/base/db_world/chrclasses_dbc.sql
@@ -12,7 +12,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Дамп структуры для таблица _acore_world.chrclasses_dbc
+-- Дамп структуры для таблица acore_world.chrclasses_dbc
 DROP TABLE IF EXISTS `chrclasses_dbc`;
 CREATE TABLE IF NOT EXISTS `chrclasses_dbc` (
   `ID` INT NOT NULL DEFAULT 0,
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `chrclasses_dbc` (
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- Дамп данных таблицы _acore_world.chrclasses_dbc: 0 rows
+-- Дамп данных таблицы acore_world.chrclasses_dbc: 0 rows
 DELETE FROM `chrclasses_dbc`;
 /*!40000 ALTER TABLE `chrclasses_dbc` DISABLE KEYS */;
 /*!40000 ALTER TABLE `chrclasses_dbc` ENABLE KEYS */;
