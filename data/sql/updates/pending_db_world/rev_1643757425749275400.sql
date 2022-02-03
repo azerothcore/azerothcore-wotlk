f60f016a7d04be2ade66eeb04e46317a29979c59
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2022-02-03 08:45:48 -0700

    Update rev_1643757425749275400.sql
    Remove comments to pass SQL code style check

--- data/sql/updates/pending_db_world/rev_1643757425749275400.sql
+++ data/sql/updates/pending_db_world/rev_1643757425749275400.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643757425749275400');
-UPDATE `quest_template_addon` SET `PrevQuestID` = 9280 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290); /* Training quests should require complete Replenishing the Healing Crystals */
-UPDATE `quest_template` SET `Flags` = 65664 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290); /* Quests should be auto-complete (Quest Complete text only) and have TBC flag set */
-UPDATE `quest_template` SET `AllowableRaces` = 1024 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290); /* Draenei training quests should only be available to Draenei */
+UPDATE `quest_template_addon` SET `PrevQuestID` = 9280 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290);
+UPDATE `quest_template` SET `Flags` = 65664 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290);
+UPDATE `quest_template` SET `AllowableRaces` = 1024 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290);
