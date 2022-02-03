f0297d6a9c7348b0cd69bebc155fb26005d6fa35
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-03 21:21:43 +0000

    chore(DB): import pending files
    Referenced commit(s): 6c17f872d64fa09ca68d04d60f30875537c8b23a

--- data/sql/updates/pending_db_world/rev_1643757425749275400.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643757425749275400');
-UPDATE `quest_template_addon` SET `PrevQuestID` = 9280 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290);
-UPDATE `quest_template` SET `Flags` = 65664, `AllowableRaces` = 1024 WHERE `id` IN (9288, 9290, 9287, 9291, 9421, 9290);
