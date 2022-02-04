ad64a260772ee3f1b41e6f3c0322d549b2316a2a
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-04 17:56:58 +0000

    chore(DB): import pending files
    Referenced commit(s): db1cc08854d7b27380cdc98ef301c1133c37d701

--- data/sql/updates/pending_db_world/rev_1643386602617683649.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643386602617683649');
--- Fixes Paladins Lawbringer's 8 set bonus
-DELETE FROM `spell_proc_event` WHERE `entry`=21747;
-INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(21747, 0, 10, 0, 0, 0, 20, 0, 20, 0, 50000);
