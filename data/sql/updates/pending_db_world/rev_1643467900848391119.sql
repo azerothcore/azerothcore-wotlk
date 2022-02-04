3123badac67522ed724b93ab235e06bc0e7b681c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-04 18:08:50 +0000

    chore(DB): import pending files
    Referenced commit(s): c2b67067cc58e3189edaf550a579210aed8fc6f5

--- data/sql/updates/pending_db_world/rev_1643467900848391119.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643467900848391119');
-
--- missed dialogue page text for npc Old Orok "How can I help you, $c?"
-DELETE FROM `gossip_menu` WHERE `MenuID` = 9856 AND `TextID` = 10887;
-INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
-(9856, 10887);
