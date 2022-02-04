7b041946e163224cb82ba85e734c9a757fa4209f
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-04 23:39:05 +0000

    chore(DB): import pending files
    Referenced commit(s): de13bf426e162ee10cbd5470cec74122d1d4afa0

--- data/sql/updates/pending_db_world/rev_1643662892603722900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643662892603722900');
-
-UPDATE `creature_template` SET `RegenHealth` = 0 WHERE `RegenHealth` >= 2;
-UPDATE `creature_onkill_reputation` SET `IsTeamAward1` = 0 WHERE `IsTeamAward1` >= 2;
-UPDATE `creature_onkill_reputation` SET `IsTeamAward2` = 0 WHERE `IsTeamAward2` >= 2;
