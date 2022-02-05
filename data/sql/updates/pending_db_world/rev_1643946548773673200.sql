69b2621de6dbd7336b5045b518af238b0c75fc8e
Nefertumm <nefertum.dev@protonmail.com>
2022-02-05 05:04:56 -0300

    Remove double Ashkandi drop

--- data/sql/updates/pending_db_world/rev_1643946548773673200.sql
+++ data/sql/updates/pending_db_world/rev_1643946548773673200.sql
@@ -22,3 +22,5 @@ UPDATE `creature_template` SET `unit_flags` = `unit_flags`|2|256|131072|33554432
 UPDATE `creature_template` SET `unit_flags` = `unit_flags`|131072, `flags_extra` = `flags_extra`|1|256|16384|4194304, `mechanic_immune_mask`=`mechanic_immune_mask`|1|2|4|8|16|128|256|512|1024|2048|4096|8192|16384|65536|131072|4194304|8388608|33554432|67108864|536870912 , `ScriptName` = 'npc_corrupted_totem' WHERE `entry` IN (14662, 14663, 14664, 14666);
 
 UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE `entry` = 14668;
+
+DELETE FROM `creature_loot_template` WHERE `entry` = 11583 AND `Item` = 19364;
