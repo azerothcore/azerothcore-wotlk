69e626b2e3b2bd71c2a6d927d3e1386851d97cf1
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-02-05 05:24:57 +0330

    Update src/server/game/Entities/Object/Position.h
    Co-authored-by: Kargatum <dowlandtop@yandex.com>

--- src/server/game/Entities/Object/Position.h
+++ src/server/game/Entities/Object/Position.h
@@ -30,11 +30,9 @@ struct Position
 
     Position(Position const& loc) { Relocate(loc); }
     /* requried as of C++ 11 */
-#if __cplusplus >= 201103L
     Position(Position&&) = default;
     Position& operator=(const Position&) = default;
     Position& operator=(Position&&) = default;
-#endif
 
     struct PositionXYStreamer
     {
