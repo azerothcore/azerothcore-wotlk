336967b324ae7c8b25f50fa391069604479a24fb
TheDdraig <62179779+TheDdraig@users.noreply.github.com>
2022-02-09 18:56:24 +0000

    Fix (Core/Scripts): Warrior T3 8P Bonus Fix (#10561)
    * Fix (Core/Scripts): Warrior T3 8P Bonus Fix
    
    * Update spell_warrior.cpp
    
    * Update spell_warrior.cpp

---
+++ data/sql/updates/pending_db_world/rev_1644160153962444055.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644160153962444055');
+DELETE FROM `spell_script_names` WHERE `spell_id`=28845;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(28845, 'spell_warr_t3_prot_8p_bonus');
