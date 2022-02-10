d5053811065b0975096f251e742a1df63111e0a6
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-02-10 13:40:07 +0100

    fix(Scripts/Spell): Don't use invalid spells for the Magic Rooster mount (#9797)
    * fix(Scripts/Spell): Don't use invalid spells for the Magic Rooster mount
    
    - cherry-pick commit (https://github.com/TrinityCore/TrinityCore/commit/246a8661fd6879666c6601dc6513954661bb55b9)
    
    Co-Authored-By: Wyrserth <43747507+Wyrserth@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1639953303883016900.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1639953303883016900');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName`="spell_gen_magic_rooster";
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=65917;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES
+(65917, 66122, "Magic Rooster");
