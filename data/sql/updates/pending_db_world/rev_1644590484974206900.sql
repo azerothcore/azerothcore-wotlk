c861675c3b174a19d5a51d9cd8b89fcd9e227197
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-11 19:40:10 +0000

    chore(DB): import pending files
    Referenced commit(s): 62e580da0132994b5cba373f3d9e04af5b670112

--- data/sql/updates/pending_db_world/rev_1644590484974206900.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644590484974206900');
-
-ALTER TABLE `spell_dbc` CHANGE COLUMN `Field227` `EffectBonusMultiplier_1` FLOAT DEFAULT 0 NOT NULL;
-ALTER TABLE `spell_dbc` CHANGE COLUMN `Field228` `EffectBonusMultiplier_2` FLOAT DEFAULT 0 NOT NULL;
-ALTER TABLE `spell_dbc` CHANGE COLUMN `Field229` `EffectBonusMultiplier_3` FLOAT DEFAULT 0 NOT NULL;
