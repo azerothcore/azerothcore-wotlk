b67beb308d215fe848918668f1bc9c221c3ab03b
acidmanifesto <joshua.lee.betts@gmail.com>
2022-02-11 10:04:27 -0500

    Update data/sql/updates/pending_db_world/rev_1642639045919873206.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1642639045919873206.sql
+++ data/sql/updates/pending_db_world/rev_1642639045919873206.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642639045919873206');
 
 #closes #10227
-UPDATE creature SET `position_x`=-5026.72, `position_y`=-949.15, `position_z`=61.9471, `orientation`=4.07623 WHERE `guid`=21706;
-UPDATE creature SET `position_x`=-5195.02, `position_y`=-1213.47, `position_z`=116.165, `orientation`=1.10743, `MovementType`=0 WHERE `guid`=21708;
+UPDATE `creature` SET `position_x`=-5026.72, `position_y`=-949.15, `position_z`=61.9471, `orientation`=4.07623 WHERE `guid`=21706;
+UPDATE `creature` SET `position_x`=-5195.02, `position_y`=-1213.47, `position_z`=116.165, `orientation`=1.10743, `MovementType`=0 WHERE `guid`=21708;
