cf5dae9d981515a5044fefbe60c64bb9cbcd5f72
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2022-02-11 08:29:52 -0700

    fix(DB/Creature): Grimbooze Thunderbrew Missing Gossip (#10501)

---
+++ data/sql/updates/pending_db_world/rev_1643834611045156900.sql
@@ -0,0 +1,17 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643834611045156900');
+
+SET @NPC_ENTRY := 239; /* Grimbooze Thunderbrew */
+SET @GOSSIP_MENU_ID := 61028;
+SET @NPC_TEXT_ID := 50034;
+
+UPDATE `creature_template` SET `gossip_menu_id` = @GOSSIP_MENU_ID WHERE `entry` = @NPC_ENTRY;
+UPDATE `creature_template` SET `npcflag` = `npcflag`|1|2 WHERE `entry` = @NPC_ENTRY;
+DELETE FROM `npc_text` WHERE `ID` = @NPC_TEXT_ID;
+INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `BroadcastTextID0`)
+VALUES (@NPC_TEXT_ID,
+        'Adventure from lands far and near $bMeeting with folks both odd and queer $bBut if of me a question you ask $bYou must first complete a simple task!',
+        'Adventure from lands far and near $bMeeting with folks both odd and queer $bBut if of me a question you ask $bYou must first complete a simple task!', 0);
+
+DELETE FROM `gossip_menu` WHERE `MenuID` = @GOSSIP_MENU_ID AND `TextID` = @NPC_TEXT_ID;
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+(@GOSSIP_MENU_ID, @NPC_TEXT_ID);
