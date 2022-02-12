5b26f05f5db9589e0b4d223be3155c696126179b
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-12 12:24:36 +0000

    chore(DB): import pending files
    Referenced commit(s): 61125a1ba1b7ee6add0994b7457910c8c0078c4c

--- data/sql/updates/pending_db_world/rev_1642354321277117900.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1642354321277117900');
-
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry` IN (19832,19873);
-INSERT INTO `conditions` VALUES
-(13,5,19832,0,0,31,0,3,12435,0,0,0,0,'','Possess targets Razorgore the Untamed'),
-(13,1,19873,0,0,31,0,5,177807,0,0,0,0,'','Destroy Egg targets Razorgore\'s Egg');
