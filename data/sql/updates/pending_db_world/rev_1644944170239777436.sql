f5977fc094d069a6ed4be059a062d0b05ba91d92
FrancescoBorzi <borzifrancesco@gmail.com>
2022-02-15 18:09:27 +0100

    fix: sql

--- data/sql/updates/pending_db_world/rev_1644944170239777436.sql
+++ data/sql/updates/pending_db_world/rev_1644944170239777436.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644944170239777436');
 
-UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `source_type` = 0 AND `action` = 11 AND `event` = 23 AND `entryorguid` = 17664;
-UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `source_type` = 0 AND `action` = 71 AND `event` = 1 AND `entryorguid` IN (
+UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `source_type` = 0 AND `action_type` = 11 AND `event_type` = 23 AND `entryorguid` = 17664;
+UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `source_type` = 0 AND `action_type` = 71 AND `event_type` = 1 AND `entryorguid` IN (
     37887,
     38039,
     38040,
