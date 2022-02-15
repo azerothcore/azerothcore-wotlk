adb8e09772fd516b14e3dadb539a7cbb1bd6d77f
郑佩茹 <zhengpeiru21@protonmail.com>
2022-02-15 14:04:04 -0700

    fix(DB/creature_queststarter): Fix The Hunter's Charm Questgivers

---
+++ data/sql/updates/pending_db_world/rev_1644959003876813300.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644959003876813300');
+
+UPDATE `creature_queststarter` SET `id`=3039 WHERE `id`=3038 AND `quest`=8151;
+UPDATE `creature_queststarter` SET `id`=3352 WHERE `id`=3406 AND `quest`=8151;
