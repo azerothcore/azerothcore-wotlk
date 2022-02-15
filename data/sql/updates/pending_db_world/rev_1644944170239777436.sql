c9cde1d142f967d7f41b1f1b2ff82d5917b5afd0
Francesco Borzì <borzifrancesco@gmail.com>
2022-02-15 21:00:50 +0100

    Update rev_1644944170239777436.sql

--- data/sql/updates/pending_db_world/rev_1644944170239777436.sql
+++ data/sql/updates/pending_db_world/rev_1644944170239777436.sql
@@ -1,13 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644944170239777436');
 
-UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `source_type` = 0 AND `action_type` = 11 AND `event_type` = 23 AND `entryorguid` = 17664;
-UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `source_type` = 0 AND `action_type` = 71 AND `event_type` = 1 AND `entryorguid` IN (
-    37887,
-    38039,
-    38040,
-    38041,
-    38042,
-    38043,
-    38044,
-    38045
-);
+UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `entryorguid` IN (37887, 38039, 38041, 38042, 38043, 38044, 38045) AND `source_type` = 0 AND `id` = 1;
+UPDATE `smart_scripts` SET `event_param1` = 0 WHERE `entryorguid` = 17664 AND `source_type` = 0 AND `id` = 23;
