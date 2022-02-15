27c0eb9aee32dbc5a74724bf14db2d76b1833fc7
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-15 17:04:33 +0000

    chore(DB): import pending files
    Referenced commit(s): 38a33e7ef00ac93c6c4e8ee80db83c8685e1334c

--- data/sql/updates/db_world/2022_02_15_04.sql
+++ data/sql/updates/db_world/2022_02_15_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_02_15_03 -> 2022_02_15_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_02_15_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_02_15_03 2022_02_15_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1641967537263276315'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1641967537263276315');
 
 -- Fix DB Errors
@@ -1046,3 +1062,13 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=10948 AND `source_type`=0 AND `i
 DELETE FROM `smart_scripts` WHERE `entryorguid`=10937 AND `source_type`=0 AND `id` = 4;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=4507 AND `source_type`=0 AND `id` IN(2, 5);
 DELETE FROM `smart_scripts` WHERE `entryorguid`=-77757 AND `source_type`=0 AND `id` = 1;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_02_15_04' WHERE sql_rev = '1641967537263276315';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
