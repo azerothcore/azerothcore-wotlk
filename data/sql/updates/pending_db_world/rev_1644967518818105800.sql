994ee4ab0ff09bac292a156669bee8ac8c45fa76
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-16 11:56:00 +0000

    chore(DB): import pending files
    Referenced commit(s): abccbcb4be5baf0d94881deb33a03718aabec5ed

--- data/sql/updates/pending_db_world/rev_1644967518818105800.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644967518818105800');
-
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceGroup` = 0 AND `SourceEntry` = 23642;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(17, 0, 23642, 0, 0, 31, 1, 3, 13020, 0, 0, 0, 0, '', 'Nefarius Corruption only affects Vaelastrasz');
