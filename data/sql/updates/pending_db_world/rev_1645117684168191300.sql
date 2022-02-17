13200136143d08e5539160ff0da9f342e299dea7
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-17 22:38:46 +0000

    chore(DB): import pending files
    Referenced commit(s): 444e7d8072bccceea58033bb65c3a83034afffd4

--- data/sql/updates/pending_db_world/rev_1645117684168191300.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645117684168191300');
-
-UPDATE `npc_text` SET `text0_0` = "", `text0_1` = (SELECT `FemaleText` FROM `broadcast_text` WHERE `ID` = 11406), `BroadcastTextID0` = 11406 WHERE `ID` = 8121;
-UPDATE `npc_text` SET `text0_0` = "", `text0_1` = (SELECT `FemaleText` FROM `broadcast_text` WHERE `ID` = 11410), `BroadcastTextID0` = 11410 WHERE `ID` = 8122;
