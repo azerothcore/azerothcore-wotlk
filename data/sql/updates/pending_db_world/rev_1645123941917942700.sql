b882c195bc4ad8934292dc8cccc1c5d758e6bf52
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-17 22:23:39 +0000

    chore(DB): import pending files
    Referenced commit(s): 5143872aed7e93ee2ff8658c7c8b6efd7702429c

--- data/sql/updates/pending_db_world/rev_1645123941917942700.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645123941917942700');
-
-DELETE FROM `acore_string` WHERE `entry` BETWEEN 600 AND 704;
-DELETE FROM `acore_string` WHERE `entry` BETWEEN 724 AND 726;
-DELETE FROM `acore_string` WHERE `entry` BETWEEN 753 AND 755;
-DELETE FROM `acore_string` WHERE `entry` BETWEEN 1205 AND 1299;
-DELETE FROM `acore_string` WHERE `entry` BETWEEN 1326 AND 1330;
-DELETE FROM `acore_string` WHERE `entry` = 1333;
