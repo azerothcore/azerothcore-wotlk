49ee0c055c9229aef95a63e93fc0b998e7c98d50
郑佩茹 <zhengpeiru21@protonmail.com>
2022-02-17 12:10:09 -0700

    fix(DB/npc_text): Fix Kraz Gossip Text

--- data/sql/updates/pending_db_world/rev_1645119124535032800.sql
+++ data/sql/updates/pending_db_world/rev_1645119124535032800.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645119124535032800');
 
-UPDATE `npc_text` SET  `text0_0` = (SELECT `MaleText` FROM `broadcast_text` WHERE `ID` = 22630), `BroadcastTextID0` = 22630 WHERE `ID` = 11596;
+UPDATE `gossip_menu` SET `TextID` = 11602 WHERE `MenuID` = 8881 AND `TextID` = 11596;
