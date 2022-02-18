a49ca466bf44f8e256117fe01c29eb8391492e19
UltraNix <80540499+UltraNix@users.noreply.github.com>
2022-02-18 02:07:52 +0100

    fix(DB/Loot): Corrected skinning loot for Scorpid Workers. (#10667)
    Fixes #10447

---
+++ data/sql/updates/pending_db_world/rev_1644769898514768700.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644769898514768700');
+
+UPDATE `creature_template` SET `skinloot`=3124 WHERE `entry`=3124;
+DELETE FROM `skinning_loot_template` WHERE `entry`=3124;
+INSERT INTO `skinning_loot_template` VALUES
+(3124,2318,0,10,0,1,1,1,1,'Light Leather'),
+(3124,2934,0,88,0,1,1,1,1,'Ruined Leather Scraps');
