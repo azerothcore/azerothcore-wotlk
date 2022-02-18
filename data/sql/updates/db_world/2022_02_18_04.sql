d0761b3abbee01c101e5b185d8670c01f24874fb
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-18 22:24:04 +0000

    chore(DB): import pending files
    Referenced commit(s): b46b60cc10ddd289b20d607e8dd7d21541bb8fbd

--- data/sql/updates/db_world/2022_02_18_04.sql
+++ data/sql/updates/db_world/2022_02_18_04.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_02_18_03 -> 2022_02_18_04
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_02_18_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_02_18_03 2022_02_18_04 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1645220638173840730'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645220638173840730');
 
 -- Telaar Fixup #1
@@ -255,3 +271,13 @@ INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language
 (18408,1,0,'That should get Telaar the assistance it needs!',12,7,100,5,0,0,15374,0,'Warden Moi''bff Jill');
 
 UPDATE `creature` SET `id1`=18488, `id2`=18549 WHERE `guid` IN (68474,68475,68476,68477,68478);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_02_18_04' WHERE sql_rev = '1645220638173840730';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
