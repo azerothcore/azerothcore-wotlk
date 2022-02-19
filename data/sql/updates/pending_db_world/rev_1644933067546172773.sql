9ca50c7b0fd4ff301d5e1c93156fc99d787e6891
acidmanifesto <joshua.lee.betts@gmail.com>
2022-02-19 12:56:57 -0500

    Update rev_1644933067546172773.sql

--- data/sql/updates/pending_db_world/rev_1644933067546172773.sql
+++ data/sql/updates/pending_db_world/rev_1644933067546172773.sql
@@ -1,7 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644933067546172773');
 
--- removes items to align with udb
--- https://tbc.wowhead.com/item=22389/plans-sageblade
--- https://tbc.wowhead.com/item=22390/plans-persuader
-DELETE FROM `creature_loot_template` WHERE  `Entry`=12397 AND `Item`=22389 AND `Reference`=0 AND `GroupId`=0;
-DELETE FROM `creature_loot_template` WHERE  `Entry`=12397 AND `Item`=22390 AND `Reference`=0 AND `GroupId`=0;
+-- This NPC does not exsist in wotlk since when the dark portal open for TBC he went back to outland and is now know as Doom Lord Kazzak NPC 12397.
+DELETE FROM `creature_loot_template` WHERE  `Entry`=12397;
