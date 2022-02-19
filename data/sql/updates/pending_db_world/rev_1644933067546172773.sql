52787c2a75c2057d245ca851a18ae0a791380949
acidmanifesto <joshua.lee.betts@gmail.com>
2022-02-19 13:57:16 -0500

    Update rev_1644933067546172773.sql

--- data/sql/updates/pending_db_world/rev_1644933067546172773.sql
+++ data/sql/updates/pending_db_world/rev_1644933067546172773.sql
@@ -1,4 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644933067546172773');
 
 -- This NPC does not exsist in wotlk since when the dark portal open for TBC he went back to outland and is now know as Doom Lord Kazzak NPC 12397.
+UPDATE `creature_template` SET `lootid`='0' WHERE  `entry`=12397;
 DELETE FROM `creature_loot_template` WHERE  `Entry`=12397;
