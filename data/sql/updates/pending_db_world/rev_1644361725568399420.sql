0dff15c10c9a9f88462458e7e6989cbb22228254
acidmanifesto <joshua.lee.betts@gmail.com>
2022-02-19 13:35:33 -0500

    Update rev_1644361725568399420.sql
    on aggro, npc casts thrash on itself

--- data/sql/updates/pending_db_world/rev_1644361725568399420.sql
+++ data/sql/updates/pending_db_world/rev_1644361725568399420.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644361725568399420');
 
 -- corrects smart script for thrash for npc entry 1783
-DELETE FROM `smart_scripts` WHERE `entryorguid`=1783 AND `source_type`=0 AND `id`=0 AND `link`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=1783;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
-(1783, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 11, 8876, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Skeletal Flayer - On Reset - Cast \'Thrash\'');
+(1783, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 11, 8876, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Skeletal Flayer - On Aggro - Cast \'Thrash\'');
