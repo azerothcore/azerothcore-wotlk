c9586a106b4b0567f0154dd85a7385282adba42a
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-19 12:50:24 +0000

    chore(DB): import pending files
    Referenced commit(s): c6f0f7dd4e6fc0a0b9cefb4710914e69b0eca907

--- data/sql/updates/pending_db_world/rev_1644524834850478500.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644524834850478500');
-
-DELETE FROM `gossip_menu` WHERE `MenuID`=2062 AND `TextID`=3496;
-INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES (2062, 3496);
