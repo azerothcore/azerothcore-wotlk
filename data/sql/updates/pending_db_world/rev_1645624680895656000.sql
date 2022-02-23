24c95b35c51eb3fa1880f2c9b22a1e0546815a50
avarishd <46330494+avarishd@users.noreply.github.com>
2022-02-23 18:17:46 +0200

    Update rev_1645624680895656000.sql

--- data/sql/updates/pending_db_world/rev_1645624680895656000.sql
+++ data/sql/updates/pending_db_world/rev_1645624680895656000.sql
@@ -1,6 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645624680895656000');
 
--- Add gossip flag to Expedition Commander, previously was 0
-UPDATE creature_template SET npcflag = 1 WHERE entry IN (33210,34254);
--- Assign the proper? menuID
+-- Add gossip flag to Expedition Commander, previously 0
+UPDATE `creature_template` SET `npcflag` = `npcflag` | 1 WHERE `entry` IN (33210,34254);
+-- Assign the correct menuID to Expedition Commander, previously 0
 UPDATE `creature_template` SET `gossip_menu_id` = 10314 WHERE `entry` IN (33210,34254);
+
