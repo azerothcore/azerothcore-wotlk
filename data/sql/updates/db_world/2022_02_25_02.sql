b64a6f3661dd248d893931b070caa6cca7621519
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-25 16:53:56 +0000

    chore(DB): import pending files
    Referenced commit(s): 35820d78fa8f0b05611380e4375a4ae028543c21

--- data/sql/updates/db_world/2022_02_25_02.sql
+++ data/sql/updates/db_world/2022_02_25_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_02_25_01 -> 2022_02_25_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_02_25_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_02_25_01 2022_02_25_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1644948930045414200'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644948930045414200');
 
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=15280;
@@ -7,3 +23,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (15280, 0, 0, 0, 1, 0, 100, 0, 5000, 8000, 5000, 8000, 0, 80, 1528000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, "Jesthenis Sunstrider - OOC - Run Script"),
 (1528000, 9, 0, 0, 1, 0, 100, 0, 0, 0, 0, 0, 0, 10, 1, 6, 273, 274, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, "Jesthenis Sunstrider - On Script - Play Random Emote"),
 (1528000, 9, 1, 0, 1, 0, 100, 0, 0, 2000, 0, 0, 0, 10, 1, 6, 273, 274, 0, 0, 19, 15284, 20, 0, 0, 0, 0, 0, 0, "Jesthenis Sunstrider - On Script - Play Random Emote (Matron Arena)");
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_02_25_02' WHERE sql_rev = '1644948930045414200';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
