80e5ce4ec69f2caf0891e456e6d43c837a89a3d0
AzerothCoreBot <azerothcorebot@gmail.com>
2022-02-26 22:21:04 +0000

    chore(DB): import pending files
    Referenced commit(s): ca333d55fb5e576e02015914feb23ab11e388760

--- data/sql/updates/db_world/2022_02_26_00.sql
+++ data/sql/updates/db_world/2022_02_26_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_02_25_03 -> 2022_02_26_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_02_25_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_02_25_03 2022_02_26_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1645823072369793653'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645823072369793653');
 
 -- Nagrand Dust Howlers, Storm Ragers, and Living Cyclones pathing and random movement fixup.
@@ -1032,3 +1048,13 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 
 UPDATE `creature` SET `curhealth`=1,`curmana`=0 WHERE `id1` IN (17158,17159,17160);
 UPDATE `creature` SET `wander_distance`=20,`MovementType`=1 WHERE `id1` IN (17158,17159,17160) AND `MovementType`<>2;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_02_26_00' WHERE sql_rev = '1645823072369793653';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
