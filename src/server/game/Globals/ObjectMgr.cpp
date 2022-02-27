6a17d75f310bb3a0686bb7256367a543272d47ff
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-02-27 11:41:48 +0100

    Update ObjectMgr.cpp

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -6170,7 +6170,6 @@ void ObjectMgr::LoadQuestGreetings()
                 LOG_ERROR("sql.sql", "Table `quest_greeting`: creature template entry {} does not exist.", id);
                 continue;
             }
-            type = TYPEID_UNIT;
             break;
         case 1: // GameObject
             if (!sObjectMgr->GetGameObjectTemplate(id))
@@ -6178,7 +6177,6 @@ void ObjectMgr::LoadQuestGreetings()
                 LOG_ERROR("sql.sql", "Table `quest_greeting`: gameobject template entry {} does not exist.", id);
                 continue;
             }
-            type = TYPEID_GAMEOBJECT;
             break;
         default:
             continue;
@@ -6226,7 +6224,6 @@ void ObjectMgr::LoadQuestGreetingsLocales()
                 LOG_ERROR("sql.sql", "Table `quest_greeting_locale`: creature template entry {} does not exist.", id);
                 continue;
             }
-            type = TYPEID_UNIT;
             break;
         case 1: // GameObject
             if (!sObjectMgr->GetGameObjectTemplate(id))
@@ -6234,7 +6231,6 @@ void ObjectMgr::LoadQuestGreetingsLocales()
                 LOG_ERROR("sql.sql", "Table `quest_greeting_locale`: gameobject template entry {} does not exist.", id);
                 continue;
             }
-            type = TYPEID_GAMEOBJECT;
             break;
         default:
             break;
