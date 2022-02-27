daa15ec0fcc1d3710692ddc6a5e6ef5c1b64b55d
UltraNix <ultranix@gmail.com>
2022-02-27 18:53:03 +0100

    fix(Scripts/Blackwing Lair): Vaelastrasz improvement:
    Vaelastrasz should face the Nefarian's throne.
    Vaelastrasz should stand up after the gossip option.
    Fixed #10771

---
+++ data/sql/updates/pending_db_world/rev_1645984331813574600.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645984331813574600');
+
+UPDATE `creature` SET `position_x`=-7483.79, `position_y`=-1015.99, `position_z`=408.652, `orientation`=4.69494 WHERE `id1`=13020;
