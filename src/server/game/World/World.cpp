f0b82997ef0e2a917095c0c68f7d2a7eb4e820cc
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-02-28 20:46:39 +0100

    Update src/server/game/World/World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1734,7 +1734,7 @@ void World::SetInitialWorldSettings()
 
     LOG_INFO("server.loading", "Loading Quest Greetings...");
     sObjectMgr->LoadQuestGreetings();                               // must be loaded after creature_template, gameobject_template tables
-    LOG_INFO("server.loading", "Loading Quest Greeting Locales...")
+    LOG_INFO("server.loading", "Loading Quest Greeting Locales...");
     sObjectMgr->LoadQuestGreetingsLocales();                        // must be loaded after creature_template, gameobject_template tables
 
     LOG_INFO("server.loading", "Loading Quest Money Rewards...");
