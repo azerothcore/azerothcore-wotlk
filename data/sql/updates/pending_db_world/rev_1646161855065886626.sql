a60c225e89af7fd4c148a40ebf28daa3685bd959
Nefertumm <nefertum.dev@protonmail.com>
2022-03-01 17:03:24 -0300

    Update data/sql/updates/pending_db_world/rev_1646161855065886626.sql

--- data/sql/updates/pending_db_world/rev_1646161855065886626.sql
+++ data/sql/updates/pending_db_world/rev_1646161855065886626.sql
@@ -435,7 +435,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (21040, 0, 0, 0, 2, 0, 100, 1, 20, 80, 0, 0, 0, 11, 3391, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Outraged Raven\'s Wood Sapling - Between 20-80% Health - Cast \'3391\' (No Repeat)');
 
 -- Fel Rager
-DELETE FROM `smart_scripts` WHERE;
+DELETE FROM `smart_scripts` WHERE  `entryorguid` = 22286;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (22286, 0, 0, 0, 2, 0, 100, 1, 20, 80, 0, 0, 0, 11, 3391, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Fel Rager - Between 20-80% Health - Cast \'3391\' (No Repeat)');
 
