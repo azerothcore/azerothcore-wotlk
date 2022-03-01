8d518d36492d23407791ea91ee60adc1e3f0560d
MDIC <joshua.lee.betts@gmail.com>
2022-03-01 14:18:34 -0500

    Update rev_1646161855065886626.sql
    Credits to @temperrr for the assist with finding me the old table to help with the undoing of things
    
    Co-Authored-By: temperrr <temperrr@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1646161855065886626.sql
+++ data/sql/updates/pending_db_world/rev_1646161855065886626.sql
@@ -1,5 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646161855065886626');
-
 -- Riverpaw Outrunner
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 478);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
