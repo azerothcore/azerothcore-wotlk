66b95f8d9d573a52cd8caec90a28310c30d95183
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-01 12:54:25 +0000

    chore(DB): import pending files
    Referenced commit(s): b296042334aa8834867adf4ba6399805e1fc71c7

--- data/sql/updates/db_world/2022_03_01_01.sql
+++ data/sql/updates/db_world/2022_03_01_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_01_00 -> 2022_03_01_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_01_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_01_00 2022_03_01_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1644954283548941000'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644954283548941000');
 
 DELETE FROM `spell_dbc` WHERE `ID` = 33271;
@@ -7,3 +23,13 @@ INSERT INTO `spell_dbc` (`ID`, `Category`, `DispelType`, `Mechanic`, `Attributes
 
 /* Update attribute */
 UPDATE `spell_dbc` SET `Attributes` = `Attributes` | 16777216 WHERE `ID` = 33271;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_01_01' WHERE sql_rev = '1644954283548941000';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
