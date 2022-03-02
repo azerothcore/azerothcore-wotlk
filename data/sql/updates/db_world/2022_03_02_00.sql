972c91702e1b9202417dc34393e6ce295483305a
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-02 08:55:04 +0000

    chore(DB): import pending files
    Referenced commit(s): 3a1e510eb08531402168914990690467da1a5b94

--- data/sql/updates/db_world/2022_03_02_00.sql
+++ data/sql/updates/db_world/2022_03_02_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_01_09 -> 2022_03_02_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_01_09';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_01_09 2022_03_02_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1645306739640504900'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645306739640504900');
 
 /* Blood Feeders */
@@ -99,3 +115,13 @@ INSERT INTO `quest_details` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `Emot
 
 /* The Lost Pages */
 UPDATE `quest_details` SET `Emote1`=1, `Emote2`=1 WHERE `ID`=6504;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_02_00' WHERE sql_rev = '1645306739640504900';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
