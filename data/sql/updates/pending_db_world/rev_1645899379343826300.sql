90cb071e1099aa7c1bc91ebf376e2339de707eac
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-03 10:31:57 +0000

    chore(DB): import pending files
    Referenced commit(s): 9b765aa2d6130cc434eae99d7d0fd58fd683f61b

--- data/sql/updates/pending_db_world/rev_1645899379343826300.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645899379343826300');
-
-UPDATE `quest_request_items` SET `CompletionText` = "Possessing a Cenarion Beacon allows one to see a corrupted soul shard on those tainted beasts that are put down for the greater good. I grind shards into a usable reagent that goes into making Cenarion plant salve. We will use that salve to turn corrupted plants into healthy ones again.$b$bIn exchange for these shards, I will give you some Cenarion plant salves I have already prepared." WHERE `id` IN (4103, 4108, 5882, 5887);
