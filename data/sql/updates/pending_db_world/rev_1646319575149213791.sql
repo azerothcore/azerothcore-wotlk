c9658948c8f05019fa2bb982f88fd98aaa306f35
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-03-03 14:13:20 -0500

    Revertir

--- data/sql/updates/pending_db_world/rev_1646319575149213791.sql
+++ data/sql/updates/pending_db_world/rev_1646319575149213791.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646319575149213791');
 
-DELETE FROM `quest_offer_reward_locale` WHERE `RewardText` = NULL;
-DELETE FROM `quest_request_items_locale` WHERE `CompletionText` = NULL;
+DELETE FROM `quest_offer_reward_locale` WHERE  RewardText IS NULL;
+DELETE FROM `quest_request_items_locale` WHERE  CompletionText IS NULL;
