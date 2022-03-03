ea10782e2cde40ac0601a64260ea65333dfa5d78
Revision <tkn963@gmail.com>
2022-03-03 16:14:50 +0100

    REVERT
    Reverting the if to what I had it at originally until I can get some input on it...

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -615,7 +615,7 @@ void WorldSession::LogoutPlayer(bool save)
         _player->RemoveAurasWithInterruptFlags(AURA_INTERRUPT_FLAG_CHANGE_MAP);
 
         ///- If the player is not in a group (invited), remove him.
-        if (!_player->GetGroup())
+        if (sWorld->getBoolConfig(CONFIG_LEAVE_GROUP_ON_LOGOUT))
             _player->UninviteFromGroup();
 
         // remove player from the group if he is:
