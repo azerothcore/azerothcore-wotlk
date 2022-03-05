5f650f769c403a7850b428f0d94862d900fdbddc
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2022-03-05 19:04:34 +0100

    Create rev_1646503273471117785.sql

---
+++ data/sql/updates/pending_db_world/rev_1646503273471117785.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646503273471117785');
+
+INSERT INTO `acore_world`.`spell_script_names` (`spell_id`, `ScriptName`) VALUES (14161, 'spell_rog_ruthlessness');
