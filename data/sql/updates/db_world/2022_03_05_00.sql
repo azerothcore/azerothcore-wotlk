742a568c12c6163701a9d140a51bf3c217d3c9aa
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-05 15:39:13 +0000

    chore(DB): import pending files
    Referenced commit(s): 0d0e6b684a951e0e85159744903cb58a4ffb91f7

--- data/sql/updates/db_world/2022_03_05_00.sql
+++ data/sql/updates/db_world/2022_03_05_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_03_04 -> 2022_03_05_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_03_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_03_04 2022_03_05_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1645313448111474600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645313448111474600');
 
 /* A Threat in Feralas */
@@ -119,3 +135,13 @@ INSERT INTO `quest_details` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `Emot
 /* Elven Legends */
 DELETE FROM `quest_details` WHERE `ID`=7481;
 INSERT INTO `quest_details` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `VerifiedBuild`) VALUES (7481,1,1,1,0,0,0,0,0,0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_05_00' WHERE sql_rev = '1645313448111474600';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
