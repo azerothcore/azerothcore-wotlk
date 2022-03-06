45203ff440d6fed24f47865e75c0613bb1ffc6aa
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2022-03-06 13:49:15 -0700

    fix(DB/Scripts): Fix Ring of Blood Quest Credit (#10889)

---
+++ data/sql/updates/pending_db_world/rev_1646325069714130000.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646325069714130000');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=18471 AND `source_type`=0 AND `id`=31;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(18471, 0, 31, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 64, 2, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, "Gurgthock - On Quest \'The Ring of Blood: The Blue Brothers\' Taken - Store Target (Invoker Party)");
+
+UPDATE `smart_scripts` SET `link`=31 WHERE `entryorguid`=18471 AND `source_type`=0 AND `id`=3;
+
+UPDATE `smart_scripts` SET `target_type`=12, `target_param1`=2 WHERE `entryorguid`=18471 AND `source_type`=0 AND `id`=28;
