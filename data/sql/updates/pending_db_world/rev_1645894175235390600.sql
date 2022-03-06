30936e31e04c833bb8fcd275db269d386f94a5d7
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-06 18:00:56 +0000

    chore(DB): import pending files
    Referenced commit(s): b03078f805192d793961a789b8701e187ad77a0b

--- data/sql/updates/pending_db_world/rev_1645894175235390600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645894175235390600');
-
-DELETE FROM `smart_scripts` WHERE `entryorguid`=12557 AND `id`=5;
-INSERT INTO `smart_scripts` VALUES
-(12557,0,5,0,1,0,100,1,500,500,0,0,0,11,45537,0,0,0,0,0,11,12435,90,0,0,0,0,0,0,'Grethok the Controller - OCC - Cast Channel Beam');
--- data/sql/updates/pending_db_world/rev_1645976772405658200.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645976772405658200');
-
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|512 WHERE `entry` IN (33186,33724,28859,31734);
