dadbb829f12fb6540d1cda846827b8e979ecf56c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-06 17:21:43 +0000

    chore(DB): import pending files
    Referenced commit(s): 09df444187066ce9ecfaa31cede12905c73b94fb

--- data/sql/updates/db_world/2022_03_06_03.sql
+++ data/sql/updates/db_world/2022_03_06_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_06_02 -> 2022_03_06_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_06_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_06_02 2022_03_06_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1643973891449948100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643973891449948100');
 
 DELETE FROM `command` WHERE `name` IN ('reload quest_greeting', 'reload quest_greeting_locale');
@@ -25,3 +41,13 @@ CREATE TABLE `quest_greeting_locale` (
   `VerifiedBuild` SMALLINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`,`type`,`locale`)
 ) ENGINE=MYISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_06_03' WHERE sql_rev = '1643973891449948100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
