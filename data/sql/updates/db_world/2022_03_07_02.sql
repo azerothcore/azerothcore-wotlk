d108b27c2b18fb18bf679af99ec0ceecb64e9701
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-07 19:07:23 +0000

    chore(DB): import pending files
    Referenced commit(s): e4f88ba960503969b0d89d87d22896fd698d3d1e

--- data/sql/updates/db_world/2022_03_07_02.sql
+++ data/sql/updates/db_world/2022_03_07_02.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_07_01 -> 2022_03_07_02
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_07_01';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_07_01 2022_03_07_02 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1646670856940613800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646670856940613800');
 
 -- El diario deteriorado - ID 11986
@@ -726,3 +742,13 @@ DELETE FROM `quest_offer_reward_locale` WHERE `ID`=12794 AND `locale` IN ('esES'
 INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES
 (12794, 'esES', 'Bienvenido a Dalaran.$B$BMe aseguraré de que el anillo sea devuelto.', 18019),
 (12794, 'esMX', 'Bienvenido a Dalaran.$B$BMe aseguraré de que el anillo sea devuelto.', 18019);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_07_02' WHERE sql_rev = '1646670856940613800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
