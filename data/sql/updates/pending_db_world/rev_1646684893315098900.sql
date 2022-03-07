a2951525058da477a7f99b44b76019ea881ef78e
郑佩茹 <zhengpeiru21@protonmail.com>
2022-03-07 13:36:53 -0700

    fix(DB/quest_template_addon): Fix The Battle for Arathi Basin

--- data/sql/updates/pending_db_world/rev_1646684893315098900.sql
+++ data/sql/updates/pending_db_world/rev_1646684893315098900.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646684893315098900');
 
-UPDATE `quest_template_addon` SET `SpecialFlags` = `SpecialFlags` &~1 WHERE ID IN (8166, 8167, 8168, 8169, 8170, 8171, 8105, 8120);
+UPDATE `quest_template_addon` SET `SpecialFlags` = `SpecialFlags` &~1 WHERE `ID` IN (8166, 8167, 8168, 8169, 8170, 8171, 8105, 8120);
