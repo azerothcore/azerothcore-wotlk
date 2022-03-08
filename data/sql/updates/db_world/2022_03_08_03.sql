eb3b803970d1397f8408088316750eb2640408e9
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-08 16:24:37 +0000

    chore(DB): import pending files
    Referenced commit(s): 085ed12a69567ae43bbd6da802b73810430e92b4

--- data/sql/updates/db_world/2022_03_08_03.sql
+++ data/sql/updates/db_world/2022_03_08_03.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_08_02 -> 2022_03_08_03
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_08_02';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_08_02 2022_03_08_03 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1646749877159440400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646749877159440400');
 
 UPDATE `quest_template_locale` SET `ObjectiveText1`='Operaciones de los enanos férreos interrumpidas' WHERE `ID`=11982 AND `locale` IN ('esES', 'esMX');
@@ -151,3 +167,13 @@ INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `Ver
 (12483, 'esMX', '¿Le has conseguido esos ingredientes al viejo Prigmon, $n?', 18019),
 (12484, 'esES', '¿Qué tienes ahí, $r?', 18019),
 (12484, 'esMX', '¿Qué tienes ahí, $r?', 18019);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_08_03' WHERE sql_rev = '1646749877159440400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
