7ab75b988ef08563d9838b843ca85c947ed89581
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-08 13:54:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 3cab4f46f28016e6987c5a3bff472d0bac324e22

--- data/sql/updates/db_world/2022_03_08_01.sql
+++ data/sql/updates/db_world/2022_03_08_01.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_08_00 -> 2022_03_08_01
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_08_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_08_00 2022_03_08_01 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1646433692860151400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1646433692860151400');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 13996 AND `source_type` = 0 AND `id` = 2;
@@ -12,3 +28,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (-84614, 0, 7, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blackwing Technician - On Respawn - Disable Auto Attack'),
 (-84615, 0, 7, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blackwing Technician - On Respawn - Disable Auto Attack'),
 (-84616, 0, 7, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blackwing Technician - On Respawn - Disable Auto Attack');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_08_01' WHERE sql_rev = '1646433692860151400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
