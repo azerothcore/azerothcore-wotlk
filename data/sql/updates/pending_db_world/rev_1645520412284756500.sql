34169be3d8f16dfbb6137c428f1d488b4cdfa687
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-10 16:56:03 +0000

    chore(DB): import pending files
    Referenced commit(s): a6c93f1cbe29f88cfda7a52e5c33d0b5c52f29c6

--- data/sql/updates/pending_db_world/rev_1645520412284756500.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645520412284756500');
-
-DELETE FROM `game_event_creature` WHERE `guid` = 146623;
-INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
-(8, 146623);
