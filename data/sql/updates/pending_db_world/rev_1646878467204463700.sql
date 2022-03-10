f7da819d7b3796ccd8fb1d7c1354a89132b8d217
Nefertumm <nefertum.dev@protonmail.com>
2022-03-10 00:22:19 -0300

    typo

--- data/sql/updates/pending_db_world/rev_1646878467204463700.sql
+++ data/sql/updates/pending_db_world/rev_1646878467204463700.sql
@@ -11,4 +11,4 @@ DELETE FROM `creature` WHERE `guid` IN (84197, 84201, 84200, 84299, 84298);
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = -84615 AND `source_type` = 0 AND `id` = 8;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(-84615, 0, 8, 0, 58, 0, 100, 1, 0, 1399600, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blackwing Technician - On waypoint ended - Despawn');
+(-84615, 0, 8, 0, 58, 0, 100, 1, 0, 1399601, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blackwing Technician - On waypoint ended - Despawn');
