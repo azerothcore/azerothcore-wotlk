ae897c63a76e28e0699bf43d33b110efb6ea607c
Kosiak <v.k.kosiak@gmail.com>
2022-03-12 17:49:34 +0000

    fix(DB/smart_scripts): Quest : [The Restless Souls] #3194

---
+++ data/sql/updates/pending_db_world/rev_1647107182833454100.sql
@@ -0,0 +1,12 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647107182833454100');
+
+UPDATE `smart_scripts` SET `id` = 0 WHERE `entryorguid` = 11122 AND `action_type` = 11;
+UPDATE `smart_scripts` SET `link` = 1 WHERE `entryorguid` = 11122 AND `action_type` = 11;
+UPDATE `smart_scripts` SET `id` = 1 WHERE `entryorguid` = 11122 AND `action_type` = 36;
+UPDATE `smart_scripts` SET `link` = 2 WHERE `entryorguid` = 11122 AND `action_type` = 36;
+UPDATE `smart_scripts` SET `id` = 2 WHERE `entryorguid` = 11122 AND `action_type` = 29;
+UPDATE `smart_scripts` SET `link` = 3 WHERE `entryorguid` = 11122 AND `action_type` = 29;
+UPDATE `smart_scripts` SET `id` = 3 WHERE `entryorguid` = 11122 AND `action_type` = 41;
+UPDATE `smart_scripts` SET `link` = 4 WHERE `entryorguid` = 11122 AND `action_type` = 41;
+UPDATE `smart_scripts` SET `id` = 4 WHERE `entryorguid` = 11122 AND `action_type` = 33;
+UPDATE `smart_scripts` SET `link` = 0 WHERE `entryorguid` = 11122 AND `action_type` = 33;
