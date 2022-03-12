1eef5a7a20295f8829194c1e9eedf17964ca81e8
Kosiak <v.k.kosiak@gmail.com>
2022-03-12 15:19:04 +0000

    fix(DB/smart_scripts): Quest : [The Restless Souls] #3194

---
+++ data/sql/updates/pending_db_world/rev_1647098306369701000.sql
@@ -0,0 +1,12 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647098306369701000');
+
+UPDATE `acore_world`.`smart_scripts` SET `id` = 0 WHERE `entryorguid` = 11122 AND `action_type` = 11;
+UPDATE `acore_world`.`smart_scripts` SET `link` = 1 WHERE `entryorguid` = 11122 AND `action_type` = 11;
+UPDATE `acore_world`.`smart_scripts` SET `id` = 1 WHERE `entryorguid` = 11122 AND `action_type` = 36;
+UPDATE `acore_world`.`smart_scripts` SET `link` = 2 WHERE `entryorguid` = 11122 AND `action_type` = 36;
+UPDATE `acore_world`.`smart_scripts` SET `id` = 2 WHERE `entryorguid` = 11122 AND `action_type` = 29;
+UPDATE `acore_world`.`smart_scripts` SET `link` = 3 WHERE `entryorguid` = 11122 AND `action_type` = 29;
+UPDATE `acore_world`.`smart_scripts` SET `id` = 3 WHERE `entryorguid` = 11122 AND `action_type` = 41;
+UPDATE `acore_world`.`smart_scripts` SET `link` = 4 WHERE `entryorguid` = 11122 AND `action_type` = 41;
+UPDATE `acore_world`.`smart_scripts` SET `id` = 4 WHERE `entryorguid` = 11122 AND `action_type` = 33;
+UPDATE `acore_world`.`smart_scripts` SET `link` = 0 WHERE `entryorguid` = 11122 AND `action_type` = 33;
