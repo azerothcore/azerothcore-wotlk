6d18c9178b1f3f67f4fca9db4706853963c20945
Kosiak <v.k.kosiak@gmail.com>
2022-03-13 20:53:20 +0000

    fix(DB/quest_template): Fix Quest: Rat Catching

---
+++ data/sql/updates/pending_db_world/rev_1647204744919951000.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647204744919951000');
+
+UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mountaineer Kadrell at Thelsamar in Loch Modan.' WHERE `ID` = 416 AND `QuestSortID` = 38;
