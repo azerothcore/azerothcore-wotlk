1dd05ac3904a513f763ba5b8012274a252819fb4
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-13 11:36:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 7decab3ccd30ab11f8e972aa1c61eb09fa8e91f7

--- data/sql/updates/db_world/2022_03_13_00.sql
+++ data/sql/updates/db_world/2022_03_13_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_12_03 -> 2022_03_13_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_12_03';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_12_03 2022_03_13_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1647042448722452100'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647042448722452100');
 
 UPDATE `creature_template` SET `unit_flags` = `unit_flags`|256|512|33554432, `AIName` = 'SmartAI', `flags_extra` = `flags_extra`|2|64 WHERE `entry` IN (14307, 14309, 14310, 14311, 14312);
@@ -18,3 +34,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_spawn_drakonid';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (22659, 'spell_spawn_drakonid');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_13_00' WHERE sql_rev = '1647042448722452100';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
