a751097d3b489a59765d0cca847752f68975da96
Bogir[rus] <m-1@bk.ru>
2022-03-14 08:12:50 +0500

    typo fix

--- data/sql/updates/pending_db_world/rev_1647226315696119904.sql
+++ data/sql/updates/pending_db_world/rev_1647226315696119904.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647226315696119904');
 
 -- I want to exchange my Emerald Essence for Amber Essence.
-UPDATE `gossip_menu_option` SET `OptionText` = 'I want to exchange my Emerald Essence for Amber Essence.' WHERE OptionBroadcastTextID` = 27038;
+UPDATE `gossip_menu_option` SET `OptionText` = 'I want to exchange my Emerald Essence for Amber Essence.' WHERE `OptionBroadcastTextID` = 27038;
