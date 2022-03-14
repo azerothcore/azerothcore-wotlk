6b1bfd021b4a5cb8c44770ee6e3825f029aad479
Kosiak <v.k.kosiak@gmail.com>
2022-03-14 11:51:30 +0000

    Update rev_1647208425086920700.sql

--- data/sql/updates/pending_db_world/rev_1647208425086920700.sql
+++ data/sql/updates/pending_db_world/rev_1647208425086920700.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647208425086920700');
 
-UPDATE `creature_template` SET `minlevel` = 25, `maxlevel` = 26 WHERE `entry` = 37984 AND `name` = 'Crown Duster';
-UPDATE `creature_template` SET `minlevel` = 33, `maxlevel` = 34 WHERE `entry` = 38006 AND `name` = 'Crown Hoodlum';
+UPDATE `creature_template` SET `minlevel` = 25, `maxlevel` = 26 WHERE `entry` = 37984;
+UPDATE `creature_template` SET `minlevel` = 33, `maxlevel` = 34 WHERE `entry` = 38006;
