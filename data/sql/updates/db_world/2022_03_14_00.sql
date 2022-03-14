e25f4068f6e84332a6f3eced5d064a1c2a70aa64
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-14 11:52:20 +0000

    chore(DB): import pending files
    Referenced commit(s): 4806769beb160a13d35aef98c095e73b750b17be

--- data/sql/updates/db_world/2022_03_14_00.sql
+++ data/sql/updates/db_world/2022_03_14_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_13_00 -> 2022_03_14_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_13_00';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_13_00 2022_03_14_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1643424341492582600'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643424341492582600');
 
 DELETE FROM `npc_text` WHERE `ID` IN (10303, 10304);
@@ -38,3 +54,13 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 UPDATE `creature_template` SET `gossip_menu_id` = 3068, `npcflag` = `npcflag`|1 WHERE `entry` = 7867; -- Thorkaf Dragoneye
 UPDATE `creature_template` SET `gossip_menu_id` = 3069, `npcflag` = `npcflag`|1 WHERE `entry` = 7869; -- Mrumn Winterhoof
 UPDATE `creature_template` SET `gossip_menu_id` = 3073, `npcflag` = `npcflag`|1 WHERE `entry` = 7871; -- Se'Jib
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_14_00' WHERE sql_rev = '1643424341492582600';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
