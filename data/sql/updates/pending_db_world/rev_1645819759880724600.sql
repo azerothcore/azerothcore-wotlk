a24af6316770d25822179827d7b6c3ded7b8ff99
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-15 17:39:07 +0000

    chore(DB): import pending files
    Referenced commit(s): 78bdc898813910c79c3a9c67f2bf9a0f1abef221

--- data/sql/updates/pending_db_world/rev_1645819759880724600.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1645819759880724600');
-
-DELETE FROM `acore_string` WHERE `entry` = 726;
-INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
-(726, '|cffff0000[Arena Queue]:|r %s (skirmish %s) -- [%u-%u] [%u/%u]|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
