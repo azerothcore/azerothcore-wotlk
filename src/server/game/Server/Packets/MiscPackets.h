5e3a40ad2e341ba5e3577c4fcc581151f3feae11
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-03-16 09:44:17 +0330

    ,

--- src/server/game/Server/Packets/MiscPackets.h
+++ src/server/game/Server/Packets/MiscPackets.h
@@ -112,6 +112,16 @@ namespace WorldPackets
             uint32 Threshold = 0;
             uint32 ItemID = 0;
         };
+
+        class UITime final : public ServerPacket
+        {
+        public:
+            UITime() : ServerPacket(SMSG_WORLD_STATE_UI_TIMER_UPDATE, 4) { }
+
+            WorldPacket const* Write() override;
+
+            uint32 Time = 0;
+        };
     }
 }
 
