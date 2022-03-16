f272d2b271eeba974cf4c810bcbab1d91c0dc3a4
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-16 01:16:31 +0000

    chore(DB): import pending files
    Referenced commit(s): 7357c886187fb01fc5bb039f4ab2f53a1dd9deb4

--- data/sql/updates/pending_db_world/rev_1647185908361212000.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647185908361212000');
-
-DELETE FROM `spell_script_names` WHERE `spell_id`=45831;
-INSERT INTO `spell_script_names` VALUES
-(45831,'spell_gen_av_drekthar_presence');
