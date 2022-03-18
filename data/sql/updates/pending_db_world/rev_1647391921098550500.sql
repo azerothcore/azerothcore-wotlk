8a027c0939e785d83fcff71a7eb8787b2b148499
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-18 22:25:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 199d2159e85b78f18ba6aab0fdd14f7e687a8d4d

--- data/sql/updates/pending_db_world/rev_1647391921098550500.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647391921098550500');
-
--- Update target for quest credit
-UPDATE `smart_scripts` SET `target_type` = 21, `target_param1` = 5 WHERE `entryorguid` = 7207 AND `id` = 1;
