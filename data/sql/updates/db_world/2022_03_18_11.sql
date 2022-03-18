b2e9f8d1da5502315a3da774e6c807b6664f8f29
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-18 22:05:14 +0000

    chore(DB): import pending files
    Referenced commit(s): 9d4cc721b49fd44a2349f71d79fe9e8292d3b3a2

--- data/sql/updates/db_world/2022_03_18_11.sql
+++ data/sql/updates/db_world/2022_03_18_11.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_18_10 -> 2022_03_18_11
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_18_10';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_18_10 2022_03_18_11 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1647269947240244800'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647269947240244800');
 
 /*  Booty Bay Bruiser - GUID 178  */
@@ -1894,3 +1910,13 @@ INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `positio
 (@PATH, 12, -14127.3, 482.846, 1.3567, 100.0, 0),
 (@PATH, 13, -14138.5, 479.154, 2.71788, 100.0, 0),
 (@PATH, 14, -14142.4, 469.113, 3.45617, 100.0, 0);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_18_11' WHERE sql_rev = '1647269947240244800';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
