c11a96b775fa97360fbeeeca53a34910a6ce938b
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-18 21:31:59 +0000

    chore(DB): import pending files
    Referenced commit(s): 3ae07698588d0d94879b8a73041ceade6c5389de

--- data/sql/updates/pending_db_world/rev_1647134885721287500.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647134885721287500');
-
-UPDATE `conditions` SET `ConditionTypeOrReference` = 8 WHERE `SourceEntry` = 1645 AND `ConditionValue1` = 2997;
-UPDATE `conditions` SET `ConditionTypeOrReference` = 8 WHERE `SourceEntry` = 1645 AND `ConditionValue1` = 2999;
-UPDATE `conditions` SET `ConditionTypeOrReference` = 8 WHERE `SourceEntry` = 1645 AND `ConditionValue1` = 3000;
-UPDATE `conditions` SET `ElseGroup` = 1 WHERE `SourceEntry` = 1645 AND `ConditionValue1` = 2997;
-UPDATE `conditions` SET `ElseGroup` = 2 WHERE `SourceEntry` = 1645 AND `ConditionValue1` = 2999;
-UPDATE `conditions` SET `ElseGroup` = 3 WHERE `SourceEntry` = 1645 AND `ConditionValue1` = 3000;
