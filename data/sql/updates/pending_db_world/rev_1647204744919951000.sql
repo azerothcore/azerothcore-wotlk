bd039d9b8008288622e54f475fcecc8d0365031c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-18 21:57:15 +0000

    chore(DB): import pending files
    Referenced commit(s): a067b4cf5c6eeab8d8863ce6ca38a268806f92b4

--- data/sql/updates/pending_db_world/rev_1647204744919951000.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647204744919951000');
-
-UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Mountaineer Kadrell at Thelsamar in Loch Modan.' WHERE `ID` = 416;
