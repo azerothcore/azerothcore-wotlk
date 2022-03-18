204043ccd21fdf0857eca6c4dbcb6d1634d937ef
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-18 23:33:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 534056f5615747e5f29e01c75ea6e508f20f6a85

--- data/sql/updates/pending_db_world/rev_1647208425086920700.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647208425086920700');
-
-UPDATE `creature_template` SET `minlevel` = 25, `maxlevel` = 26 WHERE `entry` = 37984;
-UPDATE `creature_template` SET `minlevel` = 33, `maxlevel` = 34 WHERE `entry` = 38006;
