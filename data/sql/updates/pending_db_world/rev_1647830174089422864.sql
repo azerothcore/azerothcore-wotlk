490f475ead80af3918e197a4f245f38cd5fbe13e
Bogir[rus] <m-1@bk.ru>
2022-03-21 11:16:33 +0500

    Update rev_1647830174089422864.sql

--- data/sql/updates/pending_db_world/rev_1647830174089422864.sql
+++ data/sql/updates/pending_db_world/rev_1647830174089422864.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647830174089422864');
 
 -- Trial of the Crusider - NPC_DARK_ESSENCE
-UPDATE `creature` SET `npcflag` = 1 WHERE `entry` = 34567;
+UPDATE `creature_template` SET `npcflag` = 1 WHERE `entry` = 34567;
 -- Trial of the Crusider - NPC_LIGHT_ESSENCE
-UPDATE `creature` SET `npcflag` = 1 WHERE `entry` = 34568;
+UPDATE `creature_template` SET `npcflag` = 1 WHERE `entry` = 34568;
