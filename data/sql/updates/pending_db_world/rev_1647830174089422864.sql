22b08ee566eab2e5e220ac95b7676ea8b9c9ac73
Bogir[rus] <m-1@bk.ru>
2022-03-21 07:37:36 +0500

    fix interaction with ESSENCEs

---
+++ data/sql/updates/pending_db_world/rev_1647830174089422864.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647830174089422864');
+
+-- Trial of the Crusider - NPC_DARK_ESSENCE
+UPDATE `creature` SET `npcflag` = 1 WHERE ID = 34567;
+-- Trial of the Crusider - NPC_LIGHT_ESSENCE
+UPDATE `creature` SET `npcflag` = 1 WHERE ID = 34568;
