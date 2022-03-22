3fcedbfa58ef8d2fe570cd1c8b24e321fdd960f7
Kosiak <v.k.kosiak@gmail.com>
2022-03-22 23:48:58 +0000

    typo

--- data/sql/updates/pending_db_world/rev_1647992044021568600.sql
+++ data/sql/updates/pending_db_world/rev_1647992044021568600.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647992044021568600');
 
 DELETE FROM `conditions` WHERE `SourceGroup` = 643;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (14, 643, 1203, 0, 0, 7, 0, 182, 1, 0, 0, 0, 0, '', 'Show gossip text if player is a Herbalist'),
 (14, 643, 1202, 0, 0, 7, 0, 182, 0, 0, 0, 0, 0, '', 'Show gossip text if player is not a Herbalist');
