ba5b764d22fa036f8eff25520bdfab73ec665800
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-23 00:18:26 +0000

    chore(DB): import pending files
    Referenced commit(s): 4e96604eb3118cb4e22f67fdfd50199646c5b533

--- data/sql/updates/pending_db_world/rev_1647993341915438725.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647993341915438725');
-
--- Burning Blade npc weapons
-UPDATE `creature` SET `equipment_id` = 1 WHERE `id1` IN (4663,4664,4665,4666,4667);
