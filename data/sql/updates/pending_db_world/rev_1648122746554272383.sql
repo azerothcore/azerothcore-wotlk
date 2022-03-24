cbd376cd5204b9e45bc7f1976362ec312bdd2a53
Bogir[rus] <m-1@bk.ru>
2022-03-24 16:53:50 +0500

    add missed gossip menu 4542 option 3
    High Inquisitor Fairbanks

---
+++ data/sql/updates/pending_db_world/rev_1648122746554272383.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648122746554272383');
+
+-- npc_fairbanks 4542 High Inquisitor Fairbanks
+REPLACE INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`)
+VALUES 
+('4542', '3', '0', 'Mograine?', '12483', '0', '0', '0', '0', '0', '0', '', '0', '0');
