ba0ff20965676c127dffb69f9739401b5c6abb04
Bogir[rus] <m-1@bk.ru>
2022-03-24 22:49:15 +0500

    add missed gossip menu for Deathbringer Saurfang

---
+++ data/sql/updates/pending_db_world/rev_1648144037216281638.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648144037216281638');
+
+-- boss deathbringer saurfang
+REPLACE INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+('10952', '15218'),
+('10953', '15217');
