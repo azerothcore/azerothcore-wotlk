6667a953e21a9976d0c05cfcba53fbde35154cf6
Bogir[rus] <m-1@bk.ru>
2022-03-24 19:24:58 +0500

    fix(DB/gossip_menu): add missed gossip menu
    Used by npc Thrall Warchief  (Zone Orgrimmar)

---
+++ data/sql/updates/pending_db_world/rev_1648131745456322279.sql
@@ -0,0 +1,10 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648131745456322279');
+
+-- for Zone Orgrimmar
+REPLACE INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
+('3665', '5733'),
+('3666', '5734'),
+('3667', '5735'),
+('3668', '5736'),
+('3669', '5737'),
+('3670', '5738');
