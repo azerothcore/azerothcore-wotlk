7255cbbd9ae8ad03a0fd6ce16e0ed866fb8a49ee
Bogir[rus] <m-1@bk.ru>
2022-03-24 17:15:23 +0500

    Update rev_1648122746554272383.sql

--- data/sql/updates/pending_db_world/rev_1648122746554272383.sql
+++ data/sql/updates/pending_db_world/rev_1648122746554272383.sql
@@ -3,4 +3,15 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648122746554272383');
 -- npc_fairbanks 4542 High Inquisitor Fairbanks
 REPLACE INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`)
 VALUES 
-('4542', '3', '0', 'Mograine?', '12483', '0', '0', '0', '0', '0', '0', '', '0', '0');
+('4542', '3', '0', 'Mograine?', '12483', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '5', '0', 'I still do not fully understand.', '12487', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '6', '0', 'Incredible story. So how did he die?', '12489', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '7', '0', 'You mean...', '12491', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '8', '0', 'A thousand? For one man?', '12495', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '9', '0', 'Yet? Yet what??', '12497', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '10', '0', 'And did he?', '12499', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '11', '0', 'Continue please, Fairbanks', '12501', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '12', '0', 'You were right, Fairbanks. That is tragic.', '12505', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '13', '0', 'And you did...', '12507', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '14', '0', 'You tell an incredible tale, Fairbanks. What of the blade? Is it beyond redemption?', '12509', '0', '0', '0', '0', '0', '0', '', '0', '0'),
+('4542', '15', '0', 'But his son is dead.', '12511', '0', '0', '0', '0', '0', '0', '', '0', '0');
