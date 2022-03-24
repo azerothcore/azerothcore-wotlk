8a6e3b3ec17805773abce6e1a20737e61d93566a
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-03-24 11:59:04 +0430

    10

--- src/server/scripts/Outland/Auchindoun/ShadowLabyrinth/shadow_labyrinth.cpp
+++ src/server/scripts/Outland/Auchindoun/ShadowLabyrinth/shadow_labyrinth.cpp
@@ -15,53 +15,44 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "SpellScript.h"
 #include "ScriptMgr.h"
+#include "SpellMgr.h"
+#include "SpellScript.h"
+#include "SpellAuraEffects.h"
+#include "Unit.h"
 
 enum Spells
 {
     SPELL_MARK_OF_MALICE_TRIGGERED = 33494
 };
 
-class spell_mark_of_malice : public SpellScriptLoader
+// 33493 - Mark of Malice
+class spell_mark_of_malice : public AuraScript
 {
-public:
-    spell_mark_of_malice() : SpellScriptLoader("spell_mark_of_malice") { }
+    PrepareAuraScript(spell_mark_of_malice);
 
-    class spell_mark_of_malice_AuraScript : public AuraScript
+    bool Validate(SpellInfo const* /*spellInfo*/) override
     {
-        PrepareAuraScript(spell_mark_of_malice_AuraScript);
+        return ValidateSpellInfo({ SPELL_MARK_OF_MALICE_TRIGGERED });
+    }
 
-        bool Validate(SpellInfo const* /*spellInfo*/) override
-        {
-            if (!sSpellMgr->GetSpellInfo(SPELL_MARK_OF_MALICE_TRIGGERED))
-                return false;
-            return true;
-        }
-
-        void HandleProc(AuraEffect const* aurEff, ProcEventInfo& /*eventInfo*/)
-        {
-            PreventDefaultAction();
-            // just drop charges
-            if (aurEff->GetBase()->GetCharges() > 1)
-                return;
-
-            GetTarget()->CastSpell(GetTarget(), SPELL_MARK_OF_MALICE_TRIGGERED, true);
-        }
-
-        void Register() override
-        {
-            OnEffectProc += AuraEffectProcFn(spell_mark_of_malice_AuraScript::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
-        }
-    };
-
-    AuraScript* GetAuraScript() const override
+    void HandleProc(AuraEffect const* aurEff, ProcEventInfo& /*eventInfo*/)
     {
-        return new spell_mark_of_malice_AuraScript();
+        PreventDefaultAction();
+        // just drop charges
+        if (aurEff->GetBase()->GetCharges() > 1)
+            return;
+
+        GetTarget()->CastSpell(GetTarget(), SPELL_MARK_OF_MALICE_TRIGGERED, aurEff);
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_mark_of_malice::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
     }
 };
 
 void AddSC_shadow_labyrinth()
 {
-    new spell_mark_of_malice();
+    RegisterSpellScript(spell_mark_of_malice);
 }
