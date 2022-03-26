9e5621065c244bca92ed0002a92eb2d3c36b228a
Nefertumm <nefertum.dev@protonmail.com>
2022-03-26 11:28:40 -0300

    missing door

--- data/sql/updates/pending_db_world/rev_1648289846118024900.sql
+++ data/sql/updates/pending_db_world/rev_1648289846118024900.sql
@@ -1,7 +1,7 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648289846118024900');
 
 -- Fix selectable doors
-UPDATE `gameobject_template_addon` SET `faction` = 114, `flags` = `flags`|32 WHERE `entry` IN (179117, 179365);
+UPDATE `gameobject_template_addon` SET `faction` = 114, `flags` = `flags`|32 WHERE `entry` IN (176964, 179117, 179365);
 UPDATE `gameobject_template_addon` SET `flags` = `flags`|4 WHERE `entry` = 179115;
 
 -- Fix Blackwing technicians spawn time
