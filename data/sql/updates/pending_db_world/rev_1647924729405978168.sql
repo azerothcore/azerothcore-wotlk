b18dcb7b804a16a259884b12d65c5e4847c8b467
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-27 05:04:11 +0000

    chore(DB): import pending files
    Referenced commit(s): a704aa3005c0115bea527598fda807914eb8a25e

--- data/sql/updates/pending_db_world/rev_1647924729405978168.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647924729405978168');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_death_knight_initiate_visual');
-INSERT INTO `spell_script_names` VALUES
-(51519, 'spell_death_knight_initiate_visual');
