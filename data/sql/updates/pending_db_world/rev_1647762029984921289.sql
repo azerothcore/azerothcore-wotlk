17f9b6625529a10cfc34151e5c949d9229cf24ae
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-27 04:57:48 +0000

    chore(DB): import pending files
    Referenced commit(s): 7410f1a3cb6f401c3ecce9dbb0d7d5926ae671f9

--- data/sql/updates/pending_db_world/rev_1647762029984921289.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647762029984921289');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_pri_lightwell');
-INSERT INTO `spell_script_names` VALUES
-(60123,'spell_pri_lightwell');
