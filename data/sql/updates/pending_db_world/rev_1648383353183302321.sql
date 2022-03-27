c9377a244fa99f564e316810d1f629d2c210876c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-27 15:05:38 +0000

    chore(DB): import pending files
    Referenced commit(s): b5344b638f6886c7ef108a8e20310cd6874c4b9d

--- data/sql/updates/pending_db_world/rev_1648383353183302321.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648383353183302321');
--- This updates the existing proc to allow trinket to pop on any melee damage including melee spells
-UPDATE `spell_proc_event` SET `SchoolMask`='1' AND `ProcFlags`='20' WHERE `entry`='15600';
