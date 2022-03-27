a4e68e681efc573e24d22a0c99d5a07492c4c4e0
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-27 04:55:08 +0000

    chore(DB): import pending files
    Referenced commit(s): 5719e4623661be8704f91a6dfdca18a8cfe4cb4d

--- data/sql/updates/pending_db_world/rev_1647761424947662456.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647761424947662456');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_item_runic_healing_injector';
-INSERT INTO `spell_script_names` VALUES
-(67489,'spell_item_runic_healing_injector');
