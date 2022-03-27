ab97efdce93fe0b11a9ecdf86ce247ab3f9db153
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-27 05:00:43 +0000

    chore(DB): import pending files
    Referenced commit(s): d1ecdc8f617271f5bb4720bc728eef3174bab10c

--- data/sql/updates/pending_db_world/rev_1647861814144531389.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647861814144531389');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_item_dimensional_ripper_area52';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (36890,'spell_item_dimensional_ripper_area52');
