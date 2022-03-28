bd2844bb2d247ad69145fdb12acc6b213a087ac0
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-28 16:57:37 +0000

    chore(DB): import pending files
    Referenced commit(s): cb6d579ddb27feb21245006161baf060f5dcd48a

--- data/sql/updates/db_world/2022_03_28_00.sql
+++ data/sql/updates/db_world/2022_03_28_00.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_03_27_22 -> 2022_03_28_00
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_03_27_22';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_03_27_22 2022_03_28_00 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1648398529574716400'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648398529574716400');
 
 DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (5481, 5482) AND `OptionID` = 0;
@@ -19,3 +35,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (10182, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 56, 16309, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Rokaro - On Link - Create \'Drakefire Amulet\''),
 (10929, 0, 1, 2, 62, 0, 100, 0, 5482, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Haleh - On Gossip Option 1 Selected - Close Gossip'),
 (10929, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 56, 16309, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Haleh - On Link - Create \'Drakefire Amulet\'');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_03_28_00' WHERE sql_rev = '1648398529574716400';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
