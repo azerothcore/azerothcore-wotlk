fbdc8e76343b20f364c4616f67efbd91fc2a020a
Nefertumm <nefertum.dev@protonmail.com>
2022-03-29 00:23:33 -0300

    Update rev_1648266327758563400.sql

--- data/sql/updates/pending_db_world/rev_1648266327758563400.sql
+++ data/sql/updates/pending_db_world/rev_1648266327758563400.sql
@@ -1,3 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648266327758563400');
 
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|512 WHERE `entry`=36597;
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|512 WHERE `entry` IN (36597, 39166, 39167, 39168);
