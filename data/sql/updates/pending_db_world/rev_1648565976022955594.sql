6a291b072c05d040ad3a5870339859225f34c3cd
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-03-29 11:02:43 -0400

    feat(DB/Locale): Entry 12698,12701,12722
    frFR and ruRU

---
+++ data/sql/updates/pending_db_world/rev_1648565976022955594.sql
@@ -0,0 +1,8 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648565976022955594');
+
+UPDATE `quest_template_locale` SET `ObjectiveText1`="Goule écarlate revenue" WHERE `ID`=12698 AND `locale`="frFR";
+UPDATE `quest_template_locale` SET `ObjectiveText1`="Обращено вурдалаков" WHERE `ID`=12698 AND `locale`="ruRU";
+UPDATE `quest_template_locale` SET `ObjectiveText1`="Défenseur écarlate tué" WHERE `ID`=12701 AND `locale`="frFR";
+UPDATE `quest_template_locale` SET `ObjectiveText1`="Убито защитников Алого ордена" WHERE `ID`=12701 AND `locale`="ruRU";
+UPDATE `quest_template_locale` SET `ObjectiveText1`="Soldat de la Croisade écarlate tué",`ObjectiveText2`="Habitant de la Nouvelle-Avalon tué" WHERE `ID`=12722 AND `locale`="frFR";
+UPDATE `quest_template_locale` SET `ObjectiveText1`="Убито солдат Алого ордена",`ObjectiveText2`="Убито граждан Нового Авалона" WHERE `ID`=12722 AND `locale`="ruRU";
