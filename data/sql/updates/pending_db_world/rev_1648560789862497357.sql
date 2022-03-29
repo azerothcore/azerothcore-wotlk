d270002d98dc1a78c39f5f2d53e04ef41512a09c
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-03-29 09:35:39 -0400

    feat(DB/Locale): Entry 190671 deDE
    Update Entry 190671 "deDE"

---
+++ data/sql/updates/pending_db_world/rev_1648560789862497357.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648560789862497357');
+
+UPDATE`gameobject_template_locale` SET `name` = "Westlicher Zeppelinlandeturm - Nordend und Mulgore"  WHERE `entry` = 190671 AND `locale` = "deDE" ;
