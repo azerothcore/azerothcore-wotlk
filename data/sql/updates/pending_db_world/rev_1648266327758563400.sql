e8e8c345906220d2c3dda2066b9baf14172b9897
AzerothCoreBot <azerothcorebot@gmail.com>
2022-03-30 12:49:55 +0000

    chore(DB): import pending files
    Referenced commit(s): e139ed1c0bbd342b300083e1ffbfdb883cb1b009

--- data/sql/updates/pending_db_world/rev_1648266327758563400.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648266327758563400');
-
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|512 WHERE `entry` IN (36597, 39166, 39167, 39168);
