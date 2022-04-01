b2ccee4d2867e4c8948ce6fc9881dffe6b24e2a3
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-01 10:52:28 +0000

    chore(DB): import pending files
    Referenced commit(s): cd32ac5aa0fef66ff785294e6662274cf4f9ca28

--- data/sql/updates/pending_db_world/rev_1647762263757948026.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647762263757948026');
-
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_gen_teleporting');
-INSERT INTO `spell_script_names` VALUES
-(59317,'spell_gen_teleporting');
