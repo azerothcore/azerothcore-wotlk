3f7bfeb8adb8178f07a8500807713ce1e46ef50a
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2022-04-01 12:34:42 -0600

    Update rev_1643928221060796100.sql

--- data/sql/updates/pending_db_world/rev_1643928221060796100.sql
+++ data/sql/updates/pending_db_world/rev_1643928221060796100.sql
@@ -1,4 +1,4 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1643928221060796100');
 
 UPDATE `gossip_menu` SET `TextID`=3218 WHERE `MenuID`=12726 AND `TextID`=17861;
-DELETE FROM 'npc_text' WHERE `ID`=17861;
+DELETE FROM `npc_text` WHERE `ID`=17861;
