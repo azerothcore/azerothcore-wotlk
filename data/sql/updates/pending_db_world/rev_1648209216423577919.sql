da3748b55f31ca0b3d5cda7c6bd70a8de8259735
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-02 14:58:54 +0000

    chore(DB): import pending files
    Referenced commit(s): 7a336a137079d7a0c7045a3435bc62f26dc11e76

--- data/sql/updates/pending_db_world/rev_1648209216423577919.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648209216423577919');
-
--- Lich King for Valithria Dreamwalker - AGGRO
-UPDATE `creature_text` SET `BroadcastTextId` = 38682 WHERE `CreatureID` = 38153;
