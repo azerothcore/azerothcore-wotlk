7dc122b6f7b1b759ad2659fc7360dadc9486cb4e
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-02 14:50:16 +0000

    chore(DB): import pending files
    Referenced commit(s): 883becc6d992e8d021d54af944d8c1cc29c6e945

--- data/sql/updates/pending_db_world/rev_1648810356834040900.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648810356834040900');
-
-DELETE FROM `command` WHERE `name` IN ('respawn', 'respawn all');
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('respawn', 2, 'Syntax: .respawn\nRespawn the selected unit without waiting respawn time expiration.'),
-('respawn all', 2, 'Syntax: .respawn all\nRespawn all nearest creatures and GO without waiting respawn time expiration.');
