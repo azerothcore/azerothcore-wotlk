f723c9f81527ccac79319d0d60e19e2a5de8676c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-02 10:51:45 +0000

    chore(DB): import pending files
    Referenced commit(s): d24adb4b146a5b4a332c555e3ebd52e1b1b01e93

--- data/sql/updates/db_world/2022_04_02_05.sql
+++ data/sql/updates/db_world/2022_04_02_05.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_04_02_04 -> 2022_04_02_05
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_04_02_04';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_04_02_04 2022_04_02_05 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1648470144050521037'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648470144050521037');
 
 REPLACE INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES
@@ -219,3 +235,13 @@ REPLACE INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `Verifie
 ('7492', 'ruRU', 'А, рад видеть тебя $C. Мне нужна твоя поддержка, $N, и боюсь, что должен направить тебя навстречу опасности…$B$BА именно в Забытый Город.', '0'),
 ('7495', 'ruRU', 'Наконец-то, после стольких лет ожидания... Честь Штормграда восстановлена! Правосудие свершилось! Победа! Настоящая победа!', '0'),
 ('7496', 'ruRU', '$N, немногие были удостоены подобной церемонии. Прошу, выбери свою награду, и мы начнем праздник!', '0');
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_04_02_05' WHERE sql_rev = '1648470144050521037';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
