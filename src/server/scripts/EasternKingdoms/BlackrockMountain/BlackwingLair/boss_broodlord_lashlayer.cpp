741dced259e44c48f12fe548fe4aad6428129fc4
UltraNix <ultranix@gmail.com>
2022-04-03 16:34:45 +0200

    Scripts/Spells: Suppression Aura should not hit stealthed units.
    Fixes #11249

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_broodlord_lashlayer.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_broodlord_lashlayer.cpp
@@ -247,8 +247,28 @@ class go_suppression_device : public GameObjectScript
         }
 };
 
+class spell_suppression_aura : public SpellScript
+{
+    PrepareSpellScript(spell_suppression_aura);
+
+    void FilterTargets(std::list<WorldObject*>& targets)
+    {
+        targets.remove_if([&](WorldObject* target) -> bool
+        {
+            Unit* unit = target->ToUnit();
+            return !unit || unit->HasAuraType(SPELL_AURA_MOD_STEALTH);
+        });
+    }
+
+    void Register() override
+    {
+        OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_suppression_aura::FilterTargets, EFFECT_ALL, TARGET_UNIT_DEST_AREA_ENEMY);
+    }
+};
+
 void AddSC_boss_broodlord()
 {
     new boss_broodlord();
     new go_suppression_device();
+    RegisterSpellScript(spell_suppression_aura);
 }
