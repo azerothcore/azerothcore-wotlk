a62d576ad19b16af18edba44a2819bba94a98bc7
Nefertumm <nefertum.dev@protonmail.com>
2022-04-03 14:20:06 -0300

    Update data/sql/updates/pending_db_world/rev_1648996451912542300.sql

--- data/sql/updates/pending_db_world/rev_1648996451912542300.sql
+++ data/sql/updates/pending_db_world/rev_1648996451912542300.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648996451912542300');
 
 DELETE FROM `spell_script_names` WHERE `spell_id`=22247;
-INSERT INTO `spell_script_names` VALUES
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (22247,'spell_suppression_aura');
