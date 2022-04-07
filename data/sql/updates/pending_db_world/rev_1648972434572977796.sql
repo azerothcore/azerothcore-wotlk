14463eb6f3fdccaa66de3f1d0f20fc6ebedc6908
Bogir[rus] <m-1@bk.ru>
2022-04-07 17:35:45 +0500

    fix(DB/creature_template_locale): npc Title ruRU (#11265)
    Fixed ruRU title of npc Lhara (Darkmoon Faire Exotic Goods)

---
+++ data/sql/updates/pending_db_world/rev_1648972434572977796.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648972434572977796');
+
+UPDATE `creature_template_locale` SET `Title` = 'Лавка редких товаров ярмарки Новолуния' WHERE `entry` = 14846 AND `locale` = 'ruRU';
