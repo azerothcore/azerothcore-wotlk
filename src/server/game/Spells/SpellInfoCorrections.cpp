2c0b80853a92ed0a90be12af997d4b08adc9d2c6
temperrr <temperrr@users.noreply.github.com>
2022-04-07 11:44:06 +0200

    Update SpellInfoCorrections.cpp

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4199,13 +4199,13 @@ void SpellMgr::LoadSpellInfoCorrections()
     {
         spellInfo->InterruptFlags &= ~SPELL_INTERRUPT_FLAG_INTERRUPT;
     });
-
+    
     // Improved Blessing Protection (Nefarian Class Call)
     ApplySpellFix({ 23415 }, [](SpellInfo* spellInfo)
     {
         spellInfo->Effects[EFFECT_0].TargetA = SpellImplicitTargetInfo(TARGET_DEST_TARGET_ENEMY);
     });
-
+    
     // Bestial Wrath
     ApplySpellFix({ 19574 }, [](SpellInfo* spellInfo)
     {
