577997d42d3c551e96db40394e96c17d20beb931
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-07 15:38:54 +0000

    chore(DB): import pending files
    Referenced commit(s): eee2d68a1d1eb8cfdfcb393fa1191eedc9b07dfa

--- data/sql/updates/db_world/2022_04_07_07.sql
+++ data/sql/updates/db_world/2022_04_07_07.sql
@@ -1,3 +1,19 @@
+-- DB update 2022_04_07_06 -> 2022_04_07_07
+DROP PROCEDURE IF EXISTS `updateDb`;
+DELIMITER //
+CREATE PROCEDURE updateDb ()
+proc:BEGIN DECLARE OK VARCHAR(100) DEFAULT 'FALSE';
+SELECT COUNT(*) INTO @COLEXISTS
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'version_db_world' AND COLUMN_NAME = '2022_04_07_06';
+IF @COLEXISTS = 0 THEN LEAVE proc; END IF;
+START TRANSACTION;
+ALTER TABLE version_db_world CHANGE COLUMN 2022_04_07_06 2022_04_07_07 bit;
+SELECT sql_rev INTO OK FROM version_db_world WHERE sql_rev = '1648965068000701761'; IF OK <> 'FALSE' THEN LEAVE proc; END IF;
+--
+-- START UPDATING QUERIES
+--
+
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648965068000701761');
 DELETE FROM `creature_loot_template` WHERE `Entry` = 11583;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES
@@ -293,3 +309,13 @@ INSERT INTO `reference_loot_template`(`Entry`, `Item`, `Reference`, `Chance`, `Q
 (30551, 19382, 0,  0.0, 0, 1, 1, 1,1),
 (30330, 30171, 30171, 100.0, 0, 1, 0, 1, 1),
 (30105, 30044, 30044, 100.0, 0, 1, 0, 1 ,1);
+
+--
+-- END UPDATING QUERIES
+--
+UPDATE version_db_world SET date = '2022_04_07_07' WHERE sql_rev = '1648965068000701761';
+COMMIT;
+END //
+DELIMITER ;
+CALL updateDb();
+DROP PROCEDURE IF EXISTS `updateDb`;
