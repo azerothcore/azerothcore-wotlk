a706ce6036d73e235d0e071a5747533390fc5239
Kosiak <v.k.kosiak@gmail.com>
2022-04-10 01:56:34 +0100

    different approach
    Nat Pagle was using previous `gossip menu` MenuID. So to keep Nat's gossip in order I needed new `gossip_menu`  MenuID. I hope it's right approach with creating MenuID in that table from first available.

--- data/sql/updates/pending_db_world/rev_1649549626279664100.sql
+++ data/sql/updates/pending_db_world/rev_1649549626279664100.sql
@@ -1,6 +1,11 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649549626279664100');
 
 -- Gryan Stoutmantle gossip
-UPDATE `gossip_menu` SET `TextID` = 50020 WHERE `MenuID` = 57024;
--- Farmer_Furlbrow gossip
-UPDATE `gossip_menu` SET `TextID` = 50019 WHERE `MenuID` = 57023;
+DELETE FROM `gossip_menu` WHERE `MenuID` = 61029;
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES (61029, 50020);
+UPDATE `creature_template` SET `gossip_menu_id` = 61029 WHERE `entry` = 234;
+
+-- Farmer_Furlbrow
+DELETE FROM `gossip_menu` WHERE `MenuID` = 61030;
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES (61030, 50019);
+UPDATE `creature_template` SET `gossip_menu_id` = 61030 WHERE `entry` = 237;
