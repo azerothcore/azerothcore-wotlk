15be81bc922db55ab4e78006ffca8740e2a228a7
Kosiak <v.k.kosiak@gmail.com>
2022-04-10 01:14:56 +0100

    fix(DB/gossip_menu): wrong TextId for NPC's

---
+++ data/sql/updates/pending_db_world/rev_1649549626279664100.sql
@@ -0,0 +1,6 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649549626279664100');
+
+-- Gryan Stoutmantle gossip
+UPDATE `gossip_menu` SET `TextID` = 50020 WHERE `MenuID` = 57024;
+-- Farmer_Furlbrow gossip
+UPDATE `gossip_menu` SET `TextID` = 50019 WHERE `MenuID` = 57023;
