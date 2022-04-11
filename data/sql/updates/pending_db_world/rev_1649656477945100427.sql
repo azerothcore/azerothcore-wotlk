01396b74c9600e885ee3e0490b8e12eb147de2cd
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-04-11 01:56:20 -0400

    fix(DB/Locale): Creature: Nayura
    Source TC: https://github.com/TrinityCore/TrinityCore/issues/24972

---
+++ data/sql/updates/pending_db_world/rev_1649656477945100427.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649656477945100427');
+
+DELETE FROM `npc_text_locale` WHERE `ID`=13321;
+INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`, `Text0_1`) VALUES
+(13321,"frFR","","Depuis ce quai, la Bravoure fait l'aller-retour entre Hurlevent et Auberdine."),
+(13321,"esES","","Desde este muelle, El Valentía hace el viaje de ida y vuelta entre Ventormenta y Auberdine."),
+(13321,"esMX","","Desde este muelle, El Valentía hace el viaje de ida y vuelta entre Ventormenta y Auberdine."),
+(13321,"zhCN","","从这个码头，勇敢者号在暴风城和奥伯丁之间往返"),
+(13321,"deDE","","Von diesem Dock aus fährt die Bravado zwischen Sturmwind und Auberdine hin und her.");
