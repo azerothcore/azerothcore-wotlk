54d1ed0e6378539a980460644766b45bc7828125
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-11 11:17:18 +0000

    chore(DB): import pending files
    Referenced commit(s): 1ee058dfd817d0f65d738cc6ed5b1382e9a0a593

--- data/sql/updates/pending_db_world/rev_1649370784577886400.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649370784577886400');
-
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 23414 AND `spell_effect` IN (-1856, -1857, -26889);
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
-(23414, -1856, 2, 'Nefarian Rogue class call - Vanish'),
-(23414, -1857, 2, 'Nefarian Rogue class call - Vanish'),
-(23414, -26889, 2, 'Nefarian Rogue class call - Vanish');
