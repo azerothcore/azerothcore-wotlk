192db3f7833631d9298fade0c1a1e2ec8a98c8c2
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-04-11 13:19:16 -0600

    fix(DB/item_template_locale): Spanish key translation

---
+++ data/sql/updates/pending_db_world/rev_1649704204212585721.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649704204212585721');
+
+DELETE FROM `item_template_locale` WHERE ID=13873 AND locale IN ('esES','esMX');
+INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES
+(13873, 'esES', 'Llave de la Sala de visión', '', '0'),
+(13873, 'esMX', 'Llave de la Sala de visión', '', '0');
+
