d40398c62e4a0356e598b64646c39fed865ba95e
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-04-11 13:57:14 -0600

    Create rev_1649705878401365511.sql

---
+++ data/sql/updates/pending_db_world/rev_1649705878401365511.sql
@@ -0,0 +1,9 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649705878401365511');
+
+DELETE FROM `creature_template_locale` WHERE `entry` IN (30652,30653);
+INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES
+(30652, 'esES', 'Tótem de cólera II', '', '18019'),
+(30652, 'esMX', 'Tótem de cólera II', '', '18019'),
+(30653, 'esES', 'Tótem de cólera III', '', '18019'),
+(30653, 'esMX', 'Tótem de cólera III', '', '18019');
+
