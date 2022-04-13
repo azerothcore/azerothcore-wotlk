2f79e543fe0c73fd70216d7d4956a9922721f064
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-04-13 11:12:07 -0400

    Update rev_1649856189895360036.sql

--- data/sql/updates/pending_db_world/rev_1649856189895360036.sql
+++ data/sql/updates/pending_db_world/rev_1649856189895360036.sql
@@ -1,4 +1,3 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649856189895360036');
 
-SET @ID := 183;
-UPDATE `quest_template` SET `LogDescription` = 'Talin Keeneye would like you to kill 12 Small Crag Boars.'  WHERE `ID` = @ID;
+UPDATE `quest_template` SET `LogDescription` = 'Talin Keeneye would like you to kill 12 Small Crag Boars.'  WHERE `ID` = 183;
