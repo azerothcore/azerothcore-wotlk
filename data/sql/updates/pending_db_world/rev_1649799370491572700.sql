4ac565699fcb7b88fcc15db97ec9f14a0894f331
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-13 11:57:04 +0000

    chore(DB): import pending files
    Referenced commit(s): 6a931b6be207681f834b14370bec07a35a23ffa7

--- data/sql/updates/pending_db_world/rev_1649799370491572700.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649799370491572700');
-
-DELETE FROM `areatrigger_scripts` WHERE `entry` = 3847;
-INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
-(3847, 'at_orb_of_command');
