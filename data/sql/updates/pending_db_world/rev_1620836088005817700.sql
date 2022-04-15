168fdbb81e46075c6a684c3bcb7baa3c8b4cbe34
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-15 11:19:02 +0000

    chore(DB): import pending files
    Referenced commit(s): 17e143763aa8c5d9a3d32a71acbf163ff01c5b86

--- data/sql/updates/pending_db_world/rev_1620836088005817700.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1620836088005817700');
-
-DELETE FROM `gossip_menu` WHERE (`MenuID` = 4042) AND `TextID` IN (4917, 4918);
-INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
-(4042, 4917),
-(4042, 4918);
--- data/sql/updates/pending_db_world/rev_1621544089475974100.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1621544089475974100');
-
-DELETE FROM `gossip_menu` WHERE (`MenuID` = 9676) AND (`TextID` IN (13287));
-INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES
-(9676, 13287);
-
-DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 9676) AND (`OptionID` IN (0));
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
-(9676, 0, 0, 'I''m ready to battle the dreadlord, sire.', 28508, 1, 1, 0, 0, 0, 0, '', 0, 0);
