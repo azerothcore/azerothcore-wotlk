71305f8775b23d63f05086a6a9399ce58e9e7497
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-04-15 14:34:13 +0430

    .

--- src/tools/vmap4_extractor/adtfile.h
+++ src/tools/vmap4_extractor/adtfile.h
@@ -50,33 +50,6 @@ namespace ADT
 }
 #pragma pack(pop)
 
-namespace ADT
-{
-    struct MDDF
-    {
-        uint32 Id;
-        uint32 UniqueId;
-        Vec3D Position;
-        Vec3D Rotation;
-        uint16 Scale;
-        uint16 Flags;
-    };
-
-    struct MODF
-    {
-        uint32 Id;
-        uint32 UniqueId;
-        Vec3D Position;
-        Vec3D Rotation;
-        AaBox3D Bounds;
-        uint16 Flags;
-        uint16 DoodadSet;   // can be larger than number of doodad sets in WMO
-        uint16 NameSet;
-        uint16 Scale;
-    };
-}
-#pragma pack(pop)
-
 class ADTFile
 {
 private:
