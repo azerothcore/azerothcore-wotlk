43cf7754414bd70e6a40e383d5d0de43e4813087
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-16 15:02:23 +0000

    chore(DB): import pending files
    Referenced commit(s): db360f0cfb5dde143ce0226e41399ea0e19daf43

--- data/sql/updates/pending_db_world/rev_1649944595674185734.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649944595674185734');
-
--- corrects hostil to hostile
-UPDATE `command` SET `name`='debug hostile' WHERE  `name`='debug hostil';
