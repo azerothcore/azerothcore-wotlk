8a5da97aa2b3861d9367dfa12c96f00f5b92d49a
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-04-16 14:43:27 +0200

    Update rev_1650022301338957500.sql

--- data/sql/updates/pending_db_world/rev_1650022301338957500.sql
+++ data/sql/updates/pending_db_world/rev_1650022301338957500.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650022301338957500');
 
-DELETE FROM `player_factionchange_quest` WHERE `alliance_id` = 7065 AND `horde_id` = 7064;
-INSERT INTO `player_factionchange_quest` (`alliance_id`, `horde_id`) VALUES
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id` = 7065 AND `horde_id` = 7064;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
 (7065, 7064);
