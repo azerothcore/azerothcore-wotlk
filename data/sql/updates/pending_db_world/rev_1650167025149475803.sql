b48fee69559fcaa15ec2e73d3aba5cda50161059
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-04-17 02:54:43 -0400

    Update rev_1650167025149475803.sql

--- data/sql/updates/pending_db_world/rev_1650167025149475803.sql
+++ data/sql/updates/pending_db_world/rev_1650167025149475803.sql
@@ -8,4 +8,12 @@ UPDATE `quest_template_locale` SET `Title` = 'El veneno de la Tejemadera' WHERE 
 UPDATE `quest_template_locale` SET `Objectives` = 'Lleva 10 glándulas de veneno de Tejemadera a Gilshalan Caminaviento a Aldrassil.' WHERE `ID` = 916 AND `locale` = 'esMX';
 UPDATE `quest_template_locale` SET `Details` = 'He venido a Cañada Umbría para observar a las arañas tejemadera de la Gruta Narácnida. Son parientes de unas arañas mucho más pequeñas; creo que el Árbol del Mundo ha tenido un profundo efecto en ellas y quiero estudiar unos especímenes para confirmarlo.$B$BPara empezar, necesito su veneno. Ve a la Gruta Narácnida, que está al norte de aquí, y tráeme glándulas de veneno; así podré compararlo con el veneno de sus parientes más pequeñas.' WHERE `ID` = 916 AND `locale` = 'esMX';
 UPDATE `quest_template_locale` SET `CompletedText` = 'Vuelve con el vigilante Bel\'dugur al Apothecarium en Entrañas.' WHERE `ID` = 1013 AND `locale` IN ('esES','esMX');
-
+UPDATE `quest_template_locale` SET `CompletedText` = 'Vuelve con Bashana Tótem de Runa en Elder Rise en Cima del Trueno.' WHERE `ID` = 6561 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Has limpiado Atiesh' WHERE `ID` = 9257 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Has limpiado Atiesh' WHERE `ID` = 9269 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Has limpiado Atiesh' WHERE `ID` = 9270 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Has limpiado Atiesh' WHERE `ID` = 9271 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Búhicos Cubrebosque inoculados' WHERE `ID` = 9303 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Devuelve la silitista' WHERE `ID` = 9419 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `ObjectiveText1` = 'Entrega la silitista' WHERE `ID` = 9422 AND `locale` IN ('esES','esMX');
+UPDATE `quest_template_locale` SET `Objectives` = 'El alto señor Darion Mograine quiere que utilices el Filo de las Sombras para matar a 1000 esbirros del Rey Exánime en la Ciudadela de la Corona de Hielo. Las almas se pueden obtener en el modo de 10 ó 25 jugadores.' WHERE `ID` = 24547 AND `locale` IN ('esES','esMX');
