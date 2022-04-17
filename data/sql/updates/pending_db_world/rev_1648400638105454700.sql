49978883ce335c90b345a35e0ca6fc4a70e95cff
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-17 03:15:37 +0000

    chore(DB): import pending files
    Referenced commit(s): e4df8f494c4751c0f39d254788c23e55e634956c

--- data/sql/updates/pending_db_world/rev_1648400638105454700.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1648400638105454700');
-
-DELETE FROM `spell_proc_event` WHERE `entry`=71756;
-INSERT INTO `spell_proc_event` (`entry`, `procEx`, `procPhase`) VALUES
-(71756,1027,2);
