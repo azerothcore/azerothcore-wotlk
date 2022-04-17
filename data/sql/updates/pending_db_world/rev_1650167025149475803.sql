dc34e44e443f6e2139b60031bd746e345bb90d95
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-04-16 23:48:44 -0400

    Create rev_1650167025149475803.sql

---
+++ data/sql/updates/pending_db_world/rev_1650167025149475803.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650167025149475803');
+
+UPDATE `quest_template_locale` SET `Objectives` = 'Encuentra y mata al Coleccionista y vuelve junto al alguacil Dughan con la sortija del coleccionista.' WHERE `ID` = 147;
+UPDATE `quest_template_locale` SET ` CompletedText` = 'Vuelve con: Alguacil Dughan. Zona: Aserradero de la Vega del Este, Bosque de Elwynn.' WHERE `ID` = 147;
