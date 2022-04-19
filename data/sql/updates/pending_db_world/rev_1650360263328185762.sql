64bd3c7d23ff84787643abdf9eb9cf0edd237561
Lynethris <100411212+Lynethris@users.noreply.github.com>
2022-04-19 11:33:11 +0200

    fix code style

--- data/sql/updates/pending_db_world/rev_1650360263328185762.sql
+++ data/sql/updates/pending_db_world/rev_1650360263328185762.sql
@@ -1,3 +1,3 @@
-INSERT INTO version_db_world (sql_rev) VALUES ('1650360263328185762');
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650360263328185762');
 
-DELETE FROM gameobject WHERE guid = 28668 AND id = 180323;
+DELETE FROM `gameobject` WHERE `guid` = 28668 AND `id` = 180323;
