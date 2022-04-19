61de612641fdf0639b447c01a3e1a677ae5685a1
AzerothCoreBot <azerothcorebot@gmail.com>
2022-04-19 21:45:04 +0000

    chore(DB): import pending files
    Referenced commit(s): 7ecd73867419c807c9869ebeb73c7ca4e39cd1c8

--- data/sql/updates/pending_db_world/rev_1644531208243387100.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1644531208243387100');
-
-DELETE FROM `command` WHERE `name`='reload mail_server_template';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('reload mail_server_template', 3, 'Syntax: .reload mail_server_template\nReload server_mail_template table.');
