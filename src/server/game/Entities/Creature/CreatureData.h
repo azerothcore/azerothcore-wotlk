0016c7a2c8df095256f65594569c8a4a571a095c
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-04-19 23:42:15 +0200

    Update CreatureData.h

--- src/server/game/Entities/Creature/CreatureData.h
+++ src/server/game/Entities/Creature/CreatureData.h
@@ -70,7 +70,7 @@ enum CreatureFlagsExtra : uint32
     CREATURE_FLAG_EXTRA_NO_PLAYER_DAMAGE_REQ            = 0x00200000,   // creature does not need to take player damage for kill credit
     CREATURE_FLAG_EXTRA_AVOID_AOE                       = 0x00400000,   // pussywizard: ignored by aoe attacks (for icc blood prince council npc - Dark Nucleus)
     CREATURE_FLAG_EXTRA_NO_DODGE                        = 0x00800000,   // xinef: target cannot dodge
-    CREATURE_FLAG_EXTRA_MODULE.                         = 0x01000000,
+    CREATURE_FLAG_EXTRA_MODULE                          = 0x01000000,
     CREATURE_FLAG_EXTRA_UNUSED_26                       = 0x02000000,
     CREATURE_FLAG_EXTRA_UNUSED_27                       = 0x04000000,
     CREATURE_FLAG_EXTRA_UNUSED_28                       = 0x08000000,
