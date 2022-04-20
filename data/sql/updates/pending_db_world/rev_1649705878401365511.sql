cf7c99485bd7378a0f4ec2776f2746a6ebe4a547
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-04-20 09:38:03 -0600

    upd
    Co-authored-by: Julio A. Leyva Osorio <tony931023@gmail.com>

--- data/sql/updates/pending_db_world/rev_1649705878401365511.sql
+++ data/sql/updates/pending_db_world/rev_1649705878401365511.sql
@@ -1,6 +1,6 @@
 INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649705878401365511');
 
-DELETE FROM `creature_template_locale` WHERE `entry` IN (30652,30653);
+DELETE FROM `creature_template_locale` WHERE `entry` IN (30652,30653) AND locale IN ('esES','esMX');
 INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES
 (30652, 'esES', 'Tótem de cólera II', '', 18019),
 (30652, 'esMX', 'Tótem de cólera II', '', 18019),
