343732ee0beef702181009e26c41722b87f9cbe2
temperrr <temperrr@users.noreply.github.com>
2022-04-20 14:10:31 +0200

    fix(Script/BWL): Fix initial shadowflame timers (#11443)

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_firemaw.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_firemaw.cpp
@@ -46,7 +46,7 @@ public:
         {
             BossAI::EnterCombat(who);
 
-            events.ScheduleEvent(EVENT_SHADOWFLAME, 10000, 20000);
+            events.ScheduleEvent(EVENT_SHADOWFLAME, 18000);
             events.ScheduleEvent(EVENT_WINGBUFFET, 30000);
             events.ScheduleEvent(EVENT_FLAMEBUFFET, 5000);
         }
@@ -67,7 +67,7 @@ public:
                 {
                     case EVENT_SHADOWFLAME:
                         DoCastVictim(SPELL_SHADOWFLAME);
-                        events.ScheduleEvent(EVENT_SHADOWFLAME, 10000, 20000);
+                        events.ScheduleEvent(EVENT_SHADOWFLAME, urand(15000, 25000));
                         break;
                     case EVENT_WINGBUFFET:
                         DoCastVictim(SPELL_WINGBUFFET);
