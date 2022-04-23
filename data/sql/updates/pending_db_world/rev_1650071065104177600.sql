8c5910d0c06f0cdf4872202056d3bdbf87ce297f
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-04-23 08:27:33 -0600

    fix(DB/Item): Wand of Allistarj locale esES esMX (#11412)
    * Create rev_1650071065104177600.sql
    
    * update
    
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>
    
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1650071065104177600.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650071065104177600');
+
+DELETE FROM `item_template_locale` WHERE `ID`=13065 AND `locale` IN ('esES','esMX');
+INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES
+(13065, 'esES', 'Varita de Allistarj', '', 0),
+(13065, 'esMX', 'Varita de Allistarj', '', 0);
+
