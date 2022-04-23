c20e2aca44d758c2420f93663dd3638903ee4fa0
Meramaf <76571216+Meramaf@users.noreply.github.com>
2022-04-23 08:47:34 +0200

    Update rev_1650656505854517188.sql

--- data/sql/updates/pending_db_world/rev_1650656505854517188.sql
+++ data/sql/updates/pending_db_world/rev_1650656505854517188.sql
@@ -2,6 +2,16 @@ INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650656505854517188');
 
 DELETE FROM `spell_proc_event` WHERE `entry` = '71602';
 DELETE FROM `spell_proc_event` WHERE `entry` = '75465';
+DELETE FROM `spell_proc_event` WHERE `entry` = '71845';
+DELETE FROM `spell_proc_event` WHERE `entry` = '71846';
+DELETE FROM `spell_proc_event` WHERE `entry` = '72419';
+DELETE FROM `spell_proc_event` WHERE `entry` = '75474';
 
-UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 54572);
 UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 50353);
+UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 50348);
+UPDATE `item_template` SET `spellppmRate_1` = 2 WHERE (`entry` = 49992);
+UPDATE `item_template` SET `spellppmRate_1` = 2 WHERE (`entry` = 50648);
+UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 50400);
+UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 50399);
+UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 54572);
+UPDATE `item_template` SET `spellppmRate_1` = 10 WHERE (`entry` = 54588);
