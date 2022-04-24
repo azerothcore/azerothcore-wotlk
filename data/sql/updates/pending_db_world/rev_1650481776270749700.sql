3153ff10ea1142f59c9ee15db5b41a36964aacb1
Lynethris <100411212+Lynethris@users.noreply.github.com>
2022-04-24 07:53:32 +0200

    fix DB

--- data/sql/updates/pending_db_world/rev_1650481776270749700.sql
+++ data/sql/updates/pending_db_world/rev_1650481776270749700.sql
@@ -6,5 +6,5 @@ UPDATE `creature_template_movement` SET `Flight` = 0 WHERE `CreatureId` = 14517;
 -- Update Emotes
 DELETE FROM `creature_text` WHERE `CreatureID` = 14517 AND `GroupID` IN (3,4);
 INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`Sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
-(14517,3,0,"%s emits a deafening shriek!",16,0,100,0,0,0,10370,0,"High Priestess Jeklik"),
-(14517,4,0,"%s begins to cast a Great Heal!",16,0,100,0,0,0,10494,0,"High Priestess Jeklik");
+(14517,3,0,"%s emits a deafening shriek!",16,0,100,0,0,0,10370,0,"High Priestess Jeklik - EMOTE_SUMMON_BATS"),
+(14517,4,0,"%s begins to cast a Great Heal!",16,0,100,0,0,0,10494,0,"High Priestess Jeklik - - EMOTE_GREAT_HEAL");
