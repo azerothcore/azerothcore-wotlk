a6252d34470503c6617c8329e52b906c387b63a0
Kosiak <v.k.kosiak@gmail.com>
2022-04-28 23:22:22 +0100

    Update rev_1647050684059087200.sql

--- data/sql/updates/pending_db_world/rev_1647050684059087200.sql
+++ data/sql/updates/pending_db_world/rev_1647050684059087200.sql
@@ -1,6 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1647050684059087200');
-
--- Add condition to spawn NPC only when there is no other NPC nearby
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 160445 AND `ConditionTypeOrReference` = 29;
+-- Add condition to spawn NPC only when there is no other NPC nearbyDELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 160445 AND `ConditionTypeOrReference` = 29;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (22, 1, 160445, 1, 0, 29, 1, 9136, 100, 0, 1, 0, 0, '', 'Run action if no npcs in range');
