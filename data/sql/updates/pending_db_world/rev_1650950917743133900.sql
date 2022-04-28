97ea5906e37c166bbabd91284dea8988a4f1f2ad
Nefertumm <nefertum.dev@protonmail.com>
2022-04-28 17:07:15 -0300

    update sql to newest

--- data/sql/updates/pending_db_world/rev_1650950917743133900.sql
+++ data/sql/updates/pending_db_world/rev_1650950917743133900.sql
@@ -1,5 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650950917743133900');
-
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_vem_knockback', 'spell_vem_vengeance');
 DELETE FROM `spell_script_names` WHERE `spell_id` = 18670; -- Delete the current script to prevent stacking.
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
