80959ca84e716c7c68232a5aeb5a429a6937fd0e
天鹿 <pkl_long@163.com>
2022-04-29 19:19:51 +0800

    Update and rename rev_1651226361653358110.sql to rev_1651231168158458211.sql

--- data/sql/updates/pending_db_world/rev_1651231168158458211.sql
+++ data/sql/updates/pending_db_world/rev_1651231168158458211.sql
@@ -1,3 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1651226361653358110');
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1651231168158458211');
 
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gen_proc_once_per_cast';
