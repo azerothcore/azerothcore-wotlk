97cc344f8154a4f6558e91c7efa755ee069cf4ac
temperrr <temperrr@users.noreply.github.com>
2022-05-01 11:21:26 +0200

    Update rev_1650793025980507979.sql

--- data/sql/updates/pending_db_world/rev_1650793025980507979.sql
+++ data/sql/updates/pending_db_world/rev_1650793025980507979.sql
@@ -2,4 +2,3 @@ UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 13086);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 22670);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 31918);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 37234);
-
