17529946816a9e232beadb92274a233fac1b0222
temperrr <temperrr@users.noreply.github.com>
2022-05-01 11:21:12 +0200

    Update rev_1650793025980507979.sql

--- data/sql/updates/pending_db_world/rev_1650793025980507979.sql
+++ data/sql/updates/pending_db_world/rev_1650793025980507979.sql
@@ -1,5 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1650793025980507979');
-
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 13086);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 22670);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 31918);
