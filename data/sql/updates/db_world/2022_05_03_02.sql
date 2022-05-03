d1b690e3fa89897b6eada5fc8ec11354227cf2d5
AzerothCoreBot <azerothcorebot@gmail.com>
2022-05-03 10:28:43 +0000

    chore(DB): import pending files
    Referenced commit(s): e7531fa2a99df1e2a140b8200d6a4494d38b715a

--- data/sql/updates/db_world/2022_05_03_02.sql
+++ data/sql/updates/db_world/2022_05_03_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_05_03_01 -> 2022_05_03_02
 --
 DELETE FROM `areatrigger_scripts` WHERE `entry` = 4052 AND `ScriptName` = 'at_battleguard_sartura';
 INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
