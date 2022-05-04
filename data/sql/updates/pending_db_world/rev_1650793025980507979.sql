6f46e62fc2b6c4be242e9acd2dbb5b35ed3aeb28
temperrr <temperrr@users.noreply.github.com>
2022-05-04 02:28:09 +0200

    Fix(DB/Creature): Set correct faction flag for Aggi Rumplestomp. (#11511)

---
+++ data/sql/updates/pending_db_world/rev_1650793025980507979.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 13086);
+UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 22670);
+UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 31918);
+UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 37234);
