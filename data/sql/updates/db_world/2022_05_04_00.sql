283ce18bb7b961264eae983a401aaacb89725273
AzerothCoreBot <azerothcorebot@gmail.com>
2022-05-04 00:30:09 +0000

    chore(DB): import pending files
    Referenced commit(s): 6f46e62fc2b6c4be242e9acd2dbb5b35ed3aeb28

--- data/sql/updates/db_world/2022_05_04_00.sql
+++ data/sql/updates/db_world/2022_05_04_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_05_03_02 -> 2022_05_04_00
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 13086);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 22670);
 UPDATE `creature_template` SET `faction` = 1216 WHERE (`entry` = 31918);
