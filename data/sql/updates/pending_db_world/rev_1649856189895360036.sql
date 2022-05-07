20dbe61044f4388593b8c7554c2c802bb02e07a1
Julio A. Leyva Osorio <tony931023@gmail.com>
2022-05-07 08:04:00 -0400

    Update rev_1649856189895360036.sql

--- data/sql/updates/pending_db_world/rev_1649856189895360036.sql
+++ data/sql/updates/pending_db_world/rev_1649856189895360036.sql
@@ -1,4 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649856189895360036');
 
 UPDATE `quest_template` SET `LogDescription` = 'Talin Keeneye would like you to kill 12 Small Crag Boars.'  WHERE `ID` = 183;
 UPDATE `quest_template` SET `QuestCompletionLog` = 'Return to Orgnil Soulscar in Durotar.'  WHERE `ID` = 806;
