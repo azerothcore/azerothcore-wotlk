c38b6762f44eced84ae5e6ac9a2a76ecdbc65128
Christopher Galbraith <39371221+ChrisCGalbraith@users.noreply.github.com>
2022-05-08 09:45:51 +0100

    fix(Scripts/MC): Set exit teleport location to in front of Lothos Riftwalker (#11635)
    * Set molten core exit teleport location to in front of Lothos Riftwalker
    
    * fixed layout of SQL script to better conform to standards
    
    Co-authored-by: ChrisCGalbraith <chriscgalbraith@gmai.com>

---
+++ data/sql/updates/pending_db_world/rev_1651742335531289900.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `areatrigger_teleport` SET `target_map` = 0, `target_position_x` = -7510.59, `target_position_y` = -1041.45, `target_position_z` = 180.911, `target_orientation` = 3.8666 WHERE ID = 2890;
