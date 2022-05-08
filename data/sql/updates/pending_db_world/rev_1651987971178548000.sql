589d7c8b4efd72d3b19002c4d3760b3381efbad4
Degen <61268368+aradep@users.noreply.github.com>
2022-05-08 13:14:31 -0300

    fix(DB/Creature): Korfax missing gossip (#11676)

---
+++ data/sql/updates/pending_db_world/rev_1651987971178548000.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `gossip_menu` WHERE `MenuID` IN (7106,7107);
+INSERT INTO `gossip_menu` VALUES (7106, 8363), (7107, 8364);
+
+UPDATE `gossip_menu_option` SET `ActionMenuID`=7099 WHERE  `MenuID`=7107 AND `OptionID`=0;
