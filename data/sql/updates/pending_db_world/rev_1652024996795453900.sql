75d04a4407ff830cce5698607b05b6fbd551390c
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-05-08 18:13:13 +0200

    fix(DB/Spells): 54476 triggerspell
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/11500

---
+++ data/sql/updates/pending_db_world/rev_1652024996795453900.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_dbc` WHERE `id` = 1206;
+INSERT INTO `spell_dbc` (`Id`, `Targets`, `CastingTimeIndex`, `ProcChance`, `RangeIndex`, `EquippedItemClass`, `EquippedItemSubclass`, `Effect_1`, `ImplicitTargetA_1`, `EffectTriggerSpell_1`, `Name_Lang_enUS`, `EffectBonusMultiplier_1`, `EffectBonusMultiplier_2`, `EffectBonusMultiplier_3`) VALUES
+(1206, 256, 1, 101, 1, -1, -1, 3, 1, 11, 'Dummy Proc', 1, 1, 1);
