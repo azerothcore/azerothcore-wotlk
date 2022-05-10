78577b81f53502bbd5374857821b6b12473a593c
mpfans <mpfans@vip.qq.com>
2022-05-10 13:18:12 +0800

    fix(DB/quest): quest should can see NPC
    after update 2391f454c74d0cfb51235ee3ef288ad36d2c4e1b,`Forging an Alliance`(quest id 12924 ) can't see quest target NPC

---
+++ data/sql/updates/pending_db_world/fixquest.sql
@@ -0,0 +1,2 @@
+-- fixed quest 12924 spell area
+UPDATE `spell_area` SET `quest_start` = 12956 WHERE `spell` = 55858 AND `area` = 4438 AND `quest_start` = 12924 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
