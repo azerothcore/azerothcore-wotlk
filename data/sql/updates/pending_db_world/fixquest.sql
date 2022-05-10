89894a3e1c40ca11a818a379833b58819837e233
mpfans <mpfans@vip.qq.com>
2022-05-10 16:35:05 +0800

    Update fixquest.sql

--- data/sql/updates/pending_db_world/fixquest.sql
+++ data/sql/updates/pending_db_world/fixquest.sql
@@ -1,2 +1,3 @@
 -- fixed quest 12924 spell area
 UPDATE `spell_area` SET `quest_start` = 12956 WHERE `spell` = 55858 AND `area` IN (4437,4438,4440,4455,4495) AND `quest_start` = 12924;
+UPDATE `spell_area` SET `quest_start` = 12958 WHERE `spell` = 56780 AND `area` = 4439 AND `quest_start` = 12956;
