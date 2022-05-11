5f9a251275017d236b521e05ddf331b1528c86ab
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-05-11 08:48:32 +0430

    .

--- src/server/game/Grids/ObjectGridLoader.cpp
+++ src/server/game/Grids/ObjectGridLoader.cpp
@@ -102,49 +102,21 @@ void LoadHelper(CellGuidSet const& guid_set, CellCoord &cell, GridRefMgr<T> &m, 
 {
     for (CellGuidSet::const_iterator i_guid = guid_set.begin(); i_guid != guid_set.end(); ++i_guid)
     {
+        // Don't spawn at all if there's a respawn timer
+        ObjectGuid::LowType guid = *i_guid;
+        if (!map->ShouldBeSpawnedOnGridLoad<T>(guid))
+            continue;
+
         T* obj = new T;
-
-        // Don't spawn at all if there's a respawn time
-        if ((obj->GetTypeId() == TYPEID_UNIT && !map->GetCreatureRespawnTime(*i_guid)) || (obj->GetTypeId() == TYPEID_GAMEOBJECT && !map->GetGORespawnTime(*i_guid)))
+        //TC_LOG_INFO("misc", "DEBUG: LoadHelper from table: %s for (guid: %u) Loading", table, guid);
+        if (!obj->LoadFromDB(guid, map, false, false))
         {
-            ObjectGuid::LowType guid = *i_guid;
-
-            if (obj->GetTypeId() == TYPEID_UNIT)
-            {
-                CreatureData const* cdata = sObjectMgr->GetCreatureData(guid);
-                        ASSERT(cdata, "Tried to load creature with spawnId %u, but no such creature exists.", guid);
-                SpawnGroupTemplateData const* const group = cdata->spawnGroupData;
-                // If creature in manual spawn group, don't spawn here, unless group is already active.
-                if ((group->flags & SPAWNGROUP_FLAG_MANUAL_SPAWN) && !group->isActive)
-                    continue;
-
-                // If script is blocking spawn, don't spawn but queue for a re-check in a little bit
-                if (!(group->flags & SPAWNGROUP_FLAG_COMPATIBILITY_MODE) && !sScriptMgr->CanSpawn(guid, cdata->id, cdata, map))
-                {
-                    map->SaveRespawnTime(SPAWN_TYPE_CREATURE, guid, cdata->id, time(NULL) + urand(4,7), map->GetZoneId(cdata->spawnPoint), Trinity::ComputeGridCoord(cdata->spawnPoint.GetPositionX(), cdata->spawnPoint.GetPositionY()).GetId(), false);
-                    continue;
-                }
-            }
-            else if (obj->GetTypeId() == TYPEID_GAMEOBJECT)
-            {
-                // If gameobject in manual spawn group, don't spawn here, unless group is already active.
-                GameObjectData const* godata = sObjectMgr->GetGameObjectData(guid);
-                        ASSERT(godata, "Tried to load gameobject with spawnId %u, but no such object exists.", guid);
-                if ((godata->spawnGroupData->flags & SPAWNGROUP_FLAG_MANUAL_SPAWN) && !godata->spawnGroupData->isActive)
-                    continue;
-            }
-
-            if (!obj->LoadFromDB(guid, map, false, false))
-                delete obj;
-                continue;
-            }
-            AddObjectHelper(cell, m, count, map, obj);
-        }
-        else
             delete obj;
+            continue;
+        }
+        AddObjectHelper(cell, m, count, map, obj);
     }
 }
-
 
 template <>
 void LoadHelper(CellGuidSet const& guid_set, CellCoord& cell, GridRefMgr<GameObject>& m, uint32& count, Map* map)
@@ -152,7 +124,7 @@ void LoadHelper(CellGuidSet const& guid_set, CellCoord& cell, GridRefMgr<GameObj
     for (CellGuidSet::const_iterator i_guid = guid_set.begin(); i_guid != guid_set.end(); ++i_guid)
     {
         ObjectGuid::LowType guid = *i_guid;
-        GameObjectData const* data = sObjectMgr->GetGOData(guid);
+        GameObjectData const* data = sObjectMgr->GetGameObjectData(guid);
         GameObject* obj = data && sObjectMgr->IsGameObjectStaticTransport(data->id) ? new StaticTransport() : new GameObject();
 
         if (!obj->LoadFromDB(guid, map))
