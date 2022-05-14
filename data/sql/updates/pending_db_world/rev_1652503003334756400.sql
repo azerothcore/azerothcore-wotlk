7d738803d44efaaad6ada0dcefba413f566ca68a
Pagani Walter <paganiwalter@gmail.com>
2022-05-14 01:38:11 -0300

    (DB/AcoreString) Modifying message entry 474 475 476

---
+++ data/sql/updates/pending_db_world/rev_1652503003334756400.sql
@@ -0,0 +1,4 @@
+
+UPDATE `acore_string` SET `content_default`=' [rewarded]', `locale_esES`=' [recompensada]', `locale_esMX`=' [recompensada]' WHERE `entry`=474;
+UPDATE `acore_string` SET `content_default`=' [complete]', `locale_esES`=' [completada]', `locale_esMX`=' [completada]' WHERE `entry`=475;
+UPDATE `acore_string` SET `content_default`=' [active]', `locale_esES`=' [activa]', `locale_esMX`=' [activa]' WHERE `entry`=476;
