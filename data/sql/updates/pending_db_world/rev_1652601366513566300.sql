bb16c17043bdac07ede555ad3d7c8c90bdaab190
UltraNix <ultranix@gmail.com>
2022-05-15 09:56:36 +0200

    fix(DB/Spells): Fixed Shaman's Clearcasting proc.
    Fixes #10780

---
+++ data/sql/updates/pending_db_world/rev_1652601366513566300.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `spell_proc_event` SET `SpellFamilyName`=11, `SpellFamilyMask0`=0x981001C3, `SpellFamilyMask1`=0x00001400, `SpellFamilyMask2`=0x00000010, `procFlags`=0x00015550 WHERE `entry`=16246;
