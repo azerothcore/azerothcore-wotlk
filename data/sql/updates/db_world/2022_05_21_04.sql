f7674acb58001a72dccfa3d60f63cb3bd0daeed6
AzerothCoreBot <azerothcorebot@gmail.com>
2022-05-21 21:44:46 +0000

    chore(DB): import pending files
    Referenced commit(s): 90fccacad5de8202d4f11ffd0e861b37fb469785

--- data/sql/updates/db_world/2022_05_21_04.sql
+++ data/sql/updates/db_world/2022_05_21_04.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_05_21_03 -> 2022_05_21_04
 --
 ALTER TABLE `waypoint_data` CHANGE `orientation` `orientation` FLOAT DEFAULT NULL NULL;
 UPDATE `waypoint_data` SET `orientation`= NULL WHERE `orientation`= 0;
