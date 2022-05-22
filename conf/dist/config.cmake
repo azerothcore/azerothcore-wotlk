ee48e83ef364909bb12f2f3df44753dbdbf6ad3d
Winfidonarleyan <dowlandtop@yandex.com>
2022-05-22 10:17:47 +0700

    &&

--- conf/dist/config.cmake
+++ conf/dist/config.cmake
@@ -13,14 +13,18 @@
 
 set(SCRIPTS_AVAILABLE_OPTIONS none static dynamic minimal-static minimal-dynamic)
 set(MODULES_AVAILABLE_OPTIONS none static dynamic)
-set(BUILD_APPS_AVAILABLE_OPTIONS none all servers-only dbimport-only)
+set(BUILD_APPS_AVAILABLE_OPTIONS none all auth-only world-only)
+set(BUILD_TOOLS_AVAILABLE_OPTIONS none all db-only maps-only)
 
 set(SCRIPTS "static" CACHE STRING "Build core with scripts")
 set(MODULES "static" CACHE STRING "Build core with modules")
-set(APPS_BUILD "servers-only" CACHE STRING "Build list for applications")
+set(APPS_BUILD "all" CACHE STRING "Build list for applications")
+set(TOOLS_BUILD "none" CACHE STRING "Build list for tools")
+
 set_property(CACHE SCRIPTS PROPERTY STRINGS ${SCRIPTS_AVAILABLE_OPTIONS})
 set_property(CACHE MODULES PROPERTY STRINGS ${MODULES_AVAILABLE_OPTIONS})
 set_property(CACHE APPS_BUILD PROPERTY STRINGS ${BUILD_APPS_AVAILABLE_OPTIONS})
+set_property(CACHE TOOLS_BUILD PROPERTY STRINGS ${BUILD_TOOLS_AVAILABLE_OPTIONS})
 
 # Log a error when the value of the SCRIPTS variable isn't a valid option.
 if(SCRIPTS)
@@ -49,6 +53,15 @@ if(APPS_BUILD)
   endif()
 endif()
 
+# Log a fatal error when the value of the TOOLS_BUILD variable isn't a valid option.
+if(TOOLS_BUILD)
+  list(FIND BUILD_TOOLS_AVAILABLE_OPTIONS "${TOOLS_BUILD}" BUILD_TOOLS_INDEX)
+  if(${BUILD_TOOLS_INDEX} EQUAL -1)
+    message(FATAL_ERROR "The value (${TOOLS_BUILD}) of your TOOLS_BUILD variable is invalid! "
+                        "Allowed values are: ${BUILD_TOOLS_AVAILABLE_OPTIONS}. Set default")
+  endif()
+endif()
+
 # Build a list of all script modules when -DSCRIPT="custom" is selected
 GetScriptModuleList(SCRIPT_MODULE_LIST)
 foreach(SCRIPT_MODULE ${SCRIPT_MODULE_LIST})
@@ -73,8 +86,15 @@ foreach(APPLICATION_BUILD_NAME ${APPLICATIONS_BUILD_LIST})
   set_property(CACHE ${APPLICATION_BUILD_VARIABLE} PROPERTY STRINGS default enabled disabled)
 endforeach()
 
+# Build a list of all applications when -DBUILD_APPS="custom" is selected
+GetToolsList(TOOLS_BUILD_LIST)
+foreach(TOOL_BUILD_NAME ${TOOLS_BUILD_LIST})
+  ToolNameToVariable(${TOOL_BUILD_NAME} TOOL_BUILD_VARIABLE)
+  set(${TOOL_BUILD_VARIABLE} "default" CACHE STRING "Enable build the ${TOOL_BUILD_NAME} tool.")
+  set_property(CACHE ${TOOL_BUILD_VARIABLE} PROPERTY STRINGS default enabled disabled)
+endforeach()
+
 option(BUILD_TESTING       "Build unit tests"                                            0)
-option(TOOLS               "Build map/vmap/mmap extraction/assembler tools"              0)
 option(USE_SCRIPTPCH       "Use precompiled headers when compiling scripts"              1)
 option(USE_COREPCH         "Use precompiled headers when compiling servers"              1)
 option(WITH_WARNINGS       "Show all warnings during compile"                            0)
@@ -88,6 +108,7 @@ option(WITHOUT_METRICS     "Disable metrics reporting (i.e. InfluxDB and Grafana
 option(WITH_DETAILED_METRICS  "Enable detailed metrics reporting (i.e. time each session takes to update)" 0)
 
 CheckApplicationsBuildList()
+CheckToolsBuildList()
 IsDynamicLinkingRequired(WITH_DYNAMIC_LINKING_FORCED)
 IsDynamicLinkingModulesRequired(WITH_DYNAMIC_LINKING_FORCED)
 
--- src/cmake/macros/ConfigInstall.cmake
+++ src/cmake/macros/ConfigInstall.cmake
@@ -12,11 +12,39 @@
 
 #
 # Use it like:
-# CopyDefaultConfig(${APP_PROJECT_NAME} ${APPLICATION_NAME})
+# CopyApplicationConfig(${APP_PROJECT_NAME} ${APPLICATION_NAME})
 #
 
-function(CopyDefaultConfig projectName appName)
+function(CopyApplicationConfig projectName appName)
   GetPathToApplication(${appName} SOURCE_APP_PATH)
+
+  if(WIN32)
+    if("${CMAKE_MAKE_PROGRAM}" MATCHES "MSBuild")
+      add_custom_command(TARGET ${projectName}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/configs")
+      add_custom_command(TARGET ${projectName}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy "${SOURCE_APP_PATH}/${appName}.conf.dist" "${CMAKE_BINARY_DIR}/bin/$(ConfigurationName)/configs")
+    elseif(MINGW)
+      add_custom_command(TARGET ${servertype}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/bin/configs")
+      add_custom_command(TARGET ${servertype}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy "${SOURCE_APP_PATH}/${appName}.conf.dist ${CMAKE_BINARY_DIR}/bin/configs")
+    endif()
+  endif()
+
+  if(UNIX)
+    install(FILES "${SOURCE_APP_PATH}/${appName}.conf.dist" DESTINATION "${CONF_DIR}")
+  elseif(WIN32)
+    install(FILES "${SOURCE_APP_PATH}/${appName}.conf.dist" DESTINATION "${CMAKE_INSTALL_PREFIX}/configs")
+  endif()
+endfunction()
+
+function(CopyToolConfig projectName appName)
+  GetPathToTool(${appName} SOURCE_APP_PATH)
 
   if(WIN32)
     if("${CMAKE_MAKE_PROGRAM}" MATCHES "MSBuild")
