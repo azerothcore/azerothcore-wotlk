73438a547cfad0e05601524f7e00f7578ddc925d
Winfidonarleyan <dowlandtop@yandex.com>
2022-05-22 11:01:32 +0700

    1

--- data/sql/updates/pending_db_world/rev_1651641658185841600.sql
+++
@@ -1,3 +0,0 @@
---
-DELETE FROM `updates_include` WHERE `path` = '$/data/sql/updates/pending_db_world'
-INSERT INTO `updates_include` (`path`, `state`) VALUES ('$/data/sql/updates/pending_db_world', 'CUSTOM');
