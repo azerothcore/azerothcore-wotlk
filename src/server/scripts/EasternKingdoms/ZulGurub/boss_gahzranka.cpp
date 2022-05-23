2623df2888580e8905e01d2aad0ec47c9bf8949b
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-05-23 15:12:03 -0600

    Update boss_gahzranka.cpp
    Update INIT timers and CD timers for FrostBreat & MassiveGeyser

--- src/server/scripts/EasternKingdoms/ZulGurub/boss_gahzranka.cpp
+++ src/server/scripts/EasternKingdoms/ZulGurub/boss_gahzranka.cpp
@@ -62,8 +62,8 @@ public:
         void EnterCombat(Unit* /*who*/) override
         {
             _EnterCombat();
-            events.ScheduleEvent(EVENT_FROSTBREATH, 8000);
-            events.ScheduleEvent(EVENT_MASSIVEGEYSER, 25000);
+            events.ScheduleEvent(EVENT_FROSTBREATH, 10000);
+            events.ScheduleEvent(EVENT_MASSIVEGEYSER, 15000);
             events.ScheduleEvent(EVENT_SLAM, 17000);
         }
 
@@ -82,12 +82,12 @@ public:
                 switch (eventId)
                 {
                     case EVENT_FROSTBREATH:
-                        DoCastVictim(SPELL_FROSTBREATH, true);
-                        events.ScheduleEvent(EVENT_FROSTBREATH, urand(7000, 11000));
+                        DoCastVictim(SPELL_FROSTBREATH, false);
+                        events.ScheduleEvent(EVENT_FROSTBREATH, urand(10000, 22000));
                         break;
                     case EVENT_MASSIVEGEYSER:
-                        DoCastVictim(SPELL_MASSIVEGEYSER, true);
-                        events.ScheduleEvent(EVENT_MASSIVEGEYSER, urand(22000, 32000));
+                        DoCastVictim(SPELL_MASSIVEGEYSER, false);
+                        events.ScheduleEvent(EVENT_MASSIVEGEYSER, urand(12000, 22000));
                         break;
                     case EVENT_SLAM:
                         DoCastVictim(SPELL_SLAM, true);
