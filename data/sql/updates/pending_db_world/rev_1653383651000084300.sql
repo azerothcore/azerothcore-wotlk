c8bad93153b2a47d964c6849239b8fbc03e74a8c
avarishd <46330494+avarishd@users.noreply.github.com>
2022-05-24 14:34:43 +0300

    SQL Comment update

--- data/sql/updates/pending_db_world/rev_1653383651000084300.sql
+++ data/sql/updates/pending_db_world/rev_1653383651000084300.sql
@@ -1,2 +1,2 @@
--- (Hunter) Set Lock and Load PPM from DBC
+-- (Hunter) Set Lock and Load procPhase to 0, allowing Black Arrow procs
 UPDATE `spell_proc_event` SET `procPhase`=0 WHERE `entry` = -56342;
