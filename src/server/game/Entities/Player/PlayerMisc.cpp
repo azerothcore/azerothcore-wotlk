0fd324cf849e8651a95d577816c9230849fbd484
天鹿 <pkl_long@163.com>
2022-05-25 10:26:35 +0800

    Update PlayerMisc.cpp

--- src/server/game/Entities/Player/PlayerMisc.cpp
+++ src/server/game/Entities/Player/PlayerMisc.cpp
@@ -181,16 +181,6 @@ void Player::SendResetFailedNotify(uint32 mapid)
     GetSession()->SendPacket(&data);
 }
 
-void DeleteInstanceSavedData(uint32 instanceId)
-{
-    if (instanceId)
-    {
-        CharacterDatabasePreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DELETE_INSTANCE_SAVED_DATA);
-        stmt->SetData(0, instanceId);
-        CharacterDatabase.Execute(stmt);
-    }
-}
-
 /// Reset all solo instances and optionally send a message on success for each
 void Player::ResetInstances(ObjectGuid guid, uint8 method, bool isRaid)
 {
@@ -223,7 +213,7 @@ void Player::ResetInstances(ObjectGuid guid, uint8 method, bool isRaid)
                     p->SendResetInstanceFailed(0, instanceSave->GetMapId());
                 }
 
-                DeleteInstanceSavedData(instanceSave->GetInstanceId());
+                instanceSave->DeleteInstanceSavedData(instanceSave->GetInstanceId());
             }
             for (std::vector<InstanceSave*>::const_iterator itr = toUnbind.begin(); itr != toUnbind.end(); ++itr)
             {
@@ -258,7 +248,7 @@ void Player::ResetInstances(ObjectGuid guid, uint8 method, bool isRaid)
                     p->SendResetInstanceFailed(0, instanceSave->GetMapId());
                 }
 
-                DeleteInstanceSavedData(instanceSave->GetInstanceId());
+                instanceSave->DeleteInstanceSavedData(instanceSave->GetInstanceId());
             }
             for (std::vector<InstanceSave*>::const_iterator itr = toUnbind.begin(); itr != toUnbind.end(); ++itr)
                 sInstanceSaveMgr->UnbindAllFor(*itr);
@@ -287,7 +277,7 @@ void Player::ResetInstances(ObjectGuid guid, uint8 method, bool isRaid)
                     //else
                     //  p->SendResetInstanceFailed(0, instanceSave->GetMapId());
 
-                    DeleteInstanceSavedData(instanceSave->GetInstanceId());
+                    instanceSave->DeleteInstanceSavedData(instanceSave->GetInstanceId());
                 }
                 for (std::vector<InstanceSave*>::const_iterator itr = toUnbind.begin(); itr != toUnbind.end(); ++itr)
                     sInstanceSaveMgr->PlayerUnbindInstance(p->GetGUID(), (*itr)->GetMapId(), (*itr)->GetDifficulty(), true, p);
