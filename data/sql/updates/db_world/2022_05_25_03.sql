3772582e5c62dcb8f0ce2b471f97e78567f52601
AzerothCoreBot <azerothcorebot@gmail.com>
2022-05-25 11:52:59 +0000

    chore(DB): import pending files
    Referenced commit(s): 84ccf508efd01f418ad22dc50caefc02fe273a66

--- data/sql/updates/db_world/2022_05_25_03.sql
+++ data/sql/updates/db_world/2022_05_25_03.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_05_25_02 -> 2022_05_25_03
 -- update `Slaves of the Stormforged` quest Chinese translation
 UPDATE `quest_template_locale` SET `ObjectiveText1` = '解救机械侏儒俘虏' WHERE `ID` = 12957 AND `locale` = 'zhCN'; 
 
