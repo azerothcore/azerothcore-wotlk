652ca984385161e174a3f025e768315f39cdee2c
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-05-25 17:46:09 -0600

    Update rev_1653350504760491620.sql

--- data/sql/updates/pending_db_world/rev_1653350504760491620.sql
+++ data/sql/updates/pending_db_world/rev_1653350504760491620.sql
@@ -1,6 +1,6 @@
 
-UPDATE `broadcast_text` SET `MaleText`='Grats!' WHERE `ID`=10601;
+-- Change text and remove 10601 as the BroadcastTextId 'till sniff data
 DELETE FROM `creature_text` WHERE `CreatureID`= 11380 AND `GroupID`=0 AND `ID`=0;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
-(11380, 0, 0, 'Grats!', 14, 0, 100, 0, 0, 0, 10601, 3, 'mandokir SAY_GRATS_JINDO');
+(11380, 0, 0, 'Grats!', 14, 0, 100, 0, 0, 0, 0, 3, 'mandokir SAY_GRATS_JINDO');
 
