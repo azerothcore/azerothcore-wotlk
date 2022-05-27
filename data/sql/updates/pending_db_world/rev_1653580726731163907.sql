c3d1ef70bf305738bee8b192af0c1a46fece6260
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-05-26 23:00:37 -0600

    pew

--- data/sql/updates/pending_db_world/rev_1653580726731163907.sql
+++ data/sql/updates/pending_db_world/rev_1653580726731163907.sql
@@ -1,3 +1,3 @@
 
-UPDATE `creature_template` SET `ScriptName`='npc_chained_spirit' WHERE `entry`=15117;
+UPDATE `creature_template` SET `ScriptName`='npc_chained_spirit', `speed_run`=0.4 WHERE `entry`=15117;
 
