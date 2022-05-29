64a7e5e5acec59e3d1ffbeeaa62d2ada55a6fc6e
Maelthyr <100411212+Maelthyrr@users.noreply.github.com>
2022-05-29 09:40:49 +0200

    fix wander_distance

--- data/sql/updates/pending_db_world/rev_1652901134913931900.sql
+++ data/sql/updates/pending_db_world/rev_1652901134913931900.sql
@@ -1,2 +1,4 @@
 --
-UPDATE `creature_template` SET `MovementType` = 1 WHERE `entry` = 1992;
+DELETE FROM `creature` WHERE (`id1` = 1992);
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(47347, 1992, 0, 0, 1, 0, 0, 1, 1, 0, 10296.9, 870.227, 1335.91, 5.96477, 300, 7, 0, 137, 0, 1, 0, 0, 0, '', 0);
