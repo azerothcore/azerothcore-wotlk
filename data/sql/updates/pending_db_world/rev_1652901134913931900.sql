c33df5d48e648cd776a30b87eabf04b7b3896236
Maelthyr <100411212+Maelthyrr@users.noreply.github.com>
2022-06-01 23:09:06 +0200

    replace delete/insert sql method by update

--- data/sql/updates/pending_db_world/rev_1652901134913931900.sql
+++ data/sql/updates/pending_db_world/rev_1652901134913931900.sql
@@ -1,4 +1,2 @@
---
-DELETE FROM `creature` WHERE (`id1` = 1992);
-INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(47347, 1992, 0, 0, 1, 0, 0, 1, 1, 0, 10296.9, 870.227, 1335.91, 5.96477, 300, 7, 0, 137, 0, 1, 0, 0, 0, '', 0);
+-- Update movement for Tarindralla
+UPDATE `creature` SET `wander_distance` = 7, `MovementType` = 1 WHERE `guid` = 47347 AND `id1` = 1992;
