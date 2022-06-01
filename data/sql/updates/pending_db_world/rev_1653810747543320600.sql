28d4527a3ca32c4a5c37f77891a67c17708de163
Maelthyr <100411212+Maelthyrr@users.noreply.github.com>
2022-06-01 23:14:34 +0200

    adjust drop rate with retail test

--- data/sql/updates/pending_db_world/rev_1653810747543320600.sql
+++ data/sql/updates/pending_db_world/rev_1653810747543320600.sql
@@ -1,9 +1,2 @@
---
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 2953);
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(2953, 0, 11111, 0.2, 0, 1, 0, 1, 1, 'Bristleback Shaman - (Small Pouch ReferenceTable)'),
-(2953, 0, 20000, 30, 0, 1, 0, 1, 1, 'Bristleback Shaman - (Grey 1-5 EXP 0 ReferenceTable)'),
-(2953, 0, 20014, 30, 0, 1, 0, 1, 1, 'Bristleback Shaman - (Food 1-5 EXP 0 ReferenceTable)'),
-(2953, 1388, 0, 2, 0, 1, 0, 1, 1, 'Bristleback Shaman - Crooked Staff'),
-(2953, 4770, 0, 90, 1, 1, 0, 1, 1, 'Bristleback Shaman - Bristleback Belt'),
-(2953, 6634, 0, 80, 1, 1, 0, 1, 1, 'Bristleback Shaman - Ritual Salve');
+-- Adjust drop rate of Ritual salve to 72% (based by a sniffer with 325 kills)
+UPDATE  `creature_loot_template` SET `Chance` = 72 WHERE `Entry` = 2953 AND `Item` = 6634;
