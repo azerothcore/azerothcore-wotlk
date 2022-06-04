27f4764fed1723eaf089bb430095c53ddd30c4e4
UltraNix <ultranix@gmail.com>
2022-06-04 17:11:05 +0200

    fix(Scripts/ZulGurub): Fixed spawning Edge of Madness bosses.
    Fixes #11609

--- src/server/scripts/EasternKingdoms/ZulGurub/zulgurub.h
+++ src/server/scripts/EasternKingdoms/ZulGurub/zulgurub.h
@@ -58,7 +58,11 @@ enum CreatureIds
     NPC_OHGAN               = 14988, // Mandokir Event
     NPC_VILEBRANCH_SPEAKER  = 11391, // Mandokir Event
     NPC_CHAINED_SPIRT       = 15117,  // Mandokir Event
-    NPC_HAKKAR              = 14834
+    NPC_HAKKAR              = 14834,
+    NPC_GRILEK              = 15082,
+    NPC_HAZZARAH            = 15083,
+    NPC_RENATAKI            = 15084,
+    NPC_WUSHOOLAY           = 15085
 };
 
 enum GameobjectIds
