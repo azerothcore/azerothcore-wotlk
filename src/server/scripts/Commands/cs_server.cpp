f79a860d1d7d85e28e24d497c136fa85cd24df63
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-06-05 01:25:02 +0200

    feat(Core/Command): server use TimeString

--- src/server/scripts/Commands/cs_server.cpp
+++ src/server/scripts/Commands/cs_server.cpp
@@ -269,10 +269,20 @@ public:
         return true;
     }
 
-    static bool HandleServerShutDownCommand(ChatHandler* /*handler*/, int32 time, Optional<int32> exitCode, Tail reason)
+    static bool HandleServerShutDownCommand(ChatHandler* /*handler*/, std::string time, Optional<int32> exitCode, Tail reason)
     {
         std::wstring wReason   = std::wstring();
         std::string  strReason = std::string();
+
+        if (time.empty())
+        {
+            return false;
+        }
+
+        if (atoi(time.c_str()) < 0)
+        {
+            return false;
+        }
 
         if (!reason.empty())
         {
@@ -287,23 +297,44 @@ public:
             }
         }
 
+        uint32 delay = TimeStringToSecs(time);
+        if (delay <= 0)
+        {
+            delay = atoi(time.c_str());
+        }
+
+        if (delay <= 0)
+        {
+            return false;
+        }
+
         if (exitCode && *exitCode >= 0 && *exitCode <= 125)
         {
-            sWorld->ShutdownServ(time, 0, *exitCode);
+            sWorld->ShutdownServ(delay, 0, *exitCode);
         }
         else
         {
-            sWorld->ShutdownServ(time, 0, SHUTDOWN_EXIT_CODE, strReason);
+            sWorld->ShutdownServ(delay, 0, SHUTDOWN_EXIT_CODE, strReason);
         }
 
         return true;
     }
 
-    static bool HandleServerRestartCommand(ChatHandler* /*handler*/, int32 time, Optional<int32> exitCode, Tail reason)
+    static bool HandleServerRestartCommand(ChatHandler* /*handler*/, std::string time, Optional<int32> exitCode, Tail reason)
     {
         std::wstring wReason = std::wstring();
         std::string strReason    = std::string();
 
+        if (time.empty())
+        {
+            return false;
+        }
+
+        if (atoi(time.c_str()) < 0)
+        {
+            return false;
+        }
+
         if (!reason.empty())
         {
             if (!Utf8toWStr(reason, wReason))
@@ -317,22 +348,43 @@ public:
             }
         }
 
+        uint32 delay = TimeStringToSecs(time);
+        if (delay <= 0)
+        {
+            delay = atoi(time.c_str());
+        }
+
+        if (delay <= 0)
+        {
+            return false;
+        }
+
         if (exitCode && *exitCode >= 0 && *exitCode <= 125)
         {
-            sWorld->ShutdownServ(time, SHUTDOWN_MASK_RESTART, *exitCode);
+            sWorld->ShutdownServ(delay, SHUTDOWN_MASK_RESTART, *exitCode);
         }
         else
         {
-            sWorld->ShutdownServ(time, SHUTDOWN_MASK_RESTART, RESTART_EXIT_CODE, strReason);
+            sWorld->ShutdownServ(delay, SHUTDOWN_MASK_RESTART, RESTART_EXIT_CODE, strReason);
         }
 
         return true;
     }
 
-    static bool HandleServerIdleRestartCommand(ChatHandler* /*handler*/, int32 time, Optional<int32> exitCode, Tail reason)
+    static bool HandleServerIdleRestartCommand(ChatHandler* /*handler*/, std::string time, Optional<int32> exitCode, Tail reason)
     {
         std::wstring wReason   = std::wstring();
         std::string  strReason = std::string();
+
+        if (time.empty())
+        {
+            return false;
+        }
+
+        if (atoi(time.c_str()) < 0)
+        {
+            return false;
+        }
 
         if (!reason.empty())
         {
@@ -347,22 +399,43 @@ public:
             }
         }
 
+        uint32 delay = TimeStringToSecs(time);
+        if (delay <= 0)
+        {
+            delay = atoi(time.c_str());
+        }
+
+        if (delay <= 0)
+        {
+            return false;
+        }
+
         if (exitCode && *exitCode >= 0 && *exitCode <= 125)
         {
-            sWorld->ShutdownServ(time, SHUTDOWN_MASK_RESTART | SHUTDOWN_MASK_IDLE, *exitCode);
+            sWorld->ShutdownServ(delay, SHUTDOWN_MASK_RESTART | SHUTDOWN_MASK_IDLE, *exitCode);
         }
         else
         {
-            sWorld->ShutdownServ(time, SHUTDOWN_MASK_RESTART | SHUTDOWN_MASK_IDLE, RESTART_EXIT_CODE, strReason);
+            sWorld->ShutdownServ(delay, SHUTDOWN_MASK_RESTART | SHUTDOWN_MASK_IDLE, RESTART_EXIT_CODE, strReason);
         }
 
         return true;
     }
 
-    static bool HandleServerIdleShutDownCommand(ChatHandler* /*handler*/, int32 time, Optional<int32> exitCode, Tail reason)
+    static bool HandleServerIdleShutDownCommand(ChatHandler* /*handler*/, std::string time, Optional<int32> exitCode, Tail reason)
     {
         std::wstring wReason   = std::wstring();
         std::string  strReason = std::string();
+
+        if (time.empty())
+        {
+            return false;
+        }
+
+        if (atoi(time.c_str()) < 0)
+        {
+            return false;
+        }
 
         if (!reason.empty())
         {
@@ -377,13 +450,24 @@ public:
             }
         }
 
+        uint32 delay = TimeStringToSecs(time);
+        if (delay <= 0)
+        {
+            delay = atoi(time.c_str());
+        }
+
+        if (delay <= 0)
+        {
+            return false;
+        }
+
         if (exitCode && *exitCode >= 0 && *exitCode <= 125)
         {
-            sWorld->ShutdownServ(time, SHUTDOWN_MASK_IDLE, *exitCode);
+            sWorld->ShutdownServ(delay, SHUTDOWN_MASK_IDLE, *exitCode);
         }
         else
         {
-            sWorld->ShutdownServ(time, SHUTDOWN_MASK_IDLE, SHUTDOWN_EXIT_CODE, strReason);
+            sWorld->ShutdownServ(delay, SHUTDOWN_MASK_IDLE, SHUTDOWN_EXIT_CODE, strReason);
         }
 
         return true;
