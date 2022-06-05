a07f66bfd04b170741b6c0ed0876f4de0b9b7494
claudiodfc <claudio.daniel.f.c@gmail.com>
2022-06-05 15:20:50 +0100

    trying to fix build

--- data/sql/updates/pending_db_world/rev_1654292539705612400.sql
+++ data/sql/updates/pending_db_world/rev_1654292539705612400.sql
@@ -1,5 +1,6 @@
 SET @NESTLEWOOD_OWLKIN  = 16518;
 SET @SPELL_INOCULATE    = 29528;
+
 -- Clearing Owlkin's SmartAI
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @NESTLEWOOD_OWLKIN * 100 AND `source_type` = 9;
 
@@ -7,5 +8,5 @@ UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = @NESTLEWOOD_OWLKIN;
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = @NESTLEWOOD_OWLKIN) AND (`source_type` = 0)
 
-DELETE FROM `spell_script_names` WHERE `spell_id` = @SPELL_INOCULATE;
+DELETE FROM `spell_script_names` WHERE `spell_id` = 29528;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (@SPELL_INOCULATE, 'spell_inoculate_nestlewood_owlkin');
