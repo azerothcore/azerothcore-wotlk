72527087deed4a1d064b7cd2d35afc2edb628350
mpfans <mpfans@vip.qq.com>
2022-06-09 08:37:14 +0800

    Update rev_1654674618622504335.sql

--- data/sql/updates/pending_db_world/rev_1654674618622504335.sql
+++ data/sql/updates/pending_db_world/rev_1654674618622504335.sql
@@ -1,5 +1,5 @@
 -- Add Chinese translation of road signs
-DELETE FROM `gameobject_template_locale` WHERE entry IN (184084,176365,176364);
+DELETE FROM `gameobject_template_locale` WHERE `entry` IN (184084,176365,176364) AND `locale` = 'zhCN';
 INSERT INTO `gameobject_template_locale` (`entry`, `locale`, `name`, `castBarCaption`, `VerifiedBuild`) VALUES 
 (184084, 'zhCN', '开往秘蓝岛的船只', '', 18019),
 (176365, 'zhCN', '开往泰达希尔的船只', '', 18019); 
