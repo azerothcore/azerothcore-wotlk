ee2229029d9e55254da759bf3819076c3b528e94
temperrr <temperrr@users.noreply.github.com>
2022-06-12 16:42:56 +0200

    Update boss_emerald_dragons.cpp

--- src/server/scripts/World/boss_emerald_dragons.cpp
+++ src/server/scripts/World/boss_emerald_dragons.cpp
@@ -576,7 +576,33 @@ public:
         {
             --_shades;
         }
+        
+         void DamageTaken(Unit*, uint32& /*damage*/, DamageEffectType, SpellSchoolMask) override
+        {
+            // At 75, 50 or 25 percent health, we need to activate the shades and go "banished"
+            // Note: _stage holds the amount of times they have been summoned
+            if (!_banished && me->HealthBelowPctDamaged(100 - 25 * _stage))
+            {
+                _banished = true;
+                _banishedTimer = 60000;
 
+                me->InterruptNonMeleeSpells(false);
+                DoStopAttack();
+
+                Talk(SAY_TAERAR_SUMMON_SHADES);
+
+                uint32 count = sizeof(TaerarShadeSpells) / sizeof(uint32);
+                for (uint32 i = 0; i < count; ++i)
+                    DoCastVictim(TaerarShadeSpells[i], true);
+                _shades += count;
+
+                DoCast(SPELL_SHADE);
+                me->SetUnitFlag(UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE);
+                me->SetReactState(REACT_PASSIVE);
+
+                ++_stage;
+            }
+        }
         void ExecuteEvent(uint32 eventId) override
         {
             switch (eventId)
@@ -620,30 +646,6 @@ public:
 
                 return;
             }
-            // At 75, 50 or 25 percent health, we need to activate the shades and go "banished"
-            // Note: _stage holds the amount of times they have been summoned
-            if (!_banished && !HealthAbovePct(100 - 25 * _stage))
-            {
-                _banished = true;
-                _banishedTimer = 60000;
-
-                me->InterruptNonMeleeSpells(false);
-                DoStopAttack();
-
-                Talk(SAY_TAERAR_SUMMON_SHADES);
-
-                uint32 count = sizeof(TaerarShadeSpells) / sizeof(uint32);
-                for (uint32 i = 0; i < count; ++i)
-                    DoCast(TaerarShadeSpells[i]);
-                _shades += count;
-
-                DoCast(SPELL_SHADE);
-                me->SetUnitFlag(UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE);
-                me->SetReactState(REACT_PASSIVE);
-
-                ++_stage;
-            }
-
             emerald_dragonAI::UpdateAI(diff);
         }
 
