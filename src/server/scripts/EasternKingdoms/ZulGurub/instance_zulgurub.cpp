7e703393d2ee94aa35e269fa5e7cca826126fd0d
Nefertumm <nefertum.dev@protonmail.com>
2022-06-13 19:44:20 -0300

    fix(Core/ZG): Multiple improvements to High Priestess Mar'li (#11647)

--- src/server/scripts/EasternKingdoms/ZulGurub/instance_zulgurub.cpp
+++ src/server/scripts/EasternKingdoms/ZulGurub/instance_zulgurub.cpp
@@ -36,7 +36,8 @@ ObjectData const creatureData[] =
 {
     { NPC_HIGH_PRIEST_THEKAL, DATA_THEKAL  },
     { NPC_ZEALOT_LORKHAN,     DATA_LORKHAN },
-    { NPC_ZEALOT_ZATH,        DATA_ZATH    }
+    { NPC_ZEALOT_ZATH,        DATA_ZATH    },
+    { NPC_PRIESTESS_MARLI,    DATA_MARLI   }
 };
 
 class instance_zulgurub : public InstanceMapScript
@@ -49,6 +50,7 @@ public:
         instance_zulgurub_InstanceMapScript(Map* map) : InstanceScript(map)
         {
             SetBossNumber(EncounterCount);
+            LoadObjectData(creatureData, nullptr);
             LoadDoorData(doorData);
             LoadObjectData(creatureData, nullptr);
         }
@@ -71,7 +73,15 @@ public:
                 case NPC_HAKKAR:
                     _hakkarGUID = creature->GetGUID();
                     break;
+                case NPC_SPAWN_OF_MARLI:
+                    if (Creature* marli = GetCreature(DATA_MARLI))
+                    {
+                        marli->AI()->JustSummoned(creature);
+                    }
+                    break;
             }
+
+            InstanceScript::OnCreatureCreate(creature);
         }
 
         void OnGameObjectCreate(GameObject* go) override
