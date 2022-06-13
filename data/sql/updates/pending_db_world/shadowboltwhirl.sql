2b169eaa3bbfc3187046e53a24d386fb908798af
temperrr <temperrr@users.noreply.github.com>
2022-06-13 11:53:24 +0200

    delete before insert

--- data/sql/updates/pending_db_world/shadowboltwhirl.sql
+++ data/sql/updates/pending_db_world/shadowboltwhirl.sql
@@ -1,2 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = 24834;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(24834, 'aura_shadow_bolt_whirl');
+(24834, 'spell_shadow_bolt_whirl');
