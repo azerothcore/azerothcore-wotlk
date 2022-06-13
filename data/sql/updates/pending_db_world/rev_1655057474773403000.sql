de4aee427ea7faea19c5c5482fbbe7fc3540c300
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-06-12 22:32:44 -0600

    .

--- data/sql/updates/pending_db_world/rev_1655057474773403000.sql
+++ data/sql/updates/pending_db_world/rev_1655057474773403000.sql
@@ -2,7 +2,7 @@
 UPDATE `creature_template` SET `AIName`='SmartAI',`flags_extra`=2 WHERE `entry`=14989;
 DELETE FROM `smart_scripts` WHERE `entryorguid`= 14989 AND `source_type`= 0 AND `id`= 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `event_type`, `event_flags`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES 
-(14989, 1, 1, 11, 26744, 1, 'Poisonous Cloud - OOC - Cast \'Serverside - Poisonous Blood\'');
+(14989, 54, 1, 11, 26744, 1, 'Poisonous Cloud - OOC - Cast \'Serverside - Poisonous Blood\'');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`= 11357 AND `source_type`= 0 AND `id`= 2;
 INSERT INTO `smart_scripts` (`entryorguid`, `id`, `event_type`, `event_flags`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `target_type`, `comment`) VALUES 
