b2a3df0ced9a5eecc6e8a05d6f60406684a6b15b
AzerothCoreBot <azerothcorebot@gmail.com>
2022-06-14 22:57:12 +0000

    chore(DB): import pending files
    Referenced commit(s): ddd6556681fbe38bddb0c07eb1157de3c3b4e1f1

--- data/sql/updates/db_world/2022_06_14_08.sql
+++ data/sql/updates/db_world/2022_06_14_08.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_06_14_07 -> 2022_06_14_08
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 4966;
 DELETE FROM `smart_scripts` WHERE ((`entryorguid` = 496603) AND (`source_type` = 9) AND (`id` IN (0, 1, 2, 3))) OR ((`entryorguid` = 496600) AND (`source_type` = 9) AND (`id` IN (0, 1, 2, 3, 4, 5, 6))) OR ((`entryorguid` = 5184) AND (`source_type` = 0)) OR (`source_type` = 0 AND `entryorguid` = 4966 AND (`id` IN (0, 1, 2, 3, 4, 5, 6, 7)));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
