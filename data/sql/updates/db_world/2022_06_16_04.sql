b75c67aad185991ecd802d732e5c620b7ea6512b
AzerothCoreBot <azerothcorebot@gmail.com>
2022-06-16 14:49:31 +0000

    chore(DB): import pending files
    Referenced commit(s): 59eab6aae150582f0ea93ecc57afe27a5103ce3c

--- data/sql/updates/db_world/2022_06_16_04.sql
+++ data/sql/updates/db_world/2022_06_16_04.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_06_16_03 -> 2022_06_16_04
 UPDATE `creature_template` SET `DamageModifier` = 29, `BaseAttackTime` = 1100 WHERE (`entry` = 15114);
 UPDATE `creature_template` SET `DamageModifier` = 27, `BaseAttackTime` = 1300 WHERE (`entry` = 15083);
 UPDATE `creature_template` SET `DamageModifier` = 24, `BaseAttackTime` = 2000 WHERE (`entry` = 15085);
