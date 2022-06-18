45ba146430d90ec4df645f46b048ca126fb5fc24
temperrr <temperrr@users.noreply.github.com>
2022-06-18 14:26:43 +0200

    Fix(DB/Loot): Tobacco should not be in chest (#12026)

---
+++ data/sql/updates/pending_db_world/siabispremiumtobacco.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject_loot_template` WHERE (`Entry` = 5000);
+INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(5000, 13172, 0, 0.0001, 1, 1, 0, 1, 1, 'Premium Siabi Tobacco - Siabi\'s Premium Tobacco');
