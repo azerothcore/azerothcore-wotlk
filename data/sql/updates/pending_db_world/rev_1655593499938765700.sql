206e2f3ff6bdcbc18ec74bc30680add6d6d17535
Nefertumm <nefertum.dev@protonmail.com>
2022-06-18 20:05:20 -0300

    dumb me forgot the sql

---
+++ data/sql/updates/pending_db_world/rev_1655593499938765700.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hazzarah_sleep';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(24664, 'spell_hazzarah_sleep');
