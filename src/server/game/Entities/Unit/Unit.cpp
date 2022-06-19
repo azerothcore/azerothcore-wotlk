a935e51754476e5a2d3930670e5d1cc82341dbaf
UltraNix <80540499+UltraNix@users.noreply.github.com>
2022-06-19 13:46:23 +0200

    fix(Core/Pets): Mob "soloed" by pet should not grant exp/loot to its … (#11969)
    * fix(Core/Pets): Mob "soloed" by pet should not grant exp/loot to its master.
    
    Fixes #11528

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -999,7 +999,11 @@ uint32 Unit::DealDamage(Unit* attacker, Unit* victim, uint32 damage, CleanDamage
             victim->ToCreature()->SetLootRecipient(attacker);
 
         if (!attacker || attacker->IsControlledByPlayer() || attacker->IsCreatedByPlayer())
-            victim->ToCreature()->LowerPlayerDamageReq(health < damage ?  health : damage);
+        {
+            uint32 unDamage = health < damage ? health : damage;
+            bool damagedByPlayer = unDamage && attacker && attacker->m_movedByPlayer != nullptr;
+            victim->ToCreature()->LowerPlayerDamageReq(unDamage, damagedByPlayer);
+        }
     }
 
     if (health <= damage)
