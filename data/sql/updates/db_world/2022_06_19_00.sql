713cddc1ba534cd29b5d62c3f2fb4ed2626003c9
AzerothCoreBot <azerothcorebot@gmail.com>
2022-06-19 11:52:59 +0000

    chore(DB): import pending files
    Referenced commit(s): 2db5f9a231be84092ea9eb2122a4f78ddd28652f

--- data/sql/updates/db_world/2022_06_19_00.sql
+++ data/sql/updates/db_world/2022_06_19_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_06_18_15 -> 2022_06_19_00
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_mandokir_charge';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
