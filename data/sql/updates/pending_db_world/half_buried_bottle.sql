f56b0e48f7bc1fba251466c371f875256f032c57
Gultask <100873791+Gultask@users.noreply.github.com>
2022-06-22 14:47:01 -0300

    fix(DB/gameobject): move three objects Half-Buried Bottle (2560) abov… (#12103)
    * fix(DB/gameobject): move three objects Half-Buried Bottle (2560) above ground
    
    Moves objects 11031 11034 11705 above ground as they were previously underground and unreachable.
    Created by using .go gameobject, flying up, taking the Z-axis from .gps and subtracting 0.1 from the value.
    Temporary change until a proper sniff can be made.
    
    * changed to UPDATE queries
    
    Dunno how to make it compact 😛

---
+++ data/sql/updates/pending_db_world/half_buried_bottle.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `gameobject` SET `position_z`=0.508326 WHERE  `guid`=11031 AND `id`=2560;
+UPDATE `gameobject` SET `position_z`=2.541038 WHERE  `guid`=11034 AND `id`=2560;
+UPDATE `gameobject` SET `position_z`=0.853386 WHERE  `guid`=11705 AND `id`=2560;
