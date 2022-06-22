67643f8e33e9d6b88c2eea074d543a5c95331228
acidmanifesto <joshua.lee.betts@gmail.com>
2022-06-22 16:55:51 -0400

    Update data/sql/updates/pending_db_world/rev_1655718205934882580.sql
    Co-authored-by: Skjalf <47818697+Nyeriah@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1655718205934882580.sql
+++ data/sql/updates/pending_db_world/rev_1655718205934882580.sql
@@ -1,2 +1,2 @@
 -- fix Woodlands Walker gossip
-UPDATE `creature_template` SET `npcflag`=`npcflag` + 1 WHERE `entry` = 26421
+UPDATE `creature_template` SET `npcflag`=`npcflag` |1 WHERE `entry` = 26421;
