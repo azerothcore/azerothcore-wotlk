84396d3445c33fd491e483928117e262dbd234b6
Meramaf <76571216+Meramaf@users.noreply.github.com>
2022-06-26 10:23:37 +0200

    fix(DB/GameObject): Add missing Alliance Bonfire in Feralas

---
+++ data/sql/updates/pending_db_world/extinguishing_kalimdor.sql
@@ -0,0 +1,4 @@
+DELETE FROM `gameobject` WHERE (`id` = 187929);
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
+(21018, 187929, 1, 0, 0, 1, 1, -4392.56, 2198.47, 10.857, 2.46091, 0, 0, 0.942641, 0.333809, 120, 255, 1, '', 0),
+(164905, 187929, 1, 0, 0, 1, 1, -4412.02, 3480.24, 12.6312, 6.12709, 0, 0, 0.0779684, -0.996956, 300, 0, 1, '', 0);
