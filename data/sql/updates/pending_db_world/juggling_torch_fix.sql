b6595c062e3abe0be4b076e00c756a4b6e972c62
Meramaf <76571216+Meramaf@users.noreply.github.com>
2022-06-26 16:58:31 +0200

    Update juggling_torch_fix.sql

--- data/sql/updates/pending_db_world/juggling_torch_fix.sql
+++ data/sql/updates/pending_db_world/juggling_torch_fix.sql
@@ -1,5 +1,3 @@
---Midsummer Juggling Torch Spellfix
-
 DELETE FROM `spell_script_names` WHERE `spell_id` = '45644';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (45644, "spell_midsummer_torch_catch");
