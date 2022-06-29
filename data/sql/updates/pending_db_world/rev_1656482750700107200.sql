c88e711f031bb0c6ee58ed4fbb5d85f0416f53c9
Nefertumm <nefertum.dev@protonmail.com>
2022-06-29 20:17:44 -0300

    Update rev_1656482750700107200.sql

--- data/sql/updates/pending_db_world/rev_1656482750700107200.sql
+++ data/sql/updates/pending_db_world/rev_1656482750700107200.sql
@@ -1,3 +1,3 @@
 --
 -- Mar'li - Gri'lek
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`&~256 WHERE `entry` IN (14510, 15082);
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`&~256 WHERE `entry` = 14510;
