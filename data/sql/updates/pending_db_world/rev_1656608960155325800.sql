1a70409f8dc38c6a6c033e33e09dcff269f5c52c
郑佩茹 <zhengpeiru21@protonmail.com>
2022-06-30 11:24:37 -0600

    Fix DELETE to include all added entries

--- data/sql/updates/pending_db_world/rev_1656608960155325800.sql
+++ data/sql/updates/pending_db_world/rev_1656608960155325800.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (6140, 6141);
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (6140, 6141, 6142, 4768, 4769, 4770, 4771, 4772, 4773, 4774, 4775, 4776, 4777, 4778, 4779, 4780, 4781, 4782, 4783, 4784, 4785, 4786);
 INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`) VALUES
 (6140, 25, 4, 0),
 (6141, 25, 5, 0),
