eb872493e99c9c65c74be8ca5dc47fb33561aecf
Asthalor <87535580+Asthalor@users.noreply.github.com>
2022-06-30 12:23:01 +0200

    Clean branch

--- data/sql/updates/pending_db_world/rev_1651752894543472300.sql
+++
@@ -1,5 +0,0 @@
---
--- Remove the item Cache of Zanzil's Altered Mixture (8073) from Grand Foreman Puzik Gallywix (7288)
-
-DELETE FROM `creature_loot_template` WHERE `Entry` = 7288 AND `Item` = 8073;
-
