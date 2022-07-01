71b967a21c53d81c7ef264e17019dacf5e188a52
Gultask <100873791+Gultask@users.noreply.github.com>
2022-07-01 17:28:13 -0300

    Update ctm_1.sql

--- data/sql/updates/pending_db_world/ctm_1.sql
+++ data/sql/updates/pending_db_world/ctm_1.sql
@@ -1,8 +1,6 @@
 --
-DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (15242, 18707, 22441, 15114, 15043, 8276);
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (15242, 18707, 22441, 8276);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`) VALUES (15242, 1, 0, 1, 0, 0, 0),
 (18707, 1, 0, 1, 0, 0, 0),
 (22441, 1, 0, 1, 0, 0, 0),
-(15114, 1, 1, 0, 0, 0, 0),
-(15043, 1, 1, 0, 0, 0, 0),
 (8276, 1, 0, 1, 0, 0, 0);
