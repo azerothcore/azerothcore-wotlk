8aa74a23b36bd8d129b68ed2186f458c4e6a758c
AzerothCoreBot <azerothcorebot@gmail.com>
2022-07-02 16:39:52 +0000

    chore(DB): import pending files
    Referenced commit(s): 16f2983307aeeb39bd263e3e923c93c16488444f

--- data/sql/updates/db_world/2022_07_02_00.sql
+++ data/sql/updates/db_world/2022_07_02_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_07_01_03 -> 2022_07_02_00
 --
 DELETE FROM `creature_formations` WHERE `memberGUID` IN (49313, 49314);
 INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
