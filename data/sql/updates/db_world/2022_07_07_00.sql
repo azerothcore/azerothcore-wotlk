5dec721a71bf02c382323354408c6c2db03cf453
AzerothCoreBot <azerothcorebot@gmail.com>
2022-07-07 16:48:01 +0000

    chore(DB): import pending files
    Referenced commit(s): e21011c223ae12e987a650b0717c11586a0d8b4d

--- data/sql/updates/db_world/2022_07_07_00.sql
+++ data/sql/updates/db_world/2022_07_07_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_07_06_00 -> 2022_07_07_00
 --
 DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (6140, 6141, 6142, 4768, 4769, 4770, 4771, 4772, 4773, 4774, 4775, 4776, 4777, 4778, 4779, 4780, 4781, 4782, 4783, 4784, 4785, 4786);
 INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`) VALUES
