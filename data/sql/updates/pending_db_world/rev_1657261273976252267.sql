3da725678f05473c3612e59707fef1bb0cfd9092
Asthalor <87535580+Asthalor@users.noreply.github.com>
2022-07-08 08:42:41 +0200

    Better sql

--- data/sql/updates/pending_db_world/rev_1657261273976252267.sql
+++ data/sql/updates/pending_db_world/rev_1657261273976252267.sql
@@ -1,2 +1,2 @@
--- Change modelid1 that was an infernal to Enchanted Tiki Warriors
-UPDATE `creature_template` SET `modelid1` = 25749 WHERE (`entry` = 28882);
+-- Change modelid1 that was an infernal to Enchanted Tiki Warriors modelid (25749) and delete modelid2 to not duplicate
+UPDATE `creature_template` SET `modelid1` = 25749, `modelid2` = 0 WHERE (`entry` = 28882);
