4fdeff3f35f45bb84b19fa002b763dce165e0bb6
Gultask <100873791+Gultask@users.noreply.github.com>
2022-07-09 03:08:22 -0300

    Update rocklance.sql

--- data/sql/updates/pending_db_world/rocklance.sql
+++ data/sql/updates/pending_db_world/rocklance.sql
@@ -46,3 +46,10 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 
 UPDATE `creature` SET `spawntimesecs`=43200 WHERE  `guid`=14007;
 UPDATE `creature` SET `spawntimesecs`=43200 WHERE  `guid`=20588;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 5841;
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 5841) AND (`source_type` = 0) AND (`id` IN (3, 4));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(5841, 0, 3, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 70, 500, 0, 0, 0, 0, 0, 10, 14007, 0, 0, 0, 0, 0, 0, 0, 'Rocklance - On Respawn - Respawn Formation Member 14007'),
+(5841, 0, 4, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 70, 500, 0, 0, 0, 0, 0, 10, 20588, 0, 0, 0, 0, 0, 0, 0, 'Rocklance - On Respawn - Respawn Formation Member 20588');
