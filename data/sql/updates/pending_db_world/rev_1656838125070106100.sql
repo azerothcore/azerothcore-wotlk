5da752b9398d7616661698de1ec8a09370c06f55
UltraNix <ultranix@gmail.com>
2022-07-09 17:09:39 +0200

    Update.

--- data/sql/updates/pending_db_world/rev_1656838125070106100.sql
+++ data/sql/updates/pending_db_world/rev_1656838125070106100.sql
@@ -1,3 +1,2 @@
 --
-UPDATE `creature_template` SET `unit_flags`=`unit_flags`|4 WHERE `entry`=30403;
 UPDATE `smart_scripts` SET `event_flags`=`event_flags`|512 WHERE `entryorguid`=3040300 AND `source_type`=9;
