bafac46639de2d2625f3aa32c7ad2e427487d201
Nefertumm <nefertum.dev@protonmail.com>
2022-07-09 16:49:35 -0300

    fix(Core/ZulGurub): Hakkar hardmode (#12333)

---
+++ data/sql/updates/pending_db_world/rev_1657371029609597700.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hakkar_power_down';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(24693, 'spell_hakkar_power_down');
