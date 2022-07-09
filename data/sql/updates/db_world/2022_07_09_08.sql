1a503e4f44bc439e96ba67a79edc1879b77e0a2d
AzerothCoreBot <azerothcorebot@gmail.com>
2022-07-09 19:51:26 +0000

    chore(DB): import pending files
    Referenced commit(s): bafac46639de2d2625f3aa32c7ad2e427487d201

--- data/sql/updates/db_world/2022_07_09_08.sql
+++ data/sql/updates/db_world/2022_07_09_08.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_07_09_07 -> 2022_07_09_08
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hakkar_power_down';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
