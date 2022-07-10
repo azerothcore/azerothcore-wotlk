6df6d6542afb90d08fe59caab88fdf20d145e9f1
UltraNix <ultranix@gmail.com>
2022-07-10 17:49:43 +0200

    fix(DB/SAI): Fixes starting waypoint movement during "I Sense a Disturbance" quest.
    Fixes #11857

---
+++ data/sql/updates/pending_db_world/rev_1657468147873034500.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `link`=0 WHERE `entryorguid`=28665 AND `source_type`=0 AND `id`=0;
+UPDATE `smart_scripts` SET `event_type`=27 WHERE `entryorguid`=28665 AND `source_type`=0 AND `id`=1;
