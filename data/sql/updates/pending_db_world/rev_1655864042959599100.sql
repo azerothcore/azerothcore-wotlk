98a14741514da70766864d7bc5149f6951cbd13a
Si1ker <Si1ker@users.noreply.github.com>
2022-07-10 18:18:20 -0600

    .

--- data/sql/updates/pending_db_world/rev_1655864042959599100.sql
+++ data/sql/updates/pending_db_world/rev_1655864042959599100.sql
@@ -11,3 +11,8 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`= 15521 AND `source_type`= 0 AND 
 INSERT INTO `smart_scripts` (`entryorguid`, `event_type`, `event_flags`, `event_param1`, `event_param2`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES 
 (15521, 1, 1, 10000, 10000, 41, 500, 1, 'Hive\'Zara Hatchling - Out of Combat - Despawn (No Repeat)');
 
+-- Root egg properly - avoid reset
+DELETE FROM `creature_template_movement` FROM `CreatureId`=15514;
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`) VALUES 
+(15514, 1, 1, 0, 1, 0, 0);
+
