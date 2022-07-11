d6d961174d54b3e2c459acfb0e09117ffcf3e765
AzerothCoreBot <azerothcorebot@gmail.com>
2022-07-11 13:42:04 +0000

    chore(DB): import pending files
    Referenced commit(s): a24da9b25258a5183555a30ff940d6d5a2707dfd

--- data/sql/updates/db_world/2022_07_11_00.sql
+++ data/sql/updates/db_world/2022_07_11_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_07_10_04 -> 2022_07_11_00
 --
 UPDATE `smart_scripts` SET `link`=0 WHERE `entryorguid`=28665 AND `source_type`=0 AND `id`=0;
 UPDATE `smart_scripts` SET `event_type`=27 WHERE `entryorguid`=28665 AND `source_type`=0 AND `id`=1;
