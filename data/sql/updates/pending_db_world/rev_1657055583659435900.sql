57327aa362f0702ea8878cc961206bfcc243bf5e
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-07-13 14:51:50 -0600

    fix(DB/creature template): Adjust Whitewhisker/Irondeep NPCs in Alterac Valley (#12309)

---
+++ data/sql/updates/pending_db_world/rev_1657055583659435900.sql
@@ -0,0 +1,7 @@
+--
+
+UPDATE `creature_template` SET `exp`=0, `ManaModifier`=1 WHERE `entry`=11657;
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`|512 WHERE `entry`=11677;
+UPDATE `creature_template` SET `minlevel`=53, `maxlevel`=54 WHERE `entry`=11600;
+UPDATE `creature_template` SET `lootid`=0,`mingold`=0, `maxgold`=0 WHERE `entry` IN (10987,11600,11602,11604,11605,10982,11603);
+
