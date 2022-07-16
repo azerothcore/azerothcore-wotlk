24fa59f735e6abe60fe2fdc1c999c710d6b61369
Nefertumm <nefertum.dev@protonmail.com>
2022-07-16 14:34:02 -0300

    forgot to add error fixes

--- data/sql/updates/pending_db_world/rev_1657988086857948400.sql
+++ data/sql/updates/pending_db_world/rev_1657988086857948400.sql
@@ -1,2 +1,7 @@
 --
-UPDATE `creature` SET `MovementType`= 1 WHERE `guid` IN (51879,51914);
+UPDATE `creature` SET `MovementType` = 1, `wander_distance` = 5 WHERE `guid` IN (51879,51914);
+UPDATE `creature` SET `MovementType` = 0 WHERE `guid` = 132313;
+
+UPDATE `creature_template` SET `AIName` = 'NullCreatureAI' WHERE `AIName` IN ('NullCreatureAi', 'NullAI');
+UPDATE `creature_template` SET `AIName` = 'AggressorAI' WHERE `AIName` = 'AgressorAI';
+UPDATE `creature_template` SET `AIName` = '' WHERE `AIName` = 'OutdoorPvPObjectiveAI';
