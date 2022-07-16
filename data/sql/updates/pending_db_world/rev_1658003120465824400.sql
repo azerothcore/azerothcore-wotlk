89abc2a1009155fdcb121090d79f3be735cc971e
schell244 <>
2022-07-16 22:44:45 +0200

    fix(DB/texts): Shadowfang Keep: add broadcast texts for arugal intro event

---
+++ data/sql/updates/pending_db_world/rev_1658003120465824400.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `creature_text` SET `BroadcastTextId` = 1456 WHERE `CreatureID` = 10000 AND `GroupID` = 0;
+UPDATE `creature_text` SET `BroadcastTextId` = 5680 WHERE `CreatureID` = 10000 AND `GroupID` = 1;
+UPDATE `creature_text` SET `BroadcastTextId` = 5681 WHERE `CreatureID` = 10000 AND `GroupID` = 2;
+UPDATE `creature_text` SET `BroadcastTextId` = 5682 WHERE `CreatureID` = 10000 AND `GroupID` = 3;
