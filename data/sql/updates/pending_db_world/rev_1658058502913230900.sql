43598944a4684b18b4b3e2aceb898138fe9d638a
schell244 <>
2022-07-17 13:55:08 +0200

    correct position of npc 1568

---
+++ data/sql/updates/pending_db_world/rev_1658058502913230900.sql
@@ -0,0 +1,2 @@
+-- Undertaker Mordo https://classic.wowhead.com/npc=1568/undertaker-mordo
+UPDATE `creature` SET `position_x` = 1678.99,`position_y` = 1667.86,`position_z` = 135.855, `orientation` = 3.76991 WHERE `id1` = 1568;
