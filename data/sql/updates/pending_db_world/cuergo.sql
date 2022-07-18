8882be570ce9bb88850c901643faedd0096c9596
Gultask <100873791+Gultask@users.noreply.github.com>
2022-07-18 15:20:54 -0300

    fix(DB/Loot): Cuergo's Hidden Treasure (9265) (#12283)

---
+++ data/sql/updates/pending_db_world/cuergo.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `item_loot_template` WHERE (`Entry` = 9265) AND (`Item` IN (9360, 9361));
+INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(9265, 9360, 0, 92, 0, 1, 1, 1, 1, 'Cuergo\'s Hidden Treasure - Cuergo\'s Gold'),
+(9265, 9361, 0, 0, 0, 1, 1, 1, 1, 'Cuergo\'s Hidden Treasure - Cuergo\'s Gold with worm');
