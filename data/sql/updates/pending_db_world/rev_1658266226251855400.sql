b7353ce9b297d644afb59e24ad3d1e29a99c96d3
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-07-19 23:04:23 -0500

    feat(Core/Commands): Introduce .npc guid (#12440)

---
+++ data/sql/updates/pending_db_world/rev_1658266226251855400.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `command` WHERE `name` = 'npc guid';
+INSERT INTO `command` (`name`, `security`, `help`) VALUES ('npc guid', 1, 'Syntax: .npc guid\r\n\r\nDisplays GUID, faction, NPC flags, Entry ID, Model ID for selected creature.');
