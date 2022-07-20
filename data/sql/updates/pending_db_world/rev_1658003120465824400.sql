94650c3f4ee0afcb65a5eb447ccbd05054d12a0c
schell244 <>
2022-07-20 18:15:25 +0200

    add missing source_type

--- data/sql/updates/pending_db_world/rev_1658003120465824400.sql
+++ data/sql/updates/pending_db_world/rev_1658003120465824400.sql
@@ -5,7 +5,7 @@ UPDATE `creature_text` SET `BroadcastTextId` = 5681 WHERE `CreatureID` = 10000 A
 UPDATE `creature_text` SET `BroadcastTextId` = 5682 WHERE `CreatureID` = 10000 AND `GroupID` = 3;
 
 -- SFK: Arugal intro script
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 10000;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 10000 AND `source_type`= 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (10000,0,0,0,60,0,100,1,0,0,0,0,0,47,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Arugal - Set Invisible'),
 (10000,0,1,0,60,0,100,1,5000,5000,0,0,0,47,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Arugal - Set Visible'),
