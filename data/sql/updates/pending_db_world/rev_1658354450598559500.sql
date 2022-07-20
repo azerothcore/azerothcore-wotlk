21f6f63c7925dfbf01dd3836e1b1cefadbd6e6a8
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-07-20 17:09:09 -0500

    conventions

--- data/sql/updates/pending_db_world/rev_1658354450598559500.sql
+++ data/sql/updates/pending_db_world/rev_1658354450598559500.sql
@@ -1,6 +1,6 @@
 --
 /* Crocs movement is a scripted action that occurs about every 30 seconds, not normal random movement */
-UPDATE `creature` SET `wander_distance`='0', `MovementType`='0' WHERE `id1`=15043;
+UPDATE `creature` SET `wander_distance`=0, `MovementType`=0 WHERE `id1`=15043;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=15043;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
