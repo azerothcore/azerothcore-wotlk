734da1b2a75ba2b4baaff05338146a9fa07e1645
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-07-19 20:48:55 -0500

    Update rev_1658266226251855400.sql

--- data/sql/updates/pending_db_world/rev_1658266226251855400.sql
+++ data/sql/updates/pending_db_world/rev_1658266226251855400.sql
@@ -1,2 +1,3 @@
 --
+DELETE FROM `command` WHERE name = 'npc guid';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('npc guid', 1, 'Synatx: .npc guid\r\n\r\nDisplays GUID, faction, NPC flags, Entry ID, Model ID for selected creature.');
