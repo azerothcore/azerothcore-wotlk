1692a690535166fa6117fcf13ac40845a376b9bf
Gultask <sagemochi@hotmail.com>
2022-07-22 02:06:14 -0300

    Revert "Update rev_1658464932896975300.sql"
    This reverts commit 1ec4a2aef8f0cffddfc80832fe5cfb72c0c62449.

--- data/sql/updates/pending_db_world/rev_1658464932896975300.sql
+++ data/sql/updates/pending_db_world/rev_1658464932896975300.sql
@@ -1,9 +1,2 @@
 --
-DELETE FROM `reference_loot_template` WHERE  (`Entry`=35026) AND (`Item` IN (22406, 22404));
-
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 10997) AND (`Item` IN (35026, 35027, 22406, 22404));
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(10997, 35026, 35026, 100, 0, 1, 1, 1, 1, 'Cannon Master Willey - (ReferenceTable)'),
-(10997, 35027, 35026, 100, 0, 1, 2, 1, 1, 'Cannon Master Willey - (ReferenceTable)'),
-(10997, 22406, 0, 10, 0, 1, 1, 1, 1, ''),
-(10997, 22404, 0, 8, 0, 1, 2, 1, 1, '');
+UPDATE `reference_loot_template` SET `Chance`=0 WHERE  `Entry`=35026 AND `Item` IN (22407, 22406, 22405, 22404, 22403);
