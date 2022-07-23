474fede553fafe99e8f1b8d76116736b87cbac50
AzerothCoreBot <azerothcorebot@gmail.com>
2022-07-23 13:31:24 +0000

    chore(DB): import pending files
    Referenced commit(s): 22b6c35d23d35227d7a59b70399693e51a5153c5

--- data/sql/updates/db_world/2022_07_23_00.sql
+++ data/sql/updates/db_world/2022_07_23_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_07_22_00 -> 2022_07_23_00
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17606;
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 17606) AND (`source_type` = 0) AND (`id` IN (1, 2));
