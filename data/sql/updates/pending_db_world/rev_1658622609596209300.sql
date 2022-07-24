94b37bf7d05017a649aff700e9ff70a541a915fb
Gultask <sagemochi@hotmail.com>
2022-07-23 21:31:37 -0300

    Create rev_1658622609596209300.sql

---
+++ data/sql/updates/pending_db_world/rev_1658622609596209300.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature` SET `wander_distance`=16, `MovementType`=1 WHERE  `guid`=21773;
+UPDATE `creature` SET `wander_distance`=18, `MovementType`=1 WHERE  `guid`=21774;
+UPDATE `creature` SET `wander_distance`=15, `MovementType`=1 WHERE  `guid`=21775;
