c258456f4d713524163593880175ee3fcda75382
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-07-25 00:40:02 -0300

    refactor(Scripts/TempleOfAhnQiraj): Remove Giant Flesh Claw script as it is not needed

---
+++ data/sql/updates/pending_db_world/rev_1658719876642198500.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `creature_template_movement` WHERE `creatureId` = 15802;
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
+(15802, 1, 0, 0, 1, 0, 0, 0);
+
+UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 15802;
