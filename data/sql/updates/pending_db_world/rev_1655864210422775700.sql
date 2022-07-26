b96ba5e04cf62f34fb4a8ff46fddf99002df6e68
Si1ker <Si1ker@users.noreply.github.com>
2022-07-26 02:35:18 -0600

    .

--- data/sql/updates/pending_db_world/rev_1655864210422775700.sql
+++ data/sql/updates/pending_db_world/rev_1655864210422775700.sql
@@ -1,5 +1,6 @@
 --
 
+UPDATE `creature_template` SET `mingold`=0, `maxgold`=0 WHERE `entry`=15546;
 UPDATE `creature_template` SET `flags_extra`=`flags_extra`|256 WHERE `entry`=15369;
 INSERT INTO `creature_addon` (`guid`, `auras`) VALUES 
 (144641, '8876'); -- Triggers SPELL_THRASH = 3391
