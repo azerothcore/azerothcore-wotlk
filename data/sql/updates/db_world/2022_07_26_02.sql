90d08158d9e10f6da5422793503b944022f27ba7
AzerothCoreBot <azerothcorebot@gmail.com>
2022-07-26 04:27:33 +0000

    chore(DB): import pending files
    Referenced commit(s): fdb18e0204665b3c5b1ea03a9e3fd306e9827590

--- data/sql/updates/db_world/2022_07_26_02.sql
+++ data/sql/updates/db_world/2022_07_26_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_07_26_01 -> 2022_07_26_02
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_moam_mana_drain_filter';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
