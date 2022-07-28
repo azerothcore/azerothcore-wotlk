f3afb14266f867ffb7baecd192dae884f022bafb
temperrr <speudoniem@outlook.com>
2022-07-28 13:01:01 +0200

    initial commit

--- data/sql/updates/pending_db_world/emeralddragonsskinloot.sql
+++ data/sql/updates/pending_db_world/emeralddragonsskinloot.sql
@@ -1,5 +1,7 @@
 --
-DELETE FROM `skinning_loot_template` WHERE (`Entry` = 15412) AND (`Item` IN (15412, 20381));
+UPDATE `creature_template` SET `skinloot` = 14887 WHERE (`entry` IN (14887, 14888, 14889, 14890));
+
+DELETE FROM `skinning_loot_template` WHERE (`Entry` = 14887) AND (`Item` IN (15412, 20381));
 INSERT INTO `skinning_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(15412, 15412, 0, 60, 0, 1, 1, 5, 8, ''),
-(15412, 20381, 0, 40, 0, 1, 1, 3, 5, '');
+(14887, 15412, 0, 60, 0, 1, 1, 5, 8, ''),
+(14887, 20381, 0, 40, 0, 1, 1, 3, 5, '');
