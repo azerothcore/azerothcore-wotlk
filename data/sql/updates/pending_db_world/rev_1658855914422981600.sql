df432ff3dd7f6d288d4d29b9c160447c6407a215
Nefertumm <nefertum.dev@protonmail.com>
2022-07-31 14:10:16 -0300

    Revert "use conditions instead cpp"
    This reverts commit ddc67d3da625d9179237b0b576db5e6cc71ce29c.

--- data/sql/updates/pending_db_world/rev_1658855914422981600.sql
+++ data/sql/updates/pending_db_world/rev_1658855914422981600.sql
@@ -1,8 +1,8 @@
 --
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` IN (25177, 25178, 25180, 25181, 25183)) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 15339) AND (`ConditionValue3` = 0);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(17, 0, 25177, 0, 0, 31, 1, 3, 15339, 0, 0, 0, 0, '', 'Fire Weakness should only affect Ossirian'),
-(17, 0, 25178, 0, 0, 31, 1, 3, 15339, 0, 0, 0, 0, '', 'Frost Weakness should only affect Ossirian'),
-(17, 0, 25180, 0, 0, 31, 1, 3, 15339, 0, 0, 0, 0, '', 'Nature Weakness should only affect Ossirian'),
-(17, 0, 25181, 0, 0, 31, 1, 3, 15339, 0, 0, 0, 0, '', 'Arcane Weakness should only affect Ossirian'),
-(17, 0, 25183, 0, 0, 31, 1, 3, 15339, 0, 0, 0, 0, '', 'Shadow Weakness should only affect Ossirian');
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_crystal_weakness';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(25177, 'spell_crystal_weakness'),
+(25178, 'spell_crystal_weakness'),
+(25180, 'spell_crystal_weakness'),
+(25181, 'spell_crystal_weakness'),
+(25183, 'spell_crystal_weakness');
