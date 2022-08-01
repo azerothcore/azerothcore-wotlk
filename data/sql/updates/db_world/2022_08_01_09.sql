7d0161d26ece2f47b5961ba97744062b14f3f2a4
AzerothCoreBot <azerothcorebot@gmail.com>
2022-08-01 20:17:58 +0000

    chore(DB): import pending files
    Referenced commit(s): 9f030e098a1956b22c6c55a579991067d0b87492

--- data/sql/updates/db_world/2022_08_01_09.sql
+++ data/sql/updates/db_world/2022_08_01_09.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_08_01_08 -> 2022_08_01_09
 --
 UPDATE `creature` SET `wander_distance`=16, `MovementType`=1 WHERE  `guid`=21773;
 UPDATE `creature` SET `wander_distance`=18, `MovementType`=1 WHERE  `guid`=21774;
