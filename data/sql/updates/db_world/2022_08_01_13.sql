62fbef83c457e53e67044fea44ae42a85eb79319
AzerothCoreBot <azerothcorebot@gmail.com>
2022-08-01 20:42:46 +0000

    chore(DB): import pending files
    Referenced commit(s): a4cd022c48abbedaf90b32c0f30c0939ef310bdd

--- data/sql/updates/db_world/2022_08_01_13.sql
+++ data/sql/updates/db_world/2022_08_01_13.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_08_01_12 -> 2022_08_01_13
 DELETE FROM `creature` WHERE `guid` IN (161044, 161045, 161046);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (161044, 1128, 0, 0, 0, 0, 0, 1, 1, 0, -4310.01, -1447.54, 400.56363, 3.93681, 120, 19, 0, 122, 0, 1, 0, 0, 0, '', 0),
