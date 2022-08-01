7f9ea035deb930b2cf8457a414162471469983c1
AzerothCoreBot <azerothcorebot@gmail.com>
2022-08-01 21:58:10 +0000

    chore(DB): import pending files
    Referenced commit(s): 3a4459eeebbd3bc7a59e70b50c435bc7e518a8b7

--- data/sql/updates/db_world/2022_08_01_18.sql
+++ data/sql/updates/db_world/2022_08_01_18.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_08_01_17 -> 2022_08_01_18
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_crystal_weakness';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
