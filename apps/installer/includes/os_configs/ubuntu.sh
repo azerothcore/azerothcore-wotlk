33ea0abeb16797d138f05339025e4134754f3053
Patrick Lewis <pat@lo5t.com>
2022-08-06 19:26:55 -0700

    update ubuntu.sh

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -9,12 +9,21 @@ sudo apt update
 # shared deps
 sudo apt-get -y install ccache clang cmake curl google-perftools libmysqlclient-dev make unzip
 
-if [[ $CONTINUOUS_INTEGRATION || $DOCKER ]]; then
+if [[ $CONTINUOUS_INTEGRATION && $UBUNTU_VERSION == "22.04" || $DOCKER && $UBUNTU_VERSION == "22.04" ]]; then
+  sudo apt update && sudo apt-get -y install build-essential cmake-data  \
+  libboost-dev libbz2-dev libncurses5-dev libmysql++-dev libgoogle-perftools-dev libreadline6-dev libssl-dev libtool \
+  openssl zlib1g-dev
+elif [[ $CONTINUOUS_INTEGRATION && $UBUNTU_VERSION == "20.04" || $DOCKER && $UBUNTU_VERSION == "20.04" ]]; then
   sudo add-apt-repository -y ppa:mhier/libboost-latest && sudo apt update && sudo apt-get -y install build-essential cmake-data  \
   libboost1.74-dev libbz2-dev libncurses5-dev libmysql++-dev libgoogle-perftools-dev libreadline6-dev libssl-dev libtool \
   openssl zlib1g-dev
 else
   case $UBUNTU_VERSION in
+     "22.04")
+       sudo apt-get install -y g++ gdb gdbserver gcc git \
+       libboost-all-dev libbz2-dev libncurses-dev libreadline-dev \
+       libssl-dev mysql-server
+       ;;
      "20.04")
        sudo apt-get install -y g++ gdb gdbserver gcc git \
        libboost-all-dev libbz2-dev libncurses-dev libreadline-dev \
