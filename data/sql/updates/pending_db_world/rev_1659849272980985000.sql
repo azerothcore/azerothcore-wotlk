3469216e4cc3d01ca78c81e8be40e200f62e5913
Nefertumm <nefertum.dev@protonmail.com>
2022-08-07 04:16:50 -0300

    sneaky line

--- data/sql/updates/pending_db_world/rev_1659849272980985000.sql
+++ data/sql/updates/pending_db_world/rev_1659849272980985000.sql
@@ -8,7 +8,6 @@ INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, 
 (25831, 0, 531, -8158.03, 1139.3, -83.95, 0),
 (25832, 0, 531, -8029.25, 1237.78, -85.2285, 0);
 
-
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15630) AND (`source_type` = 0) AND (`id` IN (0));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (15630, 0, 0, 0, 0, 0, 100, 3, 10000, 20000, 0, 0, 0, 11, 26662, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Spawn of Fankriss - In Combat - Cast \'Berserk\' (No Repeat) (Normal Dungeon)');
