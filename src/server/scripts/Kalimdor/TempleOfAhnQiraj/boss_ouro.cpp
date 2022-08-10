278bdb34a8c24b0e54fcd56d962022c16b4aeb18
Nefertumm <nefertum.dev@protonmail.com>
2022-08-10 01:16:55 -0300

    build?

--- src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_ouro.cpp
+++ src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_ouro.cpp
@@ -15,6 +15,8 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Cell.h"
+#include "CellImpl.h"
 #include "GridNotifiers.h"
 #include "GridNotifiersImpl.h"
 #include "Player.h"
