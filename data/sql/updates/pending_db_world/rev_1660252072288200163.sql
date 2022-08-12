d9126049d8da1c13ab1e7871ff7e10b919c6c2df
Sogla <soglahash@gmail.com>
2022-08-12 08:59:12 +0200

    add action param1

--- data/sql/updates/pending_db_world/rev_1660252072288200163.sql
+++ data/sql/updates/pending_db_world/rev_1660252072288200163.sql
@@ -1,1 +1,1 @@
-DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (15651, 15652)) AND (`source_type` = 0);
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (15651, 15652)) AND (`action_param1` = 22863)  AND (`source_type` = 0);
