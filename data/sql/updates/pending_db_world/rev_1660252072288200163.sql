e84c327fe10bdeadc72cf1b42b044adce0d4f4a1
Sogla <soglahash@gmail.com>
2022-08-12 08:56:50 +0200

    Add source_type to condition and remove unneeded update

--- data/sql/updates/pending_db_world/rev_1660252072288200163.sql
+++ data/sql/updates/pending_db_world/rev_1660252072288200163.sql
@@ -1,2 +1,1 @@
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (15651, 15652);
-DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (15651, 15652)) AND (`action_param1` = 22863);
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (15651, 15652)) AND (`source_type` = 0);
