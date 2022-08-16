8ade5619737546a0a0a0616563408f3fc8795c23
SoglaHash <74299960+SoglaHash@users.noreply.github.com>
2022-08-16 23:50:32 +0200

    Remove UBRS

--- data/sql/updates/pending_db_world/rev_1660681693491912819.sql
+++ data/sql/updates/pending_db_world/rev_1660681693491912819.sql
@@ -4,6 +4,4 @@ INSERT INTO `lfg_dungeon_template`
 `VerifiedBuild`)
 VALUES
 (2, 'Scholomance', 196.37, 127.05, 134.91, 6.09, 0),
-(276, 'Blackrock Depths - Upper City', 458.32, 26.52, -70.67, 4.95, 0),
-(44, 'Upper Blackrock Spire', 78.5083, -225.044, 49.839, 5.1, 0);
-
+(276, 'Blackrock Depths - Upper City', 458.32, 26.52, -70.67, 4.95, 0);
