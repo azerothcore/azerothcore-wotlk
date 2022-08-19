f92aa1f24a1e10c47c963755d72799649eab9b77
Darkmord1991 <florian1@pwschmidt.de>
2022-08-20 00:21:29 +0200

    New Ashzara Crater Graveyards - DBC Edit required Spawns will be updated later but tested and working

---
+++ Custom/Ashzara Crater Stuf/Graveyards.sql
@@ -0,0 +1,10 @@
+-- Graveyards require DBC edit, that was also done - included into the REPO
+
+INSERT INTO `game_graveyard` (`ID`, `Map`, `x`, `y`, `z`, `Comment`) VALUES (37, 37, 94.5, 944, 304, 'Ashzara Crater, Starter');
+INSERT INTO `game_graveyard` (`ID`, `Map`, `x`, `y`, `z`, `Comment`) VALUES (1722, 37, 62.5, 420, 306, 'Ashzara Crater, Starter2');
+INSERT INTO `game_graveyard` (`ID`, `Map`, `x`, `y`, `z`, `Comment`) VALUES (1723, 37, 625, 87, 280.74, 'Ashzara Crater, Starter2');
+
+
+INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES (1722, 268, 0, 'Ashzara Crater, Starter2');
+INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES (1723, 268, 0, 'Ashzara Crater, Starter3');
+INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES (37, 268, 0, 'Ashzara Crater, Starter');
