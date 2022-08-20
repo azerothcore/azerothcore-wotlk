1318d48da441e0e028e499aa39e9b73a4a973251
UltraNix <ultranix@gmail.com>
2022-08-20 14:12:59 +0200

    fix(Core/Spells): Death's Respite should not generate threat.
    Fixes #12798

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4348,6 +4348,12 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->AttributesEx6 |= SPELL_ATTR6_IGNORE_CASTER_DAMAGE_MODIFIERS;
     });
 
+    // Death's Respite
+    ApplySpellFix({ 67731, 68305 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->AttributesEx3 |= SPELL_ATTR3_SUPRESS_TARGET_PROCS;
+    });
+
     for (uint32 i = 0; i < GetSpellInfoStoreSize(); ++i)
     {
         SpellInfo* spellInfo = mSpellInfoMap[i];
