eaec24744e93a7373b79b1e8a6b5e472ea6f38b7
Darkmord1991 <florian1@pwschmidt.de>
2022-08-21 01:53:45 +0200

    1331

--- src/server/scripts/OutdoorPvP/OutdoorPvPAI.cpp
+++ src/server/scripts/OutdoorPvP/OutdoorPvPAI.cpp
@@ -79,7 +79,7 @@ Group* OutdoorPvPAI::GetGroupPlayer(uint32 guid, uint32 TeamId)
 {
     for(GuidSet::const_iterator itr=m_Groups[TeamId].begin();itr!=m_Groups[TeamId].end();++itr)
     {
-        temp = ObjectGuid::Create<HighGuid::Item>(guid);
+        temp = ObjectGuid::Create<HighGuid::Player>(guid);
         if (Group* group = sGroupMgr->GetGroupByGUID(temp))
             if (group->IsMember(guid))
                 return group;
