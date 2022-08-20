17396881a9bcdd6117924329b1b290e97d0587eb
Darkmord1991 <florian1@pwschmidt.de>
2022-08-20 15:16:18 +0200

    trainer fixes working now except class trainers - still only old global class trainers available some creature spawn updates

--- Custom/Ashzara Crater Stuf/Start NPCs - standard ones.sql
+++ Custom/Ashzara Crater Stuf/Start NPCs - standard ones.sql
@@ -43,3 +43,11 @@ update creature_template set minlevel = 255 where entry = 800000;
 update creature_template set minlevel = 255 where entry = 800001;
 update creature_template set minlevel = 255 where entry = 800002;
 update creature_template set minlevel = 255 where entry = 800003;
+
+DELETE FROM `creature_template` WHERE (`entry` = 800004);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(800004, 0, 0, 0, 0, 0, 20117, 20118, 16317, 16318, 'Ashzara Wyvern Rider', NULL, NULL, 0, 255, 255, 1, 475, 0, 1, 2.28571, 1, 2, 100, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 4096, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'GuardAI', 1, 50, 3, 1, 1, 1, 0, 0, 1, 0, 0, 32770, '', 12340);
+
+DELETE FROM `creature_template_addon` WHERE (`entry` = 800004);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(800004, 0, 0, 54432, 0, 0, 0, '');
