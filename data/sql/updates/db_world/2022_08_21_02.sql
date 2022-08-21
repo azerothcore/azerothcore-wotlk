cfbd870d9eb73111eb843588d55fcd929cf79246
AzerothCoreBot <azerothcorebot@gmail.com>
2022-08-21 04:44:54 +0000

    chore(DB): import pending files
    Referenced commit(s): e623e9ef49bdcc986cc32683ada3911820f858a5

--- data/sql/updates/db_world/2022_08_21_02.sql
+++ data/sql/updates/db_world/2022_08_21_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_08_21_01 -> 2022_08_21_02
 --
 UPDATE `creature_template` SET `ScriptName`='npc_obsidian_destroyer', `AiName`='' WHERE `entry`=15338;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=15338 AND `source_type`=0;
