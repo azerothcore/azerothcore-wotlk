0453bc633c47b50d155770f34fcb3584cc23e374
Darkmord1991 <florian1@pwschmidt.de>
2022-08-21 10:44:13 +0200

    afaf

--- src/server/scripts/OutdoorPvP/OutdoorPvPAI.h
+++ src/server/scripts/OutdoorPvP/OutdoorPvPAI.h
@@ -27,7 +27,7 @@ class OutdoorPvPAI : public OutdoorPvP
         bool SetupOutdoorPvP();
         bool AddOrSetPlayerToCorrectBfGroup(Player *plr);
         void HandlePlayerEnterZone(Player* player, uint32 zone);
-        void HandlePlayerLeaveZone(Player* player, uint32 zone);
+        void HandlePlayerLeaveZone(Player* plr, uint32 zone);
         Group* GetFreeBfRaid(TeamId TeamId);
         Group* GetGroupPlayer(ObjectGuid guid, TeamId TeamId);
 
