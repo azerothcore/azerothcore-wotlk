17452684b6cd1b02244553adbfffdc7a50324e6a
schell244 <Schell244@web.de>
2022-08-21 12:28:51 +0200

    Update src/server/scripts/World/npcs_special.cpp
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -328,7 +328,7 @@ public:
         if (quest->GetQuestId() == QUEST_MASTER_ANGLER)
         {
             creature->RemoveNpcFlag(UNIT_NPC_FLAG_QUESTGIVER);
-            sCreatureTextMgr->SendChat(creature, RIGGLE_SAY_WINNER, player, CHAT_MSG_MONSTER_YELL, LANG_UNIVERSAL, TEXT_RANGE_ZONE);
+            me->Yell(RIGGLE_SAY_WINNER, LANG_UNIVERSAL, player);
             sWorld->setWorldState(STV_FISHING_PREV_WIN_TIME, GameTime::GetGameTime().count());
             sWorld->setWorldState(STV_FISHING_HAS_WINNER, 1);
         }
