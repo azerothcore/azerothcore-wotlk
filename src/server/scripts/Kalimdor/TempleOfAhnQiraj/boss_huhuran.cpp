9f619debe8741c54ddfa338e7800d3b59d7175dd
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-08-21 11:29:22 -0600

    .

--- src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_huhuran.cpp
+++ src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_huhuran.cpp
@@ -73,7 +73,7 @@ struct boss_huhuran : public BossAI
         if (!_berserk && HealthBelowPct(30))
         {
             DoCastSelf(SPELL_BERSERK, true);
-            TextEmote(EMOTE_BERSERK);
+            me->TextEmote(EMOTE_BERSERK);
             _berserk = true;
         }
     }
