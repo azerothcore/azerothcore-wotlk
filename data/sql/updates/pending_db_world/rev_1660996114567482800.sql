9f4f9ce1a1a1671f0f4b373b977c894d14c9c1e5
UltraNix <ultranix@gmail.com>
2022-08-21 09:36:33 +0200

    Update.

--- data/sql/updates/pending_db_world/rev_1660996114567482800.sql
+++ data/sql/updates/pending_db_world/rev_1660996114567482800.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `gameobject` WHERE `id`=180745;
+DELETE FROM `gameobject` WHERE `id`=180745 and `guid` IN (6660,6663,6665);
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
 (6660,180745,531,0,0,1,1,-8660.63,2022.4,108.577,3.64774,0,0,-0.968147,0.250381,7200,255,1),
 (6663,180745,531,0,0,1,1,-8652.2,2020.92,108.577,0.244346,0,0,0.121869,0.992546,7200,255,1),
