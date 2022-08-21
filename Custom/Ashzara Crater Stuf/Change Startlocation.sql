8a9186d620b22464e3130206613ee45195bf6d65
Darkmord1991 <florian1@pwschmidt.de>
2022-08-21 19:52:19 +0200

    fix startlocation map, mobile teleporter added with startspell for all races/classes

--- Custom/Ashzara Crater Stuf/Change Startlocation.sql
+++ Custom/Ashzara Crater Stuf/Change Startlocation.sql
@@ -1,6 +1,6 @@
-update playercreateinfo set map = 0 where race > 0;
+update playercreateinfo set map = 37 where race > 0;
 update playercreateinfo set zone = 268;
 update playercreateinfo set position_x = '131.82' where race > 0;
-update playercreateinfo set position_y = '1012.28' where race > 0;
-update playercreateinfo set position_z = '295.27' where race > 0;
+update playercreateinfo set position_y = '1025.28' where race > 0;
+update playercreateinfo set position_z = '296.27' where race > 0;
 update playercreateinfo set orientation = '5.483' where race > 0;
