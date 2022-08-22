86154c64ab6520b823b4852ab82e2f715d92a3db
Darkmord1991 <florian1@pwschmidt.de>
2022-08-22 20:49:21 +0200

    Hinterland BG work started

---
+++ Custom/Hinterland BG/Cleanup.sql
@@ -0,0 +1,11 @@
+-- Pupellyverbos Port
+delete from gameobject where id = 2068;
+
+-- Saltwater Snapjaw
+delete from creature where id1 = 2505;
+
+-- Gammerita
+delete from creature where id1 = 7977;
+
+-- Ironback
+delete from creature where id1 = 8213;
