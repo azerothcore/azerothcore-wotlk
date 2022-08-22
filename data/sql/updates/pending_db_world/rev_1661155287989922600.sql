37e8b2a0adc5cf39c439604a2457ba6433091171
Nefertumm <nefertum.dev@protonmail.com>
2022-08-22 05:36:48 -0300

    almost messed up

--- data/sql/updates/pending_db_world/rev_1661155287989922600.sql
+++ data/sql/updates/pending_db_world/rev_1661155287989922600.sql
@@ -3,4 +3,4 @@ UPDATE `creature` SET `spawntimesecs` = 120 WHERE `id1` = 15514;
 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 29) AND (`SourceEntry` = 15514) AND (`SourceId` = 0) AND (`ConditionTypeOrReference` = 13);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(29, 0, 15514, 0, 0, 13, 0, 16, 1, 0, 1, 0, 0, '', 'Buru Egg spawn only if Buru is in egg phase.');
+(29, 0, 15514, 0, 0, 13, 0, 16, 2, 0, 1, 0, 0, '', 'Buru Egg spawn only if Buru is in egg phase.');
