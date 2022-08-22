146950eaf1f3991276e069255123ab551ebbb55d
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-08-22 02:28:26 -0500

    Update rev_1658802255073427200.sql

--- data/sql/updates/pending_db_world/rev_1658802255073427200.sql
+++ data/sql/updates/pending_db_world/rev_1658802255073427200.sql
@@ -11,6 +11,7 @@ UPDATE `creature` SET `id2`=11350 WHERE `guid`=49105;
 UPDATE `creature` SET `id1`=11371 WHERE `guid` IN (49101, 49098, 49100, 49099, 51451, 51452);
 UPDATE `creature` SET `id2`=11372 WHERE `guid` IN (49101, 49098, 49100, 49099, 51451, 51452);
 
+DELETE FROM `creature_formations` where `leaderGUID`=49101;
 -- Link the snakes so they agro together, this should ensure the entire 7 pack agros together while also allowing pooling for the trolls
 INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES 
 (49101, 49101, 0, 0, 3, 0, 0),
