d3b2c123b535ab8e2e702d674e6f473d3b5e98b6
Gultask <sagemochi@hotmail.com>
2022-08-22 22:32:43 -0300

    Update rev_1661217788397138700.sql

--- data/sql/updates/pending_db_world/rev_1661217788397138700.sql
+++ data/sql/updates/pending_db_world/rev_1661217788397138700.sql
@@ -1,3 +1,2 @@
 --
-UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE (`entry` = 15344);
-UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE (`entry` = 15555);
+UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE `entry` IN (15344, 15555);
