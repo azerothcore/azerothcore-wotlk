59318d101553c2f6a94259c4059dd4b4e5d59fd0
Gultask <sagemochi@hotmail.com>
2022-08-25 23:01:36 -0300

    Update rev_1661475791842006200.sql

--- data/sql/updates/pending_db_world/rev_1661475791842006200.sql
+++ data/sql/updates/pending_db_world/rev_1661475791842006200.sql
@@ -4,7 +4,9 @@ INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, 
 (17532, 21156, 0, 100, 0, 1, 0, 1, 1, 'Scarab Coffer - Scarab Bag'),
 (17532, 21157, 14001, 100, 0, 1, 0, 1, 1, 'Scarab Coffer - (ReferenceTable)');
 
-DELETE FROM `gameobject_loot_template` WHERE (`Entry` = 17533) AND (`Item` IN (1, 21156, 21157));
+DELETE FROM `gameobject_loot_template` WHERE (`Entry` = 17533) AND (`Item` IN (1, 21157, 21159, 21156, 21158));
 INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(17533, 21156, 0, 100, 0, 1, 0, 1, 3, 'Large Scarab Coffer - Scarab Bag'),
-(17533, 21157, 14502, 100, 0, 1, 0, 1, 1, 'Large Scarab Coffer - (ReferenceTable)');
+(17533, 21157, 14503, 34, 0, 1, 0, 1, 1, 'Large Scarab Coffer - (ReferenceTable)'),
+(17533, 21159, 14502, 100, 0, 1, 0, 1, 1, 'Large Scarab Coffer - (ReferenceTable)'),
+(17533, 21156, 14503, 100, 0, 1, 0, 1, 1, 'Large Scarab Coffer - (ReferenceTable)'),
+(17533, 21158, 14503, 34, 0, 1, 0, 1, 1, 'Large Scarab Coffer - (ReferenceTable)');
