348c08a25550859278ae8b850e9a3ca350b157ee
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-08-27 00:09:53 -0300

    fix(Scripts/TempleOfAhnQiraj): Princess Huhuran's Poison Bolts should hit the closest targets

---
+++ data/sql/updates/pending_db_world/rev_1661569695988798600.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_huhuran_poison_bolt';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(26052, 'spell_huhuran_poison_bolt');
