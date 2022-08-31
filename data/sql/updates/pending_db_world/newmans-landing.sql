da2ad4835c963cc4a25f837b342ec30230adab68
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-08-31 01:58:24 -0400

    Create newmans-landing.sql

---
+++ data/sql/updates/pending_db_world/newmans-landing.sql
@@ -0,0 +1,8 @@
+DELETE FROM `npc_vendor` WHERE `entry` = 26081;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES
+(26081, 2, 2880, 0, 0, 0, 45327),
+(26081, 0, 2901, 0, 0, 0, 45327),
+(26081, 3, 3466, 0, 0, 0, 45327),
+(26081, 5, 3857, 0, 0, 0, 45327),
+(26081, 1, 5956, 0, 0, 0, 45327),
+(26081, 4, 18567, 0, 0, 0, 45327);
