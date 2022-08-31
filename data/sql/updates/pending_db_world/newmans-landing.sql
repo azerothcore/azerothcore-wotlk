2b6859b774781cacf2189399b4d521df1c0fc6bd
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-08-31 11:19:07 -0400

    Update with sniffed data.

--- data/sql/updates/pending_db_world/newmans-landing.sql
+++ data/sql/updates/pending_db_world/newmans-landing.sql
@@ -7,11 +7,9 @@ INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `Exte
 (26081, 1, 5956, 0, 0, 0, 0),
 (26081, 4, 18567, 0, 0, 0, 0);
 
-UPDATE `creature_equip_template` SET `ItemID1` = '26081' WHERE  `CreatureID` = 26081; -- ItemID needs to be reparsed once WPP is fixed
+UPDATE `creature_equip_template` SET `ItemID1` = '19022' WHERE  `CreatureID` = 26081;
 UPDATE `creature_equip_template` SET `VerifiedBuild` = '0' WHERE  `CreatureID` = 26081;
 
--- The coordinates below need to be reparsed once WPP is fixed
-UPDATE `creature` SET `position_x` = -6378.96, `position_y` = 1263.02, `position_z` = 7.1876, `orientation` = 2.274 WHERE `guid` = 1975958;
-UPDATE `creature` SET `position_x` = -6373.69, `position_y` = 1267.84, `position_z` = 7.1876, `orientation` = 2.18761 WHERE `guid` = 1975959;
-UPDATE `creature` SET `position_x` = -6378.09, `position_y` = 1268.65, `position_z` = 7.1876, `orientation` = 2.26222 WHERE `guid` = 1975960;
-
+UPDATE `creature` SET `position_x` = -6378.61, `position_y` = 1263.21, `position_z` = 7.2709, `orientation` = 3.03687 WHERE `guid` = 1975958;
+UPDATE `creature` SET `position_x` = -6373.53, `position_y` = 1267.88, `position_z` = 7.2709, `orientation` = 2.39110 WHERE `guid` = 1975959;
+UPDATE `creature` SET `position_x` = -6378.08, `position_y` = 1268.47, `position_z` = 7.2709, `orientation` = 2.44346 WHERE `guid` = 1975960;
