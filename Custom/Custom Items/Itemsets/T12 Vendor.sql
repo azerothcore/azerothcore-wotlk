264c616dd935516314b324ffddd63a2efd65fb84
Darkmord1991 <florian1@pwschmidt.de>
2022-08-31 10:03:46 +0200

    typo

--- Custom/Custom Items/Itemsets/T12 Vendor.sql
+++ Custom/Custom Items/Itemsets/T12 Vendor.sql
@@ -1,7 +1,7 @@
 -- ID 800006
 DELETE FROM `creature_template` WHERE (`entry` = 800006);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(800006, 0, 0, 0, 0, 0, 18239, 0, 0, 0, 'Raid Tier 11', 'Custom Item Sets', NULL, 0, 80, 80, 0, 35, 4225, 1, 1.14286, 1, 1, 20, 0.25, 3, 0, 1, 0, 0, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '0', 12340);
+(800006, 0, 0, 0, 0, 0, 18239, 0, 0, 0, 'Raid Tier 12', 'Custom Item Sets', NULL, 0, 80, 80, 0, 35, 4225, 1, 1.14286, 1, 1, 20, 0.25, 3, 0, 1, 0, 0, 1, 1, 1, 0, 2048, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '0', 12340);
 
 
 -- vendor entries
