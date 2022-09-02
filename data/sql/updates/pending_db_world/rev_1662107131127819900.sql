3ac3932c1b9d8a28370a9365e2e6210cc1081566
Artem <34547317+Zenslash@users.noreply.github.com>
2022-09-02 11:30:21 +0300

    [Quest] "Unraveling the Mystery" has wrong quest marker

---
+++ data/sql/updates/pending_db_world/rev_1662107131127819900.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `quest_template_addon` WHERE (`ID` = 8314);
+INSERT INTO `quest_template_addon` (`ID`, `MaxLevel`, `AllowableClasses`, `SourceSpellID`, `PrevQuestID`, `NextQuestID`, `ExclusiveGroup`, `RewardMailTemplateID`, `RewardMailDelay`, `RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES
+(8314, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
