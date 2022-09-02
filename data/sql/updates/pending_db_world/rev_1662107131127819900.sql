bb2da3cb6a16e47bb2f66716e4c814352a986027
Artem <34547317+Zenslash@users.noreply.github.com>
2022-09-02 21:34:53 +0300

    Use UPDATE instead of INSERT

--- data/sql/updates/pending_db_world/rev_1662107131127819900.sql
+++ data/sql/updates/pending_db_world/rev_1662107131127819900.sql
@@ -1,4 +1,2 @@
 --
-DELETE FROM `quest_template_addon` WHERE (`ID` = 8314);
-INSERT INTO `quest_template_addon` (`ID`, `MaxLevel`, `AllowableClasses`, `SourceSpellID`, `PrevQuestID`, `NextQuestID`, `ExclusiveGroup`, `RewardMailTemplateID`, `RewardMailDelay`, `RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES
-(8314, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
+UPDATE `quest_template_addon` SET `SpecialFlags` = 0 WHERE (`ID` = 8314);
