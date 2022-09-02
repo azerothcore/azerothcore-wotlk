cd417e79f9244ff024b9063b9eede51e7dedde5c
SoglaHash <74299960+SoglaHash@users.noreply.github.com>
2022-09-02 18:58:59 +0200

    fix typo

--- data/sql/updates/pending_db_world/rev_1662131908035510198.sql
+++ data/sql/updates/pending_db_world/rev_1662131908035510198.sql
@@ -1,3 +1,3 @@
 --
-UPDATE `creature_template` SET `npcflag` = `npc_flag`&~(4096) WHERE (`entry` = 16280);
-UPDATE `creature_template` set `npcflag` = `npc_flag`&~(4096) where (`entry` = 16278);
+UPDATE `creature_template` SET `npcflag` = `npcflag`&~(4096) WHERE (`entry` = 16280);
+UPDATE `creature_template` set `npcflag` = `npcflag`&~(4096) where (`entry` = 16278);
