8c72f2281af92cb8b6e188f619006d722da60d0b
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-09-02 17:26:33 -0400

    Remove quotations and Build edit

--- data/sql/updates/pending_db_world/newmans-landing.sql
+++ data/sql/updates/pending_db_world/newmans-landing.sql
@@ -7,8 +7,7 @@ INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `Exte
 (26081, 1, 5956, 0, 0, 0, 0),
 (26081, 4, 18567, 0, 0, 0, 0);
 
-UPDATE `creature_equip_template` SET `ItemID1` = '19022' WHERE  `CreatureID` = 26081;
-UPDATE `creature_equip_template` SET `VerifiedBuild` = '0' WHERE  `CreatureID` = 26081;
+UPDATE `creature_equip_template` SET `ItemID1` = 19022 WHERE  `CreatureID` = 26081;
 
 UPDATE `creature` SET `position_x` = -6378.61, `position_y` = 1263.21, `position_z` = 7.2709, `orientation` = 3.03687 WHERE `guid` = 1975958;
 UPDATE `creature` SET `position_x` = -6373.53, `position_y` = 1267.88, `position_z` = 7.2709, `orientation` = 2.39110 WHERE `guid` = 1975959;
