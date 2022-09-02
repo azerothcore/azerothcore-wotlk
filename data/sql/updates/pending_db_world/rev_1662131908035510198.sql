09d1c44c1191bb6a43953364ea2b9f4ef0674df2
Sogla <soglahash@gmail.com>
2022-09-02 22:20:32 +0200

    Make it one line

--- data/sql/updates/pending_db_world/rev_1662131908035510198.sql
+++ data/sql/updates/pending_db_world/rev_1662131908035510198.sql
@@ -1,3 +1,2 @@
 --
-UPDATE `creature_template` SET `npcflag` = `npcflag`&~(4096) WHERE (`entry` = 16280);
-UPDATE `creature_template` set `npcflag` = `npcflag`&~(4096) where (`entry` = 16278);
+UPDATE `creature_template` SET `npcflag` = `npcflag`&~(4096) WHERE `entry` IN (16280, 16278);
