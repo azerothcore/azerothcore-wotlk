6dbd8c748e8f2f352c52989cd988c60f4983b35f
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-09-02 14:58:20 -0400

    Create snowfall-leather.sql

---
+++ data/sql/updates/pending_db_world/snowfall-leather.sql
@@ -0,0 +1,1 @@
+UPDATE `broadcast_text` SET `MaleText`='I received an update on that just a moment ago. It looks like out of $2060w light leather needed, we have $2061w. As for the others, the report indicated that we\'ve gathered $2063w of $2064w medium leather, and $2069w out of our $2071w thick leather quota.' WHERE  `ID`=11367;
