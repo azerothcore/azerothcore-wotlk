ff68d1a191d50c58f5e0659d8d1884bf3cd5ac93
Gultask <sagemochi@hotmail.com>
2022-09-02 11:26:22 -0300

    Create rev_1662127445191277200.sql

---
+++ data/sql/updates/pending_db_world/rev_1662127445191277200.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature_loot_template` SET `QuestRequired`=0, `Chance`=2 WHERE `Item` BETWEEN 20858 AND 20865;
+UPDATE `creature_loot_template` SET `QuestRequired`=0, `Chance`=4 WHERE `entry`=15335 AND `Item` BETWEEN 20858 AND 20865;
+UPDATE `creature_loot_template` SET `QuestRequired`=0, `Chance`=6 WHERE `entry`=15333 AND `Item` BETWEEN 20858 AND 20865;
