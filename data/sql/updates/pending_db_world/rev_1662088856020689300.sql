aea12766f32462915ecde7e10520367d29a512ed
Gultask <100873791+Gultask@users.noreply.github.com>
2022-09-02 23:47:14 -0300

    Spawn of Fankriss (15630)

--- data/sql/updates/pending_db_world/rev_1662088856020689300.sql
+++ data/sql/updates/pending_db_world/rev_1662088856020689300.sql
@@ -86,3 +86,8 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (15317, 0, 0, 0, 9, 0, 100, 0, 0, 5, 5000, 9000, 0, 11, 6016, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Qiraji Scorpion - Within 0-5 Range - Cast \'Pierce Armor\''),
 (15317, 0, 1, 0, 0, 0, 100, 0, 5000, 9000, 6000, 12000, 0, 11, 26050, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Qiraji Scorpion - In Combat - Cast \'Acid Spit\''),
 (15317, 0, 2, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Qiraji Scorpion - On Just Died - Despawn In 5000 ms');
+
+-- Spawn of Fankriss (15630) - Cmangos and Sniffs
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15630) AND (`source_type` = 0) AND (`id` IN (0));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(15630, 0, 0, 0, 0, 0, 100, 1, 30000, 30000, 0, 0, 0, 11, 26662, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Spawn of Fankriss - In Combat - Cast \'Berserk\' (No Repeat)');
