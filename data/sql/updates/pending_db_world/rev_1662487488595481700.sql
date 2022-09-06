772462e1d3feaa76aa64a6a851e9881ba841d7bb
Gultask <sagemochi@hotmail.com>
2022-09-06 18:39:14 -0300

    I promise I'm done now

--- data/sql/updates/pending_db_world/rev_1662487488595481700.sql
+++ data/sql/updates/pending_db_world/rev_1662487488595481700.sql
@@ -2,8 +2,10 @@
 UPDATE `creature_template` SET `DamageModifier` = 1.05, `ArmorModifier` = 1.1 WHERE (`entry` = 15316);
 UPDATE `creature_template` SET `ArmorModifier` = 1.1 WHERE (`entry` = 15317);
 UPDATE `creature_template` SET `DamageModifier` = 20, `ArmorModifier` = 1.15 WHERE (`entry` = 15246);
+UPDATE `creature_template` SET `DamageModifier` = 3, `ArmorModifier` = 1.15 WHERE (`entry` = 15537);
+UPDATE `creature_template` SET `DamageModifier` = 9.75, `ArmorModifier` = 1.15 WHERE (`entry` = 15538);
 
-DELETE FROM `creature_template_resistance` WHERE `CreatureID` IN (15316,15317,15246);
+DELETE FROM `creature_template_resistance` WHERE `CreatureID` IN (15316,15317,15246,15537,15538);
 INSERT INTO `creature_template_resistance` (`CreatureID`, `School`, `Resistance`, `VerifiedBuild`) VALUES
 (15316, 2, 75, 0),
 (15316, 3, 75, 0),
@@ -19,4 +21,14 @@ INSERT INTO `creature_template_resistance` (`CreatureID`, `School`, `Resistance`
 (15246, 3, 75, 0),
 (15246, 4, 75, 0),
 (15246, 5, 75, 0),
-(15246, 6, 75, 0);
+(15246, 6, 75, 0),
+(15537, 2, 75, 0),
+(15537, 3, 75, 0),
+(15537, 4, 75, 0),
+(15537, 5, 75, 0),
+(15537, 6, 75, 0),
+(15538, 2, 115, 0),
+(15538, 3, 115, 0),
+(15538, 4, 115, 0),
+(15538, 5, 115, 0),
+(15538, 6, 115, 0);
