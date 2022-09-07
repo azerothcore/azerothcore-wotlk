ad2b3b20531f2e4599cec52ce2ba0dd3bb89432e
Synful-Syn <hamecyn@gmail.com>
2022-09-07 18:24:37 -0400

    Use "IN"

--- data/sql/updates/pending_db_world/Remove-AQ40-idols-from-AQ20.sql
+++ data/sql/updates/pending_db_world/Remove-AQ40-idols-from-AQ20.sql
@@ -1,24 +1,24 @@
 DELETE FROM creature_loot_template
-WHERE (
-Item = 20874 #Idol of the Sun
-OR Item = 20875 #Idol of Night
-OR Item = 20876 #Idol of Death
-OR Item = 20877 #Idol of the Sage
-OR Item = 20878 #Idol of Rebirth
-OR Item = 20879 #Idol of Life
-OR Item = 20881 #Idol of Strife
-OR Item = 20882 #Idol of War
-) AND (
-Entry = 15318 #Hive'Zara Drone
-OR Entry = 15323 #Hive'Zara Sandstalker
-OR Entry = 15325 #Hive'Zara Wasp
-OR Entry = 15327 #Hive'Zara Stinger
-OR Entry = 15333 #Silicate Feeder
-OR Entry = 15336 #Hive'Zara Tail Lasher
-OR Entry = 15338 #Obsidian Destroyer
-OR Entry = 15343 #Qiraji Swarmguard
-OR Entry = 15355 #Anubisath Guardian
-OR Entry = 15386 #Major Yeggeth
-OR Entry = 15392 #Captain Tuubid
+WHERE Item IN (
+	20874, #Idol of the Sun
+	20875, #Idol of Night
+	20876, #Idol of Death
+	20877, #Idol of the Sage
+	20878, #Idol of Rebirth
+	20879, #Idol of Life
+	20881, #Idol of Strife
+	20882  #Idol of War
+) AND Entry IN (
+	15318, #Hive'Zara Drone
+	15323, #Hive'Zara Sandstalker
+	15325, #Hive'Zara Wasp
+	15327, #Hive'Zara Stinger
+	15333, #Silicate Feeder
+	15336, #Hive'Zara Tail Lasher
+	15338, #Obsidian Destroyer
+	15343, #Qiraji Swarmguard
+	15355, #Anubisath Guardian
+	15386, #Major Yeggeth
+	15392  #Captain Tuubid
 );
 
