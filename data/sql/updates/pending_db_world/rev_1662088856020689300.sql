64220e45ff4ddb0bfc6515ccbaa2be87dd48e9d1
Gultask <sagemochi@hotmail.com>
2022-09-06 22:49:16 -0300

    Update rev_1662088856020689300.sql

--- data/sql/updates/pending_db_world/rev_1662088856020689300.sql
+++ data/sql/updates/pending_db_world/rev_1662088856020689300.sql
@@ -35,15 +35,16 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (15247, 0, 2, 0, 0, 0, 100, 0, 8000, 11000, 14000, 17000, 0, 11, 26044, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Qiraji Brainwasher - In Combat - Cast \'Mind Flay\'');
 
 -- Vekniss Hive Crawler (15240) - Cmangos
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15240) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4, 5, 6));
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15240) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4, 5, 6, 7));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (15240, 0, 0, 0, 0, 0, 100, 0, 3000, 3000, 9000, 14000, 0, 11, 25809, 32, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - In Combat - Cast \'Crippling Poison\''),
 (15240, 0, 1, 0, 0, 0, 100, 0, 9000, 15000, 12000, 16000, 0, 11, 25810, 32, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - In Combat - Cast \'Mind-numbing Poison\''),
 (15240, 0, 2, 0, 0, 0, 100, 0, 7000, 11000, 10000, 14000, 0, 11, 26601, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - In Combat - Cast \'Poison Bolt\''),
-(15240, 0, 3, 0, 9, 1, 100, 0, 0, 5, 5000, 9000, 0, 11, 25051, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - Within 0-5 Range - Cast \'Sunder Armor\' (Phase 1)'),
+(15240, 0, 3, 0, 9, 1, 100, 0, 0, 5, 6000, 9000, 0, 11, 25051, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - Within 0-5 Range - Cast \'Sunder Armor\' (Phase 1)'),
 (15240, 0, 4, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - On Aggro - Set Event Phase 1'),
 (15240, 0, 5, 0, 24, 1, 100, 0, 25051, 5, 5000, 5000, 0, 22, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - On Target Buffed With \'Sunder Armor\' - Set Event Phase 2 (Phase 1)'),
-(15240, 0, 6, 0, 24, 2, 100, 0, 25051, 0, 5000, 5000, 0, 22, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - On Target Buffed With \'Sunder Armor\' - Set Event Phase 1 (Phase 2)');
+(15240, 0, 6, 0, 24, 2, 100, 0, 25051, 1, 5000, 5000, 0, 22, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - On Target Buffed With \'Sunder Armor\' - Set Event Phase 1 (Phase 2)'),
+(15240, 0, 7, 0, 7, 0, 100, 0, 0, 0, 0, 0, 0, 22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Hive Crawler - On Evade - Set Event Phase 0');
 
 -- Vekniss Guardian (15233) - Cmangos
 DELETE FROM `creature_text` WHERE `CreatureID`=15233 AND `GroupID`=1 AND `ID`=0;
@@ -53,7 +54,7 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Pro
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15233) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (15233, 0, 0, 1, 2, 0, 100, 1, 0, 20, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Guardian - Between 0-20% Health - Cast \'Enrage\' (No Repeat)'),
-(15233, 0, 1, 0, 61, 0, 100, 0, 0, 20, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Guardian - Between 0-20% Health - Say Line 0 (No Repeat)'),
+(15233, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Guardian - Between 0-20% Health - Say Line 0 (No Repeat)'),
 (15233, 0, 2, 0, 0, 0, 100, 0, 8000, 11000, 11000, 21000, 0, 11, 26025, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Guardian - In Combat - Cast \'Impale\''),
 (15233, 0, 3, 0, 4, 0, 50, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vekniss Guardian - On Aggro - Say Line 1 (No Repeat)');
 
@@ -97,7 +98,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15537) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (15537, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Anubisath Warrior - Between 0-30% Health - Cast \'Enrage\' (No Repeat)'),
-(15537, 0, 1, 0, 61, 0, 100, 0, 0, 30, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Anubisath Warrior - Between 0-30% Health - Say Line 0 (No Repeat)'),
+(15537, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Anubisath Warrior - Between 0-30% Health - Say Line 0 (No Repeat)'),
 (15537, 0, 2, 0, 0, 0, 100, 0, 7000, 11000, 9000, 13000, 0, 11, 15550, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Anubisath Warrior - In Combat - Cast \'Trample\''),
 (15537, 0, 3, 0, 9, 0, 100, 0, 0, 10, 7000, 11000, 0, 11, 10966, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Anubisath Warrior - In Combat - Cast \'Uppercut\'');
 
