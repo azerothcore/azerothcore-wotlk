b44132de5ab20400ab673e8f65f50f215a34448c
Maelthyrr <lynethris@protonmail.ch>
2022-09-08 09:26:15 +0200

    update codestyle

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2274,7 +2274,7 @@ public:
     void SendPetAIReaction(ObjectGuid guid);
     ///----------End of Pet responses methods----------
 
-    void propagateSpeedChange() { GetMotionMaster()->propagateSpeedChange(); }
+    void propagateSpeedChange() { GetMotionMaster()->PropagateSpeedChange(); }
 
     // reactive attacks
     void ClearAllReactives();
