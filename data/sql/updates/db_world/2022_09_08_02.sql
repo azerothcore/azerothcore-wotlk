d40e3340fb83c8477230269eb6656adaca5b9879
AzerothCoreBot <azerothcorebot@gmail.com>
2022-09-08 14:27:40 +0000

    chore(DB): import pending files
    Referenced commit(s): 331fab95fbe7113daea6ac4ad725e2d5e176a1d9

--- data/sql/updates/db_world/2022_09_08_02.sql
+++ data/sql/updates/db_world/2022_09_08_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_09_08_01 -> 2022_09_08_02
 --
 DELETE FROM `reference_loot_template` WHERE `Entry`=34024 AND `Item` BETWEEN 21281 AND 21283;
 INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Chance`, `GroupId`, `Comment`) VALUES
