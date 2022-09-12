64892b9ec7c13577f65c429f9aa4b8f93e336750
Gultask <100873791+Gultask@users.noreply.github.com>
2022-09-12 12:38:36 -0300

    Update rev_1662088856020689300.sql

--- data/sql/updates/pending_db_world/rev_1662088856020689300.sql
+++ data/sql/updates/pending_db_world/rev_1662088856020689300.sql
@@ -90,3 +90,11 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15538) AND (`source_type` = 0) AND (`id` IN (0));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (15538, 0, 0, 0, 9, 0, 100, 0, 0, 5, 5000, 7000, 0, 11, 40504, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Anubisath Swarmguard - Within 0-5 Range - Cast \'Cleave\'');
+
+-- Yauj Brood (15621) - Cmangos
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 15621;
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 15621) AND (`source_type` = 0) AND (`id` IN (0, 1));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(15621, 0, 0, 0, 9, 0, 100, 0, 0, 5, 9000, 13000, 0, 11, 25788, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Yauj Brood - Within 0-5 Range - Cast \'Head Butt\''),
+(15621, 0, 1, 0, 13, 0, 100, 0, 8000, 10000, 0, 0, 0, 11, 25788, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Yauj Brood - On Victim Casting - Cast \'Head Butt\'');
