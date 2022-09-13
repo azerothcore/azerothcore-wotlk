24d3ad238d26896888422df07e0cc533888c33e6
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-09-13 19:17:13 -0300

    Update rev_1662835816011805500.sql

--- data/sql/updates/pending_db_world/rev_1662835816011805500.sql
+++ data/sql/updates/pending_db_world/rev_1662835816011805500.sql
@@ -2,3 +2,6 @@
 DELETE FROM `creature_template_movement` WHERE `creatureId` = 15727;
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
 (15727, 1, 0, 0, 1, 0, 0, 0);
+
+DELETE FROM `areatrigger_scripts` WHERE `ScriptName` = "at_cthun_stomach_exit" AND `entry` = 4033;
+INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES (4033, "at_cthun_stomach_exit");
