8b3219ea8256b1005db85073a024b7cc8a2df3dc
AzerothCoreBot <azerothcorebot@gmail.com>
2022-09-19 00:49:51 +0000

    chore(DB): import pending files
    Referenced commit(s): 23c3b184c1f2ff45fdbe6c2d2403b6ce1656f644

--- data/sql/updates/db_world/2022_09_19_00.sql
+++ data/sql/updates/db_world/2022_09_19_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_09_17_01 -> 2022_09_19_00
 --
 UPDATE `creature_template` SET `unit_flags2`=0, `AiName`='', `ScriptName`='npc_obsidian_nullifier' WHERE `entry`=15312;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=15312 AND `source_type`=0;
