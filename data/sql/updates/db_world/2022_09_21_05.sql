62674c73a898dc34cf8d09cfefc3b9e1844dffcf
AzerothCoreBot <azerothcorebot@gmail.com>
2022-09-21 17:01:42 +0000

    chore(DB): import pending files
    Referenced commit(s): 814a8695d5d7f436f02784b1f5b4bbe0ec46854f

--- data/sql/updates/db_world/2022_09_21_05.sql
+++ data/sql/updates/db_world/2022_09_21_05.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_09_21_04 -> 2022_09_21_05
 --
 UPDATE `creature` SET `phaseMask`=16 WHERE `guid` IN (246011,246009);
 UPDATE `creature` SET `phaseMask`=8 WHERE `guid` IN (246036,246098);
