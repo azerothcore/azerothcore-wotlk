e273d6f4f0490b3a637dc738db8c9156053dd564
Grimgravy <sr.alansantos@hotmail.com>
2022-09-21 12:03:43 -0300

    fix(Scripts/Quest): improved egg freezing (#12575)

---
+++ data/sql/updates/pending_db_world/rev_1659051490224635700.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_item_freeze_rookery_egg';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(15748, 'spell_item_freeze_rookery_egg'), -- item
+(16028, 'spell_item_freeze_rookery_egg'); -- quest
