d99a4a473e2ed002f506632d3a490499e576dc91
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-09-24 10:32:16 -0300

    Update rev_1664025994910185400.sql

--- data/sql/updates/pending_db_world/rev_1664025994910185400.sql
+++ data/sql/updates/pending_db_world/rev_1664025994910185400.sql
@@ -1,5 +1,5 @@
 --
 DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8832;
-DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8815
+DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8815;
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES
 (15738, 8815);
