7400338ecb5b300dada30da2b8ae895b65c541ae
Grimgravy <sr.alansantos@hotmail.com>
2022-09-25 10:46:24 -0300

    fix(core/quest): improved egg collection (#12640)
    * fix(core/quest): improved egg collection
    
    * .
    
    * Update rev_1659836460796753400.sql
    
    * update

---
+++ data/sql/updates/pending_db_world/rev_1659836460796753400.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_q4735_collect_rookery_egg';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(15958, 'spell_q4735_collect_rookery_egg');
