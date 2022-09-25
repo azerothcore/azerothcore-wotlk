a404ae849b3516fe3fde7acd7b1115c70636115d
AzerothCoreBot <azerothcorebot@gmail.com>
2022-09-25 13:15:03 +0000

    chore(DB): import pending files
    Referenced commit(s): 2e92d1e993c1d4461ed4e7bcc5874403d9aff6d5

--- data/sql/updates/db_world/2022_09_25_00.sql
+++ data/sql/updates/db_world/2022_09_25_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_09_24_01 -> 2022_09_25_00
 --
 DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8832;
 DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8815;
