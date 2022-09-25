2e92d1e993c1d4461ed4e7bcc5874403d9aff6d5
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-09-25 10:12:58 -0300

    chore(DB/Quests): Correct wrong mark quest being added in 8fc055e44ec… (#13118)
    * chore(DB/Quests): Correct wrong mark quest being added in 8fc055e44ecc3de509bb0d5ee2f44a41b357a8eb
    
    * Update rev_1664025994910185400.sql

---
+++ data/sql/updates/pending_db_world/rev_1664025994910185400.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8832;
+DELETE FROM `creature_questender` WHERE `id` = 15738 AND `quest` = 8815;
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(15738, 8815);
