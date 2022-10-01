71dd01eaf670e895fe77ff3a41d71cdab604e948
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-10-01 17:11:02 -0300

    root several mobs

--- data/sql/updates/pending_db_world/rev_1664246616057619800.sql
+++ data/sql/updates/pending_db_world/rev_1664246616057619800.sql
@@ -1,4 +1,10 @@
 --
-DELETE FROM `creature_template_movement` WHERE `creatureId` = 21221;
+DELETE FROM `creature_template_movement` WHERE `creatureId` IN (21221, 15728, 15334, 15802, 15725, 15726);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
-(21221, 1, 0, 0, 1, 0, 0, 0);
+(21221, 1, 0, 0, 1, 0, 0, 0),
+(15728, 1, 0, 0, 1, 0, 0, 0),
+(15334, 1, 0, 0, 1, 0, 0, 0),
+(15802, 1, 0, 0, 1, 0, 0, 0),
+(15725, 1, 0, 0, 1, 0, 0, 0),
+(15726, 1, 0, 0, 1, 0, 0, 0);
+
