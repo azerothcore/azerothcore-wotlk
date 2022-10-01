34b714639c325f2e72e39f287bf37e580dfa3f45
AzerothCoreBot <azerothcorebot@gmail.com>
2022-10-01 20:38:07 +0000

    chore(DB): import pending files
    Referenced commit(s): 16856887db28c132ccf11cb2d87e054ccc45f567

--- data/sql/updates/db_world/2022_10_01_03.sql
+++ data/sql/updates/db_world/2022_10_01_03.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_10_01_02 -> 2022_10_01_03
 --
 DELETE FROM `creature_template_resistance` WHERE (`CreatureID` = 15339) AND (`School` IN (2, 3, 4, 5));
 INSERT INTO `creature_template_resistance` (`CreatureID`, `School`, `Resistance`, `VerifiedBuild`) VALUES
