16856887db28c132ccf11cb2d87e054ccc45f567
Gultask <100873791+Gultask@users.noreply.github.com>
2022-10-01 17:36:03 -0300

    fix(DB/creature_template): Fix Ossirian's resistances accidentally removed. (#13191)
    Create rev_1664637924608497900.sql

---
+++ data/sql/updates/pending_db_world/rev_1664637924608497900.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `creature_template_resistance` WHERE (`CreatureID` = 15339) AND (`School` IN (2, 3, 4, 5));
+INSERT INTO `creature_template_resistance` (`CreatureID`, `School`, `Resistance`, `VerifiedBuild`) VALUES
+(15339, 2, 1000, 0),
+(15339, 3, 1000, 0),
+(15339, 4, 1000, 0),
+(15339, 5, 1000, 0);
