e1c17170f421ebe891c2ac46d68ac1e7894cc16b
Nefertumm <nefertum.dev@protonmail.com>
2022-10-01 20:40:44 -0300

    fix(DB/Quest): The Alliance Needs Silk Bandages completion text

---
+++ data/sql/updates/pending_db_world/rev_1664667583199432200.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `quest_offer_reward` SET `RewardText` = '$C, you did it! Thanks to you we are another step closer to achieving our goal with silk bandages. I\'ll get them stored away, and then we can talk more if you like.' WHERE (`ID` = 8520);
