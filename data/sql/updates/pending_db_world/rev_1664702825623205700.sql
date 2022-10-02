67bf465ca7bdc8f4b030c63af692c68fe2dc49c6
UltraNix <80540499+UltraNix@users.noreply.github.com>
2022-10-02 16:52:48 +0200

    Update rev_1664702825623205700.sql

--- data/sql/updates/pending_db_world/rev_1664702825623205700.sql
+++ data/sql/updates/pending_db_world/rev_1664702825623205700.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|0x02000000 WHERE `entry` IN (15341,14834,11380);
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|0x02000000 WHERE `entry` IN (15341,14834,11380,12460);
