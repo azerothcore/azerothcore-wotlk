75ef70bd8f3a38800ad42d2f4ad656498c765629
Maelthyr <100411212+Maelthyrr@users.noreply.github.com>
2022-10-03 16:09:29 +0200

    Update rev_1664747895180268800.sql

--- data/sql/updates/pending_db_world/rev_1664747895180268800.sql
+++ data/sql/updates/pending_db_world/rev_1664747895180268800.sql
@@ -2,6 +2,3 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=-48516;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (-48516, 'spell_dru_eclipse');
-
--- set cooldown to 0, handled by a script
-UPDATE `spell_proc_event` SET `Cooldown`=0 WHERE `entry`=-48516; 
