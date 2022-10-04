5c5a8f9bde478689b9c2bffa7b70d012bae5bed5
Maelthyr <100411212+Maelthyrr@users.noreply.github.com>
2022-10-04 10:37:26 +0200

    Update rev_1664867262478614600.sql

--- data/sql/updates/pending_db_world/rev_1664867262478614600.sql
+++ data/sql/updates/pending_db_world/rev_1664867262478614600.sql
@@ -1,4 +1,5 @@
 --
-DELETE FROM `spell_script_names` WHERE `spell_id`=58428;
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (58428, 31666);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(58428, 'spell_rog_overkill');
+(58428, 'spell_rog_overkill'),
+(31666, 'spell_rog_master_of_subtlety');
