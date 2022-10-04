ec24a1e6393e4fe85a0e3205f8d88a750475692a
Awixs777 <gfy2302@gmail.com>
2022-10-04 04:19:11 +0300

    Revert "Revert "Merge branch 'azerothcore:master' into master""
    This reverts commit d0ece985685bddce289226365886cd4e893036e5.

--- src/server/scripts/Pet/pet_priest.cpp
+++ src/server/scripts/Pet/pet_priest.cpp
@@ -29,7 +29,7 @@
 enum PriestSpells
 {
     SPELL_PRIEST_GLYPH_OF_SHADOWFIEND       = 58228,
-    SPELL_PRIEST_GLYPH_OF_SHADOWFIEND_MANA  = 58227,
+    SPELL_PRIEST_SHADOWFIEND_DEATH          = 57989,
     SPELL_PRIEST_SHADOWFIEND_DODGE          = 8273,
     SPELL_PRIEST_LIGHTWELL_CHARGES          = 59907
 };
@@ -67,12 +67,10 @@ struct npc_pet_pri_shadowfiend : public PetAI
             AttackStart(target);
     }
 
-    void JustDied(Unit* /*killer*/) override
+    void IsSummonedBy(Unit* summoner) override
     {
-        if (me->IsSummon())
-            if (Unit* owner = me->ToTempSummon()->GetSummonerUnit())
-                if (owner->HasAura(SPELL_PRIEST_GLYPH_OF_SHADOWFIEND))
-                    owner->CastSpell(owner, SPELL_PRIEST_GLYPH_OF_SHADOWFIEND_MANA, true);
+        if (summoner->HasAura(SPELL_PRIEST_GLYPH_OF_SHADOWFIEND))
+            DoCastAOE(SPELL_PRIEST_SHADOWFIEND_DEATH);
     }
 };
 
