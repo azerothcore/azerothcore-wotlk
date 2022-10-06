d444fd927dfc16f0900ca322df17164c28087562
Angelo Venturini <nefertum.dev@protonmail.com>
2022-10-05 21:22:40 -0300

    Update boss_sartura.cpp

--- src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_sartura.cpp
+++ src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_sartura.cpp
@@ -34,7 +34,6 @@ enum Spells
     SPELL_ENRAGE                    = 8269,
     SPELL_BERSERK                   = 27680,
     SPELL_SUNDERING_CLEAVE          = 25174,
-
     // Sartura's Royal Guard
     SPELL_GUARD_WHIRLWIND           = 26038,
     SPELL_GUARD_KNOCKBACK           = 26027
