52353733636b82b77f38fb705775813035bde90d
AzerothCoreBot <azerothcorebot@gmail.com>
2022-10-06 01:23:08 +0000

    chore(DB): import pending files
    Referenced commit(s): c34cb0c54a2becaf28f4f6368a2f64c06bd874d7

--- data/sql/updates/db_world/2022_10_06_04.sql
+++ data/sql/updates/db_world/2022_10_06_04.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_10_06_03 -> 2022_10_06_04
 -- Horde "Trial of the Sea Lion" Quest ID 30 POI fix
 UPDATE `quest_poi` SET `WorldMapAreaId`=17 WHERE `QuestID`=30 AND `MapID`=1;
 UPDATE `quest_poi_points` SET `X`=1050, `Y`=-3119 WHERE `QuestID`=30 AND `Idx1`=2;
