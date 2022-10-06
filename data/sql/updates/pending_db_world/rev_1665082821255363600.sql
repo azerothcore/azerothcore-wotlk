ed6e7fad2b6ee70ebc372638c298052d4278226a
Nefertumm <nefertum.dev@protonmail.com>
2022-10-06 16:00:57 -0300

    fix(DB/Spells): Missing bonus data for paladin judgements

---
+++ data/sql/updates/pending_db_world/rev_1665082821255363600.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_bonus_data` WHERE `entry` = 54158;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(54158, 0.25, 0, 0.16, 0, 'Paladin - Judgement (Seal of Light, Seal of Wisdom, Seal of Justice)');
