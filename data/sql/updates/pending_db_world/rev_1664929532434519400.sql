339926b364c9349fc0792def52a7beed1a5c12e3
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-10-07 04:02:02 -0500

    Update data/sql/updates/pending_db_world/rev_1664929532434519400.sql
    Co-authored-by: Angelo Venturini <nefertum.dev@protonmail.com>

--- data/sql/updates/pending_db_world/rev_1664929532434519400.sql
+++ data/sql/updates/pending_db_world/rev_1664929532434519400.sql
@@ -4,4 +4,4 @@ UPDATE `creature` SET `id1`=15670, `id2`=15670, `id3`=15950 WHERE `id1` IN (1567
 UPDATE `creature` SET `id1`=15668, `id2`=15668, `id3`=15669 WHERE `id1` IN (15668, 15669);
 
 -- NPC Mmmrrrggglll (15937)'s spawn timer is just over 2 mins
-UPDATE `creature` SET `spawntimesecs`='130' WHERE  `guid`=41792;
+UPDATE `creature` SET `spawntimesecs`=130 WHERE  `guid`=41792;
