824ae326ea90668321f8e3f172ffcb9fbc5800c2
Angelo Venturini <nefertum.dev@protonmail.com>
2022-10-08 14:32:58 -0300

    Update data/sql/updates/pending_db_world/rev_1664322385002360300.sql

--- data/sql/updates/pending_db_world/rev_1664322385002360300.sql
+++ data/sql/updates/pending_db_world/rev_1664322385002360300.sql
@@ -5,4 +5,4 @@ UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Entry`=16517 AND `Item`=
 
 -- What Must Be Done... (9293) (DROP QUEST)  (DONE)
 -- Lasher Sample (item 16517): 50/50
-UPDATE `creature_loot_template` SET `Chance`=100 WHERE  `Entry`=16520 AND `Item`=22889 AND `Reference`=0 AND `GroupId`=0;
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Entry`=16520 AND `Item`=22889 AND `Reference`=0 AND `GroupId`=0;
