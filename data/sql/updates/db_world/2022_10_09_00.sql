667e584041c79c514591776bd34c5cc6e4d7755d
AzerothCoreBot <azerothcorebot@gmail.com>
2022-10-09 00:54:17 +0000

    chore(DB): import pending files
    Referenced commit(s): f5c7883d4142f0b52bbff934036083a429c44adb

--- data/sql/updates/db_world/2022_10_09_00.sql
+++ data/sql/updates/db_world/2022_10_09_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_10_08_00 -> 2022_10_09_00
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_cthun_dark_glare';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
