2caacf6edabfa29534d7ebf6c682563235cfa95c
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-10-08 21:08:29 -0300

    fix(Scripts/TempleOfAhnQiraj): Correct Dark Glare's radius

---
+++ data/sql/updates/pending_db_world/rev_1665274040139039300.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_cthun_dark_glare';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(26029, 'spell_cthun_dark_glare');
