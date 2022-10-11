e5d4a835a542e68e3c2dd933e8b6e6358c555d95
AzerothCoreBot <azerothcorebot@gmail.com>
2022-10-11 17:45:51 +0000

    chore(DB): import pending files
    Referenced commit(s): 4e3207db7f7185094c467c67d5e653349745fbf1

--- data/sql/updates/db_world/2022_10_11_02.sql
+++ data/sql/updates/db_world/2022_10_11_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_10_11_01 -> 2022_10_11_02
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_cthun_digestive_acid';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
