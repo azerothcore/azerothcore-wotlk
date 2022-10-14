c212ddedd9021fca1d5fd43a717bb85c97ed552c
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-10-14 19:33:43 -0300

    update eye combat reach

--- data/sql/updates/pending_db_world/rev_1665784753998414100.sql
+++ data/sql/updates/pending_db_world/rev_1665784753998414100.sql
@@ -1,2 +1,3 @@
 --
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` |256|512 WHERE `entry` IN (15910, 15904, 15896);
+UPDATE `creature_model_info` SET `CombatReach` = 20 WHERE displayID = 15556;
