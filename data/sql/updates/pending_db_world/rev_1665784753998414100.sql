851986e53c6bfab3d6ac332a17d69ab9b8df776d
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-10-14 20:50:37 -0300

    Update rev_1665784753998414100.sql

--- data/sql/updates/pending_db_world/rev_1665784753998414100.sql
+++ data/sql/updates/pending_db_world/rev_1665784753998414100.sql
@@ -1,3 +1,3 @@
 --
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` |256|512 WHERE `entry` IN (15910, 15904, 15896);
-UPDATE `creature_model_info` SET `CombatReach` = 20 WHERE displayID = 15556;
+UPDATE `creature_model_info` SET `CombatReach` = 15 WHERE `displayID` IN (15556, 15790, 15793);
