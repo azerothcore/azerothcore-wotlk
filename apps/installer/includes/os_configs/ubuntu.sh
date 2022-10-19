2805b24c21beb4d493c1126f8b0e3b6f737dbb0b
Helias <stefanoborzi32@gmail.com>
2022-10-20 00:15:29 +0200

    chore: update ubuntu deps

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -10,19 +10,19 @@ sudo apt update
 sudo apt-get -y install ccache clang cmake curl google-perftools libmysqlclient-dev make unzip
 
 if [[ $CONTINUOUS_INTEGRATION || $DOCKER ]]; then
-  sudo add-apt-repository -y ppa:mhier/libboost-latest && sudo apt update && sudo apt-get -y install build-essential cmake-data  \
-  libboost1.74-dev libbz2-dev libncurses5-dev libmysql++-dev libgoogle-perftools-dev libreadline6-dev libssl-dev libtool \
+  sudo apt update && sudo apt-get -y install build-essential cmake-data libboost-dev \
+  libbz2-dev libncurses5-dev libmysql++-dev libgoogle-perftools-dev libreadline6-dev libssl-dev libtool \
   openssl zlib1g-dev
 else
   case $UBUNTU_VERSION in
      "20.04")
        sudo apt-get install -y g++ gdb gdbserver gcc git \
-       libboost-all-dev libbz2-dev libncurses-dev libreadline-dev \
+       libboost-dev libbz2-dev libncurses-dev libreadline-dev \
        libssl-dev mysql-server
        ;;
      *)
-       sudo add-apt-repository -y ppa:mhier/libboost-latest && sudo apt update && sudo apt-get install -y g++ gdb gdbserver gcc git \
-       libboost-all-dev libbz2-dev libncurses-dev libreadline-dev \
+       sudo apt update && sudo apt-get install -y g++ gdb gdbserver gcc git \
+       libboost-dev libbz2-dev libncurses-dev libreadline-dev \
        libssl-dev mysql-server
        ;;
   esac
