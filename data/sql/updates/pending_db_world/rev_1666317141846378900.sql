f1481506cfe08bc80e75119c51b460494e8a0105
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-10-20 22:52:54 -0300

    fix(Scripts/TempleOfAhnQiraj): Huhuran's Wyvern Sting should hit the closest targets

---
+++ data/sql/updates/pending_db_world/rev_1666317141846378900.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_huhuran_poison_bolt' AND `spell_id` = 26180;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(26180, 'spell_huhuran_poison_bolt');
