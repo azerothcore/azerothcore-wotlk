e7c13e52359bb305ee05e1d20bd484608f0c2034
Yehonal <yehonal.azeroth@gmail.com>
2022-10-21 18:23:14 +0200

    feat: some needed implementations

---
+++ apps/ci/ci-gen-server-conf-files.sh
@@ -0,0 +1,16 @@
+APP_NAME=$1
+
+echo "LoginDatabaseInfo     = \"localhost;3306;root;root;acore_auth\"" >> ./env/dist/etc/$APP_NAME.conf
+
+if [[ $APP_NAME != "authserver" ]]; then
+    {
+        echo "WorldDatabaseInfo     = \"localhost;3306;root;root;acore_world\""
+        echo "CharacterDatabaseInfo = \"localhost;3306;root;root;acore_characters\""
+    } >> ./env/dist/etc/$APP_NAME.conf
+fi
+
+if [[ $APP_NAME == "worldserver" ]]; then
+    echo "DataDir = \"../data/\"" >> ./env/dist/etc/$APP_NAME.conf
+
+    git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./env/dist/data
+fi
