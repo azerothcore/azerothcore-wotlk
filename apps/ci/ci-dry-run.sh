d3187605ec9634be581a6b5ac648a218a74f2c05
Yehonal <yehonal.azeroth@gmail.com>
2022-10-22 13:15:33 +0200

    chore: test dry run with sudo

--- apps/ci/ci-dry-run.sh
+++ apps/ci/ci-dry-run.sh
@@ -7,7 +7,7 @@ CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Start mysql
 sudo systemctl start mysql
 
-source "$CURRENT_PATH/ci-gen-server-conf-files.sh" $1 "etc" "bin" "root"
+sudo source "$CURRENT_PATH/ci-gen-server-conf-files.sh" $1 "etc" "bin" "root"
 
 (cd ./env/dist/bin/ && timeout 5m ./$APP_NAME -dry-run)
 
