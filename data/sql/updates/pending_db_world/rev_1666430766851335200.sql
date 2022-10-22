37e3709d7245104033d5cc05f978863846b89acf
UltraNix <mariagefreek@gmail.com>
2022-10-22 11:26:55 +0200

    fix(DB/HallowsEnd): Fixed quest chaining of Fire Brigade Practice and Fire Training.
    Fixes #13439

---
+++ data/sql/updates/pending_db_world/rev_1666430766851335200.sql
@@ -0,0 +1,8 @@
+--
+UPDATE `quest_template_addon` SET `PrevQuestId`=0, `ExclusiveGroup`=11361 WHERE `Id`=11361;
+UPDATE `quest_template_addon` SET `PrevQuestId`=0, `ExclusiveGroup`=11361 WHERE `Id`=11449;
+UPDATE `quest_template_addon` SET `PrevQuestId`=0, `ExclusiveGroup`=11361 WHERE `Id`=11450;
+
+UPDATE `quest_template_addon` SET `PrevQuestId`=0, `ExclusiveGroup`=11360 WHERE `Id`=11360;
+UPDATE `quest_template_addon` SET `PrevQuestId`=0, `ExclusiveGroup`=11360 WHERE `Id`=11439;
+UPDATE `quest_template_addon` SET `PrevQuestId`=0, `ExclusiveGroup`=11360 WHERE `Id`=11440;
