9ef8466bcdf2b0553275c67c9fb83489eb542f04
Yehonal <yehonal.azeroth@gmail.com>
2022-10-22 12:14:13 +0200

    fix: data folder path

--- apps/ci/ci-gen-server-conf-files.sh
+++ apps/ci/ci-gen-server-conf-files.sh
@@ -1,6 +1,8 @@
 APP_NAME=$1
 CONFIG_FOLDER=${2:-"etc"}
-MYSQL_ROOT_PASSWORD=${3:-""}
+BIN_FOLDER=${3-"bin"}
+MYSQL_ROOT_PASSWORD=${4:-""}
+
 
 echo "LoginDatabaseInfo     = \"localhost;3306;root;$MYSQL_ROOT_PASSWORD;acore_auth\"" >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
 
@@ -13,7 +15,7 @@ if [[ $APP_NAME != "authserver" ]]; then
 fi
 
 if [[ $APP_NAME == "worldserver" ]]; then
-    echo "DataDir = \"../data/\"" >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
+    echo "DataDir = \"./data/\"" >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
 
-    git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./env/dist/data
+    git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./env/dist/$BIN_FOLDER/data
 fi
