a43a04d8d7d31a8214c35a7695a1141b0e8f61b4
UltraNix <mariagefreek@gmail.com>
2022-10-23 08:42:31 +0200

    fix(Scripts/Temple of AhnQiraj): Cthun Grasps should despawn on Cthun death.
    Fixes #13443

--- src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_cthun.cpp
+++ src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_cthun.cpp
@@ -546,6 +546,8 @@ struct boss_cthun : public BossAI
         {
             eye->DespawnOrUnsummon();
         }
+
+        instance->SetData(DATA_CTHUN_PHASE, PHASE_CTHUN_DONE);
     }
 
     void SummonedCreatureDies(Creature* creature, Unit* /*killer*/) override
