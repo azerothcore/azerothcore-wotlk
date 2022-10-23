5af3bbf03512ebed563f6269acb8846b2f80277b
Helias <stefanoborzi32@gmail.com>
2022-10-23 13:31:28 +0200

    fix: ci

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -4,17 +4,13 @@ fi
 
 UBUNTU_VERSION=$(lsb_release -sr);
 
-# if [[ $UBUNTU_VERSION == *"22.04"* ]]; then
-#   sed -i 's/^/#/' /etc/apt/sources.list.d/mhier-ubuntu-libboost-latest-jammy.list
-# fi
-
 sudo apt update
 
 # shared deps
 sudo apt-get -y install ccache clang cmake curl google-perftools libmysqlclient-dev make unzip
 
 if [[ $CONTINUOUS_INTEGRATION || $DOCKER ]]; then
-  sudo apt update && sudo apt-get -y install build-essential cmake-data libboost-dev \
+  sudo apt update && sudo apt-get -y install --no-install-recommends build-essential cmake-data libboost-dev \
   libbz2-dev libncurses5-dev libmysql++-dev libgoogle-perftools-dev libreadline6-dev libssl-dev libtool \
   openssl zlib1g-dev
 else
@@ -25,7 +21,7 @@ else
        libssl-dev mysql-server
        ;;
      *)
-       sudo apt update && sudo apt-get install -y g++ gdb gdbserver gcc git \
+       sudo apt update && sudo apt-get install -y --no-install-recommends g++ gdb gdbserver gcc git \
        libboost-dev libbz2-dev libncurses-dev libreadline-dev \
        libssl-dev mysql-server
        ;;
