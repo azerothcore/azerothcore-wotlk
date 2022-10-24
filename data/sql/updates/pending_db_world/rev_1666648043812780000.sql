31b942bb2f53a224d5706d4103c5cd26f2a30be1
Gultask <sagemochi@hotmail.com>
2022-10-24 19:09:34 -0300

    Update rev_1666648043812780000.sql

--- data/sql/updates/pending_db_world/rev_1666648043812780000.sql
+++ data/sql/updates/pending_db_world/rev_1666648043812780000.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_formations` SET `groupAI` = 523 WHERE `leaderGUID` = 87648 AND `memberGUID` IN (87649, 87650, 87651);
+UPDATE `creature_formations` SET `groupAI` = 539 WHERE `leaderGUID` = 87648 AND `memberGUID` IN (87648, 87649, 87650, 87651);
