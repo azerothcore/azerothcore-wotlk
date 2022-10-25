d5a7c2a86d245239a138872108c0fbb8fc777bcc
AzerothCoreBot <azerothcorebot@gmail.com>
2022-10-25 12:37:13 +0000

    chore(DB): import pending files
    Referenced commit(s): 77044670eade7f9160f4c12b768c8f0f25851b0a

--- data/sql/updates/db_world/2022_10_25_02.sql
+++ data/sql/updates/db_world/2022_10_25_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_10_25_01 -> 2022_10_25_02
 --
 DELETE FROM `creature_template_movement` WHERE `creatureId` = 15589;
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
