9aadb8e09491ca177a13c7ca11d9902f5aac6659
天鹿 <pkl_long@163.com>
2022-10-27 16:43:04 +0800

    Update PlayerGossip.cpp

--- src/server/game/Entities/Player/PlayerGossip.cpp
+++ src/server/game/Entities/Player/PlayerGossip.cpp
@@ -198,6 +198,11 @@ void Player::PrepareGossipMenu(WorldObject* source, uint32 menuId /*= 0*/, bool 
                 }
             }
 
+            if (strOptionText.empty())
+                strOptionText = itr->second.OptionText;
+            if (strBoxText.empty())
+                strBoxText = itr->second.BoxText;
+
             menu->GetGossipMenu().AddMenuItem(itr->second.OptionID, itr->second.OptionIcon, strOptionText, 0, itr->second.OptionType, strBoxText, itr->second.BoxMoney, itr->second.BoxCoded);
             menu->GetGossipMenu().AddGossipMenuItemData(itr->second.OptionID, itr->second.ActionMenuID, itr->second.ActionPoiID);
         }
