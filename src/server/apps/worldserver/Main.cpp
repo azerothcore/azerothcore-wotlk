4f2572f5ba9d070e36bfe8b75a883c9d7e5b4520
天鹿 <pkl_long@163.com>
2022-10-28 22:53:28 +0800

    Update Main.cpp

--- src/server/apps/worldserver/Main.cpp
+++ src/server/apps/worldserver/Main.cpp
@@ -404,7 +404,7 @@ bool StartDB()
         return false;
 
     ///- Get the realm Id from the configuration file
-    realm.Id.Realm = sConfigMgr->GetOption<uint32>("RealmID", 0);
+    realm.Id = sConfigMgr->GetOption<uint32>("RealmID", 0);
     if (!realm.Id)
     {
         LOG_ERROR("server.worldserver", "Realm ID not defined in configuration file");
