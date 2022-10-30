d3d6454c8e97ec8cba9b6ca78230c89ecd168891
Gultask <sagemochi@hotmail.com>
2022-10-30 12:21:48 -0300

    Update rev_1667138451660733600.sql

--- data/sql/updates/pending_db_world/rev_1667138451660733600.sql
+++ data/sql/updates/pending_db_world/rev_1667138451660733600.sql
@@ -70,13 +70,6 @@ DELETE FROM `creature_text` WHERE `CreatureID`=18677;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (18677,0,0,"%s becomes enraged!",16,0,100,0,0,0,10677,0,"Mekthorg the Wild");
 
--- Unyielding Knight (16906)
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 16906) AND (`source_type` = 0) AND (`id` IN (0, 1, 2));
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(16906, 0, 0, 0, 25, 0, 100, 1, 0, 0, 0, 0, 0, 11, 33900, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Unyielding Knight - On Reset - Cast \'Shroud of Death\' (No Repeat)'),
-(16906, 0, 1, 0, 4, 0, 75, 1, 0, 0, 0, 0, 0, 11, 22911, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Unyielding Knight - On Aggro - Cast \'Charge\' (No Repeat)'),
-(16906, 0, 2, 0, 12, 0, 100, 0, 0, 50, 22000, 22000, 0, 11, 33910, 0, 0, 0, 0, 0, 26, 40, 0, 0, 0, 0, 0, 0, 0, 'Unyielding Knight - Target Between 0-50% Health - Cast \'Heal Other\'');
-
 -- Arch Mage Xintor (16977)
 DELETE FROM `creature_template_addon` WHERE (`entry` = 16977);
 INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
