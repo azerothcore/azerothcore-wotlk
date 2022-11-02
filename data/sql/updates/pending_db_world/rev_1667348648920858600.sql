4e436e023271ee095561e6956fced2847ce7ab9f
BeefSteak <scottadams1996@live.co.uk>
2022-11-02 00:50:27 +0000

    Update rev_1667348648920858600.sql

--- data/sql/updates/pending_db_world/rev_1667348648920858600.sql
+++ data/sql/updates/pending_db_world/rev_1667348648920858600.sql
@@ -1,4 +1,4 @@
 --
-UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 5 WHERE (`creature_id` = 674, 675, 676, 677, 1094, 1095, 1096, 1097, 4260, 4723);
-UPDATE `creature_onkill_reputation` SET `MaxStanding1` = 5 WHERE (`creature_id` = 921);
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 5 WHERE `creature_id` IN (674, 675, 676, 677, 1094, 1095, 1096, 1097, 4260, 4723);
+UPDATE `creature_onkill_reputation` SET `MaxStanding1` = 5 WHERE `creature_id` IN  (921);
 
