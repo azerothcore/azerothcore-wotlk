d8596c3c0b1fb46ed2718040151be3863985bf10
BeefSteak <scottadams1996@live.co.uk>
2022-11-03 16:37:26 +0000

    Create rev_1667492399768442000.sql

---
+++ data/sql/updates/pending_db_world/rev_1667492399768442000.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `creature` WHERE `guid` =84701;
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(84701, 18583, 0, 0, 530, 0, 0, 1, 1, 0, -2918.13, 3419.44, 1.95939, 1.8675, 120, 0, 0, 4572, 2705, 0, 0, 0, 0, '', 0);
+
