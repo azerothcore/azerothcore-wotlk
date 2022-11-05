48bb3d396fcc2eb9adab1cf00d075e2e4f962697
BeefSteak <scottadams1996@live.co.uk>
2022-11-05 17:55:56 +0000

    Update data/sql/updates/pending_db_world/rev_1667666190202265500.sql
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1667666190202265500.sql
+++ data/sql/updates/pending_db_world/rev_1667666190202265500.sql
@@ -1,5 +1,5 @@
 -- Quest: A Pilgrim's Plight
-UPDATE `gameobject` SET `spawntimesecs`=0 WHERE `guid`=25340 AND `id`=184478;
+UPDATE `gameobject` SET `spawntimesecs`=1 WHERE `guid`=25340 AND `id`=184478;
 -- Quest: The Dread Relic
 UPDATE `gameobject` SET `spawntimesecs`=0 WHERE `guid`=26109 AND `id`=185220;
 -- Quest: The Shadow Tomb
