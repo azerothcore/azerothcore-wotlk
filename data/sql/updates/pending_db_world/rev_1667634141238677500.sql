36be274f77a38e64a61e385fc37d68216989348c
Mathematical <zajibrik@gmail.com>
2022-11-05 22:06:14 -0500

    Update rev_1667634141238677500.sql

--- data/sql/updates/pending_db_world/rev_1667634141238677500.sql
+++ data/sql/updates/pending_db_world/rev_1667634141238677500.sql
@@ -1,6 +1,6 @@
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17000;
 
-DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17000) (`id` IN (0, 1, 2, 3, 4, 5, 6, 7));
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17000) AND (`id` IN (0, 1, 2, 3, 4, 5, 6, 7));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17000, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 11, 24240, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Just Summoned - Cast \'Spawn - Red Lightning\''),
 (17000, 0, 1, 0, 54, 0, 100, 1, 0, 0, 0, 0, 0, 80, 1700000, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Just Summoned - Run Script (No Repeat)'),
