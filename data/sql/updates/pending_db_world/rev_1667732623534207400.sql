113af788f89bd348633618ae5bd7ab8f002e9c3e
Valdifer <raziel1990@gmail.com>
2022-11-06 12:05:05 +0100

    fix(DB/SAI): Missing Fire Brigade Locale eseS
    Quest id: 11360, 11439, 11440 were showing the objectiveText in english
    
    https://github.com/azerothcore/azerothcore-wotlk/issues/13676
    
    Valdifer

---
+++ data/sql/updates/pending_db_world/rev_1667732623534207400.sql
@@ -0,0 +1,14 @@
+-- Fire brigade esES / esMX
+INSERT INTO quest_offer_reward_locale (ID, locale, RewardText, VerifiedBuild) VALUES
+	(11360, 'esES', '¡Estás mojado y lleno de ceniza! ¡Tienes que haber estado apagando fuegos!$b$b¡Bien hecho! Como miembro honorífico de la brigada de bomberos, te ruego que regreses si alguna vez te necesitamos.', 0),
+	(11439, 'esES', '¡Estás mojado y lleno de ceniza! ¡Tienes que haber estado apagando fuegos!$b$b¡Bien hecho! Como miembro honorífico de la brigada de bomberos, te ruego que regreses si alguna vez te necesitamos.', 0),
+	(11440, 'esES', '¡Estás mojado y lleno de ceniza! ¡Tienes que haber estado apagando fuegos!$b$b¡Bien hecho! Como miembro honorífico de la brigada de bomberos, te ruego que regreses si alguna vez te necesitamos.', 0);
+	
+	UPDATE quest_template_locale SET ObjectiveText1 = 'Apaga fuegos' WHERE id = 11360 AND (locale = 'esES' OR locale = 'esMX');
+	UPDATE quest_template_locale SET ObjectiveText1 = 'Apaga fuegos' WHERE id = 11439 AND (locale = 'esES' OR locale = 'esMX');
+	UPDATE quest_template_locale SET ObjectiveText1 = 'Apaga fuegos' WHERE id = 11440 AND (locale = 'esES' OR locale = 'esMX');
+
+INSERT INTO quest_request_items_locale (ID, locale, CompletionText, VerifiedBuild) VALUES 
+	(11360, 'esES', 'Hola, $N. ¿Has estado perfeccionando tu técnica como bombero? ¡No te olvides del area de práctica al norte!', 0),
+	(11439, 'esES', 'Hola, $N. ¿Has estado perfeccionando tu técnica como bombero? ¡No te olvides del area de práctica al norte!', 0),
+	(11440, 'esES', 'Hola, $N. ¿Has estado perfeccionando tu técnica como bombero? ¡No te olvides del area de práctica al norte!', 0);
