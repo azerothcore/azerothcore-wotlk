5f21883649ad7abde019d97a4b000e74f3401c62
BeefSteak <scottadams1996@live.co.uk>
2022-11-06 17:43:14 +0000

    Update data/sql/updates/pending_db_world/repandspawn.sql
    Co-authored-by: M'Dic <joshua.lee.betts@gmail.com>

--- data/sql/updates/pending_db_world/repandspawn.sql
+++ data/sql/updates/pending_db_world/repandspawn.sql
@@ -1,4 +1,4 @@
---lots of Reputation changes
+-- lots of Reputation changes
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepFaction1`=87, `RewOnKillRepFaction2`=21, `MaxStanding1`=5, `IsTeamAward1`=0, `RewOnKillRepValue1`=5, `MaxStanding2`=7, `IsTeamAward2`=1, `RewOnKillRepValue2`=-25, `TeamDependent`=0 WHERE `creature_id`=26081;
 --Spawn to 8 hours
 UPDATE `creature` SET `spawntimesecs`=28800 WHERE `guid`=1975960 AND `id1`=26081;
