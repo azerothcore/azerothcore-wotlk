ac8b0e2b7d2d097cac3ff6356a4caa092d8c972b
BeefSteak <scottadams1996@live.co.uk>
2022-11-07 17:12:31 +0000

    Create questrepreqs.sql

---
+++ data/sql/updates/pending_db_world/questrepreqs.sql
@@ -0,0 +1,4 @@
+-- Watcher Leesaoh
+UPDATE `quest_template_addon` SET `RequiredMinRepFaction` = 942, `RequiredMinRepValue` = 3000 WHERE (`ID` = 9697);
+-- What's Wrong at Cenarion Thicket
+UPDATE `quest_template_addon` SET `RequiredMinRepFaction` = 942, `RequiredMinRepValue` = 3000 WHERE (`ID` = 9957);
