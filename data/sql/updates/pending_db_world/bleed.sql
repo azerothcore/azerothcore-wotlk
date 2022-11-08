c9e14fec38830dd2179d1de57def4ecd2c2aac4a
BeefSteak <scottadams1996@live.co.uk>
2022-11-08 18:37:43 +0000

    Update bleed.sql

--- data/sql/updates/pending_db_world/bleed.sql
+++ data/sql/updates/pending_db_world/bleed.sql
@@ -1,2 +1,2 @@
 -- Remove bleed immunity from Twilight Elementalist
-UPDATE `creature_template` SET `mechanic_immune_mask` = 645998431 WHERE (`entry` = 4814);
+UPDATE `creature_template` SET `mechanic_immune_mask`=645998431 WHERE `entry` = 4814;
