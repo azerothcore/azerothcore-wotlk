25ae7fba30988ba739df292876499b02339cbec2
Gultask <sagemochi@hotmail.com>
2022-11-08 14:19:57 -0300

    Update SpellInfoCorrections.cpp

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -51,7 +51,8 @@ void SpellMgr::LoadSpellInfoCorrections()
         53307,  // Thorns (Rank 8)
         53352,  // Explosive Shot (trigger)
         50783,  // Slam (Triggered spell)
-        20647   // Execute (Triggered spell)
+        20647,  // Execute (Triggered spell)
+        26476   // Digestive Acid
         }, [](SpellInfo* spellInfo)
     {
         spellInfo->AttributesEx3 |= SPELL_ATTR3_ALWAYS_HIT;
