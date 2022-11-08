0ca2994e04300a06e05795c3a5012709cccad54e
Gultask <sagemochi@hotmail.com>
2022-11-08 20:54:12 -0300

    Update rev_1667951025140607900.sql

--- data/sql/updates/pending_db_world/rev_1667951025140607900.sql
+++ data/sql/updates/pending_db_world/rev_1667951025140607900.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|33554432 WHERE `entry`=17280;
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|33554432 WHERE `entry` IN (17280, 18059);
