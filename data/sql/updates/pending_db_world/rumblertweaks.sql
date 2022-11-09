22c08fbfe3984030edc11ca0ddea2f8c8d805fdd
BeefSteak <scottadams1996@live.co.uk>
2022-11-09 20:34:29 +0000

    Update rumblertweaks.sql

--- data/sql/updates/pending_db_world/rumblertweaks.sql
+++ data/sql/updates/pending_db_world/rumblertweaks.sql
@@ -2,4 +2,4 @@
 UPDATE `creature_template` SET `subname` = 'Gelkis Earthcaller\'s Minion' WHERE (`entry` = 4661);
 -- Removes Rock Shell ability
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 4661;
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 4661) AND (`source_type` = 0)
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 4661) AND (`source_type` = 0);
