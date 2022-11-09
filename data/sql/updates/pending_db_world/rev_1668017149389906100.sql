da03bcbef098ff4b07d386757e261e85822efe7c
Gultask <sagemochi@hotmail.com>
2022-11-09 15:16:32 -0300

    Update rev_1668017149389906100.sql

--- data/sql/updates/pending_db_world/rev_1668017149389906100.sql
+++ data/sql/updates/pending_db_world/rev_1668017149389906100.sql
@@ -1,2 +1,4 @@
---
-UPDATE `creature_template` SET `unit_flags`=`unit_flags`|768 WHERE (`entry` = 8982);
+-- 
+DELETE FROM `creature_template_movement` WHERE (`CreatureId` = 8982);
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
+(8982, 1, 0, 0, 1, 0, 0, 0);
