78f1af141a7088d9278f4e82df5a92aed4e37e3e
BeefSteak <scottadams1996@live.co.uk>
2022-11-12 01:11:05 +0000

    Update data/sql/updates/pending_db_world/removegossip.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/removegossip.sql
+++ data/sql/updates/pending_db_world/removegossip.sql
@@ -1,3 +1,2 @@
 -- Remove incorrect gossip from Roetten Stonehammer
-UPDATE `creature_template` SET `gossip_menu_id` = 0 WHERE (`entry` = 5637);
-UPDATE `creature_template` SET `npcflag` = 2 WHERE (`entry` = 5637);
+UPDATE `creature_template` SET `npcflag` = 2, `gossip_menu_id` = 0 WHERE (`entry` = 5637);
