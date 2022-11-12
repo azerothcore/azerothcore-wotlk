c76cf9da9ea72b785ef07b4e49442c401c999ad1
BeefSteak <scottadams1996@live.co.uk>
2022-11-12 13:33:00 +0000

    Update removeloot.sql

--- data/sql/updates/pending_db_world/removeloot.sql
+++ data/sql/updates/pending_db_world/removeloot.sql
@@ -1,2 +1,2 @@
 -- Remove Syndicate emblems from Battered Junkbox and Worn Junkbox
-DELETE FROM `item_loot_template` WHERE (`Entry` = 16882 or `Entry` = 16883) AND (`Item` IN (17124));
+DELETE FROM `item_loot_template` WHERE `Entry` in (16882, 16883) AND (`Item` IN (17124));
