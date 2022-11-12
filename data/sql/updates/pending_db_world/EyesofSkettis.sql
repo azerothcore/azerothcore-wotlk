f5a17d2b72e08266be3273df4b56812eb80e09f6
BeefSteak <scottadams1996@live.co.uk>
2022-11-12 02:37:55 +0000

    fix(DB/Gameobject): instant spawn on Eyes of Skettis and remove Duplicate (#13721)

---
+++ data/sql/updates/pending_db_world/EyesofSkettis.sql
@@ -0,0 +1,6 @@
+-- Veil Reskk
+DELETE FROM `gameobject` WHERE (`id` = 185200) AND (`guid` IN (27551));
+UPDATE `gameobject` SET `spawntimesecs`=0 WHERE `guid`=26086 AND `id`=185200;
+-- Veil Shienor
+DELETE FROM `gameobject` WHERE (`id` = 182505) AND (`guid` IN (27550));
+UPDATE `gameobject` SET `spawntimesecs`=0 WHERE `guid`=26087 AND `id`=185201;
