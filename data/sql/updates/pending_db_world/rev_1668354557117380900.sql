8dcdf4cd7a7b903bebe190eddeb604e43783f344
UltraNix <mariagefreek@gmail.com>
2022-11-13 18:17:23 +0100

    Update.

--- data/sql/updates/pending_db_world/rev_1668354557117380900.sql
+++ data/sql/updates/pending_db_world/rev_1668354557117380900.sql
@@ -1,5 +1,17 @@
 --
+UPDATE `creature_template` SET `type_flags`=`type_flags`|0x08000000 WHERE `entry`=3443;
+
+DELETE FROM `gossip_menu_option` WHERE `MenuId`=10311;
+INSERT INTO `gossip_menu_option` VALUES
+(10311,0,1,'I want to browse your goods',3370,3,128,0,0,0,0,'',0,0);
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=10311;
+INSERT INTO `conditions` VALUES
+(15,10311,0,0,0,7,0,185,15,0,0,0,0,'','Show vendor flag if player has cooking'),
+(15,10311,0,0,0,8,0,862,0,0,0,0,0,'','Show vendor flag if player player has quest Dig Rat Stew rewarded');
+
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=23 AND `SourceGroup`=3443;
 INSERT INTO `conditions` VALUES
-(23,3443,44977,0,0,8,0,862,0,0,0,0,0,'','Sell Recipe: Dig Rat Stew if player has quest Dig Rat Stew rewarded'),
-(23,3443,5051,0,0,8,0,862,0,0,0,0,0,'','Sell Dig Rat if player has quest Dig Rat Stew rewarded');
+(23,3443,0,0,0,7,0,185,15,0,0,0,0,'','Show vendor flag if player has cooking'),
+(23,3443,0,0,0,8,0,862,0,0,0,0,0,'','Show vendor flag if player player has quest Dig Rat Stew rewarded');
+
