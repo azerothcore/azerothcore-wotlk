286a27c6a4d3cbff56304fabaa5c890793ddc9fe
Mathematical <zajibrik@gmail.com>
2022-11-17 12:51:48 -0500

    Update rev_1667634141238677500.sql
    I don't think that line was necessary

--- data/sql/updates/pending_db_world/rev_1667634141238677500.sql
+++ data/sql/updates/pending_db_world/rev_1667634141238677500.sql
@@ -1,6 +1,6 @@
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17000;
 
-DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17000) AND (`id` IN (0, 1, 2, 3, 4, 5, 6, 7));
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17000) AND (`id` IN (0, 1, 2, 3, 4, 5, 6));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17000, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 11, 24240, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Just Summoned - Cast \'Spawn - Red Lightning\''),
 (17000, 0, 1, 0, 54, 0, 100, 1, 0, 0, 0, 0, 0, 80, 1700000, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Just Summoned - Run Script (No Repeat)'),
@@ -8,8 +8,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (17000, 0, 3, 0, 60, 0, 100, 0, 7000, 7000, 0, 0, 0, 19, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Update - Remove Flags Not Attackable'),
 (17000, 0, 4, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 18, 256, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Just Summoned - Set Flags Immune To Players'),
 (17000, 0, 5, 0, 60, 0, 100, 0, 7000, 7000, 0, 0, 0, 19, 256, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Update - Remove Flags Immune To Players'),
-(17000, 0, 6, 0, 60, 0, 100, 0, 7000, 7000, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 17, 0, 20, 0, 0, 0, 0, 0, 0, 'Aggonis - On Update - Start Attacking'),
-(17000, 0, 7, 0, 5, 0, 100, 0, 0, 0, 1, 0, 0, 41, 1000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aggonis - On Killed Unit - Despawn In 1000 ms');
+(17000, 0, 6, 0, 60, 0, 100, 0, 7000, 7000, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 17, 0, 20, 0, 0, 0, 0, 0, 0, 'Aggonis - On Update - Start Attacking');
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 1700000) AND (`source_type` = 9) AND (`id` IN (0));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
