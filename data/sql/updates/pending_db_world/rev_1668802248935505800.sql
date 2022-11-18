b037016370237d2686e8d04449f79902eee9f378
郑佩茹 <zhengpeiru21@protonmail.com>
2022-11-18 13:16:31 -0700

    fix(DB/creature_template_locale): Ilkrud Magthrull name

---
+++ data/sql/updates/pending_db_world/rev_1668802248935505800.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature_template_locale` SET `Name` = 'Ilkrud Magthrull' WHERE `entry` = 3664 AND `locale` IN ('esES', 'esMX', 'frFR');
+UPDATE `creature_template_locale` SET `Name` = '伊克魯德·瑪格蘇爾' WHERE `entry` = 3664 AND `locale` = 'zhTW';
+UPDATE `creature_template_locale` SET `Name` = '일크루드 마그스룰' WHERE `entry` = 3664 AND `locale` = 'koKR';
