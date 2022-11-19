ef76a4c6b689b6f4d53322fb817598de781f6232
UltraNix <mariagefreek@gmail.com>
2022-11-19 15:45:24 +0100

    fix(Scripts/Instances): Fixed text during "Opening the Dark Portal" on Black Morass.
    Fixes #1999

---
+++ data/sql/updates/pending_db_world/rev_1668869079028371900.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_text` SET `text`='The shield is nearly gone! All that I have worked for is in danger!', `Sound`=10439, `BroadcastTextId`=16795 WHERE `CreatureID`=15608 AND `GroupId`=2;
+UPDATE `creature_text` SET `text`='Champions! My shield grows weak!', `Sound`=10437, `BroadcastTextId`=16792 WHERE `CreatureID`=15608 AND `GroupId`=4;
