40318ec6bf191bd3854d8f763a4e6f639d62a0d8
darkmord1991 <florian1@pwschmidt.de>
2022-11-20 20:46:30 +0100

    more faq

---
+++ Custom/Eluna scripts/faq progression.lua
@@ -0,0 +1,9 @@
+local cmd = "faq progression"
+
+local function OnCommand(event, player, command)
+    if command == cmd then
+        player:SendNotification("Right now Max Level is set to 80! It will be extended to the next progression step soon.")
+        end
+        return false
+end
+RegisterPlayerEvent(42, OnCommand)
