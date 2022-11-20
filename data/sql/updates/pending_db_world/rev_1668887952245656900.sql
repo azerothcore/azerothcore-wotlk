9a6843459c4ba7f7bcd0eaa585559af03c162a7f
UltraNix <80540499+UltraNix@users.noreply.github.com>
2022-11-20 13:45:46 +0100

    Update.

--- data/sql/updates/pending_db_world/rev_1668887952245656900.sql
+++ data/sql/updates/pending_db_world/rev_1668887952245656900.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (18498,18499,18500,18501) AND `source_type`=0 AND `event_type` IN (1, 7);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (18498,18499,18500,18501,18503) AND `source_type`=0 AND `event_type` IN (1, 7);
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=18497 AND `source_type`=0 AND `id`=6;
 INSERT INTO `smart_scripts` VALUES
