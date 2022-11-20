7228e3b55c4f79d50dda89923a095e2215db7d18
UltraNix <mariagefreek@gmail.com>
2022-11-20 14:36:21 +0100

    fix(DB/Dungeons): Caverns of Time: Old Hilsbrad should be available upon "Caverns of Time" completion.
    Fixes #13837

---
+++ data/sql/updates/pending_db_world/rev_1668951316420712600.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `dungeon_access_requirements` WHERE `dungeon_access_id` IN (62,63) AND `requirement_type`=1 AND `requirement_id`=10277;
+INSERT INTO `dungeon_access_requirements` VALUES
+(62,1,10277,'You must complete the quest "The Caverns of Time" before entering the Old Hillsbrad.',2,0,0,''),
+(63,1,10277,'You must complete the quest "The Caverns of Time" before entering the Old Hillsbrad (Heroic).',2,0,0,'');
