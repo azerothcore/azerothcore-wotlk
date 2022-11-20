d92e14b7ad96519071b29847a2d1f5b8795537bb
darkmord1991 <florian1@pwschmidt.de>
2022-11-20 20:12:41 +0100

    lua for FAQ number 1

---
+++ Custom/Eluna scripts/faq buff.lua
@@ -0,0 +1,9 @@
+local cmd = "faq buff"
+
+local function OnCommand(event, player, command)
+    if command == cmd then
+        player:SendNotification("Use .buff to get some buffs anywhere you are!")
+        end
+        return false
+end
+RegisterPlayerEvent(42, OnCommand)
