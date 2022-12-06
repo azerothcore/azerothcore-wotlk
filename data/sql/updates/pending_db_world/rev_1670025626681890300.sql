704485b62ec66a23d754138a3fc210738770dd9c
Gultask <100873791+Gultask@users.noreply.github.com>
2022-12-06 09:28:42 -0300

    fix(DB/Creature): Remove redundant Tamed Sporebat (18201) spawns (#14032)
    Create rev_1670025626681890300.sql

---
+++ data/sql/updates/pending_db_world/rev_1670025626681890300.sql
@@ -0,0 +1,2 @@
+-- Delete independent Tamed Sporebat. Should be spawned by the summoner instead.
+DELETE FROM `creature` WHERE `id1`=18201 AND `guid` IN (64977,64978,64979,64980,64981,64982,64983,64984,64985,64986,64987,64988,64989,64990,64991,64992,64993);
