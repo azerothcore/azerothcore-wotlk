82f4d46f3556ce318cd5f58b3d930e5e2bf62c22
Gultask <100873791+Gultask@users.noreply.github.com>
2022-12-06 14:28:33 -0300

    fix(DB/loot): Adjust AQ40 Idols and coffers (#13998)
    * Create rev_1669742453928322100.sql
    
    * Update rev_1669742453928322100.sql

---
+++ data/sql/updates/pending_db_world/rev_1669742453928322100.sql
@@ -0,0 +1,4 @@
+-- 4 rows out of 128 have QuestRequired set to 1. Also increase chances to match Cmangos.
+UPDATE `creature_loot_template` SET `QuestRequired`=0, `Chance`=0.5 WHERE `Item` IN (20874, 20875, 20876, 20877, 20878, 20879, 20881, 20882);
+-- Increase Large Scarab Coffer pool
+UPDATE `pool_template` SET `max_limit`=11 WHERE `entry`=1161 AND `description`='Large Scarab Coffers';
