80471e1e6c1f6c93c46752927fd5205ba58d1852
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-12-06 11:03:55 -0500

    fix(DB/GameEvents): Move Brewfest start dates. (#13136)

---
+++ data/sql/updates/pending_db_world/brewfest-building.sql
@@ -0,0 +1,2 @@
+UPDATE `game_event` SET `start_time`='2021-09-20 03:00:00' WHERE  `eventEntry`=24; -- Brewfest
+UPDATE `game_event` SET `start_time`='2021-09-17 03:00:00' WHERE  `eventEntry`=70; -- Brewfest building (IF)
