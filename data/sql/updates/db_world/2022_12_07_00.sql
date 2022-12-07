20da358afbd58064fdfc92c5dfdec29501bcdbff
AzerothCoreBot <azerothcorebot@gmail.com>
2022-12-07 16:24:21 +0000

    chore(DB): import pending files
    Referenced commit(s): 117212d78edf19506d126eea2c249bd7abec3b2d

--- data/sql/updates/db_world/2022_12_07_00.sql
+++ data/sql/updates/db_world/2022_12_07_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_12_06_45 -> 2022_12_07_00
 -- Starter
 DELETE FROM `creature_queststarter` WHERE (`quest` = 10259) AND (`id` IN (19942));
 INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
