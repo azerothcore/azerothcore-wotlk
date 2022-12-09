dc766b430c8e7ee1c90967a533d96b68b61b9584
AzerothCoreBot <azerothcorebot@gmail.com>
2022-12-09 16:16:50 +0000

    chore(DB): import pending files
    Referenced commit(s): cfad95cc9897d3546886d14e4c0507d86ff7cf8d

--- data/sql/updates/db_world/2022_12_09_00.sql
+++ data/sql/updates/db_world/2022_12_09_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_12_07_00 -> 2022_12_09_00
 DELETE FROM `playercreateinfo_action` WHERE `race` IN (1, 2, 3, 4, 5, 6, 7, 8, 10, 11);
 INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
   (1, 1, 96, 6603, 0),
