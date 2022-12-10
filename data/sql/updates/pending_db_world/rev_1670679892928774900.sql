310f311bea26cf67ef7259d778c2393bd99dedd1
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-12-10 19:57:15 -0300

    Update rev_1670679892928774900.sql

--- data/sql/updates/pending_db_world/rev_1670679892928774900.sql
+++ data/sql/updates/pending_db_world/rev_1670679892928774900.sql
@@ -47,7 +47,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 19407;
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 19407) AND (`source_type` = 0) AND (`id` IN (0, 1, 2));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(19407, 0, 0, 0, 2, 0, 100, 0, 40, 30, 0, 0, 0, 11, 13874, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Azuremyst Vindicator - Between 40-30% Health - Cast \'Divine Shield\''),
+(19407, 0, 0, 0, 2, 0, 100, 0, 30, 40, 0, 0, 0, 11, 13874, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Azuremyst Vindicator - Between 40-30% Health - Cast \'Divine Shield\''),
 (19407, 0, 1, 0, 0, 0, 100, 0, 3000, 6000, 12000, 16000, 0, 11, 13005, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Azuremyst Vindicator - In Combat - Cast \'Hammer of Justice\''),
 (19407, 0, 2, 0, 0, 0, 100, 0, 1000, 1000, 30000, 30000, 0, 11, 33127, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Azuremyst Vindicator - In Combat - Cast \'Seal of Command\'');
 
