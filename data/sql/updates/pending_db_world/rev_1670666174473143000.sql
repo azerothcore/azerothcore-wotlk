f0447b3fcffd41040c77e487532be1b58e46e33d
UltraNix <mariagefreek@gmail.com>
2022-12-10 10:57:15 +0100

    fix(Scripts/Spells): Fixed level requirements for Blade Ward and Blood Draining enchants.
    Fixed Blood Draining proc.
    Fixes #12131

---
+++ data/sql/updates/pending_db_world/rev_1670666174473143000.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `spell_script_names` SET `spell_id`=64579 WHERE `spell_id`=64568 AND `ScriptName`='spell_gen_proc_above_75';
+UPDATE `spell_script_names` SET `spell_id`=64441 WHERE `spell_id`=64440 AND `ScriptName`='spell_gen_proc_above_75';
