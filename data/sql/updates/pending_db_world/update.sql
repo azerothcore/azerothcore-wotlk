a3754879adab520fe604821991452ed5041d7975
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2022-12-11 17:30:11 -0600

    update

--- data/sql/updates/pending_db_world/update.sql
+++ data/sql/updates/pending_db_world/update.sql
@@ -1,4 +1,3 @@
 --
-UPDATE `creature_template` SET `flags_extra`=130 WHERE `entry`=31874;
-UPDATE `creature_template` SET `modelid1`=17200, `modelid2`=0 WHERE `entry` IN (31874,28235);
+UPDATE `creature_template` SET ``flags_extra`=`flags_extra`|130 WHERE `entry`=31874;
 
