ef01b57393108b1bec4f775e68e8563c04446af4
darkmord1991 <florian1@pwschmidt.de>
2022-12-11 20:40:00 +0100

    no 2

--- Custom/Custom Items/Itemsets/T11 Vendor.sql
+++ Custom/Custom Items/Itemsets/T11 Vendor.sql
@@ -100,5 +100,5 @@ INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `Exte
 INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (800005, 95, 81094, 0, 0, 0, 0);
 
 -- Extended Cost - 2500 coins for T11
--- Extended Cost ID = 3000
+-- Extended Cost ID = 2999
 update npc_vendor set ExtendedCost = 3000 where entry = 800005;
