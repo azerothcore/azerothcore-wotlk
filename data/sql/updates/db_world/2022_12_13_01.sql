3251e7f4219a5b573118b2d974da370b3c2e5f21
AzerothCoreBot <azerothcorebot@gmail.com>
2022-12-13 18:37:20 +0000

    chore(DB): import pending files
    Referenced commit(s): ae9bfaa291d3641d7283b38d365ea9604c061895

--- data/sql/updates/db_world/2022_12_13_01.sql
+++ data/sql/updates/db_world/2022_12_13_01.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_12_13_00 -> 2022_12_13_01
 --
 -- 3329 Spiked Wooden Plank too high rate on AC
 UPDATE `creature_loot_template` SET `Chance`=1.25 WHERE `Entry`=1753 AND `Item`=3329 AND `Reference`=0 AND `GroupId`=0;
