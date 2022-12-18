c8c1a8cbcef4cd78682c263b995bb8007f344d90
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-12-18 10:38:58 -0300

    fix(Core/Totem): Implement casting delay for the Fire Totem used by creatures

--- src/server/game/Entities/Totem/Totem.cpp
+++ src/server/game/Entities/Totem/Totem.cpp
@@ -82,14 +82,29 @@ void Totem::InitSummon()
     Minion::InitSummon();
 
     if (m_type == TOTEM_PASSIVE && GetSpell())
-        CastSpell(this, GetSpell(), true);
+    {
+        if (GetUInt32Value(UNIT_CREATED_BY_SPELL) == TotemSpellIds::FireTotemSpell)
+        {
+            m_Events.AddEventAtOffset([this]()
+            {
+                CastSpell(this, GetSpell(), true);
+            }, 4s);
+        }
+        else
+        {
+            CastSpell(this, GetSpell(), true);
+        }
+    }
 
     // Some totems can have both instant effect and passive spell
-    if(GetSpell(1))
+    if (GetSpell(1))
+    {
+        LOG_ERROR("sql.sql", "casting spell {}", GetSpell(1));
         CastSpell(this, GetSpell(1), true);
+    }
 
     // xinef: this is better than the script, 100% sure to work
-    if(GetEntry() == SENTRY_TOTEM_ENTRY)
+    if (GetEntry() == SENTRY_TOTEM_ENTRY)
     {
         SetReactState(REACT_AGGRESSIVE);
         GetOwner()->CastSpell(this, 6277, true);
@@ -132,7 +147,7 @@ void Totem::UnSummon(uint32 msTime)
 
         // Remove Sentry Totem Aura
         if (GetEntry() == SENTRY_TOTEM_ENTRY)
-            owner->RemoveAurasDueToSpell(SENTRY_TOTEM_SPELLID);
+            owner->RemoveAurasDueToSpell(TotemSpellIds::SentryTotemSpell);
 
         //remove aura all party members too
         if (Player* player = owner->ToPlayer())
