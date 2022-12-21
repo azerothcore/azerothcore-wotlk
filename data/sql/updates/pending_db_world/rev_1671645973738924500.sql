200752dd848e3377d3ba897e4c514d3709d6e6ba
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-12-21 12:07:42 -0600

    fix(db/npc_vendor): Improve two Alch Recipe Timers

---
+++ data/sql/updates/pending_db_world/rev_1671645973738924500.sql
@@ -0,0 +1,7 @@
+--
+-- Recipe: Super Mana Potion 30m
+UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=18005 AND `item`=22907 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=19837 AND `item`=22907 AND `ExtendedCost`=0;
+-- Recipe: Elixir of Major Frost Power 10m
+UPDATE `npc_vendor` SET `incrtime`=600 WHERE `entry`=18005 AND `item`=22902 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=600 WHERE `entry`=19837 AND `item`=22902 AND `ExtendedCost`=0;
