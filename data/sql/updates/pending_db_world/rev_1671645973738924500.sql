8a1cea9aace9aa7243c9e41be473ce9bb73db4f1
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-12-22 00:25:52 -0600

    fix(DB/Vendor): Reduce Super Mana Pot and Frost Power Potion Recipe timer (#14321)

---
+++ data/sql/updates/pending_db_world/rev_1671645973738924500.sql
@@ -0,0 +1,6 @@
+--
+-- Recipe: Super Mana Potion 30m
+UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry` IN (18005, 19837) AND `item`=22907 AND `ExtendedCost`=0;
+-- Recipe: Elixir of Major Frost Power 
+UPDATE `npc_vendor` SET `incrtime`=600 WHERE `entry`=18017 AND `item`=22902 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=600 WHERE `entry`=19837 AND `item`=22902 AND `ExtendedCost`=0;
