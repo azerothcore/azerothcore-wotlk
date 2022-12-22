95461e30d0982bb9797bdb057e79f3afa05345a1
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-12-22 00:26:53 -0600

    fix(DB/Vendor): Improve Xerintha Ravenoak vendor timers (#14319)

---
+++ data/sql/updates/pending_db_world/rev_1671639583415891400.sql
@@ -0,0 +1,4 @@
+--
+-- Xerintha Ravenoak vendor timers repaired
+UPDATE `npc_vendor` SET `incrtime`=300 WHERE `entry`=20916 AND `item`=31674 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=600 WHERE `entry`=20916 AND `item`=31675 AND `ExtendedCost`=0;
