b8eac24dd7785fd37881a8755497fd17d2f1fe07
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2022-12-28 00:24:01 +0100

    fix(Bash/Windows Install): Correct path to

--- apps/installer/includes/os_configs/windows.sh
+++ apps/installer/includes/os_configs/windows.sh
@@ -1,6 +1,6 @@
 # install chocolatey before
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 # install automatically following packages:
 # cmake
