28c6b42f86f9794a1db20dd79cf53dc7b3f2bcc0
Awixs777 <47874183+Awixs777@users.noreply.github.com>
2022-12-28 12:57:15 +0300

    Revert "Revert "upd""
    This reverts commit 9efc705b3e7d2329314a7ed7e1c62cafd2918d63.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14676,6 +14676,16 @@ Unit* Creature::SelectVictim()
         return nullptr;
     }
 
+    // Last chance: creature group
+    if (CreatureGroup* group = GetFormation())
+    {
+        if (Unit* groupTarget = group->GetNewTargetForMember(this))
+        {
+            SetInFront(groupTarget);
+            return groupTarget;
+        }
+    }
+
     // enter in evade mode in other case
     AI()->EnterEvadeMode();
 
