a2018edade7e0e655cfcd2157bb2afc46ceaf4af
AzerothCoreBot <azerothcorebot@gmail.com>
2022-12-29 17:41:50 +0000

    chore(DB): import pending files
    Referenced commit(s): b0c37eabefe311c5b234a37443bb0db3e8c68fdc

--- data/sql/updates/db_world/2022_12_29_10.sql
+++ data/sql/updates/db_world/2022_12_29_10.sql
@@ -1,3 +1,4 @@
+-- DB update 2022_12_29_09 -> 2022_12_29_10
 --
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 18134) AND (`Item` IN (24427));
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
