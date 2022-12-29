eca53e17be37d53691fceedb372d360c3927e1e8
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-12-29 18:47:43 -0500

    Adhere to the newly proposed SQL standard.

--- data/sql/updates/pending_db_world/bonelasher-oob.sql
+++ data/sql/updates/pending_db_world/bonelasher-oob.sql
@@ -1,1 +1,1 @@
-DELETE FROM `creature` WHERE `guid` IN (66458, 66459) AND `map` = 530 AND `spawntimesecs` = 300 AND `curhealth` = 5715;
+DELETE FROM `creature` WHERE `guid` IN (66458, 66459) AND `id1` = 18470;
