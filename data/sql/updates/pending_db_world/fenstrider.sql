6fdae29f95d195f161ee4115df86890593dcb03e
Samsequel <20357406+Samsequel@users.noreply.github.com>
2022-12-29 15:59:21 +0100

    questrequired

--- data/sql/updates/pending_db_world/fenstrider.sql
+++ data/sql/updates/pending_db_world/fenstrider.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 1) AND (`SourceGroup` = 18134) AND (`SourceEntry` = 24427) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 9) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 9801) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(1, 18134, 24427, 0, 0, 9, 0, 9801, 0, 0, 0, 0, 0, '', 'Fen Strider Tentacle Drop only when on quest: Gathering the Reagents');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 18134) AND (`Item` IN (24427));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(18134, 24427, 0, 20, 1, 1, 0, 1, 1, 'Fen Strider - Fen Strider Tentacle');
