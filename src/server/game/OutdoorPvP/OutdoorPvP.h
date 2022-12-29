2886a0951e270d08083e59b34dd813a04afe77a1
55Honey <71938210+55Honey@users.noreply.github.com>
2022-12-29 23:50:41 +0100

    Update OutdoorPvP.h

--- src/server/game/OutdoorPvP/OutdoorPvP.h
+++ src/server/game/OutdoorPvP/OutdoorPvP.h
@@ -136,7 +136,7 @@ public:
     void AddCre(uint32 type, ObjectGuid::LowType guid, uint32 entry = 0);
 
     bool SetCapturePointData(uint32 entry, uint32 map, float x, float y, float z, float o = 0,
-        float rotation0 = 0, float rotation1 = 0, float rotation2 = 0, float rotation3 = 0);
+                             float rotation0 = 0, float rotation1 = 0, float rotation2 = 0, float rotation3 = 0);
 
     void SetSlider(float slider)
     {
