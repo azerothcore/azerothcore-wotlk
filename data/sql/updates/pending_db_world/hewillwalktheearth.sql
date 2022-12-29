42456bc2737b5811798c6b695bc55bcdd6e352e1
Samsequel <20357406+Samsequel@users.noreply.github.com>
2022-12-29 11:38:39 +0100

    fix(DB/Questreward):  'He Will Walk The Earth' Should have quest rewards

---
+++ data/sql/updates/pending_db_world/hewillwalktheearth.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_template` SET `RewardChoiceItemID1` = 25574, `RewardChoiceItemQuantity1` = 1, `RewardChoiceItemID2` = 25575, `RewardChoiceItemQuantity2` = 1, `RewardChoiceItemID3` = 25576, `RewardChoiceItemQuantity3` = 1 WHERE (`ID` = 9866);
