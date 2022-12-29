e60daf03427aecbb00cf60a8ed3b50914c86795a
Samsequel <20357406+Samsequel@users.noreply.github.com>
2022-12-29 11:19:49 +0100

    Update fenstrider.sql

--- data/sql/updates/pending_db_world/fenstrider.sql
+++ data/sql/updates/pending_db_world/fenstrider.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 5) AND (`SourceGroup` = 18134) AND (`SourceEntry` = 24427) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 9) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 9801) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 1) AND (`SourceGroup` = 18134) AND (`SourceEntry` = 24427) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 9) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 9801) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(5, 18134, 24427, 0, 0, 9, 0, 9801, 0, 0, 0, 0, 0, '', 'Fen Strider Tentacle Drop only when on quest: Gathering the Reagents');
+(1, 18134, 24427, 0, 0, 9, 0, 9801, 0, 0, 0, 0, 0, '', 'Fen Strider Tentacle Drop only when on quest: Gathering the Reagents');
