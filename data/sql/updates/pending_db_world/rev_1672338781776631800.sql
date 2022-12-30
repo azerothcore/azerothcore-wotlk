95c877c2f4790130982b3bdf4207460970a715d6
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2022-12-30 06:44:04 -0600

    fix(DB/Loot): Improve Kil'sorrow gold drops (#14418)

---
+++ data/sql/updates/pending_db_world/rev_1672338781776631800.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature_template` SET `mingold`=276, `maxgold`=1836 WHERE `entry`=17148;
+UPDATE `creature_template` SET `mingold`=352, `maxgold`=1788 WHERE `entry`=17147;
+UPDATE `creature_template` SET `mingold`=303, `maxgold`=1924 WHERE `entry`=17146;
