4027a9551f2557a4c8d7d331301260a15048d839
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 16:23:57 +0100

    warmaul brute / warlock

--- data/sql/updates/pending_db_world/nagrandgoldfix.sql
+++ data/sql/updates/pending_db_world/nagrandgoldfix.sql
@@ -7,3 +7,5 @@ UPDATE `creature_template` SET `mingold` = 545, `maxgold` = 1838 WHERE (`entry` 
 UPDATE `creature_template` SET `mingold` = 266, `maxgold` = 1715 WHERE (`entry` = 17137);
 UPDATE `creature_template` SET `mingold` = 407, `maxgold` = 1582 WHERE (`entry` = 18064);
 UPDATE `creature_template` SET `mingold` = 341, `maxgold` = 1716 WHERE (`entry` = 17138);
+UPDATE `creature_template` SET `mingold` = 457, `maxgold` = 1743 WHERE (`entry` = 18065);
+UPDATE `creature_template` SET `mingold` = 280, `maxgold` = 1639 WHERE (`entry` = 18037);
