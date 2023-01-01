4d16ff472b74869612ad760ee7ad9baac57c7355
AzerothCoreBot <azerothcorebot@gmail.com>
2023-01-01 20:59:15 +0000

    chore(DB): import pending files
    Referenced commit(s): 626e68fdc05b041baf93944c43372cd64be37fdb

--- data/sql/updates/db_world/2023_01_01_05.sql
+++ data/sql/updates/db_world/2023_01_01_05.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_01_01_04 -> 2023_01_01_05
 --
 UPDATE `creature_template` SET `mingold` = 256, `maxgold` = 1723 WHERE (`entry` = 17135);
 UPDATE `creature_template` SET `mingold` = 274, `maxgold` = 1565 WHERE (`entry` = 17134);
