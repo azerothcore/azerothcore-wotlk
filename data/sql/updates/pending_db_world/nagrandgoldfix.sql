c80eea239f91da3978abc4944e960305653ed590
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 16:06:55 +0100

    boulderfist mage / warrior

--- data/sql/updates/pending_db_world/nagrandgoldfix.sql
+++ data/sql/updates/pending_db_world/nagrandgoldfix.sql
@@ -3,3 +3,5 @@ UPDATE `creature_template` SET `mingold` = 256, `maxgold` = 1723 WHERE (`entry` 
 UPDATE `creature_template` SET `mingold` = 274, `maxgold` = 1565 WHERE (`entry` = 17134);
 UPDATE `creature_template` SET `mingold` = 270, `maxgold` = 1554 WHERE (`entry` = 17139);
 UPDATE `creature_template` SET `mingold` = 303, `maxgold` = 1628 WHERE (`entry` = 17141);
+UPDATE `creature_template` SET `mingold` = 545, `maxgold` = 1838 WHERE (`entry` = 17136);
+UPDATE `creature_template` SET `mingold` = 266, `maxgold` = 1715 WHERE (`entry` = 17137);
