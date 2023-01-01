234b2fb60f887e50bbb8518d4a1e3eab05ed4a18
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 18:11:53 +0100

    fix(DB/Loot): Prospecting Adamantite adjustment (#14456)

---
+++ data/sql/updates/pending_db_world/prospectada.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `prospecting_loot_template` WHERE (`Entry` = 23425);
+INSERT INTO `prospecting_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(23425, 1, 13001, 100, 0, 1, 1, 1, 1, '(ReferenceTable)'),
+(23425, 2, 13002, 24, 0, 1, 1, 1, 1, '(ReferenceTable)'),
+(23425, 3, 13001, 15, 0, 1, 1, 1, 1, '(ReferenceTable)'),
+(23425, 24243, 0, 100, 0, 1, 0, 1, 1, 'Adamantite Powder');
