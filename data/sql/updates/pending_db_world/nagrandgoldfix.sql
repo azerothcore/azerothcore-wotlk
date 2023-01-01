9f833863b1c39eb5ac95a480a93328e12e23f42c
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 15:56:50 +0100

    windyreed

--- data/sql/updates/pending_db_world/nagrandgoldfix.sql
+++ data/sql/updates/pending_db_world/nagrandgoldfix.sql
@@ -1,3 +1,5 @@
 --
 UPDATE `creature_template` SET `mingold` = 256, `maxgold` = 1723 WHERE (`entry` = 17135);
 UPDATE `creature_template` SET `mingold` = 274, `maxgold` = 1565 WHERE (`entry` = 17134);
+UPDATE `creature_template` SET `mingold` = 270, `maxgold` = 1554 WHERE (`entry` = 17139);
+UPDATE `creature_template` SET `mingold` = 303, `maxgold` = 1628 WHERE (`entry` = 17141);
