17dd0507c6f9ed27100ed0978b8d3538456091ee
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 11:44:52 +0100

    fix(DB/Loot): Prospecting Adamantite adjustment

---
+++ data/sql/updates/pending_db_world/prospectada.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `prospecting_loot_template` WHERE (`Entry` = 23425) AND (`Item` IN (1, 2, 3));
+INSERT INTO `prospecting_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(23425, 1, 13001, 100, 0, 1, 1, 1, 2, '(ReferenceTable)'),
+(23425, 2, 13002, 24, 0, 1, 1, 1, 1, '(ReferenceTable)'),
+(23425, 3, 13001, 15, 0, 1, 2, 1, 2, '');
