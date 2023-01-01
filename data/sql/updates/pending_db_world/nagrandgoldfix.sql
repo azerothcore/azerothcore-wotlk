c596424ea304e82339447048d6afca9ee1824f48
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 15:42:05 +0100

    fix(DB/loot): Nagrand gold drop fixes

---
+++ data/sql/updates/pending_db_world/nagrandgoldfix.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_template` SET `mingold` = 256, `maxgold` = 1723 WHERE (`entry` = 17135);
+UPDATE `creature_template` SET `mingold` = 274, `maxgold` = 1565 WHERE (`entry` = 17134);
