23e6e8e23d3eaa3473d7e790d530010587d515f4
UltraNix <mariagefreek@gmail.com>
2023-01-01 16:42:39 +0100

    fix(DB/SAI): Swamplord Musel'ek prefers ranged attacks than melee.
    Fixes #13990

---
+++ data/sql/updates/pending_db_world/rev_1672587681948762700.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `smart_scripts` WHERE `entryorguid`=17826 AND `source_type`=0 AND `id`=15;
+INSERT INTO `smart_scripts` VALUES
+(17826,0,15,0,0,0,100,0,0,0,2000,2500,0,11,22907,64,0,0,0,0,2,0,0,0,0,0,0,0,0,'Swamplord Musel\'ek - In Combat - Cast Shoot');
