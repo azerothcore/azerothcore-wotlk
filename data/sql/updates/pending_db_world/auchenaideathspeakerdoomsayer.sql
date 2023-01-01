626e68fdc05b041baf93944c43372cd64be37fdb
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-01-01 21:57:14 +0100

    fix(DB/Loot): Auchenai Death-Speaker/Doomsayer money drop (#14461)

---
+++ data/sql/updates/pending_db_world/auchenaideathspeakerdoomsayer.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_template` SET `mingold` = 270, `maxgold` = 1611 WHERE (`entry` = 21285);
+UPDATE `creature_template` SET `mingold` = 259, `maxgold` = 1547 WHERE (`entry` = 21242);
