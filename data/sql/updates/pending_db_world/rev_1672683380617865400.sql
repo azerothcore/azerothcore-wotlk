572b0a77ae22fe9f8278c6b2f1476e832c036ecd
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2023-01-02 12:33:02 -0600

    Update data/sql/updates/pending_db_world/rev_1672683380617865400.sql
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1672683380617865400.sql
+++ data/sql/updates/pending_db_world/rev_1672683380617865400.sql
@@ -1,8 +1,6 @@
 --
 -- Recipe: Elixir of Major Frost Power 
-UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=18017 AND `item`=22902 AND `ExtendedCost`=0;
-UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=18005 AND `item`=22902 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry` IN (18005, 18017) AND `item`=22902 AND `ExtendedCost`=0;
 
 -- Tailoring Recipes off Aarond
-UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=19521 AND `item`=21900 AND `ExtendedCost`=0;
-UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=19521 AND `item`=21901 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=19521 AND `item` IN (21900, 21901) AND `ExtendedCost`=0;
