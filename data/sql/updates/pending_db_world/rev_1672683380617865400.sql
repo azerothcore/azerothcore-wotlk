d9f325a10094dba719682e71bd62d25dff53d8cd
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2023-01-02 12:29:17 -0600

    Update rev_1672683380617865400.sql
    bloopers removed

--- data/sql/updates/pending_db_world/rev_1672683380617865400.sql
+++ data/sql/updates/pending_db_world/rev_1672683380617865400.sql
@@ -1,7 +1,7 @@
 --
 -- Recipe: Elixir of Major Frost Power 
 UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=18017 AND `item`=22902 AND `ExtendedCost`=0;
-UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=19837 AND `item`=22902 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=18005 AND `item`=22902 AND `ExtendedCost`=0;
 
 -- Tailoring Recipes off Aarond
 UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry`=19521 AND `item`=21900 AND `ExtendedCost`=0;
