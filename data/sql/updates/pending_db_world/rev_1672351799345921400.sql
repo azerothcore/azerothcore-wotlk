a7373f1f2da29ded6dd8a1323e4419fc52c9173b
Nefertumm <nefertum.dev@protonmail.com>
2023-01-02 19:23:44 -0300

    fix heroic mushroom

--- data/sql/updates/pending_db_world/rev_1672351799345921400.sql
+++ data/sql/updates/pending_db_world/rev_1672351799345921400.sql
@@ -7,13 +7,15 @@ UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|3
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 17770 AND `source_type` = 0;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 17990 AND `source_type` = 0;
 
-DELETE FROM `creature_template_addon` WHERE `entry` = 17990;
+DELETE FROM `creature_template_addon` WHERE `entry` IN (17990, 20189);
 INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES
-(17990, '31690');
+(17990, '31690'),
+(20189, '31690');
 
-DELETE FROM `creature_template_movement` WHERE `CreatureId` = 17990;
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (17990, 20189);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`) VALUES
-(17990, 0, 0, 0, 1, 0, 0);
+(17990, 0, 0, 0, 1, 0, 0),
+(20189, 0, 0, 0, 1, 0, 0);
 
 UPDATE `spell_dbc` SET `Effect_1` = 28, `EffectMiscValueB_1` = 64 WHERE `ID` = 31692;
 
