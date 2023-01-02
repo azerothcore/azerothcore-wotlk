0dec381997b638cade7b1436707134b00d72f144
AzerothCoreBot <azerothcorebot@gmail.com>
2023-01-02 18:41:35 +0000

    chore(DB): import pending files
    Referenced commit(s): 26b36a5af10ca18303f5a639d532b418d14a30b7

--- data/sql/updates/db_world/2023_01_02_02.sql
+++ data/sql/updates/db_world/2023_01_02_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_01_02_01 -> 2023_01_02_02
 --
 -- Recipe: Elixir of Major Frost Power 
 UPDATE `npc_vendor` SET `incrtime`=1800 WHERE `entry` IN (18005, 18017) AND `item`=22902 AND `ExtendedCost`=0;
