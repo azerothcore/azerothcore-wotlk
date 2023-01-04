bb5ceb5e34ba825565249fb5dee393822adac38a
Gultask <sagemochi@hotmail.com>
2023-01-04 01:04:02 -0300

    remove leftover

--- data/sql/updates/pending_db_world/rev_1672333160913910400.sql
+++ data/sql/updates/pending_db_world/rev_1672333160913910400.sql
@@ -8,15 +8,6 @@ INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `b
 (20342, 0, 0, 0, 1, 379, 0, ''),
 (20344, 0, 0, 0, 1, 379, 0, '');
 
--- 0 Hal...
--- 2500 Yea, Nat
--- 2000 Had that dream again
--- 5500 Which one
--- 2800 Pt
--- 6000 Pt
--- 6000 Pt
--- 8500 Shut up and fish, Nat
-
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 20344);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (20344, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Nat Pagle - On Reset - Set Event Phase 1'),
