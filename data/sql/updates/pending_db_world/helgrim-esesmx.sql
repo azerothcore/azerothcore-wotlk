d45e9e7390b7bf7c3bbc3739aabbc96e8a514ae2
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-01-05 18:53:55 -0500

    fix(DB/quest_greeting_locale): Add esES/esMX quest greeting to Apothecary Helbrim. (#14482)
    * Create helgrim-esesmx.sql
    
    * We need to come together and ban semicolons.

---
+++ data/sql/updates/pending_db_world/helgrim-esesmx.sql
@@ -0,0 +1,3 @@
+INSERT INTO `quest_greeting_locale` (`ID`, `type`, `locale`, `Greeting`, `VerifiedBuild`) VALUES
+(3390, 0, 'esES', 'Los Baldíos cuentan con una gran riqueza de sustancias de las que nosotros, los boticarios de Lordaeron, podemos aprovecharnos.', 47014),
+(3390, 0, 'esMX', 'Los Baldíos cuentan con una gran riqueza de sustancias de las que nosotros, los boticarios de Lordaeron, podemos aprovecharnos.', 47014);
