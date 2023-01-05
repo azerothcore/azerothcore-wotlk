5f22400c36574471c1751de6c8702e93c7a43a28
benjymansy123 <benjymansy123@gmail.com>
2023-01-05 12:49:35 -0500

    INDENTATION

--- data/sql/updates/pending_db_world/redo-broadcast-text.sql
+++ data/sql/updates/pending_db_world/redo-broadcast-text.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `broadcast_text`
-	RENAME COLUMN `SoundEntriesId`				TO `SoundEntriesID`;
+	RENAME COLUMN `SoundEntriesId`	TO `SoundEntriesID`;
 ALTER TABLE `broadcast_text_locale`
-	RENAME COLUMN `locale`						TO `Locale`;
+	RENAME COLUMN `locale`			TO `Locale`;
