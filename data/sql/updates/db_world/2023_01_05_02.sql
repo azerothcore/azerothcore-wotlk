56d9bc8a3dd5155914e1f9a2215dd228eaeabfb2
AzerothCoreBot <azerothcorebot@gmail.com>
2023-01-05 23:55:59 +0000

    chore(DB): import pending files
    Referenced commit(s): d45e9e7390b7bf7c3bbc3739aabbc96e8a514ae2

--- data/sql/updates/db_world/2023_01_05_02.sql
+++ data/sql/updates/db_world/2023_01_05_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_01_05_01 -> 2023_01_05_02
 INSERT INTO `quest_greeting_locale` (`ID`, `type`, `locale`, `Greeting`, `VerifiedBuild`) VALUES
 (3390, 0, 'esES', 'Los Baldíos cuentan con una gran riqueza de sustancias de las que nosotros, los boticarios de Lordaeron, podemos aprovecharnos.', 47014),
 (3390, 0, 'esMX', 'Los Baldíos cuentan con una gran riqueza de sustancias de las que nosotros, los boticarios de Lordaeron, podemos aprovecharnos.', 47014);
