bef5b6eade7f652561da746505792f837ac8addf
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2023-01-06 03:05:43 -0600

    fix(DB/creature_loot): fix two quest rates in EK
    Worn Stone Tokens are 100%
    Encrusted Tail Fins should be 100% and only off Saltscale Murlocs

---
+++ data/sql/updates/pending_db_world/rev_1672995772284756600.sql
@@ -0,0 +1,7 @@
+--
+-- Worn Stone Tokens are 100%
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Item`=3714 AND `entry` IN (2271, 2272, 2358, 2415, 2628);
+
+-- Encrusted Tail Fins should be 100% and only off Saltscale Murlocs
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Item`=5796 AND `entry` IN (871, 873, 875, 877, 879);
+DELETE FROM `creature_loot_template` WHERE  `Entry` IN (4457, 4458, 4459, 4460, 4461) AND `Item`=5796 AND `Reference`=0 AND `GroupId`=0;
