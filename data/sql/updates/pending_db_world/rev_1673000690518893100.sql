918a9c9100b30ed437b02bc4e5ef15e57fd5880c
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-01-06 11:27:55 +0100

    fix(Core/Spell): Roll 'dem Bones
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/14357
    
    * Partial cp of https://github.com/TrinityCore/TrinityCore/commit/029527365f1763c45e8668c16ecd25ba7553bb22
    
    Co-Authored-By: joschiwald <736792+joschiwald@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1673000690518893100.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = 47776 AND `ScriptName` = 'spell_item_worn_troll_dice';
+INSERT INTO `spell_script_names` VALUES
+(47776, 'spell_item_worn_troll_dice');
