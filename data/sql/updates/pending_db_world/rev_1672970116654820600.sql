97a01b430dd3f2b0ba4d6c63bb5c0d7c4f5c5cab
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-01-07 23:08:19 +0100

    fix(Script/Item): Elixir of Shadows for pets (#14514)
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/13067

---
+++ data/sql/updates/pending_db_world/rev_1672970116654820600.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id`=37678 AND `ScriptName`='spell_item_elixir_of_shadows';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(37678, 'spell_item_elixir_of_shadows');
