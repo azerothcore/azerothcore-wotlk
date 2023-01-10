527a97b9bc030c3b4d2d8c41ea2692ba5dc3c3aa
BeefSteak <scottadams1996@live.co.uk>
2023-01-10 18:14:03 +0000

    Update rev_1673373739618220000.sql

--- data/sql/updates/pending_db_world/rev_1673373739618220000.sql
+++ data/sql/updates/pending_db_world/rev_1673373739618220000.sql
@@ -1,2 +1,2 @@
 -- Make infernal hound Demon
-UPDATE `creature_template` SET `type` = 3 WHERE (`entry` = 19207);
+UPDATE `creature_template` SET `type` = 3, `type_flags` = 0 WHERE (`entry` = 19207);
