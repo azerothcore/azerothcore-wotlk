99974c6a81dcddbb27606cbeb590d37dc0ac2a44
AzerothCoreBot <azerothcorebot@gmail.com>
2023-01-16 01:03:41 +0000

    chore(DB): import pending files
    Referenced commit(s): 95ae8d3feb3d922ef94bfaa54ccc7f7f0b25f9aa

--- data/sql/updates/db_world/2023_01_16_00.sql
+++ data/sql/updates/db_world/2023_01_16_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_01_15_00 -> 2023_01_16_00
 DELETE FROM `quest_request_items_locale` WHERE `ID` IN (6387, 6391) AND `locale` = 'deDE';
 INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `VerifiedBuild`) VALUES
 (6387, 'deDE', 'Habt Ihr schon so bald die Nase voll von Thelsamar? Seid Ihr bereit, in eine andere Stadt abzureisen?', 0),
