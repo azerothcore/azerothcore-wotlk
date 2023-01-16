5a595b80075b16bf24b5072037675063f4805dc0
ICXCNIKA <84076297+ohkasdasf@users.noreply.github.com>
2023-01-16 02:01:19 +0100

    fix(DB/Locale): deDE locales fixed for quests 6381 and 6391 (#14607)
    * fix(DB/Locale): deDE locales fixed for quests 6381 and 6391 
    
    This is an example for a pull request for AzerothCore.
    
    * Update 6387-6391-example.sql

---
+++ data/sql/updates/pending_db_world/6387-6391-example.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest_request_items_locale` WHERE `ID` IN (6387, 6391) AND `locale` = 'deDE';
+INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `VerifiedBuild`) VALUES
+(6387, 'deDE', 'Habt Ihr schon so bald die Nase voll von Thelsamar? Seid Ihr bereit, in eine andere Stadt abzureisen?', 0),
+(6391, 'deDE', 'Womit kann ich Euch dienen, $Gwerter Herr:werte Dame;?', 0);
