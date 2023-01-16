edcfe8ff0f85d5895d82bc7ba53d88e0ea31b017
UltraNix <80540499+UltraNix@users.noreply.github.com>
2023-01-16 02:00:22 +0100

    DB/SAI: Bog Giant and Underbog Lord uses Growth only in Heroic mode. (#14542)
    * DB/SAI: Bog Giant and Underbog Lord uses Growth only in Heroic mode.
    
    Fixes #13978
    
    * Update.

---
+++ data/sql/updates/pending_db_world/rev_1673093922693808500.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `event_flags`=4 WHERE `entryorguid`=17723 AND `source_type`=0 AND `id`=1;
+UPDATE `smart_scripts` SET `event_flags`=4 WHERE `entryorguid`=17734 AND `source_type`=0 AND `id`=1;
