08df7e2452327e993a615b0d85a151a7617d0d36
Foereaper <foereaper@elunatech.com>
2023-01-19 01:39:24 +0100

    Rewrite conf file bash script
    This should use the standard .dest file as a template

--- apps/ci/ci-gen-server-conf-files.sh
+++ apps/ci/ci-gen-server-conf-files.sh
@@ -3,58 +3,13 @@ CONFIG_FOLDER=${2:-"etc"}
 BIN_FOLDER=${3-"bin"}
 MYSQL_ROOT_PASSWORD=${4:-""}
 
+# copy dest files to conf files
+cp ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf.dest ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
 
-echo "LoginDatabaseInfo     = \"localhost;3306;root;$MYSQL_ROOT_PASSWORD;acore_auth\"" >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
-
-# worldserver or dbimport
-if [[ $APP_NAME != "authserver" ]]; then
-    {
-        echo "WorldDatabaseInfo     = \"localhost;3306;root;$MYSQL_ROOT_PASSWORD;acore_world\""
-        echo "CharacterDatabaseInfo = \"localhost;3306;root;$MYSQL_ROOT_PASSWORD;acore_characters\""
-		# Required for database update check
-		echo "Updates.EnableDatabases = 7"
-		# Realm ID required for startup
-		echo "RealmID = 1"
-		# Loggers required for log error checking
-		echo "Appender.Console=1,4,0,\"1 9 3 6 5 8\""
-		echo "Appender.Server=2,5,0,Server.log,w"
-		echo "Appender.GM=2,5,15,gm_%s.log"
-		echo "Appender.DBErrors=2,5,0,DBErrors.log"
-		echo "Logger.root=2,Console Server"
-		echo "Logger.commands.gm=4,Console GM"
-		echo "Logger.diff=3,Console Server"
-		echo "Logger.mmaps=4,Server"
-		echo "Logger.scripts.hotswap=4,Console Server"
-		echo "Logger.server=4,Console Server"
-		echo "Logger.sql.sql=2,Console DBErrors"
-		echo "Logger.sql=4,Console Server"
-		echo "Logger.time.update=4,Console Server"
-		echo "Logger.module=4,Console Server"
-		# Some default values just to reduce the amount of "errors" in the logs
-		echo "MySQLExecutable = \"\""
-		echo "TempDir = \"\""
-		echo "SourceDirectory = \"\""
-		echo "Creatures.CustomIDs = \"\""
-    } >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
-fi
-
-if [[ $APP_NAME == "authserver" ]]; then
-	{
-		# Required for database update check
-		echo "Updates.EnableDatabases = 1"
-		# Loggers required for log error checking
-		echo "Appender.Console=1,5,0,\"1 9 3 6 5 8\""
-		echo "Appender.Auth=2,5,0,Auth.log,w"
-		echo "Logger.root=4,Console Auth"
-		# Some default values just to reduce the amount of "errors" in the logs
-		echo "MySQLExecutable = \"\""
-		echo "TempDir = \"\""
-		echo "SourceDirectory = \"\""
-	} >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
-fi
+# replace login info
+sed -i 's/127.0.0.1;3306;acore;acore/localhost;3306;$MYSQL_ROOT_PASSWORD/' ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
 
 if [[ $APP_NAME == "worldserver" ]]; then
-    echo "DataDir = \"./data/\"" >> ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
-
+    sed -i 's/DataDir = \"\"/DataDir = \".\/data"/' ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
     git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./env/dist/$BIN_FOLDER/data
 fi
