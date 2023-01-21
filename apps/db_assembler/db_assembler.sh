28a2663b3de0ce63f49aa885883f6b75be2cda05
Pagani Walter <paganiwalter@gmail.com>
2023-01-21 14:34:41 -0300

    (apps/DB Assembler) Re-make db_assembler script

--- apps/db_assembler/db_assembler.sh
+++ apps/db_assembler/db_assembler.sh
@@ -1,81 +1,118 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo ----------------------------------------------------------------------------------------------- >&2
-echo ATTENTION: This tool is DEPRECATED. To assemble or update your DB, use the worldserver instead. >&2
-echo ----------------------------------------------------------------------------------------------- >&2
+CURRENT_LOCATION=`pwd`
 
-set -e
+cd $CURRENT_LOCATION
 
-CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source "$CURRENT_PATH/includes/includes.sh"
-
-cmdopt=$1
-
-PS3='[Please enter your choice]: '
-options=(
-    "all: Assemble all"                              # 1
-    "bases: Assemble only bases"                      # 2
-    "updates: Assemble only updates"                 # 3
-    "customs: Assemble only customs"                  # 4
-    "import-all: Assemble & Import all"              # 5
-    "import-bases: Assemble & Import only bases"      # 6
-    "import-updates: Assemble & Import only updates" # 7
-    "import-customs:  Assemble & Import only customs" # 8
-    "quit: Exit from this menu"                      # 9
-    )
-
-function _switch() {
-    _reply="$1"
-    _opt="$2"
-
-    case $_reply in
-        ""|"all"|"1")
-            dbasm_run true true true
-            ;;
-        ""|"bases"|"2")
-            dbasm_run true false false
-            ;;
-        ""|"updates"|"3")
-            dbasm_run false true false
-            ;;
-        ""|"customs"|"4")
-            dbasm_run false false true
-            ;;
-        ""|"import-all"|"5")
-            dbasm_import true true true
-            ;;
-        ""|"import-bases"|"6")
-            dbasm_import true false false
-            ;;
-        ""|"import-updates"|"7")
-            dbasm_import false true false
-            ;;
-        ""|"import-customs"|"8")
-            dbasm_import false false true
-            ;;
-        ""|"quit"|"9")
-            echo "Goodbye!"
-            exit
-            ;;
-        ""|"--help")
-            echo "Available commands:"
-            printf '%s\n' "${options[@]}"
-            ;;
-        *) echo "invalid option, use --help option for the commands list";;
-    esac
+all()
+{
+    echo ""
+    echo "         All files"
+    createDirectory
+    baseFiles
+    updatesFiles
 }
 
-while true
-do
-    # run option directly if specified in argument
-    [ ! -z $1 ] && _switch $@
-    [ ! -z $1 ] && exit 0
+bases()
+{
+    echo ""
+    echo "         All bases files"
+    createDirectory
+    baseFiles
+}
 
-    select opt in "${options[@]}"
-    do
-        echo "=====     DB ASSEMBLER MENU     ====="
-        _switch $REPLY
-        break
-    done
-done
+customs()
+{
+    echo ""
+    echo "         All customs files"
+    createDirectory
+    customFiles
+}
+
+updates()
+{
+    echo ""
+    echo "         All updates files"
+    createDirectory
+    updatesFiles
+}
+
+leave()
+{
+    echo ""
+    echo -e "\e[1;32m"
+    echo "         Good bye. Thank you for using our script."
+}
+
+invalid()
+{
+    echo ""
+    echo -e "\e[1;31m"
+    echo "         Invalid option. You must run the script again"
+}
+
+createDirectory()
+{
+    cd ..
+    cd env/dist/
+    if [ -d sql ]; then
+        rm -rf sql
+        mkdir sql
+    else
+        mkdir sql
+    fi
+}
+
+baseFiles()
+{
+    assembler data/sql/base/db_auth/ env/sql/auth_base.sql
+    assembler data/sql/base/db_characters/ env/sql/characters_base.sql
+    assembler data/sql/base/db_world/ env/sql/world_base.sql
+}
+
+customFiles()
+{
+    assembler data/sql/custom/db_auth/ env/sql/auth_custom.sql
+    assembler data/sql/custom/db_characters/ env/sql/characters_custom.sql
+    assembler data/sql/custom/db_world/ env/sql/world_custom.sql
+}
+
+updatesFiles()
+{
+    assembler data/sql/updates/db_auth env/sql/auth_updates.sql
+    assembler data/sql/updates/db_characters env/sql/characters_updates.sql
+    assembler data/sql/updates/db_world env/sql/world_updates.sql
+}
+
+assembler()
+{
+    if [[ `find $1 -type f -iname "*.sql" | wc -l` -gt 0 ]]; then
+        cat $1/*.sql > $2
+    fi
+}
+
+clear
+echo ""
+echo -e "\e[1;37m"
+echo "         Remember that the files are generated in the env folder."
+echo "         That is in the root of the project. Just at the same height as src."
+echo -e "\e[1;32m"
+echo "         ------  DB Assembler Version 1  ------"
+echo ""
+echo "         1. Assemble all SQL (Base, Custom and Updates)"
+echo "         2. Assemble only base files"
+echo "         3. Assemble only custom files"
+echo "         4. Assemble only updates files"
+echo "         5. Exit"
+echo ""
+echo -e "\e[1;37m"
+read -p "         Choose a number from 1 to 3: " opcion
+
+case $opcion in
+    1) all ;;
+    2) bases ;;
+    3) customs ;;
+    4) updates ;;
+    5) leave ;;
+    *) invalid ;;
+esac
