2fb019e4c7db637a0010449e9de9c3476f2c3640
M'Dic <joshua.lee.betts@gmail.com>
2023-01-22 15:38:35 -0500

    more macos12 fixit

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -137,7 +137,7 @@ class spell_the_flag_of_ownership : public SpellScript
             return;
         caster->CastSpell(target, 52605, true);
         char buff[100];
-        sprintf(buff, "%s plants the Flag of Ownership in the corpse of %s.", caster->GetName().c_str(), target->GetName().c_str());
+        snprintf(buffer, sizeof(buffer), "%s plants the Flag of Ownership in the corpse of %s.", caster->GetName().c_str(), target->GetName().c_str());
         caster->TextEmote(buff, caster);
         haveTarget = true;
     }
