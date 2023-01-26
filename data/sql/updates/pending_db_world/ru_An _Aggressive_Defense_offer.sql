01a5576d4902e231fcf0a063db543cd1902b5144
PivanDepolo <64589699+PivanDepolo@users.noreply.github.com>
2023-01-26 09:42:32 +0300

    Update data/sql/updates/pending_db_world/ru_An _Aggressive_Defense_offer.sql
    Co-authored-by: M'Dic <joshua.lee.betts@gmail.com>

--- data/sql/updates/pending_db_world/ru_An _Aggressive_Defense_offer.sql
+++ data/sql/updates/pending_db_world/ru_An _Aggressive_Defense_offer.sql
@@ -1,1 +1,2 @@
-INSERT INTO quest_offer_reward_locale (ID, locale, RewardText, VerifiedBuild) VALUES (1025, 'ruRU', 'Славная работа, $N. Спасибо тебе.', 18019);
+DELETE FROM `quest_offer_reward_locale` WHERE `ID`=1045 AND `locale`='ruRU';
+INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES (1045, 'ruRU', 'Славная работа, $N. Спасибо тебе.', 18019);
