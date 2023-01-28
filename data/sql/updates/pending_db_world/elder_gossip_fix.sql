3560f4a9a80b1ce98562abeaa64adfc5039d00ad
Flavaflav <logandros@gmail.com>
2023-01-28 05:07:11 -0500

    Update elder_gossip_fix.sql
    fix typo

--- data/sql/updates/pending_db_world/elder_gossip_fix.sql
+++ data/sql/updates/pending_db_world/elder_gossip_fix.sql
@@ -1,10 +1,3 @@
--- 
-DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (6815) AND `OptionID`=4;
-DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (6899) AND `OptionID`=2;
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `VerifiedBuild`) VALUES 
-(6815, 4, 0, "Where is Elder Darkhorn?", 11584, 1, 1, 0),
-(6899, 2, 0, "Where is Elder Wheathoof?", 11582, 1, 1, 0);
-
 DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (6815) AND `OptionID`=4;
 DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (6899) AND `OptionID` IN (1,2);
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `ActionMenuID`, `ActionPoiID`) VALUES 
@@ -12,10 +5,10 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `ActionMenuID`, `ActionP
 (6899, 1, 6854, 0),
 (6899, 2, 6895, 0);
 
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21097 WHERE `MenuID` = 6815 AND `OptionID` = 1;
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21107 WHERE `MenuID` = 6815 AND `OptionID` = 2;
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6815 AND `OptionID` = 3;
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21086 WHERE `MenuID` = 6899 AND `OptionID` = 0;
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 6854  WHERE `MenuID` = 6899 AND `OptionID` = 1;
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21122 WHERE `MenuID` = 6899 AND `OptionID` = 3;
--- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6899 AND `OptionID` = 4;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 21097 WHERE `MenuID` = 6815 AND `OptionID` = 1;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 21107 WHERE `MenuID` = 6815 AND `OptionID` = 2;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6815 AND `OptionID` = 3;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 21086 WHERE `MenuID` = 6899 AND `OptionID` = 0;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 6854  WHERE `MenuID` = 6899 AND `OptionID` = 1;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 21122 WHERE `MenuID` = 6899 AND `OptionID` = 3;
+UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6899 AND `OptionID` = 4;
