57b469c221b742d3e54f69e8e5b231535c341767
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-01-28 05:02:54 -0500

    fix(DB/npc_text_locale): Add esES/esMX text to Talin Keeneye. (#14131)
    Co-authored-by: IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>

---
+++ data/sql/updates/pending_db_world/talin-keeneye-esesmx.sql
@@ -0,0 +1,4 @@
+DELETE FROM `npc_text_locale` WHERE `ID` = 50022 AND `Locale` IN ('esES', 'esMX');
+INSERT INTO `npc_text_locale` (`ID`, `Locale`, `Text0_0`) VALUES
+(50022, 'esES', '¡Saludos, $c! Un día perfecto para ir a cazar, ¿no te parece? He estado teniendo bastante suerte con los jabalíes. ¿Te gustaría intentarlo?'),
+(50022, 'esMX', '¡Saludos, $c! Un día perfecto para ir a cazar, ¿no te parece? He estado teniendo bastante suerte con los jabalíes. ¿Te gustaría intentarlo?');
