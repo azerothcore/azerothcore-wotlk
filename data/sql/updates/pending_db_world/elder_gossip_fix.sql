70d5d70a8faac327f1193ded058f18ef00bff822
Flavaflav <logandros@gmail.com>
2023-01-28 05:41:05 -0500

    Update elder_gossip_fix.sql
    I will figure this out eventually...

--- data/sql/updates/pending_db_world/elder_gossip_fix.sql
+++ data/sql/updates/pending_db_world/elder_gossip_fix.sql
@@ -6,10 +6,10 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `ActionMenuID`, `ActionP
 (6899, 1, 6854, 0),
 (6899, 2, 6895, 0);
 
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 21097 WHERE `MenuID` = 6815 AND `OptionID` = 1;
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 21107 WHERE `MenuID` = 6815 AND `OptionID` = 2;
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6815 AND `OptionID` = 3;
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 21086 WHERE `MenuID` = 6899 AND `OptionID` = 0;
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 6854  WHERE `MenuID` = 6899 AND `OptionID` = 1;
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 21122 WHERE `MenuID` = 6899 AND `OptionID` = 3;
-UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6899 AND `OptionID` = 4;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21097 WHERE `MenuID` = 6815 AND `OptionIndex` = 1;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21107 WHERE `MenuID` = 6815 AND `OptionIndex` = 2;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6815 AND `OptionIndex` = 3;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21086 WHERE `MenuID` = 6899 AND `OptionIndex` = 0;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 6854  WHERE `MenuID` = 6899 AND `OptionIndex` = 1;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21122 WHERE `MenuID` = 6899 AND `OptionIndex` = 3;
+-- UPDATE `gossip_menu_option` SET `ActionMenuID` = 21147 WHERE `MenuID` = 6899 AND `OptionIndex` = 4;
