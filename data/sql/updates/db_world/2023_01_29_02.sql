69b2fbca74d71dd9401ab1acc00f15267b9f8583
AzerothCoreBot <azerothcorebot@gmail.com>
2023-01-29 15:14:19 +0000

    chore(DB): import pending files
    Referenced commit(s): 2aeeee9c83c530d45a7d3d124969b980e9ff4072

--- data/sql/updates/db_world/2023_01_29_02.sql
+++ data/sql/updates/db_world/2023_01_29_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_01_29_01 -> 2023_01_29_02
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_rog_pickpocket';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
