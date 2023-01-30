ef0e9eddc715b5c1fe2b84f3455253ee4549b05c
Mathematical <zajibrik@gmail.com>
2023-01-29 20:51:43 -0500

    Update rev_1675037344657947800.sql

--- data/sql/updates/pending_db_world/rev_1675037344657947800.sql
+++ data/sql/updates/pending_db_world/rev_1675037344657947800.sql
@@ -1,7 +1,5 @@
 --
 
-DELETE FROM `creature_loot_template` WHERE `entry` = 18201;
+UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0, `lootid`= 0 WHERE `entry` = 18201;
 
-UPDATE `creature_template` SET `mingold` = 0, `maxgold` = 0 WHERE `entry` = 18201;
-
-UPDATE `creature_template` SET `skinloot` =0 WHERE `entry` = 2565;
+UPDATE `creature_template` SET `skinloot` = 0 WHERE `entry` = 2565;
