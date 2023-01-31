96664159829b669c835b8c1c48d7ff36e4dbcdc1
Mathematical <zajibrik@gmail.com>
2023-01-31 00:22:57 -0500

    Update rev_1675140644244916700.sql

--- data/sql/updates/pending_db_world/rev_1675140644244916700.sql
+++ data/sql/updates/pending_db_world/rev_1675140644244916700.sql
@@ -1,5 +1,7 @@
 --
 
-DELETE FROM `creature_loot_template` WHERE `Reference` = 24076;
+DELETE FROM `reference_loot_template` WHERE `Entry` = 24076 AND `Item` = 1927;
+
+DELETE FROM `reference_loot_template` WHERE `Entry` = 594 AND `Item` = 1927;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(594,24076,24076,6,0,1,1,1,1,"Defias Henchman - (ReferenceTable)");
+(594,1927,0,6,0,1,1,1,1,"Defias Henchman - Deadmines Cleaver");
