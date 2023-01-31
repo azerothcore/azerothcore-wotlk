347e813d286ec5191957da0ad950e91335e96c2d
Mathematical <zajibrik@gmail.com>
2023-01-31 00:07:20 -0500

    Update rev_1675140644244916700.sql

--- data/sql/updates/pending_db_world/rev_1675140644244916700.sql
+++ data/sql/updates/pending_db_world/rev_1675140644244916700.sql
@@ -2,4 +2,4 @@
 
 DELETE FROM `creature_loot_template` WHERE `Reference` = 24076;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(594,24076,24076,5,0,1,1,1,1,"Defias Henchman - (ReferenceTable)");
+(594,24076,24076,6,0,1,1,1,1,"Defias Henchman - (ReferenceTable)");
