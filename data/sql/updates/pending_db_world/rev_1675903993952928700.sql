e63e711fabbe5c49429933688d597615c1169752
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2023-02-08 18:55:06 -0600

    improve(DB/npc_vendor): Address vendor exploit
    changes Qiff's limited supply frequency

---
+++ data/sql/updates/pending_db_world/rev_1675903993952928700.sql
@@ -0,0 +1,7 @@
+--
+UPDATE `npc_vendor` SET `incrtime`=3600 WHERE `entry`=19575 AND `item`=23771 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=3600 WHERE `entry`=19575 AND `item`=23783 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=32400 WHERE `entry`=19575 AND `item`=23781 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=7200 WHERE `entry`=19575 AND `item`=23782 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=180000 WHERE `entry`=19575 AND `item`=23787 AND `ExtendedCost`=0;
+UPDATE `npc_vendor` SET `incrtime`=150000 WHERE `entry`=19575 AND `item`=23786 AND `ExtendedCost`=0;
