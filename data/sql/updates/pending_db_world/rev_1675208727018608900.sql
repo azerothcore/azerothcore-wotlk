ca2f4b1c0e0d09586bff51af8ada8f1b45d23b72
Mathematical Shitpost Emissary <zajibrik@gmail.com>
2023-02-10 21:46:50 -0500

    Update rev_1675208727018608900.sql

--- data/sql/updates/pending_db_world/rev_1675208727018608900.sql
+++ data/sql/updates/pending_db_world/rev_1675208727018608900.sql
@@ -1,4 +1,4 @@
 --
 
-UPDATE `smart_scripts` SET  `event_type` = 1 WHERE `entryorguid` = 18865 and  `id` = 0;
+UPDATE `smart_scripts` SET  `event_type` = 1 WHERE `entryorguid` = 18865 AND `source_type` = 0 AND `id` = 0;
 
