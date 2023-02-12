3b178666137eabf56ecdfc547db4179ef08ea38b
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-02-12 20:59:40 -0300

    fix(Scripts/Karazhan): Rework Attumen & Midnight (#14756)

--- src/server/scripts/EasternKingdoms/Karazhan/instance_karazhan.cpp
+++ src/server/scripts/EasternKingdoms/Karazhan/instance_karazhan.cpp
@@ -32,6 +32,12 @@ const Position OptionalSpawn[] =
     { -10899.903320f, -2085.573730f, 49.474449f, 1.38f }  // Rokad the Ravager
 };
 
+ObjectData const creatureData[] =
+{
+    { NPC_ATTUMEN_THE_HUNTSMAN, DATA_ATTUMEN  },
+    { NPC_MIDNIGHT,             DATA_MIDNIGHT }
+};
+
 class instance_karazhan : public InstanceMapScript
 {
 public:
@@ -48,6 +54,7 @@ public:
         {
             SetHeaders(DataHeader);
             SetBossNumber(EncounterCount);
+            LoadObjectData(creatureData, nullptr);
 
             // 1 - OZ, 2 - HOOD, 3 - RAJ, this never gets altered.
             OperaEvent = urand(EVENT_OZ, EVENT_RAJ);
