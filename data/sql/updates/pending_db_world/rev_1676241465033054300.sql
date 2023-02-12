f33da284166cd6bd3dbf80ecd8a2dd81ecd030d2
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-02-12 20:18:51 -0300

    fix(Scripts/Arcatraz): Fix Zereketh Seed of Corruption damage and mod… (#14980)

---
+++ data/sql/updates/pending_db_world/rev_1676241465033054300.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_zereketh_seed_of_corruption';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(36123, 'spell_zereketh_seed_of_corruption'),
+(39367, 'spell_zereketh_seed_of_corruption');
