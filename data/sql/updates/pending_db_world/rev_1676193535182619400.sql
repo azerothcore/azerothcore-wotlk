2595e0c26c5549c72ca98eff59f9c1ded3c26ae5
UltraNix <80540499+UltraNix@users.noreply.github.com>
2023-02-12 12:28:51 +0100

    fix(DB/SAI): "Someone Else's Hard Work Pays Off" - fixed ending scene. (#14962)

---
+++ data/sql/updates/pending_db_world/rev_1676193535182619400.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `smart_scripts` WHERE `entryorguid`=19671 AND `source_type`=0 AND `id`=19;
+INSERT INTO `smart_scripts` VALUES
+(19671,0,19,0,54,0,100,0,0,0,0,0,0,211,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Cryo-Engineer Sha\'heen - On Just Summoned - Disallow reset phase');
