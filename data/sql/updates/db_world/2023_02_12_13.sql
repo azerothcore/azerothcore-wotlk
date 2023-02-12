6354d1598e8d3fe0520bd6e1fa973b1ed3177959
AzerothCoreBot <azerothcorebot@gmail.com>
2023-02-12 23:21:17 +0000

    chore(DB): import pending files
    Referenced commit(s): f33da284166cd6bd3dbf80ecd8a2dd81ecd030d2

--- data/sql/updates/db_world/2023_02_12_13.sql
+++ data/sql/updates/db_world/2023_02_12_13.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_02_12_12 -> 2023_02_12_13
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_zereketh_seed_of_corruption';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
