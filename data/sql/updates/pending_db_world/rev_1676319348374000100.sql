a5540c89ce68181a64e11a715c2e3d47e0fc6876
Gultask <100873791+Gultask@users.noreply.github.com>
2023-02-13 17:45:06 -0300

    B)

--- data/sql/updates/pending_db_world/rev_1676319348374000100.sql
+++ data/sql/updates/pending_db_world/rev_1676319348374000100.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `skinning_loot_template` WHERE (`Entry` IN (17952, 22163));
+DELETE FROM `skinning_loot_template` WHERE (`Entry` = 17952);
 
-UPDATE `creature_template` SET `skinloot` = 70068 WHERE (`entry` = 17952);
+UPDATE `creature_template` SET `skinloot` = 70068 WHERE (`entry` IN (17952, 22163));
