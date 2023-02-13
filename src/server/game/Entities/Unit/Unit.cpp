bdc70d8219c064f922358bf73c7ae19d812d1019
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-02-13 08:54:51 -0300

    Revert "fix(Core/Unit): Fix creatures not being able to cast spells d… (#14987)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -17852,13 +17852,6 @@ void Unit::Kill(Unit* killer, Unit* victim, bool durabilityLoss, WeaponAttackTyp
         isRewardAllowed = creature->IsDamageEnoughForLootingAndReward();
         if (!isRewardAllowed)
             creature->SetLootRecipient(nullptr);
-
-        // Call creature just died function
-        if (CreatureAI* ai = creature->AI())
-        {
-            ai->JustDied(killer);
-            sScriptMgr->OnUnitDeath(creature, killer);
-        }
     }
 
     // pussywizard: remade this if section (player is on the same map
@@ -18080,6 +18073,13 @@ void Unit::Kill(Unit* killer, Unit* victim, bool durabilityLoss, WeaponAttackTyp
         // Call KilledUnit for creatures, this needs to be called after the lootable flag is set
         if (killer && killer->GetTypeId() == TYPEID_UNIT && killer->IsAIEnabled)
             killer->ToCreature()->AI()->KilledUnit(victim);
+
+        // Call creature just died function
+        if (CreatureAI* ai = creature->AI())
+        {
+            ai->JustDied(killer);
+            sScriptMgr->OnUnitDeath(creature, killer);
+        }
 
         if (TempSummon* summon = creature->ToTempSummon())
         {
