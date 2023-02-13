5cbe3ea27b86f256fed3e5c60610a46eadc544ea
Gultask <sagemochi@hotmail.com>
2023-02-13 17:20:58 -0300

    Update rev_1676319348374000100.sql

--- data/sql/updates/pending_db_world/rev_1676319348374000100.sql
+++ data/sql/updates/pending_db_world/rev_1676319348374000100.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `skinning_loot_template` WHERE (`Entry` = 17952);
+DELETE FROM `skinning_loot_template` WHERE (`Entry` IN (17952, 22163));
 
 UPDATE `creature_template` SET `skinloot` = 70068 WHERE (`entry` = 17952);
