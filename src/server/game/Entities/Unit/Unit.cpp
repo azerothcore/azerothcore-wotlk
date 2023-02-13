f70a7aad4e8ad9d86e7fa2ead2754f07fe8bf0cb
Mike Delago <michaeldelago98@gmail.com>
2023-02-13 08:42:12 -0500

    merge to master

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -17852,13 +17852,6 @@ void Unit::Kill(Unit* killer, Unit* victim, bool durabilityLoss, WeaponAttackTyp
         isRewardAllowed = creature->IsDamageEnoughForLootingAndReward();
         if (!isRewardAllowed)
             creature->SetLootRecipient(nullptr);
-
-        // Call creature just died function
-        if (CreatureAI* ai = creature->AI())
-        {
-            ai->JustDied(killer);
-            sScriptMgr->OnUnitDeath(creature, killer);
-        }
     }
 
     // pussywizard: remade this if section (player is on the same map
