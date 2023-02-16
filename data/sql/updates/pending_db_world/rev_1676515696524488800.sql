cc13fc1bd27cb0c3a4e6c2ec2eba8c4d339b1db8
Gultask <100873791+Gultask@users.noreply.github.com>
2023-02-16 16:40:47 -0300

    Update rev_1676515696524488800.sql
    comment

--- data/sql/updates/pending_db_world/rev_1676515696524488800.sql
+++ data/sql/updates/pending_db_world/rev_1676515696524488800.sql
@@ -20,7 +20,7 @@ INSERT INTO `creature` (`guid`, `id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 2179700);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(2179700, 9, 0 , 0, 0, 0, 100, 0, 1400, 1400, 0, 0, 0, 66, 0, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0, 'Ancient Shadowmoon Spirit - Actionlist - Set Orientation [unsupported target type]'),
+(2179700, 9, 0 , 0, 0, 0, 100, 0, 1400, 1400, 0, 0, 0, 66, 0, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0, 'Ancient Shadowmoon Spirit - Actionlist - Set Orientation Player (Stored)'),
 (2179700, 9, 1 , 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0, 'Ancient Shadowmoon Spirit - Actionlist - Say Line 0'),
 (2179700, 9, 3 , 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 36, 21867, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ancient Shadowmoon Spirit - Actionlist - Update Template To \'Teron Gorefiend\''),
 (2179700, 9, 4 , 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 18, 256, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ancient Shadowmoon Spirit - Actionlist - Set Flags Immune To Players'),
