630981f001ef1fb9c16a4aed8885ce3eaf26adac
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-17 21:30:40 +0200

    Reply-Code Alpha remove quest requirement .

--- data/sql/updates/pending_db_world/rev_1676624876337239900.sql
+++ data/sql/updates/pending_db_world/rev_1676624876337239900.sql
@@ -23,3 +23,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 6412;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 6412);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (6412, 0, 0, 0, 6, 0, 100, 513, 0, 0, 0, 0, 0, 41, 1000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Skeleton - On Just Died - Despawn In 1000 ms (No Repeat)');
+
+-- Reply-Code Alpha - All Is Well That Ends Well 10 & 25 - Remove requirement for sigil quest completion.
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE (`ID` = 13631); -- Normal (10m)
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE (`ID` = 13819); -- Heroic (25m)
