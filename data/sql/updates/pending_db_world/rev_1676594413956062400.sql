fe0d4748b5fd7d9c7685be066c6e805830e04ea7
Gultask <100873791+Gultask@users.noreply.github.com>
2023-02-17 11:20:41 -0300

    Update rev_1676594413956062400.sql

--- data/sql/updates/pending_db_world/rev_1676594413956062400.sql
+++ data/sql/updates/pending_db_world/rev_1676594413956062400.sql
@@ -131,4 +131,4 @@ INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALU
 (24925, 1085, 0, 'Vorakem Doomspeaker (18679)');
 
 -- It's another rare with DISABLE_MOVE added to it
-UPDATE `creature_template` SET `unit_flags`=`unit_flags`&~0 WHERE (`entry` = 18679);
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`&~4 WHERE (`entry` = 18679);
