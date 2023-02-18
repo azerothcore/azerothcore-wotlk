847ff7dfac1754725503fec32447b7774c82dd2d
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-02-18 18:22:05 -0300

    test

---
+++ data/sql/updates/pending_db_world/rev_1676755305872220800.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 21778);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(21778, 30712, 0, 100, 0, 1, 0, 1, 1, 'Doctor Gutrick - The Doctor\'s Key');
