7bf4220389eb1a0df217a9223f6bdbc0d1309449
UltraNix <mariagefreek@gmail.com>
2023-02-18 19:09:36 +0100

    fix(DB/SAI): Shadowmoon Warlock's Fel Power shoudl be used out of combat on nearby Felguard Brute or Felguard Annihilator.
    Fixes #13819

---
+++ data/sql/updates/pending_db_world/rev_1676743728527320400.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `smart_scripts` SET `event_type`=1, `event_flags`=6, `event_param1`=2000, `event_param2`=6000, `event_param3`=15000, `event_param4`=21000, `action_param2`=0, `target_type`=1 WHERE `entryorguid`=17371 AND `source_type`=0 AND `id`=5;
