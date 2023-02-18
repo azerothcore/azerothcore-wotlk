2dd5e6cde129d72ff82957338ceffb3cbb4ada8e
Tereneckla <50233983+Tereneckla@users.noreply.github.com>
2023-02-18 21:13:16 +0000

    Update Gutrick.sql

--- data/sql/updates/pending_db_world/Gutrick.sql
+++ data/sql/updates/pending_db_world/Gutrick.sql
@@ -1,1 +1,3 @@
-DELETE FROM `creature_loot_template` WHERE (`ENTRY` = 21778);INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (21778, 30712, 0, 100, 0, 1, 0, 1, 1, 'Doctor Gutrick - The Doctor\'s Key');
+DELETE FROM `creature_loot_template` WHERE (`ENTRY` = 21778);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (21778, 30712, 0, 100, 0, 1, 0, 1, 1, 'Doctor Gutrick - The Doctor\'s Key');
+--
