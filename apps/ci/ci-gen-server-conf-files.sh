8cf1b6df9dc361385ee511810f65f625f9564749
Alvin Zhu <alvin.zhuge@gmail.com>
2023-02-19 10:48:58 +0800

    fix dry run

--- apps/ci/ci-gen-server-conf-files.sh
+++ apps/ci/ci-gen-server-conf-files.sh
@@ -9,6 +9,11 @@ cp ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf.dist ./env/dist/$CONFIG_FOLDER/$APP_
 # replace login info
 sed -i "s/127.0.0.1;3306;acore;acore/localhost;3306;root;$MYSQL_ROOT_PASSWORD/" ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
 
+if [[ $APP_NAME == "worldserver" || $APP_NAME == "dbimport" ]]; then
+    cp ./env/dist/$CONFIG_FOLDER/modules/playerbots.conf.dist ./env/dist/$CONFIG_FOLDER/modules/playerbots.conf
+    sed -i "s/127.0.0.1;3306;acore;acore/localhost;3306;root;$MYSQL_ROOT_PASSWORD/" ./env/dist/$CONFIG_FOLDER/modules/playerbots.conf
+fi
+
 if [[ $APP_NAME == "worldserver" ]]; then
     sed -i 's/DataDir = \".\"/DataDir = \".\/data"/' ./env/dist/$CONFIG_FOLDER/$APP_NAME.conf
     git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./env/dist/$BIN_FOLDER/data
