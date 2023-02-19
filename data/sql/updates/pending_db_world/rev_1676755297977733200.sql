6cd3ee461f8b1501d83bf43e3658d853e8c0abe3
Tereneckla <Tereneckla@pm.me>
2023-02-19 07:53:48 +0100

    Revert to first version of SQL

--- data/sql/updates/pending_db_world/rev_1676755297977733200.sql
+++ data/sql/updates/pending_db_world/rev_1676755297977733200.sql
@@ -1,4 +1,1 @@
---
-DELETE FROM `creature_loot_template` WHERE (`Entry` = 21778);
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(21778, 30712, 0, 100, 0, 1, 0, 1, 1, 'Doctor Gutrick - The Doctor\'s Key');
+UPDATE `creature_loot_template` SET `Chance` = 100 WHERE (`Entry` = 21778) AND (`Item` = 30712);
