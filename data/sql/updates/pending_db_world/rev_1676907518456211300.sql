893ef37ab8237f2cf35808d6a6b4c9a36179465b
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-20 22:48:52 +0200

    fix znCN locale (allegedly)

--- data/sql/updates/pending_db_world/rev_1676907518456211300.sql
+++ data/sql/updates/pending_db_world/rev_1676907518456211300.sql
@@ -7,3 +7,12 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 (33552, 3, 0, 'Yrr n\'lyeth... shuul anagg!', 12, 0, 100, 0, 0, 15766, 33628, 0, 'Yogg-Saron'),
 (33552, 4, 0, 'He will learn... no king rules forever; only death is eternal!', 12, 0, 100, 0, 0, 15767, 33629, 0, 'Yogg-Saron'),
 (33552, 5, 0, 'His brood learned their lesson before too long. You will soon learn yours!', 12, 0, 100, 0, 0, 15765, 33663, 0, 'Yogg-Saron');
+
+DELETE FROM `creature_text_locale` WHERE `CreatureID`=33552 AND `GroupID`=0 AND `ID`=0 AND `Locale`='zhCN';
+INSERT INTO `creature_text_locale` (`CreatureID`, `GroupID`, `ID`, `Locale`, `Text`) VALUES
+(33552, 0, 0, 'zhCN', '被无情地杀戮……'),
+(33552, 1, 0, 'znCN', '或者杀戮别人。'),
+(33552, 2, 0, 'zhCN', '你们微不足道的抵抗只能让我变得更加强大。'),
+(33552, 3, 0, 'zhCN', 'Yrr n\'lyeth... shuul anagg!'),
+(33552, 4, 0, 'zhCN', '他会明白……没有谁可以永远坐在王位上，只有死亡才是永恒的！'),
+(33552, 5, 0, 'zhCN', '他的族群没用多久就得到了教训。你们也快了！');
