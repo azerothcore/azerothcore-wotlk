095c9801cb043d760e58c876f20b2cf3431b955e
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-21 12:22:29 +0200

    typo

--- data/sql/updates/pending_db_world/rev_1676968393827350700.sql
+++ data/sql/updates/pending_db_world/rev_1676968393827350700.sql
@@ -32,6 +32,6 @@ UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI' WHERE `entry` = 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 1 AND `entryorguid` = 122088);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (122088, 1, 0, 1, 11, 0, 100, 1, 0, 0, 0, 0, 0, 45, 1, 0, 0, 0, 0, 0, 19, 6492, 5, 0, 0, 0, 0, 0, 0, 'Containment Coffer - On Respawn - Set Data 1 0 - Rift Spawn (6492) (No Repeat)'),
-(122088, 1, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Containment Coffer - On Just Summoned - Set Event Phase 1 (No Repeat)'),
+(122088, 1, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Containment Coffer - On Respawn - Set Event Phase 1 (No Repeat)'),
 (122088, 1, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 104, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Containment Coffer - On Respawn - Set Gameobject Flags Interact Condition (No Repeat)'),
 (122088, 1, 3, 0, 60, 1, 100, 0, 2500, 2500, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Containment Coffer - On Update - Despawn Instant (Phase 1)');
