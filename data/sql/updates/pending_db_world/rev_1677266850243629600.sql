25bddc7906727a0ef2f62a0fdec5f1e400ec8637
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-24 21:32:37 +0200

    fix(DB/Conditions): Baron Revilgaz - correct gossip for Pirates' Day event

---
+++ data/sql/updates/pending_db_world/rev_1677266850243629600.sql
@@ -0,0 +1,4 @@
+-- Baron Revilgaz - On Pirates' Day - Show appropriate gossip
+DELETE FROM `conditions` WHERE  `SourceGroup`=6685 AND `SourceEntry`=13062;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(14, 6685, 13062, 0, 0, 12, 0, 50, 0, 0, 0, 0, 0, '', 'Baron Revilgaz - On Pirates\' Day event - Show appropriate gossip');
