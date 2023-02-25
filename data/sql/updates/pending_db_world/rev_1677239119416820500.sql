6581b07934c2b49cae0a1ecde9e33690d9ecc307
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-25 14:48:35 +0200

    fix(DB/SAI): Defias Gunpowder - add no repeat flag. (#15184)

---
+++ data/sql/updates/pending_db_world/rev_1677239119416820500.sql
@@ -0,0 +1,2 @@
+-- Defias Gunpowder - add missing No Repeat flag (was already in the SAI comment)
+UPDATE `smart_scripts` SET `event_flags` = `event_flags`|1 WHERE `entryorguid` = 17155 AND `source_type` = 1 AND `id` = 0;
