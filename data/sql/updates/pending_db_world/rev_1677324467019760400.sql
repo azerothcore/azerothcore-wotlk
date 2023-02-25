2dbc91d930c8466c08a63672b1ce087c116e51f5
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-25 20:56:17 +0200

    typo

--- data/sql/updates/pending_db_world/rev_1677324467019760400.sql
+++ data/sql/updates/pending_db_world/rev_1677324467019760400.sql
@@ -9,7 +9,7 @@ INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES (173, 'SmartTri
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 2 AND `entryorguid` = 173);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(173, 2, 0, 0, 46, 0, 100, 0, 0, 0, 0, 0, 0, 12, 2056, 3, 360000, 0, 0, 0, 8, 0, 0, 0, 0, 1077, 1539, 28.89, 0, 'Areatrigger - On Trigger - Summon Creature \'Ravenclaw Apparition\' (The Dead Fields)');
+(173, 2, 0, 0, 46, 0, 100, 0, 0, 0, 0, 0, 0, 12, 2056, 3, 300000, 0, 0, 0, 8, 0, 0, 0, 0, 1077, 1539, 28.89, 0, 'Areatrigger - On Trigger - Summon Creature \'Ravenclaw Apparition\' (The Dead Fields)');
 
 -- Move creature_text from Thule Ravencla to Ravenclaw Apparition
 DELETE FROM `creature_text` WHERE `CreatureID` IN (1947, 2056);
