0de75fa7b756c1d2056a21a6c73026ef7110f7d9
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-25 13:45:42 +0200

    fix(DB/Spell): Crusader Strike now stacks from multiple sources (#15181)

---
+++ data/sql/updates/pending_db_world/rev_1677181094686770100.sql
@@ -0,0 +1,3 @@
+-- Scarlet Gallant - Crusader Strike - single aura stack
+DELETE FROM `spell_custom_attr` WHERE `spell_id` = 14517;
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (14517, 4194304);
