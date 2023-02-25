762901da065a340fb2bd9ab26c617edfa58cf5cf
Johaine <32821455+Johaine@users.noreply.github.com>
2023-02-25 20:48:50 +0100

    Drop database name from update SQL
    Other people might not use the standard database names
    
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/DM_FactoryDoor_Fix.sql
+++ data/sql/updates/pending_db_world/DM_FactoryDoor_Fix.sql
@@ -1,1 +1,1 @@
-UPDATE `acore_world`.`smart_scripts` SET `action_type`=9, `comment`='Rhahk\'Zor - On Just Died - Activate Gameobject' WHERE  `entryorguid`=644 AND `source_type`=0 AND `id`=2 AND `link`=3;
+UPDATE `smart_scripts` SET `action_type`=9, `comment`='Rhahk\'Zor - On Just Died - Activate Gameobject' WHERE `entryorguid`=644 AND `source_type`=0 AND `id`=2 AND `link`=3;
