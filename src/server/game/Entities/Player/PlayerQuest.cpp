1063119e0067a951651dc10bc4b16474d08ebff4
Helias <stefanoborzi32@gmail.com>
2023-02-27 00:54:47 +0100

    fix: codestyle

--- src/server/game/Entities/Player/PlayerQuest.cpp
+++ src/server/game/Entities/Player/PlayerQuest.cpp
@@ -1304,7 +1304,6 @@ bool Player::SatisfyQuestSeasonal(Quest const* qInfo, bool /*msg*/) const
     if (!qInfo->IsSeasonal() || m_seasonalquests.empty())
         return true;
 
-
     // cppcheck-suppress mismatchingContainers
     Player::SeasonalEventQuestMap::iterator itr = ((Player*)this)->m_seasonalquests.find(qInfo->GetEventIdForQuest());
 
