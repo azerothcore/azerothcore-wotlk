0c3e73252fd199bab6396bcfeb154724ae868adc
AzerothCoreBot <azerothcorebot@gmail.com>
2023-02-27 04:40:37 +0000

    chore(DB): import pending files
    Referenced commit(s): e5c0c7227a8b7249266222ce36fc7a9197aa6d34

--- data/sql/updates/db_world/2023_02_27_06.sql
+++ data/sql/updates/db_world/2023_02_27_06.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_02_27_05 -> 2023_02_27_06
 --
 UPDATE `creature_template` SET `ScriptName` = 'npc_malchezaar_axe', `unit_flags` = `unit_flags`|33554432 WHERE `entry` = 17650;
 UPDATE `creature_template` SET `ScriptName` = 'npc_netherspite_infernal' WHERE `entry` = 17646;
