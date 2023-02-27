f1cf4b9e893568d302c67c807687b05946f82757
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-27 09:44:26 +0200

    comment

--- data/sql/updates/pending_db_world/rev_1676673725150035900.sql
+++ data/sql/updates/pending_db_world/rev_1676673725150035900.sql
@@ -1,4 +1,4 @@
--- Sha'ni Proudtusk's Remains GO SmartAI - Add condition for QUEST_STATE (Complete and rewarded).
+-- Sha'ni Proudtusk's Remains GO SmartAI - Add condition for QUEST_STATE (Complete and rewarded) AND no Sha'ni Proudtusk within 10 yards.
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=1 AND `SourceEntry`=160445;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
 (22, 1, 160445, 1, 0, 47, 0, 3821, 66, 0, 0, 0, 0, '', '(AND) Execute SmartAI for gameobject 160445 only if player has COMPLETE and REWARDED quest 3821'),
