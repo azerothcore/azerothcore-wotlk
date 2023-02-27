fc5a222b34528b7842b04882be67ba6a6277d2b3
AzerothCoreBot <azerothcorebot@gmail.com>
2023-02-27 02:26:51 +0000

    chore(DB): import pending files
    Referenced commit(s): 90bc88d469caaa02f108060a68a6abc5bebf2a03

--- data/sql/updates/db_world/2023_02_27_00.sql
+++ data/sql/updates/db_world/2023_02_27_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_02_25_01 -> 2023_02_27_00
 --
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceID` = 15 AND `SourceGroup` = 7435;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
