316dd45c1d7263f3c5602f33c6f8ad0c0a366014
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-27 09:43:24 +0200

    typo

--- data/sql/updates/pending_db_world/rev_1676673725150035900.sql
+++ data/sql/updates/pending_db_world/rev_1676673725150035900.sql
@@ -2,4 +2,4 @@
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=1 AND `SourceEntry`=160445;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
 (22, 1, 160445, 1, 0, 47, 0, 3821, 66, 0, 0, 0, 0, '', '(AND) Execute SmartAI for gameobject 160445 only if player has COMPLETE and REWARDED quest 3821'),
-(22, 1, 160445, 1, 0, 29, 0, 9136, 10, 0, 1, 0, 0, '', '(AND) Execute SmartAI for gameobject 160445 only Spawn Sha\'ni Proudtusk if no Sha\'ni Proudtusk within 10y.');
+(22, 1, 160445, 1, 0, 29, 0, 9136, 10, 0, 1, 0, 0, '', '(AND) Execute SmartAI for gameobject 160445 only if no Sha\'ni Proudtusk is within 10y.');
