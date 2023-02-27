d09de051e2d1b88a44f95de92b616c721b3add59
avarishd <46330494+avarishd@users.noreply.github.com>
2023-02-27 09:41:58 +0200

    Update

--- data/sql/updates/pending_db_world/rev_1676673725150035900.sql
+++ data/sql/updates/pending_db_world/rev_1676673725150035900.sql
@@ -1,12 +1,5 @@
--- Sha'ni Proudtusk - On summon, despawn others.
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 9136;
-
-DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 9136);
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(9136, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 11, 9136, 5, 0, 0, 0, 0, 0, 0, 'Sha\'ni Proudtusk - On Just Summoned - Despawn Other Clones');
-
--- Sha'ni Proudtusk's Remains GO SmartAI - Add OR condition for _COMPLETE and _REWARDED.
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=1 AND `SourceEntry`=160445 AND `ConditionValue1`=3821;
+-- Sha'ni Proudtusk's Remains GO SmartAI - Add condition for QUEST_STATE (Complete and rewarded).
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=1 AND `SourceEntry`=160445;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
-(22, 1, 160445, 1, 0, 28, 0, 3821, 0, 0, 0, 0, 0, '', '(OR) Execute SmartAI for gameobject 160445 only if player has completed quest 3821'),
-(22, 1, 160445, 1, 1, 8, 0, 3821, 0, 0, 0, 0, 0, '', '(OR) Execute SmartAI for gameobject 160445 only if player has been rewarded quest 3821');
+(22, 1, 160445, 1, 0, 47, 0, 3821, 66, 0, 0, 0, 0, '', '(AND) Execute SmartAI for gameobject 160445 only if player has COMPLETE and REWARDED quest 3821'),
+(22, 1, 160445, 1, 0, 29, 0, 9136, 10, 0, 1, 0, 0, '', '(AND) Execute SmartAI for gameobject 160445 only Spawn Sha\'ni Proudtusk if no Sha\'ni Proudtusk within 10y.');
