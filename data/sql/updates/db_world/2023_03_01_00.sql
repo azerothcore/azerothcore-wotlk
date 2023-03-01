78b97930f32d9e61be38241a1030bdb8f0961a1f
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-01 20:00:55 +0000

    chore(DB): import pending files
    Referenced commit(s): a607d438b3b0b974884ea3984a7fe0cdf55485db

--- data/sql/updates/db_world/2023_03_01_00.sql
+++ data/sql/updates/db_world/2023_03_01_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_02_28_00 -> 2023_03_01_00
 -- Bonechewer Messenger
 UPDATE `smart_scripts` SET `action_param6`=2 WHERE `source_type`=0 AND `entryorguid`=21244 AND `id`=0;
 -- Coilskar Cistern
