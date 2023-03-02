0b7376a76d9f9674beaf488c76e940678748942a
Gultask <sagemochi@hotmail.com>
2023-03-02 19:56:24 -0300

    Create rev_1677797244323339200.sql

---
+++ data/sql/updates/pending_db_world/rev_1677797244323339200.sql
@@ -0,0 +1,2 @@
+-- Too many friendly NPCs dealing damage can interfere with quest objective for killing Socrethar (Quest Credit is self-cast)
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|2097152 WHERE (`entry` = 20132);
