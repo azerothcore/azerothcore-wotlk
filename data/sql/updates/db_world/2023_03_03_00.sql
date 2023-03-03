7a78146498f8064da99cf899d2d8b33030d2f0ef
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-03 08:50:15 +0000

    chore(DB): import pending files
    Referenced commit(s): 9f9b320e66f421c1c9b6886bad58166f7b9c9ef4

--- data/sql/updates/db_world/2023_03_03_00.sql
+++ data/sql/updates/db_world/2023_03_03_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_02_00 -> 2023_03_03_00
 -- Zorbin Fandazzle - show gossip menu if Quests Zapped Giants (7003) and  Fuel for the Zapping (7721) are rewarded
 DELETE FROM `conditions` WHERE `SourceGroup` = 11361 AND `SourceTypeOrReferenceId` = 14 AND `ConditionValue1` IN (7003, 7721);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
