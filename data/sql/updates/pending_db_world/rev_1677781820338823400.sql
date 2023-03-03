23a76fa781fe3304d7b82aa23d4b049a22cee78f
郑佩茹 <zhengpeiru21@protonmail.com>
2023-03-03 12:04:22 -0700

    Add missing SQL DELETE

--- data/sql/updates/pending_db_world/rev_1677781820338823400.sql
+++ data/sql/updates/pending_db_world/rev_1677781820338823400.sql
@@ -1,4 +1,5 @@
 --
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 19 AND `SourceEntry` IN (10641, 10669);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (19, 0, 10641, 0, 1, 47, 0, 10640, 64, 0, 0, 0, 0, '', 'Against the Legion - Require Quest 10640 OR 10689'),
 (19, 0, 10669, 0, 1, 47, 0, 10640, 64, 0, 0, 0, 0, '', 'Against All Odds - Require Quest 10640 OR 10689'),
