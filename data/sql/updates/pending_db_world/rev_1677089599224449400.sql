3134c91316f8b97ced0cfdf18c277fc8d6ce57cd
Revision <tkn963@gmail.com>
2023-03-04 14:12:43 +0100

    Update rev_1677089599224449400.sql

--- data/sql/updates/pending_db_world/rev_1677089599224449400.sql
+++ data/sql/updates/pending_db_world/rev_1677089599224449400.sql
@@ -1,10 +1,10 @@
 --
-DELETE FROM `npc_trainer` WHERE `SpellID`=-201041 AND `ID` IN (18749, 18772, 26964, 33613, 33636, 33684);
-INSERT INTO `npc_trainer` (`ID`, `SpellID`) VALUES
+DELETE FROM `npc_trainer` WHERE `SpellID`=60969 AND `ID` IN (18749, 18772, 26964, 33613, 33636, 33684);
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
 -- Flying Carpet
-(18749, -201041), -- Dalinna <Master Tailoring Trainer>
-(18772, -201041), -- Hama <Master Tailoring Trainer>
-(26964, -201041), -- Alexandra McQueen <Grand Master Tailoring Trainer>
-(33613, -201041), -- Tailoring
-(33636, -201041), -- Miralisse <Master Tailoring Trainer>
-(33684, -201041); -- Weaver Aoa <Master Tailoring Trainer>
+(18749, 60969, 105000, 197, 300, 0, 0), -- Dalinna <Master Tailoring Trainer>
+(18772, 60969, 105000, 197, 300, 0, 0), -- Hama <Master Tailoring Trainer>
+(26964, 60969, 105000, 197, 300, 0, 0), -- Alexandra McQueen <Grand Master Tailoring Trainer>
+(33613, 60969, 105000, 197, 300, 0, 0), -- Tailoring
+(33636, 60969, 105000, 197, 300, 0, 0), -- Miralisse <Master Tailoring Trainer>
+(33684, 60969, 105000, 197, 300, 0, 0); -- Weaver Aoa <Master Tailoring Trainer>
