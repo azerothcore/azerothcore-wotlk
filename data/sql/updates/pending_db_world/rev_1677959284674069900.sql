8931b7b0fe539b8160211874a7ea10b55c73ff68
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-03-04 17:05:41 -0300

    fix(Scripts/TheBotanica): Rework Commander Sarannis
    partial cherry-pick of https://github.com/TrinityCore/TrinityCore/commit/41da43d9f240ecd9976a440824507d73891eeeec
    
    Co-Authored-By: offl <11556157+offl@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1677959284674069900.sql
@@ -0,0 +1,8 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = 34799 AND `ScriptName` = 'spell_commander_sarannis_arcane_devastation';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(34799,'spell_commander_sarannis_arcane_devastation');
+
+UPDATE `spell_dbc` SET `Effect_1` = 28, `EffectMiscValueB_1` = 64 WHERE `ID` IN (34810, 34817, 34818, 34819);
+
+UPDATE `creature_addon` SET `auras` = '34792 19818' WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id1` = 17976);
