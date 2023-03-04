349d590d7a7c903b58ae184b8b7d6d666ff2f79a
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-04 22:42:24 +0200

    fix(DB/SAI): Scarlet Enchanter & Scarlet Cleric - OOC attack (#15180)

---
+++ data/sql/updates/pending_db_world/rev_1677174924847719800.sql
@@ -0,0 +1,5 @@
+-- Scarlet Enchanter - Remove OOC attack closest player
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 9452 AND `source_type` = 0 AND `id` = 2;
+
+-- Scarlet Cleric - Remove OOC attack closest player
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 9449 AND `source_type` = 0 AND `id` = 3;
