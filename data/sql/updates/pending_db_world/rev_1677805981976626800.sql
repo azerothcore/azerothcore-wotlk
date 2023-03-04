5cc5b3522db13e833ba6679409f5af612bf8a022
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-04 17:36:18 -0300

    fix(DB/Creature): Correct Flags for Raging Souls (#15245)

---
+++ data/sql/updates/pending_db_world/rev_1677805981976626800.sql
@@ -0,0 +1,3 @@
+-- Detected build: V3_4_0_46368
+UPDATE `creature_template` SET `unit_flags` = 33554688 WHERE (`entry` = 18778);
+UPDATE `creature_template` SET `unit_flags` = 33555200 WHERE (`entry` = 18726);
