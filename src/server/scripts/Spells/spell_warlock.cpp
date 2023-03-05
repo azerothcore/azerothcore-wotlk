fc0269a2fbd6a3f0e38ffea5955edbf8525d0c62
Tereneckla <50233983+Tereneckla@users.noreply.github.com>
2023-03-05 12:06:31 +0000

    fix(Core/Spells): Life Tap Damage and Mana scaling

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -753,7 +753,6 @@ class spell_warl_siphon_life : public AuraScript
 };
 
 // -1454 - Life Tap
-#define LIFE_TAP_COEFFICIENT 0.9F
 class spell_warl_life_tap : public SpellScript
 {
     PrepareSpellScript(spell_warl_life_tap);
@@ -773,10 +772,10 @@ class spell_warl_life_tap : public SpellScript
         Player* caster = GetCaster()->ToPlayer();
         if (Unit* target = GetHitUnit())
         {
-            int32 damage = GetEffectValue() + LIFE_TAP_COEFFICIENT;
-            int32 damage2Mana = GetEffectValue();
-            int32 mana = int32(damage2Mana + (caster->GetInt32Value(PLAYER_FIELD_MOD_DAMAGE_DONE_POS + static_cast<uint8>(SPELL_SCHOOL_SHADOW)) * 0.5f));
-
+            int32 spellEffect = GetEffectValue();
+            int32 damage = spellEffect + (caster->GetStat(STAT_SPIRIT) * 1.5f);
+            int32 mana = int32(spellEffect + (caster->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_SHADOW) * 0.5f));
+            
             // Shouldn't Appear in Combat Log
             target->ModifyHealth(-damage);
 
@@ -801,10 +800,11 @@ class spell_warl_life_tap : public SpellScript
 
     SpellCastResult CheckCast()
     {
-        if ((int32(GetCaster()->GetHealth()) > int32(GetSpellInfo()->Effects[EFFECT_0].CalcValue() + (3.1 * GetSpellInfo()->BaseLevel) + LIFE_TAP_COEFFICIENT )))
+        if ((int32(GetCaster()->GetHealth()) > int32(GetSpellInfo()->Effects[EFFECT_0].CalcValue() + (GetCaster()->GetStat(STAT_SPIRIT) * 1.5f))))
             return SPELL_CAST_OK;
         return SPELL_FAILED_FIZZLE;
     }
+
 
     void Register() override
     {
