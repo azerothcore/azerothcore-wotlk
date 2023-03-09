790916119b0f0f2995be9bb4fd1fb1fa071cf628
Revision <tkn963@gmail.com>
2023-03-09 15:27:15 +0100

    Reference
    Moved the spell to a reference. Still one trainer missing the spell and waiting for help on how to deal with it.

--- data/sql/updates/pending_db_world/rev_1677089599224449400.sql
+++ data/sql/updates/pending_db_world/rev_1677089599224449400.sql
@@ -1,10 +1,4 @@
 --
-DELETE FROM `npc_trainer` WHERE `SpellID`=60969 AND `ID` IN (18749, 18772, 26964, 33613, 33636, 33684);
+DELETE FROM `npc_trainer` WHERE `ID`=201040 AND `SpellID`=60969;
 INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
--- Flying Carpet
-(18749, 60969, 105000, 197, 300, 0, 0), -- Dalinna <Master Tailoring Trainer>
-(18772, 60969, 105000, 197, 300, 0, 0), -- Hama <Master Tailoring Trainer>
-(26964, 60969, 105000, 197, 300, 0, 0), -- Alexandra McQueen <Grand Master Tailoring Trainer>
-(33613, 60969, 105000, 197, 300, 0, 0), -- Tailoring
-(33636, 60969, 105000, 197, 300, 0, 0), -- Miralisse <Master Tailoring Trainer>
-(33684, 60969, 105000, 197, 300, 0, 0); -- Weaver Aoa <Master Tailoring Trainer>
+(201040, 60969, 105000, 197, 300, 0, 0);
