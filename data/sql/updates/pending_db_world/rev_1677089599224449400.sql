2ff0e4d35604459a8d236ab618ed4589984b3a85
Revision <tkn963@gmail.com>
2023-03-09 15:33:18 +0100

    Update rev_1677089599224449400.sql

--- data/sql/updates/pending_db_world/rev_1677089599224449400.sql
+++ data/sql/updates/pending_db_world/rev_1677089599224449400.sql
@@ -1,4 +1,5 @@
 --
+DELETE FROM `npc_trainer` WHERE `SpellID`=60969;
 DELETE FROM `npc_trainer` WHERE `ID`=201040 AND `SpellID`=60969;
 INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
 (201040, 60969, 105000, 197, 300, 0, 0);
