03d47897ece3e0a615aac05222d9fbd98b33717d
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-11 00:43:31 +0000

    chore(DB): import pending files
    Referenced commit(s): 58a502e139aebcc94b8af9bf978fd27c8473ff6c

--- data/sql/updates/db_world/2023_03_11_02.sql
+++ data/sql/updates/db_world/2023_03_11_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_11_01 -> 2023_03_11_02
 --
 DELETE FROM `creature_text` WHERE `CreatureID` IN (17805,17959);
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
