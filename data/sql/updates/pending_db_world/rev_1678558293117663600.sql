f8f3b41c126da94b597086ebe479a5a93407aa1a
UltraNix <mariagefreek@gmail.com>
2023-03-11 19:12:51 +0100

    fix(DB/SAI): Corrected Sethekk Spirit's level. Added Spirit Burst  to SAI script.
    Fixes #15175

---
+++ data/sql/updates/pending_db_world/rev_1678558293117663600.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `creature_template` SET `minlevel`=60, `maxlevel`=60 WHERE `entry` IN (18703,20700);
+UPDATE `smart_scripts` SET `action_type`=49, `action_param1`=0, `target_type`=21, `target_param1`=30, `comment`='Sethekk Spirit - On Summon - Attack nearest player' WHERE `entryorguid`=18703 AND `source_type`=0 AND `id`=0;
+DELETE FROM `creature_template_addon` WHERE `entry`=18703;
+INSERT INTO `creature_template_addon` VALUES
+(18703,0,0,0,1,0,0,'24051');
