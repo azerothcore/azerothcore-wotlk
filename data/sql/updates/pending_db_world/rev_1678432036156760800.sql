5a628fe4fcf3885045a89ddc54aedfbadeb10c26
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-11 18:00:29 +0200

    fix(DB/Creature): Starving Helboar aggro radius. (#15321)
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1678432036156760800.sql
@@ -0,0 +1,4 @@
+-- Starving Helboar
+UPDATE `creature_template_addon` SET `auras` = '33908' WHERE `entry` = 16879;
+UPDATE `creature_template` SET `AIName` = '', `detection_range` = 30 WHERE `entry` = 16879;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 16879;
