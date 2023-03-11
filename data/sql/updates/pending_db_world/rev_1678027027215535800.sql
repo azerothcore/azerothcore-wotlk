bc7dc136604a61a4e48919b2ab4b8ff8f8be3038
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-11 02:55:47 -0300

    fix(DB/Conditions) Correct Class conditions for Satchel of Helpful Goods (65-70) (#15281)

---
+++ data/sql/updates/pending_db_world/rev_1678027027215535800.sql
@@ -0,0 +1,3 @@
+-- These were switched around
+UPDATE `conditions` SET `ConditionValue1`=1032, `Comment`='SOHG: Turbulent Signet only for leatherusers' WHERE `SourceTypeOrReferenceId`=10 AND `SourceGroup`=10058 AND `SourceEntry`=51991;
+UPDATE `conditions` SET `ConditionValue1`=400, `Comment`='SOHG: Turbulent Signet only for clothusers' WHERE `SourceTypeOrReferenceId`=10 AND `SourceGroup`=10059 AND `SourceEntry`=51991;
