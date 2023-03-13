a1c644fd3971085d25d16700384110feda6bfdab
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-13 13:21:43 +0000

    chore(DB): import pending files
    Referenced commit(s): 6270c6ab64cb40b0eabf3edd8c220ebda0a35d30

--- data/sql/updates/db_world/2023_03_13_00.sql
+++ data/sql/updates/db_world/2023_03_13_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_12_00 -> 2023_03_13_00
 -- Yogg Saron Vision -- Add missing text
 DELETE FROM `creature_text` WHERE `CreatureID`=33552;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
