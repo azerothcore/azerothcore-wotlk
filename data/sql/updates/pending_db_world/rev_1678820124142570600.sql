4e8125b5e1cc9c843eff73d46e9c66843fcbc27d
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-15 09:34:33 +0200

    update areatrigger sai comment

--- data/sql/updates/pending_db_world/rev_1678820124142570600.sql
+++ data/sql/updates/pending_db_world/rev_1678820124142570600.sql
@@ -19,6 +19,6 @@ INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES (4389, 'SmartTr
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 4389 AND `source_type` = 2;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(4389,2,0,1,46,0,100,0,4389,0,0,0,0,64,1,0,0,0,0,0,7,0,0,0,0,0,0,0,0,"Areatrigger (The Stormspire) - On Trigger - Store Targetlist"),
-(4389,2,1,2,61,0,100,0,0,0,0,0,0,100,1,0,0,0,0,0,10,68758,19227,0,0,0,0,0,0,"Areatrigger (The Stormspire) - On Link - Send Target 1 (Griftah)"),
-(4389,2,2,0,61,0,100,0,0,0,0,0,0,45,0,1,0,0,0,0,10,68758,19227,0,0,0,0,0,0,"Areatrigger (The Stormspire) - On Link - Set Data 0 1 (Griftah)");
+(4389,2,0,1,46,0,100,0,4389,0,0,0,0,64,1,0,0,0,0,0,7,0,0,0,0,0,0,0,0,"Areatrigger (Lower City) - On Trigger - Store Targetlist"),
+(4389,2,1,2,61,0,100,0,0,0,0,0,0,100,1,0,0,0,0,0,10,68758,19227,0,0,0,0,0,0,"Areatrigger (Lower City) - On Link - Send Target 1 (Griftah)"),
+(4389,2,2,0,61,0,100,0,0,0,0,0,0,45,0,1,0,0,0,0,10,68758,19227,0,0,0,0,0,0,"Areatrigger (Lower City) - On Link - Set Data 0 1 (Griftah)");
