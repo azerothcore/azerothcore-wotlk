b21f98d6390c41240dc4efcb7244982bc89c6536
UltraNix <80540499+UltraNix@users.noreply.github.com>
2023-03-15 04:07:49 +0100

    fix(Scripts/Karazhan): Chess Event. (#14736)

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4470,7 +4470,13 @@ void SpellMgr::LoadSpellInfoCorrections()
     {
         spellInfo->AttributesEx2 |= SPELL_ATTR2_IGNORE_LINE_OF_SIGHT;
     });
-
+    // Game In Session
+    ApplySpellFix({ 39331 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->Effects[EFFECT_0].Effect = SPELL_EFFECT_APPLY_AURA;
+        spellInfo->Attributes |= SPELL_ATTR0_NO_AURA_CANCEL;
+        spellInfo->AuraInterruptFlags |= AURA_INTERRUPT_FLAG_CHANGE_MAP;
+    });
     // Death Ray Warning Visual, Death Ray Damage Visual
     ApplySpellFix({ 63882, 63886 }, [](SpellInfo* spellInfo)
     {
