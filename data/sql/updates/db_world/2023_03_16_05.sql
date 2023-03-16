909b5dec8c859b655dab385e9baf9b936965efa1
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-16 22:20:17 +0000

    chore(DB): import pending files
    Referenced commit(s): 4613d31f59bfb22c5f427818fd644e090de8d5d7

--- data/sql/updates/db_world/2023_03_16_05.sql
+++ data/sql/updates/db_world/2023_03_16_05.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_16_04 -> 2023_03_16_05
 --
 UPDATE `creature_template` SET `minlevel`=60, `maxlevel`=60 WHERE `entry` IN (18703,20700);
 UPDATE `smart_scripts` SET `action_type`=49, `action_param1`=0, `target_type`=21, `target_param1`=30, `comment`='Sethekk Spirit - On Summon - Attack nearest player' WHERE `entryorguid`=18703 AND `source_type`=0 AND `id`=0;
