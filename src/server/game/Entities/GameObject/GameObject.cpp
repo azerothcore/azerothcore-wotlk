0a920902b61432b84390bdbb7deabcd587cb92be
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-03-18 01:15:49 +0100

    Core/GameObjects: Use serverOnly property of GameObjectTemplate for all that have it
    * cherry-pick commit (https://github.com/TrinityCore/TrinityCore/commit/4ee64a232ad830255bc6779e595d412c934b6b74)
    
    Co-Authored-By: Shauren <shauren.trinity@gmail.com>

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1252,7 +1252,7 @@ bool GameObject::IsNeverVisible() const
     if (WorldObject::IsNeverVisible())
         return true;
 
-    if (GetGoType() == GAMEOBJECT_TYPE_SPELL_FOCUS && GetGOInfo()->spellFocus.serverOnly == 1)
+    if (GetGoType()->GetServerOnly())
         return true;
 
     return false;
