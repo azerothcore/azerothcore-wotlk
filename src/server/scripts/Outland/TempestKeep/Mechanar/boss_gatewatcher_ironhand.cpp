1d3647dd375f9e2b8976932adc45654f43466ade
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-03-19 19:54:21 -0300

    validators

--- src/server/scripts/Outland/TempestKeep/Mechanar/boss_gatewatcher_ironhand.cpp
+++ src/server/scripts/Outland/TempestKeep/Mechanar/boss_gatewatcher_ironhand.cpp
@@ -37,7 +37,13 @@ enum Spells
 
 struct boss_gatewatcher_iron_hand : public BossAI
 {
-    boss_gatewatcher_iron_hand(Creature* creature) : BossAI(creature, DATA_GATEWATCHER_IRON_HAND) { }
+    boss_gatewatcher_iron_hand(Creature* creature) : BossAI(creature, DATA_GATEWATCHER_IRON_HAND)
+    {
+        scheduler.SetValidator([this]
+        {
+            return !me->HasUnitState(UNIT_STATE_CASTING);
+        });
+    }
 
     void JustEngagedWith(Unit* /*who*/) override
     {
