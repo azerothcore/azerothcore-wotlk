9bae2702adff2372fcfa903b9ce27edb6d21fc56
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-03-19 04:39:29 +0100

    Update rev_1679196833614075500.sql

--- data/sql/updates/pending_db_world/rev_1679196833614075500.sql
+++ data/sql/updates/pending_db_world/rev_1679196833614075500.sql
@@ -1,4 +1,4 @@
 -- 20682 (Area: 3885 - Difficulty: 0) CreateObject1
 DELETE FROM `creature` WHERE `guid`=87039 AND `id1` = 20682;
-INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+INSERT INTO `creature` (`guid`, `id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (87039, 20682, 530, 3519, 3885, 1, 1, 0, 0, -3945.0205078125, 4153.3544921875, 3.636231184005737304, 1.343903541564941406, 120, 0, 0, 23656, 0, 0, 0, 0, 0, 42328);
