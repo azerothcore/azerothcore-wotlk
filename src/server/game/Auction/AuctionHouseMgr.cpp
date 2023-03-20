9678d10cd4b12ba4e45933d97e212b10037ad846
Winfidonarleyan <dowlandtop@yandex.com>
2023-03-20 07:41:34 +0700

    Update AuctionHouseMgr.cpp

--- src/server/game/Auction/AuctionHouseMgr.cpp
+++ src/server/game/Auction/AuctionHouseMgr.cpp
@@ -15,9 +15,6 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
 #include "AuctionHouseMgr.h"
 #include "AuctionHousePackets.h"
 #include "Common.h"
