a5f06252f6d791cb71b8e94c2d863d0c3a195b69
Winfidonarleyan <dowlandtop@yandex.com>
2023-03-20 07:40:49 +0700

    Update StopWatch.h

--- src/common/Utilities/StopWatch.h
+++ src/common/Utilities/StopWatch.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of the WarheadCore Project. See AUTHORS file for Copyright information
+ * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
