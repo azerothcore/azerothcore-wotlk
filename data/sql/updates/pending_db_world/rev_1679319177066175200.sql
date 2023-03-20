faad7d8e0af803e721d21ecf38dcf469774ce84d
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-20 17:35:01 +0200

    set SheatheState to 1

--- data/sql/updates/pending_db_world/rev_1679319177066175200.sql
+++ data/sql/updates/pending_db_world/rev_1679319177066175200.sql
@@ -19,6 +19,8 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 (15, 7732, 0, 0, 0, 9, 0, 10009, 0, 0, 0, 0, 0, '', 'Floon - Show gossip menu 7732 option id 0 if quest Crackin\' Some Skulls has been taken.');
 
 -- Raliq the Drunk
+UPDATE `creature_template_addon` SET `bytes2` = 1 WHERE `entry` = 18585;
+
 DELETE FROM `smart_scripts` WHERE `entryorguid`=18585 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (18585, 0, 0, 0, 0, 0, 100, 0, 5000, 5000, 15000, 15000, 0, 11, 10966, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Raliq the Drunk - In Combat - Cast \'Uppercut\''),
