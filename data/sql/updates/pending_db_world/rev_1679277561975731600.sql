5dd5e0347687403887ac805b6771d3737e3b588b
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-19 23:11:09 -0300

    Update data/sql/updates/pending_db_world/rev_1679277561975731600.sql

--- data/sql/updates/pending_db_world/rev_1679277561975731600.sql
+++ data/sql/updates/pending_db_world/rev_1679277561975731600.sql
@@ -1,7 +1,7 @@
 --
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` = 35277;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
-(13,1,35277,0,0,31,0,3,20481,0,0,0,0,"","Group 0: Spell 'Quell Raging Flames' (Effect 0) targets creature 'Raging Flames'");
+(13,1,35277,0,0,31,0,3,20481,0,0,0,0,'','Group 0: Spell 'Quell Raging Flames' (Effect 0) targets creature 'Raging Flames'');
 
 -- They're not immune even to fire, only to all mechanics
 UPDATE `creature_template` SET `ScriptName` = 'npc_raging_flames' WHERE `entry` = 20481;
