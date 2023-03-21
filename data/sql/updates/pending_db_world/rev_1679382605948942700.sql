86105824136fe0d1d51ef8fb54e4d64450b1158b
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-21 11:05:57 +0200

    remove broadcast_text update

--- data/sql/updates/pending_db_world/rev_1679382605948942700.sql
+++ data/sql/updates/pending_db_world/rev_1679382605948942700.sql
@@ -1,6 +1,4 @@
 -- Prince Keleseth (Utgarde Keep)
-UPDATE `broadcast_text` SET`SoundEntriesId`=13223 WHERE `ID`=29591 AND `LanguageID`=0;
-
 DELETE FROM `creature_text` WHERE `CreatureID`=23953 AND `GroupID`=6;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (23953, 6, 0, 'Darkness waits.', 14, 0, 0, 0, 0, 13223, 29591, 0, 'Prince Keleseth - SAY_KILL');
