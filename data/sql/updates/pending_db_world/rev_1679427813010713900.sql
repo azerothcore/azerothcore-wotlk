2de65a13b9056848cb87596760557d314bc0a258
Gultask <sagemochi@hotmail.com>
2023-03-21 21:37:28 -0300

    Update rev_1679427813010713900.sql

--- data/sql/updates/pending_db_world/rev_1679427813010713900.sql
+++ data/sql/updates/pending_db_world/rev_1679427813010713900.sql
@@ -44,7 +44,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (17801, 0, 3, 0, 0, 0, 100, 0, 9000, 12000, 10000, 17000, 0, 11, 35106, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Siren - In Combat - Cast \'Arcane Flare\''),
 (17801, 0, 5, 0, 0, 0, 100, 0, 10000, 14000, 18000, 18000, 0, 11, 38660, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Siren - In Combat - Cast \'Fear\'');
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 17803) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4));
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 17803 AND `source_type` = 0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17803, 0, 0, 0, 0, 0, 100, 2, 5000, 8000, 13000, 16000, 0, 11, 22582, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Oracle - In Combat - Cast \'Frost Shock\' (Normal Dungeon)'),
 (17803, 0, 1, 0, 0, 0, 100, 4, 5000, 8000, 13000, 16000, 0, 11, 37865, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Oracle - In Combat - Cast \'Frost Shock\' (Heroic Dungeon)'),
