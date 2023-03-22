0a2d7c8c6141be8f31e29773ebc12788af188069
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-21 22:28:05 -0300

    Update rev_1679427813010713900.sql

--- data/sql/updates/pending_db_world/rev_1679427813010713900.sql
+++ data/sql/updates/pending_db_world/rev_1679427813010713900.sql
@@ -59,7 +59,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 21338);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(21338, 0, 0, 0, 4, 0, 100, 512, 500, 500, 0, 0, 0, 31, 1, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Leper - On Aggro - Set Phase Random Between 1-3'),
+(21338, 0, 0, 0, 4, 0, 100, 512, 0, 0, 0, 0, 0, 31, 1, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Leper - On Aggro - Set Phase Random Between 1-3'),
 (21338, 0, 1, 0, 74, 1, 100, 2, 0, 40, 10000, 11000, 40, 11, 11642, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Leper - Friendly Below 40% HP - Cast \'Heal\' (Phase 1) (Normal Dungeon)'),
 (21338, 0, 2, 0, 74, 1, 100, 4, 0, 40, 10000, 11000, 40, 11, 15586, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Leper - Friendly Below 40% HP - Cast \'Heal\' (Phase 1) (Heroic Dungeon)'),
 (21338, 0, 3, 0, 0, 2, 100, 0, 4000, 6000, 7000, 8000, 0, 11, 13446, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Leper - In Combat - Cast \'Strike\' (Phase 2)'),
