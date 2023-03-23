ad7435492f815795fd20f467d721cd32dfab587f
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-23 08:53:00 +0000

    chore(DB): import pending files
    Referenced commit(s): 10c5ca69cd66f234a6023d11106eaeca17ea5057

--- data/sql/updates/db_world/2023_03_23_00.sql
+++ data/sql/updates/db_world/2023_03_23_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_22_03 -> 2023_03_23_00
 --
 UPDATE `creature_text` SET `Emote` = 33 WHERE `CreatureID` = 19220 AND `GroupID` = 6;
 UPDATE `smart_scripts` SET `action_param1` = 1 WHERE `id`=1003 AND `action_type` = 223 AND `entryorguid` IN (-138820, -138869, -138893, -138879);
