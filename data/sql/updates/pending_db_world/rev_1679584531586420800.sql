d782927931df3b5db2712ad9486847eab8b02336
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-24 13:36:31 -0300

    fix(DB/Creature): Use addon instead of SAI for Eyeless Watcher and Unseen Servant (#15551)
    Create rev_1679584531586420800.sql

---
+++ data/sql/updates/pending_db_world/rev_1679584531586420800.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (8538, 8539)) AND (`source_type` = 0) AND (`id` IN (2));
+
+DELETE FROM `creature_template_addon` WHERE (`entry` IN (8538, 8539));
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(8538, 0, 0, 0, 1, 0, 0, '16380'),
+(8539, 0, 0, 0, 1, 0, 0, '16380');
