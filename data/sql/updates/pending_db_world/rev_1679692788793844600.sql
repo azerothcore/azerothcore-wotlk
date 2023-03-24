b26837431ca7846ce69739fa0cc06c67f468eb53
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-24 19:27:13 -0300

    Update rev_1679692788793844600.sql

--- data/sql/updates/pending_db_world/rev_1679692788793844600.sql
+++ data/sql/updates/pending_db_world/rev_1679692788793844600.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `flags_extra`=`flags_extra`|64|1073741824 WHERE (`entry` = 20481);
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|64|1073741824 WHERE (`entry` IN (20481, 21538));
