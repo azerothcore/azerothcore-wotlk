da62ff127310a15f02f8308eba54c5f0f9cf03af
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-24 18:35:46 +0200

    fix(DB/SAI): Ethereal Thief disarm timer. (#15413)

---
+++ data/sql/updates/pending_db_world/rev_1678972128182408900.sql
@@ -0,0 +1,2 @@
+-- Ethereal Thief - Disarm timing
+UPDATE `smart_scripts` SET `event_param1` = 9000, `event_param2` = 12000, `event_param3` = 15000, `event_param4` = 18000 WHERE `entryorguid` = 16544 AND `source_type` = 0 AND `id` = 4;
