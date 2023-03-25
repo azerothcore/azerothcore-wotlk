4f4087d41b02a324803e9c145658612c9e30f025
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-25 15:49:14 +0200

    update

--- data/sql/updates/pending_db_world/rev_1679646147806247600.sql
+++ data/sql/updates/pending_db_world/rev_1679646147806247600.sql
@@ -106,7 +106,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 -- ID 20925 (Scalded Basilisk)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(20925, 0, 1, 0, 0, 0, 85, 0, 8000, 13000, 14000, 19000, 0, 11, 5568, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Ragestone Trampler - In Combat - Cast \'Heat Wave\''),
+(20925, 0, 1, 0, 0, 0, 85, 0, 8000, 13000, 14000, 19000, 0, 11, 35236, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Ragestone Trampler - In Combat - Cast \'Heat Wave\''),
 (20925, 0, 3, 4, 61, 0, 100, 0, 0, 0, 0, 0, 0, 224, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ragestone Trampler - Between 0-15% Health - Stop Attacking'),
 (20925, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ragestone Trampler - Between 0-15% Health - Set Event Phase 1'),
 (20925, 0, 5, 6, 0, 1, 100, 0, 6000, 6000, 0, 0, 0, 11, 35385, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Ragestone Trampler - In Combat - Cast \'Threshalisk Charge\' (Phase 1)'),
