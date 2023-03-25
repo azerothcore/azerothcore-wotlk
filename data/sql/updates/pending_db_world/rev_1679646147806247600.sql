a6472b1f45a08e6cc83ded8619b1b0aedb90ef56
avarishd <46330494+avarishd@users.noreply.github.com>
2023-03-25 17:40:18 +0200

    sniff for 20283 (Marshrock Stomper)
    Co-Authored-By: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1679646147806247600.sql
+++ data/sql/updates/pending_db_world/rev_1679646147806247600.sql
@@ -7,13 +7,13 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 -- All creatures that use Threshalisk Charge (35385) and Feign Death (28728)
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` IN (20607,20924,19730,19729,20283,19706,20279,20280,20987,20925);
 
--- ID 20283 (Marshrock Stomper) // No source that he casts Feign Death
-DELETE FROM `creature_text` WHERE `CreatureID` IN (20607,20924,19730,19729,19706,20279,20280,20987,20925);
+DELETE FROM `creature_text` WHERE `CreatureID` IN (20607,20924,19730,19729,20283,19706,20279,20280,20987,20925);
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (20607, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Craghide Basilisk - EMOTE_STOP_PLAY_DEAD'),
 (20924, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Grishnath Basilisk - EMOTE_STOP_PLAY_DEAD'),
 (19730, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Ironspine Gazer - EMOTE_STOP_PLAY_DEAD'),
 (19729, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Ironspine Threshalisk - EMOTE_STOP_PLAY_DEAD'),
+(20283, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Marshrock Stomper - EMOTE_STOP_PLAY_DEAD'),
 (19706, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Marshrock Threshalisk - EMOTE_STOP_PLAY_DEAD'),
 (20279, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Ragestone Threshalisk - EMOTE_STOP_PLAY_DEAD'),
 (20280, 0, 0, '%s stops playing dead and charges forward!', 16, 0, 100, 0, 0, 0, 18325, 0, 'Ragestone Trampler - EMOTE_STOP_PLAY_DEAD'),
@@ -63,8 +63,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 -- ID 20283 (Marshrock Stomper)
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 20283;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(20283, 0, 0, 0, 9, 0, 20, 0, 0, 40, 0, 0, 0, 11, 35385, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Stomper - Within 0-40 Range - Cast \'Threshalisk Charge\''),
-(20283, 0, 1, 0, 0, 0, 100, 0, 5000, 10000, 15000, 30000, 0, 11, 12612, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Stomper - In Combat - Cast \'Stomp\'');
+(20283, 0, 0, 0, 0, 0, 100, 0, 5000, 10000, 15000, 30000, 0, 11, 12612, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Stomper - In Combat - Cast \'Stomp\''),
+(20283, 0, 1, 2, 2, 0, 30, 0, 0, 30, 0, 0, 0, 11, 28728, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Threshalisk - Between 0-30% Health - Cast \'Feign Death\''),
+(20283, 0, 2, 3, 61, 0, 100, 0, 0, 0, 0, 0, 0, 224, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Threshalisk - Between 0-30% Health - Stop Attacking'),
+(20283, 0, 3, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Threshalisk - Between 0-30% Health - Set Event Phase 1'),
+(20283, 0, 4, 5, 0, 1, 100, 0, 6000, 6000, 0, 0, 0, 11, 35385, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Threshalisk - In Combat - Cast \'Threshalisk Charge\' (Phase 1)'),
+(20283, 0, 5, 0, 61, 1, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Marshrock Threshalisk - In Combat - Say Line 0 (Phase 1)');
 
 -- ID 19706 (Marshrock Threshalisk)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
