9f60be8a9ab6620228eba9ae47fe16465961d30a
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-03-27 19:41:09 +0200

    fix(DB/Loot): removal of BOP quest loot
    removes two quest items (Gold-Trimmed Cuffs and Bloodforged Guard) from a reference loot table used by a few mobs in Hellfire Peninsula.

---
+++ data/sql/updates/pending_db_world/remove-bop-quest-loot.sql
@@ -0,0 +1,2 @@
+DELETE FROM reference_loot_template WHERE (`Entry` = 4110) AND (`Item` = 30520); --remove row with Gold-Trimmed Cuffs
+DELETE FROM reference_loot_template WHERE (`Entry` = 4110) AND (`Item` = 30986); --remove row with Bloodforged Guard
