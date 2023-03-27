e03992dde7cb801bb349c74214ae5a48d358ce4b
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-03-27 19:49:29 +0200

    Update data/sql/updates/pending_db_world/remove-bop-quest-loot.sql
    from 2 lines to 1
    
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/remove-bop-quest-loot.sql
+++ data/sql/updates/pending_db_world/remove-bop-quest-loot.sql
@@ -1,2 +1,1 @@
-DELETE FROM reference_loot_template WHERE (`Entry` = 4110) AND (`Item` = 30520); --remove row with Gold-Trimmed Cuffs
-DELETE FROM reference_loot_template WHERE (`Entry` = 4110) AND (`Item` = 30986); --remove row with Bloodforged Guard
+DELETE FROM reference_loot_template WHERE (`Entry` = 4110) AND (`Item` IN (30520, 30986));
