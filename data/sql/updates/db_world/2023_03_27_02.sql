fc4a2f42de3628fb1f3535c1535df8335a55de24
AzerothCoreBot <azerothcorebot@gmail.com>
2023-03-27 17:28:13 +0000

    chore(DB): import pending files
    Referenced commit(s): e18aec9e5d729c2e8bbaaf6eb6082da4e7941bd7

--- data/sql/updates/db_world/2023_03_27_02.sql
+++ data/sql/updates/db_world/2023_03_27_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_27_01 -> 2023_03_27_02
 -- Repair Pathaleon the Calculator Loot Table
 UPDATE `reference_loot_template` SET `GroupId`=5 WHERE `Entry`=35005 AND `Item` IN (28285, 28278, 28286, 28288, 28275, 27899);
 DELETE FROM `creature_loot_template` WHERE `Entry`=19220 AND `Item`=35005 AND `Reference`=35005 AND `GroupId`=5;
--- data/sql/updates/db_world/2023_03_27_03.sql
+++ data/sql/updates/db_world/2023_03_27_03.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_03_27_02 -> 2023_03_27_03
 -- Verified in Build: 47213
 UPDATE `creature_template` SET `faction` = 1860 WHERE `entry` IN (
 21651, -- Time-Lost Skettis Reaver
