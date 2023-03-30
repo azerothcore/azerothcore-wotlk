81ac4b5c838565513310e02c71a0d1aac8147182
murinotania <129438230+murinotania@users.noreply.github.com>
2023-03-30 22:32:25 +0300

    Update acore.sh
    degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates

--- acore.sh
+++ acore.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-
-[ -z "$WITH_ERRORS" ] && set -e
-
-CUR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-
-source "$CUR_PATH/apps/installer/main.sh"
+degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates degenerates
