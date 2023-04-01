7e06cec402d6d011d7d21202f10d3fdce0f32a10
Gultask <100873791+Gultask@users.noreply.github.com>
2023-04-01 17:31:32 -0300

    Update rev_1680339981481024600.sql

--- data/sql/updates/pending_db_world/rev_1680339981481024600.sql
+++ data/sql/updates/pending_db_world/rev_1680339981481024600.sql
@@ -1,4 +1,4 @@
 --
 DELETE FROM `spell_proc_event` WHERE `entry`=43730;
-INSERT INTO `spell_proc_event` VALUES
+INSERT INTO `spell_proc_event` VALUES (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`)
 (43730,0,0,0,0,0,0,0,2,0,0,8000);
