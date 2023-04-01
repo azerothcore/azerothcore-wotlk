945af7668fcc73a326ab94b6700593be631f3d82
Gultask <100873791+Gultask@users.noreply.github.com>
2023-03-31 23:00:35 -0300

    Update data/sql/updates/pending_db_world/rev_1678624730000970200.sql

--- data/sql/updates/pending_db_world/rev_1678624730000970200.sql
+++ data/sql/updates/pending_db_world/rev_1678624730000970200.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature` SET `position_x`=-8399.23, `position_y`=1246.33, `position_z`=5.31357 WHERE `guid`=244630;
+UPDATE `creature` SET `position_x`=-8399.23, `position_y`=1246.33, `position_z`=5.31357 WHERE `guid`=244630 AND `id1`=37715;
