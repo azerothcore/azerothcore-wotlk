c1d11e53c1552150438d1abe0b05bbc2eb78d8c9
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-04-02 21:15:49 +0200

    Update rev_1680443009841196300.sql
    fixed according to standards. still likely needs to be looked over when it comes to details

--- data/sql/updates/pending_db_world/rev_1680443009841196300.sql
+++ data/sql/updates/pending_db_world/rev_1680443009841196300.sql
@@ -1,4 +1,6 @@
 -- Ferra loot update
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 14308) AND (`Item` IN (4500, 11414, 11415, 24016));
+
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (14308, 4500, 0, 1, 0, 1, 0, 1, 1, 'Ferra - Traveler\'s Backpack');
 
