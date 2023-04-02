0acfa86d6ceede8cabc83868393e4e9e86919bc0
avarishd <46330494+avarishd@users.noreply.github.com>
2023-04-03 00:15:43 +0300

    fix(DB/creature_addon): Remove deprecated bytes2 flags.

---
+++ data/sql/updates/pending_db_world/rev_1680463160388009300.sql
@@ -0,0 +1,11 @@
+-- Remove old/deprecated bytes2 flags.
+UPDATE `creature_addon` SET `bytes2` = 1 WHERE `bytes2` IN (4092,4096,4097,4098,256,257,301993985,318771201,285216769);
+UPDATE `creature_addon` SET `bytes2` = 0 WHERE `bytes2` IN (733,141313,234885121,251662337);
+UPDATE `creature_addon` SET `bytes2` = 2 WHERE `bytes2` IN (258,4097,4098);
+
+-- Elder Springpaw, Scorpid Bonecrawler & Blackwind Sabercat wrong spawns.
+DELETE FROM `creature_addon` WHERE `guid` IN (75829,77583,77606,77607,82379);
+DELETE FROM `creature` WHERE `guid` IN (75829,77583,82379);
+
+-- Hatecrest Warrior defensive stance, handled by SAI
+UPDATE `creature_addon` SET `auras` = NULL WHERE `guid` = 51476;
