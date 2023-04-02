d7d58625de96c7d1bbe4e32b9f34499eb9663fc4
Gultask <100873791+Gultask@users.noreply.github.com>
2023-04-02 15:52:28 -0300

    Update rev_1675140644244916700.sql

--- data/sql/updates/pending_db_world/rev_1675140644244916700.sql
+++ data/sql/updates/pending_db_world/rev_1675140644244916700.sql
@@ -1,7 +1,6 @@
 --
-
 DELETE FROM `reference_loot_template` WHERE `Entry` = 24076 AND `Item` = 1927;
 
 DELETE FROM `creature_loot_template` WHERE `Entry` = 594 AND `Item` = 1927;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(594,1927,0,6,0,1,1,1,1,"Defias Henchman - Deadmines Cleaver");
+(594,1927,0,6,0,1,1,1,1,'Defias Henchman - Deadmines Cleaver');
