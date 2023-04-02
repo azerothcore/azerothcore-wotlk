d31452067142381f1fd65fe827a0b6c93303e350
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-04-02 15:56:05 +0200

    added sql file

---
+++ data/sql/updates/pending_db_world/rev_1680443009841196300.sql
@@ -0,0 +1,6 @@
+-- Ferra loot update
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(14308, 4500, 0, 1, 0, 1, 0, 1, 1, 'Ferra - Traveler\'s Backpack');
+
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
+(14308, 24016, 24016, 1, 0, 1, 0, 1, 1, 'Ferra - (ReferenceTable)');
