6569086fec15ade43e8faf6beb8cc5a7254ef6e6
Tereneckla <Tereneckla@pm.me>
2023-04-03 14:40:11 +0200

    Create rev_1680525460717061700.sql

---
+++ data/sql/updates/pending_db_world/rev_1680525460717061700.sql
@@ -0,0 +1,2 @@
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`) VALUES
+(1, 17798, 24313, 7, 197, 1);
