dc91da3d79d8ae590f257001a714182c1de50064
avarishd <46330494+avarishd@users.noreply.github.com>
2023-04-03 23:17:29 +0300

    creature_template_addon

--- data/sql/updates/pending_db_world/rev_1680463160388009300.sql
+++ data/sql/updates/pending_db_world/rev_1680463160388009300.sql
@@ -1,10 +1,11 @@
 -- Remove old/deprecated bytes2 flags.
-UPDATE `creature_addon` SET `bytes2` = 1 WHERE `bytes2` IN (257,733,4092,4097,301993985,318771201,285216769,234885121);
-UPDATE `creature_addon` SET `bytes2` = 0 WHERE `bytes2` IN (256,4096);
+UPDATE `creature_addon` SET `bytes2` = 1 WHERE `bytes2` IN (257,733,4097,301993985,318771201,285216769,234885121);
+UPDATE `creature_addon` SET `bytes2` = 0 WHERE `bytes2` IN (256,4092,4096);
 UPDATE `creature_addon` SET `bytes2` = 2 WHERE `bytes2` IN (258,4098);
 
-UPDATE `creature_template_addon` SET `bytes2` = 1 WHERE `bytes2` IN (133121,10241,2305,2049,69,3);
-UPDATE `creature_template_addon` SET `bytes2` = 0 WHERE `bytes2` = 65536;
+UPDATE `creature_template_addon` SET `bytes2` = 1 WHERE `bytes2` IN (3,69,257,2049,2305,4097,10241,133121,234885121,285216769);
+UPDATE `creature_template_addon` SET `bytes2` = 0 WHERE `bytes2` IN (256,4092,4096,65536);
+UPDATE `creature_template_addon` SET `bytes2` = 2 WHERE `bytes2` IN (258,4098);
 
 -- Creature where neither of the values in creature_addon were used.
 DELETE FROM `creature_addon` WHERE `guid` IN (75829,77583,77606,77607,82379,73192,80419,81738,82004,32943);
