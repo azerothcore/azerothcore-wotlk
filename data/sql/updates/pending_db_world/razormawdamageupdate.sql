a69a31d9193112e376a6c826cdce468a1959754e
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-04-04 01:55:59 +0200

    Update razormawdamageupdate.sql
    credits to Gultask for value based on mangos core

--- data/sql/updates/pending_db_world/razormawdamageupdate.sql
+++ data/sql/updates/pending_db_world/razormawdamageupdate.sql
@@ -1,3 +1,3 @@
--- damage modifier edit of Razormaw from 7.5 to 2.5
+-- damage modifier edit of Razormaw from 7.5 to 1.36 based on mangos values
 
-UPDATE `creature_template` SET `DamageModifier` = 2.5 WHERE `entry` = 17592 AND `modelid1` = 17256;
+UPDATE `creature_template` SET `DamageModifier` = 1.36 WHERE `entry` = 17592 AND `modelid1` = 17256;
