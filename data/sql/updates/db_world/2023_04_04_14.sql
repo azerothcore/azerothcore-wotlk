00e42645b8b625e04de141b4aa9aabdd114dda08
AzerothCoreBot <azerothcorebot@gmail.com>
2023-04-04 17:54:37 +0000

    chore(DB): import pending files
    Referenced commit(s): db0e94c66999ea2cca50f44c8383e35c809a7e42

--- data/sql/updates/db_world/2023_04_04_14.sql
+++ data/sql/updates/db_world/2023_04_04_14.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_04_04_13 -> 2023_04_04_14
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 17798 AND `SourceEntry` = 24313 AND `ConditionTypeOrReference` = 7;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
 (1, 17798, 24313, 7, 197, 1, 'Pattern: Battlecast Hood requires Tailoring to drop');
