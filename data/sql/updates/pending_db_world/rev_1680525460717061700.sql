3fa36a6ef890b036bfba52db8eeaefdab1753807
Tereneckla <Tereneckla@pm.me>
2023-04-04 15:37:37 +0200

    Add Comment to HC drop condition

--- data/sql/updates/pending_db_world/rev_1680525460717061700.sql
+++ data/sql/updates/pending_db_world/rev_1680525460717061700.sql
@@ -1,3 +1,4 @@
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 17798 AND `SourceEntry` = 24313 AND `ConditionTypeOrReference` = 7;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
 (1, 17798, 24313, 7, 197, 1, 'Pattern: Battlecast Hood requires Tailoring to drop');
+UPDATE `conditions` SET `Comment` = 'Pattern: Battlecast Hood requires Tailoring to drop' WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 20633 AND `SourceEntry` = 24313 AND `ConditionTypeOrReference` = 7;
