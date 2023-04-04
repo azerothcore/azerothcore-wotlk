9ce767e6537df4a99a3acd0d2407961f7b27af31
Tereneckla <50233983+Tereneckla@users.noreply.github.com>
2023-04-04 13:32:32 +0000

    Add comment

--- data/sql/updates/pending_db_world/rev_1680525460717061700.sql
+++ data/sql/updates/pending_db_world/rev_1680525460717061700.sql
@@ -1,3 +1,3 @@
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 17798 AND `SourceEntry` = 24313 AND `ConditionTypeOrReference` = 7;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`) VALUES
-(1, 17798, 24313, 7, 197, 1);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
+(1, 17798, 24313, 7, 197, 1, 'Pattern: Battlecast Hood requires Tailoring to drop');
