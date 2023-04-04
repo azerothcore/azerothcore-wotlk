7539dbbf0f95c08cfd3c3b8dfa2f41c281d71bbc
Tereneckla <50233983+Tereneckla@users.noreply.github.com>
2023-04-04 08:03:15 +0000

    Update rev_1680525460717061700.sql

--- data/sql/updates/pending_db_world/rev_1680525460717061700.sql
+++ data/sql/updates/pending_db_world/rev_1680525460717061700.sql
@@ -1,3 +1,3 @@
-DELETE FROM `conditions` WHERE `SourceGroup` = 17798 AND `SourceEntry` = 24313;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 17798 AND `SourceEntry` = 24313 AND `ConditionTypeOrReference` = 7;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`) VALUES
 (1, 17798, 24313, 7, 197, 1);
