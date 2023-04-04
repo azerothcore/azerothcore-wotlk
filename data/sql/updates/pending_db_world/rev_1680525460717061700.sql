7a29fc36eab1850e8dfc574405f546cb05c7e955
Tereneckla <Tereneckla@pm.me>
2023-04-04 09:59:09 +0200

    Add delete

--- data/sql/updates/pending_db_world/rev_1680525460717061700.sql
+++ data/sql/updates/pending_db_world/rev_1680525460717061700.sql
@@ -1,2 +1,3 @@
+DELETE FROM `conditions` WHERE `SourceGroup` = 17798 AND `SourceEntry` = 24313;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,`ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`) VALUES
 (1, 17798, 24313, 7, 197, 1);
