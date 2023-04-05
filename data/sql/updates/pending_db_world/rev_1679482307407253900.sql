dbc04e33285d15ea72ab5bfd1973d04885c9278a
neifion-00000000 <91289495+neifion-00000000@users.noreply.github.com>
2023-04-05 12:22:46 -0500

    fix(DB/Loot): Junk Loot from Water Barrels removed (#15540)
    * fix(db/gameobject_loot):  Junk Loot from Water Barrels removed
    
    * Update data/sql/updates/pending_db_world/rev_1679482307407253900.sql

---
+++ data/sql/updates/pending_db_world/rev_1679482307407253900.sql
@@ -0,0 +1,3 @@
+--
+-- Remove Pollution from Water Barrels (gameobject 3658)
+DELETE FROM `gameobject_loot_template` WHERE `Entry`=2502 AND `Item` IN (851, 852, 853, 854, 858, 1196, 1197, 1198, 2207, 2455, 4765, 4766, 4777, 4778);
