bf7e1a074510de657fdb011daa1a1cc0ea2f199e
fangshun <fangshun2004@msn.com>
2023-04-05 09:34:18 +0800

    Add data to the data table

---
+++ data/sql/updates/pending_db_world/rev_1680658371591699800.sql
@@ -0,0 +1,2 @@
+-- Add Highlord Tirion Fordring - SAY_TIRION_OUTRO_3
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES (38995, 4, 0, 'The Lich King must fall!!', 14, 0, 0, 0, 0, 17389, 38113, 0, 'Highlord Tirion Fordring - SAY_TIRION_OUTRO_3');
