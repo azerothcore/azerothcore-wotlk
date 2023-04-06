dbfc87ef33734641a73234becacd94be21d41f6a
fangshun <fangshun2004@msn.com>
2023-04-06 23:54:37 +0800

    Some other text/options link to BroadcastText

---
+++ data/sql/updates/pending_db_world/rev_1680795326212575700.sql
@@ -0,0 +1,25 @@
+## I appear to have lost my ring.
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38227 WHERE `MenuID`=10996 AND `OptionID`=6;
+## You must not approach the Frost Queen. Quickly, stop them!
+UPDATE `creature_text` SET `BroadcastTextId`=37105 WHERE `CreatureID`=37503 AND `GroupID`=0 AND `ID`=0;
+## It won't.
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=0;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=1;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=2;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=3;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=4;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=5;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=6;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=7;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=8;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=9;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=10;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=11;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=12;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=13;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=14;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=15;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=16;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=17;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=18;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=38229 WHERE `MenuID`=10995 AND `OptionID`=19;
