40d67fa5b85500d584796110144e9864ce101954
avarishd <46330494+avarishd@users.noreply.github.com>
2023-04-07 13:29:35 +0300

    fix(DB/creature_formations): Aggro Strider Clutchmother when adds aggro.

---
+++ data/sql/updates/pending_db_world/rev_1680858441993201000.sql
@@ -0,0 +1,4 @@
+-- Strider Clutchmother (2172)
+UPDATE `creature_formations` SET `groupAI`=`groupAI`|1 WHERE `leaderGUID`=36692 AND`memberGUID`=36692;
+UPDATE `creature_formations` SET `groupAI`=`groupAI`|1 WHERE `leaderGUID`=37385 AND`memberGUID`=36692;
+UPDATE `creature_formations` SET `groupAI`=`groupAI`|1 WHERE `leaderGUID`=37385 AND `memberGUID`=205803;
