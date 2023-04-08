61d244592579fe7eba70505845a8ea1b3b8f58e9
Gultask <sagemochi@hotmail.com>
2023-04-08 12:13:41 -0300

    Update rev_1680966459127893700.sql

--- data/sql/updates/pending_db_world/rev_1680966459127893700.sql
+++ data/sql/updates/pending_db_world/rev_1680966459127893700.sql
@@ -1,2 +1,2 @@
 -- Terokkarantula and Arconus the Insatiable
-UPDATE `creature` SET `spawntimesecs` = 240 WHERE `guid` IN (86843, 87039) AND `id1` IN (20554, 20682);
+UPDATE `creature` SET `spawntimesecs` = 180 WHERE `guid` IN (86843, 87039) AND `id1` IN (20554, 20682);
