a8a3011278487f1e10017d082ceaa3f44a4e69b8
Timothy Vaccarelli <tmvfroid@gmail.com>
2023-04-12 07:07:00 -0400

    fix(Script/Spell): Fix animal blood pool effect and position (#15918)
    Adapted from TC Commits 3ddc6e70df0401a0677bef536e6b0e46e3dbf9e9 and cc3293c0023b36434747190103a1f0ffee0f8488
    
    Co-authored-by: sirikfoll <sirikfoll@hotmail.com>
    Co-authored-by: Rushor <rushor@github.com>

---
+++ data/sql/updates/pending_db_world/rev_1681232229972193910.sql
@@ -0,0 +1,5 @@
+-- Add spell script to database for 63471 - Spawn Blood Pool
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_spawn_blood_pool';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(63471, 'spell_spawn_blood_pool');
+
