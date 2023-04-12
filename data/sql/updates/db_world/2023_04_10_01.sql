9d99a42bbad0521a27410e8d91fd9f50b722ad30
cTrinity <58204838+cTrinity@users.noreply.github.com>
2023-04-12 08:29:50 +0800

    fix(DB/Creature): Update En'kilah Hatchling Flags
    update En'kilah Hatchling(id = 25388,25390) unit_flags add 256 (Unable to fight or communicate with players)

---
+++ data/sql/updates/db_world/2023_04_10_01.sql
@@ -0,0 +1,4 @@
+-- DB update En'kilah Hatchling unit_flags add 256 (Unable to fight or communicate with players)
+
+-- unit_flags 256- Cannot fight or communicate with players
+UPDATE creature_template SET unit_flags = unit_flags | 256 WHERE entry IN (25388,25390);
