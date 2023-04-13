a44e56766a640b2891fb3bc3910ad64952619b0c
Alvin Zhu <alvin.zhuge@gmail.com>
2023-04-13 22:05:00 +0800

    fix

--- src/server/scripts/Kalimdor/zone_silithus.cpp
+++ src/server/scripts/Kalimdor/zone_silithus.cpp
@@ -579,11 +579,10 @@ public:
                     case 60:
                         Talk(ANACHRONOS_SAY_10, player);
                         me->GetMotionMaster()->MoveCharge(-8113.46f, 1524.16f, 2.89f, 4);
-                        player->CompleteQuest(QUEST_A_PAWN_ON_THE_ETERNAL_BOARD);
                         break;
                     case 61:
                         me->GetMotionMaster()->MoveCharge(-8057.1f, 1470.32f, 2.61f, 6);
-                        if (player->IsInRange(me, 0, 15))
+                        if (player->IsInRange(me, 0, 50))
                             player->GroupEventHappens(QUEST_A_PAWN_ON_THE_ETERNAL_BOARD, me);
                         break;
                     case 62:
