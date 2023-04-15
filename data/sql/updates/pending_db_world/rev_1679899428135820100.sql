947e47a5bef0a67bc51351de5178be84445a82dd
avarishd <46330494+avarishd@users.noreply.github.com>
2023-04-15 23:06:22 +0300

    correct Morcrush threshhold, its shardling and Crazed shardling

--- data/sql/updates/pending_db_world/rev_1679899428135820100.sql
+++ data/sql/updates/pending_db_world/rev_1679899428135820100.sql
@@ -31,9 +31,9 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 20202);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (20202, 0, 0, 0, 9, 0, 100, 0, 0, 10, 12000, 16000, 0, 11, 32959, 2, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Within 0-10 Range - Cast \'Knock Away\''),
-(20202, 0, 1, 0, 2, 0, 100, 512, 0, 90, 0, 0, 0, 11, 36048, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Between 0-75% Health - Cast \'Serverside - Summon Motherlode Shardling\''),
-(20202, 0, 2, 0, 2, 0, 100, 512, 0, 60, 0, 0, 0, 11, 36049, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Between 0-50% Health - Cast \'Serverside - Summon Motherlode Shardling\''),
-(20202, 0, 3, 0, 2, 0, 100, 512, 0, 30, 0, 0, 0, 11, 36050, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Between 0-25% Health - Cast \'Serverside - Summon Motherlode Shardling\'');
+(20202, 0, 1, 0, 2, 0, 100, 512, 0, 90, 0, 0, 0, 11, 36048, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Between 0-90% Health - Cast \'Serverside - Summon Motherlode Shardling\''),
+(20202, 0, 2, 0, 2, 0, 100, 512, 0, 60, 0, 0, 0, 11, 36049, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Between 0-60% Health - Cast \'Serverside - Summon Motherlode Shardling\''),
+(20202, 0, 3, 0, 2, 0, 100, 512, 0, 30, 0, 0, 0, 11, 36050, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Cragskaar - Between 0-30% Health - Cast \'Serverside - Summon Motherlode Shardling\'');
 -- ID 21079 (Cragskaar Shardling)
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 21079;
 
@@ -71,3 +71,27 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 21323;
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 21323) AND (`source_type` = 0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (21323, 0, 0, 0, 1, 0, 100, 0, 15000, 15000, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Netherock Crumbler - Out of Combat - Despawn Instant');
+
+-- ID 18690 (Morcrush)
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 18690);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(18690, 0, 0, 0, 9, 0, 100, 0, 0, 5, 12000, 16000, 0, 11, 35238, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush - Within 0-5 Range - Cast \'War Stomp\''),
+(18690, 0, 1, 0, 2, 0, 100, 512, 0, 75, 0, 0, 0, 11, 38888, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush - Between 0-75% Health - Cast \'Serverside - Summon Morcrush Shardling\''),
+(18690, 0, 2, 0, 2, 0, 100, 512, 0, 50, 0, 0, 0, 11, 38889, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush - Between 0-50% Health - Cast \'Serverside - Summon Morcrush Shardling\''),
+(18690, 0, 3, 0, 2, 0, 100, 512, 0, 25, 0, 0, 0, 11, 38890, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush - Between 0-25% Health - Cast \'Serverside - Summon Morcrush Shardling\'');
+-- ID 22344 (Morcrush Shardling)
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 22344;
+
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 22344);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(22344, 0, 0, 0, 9, 0, 100, 0, 0, 10, 16000, 21000, 0, 11, 8078, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush Shardling - Within 0-10 Range - Cast \'Thunderclap\''),
+(22344, 0, 1, 0, 2, 0, 100, 0, 0, 50, 0, 0, 0, 11, 34970, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush Shardling - Between 0-50% Health - Cast \'Frenzy\''),
+(22344, 0, 2, 0, 1, 0, 100, 0, 60000, 60000, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Morcrush Shardling - Out of Combat - Despawn Instant');
+
+-- ID 21936 (Crazed Shardling)
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 21936;
+
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 21936);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(21936, 0, 0, 0, 0, 0, 100, 0, 3000, 3000, 120000, 120000, 0, 11, 34970, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Crazed Shardling - In Combat - Cast \'Frenzy\''),
+(21936, 0, 1, 0, 1, 0, 100, 0, 15000, 15000, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Crazed Shardling - Out of Combat - Despawn Instant');
