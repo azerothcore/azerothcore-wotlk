2936f3d6aa517c1c5948be230251cf45ac15ebd1
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2023-04-17 23:35:36 -0600

    pew

--- data/sql/updates/pending_db_world/rev_1681793209169080500.sql
+++ data/sql/updates/pending_db_world/rev_1681793209169080500.sql
@@ -1,4 +1,4 @@
 --
 DELETE FROM `spell_custom_attr` WHERE `spell_id`=33684;
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (33684,12288);
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (33684,4096);
 
