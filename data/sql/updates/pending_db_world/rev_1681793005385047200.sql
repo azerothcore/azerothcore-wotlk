3dc96990688804a88b65f98c72cba9f2a0cea017
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2023-04-18 18:31:06 -0600

    fix(DB/SAI): Fel Overseer - Frightening Shout (#15995)
    init

---
+++ data/sql/updates/pending_db_world/rev_1681793005385047200.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `event_flags`=0, `event_param1`=8000, `event_param2`=12000, `event_param3`=28000, `event_param4`=32000, `target_type`=6, `comment`='Fel Overseer - In Combat - Cast \'Frightening Shout\'' WHERE `entryorguid`=18796 AND `source_type`=0 AND `id`=3 AND `link`=0;
+
