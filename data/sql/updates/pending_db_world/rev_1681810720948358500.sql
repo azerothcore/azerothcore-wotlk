6cfa0a4a14ab92a4646a46aca614f3cea7f4555a
avarishd <46330494+avarishd@users.noreply.github.com>
2023-04-19 14:18:05 +0300

    remove no dodge extra flag

--- data/sql/updates/pending_db_world/rev_1681810720948358500.sql
+++ data/sql/updates/pending_db_world/rev_1681810720948358500.sql
@@ -1,6 +1,6 @@
 -- Bosses on the Ogrila quest chain
--- Slaag + no dodge
-UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|545470038, `flags_extra` = `flags_extra`|8388608 WHERE `entry` = 22199;
+-- Slaag
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|545470038 WHERE `entry` = 22199;
 -- Maggoc, Grulloc, Skulloc Soulgrinder
 UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|579026774 WHERE `entry` IN (20600,20216,22910);
 -- Vim'gol the Vile
