e4e05f8997daf4e9a7d345aa50e14226b83cd360
Gultask <100873791+Gultask@users.noreply.github.com>
2023-04-18 21:05:56 -0300

    Update data/sql/updates/pending_db_world/rev_1681039353004394600.sql

--- data/sql/updates/pending_db_world/rev_1681039353004394600.sql
+++ data/sql/updates/pending_db_world/rev_1681039353004394600.sql
@@ -1,3 +1,3 @@
 --
 -- Quicker Respawn Timer on Corporal Keeshan
-UPDATE `creature` SET `spawntimesecs`=120 WHERE  `guid`=17874;
+UPDATE `creature` SET `spawntimesecs`=120 WHERE `guid`=17874 AND `id1`=349;
