ef932a78dff68bdc532140c0320035097dc1d8eb
Gultask <100873791+Gultask@users.noreply.github.com>
2023-04-18 21:19:59 -0300

    Update data/sql/updates/pending_db_world/rev_1681824132109329700.sql

--- data/sql/updates/pending_db_world/rev_1681824132109329700.sql
+++ data/sql/updates/pending_db_world/rev_1681824132109329700.sql
@@ -1,5 +1,5 @@
 -- Bogblossom Bunny SAI (Source: https://www.youtube.com/watch?v=EIFhKHtvOe0) 
-UPDATE `creature_template` SET `AIName` = "SmartAI" WHERE `entry` = 23104;
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 23104;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 23104 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (23104,0,0,1,54,0,100,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,"Bogblossom Bunny - On Just Summoned - Say Line 0"),
