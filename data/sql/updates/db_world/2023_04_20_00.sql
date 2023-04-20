6634d6a581484499a2739d6c5ffffd3095ce431c
AzerothCoreBot <azerothcorebot@gmail.com>
2023-04-20 21:19:31 +0000

    chore(DB): import pending files
    Referenced commit(s): b86cf11499f148376639401d479ddc8d33723b77

--- data/sql/updates/db_world/2023_04_20_00.sql
+++ data/sql/updates/db_world/2023_04_20_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_04_19_34 -> 2023_04_20_00
 --
 UPDATE `creature_template` SET `flags_extra` = `flags_extra`|33554432 WHERE `entry` IN (16523, 16594, 17083, 17420, 17694, 16507, 17462, 18404, 17993, 18421, 19486, 18422, 19557, 19505); -- Botanica & SHH
 UPDATE `creature_template` SET `flags_extra` = `flags_extra`|33554432 WHERE `entry` IN (20567, 20576, 20577, 20587, 20591, 20593, 20595, 21548, 21549, 21555, 21570, 21571, 21572, 21577); -- Botanica & SHH Heroics
