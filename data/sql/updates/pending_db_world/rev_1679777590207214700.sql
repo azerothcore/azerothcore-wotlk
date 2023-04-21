6205ad2729e778de82899ec5e3ebc6b610bef794
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-04-21 07:33:03 -0300

    Delete rev_1679777590207214700.sql

--- data/sql/updates/pending_db_world/rev_1679777590207214700.sql
+++
@@ -1,8 +0,0 @@
---
-UPDATE `creature_template` SET `faction`=1761 WHERE `entry`=19300;
-UPDATE `creature_template` SET `faction`=1762 WHERE `entry`=19301;
-UPDATE `creature_template` SET `faction`=1763 WHERE `entry`=19302;
-UPDATE `creature_template` SET `faction`=1764 WHERE `entry`=19303;
-UPDATE `creature_template` SET `faction`=1765 WHERE `entry`=19304;
-
-DELETE FROM `creature` WHERE `id1` BETWEEN 19300 AND 19304;
