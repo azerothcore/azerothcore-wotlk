bb94b1a822122df7d122b17eaa14a268d7f7b111
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-04-22 12:08:03 -0300

    fix(Scripts/ShadowLabirynth): Clean up script and fix doors

--- src/server/scripts/Outland/Auchindoun/ShadowLabyrinth/instance_shadow_labyrinth.cpp
+++ src/server/scripts/Outland/Auchindoun/ShadowLabyrinth/instance_shadow_labyrinth.cpp
@@ -19,6 +19,13 @@
 #include "ScriptMgr.h"
 #include "shadow_labyrinth.h"
 
+DoorData const doorData[] =
+{
+    { GO_REFECTORY_DOOR,      DATA_BLACKHEARTTHEINCITEREVENT, DOOR_TYPE_PASSAGE },
+    { GO_SCREAMING_HALL_DOOR, DATA_GRANDMASTER_VORPIL,        DOOR_TYPE_PASSAGE },
+    { 0,                      0,                              DOOR_TYPE_ROOM    }  // END
+};
+
 class instance_shadow_labyrinth : public InstanceMapScript
 {
 public:
@@ -31,47 +38,20 @@ public:
 
     struct instance_shadow_labyrinth_InstanceMapScript : public InstanceScript
     {
-        instance_shadow_labyrinth_InstanceMapScript(Map* map) : InstanceScript(map) {}
-
-        uint32 m_auiEncounter[MAX_ENCOUNTER];
+        instance_shadow_labyrinth_InstanceMapScript(Map* map) : InstanceScript(map)
+        {
+            SetBossNumber(EncounterCount);
+            SetPersistentDataCount(PersistentDataCount);
+            LoadDoorData(doorData);
+        }
 
         ObjectGuid m_uiHellmawGUID;
-        ObjectGuid m_uiRefectoryDoorGUID;
-        ObjectGuid m_uiScreamingHallDoorGUID;
 
         uint32 _ritualistsAliveCount;
 
         void Initialize() override
         {
-            memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
-
             _ritualistsAliveCount = 0;
-        }
-
-        bool IsEncounterInProgress() const override
-        {
-            for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
-                if (m_auiEncounter[i] == IN_PROGRESS)
-                    return true;
-
-            return false;
-        }
-
-        void OnGameObjectCreate(GameObject* go) override
-        {
-            switch (go->GetEntry())
-            {
-                case REFECTORY_DOOR:
-                    m_uiRefectoryDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[DATA_BLACKHEARTTHEINCITEREVENT] == DONE)
-                        go->SetGoState(GO_STATE_ACTIVE);
-                    break;
-                case SCREAMING_HALL_DOOR:
-                    m_uiScreamingHallDoorGUID = go->GetGUID();
-                    if (m_auiEncounter[DATA_GRANDMASTER_VORPIL_EVENT] == DONE)
-                        go->SetGoState(GO_STATE_ACTIVE);
-                    break;
-            }
         }
 
         void OnCreatureCreate(Creature* creature) override
@@ -95,83 +75,24 @@ public:
             InstanceScript::OnUnitDeath(unit);
 
             if (unit->GetEntry() == NPC_CABAL_RITUALIST)
+            {
                 if (!--_ritualistsAliveCount)
                 {
-                    m_auiEncounter[TYPE_RITUALISTS] = DONE;
-                    SaveToDB();
+                    StorePersistentData(TYPE_RITUALISTS, DONE);
                     if (Creature* cr = instance->GetCreature(m_uiHellmawGUID))
                     {
                         cr->AI()->DoAction(1);
                     }
                 }
-        }
-
-        void SetData(uint32 type, uint32 uiData) override
-        {
-            switch (type)
-            {
-                case DATA_BLACKHEARTTHEINCITEREVENT:
-                    if (uiData == DONE)
-                        DoUseDoorOrButton(m_uiRefectoryDoorGUID);
-                    m_auiEncounter[type] = uiData;
-                    break;
-
-                case DATA_GRANDMASTER_VORPIL_EVENT:
-                    if (uiData == DONE)
-                        DoUseDoorOrButton(m_uiScreamingHallDoorGUID);
-                    m_auiEncounter[type] = uiData;
-                    break;
-
-                case DATA_MURMUREVENT:
-                case TYPE_HELLMAW:
-                    m_auiEncounter[type] = uiData;
-                    break;
             }
-
-            if (uiData == DONE)
-                SaveToDB();
         }
 
         uint32 GetData(uint32 type) const override
         {
             if (type == TYPE_RITUALISTS)
-                return m_auiEncounter[0];
+                return GetPersistentData(TYPE_RITUALISTS);
+
             return 0;
-        }
-
-        std::string GetSaveData() override
-        {
-            std::ostringstream saveStream;
-            saveStream << "S L " << m_auiEncounter[0] << ' ' << m_auiEncounter[1] << ' '
-                       << m_auiEncounter[2] << ' ' << m_auiEncounter[3] << ' ' << m_auiEncounter[4];
-
-            return saveStream.str();
-        }
-
-        void Load(const char* in) override
-        {
-            if (!in)
-            {
-                OUT_LOAD_INST_DATA_FAIL;
-                return;
-            }
-
-            OUT_LOAD_INST_DATA(in);
-
-            char dataHead1, dataHead2;
-            std::istringstream loadStream(in);
-            loadStream >> dataHead1 >> dataHead2;
-            if (dataHead1 == 'S' && dataHead2 == 'L')
-            {
-                for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
-                {
-                    loadStream >> m_auiEncounter[i];
-                    if (m_auiEncounter[i] == IN_PROGRESS)
-                        m_auiEncounter[i] = NOT_STARTED;
-                }
-            }
-
-            OUT_LOAD_INST_DATA_COMPLETE;
         }
     };
 };
