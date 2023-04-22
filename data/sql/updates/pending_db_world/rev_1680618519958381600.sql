0667454dd968dcb369c1e7e23ff35b54bad29e8a
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2023-04-22 07:42:00 -0600

    feat: Allow Float Rep Values (#15818)

---
+++ data/sql/updates/pending_db_world/rev_1680618519958381600.sql
@@ -0,0 +1,3 @@
+--
+ALTER TABLE `creature_onkill_reputation` MODIFY `RewOnKillRepValue1` FLOAT NOT NULL DEFAULT 0;
+ALTER TABLE `creature_onkill_reputation` MODIFY `RewOnKillRepValue2` FLOAT NOT NULL DEFAULT 0;
