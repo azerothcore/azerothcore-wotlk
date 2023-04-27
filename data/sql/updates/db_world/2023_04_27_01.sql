e19d3be755dcf3f060740a04cddab1f2636b31f0
AzerothCoreBot <azerothcorebot@gmail.com>
2023-04-27 23:28:51 +0000

    chore(DB): import pending files
    Referenced commit(s): ab2c062f0374efdf453adb0699ede9b382fdc5dc

--- data/sql/updates/db_world/2023_04_27_01.sql
+++ data/sql/updates/db_world/2023_04_27_01.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_04_27_00 -> 2023_04_27_01
 -- add item_dbc table
 DROP TABLE IF EXISTS `item_dbc`; 
 CREATE TABLE `item_dbc` ( `ID` INT NOT NULL DEFAULT '0', `ClassID` INT NOT NULL DEFAULT '0', `SubclassID` INT NOT NULL DEFAULT '0', `Sound_Override_Subclassid` INT NOT NULL DEFAULT '0', `Material` INT NOT NULL DEFAULT '0', `DisplayInfoID` INT NOT NULL DEFAULT '0', `InventoryType` INT NOT NULL DEFAULT '0', `SheatheType` INT NOT NULL DEFAULT '0', PRIMARY KEY (`ID`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
