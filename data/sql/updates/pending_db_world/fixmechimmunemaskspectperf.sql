9237915f07f6d5403f185359ef76fc578c3ca099
Dan <83884799+elthehablo@users.noreply.github.com>
2023-04-28 22:39:42 +0200

    add taunt immune

--- data/sql/updates/pending_db_world/fixmechimmunemaskspectperf.sql
+++ data/sql/updates/pending_db_world/fixmechimmunemaskspectperf.sql
@@ -1,2 +1,4 @@
 --
-UPDATE `creature_template` SET `mechanic_immune_mask` = 650854267 WHERE `entry` = 16473;
+UPDATE `creature_template`
+SET `mechanic_immune_mask` = 650854267, `flags_extra` = 256
+WHERE `entry` = 16473 AND `name` = 'Spectral Performer';
