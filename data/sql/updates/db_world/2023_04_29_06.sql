2bd7d23b59f5f9aa0d75d433126c208eabaae782
AzerothCoreBot <azerothcorebot@gmail.com>
2023-04-29 23:29:27 +0000

    chore(DB): import pending files
    Referenced commit(s): 1c7ac83a1ca450c45e44263630695330b10397af

--- data/sql/updates/db_world/2023_04_29_06.sql
+++ data/sql/updates/db_world/2023_04_29_06.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_04_29_05 -> 2023_04_29_06
 --
 DELETE FROM `spell_area` WHERE `spell` = 57940 AND `area` IN (4723, 4809, 4813, 4820);
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
