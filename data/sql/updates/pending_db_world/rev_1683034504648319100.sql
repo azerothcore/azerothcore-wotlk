6c66bb04af4f4704fe60eaee281a2162b7ddd461
Gultask <sagemochi@hotmail.com>
2023-05-02 12:07:25 -0300

    Update rev_1683034504648319100.sql

--- data/sql/updates/pending_db_world/rev_1683034504648319100.sql
+++ data/sql/updates/pending_db_world/rev_1683034504648319100.sql
@@ -3,10 +3,10 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (15, 8568, 1, 0, 0, 2, 0, 31880, 1, 0, 1, 0, 0, '', 'Must not have item 31880'),
 (15, 8568, 1, 0, 0, 8, 0, 10942, 0, 0, 0, 0, 0, '', 'Quest 10942 must be rewarded'),
-(15, 8568, 1, 0, 0, 12, 0, 10, 0, 0, 0, 0, 0, '', 'Childrens Week must be active'),
+(15, 8568, 1, 0, 0, 12, 0, 10, 0, 0, 0, 0, 0, '', 'Children\'s Week must be active'),
 (15, 8568, 2, 0, 0, 2, 0, 31881, 1, 0, 1, 0, 0, '', 'Must not have item 31881'),
 (15, 8568, 2, 0, 0, 8, 0, 10943, 0, 0, 0, 0, 0, '', 'Quest 10943 must be rewarded'),
-(15, 8568, 2, 0, 0, 12, 0, 10, 0, 0, 0, 0, 0, '', 'Childrens Week must be active');
+(15, 8568, 2, 0, 0, 12, 0, 10, 0, 0, 0, 0, 0, '', 'Children\'s Week must be active');
 
 DELETE FROM `gossip_menu_option` WHERE (`MenuID` = 8568);
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
