0679cd17bc7637000709e8698c7e62575511693f
pedrohfm9 <122452427+Dr-Arayashiki@users.noreply.github.com>
2023-05-03 14:10:16 -0300

    Update Fix_NPCs_Halls_of_Reflection.sql

--- data/sql/updates/pending_db_world/Fix_NPCs_Halls_of_Reflection.sql
+++ data/sql/updates/pending_db_world/Fix_NPCs_Halls_of_Reflection.sql
@@ -2,7 +2,7 @@
 -- SQL Dark Ranger Loralen emote
 DELETE FROM `creature_template_addon` WHERE (`entry` = 37779);
 INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
-(37779, 0, 0, 0, 2, 0, 0, 0);
+(37779, 0, 0, 0, 2, 0, 0, '');
 -- SQL LK Boss final
 DELETE FROM `creature` WHERE (`id1` = 36954);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
