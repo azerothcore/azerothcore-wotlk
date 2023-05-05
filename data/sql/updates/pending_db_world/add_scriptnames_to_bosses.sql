034d951e176b6a106e09157a84b4ba8d1a4c958c
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-05 10:11:27 +0200

    better formatting + two sql statements together

--- data/sql/updates/pending_db_world/add_scriptnames_to_bosses.sql
+++ data/sql/updates/pending_db_world/add_scriptnames_to_bosses.sql
@@ -1,12 +1,11 @@
 --
 UPDATE `creature_template`
-SET `Scriptname` = 'boss_mennu_the_betrayer'
-WHERE `entry` = 17941 AND `difficulty_entry_1` = 19893;
+SET `Scriptname` =
+    CASE
+    WHEN `entry` = 17941 THEN 'boss_mennu_the_betrayer'
+    WHEN `entry` = 17942 THEN 'boss_quagmirran'
+    WHEN `entry` = 17991 THEN 'boss_rokmar_the_crackler'
+    END, `AIName` = ''
+WHERE `entry` IN (17941, 17942, 17991);
 
-UPDATE `creature_template`
-SET `Scriptname` = 'boss_quagmirran'
-WHERE `entry` = 17942 AND `difficulty_entry_1` = 19894;
-
-UPDATE `creature_template`
-SET `Scriptname` = 'boss_rokmar_the_crackler'
-WHERE `entry` = 17991 AND `difficulty_entry_1` = 19895;
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (17941, 17942, 17991);
