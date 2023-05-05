3ca9f8c106ef69ced2bf8af5826becd2b9b7386c
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-05 12:05:24 +0200

    fixes to header enum

--- src/server/scripts/Outland/CoilfangReservoir/SlavePens/the_slave_pens.h
+++ src/server/scripts/Outland/CoilfangReservoir/SlavePens/the_slave_pens.h
@@ -27,23 +27,23 @@ uint32 const EncounterCount               = 3;
 
 enum SPDataTypes
 {
-    DATA_MENNU_THE_BETRAYER               = 1,
-    DATA_ROKMAR_THE_CRACKLER              = 2,
-    DATA_QUAGMIRRAN                       = 3,
-    DATA_AHUNE                            = 4,
-    MAX_ENCOUNTER                         = 5,
-    DATA_AHUNE_BUNNY                      = 6,
-    DATA_FROZEN_CORE                      = 7,
-    DATA_FLAMECALLER_000                  = 8,
-    DATA_FLAMECALLER_001                  = 9,
-    DATA_FLAMECALLER_002                  = 10,
-    DATA_BONFIRE_BUNNY_000                = 11,
-    DATA_BONFIRE_BUNNY_001                = 12,
-    DATA_BONFIRE_BUNNY_002                = 13,
-    DATA_BEAM_BUNNY_000                   = 14,
-    DATA_BEAM_BUNNY_001                   = 15,
-    DATA_BEAM_BUNNY_002                   = 16,
-    DATA_LUMA_SKYMOTHER                   = 17
+    DATA_MENNU_THE_BETRAYER               = 0,
+    DATA_ROKMAR_THE_CRACKLER              = 1,
+    DATA_QUAGMIRRAN                       = 2,
+    DATA_AHUNE                            = 3,
+    MAX_ENCOUNTER                         = 4,
+    DATA_AHUNE_BUNNY                      = 5,
+    DATA_FROZEN_CORE                      = 6,
+    DATA_FLAMECALLER_000                  = 7,
+    DATA_FLAMECALLER_001                  = 8,
+    DATA_FLAMECALLER_002                  = 9,
+    DATA_BONFIRE_BUNNY_000                = 10,
+    DATA_BONFIRE_BUNNY_001                = 11,
+    DATA_BONFIRE_BUNNY_002                = 12,
+    DATA_BEAM_BUNNY_000                   = 13,
+    DATA_BEAM_BUNNY_001                   = 14,
+    DATA_BEAM_BUNNY_002                   = 15,
+    DATA_LUMA_SKYMOTHER                   = 16
 };
 
 enum SPCreaturesIds
