a22b80e709615e82869c1e4898b43a09bdb1e91c
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-10 20:54:41 +0200

    Update data/sql/updates/pending_db_world/fixmechimmunemaskspectperf.sql
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/fixmechimmunemaskspectperf.sql
+++ data/sql/updates/pending_db_world/fixmechimmunemaskspectperf.sql
@@ -1,4 +1,2 @@
 --
-UPDATE `creature_template`
-SET `mechanic_immune_mask` = 650854267, `flags_extra` = 256
-WHERE `entry` = 16473 AND `name` = 'Spectral Performer';
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|2|16|64|256|512|1024|2048|4096|8192|131072|524288|4194304|8388608|33554432, `flags_extra` = `flags_extra`|256 WHERE `entry` = 16473;
