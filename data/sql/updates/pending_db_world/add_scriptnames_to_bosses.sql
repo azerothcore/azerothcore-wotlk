aead812e02a7312ec19cb901ea87d1a797c49b56
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-11 13:33:41 +0200

    E e

--- data/sql/updates/pending_db_world/add_scriptnames_to_bosses.sql
+++ data/sql/updates/pending_db_world/add_scriptnames_to_bosses.sql
@@ -1,14 +1,14 @@
 --
 UPDATE `creature_template` SET
 `Scriptname` = 'boss_mennu_the_betrayer', `AIName` = '',
-WHERE `Entry` = 17941;
+WHERE `entry` = 17941;
 
 UPDATE `creature_template` SET
 `Scriptname` = 'boss_quagmirran', `AIName` = '',
-WHERE `Entry` = 17942;
+WHERE `entry` = 17942;
 
 UPDATE `creature_template` SET
 `Scriptname` = 'boss_rokmar_the_crackler', `AIName` = '',
-WHERE `Entry` = 17991;
+WHERE `entry` = 17991;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (17941, 17942, 17991) AND `source_type` = 0;
