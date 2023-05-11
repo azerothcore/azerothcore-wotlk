cf5100b5360678e62ef6d4617e3dbab6ea624a41
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-11 13:31:04 +0200

    fix sql
    to have a more universal format

--- data/sql/updates/pending_db_world/add_scriptnames_to_bosses.sql
+++ data/sql/updates/pending_db_world/add_scriptnames_to_bosses.sql
@@ -1,11 +1,14 @@
 --
-UPDATE `creature_template`
-SET `Scriptname` =
-    CASE
-    WHEN `entry` = 17941 THEN 'boss_mennu_the_betrayer'
-    WHEN `entry` = 17942 THEN 'boss_quagmirran'
-    WHEN `entry` = 17991 THEN 'boss_rokmar_the_crackler'
-    END, `AIName` = ''
-WHERE `entry` IN (17941, 17942, 17991);
+UPDATE `creature_template` SET
+`Scriptname` = 'boss_mennu_the_betrayer', `AIName` = '',
+WHERE `Entry` = 17941;
+
+UPDATE `creature_template` SET
+`Scriptname` = 'boss_quagmirran', `AIName` = '',
+WHERE `Entry` = 17942;
+
+UPDATE `creature_template` SET
+`Scriptname` = 'boss_rokmar_the_crackler', `AIName` = '',
+WHERE `Entry` = 17991;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (17941, 17942, 17991) AND `source_type` = 0;
