a0eb5b61bef21043722d84d9a42b2b8d49862ccc
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-11 13:46:06 +0200

    Update tavarok_darkweaver_sai_removal.sql
    fix sql

--- data/sql/updates/pending_db_world/tavarok_darkweaver_sai_removal.sql
+++ data/sql/updates/pending_db_world/tavarok_darkweaver_sai_removal.sql
@@ -1,10 +1,5 @@
 --
-UPDATE `creature_template`
-SET `Scriptname` =
-	CASE
-	WHEN `entry` = 18343 THEN 'boss_tavarok'
-	WHEN `entry` = 18472 THEN 'boss_darkweaver_syth'
-	END, `AIName` = ''
-WHERE `entry` IN (18343, 18472);
+UPDATE `creature_template` SET `ScriptName` = 'boss_tavarok', `AIName` = '' WHERE `entry` = 18343;
+UPDATE `creature_template` SET `ScriptName` = 'boss_darkweaver_syth', `AIName` = '' WHERE `entry` = 18472;
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (18343, 18472, 1847200) AND `source_type` = 0 OR `source_type` = 9;
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (18343, 18472, 1847200) AND (`source_type` = 0 OR `source_type` = 9);
