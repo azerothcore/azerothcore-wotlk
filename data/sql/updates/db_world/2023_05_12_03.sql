f2e01028cf4d3977b4e1c5a821f44097ec032975
AzerothCoreBot <azerothcorebot@gmail.com>
2023-05-12 12:17:20 +0000

    chore(DB): import pending files
    Referenced commit(s): 2a1d706a2f11bf307cfb5ae84afbcd4418393ca2

--- data/sql/updates/db_world/2023_05_12_03.sql
+++ data/sql/updates/db_world/2023_05_12_03.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_05_12_02 -> 2023_05_12_03
 --
 UPDATE `creature_template` SET `AiName`='', `ScriptName`='npc_underbat' WHERE `entry`=17724;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=17724 AND `source_type`=0;
