3937de30f31f63387f780d1f2a8bdf3ecad1f8c1
AzerothCoreBot <azerothcorebot@gmail.com>
2023-05-12 11:26:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 79954c34d43722a292270d6ca90a7962615f73e6

--- data/sql/updates/db_world/2023_05_12_01.sql
+++ data/sql/updates/db_world/2023_05_12_01.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_05_12_00 -> 2023_05_12_01
 --
 UPDATE `creature_template` SET `ScriptName` = 'boss_tavarok', `AIName` = '' WHERE `entry` = 18343;
 UPDATE `creature_template` SET `ScriptName` = 'boss_darkweaver_syth', `AIName` = '' WHERE `entry` = 18472;
