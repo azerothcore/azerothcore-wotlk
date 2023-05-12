4afe8fef2c46aa97279586b8ce2b852ff6dbf3fe
AzerothCoreBot <azerothcorebot@gmail.com>
2023-05-12 11:23:05 +0000

    chore(DB): import pending files
    Referenced commit(s): 11eab9c3a68a3691597e9b1b5dbbf2654f358410

--- data/sql/updates/db_world/2023_05_12_00.sql
+++ data/sql/updates/db_world/2023_05_12_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_05_10_10 -> 2023_05_12_00
 --
 UPDATE `creature_template` SET `ScriptName` = 'boss_mennu_the_betrayer', `AIName` = '' WHERE `entry` = 17941;
 UPDATE `creature_template` SET `ScriptName` = 'boss_quagmirran', `AIName` = '' WHERE `entry` = 17942;
