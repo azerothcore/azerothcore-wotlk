c52117c3f6630a542bd4ae303dd70ef31a29ae75
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-05-13 14:59:24 -0300

    Update src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/instance_shattered_halls.cpp

--- src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/instance_shattered_halls.cpp
+++ src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/instance_shattered_halls.cpp
@@ -24,7 +24,7 @@ ObjectData const creatureData[] =
 {
     { NPC_GRAND_WARLOCK_NETHEKURSE  , DATA_NETHEKURSE     },
     { NPC_WARCHIEF_KARGATH          , DATA_KARGATH        },
-    { 0,                                                  , 0                                }
+    { 0,                                                  0                                }
 };
 
 class instance_shattered_halls : public InstanceMapScript
