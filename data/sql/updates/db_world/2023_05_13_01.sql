02b1e9f6476e0edefb5169c6b69d2e6ff573205e
AzerothCoreBot <azerothcorebot@gmail.com>
2023-05-13 17:05:09 +0000

    chore(DB): import pending files
    Referenced commit(s): c99dd0704943874e2acddf5746ec8cc59bf6a1b2

--- data/sql/updates/db_world/2023_05_13_01.sql
+++ data/sql/updates/db_world/2023_05_13_01.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_05_13_00 -> 2023_05_13_01
 --
 DELETE FROM `creature_equip_template` WHERE `CreatureID` = 16809 AND `ID` IN (1, 2);
 INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES 
