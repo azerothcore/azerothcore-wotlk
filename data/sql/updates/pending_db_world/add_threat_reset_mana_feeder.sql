7c7b44bd9b254a01096c77e02282ac77ec15d2a4
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-19 01:56:42 +0200

    Update add_threat_reset_mana_feeder.sql

--- data/sql/updates/pending_db_world/add_threat_reset_mana_feeder.sql
+++ data/sql/updates/pending_db_world/add_threat_reset_mana_feeder.sql
@@ -2,6 +2,6 @@
 -- improve mechanics for Mana Feeders
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 16491 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(16491, 0, 1, 0, 25, 0, 100, 512, 0, 0, 0, 0, 0, 11, 39804, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Mana Feeder - On Reset - Cast Damage Immunity: Magic'),
+(16491, 0, 0, 0, 25, 0, 100, 512, 0, 0, 0, 0, 0, 11, 39804, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Mana Feeder - On Reset - Cast Damage Immunity: Magic'),
 (16491, 0, 1, 0, 0, 0, 100, 512, 3000, 7000, 3000, 7000, 0, 11, 10095, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Mana Feeder - In Combat - Cast \'Hate to Zero\''),
-(16491, 0, 1, 0, 0, 0, 100, 512, 3000, 7000, 3000, 7000, 0, 11, 29908, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Mana Feeder - In Combat - Cast \'Astral Bite\'');
+(16491, 0, 2, 0, 0, 0, 100, 512, 3000, 7000, 3000, 7000, 0, 11, 29908, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Mana Feeder - In Combat - Cast \'Astral Bite\'');
