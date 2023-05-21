f91e11686ea420c7c687373e6570400dced3add1
KJack <kgithub@kellerg.com>
2023-05-21 05:13:24 -0400

    Resolves issue where patients were dying extremely quickly during the quest.
    - updates other parameters to make the quest more Blizz-like and completable

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -990,7 +990,7 @@ public:
                 Reset();
         }
 
-        void PatientSaved(Creature* /*soldier*/, Player* player, Location* point)
+        void PatientSaved(Creature* saved_patient, Player* player, Location* point)
         {
             if (player && PlayerGUID == player->GetGUID())
             {
@@ -1004,8 +1004,9 @@ public:
                         {
                             for (ObjectGuid const& guid : Patients)
                             {
-                                if (Creature* patient = ObjectAccessor::GetCreature(*me, guid))
-                                    patient->setDeathState(JUST_DIED);
+                                if (guid != saved_patient->GetGUID()) // Don't kill the last guy we just saved
+                                    if (Creature* patient = ObjectAccessor::GetCreature(*me, guid))
+                                        patient->setDeathState(JUST_DIED);
                             }
                         }
 
@@ -1069,6 +1070,9 @@ public:
             //no regen health
             me->SetUnitFlag(UNIT_FLAG_IN_COMBAT);
 
+            //prevent using normal bandages
+            me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_BANDAGE, true);
+
             //to make them lay with face down
             me->SetUInt32Value(UNIT_FIELD_BYTES_1, UNIT_STAND_STATE_DEAD);
 
@@ -1090,6 +1094,14 @@ public:
                     me->SetHealth(me->CountPctFromMaxHealth(25));
                     break;
             }
+
+            // emulate vanilla WoW's 200ms spell batching rate for maximum authenticity!
+            // https://www.wowhead.com/classic/news/spell-batching-in-classic-wow-290910
+            _scheduler.Schedule(200ms, [this](TaskContext context)
+            {
+                me->ModifyHealth(-5.0f);
+                context.Repeat(200ms);
+            });
         }
 
         void JustEngagedWith(Unit* /*who*/) override { }
@@ -1134,13 +1146,12 @@ public:
             }
         }
 
-        void UpdateAI(uint32 /*diff*/) override
+        void UpdateAI(uint32 diff) override
         {
-            //lower HP on every world tick makes it a useful counter, not officlone though
-            if (me->IsAlive() && me->GetHealth() > 6)
-                me->ModifyHealth(-5);
 
-            if (me->IsAlive() && me->GetHealth() <= 6)
+            _scheduler.Update(diff);
+
+            if (me->IsAlive() && me->GetHealth() <= 5)
             {
                 me->RemoveUnitFlag(UNIT_FLAG_IN_COMBAT);
                 me->SetUnitFlag(UNIT_FLAG_NOT_SELECTABLE);
@@ -1152,6 +1163,10 @@ public:
                         CAST_AI(npc_doctor::npc_doctorAI, doctor->AI())->PatientDied(Coord);
             }
         }
+
+        private:
+            TaskScheduler _scheduler;
+
     };
 
     CreatureAI* GetAI(Creature* creature) const override
@@ -1162,7 +1177,7 @@ public:
 
 void npc_doctor::npc_doctorAI::UpdateAI(uint32 diff)
 {
-    if (Event && SummonPatientCount >= 20)
+    if (Event && SummonPatientCount >= 24) // Need to keep the event going long enough to save the last few patients
     {
         Reset();
         return;
@@ -1170,7 +1185,7 @@ void npc_doctor::npc_doctorAI::UpdateAI(uint32 diff)
 
     if (Event)
     {
-        if (SummonPatientTimer <= diff)
+        if (SummonPatientTimer <= diff || SummonPatientCount < 7) // Starts with all beds filled
         {
             if (Coordinates.empty())
                 return;
