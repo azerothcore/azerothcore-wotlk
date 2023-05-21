9ec0886cf50366ba0dfcd13d437d9e2200df161b
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2023-05-21 13:54:47 -0600

    xd

--- data/sql/updates/pending_db_world/rev_1684697962199868700.sql
+++ data/sql/updates/pending_db_world/rev_1684697962199868700.sql
@@ -1,4 +1,4 @@
 --
 DELETE FROM `creature_template_spell` WHERE `CreatureID`=18847 AND `Index`=2 AND `Spell`=33096;
-INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES (18847, 2, 33096, 12340);
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES (18847, 2, 33096, 0);
 
