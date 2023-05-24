9f086ca4dbe294fc9296eebcaf241dac200d0caf
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2023-05-24 23:01:13 +0200

    fix(DB/creature) Add Magnetic Pull to Living Cyclone and remove wrong spell...

---
+++ rev_1684961632752120124.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 17160) AND (`source_type` = 0) AND (`id` IN (1));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(17160, 0, 1, 0, 4, 0, 100, 1, 0, 0, 0, 0, 0, 11, 31705, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Living Cyclone - On Aggro - Cast Magnetic Pull (No Repeat)')
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 19996) AND (`source_type` = 0) AND (`id` IN (1));
