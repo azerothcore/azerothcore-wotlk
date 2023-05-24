bc0ddaa3c2af2e7b0e92986741485d04c1a90a35
AzerothCoreBot <azerothcorebot@gmail.com>
2023-05-24 17:07:20 +0000

    chore(DB): import pending files
    Referenced commit(s): 20a5f021111065107a3b5fc6d57725a35b65f3cb

--- data/sql/updates/db_world/2023_05_24_03.sql
+++ data/sql/updates/db_world/2023_05_24_03.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_05_24_02 -> 2023_05_24_03
 --
 UPDATE `creature_loot_template` SET `GroupId` = 1 WHERE (`Entry` = 17370) AND (`Item` IN (1, 2, 3, 4, 5, 6));
 UPDATE `creature_loot_template` SET `Chance` = 5 WHERE (`Entry` = 17370) AND (`Item` IN (1, 2));
