c659b172620013becfb8eb3e57b92ec5d02f051a
KJack <kgithub@kellerg.com>
2023-05-24 14:43:16 -0400

    ZF: Adds creature_formations for trash in the last boss room.

---
+++ data/sql/updates/pending_db_world/rev_1684953661179621591.sql
@@ -0,0 +1,11 @@
+-- 
+-- Sul'lithuz Abomination creature formation
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES (81527, 81527, 0, 0, 3, 0, 0);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES (81527, 81565, 0, 0, 3, 0, 0);
+
+-- Sandfury Guardian creature formation
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES (37998, 37998, 0, 0, 3, 0, 0);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES (37998, 37999, 0, 0, 3, 0, 0);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES (37998, 38000, 0, 0, 3, 0, 0);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES (37998, 38001, 0, 0, 3, 0, 0);
+
