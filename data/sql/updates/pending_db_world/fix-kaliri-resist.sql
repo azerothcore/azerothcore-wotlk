222e308946b1d5108e5875181ac9ab703017b23b
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-25 17:25:33 +0200

    verifiedbuild
    forgot that

--- data/sql/updates/pending_db_world/fix-kaliri-resist.sql
+++ data/sql/updates/pending_db_world/fix-kaliri-resist.sql
@@ -1,2 +1,3 @@
 --
 UPDATE `creature_template_resistance` SET `Resistance` = 0 WHERE `CreatureID` IN (17034, 17035, 17039, 17053);
+UPDATE `creature_template_resistance` SET `VerifiedBuild` = 0 WHERE `CreatureID` IN (17034, 17035, 17039, 17053);
