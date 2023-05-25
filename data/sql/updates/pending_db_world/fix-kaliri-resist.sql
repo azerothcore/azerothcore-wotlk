6de51cb847aeb466d8134065646374fd71a96fe9
Dan <83884799+elthehablo@users.noreply.github.com>
2023-05-25 17:46:15 +0200

    final

--- data/sql/updates/pending_db_world/fix-kaliri-resist.sql
+++ data/sql/updates/pending_db_world/fix-kaliri-resist.sql
@@ -1,3 +1,3 @@
 --
 UPDATE `creature_template_resistance` SET `Resistance` = 0 WHERE `CreatureID` IN (17034, 17035, 17039, 17053);
-UPDATE `creature_template_resistance` SET `VerifiedBuild` = 0 WHERE `CreatureID` IN (17034, 17035, 17039, 17053);
+UPDATE `creature_template_resistance` SET `VerifiedBuild` = 49705 WHERE `CreatureID` IN (17034, 17035, 17039, 17053);
