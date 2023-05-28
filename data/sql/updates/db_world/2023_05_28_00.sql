802bb1bf321e971788256a18f8e538899fd08806
AzerothCoreBot <azerothcorebot@gmail.com>
2023-05-28 12:21:40 +0000

    chore(DB): import pending files
    Referenced commit(s): 8077c8bfafa7cd118957c759edc0ea2afb7768f1

--- data/sql/updates/db_world/2023_05_28_00.sql
+++ data/sql/updates/db_world/2023_05_28_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_05_24_03 -> 2023_05_28_00
 --
 UPDATE `smart_scripts` SET `event_param2`=2 WHERE `entryorguid`=-151131 AND `source_type`=0 AND `id`=1003 AND `link`=1004;
 UPDATE `smart_scripts` SET `event_param2`=4 WHERE `entryorguid`=-151131 AND `source_type`=0 AND `id`=1005 AND `link`=1006;
