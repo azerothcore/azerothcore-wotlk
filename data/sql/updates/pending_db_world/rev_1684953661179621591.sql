0c16073aab38cf2aeb5a68b2d1b19e6f5bd9410b
KJack <kjack@electricnightowl.com>
2023-05-28 00:06:43 -0400

    Remove unneeded comment line.
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1684953661179621591.sql
+++ data/sql/updates/pending_db_world/rev_1684953661179621591.sql
@@ -1,6 +1,5 @@
--- 
 -- Sul'lithuz Abomination and Sandfury Guardian creature formations
-DELETE from `creature_formations` WHERE `memberGUID` IN (81527, 81565, 37998, 37999, 38000, 38001);
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (81527, 81565, 37998, 37999, 38000, 38001);
 INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
 (81527, 81527, 0, 0, 3, 0, 0),
 (81527, 81565, 0, 0, 3, 0, 0),
@@ -8,4 +7,3 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 (37998, 37999, 0, 0, 3, 0, 0),
 (37998, 38000, 0, 0, 3, 0, 0),
 (37998, 38001, 0, 0, 3, 0, 0);
-
