5297ee35e6125d7ccb9d36cfccef9d454e801367
AiurJordan <jalex01x@gmail.com>
2023-05-28 19:18:01 -0500

    Update Uvuros_Immunities.sql
    Immune to Fear, Root, Silence, Snare, Stun, Freeze, Polymorph, Horror, Interrupt.
    
    Updated to match sql guidelines, renamed pull request.

--- data/sql/updates/pending_db_world/Uvuros_Immunities.sql
+++ data/sql/updates/pending_db_world/Uvuros_Immunities.sql
@@ -1,1 +1,3 @@
-UPDATE `creature_template` SET `mechanic_immune_mask` = 41949520 WHERE (`entry` = 21102);
+--
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|16|64|256|1024|2048|4096|65536|8388608|33554432 WHERE `entry` = 21102;
+
