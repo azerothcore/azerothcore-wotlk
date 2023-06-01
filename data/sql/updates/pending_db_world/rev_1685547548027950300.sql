ee18f55f63fb0926669efc0091eb9cb137114007
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-06-01 22:04:00 +0200

    fix(Scripts/Spells): Convert some Druid spells to SpellScript (#16414)
    Co-authored-by: joschiwald <736792+joschiwald@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1685547548027950300.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (34246, 60779) AND `ScriptName` = 'spell_dru_idol_lifebloom';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(34246, 'spell_dru_idol_lifebloom'),
+(60779, 'spell_dru_idol_lifebloom');
