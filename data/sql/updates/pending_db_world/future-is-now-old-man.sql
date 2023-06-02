78c17d2d4df6d874bb93021b79f04e494a8e7bad
Dan <83884799+elthehablo@users.noreply.github.com>
2023-06-02 21:11:17 +0200

    fix(DB/Creature): Correct Old Man Barlo position (#16405)
    * initial
    
    woo
    
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
    
    * Update future-is-now-old-man.sql
    
    ---------
    
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/future-is-now-old-man.sql
@@ -0,0 +1,2 @@
+-- guid 96761 is id 25580 aka old man barlo
+UPDATE `creature` SET `position_x` = -1460.62, `position_y` = 4992.52, `position_z` = -8.977667, `orientation` = 5.532693862915039062 WHERE `guid` = 96761 AND `id1` = 25580;
