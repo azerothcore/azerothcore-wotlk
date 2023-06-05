5765c06ca3d198797a9b1f52ba72d2da7ea1184e
The GhostRider <xspidey@hotmail.it>
2023-06-05 17:49:05 +0100

    Fix(Core/Spell): Net-o-Matic

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -5043,11 +5043,15 @@ void AuraEffect::HandleAuraDummy(AuraApplication const* aurApp, uint8 mode, bool
                     if (caster && target->CanHaveThreatList())
                         target->AddThreat(caster, 10.0f);
                     break;
-                case 13139:                                     // net-o-matic
+					
+			//	This Script is all wrong, it creates an additional Buff that bugs the characters. there shouldn't be here.
+             //   case 13139:                                     // net-o-matic
                     // root to self part of (root_target->charge->root_self sequence
-                    if (caster)
-                        caster->CastSpell(caster, 13138, true, nullptr, this);
-                    break;
+               //     if (caster)
+                 //       caster->CastSpell(caster, 13138, true, nullptr, this);
+                   // break;
+				   
+				   
                 case 34026:   // kill command
                     {
                         Unit* pet = target->GetGuardianPet();
