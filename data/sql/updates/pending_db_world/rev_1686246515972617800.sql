327135ef057567096cb842482141f5ce369b6b1d
Gultask <100873791+Gultask@users.noreply.github.com>
2023-06-08 22:40:43 -0300

    fix(DB/Creature): Root Mennu the Betrayer totems (#16501)
    Create rev_1686246515972617800.sql

---
+++ data/sql/updates/pending_db_world/rev_1686246515972617800.sql
@@ -0,0 +1,11 @@
+--
+DELETE FROM `creature_template_movement` WHERE (`CreatureId` IN (18176,18177,18178,18179,19897,19898,19899,19900));
+INSERT INTO `creature_template_movement` (`CreatureId`, `Flight`, `Rooted`) VALUES
+(18176, 1, 1),
+(18177, 1, 1),
+(18178, 1, 1),
+(18179, 1, 1),
+(19897, 1, 1),
+(19898, 1, 1),
+(19899, 1, 1),
+(19900, 1, 1);
