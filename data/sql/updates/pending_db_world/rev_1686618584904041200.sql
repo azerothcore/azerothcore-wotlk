9d36418b2fe585ead51ec9501d8089764b2f188b
Gultask <100873791+Gultask@users.noreply.github.com>
2023-06-14 12:15:36 -0300

    Update rev_1686618584904041200.sql

--- data/sql/updates/pending_db_world/rev_1686618584904041200.sql
+++ data/sql/updates/pending_db_world/rev_1686618584904041200.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_loot_template` SET `Chance` = 0 AND `GroupId` = 1 WHERE (`Entry` = 20303);
+UPDATE `creature_loot_template` SET `Chance` = 0, `GroupId` = 1 WHERE (`Entry` = 20303);
