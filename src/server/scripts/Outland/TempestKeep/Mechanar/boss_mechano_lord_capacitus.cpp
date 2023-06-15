6b384ce22846395cb95e967828617a03d6e9ee19
Si1ker <eddyvegahorton@hotmail.com>
2023-06-15 14:17:45 -0600

    .

--- src/server/scripts/Outland/TempestKeep/Mechanar/boss_mechano_lord_capacitus.cpp
+++ src/server/scripts/Outland/TempestKeep/Mechanar/boss_mechano_lord_capacitus.cpp
@@ -131,7 +131,7 @@ struct boss_mechano_lord_capacitus : public BossAI
         Talk(SAY_DEATH);
         if (IsHeroic() && instance)
         {
-            scheduler.Schedule(1min, [this](TaskContext context)
+            scheduler.Schedule(1min, [this](TaskContext /*context*/)
             {
                 instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POSITIVE_CHARGE_STACK);
                 instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_NEGATIVE_CHARGE_STACK);
