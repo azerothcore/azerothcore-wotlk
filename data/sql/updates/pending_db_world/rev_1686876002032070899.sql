75ec810a0d0c5a8f5adaab75fec76211cfa32c4f
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2023-06-16 03:04:12 +0200

    Update rev_1686876002032070899.sql

--- data/sql/updates/pending_db_world/rev_1686876002032070899.sql
+++ data/sql/updates/pending_db_world/rev_1686876002032070899.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `spell_proc_event` WHERE `entry` IN (27997, 33511);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (27997, 33511, 33522, 33510);
 INSERT INTO `spell_proc_event` VALUES
 (27997,0,0,0,0,0,0,0,0,0,0,30000),
 (33511,0,0,0,0,0,0,0,0,0,0,17000),
