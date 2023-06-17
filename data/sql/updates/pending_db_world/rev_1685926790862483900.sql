a36e8a3605add055196017011e6472cc0abe1f5d
The GhostRider <xspidey@hotmail.it>
2023-06-17 15:36:06 +0100

    Fix(DB/Events): Disable LoS for Cluster Launcher (#16476)
    * Fix(DB/GameObjects): Cluster Launcher
    
    * Update rev_1685926790862483900.sql
    
    * Update rev_1685926790862483900.sql
    
    * fix: my mistake
    
    * Update rev_1685926790862483900.sql

---
+++ data/sql/updates/pending_db_world/rev_1685926790862483900.sql
@@ -0,0 +1,3 @@
+-- Fix Disable LOS from the Spell 26522 (Lunar Fortune) --
+DELETE FROM `disables` WHERE `sourceType`=0 AND `entry`=26522;
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES (0, 26522, 64, '', '', 'Disable LOS for Lunar Fortune');
