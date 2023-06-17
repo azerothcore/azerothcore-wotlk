a96cf8dee45bc40b1bdee3e11433a04ee50f941d
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2023-06-17 16:31:26 +0200

    fix(DB/Spell): Add ICD to Spellsurge and some PVP trinkets (#16551)
    * fix(DB/spell_proc_event) Add ICD to Spellsurge and Mark of Defiance
    
    * ..
    
    * Update rev_1686876002032070899.sql
    
    * Update rev_1686876002032070899.sql
    
    * Update rev_1686876002032070899.sql
    
    * Update rev_1686876002032070899.sql
    
    * Update rev_1686876002032070899.sql

---
+++ data/sql/updates/pending_db_world/rev_1686876002032070899.sql
@@ -0,0 +1,8 @@
+--
+DELETE FROM `spell_proc_event` WHERE `entry` IN (27997, 33511, 33522, 33510, 24256);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(27997,0,0,0,0,0,0,0,0,0,0,50000),
+(33511,0,0,0,0,0,0,0,0,0,0,17000),
+(33522,0,0,0,0,0,0,0,0,0,0,25000),
+(33510,0,0,0,0,0,0,0,0,0,0,25000),
+(24256,0,0,0,0,0,0,0,0,0,0,240000);
