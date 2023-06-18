6c366c760884a09ce88932f6ab275584ddfb25d5
Gultask <100873791+Gultask@users.noreply.github.com>
2023-06-18 11:38:19 -0300

    fix(DB/Creature): Lower TBC Heroic bosses to level 72 (#16522)
    Create rev_1686617579364661600.sql

---
+++ data/sql/updates/pending_db_world/rev_1686617579364661600.sql
@@ -0,0 +1,11 @@
+--
+UPDATE `creature_template` SET `minlevel` = 72, `maxlevel` = 72 WHERE  `entry` IN (
+20738, -- Chrono Lord Deja (1)
+20745, -- Temporus (1)
+21558, -- High Botanist Freywinn (1)
+21559, -- Laj (1)
+21581, -- Thorngrin the Tender (1)
+21582, -- Warp Splinter (1)
+21712, -- Infinite Chrono-Lord (1)
+22167  -- Infinite Timereaver (1)
+);
