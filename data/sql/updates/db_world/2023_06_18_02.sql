58c6a515d743a016f01d3c77916f87d2bb770f6e
AzerothCoreBot <azerothcorebot@gmail.com>
2023-06-18 14:56:34 +0000

    chore(DB): import pending files
    Referenced commit(s): 7ae4b85c43d7cc5909e3778f5f76833c960250e7

--- data/sql/updates/db_world/2023_06_18_02.sql
+++ data/sql/updates/db_world/2023_06_18_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_06_18_01 -> 2023_06_18_02
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gargolmar_retalliation' AND `spell_id` = 22857;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
