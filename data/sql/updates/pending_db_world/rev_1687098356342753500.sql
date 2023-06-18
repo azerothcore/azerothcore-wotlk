9a94c8003f63c19313d1924420cffd5cda4dc87c
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-06-18 11:27:26 -0300

    fix(Scripts/HellfireRamparts): Fix Gargolmar Retalliation procs with targets behind him

---
+++ data/sql/updates/pending_db_world/rev_1687098356342753500.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gargolmar_retalliation' AND `spell_id` = 22857;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(22857, 'spell_gargolmar_retalliation');
