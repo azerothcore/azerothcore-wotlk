1dbf794d3da3c72692966f7c96541304ba6f9edc
Walter Pagani <paganiwalter@gmail.com>
2023-06-23 21:54:35 -0300

    Fix(DB/Quest): Inoculation - ID: 9303 (#16599)

---
+++ data/sql/updates/pending_db_world/rev_1687383780751220300.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `smart_scripts` SET `action_param2`=0 WHERE `entryorguid`=1651800 AND `source_type`=9 AND `id` IN (2, 6);
+
+UPDATE `creature_template` SET `faction`=7 WHERE `entry`=16534;
