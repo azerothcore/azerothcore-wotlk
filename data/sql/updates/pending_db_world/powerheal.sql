5c5e5596853be8f95bc0b506697fb09e3848c50b
Gultask <100873791+Gultask@users.noreply.github.com>
2023-06-24 17:08:56 -0300

    fix(DB/Trainer): Add missing Powerheal 4000 Lens to Engineering trainers (#16614)
    Create powerheal.sql

---
+++ data/sql/updates/pending_db_world/powerheal.sql
@@ -0,0 +1,3 @@
+DELETE FROM `npc_trainer` WHERE `SpellID` = 41321;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
+(201013, 41321, 50000, 202, 350, 0, 0);
