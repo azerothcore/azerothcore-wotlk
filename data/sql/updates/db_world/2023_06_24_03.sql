53fba47a4fbc421d41cd491c6b1eb3fdf79139cf
AzerothCoreBot <azerothcorebot@gmail.com>
2023-06-24 18:49:55 +0000

    chore(DB): import pending files
    Referenced commit(s): facbebdf8d55ef1b1c9a730a9f474c52912feb60

--- data/sql/updates/db_world/2023_06_24_03.sql
+++ data/sql/updates/db_world/2023_06_24_03.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_06_24_02 -> 2023_06_24_03
 --
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_capacitus_polarity_charge_aura';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
