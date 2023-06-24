facbebdf8d55ef1b1c9a730a9f474c52912feb60
Angelo Venturini <nefertum.dev@protonmail.com>
2023-06-24 15:47:37 -0300

    fix(Scripts/Mechanar): Fix Polarity shift mechanic (#16601)

---
+++ data/sql/updates/pending_db_world/rev_1687394707485668400.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_capacitus_polarity_charge_aura';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(39088, 'spell_capacitus_polarity_charge_aura'),
+(39091, 'spell_capacitus_polarity_charge_aura');
