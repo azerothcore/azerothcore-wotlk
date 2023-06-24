bf8ba6898f0463c7c549e3c92668ea2fa5f7fd0a
Gultask <100873791+Gultask@users.noreply.github.com>
2023-06-24 08:23:04 -0300

    fix(DB/Creature): Avoid Chain-pulling in Auchenai Crypts (#16611)
    Create rev_1687568644157138400.sql

---
+++ data/sql/updates/pending_db_world/rev_1687568644157138400.sql
@@ -0,0 +1,2 @@
+-- Add extra flag 33554432 to several skeleton creatures in Auchenai Crypts to avoid chain pulling
+UPDATE `creature_template` SET `flags_extra` = `flags_extra`|33554432 WHERE (`entry` IN (18700, 20317, 18521, 20315, 18524, 20298));
