7036d979c30169bb6e840a2a1e0b57e543f93356
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-06-27 19:58:36 +0200

    Update data/sql/updates/pending_db_world/rev_1681797511233116200.sql

--- data/sql/updates/pending_db_world/rev_1681797511233116200.sql
+++ data/sql/updates/pending_db_world/rev_1681797511233116200.sql
@@ -1,3 +1,3 @@
 -- 31994 - Shoulder Charge
 DELETE FROM `spell_custom_attr` WHERE `spell_id` = 31994;
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (31994 ,16384);
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (31994, 16384);
