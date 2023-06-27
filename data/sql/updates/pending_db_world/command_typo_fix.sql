e4036513fdae087680e6d54d927a1ce50185e415
David K <dave@paddez.com>
2023-06-27 18:54:01 +0100

    Fix SQL Codestyle in `command_typo_fix.sql`
    This change changes the format of the SQL query to be easier to read.
    
    Additionally uses a variable to improve the readability of the actual UPDATE query, due the length of the help text string.

--- data/sql/updates/pending_db_world/command_typo_fix.sql
+++ data/sql/updates/pending_db_world/command_typo_fix.sql
@@ -1,5 +1,3 @@
 
-UPDATE command 
-SET help = 'Syntax: .wp show $option\nOptions:\non $pathid (or selected creature with loaded path) - Show path\noff - Hide path\ninfo $selected_waypoint - Show info for selected waypoint.'
-WHERE 
-  name = 'wp show';
+SET @HELP_TEXT := 'Syntax: .wp show $option\nOptions:\non $pathid (or selected creature with loaded path) - Show path\noff - Hide path\ninfo $selected_waypoint - Show info for selected waypoint.';
+UPDATE `command` SET `help` = @HELP_TEXT WHERE `name` = 'wp show';
