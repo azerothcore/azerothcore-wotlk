6d3ece8ce4fd39d90795c7db4a8c27bae4ba89cd
PivanDepolo <64589699+PivanDepolo@users.noreply.github.com>
2023-06-29 17:58:23 +0300

    fix(DB/quest_offer_reward_locale): Add ruRU localization to "An Aggressive Defense" quest (#14205)

---
+++ data/sql/updates/pending_db_world/tryrenamefile.sql
@@ -0,0 +1,3 @@
+DELETE FROM `quest_offer_reward_locale` WHERE `ID`=1045 AND `locale`='ruRU';
+INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES
+(1045, 'ruRU', 'Славная работа, $N. Спасибо тебе.', 18019);
