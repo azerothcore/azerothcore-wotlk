b78e82c5b3360989c430dfd0185acc3b136c2b19
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2023-06-30 02:57:24 +0200

    Updates

--- data/sql/updates/pending_db_world/rev_1688085865524921702.sql
+++ data/sql/updates/pending_db_world/rev_1688085865524921702.sql
@@ -1,3 +1,6 @@
 --
 -- Change event UPDATE_IC to VICTIM_CASTING
-UPDATE `smart_scripts` SET `event_type` = 13 WHERE `entryorguid` = 18639 AND `id` = 1;
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 18639 AND `id`=1);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(18639, 0, 1, 0, 13, 0, 100, 0, 16300, 16900, 0, 0, 0, 11, 32691, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Cabal Spellbinder - In Combat - Cast \'Spell Shock\'')
+
