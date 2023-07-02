a5f9907544fc66acdbd75fd48a26049c3cb8bce0
Alan <sr.alansantos@hotmail.com>
2023-07-02 18:11:48 -0300

    update

--- data/sql/updates/pending_db_world/rev_1687180111436507500.sql
+++ data/sql/updates/pending_db_world/rev_1687180111436507500.sql
@@ -7,3 +7,7 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 21735) AND (`source_type` = 0) AND (`id` IN (3));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (21735, 0, 3, 0, 7, 0, 100, 0, 0, 0, 0, 0, 0, 41, 300000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Infernal Oversoul - On Evade - Despawn In 300000 ms');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_q10602_q10585_the_summoning_chamber';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(37285, 'spell_q10602_q10585_the_summoning_chamber');
