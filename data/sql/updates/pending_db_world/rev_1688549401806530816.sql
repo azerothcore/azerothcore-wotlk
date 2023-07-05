d3a4a344764cc1a08d293af34dadfed867eac959
LJFP <ljfp@ljfp.xyz>
2023-07-05 10:40:58 +0000

    Removed reference to old table.

--- data/sql/updates/pending_db_world/rev_1688549401806530816.sql
+++ data/sql/updates/pending_db_world/rev_1688549401806530816.sql
@@ -1,4 +1,2 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1688549401806530816');
-
 DELETE FROM `game_event_creature` WHERE `guid` IN (245648,245645,245649); 
 DELETE FROM `creature` WHERE `guid` IN (245648,245645,245649);
