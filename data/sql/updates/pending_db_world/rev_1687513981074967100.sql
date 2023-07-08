bbe469e55340f2d290ed08148b6584e3f733336a
fangshun <fangshun2004@msn.com>
2023-07-09 07:44:40 +0800

    quest_offer_reward emote ported to SmartAI

--- data/sql/updates/pending_db_world/rev_1687513981074967100.sql
+++ data/sql/updates/pending_db_world/rev_1687513981074967100.sql
@@ -35,9 +35,11 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 
 -- SmatAI Start-------------------------------------
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 5693;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 5693);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(5693, 0, 0, 0, 19, 0, 100, 0, 1476, 0, 0, 0, 0, 80, 569300, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Giving Hearts of the Pure');
+(5693, 0, 0, 0, 19, 0, 100, 0, 1476, 0, 0, 0, 0, 80, 569300, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Godrick Farsan - On Quest \'Hearts of the Pure\' Taken - Run Script'),
+(5693, 0, 1, 0, 20, 0, 100, 0, 1472, 0, 0, 0, 0, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Godrick Farsan - reward quest emote');
 
 -- Timed events
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 569300);
