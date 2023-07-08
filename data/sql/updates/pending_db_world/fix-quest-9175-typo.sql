49553941ac010638b8d5979fee7a92bec7b6ca26
wowquester <138876396+wowquester@users.noreply.github.com>
2023-07-09 01:00:05 +0200

    chore(DB/Quest): Fix typo in quest reward offer for ID 9175 (#16695)
    * fix(DB/quest): 9175 - Fixing typo
    
    * Update data/sql/updates/pending_db_world/fix-quest-9175-typo.sql

---
+++ data/sql/updates/pending_db_world/fix-quest-9175-typo.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_offer_reward` SET `RewardText` = 'You say that you found it on one of the Scourge at Windrunner Spire and that there\'s an inscription on it? Let me see!$B$BHere, take this coin for a job well done.' WHERE (`ID` = 9175);
