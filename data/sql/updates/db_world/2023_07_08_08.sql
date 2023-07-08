486769dbb5adf96aadeab1c87111b6d678e43ffd
AzerothCoreBot <azerothcorebot@gmail.com>
2023-07-08 23:09:54 +0000

    chore(DB): import pending files
    Referenced commit(s): c4d94c80f38a162a783dda62708afaad71a820d6

--- data/sql/updates/db_world/2023_07_08_08.sql
+++ data/sql/updates/db_world/2023_07_08_08.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_07_08_07 -> 2023_07_08_08
 --
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 4126 AND `source_type` = 0 AND `id` = 0;
 DELETE FROM `creature_template_addon` WHERE `entry` = 4126;
