998bb9d54bc84cae77ba05f02ac71931d0b7a51b
avarishd <46330494+avarishd@users.noreply.github.com>
2023-07-08 16:03:03 +0300

    fix(Core/Spells): Shriveling Gaze (#15939)
    * fix(Core/Spells): Shriveling Gaze
    
    * comment out unused parameter
    
    * again

---
+++ data/sql/updates/pending_db_world/rev_1681398806458012400.sql
@@ -0,0 +1,4 @@
+-- Shriveling Gaze
+DELETE FROM `spell_script_names` WHERE `spell_id` = 37589;
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(37589,'spell_gen_shriveling_gaze');
