d57be890f32348f156dbefc429a3d64ac68e84ed
pvp100 <134824156+pvp100com@users.noreply.github.com>
2023-07-08 22:51:34 +0800

    Update rev_1688557801143854400.sql
    following the AC sql standard

--- data/sql/updates/pending_db_world/rev_1688557801143854400.sql
+++ data/sql/updates/pending_db_world/rev_1688557801143854400.sql
@@ -1,6 +1,7 @@
 -- Vile Like Fire! (13071)
 -- fix Njorndar Proto-Drake vehicle unable to fly
-INSERT INTO `acore_world`.`creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES (30564, 6, 57403, 12340);
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 30564;
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES (30564, 6, 57403, 12340);
 
 -- fix Njorndar Proto-Drake display status on the ground
-UPDATE `acore_world`.`creature_template_movement` SET `Flight`=2 WHERE  `CreatureId`=30272;
+UPDATE `creature_template_movement` SET `Flight`=2 WHERE  `CreatureId`=30272;
