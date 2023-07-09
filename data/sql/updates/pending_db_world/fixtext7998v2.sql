f12f3c908d68f1bc47bf6a6292329af5ff04e756
Gultask <100873791+Gultask@users.noreply.github.com>
2023-07-09 13:45:01 -0300

    Update data/sql/updates/pending_db_world/fixtext7998v2.sql

--- data/sql/updates/pending_db_world/fixtext7998v2.sql
+++ data/sql/updates/pending_db_world/fixtext7998v2.sql
@@ -1,1 +1,2 @@
 UPDATE `creature_text` SET `Text` = 'FIRE IN THE HOLE!', `Type` = 14, `BroadcastTextId` = 32326 WHERE (`CreatureID` = 7998) AND (`GroupID` = 19) AND (`ID` = 0);
+UPDATE `creature_text` SET `BroadcastTextId` = 4446 WHERE `CreatureID` = 7998 AND `GroupID` = 20 AND `ID` = 0;
