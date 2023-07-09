f38391a26d5c01de00a9e656c9a94fe7f8139eda
Gultask <100873791+Gultask@users.noreply.github.com>
2023-07-09 16:19:22 -0300

    Update rev_1688928918454010300.sql

--- data/sql/updates/pending_db_world/rev_1688928918454010300.sql
+++ data/sql/updates/pending_db_world/rev_1688928918454010300.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `creature_formations` WHERE `memberGUID` IN (90978, 90979, 90980, 90981, 90982) AND `leaderGUID` = 90978;
+DELETE FROM `creature_formations` WHERE `memberGUID` IN (90978, 90979, 90980, 90981, 90982);
 INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `groupAI`) VALUES
 (90978, 90978, 3),
 (90979, 90978, 3),
