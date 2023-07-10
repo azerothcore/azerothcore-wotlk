9c6b37319506c61fc6d78ebecc19c277cedbf8e6
wowquester <138876396+wowquester@users.noreply.github.com>
2023-07-10 18:10:07 +0200

    Add files via upload

---
+++ data/sql/updates/pending_db_world/rev_1689005068014101100.sql
@@ -0,0 +1,4 @@
+INSERT INTO `version_db_auth` (`sql_rev`) VALUES ('1689005171898848000');
+DELETE FROM `gameobject` WHERE (`id` = 181637) AND (`guid` IN (22070));
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES
+(22070, 181637, 530, 3483, 3551, 1, 1, -521.589, 4872.55, 34.194775, -2.09439, 0, 0, 0.866025, -0.5, 181, 100, 1, '', 0);
