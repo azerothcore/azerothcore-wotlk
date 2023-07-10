392c7d059940a16f896873800ed95ee2a3301175
Nefertumm <nefertum.dev@protonmail.com>
2023-07-10 17:26:38 -0300

    missing sql

---
+++ data/sql/updates/pending_db_world/rev_1689020767712933600.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 30658 AND `spell_effect` = 30571;
+DELETE FROM `spell_script_names` WHERE `ScriptName` = ('spell_magtheridon_quake');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(30658, 'spell_magtheridon_quake');
