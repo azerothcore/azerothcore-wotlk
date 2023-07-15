d881b375f01d4676d81ed443a17c4823e67aa67a
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-15 07:44:05 +0200

    fix(DB/spellprocevent): Add Blade of Wizardry ICD

---
+++ data/sql/updates/pending_db_world/bladeofwizardry.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_proc_event` WHERE `entry` IN (38319);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(38319,0,0,0,0,0,0,0,0,0,0,50000);
