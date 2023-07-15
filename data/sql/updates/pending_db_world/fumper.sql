d71d35717d749e21f618847dcf9aabe0780d237c
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-15 18:08:14 +0200

    Update data/sql/updates/pending_db_world/fumper.sql
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/fumper.sql
+++ data/sql/updates/pending_db_world/fumper.sql
@@ -1,4 +1,2 @@
 --
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (22039, 22482); 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (22038, 22482) AND (`source_type` = 0) AND (`id` IN (12));
-
