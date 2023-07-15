ba948c5c35d15970f5dba2b70ca053166786fa4a
AzerothCoreBot <azerothcorebot@gmail.com>
2023-07-15 17:25:41 +0000

    chore(DB): import pending files
    Referenced commit(s): 8c0742568d034ef330a8f1577aad967bb0681e6a

--- data/sql/updates/db_world/2023_07_15_02.sql
+++ data/sql/updates/db_world/2023_07_15_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_07_15_01 -> 2023_07_15_02
 --
 DELETE FROM `spell_proc_event` WHERE `entry` IN (38319);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
