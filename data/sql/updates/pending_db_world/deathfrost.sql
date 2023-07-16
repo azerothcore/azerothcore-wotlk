55af2a46a2899d699f760881d14ada8c045a7948
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-16 10:12:28 +0200

    Update deathfrost.sql

--- data/sql/updates/pending_db_world/deathfrost.sql
+++ data/sql/updates/pending_db_world/deathfrost.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `spell_proc_event` WHERE `entry` IN (46579);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (46662);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(46579,0,0,0,0,0,0,0,0,0,0,25000);
+(46662,0,0,0,0,0,0,0,0,0,0,25000);
