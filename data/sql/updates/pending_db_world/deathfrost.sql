e5b28a07a70cd5b8b49354aaae4f543cee72f79c
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-17 00:41:40 +0200

    fix(DB/Spells): Add Deathfrost ICD (#16768)
    * fix(DB/Spell_proc_event): Add Deathfrost ICD
    
    * Update deathfrost.sql

---
+++ data/sql/updates/pending_db_world/deathfrost.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_proc_event` WHERE `entry` IN (46662);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(46662,0,0,0,0,0,0,0,0,0,0,25000);
