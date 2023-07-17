bfd65b874012661ded2b4fc9540c5dd93e8e07e5
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-17 20:20:45 +0200

    hand of justice icd

--- data/sql/updates/pending_db_world/trinkets.sql
+++ data/sql/updates/pending_db_world/trinkets.sql
@@ -2,5 +2,6 @@
 update `spell_proc_event` set `Cooldown` = '50000' where `entry` = 37655;
 update `spell_proc_event` set `Cooldown` = '40000' where `entry` = 37247;
 update `spell_proc_event` set `Cooldown` = '50000' where `entry` = 60066;
+update `spell_proc_event` set `Cooldown` = '2000' where `entry` = 15600;
 
 update `spell_proc_event` set `procEx` = '262144' where `entry` = 37655;
