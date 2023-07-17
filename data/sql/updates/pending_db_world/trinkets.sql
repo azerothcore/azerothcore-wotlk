54f6960f08fef08b60baccad7427a1f921c158f8
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-17 21:06:06 +0200

    eye of gruul

--- data/sql/updates/pending_db_world/trinkets.sql
+++ data/sql/updates/pending_db_world/trinkets.sql
@@ -5,3 +5,4 @@ update `spell_proc_event` set `Cooldown` = '50000' where `entry` = 60066;
 update `spell_proc_event` set `Cooldown` = '2000' where `entry` = 15600;
 
 update `spell_proc_event` set `procEx` = '262144' where `entry` = 37655;
+UPDATE `item_template` SET `spellcooldown_2` = -1 WHERE (`entry` = 28823);
