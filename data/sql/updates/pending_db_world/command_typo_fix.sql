7ea62f1b7c32fe9b96deb8aa51d50bc6da4816ea
David K <dave@paddez.com>
2023-07-18 17:52:31 +0100

    fix(DB/Commands): Fix typo in `wp show` command description. (#16591)
    * This adds a SQL `UPDATE` command which fixes a typo present in the `wp show` command help text.
    
    `s/$slected__waypoint/$selected_waypoint`
    
    ---
    
    Previously:
    
    `Syntax: .wp show $option
    Options:
    on $pathid (or selected creature with loaded path) - Show path
    off - Hide path
    info $selected_waypoint - Show info for selected waypoint.`
    ``
    
    * Fix SQL Codestyle in `command_typo_fix.sql`
    
    This change changes the format of the SQL query to be easier to read.
    
    Additionally uses a variable to improve the readability of the actual UPDATE query, due the length of the help text string.

---
+++ data/sql/updates/pending_db_world/command_typo_fix.sql
@@ -0,0 +1,3 @@
+
+SET @HELP_TEXT := 'Syntax: .wp show $option\nOptions:\non $pathid (or selected creature with loaded path) - Show path\noff - Hide path\ninfo $selected_waypoint - Show info for selected waypoint.';
+UPDATE `command` SET `help` = @HELP_TEXT WHERE `name` = 'wp show';
