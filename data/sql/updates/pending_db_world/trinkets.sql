1e78f6c92775db11723278b3f8e42e193fad5f08
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-19 05:00:58 +0200

    Update trinkets.sql

--- data/sql/updates/pending_db_world/trinkets.sql
+++ data/sql/updates/pending_db_world/trinkets.sql
@@ -1,8 +1,8 @@
 --
-update `spell_proc_event` set `Cooldown` = '50000' where `entry` = 37655;
-update `spell_proc_event` set `Cooldown` = '40000' where `entry` = 37247;
-update `spell_proc_event` set `Cooldown` = '50000' where `entry` = 60066;
-update `spell_proc_event` set `Cooldown` = '2000' where `entry` = 15600;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 37655;
+UPDATE `spell_proc_event` SET `Cooldown` = 40000 WHERE `entry` = 37247;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 60066;
+UPDATE `spell_proc_event` SET `Cooldown` = 2000  WHERE `entry` = 15600;
 
-update `spell_proc_event` set `procEx` = '262144' where `entry` = 37655;
+UPDATE `spell_proc_event` SET `procEx` = 262144 WHERE `entry` = 37655;
 UPDATE `item_template` SET `spellcooldown_2` = -1 WHERE (`entry` = 28823);
