0020ac56534bbb69693d6fe467ff5e7c3ad3d053
Samsequel <20357406+Samsequel@users.noreply.github.com>
2023-07-19 05:02:10 +0200

    Update wotlktrinkets.sql

--- data/sql/updates/pending_db_world/wotlktrinkets.sql
+++ data/sql/updates/pending_db_world/wotlktrinkets.sql
@@ -19,25 +19,24 @@ UPDATE `item_template` SET `spellppmRate_1` = 0 WHERE (`entry` = 50348);
 UPDATE `item_template` SET `spellppmRate_1` = 0 WHERE (`entry` = 50353);
 UPDATE `item_template` SET `spellppmRate_1` = 0 WHERE (`entry` = 54588);
 
-UPDATE `spell_proc_event` SET `CustomChance` = '35' WHERE `entry` = 71562;
-UPDATE `spell_proc_event` SET `CustomChance` = '45' WHERE `entry` = 71545;  
+UPDATE `spell_proc_event` SET `CustomChance` = 45 WHERE `entry` = 71545;  
+UPDATE `spell_proc_event` SET `CustomChance` = 35 WHERE `entry` = 71562;
 
-
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 71585;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 64738;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 64792;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 62114;
-UPDATE `spell_proc_event` SET `Cooldown` = '45000' WHERE `entry` = 58901;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 33648;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 49622;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 65013;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 65002;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 62115;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 67672;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 33648;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 60221;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 60519;
-UPDATE `spell_proc_event` SET `Cooldown` = '50000' WHERE `entry` = 63251;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 71585;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 64738;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 64792;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 62114;
+UPDATE `spell_proc_event` SET `Cooldown` = 45000 WHERE `entry` = 58901;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 33648;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 49622;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 65013;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 65002;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 62115;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 67672;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 33648;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 60221;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 60519;
+UPDATE `spell_proc_event` SET `Cooldown` = 50000 WHERE `entry` = 63251;
 
 DELETE FROM `spell_proc_event` WHERE `entry` IN (75474, 71602, 64764);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
