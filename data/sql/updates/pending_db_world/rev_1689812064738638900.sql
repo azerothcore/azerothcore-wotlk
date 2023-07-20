d435b0a669e8e9cc07dbddc31df95f7c63c1a5bb
Grimgravy <sr.alansantos@hotmail.com>
2023-07-19 21:38:24 -0300

    Update rev_1689812064738638900.sql

--- data/sql/updates/pending_db_world/rev_1689812064738638900.sql
+++ data/sql/updates/pending_db_world/rev_1689812064738638900.sql
@@ -1,4 +1,4 @@
 --
 UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 12298;
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 12298) AND (`source_type` = 0)
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 12298) AND (`source_type` = 0);
