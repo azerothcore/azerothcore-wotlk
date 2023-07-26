e6aeef2a12e645dc4c962b8138eb541ab03c9720
Dan Costinas <19265585+Dantsz@users.noreply.github.com>
2023-07-26 14:45:13 +0300

    fix(DB/BlackrockSpire): Add teleport trigger for crater (#16718)

---
+++ data/sql/updates/pending_db_world/add_teleport_to_brs_are_trigger.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `areatrigger_teleport` WHERE `ID` = 2068;
+INSERT INTO `areatrigger_teleport` (`ID`, `Name`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES (2068, 'Blackrock Spire - Jump Exit', 0, -7558.39, -1309.43, 248.454, 1.5708);
