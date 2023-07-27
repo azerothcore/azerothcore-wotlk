98ed01b9fe8965e62840cb1e820cf4d122937cd3
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2023-07-27 15:34:25 +0200

    fix(DB/Spell): Add 20% buff to Spotlight (#16819)

---
+++ data/sql/updates/pending_db_world/rev_1690342063161377379.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 29683 AND `spell_effect` = 32214;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(29683, 32214, 0, "Spotlight 20% player buff");
