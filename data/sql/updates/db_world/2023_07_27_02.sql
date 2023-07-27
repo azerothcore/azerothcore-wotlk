5085dea25e9ab248615f4dcecbe410e3d2df91a1
AzerothCoreBot <azerothcorebot@gmail.com>
2023-07-27 13:37:06 +0000

    chore(DB): import pending files
    Referenced commit(s): 98ed01b9fe8965e62840cb1e820cf4d122937cd3

--- data/sql/updates/db_world/2023_07_27_02.sql
+++ data/sql/updates/db_world/2023_07_27_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_07_27_01 -> 2023_07_27_02
 --
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 29683 AND `spell_effect` = 32214;
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
