e8d321692ef14c95e8c8604db063ad987a9df947
Valdifer <raziel1990@gmail.com>
2023-07-29 11:12:38 +0200

    Merge all SQL

--- data/sql/updates/pending_db_world/rev_1681668801770492200.sql
+++
@@ -1,5 +0,0 @@
---
-SET @ENTRY := 10074;
-DELETE FROM `acore_string` WHERE `entry` = @ENTRY;
-INSERT INTO `acore_string` (`entry`, `content_default`, `locale_esES`, `locale_esMX`) VALUES
-(@ENTRY, 'Halaa is defenseless!', '¡Halaa está indefenso!', '¡Halaa está indefenso!');
