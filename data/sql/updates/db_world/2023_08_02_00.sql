e5fe783fb956132502cdc35daea4f2aefd0a3d91
AzerothCoreBot <azerothcorebot@gmail.com>
2023-08-02 18:01:04 +0000

    chore(DB): import pending files
    Referenced commit(s): 69ce84d4bcdc25e6debb06c3d30a1beac6af3e25

--- data/sql/updates/db_world/2023_08_02_00.sql
+++ data/sql/updates/db_world/2023_08_02_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_08_01_00 -> 2023_08_02_00
 --
 -- mechanical immunities changes
 UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|2|16|64|256|512|1024|2048|4096|8192|131072|524288|4194304|8388608|33554432, `flags_extra` = `flags_extra`|256 WHERE `entry` = 16414;
