ce1f1a2d967c8957966b8094d5b2ec4250bc4c75
podmepodme <petergrocky23@gmail.com>
2023-08-02 16:35:22 +0200

    chore(bash/docker): Change deno dependency script
    Before this commit, ./acore.sh docker build failed on ARM MacBook
    Issue ./acore.sh docker client-data failed on macbook m1 #10851

--- apps/bash_shared/deno.sh
+++ apps/bash_shared/deno.sh
@@ -9,7 +9,7 @@ function denoInstall() {
         echo "Installing Deno..."
         # just one line of command that works on all OSes
         # (temporary cd into AC_PATH_DEPS)
-        curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL="$AC_PATH_DEPS/deno" sh
+        curl -fsSL https://gist.githubusercontent.com/LukeChannings/09d53f5c364391042186518c8598b85e/raw/ac8cd8c675b985edd4b3e16df63ffef14d1f0e24/deno_install.sh | DENO_INSTALL="$AC_PATH_DEPS/deno" sh
     }
 }
 
