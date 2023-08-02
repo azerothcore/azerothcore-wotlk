01b464c36e62e7c9d3f1e08edde4f03824f38578
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-08-02 15:26:50 -0300

    Update rev_1690640715748711400.sql

--- data/sql/updates/pending_db_world/rev_1690640715748711400.sql
+++ data/sql/updates/pending_db_world/rev_1690640715748711400.sql
@@ -1,7 +1,5 @@
 -- Add acore_strings for arena queue announcer with less detail about the teams
-
 DELETE FROM `acore_string` WHERE `entry` IN (773, 774, 775, 776, 777, 778);
-
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_frFR`, `locale_deDE`, `locale_zhCN`) VALUES
 (773, '|cffff0000[Arena Queue Announcer]:|r %s -- Joined : %ux%u|r', '|cffff0000[Annonce File d''Attente Arène]:|r %s -- Rejoint : %ux%u|r', '|cffff0000[BG Ansager für Warteschlange]:|r %s -- beigetreten : %ux%u|r', '|cffff0000[竞技场列队公告]:|r %s -- 加入 : %ux%u|r'),
 (774, '|cffff0000[Arena Queue Announcer]:|r %s -- Exited : %ux%u|r', '|cffff0000[Annonce File d''Attente Arène]:|r %s -- Quitté : %ux%u|r', '|cffff0000[BG Ansager für Warteschlange]:|r %s -- verlassen : %ux%u|r', '|cffff0000[竞技场列队公告]:|r %s -- 退出 : %ux%u|r'),
