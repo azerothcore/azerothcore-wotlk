8f4aae222cc9a65b6906d059168d4ac0ee0f7b59
Vanheden <smultronstigen12@gmail.com>
2023-08-06 13:27:39 +0200

    Update go_scripts.cpp

--- src/server/scripts/World/go_scripts.cpp
+++ src/server/scripts/World/go_scripts.cpp
@@ -1163,7 +1163,7 @@ public:
         return true;
     }
 
-    bool OnGossipSelect(Player* player, GameObject* go, uint32 /*sender*/, uint32 action) override
+    bool OnGossipSelect(Player* player, GameObject* /*go*/, uint32 /*sender*/, uint32 action) override
     {
         ClearGossipMenuFor(player);
         switch (action)
