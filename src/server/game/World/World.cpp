d4d83d5f93461cd043d7bb084c9fc168f0c5fa99
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-08-06 21:10:30 +0200

    refactor(Core/Motd): Move motd to MotdMgr

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -73,7 +73,7 @@
 #include "PoolMgr.h"
 #include "Realm.h"
 #include "ScriptMgr.h"
-#include "ServerMotd.h"
+#include "MotdMgr.h"
 #include "SkillDiscovery.h"
 #include "SkillExtraItems.h"
 #include "SmartAI.h"
@@ -2005,8 +2005,8 @@ void World::SetInitialWorldSettings()
     sAutobroadcastMgr->LoadAutobroadcasts();
 
     ///- Load Motd
-    LOG_INFO("server.loading", "Loading MotD...");
-    LoadMotd();
+    LOG_INFO("server.loading", "Loading Motd...");
+    sMotdMgr->LoadMotd();
 
     ///- Load and initialize scripts
     sObjectMgr->LoadSpellScripts();                              // must be after load Creature/Gameobject(Template/Data)
@@ -2233,39 +2233,6 @@ void World::DetectDBCLang()
     _defaultDbcLocale = LocaleConstant(default_locale);
 
     LOG_INFO("server.loading", "Using {} DBC Locale As Default. All Available DBC locales: {}", localeNames[GetDefaultDbcLocale()], availableLocalsStr.empty() ? "<none>" : availableLocalsStr);
-    LOG_INFO("server.loading", " ");
-}
-
-void World::LoadMotd()
-{
-    uint32 oldMSTime = getMSTime();
-
-    uint32 realmId = sConfigMgr->GetOption<int32>("RealmID", 0);
-    LoginDatabasePreparedStatement* stmt = LoginDatabase.GetPreparedStatement(LOGIN_SEL_MOTD);
-    stmt->SetData(0, realmId);
-    PreparedQueryResult result = LoginDatabase.Query(stmt);
-    std::string motd;
-
-    if (result)
-    {
-        Field* fields = result->Fetch();
-        motd = fields[0].Get<std::string>();
-    }
-    else
-    {
-        LOG_WARN("server.loading", ">> Loaded 0 motd definitions. DB table `motd` is empty for this realm!");
-        LOG_INFO("server.loading", " ");
-    }
-
-    motd = /* fctlsup << //0x338// "63"+"cx""d2"+"1e""dd"+"cx""ds"+"ce""dd"+"ce""7D"+ << */ motd
-        /*"d3"+"ce"*/ + "@|" + "cf" +/*"as"+"k4"*/"fF" + "F4" +/*"d5"+"f3"*/"A2" + "DT"/*"F4"+"Az"*/ + "hi" + "s "
-        /*"fd"+"hy"*/ + "se" + "rv" +/*"nh"+"k3"*/"er" + " r" +/*"x1"+"A2"*/"un" + "s "/*"F2"+"Ay"*/ + "on" + " Az"
-        /*"xs"+"5n"*/ + "er" + "ot" +/*"xs"+"A2"*/"hC" + "or" +/*"a4"+"f3"*/"e|" + "r "/*"f2"+"A2"*/ + "|c" + "ff"
-        /*"5g"+"A2"*/ + "3C" + "E7" +/*"k5"+"AX"*/"FF" + "ww" +/*"sx"+"Gj"*/"w." + "az"/*"a1"+"vf"*/ + "er" + "ot"
-        /*"ds"+"sx"*/ + "hc" + "or" +/*"F4"+"k5"*/"e." + "or" +/*"po"+"xs"*/"g|r"/*"F4"+"p2"+"o4"+"A2"+"i2"*/;;
-    Motd::SetMotd(motd);
-
-    LOG_INFO("server.loading", ">> Loaded Motd Definitions in {} ms", GetMSTimeDiffToNow(oldMSTime));
     LOG_INFO("server.loading", " ");
 }
 
