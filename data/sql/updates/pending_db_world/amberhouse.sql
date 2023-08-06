80d5a5bc5859f07085d64666226f64161655f47b
Roddan <33155577+Vanheden@users.noreply.github.com>
2023-08-06 13:18:20 +0200

    Update data/sql/updates/pending_db_world/amberhouse.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/amberhouse.sql
+++ data/sql/updates/pending_db_world/amberhouse.sql
@@ -4,9 +4,8 @@ DELETE FROM `spell_target_position` WHERE `ID` = 48324 AND `EffectIndex` = 0;
 INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES
 (48324, 0, 571, 3454.11, -2802.37, 202.14, 0, 34149345);
 
-DELETE FROM `spell_script_names` WHERE `spell_id` IN (48382, 53017);
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (48382);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(48382, 'spell_q12227_outhouse_groans'),
-(53017, 'spell_q12227_indisposed_i');
+(48382, 'spell_q12227_outhouse_groans');
 
 DELETE FROM `creature` WHERE  `guid`=113841;
