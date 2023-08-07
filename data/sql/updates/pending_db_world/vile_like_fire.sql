f0e459ef96d57370ca41c8955ac13b08e1b9fd43
Webdrian <info@adrianmai.de>
2023-08-08 01:50:21 +0200

    Delete vile_like_fire.sql

--- data/sql/updates/pending_db_world/vile_like_fire.sql
+++
@@ -1,6 +0,0 @@
--- Quest Vile Like Fire (13071)
-DELETE FROM `creature_template_spell` WHERE `CreatureID` = 30564;
-INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES (30564, 6, 57403, 12340);
-
--- fixed because the kites are stuck in the ground
-UPDATE `creature_template_movement` SET `Flight`=2 WHERE  `CreatureId`=30272;
