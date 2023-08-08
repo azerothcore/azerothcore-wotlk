4c6760649737e101ed6875c6a51681ecee44fa9d
Webdrian <89680380+Webdrian@users.noreply.github.com>
2023-08-08 03:44:09 +0200

    Create Horrified_Drakkari.sql

---
+++ data/sql/updates/pending_db_world/Horrified_Drakkari.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `faction` = 37, `flags_extra` = 66 WHERE (`entry` = 26582);
+UPDATE `creature_template` SET `faction` = 37, `flags_extra` = 66 WHERE (`entry` = 26583);
