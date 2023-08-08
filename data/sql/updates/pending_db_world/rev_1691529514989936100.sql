bb3aca1ef6ed0b2a91d8ec1ff95a8cbaf0ff4597
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-08-08 23:39:03 +0200

    fix dberrors

--- data/sql/updates/pending_db_world/rev_1691529514989936100.sql
+++ data/sql/updates/pending_db_world/rev_1691529514989936100.sql
@@ -9,10 +9,10 @@ SET @CGUID := 152340;
 
 DELETE FROM `creature` WHERE `id1` IN (27346, 27398, 27399);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(@CGUID, 27398, 0, 0, 0, 1537, 1537, 0, 0, 0, -5040.92, -1250.81, 507.76, 0.70, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- Grandhammer
-(@CGUID+1, 27346, 0, 0, 0, 1537, 1537, 0, 0, 0, -5041.91, -1250.2, 507.76, 1.37, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- IF Dragon
-(@CGUID+2, 27399, 0, 0, 1, 1637, 1637, 0, 0, 0, 1964.39, -4798.77, 56.99, 0.08, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- Muja
-(@CGUID+3, 27346, 0, 0, 1, 1637, 1637, 0, 0, 0, 1963.26, -4797.62, 56.99, 0.72, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0); -- Org Dragon
+(@CGUID, 27398, 0, 0, 0, 1537, 1537, 0, 1, 0, -5040.92, -1250.81, 507.76, 0.70, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- Grandhammer
+(@CGUID+1, 27346, 0, 0, 0, 1537, 1537, 0, 1, 0, -5041.91, -1250.2, 507.76, 1.37, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- IF Dragon
+(@CGUID+2, 27399, 0, 0, 1, 1637, 1637, 0, 1, 0, 1964.39, -4798.77, 56.99, 0.08, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0), -- Muja
+(@CGUID+3, 27346, 0, 0, 1, 1637, 1637, 0, 1, 0, 1963.26, -4797.62, 56.99, 0.72, 300, 0, 0, 0, 0, 0, 0, 0, 0, '', 0); -- Org Dragon
 -- Positions based on screenshots.
 INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
 (@GEVENT, @CGUID),
