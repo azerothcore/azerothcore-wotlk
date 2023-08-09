dc817d51caa23afbc78cd1893e4aa535a06b5b4c
Webdrian <89680380+Webdrian@users.noreply.github.com>
2023-08-10 01:25:53 +0200

    Update Horrified_Drakkari.sql

--- data/sql/updates/pending_db_world/Horrified_Drakkari.sql
+++ data/sql/updates/pending_db_world/Horrified_Drakkari.sql
@@ -1,2 +1,2 @@
-UPDATE `creature_template` SET `flags_extra` = REPLACE(`flags_extra`, "62", "64") WHERE (`entry` = 26582);
-UPDATE `creature_template` SET `flags_extra` = REPLACE(`flags_extra`, "62", "64") WHERE (`entry` = 26583);
+UPDATE `creature_template` SET `flags_extra` = REPLACE(`flags_extra`, "64", "66") WHERE (`entry` = 26582);
+UPDATE `creature_template` SET `flags_extra` = REPLACE(`flags_extra`, "64", "66") WHERE (`entry` = 26583);
