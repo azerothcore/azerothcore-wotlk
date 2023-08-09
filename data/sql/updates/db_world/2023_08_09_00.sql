47ae5eef50585e46c94b2d6dcb41294119c8db50
AzerothCoreBot <azerothcorebot@gmail.com>
2023-08-09 02:51:50 +0000

    chore(DB): import pending files
    Referenced commit(s): 8eeec45c59c7930fdc379da979fc4c04b2cc159c

--- data/sql/updates/db_world/2023_08_09_00.sql
+++ data/sql/updates/db_world/2023_08_09_00.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_08_08_00 -> 2023_08_09_00
 --
 UPDATE `quest_template_addon` SET `RequiredMaxRepFaction` = 0, `RequiredMaxRepValue` = 0 WHERE `ID` IN (10106, 10110);
 UPDATE `quest_template_addon` SET `PrevQuestID` = 10143 WHERE (`ID` = 13408);
