5ba2c18ce6f832f71255e5e23bd0794f1a4347ae
Axel Cocat <ax.cocat@gmail.com>
2023-08-09 10:42:50 +0200

    feat(Core/Conf): add CONFIG_ARENA_QUEUE_ANNOUNCER_DETAIL (#16850)
    * feat(Core/Conf): add CONFIG_ARENA_QUEUE_ANNOUNCER_DETAIL
    
    * fix: bad copy paste
    
    * add sql
    
    * fix bad copy paste above again
    
    * fix: move lang values
    
    * Update rev_1690640715748711400.sql
    
    * Update rev_1690640715748711400.sql
    
    * Rename rev_1690640715748711400.sql to fix.sql
    
    * reaching, maybe fix (?) c:
    
    * love me god damn it
    
    * update sql file
    
    ---------
    
    Co-authored-by: Skjalf <47818697+Nyeriah@users.noreply.github.com>
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1189,6 +1189,7 @@ void World::LoadConfigSettings(bool reload)
     _float_configs[CONFIG_ARENA_MATCHMAKER_RATING_MODIFIER]         = sConfigMgr->GetOption<float>("Arena.ArenaMatchmakerRatingModifier", 24.0f);
     _bool_configs[CONFIG_ARENA_QUEUE_ANNOUNCER_ENABLE]              = sConfigMgr->GetOption<bool>("Arena.QueueAnnouncer.Enable", false);
     _bool_configs[CONFIG_ARENA_QUEUE_ANNOUNCER_PLAYERONLY]          = sConfigMgr->GetOption<bool>("Arena.QueueAnnouncer.PlayerOnly", false);
+    _int_configs[CONFIG_ARENA_QUEUE_ANNOUNCER_DETAIL]               = sConfigMgr->GetOption<uint32>("Arena.QueueAnnouncer.Detail", 3);
 
     _bool_configs[CONFIG_OFFHAND_CHECK_AT_SPELL_UNLEARN]            = sConfigMgr->GetOption<bool>("OffhandCheckAtSpellUnlearn", true);
     _int_configs[CONFIG_CREATURE_STOP_FOR_PLAYER]                   = sConfigMgr->GetOption<uint32>("Creature.MovingStopTimeForPlayer", 3 * MINUTE * IN_MILLISECONDS);
