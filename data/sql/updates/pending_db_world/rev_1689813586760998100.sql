92acd3728e6a98fcb398cb822a31b12e7d7f32a2
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-08-12 00:39:04 +0200

    Update data/sql/updates/pending_db_world/rev_1689813586760998100.sql

--- data/sql/updates/pending_db_world/rev_1689813586760998100.sql
+++ data/sql/updates/pending_db_world/rev_1689813586760998100.sql
@@ -5,4 +5,4 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 37906) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 21178) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(17, 0, 37906, 0, 0, 31, 1, 3, 21178, 0, 0, 0, 0, '', 'Rip the pages from this book when Varedis uses Metamorphosis to deprive him of his power.');
+(17, 0, 37906, 0, 0, 31, 1, 3, 21178, 0, 0, 0, 0, '', 'Book of Fel Names can only target Varedis');
