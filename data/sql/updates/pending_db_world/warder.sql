be981f3d567910fdfad3b46bac1be2085587b5a2
Maria-sequel <20357406+Maria-sequel@users.noreply.github.com>
2023-08-13 19:13:07 +0200

    immunities

--- data/sql/updates/pending_db_world/warder.sql
+++ data/sql/updates/pending_db_world/warder.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|256|33554432|8388608 WHERE `entry` = 18829;
+UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|256|8388608 WHERE `entry` = 18829;
