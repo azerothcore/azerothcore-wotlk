17dc80e67d9c3b8ce2f5b64b5ff978606a6c470e
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-08-13 12:26:00 -0300

    fix(Scripts/OHF): Killing Drake should not be required to start Thrall's escort

---
+++ data/sql/updates/pending_db_world/rev_1691940275333956500.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `conditions` SET `ConditionValue2` = 1, `Comment` = 'Require incendiary bombs placed' WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 7499 AND `SourceEntry` = 0 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 13 AND `ConditionTarget` = 0 AND `ConditionValue1` = 0 AND `ConditionValue2` = 2 AND `ConditionValue3` = 0; 
