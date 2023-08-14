e9e2176fb304d9ae8b82828fb448671d6bf81bcf
ZhengPeiRu21 <98835050+ZhengPeiRu21@users.noreply.github.com>
2023-08-13 18:19:19 -0600

    fix(DB/SAI): Jandice Barov correct spell IDs (#17004)

---
+++ data/sql/updates/pending_db_world/rev_1691962350657672100.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `action_param1` = 16098 WHERE `entryorguid` = 10503 AND `source_type` = 0 AND `id` = 0 AND `link` = 0;
+UPDATE `smart_scripts` SET `action_param1` = 8994 WHERE `entryorguid` = 10503 AND `source_type` = 0 AND `id` = 1 AND `link` = 0;
