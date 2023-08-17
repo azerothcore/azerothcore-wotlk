2bd988cce901602523103785a7dc2650d75425ec
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-08-16 21:52:46 -0300

    update gossip req

---
+++ data/sql/updates/pending_db_world/rev_1692233536025387200.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `conditions` SET `ConditionValue2` = 1, `ConditionValue3` = 2, `Comment` = 'Require Lt. Drake encounter complete' WHERE `ConditionValue2` = 2 AND `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 7499 AND `SourceEntry` = 9090;
