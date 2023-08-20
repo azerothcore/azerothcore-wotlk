54d1ecc1d01b6beb8a9f96643063a41874af2b6c
Dan <83884799+elthehablo@users.noreply.github.com>
2023-08-21 01:11:47 +0200

    update auras and visibility

--- data/sql/updates/pending_db_world/add-path-ids-ulduar-trash.sql
+++ data/sql/updates/pending_db_world/add-path-ids-ulduar-trash.sql
@@ -2,7 +2,7 @@
 SET @GUID := 137481;
 DELETE FROM `creature_addon` WHERE `guid` IN (@GUID, @GUID+1, @GUID+2, @GUID+3);
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
-(@GUID, (@GUID)*10, 0, 0, 1, 0, 0, NULL), -- thunderer
-(@GUID+1, (@GUID+1)*10, 0, 0, 1, 0, 0, NULL), -- thunderer
-(@GUID+2, (@GUID+2)*10, 0, 0, 1, 0, 0, NULL), -- ravager
-(@GUID+3, (@GUID+3)*10, 0, 0, 1, 0, 0, NULL); -- ravager
+(@GUID, (@GUID)*10, 0, 0, 1, 0, 3, 63610), --  thunderer
+(@GUID+1, (@GUID+1)*10, 0, 0, 1, 0, 3, 63610), -- thunderer
+(@GUID+2, (@GUID+2)*10, 0, 0, 1, 0, 3, 63616), -- ravager
+(@GUID+3, (@GUID+3)*10, 0, 0, 1, 0, 3, 63616); -- ravager
