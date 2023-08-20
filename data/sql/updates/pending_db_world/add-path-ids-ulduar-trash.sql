9e6f765572edf83165d856596b68809b0d232f0f
Dan <83884799+elthehablo@users.noreply.github.com>
2023-08-21 00:28:56 +0200

    initial

---
+++ data/sql/updates/pending_db_world/add-path-ids-ulduar-trash.sql
@@ -0,0 +1,8 @@
+--
+SET @GUID := 137481;
+DELETE FROM `creature_addon` WHERE `guid` IN (@GUID, @GUID+1, @GUID+2, @GUID+3);
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(@GUID, (@GUID)*10, 0, 0, 1, 0, 0, NULL), --  thunderer
+(@GUID+1, (@GUID+1)*10, 0, 0, 1, 0, 0, NULL), -- thunderer
+(@GUID+2, (@GUID+2)*10, 0, 0, 1, 0, 0, NULL), -- ravager
+(@GUID+3, (@GUID+3)*10, 0, 0, 1, 0, 0, NULL); -- ravager
