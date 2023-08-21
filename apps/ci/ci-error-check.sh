f3bf9b82d301cd276150d085e541d09b6c3c4718
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-08-21 20:20:35 +0200

    pt2

--- apps/ci/ci-error-check.sh
+++ apps/ci/ci-error-check.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-STARTUP_ERRORS_FILE="./env/dist/bin/StartupErrors.log";
+ERRORS_FILE="./env/dist/bin/Errors.log";
 
 echo "Checking Startup Errors"
 echo
 
-if [[ -s ${STARTUP_ERRORS_FILE} ]]; then
-     printf "The StartupErrors.log file contains errors:\n\n";
-     cat ${STARTUP_ERRORS_FILE};
+if [[ -s ${ERRORS_FILE} ]]; then
+     printf "The StartupErrors.log file contains startup errors:\n\n";
+     cat ${ERRORS_FILE};
      printf "\nPlease solve the startup errors listed above!\n";
      exit 1;
 else
-     echo "> No startup errors found in StartupErrors.log";
+     echo "> No startup errors found in Errors.log";
 fi
 done
 
