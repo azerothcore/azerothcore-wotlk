1407eb904be6da60b138d1b11858416d62f7bd12
AzerothCoreBot <azerothcorebot@gmail.com>
2023-08-21 19:17:30 +0000

    chore(DB): import pending files
    Referenced commit(s): 957ccdb8453f9136cd2ae7c5ede453b5a126964a

--- data/sql/updates/db_world/2023_08_21_01.sql
+++ data/sql/updates/db_world/2023_08_21_01.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_08_21_00 -> 2023_08_21_01
 --
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (37906, 36298);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
