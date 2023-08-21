6543334b965a4f014fe5a1d208216377d9793958
Pagani Walter <paganiwalter@gmail.com>
2023-08-21 17:04:00 -0300

    Access and update modules

--- modules/updates_modules.sh
+++ modules/updates_modules.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 for i in ./*; do
     if [[ -d "$i" ]]; then
-        git -C "$i" config pull.rebase false
-        git -C "$i" pull origin "$(git rev-parse --abbrev-ref HEAD)"
+        cd $i || exit
+        git config pull.rebase false
+        git pull origin "$(git rev-parse --abbrev-ref HEAD)"
+        cd ..
     fi
 done
