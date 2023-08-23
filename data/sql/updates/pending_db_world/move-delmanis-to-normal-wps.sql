169ec25e16681795e7d229a052de4a2ec515e279
Dan <83884799+elthehablo@users.noreply.github.com>
2023-08-23 02:13:08 +0200

    typo

--- data/sql/updates/pending_db_world/move-delmanis-to-normal-wps.sql
+++ data/sql/updates/pending_db_world/move-delmanis-to-normal-wps.sql
@@ -48,7 +48,7 @@ INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `e
 (37068, @NPC, 0, 0, 1, 0, 0, '');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 3662 AND `source_type` = 0;
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 336200 AND `source_type` = 9; -- remove now unused wp scripts
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 366200 AND `source_type` = 9; -- remove now unused wp scripts
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (3662, 0, 0, 0, 0, 0, 100, 0, 0, 0, 2400, 3800, 0, 0, 11, 20792, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Delmanis the Hated - In Combat CMC - Cast \'Frostbolt\''),
 (3662, 0, 1, 0, 0, 0, 100, 0, 6000, 10000, 12000, 16000, 0, 0, 11, 7101, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Delmanis the Hated - In Combat - Cast \'Flame Blast\''),
