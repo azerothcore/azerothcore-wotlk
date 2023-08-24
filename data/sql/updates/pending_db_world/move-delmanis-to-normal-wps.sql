bcb319587fafeae6be27b90f1c6cb627d6f79601
Dan <83884799+elthehablo@users.noreply.github.com>
2023-08-24 14:45:11 +0200

    aggressive not defensive

--- data/sql/updates/pending_db_world/move-delmanis-to-normal-wps.sql
+++ data/sql/updates/pending_db_world/move-delmanis-to-normal-wps.sql
@@ -1,3 +1,3 @@
 --
-UPDATE `smart_scripts` SET `action_param6` = 1 WHERE `entryorguid` = 3662 AND `source_type` = 0 AND `id` = 3;
-UPDATE `smart_scripts` SET `action_param6` = 1 WHERE `entryorguid` = 366200 AND `source_type` = 9 AND `id` = 1;
+UPDATE `smart_scripts` SET `action_param6` = 2 WHERE `entryorguid` = 3662 AND `source_type` = 0 AND `id` = 3;
+UPDATE `smart_scripts` SET `action_param6` = 2 WHERE `entryorguid` = 366200 AND `source_type` = 9 AND `id` = 1;
