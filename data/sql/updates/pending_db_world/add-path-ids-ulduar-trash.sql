fa955f6d3b91e3c9f3d8959268bfe147b37fbc36
Dan <83884799+elthehablo@users.noreply.github.com>
2023-08-24 11:36:09 +0200

    re-add auras

--- data/sql/updates/pending_db_world/add-path-ids-ulduar-trash.sql
+++ data/sql/updates/pending_db_world/add-path-ids-ulduar-trash.sql
@@ -2,7 +2,7 @@
 SET @GUID := 137481;
 DELETE FROM `creature_addon` WHERE `guid` IN (@GUID, @GUID+1, @GUID+2, @GUID+3);
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
-(@GUID, (@GUID)*10, 0, 0, 1, 0, 0, NULL),
-(@GUID+1, (@GUID+1)*10, 0, 0, 1, 0, 0, NULL),
-(@GUID+2, (@GUID+2)*10, 0, 0, 1, 0, 0, NULL),
-(@GUID+3, (@GUID+3)*10, 0, 0, 1, 0, 0, NULL);
+(@GUID, (@GUID)*10, 0, 0, 1, 0, 0, 63610),
+(@GUID+1, (@GUID+1)*10, 0, 0, 1, 0, 0, 63610),
+(@GUID+2, (@GUID+2)*10, 0, 0, 1, 0, 0, 63616),
+(@GUID+3, (@GUID+3)*10, 0, 0, 1, 0, 0, 63616);
