d77fc93f0a60f5e76e0fe3829d163fe7d1138cfa
Dan <83884799+elthehablo@users.noreply.github.com>
2023-08-24 15:55:43 +0200

    fix(DB/SAI): Change Delmanis the Hated ReactState (#17059)
    * initial
    
    * initial
    
    * typo
    
    * change to simple update
    
    * aggressive not defensive

---
+++ data/sql/updates/pending_db_world/move-delmanis-to-normal-wps.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `action_param6` = 2 WHERE `entryorguid` = 3662 AND `source_type` = 0 AND `id` = 3;
+UPDATE `smart_scripts` SET `action_param6` = 2 WHERE `entryorguid` = 366200 AND `source_type` = 9 AND `id` = 1;
