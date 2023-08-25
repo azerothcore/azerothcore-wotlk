026742e03fb12aad9d5c6a277654deb220b467a8
hater <hater.rip@proton.me>
2023-08-25 16:12:37 -0400

    update shaman script sql

---
+++ modules/mod-Forge/sql/world/updates/20230825 new shaman spells.sql
@@ -0,0 +1,2 @@
+delete from acore_world.spell_script_names where ScriptName = 'spell_sha_lava_lash';
+INSERT INTO acore_world.spell_script_names (spell_id, ScriptName) VALUES(110006, 'spell_sha_lava_lash_trigger');
