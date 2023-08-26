5262169effea211d395700067bc6b3f7e2d7f7d9
feltviolet <125658678+feltviolet@users.noreply.github.com>
2023-08-26 19:35:04 +0300

    Make Highlord Mograine not wait before walking in
    I had earlier added a 5 second wait to get the event timings to work nicely, but
    I believe this was needed in the first place because the wrong NPC was used for
    the event (fixed in an earlier commit). Now that the correct NPC is used, if the
    5 second wait is removed, the event timings work out nicely again.

--- data/sql/updates/pending_db_world/rev_1677501542428607570.sql
+++ data/sql/updates/pending_db_world/rev_1677501542428607570.sql
@@ -706,6 +706,5 @@ DELETE FROM smart_scripts
 
 INSERT INTO smart_scripts (entryorguid,source_type,id,link,event_type,event_phase_mask,event_chance,event_flags,event_param1,event_param2,event_param3,event_param4,event_param5,event_param6,action_type,action_param1,action_param2,action_param3,action_param4,action_param5,action_param6,target_type,target_param1,target_param2,target_param3,target_param4,target_x,target_y,target_z,target_o,comment) VALUES
 	 (@NEW_HIGHLORD_MOGRAINE_ID,0,0,1,25,0,100,513,0,0,0,0,0,0,53,0,16440,0,0,0,0,1,0,0,0,0,0.0,0.0,0.0,0.0,'Highlord Mograine - On Reset - Start Waypoint (No Repeat)'),
-	 (@NEW_HIGHLORD_MOGRAINE_ID,0,1,0,61,0,100,0,0,0,0,0,0,0,54,5000,0,0,0,0,0,0,0,0,0,0,0.0,0.0,0.0,0.0,'Highlord Mograine - On Reset - Pause Waypoint (No Repeat)'),
-	 (@NEW_HIGHLORD_MOGRAINE_ID,0,2,3,40,0,100,0,5,0,0,0,0,0,103,1,0,0,0,0,0,1,0,0,0,0,0.0,0.0,0.0,0.0,'Highlord Mograine - On Waypoint 5 Reached - Set Rooted On'),
-	 (@NEW_HIGHLORD_MOGRAINE_ID,0,3,0,61,0,100,0,0,0,0,0,0,0,66,0,0,0,0,0,0,19,3976,0,0,0,0.0,0.0,0.0,0.0,'Highlord Mograine - On Waypoint 5 Reached - Set Orientation Closest Creature ''Scarlet Commander Mograine''');
+	 (@NEW_HIGHLORD_MOGRAINE_ID,0,1,2,40,0,100,0,5,0,0,0,0,0,103,1,0,0,0,0,0,1,0,0,0,0,0.0,0.0,0.0,0.0,'Highlord Mograine - On Waypoint 5 Reached - Set Rooted On'),
+	 (@NEW_HIGHLORD_MOGRAINE_ID,0,2,0,61,0,100,0,0,0,0,0,0,0,66,0,0,0,0,0,0,19,3976,0,0,0,0.0,0.0,0.0,0.0,'Highlord Mograine - On Waypoint 5 Reached - Set Orientation Closest Creature ''Scarlet Commander Mograine''');
