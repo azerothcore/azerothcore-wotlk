5773ec9435f66b9cc84ef62fe78a69fb63abd71c
Gultask <sagemochi@hotmail.com>
2023-08-26 20:46:13 -0300

    Underbat

--- data/sql/updates/pending_db_world/rev_1693093120417899200.sql
+++ data/sql/updates/pending_db_world/rev_1693093120417899200.sql
@@ -26,3 +26,8 @@ UPDATE `smart_scripts` SET `event_param1`=5000, `event_param2`=5000, `event_para
 UPDATE `smart_scripts` SET `event_param1`=3000, `event_param2`=3000, `event_param3`=3000, `event_param4`=3000, `event_param5`=0, `event_param6`=5 WHERE `entryorguid`=33824 AND `source_type`=0 AND `id`=0 AND `link`=0;
 UPDATE `smart_scripts` SET `event_param1`=1000, `event_param2`=3000, `event_param3`=1000, `event_param4`=3000, `event_param5`=0, `event_param6`=5 WHERE `entryorguid`=36774 AND `source_type`=0 AND `id`=2 AND `link`=0;
 UPDATE `smart_scripts` SET `event_param1`=1000, `event_param2`=3000, `event_param3`=1000, `event_param4`=3000, `event_param5`=0, `event_param6`=5 WHERE `entryorguid`=36776 AND `source_type`=0 AND `id`=2 AND `link`=0;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE (`entry` = 17724);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17724);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(17724, 0, 0, 0, 106, 0, 100, 0, 1200, 12500, 1200, 12500, 0, 5, 11, 34171, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Underbat - On Hostile Behind Me - Cast \'Tentacle Lash\'');
