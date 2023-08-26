d4231c9fd168a85ae711ac16755ef17d7a186bd1
hater <hater.rip@proton.me>
2023-08-26 13:53:39 -0400

    add point defaults

---
+++ modules/mod-Forge/sql/world/updates/forge_character_points__defualt.sql
@@ -0,0 +1,3 @@
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 0, 4294967295, 0, 71);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 4, 4294967295, 17, 17);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 3, 4294967295, 0, 20);
