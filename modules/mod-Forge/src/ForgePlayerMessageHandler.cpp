c722a71467def8c8642beb95a6596e2bb519c4f8
tester <tester@tester.com>
2023-08-26 14:39:42 -0400

    add to handler

--- modules/mod-Forge/src/ForgePlayerMessageHandler.cpp
+++ modules/mod-Forge/src/ForgePlayerMessageHandler.cpp
@@ -20,6 +20,7 @@
 #include <UpdateSpecHandler.cpp>
 #include <GetTalentsHandler.cpp>
 #include <GetPerksHandler.cpp>
+#include <GetPerksInspectHandler.cpp>
 #include <LearnPerkHandler.cpp>
 #include <GetPerkSelectionHandler.cpp>
 #include <RerollPerkHandler.cpp>
@@ -295,6 +296,7 @@ void AddForgePlayerMessageHandler()
     sTopicRouter->AddHandler(new LearnPerkHandler(cache, cm));
     sTopicRouter->AddHandler(new GetPerkSelectionHandler(cache, cm));
     sTopicRouter->AddHandler(new RerollPerkHandler(cache, cm));
+    sTopicRouter->AddHandler(new GetPerksInspectHandler(cache, cm));
 
     new UseSkillBook();
     new ForgeCacheCommands();
