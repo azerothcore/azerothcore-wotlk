380b56cc83193807e6c08898846fd86c9d095c82
hater <hater.rip@proton.me>
2023-08-26 15:00:56 -0400

    update dynamic xp

--- modules/mod-Forge/src/ForgePlayerMessageHandler.cpp
+++ modules/mod-Forge/src/ForgePlayerMessageHandler.cpp
@@ -183,35 +183,35 @@ public:
         }
     }
 
-    /*void OnGiveXP(Player* player, uint32& amount, Unit* victim) override
+    void OnGiveXP(Player* player, uint32& amount, Unit* victim, uint8 source) override
     {
         if (Gamemode::HasGameMode(player, GameModeType::CLASSIC))
             return;
 
         if (player->getLevel() <= 9)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.1-9", 2);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.1-9", 5);
 
         else if (player->getLevel() <= 19)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.10-19", 2);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.10-19", 5);
 
         else if (player->getLevel() <= 29)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.20-29", 3);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.20-29", 6);
 
         else if (player->getLevel() <= 39)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.30-39", 3);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.30-39", 6);
 
         else if (player->getLevel() <= 49)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.40-49", 3);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.40-49", 7);
 
         else if (player->getLevel() <= 59)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.50-59", 3);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.50-59", 7);
 
         else if (player->getLevel() <= 69)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.60-69", 4);
+            amount *= fc->GetConfig("Dynamic.XP.Rate.60-69", 8);
 
         else if (player->getLevel() <= 79)
-            amount *= fc->GetConfig("Dynamic.XP.Rate.70-79", 4);
-    }*/
+            amount *= fc->GetConfig("Dynamic.XP.Rate.70-79", 9);
+    }
 
 private:
     TopicRouter* Router;
