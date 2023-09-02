eb92b7905a420dc6c8dbd087733120c0002eaa70
hater <hater.rip@proton.me>
2023-09-02 13:37:28 -0400

    get all quest rewards

--- modules/mod-Forge/sql/world/updates/20230902 quest item level req.sql
+++ modules/mod-Forge/sql/world/updates/20230902 quest item level req.sql
@@ -17,3 +17,33 @@ update item_template it
 inner join quest_template qt on it.entry = qt.RewardItem1
 set it.RequiredLevel = qt.MinLevel
 where it.RequiredLevel = 0 and (it.class = 2 or it.class = 4);
+
+update item_template it 
+inner join quest_template qt on it.entry = qt.RewardChoiceItemID1
+set it.RequiredLevel = qt.MinLevel
+where it.RequiredLevel = 0 and (it.class = 2 or it.class = 4);
+
+update item_template it 
+inner join quest_template qt on it.entry = qt.RewardChoiceItemID2
+set it.RequiredLevel = qt.MinLevel
+where it.RequiredLevel = 0 and (it.class = 2 or it.class = 4);
+
+update item_template it 
+inner join quest_template qt on it.entry = qt.RewardChoiceItemID3
+set it.RequiredLevel = qt.MinLevel
+where it.RequiredLevel = 0 and (it.class = 2 or it.class = 4);
+
+update item_template it 
+inner join quest_template qt on it.entry = qt.RewardChoiceItemID4
+set it.RequiredLevel = qt.MinLevel
+where it.RequiredLevel = 0 and (it.class = 2 or it.class = 4);
+
+update item_template it 
+inner join quest_template qt on it.entry = qt.RewardChoiceItemID4
+set it.RequiredLevel = qt.MinLevel
+where it.RequiredLevel = 0 and (it.class = 2 or it.class = 5);
+
+update item_template it 
+inner join quest_template qt on it.entry = qt.RewardChoiceItemID4
+set it.RequiredLevel = qt.MinLevel
+where it.RequiredLevel = 0 and (it.class = 2 or it.class = 6);
