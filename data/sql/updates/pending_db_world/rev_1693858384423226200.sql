df6f66c610e7aff70b748699deaf6da4400f774e
Gultask <100873791+Gultask@users.noreply.github.com>
2023-09-04 20:24:26 -0300

    Update rev_1693858384423226200.sql

--- data/sql/updates/pending_db_world/rev_1693858384423226200.sql
+++ data/sql/updates/pending_db_world/rev_1693858384423226200.sql
@@ -1,3 +1,6 @@
 --
 ALTER TABLE `creature`
 	ADD COLUMN `CreateObject` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `VerifiedBuild`;
+
+-- Haggle
+UPDATE `creature` SET `CreateObject` = 1 WHERE `guid` = 53788 AND `id1` = 14041;
