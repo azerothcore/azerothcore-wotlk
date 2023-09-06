45818ebfdf8305bdd0ba7f1b59656bc1887d665f
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-09-06 18:26:05 +0200

    fix?

--- src/server/scripts/Outland/HellfireCitadel/MagtheridonsLair/boss_magtheridon.cpp
+++ src/server/scripts/Outland/HellfireCitadel/MagtheridonsLair/boss_magtheridon.cpp
@@ -170,7 +170,6 @@ struct boss_magtheridon : public BossAI
 
     void ScheduleCombatEvents()
     {
-        //me->GetThreatMgr().ClearAllThreat();
         DoResetThreatList();
         me->RemoveUnitFlag(UNIT_FLAG_NOT_SELECTABLE);
         me->SetImmuneToPC(false);
