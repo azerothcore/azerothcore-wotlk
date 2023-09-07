003ed2f17c1221402187376254d1e84f98385132
hater <hater.rip@proton.me>
2023-09-07 13:51:01 -0400

    update hunter spells

--- modules/mod-Forge/sql/world/updates/20230904 kill command.sql
+++ modules/mod-Forge/sql/world/updates/20230904 kill command.sql
@@ -1,1 +1,4 @@
 INSERT INTO acore_world.spell_script_names (spell_id, ScriptName) VALUES(9000000, 'spell_hun_pet_damage_spells');
+INSERT INTO acore_world.spell_script_names (spell_id, ScriptName) VALUES(16827, 'spell_hun_pet_damage_spells');
+INSERT INTO acore_world.spell_script_names (spell_id, ScriptName) VALUES(49966, 'spell_hun_pet_damage_spells');
+INSERT INTO acore_world.spell_script_names (spell_id, ScriptName) VALUES(17253, 'spell_hun_pet_damage_spells');
