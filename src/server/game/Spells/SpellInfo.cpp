0b85ce061adeb01c43e5f9a562dfdffaf10877f1
hater <hater.rip@proton.me>
2023-09-08 18:07:25 -0400

    fix scaling of non damage

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -502,17 +502,51 @@ int32 SpellEffectInfo::CalcValue(Unit const* caster, int32 const* bp, Unit const
         value = caster->ApplyEffectModifiers(_spellInfo, _effIndex, value);
 
         // amount multiplication based on caster's level
-        if (!scaling && !basePointsPerLevel && (_spellInfo->SpellLevel) &&
-            Effect != SPELL_EFFECT_WEAPON_PERCENT_DAMAGE &&
-            Effect != SPELL_EFFECT_KNOCK_BACK &&
-            Effect != SPELL_EFFECT_ADD_EXTRA_ATTACKS &&
-            ApplyAuraName != SPELL_AURA_MOD_SPEED_ALWAYS &&
-            ApplyAuraName != SPELL_AURA_MOD_SPEED_NOT_STACK &&
-            ApplyAuraName != SPELL_AURA_MOD_INCREASE_SPEED &&
-            ApplyAuraName != SPELL_AURA_MOD_DECREASE_SPEED)
-            //there are many more: slow speed, -healing pct
-            value *= 0.25f * exp(caster->getLevel() * (70 - _spellInfo->SpellLevel) / 1000.0f);
-        //value = int32(value * (int32)getLevel() / (int32)(_spellInfo->spellLevel ? _spellInfo->spellLevel : 1));
+        if (!caster->IsControlledByPlayer() &&
+            _spellInfo->SpellLevel && _spellInfo->SpellLevel != caster->GetLevel() &&
+            !basePointsPerLevel && _spellInfo->HasAttribute(SPELL_ATTR0_SCALES_WITH_CREATURE_LEVEL))
+        {
+            bool canEffectScale = false;
+            switch (Effect)
+            {
+            case SPELL_EFFECT_SCHOOL_DAMAGE:
+            case SPELL_EFFECT_DUMMY:
+            case SPELL_EFFECT_POWER_DRAIN:
+            case SPELL_EFFECT_HEALTH_LEECH:
+            case SPELL_EFFECT_HEAL:
+            case SPELL_EFFECT_WEAPON_DAMAGE:
+            case SPELL_EFFECT_POWER_BURN:
+            case SPELL_EFFECT_SCRIPT_EFFECT:
+            case SPELL_EFFECT_NORMALIZED_WEAPON_DMG:
+            case SPELL_EFFECT_FORCE_CAST_WITH_VALUE:
+            case SPELL_EFFECT_TRIGGER_SPELL_WITH_VALUE:
+            case SPELL_EFFECT_TRIGGER_MISSILE_SPELL_WITH_VALUE:
+                canEffectScale = true;
+                break;
+            default:
+                break;
+            }
+
+            switch (ApplyAuraName)
+            {
+            case SPELL_AURA_PERIODIC_DAMAGE:
+            case SPELL_AURA_DUMMY:
+            case SPELL_AURA_PERIODIC_HEAL:
+            case SPELL_AURA_DAMAGE_SHIELD:
+            case SPELL_AURA_PROC_TRIGGER_DAMAGE:
+            case SPELL_AURA_PERIODIC_LEECH:
+            case SPELL_AURA_PERIODIC_MANA_LEECH:
+            case SPELL_AURA_SCHOOL_ABSORB:
+            case SPELL_AURA_PERIODIC_TRIGGER_SPELL_WITH_VALUE:
+                canEffectScale = true;
+                break;
+            default:
+                break;
+            }
+
+            if (canEffectScale)
+                value *= 0.25f * exp(caster->getLevel() * (100 - _spellInfo->SpellLevel) / 1000.0f);
+        }
     }
 
     return int32(value);
