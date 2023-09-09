96ad3c90795aaef606d89b32402bff1766f8b4e1
hater <hater.rip@proton.me>
2023-09-09 15:04:18 -0400

    add coeff cahnges

---
+++ modules/mod-Forge/sql/world/updates/20230909 spell coeffs.sql
@@ -0,0 +1,72 @@
+Truncate acore_world.spell_bonus_data;
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (339,0.0,0.055,0.0,0.0,'Druid - Entangling Roots'),
+	 (703,0.0,0.0,0.0,0.07,'Rogue - Garrote'),
+	 (779,0.0,0.0,0.063,0.0,'Druid - Swipe (Bear)'),
+	 (879,0.15,0.0,0.15,0.0,'Paladin - Exorcism'),
+	 (1776,0.0,0.0,0.21,0.0,'Rogue - Gouge'),
+	 (1822,0.0,0.0,0.01,0.06,'Druid - Rake ($AP*0.18 / number of ticks)'),
+	 (1978,0.0,0.0,0.0,0.04,'Hunter - Serpent Sting($RAP*0.2 / number of ticks)'),
+	 (2812,0.07,0.0,0.07,0.0,'Paladin - Holy Wrath'),
+	 (2818,0.0,0.0,0.0,0.03,'Rogue - Deadly Poison Rank 1($AP*0.12 / number of ticks)'),
+	 (3044,0.0,0.0,0.15,0.0,'Hunter - Arcane Shot');
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (3110,0.714,0.0,0.0,0.0,'Pet Warlock - Firebolt'),
+	 (3674,0.0,0.0,0.0,0.02,'Hunter - Black Arrow($RAP*0.1 / number of ticks)'),
+	 (6343,0.0,0.0,0.12,0.0,'Warrior - Thunder Clap'),
+	 (6572,0.0,0.0,0.31,0.0,'Warrior - Revenge'),
+	 (7001,0.0,0.3333,0.0,0.0,'Priest - Lightwell Renew Rank 1'),
+	 (7294,0.025,0.0,0.0,0.0,'Paladin - Retribution Aura'),
+	 (7814,0.429,0.0,0.0,0.0,'Pet Warlock - Lash of Pain'),
+	 (8034,0.1,0.0,0.0,0.0,'Shaman - Frostbrand Attack'),
+	 (8680,0.0,0.0,0.1,0.0,'Rogue - Instant Poison Rank 1'),
+	 (13218,0.0,0.0,0.04,0.0,'Rogue - Wound Poison Rank 1');
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (13376,0.015,0.0,0.0,0.0,'Pet Shaman - Fire Elemental Fire Shield'),
+	 (13797,0.0,0.0,0.0,0.02,'Hunter - Immolation Trap($RAP*0.1 / number of ticks)'),
+	 (13812,0.0,0.0,0.1,0.1,'Hunter - Explosive Trap Effect'),
+	 (19306,0.0,0.0,0.2,0.0,'Hunter - Counterattack'),
+	 (20167,0.15,0.0,0.15,0.0,'Paladin - Seal of Light Proc'),
+	 (20187,0.32,0.0,0.2,0.0,'Paladin - Judgement of Righteousness'),
+	 (20253,0.0,0.0,0.12,0.0,'Warrior - Intercept'),
+	 (20925,0.09,0.0,0.056,0.0,'Paladin - Holy Shield'),
+	 (24275,0.15,0.0,0.15,0.0,'Paladin - Hammer of Wrath'),
+	 (24640,0.0,0.067,0.0,0.0,'Pet Skills - Scorpid Poison');
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (25912,0.4286,0.0,0.0,0.0,'Paladin - Holy Shock Triggered Hurt Rank 1'),
+	 (25914,0.8057,0.0,0.0,0.0,'Paladin - Holy Shock Triggered Heal Rank 1'),
+	 (30213,0.0,0.0,0.1429,0.0,'Pet Warlock - Cleave'),
+	 (31707,0.83,0.0,0.0,0.0,'Pet Mage - Water Elemental Frostbolt'),
+	 (31803,0.0,0.013,0.0,0.025,'Paladin - Seal of Vengeance'),
+	 (31804,0.22,0.0,0.14,0.0,'Paladin - Judgement of Vengeance'),
+	 (31935,0.07,0.0,0.07,0.0,'Paladin - Avenger Shield'),
+	 (33745,0.0,0.0,0.0,0.01,'Druid - Lacerate($AP*0.05 / number of ticks)'),
+	 (33778,0.655,0.0,0.0,0.0,'Druid - Lifebloom DH'),
+	 (45477,0.0,0.0,0.1,0.0,'Death Knight - Icy Touch');
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (47632,0.0,0.0,0.15,0.0,'Death Knight - Death Coil'),
+	 (47633,0.0,0.0,0.225,0.0,'Death Knight - Death Coil Heal'),
+	 (48721,0.0,0.0,0.06,0.0,'Death Knight - Blood Boil'),
+	 (49184,0.0,0.0,0.2,0.0,'Death Knight - Howling Blast'),
+	 (49966,0.119658,0.0,0.07,0.0,'Pet Skills - Smack'),
+	 (50256,0.119658,0.0,0.07,0.0,'Pet Skills - Swipe'),
+	 (51963,0.453,0.0,0.0,0.0,'Pet Death Knight, Gargoyle Strike'),
+	 (52212,0.0,0.0,0.04805,0.0,'Death Knight - Death and Decay'),
+	 (53733,0.22,0.0,0.14,0.0,'Paladin - Judgement of Corruption'),
+	 (54049,0.429,0.0,0.0,0.0,'Pet Warlock - Shadow Bite');
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (54158,0.25,0.0,0.16,0.0,'Paladin - Jugdement (Seal of Light, Seal of Wisdom, Seal of Justice)'),
+	 (55078,0.0,0.0,0.0,0.06325,'Death Knight - Blood Plague'),
+	 (55095,0.0,0.0,0.0,0.06325,'Death Knight - Frost Fever'),
+	 (56641,0.0,0.0,0.1,0.0,'Hunter - Steady Shot'),
+	 (57755,0.0,0.0,0.5,0.0,'Warrior - Shattering Throw'),
+	 (57984,0.2,0.0,0.0,0.0,'Pet Shaman - Fire Elemental Fire Blast'),
+	 (58621,0.0,0.0,0.08,0.0,'Death Knight - Glyph of Chains of Ice'),
+	 (59637,0.15,0.0,0.0,0.0,'Pet Mage - Mirror Image Fireblast'),
+	 (59638,0.3,0.0,0.0,0.0,'Pet Mage - Mirror Image Frostbolt'),
+	 (59881,0.0299,0.0299,0.0175,0.0175,'Pet Skills - Rake');
+INSERT INTO acore_world.spell_bonus_data (entry,direct_bonus,dot_bonus,ap_bonus,ap_dot_bonus,comments) VALUES
+	 (60089,0.0,0.0,0.15,0.0,'Druid - Faerie Fire (feral), damage'),
+	 (61193,0.333,0.333,0.0,0.0,'Pet Skills - Spirit Strike'),
+	 (61491,0.0,0.0,0.12,0.0,'Warrior - Intercept'),
+	 (64382,0.0,0.0,0.5,0.0,'Warrior - Shattering Throw');
