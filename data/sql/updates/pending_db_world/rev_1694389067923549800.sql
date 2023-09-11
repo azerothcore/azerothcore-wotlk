c5301d80eb029a2e53880a465a9d4fa92fadf61d
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-11 00:58:04 -0300

    Update data/sql/updates/pending_db_world/rev_1694389067923549800.sql

--- data/sql/updates/pending_db_world/rev_1694389067923549800.sql
+++ data/sql/updates/pending_db_world/rev_1694389067923549800.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `creature_template_spell` WHERE `CreatureID` = 27150;
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 27150 AND `index` = 2;
 INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
 (21750, 2, 37469, 0);
