c19ec3c56b31ed09c0658c76553da9a49688bff9
Gultask <100873791+Gultask@users.noreply.github.com>
2023-09-14 21:21:58 -0300

    varchar -> text

--- data/sql/updates/pending_db_world/rev_1694636334573006200.sql
+++ data/sql/updates/pending_db_world/rev_1694636334573006200.sql
@@ -1,8 +1,8 @@
 --
 ALTER TABLE `creature`
-    ADD COLUMN `Comment` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `CreateObject`;
+    ADD COLUMN `Comment` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `CreateObject`;
 
 ALTER TABLE `gameobject`
-    ADD COLUMN `Comment` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `VerifiedBuild`;
+    ADD COLUMN `Comment` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `VerifiedBuild`;
 
 UPDATE `creature` SET `Comment` = 'Original Orientation: 3.141592741012573242' WHERE `id1` = 18634 AND `guid` = 146209;
