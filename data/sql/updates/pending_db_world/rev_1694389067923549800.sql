f2de40d1721aa45da7d804ed2e45373d84329112
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-15 02:34:56 -0300

    fix(DB/Creature): Correct Orc Warlock controlled spell (#17232)
    * fix(DB/Creature): Correct Orc Warlock controlled spell
    
    * Update data/sql/updates/pending_db_world/rev_1694389067923549800.sql
    
    * Update data/sql/updates/pending_db_world/rev_1694389067923549800.sql
    
    * Update data/sql/updates/pending_db_world/rev_1694389067923549800.sql
    
    ---------
    
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1694389067923549800.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 21750 AND `Index` = 2;
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(21750, 2, 37469, 0);
