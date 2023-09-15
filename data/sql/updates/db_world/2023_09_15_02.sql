66379d907125926ceb74695336e8bbce343a0a9b
AzerothCoreBot <azerothcorebot@gmail.com>
2023-09-15 05:36:07 +0000

    chore(DB): import pending files
    Referenced commit(s): f2de40d1721aa45da7d804ed2e45373d84329112

--- data/sql/updates/db_world/2023_09_15_02.sql
+++ data/sql/updates/db_world/2023_09_15_02.sql
@@ -1,3 +1,4 @@
+-- DB update 2023_09_15_01 -> 2023_09_15_02
 --
 DELETE FROM `creature_template_spell` WHERE `CreatureID` = 21750 AND `Index` = 2;
 INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
