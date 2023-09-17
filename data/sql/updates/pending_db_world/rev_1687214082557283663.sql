3b9b29fa77e0bea7d9229f0aa9a86b96ba324808
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-09-17 21:15:23 +0200

    Update rev_1687214082557283663.sql

--- data/sql/updates/pending_db_world/rev_1687214082557283663.sql
+++ data/sql/updates/pending_db_world/rev_1687214082557283663.sql
@@ -1,2 +1,6 @@
 --
-UPDATE `smart_scripts` SET `action_param1` = 38461 WHERE `source_type`=0 AND `entryorguid` = 21246 AND `id`=0;
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 21246;
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 21246) AND (`source_type` = 0) AND (`id` IN (0));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(21246, 0, 0, 0, 0, 0, 100, 0, 4850, 19400, 10900, 14500, 0, 0, 11, 39197, 0, 0, 0, 0, 0, 5, 25, 0, 0, 0, 0, 0, 0, 0, 'Serpentshrine Sporebat - In Combat - Cast \'Sonic Charge\'');
