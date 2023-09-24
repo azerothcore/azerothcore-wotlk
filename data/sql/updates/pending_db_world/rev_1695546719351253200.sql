54ad1843996521e84a2bad00c225431bb188609f
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-24 07:17:02 -0300

    missing sql

--- data/sql/updates/pending_db_world/rev_1695546719351253200.sql
+++ data/sql/updates/pending_db_world/rev_1695546719351253200.sql
@@ -30,3 +30,11 @@ INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`
 (@PATH,14,-11178.082,-1922.4479,231.99551,NULL,0,0,0,100,0),
 (@PATH,15,-11180.802,-1915.3156,231.99551,NULL,0,0,0,100,0),
 (@PATH,16,-11181.109,-1909.6647,231.99551,NULL,0,0,0,100,0);
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17161;
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 17161) AND (`source_type` = 0) AND (`id` IN (0, 1, 2));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(17161, 0, 0, 0, 8, 0, 100, 0, 29969, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blizzard (Shade of Aran) - On Spellhit \'Summon Blizzard\' - Set Event Phase 1'),
+(17161, 0, 1, 0, 60, 1, 100, 0, 1000, 1000, 2000, 2000, 0, 11, 29951, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blizzard (Shade of Aran) - On Update - Cast \'Blizzard\' (Phase 1)'),
+(17161, 0, 2, 0, 60, 1, 100, 0, 20000, 20000, 20000, 20000, 0, 22, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blizzard (Shade of Aran) - On Update - Set Event Phase 0 (Phase 1)');
