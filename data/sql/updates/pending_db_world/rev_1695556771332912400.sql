7a1d17cc7fff6172b726b369f772d322ac8494e1
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-24 09:00:17 -0300

    feat(Scripts/Commands): Display creature entry when using the npc near command

---
+++ data/sql/updates/pending_db_world/rev_1695556771332912400.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `acore_string` SET `content_default` = '%d - (entry: %d) |cffffffff|Hcreature:%d|h[%s X:%f Y:%f Z:%f MapId:%d]|h|r',
+`locale_deDE` = '%d - (entry: %d) |cffffffff|Hcreature:%d|h[%s X:%f Y:%f Z:%f MapId:%d]|h|r',
+`locale_zhCN` = '%d%s - (entry: %d) |cffffffff|H生物:%d|h[%s X:%f Y:%f Z:%f 地图号:%d]|h|r' WHERE `entry` = 515;
