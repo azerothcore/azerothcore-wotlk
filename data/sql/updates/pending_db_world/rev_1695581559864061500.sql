70cf19e9f6e4b8e1c29d76bba7686ff67ee0e1fd
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-24 16:03:54 -0300

    fix(DB/Conditions): Fix Berthold teleport gossip conditions

---
+++ data/sql/updates/pending_db_world/rev_1695581559864061500.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `conditions` SET `ConditionValue1` = 6, `ConditionValue3` = 2 WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 7139 AND `SourceEntry` = 3;
+UPDATE `spell_target_position` SET `PositionX` = -11165.2, `PositionY` = -1911.95, `PositionZ` = 232.009, `Orientation` = 2.14352, VerifiedBuild = 51845 WHERE `ID` = 39567;
