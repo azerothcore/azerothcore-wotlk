cc60bc0c353573f8b3d5b3d8024407f80912f24f
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-25 06:10:23 -0300

    fix(Scripts/Spells): Fix Healing & Mana injectors not giving bonus to… (#17348)
    * fix(Scripts/Spells): Fix Healing & Mana injectors not giving bonus to engineers
    
    * Update SpellEffects.cpp
    
    * Update spell_item.cpp

---
+++ data/sql/updates/pending_db_world/rev_1695609935652952000.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (67486, 67489, 67487, 67490);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(67486, 'spell_item_healing_injector'),
+(67489, 'spell_item_healing_injector'),
+(67487, 'spell_item_mana_injector'),
+(67490, 'spell_item_mana_injector');
