e67a0b1a82a0173c6100921b1bcf91b8834c9308
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-09-25 21:31:36 -0300

    fix(Scripts/Spells): Fix Furious Howl not giving ranged attack power … (#17345)

---
+++ data/sql/updates/pending_db_world/rev_1695604876617457700.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id`= -24604 AND `ScriptName` = 'spell_hun_furious_howl';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(-24604, 'spell_hun_furious_howl');
+
+DELETE FROM `spell_dbc` WHERE `ID` IN (24604, 64491, 64492, 64493, 64494, 64495, 53434);
