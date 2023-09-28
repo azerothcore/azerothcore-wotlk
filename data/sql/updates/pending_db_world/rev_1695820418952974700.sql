1ee770e159f2548883ce3f81642f98cf5687d4aa
Ludwig <sudlud@users.noreply.github.com>
2023-09-28 21:12:23 +0200

    Update data/sql/updates/pending_db_world/rev_1695820418952974700.sql
    Co-authored-by: Dan <83884799+elthehablo@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1695820418952974700.sql
+++ data/sql/updates/pending_db_world/rev_1695820418952974700.sql
@@ -1,6 +1,6 @@
 -- #12145 midsummer add spell script spell_midsummer_ribbon_pole_visual
-DELETE FROM acore_world.spell_script_names WHERE spell_id IN (29531, 29705, 29726, 29727);
-INSERT INTO acore_world.spell_script_names (spell_id, ScriptName) VALUES
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (29531, 29705, 29726, 29727);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (29531, 'spell_midsummer_ribbon_pole_visual'),
 (29705, 'spell_midsummer_ribbon_pole_visual'),
 (29726, 'spell_midsummer_ribbon_pole_visual'),
