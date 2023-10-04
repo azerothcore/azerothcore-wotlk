708c04050bb65a0a27426639002abc1fb7baf1d7
Ludwig <sudlud@users.noreply.github.com>
2023-10-04 22:09:39 +0200

    fix(Scripts/Midsummer): Implement Ribbon Pole dance effects

---
+++ data/sql/updates/pending_db_world/rev_1696363960744890900.sql
@@ -0,0 +1,4 @@
+-- midsummer add spell script spell_midsummer_ribbon_pole_firework
+DELETE FROM `spell_script_names` WHERE `spell_id` = 46847;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(46847, 'spell_midsummer_ribbon_pole_firework');
