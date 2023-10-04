d39837027c5cae8ef34393ed14794d3075dc3baf
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-04 10:09:16 +0300

    Update rev_1696402821792959500.sql
    Co-Authored-By: Carlos Morales <carlos.morales@insmet.cu>

--- data/sql/updates/pending_db_world/rev_1696402821792959500.sql
+++ data/sql/updates/pending_db_world/rev_1696402821792959500.sql
@@ -1,4 +1,4 @@
--- Gor'mul
+-- Gor'mul RP
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 2792 AND `source_type` = 0;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 279200 AND `source_type` = 9;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
