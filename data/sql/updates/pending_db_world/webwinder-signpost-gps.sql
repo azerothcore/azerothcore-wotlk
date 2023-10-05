b92c83abb5cb17ad685b8b911dda3802ff1f5176
damnablebear <127447064+damnablebear@users.noreply.github.com>
2023-10-05 09:22:27 -0400

    Update data/sql/updates/pending_db_world/webwinder-signpost-gps.sql
    Co-authored-by: Dan <83884799+elthehablo@users.noreply.github.com>

--- data/sql/updates/pending_db_world/webwinder-signpost-gps.sql
+++ data/sql/updates/pending_db_world/webwinder-signpost-gps.sql
@@ -1,3 +1,3 @@
 UPDATE `gameobject` SET `position_x` = '634.220', `position_y` =  '359.989' WHERE (`guid` = '47468' and id = '19569');
 UPDATE `gameobject` SET `position_x` = '634.065', `position_y` =  '360.295' WHERE (`guid` = '47480' and id = '19570');
-UPDATE `gameobject` SET `position_x` = '633.923', `position_y` =  '359.858', `position_z` = '50.713' WHERE (`guid` = '47481' and id = '19571');
+UPDATE `gameobject` SET `position_x` = 633.923, `position_y` =  359.858, `position_z` = 50.713 WHERE `guid` = 47481 AND `id` = 19571;
