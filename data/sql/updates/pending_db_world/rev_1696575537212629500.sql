9b98242d4980ffae6fd22e1608c7a5cc27d06fee
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-06 10:05:43 +0300

    fix(DB/Conditions): Don't allow multiple Banner of Provocation

---
+++ data/sql/updates/pending_db_world/rev_1696575537212629500.sql
@@ -0,0 +1,4 @@
+-- Banner of Provocation
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceGroup` = 0 AND `SourceEntry` = 27517;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(17, 0, 27517, 0, 0, 30, 0, 181058, 20, 0, 1, 25, 0, '', 'Allow using "Banner of Provocation" if there are no other banners within 20y.');
