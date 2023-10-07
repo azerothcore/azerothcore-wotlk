0ea052be1390660e4e7eddd6ceaad4adc868f024
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-08 00:27:39 +0300

    zzz

--- data/sql/updates/pending_db_world/rev_1696691443957086100.sql
+++ data/sql/updates/pending_db_world/rev_1696691443957086100.sql
@@ -8,11 +8,12 @@ INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, 
 (9273, 1735, 1, 405, 598, 1, 1, -1778, 2997.54, 35.715, 4.76, 0, 0, 0, 0, 2700, 100, 1, '', 0, NULL),
 (9274, 1735, 1, 405, 598, 1, 1, -1765, 2986.9, 40.316, 5.803, 0, 0, 0, 0, 2700, 100, 1, '', 0, NULL);
 
-DELETE FROM `pool_template` WHERE `entry` IN (11699,11706,11707);
+DELETE FROM `pool_template` WHERE `entry` IN (11699,11706,11707,11708);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`) VALUES
 (11699, 1, 'Spawn Point 1 - Desolace'),
 (11706, 1, 'Spawn Point 2 - Desolace'),
-(11707, 1, 'Spawn Point 3 - Desolace');
+(11707, 1, 'Spawn Point 3 - Desolace'),
+(11708, 1, 'Desolace Mithril Deposit Pool of Pools 1');
 
 DELETE FROM `pool_gameobject` WHERE `guid` IN (9256,9257,9258,9266,9268,9270,9272,9273,9274);
 INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VALUES
@@ -26,7 +27,8 @@ INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`) VA
 (9270, 11707, 0, 'Spawn Point 3 - Mithril'),
 (9274, 11707, 25, 'Spawn Point 3 - Iron');
 
-DELETE FROM `pool_pool` WHERE `mother_pool` IN (11699);
+DELETE FROM `pool_pool` WHERE `mother_pool` = 11708;
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`) VALUES
-(11706, 11699, 0, 'Desolace - Mithril Deposit - Group 1'),
-(11707, 11699, 0, 'Desolace - Mithril Deposit - Group 1');
+(11699, 11708, 0, 'Desolace - Mithril Deposit - Group 1'),
+(11706, 11708, 0, 'Desolace - Mithril Deposit - Group 1'),
+(11707, 11708, 0, 'Desolace - Mithril Deposit - Group 1');
