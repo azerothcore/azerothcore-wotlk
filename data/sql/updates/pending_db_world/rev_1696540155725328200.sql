771b3d7df941881317d0e5a15298cecf2262ed75
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-08 15:38:02 +0300

    fix(DB/Spell): Allow Armor Shatter/Puncture armor to stack (#17429)

---
+++ data/sql/updates/pending_db_world/rev_1696540155725328200.sql
@@ -0,0 +1,7 @@
+-- Annihilator - Armor Shatter
+DELETE FROM `spell_custom_attr` WHERE `spell_id`=16928;
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (16928, 4194304);
+
+-- Bashguuder & Bashguuder - Puncture Armor
+DELETE FROM `spell_custom_attr` WHERE `spell_id`=17315;
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (17315, 4194304);
