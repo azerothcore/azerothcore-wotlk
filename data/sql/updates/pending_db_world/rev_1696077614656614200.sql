409660b1038ba7992e3d2e7d6488c45de7275239
Ludwig <sudlud@users.noreply.github.com>
2023-10-08 12:23:19 +0200

    sql: do not set SmartAI for NPC 17066
    already using ScriptName and can't have both

--- data/sql/updates/pending_db_world/rev_1696077614656614200.sql
+++ data/sql/updates/pending_db_world/rev_1696077614656614200.sql
@@ -1,3 +1,2 @@
 -- midsummer creatureAI npc_midsummer_ribbon_pole_target
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17066;
 UPDATE `creature_template` SET `ScriptName` = 'npc_midsummer_ribbon_pole_target' WHERE `entry` = 17066;
