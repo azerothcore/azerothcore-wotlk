9b116a4918026268393e30684ca4a82b11032800
Ludwig <sudlud@users.noreply.github.com>
2023-10-08 14:20:25 +0200

    combine 3 sql files into 1

--- data/sql/updates/pending_db_world/rev_1696077614656614200.sql
+++
@@ -1,2 +0,0 @@
--- midsummer creatureAI npc_midsummer_ribbon_pole_target
-UPDATE `creature_template` SET `ScriptName` = 'npc_midsummer_ribbon_pole_target' WHERE `entry` = 17066;
--- data/sql/updates/pending_db_world/rev_1696363960744890900.sql
+++
@@ -1,4 +0,0 @@
--- midsummer add spell script spell_midsummer_ribbon_pole_firework
-DELETE FROM `spell_script_names` WHERE `spell_id` = 46847;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(46847, 'spell_midsummer_ribbon_pole_firework');
