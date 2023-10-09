67eac3fbab9f6c3e7525c778f4a92582366b7a22
KJack <kgithub@kellerg.com>
2023-10-09 14:12:53 -0400

    Update Bloodkelp (22094) drop rates per WoWHead. https://www.wowhead.com/wotlk/item=22094/bloodkelp

---
+++ data/sql/updates/pending_db_world/rev_1696875112573535391.sql
@@ -0,0 +1,120 @@
+-- Reference: https://www.wowhead.com/wotlk/item=22094/bloodkelp
+-- Strashz Warrior (4364) loot table
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4364);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4364, 5759, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Warrior - Thorium Lockbox'),
+(4364, 7910, 0, 0.368, 0, 1, 0, 1, 1, 'Strashaz Warrior - Star Ruby'),
+(4364, 7973, 0, 40.884, 0, 1, 0, 1, 1, 'Strashaz Warrior - Big-mouth Clam'),
+(4364, 8766, 0, 2.947, 0, 1, 0, 1, 1, 'Strashaz Warrior - Morning Glory Dew'),
+(4364, 8959, 0, 0.921, 0, 1, 0, 1, 1, 'Strashaz Warrior - Raw Spinefin Halibut'),
+(4364, 12714, 0, 5.52, 0, 1, 0, 1, 1, 'Strashaz Warrior - Plans: Runic Plate Helm'),
+(4364, 13446, 0, 1.657, 0, 1, 0, 1, 1, 'Strashaz Warrior - Major Healing Potion'),
+(4364, 16251, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Warrior - Formula: Enchant Bracer - Superior Stamina'),
+(4364, 22094, 0, 25, 1, 1, 0, 1, 2, 'Strashaz Warrior - Bloodkelp'),
+(4364, 24016, 24016, 1, 0, 1, 1, 1, 1, 'Strashaz Warrior - (ReferenceTable)'),
+(4364, 24018, 24018, 1, 0, 1, 1, 1, 1, 'Strashaz Warrior - (ReferenceTable)'),
+(4364, 24024, 24024, 5, 0, 1, 1, 1, 1, 'Strashaz Warrior - (ReferenceTable)'),
+(4364, 24033, 24033, 1, 0, 1, 1, 1, 1, 'Strashaz Warrior - (ReferenceTable)'),
+(4364, 24036, 24036, 5, 0, 1, 1, 1, 1, 'Strashaz Warrior - (ReferenceTable)');
+
+-- Strashaz Serpent Guard (4366)
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4366);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4366, 5759, 0, 0.368, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Thorium Lockbox'),
+(4366, 7909, 0, 0.921, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Aquamarine'),
+(4366, 7973, 0, 38.858, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Big-mouth Clam'),
+(4366, 8766, 0, 2.394, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Morning Glory Dew'),
+(4366, 8959, 0, 1.105, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Raw Spinefin Halibut'),
+(4366, 12706, 0, 9.21, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Plans: Runic Plate Shoulders'),
+(4366, 13446, 0, 1.473, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Major Healing Potion'),
+(4366, 14467, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Pattern: Frostweave Robe'),
+(4366, 14508, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Pattern: Felcloth Shoulders'),
+(4366, 15746, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Pattern: Chimeric Leggings'),
+(4366, 15757, 0, 0.368, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Pattern: Wicked Leather Pants'),
+(4366, 15765, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Pattern: Runic Leather Pants'),
+(4366, 16251, 0, 0.184, 0, 1, 0, 1, 1, 'Strashaz Serpent Guard - Formula: Enchant Bracer - Superior Stamina'),
+(4366, 22094, 0, 25, 1, 1, 0, 1, 2, 'Strashaz Serpent Guard - Bloodkelp'),
+(4366, 24016, 24016, 1, 0, 1, 1, 1, 1, 'Strashaz Serpent Guard - (ReferenceTable)'),
+(4366, 24018, 24018, 1, 0, 1, 1, 1, 1, 'Strashaz Serpent Guard - (ReferenceTable)'),
+(4366, 24024, 24024, 5, 0, 1, 1, 1, 1, 'Strashaz Serpent Guard - (ReferenceTable)'),
+(4366, 24033, 24033, 1, 0, 1, 1, 1, 1, 'Strashaz Serpent Guard - (ReferenceTable)'),
+(4366, 24036, 24036, 5, 0, 1, 1, 1, 1, 'Strashaz Serpent Guard - (ReferenceTable)');
+
+-- Strashaz Myrmidon (4368)
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4368);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4368, 5498, 0, 0.327, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Small Lustrous Pearl'),
+(4368, 5759, 0, 0.49, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Thorium Lockbox'),
+(4368, 7909, 0, 0.163, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Aquamarine'),
+(4368, 7910, 0, 0.163, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Star Ruby'),
+(4368, 7973, 0, 41.013, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Big-mouth Clam'),
+(4368, 8766, 0, 1.961, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Morning Glory Dew'),
+(4368, 8959, 0, 1.471, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Raw Spinefin Halibut'),
+(4368, 10310, 0, 0.327, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Scroll of Strength IV'),
+(4368, 12718, 0, 11.44, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Plans: Runic Breastplate'),
+(4368, 13446, 0, 0.98, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Major Healing Potion'),
+(4368, 14491, 0, 0.163, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Pattern: Runecloth Pants'),
+(4368, 14506, 0, 0.163, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Pattern: Felcloth Robe'),
+(4368, 15755, 0, 0.163, 0, 1, 0, 1, 1, 'Strashaz Myrmidon - Pattern: Chimeric Vest'),
+(4368, 24016, 24016, 1, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 24017, 24017, 0.5, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 24018, 24018, 1, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 24020, 24020, 1, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 24024, 24024, 5, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 24033, 24033, 1, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 24036, 24036, 5, 0, 1, 1, 1, 1, 'Strashaz Myrmidon - (ReferenceTable)'),
+(4368, 22094, 0, 25, 1, 1, 0, 1, 1, 'Strashaz Myrmidon - Bloodkelp');
+
+-- Strashaz Sorceress (4370)
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4370);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4370, 4500, 0, 0.181, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Traveler\'s Backpack'),
+(4370, 7909, 0, 0.181, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Aquamarine'),
+(4370, 7910, 0, 0.181, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Star Ruby'),
+(4370, 7973, 0, 42.029, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Big-mouth Clam'),
+(4370, 8766, 0, 3.986, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Morning Glory Dew'),
+(4370, 8959, 0, 1.087, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Raw Spinefin Halibut'),
+(4370, 12713, 0, 0.362, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Plans: Radiant Leggings'),
+(4370, 13446, 0, 1.812, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Major Healing Potion'),
+(4370, 15765, 0, 0.181, 0, 1, 0, 1, 1, 'Strashaz Sorceress - Pattern: Runic Leather Pants'),
+(4370, 22094, 0, 25, 1, 1, 0, 1, 2, 'Strashaz Sorceress - Bloodkelp'),
+(4370, 24016, 24016, 1, 0, 1, 1, 1, 1, 'Strashaz Sorceress - (ReferenceTable)'),
+(4370, 24018, 24018, 1, 0, 1, 1, 1, 1, 'Strashaz Sorceress - (ReferenceTable)'),
+(4370, 24020, 24020, 1, 0, 1, 1, 1, 1, 'Strashaz Sorceress - (ReferenceTable)'),
+(4370, 24024, 24024, 5, 0, 1, 1, 1, 1, 'Strashaz Sorceress - (ReferenceTable)'),
+(4370, 24033, 24033, 1, 0, 1, 1, 1, 1, 'Strashaz Sorceress - (ReferenceTable)'),
+(4370, 24036, 24036, 5, 0, 1, 1, 1, 1, 'Strashaz Sorceress - (ReferenceTable)');
+
+-- Strashaz Siren (4371)
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4371);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4371, 5759, 0, 0.201, 0, 1, 0, 1, 1, 'Strashaz Siren - Thorium Lockbox'),
+(4371, 7909, 0, 0.201, 0, 1, 0, 1, 1, 'Strashaz Siren - Aquamarine'),
+(4371, 7973, 0, 43.662, 0, 1, 0, 1, 1, 'Strashaz Siren - Big-mouth Clam'),
+(4371, 8766, 0, 2.817, 0, 1, 0, 1, 1, 'Strashaz Siren - Morning Glory Dew'),
+(4371, 8959, 0, 1.408, 0, 1, 0, 1, 1, 'Strashaz Siren - Raw Spinefin Halibut'),
+(4371, 13446, 0, 0.805, 0, 1, 0, 1, 1, 'Strashaz Siren - Major Healing Potion'),
+(4371, 14491, 0, 0.201, 0, 1, 0, 1, 1, 'Strashaz Siren - Pattern: Runecloth Pants'),
+(4371, 15743, 0, 0.201, 0, 1, 0, 1, 1, 'Strashaz Siren - Pattern: Heavy Scorpid Belt'),
+(4371, 22094, 0, 25, 1, 1, 0, 1, 2, 'Strashaz Siren - Bloodkelp'),
+(4371, 24016, 24016, 1, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)'),
+(4371, 24017, 24017, 0.5, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)'),
+(4371, 24018, 24018, 1, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)'),
+(4371, 24019, 24019, 0.5, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)'),
+(4371, 24024, 24024, 5, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)'),
+(4371, 24033, 24033, 1, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)'),
+(4371, 24036, 24036, 5, 0, 1, 1, 1, 1, 'Strashaz Siren - (ReferenceTable)');
+
+-- Tidelord Rrurgaz (16072)
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16072);
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(16072, 5760, 0, 1.042, 0, 1, 0, 1, 1, 'Tidelord Rrurgaz - Eternium Lockbox'),
+(16072, 7973, 0, 36.458, 0, 1, 0, 1, 1, 'Tidelord Rrurgaz - Big-mouth Clam'),
+(16072, 8766, 0, 2.083, 0, 1, 0, 1, 1, 'Tidelord Rrurgaz - Morning Glory Dew'),
+(16072, 12718, 0, 10.42, 0, 1, 0, 1, 1, 'Tidelord Rrurgaz - Plans: Runic Breastplate'),
+(16072, 13444, 0, 1.042, 0, 1, 0, 1, 1, 'Tidelord Rrurgaz - Major Mana Potion'),
+(16072, 13446, 0, 1.042, 0, 1, 0, 1, 1, 'Tidelord Rrurgaz - Major Healing Potion'),
+(16072, 24018, 24018, 1, 0, 1, 1, 1, 1, 'Tidelord Rrurgaz - (ReferenceTable)'),
+(16072, 24020, 24020, 1, 0, 1, 1, 1, 1, 'Tidelord Rrurgaz - (ReferenceTable)'),
+(16072, 24024, 24024, 5, 0, 1, 1, 1, 1, 'Tidelord Rrurgaz - (ReferenceTable)'),
+(16072, 22094, 0, 100, 1, 1, 0, 1, 1, 'Tidelord Rrurgaz - Bloodkelp');
