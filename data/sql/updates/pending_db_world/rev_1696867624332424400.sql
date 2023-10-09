e5c636c76fb96d69321fa034c625f5f52f7bf375
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-09 20:17:24 +0300

    ocd flag

--- data/sql/updates/pending_db_world/rev_1696867624332424400.sql
+++ data/sql/updates/pending_db_world/rev_1696867624332424400.sql
@@ -4,5 +4,5 @@ DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 14) AND (`SourceGrou
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (1, 10816, 12738, 0, 0, 8, 0, 5058, 0, 0, 0, 0, 0, '', 'Drop Dalson Outhouse Key from Wandering Skeletons only if quest Mrs. Dalson\'s Diary has been rewarded.'),
 (14, 3001, 3694, 0, 0, 8, 0, 5058, 0, 0, 0, 0, 0, '', 'Show Mrs. Dalson\'s Diary\'s gossip menu only if quest Mrs. Dalson\'s Diary has been rewarded.');
-
-UPDATE `quest_template_addon` SET `SpecialFlags` = 0 WHERE (`ID` = 5058);
+-- Repeatable flag (also messes with condition quest_rewarded for some reason)
+UPDATE `quest_template_addon` SET `SpecialFlags` = `SpecialFlags`&~1 WHERE (`ID` = 5058);
