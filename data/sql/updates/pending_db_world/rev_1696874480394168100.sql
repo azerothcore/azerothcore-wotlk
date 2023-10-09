f21204d18de1d6ce6c5be3c721b0d4722b587596
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-09 21:19:10 +0300

    nice bait wowhead

--- data/sql/updates/pending_db_world/rev_1696874480394168100.sql
+++ data/sql/updates/pending_db_world/rev_1696874480394168100.sql
@@ -1,2 +1,2 @@
 -- Seeking the Kor Gem, Bailor's Ore Shipment
-UPDATE `quest_template_addon` SET `PrevQuestID` = 1654 WHERE `ID` IN (1442,1655);
+UPDATE `quest_template_addon` SET `PrevQuestID` = 1653 WHERE `ID` IN (1442,1655);
