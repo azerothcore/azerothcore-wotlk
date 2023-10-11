eface3e45d0490cbb64b9f0fd1113f7e1a5ade2e
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-11 22:41:32 +0300

    fix(Core/Spells): Call of the Wild targets

---
+++ data/sql/updates/pending_db_world/rev_1697052883726753200.sql
@@ -0,0 +1,5 @@
+-- Call of the Wild
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (-24604,53434);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(-24604, 'spell_hun_target_self_and_pet'),
+(53434, 'spell_hun_target_self_and_pet');
