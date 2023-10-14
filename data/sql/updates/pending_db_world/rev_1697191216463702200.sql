fe0dea82d43af3fba91ba62b4396e510cd969672
Angelo Venturini <nefertum.dev@protonmail.com>
2023-10-14 01:52:38 -0300

    Update data/sql/updates/pending_db_world/rev_1697191216463702200.sql

--- data/sql/updates/pending_db_world/rev_1697191216463702200.sql
+++ data/sql/updates/pending_db_world/rev_1697191216463702200.sql
@@ -2,4 +2,4 @@
 UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 5891;
 UPDATE `creature_template_addon` SET `auras` = '8203' WHERE (`entry` = 5891);
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 5891) AND (`source_type` = 0)
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 5891) AND (`source_type` = 0);
