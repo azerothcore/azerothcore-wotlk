02fb063825b8af9e98156af5790dc3a462b061f1
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-16 18:48:33 +0300

    fix(Core/Spells): Script Scourge Banner

---
+++ data/sql/updates/pending_db_world/rev_1697469523592686600.sql
@@ -0,0 +1,3 @@
+-- Scourge Banner
+DELETE FROM `spell_script_names` WHERE `spell_id`=16989 AND `ScriptName`='spell_gen_planting_scourge_banner';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (16989, 'spell_gen_planting_scourge_banner');
