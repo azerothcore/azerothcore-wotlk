9831689f4834616710bb8293c04406e5343e42ec
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-17 23:33:07 +0300

    fix(DB/SAI): Ravenous Windroc not stacking eagle claw (#17531)
    DB/SAI: Ravenous Windroc not stacking eagle claw

---
+++ data/sql/updates/pending_db_world/rev_1697523119963738400.sql
@@ -0,0 +1,5 @@
+-- Ravenous Windroc
+UPDATE `smart_scripts` SET `action_param2` = 0 WHERE `entryorguid` = 18220 AND `source_type` = 0 AND `id` = 0;
+
+DELETE FROM `spell_custom_attr` WHERE `spell_id`=30285;
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (30285, 4194304);
