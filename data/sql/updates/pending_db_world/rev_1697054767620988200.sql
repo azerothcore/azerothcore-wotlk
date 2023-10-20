1510830147b283640133db53efaebabcb5d1752f
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-20 15:49:53 +0300

    fix(DB/Spell): Karazhan Bloodlust/Heroism (#17486)

---
+++ data/sql/updates/pending_db_world/rev_1697054767620988200.sql
@@ -0,0 +1,5 @@
+-- Bloodlust/Heroism - Karazhan chest event
+DELETE FROM `spell_cooldown_overrides` WHERE `Id` IN (37472,37471);
+INSERT INTO `spell_cooldown_overrides` (`Id`, `RecoveryTime`, `CategoryRecoveryTime`, `StartRecoveryTime`, `StartRecoveryCategory`) VALUES
+(37472, 15000, 15000, 0, 0),
+(37471, 15000, 15000, 0, 0);
