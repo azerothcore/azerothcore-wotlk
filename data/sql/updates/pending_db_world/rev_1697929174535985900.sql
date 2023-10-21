df7a12526fb9efdf7aaeaf5b8343c2c69b4958f4
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2023-10-21 20:00:22 -0300

    fix(Scripts/Karazhan): Script Aran's Flame Wreath ability

---
+++ data/sql/updates/pending_db_world/rev_1697929174535985900.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (30004, 29946);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(30004, 'spell_flamewreath'),
+(29946, 'spell_flamewreath_aura');
