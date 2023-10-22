945d7c6afef52bfdc677d3b06b1b4c27e5d86ef1
podmepodme <57728656+podmepodme@users.noreply.github.com>
2023-10-22 03:16:51 +0200

    fix(Script/Misc): Replace hardcoded gossip and texts with DB ones. (#17502)
    * fix(CORE/Creature): replace shadowmoon valley hardcoded gossips
    
    * fix(CORE/GameObject): replace terokkar forrest hardcoded gossips
    
    * fix(Core/Creature): replace shattrath city hardcoded gossips
    
    * style(Core): code style edit
    
    * style(CORE) codestyle edit
    
    * style(CORE) codestyle edit
    
    * style(CORE): codestyle edit
    
    * ready for merge
    
    ---------
    
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- src/server/scripts/Outland/zone_shadowmoon_valley.cpp
+++ src/server/scripts/Outland/zone_shadowmoon_valley.cpp
@@ -731,8 +731,19 @@ public:
 ## npc_flanis_swiftwing_and_kagrosh
 ######*/
 
-#define GOSSIP_HSK1 "Take Flanis's Pack"
-#define GOSSIP_HSK2 "Take Kagrosh's Pack"
+enum Flanis : uint32
+{
+    QUEST_THE_FATE_OF_FLANIS    = 10583,
+    ITEM_FLAUNISS_PACK          = 30658,
+    GOSSIP_MENU_FLANIS          = 8356,
+};
+
+enum Kagrosh : uint32
+{
+    QUEST_THE_FATE_OF_KAGROSH   = 10601,
+    ITEM_KAGROSHS_PACK          = 30659,
+    GOSSIP_MENU_KAGROSH         = 8371,
+};
 
 class npcs_flanis_swiftwing_and_kagrosh : public CreatureScript
 {
@@ -745,32 +756,33 @@ public:
         if (action == GOSSIP_ACTION_INFO_DEF + 1)
         {
             ItemPosCountVec dest;
-            uint8 msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, 30658, 1, nullptr);
+            uint8 msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, ITEM_FLAUNISS_PACK, 1, nullptr);
             if (msg == EQUIP_ERR_OK)
             {
-                player->StoreNewItem(dest, 30658, true);
-                ClearGossipMenuFor(player);
+                player->StoreNewItem(dest, ITEM_FLAUNISS_PACK, true);
             }
         }
         if (action == GOSSIP_ACTION_INFO_DEF + 2)
         {
             ItemPosCountVec dest;
-            uint8 msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, 30659, 1, nullptr);
+            uint8 msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, ITEM_KAGROSHS_PACK, 1, nullptr);
             if (msg == EQUIP_ERR_OK)
             {
-                player->StoreNewItem(dest, 30659, true);
-                ClearGossipMenuFor(player);
+                player->StoreNewItem(dest, ITEM_KAGROSHS_PACK, true);
             }
         }
+
+        CloseGossipMenuFor(player);
+
         return true;
     }
 
     bool OnGossipHello(Player* player, Creature* creature) override
     {
-        if (player->GetQuestStatus(10583) == QUEST_STATUS_INCOMPLETE && !player->HasItemCount(30658, 1, true))
-            AddGossipItemFor(player, 0, GOSSIP_HSK1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-        if (player->GetQuestStatus(10601) == QUEST_STATUS_INCOMPLETE && !player->HasItemCount(30659, 1, true))
-            AddGossipItemFor(player, 0, GOSSIP_HSK2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+        if (player->GetQuestStatus(QUEST_THE_FATE_OF_FLANIS) == QUEST_STATUS_INCOMPLETE && !player->HasItemCount(ITEM_FLAUNISS_PACK, 1, true))
+            AddGossipItemFor(player, GOSSIP_MENU_FLANIS, 0, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+        if (player->GetQuestStatus(QUEST_THE_FATE_OF_KAGROSH) == QUEST_STATUS_INCOMPLETE && !player->HasItemCount(ITEM_KAGROSHS_PACK, 1, true))
+            AddGossipItemFor(player, GOSSIP_MENU_KAGROSH, 0, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
 
         SendGossipMenuFor(player, player->GetGossipTextId(creature), creature->GetGUID());
 
