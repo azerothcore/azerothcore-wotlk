d3c886c64fd0cd42bcfc86dc501995607c656408
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-25 08:22:38 +0300

    remove absorb, wait for test on recycled

--- data/sql/updates/pending_db_world/rev_1698183186698472700.sql
+++ data/sql/updates/pending_db_world/rev_1698183186698472700.sql
@@ -1,4 +1,4 @@
 -- Darkmoon Card: Vengeance
 DELETE FROM `spell_proc_event` WHERE `entry`=39444;
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(39444, 0, 0, 0, 0, 0, 0, 9283, 0, 0, 0, 0);
+(39444, 0, 0, 0, 0, 0, 0, 8259, 0, 0, 0, 0);
