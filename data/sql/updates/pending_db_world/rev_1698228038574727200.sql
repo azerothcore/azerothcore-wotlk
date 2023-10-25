a275ef4477c5148a09f642ef4f6ed36905f0af07
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-25 16:35:59 +0300

    immune to coffee

--- data/sql/updates/pending_db_world/rev_1698228038574727200.sql
+++ data/sql/updates/pending_db_world/rev_1698228038574727200.sql
@@ -1,7 +1,6 @@
 -- Skullflame Shield / Demon Forged Breastplate proc on critical/normal/partial block
-DELETE FROM `spell_proc_event` WHERE `entry`=16611; -- Demon Forged Breastplate
-DELETE FROM `spell_proc_event` WHERE `entry`=18815; -- Drain Life
-DELETE FROM `spell_proc_event` WHERE `entry`=18816; -- Flamestrike
+-- Demon Forged Breastplate, Drain Life, Flamestrike
+DELETE FROM `spell_proc_event` WHERE `entry` IN (16611,18815,18816);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
 (16611, 0, 0, 0, 0, 0, 0, 67, 0, 0, 0, 0),
 (18815, 0, 0, 0, 0, 0, 0, 67, 0, 0, 0, 0),
