74159076a50122f7be88c5caaf22a984d71cd374
avarishd <46330494+avarishd@users.noreply.github.com>
2023-10-29 21:17:58 +0200

    index

--- data/sql/updates/pending_db_world/rev_1698326815078104700.sql
+++ data/sql/updates/pending_db_world/rev_1698326815078104700.sql
@@ -1,2 +1,2 @@
 -- Fire Nova Totem VII - Cast Fire Nova (Rank 7)
-UPDATE `creature_template_spell` SET `Spell` = 25537, `VerifiedBuild` = 42328 WHERE `CreatureID` = 15483;
+UPDATE `creature_template_spell` SET `Spell` = 25537, `VerifiedBuild` = 42328 WHERE `CreatureID` = 15483 AND `Index` = 0;
