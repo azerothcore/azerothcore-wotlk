9674bccc524867ddd6afe5df9289d690fb54d41c
Tim Dillon <tim@itsdits.com>
2023-11-05 06:20:19 +0900

    fix(DB/Quest): handle gender in text

--- data/sql/updates/pending_db_world/rev_1698965454224484600.sql
+++ data/sql/updates/pending_db_world/rev_1698965454224484600.sql
@@ -1,3 +1,3 @@
 --
-INSERT INTO `acore_world`.`quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES (3446, 'esMX', '¡Sí, sí, sí! ¡Justo el $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
-INSERT INTO `acore_world`.`quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES (3446, 'esES', '¡Sí, sí, sí! ¡Justo el $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
+INSERT INTO `acore_world`.`quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES (3446, 'esMX', '¡Sí, sí, sí! ¡Justo $gel:la; $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
+INSERT INTO `acore_world`.`quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES (3446, 'esES', '¡Sí, sí, sí! ¡Justo $gel:la; $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
