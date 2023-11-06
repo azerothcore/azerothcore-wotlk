60c5f481806cc0907b1e355b854c5bf7ed03d68b
KJack <kgithub@kellerg.com>
2023-11-06 17:39:53 -0500

    Move condition/linked respawn logic to `Respawn()` function

--- src/server/game/Entities/Creature/CreatureGroups.cpp
+++ src/server/game/Entities/Creature/CreatureGroups.cpp
@@ -326,22 +326,7 @@ void CreatureGroup::MemberEvaded(Creature* member)
                 continue;
             }
 
-            // make invisible temporarily
-            pMember->SetVisible(false);
-
-            // get the components of the home position
-            float x, y, z, o;
-            pMember->GetHomePosition().GetPosition(x, y, z, o);
-
-            // teleport home
-            pMember->NearTeleportTo(x, y, z, o);
-
-            // schedule an immediate respawn (if all checks in Creature::Update pass)
-            pMember->SetRespawnTime(0);
-            pMember->setDeathState(DeathState::Dead);
-
-            // restore visibility
-            pMember->SetVisible(true);
+            pMember->Respawn();
         }
     }
 }
