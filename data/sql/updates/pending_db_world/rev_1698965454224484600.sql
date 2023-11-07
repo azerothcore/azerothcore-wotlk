3d42c92a1817741dccb57b02a05b0991787b29eb
Tim Dillon <tim@itsdits.com>
2023-11-08 07:37:43 +0900

    fix(DB/Quest): format of sql

--- data/sql/updates/pending_db_world/rev_1698965454224484600.sql
+++ data/sql/updates/pending_db_world/rev_1698965454224484600.sql
@@ -1,3 +1,4 @@
---
-INSERT INTO `acore_world`.`quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES (3446, 'esMX', '¡Sí, sí, sí! ¡Justo $gel:la; $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
-INSERT INTO `acore_world`.`quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES (3446, 'esES', '¡Sí, sí, sí! ¡Justo $gel:la; $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
+-- Mebok Mizzyrix
+INSERT INTO `quest_greeting_locale` (`ID`, `locale`, `Greeting`) VALUES
+(3446, 'esMX', '¡Sí, sí, sí! ¡Justo $gel:la; $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.'),
+(3446, 'esES', '¡Sí, sí, sí! ¡Justo $gel:la; $r que estaba buscando!$B$B¡Siéntate! Tenemos mucho que hablar.');
