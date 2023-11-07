3e1620596eb0767f34fc8a35a789869a251541ad
Ludwig <sudlud@users.noreply.github.com>
2023-11-07 21:30:38 +0100

    SpellInfoCorrections: fix target of spell 46747 Fling torch

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4602,6 +4602,12 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->Effects[EFFECT_1].SpellClassMask[0] = 6;
     });
 
+    // 46747 Fling torch
+    ApplySpellFix({ 46747 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->Effects[EFFECT_0].TargetA = SpellImplicitTargetInfo(TARGET_DEST_CASTER);
+    });
+    
     for (uint32 i = 0; i < GetSpellInfoStoreSize(); ++i)
     {
         SpellInfo* spellInfo = mSpellInfoMap[i];
