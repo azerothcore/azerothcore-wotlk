6a8a86909b238ea956e488a1aa0cbda0677bb521
adame1181 <47040311+adame1181@users.noreply.github.com>
2023-11-10 14:21:15 +0100

    fix(DB/Gameobject): making beds/chairs unclickable

--- data/sql/updates/pending_db_world/fix-humans-emotes-in-culling-of-stratholme.sql
+++ data/sql/updates/pending_db_world/fix-humans-emotes-in-culling-of-stratholme.sql
@@ -11,3 +11,11 @@ INSERT INTO creature_addon VALUES
 -- CREATURES SLEEP:
 (1970927, 0, 0, 3, 0, 0, 0, NULL),
 (1970877, 0, 0, 3, 0, 0, 0, NULL);
+
+-- UNCLICKABLE CHAIRS:
+UPDATE `gameobject_template` SET `type` = 5, `Data0` = 0, `Data1` = 0 WHERE `entry` IN
+(187521, 187515, 187514, 187512, 187519, 187522, 187523);
+
+-- UNCLICKABLE BED:
+UPDATE `gameobject_template` SET `type` = 5, `Data0` = 0, `Data1` = 0 WHERE
+(`entry` = 187526 );
