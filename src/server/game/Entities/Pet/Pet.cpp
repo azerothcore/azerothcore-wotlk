c9c3a7cb4d00322882aa04bab8dc214dbed0b6ed
forgives <forgives@foxmail.com>
2023-11-11 20:46:56 +0800

    feat: add pet talent rate config

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -2234,6 +2234,7 @@ void Pet::InitTalentForLevel()
 uint8 Pet::GetMaxTalentPointsForLevel(uint8 level)
 {
     uint8 points = (level >= 20) ? ((level - 16) / 4) : 0;
+    points = uint8(points * sWorld->getRate(RATE_TALENT_PET));
     // Mod points from owner SPELL_AURA_MOD_PET_TALENT_POINTS
     if (Unit* owner = GetOwner())
         points += owner->GetTotalAuraModifier(SPELL_AURA_MOD_PET_TALENT_POINTS);
