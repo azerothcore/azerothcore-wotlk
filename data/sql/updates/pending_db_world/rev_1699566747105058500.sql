7f25b4d983aed85ba3bd452e73c2a88dd4c17ba9
fangshun <fangshun2004@msn.com>
2023-11-12 12:10:58 +0800

    fix "Grand Crusader Dathrohan" Unable to speak when transforming and dying and the worldserver console error

--- data/sql/updates/pending_db_world/rev_1699566747105058500.sql
+++ data/sql/updates/pending_db_world/rev_1699566747105058500.sql
@@ -96,7 +96,25 @@ UPDATE `creature_text` SET `BroadcastTextId`=2625 WHERE `CreatureID`=11043 AND `
 UPDATE `creature_text` SET `BroadcastTextId`=6380 WHERE `CreatureID`=11043 AND `GroupID`=1 AND `ID`=0;
 
 -- Grand Crusader Dathrohan
-UPDATE `creature_text` SET `BroadcastTextId`=6441 WHERE `CreatureID`=10812 AND `GroupID`=0 AND `ID`=0;
+DELETE FROM `creature_text` WHERE `CreatureID`=10812;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
+(10812, 0, 0, 'Today you have unmade what took me years to create! For this you shall all die by my hand!', 14, 0, 100, 0, 0, 0, 6441, 0, 'Grand Crusader Dathrohan'),
+
+-- This text must be inserted, otherwise the server will start with an error
+(10812, 1, 0, 'Damn you mortals! All my plans of revenge, all my hate...I will be avenged...', 12, 0, 100, 0, 0, 0, 6442, 0, 'Grand Crusader Dathrohan');
+
+DELETE FROM `creature_text` WHERE `CreatureID`=10813;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(10813, 0, 0, 'You fools think you can defeat me so easily? Face the true might of the Nathrezim!', 14, 0, 100, 0, 0, 0, 6447, 0, 'Grand Crusader Dathrohan'),
+(10813, 1, 0, 'Damn you mortals! All my plans of revenge, all my hate...I will be avenged...', 12, 0, 100, 0, 0, 0, 6442, 0, 'Grand Crusader Dathrohan');
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 10812;
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 10812) AND (`source_type` = 0) AND (`id` IN (9,15));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(10812, 0, 9, 0, 0, 2, 100, 1, 1000, 1000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Grand Crusader Dathrohan - In Combat - Balnazzar Say Line 0'),
+(10812, 0, 15, 16, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Grand Crusader Dathrohan - On Death - Say Line 1');
+
+
 
 -- Ravaged Cadaver
 UPDATE `creature_text` SET `BroadcastTextId`=5772 WHERE `CreatureID`=10381 AND `GroupID`=0 AND `ID`=0;
