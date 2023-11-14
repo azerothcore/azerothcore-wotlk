dd267aeebf44f88e4baa16c31413bcfc8ff36f99
avarishd <46330494+avarishd@users.noreply.github.com>
2023-11-14 18:13:18 +0200

    fix(DB/Creature): Port Underbog Colossus immunities from Mangos

---
+++ data/sql/updates/pending_db_world/rev_1699978192145266700.sql
@@ -0,0 +1,2 @@
+-- Underbog Colossus
+UPDATE `creature_template` SET `unit_flags` = `unit_flags`|32832, `mechanic_immune_mask` = `mechanic_immune_mask`|42090322, `detection_range` = 30 WHERE (`entry` = 21251);
