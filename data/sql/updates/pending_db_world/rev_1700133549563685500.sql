e42e8ee7da10d57367508904b3438faec5f1d8e4
avarishd <46330494+avarishd@users.noreply.github.com>
2023-11-16 13:47:43 +0200

    index

--- data/sql/updates/pending_db_world/rev_1700133549563685500.sql
+++ data/sql/updates/pending_db_world/rev_1700133549563685500.sql
@@ -3,5 +3,5 @@ UPDATE `creature_template_addon` SET `auras` = '35408' WHERE `entry` = 22204;
 
 DELETE FROM `creature_template_spell` WHERE `CreatureID` = 22204;
 INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
-(22204, 0, 36248, 0),
-(22204, 1, 34259, 0);
+(22204, 4, 36248, 0),
+(22204, 5, 34259, 0);
