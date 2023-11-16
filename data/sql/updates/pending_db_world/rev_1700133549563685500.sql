176b94c0f5abdec4c839b7ae855ea63d5faf0e54
avarishd <46330494+avarishd@users.noreply.github.com>
2023-11-16 13:24:36 +0200

    creature_template_spell

--- data/sql/updates/pending_db_world/rev_1700133549563685500.sql
+++ data/sql/updates/pending_db_world/rev_1700133549563685500.sql
@@ -1,2 +1,7 @@
 -- Fear Fiend
 UPDATE `creature_template_addon` SET `auras` = '35408' WHERE `entry` = 22204;
+
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 22204;
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(22204, 0, 36248, 0),
+(22204, 1, 34259, 0);
