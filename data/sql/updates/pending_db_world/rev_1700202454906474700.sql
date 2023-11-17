6bf68dbb5a71d1ba795400562c46410c1ff5cb4c
avarishd <46330494+avarishd@users.noreply.github.com>
2023-11-17 08:32:25 +0200

    fix(DB/Procs): Spell Bomb should proc only on cast

---
+++ data/sql/updates/pending_db_world/rev_1700202454906474700.sql
@@ -0,0 +1,4 @@
+-- SH - Spell Bomb - proc only on cast
+DELETE FROM `spell_proc_event` WHERE `entry`=40303;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(40303, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
