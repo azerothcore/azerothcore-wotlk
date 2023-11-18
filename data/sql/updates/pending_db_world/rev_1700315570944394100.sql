a1b31c96d0f51822b8f4a65a0e140e03234a7d09
fangshun <fangshun2004@msn.com>
2023-11-18 22:55:46 +0800

    Modify the sniff data again

--- data/sql/updates/pending_db_world/rev_1700315570944394100.sql
+++ data/sql/updates/pending_db_world/rev_1700315570944394100.sql
@@ -10,9 +10,9 @@ DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 626600)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (626600, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 83, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Menara Voidrender  - Remove Gossip and Quest Giver npc flags from self.'),
 (626600, 9, 1, 0, 0, 0, 100, 0, 500, 500, 0, 0, 0, 0, 11, 16633, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Menara Voidrender - Cast self - Create Item Visual'),
-(626600, 9, 2, 0, 0, 0, 100, 0, 6500, 6500, 0, 0, 0, 0, 15, 4975, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Menara Voidrender - On Script - Quest Credit \'The Completed Orb of Noh\'Orahil\''),
+(626600, 9, 2, 0, 0, 0, 100, 0, 7000, 7000, 0, 0, 0, 0, 15, 4975, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Menara Voidrender - On Script - Quest Credit \'The Completed Orb of Noh\'Orahil\''),
 (626600, 9, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 15, 4964, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Menara Voidrender - On Script - Quest Credit \'The Completed Orb of Dar\'Orahil\''),
-(626600, 9, 4, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 82, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Add Gossip and Quest Giver npc flags to self.');
+(626600, 9, 4, 0, 0, 0, 100, 0, 500, 500, 0, 0, 0, 0, 82, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Menara Voidrender - Add Gossip and Quest Giver npc flags to self.');
 
 
 
