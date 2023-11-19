b6247c90f73ec83ebe159628cb3e00f07db6adbd
avarishd <46330494+avarishd@users.noreply.github.com>
2023-11-19 14:11:39 +0200

    fix(DB/Spells): Static Shock procs

---
+++ data/sql/updates/pending_db_world/rev_1700395742200811100.sql
@@ -0,0 +1,4 @@
+-- Static Shock
+DELETE FROM `spell_proc_event` WHERE `entry`=-51525;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(-51525, 0, 0, 0, 0, 0, 12582912, 0, 0, 0, 0, 0);
