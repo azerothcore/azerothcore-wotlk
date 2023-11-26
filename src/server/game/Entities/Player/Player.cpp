f440a7479f75dfb60e2a09c81471791579aac58c
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-11-26 20:34:45 +0100

    fix(Core/Groups): Create the group at first invite (#17869)
    * cherry-pick commit (https://github.com/TrinityCore/TrinityCore/commit/132538db1dfb3c495294eeb5acfead59dea02b5c)
    * cherry-pick commit (https://github.com/TrinityCore/TrinityCore/commit/250e8d998bca7f239e2393b6d002441f70d4daf0)
    * part https://github.com/azerothcore/azerothcore-wotlk/issues/11070
    
    Co-authored-by: Treeston <14020072+Treeston@users.noreply.github.com>

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2298,7 +2298,6 @@ bool Player::IsInSameGroupWith(Player const* p) const
 }
 
 ///- If the player is invited, remove him. If the group if then only 1 person, disband the group.
-/// \todo Shouldn't we also check if there is no other invitees before disbanding the group?
 void Player::UninviteFromGroup()
 {
     Group* group = GetGroupInvite();
@@ -2307,14 +2306,17 @@ void Player::UninviteFromGroup()
 
     group->RemoveInvite(this);
 
-    if (group->GetMembersCount() <= 1)                       // group has just 1 member => disband
+    if (group->IsCreated())
     {
-        if (group->IsCreated())
+        if (group->GetMembersCount() <= 1)                       // group has just 1 member => disband
         {
             group->Disband(true);
             group = nullptr; // gets deleted in disband
         }
-        else
+    }
+    else
+    {
+        if (group->GetInviteeCount() <= 1)
         {
             group->RemoveAllInvites();
             delete group;
