0905766387a4ba21ec0d2f4eae27553068418d9a
AG <43139552+AGandrup@users.noreply.github.com>
2023-11-28 19:59:09 +0100

    Fix infinite loop in npc_air_force_bots
    Cherry-pick: https://github.com/TrinityCore/TrinityCore/commit/844e7ee9262a5ba035ac9e8a6ffc298fb97fd3a9
    Cherry-pick: https://github.com/TrinityCore/TrinityCore/commit/e840d4967eae376d1bb888ae471056d05c2f0acc (because why not)
    Co-Authored-By: Giacomo Pozzoni <giacomopoz@gmail.com>
    Co-Authored-By: Joshua L <9509069+langerz82@users.noreply.github.com>

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -460,11 +460,11 @@ struct SpawnAssociation
 enum AirFoceBots
 {
     SPELL_GUARDS_MARK               = 38067,
-    AURA_DURATION_TIME_LEFT         = 5000
+    AURA_DURATION_TIME_LEFT         = 30000
 };
 
 float const RANGE_TRIPWIRE          = 15.0f;
-float const RANGE_GUARDS_MARK       = 50.0f;
+float const RANGE_GUARDS_MARK       = 100.0f;
 
 SpawnAssociation spawnAssociations[] =
 {
@@ -566,6 +566,13 @@ public:
             return nullptr;
         }
 
+        void UpdateAI(uint32 diff) override
+        {
+            ScriptedAI::UpdateAI(diff);
+
+            inLineOfSightSinceLastUpdate.clear();
+        }
+
         void MoveInLineOfSight(Unit* who) override
 
         {
@@ -590,6 +597,10 @@ public:
                 {
                     case SPAWNTYPE_ALARMBOT:
                         {
+                            // handle only 1 change for world update for each target
+                            if (!inLineOfSightSinceLastUpdate.insert(who->GetGUID()).second)
+                                return;
+
                             if (!who->IsWithinDistInMap(me, RANGE_GUARDS_MARK))
                                 return;
 
@@ -641,6 +652,7 @@ public:
                 }
             }
         }
+        GuidSet inLineOfSightSinceLastUpdate;
     };
 
     CreatureAI* GetAI(Creature* creature) const override
