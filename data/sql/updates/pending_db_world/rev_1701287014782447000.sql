6cf90dcda02d726ee3f73a232629983ffb5811ef
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-11-29 20:55:13 +0100

    Update rev_1701287014782447000.sql
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/17897

--- data/sql/updates/pending_db_world/rev_1701287014782447000.sql
+++ data/sql/updates/pending_db_world/rev_1701287014782447000.sql
@@ -1,2 +1,5 @@
---
+-- Bombing Run (11010) should not be available to druids
 UPDATE `quest_template_addon` SET `AllowableClasses`=`AllowableClasses`|1|2|4|8|16|32|64|128|256 WHERE `ID` = 11010;
+
+-- Banish More Demons should not be available without honoured reputation
+UPDATE `quest_template_addon` SET `RequiredMinRepFaction` = 1030, `RequiredMinRepValue` = 9000 WHERE `ID` = 11026;
