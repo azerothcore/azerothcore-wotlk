3f37456f1d41a0db0bae45801c6f90e72ce19292
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-12-01 01:09:25 +0100

    build?

--- data/sql/updates/pending_db_world/rev_1701389243654647400.sql
+++ data/sql/updates/pending_db_world/rev_1701389243654647400.sql
@@ -1,2 +1,3 @@
 --
 UPDATE `command` SET `help`='Syntax: .additem Optional(playerName/playerGUID) #itemID/[#itemName]/#itemLink #itemCount\r\nAdds the specified item to you, the selected character or the specifed character name/GUID.\r\nIf #itemCount is negative, you will remove #itemID.' WHERE `name`='additem';
+
