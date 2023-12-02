b90274d851db4e044a8b3f4128992de78be78d49
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2023-12-02 20:05:58 +0100

    Update rev_1701381015172273800.sql

--- data/sql/updates/pending_db_world/rev_1701381015172273800.sql
+++ data/sql/updates/pending_db_world/rev_1701381015172273800.sql
@@ -1,6 +1,6 @@
 --
 DELETE FROM `command` WHERE `name`='go creature name';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('go creature name', 1, 'Syntax: .go creature name [name]\r\nTeleports you to the first spawn for the given creature name.\r\n*If* more than one creature is found, then you are teleported to the first that is found inside the database.');
+('go creature name', 1, 'Syntax: .go creature name #name\r\nTeleports you to the first spawn for the given creature name.\r\n*If* more than one creature is found, then you are teleported to the first that is found inside the database.');
 
 UPDATE `command` SET `help` = 'Syntax: .go creature id #creature_entry\r\nTeleports you to the given creature entry.\r\n*If* more than one creature is found, then you are teleported to the first that is found inside the database.' WHERE `name` = 'go creature id';
