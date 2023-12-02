c9144f12dfa02943eae726f9ea29397723613a44
Gultask <100873791+Gultask@users.noreply.github.com>
2023-12-02 11:15:58 -0300

    Update ssc.sql

--- data/sql/updates/pending_db_world/ssc.sql
+++ data/sql/updates/pending_db_world/ssc.sql
@@ -1489,3 +1489,13 @@ INSERT INTO `creature_formations` (`memberGUID`, `leaderGUID`, `groupAI`) VALUES
 
 UPDATE `smart_scripts` SET `action_param2`=0 WHERE `entryorguid`=21230 AND `source_type`=0 AND `id`=1 AND `link`=0;
 UPDATE `smart_scripts` SET `action_param2`=0 WHERE `entryorguid`=21230 AND `source_type`=0 AND `id`=5 AND `link`=0;
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 21301) AND (`source_type` = 0) AND (`id` IN (3, 4));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(21301, 0, 3, 0, 11, 0, 100, 512, 0, 0, 0, 0, 0, 0, 34, 20, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Shatterer - On Respawn - Set Instance Data 20 to 1'),
+(21301, 0, 4, 0, 6, 0, 100, 512, 0, 0, 0, 0, 0, 0, 34, 21, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Shatterer - On Just Died - Set Instance Data 21 to 1');
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 21220) AND (`source_type` = 0) AND (`id` IN (5, 6));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(21220, 0, 5, 0, 11, 0, 100, 512, 0, 0, 0, 0, 0, 0, 34, 20, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Priestess - On Respawn - Set Instance Data 20 to 1'),
+(21220, 0, 6, 0, 6, 0, 100, 512, 0, 0, 0, 0, 0, 0, 34, 21, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Coilfang Priestess - On Just Died - Set Instance Data 21 to 1');
