80b2ed0b7fac4fa418f107900185856767e202ce
The GhostRider <xspidey@hotmail.it>
2023-12-02 18:51:04 +0000

    Update rev_1701536212461529900.sql

--- data/sql/updates/pending_db_world/rev_1701536212461529900.sql
+++ data/sql/updates/pending_db_world/rev_1701536212461529900.sql
@@ -1,4 +1,4 @@
---Fix loot Prospecting Adamantite Ore
+-- Fix loot Prospecting Adamantite Ore --
 
 DELETE FROM `reference_loot_template` WHERE (`Entry` = 13003);
 INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
