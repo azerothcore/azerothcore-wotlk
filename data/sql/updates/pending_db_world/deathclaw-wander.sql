24693cd55937545d82d653df8c06db99a708df95
Dan <83884799+elthehablo@users.noreply.github.com>
2023-12-03 11:51:29 +0100

    revised wander distances after sniff review

--- data/sql/updates/pending_db_world/deathclaw-wander.sql
+++ data/sql/updates/pending_db_world/deathclaw-wander.sql
@@ -4,7 +4,7 @@ UPDATE `creature` SET
     `position_y` = -11284.973,
     `position_z` = 8.427056,
     `orientation` = 1.963490843772888183,
-    `wander_distance` = 10,
+    `wander_distance` = 30,
     `MovementType` = 1,
     `VerifiedBuild` = 52237
 WHERE `guid` = 63385 AND `id1` = 17661;
