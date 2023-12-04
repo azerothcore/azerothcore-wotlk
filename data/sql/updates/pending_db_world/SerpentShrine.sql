f3ed2cd10acf7fd8234d486e3d6f6e7b36725078
天鹿 <pkl_long@163.com>
2023-12-05 00:38:51 +0800

    Update SerpentShrine.sql

--- data/sql/updates/pending_db_world/SerpentShrine.sql
+++ data/sql/updates/pending_db_world/SerpentShrine.sql
@@ -1,6 +1,5 @@
--- 21215
 SET @CGUID = 153000;
-
+-- 21215
 DELETE FROM `creature_addon` WHERE `guid` = @CGUID+139;
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `auras`) VALUES
 (@CGUID+139, 0, 0, 8, 1, '37546');
@@ -13,8 +12,6 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 (@CGUID+139, @CGUID+142, 0, 0, 24, 0, 0);
 
 -- 21216
-SET @CGUID = 153000;
-
 UPDATE `creature` SET `position_x`= -259.13, `position_y`= -356.28, `position_z`= 15.03, `orientation`=6.04 WHERE `guid` = @CGUID+21;
 UPDATE `creature` SET `position_x`= -218.8, `position_y`= -371.34, `position_z`= 14.608, `orientation`=2.72 WHERE `guid` = @CGUID+20;
 UPDATE `creature` SET `position_x`= -239.43, `position_y`  -363.48, `position_z`= 12, `orientation`=1.19093 WHERE `guid` = @CGUID+19;
