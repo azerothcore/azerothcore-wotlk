7020ebfb8a5c02249101aa0c408298345b86cdef
天鹿 <pkl_long@163.com>
2023-12-05 07:56:32 +0800

    Add files via upload

--- data/sql/updates/pending_db_world/SerpentShrine.sql
+++ data/sql/updates/pending_db_world/SerpentShrine.sql
@@ -1,5 +1,5 @@
 SET @CGUID = 153000;
--- 21215
+-- 21215 Leotheras the Blind
 DELETE FROM `creature_addon` WHERE `guid` = @CGUID+139;
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `auras`) VALUES
 (@CGUID+139, 0, 0, 8, 1, '37546');
@@ -11,7 +11,7 @@ INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, 
 (@CGUID+139, @CGUID+141, 0, 0, 24, 0, 0),
 (@CGUID+139, @CGUID+142, 0, 0, 24, 0, 0);
 
--- 21216
+-- 21216 Hydross the Unstable
 UPDATE `creature` SET `position_x`= -259.13, `position_y`= -356.28, `position_z`= 15.03, `orientation`=6.04 WHERE `guid` = @CGUID+21;
 UPDATE `creature` SET `position_x`= -218.8, `position_y`= -371.34, `position_z`= 14.608, `orientation`=2.72 WHERE `guid` = @CGUID+20;
 UPDATE `creature` SET `position_x`= -239.43, `position_y`= -363.48, `position_z`= 12, `orientation`=1.19093 WHERE `guid` = @CGUID+19;
