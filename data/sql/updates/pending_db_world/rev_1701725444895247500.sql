79ba1a3d7abcaadb8138e0df419d73671e69848d
The GhostRider <xspidey@hotmail.it>
2023-12-04 22:03:26 +0000

    Update rev_1701725444895247500.sql

--- data/sql/updates/pending_db_world/rev_1701725444895247500.sql
+++ data/sql/updates/pending_db_world/rev_1701725444895247500.sql
@@ -1,3 +1,3 @@
 -- PACIFIED Flags for NPC 20417 Coruu Control Console --
 
-UPDATE `creature_template` SET `unit_flags` = 33685760 WHERE (`entry` = 20417);
+UPDATE `creature_template` SET `unit_flags` = `unit_flags`|131072 WHERE (`entry` = 20417);
