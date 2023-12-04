7a13b01d217a6e97f922cff946428536deec9f02
avarishd <46330494+avarishd@users.noreply.github.com>
2023-12-04 15:42:46 +0200

    sniff

--- data/sql/updates/pending_db_world/rev_1701692168915380100.sql
+++ data/sql/updates/pending_db_world/rev_1701692168915380100.sql
@@ -1,5 +1,5 @@
 -- Portal Effect: Hellfire Peninsula A/H
 DELETE FROM `spell_target_position` WHERE `id` IN (65728,65729);
 INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES
-(65728, 0, 0, -11708.4, -3168, -5.07, 2.66028, 0),
-(65729, 0, 0, -11708.4, -3168, -5.07, 4.32218, 0);
+(65728, 0, 0, -11708.4, -3168, -5.07, 3.351032, 52237),
+(65729, 0, 0, -11708.4, -3168, -5.07, 3.351032, 52237);
