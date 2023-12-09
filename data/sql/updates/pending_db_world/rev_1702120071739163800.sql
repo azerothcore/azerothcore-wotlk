61034fa02567322a20e528d090466d1ad16d0fd3
Ludwig <sudlud@users.noreply.github.com>
2023-12-09 17:14:36 +0100

    add missing ticks

--- data/sql/updates/pending_db_world/rev_1702120071739163800.sql
+++ data/sql/updates/pending_db_world/rev_1702120071739163800.sql
@@ -21,10 +21,10 @@ DELETE FROM `game_event_creature` WHERE (`guid` IN (94531, 94547));
 
 -- 16781, 'Midsummer Celebrant'
 -- update orientations (completes TODO of PR #17549)
-UPDATE `creature` SET `orientation` = 5.19 WHERE (`id1` = 16781) AND (guid = 139128);
-UPDATE `creature` SET `orientation` = 2.86 WHERE (`id1` = 16781) AND (guid = 139129);
-UPDATE `creature` SET `orientation` = 5.94 WHERE (`id1` = 16781) AND (guid = 139130);
-UPDATE `creature` SET `orientation` = 0.84 WHERE (`id1` = 16781) AND (guid = 139151);
-UPDATE `creature` SET `orientation` = 0.73 WHERE (`id1` = 16781) AND (guid = 139153);
-UPDATE `creature` SET `orientation` = 0.89 WHERE (`id1` = 16781) AND (guid = 139184);
-UPDATE `creature` SET `Comment` = "orientation corrected manually (not sniffed)" WHERE (`id1` = 16781) AND (guid IN (139128, 139129, 139130, 139151, 139153, 139184));
+UPDATE `creature` SET `orientation` = 5.19 WHERE (`id1` = 16781) AND (`guid` = 139128);
+UPDATE `creature` SET `orientation` = 2.86 WHERE (`id1` = 16781) AND (`guid` = 139129);
+UPDATE `creature` SET `orientation` = 5.94 WHERE (`id1` = 16781) AND (`guid` = 139130);
+UPDATE `creature` SET `orientation` = 0.84 WHERE (`id1` = 16781) AND (`guid` = 139151);
+UPDATE `creature` SET `orientation` = 0.73 WHERE (`id1` = 16781) AND (`guid` = 139153);
+UPDATE `creature` SET `orientation` = 0.89 WHERE (`id1` = 16781) AND (`guid` = 139184);
+UPDATE `creature` SET `Comment` = "orientation corrected manually (not sniffed)" WHERE (`id1` = 16781) AND (`guid` IN (139128, 139129, 139130, 139151, 139153, 139184));
