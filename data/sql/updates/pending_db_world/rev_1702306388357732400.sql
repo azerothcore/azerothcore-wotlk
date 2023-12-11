5948a86eece45b67e998161f8287c70ab90482c1
avarishd <46330494+avarishd@users.noreply.github.com>
2023-12-11 17:29:54 +0200

    ???

--- data/sql/updates/pending_db_world/rev_1702306388357732400.sql
+++ data/sql/updates/pending_db_world/rev_1702306388357732400.sql
@@ -1,5 +1,2 @@
 --
 UPDATE `command` SET `help`='Syntax: .wpgps\r\nOutput current position to sql developer log as partial SQL query to be used in pathing (formated for waypoint_data table).\r\nUse .wpgps sai for waypoint (SAI) table format.' WHERE `name`='wpgps';
-
-DELETE FROM `command` WHERE `name`='wpgps sai';
-INSERT INTO `command` (`name`, `security`, `help`) VALUES ('wpgps sai', 3, 'Syntax: .wpgps sai\r\n\r\nOutput current position to sql developer log as partial SQL query to be used in pathing (formated for waypoint (SAI) table).');
