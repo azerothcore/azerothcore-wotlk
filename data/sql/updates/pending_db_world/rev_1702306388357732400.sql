0ab4931bf5239c09fc0f1f6533ad2be938bd5b0e
avarishd <46330494+avarishd@users.noreply.github.com>
2023-12-12 04:54:52 +0200

    fix(Scripts/Commands): fix wpgps and add sai option for formating (#17990)
    * fix(Scripts/Commands): fix wpgps and add sai option for formating
    
    * ???

---
+++ data/sql/updates/pending_db_world/rev_1702306388357732400.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `command` SET `help`='Syntax: .wpgps\r\nOutput current position to sql developer log as partial SQL query to be used in pathing (formated for waypoint_data table).\r\nUse .wpgps sai for waypoint (SAI) table format.' WHERE `name`='wpgps';
