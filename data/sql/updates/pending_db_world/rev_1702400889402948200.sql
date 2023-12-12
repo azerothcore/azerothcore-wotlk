18af46d35754172013ad7c15e6bb48f365dbe043
Angelo Venturini <nefertum.dev@protonmail.com>
2023-12-12 18:29:05 -0300

    fix(Core/SSC): Rancid Mushroom mechanic (#18008)
    * fix(Core/SSC): Rancid Mushroom mechanic
    
    * missing clear smart_scripts
    
    * build
    
    * build again

---
+++ data/sql/updates/pending_db_world/rev_1702400889402948200.sql
@@ -0,0 +1,8 @@
+--
+UPDATE `creature_template` SET `scale` = 0.5, `unit_flags` = `unit_flags`|2048|524288, `mechanic_immune_mask` = 1073741823, `AIName` = '', `ScriptName` = 'npc_rancid_mushroom' WHERE `entry` = 22250;
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = 38652 AND `ScriptName` = 'spell_rancid_spore_cloud';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(38652, 'spell_rancid_spore_cloud');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 22250 AND `source_type` = 0;
