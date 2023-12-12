e4f9dc4a8500ca582973002faa6978d93e42d2af
Angelo Venturini <nefertum.dev@protonmail.com>
2023-12-11 23:33:14 -0300

    fix(DB/Loot): Lady Vashj non-set items (#17996)

---
+++ data/sql/updates/pending_db_world/rev_1702340603344359300.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 21212) AND (`Item` IN (90062));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(21212, 90062, 34062, 100, 0, 1, 4, 1, 2, 'Lady Vashj - (ReferenceTable)');
