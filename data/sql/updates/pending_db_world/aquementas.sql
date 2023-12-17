0ace68c995ffa800099b1fb05c137f3017309214
Dan <83884799+elthehablo@users.noreply.github.com>
2023-12-17 23:41:02 +0100

    consistency

--- data/sql/updates/pending_db_world/aquementas.sql
+++ data/sql/updates/pending_db_world/aquementas.sql
@@ -15,10 +15,10 @@ INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `positi
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 9453 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(9453, 0, 0, 0, 0, 0, 100, 0, 10200, 10200, 12600, 15840, 0, 0, 11, 15089, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - IC - cast spell \'Frost Shock\''),
-(9453, 0, 1, 0, 0, 0, 100, 0, 11800, 11800, 29300, 30480, 0, 0, 11, 13586, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - IC - cast spell \'Aqua Jet\''),
-(9453, 0, 2, 3, 54, 0, 100, 513, 0, 0, 0, 0, 0, 0, 11, 7398, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - cast spell \'Birth\''),
-(9453, 0, 3, 4, 54, 0, 100, 513, 0, 0, 0, 0, 0, 0, 117, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - disable evade'),
+(9453, 0, 0, 0, 0, 0, 100, 0, 10200, 10200, 12600, 15840, 0, 0, 11, 15089, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - IC - Cast spell \'Frost Shock\''),
+(9453, 0, 1, 0, 0, 0, 100, 0, 11800, 11800, 29300, 30480, 0, 0, 11, 13586, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - IC - Cast spell \'Aqua Jet\''),
+(9453, 0, 2, 3, 54, 0, 100, 513, 0, 0, 0, 0, 0, 0, 11, 7398, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - Cast spell \'Birth\''),
+(9453, 0, 3, 4, 61, 0, 100, 513, 0, 0, 0, 0, 0, 0, 117, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - Disable evade'),
 (9453, 0, 4, 5, 61, 0, 100, 513, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - Say 0'),
 (9453, 0, 5, 6, 61, 0, 100, 513, 0, 0, 0, 0, 0, 0, 2, 35, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - Set faction friendly'),
 (9453, 0, 6, 0, 61, 0, 100, 513, 0, 0, 0, 0, 0, 0, 67, 1, 3000, 3000, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Aquementas - Just Summoned - Do timed event'),
