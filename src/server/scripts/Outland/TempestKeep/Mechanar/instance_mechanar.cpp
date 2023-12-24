79a2bb84e932a23de84b0d1f2bbe3b5d86947527
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2023-12-24 16:07:41 +0330

    chore(Scripts/Outland): Remove useless includes (#18049)

--- src/server/scripts/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
+++ src/server/scripts/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
@@ -15,7 +15,6 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "CreatureScript.h"
 #include "InstanceMapScript.h"
 #include "InstanceScript.h"
 #include "mechanar.h"
