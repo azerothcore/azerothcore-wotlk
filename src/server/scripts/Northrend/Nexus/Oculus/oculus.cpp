4ec284577e09caf25e927130a8cadb7375843148
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2023-12-25 08:45:56 +0330

    chore(Scripts/Northrend): remove useless includes (#18061)

--- src/server/scripts/Northrend/Nexus/Oculus/oculus.cpp
+++ src/server/scripts/Northrend/Nexus/Oculus/oculus.cpp
@@ -18,7 +18,6 @@
 #include "CombatAI.h"
 #include "CreatureScript.h"
 #include "InstanceScript.h"
-#include "MotionMaster.h"
 #include "ObjectAccessor.h"
 #include "Player.h"
 #include "ScriptedCreature.h"
