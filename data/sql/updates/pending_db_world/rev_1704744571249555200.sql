6f1819ae0b9ed56c2f023532e8faf2ca433cb2dd
Ludwig <sudlud@users.noreply.github.com>
2024-01-08 21:11:44 +0100

    fix(DB/Gameobject): Sniffed Values for 180665 'Draconic for Dummies'

---
+++ data/sql/updates/pending_db_world/rev_1704744571249555200.sql
@@ -0,0 +1,4 @@
+-- Update gameobject 180665 'Draconic for Dummies' with sniffed values
+DELETE FROM `gameobject` WHERE (`id` = 180665) AND (`guid` IN (268645));
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
+(268645, 180665, 0, 0, 0, 1, 1, -8340.1962890625, 413.749908447265625, 124.4896774291992187, 2.827429771423339843, 0, 0, 0.987688064575195312, 0.156436234712600708, 120, 255, 1, "", 50664, NULL);
