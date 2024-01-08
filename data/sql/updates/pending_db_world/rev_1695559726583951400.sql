ff9c2a132219d80c6854a83a3001d3869dec7b16
Alan <sr.alansantos@hotmail.com>
2024-01-08 20:56:25 -0300

    improvements added

--- data/sql/updates/pending_db_world/rev_1695559726583951400.sql
+++ data/sql/updates/pending_db_world/rev_1695559726583951400.sql
@@ -5,3 +5,11 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 
 -- both missions are for the horde and alliance factions
 UPDATE `spell_script_names` SET `ScriptName`='spell_catch_the_wild_wolpertinger' WHERE  `spell_id`=41621 AND `ScriptName`='spell_q11117_catch_the_wild_wolpertinger';
+
+-- doesn't match the spell
+DELETE FROM `spell_linked_spell` WHERE  `spell_trigger`=41621 AND `spell_effect`=63726 AND `type`=2;
+DELETE FROM `spell_linked_spell` WHERE  `spell_trigger`=41621 AND `spell_effect`=52656 AND `type`=2;
+
+UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_wild_wolpertinger' WHERE (`entry` = 23487);
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 23487) AND (`source_type` = 0);
