2b65db57a3652a8f79739fffffd0e1211a6d92d7
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-01-20 16:21:07 +0100

    revert some stuff

--- data/sql/updates/db_auth/2024_01_20_01.sql
+++
@@ -1,8 +0,0 @@
---
--- 1. First we need to delete the existing archived updates
--- 2. We move the old archives from /sql/archive/ to the /sql/old/ directory
--- 3. We move the /sql/updates/ files into /sql/archive/ directory
--- 4. We update the current RELEASED updates to ARCHIVED as they change directory
-
--- DELETE FROM `updates` WHERE `state`='ARCHIVED'; -- In the future the old version archive should be moved in the squash commit. This time it was done here ea93531bd546867e25979e183dcf1ab25c388605
-UPDATE `updates` SET `state`='ARCHIVED' WHERE `state`='RELEASED';
