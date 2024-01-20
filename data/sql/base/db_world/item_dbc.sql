ea6f0a8e8d6dd4c4aaf88e60b4a6578365ba4347
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-01-20 23:18:32 +0100

    fix(DB): Collation error
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/18202

--- data/sql/base/db_world/item_dbc.sql
+++ data/sql/base/db_world/item_dbc.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `item_dbc` (
   `InventoryType` int NOT NULL DEFAULT '0',
   `SheatheType` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table acore_world.item_dbc: ~0 rows (approximately)
 DELETE FROM `item_dbc`;
