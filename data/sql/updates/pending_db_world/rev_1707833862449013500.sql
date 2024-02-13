b6b2d0cac1060ef1b2d7a84c15f4ac1050d62adf
kasvati <96998760+kasvati@users.noreply.github.com>
2024-02-13 18:01:08 +0100

    fix(DB/Spell): Valentine event buffs grouping
    fixes AzerothCore issue #18285
    compacted the original query
    added back the column names

--- data/sql/updates/pending_db_world/rev_1707833862449013500.sql
+++ data/sql/updates/pending_db_world/rev_1707833862449013500.sql
@@ -1,5 +1,5 @@
 -- disables buff stacking for items 49856, 49857, 49858, 49859, 49861, 49860
-INSERT INTO `spell_group` VALUES
+INSERT INTO `spell_group` (`id`, `spell_id`, `special_flag`) VALUES
 (2, 70233, 16),
 (2, 70234, 16),
 (2, 70235, 16),
