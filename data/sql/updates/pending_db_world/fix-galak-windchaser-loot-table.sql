961c1fbc31a81f03a78b9ab1b9b3e3b33570cd0a
podmepodme <57728656+podmepodme@users.noreply.github.com>
2024-02-14 09:10:00 +0100

    refactor(DB/Creature): codestyle

--- data/sql/updates/pending_db_world/fix-galak-windchaser-loot-table.sql
+++ data/sql/updates/pending_db_world/fix-galak-windchaser-loot-table.sql
@@ -1,3 +1,1 @@
-DELETE `creature_loot_template` FROM `creature_loot_template`
-JOIN `item_template` ON `item_template`.`Entry` = `creature_loot_template`.`Item`
-WHERE `creature_loot_template`.`Entry` = 4096 AND `item_template`.`RequiredLevel` > 30;
+DELETE FROM `creature_loot_template` WHERE `Entry` = 4096 AND `Item` IN (24063, 24064, 24069, 24077, 24078);
