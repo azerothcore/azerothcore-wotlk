3c6a3a56e59fd6c8183dca18d78dc74540ba6055
sudlud <sudlud@users.noreply.github.com>
2024-03-04 20:58:01 +0100

    fix(DB/Gameobject): Add some missing 'Anvil, Forge, Alchemy Lab' spawns (#18432)

---
+++ data/sql/updates/pending_db_world/rev_1709065962197103700.sql
@@ -0,0 +1,7 @@
+-- Add some missing gameobject 'Anvil, Forge, Alchemy Lab' based on sniffed values
+DELETE FROM `gameobject` WHERE (`id` IN (194466, 194467, 194468))
+AND (`guid` IN (465, 466, 467));
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
+(465, 194466, 530, 0, 0, 1, 1, -1737.48095703125, 5632.6103515625, 128.9701995849609375, 0, 0, 0, 0, 1, 120, 255, 1, "", 49345, NULL),
+(466, 194467, 530, 0, 0, 1, 1, -1745.2545166015625, 5646.84228515625, 128.023193359375, 5.567600727081298828, 0, 0, -0.35020732879638671, 0.936672210693359375, 120, 255, 1, "", 49345, NULL),
+(467, 194468, 530, 0, 0, 1, 1, -1747.6688232421875, 5648.66162109375, 128.023193359375, 4.049167633056640625, 0, 0, -0.89879322052001953, 0.438372820615768432, 120, 255, 1, "", 49345, NULL);
