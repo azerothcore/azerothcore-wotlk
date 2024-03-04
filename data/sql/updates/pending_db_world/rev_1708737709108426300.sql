77dfc7ee1eb3ecf57cb2707bbb8c823616bd2100
Gultask <sagemochi@hotmail.com>
2024-03-04 19:32:15 -0300

    Update rev_1708737709108426300.sql

--- data/sql/updates/pending_db_world/rev_1708737709108426300.sql
+++ data/sql/updates/pending_db_world/rev_1708737709108426300.sql
@@ -2267,7 +2267,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE (`e
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 17921);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (17921, 0, 0, 0, 0, 0, 100, 0, 5000, 9000, 5000, 8000, 0, 0, 11, 7896, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'Alliance Rifleman - In Combat - Cast \'Exploding Shot\''),
-(17921, 0, 1, 0, 9, 0, 100, 0, 0, 2100, 2100, 3200, 8, 40, 11, 32103, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Alliance Rifleman - Within 8-40 Range - Cast \'Shoot\'');
+(17921, 0, 1, 0, 9, 0, 100, 0, 0, 2100, 2100, 3200, 8, 40, 11, 32103, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Alliance Rifleman - Within 8-40 Range - Cast \'Shoot\'');
 
 -- Fix Archimonde
 UPDATE `creature_template` SET `speed_walk` = 3.2, `speed_run` = 2.285714, `scale` = 1, `BaseAttackTime` = 1500 WHERE (`entry` = 17968);
