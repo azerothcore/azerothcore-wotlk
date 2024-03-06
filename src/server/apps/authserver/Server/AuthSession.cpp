d73115993ba26deffb906f057ccf359c224dc09d
Tristan Cormier <cormiert2@outlook.com>
2024-03-06 14:10:00 -0500

    chore(AuthSession): move eAuthCmd out of cpp file and into header

--- src/server/apps/authserver/Server/AuthSession.cpp
+++ src/server/apps/authserver/Server/AuthSession.cpp
@@ -35,20 +35,6 @@
 
 using boost::asio::ip::tcp;
 
-enum eAuthCmd
-{
-    AUTH_LOGON_CHALLENGE = 0x00,
-    AUTH_LOGON_PROOF = 0x01,
-    AUTH_RECONNECT_CHALLENGE = 0x02,
-    AUTH_RECONNECT_PROOF = 0x03,
-    REALM_LIST = 0x10,
-    XFER_INITIATE = 0x30,
-    XFER_DATA = 0x31,
-    XFER_ACCEPT = 0x32,
-    XFER_RESUME = 0x33,
-    XFER_CANCEL = 0x34
-};
-
 #pragma pack(push, 1)
 
 typedef struct AUTH_LOGON_CHALLENGE_C
