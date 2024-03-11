6cae7216949d9396ad0ef55898deeada1e70c739
elthehablo <83884799+elthehablo@users.noreply.github.com>
2024-03-11 23:06:19 +0100

    init

--- src/server/scripts/Outland/TempestKeep/Eye/boss_alar.cpp
+++ src/server/scripts/Outland/TempestKeep/Eye/boss_alar.cpp
@@ -94,6 +94,10 @@ struct boss_alar : public BossAI
     boss_alar(Creature* creature) : BossAI(creature, DATA_ALAR)
     {
         me->SetCombatMovement(false);
+        scheduler.SetValidator([this]
+        {
+            return !me->HasUnitState(UNIT_STATE_CASTING);
+        });
     }
 
     void JustReachedHome() override
