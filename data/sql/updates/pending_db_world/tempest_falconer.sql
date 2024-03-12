9a6cb7315ce90713e928e09d9428478a94bb55cc
elthehablo <83884799+elthehablo@users.noreply.github.com>
2024-03-12 20:56:37 +0100

    initial

---
+++ data/sql/updates/pending_db_world/tempest_falconer.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `smart_scripts` SET `event_type` = 0, `comment` = 'Tempest Falconer - In Combat - Cast \'Shoot\'', `event_param6` = 0 WHERE `entryorguid` = 20037 AND `source_type` = 0 AND `id` = 1;
