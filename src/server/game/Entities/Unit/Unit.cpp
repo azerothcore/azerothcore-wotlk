65e19bd35cf0517b4129c31a6bd9622820943497
trickerer <onlysuffering@gmail.com>
2024-03-14 10:18:17 +0700

    NPCBots: Fix bot item procs not triggeing cooldown

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10407,6 +10407,9 @@ bool Unit::HandleProcTriggerSpell(Unit* victim, uint32 damage, AuraEffect* trigg
             return false;
 
         AddSpellCooldown(triggerEntry->Id, 0, cooldown);
+
+        if (IsNPCBot() && triggeredByAura->GetBase()->GetCastItemGUID() != ObjectGuid::Empty)
+            ToCreature()->AddBotSpellCooldown(triggerEntry->Id, cooldown);
     }
 
     if(basepoints0)
