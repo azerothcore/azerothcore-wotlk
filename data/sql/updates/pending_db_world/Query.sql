52f42ae997b280f14811b6644f46e207fdb04afa
Pagani Walter <paganiwalter@gmail.com>
2024-03-14 23:57:09 -0300

    delete creature_addon

--- data/sql/updates/pending_db_world/Query.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM `creature` WHERE (`id1` = 3296) AND (`guid` IN (10299));
---
+++ data/sql/updates/pending_db_world/rev_1710471363617470300.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `creature` WHERE `id1`=3296 AND `guid`=10299;
+DELETE FROM `creature_addon` WHERE `guid`=10299;
