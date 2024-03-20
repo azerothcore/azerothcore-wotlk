e309a8090aedff458f9863585073263922ae9f90
KJack <kgithub@kellerg.com>
2024-03-20 00:45:44 -0400

    fix(DB/Formation) Scholo: Make Alexei Barov's adds respawn correctly (#17590)
    * Make Alexi Barov's adds respawn correctly
    
    * Add `linked_respawn` between Alexei and his adds
    
    * Don't pull formation if boss aura hits
    - no longer pulls the group if the boss' aura hits a player
    - all three creatures still pull together

---
+++ data/sql/updates/pending_db_world/rev_1698310971939947916.sql
@@ -0,0 +1,12 @@
+-- Lord Alexei Barov (10504) creature formation with adds + respawn
+DELETE FROM `creature_formations` WHERE (`leaderGUID` = 48863);
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(48863, 48862, 0, 0, 24, 0, 0),
+(48863, 48863, 0, 0, 24, 0, 0),
+(48863, 48864, 0, 0, 24, 0, 0);
+
+-- Lord Alexei Barov (10504) linked respawns with adds
+DELETE FROM `linked_respawn` WHERE `linkedGuid`=48863 AND `linkType`=0;
+INSERT INTO `linked_respawn` (`guid`, `linkedGuid`, `linkType`) VALUES
+(48862, 48863, 0),
+(48864, 48863, 0);
