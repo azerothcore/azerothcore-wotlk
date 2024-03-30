598ce580544b0f0a63f9bb7f601d98c3af199d72
Ludwig <sudlud@users.noreply.github.com>
2024-03-30 14:21:11 +0100

    fix(DB/Gameobject): update initial state of some Brazier spawns
    - sniff sql parses were showing both initial states and
      apparently the wrong one was chosen when updating all
      Brazier spawns with sniffed values

---
+++ data/sql/updates/pending_db_world/rev_1711804492859268400.sql
@@ -0,0 +1,4 @@
+-- update initial state of some Brazier gameobject spawns
+UPDATE `gameobject` SET `state` = 1 WHERE (`id` = 174744) AND (`guid` = 15229);
+UPDATE `gameobject` SET `state` = 1 WHERE (`id` = 174745) AND (`guid` = 15544);
+UPDATE `gameobject` SET `state` = 1 WHERE (`id` = 178247) AND (`guid` = 47873);
