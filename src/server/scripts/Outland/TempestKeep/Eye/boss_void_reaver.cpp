8a58d31b2fccbd2d20776023f644b82283038bd7
elthehablo <83884799+elthehablo@users.noreply.github.com>
2024-04-01 20:07:45 +0200

    initial
    this works way better

--- src/server/scripts/Outland/TempestKeep/Eye/boss_void_reaver.cpp
+++ src/server/scripts/Outland/TempestKeep/Eye/boss_void_reaver.cpp
@@ -95,7 +95,8 @@ struct boss_void_reaver : public BossAI
             context.Repeat(12100ms, 15800ms);
         }).Schedule(3450ms, GROUP_ARCANE_ORB, [this](TaskContext context)
         {
-            if (!DoCastRandomTarget(SPELL_ARCANE_ORB, 0, -18.0f))
+            SpellCastResult orbResult = DoCastRandomTarget(SPELL_ARCANE_ORB, 0, -20.0f);
+            if (orbResult != SPELL_CAST_OK)
             {
                 DoCastRandomTarget(SPELL_ARCANE_ORB, 0, 18.0f);
             }
