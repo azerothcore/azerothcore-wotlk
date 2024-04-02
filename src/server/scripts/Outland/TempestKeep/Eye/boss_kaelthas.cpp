b9d2603938967256fc0fa7b1d8c0b613ebe9a528
Balleny <12682004+balleny@users.noreply.github.com>
2024-04-02 22:20:02 +0200

    Update Kaelthas Phoenix timers

--- src/server/scripts/Outland/TempestKeep/Eye/boss_kaelthas.cpp
+++ src/server/scripts/Outland/TempestKeep/Eye/boss_kaelthas.cpp
@@ -472,11 +472,11 @@ struct boss_kaelthas : public BossAI
             {
                 DoCastRandomTarget(SPELL_FLAME_STRIKE, 0, 100.0f);
             }, 30250ms, 50650ms);
-            ScheduleTimedEvent(20000ms, [&]
+            ScheduleTimedEvent(71000ms, [&]
             {
                 Talk(SAY_SUMMON_PHOENIX);
                 DoCastSelf(SPELL_PHOENIX);
-            }, 31450ms, 66550ms);
+            }, 61450ms, 96550ms);
             ScheduleTimedEvent(5s, [&]
             {
                 scheduler.DelayAll(30s);
@@ -735,11 +735,11 @@ struct boss_kaelthas : public BossAI
         {
             DoCastRandomTarget(SPELL_FLAME_STRIKE, 0, 100.0f);
         }, 30250ms, 50650ms);
-        ScheduleTimedEvent(30000ms, [&]
+        ScheduleTimedEvent(50000ms, [&]
         {
             Talk(SAY_SUMMON_PHOENIX);
             DoCastSelf(SPELL_PHOENIX);
-        }, 31450ms, 66550ms);
+        }, 35450ms, 41550ms);
         //sequence
         ScheduleTimedEvent(20s, 23s, [&]
         {
