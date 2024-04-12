ddca7092b932ebc25cc789c79c4641a5f00ec3f1
天鹿 <18535853+PkllonG@users.noreply.github.com>
2024-04-12 13:37:59 +0800

    Add files via upload

--- src/server/game/DungeonFinding/LFGScripts.cpp
+++ src/server/game/DungeonFinding/LFGScripts.cpp
@@ -140,15 +140,7 @@ namespace lfg
         }
     }
 
-    LFGGroupScript::LFGGroupScript() :
-        GroupScript("LFGGroupScript",
-        {
-            GROUPHOOK_ON_ADD_MEMBER,
-            GROUPHOOK_ON_REMOVE_MEMBER,
-            GROUPHOOK_ON_DISBAND,
-            GROUPHOOK_ON_CHANGE_LEADER,
-            GROUPHOOK_ON_INVITE_MEMBER
-        })
+    LFGGroupScript::LFGGroupScript() : GroupScript("LFGGroupScript")
     {
     }
 
