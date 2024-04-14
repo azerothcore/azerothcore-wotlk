af3fc188e54f6118ca92067d60c58d31257ff1dd
fangshun <fangshun2004@msn.com>
2024-04-14 08:07:11 +0800

    edit

--- data/sql/updates/pending_db_world/rev_1713019573985621400.sql
+++ data/sql/updates/pending_db_world/rev_1713019573985621400.sql
@@ -10,9 +10,7 @@ DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (@ENTRY, 0, 0, 0, 19, 0, 100, 0, 1781, 0, 0, 0, 86, 8596, 0, 10, 37571, 5489, 0, 10, 39538, 6173, 0, 0, 0, 0, 0, 'On player accepted quest The Tome of Divinity (1781) - Creature Brother Joshua (5489) with guid 37571 (fetching): Cast spell  Heal Visual (8596) at Creature Gazin Tenorm (6173) with guid 39538 (fetching)');
 
-
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 6171 AND `SourceId` = 0;
-
 
  -- Gazin Tenorm smart ai
 SET @ENTRY := 6173;
