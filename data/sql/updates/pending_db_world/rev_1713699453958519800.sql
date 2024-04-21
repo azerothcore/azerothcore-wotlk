1c61246994b8541f07933118f4dbf851f39e1fd8
sudlud <sudlud@users.noreply.github.com>
2024-04-21 21:57:08 +0200

    fix(DB/Gameobject): Sniffed Values for 179548 'A Dusty Tome' spawns (#18775)
    * fix(DB/Gameobject): Sniffed Values for 'A Dusty Tome' spawns
    
    * fix spawnmask

---
+++ data/sql/updates/pending_db_world/rev_1713699453958519800.sql
@@ -0,0 +1,7 @@
+-- Update gameobject 'A Dusty Tome' with sniffed values
+
+-- new spawns
+DELETE FROM `gameobject` WHERE (`id` IN (179548))
+AND (`guid` IN (20));
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
+(20, 179548, 429, 0, 0, 1, 1, 4.497807025909423828, -437.5672607421875, 16.41251754760742187, 4.136432647705078125, 0, 0, -0.87881660461425781, 0.477159708738327026, 7200, 255, 1, "", 52237, NULL);
