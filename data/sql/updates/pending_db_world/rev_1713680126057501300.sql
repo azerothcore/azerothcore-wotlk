86d068952e0b617edaf52fe5b5f050eb9c6572e9
Ludwig <sudlud@users.noreply.github.com>
2024-04-21 21:00:27 +0200

    add missing delete statement

--- data/sql/updates/pending_db_world/rev_1713680126057501300.sql
+++ data/sql/updates/pending_db_world/rev_1713680126057501300.sql
@@ -1,3 +1,4 @@
 -- Fireworks Spectacular
+DELETE FROM `game_event` WHERE `eventEntry` = 72;
 INSERT INTO `game_event` (`eventEntry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `holidayStage`, `description`, `world_event`, `announce`) VALUES
 (72, '2024-07-04 06:00:00', '2030-12-31 12:00:00', 525600, 1080, 62, 0, 'Fireworks Spectacular', 0, 2);
