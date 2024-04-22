bf5288178c0370aa82a1a1255a2d65ec13eecd61
V <44802344+V-Cyberpunk@users.noreply.github.com>
2024-04-22 19:11:08 +0200

    new line added

--- data/sql/updates/pending_db_world/rev_1713803843196763300.sql
+++ data/sql/updates/pending_db_world/rev_1713803843196763300.sql
@@ -3,4 +3,4 @@ UPDATE gossip_menu_option_locale set OptionText=REPLACE(OptionText,'Ã¼','ü');
 UPDATE gossip_menu_option_locale set OptionText=REPLACE(OptionText,'Ã¶','ö');
 UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¤','ä');
 UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¼','ü');
-UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¶','ö'); 
+UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¶','ö');
