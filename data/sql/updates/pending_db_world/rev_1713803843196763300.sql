b4ff7dffe48441bba48547dae43f757c69f0ec0c
V <44802344+V-Cyberpunk@users.noreply.github.com>
2024-04-22 18:38:51 +0200

    fix(DB/Gossip_Menu_Option_Locale): fix german umlauts

---
+++ data/sql/updates/pending_db_world/rev_1713803843196763300.sql
@@ -0,0 +1,6 @@
+UPDATE gossip_menu_option_locale set OptionText=REPLACE(OptionText,'Ã¤','ä');
+UPDATE gossip_menu_option_locale set OptionText=REPLACE(OptionText,'Ã¼','ü');
+UPDATE gossip_menu_option_locale set OptionText=REPLACE(OptionText,'Ã¶','ö');
+UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¤','ä');
+UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¼','ü');
+UPDATE gossip_menu_option_locale set BoxText=REPLACE(BoxText,'Ã¶','ö'); 
