58f1cffb10f069c1d8e44833707e1ab2f3090b07
Aloy <44802344+V-Cyberpunk@users.noreply.github.com>
2024-04-29 08:17:54 +0200

    fix(DB/Gossip_Menu_Option_Locale): fix german umlauts (#18781)
    * fix(DB/Gossip_Menu_Option_Locale): fix german umlauts
    
    * new line added
    
    * Specify German language in the query
    
    ---------
    
    Co-authored-by: Pagani Walter <paganiwalter@gmail.com>

---
+++ data/sql/updates/pending_db_world/rev_1713803843196763300.sql
@@ -0,0 +1,6 @@
+UPDATE `gossip_menu_option_locale` SET `OptionText`=REPLACE(`OptionText`,'Ã¤','ä') WHERE `Locale`='deDE';
+UPDATE `gossip_menu_option_locale` SET `OptionText`=REPLACE(`OptionText`,'Ã¼','ü') WHERE `Locale`='deDE';
+UPDATE `gossip_menu_option_locale` SET `OptionText`=REPLACE(`OptionText`,'Ã¶','ö') WHERE `Locale`='deDE';
+UPDATE `gossip_menu_option_locale` SET `BoxText`=REPLACE(`BoxText`,'Ã¤','ä') WHERE `Locale`='deDE';
+UPDATE `gossip_menu_option_locale` SET `BoxText`=REPLACE(`BoxText`,'Ã¼','ü') WHERE `Locale`='deDE';
+UPDATE `gossip_menu_option_locale` SET `BoxText`=REPLACE(`BoxText`,'Ã¶','ö') WHERE `Locale`='deDE';
