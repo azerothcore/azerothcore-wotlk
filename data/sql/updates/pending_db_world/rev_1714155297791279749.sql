7496d83d33df6d4cbe5a5803bae91986c5ad6a00
Jelle Meeus <sogladev@gmail.com>
2024-04-30 11:30:49 +0200

    fix Effects start at 1 in DBC

--- data/sql/updates/pending_db_world/rev_1714155297791279749.sql
+++ data/sql/updates/pending_db_world/rev_1714155297791279749.sql
@@ -8,4 +8,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`)
 VALUES(28622, 'spell_web_wrap_damage');
 
 -- 28618: Disable pull effect and periodic trigger event. Keep pacify silence and set duration to 5 seconds
-UPDATE `spell_dbc` SET `DurationIndex` = 27, `Effect_0` = 0, `Effect_1` = 0 WHERE `id` = 28618;
+UPDATE `spell_dbc` SET `DurationIndex` = 27, `Effect_1` = 0, `Effect_2` = 0 WHERE `ID` = 28618;
