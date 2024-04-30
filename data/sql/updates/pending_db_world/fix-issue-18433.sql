f6ef59e16fe1f20e9819a68a928f671e4580a377
Pagani Walter <paganiwalter@gmail.com>
2024-04-30 02:16:59 -0300

    delete const

--- data/sql/updates/pending_db_world/fix-issue-18433.sql
+++ data/sql/updates/pending_db_world/fix-issue-18433.sql
@@ -1,6 +1,1 @@
-
--- Timber creature ID
-SET @ENTRY := 1132;
-
--- Updating all spawnpoint
-UPDATE `creature` SET `spawntimesecs` = 2700 WHERE `guid` IN (3154, 134483, 134484) AND `id1` = @ENTRY;
+UPDATE `creature` SET `spawntimesecs`=2700 WHERE `guid` IN (3154, 134483, 134484) AND `id1`=1132;
