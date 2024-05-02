c928e4c14c6f2f7b3d78641bf9dcf34009d1b2d3
Jelle Meeus <sogladev@gmail.com>
2024-05-02 17:50:11 +0200

    fixMaraPortal

---
+++ data/sql/updates/pending_db_world/rev_1714664171505149429.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `spell_target_position` WHERE `ID` = 21128;
+INSERT INTO `spell_target_position`
+(`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`)
+VALUES
+(21128, 0, 349, 386.27, 33.4144, -130.934, 0, 52237);
