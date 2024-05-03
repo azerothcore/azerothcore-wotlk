a4f0535230eba53201e08256da07c74250b54fd9
Ludwig <sudlud@users.noreply.github.com>
2024-05-03 21:25:50 +0200

    move firework_show arrays to seperate header files

---
+++ src/server/scripts/Events/firework_show/firework_show.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DEF_FIREWORK_SHOW_H
+#define DEF_FIREWORK_SHOW_H
+
+enum eFireworks
+{
+    GO_FIREWORK_SHOW_TYPE_1_RED = 180703,
+    GO_FIREWORK_SHOW_TYPE_2_RED = 180704,
+    GO_FIREWORK_SHOW_TYPE_1_RED_BIG = 180707,
+    GO_FIREWORK_SHOW_TYPE_2_RED_BIG = 180708,
+    GO_FIREWORK_SHOW_TYPE_1_BLUE = 180720,
+    GO_FIREWORK_SHOW_TYPE_2_BLUE = 180721,
+    GO_FIREWORK_SHOW_TYPE_1_BLUE_BIG = 180722,
+    GO_FIREWORK_SHOW_TYPE_2_BLUE_BIG = 180723,
+    GO_FIREWORK_SHOW_TYPE_1_GREEN = 180724,
+    GO_FIREWORK_SHOW_TYPE_2_GREEN_BIG = 180725,
+    GO_FIREWORK_SHOW_TYPE_1_GREEN_BIG = 180726,
+    GO_FIREWORK_SHOW_TYPE_2_GREEN = 180727,
+    GO_FIREWORK_SHOW_TYPE_1_WHITE = 180728,
+    GO_FIREWORK_SHOW_TYPE_1_WHITE_BIG = 180729,
+    GO_FIREWORK_SHOW_TYPE_2_WHITE = 180730,
+    GO_FIREWORK_SHOW_TYPE_2_WHITE_BIG = 180731,
+    GO_FIREWORK_SHOW_TYPE_2_PURPLE_BIG = 180733,
+    GO_FIREWORK_SHOW_TYPE_1_YELLOW = 180736,
+    GO_FIREWORK_SHOW_TYPE_1_YELLOW_BIG = 180737,
+    GO_FIREWORK_SHOW_TYPE_2_YELLOW = 180738,
+    GO_FIREWORK_SHOW_TYPE_2_YELLOW_BIG = 180739,
+    GO_FIREWORK_SHOW_TYPE_2_PURPLE = 180740,
+    GO_FIREWORK_SHOW_TYPE_1_PURPLE_BIG = 180741,
+
+    COUNT_FIREWORK_SPAWN_POSITIONS = 569,
+};
+
+#endif
