6b5ad7279861956e01bfe24b7ae22ada5b23f71b
Mikhail <xubpwgxubpwg@gmail.com>
2024-05-12 12:55:47 +0300

    fix(DB/Creature):  Correct spells order for Broken-down Shredder (issue 18729) (#18866)
    * fix(DB/Creature):  Correct spells order has been restored for Broken-down Shredder
    
    * Update fix_issue18729.sql
    
    ---------
    
    Co-authored-by: Dan <83884799+elthehablo@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/fix_issue18729.sql
@@ -0,0 +1,7 @@
+
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 27354;
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(27354, 0, 48548, 53788),
+(27354, 1, 48558, 53788),
+(27354, 2, 48604, 53788),
+(27354, 4, 48610, 53788);
