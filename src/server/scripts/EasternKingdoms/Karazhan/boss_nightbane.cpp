ae53cced48336ce594239a1c711dd3ce59a14bfb
Jelle Meeus <sogladev@gmail.com>
2024-05-12 19:40:51 +0200

    random target and remember position

--- src/server/scripts/EasternKingdoms/Karazhan/boss_nightbane.cpp
+++ src/server/scripts/EasternKingdoms/Karazhan/boss_nightbane.cpp
@@ -160,17 +160,21 @@ struct boss_nightbane : public BossAI
         scheduler.Schedule(2s, GROUP_FLYING, [this](TaskContext)
         {
             DoResetThreatList();
-            DoCastVictim(SPELL_RAIN_OF_BONES);
-            _skeletonscheduler.Schedule(50ms, [this](TaskContext context)
+            if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 100, true))
             {
-                //spawns skeletons every second until skeletonCount is reached
-                if(_skeletonSpawnCounter < _skeletonCount)
+                me->CastSpell(target, SPELL_RAIN_OF_BONES);
+                _skeletonSpawnPos = target->GetPosition();
+                _skeletonscheduler.Schedule(50ms, [this](TaskContext context)
                 {
-                    DoCastVictim(SPELL_SUMMON_SKELETON, true);
-                    _skeletonSpawnCounter++;
-                    context.Repeat(2s);
-                }
-            });
+                    //spawns skeletons every 2 seconds until skeletonCount is reached
+                    if(_skeletonSpawnCounter < _skeletonCount)
+                    {
+                        me->CastSpell(_skeletonSpawnPos.GetPositionX(), _skeletonSpawnPos.GetPositionY(), _skeletonSpawnPos.GetPositionZ(), SPELL_SUMMON_SKELETON, true);
+                        _skeletonSpawnCounter++;
+                        context.Repeat(2s);
+                    }
+                });
+            }
         }).Schedule(20s, GROUP_FLYING, [this](TaskContext context)
         {
             DoCastRandomTarget(SPELL_DISTRACTING_ASH);
@@ -371,6 +375,7 @@ private:
     uint32 _movePhase;
     uint8 _skeletonCount;
     uint8 _skeletonSpawnCounter;
+    Position _skeletonSpawnPos;
 };
 
 class go_blackened_urn : public GameObjectScript
