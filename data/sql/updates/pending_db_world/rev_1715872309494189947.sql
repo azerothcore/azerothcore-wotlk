ef6bcd82aab64a2b8bbc16d0b7b855b40ad60324
Jelle Meeus <sogladev@gmail.com>
2024-05-16 17:16:22 +0200

    add quality colors to .item lookup command

---
+++ data/sql/updates/pending_db_world/rev_1715872309494189947.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `acore_string` SET `content_default`='%d - |c%x|Hitem:%d:0:0:0:0:0:0:0:0|h[%s]|h|r ', `locale_deDE`='%d - |c%x|Hitem:%d:0:0:0:0:0:0:0:0|h[%s]|h|r ', `locale_zhCN`='%d - |c%x|Hitem:%d:0:0:0:0:0:0:0:0|h[%s]|h|r' WHERE `entry`=512;
