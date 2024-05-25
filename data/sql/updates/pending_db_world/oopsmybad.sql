88c485f6c54d45d05d638e38126c4d57c5071f5a
Gultask <100873791+Gultask@users.noreply.github.com>
2024-05-25 13:41:27 -0300

    fix(DB/Loot): Fix mistake in generic overworld profession drops (#18941)
    * Create oopsmybad.sql
    
    * Update oopsmybad.sql
    
    * Update oopsmybad.sql

---
+++ data/sql/updates/pending_db_world/oopsmybad.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `Reference` = 1080007 WHERE `Reference` = 1080008 AND `Comment` = 'World Drop - Profession Recipes - NPC Levels ~50-63';
+DELETE FROM `reference_loot_template` WHERE `Entry` = 1080008;
