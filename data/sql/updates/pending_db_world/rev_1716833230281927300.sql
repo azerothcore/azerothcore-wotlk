73e44859f075321bb53023e48aa13f717f321098
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-05-27 15:48:29 -0300

    fix(Scripts/Hyjal): Azgalor mark of doom spawn demons on target death (#18959)

---
+++ data/sql/updates/pending_db_world/rev_1716833230281927300.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_azgalor_doom' AND `spell_id` = 31347;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(31347, 'spell_azgalor_doom');
