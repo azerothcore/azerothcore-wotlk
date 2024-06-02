a0b942332b1ababd8ffdcdb26a1188d64a049b67
Dan <83884799+elthehablo@users.noreply.github.com>
2024-06-02 17:07:34 +0200

    fix(DB/achievement_reward): award title on Hand of A'dal FoS (#18998)
    * init
    
    * oops

---
+++ data/sql/updates/pending_db_world/hand_of_adal.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `achievement_reward` WHERE `ID` = 431;
+INSERT INTO `achievement_reward` (`ID`, `TitleA`, `TitleH`, `ItemID`, `Sender`, `Subject`, `Body`, `MailTemplateID`) VALUES
+(431, 64, 64, 0, 0, null, null, 0);
