299631f3b561fb63eb9aab8619663a20f76643fc
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-06-08 15:03:53 -0400

    fix(DB/Creature): Correct model info for Hyjal bosses. (#19032)
    * Init.
    
    * Remove comments.
    
    Bad CI.

---
+++ data/sql/updates/pending_db_world/hyjal-models.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_model_info` SET `BoundingRadius` = 4.5, `CombatReach` = 7.875 WHERE `DisplayID` = 17886;
+UPDATE `creature_model_info` SET `BoundingRadius` = 4.2 WHERE `DisplayID` = 20939;
+UPDATE `creature_model_info` SET `BoundingRadius` = 5, `CombatReach` = 7.5 WHERE `DisplayID` = 21069;
