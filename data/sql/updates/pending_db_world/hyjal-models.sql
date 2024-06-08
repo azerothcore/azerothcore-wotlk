6b5f012c8bb806fc57a7498f563d8c9378165290
heyitsbench <benjymansy123@gmail.com>
2024-06-08 13:55:12 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/hyjal-models.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_model_info` SET `BoundingRadius` = 4.5, `CombatReach` = 7.875 WHERE `DisplayID` = 17886; -- Both were 0
+UPDATE `creature_model_info` SET `BoundingRadius` = 4.2 WHERE `DisplayID` = 20939; -- Was ~9.9
+UPDATE `creature_model_info` SET `BoundingRadius` = 5, `CombatReach` = 7.5 WHERE `DisplayID` = 21069; -- Both were 0
