415f52d66263c3b3207e3dff925e8e5ffc643ad3
Jelle Meeus <sogladev@gmail.com>
2024-06-09 13:39:16 +0200

    refactor(Scripts/Naxxramas): Spell Scripts use registry macros (#19034)
    * ktz frost blast
    
    * ktz detonate mana
    
    * thaddius pos_neg_charge
    
    * thaddius polarity shift
    
    * sapph frost explosion
    
    * grobbulus poison
    
    * grobbulus mutating injection
    
    * gothik shadow bolt volley
    
    * gluth decimate
    
    * horsemen mark
    
    * add sql
    
    * trigger ci
    
    * Revert "trigger ci"
    
    This reverts commit d8b18d5c5c37d12da1deab0be50941bfe96641a6.
    
    * add validate horsemen mark
    
    * add validate gluth decimate
    
    * add validate gothik shadow mark
    
    * add validate ktz frost blast
    
    * add validate thaddius pos_neg_charge

---
+++ data/sql/updates/pending_db_world/rev_1717886186186050826.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `spell_script_names` SET `ScriptName`='spell_four_horsemen_mark_aura' WHERE `spell_id` IN (28832, 28833, 28834, 28835);
+UPDATE `spell_script_names` SET `ScriptName`='spell_grobbulus_mutating_injection_aura' WHERE `spell_id`=28169;
+UPDATE `spell_script_names` SET `ScriptName`='spell_kelthuzad_detonate_mana_aura' WHERE `spell_id`=27819;
