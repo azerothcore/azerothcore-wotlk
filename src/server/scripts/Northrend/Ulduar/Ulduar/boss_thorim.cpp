3c57328b3248c2c38025e5f58320740211f8756f
Jelle Meeus <sogladev@gmail.com>
2024-06-09 18:25:24 +0200

    thorim lightning pillar

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_thorim.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_thorim.cpp
@@ -1729,31 +1729,20 @@ public:
     }
 };
 
-class spell_thorim_lightning_pillar_P2 : public SpellScriptLoader
+class spell_thorim_lightning_pillar_P2_aura : public AuraScript
 {
-public:
-    spell_thorim_lightning_pillar_P2() : SpellScriptLoader("spell_thorim_lightning_pillar_P2") { }
+    PrepareAuraScript(spell_thorim_lightning_pillar_P2_aura);
 
-    class spell_thorim_lightning_pillar_P2_AuraScript : public AuraScript
+    void OnPeriodic(AuraEffect const* aurEff)
     {
-        PrepareAuraScript(spell_thorim_lightning_pillar_P2_AuraScript);
+        PreventDefaultAction();
+        if (Unit* caster = GetCaster())
+            GetUnitOwner()->CastSpell(caster, GetSpellInfo()->Effects[aurEff->GetEffIndex()].TriggerSpell, true);
+    }
 
-        void OnPeriodic(AuraEffect const* aurEff)
-        {
-            PreventDefaultAction();
-            if (Unit* caster = GetCaster())
-                GetUnitOwner()->CastSpell(caster, GetSpellInfo()->Effects[aurEff->GetEffIndex()].TriggerSpell, true);
-        }
-
-        void Register() override
-        {
-            OnEffectPeriodic += AuraEffectPeriodicFn(spell_thorim_lightning_pillar_P2_AuraScript::OnPeriodic, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
-        }
-    };
-
-    AuraScript* GetAuraScript() const override
+    void Register() override
     {
-        return new spell_thorim_lightning_pillar_P2_AuraScript();
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_thorim_lightning_pillar_P2_aura::OnPeriodic, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
     }
 };
 
@@ -1838,7 +1827,7 @@ void AddSC_boss_thorim()
     new go_thorim_lever();
 
     // Spells
-    new spell_thorim_lightning_pillar_P2();
+    RegisterSpellScript(spell_thorim_lightning_pillar_P2_aura);
     new spell_thorim_trash_impale();
 
     // Achievements
