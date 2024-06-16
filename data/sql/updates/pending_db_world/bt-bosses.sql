8a86bb2fafcc9bb7fb161920a46c9537e020f249
heyitsbench <benjymansy123@gmail.com>
2024-06-16 17:23:57 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/bt-bosses.sql
@@ -0,0 +1,12 @@
+UPDATE `creature_model_info` SET `BoundingRadius` = 2.10000014305114746                                        WHERE `DisplayID` = 21174; -- naj'entus
+UPDATE `creature_model_info` SET `BoundingRadius` = 80,                   `CombatReach` = 24                   WHERE `DisplayID` = 21145; -- supremus
+UPDATE `creature_model_info` SET                                          `CombatReach` = 5.774999618530273437 WHERE `DisplayID` = 21443; -- gurtogg
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.310000002384185791, `CombatReach` = 8                    WHERE `DisplayID` = 21483; -- suffering reliquary
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.310000002384185791, `CombatReach` = 8                    WHERE `DisplayID` = 21485; -- desire reliquary
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.310000002384185791, `CombatReach` = 8                    WHERE `DisplayID` = 21484; -- anger reliquary
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.80429995059967041,  `CombatReach` = 3.149999856948852539 WHERE `DisplayID` = 21417; -- zerevor
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.842599987983703613, `CombatReach` = 3.300000190734863281 WHERE `DisplayID` = 21416; -- gathios
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.861749947071075439                                       WHERE `DisplayID` = 21418; -- veras
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.80429995059967041,  `CombatReach` = 3.149999856948852539 WHERE `DisplayID` = 21419; -- malande
+-- stormrage
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.458999991416931152                                       WHERE `DisplayID` = 21135;
