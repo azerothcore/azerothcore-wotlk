d568544c5c81bd59bd9518638ceaa2c5d3980503
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-06-20 03:59:59 -0400

    fix(DB/Loot): Adjust Ancient Gem Vein loot table. (#19097)
    Init.
    
    https: //github.com/cmangos/tbc-db/commit/2f458511c4e9d02b5a90891c706ce86b0c3b8013
    
    Co-authored-by: AnonXS <19734826+AnonXS@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/hyjal-gem-veins.sql
@@ -0,0 +1,6 @@
+UPDATE `gameobject_loot_template` SET `Reference` = 13002, `Chance` = 20 WHERE `Entry` = 26862 AND `Reference` = 12901; -- Rare gems, reference currently unused I think
+UPDATE `gameobject_loot_template` SET `MaxCount` = 3 WHERE `Entry` = 26862 AND `Item` = 34907; -- Shattered gem fragments
+
+UPDATE `gameobject_loot_template` SET `Chance` = 50 WHERE `Entry` = 26862 AND `Reference` = 12903; -- Epic gems
+ -- Rare gem reference
+UPDATE `reference_loot_template` SET `MaxCount` = 2 WHERE `Entry` = 13002;
