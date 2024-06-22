9c7cf93cd8e082aa63d9dbe94f115a94616587f6
Jelle Meeus <sogladev@gmail.com>
2024-06-22 20:22:24 +0200

    refactor(Scripts/Hodir): spellscripts use registry macros (#19126)

---
+++ data/sql/updates/pending_db_world/rev_1719051177326126718.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `spell_script_names` SET `ScriptName`='spell_hodir_storm_power_aura' WHERE `spell_id` IN (63711,65134);
+UPDATE `spell_script_names` SET `ScriptName`='spell_hodir_storm_cloud_aura' WHERE `spell_id` IN (65123,65133);
+UPDATE `spell_script_names` SET `ScriptName`='spell_hodir_toasty_fire_aura' WHERE `spell_id`=62821;
+UPDATE `spell_script_names` SET `ScriptName`='spell_hodir_starlight_aura' WHERE `spell_id`=62807;
