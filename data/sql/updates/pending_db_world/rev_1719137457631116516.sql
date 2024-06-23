8cc1bbcbd64a86ced1f13c8a6e8f985126235d60
Jelle Meeus <sogladev@gmail.com>
2024-06-23 12:56:54 +0200

    instance_drak_tharon_keep aura:spell_dtk_raise_dead_aura

--- data/sql/updates/pending_db_world/rev_1719137457631116516.sql
+++ data/sql/updates/pending_db_world/rev_1719137457631116516.sql
@@ -3,3 +3,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_azjol_nerub_web_wrap_aura' W
 UPDATE `spell_script_names` SET `ScriptName`='spell_hadronox_summon_periodic_crypt_fiend_aura' WHERE `spell_id`=53037;
 UPDATE `spell_script_names` SET `ScriptName`='spell_hadronox_leech_poison_aura' WHERE `spell_id` IN (53030,59417);
 UPDATE `spell_script_names` SET `ScriptName`='spell_reflective_shield_aura' WHERE `spell_id`=66515;
+UPDATE `spell_script_names` SET `ScriptName`='spell_dtk_raise_dead_aura' WHERE `spell_id` IN (48597,48605);
