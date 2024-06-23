cd17fccc83155006b4f266a195cf81787d2a5fb8
Jelle Meeus <sogladev@gmail.com>
2024-06-23 11:19:25 +0200

    boss_doomlord_kazzak aura:spell_mark_of_kazzak_aura

--- data/sql/updates/pending_db_world/rev_1719134265835105342.sql
+++ data/sql/updates/pending_db_world/rev_1719134265835105342.sql
@@ -1,2 +1,3 @@
 --
 UPDATE `spell_script_names` SET `ScriptName`='spell_q10923_evil_draws_near_periodic_aura' WHERE `spell_id`=39259;
+UPDATE `spell_script_names` SET `ScriptName`='spell_mark_of_kazzak_aura' WHERE `spell_id`=32960;
