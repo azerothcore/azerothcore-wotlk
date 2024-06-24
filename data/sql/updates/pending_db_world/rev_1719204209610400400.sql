7a5e8932697cd51bba9791306e9e253a57a240df
avarishd <46330494+avarishd@users.noreply.github.com>
2024-06-24 07:48:08 +0300

    fix(DB/SAI): Torek's Assault not completing for party members

---
+++ data/sql/updates/pending_db_world/rev_1719204209610400400.sql
@@ -0,0 +1,2 @@
+-- Torek
+UPDATE `smart_scripts` SET `target_type` = 16, `comment` = 'Torek - On Quest Accept - Store Party Targetlist' WHERE (`entryorguid` = 12858) AND (`source_type` = 0) AND (`id` = 1);
