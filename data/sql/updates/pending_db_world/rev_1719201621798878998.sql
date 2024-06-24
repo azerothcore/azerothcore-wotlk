46f985e429aa9435d92adaf03351eb93b4f9c6e6
Jelle Meeus <sogladev@gmail.com>
2024-06-24 06:02:15 +0200

    rename other args
    fixup https://github.com/azerothcore/azerothcore-wotlk/commit/cd9dff503258445958922be5643829aa21a8461e#commitcomment-143444310

---
+++ data/sql/updates/pending_db_world/rev_1719201621798878998.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `spell_script_names` SET `ScriptName`='spell_hadronox_summon_periodic_champion_aura' WHERE `spell_id`=53035;
+UPDATE `spell_script_names` SET `ScriptName`='spell_hadronox_summon_periodic_necromancer_aura' WHERE `spell_id`=53036;
