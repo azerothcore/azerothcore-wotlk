9afd862ae80ea813c31201ee501cbff254086a2c
heyitsbench <benjymansy123@gmail.com>
2024-06-24 09:30:19 -0400

    Forgot the heroic difficulty entry, not base.

--- data/sql/updates/pending_db_world/ahune-frozen-core-level.sql
+++ data/sql/updates/pending_db_world/ahune-frozen-core-level.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` IN (25865, 26339);
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 26339;
