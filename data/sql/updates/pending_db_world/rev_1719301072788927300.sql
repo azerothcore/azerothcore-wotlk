131528e06871d2426a4f996eda9c501077121f9c
avarishd <46330494+avarishd@users.noreply.github.com>
2024-06-26 00:37:33 +0300

    Set Reactstate Aggressive

--- data/sql/updates/pending_db_world/rev_1719301072788927300.sql
+++ data/sql/updates/pending_db_world/rev_1719301072788927300.sql
@@ -9,8 +9,9 @@ DELETE FROM `creature` WHERE `id1`=20480;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` IN (20480,20512));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (20512, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 80, 2051200, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Tormented Soul - On Aggro - Run Script'),
-(20512, 0, 1, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 36, 20512, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Tormented Soul - On Reset - Update Template To \'Tormented Soul\''),
-(20512, 0, 2, 0, 0, 0, 100, 0, 7000, 12000, 0, 0, 0, 0, 11, 36153, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Tormented Soul - In Combat - Cast \'Soulbind\'');
+(20512, 0, 1, 0, 0, 0, 100, 0, 7000, 12000, 0, 0, 0, 0, 11, 36153, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Tormented Soul - In Combat - Cast \'Soulbind\''),
+(20512, 0, 2, 3, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 36, 20512, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Tormented Soul - On Reset - Update Template To \'Tormented Soul\''),
+(20512, 0, 3, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Tormented Soul - On Reset - Set Reactstate Aggressive');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` - 2051200);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
