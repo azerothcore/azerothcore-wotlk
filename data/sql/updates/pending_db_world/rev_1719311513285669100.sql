76b389ed078036db897396d4afd1babc5b2624a8
avarishd <46330494+avarishd@users.noreply.github.com>
2024-06-25 23:36:25 +0300

    fix(Core/Spells):  Script Drunken Haze & Drunken Skull Crack (#19164)
    * fix(Core/Spells):  Script Drunken Haze & Drunken Skull Crack
    
    * bruh
    
    * sigh

---
+++ data/sql/updates/pending_db_world/rev_1719311513285669100.sql
@@ -0,0 +1,5 @@
+-- Drunken Haze, Drunken Skull Crack
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (37591,29690);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(37591, 'spell_gen_sober_up'),
+(29690, 'spell_gen_sober_up');
