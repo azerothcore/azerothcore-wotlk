dbba6500334a1c6e71375169738c94537ee2f118
Jelle Meeus <sogladev@gmail.com>
2024-06-26 10:18:50 +0200

    molten_core aura:spell_mc_play_dead_aura

--- data/sql/updates/pending_db_world/rev_1719389897107886822.sql
+++ data/sql/updates/pending_db_world/rev_1719389897107886822.sql
@@ -1,2 +1,3 @@
 --
 UPDATE `spell_script_names` SET `ScriptName`='spell_garr_separation_nexiety_aura' WHERE `spell_id`=23487;
+UPDATE `spell_script_names` SET `ScriptName`='spell_mc_play_dead_aura' WHERE `spell_id`=19822;
