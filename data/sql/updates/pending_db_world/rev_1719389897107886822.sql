f380df72f12194a076c6d54605e5c2f26da04ede
Jelle Meeus <sogladev@gmail.com>
2024-06-27 00:51:36 +0200

    fixup! fixup! boss_garr aura:spell_garr_separation_nexiety_aura

--- data/sql/updates/pending_db_world/rev_1719389897107886822.sql
+++ data/sql/updates/pending_db_world/rev_1719389897107886822.sql
@@ -1,5 +1,5 @@
 --
-UPDATE `spell_script_names` SET `ScriptName`='spell_garr_separation_nexiety_aura' WHERE `spell_id`=23487;
+UPDATE `spell_script_names` SET `ScriptName`='spell_garr_separation_anxiety_aura' WHERE `spell_id`=23487;
 UPDATE `spell_script_names` SET `ScriptName`='spell_mc_play_dead_aura' WHERE `spell_id`=19822;
 UPDATE `spell_script_names` SET `ScriptName`='spell_majordomo_separation_anxiety_aura' WHERE `spell_id`=21094;
 UPDATE `spell_script_names` SET `ScriptName`='spell_geddon_inferno_aura' WHERE `spell_id`=19695;
