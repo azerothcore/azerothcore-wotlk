a740c252f96daa22f8d433ef214cb8175cfbb122
Jelle Meeus <sogladev@gmail.com>
2024-06-27 18:03:08 +0200

    refactor(Scripts/BlackrockMountain): Spell Scripts registry macros (#19186)
    * boss_garr aura:spell_garr_separation_nexiety_aura
    
    * boss_garr spell:spell_garr_frenzy
    
    * molten_core aura:spell_mc_play_dead_aura
    
    * boss_magmadar spell:spell_magmadar_lava_bomb
    
    * boss_majordomo_executus spell:spell_hate_to_zero
    
    * boss_majordomo_executus aura:spell_majordomo_separation_nexiety_aura
    
    * boss_majordomo_executus spell:spell_summon_ragnaros
    
    * boss_shazzrah spell:spell_shazzrah_gate_dummy
    
    * boss_baron_geddon aura:spell_geddon_inferno_aura
    
    * boss_baron_geddon aura:spell_geddon_armageddon_aura
    
    * boss_razorgore spell:spell_egg_event
    
    * instance_blackwing_lair spell:spell_bwl_shadowflame
    
    * fixup! boss_majordomo_executus aura:spell_majordomo_separation_nexiety_aura
    
    * fixup! boss_garr aura:spell_garr_separation_nexiety_aura
    
    * fixup! fixup! boss_garr aura:spell_garr_separation_nexiety_aura

---
+++ data/sql/updates/pending_db_world/rev_1719389897107886822.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `spell_script_names` SET `ScriptName`='spell_garr_separation_anxiety_aura' WHERE `spell_id`=23487;
+UPDATE `spell_script_names` SET `ScriptName`='spell_mc_play_dead_aura' WHERE `spell_id`=19822;
+UPDATE `spell_script_names` SET `ScriptName`='spell_majordomo_separation_anxiety_aura' WHERE `spell_id`=21094;
+UPDATE `spell_script_names` SET `ScriptName`='spell_geddon_inferno_aura' WHERE `spell_id`=19695;
+UPDATE `spell_script_names` SET `ScriptName`='spell_geddon_armageddon_aura' WHERE `spell_id`=20478;
