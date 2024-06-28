7948a703c71e17193f42fbf795e4deeaab355a48
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-06-28 19:59:11 -0400

    fix(Loot/Midsummer): Add loot to Pocket Full of Snow. (#19218)
    Init.

---
+++ data/sql/updates/pending_db_world/ahune-pocket-full-of-loot.sql
@@ -0,0 +1,3 @@
+DELETE FROM `item_loot_template` WHERE `Entry` = 35512;
+INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(35512, 17202, 0, 100, 0, 1, 0, 2, 5, 'Pocket Full of Snow - Snowball');
