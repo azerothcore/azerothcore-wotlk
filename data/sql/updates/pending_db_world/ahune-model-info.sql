04aaeec9e44080cf6633381374d5df9d811e0f43
heyitsbench <benjymansy123@gmail.com>
2024-06-28 15:08:26 -0400

    Remove unnecessary info.

--- data/sql/updates/pending_db_world/ahune-model-info.sql
+++ data/sql/updates/pending_db_world/ahune-model-info.sql
@@ -1,6 +1,5 @@
-UPDATE `creature_model_info` SET `BoundingRadius` = 1,                    `CombatReach` = 11   WHERE `DisplayID` = 23344; -- Ahune
-UPDATE `creature_model_info` SET `BoundingRadius` = 0.694444000720977783                       WHERE `DisplayID` = 20990; -- Skar'this
-UPDATE `creature_model_info` SET `BoundingRadius` = 0.620000004768371582, `CombatReach` = 3    WHERE `DisplayID` = 20433; -- Ahunite Hailstone
-UPDATE `creature_model_info` SET `BoundingRadius` = 0.305559992790222167                       WHERE `DisplayID` = 23504; -- Ahunite Coldwave
--- UPDATE `creature_model_info` SET `BoundingRadius` = 1,                 `CombatReach` = 1.25 WHERE `DisplayID` = 8714; -- Ahunite Frostwind
-UPDATE `creature_model_info` SET `BoundingRadius` = 2 WHERE `DisplayID` = 23447; -- Frozen Core
+UPDATE `creature_model_info` SET `BoundingRadius` = 1,                    `CombatReach` = 11 WHERE `DisplayID` = 23344;
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.694444000720977783                     WHERE `DisplayID` = 20990;
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.620000004768371582, `CombatReach` = 3  WHERE `DisplayID` = 20433;
+UPDATE `creature_model_info` SET `BoundingRadius` = 0.305559992790222167                     WHERE `DisplayID` = 23504;
+UPDATE `creature_model_info` SET `BoundingRadius` = 2                                        WHERE `DisplayID` = 23447;
