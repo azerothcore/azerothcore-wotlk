7d887c97425f04b2762813d02d88b6f86c1435b6
avarishd <46330494+avarishd@users.noreply.github.com>
2024-06-29 15:09:08 +0300

    .
    GO spawn https://github.com/TrinityCore/TrinityCore/commit/e0ccfefb462e045cfaa07353421c4d4e5b02bc49
    
    Co-Authored-By: offl <11556157+offl@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1719517115175571300.sql
+++ data/sql/updates/pending_db_world/rev_1719517115175571300.sql
@@ -1,4 +1,4 @@
--- Finding the Keymaster
+-- Finding the Keymaster.
 DELETE FROM `event_scripts` WHERE `id`=12857;
 INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`) VALUES
 (12857, 0, 10, 19938, 10000, 0, 2248.43, 2227.97, 138.56, 2.48121),
