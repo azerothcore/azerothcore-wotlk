d69fafbc7f9357c4d46b50fd2fef50a8faa38e83
Ludwig <sudlud@users.noreply.github.com>
2024-06-29 13:20:39 +0200

    fix(DB/Creature): remove duplicate 25962 'Fire Eater' spawns

---
+++ data/sql/updates/pending_db_world/rev_1719659662833833500.sql
@@ -0,0 +1,3 @@
+-- remove duplicate 25962 'Fire Eater' spawns
+DELETE FROM `creature` WHERE (`id1` = 25962) AND (`guid` IN (25954, 25957));
+DELETE FROM `game_event_creature` WHERE (`eventEntry` = 1) AND (`guid` IN (25954, 25957));
