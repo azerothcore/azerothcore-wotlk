012b2900b2ee289763c20c4c24761c2cac8b2b17
avarishd <46330494+avarishd@users.noreply.github.com>
2024-06-30 00:49:15 +0300

    fix(Scripts/Shattrath): Daily Dungeon quests holographs

---
+++ data/sql/updates/pending_db_world/rev_1719691851180437100.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_template` SET `ScriptName` = 'npc_shattrath_daily_heroic_quest' WHERE (`entry` = 24854);
+UPDATE `creature_template` SET `ScriptName` = 'npc_shattrath_daily_normal_quest' WHERE (`entry` = 24410);
--- src/server/scripts/Outland/zone_shattrath_city.cpp
+++ src/server/scripts/Outland/zone_shattrath_city.cpp
@@ -16,6 +16,7 @@
  */
 
 #include "CreatureScript.h"
+#include "PassiveAI.h"
 #include "Player.h"
 #include "ScriptedCreature.h"
 #include "ScriptedEscortAI.h"
@@ -253,9 +254,170 @@ public:
     };
 };
 
+enum ShattrathQuests
+{
+    // QuestID : Creature Template ID
+
+    QUEST_H_NAZZAN = 11354, // 24410
+    QUEST_H_KELIDAN = 11362, // 24413
+    QUEST_H_BLADEFIST = 11363, // 24414
+    QUEST_H_QUAG = 11368, // 24419
+    QUEST_H_BLACKSTALKER = 11369, // 24420
+    QUEST_H_WARLORD = 11370, // 24421
+    QUEST_H_IKISS = 11372, // 24422
+    QUEST_H_SHAFFAR = 11373, // 24423
+    QUEST_H_EXARCH = 11374, // 24424
+    QUEST_H_MURMUR = 11375, // 24425
+    QUEST_H_EPOCH = 11378, // 24427
+    QUEST_H_AEONUS = 11382, // 24428
+    QUEST_H_WARP = 11384, // 24431
+    QUEST_H_CALCULATOR = 11386, // 21504
+    QUEST_H_SKYRISS = 11388, // 24435
+    QUEST_H_KAEL = 11499, // 24855
+
+    QUEST_N_CENTURIONS = 11364, // 24411
+    QUEST_N_MYRMIDONS = 11371, // 24415
+    QUEST_N_INSTRUCTORS = 11376, // 24426
+    QUEST_N_LORDS = 11383, // 24429
+    QUEST_N_CHANNELERS = 11385, // 24430
+    QUEST_N_DESTROYERS = 11387, // 24432
+    QUEST_N_SENTINELS = 11389, // 24434
+    QUEST_N_SISTERS = 11500 // 24854
+};
+
+struct npc_shattrath_daily_heroic_quest : public NullCreatureAI
+{
+    npc_shattrath_daily_heroic_quest(Creature* c) : NullCreatureAI(c)
+    {
+        QueryResult result = CharacterDatabase.Query("SELECT `quest_id` FROM `pool_quest_save` WHERE `pool_id` = 356;");
+        if (result)
+        {
+            Field *fields = result->Fetch();
+            int quest_id = fields[0].Get<int>();
+            uint32 templateID;
+
+            switch (quest_id)
+            {
+                case QUEST_H_NAZZAN:
+                    templateID = 24410;
+                    break;
+                case QUEST_H_KELIDAN:
+                    templateID = 24413;
+                    break;
+                case QUEST_H_BLADEFIST:
+                    templateID = 24414;
+                    break;
+                case QUEST_H_QUAG:
+                    templateID = 24419;
+                    break;
+                case QUEST_H_BLACKSTALKER:
+                    templateID = 24420;
+                    break;
+                case QUEST_H_WARLORD:
+                    templateID = 24421;
+                    break;
+                case QUEST_H_IKISS:
+                    templateID = 24422;
+                    break;
+                case QUEST_H_SHAFFAR:
+                    templateID = 24423;
+                    break;
+                case QUEST_H_EXARCH:
+                    templateID = 24424;
+                    break;
+                case QUEST_H_MURMUR:
+                    templateID = 24425;
+                    break;
+                case QUEST_H_EPOCH:
+                    templateID = 24427;
+                    break;
+                case QUEST_H_AEONUS:
+                    templateID = 24428;
+                    break;
+                case QUEST_H_WARP:
+                    templateID = 24431;
+                    break;
+                case QUEST_H_CALCULATOR:
+                    templateID = 21504;
+                    break;
+                case QUEST_H_SKYRISS:
+                    templateID = 24435;
+                    break;
+                case QUEST_H_KAEL:
+                    templateID = 24855;
+                    break;
+                default:
+                    break;
+            }
+
+            if (CreatureTemplate const* ci = sObjectMgr->GetCreatureTemplate(templateID))
+            {
+                CreatureModel const* model = ObjectMgr::ChooseDisplayId(ci);
+                me->SetDisplayId(model->CreatureDisplayID, model->DisplayScale);
+            }
+        }
+    }
+
+    void UpdateAI(uint32 /*diff*/) override {}
+};
+
+struct npc_shattrath_daily_normal_quest : public NullCreatureAI
+{
+    npc_shattrath_daily_normal_quest(Creature* c) : NullCreatureAI(c)
+    {
+        QueryResult result = CharacterDatabase.Query("SELECT `quest_id` FROM `pool_quest_save` WHERE `pool_id` = 357");
+        if (result)
+        {
+            Field *fields = result->Fetch();
+            int quest_id = fields[0].Get<int>();
+            uint32 templateID;
+
+            switch (quest_id)
+            {
+                case QUEST_N_CENTURIONS:
+                    templateID = 24411;
+                    break;
+                case QUEST_N_MYRMIDONS:
+                    templateID = 24415;
+                    break;
+                case QUEST_N_INSTRUCTORS:
+                    templateID = 24426;
+                    break;
+                case QUEST_N_LORDS:
+                    templateID = 24429;
+                    break;
+                case QUEST_N_CHANNELERS:
+                    templateID = 24430;
+                    break;
+                case QUEST_N_DESTROYERS:
+                    templateID = 24432;
+                    break;
+                case QUEST_N_SENTINELS:
+                    templateID = 24434;
+                    break;
+                case QUEST_N_SISTERS:
+                    templateID = 24854;
+                    break;
+                default:
+                    break;
+            }
+
+            if (CreatureTemplate const* ci = sObjectMgr->GetCreatureTemplate(templateID))
+            {
+                CreatureModel const* model = ObjectMgr::ChooseDisplayId(ci);
+                me->SetDisplayId(model->CreatureDisplayID, model->DisplayScale);
+            }
+        }
+    }
+
+    void UpdateAI(uint32 /*diff*/) override {}
+};
+
 void AddSC_shattrath_city()
 {
     new npc_shattrathflaskvendors();
     new npc_zephyr();
     new npc_kservant();
+    RegisterCreatureAI(npc_shattrath_daily_heroic_quest);
+    RegisterCreatureAI(npc_shattrath_daily_normal_quest);
 }
