bce5c504e39c02853dc67403f29b3191db512071
heyitsbench <benjymansy123@gmail.com>
2024-07-01 12:03:42 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/hyjal-boss-speeds.sql
@@ -0,0 +1,10 @@
+-- Anetheron
+UPDATE `creature_template` SET `speed_walk` = 1.6, `speed_run` = 1.714285714285714 WHERE `entry` = 17808;
+-- Archimonde
+UPDATE `creature_template` SET `speed_walk` = 3.2, `speed_run` = 2.285714285714286 WHERE `entry` = 17968;
+-- Azgalor
+UPDATE `creature_template` SET `speed_walk` = 2.4, `speed_run` = 2.142857142857143 WHERE `entry` = 17842;
+-- Kaz'rogal
+UPDATE `creature_template` SET `speed_walk` = 2.4, `speed_run` = 1.714285714285714 WHERE `entry` = 17888;
+-- Rage Winterchill
+UPDATE `creature_template` SET `speed_walk` = 1.6, `speed_run` = 2.285714285714286 WHERE `entry` = 17767;
