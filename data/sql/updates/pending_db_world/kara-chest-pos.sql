4672534e5f6fb4835f8e769e9d083210436a8216
heyitsbench <benjymansy123@gmail.com>
2024-07-04 09:36:40 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/kara-chest-pos.sql
@@ -0,0 +1,1 @@
+UPDATE `gameobject` SET `position_x` = -11102.743, `position_y` = -1848.9752, `position_z` = 221.06969, `orientation` = 5.393069, `rotation2` = -0.43051052, `rotation3` = 0.90258557 WHERE `guid` = 56429 AND `id` = 185119;
