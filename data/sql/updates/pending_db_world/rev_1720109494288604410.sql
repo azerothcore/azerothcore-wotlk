5acbb7d83b9c49bff6c189e2525039e4827d71b4
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-07-04 14:32:26 -0300

    Update data/sql/updates/pending_db_world/rev_1720109494288604410.sql
    Co-authored-by: Kitzunu <24550914+Kitzunu@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1720109494288604410.sql
+++ data/sql/updates/pending_db_world/rev_1720109494288604410.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 128 WHERE `entry` = 8611 AND `name` = 'Idol Room Spawner';
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 128 WHERE `entry` = 8611;
