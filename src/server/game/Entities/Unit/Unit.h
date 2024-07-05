b24e5c6c472ffff6ba6f4587f8a3aa0e0854587d
Anton Popovichenko <anton.popovichenko@mendix.com>
2024-07-05 20:05:33 +0200

    Another codestyle fix

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1572,7 +1572,7 @@ public:
     void ApplyCastTimePercentMod(float val, bool apply);
 
     void SetUInt32Value(uint16 index, uint32 value);
-    
+
     UnitFlags GetUnitFlags() const { return UnitFlags(GetUInt32Value(UNIT_FIELD_FLAGS)); }
     bool HasUnitFlag(UnitFlags flags) const { return HasFlag(UNIT_FIELD_FLAGS, flags); }
     void SetUnitFlag(UnitFlags flags) { SetFlag(UNIT_FIELD_FLAGS, flags); }
