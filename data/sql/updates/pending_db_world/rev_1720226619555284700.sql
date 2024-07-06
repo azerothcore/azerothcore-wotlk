c463d8c54a9f6c272fe0280676703798967afd7c
Gultask <sagemochi@hotmail.com>
2024-07-05 21:50:52 -0300

    no likey comments on end

--- data/sql/updates/pending_db_world/rev_1720226619555284700.sql
+++ data/sql/updates/pending_db_world/rev_1720226619555284700.sql
@@ -1,2 +1,2 @@
---
-UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0 WHERE (`ID` IN (10412, 10414, 10415, 10325, 10326, 10327)); -- was 1
+-- RequiredMinRepValue was set to 1
+UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0 WHERE (`ID` IN (10412, 10414, 10415, 10325, 10326, 10327));
