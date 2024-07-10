77bf2ee4d86cc3a094df32f4a474effc6a5c1895
avarishd <46330494+avarishd@users.noreply.github.com>
2024-07-10 20:57:43 +0300

    sniff
    Co-Authored-By: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1720539450987942100.sql
+++ data/sql/updates/pending_db_world/rev_1720539450987942100.sql
@@ -10,12 +10,13 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 15429;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 15429);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(15429, 0, 0, 0, 38, 0, 100, 0, 1, 1, 60000, 60000, 0, 0, 80, 1542900, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - On Data Set 1 1 - Run Script'),
+(15429, 0, 0, 0, 38, 0, 100, 0, 1, 1, 5000, 5000, 0, 0, 80, 1542900, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - On Data Set 1 1 - Run Script'),
 (15429, 0, 1, 0, 34, 0, 100, 0, 8, 1, 0, 0, 0, 0, 80, 1542901, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - On Reached Point 1 - Run Script');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` IN (1542900,1542901));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(1542900, 9, 0, 0, 0, 0, 100, 0, 1500, 1500, 0, 0, 0, 0, 69, 1, 0, 0, 1, 0, 0, 20, 165578, 10, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Move To Closest \'Dark Iron Ale\''),
-(1542901, 9, 0, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Say Line 0'),
-(1542901, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 1000, 0, 0, 0, 0, 0, 20, 165578, 10, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Despawn In 1000 ms'),
-(1542901, 9, 2, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, 0, 29, 1, 90, 0, 0, 0, 0, 23, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Start Follow Owner Or Summoner');
+(1542900, 9, 0, 0, 0, 0, 100, 0, 500, 500, 0, 0, 0, 0, 69, 1, 0, 0, 1, 0, 0, 20, 165578, 10, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Move To Closest \'Dark Iron Ale\''),
+(1542901, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 5, 33, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Play Emote 33'),
+(1542901, 9, 1, 0, 0, 0, 100, 0, 200, 200, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Say Line 0'),
+(1542901, 9, 2, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 20, 165578, 10, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Despawn In 1000 ms'),
+(1542901, 9, 3, 0, 0, 0, 100, 0, 2500, 2500, 0, 0, 0, 0, 29, 1, 90, 0, 0, 0, 0, 23, 0, 0, 0, 0, 0, 0, 0, 0, 'Disgusting Oozeling - Actionlist - Start Follow Owner Or Summoner');
