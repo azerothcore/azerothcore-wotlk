fa803deb7f6a075a888dfcbc908d209ac02a1ac0
heyitsbench <benjymansy123@gmail.com>
2024-07-12 08:56:43 -0400

    Whoops.

--- data/sql/updates/pending_db_world/hyjal-trash-adjustments.sql
+++ data/sql/updates/pending_db_world/hyjal-trash-adjustments.sql
@@ -1,6 +1,6 @@
-UPDATE `creature_template` SET `speed_walk` = 1.2 WHERE `entry` IN (17895, 17897, 17898, 17899, 17905, 17916); -- Swim speed = 4.72221994400024414, flight speed = 7
-UPDATE `creature_template` SET `speed_walk` = 1.6 WHERE `entry` IN (17906, 17907, 17908);
-UPDATE `creature_template` SET `speed_run` = 1.428571428571429 WHERE `entry` IN (17895, 17897, 17898, 17899, 17905, 17916);
+UPDATE `creature_template`   SET `speed_walk`     = 1.2                                        WHERE `entry`     IN (17895, 17897, 17898, 17899, 17905, 17916); -- Swim speed = 4.72221994400024414, flight speed = 7
+UPDATE `creature_template`   SET `speed_walk`     = 1.6                                        WHERE `entry`     IN (17906, 17907, 17908);
+UPDATE `creature_template`   SET `speed_run`      = 1.428571428571429                          WHERE `entry`     IN (17895, 17897, 17898, 17899, 17905, 17916);
 UPDATE `creature_template`   SET `speed_run`      = 1.714285714285714                          WHERE `entry`     IN (17906, 17908);
 UPDATE `creature_template`   SET `speed_run`      = 2.571428571428571                          WHERE `entry`     =  17907;
 UPDATE `creature_template`   SET `BaseAttackTime` = 2000                                       WHERE `entry`     IN (17895, 17897, 17898, 17899, 17905, 17906, 17907, 17908, 17916);
