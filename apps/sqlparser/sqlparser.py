e6b58e22aebc75b359b9ac0dfbc86dde0ec8e179
slavanorm <48209342+slavanorm@users.noreply.github.com>
2024-07-12 19:01:39 +0400

    Update sqlparser.py

--- apps/sqlparser/sqlparser.py
+++ apps/sqlparser/sqlparser.py
@@ -2,176 +2,193 @@ import argparse
 import shutil
 import csv
 import sys
+import os
 
 from datetime import datetime
 from pathlib import Path
 from collections import Counter
 
-# constants, can be overriden using arguments
 
-## implied folder structure: modules/*/sql/dbname/*/*.sql
-glob = "**/sql/**/*.sql"
+def collect_sql_files(
+    full_filepath: Path,
+    source_dir: Path,
+    target_dir: Path,
+    counter: dict,
+    dry: bool = False,
+    files_to_ignore: list[Path|str] | None = None,
+    modules_to_ignore: list[str] | None = None,
+):
+    """
+    collects a file from full_filepath.
+    updates counter.
+    prints action taken.
+    picks up some info from script's filepath:
+        assumed [filepath] structure:
+            [module]/*/sql/[database]/[*other_folders]/[filename].sql
+    output file path:
+        [target_dir]/[database]/[module]_[counter['written']]_[*other_folders]_[filename].sql
+    Returns:
+        dict, a row for report.
+    """
 
-repo_folder = Path("/Users/v0/Py/_hobby/wow/server")
+    def get_db_name(db_name_to_parse: str) -> Path:
+        if db_name_to_parse.startswith("db"):
+            # assumes db_*.sql or db-*.sql filename
+            db_name_to_parse = db_name_to_parse[3:]
+        return Path(f"db_{db_name_to_parse}")
 
+    filepath = full_filepath.relative_to(source_dir)
 
-default_argparsed_args = ["--help"]
-# default_argparsed_args = "-y", ""  # , "--glob", "test"
-
-source_suffix = "modules"
-source_dir = lambda: repo_folder / source_suffix
-target_suffix = "data/sql/custom"
-target_dir = lambda: repo_folder / target_suffix
-report_suffix = "modules_sql_parsed.csv"
-report_path = lambda: repo_folder / report_suffix
-
-modules_to_ignore = ["mod-playerbots"]
-
-counters = dict(
-    written=0, skipped=0, total=0, modules=Counter()  # module: counter
-)
-
-
-def get_db_name(arg):
-    if arg.startswith("db"):
-        # assumes db_*.sql or db-*.sql filename
-        arg = arg[3:]
-    return f"db_{arg}"
-
-
-def collect_sql_files(full_fp, files_to_ignore=None, dry=False):
-    # fp means filepath
-    global counters
-
-    fp = full_fp.relative_to(source_dir())
-
-    module = fp.parents[-2]
-    path_to_parse = fp.parts[fp.parts.index("sql") + 1 :]
+    module = filepath.parents[-2]
+    path_to_parse = filepath.parts[filepath.parts.index("sql") + 1 :]
     database = get_db_name(path_to_parse[0])
     other_folders = "".join(path_to_parse[1:-1])
 
-    target_file = (
-        target_dir()
-        / database
-        / f"{module}_{counters['written']}_{other_folders}_{fp.parts[-1]}"
-    )
-    fp_out = target_file.relative_to(target_dir())
+    fp_out = database / f"{module}_{counter['written']}_{other_folders}_{filepath.parts[-1]}"
 
-    if files_to_ignore and str(fp) in files_to_ignore:
-        counters["skipped"] += 1
+    target_file = target_dir / fp_out
+
+    if files_to_ignore and str(filepath) in files_to_ignore:
+        counter["skipped"] += 1
         print(f"skipped existing module:{module} name:{fp_out} ")
+        return
+    elif modules_to_ignore and str(module) in modules_to_ignore:
+        counter["skipped"] += 1
+        print(f"skipped ignored module:{module} name:{fp_out} ")
         return
 
     if not dry:
         # required by shutil.copy2
         target_file.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copy2(full_fp, target_file)
+        shutil.copy2(full_filepath, target_file)
 
     result = dict(
-        index=counters["total"],
-        module_index=counters["modules"][str(module)],
+        index=counter["total"],
+        module_index=counter["modules"][str(module)],
         module=module,
         database=database,
         applied=datetime.now().isoformat(timespec="seconds"),
-        fp_in=fp,
+        fp_in=filepath,
         fp_out=fp_out,
-        target_dir=target_dir(),
+        target_dir=target_dir,
     )
-    counters["written"] += 1
-    counters["total"] += 1
-    counters["modules"][str(module)] += 1
+    counter["written"] += 1
+    counter["total"] += 1
+    counter["modules"][str(module)] += 1
     print(f"parsed module:{module} name:{fp_out} ")
     return result
 
 
-def write_report(data):
-    with report_path().open("w") as csvfile:
+def write_report(data: list[dict], report_dir):
+    """Dump report to csv"""
+    data = [e for e in data if e is not None]
+    if not data:
+        return
+    with report_dir.open("w") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
         writer.writeheader()
         writer.writerows(data)
 
 
-def read_report():
-    # reads report, updates counters, returns report and files_to_ignore
-    global counters
-    if not report_path().exists():
-        return None, None
+def read_report(report_dir: Path):
+    """
+    Reads csv report
+    Returns:
+        report:list[dict] for debugging
+        files_to_ignore:list of already parsed files
+        counter:Counter with report stats
+    """
+    counter: dict = Counter(dict(written=0, skipped=0, total=0, modules=Counter()))
 
-    with report_path().open() as csvfile:
+    if not report_dir.exists():
+        return None, None, counter
+
+    with report_dir.open() as csvfile:
         reader = csv.DictReader(csvfile)
         report = list(reader)
 
-    counters["total"] = len(report)
+    counter["total"] = len(report)
 
     files_to_ignore = []
     for e in report:
         files_to_ignore.append(e["fp_in"])
-        counters["modules"][e["module"]] += 1
+        counter["modules"][e["module"]] += 1
+    return report, files_to_ignore, counter
 
-    return report, files_to_ignore
 
+def main(
+    dry_run: bool = False,
+    glob: str = "**/sql/**/*.sql",
+    modules_to_ignore: list = ["mod-playerbots"],
+    repo_folder: Path | str = Path(__file__).parents[2],
+    source_path: Path | str = "modules",
+    target_path: Path | str = "data/sql/custom",
+    report_path: Path | str = "apps/sqlparser/modules_sql_parsed.csv",
+    **kwargs,
+):
+    """control flow for the whole script"""
+    repo_folder = Path(repo_folder)
+    source_dir = repo_folder / source_path
+    target_dir = repo_folder / target_path
+    report_dir = repo_folder / report_path
 
-def main(dry=False):
-    report, files_to_ignore = read_report()
-    collected = [
-        collect_sql_files(e, files_to_ignore, dry)
-        for e in source_dir().glob(glob)
+    report, files_to_ignore, counter = read_report(report_dir)
+    ## implied folder structure: modules/*/sql/dbname/*/*.sql
+    report = [
+        collect_sql_files(
+            e,
+            source_dir,
+            target_dir,
+            counter,
+            dry_run,
+            files_to_ignore,
+            modules_to_ignore,
+        )
+        for e in source_dir.glob(glob)
     ]
-    collected = [e for e in collected if e is not None]
-    if not dry and collected:
-        write_report(collected)
+    if not dry_run:
+        write_report(report, report_dir)
 
-    print("report stats", counters)
+    print("report stats", counter)
 
 
-def argparsed():
-    global glob, repo_folder, source_suffix, target_suffix, report_suffix
-
-    default = ["--help"]
-    # default = "-y", "", "--glob", "test"
+def argparsed(default_args=["--help"]):
+    """wrap main function for CLI invocation"""
     parser = argparse.ArgumentParser(
-        description="Collect and copy SQL files to new folder, generate report. Skips collecting pulled updates based on report."
+        description="""
+        Collect and copy SQL files to new folder, generate report. 
+        Skips collecting pulled updates based on report.""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument(
-        "option",
-        nargs="?",
-        choices=["dry-run", "run", None],
-        help="Simulate without changes OR run parser",
+    parser.add_argument("-d", "--dry_run", action="store_true", help="Simulate without changes")
+    parser.add_argument("-r", "--run", action="store_true", help="Run")
+    parser.add_argument("-y", "--force", action="store_true", help="Run without confirmation")
+
+    main_params = dict(
+        glob=dict(help="glob pattern to find scripts", default="**/sql/**/*.sql"),
+        source_path=dict(help="location of modules folder, relative to repo", default="modules"),
+        target_path=dict(
+            help="location of target folder, relative to repo", default="data/sql/custom"
+        ),
+        modules_to_ignore=dict(help="list of modules to ignore", default=["mod-playerbots"]),
     )
-    parser.add_argument("-y", nargs="?", help="Run without confirmation")
+    for k, v in main_params.items():
+        parser.add_argument(f"--{k}", **v)
 
-    globals_to_parse = [
-        ["glob", "glob pattern to find scripts"],
-        ["repo_folder", "repository folder"],
-        ["source_suffix", "location of modules folder, appends to repo_folder"],
-        ["target_suffix", "location of target folder, appends to repo_folder"],
-        ["report_suffix", "location of module results, appends to repo_folder"],
-        ["modules_to_ignore", "list of modules to ignore"],
-    ]
+    args = parser.parse_args(args=None if sys.argv[1:] else default_args)
 
-    for e in globals_to_parse[:-1]:
-        parser.add_argument(f"--{e[0]}", help=e[1])
+    if args.run or args.force or args.dry_run:
+        if args.run:
+            prompt = "This script will copy SQL files. are you sure? [y/N]\n"
+            if input(prompt).lower() != "y":
+                print("Aborting")
+                return
+        kwargs = {
+            e: os.getenv(f"AC_SQLPARSER_{e.upper()}", getattr(args, e)) for e in main_params.keys()
+        }
+        kwargs = {k: v for k, v in kwargs.items() if v is not None}
 
-    e = globals_to_parse[-1]
-    parser.add_argument(f"--{e[0]}", help=e[1], nargs="+")
-
-    args = parser.parse_args(
-        args=None if sys.argv[1:] else default_argparsed_args
-    )
-
-    if args.option == "run":
-        prompt = "This script will copy SQL files. are you sure? [y/N]\n"
-        if input(prompt).lower() != "y":
-            print("Aborting")
-            return
-    elif args.option == "--help":
-        return
-    for e in globals_to_parse:
-        value = getattr(args, e[0])
-        if value:
-            globals()[e[0]] = value
-
-    main(args.option == "dry-run")
+        main(**kwargs)
 
 
 if __name__ == "__main__":
