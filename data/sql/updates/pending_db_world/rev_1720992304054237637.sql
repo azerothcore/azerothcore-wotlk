a1d0ee49b06f98bde0f63b18d1077877d5ba1848
zer0pwn <zer0pwn@outlook.com>
2024-07-14 18:45:32 -0700

    fix(DB/Creature): fix spout emote text for The Lurker Below

---
+++ data/sql/updates/pending_db_world/rev_1720992304054237637.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_text` SET `Text` = '%s takes a deep breath!' WHERE `creatureId` = 21217;
+UPDATE `broadcast_text` SET `MaleText` = '%s takes a deep breath!' WHERE `ID` = '20021';
