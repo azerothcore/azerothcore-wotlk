75e315853fd76597e3cb0916804cc21b9fdb60d2
zer0pwn <zer0pwn@outlook.com>
2024-07-14 19:07:22 -0700

    Update data/sql/updates/pending_db_world/rev_1720992304054237637.sql
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1720992304054237637.sql
+++ data/sql/updates/pending_db_world/rev_1720992304054237637.sql
@@ -1,3 +1,2 @@
 --
-UPDATE `creature_text` SET `Text` = '%s takes a deep breath!' WHERE `creatureId` = 21217;
-UPDATE `broadcast_text` SET `MaleText` = '%s takes a deep breath!' WHERE `ID` = '20021';
+UPDATE `creature_text` SET `Text` = '%s takes a deep breath!', `BroadcastTextId` = 0 WHERE `creatureId` = 21217;
