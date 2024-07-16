2008d64e7b9e1d27d1e8c161a026d06a2f7b7e72
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-07-16 03:22:23 +0200

    fix(Scripts/Commands): Ticket response (#19406)
    * fix(Scripts/Commands): Ticket response
    
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/19390
    
    * Implements two new commands
        * ticket response delete - Delete a ticket response
        * ticket response show - Show current ticket response
    
    * this better
    
    * English?
    
    * bette rlogic
    
    * Update cs_ticket.cpp

---
+++ data/sql/updates/pending_db_world/rev_1721087507268967000.sql
@@ -0,0 +1,14 @@
+--
+DELETE FROM `acore_string` WHERE `entry` IN (2031,2032);
+INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
+(2031, '|cff00ff00Response Appended|r:|cff00ccff [%s]|r'),
+(2032, '|cff00ff00Response deleted by|r:|cff00ccff %s|r');
+
+UPDATE `command` SET `help` = 'Add a response to a new line.\n\nSyntax: ticket response appendln $ticketId $response' WHERE `name` = 'ticket response appendln';
+UPDATE `command` SET `help` = 'Add a response\n\nSyntax: ticket response append $ticketId $response' WHERE `name` = 'ticket response append';
+
+DELETE FROM `command` WHERE `name` = 'ticket response delete';
+DELETE FROM `command` WHERE `name` = 'ticket response show';
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('ticket response delete', 2, 'Delete a ticket response\n\nSyntax: ticket response delete $ticketId'),
+('ticket response show', 2, 'Show a ticket response\n\nSyntax: ticket response show $ticketId');
