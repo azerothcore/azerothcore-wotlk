ab2dd8495d9eb471aad7e8528caedb35ab5ed39b
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-07-16 02:26:55 +0200

    English?

--- data/sql/updates/pending_db_world/rev_1721087507268967000.sql
+++ data/sql/updates/pending_db_world/rev_1721087507268967000.sql
@@ -1,7 +1,7 @@
 --
 DELETE FROM `acore_string` WHERE `entry` IN (2031,2032);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(2031, '|cff00ff00Responde Appended|r:|cff00ccff [%s]|r'),
+(2031, '|cff00ff00Response Appended|r:|cff00ccff [%s]|r'),
 (2032, '|cff00ff00Response deleted by|r:|cff00ccff %s|r');
 
 UPDATE `command` SET `help` = 'Add a response to a new line.\n\nSyntax: ticket response appendln $ticketId $response' WHERE `name` = 'ticket response appendln';
