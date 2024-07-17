aabec7c8be7a6c6356bbb27216c946ec4f7901eb
Gultask <100873791+Gultask@users.noreply.github.com>
2024-07-17 07:31:18 -0300

    fix(DB/Creature): Small Black Temple adjustments (#19420)
    * Create rev_1721186384223205700.sql
    
    * Update rev_1721186384223205700.sql

---
+++ data/sql/updates/pending_db_world/rev_1721186384223205700.sql
@@ -0,0 +1,10 @@
+-- Ashtongue Feral Spirit can't see through Invisiblity
+UPDATE `creature_template_addon` SET `auras` = '18950' WHERE (`entry` = 22849);
+-- Illidari Defiler missing Interrupt Immunity
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|256|4194304|33554432 WHERE (`entry` = 22853);
+-- Illidari Boneslicer missing Stun immunity
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|256|2048|4194304|33554432 WHERE (`entry` = 22869);
+-- Illidari Heartseeker missing Stun immunity
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|256|2048|4194304|33554432 WHERE (`entry` = 23339);
+-- Illidari Nightlord missing Taunt immunity
+UPDATE `creature_template` SET `flags_extra` = `flags_extra`|256 WHERE (`entry` = 22855);
