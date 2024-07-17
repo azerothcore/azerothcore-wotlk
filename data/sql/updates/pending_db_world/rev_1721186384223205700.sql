ac2f5060e2499b553fb761e22e22bfc3d25b4635
Gultask <100873791+Gultask@users.noreply.github.com>
2024-07-17 00:30:58 -0300

    Update rev_1721186384223205700.sql

--- data/sql/updates/pending_db_world/rev_1721186384223205700.sql
+++ data/sql/updates/pending_db_world/rev_1721186384223205700.sql
@@ -1,7 +1,7 @@
 -- Ashtongue Feral Spirit can't see through Invisiblity
 UPDATE `creature_template_addon` SET `auras` = '18950' WHERE (`entry` = 22849);
 -- Illidari Defiler missing Interrupt Immunity
-UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|256|2048|4194304|33554432 WHERE (`entry` = 22853);
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|256|4194304|33554432 WHERE (`entry` = 22853);
 -- Illidari Boneslicer missing Stun immunity
 UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1|256|2048|4194304|33554432 WHERE (`entry` = 22869);
 -- Illidari Heartseeker missing Stun immunity
