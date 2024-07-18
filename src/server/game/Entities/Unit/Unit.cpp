05545fe6d155a211379e98ed27693fc9a105c07d
heyitsbench <benjymansy123@gmail.com>
2024-07-18 19:46:41 -0400

    Init.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -3051,10 +3051,12 @@ void Unit::SendMeleeAttackStop(Unit* victim)
 
     WorldPacket data(SMSG_ATTACKSTOP, (8 + 8 + 4));
     data << GetPackGUID();
+    uint8 nowDead = victim->isDead();
+
     if (victim)
     {
         data << victim->GetPackGUID();
-        data << victim->isDead();
+        data << nowDead;
     }
     SendMessageToSet(&data, true);
     LOG_DEBUG("entities.unit", "WORLD: Sent SMSG_ATTACKSTOP");
