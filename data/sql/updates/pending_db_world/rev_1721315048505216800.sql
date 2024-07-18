0305fc08ec6c2dab7d349c97e2a7c33e64efddd0
Gultask <sagemochi@hotmail.com>
2024-07-18 20:04:07 -0300

    shadowremove

--- data/sql/updates/pending_db_world/rev_1721315048505216800.sql
+++ data/sql/updates/pending_db_world/rev_1721315048505216800.sql
@@ -8,3 +8,7 @@ INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `Verified
 (23109, 6, 40322, 0);
 
 UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_vengeful_spirit' WHERE (`entry` = 23109);
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = 41999 AND `ScriptName` = 'spell_teron_gorefiend_shadow_of_death_remove';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(41999, 'spell_teron_gorefiend_shadow_of_death_remove');
