ca931f800ff8e20302e21efcb305deef69ecdf68
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-07-20 06:58:05 +0200

    fmt

--- data/sql/updates/pending_db_world/rev_1721436522058676600.sql
+++ data/sql/updates/pending_db_world/rev_1721436522058676600.sql
@@ -196,3 +196,9 @@ UPDATE `acore_string` SET `locale_ruRU` = REPLACE(locale_ruRU, '%-10s', '{}');
 UPDATE `acore_string` SET `locale_ruRU` = REPLACE(locale_ruRU, '%6u', '{}');
 UPDATE `acore_string` SET `locale_ruRU` = REPLACE(locale_ruRU, '%8u', '{}');
 UPDATE `acore_string` SET `locale_ruRU` = REPLACE(locale_ruRU, '%10u', '{}');
+
+-- Only case I found that got broke. Due to these being sent to the client via WorldPacket
+DELETE FROM `acore_string` WHERE `entry` IN (6613,6615);
+INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
+(6613, '|cfff00000[GM Announcement]: %s|r', NULL, NULL, '|cfff00000[GM Ankündigung von [%s]]: %s|r', '|cfff00000[管理员公告]: %s|r', NULL, NULL, NULL, NULL),
+(6615, '|cffffff00[|c1f40af20GM Announce by|r |cffff0000%s|cffffff00]:|r %s|r', NULL, NULL, '|cffffff00[|c1f40af20GM Ankündigung von|r |cffff0000%s|cffffff00]:|r %s|r', '|cffffff00[|c1f40af20管理员广播|r |cffff0000%s|cffffff00]:|r %s|r', NULL, NULL, NULL, NULL);
