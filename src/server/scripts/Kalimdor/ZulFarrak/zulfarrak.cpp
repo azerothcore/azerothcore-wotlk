14c31480d7facccae3cbf256235692423b0c1f36
FrancescoBorzi <borzifrancesco@gmail.com>
2024-07-21 21:13:55 +0200

    chore: wip

--- src/server/scripts/Kalimdor/ZulFarrak/zulfarrak.cpp
+++ src/server/scripts/Kalimdor/ZulFarrak/zulfarrak.cpp
@@ -28,6 +28,13 @@
 #include "ScriptSystem.h"
 #include "ScriptedCreature.h"
 #include "ScriptedGossip.h"
+
+/// @todo: this import is not necessary for compilation and marked as unused by the IDE
+//  however, for some reasons removing it would cause a damn linking issue
+//  there is probably some underlying problem with imports which should properly addressed
+//  see: https://github.com/azerothcore/azerothcore-wotlk/issues/9766
+#include "GridNotifiersImpl.h"
+
 /* ScriptData
 SDName: Zulfarrak
 SD%Complete: 50
