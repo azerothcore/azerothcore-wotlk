1c9fc71dd4fc7e543f3fa4cc845f7b0dc01ffa3d
Jelle Meeus <sogladev@gmail.com>
2024-07-27 12:54:59 +0200

    boss_halion aura:spell_halion_twilight_realm_aura

--- data/sql/updates/pending_db_world/rev_1722077567551108843.sql
+++ data/sql/updates/pending_db_world/rev_1722077567551108843.sql
@@ -7,3 +7,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_halion_soul_consumption_aura
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_fiery_combustion_aura' WHERE `spell_id`=74562 AND `ScriptName`='spell_halion_fiery_combustion';
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_mark_of_combustion_aura' WHERE `spell_id`=74567 AND `ScriptName`='spell_halion_mark_of_combustion';
 UPDATE `spell_script_names` SET `ScriptName`='spell_halion_mark_of_consumption_aura' WHERE `spell_id`=74795 AND `ScriptName`='spell_halion_mark_of_consumption';
+UPDATE `spell_script_names` SET `ScriptName`='spell_halion_twilight_realm_aura' WHERE `spell_id`=74807 AND `ScriptName`='spell_halion_twilight_realm';
--- src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
+++ src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
@@ -1089,36 +1089,30 @@ class spell_halion_twilight_phasing_aura : public AuraScript
     }
 };
 
-class spell_halion_twilight_realm : public SpellScriptLoader
+class spell_halion_twilight_realm_aura : public AuraScript
 {
-public:
-    spell_halion_twilight_realm() : SpellScriptLoader("spell_halion_twilight_realm") { }
+    PrepareAuraScript(spell_halion_twilight_realm_aura);
 
-    class spell_halion_twilight_realm_AuraScript : public AuraScript
+    bool Validate(SpellInfo const* /*spellInfo*/) override
     {
-        PrepareAuraScript(spell_halion_twilight_realm_AuraScript);
+        return ValidateSpellInfo({ SPELL_FIERY_COMBUSTION });
+    }
 
-        void OnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*handle*/)
-        {
-            Unit* target = GetTarget();
-            if (!target)
-                return;
-
-            target->RemoveAurasDueToSpell(SPELL_FIERY_COMBUSTION, ObjectGuid::Empty, 0, AURA_REMOVE_BY_ENEMY_SPELL);
-            if (GetTarget()->GetTypeId() != TYPEID_PLAYER)
-                return;
-            GetTarget()->m_Events.AddEvent(new SendEncounterUnit(GetTarget()->ToPlayer()), GetTarget()->m_Events.CalculateTime(500));
-        }
-
-        void Register() override
-        {
-            AfterEffectApply += AuraEffectApplyFn(spell_halion_twilight_realm_AuraScript::OnApply, EFFECT_0, SPELL_AURA_PHASE, AURA_EFFECT_HANDLE_REAL);
-        }
-    };
-
-    AuraScript* GetAuraScript() const override
+    void OnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*handle*/)
     {
-        return new spell_halion_twilight_realm_AuraScript();
+        Unit* target = GetTarget();
+        if (!target)
+            return;
+
+        target->RemoveAurasDueToSpell(SPELL_FIERY_COMBUSTION, ObjectGuid::Empty, 0, AURA_REMOVE_BY_ENEMY_SPELL);
+        if (GetTarget()->GetTypeId() != TYPEID_PLAYER)
+            return;
+        GetTarget()->m_Events.AddEvent(new SendEncounterUnit(GetTarget()->ToPlayer()), GetTarget()->m_Events.CalculateTime(500));
+    }
+
+    void Register() override
+    {
+        AfterEffectApply += AuraEffectApplyFn(spell_halion_twilight_realm_aura::OnApply, EFFECT_0, SPELL_AURA_PHASE, AURA_EFFECT_HANDLE_REAL);
     }
 };
 
@@ -1401,7 +1395,7 @@ void AddSC_boss_halion()
     RegisterSpellScriptWithArgs(spell_halion_damage_aoe_summon, "spell_halion_consumption_summon", SPELL_SOUL_CONSUMPTION_EXPLOSION, SPELL_CONSUMPTION_DAMAGE_AURA);
     RegisterSpellScript(spell_halion_clear_debuffs);
     RegisterSpellAndAuraScriptPair(spell_halion_twilight_phasing, spell_halion_twilight_phasing_aura);
-    new spell_halion_twilight_realm();
+    RegisterSpellScript(spell_halion_twilight_realm_aura);
     new spell_halion_leave_twilight_realm();
     new spell_halion_twilight_cutter_periodic();
     new spell_halion_twilight_cutter();
