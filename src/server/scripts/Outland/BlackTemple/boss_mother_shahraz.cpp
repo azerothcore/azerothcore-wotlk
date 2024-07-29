50d9d3017793d6db81d2d6508efe3ee2570bbcbe
Gultask <sagemochi@hotmail.com>
2024-07-29 18:56:30 -0300

    init

--- src/server/scripts/Outland/BlackTemple/boss_mother_shahraz.cpp
+++ src/server/scripts/Outland/BlackTemple/boss_mother_shahraz.cpp
@@ -110,7 +110,7 @@ struct boss_mother_shahraz : public BossAI
         me->m_Events.AddEventAtOffset([&] {
             DoCastSelf(SPELL_ENRAGE, true);
             Talk(SAY_ENRAGE);
-        }, 10min);
+        }, 10min, GROUP_ENRAGE);
     }
 
     void KilledUnit(Unit* /*victim*/) override
