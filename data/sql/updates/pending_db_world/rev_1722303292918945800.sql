3139162694b16e14b056e10df8ef191af493775b
Gultask <sagemochi@hotmail.com>
2024-07-29 22:35:20 -0300

    Create rev_1722303292918945800.sql

---
+++ data/sql/updates/pending_db_world/rev_1722303292918945800.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_proc_event` WHERE `entry` = 40484;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(40484, 0, 0, 0, 0, 0, 0, 67, 0, 0, 0, 0);
