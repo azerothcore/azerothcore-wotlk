d69a8227b0c11f451c641e0ff1bc25422c2a0df3
kasvati <sontag.adam.99@gmail.com>
2024-08-01 12:09:26 +0200

    added sql statement

--- data/sql/updates/pending_db_world/rev_1722505977523322600.sql
+++ data/sql/updates/pending_db_world/rev_1722505977523322600.sql
@@ -1,1 +1,4 @@
---
+-- Added missing MaleText
+DELETE FROM `broadcast_text` WHERE (`ID` = 12690);
+INSERT INTO `broadcast_text` (`ID`, `LanguageID`, `MaleText`, `FemaleText`, `EmoteID1`, `EmoteID2`, `EmoteID3`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `SoundEntriesId`, `EmotesID`, `Flags`, `VerifiedBuild`) VALUES
+(12690, 0, '%s shimmers and becomes intangible!', '%s shimmers and becomes intangible!', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019);
