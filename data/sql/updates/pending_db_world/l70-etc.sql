8653888c0d639b90ba23e4198ff4c8eb969fc55a
heyitsbench <benjymansy123@gmail.com>
2024-08-04 00:21:57 -0400

    Whoops.

--- data/sql/updates/pending_db_world/l70-etc.sql
+++ data/sql/updates/pending_db_world/l70-etc.sql
@@ -224,7 +224,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 28210) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (28210, 0, 0, 1, 62, 0, 100, 0, 9667, 0, 0, 0, 0, 72, 0,       0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Ognip Blastbolt - On Gossip Option 0 Selected - Close Gossip'),
-(28210, 0, 1, 2, 61, 0, 100, 0, 0,    0, 0, 0, 0, 80, 2385000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ognip Blastbolt - On Gossip Option 0 Selected - Run Script');
+(28210, 0, 1, 2, 61, 0, 100, 0, 0,    0, 0, 0, 0, 80, 2385000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ognip Blastbolt - On Gossip Option 0 Selected - Run Script'),
 (28210, 0, 2, 3, 61, 0, 100, 0, 9667, 0, 0, 0, 0, 1,  0,       0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ognip Blastbolt - On Gossip Option 0 Selected - Say Line 0'),
 (28210, 0, 3, 0, 61, 0, 100, 0, 0,    0, 0, 0, 0, 80, 2821000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ognip Blastbolt - On Gossip Option 0 Selected - Run Script'),
 (28210, 0, 4, 0, 68, 0, 100, 0, 81,   0, 0, 0, 0, 80, 2821000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ognip Blastbolt - On Game Event 81 Started - Run Script');
