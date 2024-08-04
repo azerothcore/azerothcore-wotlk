a76b96652d89228343e77e150422b6f4f90e0c3d
kasvati <sontag.adam.99@gmail.com>
2024-08-04 13:46:35 +0200

    Changed implementation from broadcast_text to creature_text

--- data/sql/updates/pending_db_world/rev_1722505977523322600.sql
+++ data/sql/updates/pending_db_world/rev_1722505977523322600.sql
@@ -1,4 +1,6 @@
--- Added missing MaleText
-DELETE FROM `broadcast_text` WHERE (`ID` = 12690);
-INSERT INTO `broadcast_text` (`ID`, `LanguageID`, `MaleText`, `FemaleText`, `EmoteID1`, `EmoteID2`, `EmoteID3`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `SoundEntriesId`, `EmotesID`, `Flags`, `VerifiedBuild`) VALUES
-(12690, 0, '%s shimmers and becomes intangible!', '%s shimmers and becomes intangible!', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019);
+-- Removes creature reference to broadcast_text
+DELETE FROM `creature_text` WHERE `CreatureID` IN (16325, 16326);
+
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(16325, 0, 0, '%s shimmers and becomes intangible!', 16, 0, 100, 0, 0, 0, 0, 0, 'Quel\'dorei Ghost'),
+(16326, 0, 0, '%s shimmers and becomes intangible!', 16, 0, 100, 0, 0, 0, 0, 0, 'Quel\'dorei Wraith');
