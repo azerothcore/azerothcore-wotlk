a13410cf2530dc068f9ceb8cf682dfc09ebcf3b6
heyitsbench <benjymansy123@gmail.com>
2024-08-05 23:27:43 -0400

    Init values.

---
+++ data/sql/updates/pending_db_world/black-temple-values.sql
@@ -0,0 +1,920 @@
+1420
+speed_walk 2.5
+speed_run 6
+display 901
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.200000002980232238
+combatreach 1
+
+2914
+speed_walk 2.5
+speed_run 6
+display 6303
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.349999994039535522
+combatreach 0.5
+
+14881
+speed_walk 2.5
+speed_run 6
+display 9829
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.187000006437301635
+combatreach 0.150000005960464477
+
+22515
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 16925
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+22517
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 16925
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+22841
+speed_walk 2
+speed_run 0.039999999105930328
+display 21357
+baseattacktime 1.8k
+rangeattacktime 1.8k
+boundingradius 5
+combatreach 5
+
+22844
+speed_walk 2.5
+speed_run 10
+display 21115
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2
+combatreach 0
+
+22845
+speed_walk 2.5
+speed_run 7
+display 21116
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.799999952316284179
+combatreach 0
+
+22846
+speed_walk 2.5
+speed_run 7
+display 21118
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.799999952316284179
+combatreach 0
+
+22847
+speed_walk 2.5
+speed_run 7
+display 21117
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2
+combatreach 0
+
+22848
+speed_walk 2.5
+speed_run 8
+display 5187
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1
+combatreach 1.25
+
+22849
+speed_walk 5
+speed_run 8
+display 21114
+baseattacktime 1k
+rangeattacktime 1k
+boundingradius 0.521999955177307128
+combatreach 1.5
+
+22853
+speed_walk 5
+speed_run 10
+display 11335
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.899999976158142089
+combatreach 2.5
+
+22855
+speed_walk 5
+speed_run 12
+display 19991
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 4
+combatreach 6
+
+22856
+speed_walk 4
+speed_run 12
+display 21146
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.300000011920928955
+combatreach 1
+
+22869
+speed_walk 5
+speed_run 10
+display 21120
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.899999976158142089
+combatreach 2.5
+
+22871
+speed_walk 4
+speed_run 12
+display 21262
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2.5
+combatreach 3.75
+
+22873
+speed_walk 4
+speed_run 12
+display 21159
+baseattacktime 3k
+rangeattacktime 3k
+boundingradius 0.868054986000061035
+combatreach 3.75
+
+22874
+speed_walk 4
+speed_run 12
+display 21161
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.781249523162841796
+combatreach 3.375
+
+22875
+speed_walk 4
+speed_run 12
+display 21162
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2
+combatreach 2
+
+22876
+speed_walk 4
+speed_run 12
+display 21164
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.799999952316284179
+combatreach 1.799999952316284179
+
+22877
+speed_walk 4
+speed_run 12
+display 21164
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.799999952316284179
+combatreach 1.799999952316284179
+
+22878
+speed_walk 4
+speed_run 12
+display 20609
+baseattacktime 1.5k
+rangeattacktime 1.5k
+boundingradius 1.527799963951110839
+combatreach 5
+
+22879
+speed_walk 2.5
+speed_run 8
+display 21370
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.520833015441894531
+combatreach 2.25
+
+22880
+speed_walk 2.5
+speed_run 8
+display 21367
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.607638478279113769
+combatreach 2.625
+
+-- 22881
+
+22882
+speed_walk 2.5
+speed_run 8
+display 21373
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.502200007438659667
+combatreach 2.02500009536743164
+
+22883
+speed_walk 4
+speed_run 12
+display 5492
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.213891997933387756
+combatreach 0.699999988079071044
+
+22884
+speed_walk 4
+speed_run 12
+display 17528
+baseattacktime 1.2k
+rangeattacktime 1.2k
+boundingradius 3
+combatreach 4.199999809265136718
+
+22885
+speed_walk 4
+speed_run 7
+display 21457
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.699999988079071044
+combatreach 1.60000002384185791
+
+22886
+speed_walk 2.5
+speed_run 8
+display 18790
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.347000002861022949
+combatreach 1.5
+
+22887
+speed_walk 4
+speed_run 12
+display 21174
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2.10000014305114746
+combatreach 10.5
+
+22898
+speed_walk 6
+speed_run 15
+display 21145
+baseattacktime 1.5k
+rangeattacktime 1.5k
+boundingradius 80
+combatreach 24
+
+22917
+speed_walk 4
+speed_run 15
+display 21135
+baseattacktime 1.5k
+rangeattacktime 1.5k
+boundingradius 0.458999991416931152
+combatreach 7.5
+
+22939
+speed_walk 2.5
+speed_run 10
+display 21449
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.517049968242645263
+combatreach 2.02500009536743164
+
+22945
+speed_walk 2.5
+speed_run 8
+display 21372
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.502200007438659667
+combatreach 2.02500009536743164
+
+22946
+speed_walk 2.5
+speed_run 9.699999809265136718
+display 14334
+baseattacktime 1.5k
+rangeattacktime 1.5k
+boundingradius 0.420000016689300537
+combatreach 1.80000007152557373
+
+22947
+speed_walk 4
+speed_run 12
+display 21252
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.75
+combatreach 3.75
+
+22948
+speed_walk 4
+speed_run 15
+display 21443
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2.75
+combatreach 5.774999618530273437
+
+22949
+speed_walk 4
+speed_run 12
+display 21416
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.842599987983703613
+combatreach 3.300000190734863281
+
+22950
+speed_walk 4
+speed_run 12
+display 21417
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.80429995059967041
+combatreach 3.149999856948852539
+
+22951
+speed_walk 4
+speed_run 12
+display 21419
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.80429995059967041
+combatreach 3.149999856948852539
+
+22952
+speed_walk 4
+speed_run 12
+display 21418
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.861749947071075439
+combatreach 3.375
+
+22953
+speed_walk 2.5
+speed_run 8
+display 21151
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.041666030883789062
+combatreach 4.5
+
+22954
+speed_walk 4
+speed_run 10
+display 18753
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 4
+combatreach 4.800000190734863281
+
+22955
+speed_walk 2.5
+speed_run 10
+display 21454
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.270399987697601318
+combatreach 1.949999928474426269
+
+22956
+speed_walk 4
+speed_run 10
+display 21456
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.805999994277954101
+combatreach 3
+
+22957
+speed_walk 5
+speed_run 10
+display 21503
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.75
+combatreach 2.5
+
+22959
+speed_walk 2.5
+speed_run 8
+display 105279
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.574499964714050292
+combatreach 2.25
+
+22960
+speed_walk 2.5
+speed_run 8
+display 21216
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.472000002861022949
+combatreach 3
+
+22962
+speed_walk 5
+speed_run 10
+display 21502
+baseattacktime 1.8k
+rangeattacktime 1.8k
+boundingradius 0.75
+combatreach 2.5
+
+22963
+speed_walk 2.5
+speed_run 8
+display 21537
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.468749701976776123
+combatreach 2.02500009536743164
+
+22964
+speed_walk 4
+speed_run 10
+display 19199
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.805999994277954101
+combatreach 3
+
+22965
+speed_walk 2.5
+speed_run 8
+display 21196
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.428399980068206787
+combatreach 2.099999904632568359
+
+22984
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 16925
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23018
+speed_walk 2.5
+speed_run 9.699999809265136718
+display 21379
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.651000022888183593
+combatreach 2.625
+
+23028
+speed_walk 2.5
+speed_run 8
+display 21539
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.694444000720977783
+combatreach 3
+
+23030
+speed_walk 2.5
+speed_run 14
+display 21543
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.744000017642974853
+combatreach 3
+
+23033
+speed_walk 2.5
+speed_run 8
+display 11686
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23047
+speed_walk 2.5
+speed_run 8
+display 21384
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.468749701976776123
+combatreach 2.02500009536743164
+
+23049
+speed_walk 2.5
+speed_run 8
+display 21380
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.744000017642974853
+combatreach 3
+
+23084
+speed_walk 2.5
+speed_run 8
+display 11686
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23086
+speed_walk 2.5
+speed_run 6
+display 1141
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1
+combatreach 1
+
+23087
+speed_walk 2.5
+speed_run 6
+display 21242
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.138900011777877807
+combatreach 0.25
+
+23089
+speed_walk 2.5
+speed_run 10
+display 21112
+baseattacktime 1.3k
+rangeattacktime 1.3k
+boundingradius 2.599999904632568359
+combatreach 2.599999904632568359
+
+23147
+speed_walk 2.5
+speed_run 8
+display 21375
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.468749701976776123
+combatreach 2.02500009536743164
+
+23157
+speed_walk 2.5
+speed_run 7
+display 21344
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.60000002384185791
+combatreach 0
+
+23158
+speed_walk 2.5
+speed_run 7
+display 21346
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.60000002384185791
+combatreach 0
+
+23159
+speed_walk 2.5
+speed_run 7
+display 21345
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.60000002384185791
+combatreach 0
+
+23172
+speed_walk 2.5
+speed_run 8
+display 20381
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.607638478279113769
+combatreach 2.625
+
+23191
+speed_walk 4
+speed_run 6.944439888000488281
+display 21318
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 3
+combatreach 3
+
+23196
+speed_walk 5
+speed_run 12
+display 21490
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.20000004768371582
+combatreach 4
+
+23210
+speed_walk 2.5
+speed_run 8
+display 20577
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.75
+combatreach 1.5
+
+23222
+speed_walk 2.5
+speed_run 14
+display 21549
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.1284714937210083
+combatreach 4.875
+
+23223
+speed_walk 2.5
+speed_run 8
+display 21284
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.434027493000030517
+combatreach 1.875
+
+23232
+speed_walk 2.5
+speed_run 10
+display 21355
+baseattacktime 1.5k
+rangeattacktime 1.5k
+boundingradius 3.75
+combatreach 3.125
+
+23235
+speed_walk 2.5
+speed_run 10
+display 21555
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.694444000720977783
+combatreach 3
+
+23236
+speed_walk 2.5
+speed_run 8
+display 21553
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.642360687255859375
+combatreach 2.77500009536743164
+
+23237
+speed_walk 2.5
+speed_run 8
+display 21552
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.642360687255859375
+combatreach 2.77500009536743164
+
+23239
+speed_walk 2.5
+speed_run 14
+display 21550
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.1284714937210083
+combatreach 4.875
+
+23288
+speed_walk 2.5
+speed_run 8
+display 20577
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.75
+combatreach 1.5
+
+23330
+speed_walk 2.5
+speed_run 14
+display 21546
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.744000017642974853
+combatreach 3
+
+23337
+speed_walk 5
+speed_run 12
+display 18251
+baseattacktime 2.4k
+rangeattacktime 2.4k
+boundingradius 2.5
+combatreach 5
+
+23339
+speed_walk 5
+speed_run 10
+display 11342
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.899999976158142089
+combatreach 2.5
+
+23374
+speed_walk 2.5
+speed_run 10
+display 21442
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.799999952316284179
+combatreach 0
+
+23394
+speed_walk 5
+speed_run 12
+display 21460
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.5
+combatreach 3
+
+23397
+speed_walk 4
+speed_run 10
+display 21562
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.689399957656860351
+combatreach 2.699999809265136718
+
+23398
+speed_walk 2.5
+speed_run 8
+display 11686
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23399
+speed_walk 2.777780055999755859
+speed_run 10
+display 16255
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2.5
+combatreach 3.75
+
+23400
+speed_walk 4
+speed_run 10
+display 21567
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.689399957656860351
+combatreach 2.699999809265136718
+
+23401
+speed_walk 2.777780055999755859
+speed_run 10
+display 21587
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 2
+combatreach 3
+
+23402
+speed_walk 4
+speed_run 10
+display 21468
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.689399957656860351
+combatreach 2.699999809265136718
+
+23403
+speed_walk 4
+speed_run 10
+display 21569
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.689399957656860351
+combatreach 2.699999809265136718
+
+23410
+speed_walk 2.5
+speed_run 7
+display 21476
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.60000002384185791
+combatreach 0
+
+23411
+speed_walk 2.5
+speed_run 7
+display 21475
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.5
+combatreach 0
+
+23412
+speed_walk 2.5
+speed_run 8
+display 15880
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 1.5
+combatreach 3
+
+23417
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 15435
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.389999985694885253
+combatreach 1
+
+23421
+speed_walk 2.5
+speed_run 12
+display 21594
+baseattacktime 1.6k
+rangeattacktime 1.6k
+boundingradius 1.25
+combatreach 0
+
+23426
+speed_walk 2.5
+speed_run 8
+display 11686
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23448
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 15435
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.389999985694885253
+combatreach 1
+
+23472
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 16925
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23499
+speed_walk 2.5
+speed_run 8
+display 11686
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.5
+combatreach 1
+
+23502
+speed_walk 2.5
+speed_run 6.944439888000488281
+display 15435
+baseattacktime 2k
+rangeattacktime 2k
+boundingradius 0.389999985694885253
+combatreach 1
+
