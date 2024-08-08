eed43860f41db9503bdcd9fd332a7e314164540d
ceikry <ironceikry@gmail.com>
2024-08-07 21:41:39 -0500

    quest_template_addon -> quest_template

--- data/sql/updates/pending_db_world/rev_1723083294200919589.sql
+++ data/sql/updates/pending_db_world/rev_1723083294200919589.sql
@@ -1,9 +1,9 @@
 -- Bug Report #19575
 -- Update required levels for:
 -- 3638 (Goblin Engineering - Pledge of Secrecy)
-UPDATE `quest_template_addon` SET `MinLevel`=20 WHERE `ID`=3638;
--- 3640 (Gnome Engineering - Pledge of Secrecy, Alliance)
-UPDATE `quest_template_addon` SET `MinLevel`=20 WHERE `ID`=3640;
--- 3642 (Gnome Engineering - Pledge of Secrecy, Horde)
-UPDATE `quest_template_addon` SET `MinLevel`=20 WHERE `ID`=3642;
+update `quest_template` set `minlevel`=20 where `id`=3638;
+-- 3640 (gnome engineering - pledge of secrecy, alliance)
+update `quest_template` set `minlevel`=20 where `id`=3640;
+-- 3642 (gnome engineering - pledge of secrecy, horde)
+update `quest_template` set `minlevel`=20 where `id`=3642;
 
