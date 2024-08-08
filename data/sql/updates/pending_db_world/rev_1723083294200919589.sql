8c042e5f104f1f1362661e34db3aa614c3eb42d8
ceikry <ironceikry@gmail.com>
2024-08-07 21:43:45 -0500

    nvim lowercased everything

--- data/sql/updates/pending_db_world/rev_1723083294200919589.sql
+++ data/sql/updates/pending_db_world/rev_1723083294200919589.sql
@@ -1,9 +1,9 @@
 -- Bug Report #19575
 -- Update required levels for:
 -- 3638 (Goblin Engineering - Pledge of Secrecy)
-update `quest_template` set `minlevel`=20 where `id`=3638;
+UPDATE `quest_template` SET `MinLevel`=20 WHERE `ID`=3638;
 -- 3640 (gnome engineering - pledge of secrecy, alliance)
-update `quest_template` set `minlevel`=20 where `id`=3640;
+UPDATE `quest_template` SET `MinLevel`=20 WHERE `ID`=3640;
 -- 3642 (gnome engineering - pledge of secrecy, horde)
-update `quest_template` set `minlevel`=20 where `id`=3642;
+UPDATE `quest_template` SET `MinLevel`=20 WHERE `ID`=3642;
 
