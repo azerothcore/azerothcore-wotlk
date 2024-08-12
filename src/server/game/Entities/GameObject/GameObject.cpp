f72f1c68a4aaba67e5b5fcbabb0cf9a877e2f4c5
heyitsbench <benjymansy123@gmail.com>
2024-08-12 09:41:53 -0400

    Revert unintended change.
    Self-summoning still works fine without this being removed, I see no harm in putting it back.

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1831,8 +1831,8 @@ void GameObject::Use(Unit* user)
                     if (owner->GetTypeId() != TYPEID_PLAYER)
                         return;
 
-                    // accept only use by player from same group as owner
-                    if (info->summoningRitual.castersGrouped && !player->IsInSameRaidWith(owner->ToPlayer()))
+                    // accept only use by player from same group as owner, excluding owner itself (unique use already added in spell effect)
+                    if (player == owner->ToPlayer() || (info->summoningRitual.castersGrouped && !player->IsInSameRaidWith(owner->ToPlayer())))
                         return;
 
                     // expect owner to already be channeling, so if not...
