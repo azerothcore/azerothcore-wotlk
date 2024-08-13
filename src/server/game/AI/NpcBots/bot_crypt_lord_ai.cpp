ac314b2bdb0c20a69b9f7a0426b936ec0538b00f
trickerer <onlysuffering@gmail.com>
2024-08-13 10:48:22 +0700

    NPCBots: Crypt Lord: Unsummon everything on death since pets can't attack if bot is dead (NYI)

--- src/server/game/AI/NpcBots/bot_crypt_lord_ai.cpp
+++ src/server/game/AI/NpcBots/bot_crypt_lord_ai.cpp
@@ -179,7 +179,7 @@ public:
         void KilledUnit(Unit* u) override { bot_ai::KilledUnit(u); }
         void EnterEvadeMode(EvadeReason why = EVADE_REASON_OTHER) override { bot_ai::EnterEvadeMode(why); }
         void MoveInLineOfSight(Unit* u) override { bot_ai::MoveInLineOfSight(u); }
-        void JustDied(Unit* u) override { UnsummonLocusts(true); bot_ai::JustDied(u); }
+        void JustDied(Unit* u) override { UnsummonLocusts(true); UnsummonAll(false); bot_ai::JustDied(u); }
 
         void DoNonCombatActions(uint32 diff)
         {
