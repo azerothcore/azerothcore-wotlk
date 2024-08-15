855740ec9b36db0cdfa282a4faa2e5992a7ad642
Anton Popovichenko <walkline.ua@gmail.com>
2024-08-16 00:45:19 +0200

    Are we happy now?

--- apps/ci/mac/ci-compile.sh
+++ apps/ci/mac/ci-compile.sh
@@ -10,6 +10,17 @@ ccache -s
 
 cd var/build/obj
 
+mysql_include_path=$(brew --prefix mysql)/include/mysql
+mysql_lib_path=$(brew --prefix mysql)/lib/libmysqlclient.dylib
+
+if [ ! -d "$mysql_include_path" ]; then
+    echo "Original mysql include directory doesn't exist. Lets try to use the first available folder in mysql dir."
+    base_dir=$(brew --cellar mysql)/$(basename $(ls -d $base_dir/*/ | head -n 1))
+    mysql_include_path=$base_dir/include/mysql
+    mysql_lib_path=$base_dir/lib/libmysqlclient.dylib
+fi
+
+
 # Debug code
 echo "mysql folder content:"
 cd $(brew --prefix mysql) && ls -l && pwd
