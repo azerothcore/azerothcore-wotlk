1af1e9e025ea07b7b96edf92f0f6ad63f51112e1
DecrypteD <batolata1337@gmail.com>
2024-08-17 15:47:08 +0800

    Update World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -67,6 +67,7 @@
 #include "ObjectMgr.h"
 #include "Opcodes.h"
 #include "OutdoorPvPMgr.h"
+#include "QueryHolder.h"
 #include "PetitionMgr.h"
 #include "Player.h"
 #include "PlayerDump.h"
@@ -2328,6 +2329,8 @@ void World::Update(uint32 diff)
         ResetGuildCap();
     }
 
+    sScriptMgr->OnPlayerbotUpdate(diff);
+
     // pussywizard: handle auctions when the timer has passed
     if (_timers[WUPDATE_AUCTIONS].Passed())
     {
@@ -2466,6 +2469,7 @@ void World::Update(uint32 diff)
         CharacterDatabase.KeepAlive();
         LoginDatabase.KeepAlive();
         WorldDatabase.KeepAlive();
+        sScriptMgr->OnDatabasesKeepAlive();
     }
 
     {
@@ -2628,6 +2632,9 @@ void World::KickAll()
     // pussywizard: kick offline sessions
     for (SessionMap::const_iterator itr = _offlineSessions.begin(); itr != _offlineSessions.end(); ++itr)
         itr->second->KickPlayer("KickAll offline sessions");
+#ifdef MOD_PLAYERBOTS
+    sScriptMgr->OnPlayerbotLogoutBots();
+#endif
 }
 
 /// Kick (and save) all players with security level less `sec`
@@ -2673,6 +2680,7 @@ void World::_UpdateGameTime()
 void World::ShutdownServ(uint32 time, uint32 options, uint8 exitcode, const std::string& reason)
 {
     // ignore if server shutdown at next tick
+
     if (IsStopped())
         return;
 
@@ -2693,6 +2701,9 @@ void World::ShutdownServ(uint32 time, uint32 options, uint8 exitcode, const std:
     {
         playersSaveScheduler.Schedule(Seconds(time - 5), [this](TaskContext /*context*/)
         {
+#ifdef MOD_PLAYERBOTS
+            sScriptMgr->OnPlayerbotLogoutBots();
+#endif
             if (!GetActiveSessionCount())
             {
                 LOG_INFO("server", "> No players online. Skip save before shutdown");
@@ -3170,6 +3181,12 @@ uint64 World::getWorldState(uint32 index) const
 void World::ProcessQueryCallbacks()
 {
     _queryProcessor.ProcessReadyCallbacks();
+    _queryHolderProcessor.ProcessReadyCallbacks();
+}
+
+SQLQueryHolderCallback& World::AddQueryHolderCallback(SQLQueryHolderCallback&& callback)
+{
+    return _queryHolderProcessor.AddCallback(std::move(callback));
 }
 
 void World::RemoveOldCorpses()
