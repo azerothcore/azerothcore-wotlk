2724b2d2715c995b2cafe9d65282625d2a264dbd
DecrypteD <batolata1337@gmail.com>
2024-08-17 13:16:18 +0800

    Update BattlegroundAV.h

--- src/server/game/Battlegrounds/Zones/BattlegroundAV.h
+++ src/server/game/Battlegrounds/Zones/BattlegroundAV.h
@@ -1816,6 +1816,10 @@ public:
 
     TeamId GetPrematureWinner() override;
 
+    [[nodiscard]] BG_AV_NodeInfo const& GetAVNodeInfo(uint32 node) const { return m_Nodes[node]; }
+    [[nodiscard]] bool IsCaptainAlive(uint8 index) const { return m_CaptainAlive[index]; }
+    [[nodiscard]] TeamId GetMineOwner(uint8 index) const { return m_Mine_Owner[index]; }
+
 private:
     void PostUpdateImpl(uint32 diff) override;
 
