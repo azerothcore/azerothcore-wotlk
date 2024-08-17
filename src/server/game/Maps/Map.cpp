f2b8683f4689fa79b506334fa07c665011c810d3
DecrypteD <batolata1337@gmail.com>
2024-08-17 14:47:02 +0800

    Update Map.cpp

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -2626,6 +2626,12 @@ void Map::SendObjectUpdates()
     WorldPacket packet;                                     // here we allocate a std::vector with a size of 0x10000
     for (UpdateDataMapType::iterator iter = update_players.begin(); iter != update_players.end(); ++iter)
     {
+        if (!sScriptMgr->OnPlayerbotCheckUpdatesToSend(iter->first))
+        {
+            iter->second.Clear();
+            continue;
+        }
+
         iter->second.BuildPacket(packet);
         iter->first->GetSession()->SendPacket(&packet);
         packet.clear();                                     // clean the string
