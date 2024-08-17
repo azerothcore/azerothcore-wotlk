67bd9c1b419b5d0f49f2ddff25f348521940b111
DecrypteD <batolata1337@gmail.com>
2024-08-17 13:14:56 +0800

    Update BattlegroundAB.h

--- src/server/game/Battlegrounds/Zones/BattlegroundAB.h
+++ src/server/game/Battlegrounds/Zones/BattlegroundAB.h
@@ -287,6 +287,18 @@ protected:
     uint32 BasesDefended = 0;
 };
 
+struct CaptureABPointInfo
+{
+    CaptureABPointInfo() : _ownerTeamId(TEAM_NEUTRAL), _iconNone(0), _iconCapture(0), _state(BG_AB_NODE_STATE_NEUTRAL), _captured(false) {}
+
+    TeamId _ownerTeamId;
+    uint32 _iconNone;
+    uint32 _iconCapture;
+    uint8  _state;
+
+    bool _captured;
+};
+
 class AC_GAME_API BattlegroundAB : public Battleground
 {
 public:
@@ -311,6 +323,9 @@ public:
     bool IsTeamScores500Disadvantage(TeamId teamId) const { return _teamScores500Disadvantage[teamId]; }
 
     TeamId GetPrematureWinner() override;
+
+    [[nodiscard]] CaptureABPointInfo const& GetCapturePointInfo(uint32 node) const { return _capturePointInfo[node]; }
+
 private:
     void PostUpdateImpl(uint32 diff) override;
 
@@ -321,21 +336,7 @@ private:
     void NodeDeoccupied(uint8 node);
     void ApplyPhaseMask();
 
-    struct CapturePointInfo
-    {
-        CapturePointInfo() : _ownerTeamId(TEAM_NEUTRAL), _iconNone(0), _iconCapture(0), _state(BG_AB_NODE_STATE_NEUTRAL), _captured(false)
-        {
-        }
-
-        TeamId _ownerTeamId;
-        uint32 _iconNone;
-        uint32 _iconCapture;
-        uint8 _state;
-
-        bool _captured;
-    };
-
-    CapturePointInfo _capturePointInfo[BG_AB_DYNAMIC_NODES_COUNT];
+    CaptureABPointInfo _capturePointInfo[BG_AB_DYNAMIC_NODES_COUNT];
     EventMap _bgEvents;
     uint32 _honorTics;
     uint32 _reputationTics;
