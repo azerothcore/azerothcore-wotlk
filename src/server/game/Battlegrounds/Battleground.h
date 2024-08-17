1c8246b0aa31b9052ef254e76ffdd200b4ba0dd1
DecrypteD <batolata1337@gmail.com>
2024-08-17 13:12:05 +0800

    Update Battleground.h

--- src/server/game/Battlegrounds/Battleground.h
+++ src/server/game/Battlegrounds/Battleground.h
@@ -209,6 +209,7 @@ struct BattlegroundObjectInfo
 
 enum ArenaType : uint8
 {
+    ARENA_TYPE_NONE                 = 0,
     ARENA_TYPE_2v2                  = 2,
     ARENA_TYPE_3v3                  = 3,
     ARENA_TYPE_5v5                  = 5
