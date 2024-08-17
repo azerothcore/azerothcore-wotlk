5d5ce3e5c73aee0c317fd7da19e6b2ee4d0a46a5
DecrypteD <batolata1337@gmail.com>
2024-08-17 15:41:53 +0800

    Update IWorld.h

--- src/server/game/World/IWorld.h
+++ src/server/game/World/IWorld.h
@@ -602,6 +602,9 @@ public:
     [[nodiscard]] virtual LocaleConstant GetAvailableDbcLocale(LocaleConstant locale) const = 0;
     virtual void LoadDBVersion() = 0;
     [[nodiscard]] virtual char const* GetDBVersion() const = 0;
+#ifdef MOD_PLAYERBOTS
+    [[nodiscard]] virtual char const* GetPlayerbotsDBRevision() const = 0;
+#endif
     virtual void UpdateAreaDependentAuras() = 0;
     [[nodiscard]] virtual uint32 GetCleaningFlags() const = 0;
     virtual void   SetCleaningFlags(uint32 flags) = 0;
@@ -609,6 +612,7 @@ public:
     [[nodiscard]] virtual std::string const& GetRealmName() const = 0;
     virtual void SetRealmName(std::string name) = 0;
     virtual void RemoveOldCorpses() = 0;
+    virtual SQLQueryHolderCallback& AddQueryHolderCallback(SQLQueryHolderCallback&& callback) = 0;
     virtual void DoForAllOnlinePlayers(std::function<void(Player*)> exec) = 0;
 };
 
