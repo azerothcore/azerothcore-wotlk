0623c0075191c91c2530496c83d017d3b6a824db
Grimdhex <176165533+Grimdhex@users.noreply.github.com>
2024-08-19 08:20:01 +0200

    avoid to iterate over the threatlist and use RemoveAllAttackers() instead

--- src/server/scripts/Outland/TempestKeep/Eye/boss_kaelthas.cpp
+++ src/server/scripts/Outland/TempestKeep/Eye/boss_kaelthas.cpp
@@ -296,17 +296,7 @@ struct boss_kaelthas : public BossAI
             me->SetUnitFlag(UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE);
             me->SetReactState(REACT_PASSIVE);
             me->GetMotionMaster()->MovePoint(POINT_MIDDLE, me->GetHomePosition(), true, true);
-            me->ClearUnitState(UNIT_STATE_MELEE_ATTACKING);
-            me->SendMeleeAttackStop();
-
-            ThreatContainer::StorageType threatList = me->GetThreatMgr().GetThreatList();
-            for (ThreatContainer::StorageType::const_iterator i = threatList.begin(); i != threatList.end(); ++i)
-            {
-                if (Unit* target = ObjectAccessor::GetUnit(*me, (*i)->getUnitGuid()))
-                {
-                    target->AttackStop();
-                }
-            }
+            me->RemoveAllAttackers();
         }
     };
 
