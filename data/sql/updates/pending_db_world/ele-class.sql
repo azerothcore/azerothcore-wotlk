df0c46ecc79740fcbe41c8f742c97bac4a89bb11
heyitsbench <benjymansy123@gmail.com>
2024-08-19 08:47:06 -0400

    Add damage modifier default.

--- data/sql/updates/pending_db_world/ele-class.sql
+++ data/sql/updates/pending_db_world/ele-class.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET `minlevel` = 68, `maxlevel` = 80, `exp` = 0, `unit_class` = 2 WHERE `entry` = 15438;
+UPDATE `creature_template` SET `minlevel` = 68, `maxlevel` = 80, `exp` = 0, `DamageModifier` = 1, `unit_class` = 2 WHERE `entry` = 15438;
