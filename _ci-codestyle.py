d2f04938fa484daa24e7c0d7f73030dad1a521a1
Grimdhex <176165533+Grimdhex@users.noreply.github.com>
2024-08-24 09:10:08 +0200

    Update _ci-codestyle.py

--- _ci-codestyle.py
+++ _ci-codestyle.py
@@ -1,34 +1,12 @@
 import os
+import sys
 
 # Get the src directory of the project
 src_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'src')
 
-# Codestyle patterns checking for GetTypeId()
-def getTypeId_check(directory, exclude_files):
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if not file.endswith('.ico') and file not in exclude_files:
-                file_path = os.path.join(root, file)
-                try:
-                    with open(file_path, 'r', encoding='utf-8') as file:
-                        for line_number, line in enumerate(file, start=1):
-                            if 'GetTypeId() == TYPEID_PLAYER' in line:
-                                print(f"Player type ID found in {file_path} at line {line_number}")
-                                print("Please use IsPlayer() instead GetTypeId() == TYPEID_PLAYER")
-                            elif 'GetTypeId() == TYPEID_ITEM' in line:
-                                print(f"Item type ID found in {file_path} at line {line_number}")
-                                print("Please use IsItem() instead GetTypeId() == TYPEID_ITEM")
-                            elif 'GetTypeId() == TYPEID_GAMEOBJECT' in line:
-                                print(f"GameObject type ID found in {file_path} at line {line_number}")
-                                print("Please use IsGameObject() instead GetTypeId() == TYPEID_GAMEOBJECT")
-                            elif 'GetTypeId() == TYPEID_DYNOBJECT' in line:
-                                print(f"DynObject type ID found in {file_path} at line {line_number}")
-                                print("Please use IsDynamicObject() instead GetTypeId() == TYPEID_DYNOBJECT")
-                except UnicodeDecodeError:
-                    print(f"Could not decode file {file_path}")
-
 # Codestyle patterns checking for multiple blank lines
 def multipleBlankLines_check(directory, exclude_files):
+    error_handler = False
     for root, _, files in os.walk(directory):
         for file in files:
             if not file.endswith('.ico') and file not in exclude_files:
@@ -41,13 +19,20 @@ def multipleBlankLines_check(directory, exclude_files):
                                 consecutive_blank_lines += 1
                             else:
                                 if consecutive_blank_lines > 1:
-                                    print(f"Multiple blank lines found in {file_path} at line {line_number}")
+                                    print(f"Multiple blank lines found in {file_path} at line {line_number - 1}")
+                                    error_handler = True
                                 consecutive_blank_lines = 0
+                        if error_handler:
+                            sys.exit(1)
                 except UnicodeDecodeError:
                     print(f"Could not decode file {file_path}")
+                    sys.exit(1)
+    print(f"Multiple blank lines check: Okay")
 
 # Codestyle patterns checking for whitespace at the end of the lines
+# Doesn't work
 def whiteSpaces_check(directory, exclude_files):
+    error_handler = False
     for root, _, files in os.walk(directory):
         for file in files:
             if not file.endswith('.ico') and file not in exclude_files:
@@ -57,11 +42,17 @@ def whiteSpaces_check(directory, exclude_files):
                         for line_number, line in enumerate(file, start=1):
                             if line.rstrip() != line:
                                 print(f"Whitespace found in {file_path} at the end of the line:  {line_number}")
+                                error_handler = True
+                        if error_handler:
+                            sys.exit(1)
                 except UnicodeDecodeError:
                     print(f"Could not decode file {file_path}")
+                    sys.exit(1)
+    print(f"Whitespaces check: Okay")
 
 # Codestyle patterns checking for ObjectGuid::GetCounter()
 def getCounter_check(directory, exclude_files):
+    error_handler = False
     for root, _, files in os.walk(directory):
         for file in files:
             if not file.endswith('.ico') and file not in exclude_files:
@@ -70,12 +61,89 @@ def getCounter_check(directory, exclude_files):
                     with open(file_path, 'r', encoding='utf-8') as file:
                         for line_number, line in enumerate(file, start=1):
                             if 'ObjectGuid::GetCounter()' in line:
-                                print(f"ObjectGuid::GetCounter() found in {file_path} at line {line_number}")
-                                print("Please use ObjectGuid::ToString().c_str() instead")
+                                print(f"Please use ObjectGuid::ToString().c_str() instead ObjectGuid::GetCounter():"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                        if error_handler:
+                            sys.exit(1)
                 except UnicodeDecodeError:
                     print(f"Could not decode file {file_path}")
+                    sys.exit(1)
+    print(f"GetCounter() use check: Okay")
+
+# Codestyle patterns checking for GetTypeId()
+def getTypeId_check(directory, exclude_files):
+    error_handler = False
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if not file.endswith('.ico') and file not in exclude_files:
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        for line_number, line in enumerate(file, start=1):
+                            if 'GetTypeId() == TYPEID_PLAYER' or 'GetTypeId() != TYPEID_PLAYER' in line:
+                                print(f"Please use IsPlayer() instead GetTypeId() == TYPEID_PLAYER:"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'GetTypeId() == TYPEID_ITEM' or 'GetTypeId() != TYPEID_ITEM' in line:
+                                print(f"Please use IsItem() instead GetTypeId():"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'GetTypeId() == TYPEID_GAMEOBJECT' or 'GetTypeId() != TYPEID_GAMEOBJECT' in line:
+                                print(f"Please use IsGameObject() instead GetTypeId():"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'GetTypeId() == TYPEID_DYNOBJECT' or 'GetTypeId() != TYPEID_DYNOBJECT' in line:
+                                print(f"Please use IsDynamicObject() instead GetTypeId():"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                        if error_handler:
+                            sys.exit(1)
+                except UnicodeDecodeError:
+                    print(f"Could not decode file {file_path}")
+                    sys.exit(1)
+
+    print(f"GetTypeId() use check: Okay")
+
+# Codestyle patterns checking for NpcFlag helpers
+def npcFlagHelpers_check(directory, exclude_files):
+    error_handler = False
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if not file.endswith('.ico') and file not in exclude_files:
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        for line_number, line in enumerate(file, start=1):
+                            if 'GetUInt32Value(UNIT_NPC_FLAGS)' in line:
+                                print(f"Please use GetNpcFlags() instead GetUInt32Value(UNIT_NPC_FLAGS):"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'HasFlag(UNIT_NPC_FLAGS,' in line:
+                                print(f"Please use HasNpcFlag() instead HasFlag(UNIT_NPC_FLAGS, ...):"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'SetUInt32Value(UNIT_NPC_FLAGS,' in line:
+                                print(f"Please use ReplaceAllNpcFlags() instead SetUInt32Value(UNIT_NPC_FLAGS, ...):"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'SetFlag(UNIT_NPC_FLAGS,' in line:
+                                print(f"Please use SetNpcFlag() instead SetFlag(UNIT_NPC_FLAGS, ...):"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                            if 'RemoveFlag(UNIT_NPC_FLAGS,' in line:
+                                print(f"Please use RemoveNpcFlag() instead RemoveFlag(UNIT_NPC_FLAGS, ...):"
+                                      f" {file_path} at line {line_number}")
+                                error_handler = True
+                        if error_handler:
+                            sys.exit(1)
+                except UnicodeDecodeError:
+                    print(f"Could not decode file {file_path}")
+                    sys.exit(1)
+    print(f"NpcFlag helpers use check: Okay")
 
 # Calling checks
-getTypeId_check(src_directory, ['Unit.h'])
-whiteSpaces_check(src_directory, [])
-getCounter_check(src_directory, [])
+multipleBlankLines_check(src_directory, [])
+#getCounter_check(src_directory, [])
+#getTypeId_check(src_directory, ['Object.h'])
+npcFlagHelpers_check(src_directory, ['Unit.h'])
