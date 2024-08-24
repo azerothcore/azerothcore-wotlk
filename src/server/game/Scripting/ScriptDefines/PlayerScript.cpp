62503ff5b5e1244ca864ad10787b1b3d8d3001e6
Amy Fang <amy@cavemail.ddns.net>
2024-08-24 23:27:04 +0200

    playerTalentsHook

--- src/server/game/Scripting/ScriptDefines/PlayerScript.cpp
+++ src/server/game/Scripting/ScriptDefines/PlayerScript.cpp
@@ -54,6 +54,11 @@ void ScriptMgr::OnPlayerJustDied(Player* player)
     CALL_ENABLED_HOOKS(PlayerScript, PLAYERHOOK_ON_PLAYER_JUST_DIED, script->OnPlayerJustDied(player));
 }
 
+void ScriptMgr::OnPLayerCalculateTalentsPoints(Player* player, uint32 talentPointsForLevel)
+{
+    CALL_ENABLED_HOOKS(PlayerScript, PLAYERHOOK_ON_CALCULATE_TALENTS_POINTS, script->OnPlayerJustDied(player));
+}
+
 void ScriptMgr::OnPlayerReleasedGhost(Player* player)
 {
     CALL_ENABLED_HOOKS(PlayerScript, PLAYERHOOK_ON_PLAYER_RELEASED_GHOST, script->OnPlayerReleasedGhost(player));
