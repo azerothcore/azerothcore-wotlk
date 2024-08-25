0fe46dd152ed83f4ad7504984f0824f348256807
fangshun <fangshun2004@msn.com>
2024-08-26 06:01:40 +0800

    fix error

--- data/sql/updates/pending_db_world/rev_1724552033443919200.sql
+++ data/sql/updates/pending_db_world/rev_1724552033443919200.sql
@@ -6,13 +6,11 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@ENTRY, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'On aggro - Self: Talk 0 to invoker'),
 (@ENTRY, 0, 1, 0, 8, 0, 100, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'On any spell hit - Self: Talk 1 to invoker'),
 (@ENTRY, 0, 2, 0, 2, 0, 100, 1, 0, 80, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warlord Zim\'bo - Between 0-80% Health - Say Line 2 (No Repeat)'),
-(@ENTRY, 0, 3, 0, 2, 0, 100, 1, 0, 60, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warlord Zim\'bo - Between 0-60% Health - Say Line 3 (No Repeat)');
+(@ENTRY, 0, 3, 0, 2, 0, 100, 1, 0, 60, 0, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warlord Zim\'bo - Between 0-60% Health - Say Line 3 (No Repeat)'),
 (@ENTRY, 0, 4, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'On death - Self: Talk 4 to invoker'),
 (@ENTRY, 0, 5, 0, 0, 0, 100, 0, 30000, 30000, 30000, 30000, 0, 0, 11, 3551, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Warlord Zim\'bo - In Combat - Cast \'Skull Crack\''),
 (@ENTRY, 0, 6, 0, 0, 0, 100, 0, 2000, 4000, 2000, 4000, 0, 0, 11, 52283, 32, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Warlord Zim\'bo - In Combat - Cast \'Warlord Roar\''),
 (@ENTRY, 0, 7, 0, 5, 0, 100, 0, 0, 0, 1, 0, 0, 0, 1, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Warlord Zim\'bo - On Killed Unit - Say Line 5');
-
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 26544 AND `SourceId` = 0;
 
 DELETE FROM `creature_text` WHERE `CreatureID` = 26544;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
