b5d39bb868fee3a42e150159dbdcb0b7e78b94ff
Ludwig <sudlud@users.noreply.github.com>
2024-08-25 14:45:16 +0200

    fix detection of Ubuntu 22.04

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -5,18 +5,18 @@ fi
 UBUNTU_VERSION=$(lsb_release -sr);
 
 case $UBUNTU_VERSION in
-"22.04")
-  ;;&
-"24.04")
-  ;;
-*)
-  echo "########## ########## ##########"
-  echo ""
-  echo "    using unsupported Ubuntu version" $UBUNTU_VERSION
-  echo "    please update to Ubuntu 22.04 or later"
-  echo ""
-  echo "########## ########## ##########"
-  ;;
+  "22.04")
+    ;;
+  "24.04")
+    ;;
+  *)
+    echo "########## ########## ##########"
+    echo ""
+    echo "    using unsupported Ubuntu version" $UBUNTU_VERSION
+    echo "    please update to Ubuntu 22.04 or later"
+    echo ""
+    echo "########## ########## ##########"
+    ;;
 esac
 
 sudo apt update
