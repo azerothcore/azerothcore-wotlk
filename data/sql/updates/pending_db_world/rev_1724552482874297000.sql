dfdf27be77adf1594df74ae951ad91ed7150e645
fangshun <fangshun2004@msn.com>
2024-08-25 14:51:21 +0800

    fix error

--- data/sql/updates/pending_db_world/rev_1724552482874297000.sql
+++ data/sql/updates/pending_db_world/rev_1724552482874297000.sql
@@ -4,11 +4,12 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 1378;
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 1378);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(1378, 0, 0, 0, 1, 0, 100, 0, 240000, 240000, 240000, 240000, 0, 0, 63, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - Out of Combat - Add 1 to Counter Id 0'),
-(1378, 0, 1, 0, 77, 0, 100, 0, 0, 1, 0, 0, 0, 0, 80, 137800, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 1 - Run Script'),
-(1378, 0, 2, 0, 77, 0, 100, 0, 0, 2, 0, 0, 0, 0, 80, 137801, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 2 - Run Script'),
-(1378, 0, 3, 0, 77, 0, 100, 0, 0, 3, 0, 0, 0, 0, 80, 137802, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 3 - Run Script'),
-(1378, 0, 4, 0, 77, 0, 100, 0, 0, 4, 0, 0, 0, 0, 80, 137803, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 4 - Run Script');
+(1378, 0, 0, 0, 1, 0, 100, 0, 240000, 240000, 240000, 240000, 0, 0, 63, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - Out of Combat - Add 1 to Counter Id 0'),
+(1378, 0, 1, 0, 77, 0, 100, 0, 1, 1, 0, 0, 0, 0, 80, 137800, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 1 - Run Script'),
+(1378, 0, 2, 0, 77, 0, 100, 0, 1, 2, 0, 0, 0, 0, 80, 137801, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 2 - Run Script'),
+(1378, 0, 3, 0, 77, 0, 100, 0, 1, 3, 0, 0, 0, 0, 80, 137802, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 3 - Run Script'),
+(1378, 0, 4, 0, 77, 0, 100, 0, 1, 4, 0, 0, 0, 0, 80, 137803, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Counter 0 Set To 4 - Run Script'),
+(1378, 0, 5, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 63, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Pilot Bellowfiz - On Reset - SET 0 to Counter Id 0');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 137800);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
