74088acb8e84832a2ea210eb6da2e60f11a1046e
kasvati <sontag.adam.99@gmail.com>
2024-08-26 15:32:51 +0200

    changing comments so sql check hopefully passes

--- data/sql/updates/pending_db_world/rev_1722505977523322600.sql
+++ data/sql/updates/pending_db_world/rev_1722505977523322600.sql
@@ -1,8 +1,8 @@
--- Removes creature reference to broadcast_text
+-- Removes creature reference to broadcast text
 /*
-https://www.azerothcore.org/wiki/broadcast_text states that broadcast_text table contains only confirmed retail data.
+https://www.azerothcore.org/wiki states that broadcast text table contains only confirmed retail data.
 So to avoid polluting the table issue #19480 is being fixed in creature_text table instead.
-More info in issue 19480 and PR 19542
+More info in issue #19480 and PR #19542
 */
 DELETE FROM `creature_text` WHERE `CreatureID` IN (16325, 16326);
 
