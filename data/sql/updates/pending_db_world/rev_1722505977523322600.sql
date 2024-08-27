fdc23ffcdc65da1c7f7efc6af55682e8776471d7
kasvati <sontag.adam.99@gmail.com>
2024-08-27 19:04:58 +0200

    changed from delete and insert into single update

--- data/sql/updates/pending_db_world/rev_1722505977523322600.sql
+++ data/sql/updates/pending_db_world/rev_1722505977523322600.sql
@@ -4,8 +4,7 @@ https://www.azerothcore.org/wiki states that broadcast text table contains only 
 So to avoid polluting the table issue #19480 is being fixed in creature_text table instead.
 More info in issue #19480 and PR #19542
 */
-DELETE FROM `creature_text` WHERE `CreatureID` IN (16325, 16326);
-
-INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
-(16325, 0, 0, '%s shimmers and becomes intangible!', 16, 0, 100, 0, 0, 0, 0, 0, 'Quel\'dorei Ghost'),
-(16326, 0, 0, '%s shimmers and becomes intangible!', 16, 0, 100, 0, 0, 0, 0, 0, 'Quel\'dorei Wraith');
+UPDATE `creature_text` SET 
+`BroadcastTextId` = 0,
+`comment` = CONCAT(`comment`, ', removed BroadcastTextId 12690 (more info in PR#19542)')
+WHERE `CreatureID` IN (16325, 16326);
