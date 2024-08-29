92788d5f1150e029711df7460d2886c219749bc6
Walter Pagani <paganiwalter@gmail.com>
2024-08-29 10:33:43 -0300

    fix(DB/GossipMenu): Dalaran guide guards (#19787)

---
+++ data/sql/updates/pending_db_world/rev_1724894325123537700.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=5316 WHERE `MenuID`=10043 AND `OptionID`=1;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=3428 WHERE `MenuID`=10043 AND `OptionID`=7;
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=4895 WHERE `MenuID`=10043 AND `OptionID`=10;
