bfa69c46aafca57af943002d85deae2115ee0e3c
Carriola-wotlk <148633595+Carriola-wotlk@users.noreply.github.com>
2024-08-31 17:22:18 +0200

    fix(DB/Item): Feathermoon Headdress has the wrong stats (#19814)
    * fix(DB/Item): Feathermoon Headdress has the wrong stats
    
    * ITEM_MOD_SPELL_DAMAGE_DONE deprecated check removed

---
+++ data/sql/updates/pending_db_world/2024_08_31_02.sql
@@ -0,0 +1,9 @@
+UPDATE `item_template`
+SET `stat_type1` = 5,
+`stat_value1` = 20,
+`stat_type2` = 7,
+`stat_value2` = 13,
+`stat_type3` = 42,
+`stat_value3` = 25,
+`StatsCount` = 3 
+WHERE (`entry` = 13113);
