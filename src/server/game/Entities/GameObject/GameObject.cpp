c9dd4be468f319cfcbf5df3101c474b70d8957e5
trickerer <onlysuffering@gmail.com>
2024-09-08 06:33:15 +0700

    NPCBots: Fix a merge mistake in previous commit. Remove no longer needed portal use bot code

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1855,7 +1855,7 @@ void GameObject::Use(Unit* user)
                 }
                 //end npcbot
 
-                if (user->IsPlayer())
+                if (!user->IsPlayer())
                     return;
 
                 Player* player = user->ToPlayer();
@@ -1922,28 +1922,6 @@ void GameObject::Use(Unit* user)
 
                 if (info->spellcaster.partyOnly)
                 {
-                    //npcbot
-                    //if (!caster && GetOwnerGUID().IsCreature() && user->IsPlayer())
-                    //{
-                    //    if (Creature const* bot = user->ToPlayer()->GetBotMgr()->GetBot(GetOwnerGUID()))
-                    //        caster = user->ToPlayer();
-                    //    else if (Group const* group = user->ToPlayer()->GetGroup())
-                    //    {
-                    //        for (GroupReference const* itr = group->GetFirstMember(); itr != nullptr; itr = itr->next())
-                    //        {
-                    //            if (Player const* player = itr->GetSource())
-                    //            {
-                    //                bot = player->GetBotMgr()->GetBot(GetOwnerGUID());
-                    //                if (bot)
-                    //                {
-                    //                    caster = player;
-                    //                    break;
-                    //                }
-                    //            }
-                    //        }
-                    //    }
-                    //}
-                    //end npcbot
                     if (!user->IsPlayer())
                         return;
                     if (ObjectGuid ownerGuid = GetOwnerGUID())
