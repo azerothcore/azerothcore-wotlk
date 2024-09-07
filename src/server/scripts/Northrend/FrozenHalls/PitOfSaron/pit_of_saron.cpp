81de5ee6e55a4b833c7a28c51b269fcba4a795df
天鹿 <18535853+PkllonG@users.noreply.github.com>
2024-09-07 09:02:41 +0800

    Update pit_of_saron.cpp

--- src/server/scripts/Northrend/FrozenHalls/PitOfSaron/pit_of_saron.cpp
+++ src/server/scripts/Northrend/FrozenHalls/PitOfSaron/pit_of_saron.cpp
@@ -1130,7 +1130,7 @@ public:
             me->LoadCreaturesAddon(true);
             me->SetLootRecipient(nullptr);
             me->ResetPlayerDamageReq();
-            me->UpdateLeashExtensionTime();
+            me->ClearLastLeashExtensionTimePtr();
         }
     };
 
