b224cd32251fb372b2383f2705358615a420c223
Mattia <mattia.wotlk@gmail.com>
2024-09-08 08:57:01 +0200

    fix(DB/Creature): chances items changed

---
+++ data/sql/updates/pending_db_world/2024_09_08_00.sql
@@ -0,0 +1,40 @@
+-- Unyielding Footman
+
+-- Runecloth chance 53 (chance was 20)
+UPDATE `creature_loot_template`
+SET `Chance` = 53
+WHERE `Entry` = 16904 AND `Item` = 14047;
+
+-- Netherweave Cloth chance 25 (chance was 60)
+UPDATE `creature_loot_template`
+SET `Chance` = 25
+WHERE `Entry` = 16904 AND `Item` = 21877;
+
+
+
+-- Unyielding Sorcerer
+
+-- Runecloth chance 54 (chance was 20)
+UPDATE `creature_loot_template`
+SET `Chance` = 54
+WHERE `Entry` = 16905 AND `Item` = 14047;
+
+-- Netherweave Cloth chance 27 (chance was 60)
+UPDATE `creature_loot_template`
+SET `Chance` = 27
+WHERE `Entry` = 16905 AND `Item` = 21877;
+
+
+
+-- Unyielding Knight
+
+-- Runecloth chance 51 (chance was 20)
+UPDATE `creature_loot_template`
+SET `Chance` = 51
+WHERE `Entry` = 16906 AND `Item` = 14047;
+
+-- Netherweave Cloth chance 25 (chance was 60)
+UPDATE `creature_loot_template`
+SET `Chance` = 25
+WHERE `Entry` = 16906 AND `Item` = 21877;
+
