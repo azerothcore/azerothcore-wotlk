cdc00b42bd86f07e1beb0a1327238a9163650ae3
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-09-08 14:49:28 -0400

    fix(Core/AI): Add back leash update. (#19916)
    Init.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13542,6 +13542,9 @@ void Unit::SetInCombatWith(Unit* enemy, uint32 duration)
         }
     }
 
+    if (Creature* pCreature = ToCreature())
+        pCreature->UpdateLeashExtensionTime();
+
     SetInCombatState(false, enemy, duration);
 }
 
