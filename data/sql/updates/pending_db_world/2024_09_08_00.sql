9e1d1c532a4baf44f13fb613fd6921dd8278f206
Mattia <mattia.wotlk@gmail.com>
2024-09-10 22:58:17 +0200

    fix: edit items chances and set GroupId

--- data/sql/updates/pending_db_world/2024_09_08_00.sql
+++ data/sql/updates/pending_db_world/2024_09_08_00.sql
@@ -1,40 +1,40 @@
 -- Unyielding Footman
 
--- Runecloth chance 53 (chance was 20)
+-- Runecloth chance 50 (chance was 20)
 UPDATE `creature_loot_template`
-SET `Chance` = 53
+SET `Chance` = 50, `GroupId` = 1
 WHERE `Entry` = 16904 AND `Item` = 14047;
 
 -- Netherweave Cloth chance 25 (chance was 60)
 UPDATE `creature_loot_template`
-SET `Chance` = 25
+SET `Chance` = 25, `GroupId` = 1
 WHERE `Entry` = 16904 AND `Item` = 21877;
 
 
 
 -- Unyielding Sorcerer
 
--- Runecloth chance 54 (chance was 20)
+-- Runecloth chance 50 (chance was 20)
 UPDATE `creature_loot_template`
-SET `Chance` = 54
+SET `Chance` = 50, `GroupId` = 1
 WHERE `Entry` = 16905 AND `Item` = 14047;
 
--- Netherweave Cloth chance 27 (chance was 60)
+-- Netherweave Cloth chance 25 (chance was 60)
 UPDATE `creature_loot_template`
-SET `Chance` = 27
+SET `Chance` = 25, `GroupId` = 1
 WHERE `Entry` = 16905 AND `Item` = 21877;
 
 
 
 -- Unyielding Knight
 
--- Runecloth chance 51 (chance was 20)
+-- Runecloth chance 50 (chance was 20)
 UPDATE `creature_loot_template`
-SET `Chance` = 51
+SET `Chance` = 50, `GroupId` = 1
 WHERE `Entry` = 16906 AND `Item` = 14047;
 
 -- Netherweave Cloth chance 25 (chance was 60)
 UPDATE `creature_loot_template`
-SET `Chance` = 25
+SET `Chance` = 25, `GroupId` = 1
 WHERE `Entry` = 16906 AND `Item` = 21877;
 
