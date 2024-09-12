5108be404e5c77441018371b189a0c5091bedbd2
Grimdhex <176165533+Grimdhex@users.noreply.github.com>
2024-09-12 12:33:19 +0200

    fix(DB/Creatures): add Intervene spell to Bonechewer Shield Disciple

---
+++ data/sql/updates/pending_db_world/rev_1726137102045054800.sql
@@ -0,0 +1,4 @@
+-- Add Intervene spell to Bonechewer Shield Disciple (23236)
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 23236) AND (`source_type` = 0) AND (`id` IN (3));
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(23236, 0, 3, 0, 0, 0, 100, 0, 10000, 15000, 20000, 25000, 0, 0, 11, 41198, 0, 0, 0, 0, 0, 26, 0, 0, 0, 0, 0, 0, 0, 0, '');
