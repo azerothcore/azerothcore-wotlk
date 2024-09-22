44a10e7be86803bbe64cf65508d70d16e13a947d
Lukas <53310556+proxical@users.noreply.github.com>
2024-09-22 00:37:49 -0700

    fix(DB/quest_offer_reward): C'Thun's Legacy turnin dialogue (#19130)
    * fix(DB/quest_offer_reward): C'Thun's Legacy turnin dialogue
    
    * fix(DB/quest_offer_reward): C'Thun's Legacy turnin dialogue

---
+++ data/sql/updates/pending_db_world/rev_1719113163679056400.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `quest_offer_reward` SET `RewardText`='We will leave this place on our own, $G Lord:Lady; $N - once we are certain that the evil within has been wholly destroyed. Your journey of legend is almost at an end.', `VerifiedBuild`=0 WHERE `ID`=8801;
+UPDATE `quest_request_items` SET `CompletionText`='$G Lord:Lady; $N, you have freed us of its grasp.', `VerifiedBuild`=0 WHERE `ID`=8801;
