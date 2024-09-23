6ee7917ef0a1174ebe67ef04a4b3f51d994f0200
sudlud <sudlud@users.noreply.github.com>
2024-09-23 13:42:37 +0200

    Update data/sql/updates/pending_db_world/rev_1724384703130549345.sql

--- data/sql/updates/pending_db_world/rev_1724384703130549345.sql
+++ data/sql/updates/pending_db_world/rev_1724384703130549345.sql
@@ -7,5 +7,5 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 -- Updating loot for Eye of Culuthas (Entry: 20394)
 DELETE FROM `creature_loot_template` WHERE `Entry` = 20394 AND `Item` IN (29799, 29800);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(20394, 29799, 0, 79.7406, 0, 1, 0, 1, 1, 'Eye of Culuthas - Lifeless Tendril'),
-(20394, 29800, 0, 18.9738, 0, 1, 0, 1, 1, 'Eye of Culuthas - Evil Eye');
+(20394, 29799, 0, 80, 0, 1, 2, 1, 1, 'Eye of Culuthas - Lifeless Tendril'),
+(20394, 29800, 0, 0, 0, 1, 2, 1, 1, 'Eye of Culuthas - Evil Eye');
