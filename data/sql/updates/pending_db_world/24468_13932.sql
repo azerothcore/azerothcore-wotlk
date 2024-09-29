d23281af8b079eeae3577a2e5f7e56427e8aaa36
FlyingArowana <AugustoMendes4426@gmail.com>
2024-09-29 23:57:57 +0100

    Create 24468_13932.sql
    Adds "Pol Amberstill <Ram Racing Apprentice>"[24468] as Quest Starter for "Another Year, Another Souvenir"[13932] after completing "Now This is Ram Racing... Almost"[11318].

---
+++ data/sql/updates/pending_db_world/24468_13932.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature_queststarter` WHERE (`quest` = 13932) AND (`id` IN (24468));
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(24468, 13932);
+
+
+# Adds "Pol Amberstill <Ram Racing Apprentice>"[24468] as Quest Starter for "Another Year, Another Souvenir"[13932] after completing "Now This is Ram Racing... Almost"[11318].
