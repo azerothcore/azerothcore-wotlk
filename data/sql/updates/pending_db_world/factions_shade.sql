ff01691077d35eeef649b85f94ffbe8c53342f23
Dan <83884799+elthehablo@users.noreply.github.com>
2024-09-29 12:09:36 +0200

    Update factions_shade.sql

--- data/sql/updates/pending_db_world/factions_shade.sql
+++ data/sql/updates/pending_db_world/factions_shade.sql
@@ -1,4 +1,4 @@
---
+-- 
 UPDATE `creature_template` SET `faction` = 16, `VerifiedBuild` = 53788 WHERE `entry` IN (23318, 23523, 23524); -- Rogue, elementalist, spiritbinder
 UPDATE `creature_template` SET `faction` = 1692, `VerifiedBuild` = 53788 WHERE `entry` = 22841; -- Shade of Akama
 UPDATE `creature_template` SET `faction` = 1847, `VerifiedBuild` = 53788 WHERE `entry` = 23216; -- Ashtongue Defender
