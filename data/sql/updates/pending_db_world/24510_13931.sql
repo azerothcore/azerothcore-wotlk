478192adfdcba2330547337ac435dd1d3e097f04
FlyingArowana <AugustoMendes4426@gmail.com>
2024-09-30 16:02:49 +0100

    Delete data/sql/updates/pending_db_world/24510_13931.sql

--- data/sql/updates/pending_db_world/24510_13931.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `creature_queststarter` WHERE (`quest` = 13931) AND (`id` IN (24510));
-INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
-(24510, 13931);
-
-# Adds "Driz Tumblequick <Ram Racing Apprentice>"[24510] as Quest Starter for "Another Year, Another Souvenir"[13931] after completing "Now This is Ram Racing... Almost"[11409].
