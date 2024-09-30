255baab308c88f28cc656d9fdf7386f2fa2e89da
FlyingArowana <AugustoMendes4426@gmail.com>
2024-09-30 16:04:05 +0100

    Corrected the SQL File
    # Adds "Pol Amberstill <Ram Racing Apprentice>"[24468] as Quest Starter for "Another Year, Another Souvenir"[13932] after completing "Now This is Ram Racing... Almost"[11318].
    # Adds "Driz Tumblequick <Ram Racing Apprentice>"[24510] as Quest Starter for "Another Year, Another Souvenir"[13931] after completing "Now This is Ram Racing... Almost"[11409].
    
    Thanks @pangolp for the help

---
+++ data/sql/updates/pending_db_world/rev_1727708457969593200.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `creature_queststarter` WHERE `quest`=13932 AND `id`=24468;
+DELETE FROM `creature_queststarter` WHERE `quest`=13931 AND `id`=24510;
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(24468, 13932),
+(24510, 13931);
