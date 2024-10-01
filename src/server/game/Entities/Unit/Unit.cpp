856a966b799f02dcf4ca751f822c14d00c19dc6b
Alan <sr.alansantos@hotmail.com>
2024-10-01 09:55:26 -0300

    update

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14282,7 +14282,7 @@ void Unit::UpdateSpeed(UnitMoveType mtype, bool forced)
                 {
                     main_speed_mod  = GetTotalAuraModifier(SPELL_AURA_MOD_INCREASE_FLIGHT_SPEED) + GetTotalAuraModifier(SPELL_AURA_MOD_INCREASE_VEHICLE_FLIGHT_SPEED);
 
-                    if (GetShapeshiftForm() != FORM_FLIGHT)
+                    if (GetShapeshiftForm() != FORM_FLIGHT_EPIC | GetShapeshiftForm() != FORM_FLIGHT)
                         stack_bonus = GetTotalAuraMultiplier(SPELL_AURA_MOD_MOUNTED_FLIGHT_SPEED_ALWAYS);
                 }
 
