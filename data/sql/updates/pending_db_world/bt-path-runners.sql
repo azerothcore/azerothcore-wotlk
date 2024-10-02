dca9c214694e7dbcb88f3ab3c3b4a23182a2a6c6
heyitsbench <benjymansy123@gmail.com>
2024-10-02 07:45:22 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/bt-path-runners.sql
@@ -0,0 +1,78 @@
+UPDATE `waypoint_data` SET `move_type` = 0 WHERE `id` IN (
+1480150, -- Aqueous Spawn
+1480210, -- Aqueous Spawn
+1480170, -- Aqueous Spawn
+1480130, -- Aqueous Spawn
+1480190, -- Aqueous Spawn
+-- 1480580, running
+-- 1480550, running
+-- 1480870, running
+-- 1480800, running
+-- 1480720, running
+-- 1480940, running
+-- 1480650, running
+-- 1481410, running
+-- 1481420, running
+-- 1481430, running
+-- 1481400, running
+-- 1481080, running
+-- 1481060, running
+-- 1481000, running
+-- 1481040, running
+-- 1481020, running
+-- 1481390, flying
+-- 1481380, flying
+-- 1481290, flying
+-- 1481280, flying
+-- 1481240, mostly flying
+-- 1481120, mostly flying
+-- 1481250, mostly flying
+-- 1481220, flying
+-- 1481270, flying
+-- 1481230, flying
+-- 1482130, running
+-- 1482050, running
+-- 1481790, running
+-- 1481760, running
+-- 1481820, running
+-- 1481850, running
+-- 1482180, running
+-- 1482140, running
+-- 1482300, running
+-- 1482470, running
+-- 1482510, running
+-- 1482490, running
+-- 1483180, running
+-- 1482680, running
+-- 1483660, running
+-- 1483690, running
+1483640, -- Bonechewer Behemoth
+1483630, -- Bonechewer Behemoth
+1483600, -- Bonechewer Behemoth
+-- 1484860, running
+-- 1484350, running
+-- 1484580, running
+-- 1484300, running
+-- 1484620, running
+-- 1484490, running
+-- 1484470, running
+-- 1484270, running
+-- 1484810, running
+-- 1484730, running
+-- 1485240, running
+-- 1485280, running
+-- 1485340, running
+-- 1485320, running
+-- 1485300, running
+-- 1485260, running
+-- 1486780, running
+-- 1485910, running
+-- 1485900, running
+-- 1485360, running
+-- 1486040, running
+-- 1486880, running
+-- 1486850, running
+-- 1486860, running
+-- 1486870, running
+-- 1487580, running
+);
