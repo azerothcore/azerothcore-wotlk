036654481a3926b63e23d696de5936aaa39e5192
heyitsbench <benjymansy123@gmail.com>
2024-10-02 08:27:41 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/shattrath-flasks.sql
@@ -0,0 +1,8 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (-41608, -41609, -41610, -41611, -46837, -46839);
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(-41609, -41607, 0, 'Shattrath Flask of Fortification'),
+(-41610, -41605, 0, 'Shattrath Flask of Mighty Restoration'),
+(-41611, -41604, 0, 'Shattrath Flask of Supreme Power'),
+(-41608, -41606, 0, 'Shattrath Flask of Relentless Assault'),
+(-46837, -46838, 0, 'Shattrath Flask of Pure Death'),
+(-46839, -46840, 0, 'Shattrath Flask of Blinding Light');
