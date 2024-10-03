de7b1eac45e2a9b707c9b9779891ddf7a338ed8a
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-10-02 21:06:11 -0400

    fix(DB/Creature): Adjust move type of various Black Temple creatures. (#20120)

---
+++ data/sql/updates/pending_db_world/bt-path-runners.sql
@@ -0,0 +1,10 @@
+UPDATE `waypoint_data` SET `move_type` = 0 WHERE `id` IN (
+1480150, -- Aqueous Spawn
+1480210, -- Aqueous Spawn
+1480170, -- Aqueous Spawn
+1480130, -- Aqueous Spawn
+1480190, -- Aqueous Spawn
+1483640, -- Bonechewer Behemoth
+1483630, -- Bonechewer Behemoth
+1483600 -- Bonechewer Behemoth
+);
