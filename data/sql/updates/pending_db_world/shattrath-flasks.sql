73a674dd2f27481543bdb4f3358bba3ae4c876c4
heyitsbench <benjymansy123@gmail.com>
2024-10-03 08:40:10 -0400

    Add root spell, toggle autocast for area effects.

--- data/sql/updates/pending_db_world/shattrath-flasks.sql
+++ data/sql/updates/pending_db_world/shattrath-flasks.sql
@@ -8,10 +8,10 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comm
 (-46839, -46840, 0, 'Shattrath Flask of Blinding Light'),
 (-45373, -45374, 0, 'Bloodberry Elixir');
 
-UPDATE `spell_area` SET `spell` = 41607 WHERE `spell` = 41609;
-UPDATE `spell_area` SET `spell` = 41605 WHERE `spell` = 41610;
-UPDATE `spell_area` SET `spell` = 41604 WHERE `spell` = 41611;
-UPDATE `spell_area` SET `spell` = 41606 WHERE `spell` = 41608;
-UPDATE `spell_area` SET `spell` = 46838 WHERE `spell` = 46837;
-UPDATE `spell_area` SET `spell` = 46840 WHERE `spell` = 46839;
-UPDATE `spell_area` SET `spell` = 45374 WHERE `spell` = 45373;
+UPDATE `spell_area` SET `spell` = 41607, `aura_spell` = 41609, `autocast` = 1 WHERE `spell` = 41609;
+UPDATE `spell_area` SET `spell` = 41605, `aura_spell` = 41610, `autocast` = 1 WHERE `spell` = 41610;
+UPDATE `spell_area` SET `spell` = 41604, `aura_spell` = 41611, `autocast` = 1 WHERE `spell` = 41611;
+UPDATE `spell_area` SET `spell` = 41606, `aura_spell` = 41608, `autocast` = 1 WHERE `spell` = 41608;
+UPDATE `spell_area` SET `spell` = 46838, `aura_spell` = 46837, `autocast` = 1 WHERE `spell` = 46837;
+UPDATE `spell_area` SET `spell` = 46840, `aura_spell` = 46839, `autocast` = 1 WHERE `spell` = 46839;
+UPDATE `spell_area` SET `spell` = 45374, `aura_spell` = 45373, `autocast` = 1 WHERE `spell` = 45373;
