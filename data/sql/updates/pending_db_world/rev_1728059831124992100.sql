a26b57656daa6c7a93bd6acfa498bef223a8d2cc
conorb1745 <conorb17455@gmail.com>
2024-10-04 18:47:17 +0200

    fix(cpp/db): Quest how to win friends and infleunce enemies missing dialogue during torture
    Scarlet Crusader (id: 28940), Scarlet Marksman (id: 28610), Scarlet Preacher (id: 28939) and
    Scarlet Commander (id: 28936) were missing some dialog in the database and script
    npc_crusade_persuaded always succeeded when the spell on Keleseth's Persuader (item id: 39371
    and spell id: 52781) procced. It now only succeeds thirty percent of the time. Increased ppmRate for spell to 8.
    Finally removed aggro when scarlet was 'persuaded'
    
    closes Azerothcore issue #20110

---
+++ data/sql/updates/pending_db_world/rev_1728059831124992100.sql
@@ -0,0 +1,62 @@
+INSERT INTO acore_world.broadcast_text
+(ID, LanguageID, MaleText, FemaleText, EmoteID1, EmoteID2, EmoteID3, EmoteDelay1, EmoteDelay2, EmoteDelay3, SoundEntriesId, EmotesID, Flags, VerifiedBuild)
+VALUES(77372, 0, 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019),
+(77373, 0, 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019),
+(77374, 0, 'Look out! There''s a bee loose and it''s on a stinging rampage!', 'Look out! There''s a bee loose and it''s on a stinging rampage!', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019),
+(77375, 0, 'That one tickled...', 'That one tickled...', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019),
+(77376, 0, 'I''m going to send you back to the Lich King in pieces!', 'I''m going to send you back to the Lich King in pieces!', 0, 0, 0, 0, 0, 0, 0, 0, 1, 18019);
+
+INSERT INTO acore_world.creature_text
+(CreatureID, GroupID, ID, `Text`, `Type`, `Language`, Probability, Emote, Duration, Sound, BroadcastTextId, TextRange, comment)
+VALUES(28939, 1, 6, 'You... You''ll get nothing... And like it...', 12, 0, 100.0, 0, 0, 0, 77372, 0, 'crusader SAY_CURSADER7'),
+(28939, 1, 7, 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 12, 0, 100.0, 0, 0, 0, 77373, 0, 'crusader SAY_CURSADER8'),
+(28939, 1, 8, 'Look out! There''s a bee loose and it''s on a stinging rampage!', 12, 0, 100.0, 0, 0, 0, 77374, 0, 'crusader SAY_CURSADER9'),
+(28939, 1, 9, 'That one tickled...', 12, 0, 100.0, 0, 0, 0, 77375, 0, 'crusader SAY_CURSADER10'),
+(28939, 1, 10, 'I''m going to send you back to the Lich King in pieces!', 12, 0, 100.0, 0, 0, 0, 6, 0, 'crusader SAY_CURSADER11');
+
+INSERT INTO acore_world.creature_text
+(CreatureID, GroupID, ID, `Text`, `Type`, `Language`, Probability, Emote, Duration, Sound, BroadcastTextId, TextRange, comment)
+VALUES(28940, 1, 6, 'You... You''ll get nothing... And like it...', 12, 0, 100.0, 0, 0, 0, 77372, 0, 'crusader SAY_CURSADER7'),
+(28940, 1, 7, 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 12, 0, 100.0, 0, 0, 0, 77373, 0, 'crusader SAY_CURSADER8'),
+(28940, 1, 8, 'Look out! There''s a bee loose and it''s on a stinging rampage!', 12, 0, 100.0, 0, 0, 0, 77374, 0, 'crusader SAY_CURSADER9'),
+(28940, 1, 9, 'That one tickled...', 12, 0, 100.0, 0, 0, 0, 77375, 0, 'crusader SAY_CURSADER10'),
+(28940, 1, 10, 'I''m going to send you back to the Lich King in pieces!', 12, 0, 100.0, 0, 0, 0, 77376, 0, 'crusader SAY_CURSADER11');
+
+INSERT INTO acore_world.creature_text
+(CreatureID, GroupID, ID, `Text`, `Type`, `Language`, Probability, Emote, Duration, Sound, BroadcastTextId, TextRange, comment)
+VALUES(28610, 1, 6, 'You... You''ll get nothing... And like it...', 12, 0, 100.0, 0, 0, 0, 77372, 0, 'crusader SAY_CURSADER7'),
+(28610, 1, 7, 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 12, 0, 100.0, 0, 0, 0, 77373, 0, 'crusader SAY_CURSADER8'),
+(28610, 1, 8, 'Look out! There''s a bee loose and it''s on a stinging rampage!', 12, 0, 100.0, 0, 0, 0, 77374, 0, 'crusader SAY_CURSADER9'),
+(28610, 1, 9, 'That one tickled...', 12, 0, 100.0, 0, 0, 0, 77375, 0, 'crusader SAY_CURSADER10'),
+(28610, 1, 10, 'I''m going to send you back to the Lich King in pieces!', 12, 0, 100.0, 0, 0, 0, 77376, 0, 'crusader SAY_CURSADER11');
+
+INSERT INTO acore_world.creature_text
+(CreatureID, GroupID, ID, `Text`, `Type`, `Language`, Probability, Emote, Duration, Sound, BroadcastTextId, TextRange, comment)
+VALUES(28936, 1, 0, 'You'll be hanging in the gallows shortly, Scourge fiend!', 12, 0, 100.0, 0, 0, 0, 29160, 0, 'crusader SAY_CURSADER1'),
+(28936, 1, 1, 'You'll have to kill me, monster. I will tell you NOTHING!', 12, 0, 100.0, 0, 0, 0, 29142, 0, 'crusader SAY_CURSADER2'),
+(28936, 1, 2, 'You hit like a girl. Honestly. Is that the best you can do?', 12, 0, 100.0, 0, 0, 0, 29146, 0, 'crusader SAY_CURSADER3'),
+(28936, 1, 3, 'ARGH! You burned my last good tabard!', 12, 0, 100.0, 0, 0, 0, 29162, 0, 'crusader SAY_CURSADER4'),
+(28936, 1, 4, 'Argh... The pain... The pain is almost as unbearable as the lashings I received in grammar school when I was but a child.', 12, 0, 100.0, 0, 0, 0, 29143, 0, 'crusader SAY_CURSADER5');
+(28936, 1, 5, 'I used to work for Grand Inquisitor Isillien! Your idea of pain is a normal mid-afternoon for me!', 12, 0, 100.0, 0, 0, 0, 29161, 0, 'crusader SAY_CURSADER6');
+(28936, 1, 6, 'You... You''ll get nothing... And like it...', 12, 0, 100.0, 0, 0, 0, 77372, 0, 'crusader SAY_CURSADER7'),
+(28936, 1, 7, 'Do you think this to be pain? To be suffering? HAH! Destroy this mortal shell so that I may ascend to the heavens!', 12, 0, 100.0, 0, 0, 0, 77373, 0, 'crusader SAY_CURSADER8'),
+(28936, 1, 8, 'Look out! There''s a bee loose and it''s on a stinging rampage!', 12, 0, 100.0, 0, 0, 0, 77374, 0, 'crusader SAY_CURSADER9'),
+(28936, 1, 9, 'That one tickled...', 12, 0, 100.0, 0, 0, 0, 77375, 0, 'crusader SAY_CURSADER10'),
+(28936, 1, 10, 'I''m going to send you back to the Lich King in pieces!', 12, 0, 100.0, 0, 0, 0, 77376, 0, 'crusader SAY_CURSADER11'),
+(28936, 2, 0, 'I''ll tell you everything! STOP! PLEASE!', 12, 0, 100.0, 0, 0, 0, 29149, 0, 'break crusader SAY_PERSUADED1'),
+(28936, 3, 0, 'We... We have only been told that the "Crimson Dawn" is an awakening. You... You see, the Light speaks to the High General. It is the Light...', 12, 0, 100.0, 0, 0, 0, 29150, 0, 'break crusader SAY_PERSUADED2'),
+(28936, 4, 0, 'The Light that guides us. This movement was set in motion before you came... We... We do as we are told. It is what must be done.', 12, 0, 100.0, 0, 0, 0, 29151, 0, 'break crusader SAY_PERSUADED3'),
+(28936, 5, 0, 'I know very litte else... The High General chooses who may go and who must stay behind. There''s nothing else... You must believe me!', 12, 0, 100.0, 0, 0, 0, 29152, 0, 'break crusader SAY_PERSUADED4'),
+(28936, 6, 0, 'LIES! The pain you are about to endure will be talked about for years to come!', 12, 0, 100.0, 0, 0, 0, 29163, 0, 'break crusader SAY_PERSUADED5'),
+(28936, 7, 0, 'NO! PLEASE! There is one more thing that I forgot to mention... A courier comes soon... From Hearthglen. It...', 12, 0, 100.0, 0, 0, 0, 29153, 0, 'break crusader SAY_PERSUADED6'),
+(28936, 8, 0, 'I''ll tear the secrets from your soul! Tell me about the "Crimson Dawn" and your life may be spared!', 12, 0, 100.0, 0, 0, 0, 29138, 0, 'player SAY_PERSUADE1'),
+(28936, 8, 1, 'Tell me what you know about "Crimson Dawn" or the beatings will continue!', 12, 0, 100.0, 0, 0, 0, 29134, 0, 'player SAY_PERSUADE2'),
+(28936, 8, 2, 'I''m through being courteous with your kind, human! What is the "Crimson Dawn?"', 12, 0, 100.0, 0, 0, 0, 29135, 0, 'player SAY_PERSUADE3'),
+(28936, 8, 3, 'Is your life worth so little? Just tell me what I need to know about "Crimson Dawn" and I''ll end your suffering quickly.', 12, 0, 100.0, 0, 0, 0, 29139, 0, 'player SAY_PERSUADE4'),
+(28936, 8, 4, 'I can keep this up for a very long time, Scarlet dog! Tell me about the "Crimson Dawn!"', 12, 0, 100.0, 0, 0, 0, 29137, 0, 'player SAY_PERSUADE5'),
+(28936, 8, 5, 'What is the "Crimson Dawn?"', 12, 0, 100.0, 0, 0, 0, 29133, 0, 'player SAY_PERSUADE6'),
+(28936, 8, 6, '"Crimson Dawn!" What is it! Speak!', 12, 0, 100.0, 0, 0, 0, 29136, 0, 'player SAY_PERSUADE7');
+
+UPDATE `creature_template` SET `ScriptName` = 'npc_crusade_persuaded' WHERE (`entry` = 28940);
+
+UPDATE `item_template` SET `spellppmRate_1` = 8 WHERE (`entry` = 39371);
