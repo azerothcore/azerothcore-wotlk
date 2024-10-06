0091b43f3d40c6114ae8fa55f851999b11cf9c9d
trickerer <onlysuffering@gmail.com>
2024-10-06 14:11:50 +0700

    NPCBots: correct merge error from 80e1dbe736

--- src/server/game/Handlers/BattleGroundHandler.cpp
+++ src/server/game/Handlers/BattleGroundHandler.cpp
@@ -237,10 +237,6 @@ void WorldSession::HandleBattlemasterJoinOpcode(WorldPacket& recvData)
     else
     {
         Group* grp = _player->GetGroup();
-        //npcbot: check premade for bots
-        if (isPremade && !BotMgr::IsNpcBotsPremadeEnabled() && grp->GetFirstBotMember() != nullptr)
-            isPremade = false;
-        //end npcbot
 
         // no group or not a leader
         if (!grp || grp->GetLeaderGUID() != _player->GetGUID())
@@ -295,6 +291,10 @@ void WorldSession::HandleBattlemasterJoinOpcode(WorldPacket& recvData)
         }
 
         isPremade = (grp->GetMembersCount() >= bg->GetMinPlayersPerTeam() && bgTypeId != BATTLEGROUND_RB);
+        //npcbot: check premade for bots
+        if (isPremade && !BotMgr::IsNpcBotsPremadeEnabled() && grp->GetFirstBotMember() != nullptr)
+            isPremade = false;
+        //end npcbot
         uint32 avgWaitTime = 0;
 
         GroupQueueInfo* ginfo = bgQueue.AddGroup(_player, grp, bgTypeId, bracketEntry, 0, false, isPremade, 0, 0);
