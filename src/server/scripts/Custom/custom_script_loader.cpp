dd38688e0d8e903c1feed2a9468952abc452ca60
Indra <66976753+Shirokosmiles@users.noreply.github.com>
2024-10-07 22:36:17 +0600

    Исправление баффа на оружие чтобы был только на кастере

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -28,6 +28,7 @@ void AddSC_npc_1v1arena();
 void AddSC_DeathMatchkill();
 void AddSC_NPC_RANK_VENDOR();
 void AddSC_ServerMenuPlayerGossip();
+void AddSC_npc_custom_boss();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -44,4 +45,5 @@ void AddCustomScripts()
     AddSC_DeathMatchkill();
     AddSC_NPC_RANK_VENDOR();
     AddSC_ServerMenuPlayerGossip();
+    AddSC_npc_custom_boss();
 }
