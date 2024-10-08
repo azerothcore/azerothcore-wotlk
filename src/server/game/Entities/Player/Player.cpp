3b56253821bd654d877e9258361de5120a30ca8c
Indra <66976753+Shirokosmiles@users.noreply.github.com>
2024-10-08 10:40:25 +0600

    Теперь бонусные баффы в инстах даются за каждые 2 ранга а для премиум за каждый ранг

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -16541,14 +16541,11 @@ void Player::RankControlOnLogin()
 
 void Player::GetRangBuffInInstance(int amount)
 {
+    amount = this->GetSession()->IsPremium() ? amount : amount / 2;
     for (int i = 0; i < amount; i++) {
         AddAura(62519, this); /* лечение +8% */
         AddAura(66721, this); /* урон +5% */
     }
-
-    // Берсерк для випов
-    if (this->GetSession()->IsPremium())
-        AddAura(41107, this);
 }
 
 void Player::RemoveRankBuff() {
@@ -16566,7 +16563,13 @@ void Player::VerifiedRankBuff(Map* map)
         if (!HasAura(62519) && !HasAura(66721))
             GetRangBuffInInstance(GetRankByExp());
         else {
-            if (GetAuraCount(62519) != uint32(GetRankByExp())) {
+            if (this->GetSession()->IsPremium()) {
+                if (GetAuraCount(62519) != uint32(GetRankByExp())) {
+                    RemoveRankBuff();
+                    GetRangBuffInInstance(GetRankByExp());
+                }               
+            }
+            else if (GetAuraCount(62519)*2 != uint32(GetRankByExp())) {
                 RemoveRankBuff();
                 GetRangBuffInInstance(GetRankByExp());
             }
