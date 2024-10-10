95874273a1ce58a803fecedd4e541fe2f9532632
Rorschach91 <108557877+Rorschach91@users.noreply.github.com>
2024-10-11 00:55:41 +0200

    Create Asp_Npcs_Flags_change.sql

---
+++ data/sql/updates/pending_db_world/Asp_Npcs_Flags_change.sql
@@ -0,0 +1,20 @@
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29072);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29074);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29070);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29032);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29065);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29068);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29073);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29061);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29071);
+
+UPDATE `creature_template` SET `unit_flags` = 163842 WHERE (`entry` = 29067);
