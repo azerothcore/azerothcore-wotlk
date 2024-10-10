2809f300074b632aa50b496c50e480a13c6a725f
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2024-10-10 14:08:52 -0400

    fix(DB/Creature): Adjust model probability for Whirling Blade trigger creatures. (#20177)
    Init.

---
+++ data/sql/updates/pending_db_world/whirling-blade-model.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 23369;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(23369, 0, 1126, 1, 0, 51831),
+(23369, 1, 16946, 1, 1, 51831);
