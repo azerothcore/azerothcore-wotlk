76724f9d441b9ad07f5b20926f12cfba4c3fc987
heyitsbench <benjymansy123@gmail.com>
2024-10-11 13:39:59 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/gorefiend-model-data.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 22871;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(22871, 0, 21254, 1, 0, 51831),
+(22871, 1, 21262, 1, 1, 51831);
