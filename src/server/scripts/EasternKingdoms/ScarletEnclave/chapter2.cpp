40e034515128d6dee5e85c745dc46b5818644823
Pagani Walter <paganiwalter@gmail.com>
2024-10-16 22:34:46 -0300

    Reset npc if no player

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter2.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter2.cpp
@@ -214,20 +214,6 @@ class npc_koltira_deathweaver : public CreatureScript
 public:
     npc_koltira_deathweaver() : CreatureScript("npc_koltira_deathweaver") { }
 
-    bool OnQuestAccept(Player* player, Creature* creature, const Quest* quest) override
-    {
-        if (quest->GetQuestId() == QUEST_BREAKOUT)
-        {
-            creature->SetStandState(UNIT_STAND_STATE_STAND);
-            creature->setActive(true);
-            creature->SetReactState(REACT_DEFENSIVE);
-
-            if (npc_escortAI* pEscortAI = CAST_AI(npc_koltira_deathweaver::npc_koltira_deathweaverAI, creature->AI()))
-                pEscortAI->Start(false, false, player->GetGUID());
-        }
-        return true;
-    }
-
     CreatureAI* GetAI(Creature* creature) const override
     {
         return new npc_koltira_deathweaverAI(creature);
@@ -235,16 +221,30 @@ public:
 
     struct npc_koltira_deathweaverAI : public npc_escortAI
     {
+        void sQuestAccept(Player* player, Quest const* quest) override
+        {
+            if (quest->GetQuestId() == QUEST_BREAKOUT)
+            {
+                me->SetStandState(UNIT_STAND_STATE_STAND);
+                me->setActive(true);
+                me->SetReactState(REACT_DEFENSIVE);
+                npc_escortAI::Start(true, false, player->GetGUID(), quest);
+                active = true;
+            }
+        }
+
         npc_koltira_deathweaverAI(Creature* creature) : npc_escortAI(creature), summons(me)
         {
             m_uiWave = 0;
             m_uiWave_Timer = 3000;
+            active = false;
         }
 
         uint32 m_uiWave;
         uint32 m_uiWave_Timer;
         ObjectGuid m_uiValrothGUID;
         SummonList summons;
+        bool active;
 
         void Reset() override
         {
@@ -258,6 +258,7 @@ public:
                 me->LoadEquipment(0, true);
                 me->RemoveAllAuras();
                 summons.DespawnAll();
+                active = false;
             }
         }
 
@@ -371,7 +372,9 @@ public:
                     if (player->GetQuestStatus(QUEST_BREAKOUT) == QUEST_STATUS_INCOMPLETE)
                         player->FailQuest(QUEST_BREAKOUT);
                 }
-            }
+            } else
+                if (active)
+                    me->DespawnOrUnsummon();
 
             npc_escortAI::UpdateAI(uiDiff);
 
