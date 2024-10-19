05927145d6de77b3bfcf3b6b3e63b08fe3a5723c
Zoidwaffle <zoidwaffle@gmail.com>
2024-10-19 13:48:34 +0200

    fix(DB/Quest): Stones of Binding

---
+++ data/sql/updates/pending_db_world/rev_1729179186645856952.sql
@@ -0,0 +1,7 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1729179186645856952');
+
+-- Looting the 3 keys is annoying as a group, since the stones appear ready to loot but
+-- only allow looting after a while after the previous pickup
+-- Objects: Burning Key, Cresting Key & Thundering Key from Stones of Binding
+
+UPDATE `gameobject_template` SET `Data2` = 0 WHERE `entry` IN (2689,2690,2691);
