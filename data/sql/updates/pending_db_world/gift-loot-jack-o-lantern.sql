07cf554498de921314e8fd2d087e088f2c17ae3d
heyitsbench <benjymansy123@gmail.com>
2024-10-28 12:55:40 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/gift-loot-jack-o-lantern.sql
@@ -0,0 +1,3 @@
+DELETE FROM `item_loot_template` WHERE `Entry` = 34077 AND `Item` = 34068;
+INSERT INTO `item_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(34077, 34068, 0, 100, 0, 1, 0, 3, 5, 'Crudely Wrapped Gift - Weighted Jack-o\'-Lantern');
