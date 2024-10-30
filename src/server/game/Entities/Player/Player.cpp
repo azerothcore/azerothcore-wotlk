d1d39b749114d3c7340d177e80349600aa6549ba
silviu20092 <dobrescu.silviu92@gmail.com>
2024-10-30 14:21:15 +0200

    Add hook for applying weapon damage

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -6934,6 +6934,8 @@ void Player::_ApplyWeaponDamage(uint8 slot, ItemTemplate const* proto, ScalingSt
 
         if (apply)
         {
+            sScriptMgr->OnApplyWeaponDamage(this, slot, proto, minDamage, maxDamage, i);
+
             if (minDamage > 0.f)
             {
                 SetBaseWeaponDamage(WeaponAttackType(attType), MINDAMAGE, minDamage, i);
