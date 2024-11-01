617d574f812ca9c375db2fcd1e21fce33e041046
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2024-11-01 11:49:51 +0100

    Create Grikkin_Copperspring_Spawn.sql
    Adding spawn point for Grikkin

---
+++ data/sql/updates/pending_db_world/Grikkin_Copperspring_Spawn.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM `creature` WHERE (`id1` = 25176) AND (`guid` IN (3110422));
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+(3110422, 25176, 0, 0, 530, 0, 0, 1, 1, 1, -2160.66, 6639.05, 0.27, 0.24, 300, 0, 0, 7, 0, 0, 0, 0, 0, '', 0);
