bbbcaac77c86d648690f71b6a7d9c2915b4804fb
Jelle Meeus <sogladev@gmail.com>
2024-11-01 20:54:14 +0100

    refactor(scripts/CrusadersColiseum): spell scripts use registry macros  (#20400)

---
+++ data/sql/updates/pending_db_world/rev_1730483438978049424.sql
@@ -0,0 +1,8 @@
+--
+UPDATE `spell_script_names` SET `ScriptName`='spell_toc25_mistress_kiss_aura' WHERE `spell_id` IN (66334,67905,67906,67907) AND `ScriptName`='spell_toc25_mistress_kiss';
+UPDATE `spell_script_names` SET `ScriptName`='spell_faction_champion_warl_unstable_affliction_aura' WHERE `spell_id` IN (65812,68154,68155,68156) AND `ScriptName`='spell_faction_champion_warl_unstable_affliction';
+UPDATE `spell_script_names` SET `ScriptName`='spell_pursuing_spikes_aura' WHERE `spell_id` IN (65920,65922,65923) AND `ScriptName`='spell_pursuing_spikes';
+UPDATE `spell_script_names` SET `ScriptName`='spell_gen_leeching_swarm_aura' WHERE `spell_id` IN (66118,67630,68646,68647) AND `ScriptName`='spell_gen_leeching_swarm';
+UPDATE `spell_script_names` SET `ScriptName`='spell_valkyr_essence_aura' WHERE `spell_id` IN (65684,65686,67176,67177,67178,67222,67223,67224) AND `ScriptName`='spell_valkyr_essence';
+UPDATE `spell_script_names` SET `ScriptName`='spell_valkyr_touch_aura' WHERE `spell_id` IN (65950,66001,67281,67282,67283,67296,67297,67298) AND `ScriptName`='spell_valkyr_touch';
+UPDATE `spell_script_names` SET `ScriptName`='spell_valkyr_ball_periodic_dummy_aura' WHERE `spell_id`=100101 AND `ScriptName`='spell_valkyr_ball_periodic_dummy';
