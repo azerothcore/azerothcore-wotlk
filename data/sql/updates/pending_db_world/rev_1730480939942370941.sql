984d67aefb304f0f285ff1e3837583fa1d245efd
Jelle Meeus <sogladev@gmail.com>
2024-11-01 18:09:55 +0100

    boss_mandokir aura:spell_threatening_gaze_aura

--- data/sql/updates/pending_db_world/rev_1730480939942370941.sql
+++ data/sql/updates/pending_db_world/rev_1730480939942370941.sql
@@ -3,3 +3,4 @@ UPDATE `spell_script_names` SET `ScriptName`='spell_shadowfang_keep_haunting_spi
 UPDATE `spell_script_names` SET `ScriptName`='spell_shadowfang_keep_forsaken_skills_aura' WHERE `spell_id`=7054 AND `ScriptName`='spell_shadowfang_keep_forsaken_skills';
 UPDATE `spell_script_names` SET `ScriptName`='spell_scholomance_fixate_aura' WHERE `spell_id`=12021 AND `ScriptName`='spell_scholomance_fixate';
 UPDATE `spell_script_names` SET `ScriptName`='spell_scholomance_boon_of_life_aura' WHERE `spell_id`=17179 AND `ScriptName`='spell_scholomance_boon_of_life';
+UPDATE `spell_script_names` SET `ScriptName`='spell_threatening_gaze_aura' WHERE `spell_id`=24314 AND `ScriptName`='spell_threatening_gaze';
