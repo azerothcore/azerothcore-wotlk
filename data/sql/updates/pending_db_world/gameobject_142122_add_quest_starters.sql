f49f4dd3852c67c8225461406ff02b0b060051f5
Manuel Velasco <48597766+manuel-velascodev@users.noreply.github.com>
2024-11-02 05:32:21 -0600

    fix(DB/gameobject_queststarter): Add queststarter records for GameObject 142122 (#20293)
    * fix(DB/gameobject_queststarter): Added queststarter records for GameObject 14212
    
    I added the records so that these quests can be initiated from both wanted posters.
    
    Quests:
    https://www.wowhead.com/wotlk/quest=2781/wanted-caliph-scorpidsting
    https://www.wowhead.com/wotlk/quest=2875/wanted-andre-firebeard
    
    * Update gameobject_142122_add_quest_starters.sql
    
    Added backticks around the table- and column names.

---
+++ data/sql/updates/pending_db_world/gameobject_142122_add_quest_starters.sql
@@ -0,0 +1,5 @@
+DELETE FROM `gameobject_queststarter` WHERE `id` = 142122 AND `quest` IN (2781, 2875);
+
+INSERT INTO `gameobject_queststarter` (`id`, `quest`) VALUES 
+(142122, 2781), 
+(142122, 2875);
