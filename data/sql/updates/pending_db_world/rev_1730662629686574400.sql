50429973740ee0ebd85efc925d65f4c7596e85ce
avarishd <46330494+avarishd@users.noreply.github.com>
2024-11-03 21:52:11 +0200

    fix(Scripts/Karazhan): Return Fire (Spell) hitting rogues

---
+++ data/sql/updates/pending_db_world/rev_1730662629686574400.sql
@@ -0,0 +1,3 @@
+-- Return Fire (Spell)
+DELETE FROM `spell_script_names` WHERE `spell_id`=29793 AND `ScriptName`='spell_karazhan_returnfire_spell';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (29793, 'spell_karazhan_returnfire_spell');
