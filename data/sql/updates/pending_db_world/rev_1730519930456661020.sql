a8d7b8d736bea309d3a4982c9752b11c32dd686e
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-04 19:16:17 +0330

    Fix(SQL): deactivated sunblade protectors is not linked properly (#20404)
    * Fix(SQL): deactivated sunblade protectors is not linked properly
    
    * Update rev_1730519930456661020.sql
    
    Added backticks.
    
    ---------
    
    Co-authored-by: Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1730519930456661020.sql
@@ -0,0 +1,9 @@
+DELETE FROM `creature_formations` WHERE `leaderGUID` = 42587;
+
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(42587, 42587, 0, 0, 515),
+(42587, 54824, 1.0, 0.0, 515),
+(42587, 42640, 1.0, 1.57, 515),
+(42587, 54829, 1.0, 3.14, 515),
+(42587, 42659, 1.0, 4.71, 515),
+(42587, 42586, 1.0, 6.28, 515);
