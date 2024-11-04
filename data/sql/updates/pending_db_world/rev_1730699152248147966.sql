ce4474e00890c43f470def07315eaae8d1428a6b
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-04 10:41:10 +0330

    fix fel power

--- data/sql/updates/pending_db_world/rev_1730699152248147966.sql
+++ data/sql/updates/pending_db_world/rev_1730699152248147966.sql
@@ -1,1 +1,6 @@
 UPDATE `creature` SET `id1` = 17400 WHERE `guid` IN (138247, 138248, 138249, 138250, 138252, 138253);
+
+INSERT INTO `conditions` 
+(`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) 
+VALUES 
+(22, 6, 17371, 0, 1, 29, 1, 17400, 10, 0, 0, 0, 0, '', 'Only cast Fel Power (33111) if a Felguard Annihilator (17400) is nearby');
