d347eb3fa0bac997878656d607f435f3b8a50f65
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-04 16:37:33 +0330

    Fix(SQL): Unleashed Hellion's Rain of Fire (#20421)
    * Fix(SQL): Unleashed Hellion's Rain of Fire
    
    * Update data/sql/updates/pending_db_world/rev_1730575812039136017.sql
    
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
    
    * back
    
    ---------
    
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1730575812039136017.sql
@@ -0,0 +1,4 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 25002 AND `id` = 1;
+
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `comment`) VALUES
+(25002, 0, 1, 0, 0, 0, 100, 0, 5000, 10000, 0, 0, 11, 31598, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 'Unleashed Hellion - In Combat - Cast Rain of Fire');
