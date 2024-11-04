009dd9fda1a7701ede7eaec5cb5a956ed456d0bc
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-04 04:09:08 +0330

    fix(DB/Formations):  Pack near the first scout is not linked properly (#20403)
    * Fix(Core/SQL): Pack near the first scout is not linked properly
    
    * Update rev_1730519194220305125.sql
    
    Added backticks.
    
    ---------
    
    Co-authored-by: Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1730519194220305125.sql
@@ -0,0 +1,8 @@
+DELETE FROM `creature_formations` WHERE `leaderGUID` = 41817;
+
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(41817, 41817, 0, 0, 515),
+(41817, 42661, 1.0, 0.0, 515),
+(41817, 54838, 1.0, 1.57, 515),
+(41817, 42595, 1.0, 3.14, 515),
+(41817, 43739, 1.0, 4.71, 515);
