95a5677e86e40a226625721e96c3460dfb1c4e23
zalazoltan86 <zalazoltan86@gmail.com>
2024-11-04 21:45:06 +0100

    Fix(DB/quest): 6001-Body and Heart poi

--- data/sql/updates/pending_db_world/rev_1730752429875087600.sql
+++ data/sql/updates/pending_db_world/rev_1730752429875087600.sql
@@ -1,3 +1,3 @@
 DELETE FROM `quest_poi` WHERE `QuestID`=6001;
-INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) 
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`)
 VALUES (6001, 0, 16, 1, 41, 0, 0, 1, 0), (6001, 1, -1, 1, 42, 0, 0, 1, 0);
