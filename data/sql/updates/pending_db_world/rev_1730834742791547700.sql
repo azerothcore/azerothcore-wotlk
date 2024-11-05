2a71a07bef72f0f2e8e97e27e621268903f30211
avarishd <46330494+avarishd@users.noreply.github.com>
2024-11-05 22:43:38 +0200

    fix(Scripts/Item): Obsidian Armor & Frostfire Regalia Set

---
+++ data/sql/updates/pending_db_world/rev_1730834742791547700.sql
@@ -0,0 +1,4 @@
+-- Obsidian Armor
+DELETE FROM `spell_proc_event` WHERE `entry`=27539;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(27539, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 10000);
