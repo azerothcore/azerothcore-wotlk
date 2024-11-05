798cc0078017bcc1ef592a1c1dfed95a79c8e072
avarishd <46330494+avarishd@users.noreply.github.com>
2024-11-05 18:30:30 +0200

    use game_event_creature_quest
    Co-Authored-By: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1730822827611002400.sql
+++ data/sql/updates/pending_db_world/rev_1730822827611002400.sql
@@ -1,5 +1,6 @@
 -- New Year Celebrations!
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 19) AND (`SourceGroup` = 0) AND `SourceEntry` IN (8860,8861);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(19, 0, 8860, 0, 0, 12, 0, 6, 0, 0, 0, 0, 0, '', 'Show quest \'New Year Celebrations!\' (A) only on New Year\'s Eve'),
-(19, 0, 8861, 0, 0, 12, 0, 6, 0, 0, 0, 0, 0, '', 'Show quest \'New Year Celebrations!\' (H) only on New Year\'s Eve');
+DELETE FROM `creature_queststarter` WHERE `quest` IN (8860,8861);
+DELETE FROM `game_event_creature_quest` WHERE `quest` IN (8860,8861);
+INSERT INTO `game_event_creature_quest` (`eventEntry`, `id`, `quest`) VALUES
+(6, 15732, 8860),
+(6, 15732, 8861);
