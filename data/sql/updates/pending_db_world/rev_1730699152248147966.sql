9bf97597316beb48bcf43775345ff386e3c78b03
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-06 09:26:59 +0330

    Update rev_1730699152248147966.sql

--- data/sql/updates/pending_db_world/rev_1730699152248147966.sql
+++ data/sql/updates/pending_db_world/rev_1730699152248147966.sql
@@ -1,5 +1,6 @@
 UPDATE `creature` SET `id1` = 17400 WHERE `guid` IN (138247, 138248, 138249, 138250, 138252, 138253);
 
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceGroup` = 6 AND `SourceEntry` = 17371 AND `ConditionValue1` = 17400;
 INSERT INTO `conditions` 
 (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) 
 VALUES 
