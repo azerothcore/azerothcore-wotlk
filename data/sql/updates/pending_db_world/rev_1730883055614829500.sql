bcb21ac58ec2637580060c31ffefb382995c6acb
avarishd <46330494+avarishd@users.noreply.github.com>
2024-11-06 10:51:44 +0200

    fix(DB/Creature): Darting Hatchling missing periodic aura

---
+++ data/sql/updates/pending_db_world/rev_1730883055614829500.sql
@@ -0,0 +1,4 @@
+-- Darting Hatchling
+DELETE FROM `creature_template_addon` WHERE (`entry` = 35396);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(35396, 0, 0, 0, 0, 0, 0, '62586');
