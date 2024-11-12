dff4b37d2a1e44a7b6f552de093773504a5d5371
FlyingArowana <AugustoMendes4426@gmail.com>
2024-11-12 19:57:06 +0000

    Banners corrected

--- data/sql/updates/pending_db_world/rev_1731438952898922800.sql
+++ data/sql/updates/pending_db_world/rev_1731438952898922800.sql
@@ -1,1 +1,26 @@
 --
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151423 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151424 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151425 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151426 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151427 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151428 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151429 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151430 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151431 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151432 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151433 AND `id` = 180398;
+
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151434 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151435 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151436 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151437 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151438 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151439 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151440 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151441 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151442 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151443 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151444 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151445 AND `id` = 180396;
+
