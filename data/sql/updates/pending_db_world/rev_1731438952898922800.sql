802ba0937b0d71cf91013833b3858894da2346d8
FlyingArowana <AugustoMendes4426@gmail.com>
2024-11-12 20:07:57 +0000

    Updated to match Bench's suggestion
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1731438952898922800.sql
+++ data/sql/updates/pending_db_world/rev_1731438952898922800.sql
@@ -1,26 +1,5 @@
 --
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151423 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151424 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151425 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151426 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151427 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151428 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151429 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151430 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151431 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151432 AND `id` = 180398;
-UPDATE `gameobject` SET `id` = 180396 WHERE `guid` = 151433 AND `id` = 180398;
+UPDATE `gameobject` SET `id` = 180396 WHERE `guid` IN (151423, 151424, 151425, 151426, 151427, 151428, 151429, 151430, 151431, 151432, 151433) AND `id` = 180398;
 
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151434 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151435 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151436 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151437 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151438 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151439 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151440 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151441 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151442 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151443 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151444 AND `id` = 180396;
-UPDATE `gameobject` SET `id` = 180398 WHERE `guid` = 151445 AND `id` = 180396;
+UPDATE `gameobject` SET `id` = 180398 WHERE `guid` IN (151434, 151435, 151436, 151437, 151438, 151439, 151440, 151441, 151442, 151443, 151444, 151445) AND `id` = 180396;
 
