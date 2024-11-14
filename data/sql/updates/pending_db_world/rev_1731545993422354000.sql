11cf1f9c2f1940b2889e417d79ee19bc735960be
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-11-13 22:00:06 -0300

    Create rev_1731545993422354000.sql

---
+++ data/sql/updates/pending_db_world/rev_1731545993422354000.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_electrial_storm' AND `spell_id` = 43648;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(43648, 'spell_electrial_storm');
