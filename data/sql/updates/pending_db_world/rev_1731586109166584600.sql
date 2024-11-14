811d9e552ac28ea9cbe22539452e0a08c08b86d1
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-11-14 09:09:33 -0300

    fix(Scripts/Spells): Spell school reflectors should have failure chance at higher levels

---
+++ data/sql/updates/pending_db_world/rev_1731586109166584600.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_item_spell_reflectors';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(23132, 'spell_item_spell_reflectors'),
+(23097, 'spell_item_spell_reflectors'),
+(23131, 'spell_item_spell_reflectors');
