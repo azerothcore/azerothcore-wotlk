947c192bbf372d14d806770fa8391785403e90f6
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-19 10:22:41 +0330

    Fix(SQL/Conditions): Ur'dan required quest to buy stardust

---
+++ data/sql/updates/pending_db_world/rev_1731998925094021764.sql
@@ -0,0 +1,8 @@
+INSERT INTO `conditions` (
+    `SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`,
+    `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`,
+    `ConditionValue1`, `ConditionValue2`, `ConditionValue3`,
+    `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`
+)
+VALUES
+(23, 14522, 18687, 0, 0, 9, 0, 7625, 0, 0, 0, 0, 0, '', 'Vendor: Require quest 7625 accepted to buy Xorothian Stardust');
