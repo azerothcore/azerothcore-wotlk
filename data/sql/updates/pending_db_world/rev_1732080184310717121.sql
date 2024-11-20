e07de29d3b1ed43a8335eda695025fa4f75ccbca
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-20 08:53:48 +0330

    Fix(SQL/Conditions): Mana Remnants can be used only near Crystal Wards.

---
+++ data/sql/updates/pending_db_world/rev_1732080184310717121.sql
@@ -0,0 +1,6 @@
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 44969;
+
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`)
+VALUES
+(17, 0, 44969, 0, 0, 29, 0, 24980, 15, 0, 0, 0, 0, '', 'Must be near Crystal Ward');
+
