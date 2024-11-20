ecba1c4592a0047ace9fbc172362b9494ef5552c
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2024-11-20 08:26:43 +0330

    I Dont Know

--- data/sql/updates/pending_db_world/rev_1731910244148503352.sql
+++ data/sql/updates/pending_db_world/rev_1731910244148503352.sql
@@ -12,4 +12,6 @@ VALUES
 (24972, 0, 1, 0, 0, 0, 100, 256, 3000, 7000, 3000, 7000, 0, 0, 11, 33688, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - Combat - Cast Crystal Strike'),
 (24972, 0, 2, 0, 0, 0, 100, 256, 8000, 15000, 8000, 15000, 0, 0, 11, 45336, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - Combat - Cast Electrical Overload'),
 (24972, 0, 3, 0, 0, 0, 100, 256, 12000, 20000, 12000, 20000, 0, 0, 11, 35892, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - Combat - Cast Suppression'),
-(24972, 0, 4, 0, 8, 0, 100, 512, 44997, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - On Spellhit (Converting Sentry) - Despawn');
+(24972, 0, 4, 0, 8, 0, 100, 512, 44997, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - On Spellhit (Converting Sentry) - Despawn'),
+(24972, 0, 5, 0, 8, 0, 100, 512, 44997, 0, 0, 0, 0, 0, 29, 5, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - On Spellhit (Attuned Crystal Core) - Start Following Player'),
+(24972, 0, 6, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 30000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Erratic Sentry - After 30 seconds - Despawn');
