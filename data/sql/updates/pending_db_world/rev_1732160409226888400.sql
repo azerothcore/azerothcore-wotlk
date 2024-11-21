f19c98f341ec5480febbb6fd037c2cf3963b463b
Andrew <47818697+Nyeriah@users.noreply.github.com>
2024-11-21 08:28:03 -0300

    fix(DB/Loot): First 4 Zul'Aman bosses should only drop 1 item (#20666)

---
+++ data/sql/updates/pending_db_world/rev_1732160409226888400.sql
@@ -0,0 +1,9 @@
+--
+-- Akil'zon
+UPDATE `creature_loot_template` SET `MaxCount` = 1 WHERE `Entry` = 23574 AND `Reference` = 34150;
+-- Nalorakk
+UPDATE `creature_loot_template` SET `MaxCount` = 1 WHERE `Entry` = 23576 AND `Reference` = 34151;
+-- Jan'alai
+UPDATE `creature_loot_template` SET `MaxCount` = 1 WHERE `Entry` = 23578 AND `Reference` = 34152;
+-- Halazzi
+UPDATE `creature_loot_template` SET `MaxCount` = 1 WHERE `Entry` = 23577 AND `Reference` = 34153;
