9a5e47d1983d4e3e21e631c10ed5b3c754f63a07
FlyingArowana <AugustoMendes4426@gmail.com>
2024-11-26 23:47:44 +0000

    Space correction

--- data/sql/updates/pending_db_world/rev_1732663460298122300.sql
+++ data/sql/updates/pending_db_world/rev_1732663460298122300.sql
@@ -2,5 +2,5 @@
 -- To Skettis! requirement removed from Escape from Skettis
 UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE (`ID` = 11085);
 
--- To Skettis! requirement removed from  Hungry Nether Rays
+-- To Skettis! requirement removed from Hungry Nether Rays
 UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE (`ID` = 11093);
