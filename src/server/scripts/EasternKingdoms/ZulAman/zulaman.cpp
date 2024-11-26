227764a7962eb90c0808923156a97a027268ddc7
Jelle Meeus <sogladev@gmail.com>
2024-11-26 15:04:13 +0100

    add passiveAI header and sort

--- src/server/scripts/EasternKingdoms/ZulAman/zulaman.cpp
+++ src/server/scripts/EasternKingdoms/ZulAman/zulaman.cpp
@@ -15,14 +15,15 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "zulaman.h"
 #include "CreatureScript.h"
+#include "PassiveAI.h"
 #include "Player.h"
 #include "ScriptedCreature.h"
 #include "ScriptedGossip.h"
 #include "SpellInfo.h"
 #include "SpellScript.h"
 #include "SpellScriptLoader.h"
+#include "zulaman.h"
 
 /*######
 ## npc_forest_frog
