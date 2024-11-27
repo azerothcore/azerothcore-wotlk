66c672e827c9da325be17db0654fb8f2bebcba2a
Ludwig <sudlud@users.noreply.github.com>
2024-11-27 17:38:43 +0100

    fix(DB/item_template): add flag CU_DURATION_REAL_TIME to Pilgrim's Bounty items

---
+++ data/sql/updates/pending_db_world/rev_1732725060262033200.sql
@@ -0,0 +1,18 @@
+-- add flag CU_DURATION_REAL_TIME to Pilgrim's Bounty items
+-- Wild Turkey
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 44834);
+-- Recipe: Cranberry Chutney
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 44858);
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 46805);
+-- Recipe: Candied Sweet Potato
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 44859);
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 46806);
+-- Recipe: Spice Bread Stuffing
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 44860);
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 46803);
+-- Recipe: Slow-Roasted Turkey
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 44861);
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 46807);
+-- Recipe: Pumpkin Pie
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 44862);
+UPDATE `item_template` SET `flagsCustom` = (`flagsCustom` | 1) WHERE (`entry` = 46804);
