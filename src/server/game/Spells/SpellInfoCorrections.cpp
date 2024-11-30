d066a05eca93491d54516d59e698b8b59afab5f7
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-11-30 12:12:30 -0300

    fix(Scripts/ZulAman): Fix Vortex not following players and also spell missing

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4856,6 +4856,12 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->AuraInterruptFlags &= ~(AURA_INTERRUPT_FLAG_CHANGE_MAP | AURA_INTERRUPT_FLAG_TELEPORTED);
     });
 
+    // Summon Cyclone
+    ApplySpellFix({ 43112 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->RequiresSpellFocus = 0;
+    });
+
     for (uint32 i = 0; i < GetSpellInfoStoreSize(); ++i)
     {
         SpellInfo* spellInfo = mSpellInfoMap[i];
