2ff4788b90c26087eb77d784a6281f362744ab0d
FlyingArowana <AugustoMendes4426@gmail.com>
2024-11-30 09:35:17 +0000

    fix(DB/Quest) - Remove the wrong prerequisite Escape from Skettis and Hungry Nether Rays (#20753)
    * Created File
    
    * Removed the prequiste quests
    
    * Space correction

---
+++ data/sql/updates/pending_db_world/rev_1732663460298122300.sql
@@ -0,0 +1,6 @@
+--
+-- To Skettis! requirement removed from Escape from Skettis
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE (`ID` = 11085);
+
+-- To Skettis! requirement removed from Hungry Nether Rays
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE (`ID` = 11093);
