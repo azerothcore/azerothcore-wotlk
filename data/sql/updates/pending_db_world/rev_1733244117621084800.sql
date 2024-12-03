728a340fad5dcce92a4004856fcb43e2493e4159
FlyingArowana <AugustoMendes4426@gmail.com>
2024-12-03 17:09:57 +0000

    Corrected the table of the query

--- data/sql/updates/pending_db_world/rev_1733244117621084800.sql
+++ data/sql/updates/pending_db_world/rev_1733244117621084800.sql
@@ -1,3 +1,3 @@
 --
--- map: 530 (Outland) | zoneId: 3522 (Blade's Edge Mountains) | 124 | 124
-DELETE FROM `creature_copy` WHERE id1 IN (22202, 22385, 22187, 22196, 22392, 22286, 22174, 22217, 22218, 22289, 22221, 22283, 22287, 22327) AND map = 530 AND zoneId = 3522;
+-- map: 530 (Outland) | zoneId: 3522 (Blade's Edge Mountains) 
+DELETE FROM `creature` WHERE id1 IN (22202, 22385, 22187, 22196, 22392, 22286, 22174, 22217, 22218, 22289, 22221, 22283, 22287, 22327) AND map = 530 AND zoneId = 3522;
