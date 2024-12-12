49ba3dbcaf2fc927a3aaf884e8ad5b290c35e87d
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-12-11 23:56:34 -0300

    fix zuljin despawn

--- data/sql/updates/pending_db_world/rev_1733968037822883300.sql
+++ data/sql/updates/pending_db_world/rev_1733968037822883300.sql
@@ -1,2 +1,3 @@
 --
-UPDATE `creature_template` SET `flags_extra` = `flags_extra`|2147483648 WHERE `entry` IN (23574, 23578, 24239, 23863);
+UPDATE `creature_template` SET `flags_extra` = `flags_extra`|2147483648 WHERE `entry` IN (23574, 23578, 24239);
+
