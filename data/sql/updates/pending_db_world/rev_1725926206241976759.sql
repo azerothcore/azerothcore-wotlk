80e6a3a412740da7d0203ad0f30ab17fbf0ac57f
Jelle Meeus <sogladev@gmail.com>
2024-12-12 06:54:40 +0100

    fix(Scripts/TrialOfChampion): fail and send custom spell error when trying to mount without lance equipped (#19932)

---
+++ data/sql/updates/pending_db_world/rev_1725926206241976759.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `conditions` SET `Comment` = 'Player must have aura Lance Equipped' WHERE (`SourceTypeOrReferenceId` = 16) AND (`SourceGroup` = 0) AND (`SourceEntry` IN (35644, 36588)) AND (`ConditionValue1` = 62853);
