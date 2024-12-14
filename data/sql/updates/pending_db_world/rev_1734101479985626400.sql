16940b8b160053686854fb6af5e32bcb29a02d6b
fangshun <fangshun2004@msn.com>
2024-12-14 08:36:41 +0800

    fix error

--- data/sql/updates/pending_db_world/rev_1734101479985626400.sql
+++ data/sql/updates/pending_db_world/rev_1734101479985626400.sql
@@ -1,10 +1,9 @@
--- Archmage Mordent Evenshade smart ai
-
+ -- Archmage Mordent Evenshade smart ai
 SET @ENTRY := 36479;
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(@ENTRY, 0, 0, 0, 60, 0, 100, 0, 540000, 540000, 540000, 540000, 0, 0, 87, 3647900, 3647901, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Archmage Mordent Evenshade - On Update - Run Random Script');
+(@ENTRY, 0, 0, 0, 1, 0, 100, 0, 540000, 540000, 540000, 540000, 0, 0, 87, 3647900, 3647901, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Archmage Mordent Evenshade - Out of Combat - Run Random Script');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 3647900);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
@@ -31,7 +30,3 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (3647901, 9, 7, 0, 0, 0, 100, 0, 9000, 9000, 0, 0, 0, 0, 1, 5, 0, 0, 0, 0, 0, 19, 36481, 10, 0, 0, 0, 0, 0, 0, 'Closest alive creature Sentinel Stillbough (36481) in 10 yards: Talk 5 to invoker'),
 (3647901, 9, 8, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 0, 1, 12, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Archmage Mordent Evenshade - Actionlist - Say Line 12'),
 (3647901, 9, 9, 0, 0, 0, 100, 0, 12000, 12000, 0, 0, 0, 0, 1, 6, 0, 0, 0, 0, 0, 19, 36481, 10, 0, 0, 0, 0, 0, 0, 'Closest alive creature Sentinel Stillbough (36481) in 10 yards: Talk 6 to invoker');
-
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` = 36479 AND `SourceId` = 0;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `Comment`) VALUES 
-(22, 1, 36479, 0, 0, 21, 1, 16384, 0, 0, 1, 'Object doesn\'t have unit state UNIT_STATE_ATTACK_PLAYER');
