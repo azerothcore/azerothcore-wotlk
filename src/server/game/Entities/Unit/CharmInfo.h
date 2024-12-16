fd15dcdf724b5ac541b9949eeb151503d3839e31
root <root@koxsxcg5urxcnjp2.novalocal>
2024-12-16 20:51:20 +0800

    Revert "feat(Core/Player): implement Spell Queue (#20797)"
    This reverts commit 993bdcb84eaa8eeda92d8803a208311dfa831ce1.

--- src/server/game/Entities/Unit/CharmInfo.h
+++ src/server/game/Entities/Unit/CharmInfo.h
@@ -83,7 +83,6 @@ public:
 
 public:
     bool HasGlobalCooldown(SpellInfo const* spellInfo) const;
-    uint32 GetGlobalCooldown(SpellInfo const* spellInfo) const;
     void AddGlobalCooldown(SpellInfo const* spellInfo, uint32 gcd);
     void CancelGlobalCooldown(SpellInfo const* spellInfo);
 
