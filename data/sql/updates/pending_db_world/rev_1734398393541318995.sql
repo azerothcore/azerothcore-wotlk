d0eee2909d1e21edf87f5ce313041839bfe33b1e
Jelle Meeus <sogladev@gmail.com>
2024-12-17 03:18:54 +0100

    set spawn time 30min of Tempest

--- data/sql/updates/pending_db_world/rev_1734398393541318995.sql
+++ data/sql/updates/pending_db_world/rev_1734398393541318995.sql
@@ -1,3 +1,5 @@
 --
 -- Eagle Trash Aggro Trigger
 UPDATE `creature_template` SET `ScriptName` = '' WHERE (`entry` = 24223);
+-- set spawntime to 30min Amani'shi Tempest
+UPDATE `creature` SET `spawntimesecs` = 1800 WHERE (`id1` = 24549) AND (`guid` IN (89283));
