5f55f5bd7a65cb33c63a663b3ab7c36d1bbd557c
Jelle Meeus <sogladev@gmail.com>
2024-12-17 03:19:03 +0100

    remove adds next to Tempest
    should be alone

--- data/sql/updates/pending_db_world/rev_1734398393541318995.sql
+++ data/sql/updates/pending_db_world/rev_1734398393541318995.sql
@@ -3,3 +3,7 @@
 UPDATE `creature_template` SET `ScriptName` = '' WHERE (`entry` = 24223);
 -- set spawntime to 30min Amani'shi Tempest
 UPDATE `creature` SET `spawntimesecs` = 1800 WHERE (`id1` = 24549) AND (`guid` IN (89283));
+-- delete Amani'shi Wind Walker #5 next to Tempest
+DELETE FROM `creature` WHERE (`id1` = 24179) AND (`guid` IN (1107));
+-- delete Amani'shi Protector #5 next to Tempest
+DELETE FROM `creature` WHERE (`id1` = 24180) AND (`guid` IN (89311));
