09c16071619da4f3640b28327bdc1c3d9fb6f1d9
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2024-12-18 01:21:49 +0100

    Update Mit_Gol_Trusil_veins.sql

--- data/sql/updates/pending_db_world/Mit_Gol_Trusil_veins.sql
+++ data/sql/updates/pending_db_world/Mit_Gol_Trusil_veins.sql
@@ -1,4 +1,4 @@
 
-UPDATE `creature` SET `position_z` = -6,5780087 WHERE `guid` = 208397;
-UPDATE `creature` SET `position_z` = -6,5780087 WHERE `guid` = 208414;
-UPDATE `creature` SET `position_z` = -6,5780087 WHERE `guid` = 208431;
+UPDATE `gameobject` SET `position_z` = -6.5780087 WHERE `guid` = 208397;
+UPDATE `gameobject` SET `position_z` = -6.5780087 WHERE `guid` = 208414;
+UPDATE `gameobject` SET `position_z` = -6.5780087 WHERE `guid` = 208431;
