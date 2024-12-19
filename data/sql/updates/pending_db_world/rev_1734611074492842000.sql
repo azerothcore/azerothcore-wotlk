786ac8c63b355fa637b008ecbb05953e14521da8
avarishd <46330494+avarishd@users.noreply.github.com>
2024-12-19 15:29:08 +0200

    ocd

--- data/sql/updates/pending_db_world/rev_1734611074492842000.sql
+++ data/sql/updates/pending_db_world/rev_1734611074492842000.sql
@@ -2,6 +2,6 @@
 UPDATE `spell_proc_event` SET `SpellFamilyMask0`=2858435686, `SpellFamilyMask1`=0 WHERE `entry`= -12281;
 
 -- Sudden Death - Hamstring/Sunder Armor
-DELETE FROM `spell_proc_event` WHERE `entry`=-29723;
+DELETE FROM `spell_proc_event` WHERE `entry`= -29723;
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
 (-29723, 0, 0, 16386, 0, 0, 0, 0, 0, 0, 0, 0);
