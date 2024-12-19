1d0699a81440af3776e87976ed244e2f7a132784
Jelle Meeus <sogladev@gmail.com>
2024-12-19 21:15:53 +0100

    style: add comment with name

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -9796,7 +9796,7 @@ void Player::ApplySpellMod(uint32 spellId, SpellModOp op, T& basevalue, Spell* s
             totalflat += flatValue;
 
             // special case for Mutilate, do not consume crit mod if main hand
-            if (mod->op == SPELLMOD_CRITICAL_CHANCE && spellInfo->GetFirstRankSpell()->Id == 5374)
+            if (mod->op == SPELLMOD_CRITICAL_CHANCE && spellInfo->GetFirstRankSpell()->Id == 5374) // Mutilate (Rank 1)
                 return;
         }
         else if (mod->type == SPELLMOD_PCT)
