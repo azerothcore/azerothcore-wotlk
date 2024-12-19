db168d20f91b0657c4793f4c4c5f9823fe3b908b
avarishd <46330494+avarishd@users.noreply.github.com>
2024-12-19 14:56:51 +0200

    not rend

--- data/sql/updates/pending_db_world/rev_1734611074492842000.sql
+++ data/sql/updates/pending_db_world/rev_1734611074492842000.sql
@@ -1,7 +1,7 @@
 -- Sword Specialization - Sunder Armor/Hamstring/Rend (Mask1 was 1:1 with DBC)
 UPDATE `spell_proc_event` SET `SpellFamilyMask0`=2858435686, `SpellFamilyMask1`=0 WHERE `entry`= -12281;
 
--- Sudden Death - Hamstring/Rend
+-- Sudden Death - Hamstring/Sunder Armor
 DELETE FROM `spell_proc_event` WHERE `entry`=-29723;
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(-29723, 0, 0, 34, 0, 0, 0, 0, 0, 0, 0, 0);
+(-29723, 0, 0, 16386, 0, 0, 0, 0, 0, 0, 0, 0);
