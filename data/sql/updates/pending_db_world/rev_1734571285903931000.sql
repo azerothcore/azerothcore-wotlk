37cfe62cb210860ac6473d4b2f46c641a213d3ca
Exitare <business.exitare@icloud.com>
2024-12-18 17:49:32 -0800

    Add missing heavy stone loot entries

---
+++ data/sql/updates/pending_db_world/rev_1734571285903931000.sql
@@ -0,0 +1,80 @@
+--
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 2359) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(2359, 2838, 0, 25, 0, 1, 0, 1, 2, 'Heavy Stone -- Elemental Slave');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4120) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4120, 2838, 0, 25, 0, 1, 0, 1, 2, 'Heavy Stone -- Thundering Boulderkin');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 6073) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(6073, 2838, 0, 25, 0, 1, 0, 1, 2, 'Heavy Stone -- Searing Infernal');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4499) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4499, 2838, 0, 4, 0, 1, 0, 1, 1, 'Heavy Stone -- Defias Worker');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 824) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(824, 2838, 0, 4, 0, 1, 0, 1, 1, 'Defias Digger');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 624) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(624, 2838, 0, 3, 0, 1, 0, 1, 1, 'Heavy Stone -- Undead Excavator');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 2450) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(2450, 2838, 0, 2, 0, 1, 0, 1, 3, 'Heavy Stone -- Miner Hackett');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 2450) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(2450, 2838, 0, 2.66, 0, 1, 0, 1, 3, 'Heavy Stone -- Gravelsnout Digger');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 2269) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(2269, 2838, 0, 1.44, 0, 1, 0, 1, 3, 'Heavy Stone -- Hillsbrad Miner');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4844) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4844, 2838, 0, 1.1, 0, 1, 0, 1, 3, 'Heavy Stone -- Shadowforge Surveyor');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4846) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4846, 2838, 0, 1.1, 0, 1, 0, 1, 3, 'Heavy Stone -- Shadowforge Digger');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 623) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(623, 2838, 0, 1, 0, 1, 0, 1, 1, 'Heavy Stone -- Skeletal Miner');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 674) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(674, 2838, 0, 0.9, 0, 1, 0, 1, 3, 'Heavy Stone -- Venture CO Stripminer');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 1094) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(1094, 2838, 0, 0.9, 0, 1, 0, 1, 3, 'Heavy Stone -- Venture CO Miner');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 4849) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(4849, 2838, 0, 0.81, 0, 1, 0, 1, 3, 'Heavy Stone -- Shadowforge Archaeologist');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 3586) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(3586, 2838, 0, 0.8, 0, 1, 0, 1, 1, 'Heavy Stone -- Miner Johnson');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 1167) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(1167, 2838, 0, 0.8, 0, 1, 0, 1, 1, 'Heavy Stone -- Stonesplinter Digger');
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 1393) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(1393, 2838, 0, 0.6, 0, 1, 0, 1, 1, 'Heavy Stone -- Beserk Trogg');
+
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 14427) AND (`Item` IN (2838));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(14427, 2838, 0, 0.4, 0, 1, 0, 3, 3, 'Heavy Stone -- Gibblesnik');
+
+
+
