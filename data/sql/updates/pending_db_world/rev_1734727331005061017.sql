3c6f27f55b27db3d577fc10a40a65911f195050d
Jelle Meeus <sogladev@gmail.com>
2024-12-21 00:10:03 +0100

    fixup! init

--- data/sql/updates/pending_db_world/rev_1734727331005061017.sql
+++ data/sql/updates/pending_db_world/rev_1734727331005061017.sql
@@ -3,3 +3,4 @@ UPDATE `creature_template_addon` SET `auras` = '28305 8273 57989' WHERE (`entry`
 DELETE FROM `spell_script_names` WHERE `spell_id` = 57989 AND `ScriptName` = 'spell_pri_shadowfiend_death';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (57989, 'spell_pri_shadowfiend_death');
+UPDATE `creature_template` SET `ScriptName` = '' WHERE (`entry` = 19668);
