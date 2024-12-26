9ad135a642dbc44a18000507987f18a256d2b4ae
Exitare <business.exitare@icloud.com>
2024-12-25 17:47:04 -0800

    Update rev_1734978274366357000.sql

--- data/sql/updates/pending_db_world/rev_1734978274366357000.sql
+++ data/sql/updates/pending_db_world/rev_1734978274366357000.sql
@@ -1,3 +1,3 @@
 -- Add interrupt immunity to Illidari Nightlord
-UPDATE `creature_template` SET `mechanic_immune_mask` = 650526583 WHERE (`entry` = 22855);
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask` | 33554432 WHERE (`entry` = 22855);
 
