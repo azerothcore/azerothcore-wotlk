a02c15ae6d890e5af8c5a9af5f9f88b4d865a632
FlyingArowana <AugustoMendes4426@gmail.com>
2024-12-28 02:16:03 +0000

    Corrected source_type

--- data/sql/updates/pending_db_world/rev_1735350584050913500.sql
+++ data/sql/updates/pending_db_world/rev_1735350584050913500.sql
@@ -1,6 +1,6 @@
 --
 -- Removes NOT_REPEATABL` and WHILE_CHARMED event_flag, allowing to always drop the flag no matter if it's CC'd or not.
-UPDATE smart_scripts SET `event_flags` = `event_flags` &~1 &~512 WHERE `entryorguid` = 23576 AND ´source_type` = 0 AND `id` = 1;
+UPDATE smart_scripts SET `event_flags` = `event_flags` &~1 &~512 WHERE `entryorguid` = 23576 AND `source_type` = 0 AND `id` = 1;
 
 -- Adds Strike sniffed from: 4.4.1.58158 and 1.15.5.57979
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
