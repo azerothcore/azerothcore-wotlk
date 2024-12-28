147a2019935559bb36f31c05a4f25a3796026e77
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-12-28 20:46:49 +0100

    chore(Misc): Drop unused deps/tools

--- apps/git_tools/setup_git_commit_template.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-## Set a local git commit template
-git config --local commit.template ".git_commit_template.txt" ;
-echo "--- Successfully set the default commit template for this repository only. Verify with: git config -e"
