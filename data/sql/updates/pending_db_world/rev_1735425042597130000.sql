54a887ab49714339394673da2b2a697dd2458c11
blinkysc <binders-36-gap@icloud.com>
2024-12-29 14:07:20 -0600

    Fix SQL Syntax

--- data/sql/updates/pending_db_world/rev_1735425042597130000.sql
+++ data/sql/updates/pending_db_world/rev_1735425042597130000.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `flags_extra` = 1073741824 WHERE `entry` = 36658;
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 1073741824 WHERE `entry` = 36658;
