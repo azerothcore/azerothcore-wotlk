05f72fb1e983a28f780929ffbd054f555a3911cf
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-12-29 15:18:09 +0100

    Update rev_1735437140186286300.sql

--- data/sql/updates/pending_db_world/rev_1735437140186286300.sql
+++ data/sql/updates/pending_db_world/rev_1735437140186286300.sql
@@ -1,7 +1,7 @@
 --
-SELECT * FROM `gameobject_template_locale` WHERE `name` = '';
-SELECT * FROM `gameobject_template_locale` WHERE `name` = 'NULL';
-SELECT * FROM `gameobject_template_locale` WHERE `name` IS NULL;
-SELECT * FROM `quest_offer_reward_locale` WHERE `RewardText` = '';
-SELECT * FROM `quest_offer_reward_locale` WHERE `RewardText` = 'NULL';
-SELECT * FROM `quest_offer_reward_locale` WHERE `RewardText` IS NULL;
+DELETE FROM `gameobject_template_locale` WHERE `name` = '';
+DELETE FROM `gameobject_template_locale` WHERE `name` = 'NULL';
+DELETE FROM `gameobject_template_locale` WHERE `name` IS NULL;
+DELETE FROM `quest_offer_reward_locale` WHERE `RewardText` = '';
+DELETE FROM `quest_offer_reward_locale` WHERE `RewardText` = 'NULL';
+DELETE FROM `quest_offer_reward_locale` WHERE `RewardText` IS NULL;
