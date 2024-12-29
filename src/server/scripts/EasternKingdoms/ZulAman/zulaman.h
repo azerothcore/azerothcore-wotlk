c01bd771486cf14bd44361f5c10f0ffdf8ce539c
heyitsbench <benjymansy123@gmail.com>
2024-12-29 10:37:50 -0500

    Init.

--- src/server/scripts/EasternKingdoms/ZulAman/zulaman.h
+++ src/server/scripts/EasternKingdoms/ZulAman/zulaman.h
@@ -41,7 +41,8 @@ enum DataTypes
     DATA_HARRISON_JONES                 = 13,
     TYPE_AKILZON_GAUNTLET               = 14,
     DATA_LOOKOUT                        = 15,
-    DATA_ZULJIN_GATE                    = 16
+    DATA_ZULJIN_GATE                    = 16,
+    DATA_CHEST_LOOTED                   = 17 // Used for hostage loot DB conditions
 };
 
 enum CreatureIds
