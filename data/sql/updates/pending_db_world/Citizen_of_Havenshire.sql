5a82a7402d190d4808be933036cbf72014ea6e4e
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2024-12-29 09:56:27 +0100

    Update Citizen_of_Havenshire.sql

--- data/sql/updates/pending_db_world/Citizen_of_Havenshire.sql
+++ data/sql/updates/pending_db_world/Citizen_of_Havenshire.sql
@@ -468,7 +468,6 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (28577, 0, 10, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Citizen of Havenshire - On Just Died - Say Line 2'),
 (28577, 0, 11, 0, 4, 0, 50, 0, 0, 0, 0, 0, 0, 0, 86, 58207, 0, 10, 128581, 28765, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Citizen of Havenshire - On Aggro - Cross Cast \'Lich King VO Blocker\'');
 
-
 -- Set Guid SmartAI
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = -128919);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
