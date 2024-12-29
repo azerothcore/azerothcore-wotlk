0c8c03fb966e4499257f8104141fb08366c928a9
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2024-12-29 02:54:50 +0100

    chore(DB): Delete empty/null entries
    - gameobject_template_locale
    - quest_offer_reward_locale

---
+++ data/sql/updates/pending_db_world/rev_1735437140186286300.sql
@@ -0,0 +1,7 @@
+--
+SELECT * FROM `gameobject_template_locale` WHERE `name` = '';
+SELECT * FROM `gameobject_template_locale` WHERE `name` = 'NULL';
+SELECT * FROM `gameobject_template_locale` WHERE `name` IS NULL;
+SELECT * FROM `quest_offer_reward_locale` WHERE `RewardText` = '';
+SELECT * FROM `quest_offer_reward_locale` WHERE `RewardText` = 'NULL';
+SELECT * FROM `quest_offer_reward_locale` WHERE `RewardText` IS NULL;
