51d8437ffddfea1d984d56ca4d59338615f42da3
blinkysc <binders-36-gap@icloud.com>
2024-12-30 13:27:37 -0600

    Fix bitwise operation removing CREATURE_FLAG_EXTRA_HARD_RESET

--- data/sql/updates/pending_db_world/rev_1735425042597130000.sql
+++ data/sql/updates/pending_db_world/rev_1735425042597130000.sql
@@ -1,2 +1,2 @@
 -- Remove HARD_RESET flag while retaining IMMUNITY_KNOCKBACK
-UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 1073741824 WHERE `entry` = 36658;
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` &~ (2147483648) WHERE `entry` = 36658;
