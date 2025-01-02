277e1fe0f051f2d18afc6ac7a43eab209ab44a36
FlyingArowana <AugustoMendes4426@gmail.com>
2025-01-02 23:38:32 +0000

    Waypoint Data moved around

--- data/sql/updates/pending_db_world/rev_1732228871285414000.sql
+++ data/sql/updates/pending_db_world/rev_1732228871285414000.sql
@@ -1,6 +1,15 @@
 --
-UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE (`entry` = 11625);
+-- Currently in AC there's no `waypoints` data for `Cork Gizelton`. But there's `script_waypoint`. But in the case of:
+-- Deletes all entries for `Cork Gizelton` between `waypoint id` 1 to 282 in `waypoints`.
+DELETE FROM `waypoints` WHERE `entry` = 11625 AND`pointid` BETWEEN 1 AND 282;
 
+-- Copies the waypoint data from `script_waypoint` to `waypoints` for `Cork Gizelton`
+INSERT IGNORE INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `delay`, `point_comment`) SELECT `entry`, `pointid`, `location_x`, `location_y`, `location_z`, `waittime`, `point_comment` FROM `script_waypoint` WHERE `entry` = 11625;
+
+-- Deletes all entries for `Cork Gizelton` between `waypoint id` 1 to 282 in `script_waypoints`.
+DELETE FROM `script_waypoint` WHERE `entry` = 11625 AND`pointid` BETWEEN 1 AND 282;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE (`entry` = 11625);
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (11625, 0, 0, 1, 37, 0, 100, 0, 0, 0, 0, 0, 0, 0, 12, 11564, 6, 10000, 0, 0, 0, 1, 0, 0, 0, 0, -10, 0, 5, 0, 'Cork Gizelton - On Initialize - Summon Creature \'Gizelton Caravan Kodo\''),
