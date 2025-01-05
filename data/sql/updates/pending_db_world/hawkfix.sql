15b11ae6e0142dc05d0d16e92b4d42ae6f380baf
mostlynick3 <nick@nikitas.no>
2025-01-05 16:53:26 +0100

    fix(DB/Creature): Correct behavior and emote of oil-covered birds (#20923)

---
+++ data/sql/updates/pending_db_world/hawkfix.sql
@@ -0,0 +1,6 @@
+
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (25817, 25748);
+
+UPDATE `creature` SET `MovementType` = 0, `wander_distance` = 0 WHERE `id1` IN (25817, 25748);
+-- set to 'sitting'
+UPDATE `creature_template_addon`SET `bytes1` = 1 WHERE `entry` IN (25817, 25748);
