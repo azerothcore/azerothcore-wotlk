b2e3262f27c5dc54a3ca6ddd7947976ebf2d4de7
Exitare <Exitare@users.noreply.github.com>
2025-01-09 10:50:29 -0800

    Update rev_1734400454914932600.sql

--- data/sql/updates/pending_db_auth/rev_1734400454914932600.sql
+++ data/sql/updates/pending_db_auth/rev_1734400454914932600.sql
@@ -5,7 +5,6 @@ CREATE TABLE `autobroadcast_locale` (
   `locale` VARCHAR(4) NOT NULL,
   `text` VARCHAR(45) NULL,
   PRIMARY KEY (`realmid`, `id`))
-  
 CHARSET = utf8mb4
 COLLATE = utf8mb4_unicode_ci
 ENGINE = InnoDB
