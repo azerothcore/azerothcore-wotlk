50420f2cc63fdfb7ebade44c1ab12e9c1dbf8e8c
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-01-09 09:28:57 +0100

    Update Band_Aid_Tanzar.sql

--- data/sql/updates/pending_db_world/Band_Aid_Tanzar.sql
+++ data/sql/updates/pending_db_world/Band_Aid_Tanzar.sql
@@ -1,3 +1,4 @@
 
 DELETE FROM `creature` WHERE `id1` = 23790;
 DELETE FROM `creature_addon` WHERE (`guid` IN (89157));
+UPDATE `gameobject_template` SET `Data15` = `Data15`|1 WHERE (`entry` = 186648);
