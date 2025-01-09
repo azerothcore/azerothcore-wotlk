a555a1e94bed4bb9375f200826d30319ac2b3b13
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-01-09 23:20:21 +0100

    revert Battleground.h (was a commit error)

--- src/server/game/Battlegrounds/Battleground.h
+++ src/server/game/Battlegrounds/Battleground.h
@@ -176,9 +176,9 @@ enum BattlegroundTimeIntervals
 
 enum BattlegroundStartTimeIntervals
 {
-    BG_START_DELAY_2M               = 5000,               // ms (2 minutes)
-    BG_START_DELAY_1M               = 5000,                // ms (1 minute)
-    BG_START_DELAY_30S              = 5000,                // ms (30 seconds)
+    BG_START_DELAY_2M               = 120000,               // ms (2 minutes)
+    BG_START_DELAY_1M               = 60000,                // ms (1 minute)
+    BG_START_DELAY_30S              = 30000,                // ms (30 seconds)
     BG_START_DELAY_15S              = 15000,                // ms (15 seconds) Used only in arena
     BG_START_DELAY_NONE             = 0,                    // ms
 };
