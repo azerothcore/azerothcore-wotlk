4bdeda640347b54edf2036ec2caf3abd1098cb5c
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-01-10 19:03:06 -0300

    feat(Scripts/Commands): Update GetBossState to display all states

---
+++ data/sql/updates/pending_db_world/rev_1736546523943069600.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `command` SET `help` = 'Syntax: .instance getbossstate [$Name]\nDisplays the state for every available encounter.\nIf no character name is provided, the current map will be used as target.' WHERE `name` = 'instance getbossstate';
