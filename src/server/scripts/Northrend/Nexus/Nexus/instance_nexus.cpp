46393a2e75ef3d01096b649e99c5276b26f72c47
manstfu <33990720+manstfu@users.noreply.github.com>
2025-01-10 22:43:42 +0100

    Update instance_nexus.cpp

--- src/server/scripts/Northrend/Nexus/Nexus/instance_nexus.cpp
+++ src/server/scripts/Northrend/Nexus/Nexus/instance_nexus.cpp
@@ -109,7 +109,7 @@ public:
             {
                 if (Group* group = player->GetGroup())
                 {
-                    if (Player* gLeader = ObjectAccessor::FindPlayer(player->GetGroup()->GetLeaderGUID()))
+                    if (Player* gLeader = ObjectAccessor::FindPlayer(group->GetLeaderGUID()))
                         TeamIdInInstance = Player::TeamIdForRace(gLeader->getRace());
                     else
                         TeamIdInInstance = player->GetTeamId();
