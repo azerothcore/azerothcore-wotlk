d3a8245c1bea7aa1f77ab645de9b0a76503b9ade
EricksOliveira <ericksoliveira258@gmail.com>
2025-01-11 04:38:16 -0300

    Update LootHandler.cpp

--- src/server/game/Handlers/LootHandler.cpp
+++ src/server/game/Handlers/LootHandler.cpp
@@ -485,8 +485,7 @@ void WorldSession::HandleLootMasterGiveOpcode(WorldPacket& recvData)
 
     ItemPosCountVec dest;
     InventoryResult msg = target->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, item.itemid, item.count);
-    bool isMasterLooter = _player->GetGUID() == _player->GetGroup()->GetMasterLooterGuid();
-    if (!isMasterLooter && !item.AllowedForPlayer(target, loot->sourceWorldObjectGUID))
+    if (!item.AllowedForPlayer(target, loot->sourceWorldObjectGUID))
         msg = EQUIP_ERR_YOU_CAN_NEVER_USE_THAT_ITEM;
     if (msg != EQUIP_ERR_OK)
     {
