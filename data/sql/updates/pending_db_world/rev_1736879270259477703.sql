0dbbd3c7405e9191d4d1f4aab633f64d13493480
Jelle Meeus <sogladev@gmail.com>
2025-01-14 22:31:34 +0100

    remove extra procFlags that aren't in DBC
    remove PROC_FLAG_DONE_SPELL_NONE_DMG_CLASS_POS |
    PROC_FLAG_DONE_SPELL_NONE_DMG_CLASS_NEG

--- data/sql/updates/pending_db_world/rev_1736879270259477703.sql
+++ data/sql/updates/pending_db_world/rev_1736879270259477703.sql
@@ -1,5 +1,3 @@
 --
 DELETE FROM `spell_script_names` WHERE `spell_id`=43983 AND `ScriptName`='spell_gen_allow_proc_from_spells_with_cost';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES(43983, 'spell_gen_allow_proc_from_spells_with_cost');
--- PROC_FLAG_DONE_SPELL_NONE_DMG_CLASS_POS | PROC_FLAG_DONE_SPELL_NONE_DMG_CLASS_NEG
-UPDATE `spell_proc_event` SET `procFlags`=`procFlags`|(1024|4096) WHERE `entry` = 43983;
