550601b0ff12000b6cb9ad1b16a20f9e7001c681
Jelle Meeus <sogladev@gmail.com>
2025-01-14 21:18:46 +0100

    fix(Scripts/ZulAman): add extra proc requirements to Energy Storm Aura

---
+++ data/sql/updates/pending_db_world/rev_1736879270259477703.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id`=43983 AND `ScriptName`='spell_energy_storm_aura';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES(43983, 'spell_energy_storm_aura');
