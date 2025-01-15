a1cf6a7cebb9bd5855114e267834fe34585d9b2f
Jelle Meeus <sogladev@gmail.com>
2025-01-15 02:09:07 +0100

    fix(Scripts/ZulAman): Zul'Jin reduce procs of Energy Storm Aura (#21169)

---
+++ data/sql/updates/pending_db_world/rev_1736879270259477703.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id`=43983 AND `ScriptName`='spell_gen_allow_proc_from_spells_with_cost';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES(43983, 'spell_gen_allow_proc_from_spells_with_cost');
