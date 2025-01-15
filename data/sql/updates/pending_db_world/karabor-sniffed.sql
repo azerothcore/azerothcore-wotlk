ca07216657844bf58bd79f31dcc814fa5ae2be45
heyitsbench <benjymansy123@gmail.com>
2025-01-15 17:05:21 -0500

    Init.

---
+++ data/sql/updates/pending_db_world/karabor-sniffed.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_target_position` WHERE `ID` = 41234 AND `EffectIndex` = 0;
+INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES
+(41234, 0, 530, -3560.52, 583.353, 10.9431, 4.751223087310791015, 58558);
