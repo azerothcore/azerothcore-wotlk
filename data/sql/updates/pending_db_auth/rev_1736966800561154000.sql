1acd740229f02f1fd08b8cc5eb8bbfbefa525eb6
FlyingArowana <AugustoMendes4426@gmail.com>
2025-01-15 20:18:02 +0000

    Removed the changes from 32 to 17 characters for username

--- data/sql/updates/pending_db_auth/rev_1736966800561154000.sql
+++
@@ -1,3 +0,0 @@
---
--- Changes the Length of `username` from 32 to 17 characters as it the client limit.
-ALTER TABLE `account` CHANGE COLUMN `username` `username` VARCHAR(17) NOT NULL DEFAULT '' COLLATE 'utf8mb4_unicode_ci' AFTER `id`;
