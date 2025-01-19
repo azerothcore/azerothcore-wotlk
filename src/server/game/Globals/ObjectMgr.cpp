c7f6fd7079f7f0d5046d1be6963977a8604c0640
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-01-18 22:50:59 -0300

    Revert "Update ObjectMgr.cpp"
    This reverts commit dbed8d8d9a45a75a48713dd519f813a93963f5a3.

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -8746,8 +8746,7 @@ char const* ObjectMgr::GetAcoreString(uint32 entry, LocaleConstant locale) const
 
     LOG_ERROR("sql.sql", "Acore string entry {} not found in DB.", entry);
 
-    char const* msg = Acore::StringFormat("Acore string entry {} not found in DB.", entry).c_str();
-    return msg;
+    return Acore::StringFormat("Acore string entry {} not found in DB.", entry).c_str();
 }
 
 void ObjectMgr::LoadFishingBaseSkillLevel()
