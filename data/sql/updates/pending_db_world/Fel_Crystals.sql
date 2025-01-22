4927d20d489ddcf093db55042b55c18304b9de82
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-01-22 15:18:02 +0100

    fix(DB/Creature) Fel Crystals no longer follow players. (#21227)

---
+++ data/sql/updates/pending_db_world/Fel_Crystals.sql
@@ -0,0 +1,7 @@
+
+-- Change creature template flags (before: IMMUNE_TO_PC, IMMUNE_TO_NPC, NOT_SELECTABLE. now: STUNNED, NOT_SELECTABLE).
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`& ~33555200 WHERE (`entry` = 24722);
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`|33816576 WHERE (`entry` = 24722);
+
+-- Add Rooted and Flight (Sniffed Flags).
+UPDATE `creature_template_movement` SET `Flight` = 1, `Rooted` = 1 WHERE (`CreatureId` = 24722);
