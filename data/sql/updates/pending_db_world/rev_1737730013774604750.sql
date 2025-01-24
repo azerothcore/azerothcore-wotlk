1c1f741f67fc28f7500e0221d27dc35088358c02
Tereneckla <Tereneckla@pm.me>
2025-01-24 16:08:59 +0100

    ...

--- data/sql/updates/pending_db_world/rev_1737730013774604750.sql
+++ data/sql/updates/pending_db_world/rev_1737730013774604750.sql
@@ -1,4 +1,4 @@
---Fix Kaelthas HC
+-- Fix Kaelthas HC
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 24857) AND (`Item` IN (23572, 25028, 25029, 34609, 34610, 34611, 34612, 34613, 34614, 34615, 34616));
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (24857, 23572, 0, 100, 0, 1, 0, 1, 1, 'Kael\'thas Sunstrider (1) - Primal Nether'),
@@ -11,11 +11,11 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (24857, 34613, 0, 0, 0, 1, 2, 1, 1, 'Kael\'thas Sunstrider (1) - Shoulderpads of the Silvermoon Retainer'),
 (24857, 34609, 0, 0, 0, 1, 2, 1, 1, 'Kael\'thas Sunstrider (1) - Quickening Blade of the Prince');
 
---Remove normal loot from Delrissa HC
+-- Remove normal loot from Delrissa HC
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 25560) AND (`Item` IN (25027));
 
---Remove normal loot from Vexallus HC
+-- Remove normal loot from Vexallus HC
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 25573) AND (`Item` IN (25026));
 
---Remove normal loot from Selin Fireheart HC
+-- Remove normal loot from Selin Fireheart HC
 DELETE FROM `creature_loot_template` WHERE (`Entry` = 25562) AND (`Item` IN (25025));
