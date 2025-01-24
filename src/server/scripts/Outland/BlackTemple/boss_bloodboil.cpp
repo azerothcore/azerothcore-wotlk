dffb89bbede1d18c8592d217863a70ff02c32926
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-01-23 20:57:24 -0600

    Update boss_bloodboil.cpp
    Back to DoCastSelf()

--- src/server/scripts/Outland/BlackTemple/boss_bloodboil.cpp
+++ src/server/scripts/Outland/BlackTemple/boss_bloodboil.cpp
@@ -79,7 +79,7 @@ struct boss_gurtogg_bloodboil : public BossAI
         BossAI::JustEngagedWith(who);
         Talk(SAY_AGGRO);
 
-        DoCastVictim(SPELL_ACIDIC_WOUND, true);
+        DoCastSelf(SPELL_ACIDIC_WOUND, true);
 
         ScheduleTimedEvent(10s, [&] {
             if (!me->HasAura(SPELL_FEL_RAGE_SELF))
@@ -118,7 +118,7 @@ struct boss_gurtogg_bloodboil : public BossAI
                 }, 2s);
 
                 me->m_Events.AddEventAtOffset([&] {
-                    DoCastVictim(SPELL_ACIDIC_WOUND, true);
+                    DoCastSelf(SPELL_ACIDIC_WOUND, true);
                 }, 28s);
 
                 scheduler.DelayGroup(GROUP_DELAY, 30s);
