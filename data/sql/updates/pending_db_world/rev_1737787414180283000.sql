98ee2d75a754d728e5d0f376666c3c085b8f416a
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-01-25 00:49:34 -0600

    Update rev_1737787414180283000.sql

--- data/sql/updates/pending_db_world/rev_1737787414180283000.sql
+++ data/sql/updates/pending_db_world/rev_1737787414180283000.sql
@@ -1,1 +1,3 @@
---
+-- Add Call of Beast SpellScript
+DELETE FROM spell_script_names WHERE spell_id = 43359;
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (43359, 'spell_gen_call_of_the_beast');
