93ea180af614e8cd88268d1bc7584f4a70e84d89
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-01-26 14:00:27 -0600

    Update rev_1737753927638017570.sql

--- data/sql/updates/pending_db_world/rev_1737753927638017570.sql
+++ data/sql/updates/pending_db_world/rev_1737753927638017570.sql
@@ -1,2 +1,4 @@
 -- Add pure energy immune script for Vex
 UPDATE `creature_template` SET `ScriptName` = 'npc_pure_energy' WHERE (`entry` = 24745);
+-- AOE_IMMUNE Flag for pure energy
+UPDATE `creature_template` SET `flags_extra` = (`flags_extra` | 4194304) WHERE (`entry` = 24745);
