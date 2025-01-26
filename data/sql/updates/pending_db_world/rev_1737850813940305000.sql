7c56eb02d4b616fd0ee44d88c8c4dcd7d1c326cd
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-01-25 18:26:02 -0600

    Update rev_1737850813940305000.sql

--- data/sql/updates/pending_db_world/rev_1737850813940305000.sql
+++ data/sql/updates/pending_db_world/rev_1737850813940305000.sql
@@ -1,1 +1,4 @@
---
+-- Add spellscript for Vexallus Overload
+DELETE FROM spell_script_names WHERE spell_id = 44352;
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES 
+(44352, 'spell_vexallus_overload');
