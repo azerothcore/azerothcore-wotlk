ff18774b4e328b7570bfac9ee720ea7f0fa5b183
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-01-27 11:14:03 -0600

    Update rev_1737753927638017570.sql

--- data/sql/updates/pending_db_world/rev_1737753927638017570.sql
+++ data/sql/updates/pending_db_world/rev_1737753927638017570.sql
@@ -4,5 +4,6 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_pure_energy' WHERE (`entry` =
 -- AOE_IMMUNE Flag for pure energy
 UPDATE `creature_template` SET `flags_extra` = (`flags_extra` | 4194304) WHERE (`entry` = 24745);
 -- Vexallus (Entry: 24744) Overload Emote
+DELETE FROM `creature_text` WHERE `CreatureID` = 24744 AND `GroupID` = 5 AND `ID` = 0;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (24744, 5, 0, 'Vexallus overloads!', 41, 0, 100, 0, 0, 0, 0, 0, 'Vexallus - EMOTE_OVERLOAD');
