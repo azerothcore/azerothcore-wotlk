a4de83bb440c6a831b091a47028f1eb33020754f
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2025-02-03 21:16:53 +0100

    Update apps/installer/includes/os_configs/windows.sh

--- apps/installer/includes/os_configs/windows.sh
+++ apps/installer/includes/os_configs/windows.sh
@@ -1,6 +1,6 @@
 # install chocolatey before
 
-powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+# powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 # install automatically following packages:
 # cmake
