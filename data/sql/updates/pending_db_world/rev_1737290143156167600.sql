06fd67a8b6251fc791966b980ba7b3e26a937df3
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2025-02-03 20:45:47 +0100

    Revert "for demonstation"
    This reverts commit bdfb08a94787f248a08990cc90de331e0fa3b725.

--- data/sql/updates/pending_db_world/rev_1737290143156167600.sql
+++
@@ -1,4 +0,0 @@
---
-DELETE FROM `acore_string` WHERE entry = 6617;
-INSERT INTO acore_string WHERE (`entry`, `lol`, abc, nothing, `cool`, sjfajs)
-UPDATE updatecoolstuff SET `val` = 1, somethingcoolyo = 1;
