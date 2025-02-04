7b8b830662891169ccdd215c88186bb26ec930b2
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-02-04 17:07:00 -0600

    One line

--- data/sql/updates/pending_db_world/rev_1737641499678082104.sql
+++ data/sql/updates/pending_db_world/rev_1737641499678082104.sql
@@ -1,9 +1,3 @@
 -- Update gold drop rate with min 437 and max 3869 like picture
 -- I suspect realistically ~200 ~4000
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 18853);
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 19453);
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 18852);
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 18857);
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 19779);
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 18855);
-UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE (`entry` = 19643);
+UPDATE `creature_template` SET `mingold` = 437, `maxgold` = 3869 WHERE `entry` IN (18853, 19453, 18852, 18857, 19779, 18855, 19643);
