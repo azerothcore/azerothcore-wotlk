7e5e682246866728d545c3e9a02571ddff6d1f8f
Hexadecimal <hd@hd.hd>
2025-02-04 18:29:38 +0100

    update conditions table

--- data/sql/updates/pending_db_world/rev_1738495222613876100.sql
+++ data/sql/updates/pending_db_world/rev_1738495222613876100.sql
@@ -1,10 +1,15 @@
 -- Delete Severed Arm from fish loot
 DELETE FROM `fishing_loot_template` WHERE `Entry`=4567 AND `Item`=45323;
--- Delete Bloated Slippery Eel from fish loot
+-- Delete Bloated Slippery Eel from fish loot (water outside of Dalaran, where Quest Item was before patch 3.3.3)
 DELETE FROM `fishing_loot_template` WHERE `Entry`=3979 AND `Item`=45328;
--- Delete Bloated Slippery Eel from gameobject loot
-DELETE FROM `gameobject_loot_template` WHERE  `Entry`=25671 AND `Item`=45328;
--- Delete Corroded Jewelry from fish loot
-DELETE FROM `fishing_loot_template` WHERE `Entry`=4560 AND `Item`=45903;
--- Update Bloated Slippery Eel Entry in reference loot
+-- Delete Bloated Slippery Eel from gameobject loot (fishing holes outside of Dalaran, where Quest Item was before patch 3.3.3)
+DELETE FROM `gameobject_loot_template` WHERE `Entry`=25671 AND `Item`=45328;
+-- Update Bloated Slippery Eel Entry in reference loot (the quest fish already exist in loot table, but it is in "wrong" water. Right now it in the sewers. Lets move it to outside of the prison)
 UPDATE `reference_loot_template` SET `Entry`=11024 WHERE `Entry`=11026 AND `Item`=45328;
+-- Update conditions according to changed entry in `reference_loot_template`
+UPDATE `conditions` SET `SourceGroup`=11024 WHERE `SourceGroup`=11026 AND `SourceEntry`=45328 AND `ConditionValue1`=13836;
+-- Delete outdated condition
+DELETE FROM `conditions` WHERE `SourceGroup`=25671 AND `SourceEntry`=45328 AND `ConditionValue1`=13836;
+
+-- Delete duplicated Corroded Jewelry from fish loot
+DELETE FROM `fishing_loot_template` WHERE `Entry`=4560 AND `Item`=45903;
