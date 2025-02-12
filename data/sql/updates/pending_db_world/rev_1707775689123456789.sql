1aef93200de53b99fdba06f703d537ff4ef46d58
manstfu <33990720+manstfu@users.noreply.github.com>
2025-02-12 12:59:54 +0100

    Update rev_1707775689123456789.sql

--- data/sql/updates/pending_db_world/rev_1707775689123456789.sql
+++ data/sql/updates/pending_db_world/rev_1707775689123456789.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `npc_vendor`
 ADD COLUMN `buycount` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' AFTER `maxcount`;
 ALTER TABLE `game_event_npc_vendor`
-ADD COLUMN `buycount` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' AFTER `maxcount`;
+ADD COLUMN `buycount` INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `maxcount`;
