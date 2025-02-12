98247f2641a87b7e79ac448e5bd4ed6ef7a816e2
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-02-12 10:39:39 -0300

    Update rev_1739361382506034400.sql

--- data/sql/updates/pending_db_world/rev_1739361382506034400.sql
+++ data/sql/updates/pending_db_world/rev_1739361382506034400.sql
@@ -1,2 +1,2 @@
 --
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_eredar_twins_apply_dark_touched';
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_eredar_twins_apply_dark_touched', 'spell_eredar_twins_apply_flame_touched');
