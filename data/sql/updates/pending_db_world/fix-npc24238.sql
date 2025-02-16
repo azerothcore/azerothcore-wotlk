d3fad79e270c7f25e5c9c6d0d9363ff37d32ef88
Smirnov Sergey <lyns@lyns.pro>
2025-02-16 18:29:02 +0300

    Update fix-npc24238.sql

--- data/sql/updates/pending_db_world/fix-npc24238.sql
+++ data/sql/updates/pending_db_world/fix-npc24238.sql
@@ -16,7 +16,7 @@ INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `b
 UPDATE `creature_template_movement` SET `Swim` = 0, `Flight` = 0 WHERE (`CreatureId` = 24238);
 
 -- Bjorn Halgurdsson - SmartAI
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE (`entry` = 24051);
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE (`entry` = 24238);
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 24238 AND `source_type` = 0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (24238, 0, 0, 0, 1, 0, 100, 0, 10000, 15000, 45000, 60000, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Bjorn Halgurdsson - On update OOC - Say text 2'),
