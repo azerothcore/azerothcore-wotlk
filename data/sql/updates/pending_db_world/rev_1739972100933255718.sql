4f77881a2f812116b3ed71b975b96d403eecf603
Tereneckla <Tereneckla@pm.me>
2025-02-19 14:38:31 +0100

    update max rep

---
+++ data/sql/updates/pending_db_world/rev_1739972100933255718.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 120 WHERE (`creature_id` = 24664);
+UPDATE `creature_onkill_reputation` SET `MaxStanding1` = 7 WHERE `creature_id` IN (24723, 24560, 24664, 24744);
