8a072cf1253bfa84cb71905aa5f12b466c61c92f
daobashun <49193927+fangshun2004@users.noreply.github.com>
2025-02-20 13:01:52 +0800

    Update rev_1739101498649767900.sql

--- data/sql/updates/pending_db_world/rev_1739101498649767900.sql
+++ data/sql/updates/pending_db_world/rev_1739101498649767900.sql
@@ -21,7 +21,8 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceEntry` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `Comment`) VALUES
 (22, 3, @ENTRY, 0, 0, 36, 1, 0, 0, 0, 1, 'Creatures are reborn when they die');
 
-UPDATE `creature_template` SET `flags_extra` = 134217728 WHERE (`entry` = 26280);
+-- 134217728 - CREATURE_FLAG_EXTRA_DONT_OVERRIDE_SAI_ENTRY
+UPDATE `creature_template` SET 'flags_extra'='flags_extra'|134217728 WHERE (`entry` = 26280);
 
 -- Wind Trader Mu'fah smart ai
 SET @ENTRY := 26496;
