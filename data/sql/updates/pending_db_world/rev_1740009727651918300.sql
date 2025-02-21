609b1b731a01010817f9d82ddfa1bf7159132e57
Andrew <47818697+Nyeriah@users.noreply.github.com>
2025-02-21 16:26:41 -0300

    fix(Scripts/SunwellPlateau): Fix twin marks not applying from dots (#21415)

---
+++ data/sql/updates/pending_db_world/rev_1740009727651918300.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN
+('spell_eredar_twins_handle_dark_touched_periodic', 'spell_eredar_twins_handle_flame_touched_periodic', 'spell_eredar_twins_handle_flame_touched_flame_sear');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(45342, 'spell_eredar_twins_handle_flame_touched_periodic'),
+(45271, 'spell_eredar_twins_handle_dark_touched_periodic'),
+(46771, 'spell_eredar_twins_handle_flame_touched_flame_sear');
