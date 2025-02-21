ca96b7f1706abb4b70020e48c5a5574bd6ae190f
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-02-21 09:48:13 -0300

    Update rev_1740009727651918300.sql

--- data/sql/updates/pending_db_world/rev_1740009727651918300.sql
+++ data/sql/updates/pending_db_world/rev_1740009727651918300.sql
@@ -5,4 +5,3 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (45342, 'spell_eredar_twins_handle_flame_touched_periodic'),
 (45271, 'spell_eredar_twins_handle_dark_touched_periodic'),
 (46771, 'spell_eredar_twins_handle_flame_touched_flame_sear');
-
