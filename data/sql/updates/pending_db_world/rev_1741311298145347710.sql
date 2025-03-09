81a77218d9e00b5a753de54bce4075e12683bdb2
sudlud <sudlud@users.noreply.github.com>
2025-03-09 22:37:18 +0100

    Update data/sql/updates/pending_db_world/rev_1741311298145347710.sql

--- data/sql/updates/pending_db_world/rev_1741311298145347710.sql
+++ data/sql/updates/pending_db_world/rev_1741311298145347710.sql
@@ -1,2 +1,2 @@
 -- XOR PrevQuestID that was 499 Elixir of Suffering
-UPDATE `quest_template_addon` SET `PrevQuestID` = `PrevQuestID` ^ `PrevQuestID` WHERE `ID` = 501;
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` = 501;
