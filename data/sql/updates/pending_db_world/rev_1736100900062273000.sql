1b15d4c1082abca80eb4e5f7561061626849a45f
Ludwig <sudlud@users.noreply.github.com>
2025-03-10 21:38:44 +0100

    add conditions as replacement for spellscript

--- data/sql/updates/pending_db_world/rev_1736100900062273000.sql
+++ data/sql/updates/pending_db_world/rev_1736100900062273000.sql
@@ -19,6 +19,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 5358) AND (`source_type` = 0) AND (`id` = 1);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (5358, 0, 1, 0, 8, 0, 100, 0, 23359, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Cliff Giant - On Spellhit \'Transmogrify!\' - Starts an attack');
--- Add spell script for Transmogrify!
-DELETE FROM `spell_script_names` WHERE `spell_id`=23359 AND `ScriptName`='spell_transmogrify';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES(23359, 'spell_transmogrify');
+
+-- add conditions for spell 'Transmogrify!' targetting
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 13) AND (`SourceGroup` = 1) AND (`SourceEntry` = 23359) AND (`SourceId` = 0) AND (`ElseGroup` = null) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 5359) AND (`ConditionValue3` = 0);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(13, 1, 23359, 0, 0, 31, 0, 3, 5359, 0, 0, 0, 0, '', 'Spell 23359 targets Shore Strider'),
+(13, 1, 23359, 0, 1, 31, 0, 3, 5360, 0, 0, 0, 0, '', 'Spell 23359 targets Deep Strider'),
+(13, 1, 23359, 0, 2, 31, 0, 3, 5357, 0, 0, 0, 0, '', 'Spell 23359 targets Land Walker'),
+(13, 1, 23359, 0, 3, 31, 0, 3, 5361, 0, 0, 0, 0, '', 'Spell 23359 targets Wave Strider'),
+(13, 1, 23359, 0, 4, 31, 0, 3, 5358, 0, 0, 0, 0, '', 'Spell 23359 targets Cliff Giant');
