bd5fd7d61ea311792dc497c036bf935c2e16e505
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-03-11 21:13:47 +0100

    Update rev_1741721422424352200.sql

--- data/sql/updates/pending_db_world/rev_1741721422424352200.sql
+++ data/sql/updates/pending_db_world/rev_1741721422424352200.sql
@@ -1,7 +1,6 @@
 -- Update gameobject 'Orb of the Blue Flight' with sniffed values
 -- new spawns
-DELETE FROM `gameobject` WHERE (`id` IN (188415))
-AND (`guid` IN (257, 258, 259, 260));
+DELETE FROM `gameobject` WHERE (`id` IN (188415)) AND (`guid` IN (257, 258, 259, 260));
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
 (257, 188415, 585, 0, 0, 3, 1, -19.2306861877441406, -551.10784912109375, -63.7666511535644531, 4.607671737670898437, 0, 0, -0.74314403533935546, 0.669131457805633544, 7200, 255, 1, "", 50664, NULL),
 (258, 188415, 585, 0, 0, 3, 1, -32.2288398742675781, -646.16180419921875, -63.6902046203613281, 1.48352813720703125, 0, 0, 0.675589561462402343, 0.737277925014495849, 7200, 255, 1, "", 50664, NULL),
