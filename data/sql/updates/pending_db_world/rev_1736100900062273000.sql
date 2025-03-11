7e0eeab9f0cd06fd8a614684d375b8edf62ae2a2
Ludwig <sudlud@users.noreply.github.com>
2025-03-11 05:47:42 +0100

    fix conditions for explicit target
    Co-authored-by: Jelle Meeus <sogladev@gmail.com>

--- data/sql/updates/pending_db_world/rev_1736100900062273000.sql
+++ data/sql/updates/pending_db_world/rev_1736100900062273000.sql
@@ -21,10 +21,10 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (5358, 0, 1, 0, 8, 0, 100, 0, 23359, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Cliff Giant - On Spellhit \'Transmogrify!\' - Starts an attack');
 
 -- add conditions for spell 'Transmogrify!' targetting
-DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 13) AND (`SourceGroup` = 1) AND (`SourceEntry` = 23359) AND (`SourceId` = 0) AND (`ElseGroup` = null) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 5359) AND (`ConditionValue3` = 0);
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 23359) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 1);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(13, 1, 23359, 0, 0, 31, 0, 3, 5359, 0, 0, 0, 0, '', 'Spell 23359 targets Shore Strider'),
-(13, 1, 23359, 0, 1, 31, 0, 3, 5360, 0, 0, 0, 0, '', 'Spell 23359 targets Deep Strider'),
-(13, 1, 23359, 0, 2, 31, 0, 3, 5357, 0, 0, 0, 0, '', 'Spell 23359 targets Land Walker'),
-(13, 1, 23359, 0, 3, 31, 0, 3, 5361, 0, 0, 0, 0, '', 'Spell 23359 targets Wave Strider'),
-(13, 1, 23359, 0, 4, 31, 0, 3, 5358, 0, 0, 0, 0, '', 'Spell 23359 targets Cliff Giant');
+(17, 0, 23359, 0, 0, 31, 1, 3, 5359, 0, 0, 0, 0, '', 'Spell 23359 targets Shore Strider'),
+(17, 0, 23359, 0, 1, 31, 1, 3, 5360, 0, 0, 0, 0, '', 'Spell 23359 targets Deep Strider'),
+(17, 0, 23359, 0, 2, 31, 1, 3, 5357, 0, 0, 0, 0, '', 'Spell 23359 targets Land Walker'),
+(17, 0, 23359, 0, 3, 31, 1, 3, 5361, 0, 0, 0, 0, '', 'Spell 23359 targets Wave Strider'),
+(17, 0, 23359, 0, 4, 31, 1, 3, 5358, 0, 0, 0, 0, '', 'Spell 23359 targets Cliff Giant');
