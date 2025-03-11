7438b928f7edcb2f2098e99de36f4f762486a018
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-03-11 13:25:47 +0100

    Update Twins_Entropius_loots.sql

--- data/sql/updates/pending_db_world/Twins_Entropius_loots.sql
+++ data/sql/updates/pending_db_world/Twins_Entropius_loots.sql
@@ -1,7 +1,7 @@
 
--- Add RLT for Pendants 
+-- Add RLT for Pendants
 DELETE FROM `reference_loot_template` WHERE (`Entry`= 34081) AND (`Item` IN (35292, 35291, 35290));
-INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 ('34081', '35292', '0', '0', '0', '1', '1', '1', '1', 'Sin\'dorei Pendant of Triumph'),
 ('34081', '35291', '0', '0', '0', '1', '1', '1', '1', 'Sin\'dorei Pendant of Salvation'),
 ('34081', '35290', '0', '0', '0', '1', '1', '1', '1', 'Sin\'dorei Pendant of Conquest');
@@ -13,7 +13,7 @@ INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `
 
 -- Add two items into Sacrolash/Alythes RLT.
 DELETE FROM `reference_loot_template` WHERE (`Entry` = 34085) AND (`Item` IN (34209, 34212));
-INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES 
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 ('34085', '34209', '0', '0', '0', '1', '1', '1', '1', 'Spaulders of Reclamation'),
 ('34085', '34212', '0', '0', '0', '1', '1', '1', '1', 'Sunglow Vest');
 
