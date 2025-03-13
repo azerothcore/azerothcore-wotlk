e22f009cebd66404caa12767cd81f27599d45ad7
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-03-13 21:16:50 +0100

    Update SwP_Trash_Loots.sql

--- data/sql/updates/pending_db_world/SwP_Trash_Loots.sql
+++ data/sql/updates/pending_db_world/SwP_Trash_Loots.sql
@@ -4,6 +4,6 @@ INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `
 ('34091', '35202', '0', '0', '0', '1', '1', '1', '1', 'Design: Amulet of Flowing Life');
 
 DELETE FROM `reference_loot_template` WHERE (`Entry`= 34092) AND (`Item` IN (35215, 35209));
-INSERT INTO `acore_world`.`reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 ('34092', '35215', '0', '0', '0', '1', '1', '1', '1', 'Pattern: Sun-Drenched Scale Gloves'),
 ('34092', '35209', '0', '0', '0', '1', '1', '1', '1', 'Plans: Hard Khorium Battlefists');
