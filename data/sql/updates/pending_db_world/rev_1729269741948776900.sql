7a1c5ea8e94b955ab9ec5af066e8abffb52cd09b
Ludwig <sudlud@users.noreply.github.com>
2025-03-15 09:27:05 +0100

    add spell_custom_attr update

--- data/sql/updates/pending_db_world/rev_1729269741948776900.sql
+++ data/sql/updates/pending_db_world/rev_1729269741948776900.sql
@@ -3,3 +3,9 @@ DELETE FROM `smart_scripts`
 WHERE `entryorguid` = 25969;
 
 UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_jenny' WHERE (`entry` = 25969);
+
+-- spell 46340 Crates Carried
+-- add custom attribute SPELL_ATTR0_CU_IGNORE_EVADE
+DELETE FROM `spell_custom_attr` WHERE `spell_id` = 46340;
+INSERT INTO `spell_custom_attr` (spell_id, `attributes`) VALUES
+(46340, 2048);
