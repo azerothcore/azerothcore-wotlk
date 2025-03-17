b808aa0950d59ca706468b27eb50f9a1492a39af
sudlud <sudlud@users.noreply.github.com>
2025-03-17 22:01:53 +0100

    Update bug_9940.sql - update with sniffed data

--- data/sql/updates/pending_db_world/bug_9940.sql
+++ data/sql/updates/pending_db_world/bug_9940.sql
@@ -1,2 +1,2 @@
-DELETE FROM `quest_poi` WHERE `QuestID` = 8948;
-DELETE FROM `quest_poi_points` WHERE `QuestID` = 8948;
+UPDATE `quest_poi` SET `WorldMapAreaId` = 1421, `VerifiedBuild` = 51831 WHERE (`QuestID` = 8948) AND (`id` = 0);
+UPDATE `quest_poi_points` SET `VerifiedBuild` = 51831 WHERE (`QuestID` = 8948) AND (`Idx1` = 0) AND (`Idx2` = 0);
