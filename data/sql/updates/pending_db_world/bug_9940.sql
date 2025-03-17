83d9c6c763c4806405686df3d18c06b455c88566
kamenitxan <tomas.sibe@gmail.com>
2025-03-17 22:27:15 +0100

    fix(DB/POI): update poi for Anthion's Old Friend quest with sniffed data (#20980)

---
+++ data/sql/updates/pending_db_world/bug_9940.sql
@@ -0,0 +1,2 @@
+UPDATE `quest_poi` SET `WorldMapAreaId` = 1421, `VerifiedBuild` = 51831 WHERE (`QuestID` = 8948) AND (`id` = 0);
+UPDATE `quest_poi_points` SET `VerifiedBuild` = 51831 WHERE (`QuestID` = 8948) AND (`Idx1` = 0) AND (`Idx2` = 0);
