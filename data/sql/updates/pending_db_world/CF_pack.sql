87effd1841269c377a67cb2789582ad7c91b36c3
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-03-19 23:43:09 +0100

    fix(DB/Creature) Add formation for the the last pack before Kalecgos. (#21735)

---
+++ data/sql/updates/pending_db_world/CF_pack.sql
@@ -0,0 +1,10 @@
+
+-- Add creature formation for the last pack before Kalecgos
+DELETE FROM `creature_formations` WHERE `leaderGUID` = 54834;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(54834, 54834, 0, 0, 3, 0, 0),
+(54834, 43654, 0, 0, 3, 0, 0),
+(54834, 42573, 0, 0, 3, 0, 0),
+(54834, 42574, 0, 0, 3, 0, 0),
+(54834, 54817, 0, 0, 3, 0, 0),
+(54834, 42656, 0, 0, 3, 0, 0);
