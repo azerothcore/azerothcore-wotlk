014d1157d60d6b760bc7a582ca9ed45247433f4a
Ryan Turner <AugustoMendes4426@gmail.com>
2025-03-27 22:09:39 +0000

    Update rev_1743112818280220600.sql

--- data/sql/updates/pending_db_world/rev_1743112818280220600.sql
+++ data/sql/updates/pending_db_world/rev_1743112818280220600.sql
@@ -2,4 +2,4 @@
 UPDATE `creature_template` SET `npcflag` = `npcflag` &~ 2, `type_flags` = `type_flags` | 1048576 WHERE (`entry` = 17600);
  
 -- Adds the `Quest Giver` to the only NPC (by GUID) that allow you to deliver the quest.
-UPDATE `creature` SET `npcflag` = `npcflag` | 2, ` WHERE (`id1` = 17600 AND `guid` = 84427);
+UPDATE `creature` SET `npcflag` = `npcflag` | 2 WHERE (`id1` = 17600 AND `guid` = 84427);
