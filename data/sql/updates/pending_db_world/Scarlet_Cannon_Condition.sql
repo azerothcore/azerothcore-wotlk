cf0bcf8616968b947f49a080b7b2a490981dd601
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-03-28 17:24:16 +0100

    fix(DB/Condition) Scarlet Cannons now require Massacre at Light’s Point to be used. (#21796)

---
+++ data/sql/updates/pending_db_world/Scarlet_Cannon_Condition.sql
@@ -0,0 +1,5 @@
+
+-- Add Condition to use Scarlet Cannons
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 18) AND (`SourceGroup` = 28833) AND (`SourceEntry` = 52447) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 9) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 12701) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(18, 28833, 52447, 0, 0, 9, 0, 12701, 0, 0, 0, 0, 0, '', 'Scarlet Cannon spellclick require Massacre at Light’s Point quest taken');
