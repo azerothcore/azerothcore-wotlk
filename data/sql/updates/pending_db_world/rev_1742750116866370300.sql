80627efd6f34e0dc87a9e2f90a3e6aa8873d0d7a
Ryan Turner <AugustoMendes4426@gmail.com>
2025-03-29 08:35:27 +0000

    (db/Creautre) - Adds Heroic version of Brann Bronzebeard to Halls of Stone (#21755)

---
+++ data/sql/updates/pending_db_world/rev_1742750116866370300.sql
@@ -0,0 +1,2 @@
+-- Matches the Heroic Faction to the base brann, Adds "Brann Bronzebeard" (1) [Heroic] to Heroic Version | 32'052 => 40'065 HP
+UPDATE `creature_template` SET `faction` = 1665, `difficulty_entry_1` = 31366 WHERE (`entry` = 28070);
