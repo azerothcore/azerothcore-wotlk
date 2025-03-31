658f23b9a14fb62250102f745dbf9e5a40a7f487
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-03-31 20:46:35 +0200

    Update Dark_Fiend.sql

--- data/sql/updates/pending_db_world/Dark_Fiend.sql
+++ data/sql/updates/pending_db_world/Dark_Fiend.sql
@@ -5,4 +5,4 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 25744;
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 25744) AND (`source_type` = 0) AND (`id` IN (3, 4));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (25744, 0, 3, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 42, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dark Fiend - On Initialize - Set Invincibility Hp 1%'),
-(25744, 0, 4, 0, 60, 0, 100, 1, 1000, 1000, 1000, 1000, 0, 0, 49, 0, 0, 0, 0, 0, 0, 21, 200, 0, 0, 0, 0, 0, 0, 0, 'Dark Fiend - On Update - Start Attacking (No Repeat)');
+(25744, 0, 4, 0, 60, 0, 100, 1, 1000, 2000, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 21, 200, 0, 0, 0, 0, 0, 0, 0, 'Dark Fiend - On Update - Start Attacking (No Repeat)');
