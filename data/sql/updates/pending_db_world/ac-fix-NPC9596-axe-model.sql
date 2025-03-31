c41973b20e275f30bf2aaa3baa8456cefa01fa1b
Smirnov Sergey <lyns@lyns.pro>
2025-03-31 10:42:42 +0300

    Update ac-fix-NPC9596-axe-model.sql

--- data/sql/updates/pending_db_world/ac-fix-NPC9596-axe-model.sql
+++ data/sql/updates/pending_db_world/ac-fix-NPC9596-axe-model.sql
@@ -1,4 +1,2 @@
 -- Added missing ranged weapon for SpellID 6466
-DELETE FROM `creature_equip_template` WHERE (`CreatureID` = 9596);
-INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES
-(9596, 1, 7612, 0, 5856, 18019);
+UPDATE `creature_equip_template` SET `ItemID3` = 5856, `VerifiedBuild` = 0 WHERE (`CreatureID` = 9596 AND `VerifiedBuild` = 18019)
