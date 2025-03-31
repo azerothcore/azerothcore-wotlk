68ecde23e13be7d9e7b8c04753d929bb8de48d57
sudlud <sudlud@users.noreply.github.com>
2025-03-31 17:57:37 +0200

    Update ac-fix-NPC9596-axe-model.sql

--- data/sql/updates/pending_db_world/ac-fix-NPC9596-axe-model.sql
+++ data/sql/updates/pending_db_world/ac-fix-NPC9596-axe-model.sql
@@ -1,2 +1,2 @@
 -- Added missing ranged weapon for SpellID 6466
-UPDATE `creature_equip_template` SET `ItemID3` = 5856, `VerifiedBuild` = 0 WHERE (`CreatureID` = 9596 AND `VerifiedBuild` = 18019)
+UPDATE `creature_equip_template` SET `ItemID3` = 5856, `VerifiedBuild` = 0 WHERE (`CreatureID` = 9596);
