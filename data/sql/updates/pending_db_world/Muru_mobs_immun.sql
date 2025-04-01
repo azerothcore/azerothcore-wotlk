b1362b68932a4260a81da9964e9a1097c6ba889a
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-04-01 17:29:59 +0200

    Create Muru_mobs_immun.sql

---
+++ data/sql/updates/pending_db_world/Muru_mobs_immun.sql
@@ -0,0 +1,6 @@
+
+-- Add Charm, Disoriented, Distract, Fear, Sleep, Banish immunities for Berserkers and Fury Mages.
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask` |131611 WHERE (`entry` IN(25798, 25799));
+
+-- Add Charm, Disoriented, Distract, Fear, Root, Slow Attack, Silence, Sleep, Snare, Stun, Freeze, Knockout, Polymorph, Banish, Interrupt, Daze, Sapped immunities for Void Sentinel.
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask` |637747163 WHERE (`entry` = 25772);
