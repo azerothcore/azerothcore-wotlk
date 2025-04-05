711bd091c87670b7621ef9323db858df13648f82
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-04-05 22:13:29 +0200

    Update Dk_Phase_64.sql

--- data/sql/updates/pending_db_world/Dk_Phase_64.sql
+++ data/sql/updates/pending_db_world/Dk_Phase_64.sql
@@ -256,3 +256,14 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 29136;
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29136) AND (`source_type` = 0) AND (`id` IN (0));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (29136, 0, 0, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 53278, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Volatile Ghoul - On Just Died - Cast \'Ghoulplosion\'');
+
+-- Add Death Knight Champion SmartAI
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 29106;
+
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 29106);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(29106, 0, 0, 0, 0, 0, 100, 0, 4000, 6000, 8000, 12000, 0, 0, 11, 52372, 32, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Death Knight Champion - In Combat - Cast \'Icy Touch\''),
+(29106, 0, 1, 0, 0, 0, 100, 0, 3000, 5000, 8000, 12000, 0, 0, 11, 52373, 32, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Death Knight Champion - In Combat - Cast \'Plague Strike\''),
+(29106, 0, 2, 0, 0, 0, 100, 0, 4000, 8000, 4000, 8000, 0, 0, 11, 52374, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Death Knight Champion - In Combat - Cast \'Blood Strike\''),
+(29106, 0, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 52375, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Death Knight Champion - In Combat - Cast \'Death Coil\'');
+
