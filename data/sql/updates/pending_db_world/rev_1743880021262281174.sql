bee1c4b80980a986d60355d6c92ae7dcf7e0188f
Jelle Meeus <sogladev@gmail.com>
2025-04-05 23:02:28 +0200

    fix(DB/GossipMenuOption): Swap Exarch Nasuun Anvil and Harbor progress (#21864)

---
+++ data/sql/updates/pending_db_world/rev_1743880021262281174.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `gossip_menu_option` SET `ActionMenuID`=51001 WHERE `MenuID`=9046 AND `OptionID`=1;
+UPDATE `gossip_menu_option` SET `ActionMenuID`=51002 WHERE `MenuID`=9046 AND `OptionID`=2;
