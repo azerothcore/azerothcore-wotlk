81c4d90ec90b2f63c4b8d1b09f35f786e2d7bc3b
kissingers <kissinger@foxmail.com>
2025-04-08 21:55:23 +0800

    修复风暴要塞的星术师隐身打人问题

--- src/server/scripts/Outland/TempestKeep/Eye/boss_astromancer.cpp
+++ src/server/scripts/Outland/TempestKeep/Eye/boss_astromancer.cpp
@@ -198,7 +198,6 @@ struct boss_high_astromancer_solarian : public BossAI
                 });
             }).Schedule(23s, [this](TaskContext)
             {
-                me->SetReactState(REACT_AGGRESSIVE);
                 summons.DoForAllSummons([&](WorldObject* summon)
                 {
                     if (Creature* light = summon->ToCreature())
@@ -210,6 +209,7 @@ struct boss_high_astromancer_solarian : public BossAI
                             {
                                 me->RemoveAllAuras();
                                 me->SetModelVisible(true);
+                                me->SetReactState(REACT_AGGRESSIVE);
                             }
                             else
                             {
