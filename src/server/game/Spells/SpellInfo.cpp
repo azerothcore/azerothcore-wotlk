36e62333be949f1c8b626ac1f3774bca3b0d78af
kissingers <kissinger@foxmail.com>
2025-04-08 22:02:20 +0800

    支持不同类型卷轴可以叠使用

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2236,7 +2236,7 @@ SpellSpecificType SpellInfo::LoadSpellSpecific() const
                         return SPELL_SPECIFIC_DRINK;
                 }
                 // scrolls effects
-                else
+             /* else
                 {
                     SpellInfo const* firstRankSpellInfo = GetFirstRankSpell();
                     switch (firstRankSpellInfo->Id)
@@ -2249,7 +2249,7 @@ SpellSpecificType SpellInfo::LoadSpellSpecific() const
                         case 8091: // Armor
                             return SPELL_SPECIFIC_SCROLL;
                     }
-                }
+                } */
                 break;
             }
         case SPELLFAMILY_MAGE:
