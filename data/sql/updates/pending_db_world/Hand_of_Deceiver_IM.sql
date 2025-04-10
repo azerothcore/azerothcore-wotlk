c8eb24ef1ee11d38428f537e1831ddab268c3de8
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-04-10 22:03:29 +0200

    Update Hand_of_Deceiver_IM.sql

--- data/sql/updates/pending_db_world/Hand_of_Deceiver_IM.sql
+++ data/sql/updates/pending_db_world/Hand_of_Deceiver_IM.sql
@@ -1,3 +1,4 @@
 
 -- Remove Stun and Silence immunities
-UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`& ~ 2304  WHERE (`entry` = 25588);
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`& ~ 2048  WHERE (`entry` = 25588);
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`& ~ 256  WHERE (`entry` = 25588);
