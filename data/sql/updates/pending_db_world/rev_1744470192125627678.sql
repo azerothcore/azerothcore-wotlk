3f77bc364bf12044ac41ed16ca24a6697cdf69c7
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-04-12 12:09:13 -0500

    Update rev_1744470192125627678.sql

--- data/sql/updates/pending_db_world/rev_1744470192125627678.sql
+++ data/sql/updates/pending_db_world/rev_1744470192125627678.sql
@@ -3,3 +3,7 @@ UPDATE `creature_template` SET
   `AIName` = '',
   `ScriptName` = 'npc_dark_fiend'
 WHERE `entry` = 25744;
+
+DELETE FROM `creature_template_spell` WHERE `CreatureID` = 25744 AND `Index` = 0;
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(25744, 0, 45944, 0); -- Dark Fiend spell
