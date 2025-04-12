4e47159c137104263bcc55cde0b755d067e2df9f
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-04-12 08:32:58 -0500

    placeholder blink

--- src/server/scripts/EasternKingdoms/SunwellPlateau/instance_sunwell_plateau.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/instance_sunwell_plateau.cpp
@@ -285,7 +285,7 @@ private:
 enum SunbladeArchMageSpells
 {
     SPELL_ARCANE_EXPLOSION    = 46553,
-    SPELL_BLINK               = 28401,
+    SPELL_BLINK               = 29208, // Placeholder blink should be 28401
     SPELL_FROST_NOVA          = 46555
 };
 
