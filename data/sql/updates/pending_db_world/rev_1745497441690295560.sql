a9318af129cb41728c167235454c0d8a23376b4c
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-04-24 07:28:47 -0500

    name change

--- data/sql/updates/pending_db_world/rev_1745497441690295560.sql
+++ data/sql/updates/pending_db_world/rev_1745497441690295560.sql
@@ -1,4 +1,4 @@
 -- blue dragon 25% haste aura
 DELETE FROM `spell_script_names` WHERE `spell_id` = 45856;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(45856, 'spell_blue_dragon_breath_haste');
+(45856, 'spell_kiljaeden_breath_haste');
