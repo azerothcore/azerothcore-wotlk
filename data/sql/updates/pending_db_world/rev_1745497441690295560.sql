ab5cd79a1d6158879078a393660d4f367bdb62ec
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-04-25 14:13:37 -0500

    spell_script_names

--- data/sql/updates/pending_db_world/rev_1745497441690295560.sql
+++ data/sql/updates/pending_db_world/rev_1745497441690295560.sql
@@ -3,3 +3,7 @@ DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 45856 AND `spell_effect
 INSERT INTO `spell_linked_spell`
 (`spell_trigger`, `spell_effect`, `type`, `comment`)
 VALUES(45856, 49725, 1, 'Power of the Blue Flight - Breath: Haste');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = 45856;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(45856, 'spell_kiljaeden_breath_haste');
