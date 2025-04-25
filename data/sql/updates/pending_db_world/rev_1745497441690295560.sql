7a84999ef1deda5194e1e1654e4dfe02e07148ab
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-04-25 16:16:48 -0500

    fix(Scripts/SunwellPlateau): Make Blue Dragon Breath Haste apply 25% haste effect (#21984)
    Co-authored-by: Jelle Meeus <sogladev@gmail.com>

---
+++ data/sql/updates/pending_db_world/rev_1745497441690295560.sql
@@ -0,0 +1,5 @@
+-- blue dragon 25% haste aura
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 45856 AND `spell_effect` = 49725;
+INSERT INTO `spell_linked_spell`
+(`spell_trigger`, `spell_effect`, `type`, `comment`)
+VALUES(45856, 49725, 1, 'Power of the Blue Flight - Breath: Haste');
