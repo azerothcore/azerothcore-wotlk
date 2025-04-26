da55f05cfc2db9bb49a37cd0b3697bbda565856a
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2025-04-26 14:31:30 -0400

    refactor(Core/WorldState): Initial addition of world state definition file. (#21875)
    Many world states had their enums from script's header and hardcoded values to their respective header file (WorldStateDefines.h)
    Co-authored-by: Jelle Meeus <sogladev@gmail.com>

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -127,18 +127,6 @@ enum RealmZone
     REALM_ZONE_CN5_8         = 37                           // basic-Latin at create, any at login
 };
 
-enum WorldStates
-{
-    WS_ARENA_DISTRIBUTION_TIME                 = 20001,                     // Next arena distribution time
-    WS_WEEKLY_QUEST_RESET_TIME                 = 20002,                     // Next weekly reset time
-    WS_BG_DAILY_RESET_TIME                     = 20003,                     // Next daily BG reset time
-    WS_CLEANING_FLAGS                          = 20004,                     // Cleaning Flags
-    WS_DAILY_QUEST_RESET_TIME                  = 20005,                     // Next daily reset time
-    WS_GUILD_DAILY_RESET_TIME                  = 20006,                     // Next guild cap reset time
-    WS_MONTHLY_QUEST_RESET_TIME                = 20007,                     // Next monthly reset time
-    WS_DAILY_CALENDAR_DELETION_OLD_EVENTS_TIME = 20008                      // Next daily calendar deletions of old events time
-};
-
 // xinef: petitions storage
 struct PetitionData
 {
