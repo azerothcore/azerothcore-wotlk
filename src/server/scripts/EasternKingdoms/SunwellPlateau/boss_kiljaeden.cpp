0fb03fcd75ffeb3e965f61cbb9dacc187269acbb
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-04-29 20:55:10 -0500

    Codestyle

--- src/server/scripts/EasternKingdoms/SunwellPlateau/boss_kiljaeden.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/boss_kiljaeden.cpp
@@ -522,7 +522,7 @@ struct boss_kiljaeden : public BossAI
         Talk(SAY_KJ_EMERGE);
         ScheduleBasicAbilities();
     }
-    
+
     void JustSummoned(Creature* summon) override
     {
         BossAI::JustSummoned(summon);
