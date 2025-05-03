096af2425ff7bc1200554877051456f6ef2d53ea
Jelle Meeus <sogladev@gmail.com>
2025-05-03 11:23:01 +0200

    style

--- data/sql/updates/pending_db_world/rev_1746262733742509979.sql
+++ data/sql/updates/pending_db_world/rev_1746262733742509979.sql
@@ -1,3 +1,3 @@
 --
 -- Hamstring 0x0002, Rend 0x0020, Sunder Armor 0x4000
-UPDATE `spell_proc_event` SET `SpellFamilyMask0`=`SpellFamilyMask0` | (0x0002 | 0x0020 | 0x4000) WHERE entry=-12281;
+UPDATE `spell_proc_event` SET `SpellFamilyMask0`=`SpellFamilyMask0` | (0x0002 | 0x0020 | 0x4000) WHERE `entry`=-12281;
