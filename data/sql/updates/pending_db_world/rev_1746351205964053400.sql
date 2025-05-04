64d7deec6e6b08ead844a36d7ee4755d3a90d538
avarishd <46330494+avarishd@users.noreply.github.com>
2025-05-05 02:13:26 +0300

    fix(DB/Conditions): Gather Lumber not despawning Coldwind Tree GOs (#22050)

---
+++ data/sql/updates/pending_db_world/rev_1746351205964053400.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 13) AND (`SourceGroup` = 1) AND (`SourceEntry` = 47939);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(13, 1, 47939, 0, 0, 30, 0, 188539, 5, 0, 0, 0, 0, '', '\'Lumber Hack\' - Limit \'Gather Lumber\' to Coldwind Tree GOs within 5y.');
