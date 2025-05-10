6a387fe6377d9d82f2b8b832ab79e72b51cc7169
Ryan Turner <AugustoMendes4426@gmail.com>
2025-05-10 01:10:09 +0100

    fix(DB/Loot) - Lorgalis Manuscript no longer drops from creatures. (#22092)

---
+++ data/sql/updates/pending_db_world/rev_1746808964702490700.sql
@@ -0,0 +1,2 @@
+-- Deletes `Lorgalis Manuscript` (5359) from `Aku'mai Fisher` (4824), `Blackfathom Myrmidon` (4807), `Blackfathom Sea Witch` (4805) and `Snapping Crustacean` (4822)
+DELETE FROM `creature_loot_template` WHERE `Item` = 5359;
