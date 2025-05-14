8c7c67a10524b9ca9098bf8fe793fd0f5d8fa55a
Jelle Meeus <sogladev@gmail.com>
2025-05-14 23:19:51 +0200

    fix(Scripts/SunwellPlateau)SpellFurySpellsteal

---
+++ data/sql/updates/pending_db_world/rev_1747255986267456043.sql
@@ -0,0 +1,10 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = 46102;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(46102, 'spell_spell_fury_aura');
+
+-- Remove 'Deal periodic damage'
+SET @procFlags = (0x4000 | 0x10000);
+DELETE FROM `spell_proc_event` WHERE `entry` = 46102;
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`) VALUES
+(46102, @procFlags);
