9ebc882b0b58952697ea49d53e7c7bf7c55252a3
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-05-23 17:11:30 +0200

    Update SE_Npcs_Set_Active.sql
    Removed Set_Active on Citizen of Havenshire smartAI.
    I moved Set_Active into another dedicated PR:
    https://github.com/azerothcore/azerothcore-wotlk/pull/22185

--- data/sql/updates/pending_db_world/SE_Npcs_Set_Active.sql
+++ data/sql/updates/pending_db_world/SE_Npcs_Set_Active.sql
@@ -1,12 +1,4 @@
 
--- Citizen of Havenshire
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE (`entry` IN (28576, 28577));
-
-DELETE FROM `smart_scripts` WHERE (`entryorguid` IN (28576, 28577)) AND (`source_type` = 0) AND (`id` IN (16));
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(28576, 0, 16, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 48, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Citizen of Havenshire - On Reset - Set Active On'),
-(28577, 0, 16, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 48, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Citizen of Havenshire - On Reset - Set Active On');
-
 -- Mine Car
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28821;
 
