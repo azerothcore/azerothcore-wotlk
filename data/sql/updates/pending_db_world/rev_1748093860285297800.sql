d9f4549e26f9dba7cfccf4449f62fef12afcfff9
avarishd <46330494+avarishd@users.noreply.github.com>
2025-05-24 18:27:16 +0300

    fix(Instance/Oculus):  Varos Cloudstrider does not summon Azure Ring Captain

---
+++ data/sql/updates/pending_db_world/rev_1748093860285297800.sql
@@ -0,0 +1,4 @@
+-- Azure Ring Captain (28236)
+DELETE FROM `creature_template_movement` WHERE (`CreatureId` = 28236);
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
+(28236, 0, 0, 1, 0, 0, 0, 0);
