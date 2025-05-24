63b4f41f05627c305e082238f182c794870caa22
avarishd <46330494+avarishd@users.noreply.github.com>
2025-05-24 11:31:03 +0300

    fix(DB/Spells): Shield of the Blue output damage reduction (#22191)

---
+++ data/sql/updates/pending_db_world/rev_1748062500283594000.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=45848 AND `spell_effect`=47314;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (45848, 47314, 0, 'Shield of the Blue 95% output damage reduction');
