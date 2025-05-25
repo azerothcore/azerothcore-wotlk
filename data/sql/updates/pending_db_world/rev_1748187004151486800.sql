bd297250b13fd6fae2fd0c577cc46722ccd935fa
brian8544 <brianleon77@gmail.com>
2025-05-25 18:08:33 +0200

    CR LF -> LF

--- data/sql/updates/pending_db_world/rev_1748187004151486800.sql
+++ data/sql/updates/pending_db_world/rev_1748187004151486800.sql
@@ -2,4 +2,3 @@
 DELETE FROM `command` WHERE `name` = 'group revive';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('group revive', 2, 'Syntax: .group revive\r\n\r\nRevives all players in your group.');
-
