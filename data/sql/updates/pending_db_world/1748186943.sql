eefcec5b968b187158ec740b53fc350ad2d8cd5c
= <contact@ovv.dev>
2025-05-25 17:39:38 +0200

    codestyle fix

--- data/sql/updates/pending_db_world/1748186943.sql
+++ data/sql/updates/pending_db_world/1748186943.sql
@@ -1,5 +1,5 @@
 -- Rune-Smith Durar - Durar\'s Power Cell
-UPDATE creature_loot_template SET Chance=100 WHERE Entry=26409 AND Item=36857;
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Entry`=26409 AND `Item`=36857;
 
 -- Rune-Smith Kathorn - Kathorn\'s Power Cell
-UPDATE creature_loot_template SET Chance=100 WHERE Entry=26410 AND Item=36858;
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Entry`=26410 AND `Item`=36858;
