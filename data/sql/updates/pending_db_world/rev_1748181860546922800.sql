fdaaaae2574315818ef96cbbe178e960c8b70347
avarishd <46330494+avarishd@users.noreply.github.com>
2025-05-26 13:44:42 +0300

    fix(Scripts/ScarletEnclave): Script Recall Eye of Acherus & fix Summo… (#22205)

---
+++ data/sql/updates/pending_db_world/rev_1748181860546922800.sql
@@ -0,0 +1,10 @@
+-- Eye of Acherus
+DELETE FROM `creature_template_spell` WHERE (`CreatureID` = 28511);
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(28511, 0, 51859, 0),
+(28511, 1, 51904, 0),
+(28511, 2, 52006, 0),
+(28511, 4, 52694, 0);
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=52694 AND `ScriptName`='spell_q12641_death_comes_from_on_high_recall_eye';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (52694, 'spell_q12641_death_comes_from_on_high_recall_eye');
