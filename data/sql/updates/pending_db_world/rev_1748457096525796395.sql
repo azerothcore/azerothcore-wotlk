117ceaab8db2754f76cc3cc98e0c78b493ab1e33
LuckyLuc96 <lucsaybibi@gmail.com>
2025-05-28 14:47:13 -0400

    And the same edit for the horde. I was not aware the horde had the same issue.

--- data/sql/updates/pending_db_world/rev_1748457096525796395.sql
+++ data/sql/updates/pending_db_world/rev_1748457096525796395.sql
@@ -2,3 +2,5 @@
 -- Changes "AllowableRaces" to only alliance races. Should stop this quest from being sharable to horde players.
 -- Changes RequiredNpcOrGo1 from Captain Alina to the Shattered Hand Executioner
 UPDATE `quest_template` SET `AllowableRaces` = 1101, `RequiredNpcOrGo1` = 17301 WHERE (`ID` = 9524);
+-- And the same for the horde version of the quest. I didn't realize they shared the issue!
+UPDATE `quest_template` SET `AllowableRaces` = 690, `RequiredNpcOrGo1` = 17301 WHERE (`ID` = 9525);
