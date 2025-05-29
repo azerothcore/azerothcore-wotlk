3916f6d3bf4b655c83f08845e5618b774d600f38
Macs-Account <macsgithub@gmail.com>
2025-05-29 11:40:55 -0600

    Update SpellEffects.cpp
    Revert an earlier change, as a different place in code was changed instead

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3620,12 +3620,7 @@ void Spell::EffectWeaponDmg(SpellEffIndex effIndex)
     }
     else
     {
-        //Abilities that deal magic damage based on weapon percent damage interact with percent damage modifiers incorrectly in CalculateDamage()
-        //This is the lowest risk way to fix this bug, but if someone who understands CalculateDamage() very well wants to see why this happens that would be better
-        bool isPhysical = true;
-        if (const SpellInfo* spellInfo = GetSpellInfo())
-            isPhysical = spellInfo->GetSchoolMask() == SpellSchoolMask::SPELL_SCHOOL_MASK_NORMAL;
-        weaponDamage = m_caster->CalculateDamage(m_attackType, normalized, isPhysical);
+        weaponDamage = m_caster->CalculateDamage(m_attackType, normalized, true);
     }
 
     // Sequence is important
