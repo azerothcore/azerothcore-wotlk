e214ea141afc09be3b18ef283afddb00f2cedfbd
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-05-30 17:36:46 +0200

    Update Eye_of_Acherus.sql

--- data/sql/updates/pending_db_world/Eye_of_Acherus.sql
+++ data/sql/updates/pending_db_world/Eye_of_Acherus.sql
@@ -25,7 +25,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (2851100, 9, 10, 0, 0, 0, 100, 512, 500, 500, 0, 0, 0, 0, 103, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Eye of Acherus - Actionlist - Set Rooted Off');
 
 -- Set General SmartAI and remove script name
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28511;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = 28511;
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 28511);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
