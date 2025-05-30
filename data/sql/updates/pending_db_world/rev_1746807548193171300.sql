b15507eb06accb17d30f474dde4aefd38a8a24e5
Jelle Meeus <sogladev@gmail.com>
2025-05-30 16:42:11 +0200

    feat(Scripts/Commands): `.debug boundary` to visualize `CreatureBoundary`  (#22099)
    Co-authored-by: avarishd <46330494+avarishd@users.noreply.github.com>
    Co-authored-by: treeston <treeston.mmoc@gmail.com>

---
+++ data/sql/updates/pending_db_world/rev_1746807548193171300.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `command` WHERE `name`IN('debug boundary');
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('debug boundary', 3, 'Syntax: .debug boundary [duration] [fill] [z]\nOptional arguments:\n- duration: Duration in ms (default: 5000, max: 180000).\n- fill: Fills the boundary with markers.\n- z: Includes z-axis in visualization.');
