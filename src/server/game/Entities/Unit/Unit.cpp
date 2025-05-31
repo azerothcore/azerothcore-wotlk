388455f38700c63007e43d43e2b87df0d5400982
kissingers <kissinger@foxmail.com>
2025-05-31 15:58:22 +0800

    回退审判回血太快bug

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8233,36 +8233,37 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                 switch (dummySpell->Id)
                 {
                     // Judgement of Light
-                    case 20185:
-                        {
-                            if (!victim || !victim->IsAlive())
-                                return false;
+                case 20185:
+                {
+                    if (!victim || !victim->IsAlive() || victim->HasSpellCooldown(20267))
+                        return false;
 
-                            auto* caster = triggeredByAura->GetBase()->GetCaster();
-                            if (!caster || !victim->IsFriendlyTo(caster))
-                                return false;
+                    auto* caster = triggeredByAura->GetBase()->GetCaster();
+                    if (!caster || !victim->IsFriendlyTo(caster))
+                        return false;
 
-                            // 2% of base health
-                            basepoints0 = int32(victim->CountPctFromMaxHealth(2));
-                            victim->CastCustomSpell(victim, 20267, &basepoints0, 0, 0, true, 0, triggeredByAura);
-                            return true;
-                        }
-                    // Judgement of Wisdom
-                    case 20186:
-                        {
-                            if (!victim || !victim->IsAlive() || !victim->HasActivePowerType(POWER_MANA))
-                                return false;
+                    // 2% of base health
+                    basepoints0 = int32(victim->CountPctFromMaxHealth(2));
+                    victim->AddSpellCooldown(20267, 0, 4 * IN_MILLISECONDS);
+                    victim->CastCustomSpell(victim, 20267, &basepoints0, 0, 0, true, 0, triggeredByAura);
+                    return true;
+                }
+                // Judgement of Wisdom
+                case 20186:
+                {
+                    if (!victim || !victim->IsAlive() || !victim->HasActivePowerType(POWER_MANA) || victim->HasSpellCooldown(20268))
+                        return false;
 
-                            auto* caster = triggeredByAura->GetBase()->GetCaster();
-                            if (!caster || !victim->IsFriendlyTo(caster))
-                                return false;
+                    auto* caster = triggeredByAura->GetBase()->GetCaster();
+                    if (!caster || !victim->IsFriendlyTo(caster))
+                        return false;
 
-                            // 2% of base mana
-                            basepoints0 = int32(CalculatePct(victim->GetCreateMana(), 2));
-                            victim->CastCustomSpell(victim, 20268, &basepoints0, nullptr, nullptr, true, 0, triggeredByAura);
-                            return true;
-                        }
-                    // Holy Power (Redemption Armor set)
+                    // 2% of base mana
+                    basepoints0 = int32(CalculatePct(victim->GetCreateMana(), 2));
+                    victim->CastCustomSpell(victim, 20268, &basepoints0, nullptr, nullptr, true, 0, triggeredByAura);
+                    return true;
+                }
+                // Holy Power (Redemption Armor set)
                     case 28789:
                         {
                             if (!victim)
