1f082cb29c6e00aff853a1b1c8ba2244b02e5815
Martin Leuner <martin-leuner@users.noreply.github.com>
2025-06-05 17:43:52 +0200

    fix(DB/Creature): Remove extra Shaffar adds (#22255)

---
+++ data/sql/updates/pending_db_world/rev_1748707579733807961.sql
@@ -0,0 +1,10 @@
+-- Link pre-spawned Ethereal Beacons to Nexus-Prince Shaffar so they respawn on reset
+SET @SHAFFAR_GUID := 91162;
+SET @AI_FLAGS := 1 | 2 | 8 | 16;
+
+DELETE FROM `creature_formations` WHERE `leaderGUID` = @SHAFFAR_GUID;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@SHAFFAR_GUID, @SHAFFAR_GUID, 0, 0, @AI_FLAGS, 0, 0),
+(@SHAFFAR_GUID, 91131, 0, 0, @AI_FLAGS, 0, 0),
+(@SHAFFAR_GUID, 91132, 0, 0, @AI_FLAGS, 0, 0),
+(@SHAFFAR_GUID, 91133, 0, 0, @AI_FLAGS, 0, 0);
