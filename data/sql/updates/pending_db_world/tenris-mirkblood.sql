bae8e9b422fdedfa9951f85455bfb254579f4828
heyitsbench <benjymansy123@gmail.com>
2025-06-06 06:58:53 -0400

    If I only had a brain.

--- data/sql/updates/pending_db_world/tenris-mirkblood.sql
+++ data/sql/updates/pending_db_world/tenris-mirkblood.sql
@@ -1,11 +1,11 @@
-SET @SAY_APPROACH := 0,
+SET @SAY_APPROACH = 0,
 @SAY_AGGRO = 1,
 @SAY_SUMMON = 2;
 
 UPDATE `creature_template` SET `ScriptName` = 'boss_tenris_mirkblood' WHERE `entry` = 28194;
 
-DELETE FROM `creature_text` WHERE `CreatureID` = 28167;
+DELETE FROM `creature_text` WHERE `CreatureID` = 28194;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (28194, @SAY_APPROACH, 0, 'I smell... $r.  Delicious!', 14, 0, 100, 0, 0, 0, 27780, 0, 'Prince Tenris Mirkblood - SAY_APPROACH'), -- Nothing sniffed, types need verification
 (28194, @SAY_AGGRO, 0, 'I shall consume you!', 14, 0, 100, 0, 0, 0, 27781, 0, 'Prince Tenris Mirkblood - SAY_AGGRO'),
-(28194, @SAY_SUMMON, 0, 'Drink, mortals!  Taste my blood!  Taste your death!', 12, 0, 100, 0, 0, 0, 27712, 0, 'Prince Tenris Mirkblood - SAY_SUMMON'),
+(28194, @SAY_SUMMON, 0, 'Drink, mortals!  Taste my blood!  Taste your death!', 12, 0, 100, 0, 0, 0, 27712, 0, 'Prince Tenris Mirkblood - SAY_SUMMON');
