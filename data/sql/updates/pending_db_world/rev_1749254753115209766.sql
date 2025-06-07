a4e609f7624b1997280f64815d63ea3c36e85b10
Jelle Meeus <sogladev@gmail.com>
2025-06-07 10:43:25 +0200

    fix(DB/Command): update `wchange` help (#22281)

---
+++ data/sql/updates/pending_db_world/rev_1749254753115209766.sql
@@ -0,0 +1,7 @@
+--
+UPDATE `command` SET `help`='Syntax: .wchange #weathertype #grade
+Set current weather to #weathertype with an intensity of #grade.
+
+#weathertype can be 0 for fine, 1 for rain, 2 for snow, 3 for storm, 86 for thunders, 90 for blackrain.
+#grade is a float value from 0.0 (disabled) to 1.0 (maximum intensity).'
+WHERE `name`='wchange';
