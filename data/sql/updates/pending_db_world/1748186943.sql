e3096010a476ccadefde6a149a65e679e3667612
Quentin Dawans <contact@ovv.dev>
2025-06-08 02:14:34 +0200

    fix(DB/creature_loot) - Durar's and Kathorn's Power Cell corrected drop rate to 100% (#22203)

---
+++ data/sql/updates/pending_db_world/1748186943.sql
@@ -0,0 +1,5 @@
+-- Rune-Smith Durar - Durar\'s Power Cell
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Entry`=26409 AND `Item`=36857;
+
+-- Rune-Smith Kathorn - Kathorn\'s Power Cell
+UPDATE `creature_loot_template` SET `Chance`=100 WHERE `Entry`=26410 AND `Item`=36858;
