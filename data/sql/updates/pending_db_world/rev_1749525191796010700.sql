e47743eb6a339c0d8296704dbfc6a115b5127694
Andrew <47818697+Nyeriah@users.noreply.github.com>
2025-06-12 11:16:59 -0300

    Update rev_1749525191796010700.sql

--- data/sql/updates/pending_db_world/rev_1749525191796010700.sql
+++ data/sql/updates/pending_db_world/rev_1749525191796010700.sql
@@ -1,4 +1,6 @@
---
-UPDATE `creature_template` SET `dmgschool` = 2, `spell_school_immune_mask` = 4 WHERE `entry` IN (30416, 31453); -- Bound Fire Elemental
-UPDATE `creature_template` SET `dmgschool` = 4, `spell_school_immune_mask` = 16 WHERE `entry` IN (30419, 31454); -- Bound Water Elemental
-UPDATE `creature_template` SET `dmgschool` = 3, `spell_school_immune_mask` = 8 WHERE `entry` IN (30418, 31452); -- Bound Air Elemental
+ -- Bound Fire Elemental
+UPDATE `creature_template` SET `dmgschool` = 2, `spell_school_immune_mask` = 4 WHERE `entry` IN (30416, 31453);
+ -- Bound Water Elemental
+UPDATE `creature_template` SET `dmgschool` = 4, `spell_school_immune_mask` = 16 WHERE `entry` IN (30419, 31454);
+ -- Bound Air Elemental
+UPDATE `creature_template` SET `dmgschool` = 3, `spell_school_immune_mask` = 8 WHERE `entry` IN (30418, 31452);
