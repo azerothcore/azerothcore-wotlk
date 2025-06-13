b2c39062d1b091c604dbf25305aee749de4c0f5d
Jelle Meeus <sogladev@gmail.com>
2025-06-14 00:01:51 +0200

    remove seasonal table

--- data/sql/updates/pending_db_world/rev_1749848424024494127.sql
+++ data/sql/updates/pending_db_world/rev_1749848424024494127.sql
@@ -4,4 +4,3 @@ ALTER TABLE `game_event_gameobject` MODIFY COLUMN `eventEntry` smallint NOT NULL
 ALTER TABLE `game_event_model_equip` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_npc_vendor` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_pool` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
-ALTER TABLE `game_event_seasonal_questrelation` MODIFY COLUMN `eventEntry` smallint DEFAULT 0 NOT NULL COMMENT 'Entry of the game event';
