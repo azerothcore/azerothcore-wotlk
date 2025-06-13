1fa2d3a68dd98b938adfdcc5fdc39fc788e4c92c
Jelle Meeus <sogladev@gmail.com>
2025-06-13 23:05:44 +0200

    fix(DB): set game_event_* tables to use tinyint unsigned

---
+++ data/sql/updates/pending_db_world/rev_1729848424024494127.sql
@@ -0,0 +1,6 @@
+--
+ALTER TABLE `game_event_creature` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
+ALTER TABLE `game_event_gameobject` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
+ALTER TABLE `game_event_model_equip` MODIFY COLUMN `eventEntry` tinyint unsigned NOT NULL COMMENT 'Entry of the game event.';
+ALTER TABLE `game_event_npc_vendor` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event.';
+ALTER TABLE `game_event_pool` MODIFY COLUMN `eventEntry` smallint NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
