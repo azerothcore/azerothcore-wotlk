c55e737fdda996abbfc41acbfb1ef7ba37fae3b2
Ryan Turner <AugustoMendes4426@gmail.com>
2025-06-15 19:32:55 +0100

    fix(DB/Loot) - Archimonde now properly drops the Helms (#22322)
    Co-authored-by: amed80 <8395873+amed80@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1750008344185215400.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `reference_loot_template` SET `Chance` = 0,  `GroupId` = 3 WHERE `Entry` = 1276884;
+
+UPDATE `creature_loot_template` SET `MinCount` = 3, `MaxCount` = 3 WHERE `Entry`= 17968 AND`Item` = 34069 AND `Reference` = 1276884;
