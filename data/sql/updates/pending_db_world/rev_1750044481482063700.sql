b79406631030e9b047f54542bf0aa1509de2156a
avarishd <46330494+avarishd@users.noreply.github.com>
2025-06-16 17:28:05 +0300

    fix(Scripts/Items): Bloodsail Admiral's hat should despawn parrot on unequip

---
+++ data/sql/updates/pending_db_world/rev_1750044481482063700.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id`=60244 AND `ScriptName`='spell_item_bloodsail_admiral_hat';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (60244, 'spell_item_bloodsail_admiral_hat');
