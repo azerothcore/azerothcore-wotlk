7a36d72a9697593de6fcfd6071ddc275cf8aec4f
Jelle Meeus <sogladev@gmail.com>
2025-06-17 19:23:37 +0200

    remove condition for gameevent 17 to be active
    creatures only spawn when the gameevent is active

--- data/sql/updates/pending_db_world/rev_1750179216073004179.sql
+++ data/sql/updates/pending_db_world/rev_1750179216073004179.sql
@@ -10,8 +10,3 @@ INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 (16141, 40110, 40110, 2, 0, 1, 1, 1, 1, 'Reference Loot: Scourge Invasion Event - Item: Haunted Memento'),
 (16298, 40110, 40110, 2, 0, 1, 1, 1, 1, 'Reference Loot: Scourge Invasion Event - Item: Haunted Memento'),
 (16299, 40110, 40110, 2, 0, 1, 1, 1, 1, 'Reference Loot: Scourge Invasion Event - Item: Haunted Memento');
-
--- Reference loot (40110) is only active during the Scourge Invasion Event (17)
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 10 AND `SourceGroup` = 40110 AND `SourceEntry` = 40110 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 12 AND `ConditionTarget` = 0 AND `ConditionValue1` = 17 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(10, 40110, 40110, 0, 0, 12, 0, 17, 0, 0, 0, 0, 0, '', 'Reference Loot: Item: Haunted Memento only drops during the Scourge Invasion Event (17)');
