e699b10d152d7a1a54691eb5017a32b73b5ec09e
LuckyLuc96 <lucsaybibi@gmail.com>
2025-06-20 18:05:54 -0400

    Added a 2 second delay between use of DK's Bone Shield Charges

--- src/server/scripts/Spells/spell_dk.cpp
+++ src/server/scripts/Spells/spell_dk.cpp
@@ -530,11 +530,20 @@ class spell_dk_bone_shield : public AuraScript
 {
     PrepareAuraScript(spell_dk_bone_shield);
 
+    uint32 lastChargeUsedTime = 0;
+    const uint32 ICD_MS = 2000;
+
     void HandleProc(ProcEventInfo& eventInfo)
     {
         PreventDefaultAction();
+        uint32 currentTime = getMSTime();
+        // Checks for 2 seconds between uses of bone shield charges
+        if ((currentTime - lastChargeUsedTime) < ICD_MS)
+            return;
+
         if (!eventInfo.GetSpellInfo() || !eventInfo.GetSpellInfo()->IsTargetingArea())
             DropCharge();
+            lastChargeUsedTime = currentTime;
     }
 
     void Register() override
