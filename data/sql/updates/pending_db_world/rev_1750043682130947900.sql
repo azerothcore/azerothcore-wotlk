1706f902fb8a3442cadcb8d1fdca53a47c299849
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-06-20 11:57:37 -0300

    Update rev_1750043682130947900.sql

--- data/sql/updates/pending_db_world/rev_1750043682130947900.sql
+++ data/sql/updates/pending_db_world/rev_1750043682130947900.sql
@@ -4,10 +4,7 @@ SET @NPC := 29007;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1649.966,-6042.5713,127.57849,0,0,1,0,100,0),
-(@PATH,2,1640.2224,-6045.455,127.940674,0,0,1,0,100,0),
-(@PATH,3,1646.924,-6043.4717,127.70998,0.340187877416610717,0,1,0,100,0),
-(@PATH,4,1646.924,-6043.4717,127.70998,0,0,1,0,100,0);
+(@PATH,1,1649.966,-6042.5713,127.57849,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0000465DB5 .go xyz 1649.966 -6042.5713 127.57849
 
 -- Pathing for  Entry: 29007
@@ -15,10 +12,7 @@ SET @NPC := 29008;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1649.966,-6042.5713,127.57849,0,0,1,0,100,0),
-(@PATH,2,1649.3751,-6042.3633,127.578415,0,0,1,0,100,0),
-(@PATH,3,1649.966,-6042.5713,127.57849,1.017951488494873046,0,1,0,100,0),
-(@PATH,4,1651.0154,-6040.9644,127.57812,0,0,1,0,100,0);
+(@PATH,1,1649.966,-6042.5713,127.57849,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0000465DD2 .go xyz 1649.966 -6042.5713 127.57849
 
 -- Pathing for  Entry: 29007
@@ -26,12 +20,7 @@ SET @NPC := 29009;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1649.966,-6042.5713,127.57849,0,0,1,0,100,0),
-(@PATH,2,1649.2599,-6042.222,127.57838,0,0,1,0,100,0),
-(@PATH,3,1649.966,-6042.5713,127.57849,0.980158209800720214,0,1,0,100,0),
-(@PATH,4,1650.5848,-6039.357,127.57965,0,0,1,0,100,0),
-(@PATH,5,1650.5848,-6039.357,127.57965,0.540810346603393554,0,1,0,100,0),
-(@PATH,6,1650.5848,-6039.357,127.57965,0,0,1,0,100,0);
+(@PATH,1,1649.966,-6042.5713,127.57849,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0000465DE7 .go xyz 1649.966 -6042.5713 127.57849
 
 -- Pathing for  Entry: 29007
@@ -39,12 +28,7 @@ SET @NPC := 29010;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1640.7407,-6032.02,134.73505,0,0,1,0,100,0),
-(@PATH,2,1640.7407,-6032.02,134.73505,3.588693141937255859,0,1,0,100,0),
-(@PATH,3,1645.748,-6044.1543,127.57857,0,0,1,0,100,0),
-(@PATH,4,1640.4349,-6031.841,134.73505,0,0,1,0,100,0),
-(@PATH,5,1638.4993,-6031.8945,134.73506,0,0,1,0,100,0),
-(@PATH,6,1644.2454,-6044.0107,127.83961,0,0,1,0,100,0);
+(@PATH,1,1640.7407,-6032.02,134.73505,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0000C65DB5 .go xyz 1640.7407 -6032.02 134.73505
 
 -- Pathing for  Entry: 29007
@@ -52,11 +36,7 @@ SET @NPC := 29011;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1640.7958,-6030.307,134.73505,0,0,1,0,100,0),
-(@PATH,2,1640.7958,-6030.307,134.73505,2.698258876800537109,0,1,0,100,0),
-(@PATH,3,1641.0571,-6044.1777,128.49342,0,0,1,0,100,0),
-(@PATH,4,1641.8213,-6034.916,135.17213,0,0,1,0,100,0),
-(@PATH,5,1641.0426,-6043.4053,128.95442,0.438137441873550415,0,1,0,100,0);
+(@PATH,1,1640.7958,-6030.307,134.73505,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0000C65DD2 .go xyz 1640.7958 -6030.307 134.73505
 
 -- Pathing for  Entry: 29007
@@ -64,11 +44,7 @@ SET @NPC := 29012;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1646.2389,-6032.526,134.73506,0,0,1,0,100,0),
-(@PATH,2,1640.494,-6039.912,130.66493,0,0,1,0,100,0),
-(@PATH,3,1642.1074,-6045.5205,127.578636,0,0,1,0,100,0),
-(@PATH,4,1654.6193,-6049.721,128.34444,0,0,1,0,100,0),
-(@PATH,5,1642.1074,-6045.5205,127.578636,0.609118878841400146,0,1,0,100,0);
+(@PATH,1,1646.2389,-6032.526,134.73506,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0000C65DE7 .go xyz 1646.2389 -6032.526 134.73506
 
 -- Pathing for  Entry: 29007
@@ -76,13 +52,7 @@ SET @NPC := 29013;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1640.6724,-6032.0527,134.73506,0,0,1,0,100,0),
-(@PATH,2,1640.0288,-6043.2466,128.68744,0,0,1,0,100,0),
-(@PATH,3,1640.0288,-6043.2466,128.68744,4.654959678649902343,0,1,0,100,0),
-(@PATH,4,1640.0394,-6029.179,134.73505,0,0,1,0,100,0),
-(@PATH,5,1641.0131,-6043.469,128.91457,0.118018209934234619,0,1,0,100,0),
-(@PATH,6,1650.1416,-6039.869,127.57904,0,0,1,0,100,0),
-(@PATH,7,1648.7196,-6040.43,127.57797,0,0,1,0,100,0);
+(@PATH,1,1640.6724,-6032.0527,134.73506,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0001465DB5 .go xyz 1640.6724 -6032.0527 134.73506
 
 -- Pathing for  Entry: 29007
@@ -90,11 +60,7 @@ SET @NPC := 29014;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1638.7998,-6036.4976,132.57643,0,0,1,0,100,0),
-(@PATH,2,1640.2987,-6038.744,131.33873,0,0,1,0,100,0),
-(@PATH,3,1640.8878,-6045.9785,127.94292,0,0,1,0,100,0),
-(@PATH,4,1654.1608,-6045.006,127.579254,0,0,1,0,100,0),
-(@PATH,5,1640.8878,-6045.9785,127.94292,0.595259606838226318,0,1,0,100,0);
+(@PATH,1,1638.7998,-6036.4976,132.57643,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0001465DD2 .go xyz 1638.7998 -6036.4976 132.57643
 
 -- Pathing for  Entry: 29007
@@ -102,11 +68,7 @@ SET @NPC := 29015;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1638.2631,-6030.1514,134.73505,0,0,1,0,100,0),
-(@PATH,2,1640.494,-6039.912,130.66493,0,0,1,0,100,0),
-(@PATH,3,1642.1074,-6045.5205,127.578636,0,0,1,0,100,0),
-(@PATH,4,1652.1564,-6043.159,127.57875,0,0,1,0,100,0),
-(@PATH,5,1641.5913,-6045.13,127.908615,0.564118027687072753,0,1,0,100,0);
+(@PATH,1,1638.2631,-6030.1514,134.73505,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0001465DE7 .go xyz 1638.2631 -6030.1514 134.73505
 
 -- Pathing for  Entry: 29007
@@ -114,20 +76,17 @@ SET @NPC := 29016;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1642.4694,-6029.949,134.73505,0,0,1,0,100,0),
-(@PATH,2,1640.494,-6039.912,130.66493,0,0,1,0,100,0),
-(@PATH,3,1642.1074,-6045.5205,127.578636,0,0,1,0,100,0),
-(@PATH,4,1638.5334,-6047.774,127.94239,0,0,1,0,100,0),
-(@PATH,5,1641.9312,-6045.387,127.691315,0.593597829341888427,0,1,0,100,0);
+(@PATH,1,1642.4694,-6029.949,134.73505,0,0,1,0,100,0);
 -- 0x202F2C4C201C53C000084E0001C65DE7 .go xyz 1642.4694 -6029.949 134.73505
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 29007;
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29007) AND (`source_type` = 0) AND (`id` IN (1, 2, 3));
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29007) AND (`source_type` = 0) AND (`id` IN (1, 2, 3, 4));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (29007, 0, 1, 0, 0, 0, 100, 0, 1000, 4000, 4000, 6000, 0, 0, 11, 15498, 64, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Crimson Acolyte - In Combat - Cast \'Holy Smite\''),
 (29007, 0, 2, 0, 0, 0, 100, 0, 6000, 9000, 25000, 30000, 0, 0, 11, 19725, 64, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Crimson Acolyte - In Combat - Cast \'Turn Undead\''),
-(29007, 0, 3, 0, 109, 0, 100, 0, 0, 0, 0, 0, 0, 0, 19, 256, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Crimson Acolyte - On Path 0 Finished - Remove Flags Immune To Players');
+(29007, 0, 3, 4, 109, 0, 100, 0, 0, 0, 0, 0, 0, 0, 19, 256, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Crimson Acolyte - On Path 0 Finished - Remove Flags Immune To Players'),
+(29007, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 9, 0, 0, 50, 0, 0, 0, 0, 0, 'Crimson Acolyte - On Path 0 Finished - Set In Combat With Zone');
 
 UPDATE `creature_template` SET `unit_flags` = `unit_flags`|256 WHERE `entry` = 29007;
 
@@ -136,6 +95,4 @@ SET @NPC := 29001;
 SET @PATH := @NPC * 10;
 DELETE FROM `waypoint_data` WHERE `id`=@PATH;
 INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`orientation`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
-(@PATH,1,1648.3103,-6043.6396,127.57861,0,0,1,0,100,0),
-(@PATH,2,1646.5052,-6041.8794,127.57814,0,0,1,0,100,0),
-(@PATH,3,1648.3103,-6043.6396,127.57861,0.892392218112945556,0,1,0,100,0);
+(@PATH,1,1648.3103,-6043.6396,127.57861,0,0,1,0,100,0);
