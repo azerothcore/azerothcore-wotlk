8f6c24dbd62c8c4e8874bc9694f61b5f7f84479a
LuckyLuc96 <lucsaybibi@gmail.com>
2025-06-21 19:06:49 -0400

    Removed superfluous file.

--- data/sql/updates/pending_db_world/rev_1750460435239275931.sql
+++
@@ -1,3 +0,0 @@
--- Adds a spell into server to track internal cooldown of DK's Bone Shield charges
-DELETE from `spell_dbc` WHERE `id` = 49221;
-INSERT INTO `spell_dbc` SET `id` = 49221, `Attributes` = 0x100, `DurationIndex` = 21, `Name_Lang_enUS` = 'Bone Shield ICD', `SchoolMask` = 1;
