934d257bbc310eeace87dc91ddb1aaca4aa6a391
LuckyLuc96 <lucsaybibi@gmail.com>
2025-06-21 22:01:53 -0400

    Fixed missing curly braces.

--- src/server/scripts/Spells/spell_dk.cpp
+++ src/server/scripts/Spells/spell_dk.cpp
@@ -542,8 +542,10 @@ class spell_dk_bone_shield : public AuraScript
             return;
 
         if (!eventInfo.GetSpellInfo() || !eventInfo.GetSpellInfo()->IsTargetingArea())
+            {
             DropCharge();
             lastChargeUsedTime = currentTime;
+            }
     }
 
     void Register() override
