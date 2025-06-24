99af7bc024ad70b7e27de2de56257a5c325a67c8
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2025-06-24 23:01:34 +0200

    Create command.sql

---
+++ data/sql/updates/pending_db_world/command.sql
@@ -0,0 +1,1 @@
+UPDATE `command` SET `help`='Syntax: .account create $account $password $email\r\n\r\nCreate account and set password to it.\r\n$email is optional, can be left blank.' WHERE `name`='account create';
