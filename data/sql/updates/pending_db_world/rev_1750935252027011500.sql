87ba078d857c95afbfc7faeec0a3bffb6f4cf3e0
FlyingArowana <AugustoMendes4426@gmail.com>
2025-06-26 12:16:32 +0100

    Tested and corrected

--- data/sql/updates/pending_db_world/rev_1750935252027011500.sql
+++ data/sql/updates/pending_db_world/rev_1750935252027011500.sql
@@ -1,9 +1,9 @@
 -- The Aldor
--- Venom Sac: From Hatred to Unfriendly (-1/0), 1 point from Neutral
-UPDATE `quest_template_addon` SET `RequiredMinRepValue` = -42000, `RequiredMaxRepValue` = -1 WHERE `id` IN (10017, 10019);
+-- Venom Sac: From Hatred to Neutral
+UPDATE `quest_template_addon` SET `RequiredMinRepValue` = -42000, `RequiredMaxRepValue` = 0 WHERE `id` IN (10017, 10019);
 
--- Mark of Kil'jaden: From Neutral to Friendly (8999/9000), 1 point from Honour
-UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 8999 WHERE `id` IN (10325, 10326, 10327);
+-- Mark of Kil'jaden: From Neutral to Honoured
+UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 9000 WHERE `id` IN (10325, 10326, 10327);
 
 -- Mark of Sargeras: From Neutral to Exalted
 UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 0 WHERE `id` IN (10653, 10654, 10655, 10826, 10827, 10828);
@@ -12,11 +12,11 @@ UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValu
 UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 0 WHERE `id` IN (10420, 10421);
 
 -- The Scryers
--- Basilisk Eye: From Hatred to Unfriendly (-1/0), 1 point from Neutral
-UPDATE `quest_template_addon` SET `RequiredMinRepValue` = -42000, `RequiredMaxRepValue` = -1 WHERE `id` IN (10024, 10025);
+-- Basilisk Eye: From Hatred to Neutral
+UPDATE `quest_template_addon` SET `RequiredMinRepValue` = -42000, `RequiredMaxRepValue` = 0 WHERE `id` IN (10024, 10025);
 
--- Firewing Signet: From Neutral to Friendly (8999/9000), 1 point from Honour
-UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 8999 WHERE `id` IN (10412, 10415, 10414);
+-- Firewing Signet: From Neutral to Honoured
+UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 9000 WHERE `id` IN (10412, 10415, 10414);
 
 -- Sunfury Signet: From Neutral to Exalted
 UPDATE `quest_template_addon` SET `RequiredMinRepValue` = 0, `RequiredMaxRepValue` = 0 WHERE `id` IN (10656, 10658, 10659, 10822, 10822, 10823, 10824);
