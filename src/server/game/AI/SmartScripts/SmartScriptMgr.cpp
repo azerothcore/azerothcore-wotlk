a4cecf7da2a6487ba91771f29b6022173cee744f
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-06-27 12:46:02 +0200

    Update SmartScriptMgr.cpp

--- src/server/game/AI/SmartScripts/SmartScriptMgr.cpp
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.cpp
@@ -1545,7 +1545,7 @@ bool SmartAIMgr::IsEventValid(SmartScriptHolder& e)
             }
             if (e.action.startClosestWaypoint.repeat > 1 || e.action.startClosestWaypoint.run > 1)
             {
-                LOG_ERROR("sql.sql", "SmartAIMgr: Entry {} SourceType {} Event {} Action {} has invalid repeat ({}) or run ({}) parameter, must be 0 or 1.",
+                LOG_ERROR("sql.sql", "SmartAIMgr: Entry {} SourceType {} Event {} Action {} has invalid run ({}) or repeat ({}) parameter, must be 0 or 1.",
                     e.entryOrGuid, e.GetScriptType(), e.event_id, e.GetActionType(),
                     e.action.startClosestWaypoint.repeat, e.action.startClosestWaypoint.run);
                 return false;
