f57ec0e866766ba09bb0ea75f0b5fb3a2f46d218
Jelle Meeus <sogladev@gmail.com>
2025-06-29 00:12:11 +0200

    add script to reset cooldown spell of nearby players

---
+++ data/sql/updates/pending_db_world/rev_1751148569961282780.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = 29313 AND `ScriptName` = 'spell_gen_cooldown_all';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (29313 , 'spell_gen_cooldown_all');
