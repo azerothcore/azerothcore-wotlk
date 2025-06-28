7d16278b05d009ebed8019fe1f01bb51cfa96c7f
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2025-06-28 22:36:19 +0200

    feat(Scripts/Commands): Add guild ranks display to guild info command
    Updates the guild info command to query and list all guild ranks, showing their IDs and names to the user.
    
    * closes https://github.com/azerothcore/azerothcore-wotlk/issues/22184

--- src/server/scripts/Commands/cs_guild.cpp
+++ src/server/scripts/Commands/cs_guild.cpp
@@ -249,6 +249,20 @@ public:
         handler->PSendSysMessage(LANG_GUILD_INFO_BANK_GOLD, guild->GetTotalBankMoney() / 100 / 100); // Bank Gold (in gold coins)
         handler->PSendSysMessage(LANG_GUILD_INFO_MOTD, guild->GetMOTD()); // Message of the Day
         handler->PSendSysMessage(LANG_GUILD_INFO_EXTRA_INFO, guild->GetInfo()); // Extra Information
+
+        QueryResult result = CharacterDatabase.Query("SELECT rid, rname FROM guild_rank WHERE guildid = {}", guild->GetId());
+        if (result)
+        {
+            handler->PSendSysMessage(LANG_GUILD_INFO_RANKS);
+            do
+            {
+                Field* fields = result->Fetch();
+                uint32 rid = fields[0].Get<uint32>();
+                std::string rname = fields[1].Get<std::string>();
+
+                handler->PSendSysMessage(LANG_GUILD_INFO_RANKS_LIST, rid, rname);
+            } while (result->NextRow());
+        }
         return true;
     }
 };
