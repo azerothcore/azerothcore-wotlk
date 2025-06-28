daf2a9ed3a35a3b62781d2ade277a316d6063c63
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2025-06-28 23:03:16 +0200

    Update rev_1751142733581192500.sql

--- data/sql/updates/pending_db_world/rev_1751142733581192500.sql
+++ data/sql/updates/pending_db_world/rev_1751142733581192500.sql
@@ -1,5 +1,5 @@
 --
 DELETE FROM `acore_string` WHERE `entry` IN (1184,1185);
 INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
-(1184, '| Guild Ranks:')
+(1184, '| Guild Ranks:'),
 (1185, '| {} - {}');
