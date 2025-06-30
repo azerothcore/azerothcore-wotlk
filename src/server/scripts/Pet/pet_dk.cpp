78f5fd8fc3f37dc4aedcaf86af05c25e305a29a3
Jelle Meeus <sogladev@gmail.com>
2025-06-30 14:00:55 +0200

    fix DamageCalc doing nothing

--- src/server/scripts/Pet/pet_dk.cpp
+++ src/server/scripts/Pet/pet_dk.cpp
@@ -358,7 +358,7 @@ class spell_pet_dk_gargoyle_strike : public SpellScript
 
     void Register() override
     {
-        OnEffectHitTarget += SpellEffectFn(spell_pet_dk_gargoyle_strike::HandleDamageCalc, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
+        OnEffectLaunchTarget += SpellEffectFn(spell_pet_dk_gargoyle_strike::HandleDamageCalc, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
     }
 };
 
