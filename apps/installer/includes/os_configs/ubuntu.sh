e1b2689c3a2b1395323d2fc58588b1e1b3c07c53
Yehonal <yehonal.azeroth@gmail.com>
2025-07-01 15:35:54 +0200

    feat(bash): startup-scripts reworked + bash scripts workflow  integration (#22401)

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -2,8 +2,11 @@
 
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Set SUDO variable - one liner
+SUDO=$([ "$EUID" -ne 0 ] && echo "sudo" || echo "")
+
 if ! command -v lsb_release &>/dev/null ; then
-    sudo apt-get install -y lsb-release
+    $SUDO apt-get install -y lsb-release
 fi
 
 UBUNTU_VERSION=$(lsb_release -sr);
@@ -23,28 +26,29 @@ case $UBUNTU_VERSION in
     ;;
 esac
 
-sudo apt update
+$SUDO apt update
 
 # shared deps
-sudo DEBIAN_FRONTEND="noninteractive" \
-apt-get -y install ccache clang cmake curl google-perftools libmysqlclient-dev make unzip
-
-if [[ $CONTINUOUS_INTEGRATION || $DOCKER ]]; then
-  # TODO: update CI / Docker section for Ubuntu 22.04+
-  sudo add-apt-repository -y ppa:mhier/libboost-latest && sudo apt update && sudo apt-get -y install build-essential cmake-data  \
-  libboost1.74-dev libbz2-dev libncurses5-dev libmysql++-dev libgoogle-perftools-dev libreadline6-dev libssl-dev libtool \
-  openssl zlib1g-dev
-else
-  sudo DEBIAN_FRONTEND="noninteractive" \
-  apt-get install -y g++ gdb gdbserver gcc git \
-  libboost-all-dev libbz2-dev libncurses-dev libreadline-dev \
-  libssl-dev
+DEBIAN_FRONTEND="noninteractive" $SUDO \
+apt-get -y install ccache clang cmake curl google-perftools libmysqlclient-dev make unzip jq screen tmux \
+  libreadline-dev libncurses5-dev libncursesw5-dev libbz2-dev git gcc g++ libssl-dev \
+  libncurses-dev libboost-all-dev gdb gdbserver
 
   VAR_PATH="$CURRENT_PATH/../../../../var"
 
+
+# Do not install MySQL if we are in docker (It will be used a docker container instead) or we are explicitly skipping it.
+if [[ $DOCKER != 1 && $SKIP_MYSQL_INSTALL != 1 ]]; then
   # run noninteractive install for MYSQL 8.4 LTS
   wget https://dev.mysql.com/get/mysql-apt-config_0.8.32-1_all.deb -P "$VAR_PATH"
-  sudo DEBIAN_FRONTEND="noninteractive" dpkg -i "$VAR_PATH/mysql-apt-config_0.8.32-1_all.deb"
-  sudo apt-get update
-  sudo DEBIAN_FRONTEND="noninteractive" apt-get install -y mysql-server
+  DEBIAN_FRONTEND="noninteractive" $SUDO dpkg -i "$VAR_PATH/mysql-apt-config_0.8.32-1_all.deb"
+  $SUDO apt-get update
+  DEBIAN_FRONTEND="noninteractive" $SUDO apt-get install -y mysql-server
 fi
+
+
+if [[ $CONTINUOUS_INTEGRATION ]]; then
+  $SUDO systemctl enable mysql.service
+  $SUDO systemctl start mysql.service
+fi
+
