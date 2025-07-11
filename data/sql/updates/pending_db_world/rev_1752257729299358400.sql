192d066447fd19e728d54a84239861229b64c978
FlyingArowana <AugustoMendes4426@gmail.com>
2025-07-11 19:21:20 +0100

    Create rev_1752257729299358400.sql

---
+++ data/sql/updates/pending_db_world/rev_1752257729299358400.sql
@@ -0,0 +1,4 @@
+-- Only allows HORDE players to pick Necklace Recovery quest (and not be able to share with alliance)
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 19) AND (`SourceGroup` = 0) AND (`SourceEntry` = 2283) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 16) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 690) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(19, 0, 2283, 0, 0, 16, 0, 690, 0, 0, 0, 0, 0, '', 'Only HORDE faction pick up quest "Necklace Recovery"');
