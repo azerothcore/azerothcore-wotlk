56f9f567fa987060de38583febf721aecfd852e9
FlyingArowana <AugustoMendes4426@gmail.com>
2025-07-11 12:25:32 +0100

    deleted test file

--- data/sql/updates/pending_db_world/rev_1752232806138408300.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `world_state`;
-CREATE TABLE IF NOT EXISTS `world_state` (
-   `Id` INT UNSIGNED NOT NULL COMMENT 'Internal save ID',
-   `Data` longtext,
-   PRIMARY KEY(`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='WorldState save system';
