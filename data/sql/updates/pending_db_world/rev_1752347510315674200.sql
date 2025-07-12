29b47ccbfbc928c51914fa6312115610f0e90074
FlyingArowana <AugustoMendes4426@gmail.com>
2025-07-12 20:14:28 +0100

    Test

---
+++ data/sql/updates/pending_db_world/rev_1752347510315674200.sql
@@ -0,0 +1,5 @@
+--
+DELETE         FROM `creature` WHERE   `ID` = 100;
+-- comment
+INSERT      INTO `creature` (`guid`, `id1`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `VerifiedBuild`, `Comment`) VALUES
+(100, 16434, 0, 1, 1, -8834.5302734375, 643.0269775390625, 95.25319671630861, 4.328420162200928, 120, 0.0, 0, 0, '');
