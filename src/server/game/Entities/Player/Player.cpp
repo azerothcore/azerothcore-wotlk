a249f7e3fba9a8c43e3b949507dbe12ea4b06fe5
Luckyluc96 <Christian-McCrea@outlook.com>
2025-07-21 17:23:37 -0400

    Changes as recommended by Tereneckla

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -3103,7 +3103,7 @@ bool Player::addSpell(uint32 spellId, uint8 addSpecMask, bool updateActive, bool
 
 bool Player::CheckSkillLearnedBySpell(uint32 spellId)
 {
-    if (sConfigMgr->GetOption<bool>("ValidateSkillLearnedBySpells", true))
+    if (!sConfigMgr->GetOption<bool>("ValidateSkillLearnedBySpells", true))
         return true;
 
     SkillLineAbilityMapBounds skill_bounds = sSpellMgr->GetSkillLineAbilityMapBounds(spellId);
