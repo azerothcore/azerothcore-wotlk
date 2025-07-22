ae81ce2a24ed7f33096d7e196576510ea104c2f3
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-07-22 18:12:24 +0200

    fix (DB/Creature) Edit Geirrvif's position. (#22531)

---
+++ data/sql/updates/pending_db_world/Geirrvif.sql
@@ -0,0 +1,3 @@
+
+-- Update position, orientation and Verified Build (Sniffed).
+UPDATE `creature` SET `position_x` = 8216.233, `position_y` = 3516.222, `position_z` = 630.9635, `orientation` = 3.83972, `VerifiedBuild` = 61967 WHERE (`id1` = 31135) AND (`guid` = 123494);
