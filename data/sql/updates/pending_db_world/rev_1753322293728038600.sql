92c13647e030405e621df61083e3724ec5a18f09
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-07-23 22:59:44 -0300

    fix(DB/Creature): Update Amanitar spawn position
    Adjusts the position and orientation values for creature with id1=30258 in the database to reflect new coordinates and orientation. Also updates the VerifiedBuild field.

---
+++ data/sql/updates/pending_db_world/rev_1753322293728038600.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `creature` SET `position_x` = 340.172637939453125, `position_y` = -878.5924072265625, `position_z` = -74.8638458251953125, `orientation` =  0.331612557172775268, `VerifiedBuild` = 61967 WHERE `id1` = 30258;
