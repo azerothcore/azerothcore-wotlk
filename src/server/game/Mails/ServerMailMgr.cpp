2be98e965392f4133329515d89279e67365a6370
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2025-07-27 12:03:48 +0200

    Update ServerMailMgr.cpp

--- src/server/game/Mails/ServerMailMgr.cpp
+++ src/server/game/Mails/ServerMailMgr.cpp
@@ -354,7 +354,7 @@ bool ServerMailCondition::CheckCondition(Player* player) const
     case ServerMailConditionType::Class:
         return (player->getClassMask() & value) != 0;
     case ServerMailConditionType::AccountFlags:
-        return (player->GetSession()->HasAccountFlag(value)) != 0;
+        return player->GetSession()->HasAccountFlag(value);
     default:
         [[unlikely]] LOG_ERROR("server.mail", "Unknown server mail condition type '{}'", static_cast<uint32>(type));
         return false;
