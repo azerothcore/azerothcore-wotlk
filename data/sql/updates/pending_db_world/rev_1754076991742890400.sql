318d404c7e0ba1c49c5ddc7e13ca672825d42ba2
Ludwig <sudlud@users.noreply.github.com>
2025-08-01 23:00:03 +0200

    init

---
+++ data/sql/updates/pending_db_world/rev_1754076991742890400.sql
@@ -0,0 +1,34 @@
+-- queststarters / questenders
+UPDATE `creature_template` SET `npcflag` = `npcflag` | 2 WHERE (`entry` IN (16484, 16493, 16495));
+
+DELETE FROM `creature_queststarter` WHERE (`quest` IN (9261, 9262)) AND (`id` IN (16484, 16493, 16495));
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(16484, 9261), -- Lieutenant Nevell
+(16495, 9262), -- Lieutenant Beitha
+(16493, 9263); -- Lieutenant Dagel
+
+DELETE FROM `creature_questender` WHERE (`quest` IN (9261, 9262)) AND (`id` IN (16484, 16493, 16495));
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(16484, 9261), -- Lieutenant Nevell
+(16495, 9262), -- Lieutenant Beitha
+(16493, 9263); -- Lieutenant Dagel
+
+-- areatrigger
+DELETE FROM areatrigger_involvedrelation WHERE id IN (4105);
+INSERT INTO areatrigger_involvedrelation (id, quest) VALUES
+(4105, 9262);
+
+-- 9261 Investigate the Scourge of Ironforge
+-- missing circle and mobs in front of town
+-- missing areatrigger link
+
+-- 9262 Investigate the Scourge of Darnassus
+-- done
+
+-- 9263 Investigate the Scourge of Orgrimmar
+-- wip
+
+-- 12817 Investigate the Scourge of Exodar
+-- missing circle and mobs in front of town
+-- missing areatrigger link
+
