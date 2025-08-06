01c8dc47e9693f9d31d059beba25458034d3b5b9
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-08-06 21:53:07 +0200

    Update Icemist_village.sql

--- data/sql/updates/pending_db_world/Icemist_village.sql
+++ data/sql/updates/pending_db_world/Icemist_village.sql
@@ -22,6 +22,6 @@ DELETE FROM `smart_scripts` WHERE (`entryorguid` = 26769) AND (`source_type` = 0
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 26770) AND (`source_type` = 0) AND (`id` IN (11));
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 26771) AND (`source_type` = 0) AND (`id` IN (2));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(26769, 0, 12, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Anok\\'ra the Manipulator - On Just Died - Say Line 0'),
+(26769, 0, 12, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Anok\'ra the Manipulator - On Just Died - Say Line 0'),
 (26770, 0, 11, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Tivax the Breaker - On Just Died - Say Line 0'),
 (26771, 0, 2, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Sinok the Shadowrager - On Just Died - Say Line 0');
