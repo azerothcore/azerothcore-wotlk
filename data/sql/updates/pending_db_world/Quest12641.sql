9d9dc3b111f79c54ce96df9b5ea4c97fc9720e19
天鹭 <18535853+PkllonG@users.noreply.github.com>
2025-08-07 01:36:51 +0800

    Update Quest12641.sql

--- data/sql/updates/pending_db_world/Quest12641.sql
+++ data/sql/updates/pending_db_world/Quest12641.sql
@@ -1,2 +1,2 @@
 -- 
-DELETE FROM `smart_scripts` WHERE `source_type` = 9 AND `entryorguid` = 2851100 AND `id` IN (3,5);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 2851100 AND `id` IN (3,5));
