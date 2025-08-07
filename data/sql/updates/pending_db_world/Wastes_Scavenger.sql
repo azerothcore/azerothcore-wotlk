0860d1d0640c6cd8ba2344e58e842fc80a1df239
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-08-07 11:29:18 +0200

    Create Wastes_Scavenger.sql

---
+++ data/sql/updates/pending_db_world/Wastes_Scavenger.sql
@@ -0,0 +1,5 @@
+
+-- Init creature_template_addon (Wastes Scavenger)
+DELETE FROM `creature_template_addon` WHERE (`entry` = 28005);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(28005, 0, 0, 0, 0, 438, 0, '');
