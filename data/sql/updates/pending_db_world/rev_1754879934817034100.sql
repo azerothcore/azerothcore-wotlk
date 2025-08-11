9b9f0521db8789c31512f4e3ea4fb2f6a03db43c
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2025-08-11 04:41:22 +0200

    fix(DB) kill credit should be 1

---
+++ data/sql/updates/pending_db_world/rev_1754879934817034100.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29978) AND (`source_type` = 0);
+DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29984) AND (`source_type` = 0) AND (`id` IN (0));
