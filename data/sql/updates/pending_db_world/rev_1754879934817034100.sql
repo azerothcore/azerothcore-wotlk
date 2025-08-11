f9d5467a0464221993cbd93eef45b45dd5efba01
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2025-08-11 10:41:30 +0200

    correct entry

--- data/sql/updates/pending_db_world/rev_1754879934817034100.sql
+++ data/sql/updates/pending_db_world/rev_1754879934817034100.sql
@@ -1,5 +1,5 @@
 --
-UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 29984;
+UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 29978;
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29984) AND (`source_type` = 0) AND (`id` = 0);
 
