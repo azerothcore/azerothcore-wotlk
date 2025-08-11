f66e84aca8dee682e8b91503a17480662ddc9868
sudlud <sudlud@users.noreply.github.com>
2025-08-11 20:35:34 +0200

    Update rev_1754911204417603800.sql

--- data/sql/updates/pending_db_world/rev_1754911204417603800.sql
+++ data/sql/updates/pending_db_world/rev_1754911204417603800.sql
@@ -1,6 +1,6 @@
 -- Anub'ar Guardian - Sunder Armor, Sunder Armor(H)
 DELETE FROM `spell_custom_attr` WHERE `spell_id` IN (53618, 59350);
 
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (53618, 4194304);
-INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (59350, 4194304);
-
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
+(53618, 4194304),
+(59350, 4194304);
