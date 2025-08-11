4a9a31432bda2b9cb29aa2a03fcce4a388983620
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2025-08-11 16:19:38 +0200

    style idk

--- data/sql/updates/pending_db_world/rev_1754910707047949800.sql
+++ data/sql/updates/pending_db_world/rev_1754910707047949800.sql
@@ -1,8 +1,9 @@
---
--- Anub'ar Venomancer - Poison Bolt, Poison Bolt(H)
-DELETE FROM `spell_custom_attr` WHERE `spell_id` IN (53617, 59359);
-
+-- Anub'ar Venomancer - Poison Bolt
+DELETE FROM `spell_custom_attr` WHERE `spell_id` = (53617);
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (53617, 4194304);
+
+-- Anub'ar Venomancer - Poison Bolt(H)
+DELETE FROM `spell_custom_attr` WHERE `spell_id` = (59359);
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES (59359, 4194304);
 
 
