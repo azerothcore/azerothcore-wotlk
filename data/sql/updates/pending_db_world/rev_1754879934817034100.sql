591d74a64332b5a819b4858a17230e2e527b46c2
sudlud <sudlud@users.noreply.github.com>
2025-08-11 20:36:29 +0200

    Update rev_1754879934817034100.sql

--- data/sql/updates/pending_db_world/rev_1754879934817034100.sql
+++ data/sql/updates/pending_db_world/rev_1754879934817034100.sql
@@ -4,4 +4,3 @@ UPDATE `creature_template` SET `AIName` = '' WHERE `entry` = 29978;
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29984) AND (`source_type` = 0) AND (`id` = 0);
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29978) AND (`source_type` = 0);
-
