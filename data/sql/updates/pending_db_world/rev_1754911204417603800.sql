5097bf3e6a583a0552b1d1a71be68b700a564b0e
v-mstrs <104088833+v-mstrs@users.noreply.github.com>
2025-08-13 00:09:37 +0200

    fix(DB/Spell): Allow Sunder Armor to stack (#22669)

---
+++ data/sql/updates/pending_db_world/rev_1754911204417603800.sql
@@ -0,0 +1,5 @@
+-- Anub'ar Guardian - Sunder Armor, Sunder Armor(H)
+DELETE FROM `spell_custom_attr` WHERE `spell_id` IN (53618, 59350);
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
+(53618, 4194304),
+(59350, 4194304);
