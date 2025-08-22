98749df72bc389c311a5d2a7b5e2c68d8d8915cd
Ludwig <sudlud@users.noreply.github.com>
2025-08-22 07:03:39 +0200

    fix(CI/mac): fix build

--- apps/ci/mac/ci-compile.sh
+++ apps/ci/mac/ci-compile.sh
@@ -26,6 +26,7 @@ time cmake ../../../ \
 -DBUILD_TESTING=1 \
 -DSCRIPTS=static \
 -DCMAKE_BUILD_TYPE=Release \
+-DBoost_NO_BOOST_CMAKE=ON \
 -DMYSQL_ADD_INCLUDE_PATH=$mysql_include_path \
 -DMYSQL_LIBRARY=$mysql_lib_path \
 -DREADLINE_INCLUDE_DIR=$(brew --prefix readline)/include \
