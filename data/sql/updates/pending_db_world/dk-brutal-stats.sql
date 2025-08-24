ab35685968a4090f2a557937f643a4cc9fa2fb6c
heyitsbench <benjymansy123@gmail.com>
2025-08-24 13:32:30 -0400

    Init.

---
+++ data/sql/updates/pending_db_world/dk-brutal-stats.sql
@@ -0,0 +1,5 @@
+UPDATE `item_template` SET `armor` = 1983, `MaxDurability` = 165 WHERE `entry` = 40440; -- Chestpiece
+UPDATE `item_template` SET `armor` = 1239, `MaxDurability` = 55  WHERE `entry` = 40441; -- Gauntlets
+UPDATE `item_template` SET `armor` = 1611, `MaxDurability` = 100 WHERE `entry` = 40442; -- Helm
+UPDATE `item_template` SET `armor` = 1735, `MaxDurability` = 120 WHERE `entry` = 40443; -- Legguards
+UPDATE `item_template` SET `armor` = 1487, `MaxDurability` = 100 WHERE `entry` = 40444; -- Shoulders
