51a88309238462b412da9ddcafb01a92ee95f3fd
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2025-08-26 23:02:04 -0400

    fix(DB/Item): Add some stat values to Death Knight Brutal Gladiator set. (#22725)

---
+++ data/sql/updates/pending_db_world/dk-brutal-stats.sql
@@ -0,0 +1,5 @@
+UPDATE `item_template` SET `armor` = 1983, `MaxDurability` = 165 WHERE `entry` = 40440;
+UPDATE `item_template` SET `armor` = 1239, `MaxDurability` = 55  WHERE `entry` = 40441;
+UPDATE `item_template` SET `armor` = 1611, `MaxDurability` = 100 WHERE `entry` = 40442;
+UPDATE `item_template` SET `armor` = 1735, `MaxDurability` = 120 WHERE `entry` = 40443;
+UPDATE `item_template` SET `armor` = 1487, `MaxDurability` = 100 WHERE `entry` = 40444;
