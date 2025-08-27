22d0915baad33826188c501435d3e42ca1ed6c9e
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2025-08-26 21:00:26 -0400

    Make workflow happy.

--- data/sql/updates/pending_db_world/dk-brutal-stats.sql
+++ data/sql/updates/pending_db_world/dk-brutal-stats.sql
@@ -1,5 +1,5 @@
-UPDATE `item_template` SET `armor` = 1983, `MaxDurability` = 165 WHERE `entry` = 40440; -- Chestpiece
-UPDATE `item_template` SET `armor` = 1239, `MaxDurability` = 55  WHERE `entry` = 40441; -- Gauntlets
-UPDATE `item_template` SET `armor` = 1611, `MaxDurability` = 100 WHERE `entry` = 40442; -- Helm
-UPDATE `item_template` SET `armor` = 1735, `MaxDurability` = 120 WHERE `entry` = 40443; -- Legguards
-UPDATE `item_template` SET `armor` = 1487, `MaxDurability` = 100 WHERE `entry` = 40444; -- Shoulders
+UPDATE `item_template` SET `armor` = 1983, `MaxDurability` = 165 WHERE `entry` = 40440;
+UPDATE `item_template` SET `armor` = 1239, `MaxDurability` = 55  WHERE `entry` = 40441;
+UPDATE `item_template` SET `armor` = 1611, `MaxDurability` = 100 WHERE `entry` = 40442;
+UPDATE `item_template` SET `armor` = 1735, `MaxDurability` = 120 WHERE `entry` = 40443;
+UPDATE `item_template` SET `armor` = 1487, `MaxDurability` = 100 WHERE `entry` = 40444;
