0def4c16acfd50b38b941b9569aced655fc6ae3f
Yehonal <yehonal.azeroth@gmail.com>
2025-09-02 22:59:59 +0200

    fix(SQL): standardize formatting in petition and item_instance updates for clarity

--- data/sql/updates/pending_db_characters/rev_1756841669142726711.sql
+++ data/sql/updates/pending_db_characters/rev_1756841669142726711.sql
@@ -19,17 +19,17 @@ ALTER TABLE `petition`
 ALTER TABLE `petition_sign`
     ADD COLUMN `petition_id` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `petitionguid`;
 
-UPDATE `petition_sign` ps
-JOIN `petition` p ON p.`petitionguid` = ps.`petitionguid`
-SET ps.`petition_id` = p.`petition_id`
-WHERE ps.`petition_id` = 0;
+UPDATE `petition_sign` `ps`
+JOIN `petition` `p` ON `p`.`petitionguid` = `ps`.`petitionguid`
+SET `ps`.`petition_id` = `p`.`petition_id`
+WHERE `ps`.`petition_id` = 0;
 
 -- Index to support deletes by petition_id
 ALTER TABLE `petition_sign`
     ADD INDEX `idx_petition_id_player` (`petition_id`, `playerguid`);
 
 -- Repair existing charter items: set first enchantment token to petition_id
-UPDATE `item_instance` ii
-JOIN `petition` p ON p.`petitionguid` = ii.`guid`
-SET ii.`enchantments` = CONCAT(p.`petition_id`, SUBSTRING(ii.`enchantments`, LOCATE(' ', ii.`enchantments`)))
-WHERE ii.`enchantments` IS NOT NULL AND ii.`enchantments` <> '';
+UPDATE `item_instance` `ii`
+JOIN `petition` `p` ON `p`.`petitionguid` = `ii`.`guid`
+SET `ii`.`enchantments` = CONCAT(`p`.`petition_id`, SUBSTRING(`ii`.`enchantments`, LOCATE(' ', `ii`.`enchantments`)))
+WHERE `ii`.`enchantments` IS NOT NULL AND `ii`.`enchantments` <> '';
