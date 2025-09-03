8e5aced3701502b21a5dff045ff16f00295b1093
FlyingArowana <AugustoMendes4426@gmail.com>
2025-09-03 01:48:19 +0100

    Update test.sql

--- data/sql/updates/pending_db_world/test.sql
+++ data/sql/updates/pending_db_world/test.sql
@@ -1,5 +1,5 @@
-USE testdb;
+USE testdb
 DELETE FROM creature_template WHERE entry = 1
-INSERT INTO creature_template (entry, name, AIName, ScriptName, dynamicflags, unit_flags, type_flags, float_col, int_col, bool_col, mediumint_col) VALUES (1, 'Test', 'SmartAI', 'my_script', 123, 1, 2, 1.0 UNSIGNED, 1234567890, TRUE, 123);
-UPDATE creature_template SET AIName = 'SmartAI', ScriptName = 'my_script', dynamicflags = 123, unit_flags = 1, type_flags = 2, name = 'Test', float_col = 1.0 UNSIGNED, int_col = 1234567890, bool_col = TRUE, mediumint_col = 123 WHERE entry = 1;;
-CREATE TABLE creature_template (entry int(11), name VARCHAR(255) COLLATE latin1_swedish_ci, bool_col BOOL, mediumint_col MEDIUMINT, float_col FLOAT UNSIGNED, int_col INT(11), PRIMARY KEY (entry)) ENGINE=MyISAM CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+INSERT INTO creature_template (entry, name, AIName, ScriptName, dynamicflags, unit_flags, type_flags) VALUES (1, 'Test', 'SmartAI', 'my_script', 123, 1, 2)
+UPDATE creature_template SET AIName = 'SmartAI', ScriptName = 'my_script', dynamicflags = 123, unit_flags = 1, type_flags = 2, name = 'Test' WHERE entry = 1
+CREATE TABLE creature_template (entry int, name VARCHAR(255), AIName VARCHAR(255), ScriptName VARCHAR(255), dynamicflags INT, unit_flags INT, type_flags INT, PRIMARY KEY (entry)) ENGINE=MyISAM CHARSET=latin1 ROW_FORMAT=COMPRESSED
