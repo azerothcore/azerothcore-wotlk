c462c4ec7f3c288896fc2733fe3018bb9e9e9af0
heyitsbench <benjymansy123@gmail.com>
2025-09-03 11:58:34 -0400

    Use the right table.

--- data/sql/updates/pending_db_world/vurtok-axebreaker.sql
+++ data/sql/updates/pending_db_world/vurtok-axebreaker.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template_model` SET `DisplayID_Other_Gender` = 0 WHERE `DisplayID` IN (16292, 16294);
+UPDATE `creature_model_info` SET `DisplayID_Other_Gender` = 0 WHERE `DisplayID` IN (16292, 16294);
