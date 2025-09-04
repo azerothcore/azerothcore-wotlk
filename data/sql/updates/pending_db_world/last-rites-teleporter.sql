40ef7472f5e20199c4ade05b69e25dc5caecc389
mostlynick3 <nick@nikitas.no>
2025-09-04 13:25:40 +0200

    Update last-rites-teleporter.sql

--- data/sql/updates/pending_db_world/last-rites-teleporter.sql
+++ data/sql/updates/pending_db_world/last-rites-teleporter.sql
@@ -1,4 +1,3 @@
 DELETE FROM areatrigger_scripts WHERE entry = 5338;
 
-INSERT INTO areatrigger_teleport (ID, Name, target_map, target_position_x, target_position_y, target_position_z, target_orientation)
-VALUES (5338, 'Last Rites Top -> Down', 571, 3800, 3585, 49.7, 1.1);
+INSERT INTO areatrigger_teleport (ID, Name, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES (5338, 'Last Rites Top -> Down', 571, 3800, 3585, 49.7, 1.1);
