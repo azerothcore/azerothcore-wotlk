d968b5e1cdfb2887a14dc460fde2328aa29293c6
EricksOliveira <ericksoliveira258@gmail.com>
2025-09-06 00:03:12 -0300

    Fix comment

--- data/sql/updates/pending_db_world/rev_1757127010469220400.sql
+++ data/sql/updates/pending_db_world/rev_1757127010469220400.sql
@@ -1,2 +1,2 @@
--- Fixes Horn of Winter rank chain
-UPDATE `npc_trainer` SET `ReqSpell` = 57330 WHERE `SpellID` = 57623; -- Rank 2 requires Rank 1
+-- Fixes Horn of Winter `Rank` chain
+UPDATE `npc_trainer` SET `ReqSpell` = 57330 WHERE `SpellID` = 57623;
