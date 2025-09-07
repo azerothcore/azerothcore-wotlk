1c2e61fb031c018ccaaa1e36a702be1c0568a88a
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-06 20:11:28 -0500

    Add Blink to Beryl Sorcerer

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -568,6 +568,9 @@ enum BerylSorcerer
 {
     EVENT_FROSTBOLT                                = 1,
     EVENT_ARCANE_CHAINS                            = 2,
+    EVENT_CHECK_HEALTH                             = 3,
+    EVENT_BLINK                                    = 4,
+    EVENT_RESUME_MOVEMENT                          = 5,
     NPC_LIBRARIAN_DONATHAN                         = 25262,
     NPC_CAPTURED_BERLY_SORCERER                    = 25474,
     SPELL_FROSTBOLT                                = 9672,
@@ -575,7 +578,8 @@ enum BerylSorcerer
     SPELL_ARCANE_CHAINS_CHARACTER_FORCE_CAST       = 45625,
     SPELL_ARCANE_CHAINS_SUMMON_CHAINED_MAGE_HUNTER = 45626,
     SPELL_COSMETIC_ENSLAVE_CHAINS_SELF             = 45631,
-    SPELL_ARCANE_CHAINS_CHANNEL_II                 = 45735
+    SPELL_ARCANE_CHAINS_CHANNEL_II                 = 45735,
+    SPELL_BLINK                                    = 50648
 };
 
 class npc_beryl_sorcerer : public CreatureScript
@@ -583,7 +587,7 @@ class npc_beryl_sorcerer : public CreatureScript
 public:
     npc_beryl_sorcerer() : CreatureScript("npc_beryl_sorcerer") { }
 
-struct npc_beryl_sorcererAI : public CreatureAI
+    struct npc_beryl_sorcererAI : public CreatureAI
     {
         npc_beryl_sorcererAI(Creature* creature) : CreatureAI(creature)
         {
@@ -594,6 +598,9 @@ struct npc_beryl_sorcererAI : public CreatureAI
         {
             _playerGUID.Clear();
             _chainsCast = false;
+            _blinkUsed = false;
+            _movementPaused = false;
+            _frostboltFinished = false;
         }
 
         void Reset() override
@@ -610,6 +617,7 @@ struct npc_beryl_sorcererAI : public CreatureAI
             }
 
             _events.ScheduleEvent(EVENT_FROSTBOLT, 3000, 4000);
+            _events.ScheduleEvent(EVENT_CHECK_HEALTH, 1000);
         }
 
         void SpellHit(Unit* unit, SpellInfo const* spell) override
@@ -625,6 +633,19 @@ struct npc_beryl_sorcererAI : public CreatureAI
             }
         }
 
+        void OnSpellCastFinished(SpellInfo const* spell, SpellFinishReason reason) override
+        {
+            if (spell->Id == SPELL_FROSTBOLT && reason == SPELL_FINISHED_SUCCESSFUL_CAST)
+            {
+                if (me->HealthBelowPct(50) && !_blinkUsed && !_frostboltFinished)
+                {
+                    _frostboltFinished = true;
+                    _events.CancelEvent(EVENT_FROSTBOLT);
+                    _events.ScheduleEvent(EVENT_BLINK, 250);
+                }
+            }
+        }
+
         void UpdateAI(uint32 diff) override
         {
             if (!UpdateVictim())
@@ -633,6 +654,9 @@ struct npc_beryl_sorcererAI : public CreatureAI
             }
 
             _events.Update(diff);
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
 
             if (uint32 eventId = _events.ExecuteEvent())
             {
@@ -653,15 +677,56 @@ struct npc_beryl_sorcererAI : public CreatureAI
                             }
                         }
                         break;
+                    case EVENT_CHECK_HEALTH:
+                        _events.ScheduleEvent(EVENT_CHECK_HEALTH, 1s);
+                        break;
+                    case EVENT_BLINK:
+                        if (!_blinkUsed)
+                        {
+                            DoCast(me, SPELL_BLINK, true);
+                            _blinkUsed = true;
+                            
+                            // Stop movement and disable pathing
+                            me->StopMoving();
+                            me->GetMotionMaster()->Clear();
+                            me->GetMotionMaster()->MoveIdle();
+                            _movementPaused = true;
+                            
+                            // Schedule next frostbolt after blink
+                            _events.ScheduleEvent(EVENT_FROSTBOLT, 1000);
+                            
+                            // Schedule resuming movement after a delay
+                            _events.ScheduleEvent(EVENT_RESUME_MOVEMENT, 3500);
+                        }
+                        break;
+                    case EVENT_RESUME_MOVEMENT:
+                        if (_movementPaused)
+                        {
+                            me->GetMotionMaster()->Clear();
+                            me->SetReactState(REACT_AGGRESSIVE);
+                            if (Unit* victim = me->GetVictim())
+                            {
+                                me->GetMotionMaster()->MoveChase(victim);
+                            }
+                            _movementPaused = false;
+                        }
+                        break;
                 }
             }
-            DoMeleeAttackIfReady();
+
+            if (!_movementPaused)
+            {
+                DoMeleeAttackIfReady();
+            }
         }
 
     private:
         EventMap   _events;
         ObjectGuid _playerGUID;
         bool       _chainsCast;
+        bool       _blinkUsed;
+        bool       _movementPaused;
+        bool       _frostboltFinished;
     };
 
     CreatureAI* GetAI(Creature* creature) const override
