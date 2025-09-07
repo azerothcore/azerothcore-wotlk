45e1ad53310381aba1e750855c5891108cfc54e4
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-06 21:30:03 -0500

    sql codestyle

--- data/sql/updates/pending_db_world/rev_1757207529434522565.sql
+++ data/sql/updates/pending_db_world/rev_1757207529434522565.sql
@@ -1,4 +1,3 @@
 -- Add Blink spell to Beryl Sorcerer
 DELETE FROM `creature_template_spell` WHERE `CreatureID`=25316 AND `Index`=0;
-INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES 
-(25316, 0, 50648, 0);
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES (25316, 0, 50648, 0);
