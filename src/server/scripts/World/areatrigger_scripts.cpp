9adb12e8c8094f81a8f0603f67ff9a249e2acd1e
Jelle Meeus <sogladev@gmail.com>
2025-09-11 01:35:05 +0200

    fix(DB/SmartAI): Zul'Drak Voltarus teleporters sometimes do not work

--- src/server/scripts/World/areatrigger_scripts.cpp
+++ src/server/scripts/World/areatrigger_scripts.cpp
@@ -20,27 +20,6 @@
 #include "Player.h"
 #include "ScriptedCreature.h"
 
-class AreaTrigger_at_voltarus_middle : public AreaTriggerScript
-{
-public:
-    AreaTrigger_at_voltarus_middle()
-        : AreaTriggerScript("at_voltarus_middle")
-    {
-    }
-
-    bool OnTrigger(Player* player, AreaTrigger const* /*trigger*/) override
-    {
-        if (player->IsAlive() && !player->IsInCombat())
-            if (player->HasItemCount(39319)) // Scepter of Domination
-            {
-                player->TeleportTo(MAP_NORTHREND, 6242.67f, -1972.10f, 484.783f, 0.6f);
-                return true;
-            }
-
-        return false;
-    }
-};
-
 /*######
 ## at_coilfang_waterfall
 ######*/
