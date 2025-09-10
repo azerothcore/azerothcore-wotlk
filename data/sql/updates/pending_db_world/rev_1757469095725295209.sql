f6c78c2e3e61daca6ba4548e16a3f34fc25457dc
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-09 20:57:49 -0500

    Delete statement

--- data/sql/updates/pending_db_world/rev_1757469095725295209.sql
+++ data/sql/updates/pending_db_world/rev_1757469095725295209.sql
@@ -1,3 +1,4 @@
+DELETE FROM `spelldifficulty_dbc` WHERE `ID` IN (53472, 53509);
 INSERT INTO `spelldifficulty_dbc` (`id`, `difficulty0`, `difficulty1`) VALUES
 (53472, 53472, 59433),
 (53509, 53509, 59432);
