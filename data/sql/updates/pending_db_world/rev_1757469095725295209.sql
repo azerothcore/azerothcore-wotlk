cf550ebff4e67883acc74c07eaf15b503ef8ece5
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-09 21:34:06 -0500

    Update rev_1757469095725295209.sql

--- data/sql/updates/pending_db_world/rev_1757469095725295209.sql
+++ data/sql/updates/pending_db_world/rev_1757469095725295209.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spelldifficulty_dbc` WHERE `ID` IN (53472, 53509);
-INSERT INTO `spelldifficulty_dbc` (`id`, `difficulty0`, `difficulty1`) VALUES
+INSERT INTO `spelldifficulty_dbc` (`ID`, `DifficultySpellID_1`, `DifficultySpellID_2`) VALUES
 (53472, 53472, 59433),
 (53509, 53509, 59432);
