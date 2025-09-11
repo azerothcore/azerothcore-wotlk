b76716936639f7ca11e4932e236bd8cb75af48e9
Andrew <47818697+Nyeriah@users.noreply.github.com>
2025-09-10 23:37:37 -0300

    Update conditions and smart_scripts in SQL file

--- data/sql/updates/pending_db_world/rev_1757550472387622900.sql
+++ data/sql/updates/pending_db_world/rev_1757550472387622900.sql
@@ -1,10 +1,10 @@
 --
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 23301 AND `ConditionTypeOrReference` IN (30, 104);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(17, 0, 23301, 0, 0, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require no Ebon Blade Banner used on body (set in AI data)'),
-(17, 0, 23301, 0, 1, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require no Ebon Blade Banner used on body (set in AI data)'),
-(17, 0, 23301, 0, 2, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require no Ebon Blade Banner used on body (set in AI data)'),
-(17, 0, 23301, 0, 3, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require no Ebon Blade Banner used on body (set in AI data)');
+(17, 0, 23301, 0, 0, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require Ebon Blade Banner NOT used on body (set in AI data)'),
+(17, 0, 23301, 0, 1, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require Ebon Blade Banner NOT used on body (set in AI data)'),
+(17, 0, 23301, 0, 2, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require Ebon Blade Banner NOT used on body (set in AI data)'),
+(17, 0, 23301, 0, 3, 104, 1, 1, 1, 0, 1, 0, 0, '', 'Require Ebon Blade Banner NOT used on body (set in AI data)');
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 29880) AND (`source_type` = 0) AND (`id` IN (0, 1, 2, 3, 4, 5));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
