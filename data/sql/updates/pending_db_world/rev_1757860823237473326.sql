b0f2af279d77608177d2e81530153b80862f7217
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-14 11:00:10 -0500

    Gossip Chain

--- data/sql/updates/pending_db_world/rev_1757860823237473326.sql
+++ data/sql/updates/pending_db_world/rev_1757860823237473326.sql
@@ -1,8 +1,15 @@
---
+-- Update gossip option 0 to show after completing quest The Frostborn King 12873
 UPDATE `conditions` 
-SET `ConditionTypeOrReference` = 8, 
-    `Comment` = 'Show gossip only if Fervor of the Frostborn quest completed'
+SET `ConditionTypeOrReference` = 8,
+    `ConditionValue1` = 12873,
+    `Comment` = 'Show frostborn test gossip only after completing quest The Frostborn King 12873'
 WHERE `SourceTypeOrReferenceId` = 15 
 AND `SourceGroup` = 9891 
 AND `SourceEntry` = 0 
 AND `ConditionValue1` = 12874;
+
+-- Add new condition for gossip option 1 to show after accepting quest Pushed too Far 12869
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9891 AND `SourceEntry` = 1;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`)
+VALUES 
+(15, 9891, 1, 0, 0, 9, 0, 12869, 0, 0, 0, 0, 0, '', 'Show wyrm battle gossip only after accepting quest Pushed too Far 12869');
