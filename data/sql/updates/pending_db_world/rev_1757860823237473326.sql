14e1b696b96e55c50254b39f8371dd8c8cf6ea5a
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-14 11:25:11 -0500

    Slot 4 not 0

--- data/sql/updates/pending_db_world/rev_1757860823237473326.sql
+++ data/sql/updates/pending_db_world/rev_1757860823237473326.sql
@@ -18,4 +18,4 @@ VALUES
 -- This makes the gossip behave the same as accepting quest 12869 (auto-mount on bird)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
 VALUES 
-(29732, 0, 0, 0, 62, 0, 100, 0, 9891, 1, 0, 0, 11, 57049, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Fjorlin Frostbrow - On Gossip Option Selected (Menu 9891 Option 1) - Cast Spell 57049');
+(29732, 0, 4, 0, 62, 0, 100, 0, 9891, 1, 0, 0, 11, 57049, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Fjorlin Frostbrow - On Gossip Select Option 1 (Wyrm Battle) - Cast Mount Spell 57049');
