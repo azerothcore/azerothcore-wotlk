060e96929b181442adb1f00cdeef42c1a28d7847
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-14 11:15:16 -0500

    Click for Bird

--- data/sql/updates/pending_db_world/rev_1757860823237473326.sql
+++ data/sql/updates/pending_db_world/rev_1757860823237473326.sql
@@ -13,3 +13,9 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`)
 VALUES 
 (15, 9891, 1, 0, 0, 9, 0, 12869, 0, 0, 0, 0, 0, '', 'Show wyrm battle gossip only after accepting quest Pushed too Far 12869');
+
+-- Smart script to cast mount spell when player clicks "wyrm battle" gossip option
+-- This makes the gossip behave the same as accepting quest 12869 (auto-mount on bird)
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`)
+VALUES 
+(29732, 0, 0, 0, 62, 0, 100, 0, 9891, 1, 0, 0, 11, 57049, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Fjorlin Frostbrow - On Gossip Option Selected (Menu 9891 Option 1) - Cast Spell 57049');
