c300dea70803acb4694cd051cf020bcf1e9f8642
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-14 11:43:58 -0500

    One line for checks...

--- data/sql/updates/pending_db_world/rev_1757860823237473326.sql
+++ data/sql/updates/pending_db_world/rev_1757860823237473326.sql
@@ -1,12 +1,5 @@
 -- Update gossip option 0 to show after completing quest The Frostborn King 12873
-UPDATE `conditions`
-SET `ConditionTypeOrReference` = 8,
-    `ConditionValue1` = 12873,
-    `Comment` = 'Show frostborn test gossip only after completing quest The Frostborn King 12873'
-WHERE `SourceTypeOrReferenceId` = 15
-AND `SourceGroup` = 9891
-AND `SourceEntry` = 0
-AND `ConditionValue1` = 12874;
+UPDATE `conditions` SET `ConditionTypeOrReference` = 8, `ConditionValue1` = 12873, `Comment` = 'Show frostborn test gossip only after completing quest The Frostborn King 12873' WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9891 AND `SourceEntry` = 0 AND `ConditionValue1` = 12874;
 
 -- Add new condition for gossip option 1 to show after accepting quest Pushed too Far 12869
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9891 AND `SourceEntry` = 1;
