a548e2e1e9c8cbb9380fb69fbb1e87d9303d02b7
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-16 14:18:20 -0500

    codestyle

--- data/sql/updates/pending_db_world/rev_1758041289461617900.sql
+++ data/sql/updates/pending_db_world/rev_1758041289461617900.sql
@@ -1,1 +1,1 @@
-UPDATE gameobject SET position_x = 5716.26, position_y = -4369.34, position_z = 385.885 WHERE guid = 99747;
+UPDATE `gameobject` SET `position_x` = 5716.26, `position_y` = -4369.34, `position_z` = 385.885 WHERE `guid` = 99747;
