b8a7795c327d3ff8f0d472f8e96fd7b39575c1d9
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-16 12:41:17 -0500

    Update rev_1758041289461617900.sql

--- data/sql/updates/pending_db_world/rev_1758041289461617900.sql
+++ data/sql/updates/pending_db_world/rev_1758041289461617900.sql
@@ -1,2 +1,1 @@
-UPDATE `gameobject` SET `position_x` = 5716.26, `position_y` = -4369.34, `position_z` = 385.885 WHERE `guid` = 99747;
 UPDATE `gameobject_template` SET `data1` = 20 WHERE `entry` = 190739;
