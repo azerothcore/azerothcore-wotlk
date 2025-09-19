b75a067ccdb98c99f0413897e14868dc577d64d5
Jelle Meeus <sogladev@gmail.com>
2025-09-19 20:28:25 +0200

    add feign death action

--- data/sql/updates/pending_db_world/rev_1758045296314496529.sql
+++ data/sql/updates/pending_db_world/rev_1758045296314496529.sql
@@ -75,3 +75,8 @@ INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 13) AND (`SourceGroup` = 1) AND (`SourceEntry` = 45595) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 31) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 3) AND (`ConditionValue2` = 25424) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 1, 45595, 0, 0, 31, 0, 3, 25424, 0, 0, 0, 0, '', '45595 Fireball requires target entry 25424');
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 25424;
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 25424);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(25424, 0, 0, 0, 8, 0, 100, 0, 45595, 0, 0, 0, 0, 0, 11, 58951, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Vision of Farseer Grimwalker - On Spellhit \'Fireball\' - Cast \'Permanent Feign Death\'');
