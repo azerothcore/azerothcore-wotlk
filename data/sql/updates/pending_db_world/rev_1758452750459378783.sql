819a5675c2b70a7d03344ea8a8341902a7c97021
Jelle Meeus <sogladev@gmail.com>
2025-09-21 15:11:41 +0200

    fixup! fix(Scripts/Spells): Brewfest Hops

--- data/sql/updates/pending_db_world/rev_1758452750459378783.sql
+++ data/sql/updates/pending_db_world/rev_1758452750459378783.sql
@@ -1,6 +1,6 @@
 --
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (66050, 62062, 66052) AND `spell_effect` IN (49357, 52845);
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
-(62062, 49357, 0, 'Brewfest Holiday Mount'),
-(66052, 49357, 0, 'Brewfest Holiday Mount'),
-(66050, 52845, 0, 'Brewfest Holiday Mount Reverse');
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (66050, 62062, 66052) AND `ScriptName` = 'spell_item_brewfest_hops';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(66050, 'spell_item_brewfest_hops'),
+(62062, 'spell_item_brewfest_hops'),
+(66052, 'spell_item_brewfest_hops');
