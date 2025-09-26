b42f3a449c5c9da3103deb7c51e2fcc69430fc2e
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-09-26 14:26:08 +0200

    Update Crusader_pm.sql

--- data/sql/updates/pending_db_world/Crusader_pm.sql
+++ data/sql/updates/pending_db_world/Crusader_pm.sql
@@ -1,4 +1,3 @@
 
 -- Remove Phase Mask 1 and add Phasemask 2.
-UPDATE `creature` SET `phaseMask` = `phaseMask` &~ 1 WHERE (`id1` = 28529) AND (`guid` IN (128641, 128697));
-UPDATE `creature` SET `phaseMask` = `phaseMask` |2 WHERE (`id1` = 28529) AND (`guid` IN (128641, 128697));
+UPDATE `creature` SET `phaseMask` = `phaseMask` &~1 | 2 WHERE `id1` = 28529 AND `guid` IN (128641,128697);
