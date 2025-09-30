52270bac0f74a28ca6b9eab5007c6634cbde794e
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-09-29 19:18:11 -0500

    Update rev_1759191371018343890.sql

--- data/sql/updates/pending_db_world/rev_1759191371018343890.sql
+++ data/sql/updates/pending_db_world/rev_1759191371018343890.sql
@@ -2,4 +2,4 @@ DELETE FROM creature_formations WHERE (leaderGUID = 127046);
 INSERT INTO creature_formations (leaderGUID, memberGUID, dist, angle, groupAI, point_1, point_2) VALUES
 (127046, 127046, 0, 0, 1, 0, 0),
 (127046, 127080, 0, 0, 1, 0, 0),
-(127046, 127081, 0, 0, 1, 0, 0);--
+(127046, 127081, 0, 0, 1, 0, 0);
