ca94014540a4eedac6d90c81a8b4f8283766bd3e
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2025-10-01 08:56:02 -0400

    Consolidate update query.
    Co-authored-by: Andrew <47818697+Nyeriah@users.noreply.github.com>

--- data/sql/updates/pending_db_world/engineer-blastwrench.sql
+++ data/sql/updates/pending_db_world/engineer-blastwrench.sql
@@ -1,3 +1,2 @@
-UPDATE `creature_template_model` SET `VerifiedBuild` = 51831 WHERE `CreatureID` = 33666;
-UPDATE `creature_template_model` SET `VerifiedBuild` = 51831 WHERE `CreatureID` = 33669;
+UPDATE `creature_template_model` SET `VerifiedBuild` = 51831 WHERE `CreatureID` IN (33666, 33669);
 UPDATE `creature_template_model` SET `Probability` = 0 WHERE `CreatureID` = 33669 AND `CreatureDisplayID` = 27343;
