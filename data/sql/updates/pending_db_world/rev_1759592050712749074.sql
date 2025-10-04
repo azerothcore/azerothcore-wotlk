6644a32fdf9c3400840d754c7c50082c89ee9e91
Jelle Meeus <sogladev@gmail.com>
2025-10-04 22:51:56 +0200

    fixup! fix(DB/SmartAI): Scorn is summoned after Bloodmage Thalnos dies

--- data/sql/updates/pending_db_world/rev_1759592050712749074.sql
+++ data/sql/updates/pending_db_world/rev_1759592050712749074.sql
@@ -1,5 +1,6 @@
 --
-DELETE FROM `creature` WHERE (`id1` = 14693);
+DELETE FROM `creature` WHERE (`guid` = 248652) AND (`id1` = 14693);
+DELETE FROM `game_event_creature` WHERE (`guid` = 248652) AND (`eventEntry` = 120);
 
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 4543) AND (`source_type` = 0) AND (`id` IN (7));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
