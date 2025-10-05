e2550bbc86532547bff6cac3c50515627ed5a5f2
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-10-05 09:25:46 -0500

    Add files via upload

---
+++ data/sql/updates/pending_db_world/rev_1759674278855478977.sql
@@ -0,0 +1,4 @@
+-- Spell 55815 eck spring
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_eck_spring';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(55815, 'spell_eck_spring');
