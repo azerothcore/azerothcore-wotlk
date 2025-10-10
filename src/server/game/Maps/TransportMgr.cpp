6b817315598e3029862e1205acbfbb1163646331
zeb <37308742+zeb139@users.noreply.github.com>
2025-10-10 12:51:16 -0400

    Removed trailing whitespace

--- src/server/game/Maps/TransportMgr.cpp
+++ src/server/game/Maps/TransportMgr.cpp
@@ -449,7 +449,7 @@ void TransportMgr::SpawnContinentTransports()
     }
 
     // Preloads Deeprun Tram to fix issues with Subway carts syncronization
-    /*  
+    /*
         TODO: This is a temporary workaround. Consider removing TransportMgr::PreloadGridsFromQuery() as part of fix.
         Takenbacon: "In the long run the most likely ideal fix would be to always spawn all transport types (and thus loading their grid) on map creation"
         See https://github.com/azerothcore/azerothcore-wotlk/pull/23009 for more details.
