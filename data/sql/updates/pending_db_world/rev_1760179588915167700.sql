d2615c2d60c1fc2480894490a64287678892ef94
Ryan Turner <AugustoMendes4426@gmail.com>
2025-10-11 22:51:33 +0100

    fix(DB/Gossip) - Ancient Skull Pile no longer shows 2 gossip menu options (#23183)

---
+++ data/sql/updates/pending_db_world/rev_1760179588915167700.sql
@@ -0,0 +1,5 @@
+-- Correctly uses Option0 instead of Option1 to summon the Terokk
+UPDATE `smart_scripts` SET `event_param2` = 0 WHERE `entryorguid` = 185928 AND `source_type` = 1 AND `id` = 0;
+
+-- Removes the duplicated option.
+DELETE FROM `gossip_menu_option` WHERE `MenuID` = 8687 AND `OptionID` = 1;
