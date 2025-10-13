8fe081f3e4bde64e387e2e1cd5848fc7125c7f53
Jelle Meeus <sogladev@gmail.com>
2025-10-14 00:30:22 +0200

    Revert "more reliable interrupt"
    This reverts commit 643b795f988b02eb3b925fa6c878fcea860dce94.

--- data/sql/updates/pending_db_world/rev_1759339786373011744.sql
+++ data/sql/updates/pending_db_world/rev_1759339786373011744.sql
@@ -8,10 +8,9 @@ DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 31280);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (31280, 0, 0, 0, 60, 1, 70, 512, 5000, 5000, 3000, 4000, 0, 0, 11, 59894, 0, 0, 0, 0, 0, 19, 31884, 90, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Update - Cast \'Launch Spear\' (Phase 1)'),
 (31280, 0, 1, 2, 8, 0, 100, 512, 59880, 0, 0, 0, 0, 0, 22, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Spellhit \'Suppression Charge\' - Set Event Phase 2'),
-(31280, 0, 2, 5, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 80, 3128000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Spellhit \'Suppression Charge\' - Run Script'),
+(31280, 0, 2, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 80, 3128000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Spellhit \'Suppression Charge\' - Run Script'),
 (31280, 0, 3, 0, 25, 0, 100, 512, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Reset - Set Event Phase 1'),
-(31280, 0, 4, 0, 60, 1, 70, 512, 5000, 5000, 3000, 4000, 0, 0, 11, 59894, 0, 0, 0, 0, 0, 19, 32227, 90, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Update - Cast \'Launch Spear\' (Phase 1)'),
-(31280, 0, 5, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 92, 0, 59894, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Spellhit \'Suppression Charge\' - Interrupt Spell \'Launch Spear\'');
+(31280, 0, 4, 0, 60, 1, 70, 512, 5000, 5000, 3000, 4000, 0, 0, 11, 59894, 0, 0, 0, 0, 0, 19, 32227, 90, 0, 0, 0, 0, 0, 0, 'Ymirheim Spear Gun - On Update - Cast \'Launch Spear\' (Phase 1)');
 
 UPDATE `creature_template` SET `ArmorModifier` = 0 WHERE (`entry` = 32227);
 
