869aad1bc218ad9eddf9185675539e60f1298566
SAS2000 <peter.hejjas@gmail.com>
2025-10-13 10:28:50 +0200

    On branch fix-issue-22740 Changes to be committed: 	renamed:    data/sql/updates/db_world/2025_08_28_01.sql -> data/sql/updates/db_world/2025_10_12_07.sql

--- data/sql/updates/db_world/2025_10_12_07.sql
+++ data/sql/updates/db_world/2025_10_12_07.sql
@@ -1,4 +1,4 @@
--- DB update 2025_08_28_00 -> 2025_08_28_01
+-- DB update 2025_10_12_06 -> 2025_10_12_07
 -- add wp show first/last to command help
 UPDATE `command` SET
 `help`='Syntax: .wp show $option\nOptions:\non $pathid (or selected creature with loaded path) - Show path\nfirst $pathid (or selected creature with loaded path) - Show first waypoint\nlast $pathid (or selected creature with loaded path) - Show last waypoint\noff - Hide path (all waypoints in current map)\ninfo $selected_waypoint - Show info for selected waypoint.'
