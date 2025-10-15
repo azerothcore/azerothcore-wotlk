3d5feb1c7862a61606809300a4b4ce7707e3979d
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-10-15 07:55:32 -0300

    Update rev_1760478939949751800.sql

--- data/sql/updates/pending_db_world/rev_1760478939949751800.sql
+++ data/sql/updates/pending_db_world/rev_1760478939949751800.sql
@@ -1,4 +1,4 @@
 --
-DELETE FROM `command` WHERE `name` = 'zonestats';
+DELETE FROM `command` WHERE `name` = 'debug zonestats';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('zonestats', 1, '.debug zonestats [$playerName]\nDisplays the amount of players in the player\'s current zone.');
+('debug zonestats', 1, '.debug zonestats [$playerName]\nDisplays the amount of players in the player\'s current zone.');
