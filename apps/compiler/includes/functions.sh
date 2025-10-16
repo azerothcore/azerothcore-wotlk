4c3eab650c33c21542f5b111bf19693254737ae2
Yehonal <yehonal.azeroth@gmail.com>
2025-10-16 11:30:04 +0200

    feat(apps/service-manager): Add logging output for service uptime checks (#23252)

--- apps/compiler/includes/functions.sh
+++ apps/compiler/includes/functions.sh
@@ -143,6 +143,7 @@ function comp_compile() {
       mkdir -p "$AC_BINPATH_FULL"
       echo "Creating $confDir..."
       mkdir -p "$confDir"
+      mkdir -p "$confDir/modules"
 
       echo "Cmake install..."
       $SUDO cmake --install . --config $CTYPE
@@ -166,6 +167,7 @@ function comp_compile() {
           cp -v --no-clobber "$confDir/authserver.conf.dist" "$confDir/authserver.conf"
       [[ -f "$confDir/dbimport.conf.dist" ]] && \
           cp -v --no-clobber "$confDir/dbimport.conf.dist" "$confDir/dbimport.conf"
+
       for f in "$confDir/modules/"*.dist
       do
           [[ -e $f ]] || break  # handle the case of no *.dist files
