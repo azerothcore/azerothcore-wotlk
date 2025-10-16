8d7366c20be12fb29b5b42bb5da761777f071ed7
killerwife <killerwife@gmail.com>
2025-10-16 09:49:08 +0200

    Add sql file for chat command

---
+++ data/sql/updates/pending_db_world/rev_1760600911436478900.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `command` WHERE `name` = "packetlog";
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+("packetlog", 2, "Syntax: .packetlog [on/off]\n Toggles to allow the character using the command to start to log their packets into the server, PacketLogFile needs to be set with a valid filename");
