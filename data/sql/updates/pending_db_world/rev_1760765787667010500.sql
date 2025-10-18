b4752eaaae08c54f6b2bc4a6d156fdc71ece321b
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-10-18 02:39:58 -0300

    fix(DB/Proc): fix Mind Flay not applying Misery
    partial cherry-pick from: https://github.com/TrinityCore/TrinityCore/commit/86537a1a52e5d88a33a867322f1dc56d986449c0
    
    Co-Authored-By: errodia <1046672+errodia@users.noreply.github.com>
    Co-Authored-By: Sebastian Valle <subv2112@gmail.com>

---
+++ data/sql/updates/pending_db_world/rev_1760765787667010500.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `spell_proc_event` SET `SpellFamilyMask2` = 0x00000040 WHERE `entry` = -33191;
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = -33191;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(-33191, 'spell_gen_proc_on_victim');
