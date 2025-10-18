5b7630fd9fd4dd1ae5dbd4cd92f341d9cba96077
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-10-18 03:18:39 -0300

    add to heroic

--- data/sql/updates/pending_db_world/rev_1760735142745843700.sql
+++ data/sql/updates/pending_db_world/rev_1760735142745843700.sql
@@ -12,7 +12,9 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28961;
 DELETE FROM `creature_template_addon` WHERE `entry` IN (28965, 28961);
 INSERT INTO `creature_template_addon` (`entry`,`path_id`,`bytes1`,`mount`,`auras`) VALUES
 (28965, 0, 0, 0, '52881'), -- 28965 - 16245, 52881
-(28961, 0, 0, 0, '52898'); -- 28961 - 16245, 52898
+(28961, 0, 0, 0, '52898'), -- 28961 - 16245, 52898
+(30980, 0, 0, 0, '52898'),
+(30982, 0, 0, 0, '52881');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 28961);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
