53c47d0ea76dd5125e9b8096e744816d923cc017
Ludwig <sudlud@users.noreply.github.com>
2025-10-24 07:30:09 +0200

    force installation from MySQL’s repo

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -45,7 +45,9 @@ if [[ $DOCKER != 1 && $SKIP_MYSQL_INSTALL != 1 ]]; then
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A8D3785C
   DEBIAN_FRONTEND="noninteractive" $SUDO dpkg -i "$VAR_PATH/mysql-apt-config_0.8.35-1_all.deb"
   $SUDO apt-get update
-  DEBIAN_FRONTEND="noninteractive" $SUDO apt-get install -y mysql-server
+
+  # Force installation from MySQL’s repo
+  DEBIAN_FRONTEND="noninteractive" $SUDO apt-get install -y mysql-community-server
 fi
 
 
