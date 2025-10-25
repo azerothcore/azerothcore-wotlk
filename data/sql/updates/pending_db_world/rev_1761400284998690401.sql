b02afb3a4a1a5fbe6e0428b3ae7e0c8bb32bac71
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-10-25 17:49:41 -0500

    codestyle

--- data/sql/updates/pending_db_world/rev_1761400284998690401.sql
+++ data/sql/updates/pending_db_world/rev_1761400284998690401.sql
@@ -1,5 +1,5 @@
 --
 DELETE FROM `creature_formations` WHERE `leaderGUID` = 126740 OR `memberGUID` = 126740;
-INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES 
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
 (126740, 126740, 0, 0, 3, 0, 0),
 (126740, 126741, 0, 0, 3, 0, 0);
