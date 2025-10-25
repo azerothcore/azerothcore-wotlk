b89da57d2f56960d0dea25b72e632babb65fb5af
John Doe <john.doe@gmail.com>
2025-10-25 09:28:06 -0500

    Forgot commit?

--- data/sql/updates/pending_db_world/rev_1761401571384059169.sql
+++ data/sql/updates/pending_db_world/rev_1761401571384059169.sql
@@ -1,1 +1,5 @@
 --
+DELETE FROM `creature_formations` WHERE (`leaderGUID` = 126747) AND (`memberGUID` IN (126747, 126748));
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(126747, 126747, 0, 0, 3, 0, 0),
+(126747, 126748, 0, 0, 3, 0, 0);
