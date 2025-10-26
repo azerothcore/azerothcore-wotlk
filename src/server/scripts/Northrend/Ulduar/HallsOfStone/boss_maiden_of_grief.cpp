f3050bc53bdf6030b36e977b458a0d2401e71786
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-10-25 23:11:34 -0500

    back to time

--- src/server/scripts/Northrend/Ulduar/HallsOfStone/boss_maiden_of_grief.cpp
+++ src/server/scripts/Northrend/Ulduar/HallsOfStone/boss_maiden_of_grief.cpp
@@ -136,7 +136,7 @@ public:
                         {
                             me->CastSpell(target, PARTING_SORROW, false);
                         }
-                        events.Repeat(27s, 45s);
+                        events.Repeat(6s, 16s);
                         break;
                     }
             }
