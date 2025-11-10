70f545124da19aaa763428946554f9433f5aa28a
sogladev <sogladev@gmail.com>
2025-11-10 19:10:47 +0100

    fix(DB/Quest): The Kalu'ak dailies reward 500 rep (#23600)

---
+++ data/sql/updates/pending_db_world/rev_1762784053392314466.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `quest_template` SET `RewardFactionValue1` = 5 WHERE (`ID` = 11472);
+UPDATE `quest_template` SET `RewardFactionValue1` = 5, `RewardFactionOverride1` = 0 WHERE (`ID` = 11945);
