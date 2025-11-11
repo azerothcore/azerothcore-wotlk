ae89f6419b9cdae5fb48dd4aaf136c0dddd6d620
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2025-11-11 09:17:48 +0000

    Update rev_1762695074553179900.sql

--- data/sql/updates/pending_db_world/rev_1762695074553179900.sql
+++ data/sql/updates/pending_db_world/rev_1762695074553179900.sql
@@ -1,3 +1,3 @@
 -- Makes sure the Shaman player gets the required ritual toch item and that requires the quest to drop
-UPDATE `item_loot_template` SET `Chance` = 100, `QuestRequired` = 1 WHERE `Entry` = 24336 AND `Item` = 23682;
-UPDATE `item_loot_template` SET `Chance` = 85 WHERE `Entry` = 24336 AND `Item` = 24335;
+UPDATE `item_loot_template` SET `Chance` = 0, `QuestRequired` = 1 WHERE `Entry` = 24336 AND `Item` = 23682;
+UPDATE `item_loot_template` SET `Chance` = 0 WHERE `Entry` = 24336 AND `Item` = 24335;
