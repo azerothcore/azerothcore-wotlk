5104463812bb59d3728499f665fd4a76f6b5cd06
sogladev <sogladev@gmail.com>
2025-11-12 08:50:51 +0100

    fix(DB/QuestOfferReward): remove mention of a beta recipe in text (#23629)

---
+++ data/sql/updates/pending_db_world/rev_1762878144735563640.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `quest_offer_reward` SET `RewardText` = 'Aw yes, these will do nicely.$b$bJust plop them down anywhere. You can have some of my leftover stew from last night.' WHERE (`ID` = 11564);
