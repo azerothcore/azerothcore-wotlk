cdfa50c9905cb403d33503b88b882e0eccf678eb
sogladev <sogladev@gmail.com>
2025-11-15 02:35:07 +0100

    fix(DB/Creature): update IOC Demolisher spells (#23685)

---
+++ data/sql/updates/pending_db_world/rev_1763152689634414489.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `creature_template_spell` WHERE (`CreatureID` = 35415);
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(35415, 0, 68068, 0),
+(35415, 1, 67442, 0);
