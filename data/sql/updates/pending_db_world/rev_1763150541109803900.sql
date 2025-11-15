388f18895d38940045bdd2c5d63661956efd6fea
sudlud <sudlud@users.noreply.github.com>
2025-11-15 02:24:05 +0100

    fix(DB/Gameobject): fix spell focus location for 'Will of the Titans' (#23683)

---
+++ data/sql/updates/pending_db_world/rev_1763150541109803900.sql
@@ -0,0 +1,2 @@
+-- fix spell focus location for quest 'Will of the Titans'
+UPDATE `gameobject` SET `position_x` = 5110.11, `position_y` = 5471.22, `position_z` = -91.84 WHERE `id` = 190781 AND `guid` = 99745;
