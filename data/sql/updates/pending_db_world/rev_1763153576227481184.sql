b6ed4347fef672b97798d5a916eb6022af7163e8
sogladev <sogladev@gmail.com>
2025-11-15 01:56:07 +0100

    fix(DB/Loot): add Scourge Curio drop to Lost Shandaral Spirit (#23686)

---
+++ data/sql/updates/pending_db_world/rev_1763153576227481184.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 31231) AND (`Item` IN (42108));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(31231, 42108, 0, 33, 1, 1, 0, 1, 1, 'Lost Shandaral Spirit - Scourge Curio');
