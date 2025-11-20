c2ff7b62554ddf6ae5e58f3ba4022d3a1c144e3a
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-11-20 14:15:57 -0300

    fix(DB/Creature): Update run speed of Novos minions

---
+++ data/sql/updates/pending_db_world/rev_1763658928173617200.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature_template` SET `speed_run` = 1.142857 WHERE `entry` = 31356; -- Risen Shadowcaster
+UPDATE `creature_template` SET `speed_run` = 0.428571 WHERE `entry` = 31873; -- Fetid Troll Corpse
+UPDATE `creature_template` SET `speed_run` = 0.714285 WHERE `entry` = 31348; -- Hulking Corpse
