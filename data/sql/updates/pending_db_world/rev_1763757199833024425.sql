f9ab2f1bdd3026e6a38f91518e0f84a37563e453
Jelle Meeus <sogladev@gmail.com>
2025-11-21 21:36:20 +0100

    fix(DB/Creature): Warbear Matriarch vehicle

---
+++ data/sql/updates/pending_db_world/rev_1763757199833024425.sql
@@ -0,0 +1,10 @@
+--
+-- Warbear Matriarch
+-- Disable AA
+UPDATE `creature_template` SET `AIName` = 'VehicleAI' WHERE (`entry` = 29918);
+-- Spells from 1,2,3 to 4,5,6
+DELETE FROM `creature_template_spell` WHERE (`CreatureID` = 29918);
+INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
+(29918, 3, 54459, 12340),
+(29918, 4, 54458, 12340),
+(29918, 5, 54460, 12340);
