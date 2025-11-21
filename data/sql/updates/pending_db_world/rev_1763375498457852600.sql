d7c8a406f41f1af264ecc957012481d431cf05cc
EricksOliveira <ericksoliveira258@gmail.com>
2025-11-21 11:10:52 -0300

    Script model fix

--- data/sql/updates/pending_db_world/rev_1763375498457852600.sql
+++ data/sql/updates/pending_db_world/rev_1763375498457852600.sql
@@ -1,11 +1,2 @@
 --
-DELETE FROM `creature_template_model` WHERE (`CreatureID` = 25342);
-INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
-(25342, 0, 23250, 1, 1, 12340);
-
-DELETE FROM `creature_template_model` WHERE (`CreatureID` = 25343);
-INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
-(25343, 0, 23246, 1, 1, 12340),
-(25343, 1, 23247, 1, 1, 12340),
-(25343, 2, 23248, 1, 1, 12340),
-(25343, 3, 23249, 1, 1, 12340);
+UPDATE `creature_template` SET `ScriptName` = 'npc_dead_caravan_guard_model' WHERE `entry` IN (25342, 25343);
