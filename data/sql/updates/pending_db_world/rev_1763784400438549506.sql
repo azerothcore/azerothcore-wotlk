5fb047c3c722cb6a23d2cf11bbca57047acd7b8b
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-11-24 15:47:59 -0600

    derp

--- data/sql/updates/pending_db_world/rev_1763784400438549506.sql
+++ data/sql/updates/pending_db_world/rev_1763784400438549506.sql
@@ -1,9 +1,10 @@
 -- Winterskorn Vrykul Dismembering Bunny should only be giblet
 DELETE FROM `creature_template_model` WHERE (`CreatureID` = 24095) AND (`Idx` IN (0));
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
-(24095, 0, 25628, 1, 0, 12340);
+(24095, 0, 25628, 1, 1, 0);
 
 -- Conditions
 DELETE FROM `conditions` WHERE `SourceEntry` = 43036 AND `SourceTypeOrReferenceId` IN (13, 17);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`)
-VALUES (17, 0, 43036, 0, 0, 1, 1, 43059, 0, 0, 1, 0, 0, '', 'Spell 43036 (Dismembering Corpse) - Target must not already be transformed'), (17, 0, 43036, 0, 0, 36, 1, 0, 0, 0, 1, 0, 0, '', 'Spell 43036 (Dismembering Corpse) - Explicit target must be dead');
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(17, 0, 43036, 0, 0, 1, 1, 43059, 0, 0, 1, 0, 0, '', 'Spell 43036 (Dismembering Corpse) - Target must not already be transformed'),
+(17, 0, 43036, 0, 0, 36, 1, 0, 0, 0, 1, 0, 0, '', 'Spell 43036 (Dismembering Corpse) - Explicit target must be dead');
