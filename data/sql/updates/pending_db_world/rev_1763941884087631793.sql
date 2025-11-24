988d5cd1733ad51a10a64996a2293ddbbf52ac72
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-11-24 10:42:11 -0600

    Modify PrevQuestID for quest 12904
    Updated PrevQuestID for quest ID 12904 to reflect new requirements.

--- data/sql/updates/pending_db_world/rev_1763941884087631793.sql
+++ data/sql/updates/pending_db_world/rev_1763941884087631793.sql
@@ -1,2 +1,2 @@
---
-UPDATE `quest_template_addon` SET `PrevQuestID` = -12903 WHERE `ID` = 12904;
+-- Light Won't Grant Me Vengeance requires Crusader Forward Camp
+UPDATE `quest_template_addon` SET `PrevQuestID` = 12894 WHERE `ID` = 12904;
