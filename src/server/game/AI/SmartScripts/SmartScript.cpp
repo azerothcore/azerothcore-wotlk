58f07d75cfc6b9e1a9834f3e236767227d4fd20e
fangshun <fangshun2004@msn.com>
2025-11-24 21:03:09 +0800

    fix code error

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -2696,7 +2696,7 @@ void SmartScript::ProcessAction(SmartScriptHolder& e, Unit* unit, uint32 var0, u
         {
             if (me && me->FindMap())
                 me->FindMap()->LoadGrid(e.target.x, e.target.y);
-            else (go && go->FindMap())
+            else if (go && go->FindMap())
                 go->FindMap()->LoadGrid(e.target.x, e.target.y);
             break;
         }
