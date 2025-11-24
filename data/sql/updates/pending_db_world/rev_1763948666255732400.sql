11f6e39566afd70694b38907b7de7ae000798d18
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2025-11-24 12:08:39 +0000

    Update rev_1763948666255732400.sql

--- data/sql/updates/pending_db_world/rev_1763948666255732400.sql
+++ data/sql/updates/pending_db_world/rev_1763948666255732400.sql
@@ -1,2 +1,2 @@
 -- Makes the quest "Remember Everfrost" repetable
-UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE `ID` = 13421;
+UPDATE `quest_template_addon` SET `SpecialFlags` = `SpecialFlags` | 1 WHERE `ID` = 13421;
