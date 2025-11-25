79ba5ee1f10caa68a4625e934b97fd71d52bfcbc
blinkysc <blinkysc@users.noreply.github.com>
2025-11-25 12:54:47 -0600

    fix: removed debug timings

--- data/sql/updates/pending_db_world/rev_1764095157855610481.sql
+++ data/sql/updates/pending_db_world/rev_1764095157855610481.sql
@@ -31,11 +31,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 DELETE FROM `smart_scripts` WHERE (`entryorguid` = 2863600) AND (`source_type` = 9) AND (`id` IN (0, 1, 2, 3, 4, 5));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (2863600, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 52353, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Cast \'Script Effect - Creature Capture GUID to Dot Variable\''),
-(2863600, 9, 1, 0, 0, 0, 100, 0, 3000, 3000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Say Line 0'),
-(2863600, 9, 2, 0, 0, 0, 100, 0, 3000, 3000, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Say Line 1'),
-(2863600, 9, 3, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, 0, 12, 28639, 2, 300000, 0, 0, 0, 8, 0, 0, 0, 0, 5984.55, -3882.62, 417.438, 1.91986, 'Heb\'Jin - Actionlist - Summon Creature \'Heb\'Jin\'s Bat\''),
-(2863600, 9, 4, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 19, 28639, 100, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Send Target 1'),
-(2863600, 9, 5, 0, 0, 0, 100, 0, 1000, 1000, 0, 0, 0, 0, 11, 43671, 0, 0, 0, 0, 0, 19, 28639, 100, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Cast \'Ride Vehicle\'');
+(2863600, 9, 1, 0, 0, 0, 100, 0, 3200, 3200, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Say Line 0'),
+(2863600, 9, 2, 0, 0, 0, 100, 0, 300, 300, 0, 0, 0, 0, 12, 28639, 2, 300000, 0, 0, 0, 8, 0, 0, 0, 0, 5984.55, -3882.62, 417.438, 1.91986, 'Heb\'Jin - Actionlist - Summon Creature \'Heb\'Jin\'s Bat\''),
+(2863600, 9, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 19, 28639, 100, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Send Target 1'),
+(2863600, 9, 4, 0, 0, 0, 100, 0, 5700, 5700, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Say Line 1'),
+(2863600, 9, 5, 0, 0, 0, 100, 0, 2400, 2400, 0, 0, 0, 0, 11, 43671, 0, 0, 0, 0, 0, 19, 28639, 100, 0, 0, 0, 0, 0, 0, 'Heb\'Jin - Actionlist - Cast \'Ride Vehicle\'');
+
 
 -- Heb'Jin's Bat (creature)
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28639;
