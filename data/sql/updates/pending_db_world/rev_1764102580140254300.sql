33c19046c0fdf645d4561db4a24a551d73a6e149
RëXol <66186993+jorge990125@users.noreply.github.com>
2025-11-25 15:57:27 -0500

    correct sql

--- data/sql/updates/pending_db_world/rev_1764102580140254300.sql
+++ data/sql/updates/pending_db_world/rev_1764102580140254300.sql
@@ -1,10 +1,7 @@
+--
 -- flight animation correction
 -- https://www.wowhead.com/npc=37230/spire-frostwyrm
 -- https://www.wowhead.com/npc=37533/rimefang
 -- https://www.wowhead.com/npc=37534/spinestalker
 -- https://www.wowhead.com/npc=36853/sindragosa
-
-UPDATE `creature_template_addon` SET `bytes1` = 50331648 WHERE (`entry` = 37230);
-UPDATE `creature_template_addon` SET `bytes1` = 50331648 WHERE (`entry` = 37533);
-UPDATE `creature_template_addon` SET `bytes1` = 50331648 WHERE (`entry` = 37534);
-UPDATE `creature_template_addon` SET `bytes1` = 50331648 WHERE (`entry` = 36853);
+UPDATE `creature_template_addon` SET `bytes1` = `bytes1` | 16777216 | 33554432 WHERE `entry` IN (37230, 37533, 37534, 36853);
