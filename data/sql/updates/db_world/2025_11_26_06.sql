b619dc9ca9088f80191794c188f5c3ac93bba3d0
thomasjteachey <thomasjteachey@gmail.com>
2025-11-26 10:39:58 -0500

    fix(DB/SAI): spell proc event phase mask changed to on hit for entrapment

---
+++ data/sql/updates/db_world/2025_11_26_06.sql
@@ -0,0 +1,3 @@
+DELETE FROM spell_proc_event WHERE entry = -19184;
+INSERT INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, procPhase, ppmRate, CustomChance, Cooldown) VALUES
+(-19184, 0, 9, 16, 8192, 262144, 0, 0, 2, 0, 0, 0);
