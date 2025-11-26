522d7f944d59d182fd232e97b05bd215ff30220e
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-26 22:38:58 +0000

    Trailing whitespace

--- data/sql/updates/pending_db_world/rev_1764194426963825600.sql
+++ data/sql/updates/pending_db_world/rev_1764194426963825600.sql
@@ -12,9 +12,9 @@ INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `
 -- Deletes "Pattern: Robes of Arcana" from every creature's loot
 DELETE FROM `creature_loot_template` WHERE `item` = 5773;
 
--- Adds reference loot for "Pattern: Robes of Arcana" for 
+-- Adds reference loot for "Pattern: Robes of Arcana" for each creature below
 DELETE FROM `creature_loot_template` WHERE `Reference` = @ENTRY AND `Entry` IN (910, 2337, 10760);
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (910, @ITEM, @ENTRY, 2, 0, 1, 0, 1, 1, 'Defias Enchanter - Pattern: Robes of Arcana'),
-(2337, @ITEM, @ENTRY, 2, 0, 1, 0, 1, 1, 'Dark Strand Voidcalle - Pattern: Robes of Arcana'),  
+(2337, @ITEM, @ENTRY, 2, 0, 1, 0, 1, 1, 'Dark Strand Voidcalle - Pattern: Robes of Arcana'),
 (10760, @ITEM, @ENTRY, 2, 0, 1, 0, 1, 1, 'Grimtotem Geomancer - Pattern: Robes of Arcana');
