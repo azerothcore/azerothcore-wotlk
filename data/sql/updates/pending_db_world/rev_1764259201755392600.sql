ff7db3d448f2894f5c28404652fd13013240cd3f
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-11-27 21:53:59 +0000

    Turn this PR into everfrost fix for good

--- data/sql/updates/pending_db_world/rev_1764259201755392600.sql
+++ data/sql/updates/pending_db_world/rev_1764259201755392600.sql
@@ -1,10 +1,2 @@
--- Following the same rule as the herbs, ores and fishing for the everfrost to be visible by all possible.
+-- Following the same rule as the herbs, ores and fishing for the everfrost to be visible by all possible, they all use phase 255.
 UPDATE `gameobject` SET `phaseMask` = 255 WHERE `id` = 193997;
-
--- Sets all Herbs spawn time to be 1 hour (as they are currently not pooled).
--- Goldclover, Tiger Lily, Talandra's Rose, Lichbloom, Icethorn, Frozen Herb (190173, 190174 and 190175), Adder's Tongue and Frost Lotus (currently can be farmed in an instance for free.)
-UPDATE `gameobject` SET `spawntimesecs` = 3600 WHERE `id` IN (189973, 190169, 190170, 190171, 190172, 190173, 190174, 190175, 191019, 190176) AND `SpawnMask` != 3;
-
--- Sets all Ore Veins spawn time to be 1 hour (as they are currently not pooled).
--- Colbat Deposit, Rich Cobalt Deposit, Sranoite Depoist, Rich Saronite Deposit, Titatinum Vein and Pure Sarotine Depoist
-UPDATE `gameobject` SET `spawntimesecs` = 3600 WHERE `id` IN (189980, 189981, 189978, 189979, 191133, 195036) AND `SpawnMask` != 3;
