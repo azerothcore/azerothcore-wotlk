b52cf97bca22435644723a891c56de931510c8b9
Ovv <contact@ovv.dev>
2025-11-27 17:38:09 +0100

    chore(license): update copyright and license headers following review

--- src/common/Utilities/Systemd.cpp
+++ src/common/Utilities/Systemd.cpp
@@ -1,5 +1,13 @@
 /*
- * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
+ * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information.
+ *
+ * Portions of this file are derived from systemd, licensed under:
+ * - GPL-2.0-or-later (if the original systemd file was GPL-2.0+)
+ *   OR
+ * - LGPL-2.1-or-later, relicensed under GPL-2.0-or-later as permitted by LGPL-2.1+
+ *
+ * Original systemd copyright:
+ *   Copyright (c) the systemd contributors.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
