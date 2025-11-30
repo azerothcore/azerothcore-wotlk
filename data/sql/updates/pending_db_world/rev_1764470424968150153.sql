a8e2c5e7815e70b2e122b5a255b3f689d10bd1e2
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-11-29 23:19:53 -0600

    Update conditions for SourceGroup and SourceEntry

--- data/sql/updates/pending_db_world/rev_1764470424968150153.sql
+++ data/sql/updates/pending_db_world/rev_1764470424968150153.sql
@@ -1,3 +1,5 @@
+UPDATE `conditions` SET `SourceGroup` = 1 WHERE `SourceTypeOrReferenceId` = 13 AND `SourceGroup` = 7 AND `SourceEntry` = 52457;
+
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` = 52457;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 7, 52457, 0, 0, 31, 0, 3, 26797, 0, 0, 0, 0, '', 'Spell 52457 (Drak\'aguul\'s Soldiers) - Target Drakkari Protector'),
