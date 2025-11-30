3c679a24df33e413c9de0f2e325b86f119f1f238
blinkysc <blinkysc@users.noreply.github.com>
2025-11-29 19:09:16 -0600

    rem old sql

--- data/sql/updates/pending_db_world/rev_1764455969202502000.sql
+++
@@ -1,3 +0,0 @@
--- Fix Troll Patrol quests not granting Congratulations! bonus quest
-UPDATE `quest_template_addon` SET `SourceSpellID` = 51573 WHERE `ID` = 12563;
-UPDATE `quest_template` SET `RewardSpell` = 53707 WHERE `ID` IN (12563, 12587);
