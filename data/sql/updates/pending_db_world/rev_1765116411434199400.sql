deb08f08c14265f9ca595e443da8ef730820963b
rorshan <60732320+rorshan@users.noreply.github.com>
2025-12-07 15:17:22 +0100

    fix(DB/Quests): Make Zul'Drak blessing quests repeatable

---
+++ data/sql/updates/pending_db_world/rev_1765116411434199400.sql
@@ -0,0 +1,5 @@
+-- SpecialFlags 0 to 1, make Blessing of Zim'Torga repeatable
+UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE (`ID` = 12618);
+
+-- SpecialFlags 0 to 1, make Blessing of Zim'Rhuk repeatable
+UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE (`ID` = 12656);
