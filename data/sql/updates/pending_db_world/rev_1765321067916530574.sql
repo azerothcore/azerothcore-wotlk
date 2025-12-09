def382d258a9cf622b6c979eab63c3bb2cc602dc
blinkysc <blinkysc@users.noreply.github.com>
2025-12-09 17:24:51 -0600

    style: just update

--- data/sql/updates/pending_db_world/rev_1765321067916530574.sql
+++ data/sql/updates/pending_db_world/rev_1765321067916530574.sql
@@ -3,10 +3,8 @@
 -- 11287 is an optional breadcrumb - player can skip it and go directly to 11286
 -- If player takes 11286 first, breadcrumb 11287 should become unavailable
 --
-DELETE FROM `quest_template_addon` WHERE `ID` IN (11286, 11287);
-INSERT INTO `quest_template_addon` (`ID`, `MaxLevel`, `AllowableClasses`, `SourceSpellID`, `PrevQuestID`, `NextQuestID`, `ExclusiveGroup`, `RewardMailTemplateID`, `RewardMailDelay`, `RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES
-(11286, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(11287, 0, 0, 0, 11261, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0, `ExclusiveGroup` = 0 WHERE `ID` = 11286;
+UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE `ID` = 11287;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 19 AND `SourceEntry` = 11287;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (19, 0, 11287, 0, 0, 9, 0, 11286, 0, 0, 1, 0, 0, '', 'Find Sage Mistwalker - Not available if The Artifacts of Steel Gate is taken'),
