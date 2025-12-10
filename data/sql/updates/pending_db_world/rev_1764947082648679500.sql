4acd5d1d42d9a30fcfc80ac2fa7db1672f5b1ff8
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-12-10 12:33:13 -0500

    Replace delete and insert with update for entry -19184

--- data/sql/updates/pending_db_world/rev_1764947082648679500.sql
+++ data/sql/updates/pending_db_world/rev_1764947082648679500.sql
@@ -1,4 +1,2 @@
 --
-DELETE FROM `spell_proc_event` WHERE `entry` = -19184;
-INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(-19184, 0, 9, 16, 8192, 262144, 0, 0, 2, 0, 0, 0);
+UPDATE `spell_proc_event` SET `procPhase`=2 WHERE `entry`=-19184;
