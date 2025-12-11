02b32bc3ba22f544ecda5d247f43d555cc1aeefc
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2025-12-11 17:50:39 +0100

    Update rev_1765116411434199400.sql

--- data/sql/updates/pending_db_world/rev_1765116411434199400.sql
+++ data/sql/updates/pending_db_world/rev_1765116411434199400.sql
@@ -1,5 +1,2 @@
--- SpecialFlags 0 to 1, make Blessing of Zim'Torga repeatable
-UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE (`ID` = 12618);
 
--- SpecialFlags 0 to 1, make Blessing of Zim'Rhuk repeatable
-UPDATE `quest_template_addon` SET `SpecialFlags` = 1 WHERE (`ID` = 12656);
+UPDATE `quest_template_addon` SET `SpecialFlags` = `SpecialFlags`  | 1 WHERE `ID` IN (12618, 12656);
