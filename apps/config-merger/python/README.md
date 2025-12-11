4a4852013ff36d64214be8f94a2edeb4a6446bd4
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-12-11 20:06:49 +0000

    Updated license for the py file

--- apps/config-merger/python/README.md
+++ apps/config-merger/python/README.md
@@ -97,3 +97,13 @@ configs/
     ├── mod_example.conf
     └── ...
 ```
+
+## License
+
+This file is part of the AzerothCore Project. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+**Note:** Original code portions were licensed under the MIT License by Brian Aldridge.
--- apps/config-merger/python/config_merger.py
+++ apps/config-merger/python/config_merger.py
@@ -1,5 +1,23 @@
 # Version 1
 # Based and modified from: https://github.com/Brian-Aldridge/update_module_confs 
+#
+# This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Original code portions licensed under MIT License by Brian Aldridge (https://github.com/BoiseComputer)
+# Original project: https://github.com/Brian-Aldridge/update_module_confs
 
 VERSION = "1"
 
