f1d86de7d103211a77eb5177876b1c3187024e15
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-17 13:23:56 -0300

    Update rev_1765986105641229900.sql

--- data/sql/updates/pending_db_world/rev_1765986105641229900.sql
+++ data/sql/updates/pending_db_world/rev_1765986105641229900.sql
@@ -1,6 +1,7 @@
 -- Delete Duplicate
 DELETE FROM `creature` WHERE (`id1` = 24439) AND (`guid` IN (142710));
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 24439) AND (`source_type` = 0) AND (`id` IN (8));
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(24439, 0, 8, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 3, 0, 24780, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Hack: On Respawn - Set Model to Self');
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 24439);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(24439, 0, 24780, 1, 1, 12340),
+(24439, 1, 11686, 1, 0, 12340);
