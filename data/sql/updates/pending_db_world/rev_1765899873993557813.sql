0786f573e1403cd6d88d9495e27bf355bddee3b9
sogladev <sogladev@gmail.com>
2025-12-17 08:55:15 +0100

    fix(DB/SpellGroup): feral Faerie Fire can apply with imp Faerie Fire (#24119)

---
+++ data/sql/updates/pending_db_world/rev_1765899873993557813.sql
@@ -0,0 +1,4 @@
+--
+-- Faerie Fire group
+-- SPELL_GROUP_STACK_RULE_EXCLUSIVE_SAME_EFFECT	'Same effects of spells will not stack, yet auras will remain on a target'
+UPDATE `spell_group_stack_rules` SET `stack_rule`=3 WHERE `group_id`=1016;
