04ad109489fbc3937f5ae042c240117cbec92310
EricksOliveira <ericksoliveira258@gmail.com>
2025-12-16 23:31:05 -0300

    fix

--- data/sql/updates/pending_db_world/rev_1765937826391299200.sql
+++ data/sql/updates/pending_db_world/rev_1765937826391299200.sql
@@ -1,5 +1,5 @@
 --
 -- Boss Supremus
-UPDATE creature_model_info SET BoundingRadius = 4.2, CombatReach = 12 WHERE DisplayID = 21145;
+UPDATE `creature_model_info` SET `BoundingRadius` = 4.2, `CombatReach` = 12 WHERE `DisplayID` = 21145;
 -- Sholazar Guardian
-UPDATE creature_model_info SET BoundingRadius = 4.2 WHERE DisplayID = 26140;
+UPDATE `creature_model_info` SET `BoundingRadius` = 1 WHERE `DisplayID` = 26140;
