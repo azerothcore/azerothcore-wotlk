9dab2d408d36623592d5a49e736e2772a44fbe1c
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-18 14:11:28 -0300

    fix(Scripts/Achievement): Add OR condition to criteria check for achievement Fa-la-la-la-Ogri'la (#24155)

---
+++ data/sql/updates/pending_db_world/rev_1766072417278780600.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` = 3938;
+INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES
+(3938, 11, 0, 0, 'achievement_fa_la_la_la_ogrila');
