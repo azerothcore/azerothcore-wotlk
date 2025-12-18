f1cc5ef53a4e42414d3a7e3369160750b8079ee6
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-18 17:52:52 -0300

    Create rev_1766089848113030100.sql

---
+++ data/sql/updates/pending_db_world/rev_1766089848113030100.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 18 AND `SourceGroup` IN (29433, 29555) AND `SourceEntry` = 47020;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(18, 29433, 47020, 0, 0, 31, 0, 3, 0, 0, 0, 0, 0, '', 'Only Backpacks can mount Goblin Sappers'),
+(18, 29555, 47020, 0, 0, 31, 0, 3, 0, 0, 0, 0, 0, '', 'Only Backpacks can mount Goblin Sappers');
