cf742618957aca869a428e26f91849e96f5a8425
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-12-19 16:59:22 -0600

    Update data/sql/updates/pending_db_world/rev_1765066149291553835.sql
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1765066149291553835.sql
+++ data/sql/updates/pending_db_world/rev_1765066149291553835.sql
@@ -3,7 +3,7 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_oathbound_warder' WHERE `entr
 
 DELETE FROM `creature_template_spell` WHERE `CreatureID` = 30270;
 INSERT INTO `creature_template_spell` (`CreatureID`, `Index`, `Spell`, `VerifiedBuild`) VALUES
-(30270, 0, 56491, 12340),
-(30270, 1, 56425, 12340),
-(30270, 2, 56451, 12340),
-(30270, 3, 56506, 12340);
+(30270, 0, 56491, 0),
+(30270, 1, 56425, 0),
+(30270, 2, 56451, 0),
+(30270, 3, 56506, 0);
