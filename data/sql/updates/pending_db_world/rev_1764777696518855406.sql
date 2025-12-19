2b7e022f1e0d8dc123050e82ec1b28b056ed07b4
blinkysc <blinkysc@users.noreply.github.com>
2025-12-19 17:24:38 -0600

    fix: add ref 4000 as well

--- data/sql/updates/pending_db_world/rev_1764777696518855406.sql
+++ data/sql/updates/pending_db_world/rev_1764777696518855406.sql
@@ -1,1 +1,2 @@
+UPDATE `reference_loot_template` SET `Chance` = 0.5 WHERE `Entry` = 6010 AND `Item` = 1 AND `Reference` = 4000;
 UPDATE `reference_loot_template` SET `Chance` = 0.5 WHERE `Entry` = 6010 AND `Item` = 2 AND `Reference` = 4001;
