2de1113fcad407880bd48eace7fc2875a4993eca
blinkysc <blinkysc@users.noreply.github.com>
2025-12-19 18:07:26 -0600

    remove orientation

--- data/sql/updates/pending_db_world/rev_1764222370793190215.sql
+++ data/sql/updates/pending_db_world/rev_1764222370793190215.sql
@@ -3,6 +3,3 @@
 
 -- Increase flight speed from 1.0 to 3.0 (flying creatures use speed_run for flight)
 UPDATE `creature_template` SET `speed_run` = 3, `speed_walk` = 2 WHERE `entry` = 28192;
-
--- Fix orientation so the flying machine faces INTO the portal instead of away from it
-UPDATE `smart_scripts` SET `target_o` = -1.5984 WHERE `entryorguid` = 2819200 AND `source_type` = 9 AND `id` = 2;
