3b8f891ae7b6d8038c9fe893940c91124bcfc20a
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-25 23:09:47 -0300

    Update rev_1766254536399579700.sql

--- data/sql/updates/pending_db_world/rev_1766254536399579700.sql
+++ data/sql/updates/pending_db_world/rev_1766254536399579700.sql
@@ -1,10 +1,10 @@
 --
 DELETE FROM `command` WHERE `name` LIKE 'pooltools%';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('pooltools', 3, 'Syntax: .pooltools [subcommand]\nTools for creating herb/mining pools from live spawns.'),
-('pooltools start', 3, 'Syntax: .pooltools start [Description]\nStarts a new pooling session with the specified description.'),
-('pooltools def', 3, 'Syntax: .pooltools def [EntryID] [Chance] [EntryID] [Chance]...\nDefines which IDs of detected groups along with their chances.'),
-('pooltools add', 3, 'Syntax: .pooltools add [Radius]\nSearches for nodes defined in your template within 5 yards or the defined radius and adds to the current session.'),
-('pooltools remove', 3, 'Syntax: .pooltools remove\nRemoves the last group added via .pooltools add'),
-('pooltools end', 3, 'Syntax: .pooltools end\nLogs the SQL.'),
-('pooltools clear', 3, 'Syntax: .pooltools clear\nClears the current session from memory.');
+('pooltools', 3, 'Syntax: .pooltools $subcommand\nTools for creating gameobject pools ingame.\nTo use pooltools, uncomment Appender.Dev and Logger.sql.dev in the worldserver config.'),
+('pooltools start', 3, 'Syntax: .pooltools start #description\nStarts a pooling session with the specified description.'),
+('pooltools def', 3, 'Syntax: .pooltools def #GameobjectID #Chance #GameobjectID #Chance (...)\nDefines the Gameobject entries to be detected along with their associated chances.'),
+('pooltools add', 3, 'Syntax: .pooltools add [radius]\nAdds nearby gameobjects to the pooling session. Default radius is 5y.'),
+('pooltools remove', 3, 'Syntax: .pooltools remove\nRemoves the last group from the pooling session.'),
+('pooltools end', 3, 'Syntax: .pooltools end\nLogs the current pooling session and clears it.'),
+('pooltools clear', 3, 'Syntax: .pooltools clear\nClears the current pooling session.');
