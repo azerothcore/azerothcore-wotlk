32351bff6f0eb425081741ac2373922afcfc2341
blinkysc <37940565+blinkysc@users.noreply.github.com>
2025-12-26 11:07:36 -0600

    Refine DELETE condition in pending_db_world SQL

--- data/sql/updates/pending_db_world/rev_1763784400438549506.sql
+++ data/sql/updates/pending_db_world/rev_1763784400438549506.sql
@@ -5,7 +5,7 @@ INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`,
 (24095, 1, 25628, 1, 1, 51831);
 
 -- Conditions
-DELETE FROM `conditions` WHERE `SourceEntry` = 43036 AND `SourceTypeOrReferenceId` IN (13, 17);
+DELETE FROM `conditions` WHERE `SourceEntry` = 43036 AND `SourceTypeOrReferenceId` = 17;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (17, 0, 43036, 0, 0, 1, 1, 43059, 0, 0, 1, 0, 0, '', 'Spell 43036 (Dismembering Corpse) - Target must not already be transformed'),
 (17, 0, 43036, 0, 0, 36, 1, 0, 0, 0, 1, 0, 0, '', 'Spell 43036 (Dismembering Corpse) - Explicit target must be dead');
