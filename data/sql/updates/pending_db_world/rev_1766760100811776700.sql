82ae390850d1b389b929e7a993bee064d22a8b30
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-26 15:11:36 -0300

    fix(DB/Creature): Adjust Naxxramas Worshipper 10M/25M immunity mask (#24212)

---
+++ data/sql/updates/pending_db_world/rev_1766760100811776700.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1 WHERE (`entry` = 16506);
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|2|8|16|64|512|1024|4096|8192|32768|65536|131072|262144|524288|1048576|4194304|8388608|67108864|134217728|268435456|536870912 WHERE (`entry` = 29274);
