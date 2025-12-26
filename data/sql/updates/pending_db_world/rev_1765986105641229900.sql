16e89166e5d28f633ace8553c617af9b1e0a4d5e
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-26 15:19:46 -0300

    fix(DB/Creature): Sack of Relics (24439) update models and remove duplicate spawn (#24131)

---
+++ data/sql/updates/pending_db_world/rev_1765986105641229900.sql
@@ -0,0 +1,7 @@
+-- Delete Duplicate
+DELETE FROM `creature` WHERE (`id1` = 24439) AND (`guid` IN (142710));
+
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 24439);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(24439, 0, 24780, 1, 1, 51831),
+(24439, 1, 11686, 1, 0, 51831);
