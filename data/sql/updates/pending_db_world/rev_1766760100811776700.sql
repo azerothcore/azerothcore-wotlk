8ecacd9cbe163f4a8526544c2561ae4b47bb71bb
Gultask <100873791+Gultask@users.noreply.github.com>
2025-12-26 13:56:48 -0300

    Modify mechanic_immune_mask update for entry 29274

--- data/sql/updates/pending_db_world/rev_1766760100811776700.sql
+++ data/sql/updates/pending_db_world/rev_1766760100811776700.sql
@@ -1,3 +1,3 @@
 --
 UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1 WHERE (`entry` = 16506);
-UPDATE `creature_template` SET `mechanic_immune_mask` = 1021294170 WHERE (`entry` = 29274);
+UPDATE `creature_template` SET `mechanic_immune_mask` = `mechanic_immune_mask`|1021294170 WHERE (`entry` = 29274);
