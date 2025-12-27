837bbe3126e37c0840c3d968bf8841641b2dd2ba
heyitsbench <benjymansy123@gmail.com>
2025-12-26 22:09:25 -0500

    Init.

---
+++ data/sql/updates/pending_db_world/explosive-decoy-rabbit-model.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 29134;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(29134, 0, 6302, 1, 1, 51831),
+(29134, 1, 1924, 1, 0, 51831),
+(29134, 2, 2176, 1, 0, 51831),
+(29134, 3, 304, 1, 0, 51831);
