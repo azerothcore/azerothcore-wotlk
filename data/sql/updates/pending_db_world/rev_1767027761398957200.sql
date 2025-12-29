fe9c65103199b9cea6ff83b270a0fb0bfb176869
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-12-29 22:03:12 +0000

    Update rev_1767027761398957200.sql

--- data/sql/updates/pending_db_world/rev_1767027761398957200.sql
+++ data/sql/updates/pending_db_world/rev_1767027761398957200.sql
@@ -5,3 +5,6 @@ DELETE FROM `acore_string` WHERE `entry` IN (603, 604);
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
 (603, 'No account with that IP: {} was found.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (604, 'No account with that e-mail: {} was found.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- From: "No players found!" to "No characters were found!"
+UPDATE `acore_string` SET `content_default` = 'No characters were found!' WHERE `entry` = 330;
