8b9b0cfba5cf7f22c6fbfa43c03e0711d2e1ab9b
afdz1 <afdez1@icloud.com>
2026-01-01 02:29:18 -0500

    Updated to follow AC format

--- data/sql/updates/pending_db_world/debug-commands.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM `command` WHERE `name` IN ('list auras id', 'list auras name', 'account remove country', 'debug mapdata', 'debug visibilitydata', 'event info');
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('list auras id', 1, 'Syntax: .list auras id\n\nLists all active auras on the selected unit by spell ID.'),
-('list auras name', 1, 'Syntax: .list auras name\n\nLists all active auras on the selected unit by spell name.'),
-('account remove country', 3, 'Syntax: .account remove country <account>\n\nRemoves the country information associated with the specified account.'),
-('debug mapdata', 3, 'Syntax: .debug mapdata\n\nDisplays debug information about the current map.'),
-('debug visibilitydata', 3, 'Syntax: .debug visibilitydata\n\nDisplays debug information related to object visibility around the player.'),
-('event info', 2, 'Syntax: .event info [event_id]\n\nDisplays information about game events.');
--- data/sql/updates/pending_db_world/instance-commands.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM `command` WHERE `name` IN ('instance stats', 'cheat status','gm spectator','gm on','gm off','debug unitstate');
-INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('instance stats', 1, 'Syntax: .instance stats\n\nShows statistics about instances.'),
-('cheat status', 2, 'Syntax: .cheat status\n\nShows the cheats you currently have enabled.'),
-('gm spectator', 2, 'Syntax: .gm spectator on|off\n\nRequires .gm on. Allows the GM character to follow members of the opposite faction. You may need to change zones for the effect to apply.'),
-('gm on', 1, 'Syntax: .gm on\n\nTurns on GM flag.'),
-('gm off', 1, 'Syntax: .gm off\n\nTurns off GM flag.'),
-('debug unitstate', 3, 'Syntax: .debug unitstate [#unitstate]\n\nSets the unit state for the selected unit or displays current unit and react state.');
