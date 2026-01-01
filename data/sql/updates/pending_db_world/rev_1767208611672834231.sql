1fc66dc3dc2e3d9df64f716f0c4f3862063427ea
sogladev <sogladev@gmail.com>
2026-01-01 12:33:08 +0100

    fix(Core/Condition): CONDITION_RANDOM_DUNGEON difficulty param (#24283)

---
+++ data/sql/updates/pending_db_world/rev_1767208611672834231.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `conditions` SET `ConditionValue1` = 0 WHERE (`SourceTypeOrReferenceId` = 4) AND (`SourceGroup` = 24524) AND (`SourceEntry` = 52676) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 105);
