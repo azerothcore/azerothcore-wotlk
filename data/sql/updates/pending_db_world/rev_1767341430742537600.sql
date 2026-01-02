b08ce9ddaf9b07942d970114bf389cb351802f18
Ludwig <sudlud@users.noreply.github.com>
2026-01-02 09:48:50 +0100

    remove start end end times from automatically handled events

--- data/sql/updates/pending_db_world/rev_1767341430742537600.sql
+++ data/sql/updates/pending_db_world/rev_1767341430742537600.sql
@@ -1,2 +1,21 @@
 -- correctly link brewfest building event in ironforge to brewfest
 UPDATE `game_event` SET `holiday` = 372, `holidayStage` = 1 WHERE `eventEntry` = 70;
+
+-- remove start end end times from automatically handled events
+UPDATE `game_event` SET `start_time` = NULL, `end_time` = NULL WHERE `eventEntry` IN
+(
+    1,  -- Midsummer Fire Festival
+    2,  -- Winter Veil
+    7,  -- Lunar Festival
+    8,  -- Love is in the Air
+    9,  -- Noblegarden
+    10, -- Children's Week
+    11, -- Harvest Festival
+    12, -- Hallow's End
+    24, -- Brewfest
+    26, -- Pilgrim's Bounty
+    50, -- Pirates' Day
+    51, -- Day of the Dead
+    70, -- Brewfest Building (Iron Forge)
+    72  -- Fireworks Spectacular
+);
