80ff57dda8903071a480c45ed5c80067e28e1d0d
afdz1 <afdez1@icloud.com>
2026-01-01 19:06:15 -0500

    Update for codestyle.

--- data/sql/updates/pending_db_world/rev_1767311521159307200.sql
+++ data/sql/updates/pending_db_world/rev_1767311521159307200.sql
@@ -1,4 +1,4 @@
--- Update Smart Script to directly invoke whisper to player without needing to scan for closest player. 
+-- Update Smart Script to directly invoke whisper to player without needing to scan for closest player.
 DELETE FROM `smart_scripts` WHERE `id` = 0 AND `entryorguid` IN (19293, 19294);
 INSERT INTO `smart_scripts`
 (`entryorguid`, `event_type`,`event_chance`,`event_flags`, `event_param1`, `action_type`, `target_type`,`target_param1`, `comment`)
