4fec80121d6923c57b3a36eaf45c3a2d18b92997
Gultask <100873791+Gultask@users.noreply.github.com>
2026-01-03 06:43:39 -0300

    fix(DB/Loot): Adjust Shoveltusk Ligament drop rates (#24307)

---
+++ data/sql/updates/pending_db_world/rev_1767371382135786200.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature_loot_template` SET `Chance` = 40 WHERE `Entry` = 23691 AND `Item` = 33611;
+UPDATE `creature_loot_template` SET `Chance` = 40 WHERE `Entry` = 23690 AND `Item` = 33611;
+UPDATE `creature_loot_template` SET `Chance` = 20 WHERE `Entry` = 24791 AND `Item` = 33611;
