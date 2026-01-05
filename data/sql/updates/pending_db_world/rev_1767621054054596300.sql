2568b98c1443d4afaf6224e3ecbe2e8a550744e9
Gultask <100873791+Gultask@users.noreply.github.com>
2026-01-05 20:15:46 -0300

    fix(DB/Gameobject): Move flying ore node to the ground in Storm Peaks (#24328)
    Co-authored-by: sudlud <sudlud@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1767621054054596300.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `gameobject` SET `position_z` = 864.1913, `Comment` = 'original position_z: 870.297', `VerifiedBuild` = -47720 WHERE `guid` IN (5710989, 5710990, 5710991) AND `id` IN (189980, 189981, 191133);
