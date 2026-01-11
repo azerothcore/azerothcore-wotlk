3aa38cad05f82d2136b6bc21b555a68ee9d33c62
blinkysc <blinkysc@users.noreply.github.com>
2026-01-11 15:33:47 -0600

    fix(Core/Network): Rename AuthSession to WorldAuthSession to fix ODR violation

--- src/server/game/Server/WorldSocket.h
+++ src/server/game/Server/WorldSocket.h
@@ -65,7 +65,7 @@ struct ClientPktHeader
 };
 #pragma pack(pop)
 
-struct AuthSession;
+struct WorldAuthSession;
 
 class AC_GAME_API WorldSocket final : public Socket<WorldSocket>
 {
@@ -113,7 +113,7 @@ private:
     void SendPacketAndLogOpcode(WorldPacket const& packet);
     void HandleSendAuthSession();
     void HandleAuthSession(WorldPacket& recvPacket);
-    void HandleAuthSessionCallback(std::shared_ptr<AuthSession> authSession, PreparedQueryResult result);
+    void HandleAuthSessionCallback(std::shared_ptr<WorldAuthSession> authSession, PreparedQueryResult result);
     void LoadSessionPermissionsCallback(PreparedQueryResult result);
     void SendAuthResponseError(uint8 code);
 
