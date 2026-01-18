0ecf5204c5cc27c3c8c320630e31e740b9822e2e
sogla <sogladev@gmail.com>
2026-01-18 01:57:26 +0100

    update requirement for Khorium

--- data/sql/updates/pending_db_world/rev_1768532768590148239.sql
+++ data/sql/updates/pending_db_world/rev_1768532768590148239.sql
@@ -1,3 +1,4 @@
 --
 UPDATE `trainer_spell` SET `ReqSkillRank`=275 WHERE `SpellId`=29356;
 UPDATE `trainer_spell` SET `ReqSkillRank`=230 WHERE `SpellId`=16153;
+UPDATE `trainer_spell` SET `ReqSkillRank`=375 WHERE `SpellId`=29361;
