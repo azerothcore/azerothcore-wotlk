2d2de0b6179439ddab1b44efdfc5795863ba59fb
Gultask <100873791+Gultask@users.noreply.github.com>
2026-01-22 13:40:36 -0300

    Update rev_1769021542374622600.sql
    Co-Authored-By: Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1769021542374622600.sql
+++ data/sql/updates/pending_db_world/rev_1769021542374622600.sql
@@ -1,2 +1,2 @@
 -- Sniffed from 50664 Build
-UPDATE `creature_template` SET `unit_flags` = 768 WHERE (`entry` IN (31079, 31492));
+UPDATE `creature_template` SET `unit_flags` = `unit_flags`&~(2|131072), `unit_flags` = `unit_flags`|256 WHERE (`entry` IN (31079, 31492));
