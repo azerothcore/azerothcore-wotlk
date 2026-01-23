71250d9bb013b704480a1d1dcbbc652ae31b09b3
heyitsbench <benjymansy123@gmail.com>
2026-01-23 05:53:01 -0500

    Add additional fields.

--- data/sql/updates/pending_db_world/glimmering-pillar-npc-pos.sql
+++ data/sql/updates/pending_db_world/glimmering-pillar-npc-pos.sql
@@ -1,1 +1,1 @@
-UPDATE `creature` SET `position_x` = 6201.761, `position_y` = 4764.4507, `position_z` = 225.83981, `orientation` = 3.193952560424804687 WHERE `id1` = 29056 AND `guid` = 200007
+UPDATE `creature` SET `position_x` = 6201.761, `position_y` = 4764.4507, `position_z` = 225.83981, `orientation` = 3.193952560424804687, `curhealth` = 6722, `VerifiedBuild` = 54261, `CreateObject` = 1 WHERE `id1` = 29056 AND `guid` = 200007;
