6257d715e94aac1645bcfae0bb36bdad3e154eda
blinkysc <blinkysc@users.noreply.github.com>
2026-01-24 17:19:18 -0600

    Revert "fix(DB/World): Separate spell_proc schema change from data changes"
    This reverts commit e8549c05845d73002bbce6487c3c9c618b709cf4.

--- data/sql/updates/pending_db_world/rev_1769296150491468818.sql
+++
@@ -1,3 +0,0 @@
--- QAston Proc System - Add DisableEffectsMask column to spell_proc table
--- Schema change separated from data changes to avoid re-run issues
-ALTER TABLE `spell_proc` ADD COLUMN `DisableEffectsMask` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `AttributesMask`;
