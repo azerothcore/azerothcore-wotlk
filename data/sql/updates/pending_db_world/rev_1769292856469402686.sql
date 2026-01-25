192bc20f58ede0339164ae1c5073bba35a62d58a
blinkysc <blinkysc@users.noreply.github.com>
2026-01-25 10:12:40 -0600

    fix(DB/World): Register spell_dk_killing_machine script

--- data/sql/updates/pending_db_world/rev_1769292856469402686.sql
+++ data/sql/updates/pending_db_world/rev_1769292856469402686.sql
@@ -4,3 +4,8 @@
 DELETE FROM `spell_proc` WHERE `SpellId` = -56342;
 INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
 (-56342, 0, 9, 24, 134217728, 147456, 0, 0, 2, 0, 2, 0, 0, 0, 22000, 0);
+
+-- Killing Machine - register spell script
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dk_killing_machine';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(51124, 'spell_dk_killing_machine');
