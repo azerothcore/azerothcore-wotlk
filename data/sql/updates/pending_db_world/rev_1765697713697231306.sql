79ac82b82f6bbeae0dfc6a251214a4a847ac12d2
Andrew <47818697+Nyeriah@users.noreply.github.com>
2026-01-27 21:13:09 -0300

    Update data/sql/updates/pending_db_world/rev_1765697713697231306.sql
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1765697713697231306.sql
+++ data/sql/updates/pending_db_world/rev_1765697713697231306.sql
@@ -3,4 +3,7 @@ SET @ENTRY := 24705;
 
 UPDATE `creature_template` SET `flags_extra` = 128 WHERE `entry` = @ENTRY;
 
-UPDATE `creature_template_model` SET `CreatureDisplayID` = 17612 WHERE `CreatureID` = @ENTRY AND `CreatureDisplayID` = 11686;
+DELETE FROM `creature_template_model` WHERE `CreatureID` = @ENTRY;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(@ENTRY, 0, 1126, 1, 0, 51831),
+(@ENTRY, 1, 11686, 1, 1, 51831);
