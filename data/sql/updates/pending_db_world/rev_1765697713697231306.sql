c4eec5231bed3f67cb7ae80b9b79883f383b68ad
zackchadwick <73787423+zackchadwick@users.noreply.github.com>
2026-01-27 20:38:25 -0500

    fix(DB/creature): Howling Fjord Rope Swing Invisible Vehicle Stalker visibility (#24101)
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
    Co-authored-by: Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1765697713697231306.sql
@@ -0,0 +1,9 @@
+--
+SET @ENTRY := 24705;
+
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 128 WHERE `entry` = @ENTRY;
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = @ENTRY;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(@ENTRY, 0, 1126, 1, 0, 51831),
+(@ENTRY, 1, 11686, 1, 1, 51831);
