e1651e269bfd031207560e619d95ad9c346c2813
Gultask <100873791+Gultask@users.noreply.github.com>
2026-01-31 17:32:29 -0300

    Create rev_1769891519807069500.sql

---
+++ data/sql/updates/pending_db_world/rev_1769891519807069500.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `smart_scripts` SET `event_chance` = 20 WHERE (`entryorguid` = 12380) AND (`source_type` = 0) AND (`id` IN (1));
+UPDATE `smart_scripts` SET `event_chance` = 20 WHERE (`entryorguid` = 116) AND (`source_type` = 0) AND (`id` IN (1));
+UPDATE `smart_scripts` SET `event_chance` = 30 WHERE (`entryorguid` = 4845) AND (`source_type` = 0) AND (`id` IN (0));
