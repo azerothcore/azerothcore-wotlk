d5006f55ef0af73d1f2cf60301ccd430ff63ee36
Gultask <100873791+Gultask@users.noreply.github.com>
2026-02-02 18:21:52 -0300

    Remove faction update from creature_template

--- data/sql/updates/pending_db_world/rev_1770063321798974800.sql
+++ data/sql/updates/pending_db_world/rev_1770063321798974800.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `creature_template` SET `minlevel` = 1, `maxlevel` = 1, `faction` = 1764 WHERE (`entry` = 19381);
+UPDATE `creature_template` SET `minlevel` = 1, `maxlevel` = 1 WHERE (`entry` = 19381);
