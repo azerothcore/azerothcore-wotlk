37ddc82d3ddb4a94d800d3fed094e93d26a9ed9a
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2026-02-02 11:02:24 +0000

    Update rev_1770028284372314400.sql

--- data/sql/updates/pending_db_world/rev_1770028284372314400.sql
+++ data/sql/updates/pending_db_world/rev_1770028284372314400.sql
@@ -1,2 +1,2 @@
--- Removes "Warsong Report" from "Battered Junkbox"
-DELETE FROM `item_loot_template` WHERE `Entry` = 16882 AND `Item` = 16746;
+-- Removes "Warsong Report" and "Battered Junkbox" (yes remove itself from itself) from "Battered Junkbox"
+DELETE FROM `item_loot_template` WHERE `Entry` = 16882 AND `Item` IN (16746, 16882);
