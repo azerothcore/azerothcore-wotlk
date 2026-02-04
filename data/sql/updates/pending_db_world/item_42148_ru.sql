ad8d54b56207b8c0ed5495c154509e214b614f5f
chaosua <544218+chaosua@users.noreply.github.com>
2026-02-04 17:35:37 +0200

    fix socket requirement description ruRU locale

---
+++ data/sql/updates/pending_db_world/item_42148_ru.sql
@@ -0,0 +1,3 @@
+DELETE FROM `item_template_locale` WHERE `ID`='42148' AND `locale`='ruRU';
+INSERT INTO `item_template_locale` (`ID`, `locale`, `Name`, `Description`, `VerifiedBuild`) VALUES 
+(42148, 'ruRU', 'Сверкающее око дракона', 'Для желтого гнезда.', 15050);
