fbb25280cbbbe471de2f389cb46a308a84093aab
Gultask <100873791+Gultask@users.noreply.github.com>
2026-02-06 18:04:56 -0300

    Update rev_1770302622700625500.sql

--- data/sql/updates/pending_db_world/rev_1770302622700625500.sql
+++ data/sql/updates/pending_db_world/rev_1770302622700625500.sql
@@ -37,7 +37,6 @@ DELETE FROM `game_event` WHERE `eventEntry` = 97;
 INSERT INTO `game_event` (`eventEntry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `holidayStage`, `description`, `world_event`, `announce`) VALUES
 (97, NULL, NULL, 5184000, 2592000, 0, 0, 'Terokk Boss Event', 0, 2);
 
-DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 23377);
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = -12478);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (-12478, 0, 1000, 0, 60, 0, 100, 0, 10000, 10000, 20000, 30000, 0, 0, 11, 40655, 0, 0, 0, 0, 0, 19, 21838, 60, 0, 0, 0, 0, 0, 0, 'Skyguard Ace - On Update - Cast \'Skyguard Flare\''),
