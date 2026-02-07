644ba4d5f4a18b782eee5728eecdc02ee85a48ba
Andrew <47818697+Nyeriah@users.noreply.github.com>
2026-02-07 17:18:43 -0300

    Apply suggestion from @Nyeriah

--- data/sql/updates/pending_db_world/rev_1738958242619.sql
+++ data/sql/updates/pending_db_world/rev_1738958242619.sql
@@ -1,5 +1,5 @@
 --
 DELETE FROM `command` WHERE `name` IN ('reload creature', 'reload gameobject', 'npc load', 'gobject load');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('npc load', 3, 'Syntax: .npc load #spawnId\nLoad a creature spawn from the database into the world by its spawn ID.'),
+('npc load', 3, 'Syntax: .npc load #spawnId\nLoad a creature spawn from the database into the world by its GUID.'),
 ('gobject load', 3, 'Syntax: .gobject load #spawnId\nLoad a gameobject spawn from the database into the world by its spawn ID.');
