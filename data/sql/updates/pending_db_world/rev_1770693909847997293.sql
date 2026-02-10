519007afc85ce84dcc7bb92c06887572b6fe86b0
sogla <sogladev@gmail.com>
2026-02-10 04:05:49 +0100

    fix(DB/Gossip): Enable BoxCoded for Zas'Tysh and Tharl Stonebleeder

---
+++ data/sql/updates/pending_db_world/rev_1770693909847997293.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `gossip_menu_option` SET `BoxCoded` = 1 WHERE `MenuID` = 6565 AND `OptionID` IN (0, 1, 2);
+UPDATE `gossip_menu_option` SET `BoxCoded` = 1 WHERE `MenuID` = 7034 AND `OptionID` = 0;
