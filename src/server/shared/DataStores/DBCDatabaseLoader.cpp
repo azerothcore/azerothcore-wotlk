975a612faba25d4558f8d75a666c85dee0d28626
iThorgrim <contact@warlog.dev>
2026-02-13 11:29:11 +0100

    remove trailingspace

--- src/server/shared/DataStores/DBCDatabaseLoader.cpp
+++ src/server/shared/DataStores/DBCDatabaseLoader.cpp
@@ -42,7 +42,7 @@ char* DBCDatabaseLoader::Load(uint32& records, char**& indexTable)
     // spell_dbc remains in World database for legacy/compatibility reasons
     // All other DBC tables are in the DBC database
     bool useWorldDB = (std::string(_sqlTableName) == "spell_dbc");
-    
+
     // no error if empty set
     QueryResult result = useWorldDB ? WorldDatabase.Query(query) : DBCDatabase.Query(query);
     if (!result)
