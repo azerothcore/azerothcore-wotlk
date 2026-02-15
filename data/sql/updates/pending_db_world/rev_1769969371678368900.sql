1dcd086d420fad3e56415523a11b8e1234ffaa95
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2026-02-15 12:22:40 -0300

    Revert "copilot suggestions"
    This reverts commit cab6c366232d1125991cca6195cb9e20d4ea5c4d.

--- data/sql/updates/pending_db_world/rev_1769969371678368900.sql
+++ data/sql/updates/pending_db_world/rev_1769969371678368900.sql
@@ -2,13 +2,13 @@
 DELETE FROM `spell_group_stack_rules` WHERE `group_id` = 53000;
 INSERT INTO `spell_group_stack_rules` (`group_id`, `stack_rule`, `description`)
 VALUES
-(1127, 4, 'Toughness - Only Highest Rank');
+(53000, 4, 'Toughness - Only Highest Rank');
 
 DELETE FROM `spell_group` WHERE `id` = 53000;
 INSERT INTO `spell_group` (`id`, `spell_id`) VALUES
-(1127, 53120),
-(1127, 53121),
-(1127, 53122),
-(1127, 53123),
-(1127, 53124),
-(1127, 53040);
+(53000, 53120),
+(53000, 53121),
+(53000, 53122),
+(53000, 53123),
+(53000, 53124),
+(53000, 53040);
