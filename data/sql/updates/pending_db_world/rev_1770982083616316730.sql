19f418f4893115fde76b3e4fbe1328ec6a80b6bd
sogladev <sogladev@gmail.com>
2026-02-15 13:00:27 +0100

    fix(Scripts/ObsidianSanctum): enable Sartharion drakes patrols (#24702)
    Co-authored-by: Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1770982083616316730.sql
@@ -0,0 +1,4 @@
+DELETE FROM `waypoint_data` WHERE `id` IN (304490, 304520, 304510);
+
+-- CREATURE_FLAG_EXTRA_HARD_RESET
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 0x80000000 WHERE (`entry` = 28860);
