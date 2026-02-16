a1fbecd61a46995fecb5231b834fee7ab7740795
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2026-02-16 11:32:24 +0000

    Update rev_1767027761398957200.sql

--- data/sql/updates/pending_db_world/rev_1767027761398957200.sql
+++ data/sql/updates/pending_db_world/rev_1767027761398957200.sql
@@ -7,9 +7,7 @@ INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_f
 (605, 'No account with that e-mail: {} was found.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (606, 'Missing error handling prior to function: "{}" in file: "cs_lookup.cpp", line: "{}", Fix your handle in "HandleLookupPlayer%"', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (607, 'Found a total of: {} accounts.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(608, 'Found a total of: {} characters.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-
-
+(608, 'Found a total of: {} characters.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- From: "No players found!" to "No characters were found!"
 UPDATE `acore_string` SET `content_default` = 'No characters were found!' WHERE `entry` = 330;
