9964d227dc3321df178f26be3c23b762b5325a75
Gultask <100873791+Gultask@users.noreply.github.com>
2026-02-23 07:12:55 -0300

    Update Playing_Along.sql

--- data/sql/updates/pending_db_world/Playing_Along.sql
+++ data/sql/updates/pending_db_world/Playing_Along.sql
@@ -1,3 +1,2 @@
-
 -- Remove conditions
-DELETE FROM `conditions` WHERE `SourceEntry` = 12528;
+DELETE FROM `conditions` WHERE `SourceEntry` = 12528 AND `SourceTypeOrReferenceId` = 19 AND `ConditionValue1` = 12654;
