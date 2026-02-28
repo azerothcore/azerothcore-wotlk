fc40cb0472c0a504c04016a852a019abff7f6aca
Gultask <100873791+Gultask@users.noreply.github.com>
2026-02-28 17:39:35 -0300

    Update rev_1772310671442881400.sql

--- data/sql/updates/pending_db_world/rev_1772310671442881400.sql
+++ data/sql/updates/pending_db_world/rev_1772310671442881400.sql
@@ -26,8 +26,7 @@ INSERT INTO `gameobject_summon_groups` (`summonerId`, `summonerType`, `groupId`,
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 30871;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 30871);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(30871, 0, 0, 0, 20, 0, 100, 0, 13131, 0, 0, 0, 0, 0, 241, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Brigg Smallshanks - On Quest \'Junk in My Trunk\' Finished - Spawn Gameobject Group'),
-(30871, 0, 1, 0, 72, 0, 100, 0, 1, 0, 0, 0, 0, 0, 241, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Brigg Smallshanks - On Quest \'Junk in My Trunk\' Finished - Spawn Gameobject Group');
+(30871, 0, 0, 0, 20, 0, 100, 0, 13131, 0, 0, 0, 0, 0, 241, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Brigg Smallshanks - On Quest \'Junk in My Trunk\' Finished - Spawn Gameobject Group');
 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 22) AND (`SourceEntry` = 30871) AND (`SourceId` = 0) AND (`ConditionTypeOrReference` = 30) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 192946) AND (`ConditionValue2` = 5) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
