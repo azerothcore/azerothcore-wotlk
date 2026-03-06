b0bf94f3d7b5dba97e96d978130c5230951482f5
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-03-06 21:55:54 +0100

    Create rev_1772830500.sql

---
+++ data/sql/updates/pending_db_world/rev_1772830500.sql
@@ -0,0 +1,5 @@
+
+-- Create new Guid.
+DELETE FROM `creature` WHERE (`id1` = 33430) AND (`guid` IN (136607));
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `Comment`, `VerifiedBuild`) VALUES
+(136607, 33430, 0, 0, 603, 0, 0, 3, 1, 0, 2215.2, -16.2571, 424.161, 3.04182, 604800, 5, 0, 195495, 0, 1, 0, 0, 0, '', NULL, 0);
