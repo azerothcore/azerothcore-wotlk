abe5a22d78aca76d67256bafece1357c64e606bf
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-03-13 10:45:14 +0100

    Update rev_1773159064.sql

--- data/sql/updates/pending_db_world/rev_1773159064.sql
+++ data/sql/updates/pending_db_world/rev_1773159064.sql
@@ -66,5 +66,5 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 -- Set Condition.
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 15) AND (`SourceGroup` = 9765) AND (`SourceEntry` = 0) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` IN (1, 47)) AND (`ConditionTarget` = 0) AND (`ConditionValue1` IN (12733, 54238)) AND (`ConditionValue2` IN (0, 8)) AND (`ConditionValue3` = 0);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(15, 9765, 0, 0, 0, 47, 0, 12733, 8, 0, 0, 0, 0, '', 'Death Knight Initiate challenge option only avaiable if the player has Death\'s Challenge quest incomplete.'),
-(15, 9765, 0, 0, 0, 1, 0, 54238, 0, 0, 1, 0, 0, '', 'Death Knight Initiate challenge option only avaiable if the player his not in duel.');
+(15, 9765, 0, 0, 0, 47, 0, 12733, 8, 0, 0, 0, 0, '', 'Death Knight Initiate challenge option only available if the player has Death\'s Challenge quest incomplete.'),
+(15, 9765, 0, 0, 0, 1, 0, 54238, 0, 0, 1, 0, 0, '', 'Death Knight Initiate challenge option only available if the player is not in duel.');
