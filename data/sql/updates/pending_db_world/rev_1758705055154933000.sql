179aee68e0fd3b171517756f6f9d56318a9c51e8
sudlud <sudlud@users.noreply.github.com>
2026-03-14 21:13:52 +0100

    Apply suggestions from code review
    Co-authored-by: sudlud <sudlud@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1758705055154933000.sql
+++ data/sql/updates/pending_db_world/rev_1758705055154933000.sql
@@ -1,9 +1,9 @@
 --
-DELETE FROM `smart_scripts` WHERE (`entryorguid` in (-96556)) AND (`source_type` = 0);
+DELETE FROM `smart_scripts` WHERE (`entryorguid` in (-96556, 26115)) AND (`source_type` = 0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (26115,0,0,0,25,0,100,512, 0,0,0,0,0,0, 53,0,261150,1,0,0,2, 1,0,0,0,0, 0,0,0,0,'Darkfallen Bloodbearer - On Reset - Start Waypoint');
 
-DELETE FROM `waypoints` WHERE (`entry` in (965560));
+DELETE FROM `waypoints` WHERE (`entry` in (965560, 261150));
 INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `point_comment`) VALUES
 (261150, 1, 3923.71, 3581.69, 70.9999, NULL, 0, 'Darkfallen Bloodbearer'),
 (261150, 2, 3918.45, 3585.35, 69.3552, NULL, 0, 'Darkfallen Bloodbearer'),
@@ -27,4 +27,3 @@ DELETE FROM `creature_addon` WHERE (`guid` = 96556);
 
 UPDATE `creature` SET `MovementType` = 2, `wander_distance` = 0 WHERE `id1` = 26115 and `guid` = 96556;
 
-UPDATE  `creature_template` SET `MovementType` = 2 WHERE `entry` = 26115 and `AIName` = 'SmartAI';
