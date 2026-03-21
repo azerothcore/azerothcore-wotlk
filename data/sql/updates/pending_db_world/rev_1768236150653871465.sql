17f2ffe1bb989b3b9e8fa31851716696204afa1a
Andrew <47818697+Nyeriah@users.noreply.github.com>
2026-03-21 00:05:15 -0300

    Update language string for pinfo online time

--- data/sql/updates/pending_db_world/rev_1768236150653871465.sql
+++ data/sql/updates/pending_db_world/rev_1768236150653871465.sql
@@ -1,4 +1,4 @@
 -- Add language string for pinfo online time
 DELETE FROM `acore_string` WHERE `entry` = 35410;
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
-(35410, '¦ Online for: {}', '¦ 온라인 시간: {}', '¦ En ligne depuis : {}', '¦ Online seit: {}', '¦ 在线时长：{}', '¦ 線上時間：{}', '¦ Conectado durante: {}', '¦ Conectado durante: {}', '¦ Онлайн: {}');
+(35410, '¦ Online for: {}', '¦ 온라인 시간: {}', '¦ Online depuis : {}', '¦ Online seit: {}', '¦ 在线时长：{}', '¦ 線上時間：{}', '¦ Conectado durante: {}', '¦ Conectado durante: {}', '¦ Онлайн: {}');
