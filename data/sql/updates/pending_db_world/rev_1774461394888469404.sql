247734cf53e1f1afd7ca7c5f47d67abff39c455a
sogla <sogladev@gmail.com>
2026-03-25 18:57:27 +0100

    fix(DB/Creature): add Power Spark movement to Power Spark (1)

---
+++ data/sql/updates/pending_db_world/rev_1774461394888469404.sql
@@ -0,0 +1,4 @@
+-- Set Disable Gravity from Power Spark to Power Spark (1)
+DELETE FROM `creature_template_movement` WHERE (`CreatureId` = 32187);
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
+(32187, 0, 0, 1, 0, 0, 0, 0);
