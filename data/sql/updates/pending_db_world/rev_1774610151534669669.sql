df6d798b206c112470acc15e1cdb6adbd6cd679a
sogla <sogladev@gmail.com>
2026-03-27 12:58:45 +0100

    fixup! fix(DB/Immunities): update magic immunity for Atal'ai Deathwalker

--- data/sql/updates/pending_db_world/rev_1774610151534669669.sql
+++ data/sql/updates/pending_db_world/rev_1774610151534669669.sql
@@ -1,4 +1,3 @@
 --
 -- School immunities are set by `creature_addon.auras` immunity auras
-DELETE FROM `creature_immunities` WHERE `ID` = -368; -- only used by 8317
 UPDATE `creature_template` SET `CreatureImmunitiesId` = '-367' WHERE (`entry` = 8317);
