5a7a01d3fc0adbc61b71b09e09667f422be11b80
sogla <sogladev@gmail.com>
2026-03-27 11:52:41 +0100

    fix(DB/Creature): immunities, spawn and equipment of Koltira Deathweaver
    Scripts/Ebon Hold: Bloody Breakout
    [1]: https://github.com/TrinityCore/TrinityCore/commit/2b14b720da2bc38c84f373cb3ff4bcc24c1ddcf0
    
    Co-authored-by: Sorikoff <46191832+Sorikoff@users.noreply.github.com>
    
    update Koltira Deathweaver immunities

---
+++ data/sql/updates/pending_db_world/rev_1774608614129288192.sql
@@ -0,0 +1,9 @@
+--
+DELETE FROM `creature_equip_template` WHERE (`CreatureID` = 28912);
+INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES
+(28912, 1, 38633, 0, 0, 42010);
+
+UPDATE `creature` SET `spawntimesecs` = 60 WHERE (`id1` = 28912) AND (`guid` IN (130354));
+
+DELETE FROM `creature_immunities` WHERE `ID` = -99; -- only used by 28912
+UPDATE `creature_template` SET `CreatureImmunitiesId` = -93 WHERE (`entry` = 28912);
