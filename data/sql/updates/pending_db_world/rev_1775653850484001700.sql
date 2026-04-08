f7e590cbea1a42cc906e1fffe5135d8fc3ef65a5
EricksOliveira <ericksoliveira258@gmail.com>
2026-04-08 11:15:54 -0300

    fix point correct

--- data/sql/updates/pending_db_world/rev_1775653850484001700.sql
+++ data/sql/updates/pending_db_world/rev_1775653850484001700.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `waypoint_data` SET `delay` = 40000 WHERE `id` = 1281350 AND `point` IN (1, 7) AND `delay` <> 40000;
+UPDATE `waypoint_data` SET `delay` = 40000 WHERE `id` = 1281350 AND `point` IN (1, 8) AND `delay` <> 40000;
