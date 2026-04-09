1e32d6e98bba8a3866ee897cc7e51e98174c8ebf
EricksOliveira <ericksoliveira258@gmail.com>
2026-04-09 09:15:14 -0300

    add time medium

--- data/sql/updates/pending_db_world/rev_1775653850484001700.sql
+++ data/sql/updates/pending_db_world/rev_1775653850484001700.sql
@@ -1,3 +1,3 @@
 --
-UPDATE `waypoint_data` SET `delay` = 40000 WHERE `id` = 1281350 AND `point` = 1;
-UPDATE `waypoint_data` SET `delay` = 20000 WHERE `id` = 1281350 AND `point` = 8;
+UPDATE `waypoint_data` SET `delay` = 32000 WHERE `id` = 1281350 AND `point` = 1;
+UPDATE `waypoint_data` SET `delay` = 38000 WHERE `id` = 1281350 AND `point` = 8;
