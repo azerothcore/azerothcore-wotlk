c51210fdb668cdb3ea901f932505b5bc66c8227d
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-04-13 00:30:45 +0200

    Create rev_1776033024.sql

---
+++ data/sql/updates/pending_db_world/rev_1776033024.sql
@@ -0,0 +1,27 @@
+
+-- Shadron
+UPDATE `creature_template` SET `lootid` = 30451 WHERE (`entry` = 30451);
+UPDATE `creature_template` SET `lootid` = 31520 WHERE (`entry` = 31520);
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` IN (30451, 31520)) AND (`Item` IN (47241));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(30451, 47241, 0, 100, 0, 1, 0, 1, 1, ''),
+(31520, 47241, 0, 100, 0, 1, 0, 1, 1, '');
+
+-- Vesperon
+UPDATE `creature_template` SET `lootid` = 30449 WHERE (`entry` = 30449);
+UPDATE `creature_template` SET `lootid` = 31535 WHERE (`entry` = 31535);
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` IN (30449, 31535)) AND (`Item` IN (47241));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(30449, 47241, 0, 100, 0, 1, 0, 1, 1, ''),
+(31535, 47241, 0, 100, 0, 1, 0, 1, 1, '');
+
+-- Tenebron
+UPDATE `creature_template` SET `lootid` = 30452 WHERE (`entry` = 30452);
+UPDATE `creature_template` SET `lootid` = 31534 WHERE (`entry` = 31534);
+
+DELETE FROM `creature_loot_template` WHERE (`Entry` IN (30452, 31534)) AND (`Item` IN (47241));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(30452, 47241, 0, 100, 0, 1, 0, 1, 1, ''),
+(31534, 47241, 0, 100, 0, 1, 0, 1, 1, '');
