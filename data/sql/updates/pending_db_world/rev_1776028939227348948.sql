e8cdb611820ccacda97fa3b568d86723b3b5bb82
sogla <sogladev@gmail.com>
2026-04-12 23:24:10 +0200

    fix(DB/Loot): Allow 10m Heart of Magic to be looted in 25man EoE
    Add 10m Heart of Magic to 25m Chest

---
+++ data/sql/updates/pending_db_world/rev_1776028939227348948.sql
@@ -0,0 +1,9 @@
+--
+DELETE FROM `gameobject_loot_template` WHERE (`Entry` = 26860);
+INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(26860, 44651, 0, 100, 1, 1, 1, 1, 1, 'Heart of Magic - Heart of Magic - 25m'),
+(26860, 44650, 0, 100, 1, 1, 2, 1, 1, 'Heart of Magic - Heart of Magic - 10m');
+
+DELETE FROM `gameobject_questitem` WHERE (`GameObjectEntry` = 194159) AND (`Idx` IN (1));
+INSERT INTO `gameobject_questitem` (`GameObjectEntry`, `Idx`, `ItemId`, `VerifiedBuild`) VALUES
+(194159, 1, 44650, 0);
