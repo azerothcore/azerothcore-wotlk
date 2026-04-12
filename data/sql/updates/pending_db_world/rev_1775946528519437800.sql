2a1e73fe4285a524fdb84aa9897e1a5bfec943ee
youdeoo <youdeoooo@gmail.com>
2026-04-12 08:49:02 +0800

    Fix spell_proc entries for spells 60485 and 60519

--- data/sql/updates/pending_db_world/rev_1775946528519437800.sql
+++ data/sql/updates/pending_db_world/rev_1775946528519437800.sql
@@ -3,4 +3,3 @@ DELETE FROM `spell_proc` WHERE `SpellId` IN (60485, 60519);
 INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
 (60485, 0, 0, 0, 0, 0, 81920, 3, 4, 0, 0, 0, 0, 0, 0, 0),
 (60519, 0, 0, 0, 0, 0, 82944, 3, 2, 0, 0, 0, 0, 0, 50000, 0);
-
