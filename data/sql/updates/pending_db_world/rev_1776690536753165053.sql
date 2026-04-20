eb39e72f093c8f73ffe77a0eed74fb19247e90c8
sogla <sogladev@gmail.com>
2026-04-20 15:10:09 +0200

    fix(DB/Trainer): low level trainers only train up to Level 6 abilities

---
+++ data/sql/updates/pending_db_world/rev_1776690536753165053.sql
@@ -0,0 +1,5 @@
+--
+-- Mai'ah the mage trainer only trains up to level 6
+UPDATE `creature_default_trainer` SET `TrainerId` = 17 WHERE (`CreatureId` = 5884);
+-- Shanda night elf priest trainer only trainers up to level 6
+UPDATE `creature_default_trainer` SET `TrainerId` = 12 WHERE (`CreatureId` = 3595);
