9ca7b7713e4a6b6412f2df6b6ea453019d4c8c29
sogla <sogladev@gmail.com>
2026-04-22 17:10:34 +0200

    fix(DB/Conditions): Vesperon's (solo) Twilight Torment hits players
    previously, this hit the phased Disciple only.

---
+++ data/sql/updates/pending_db_world/rev_1776870484034455872.sql
@@ -0,0 +1,6 @@
+--
+-- Removes 'Twilight Torment - Target players which has phase mask 1';
+-- This does not work as described in the comment, nor is it accurate.
+-- This condition is also only set for the Vesperon Twilight Torment
+-- application, not for the Sartharion fight.
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 57935) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 26) AND (`ConditionTarget` = 1) AND (`ConditionValue1` = 1) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
