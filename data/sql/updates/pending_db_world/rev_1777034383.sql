b2eec5dcdb134510405a4fc08868ffc4628eb487
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-04-24 14:40:41 +0200

    Create rev_1777034383.sql

---
+++ data/sql/updates/pending_db_world/rev_1777034383.sql
@@ -0,0 +1,3 @@
+
+-- Edit Condition.
+UPDATE `conditions` SET `ConditionTarget` = 1 WHERE (`SourceTypeOrReferenceId` = 20) AND (`SourceGroup` = 0) AND (`SourceEntry` = 28406) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 106) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 0) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
