90b99186875651a4dc88f3a498fb6152de9b3c99
sogla <sogladev@gmail.com>
2026-04-25 01:16:50 +0200

    fix(DB/Loot): Sartharion 25M always drops 2 T7.5 Tokens

---
+++ data/sql/updates/pending_db_world/rev_1777072262285005362.sql
@@ -0,0 +1,12 @@
+--
+-- Sartharion 25M drops 2x T7.5 Gloves
+SET @REF := 34370
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 31311) AND (`Item` IN (5, 40628, 40629, 40630));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(31311, 5, -@REF, 100, 0, 1, 0, 2, 2, 'Sartharion (1) - (ReferenceTable)');
+
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 34349) AND (`Item` IN (40752, 40628, 40629, 40630));
+INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(@REF, 40628, 0, 100, 0, 1, 0, 1, 1, 'T7.5 Gloves Token'),
+(@REF, 40629, 0, 100, 0, 1, 0, 1, 1, 'T7.5 Gloves Token'),
+(@REF, 40630, 0, 100, 0, 1, 0, 1, 1, 'T7.5 Gloves Token');
