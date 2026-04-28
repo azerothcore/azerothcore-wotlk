681dcbeb3450f5b82891f3b603281c7cada49003
EricksOliveira <ericksoliveira258@gmail.com>
2026-04-28 08:54:35 -0300

    fix

--- data/sql/updates/pending_db_world/rev_1777307230974435500.sql
+++ data/sql/updates/pending_db_world/rev_1777307230974435500.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `smart_scripts` SET `action_param2` = 32 WHERE `entryorguid` = 29404 AND `source_type` = 0 AND `id` = 0;
+UPDATE `smart_scripts` SET `action_param2` = 0 WHERE `entryorguid` = 29404 AND `source_type` = 0 AND `id` = 0;
