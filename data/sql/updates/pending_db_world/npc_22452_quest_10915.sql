88aceb1a65822d36229413146e59c3ff1cf62926
Steffen Schmidt <Steffen.Schmidt@bit-in.de>
2026-04-30 19:49:24 +0000

    chore: :lipstick: final touches

--- data/sql/updates/pending_db_world/npc_22452_quest_10915.sql
+++ data/sql/updates/pending_db_world/npc_22452_quest_10915.sql
@@ -9,5 +9,5 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 22452;
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 22452);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(22452, 0, 0, 0, 54, 0, 100, 0, 500, 500, 0, 0, 0, 0, 80, 2245200, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Reanimated Exarch - On Update - Run Script'),
+(22452, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 0, 80, 2245200, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Reanimated Exarch - On Just Summoned - Run Script'),
 (22452, 0, 1, 0, 0, 0, 100, 1, 2000, 4000, 0, 0, 0, 0, 11, 8258, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Reanimated Exarch - In Combat - Cast \'Devotion Aura\' (No Repeat)');
