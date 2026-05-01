757f5160cf1680620e17f24cb3190d707dd56219
Steffen Schmidt <Steffen.Schmidt@bit-in.de>
2026-05-01 10:28:47 +0000

    refactor: :recycle: move spell cast to quest

--- data/sql/updates/pending_db_world/quest_12630_npc_28518.sql
+++ data/sql/updates/pending_db_world/quest_12630_npc_28518.sql
@@ -1,6 +1,8 @@
 
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28518;
+-- UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28518;
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 28518) AND (`source_type` = 0) AND (`id` IN (9));
-INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(28518, 0, 9, 0, 19, 0, 100, 257, 12630, 0, 0, 0, 0, 0, 11, 51864, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Stefan Vadu - On Quest \'Kickin\' Nass and Takin\' Manes\' Taken - Cast \'Player Summon Nass\' (No Repeat)');
+-- DELETE FROM `smart_scripts` WHERE (`entryorguid` = 28518) AND (`source_type` = 0) AND (`id` IN (9));
+-- INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+-- (28518, 0, 9, 0, 19, 0, 100, 257, 12630, 0, 0, 0, 0, 0, 11, 51864, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Stefan Vadu - On Quest \'Kickin\' Nass and Takin\' Manes\' Taken - Cast \'Player Summon Nass\' (No Repeat)');
+
+UPDATE `quest_template_addon` SET `SourceSpellID` = 51864 WHERE (`ID` = 12630);
