6c643bbf7a2cb84169bd9b625e13936236904cf1
blinkysc <blinkysc@users.noreply.github.com>
2026-05-05 07:42:22 -0500

    fix(DB/Spells): Talisman of Troll Divinity direct heal proc

---
+++ data/sql/updates/pending_db_world/rev_1777984830390464692.sql
@@ -0,0 +1,4 @@
+-- Talisman of Troll Divinity (37734) - Touched by a Troll proc on direct heals
+DELETE FROM `spell_proc` WHERE `SpellId` = 60517;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
+(60517, 0, 0, 0, 0, 0, 0x4000, 2, 2, 0, 0, 0, 0, 100, 0, 0);
