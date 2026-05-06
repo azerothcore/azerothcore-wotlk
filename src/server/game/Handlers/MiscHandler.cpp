687a998e3f010074a61768a9d397422b1f5cca9d
polly <mpfans@vip.qq.com>
2026-05-06 21:59:49 +0800

    fix(Core/WhoList): restore AllowTwoSide.WhoList config functionality
    The RBAC refactor in 779331fc4b1d35577c34923c90459764f5834c04 replaced the AllowTwoSide.WhoList config check with RBAC_PERM_TWO_SIDE_WHO_LIST permission check, breaking the worldserver.conf setting.
    
    Restore the original behavior: AllowTwoSide.WhoList = 1 allows cross-faction who list queries, 0 disables them.

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -294,7 +294,7 @@ void WorldSession::HandleWhoOpcode(WorldPacket& recvData)
     for (auto const& target : sWhoListCacheMgr->GetWhoList())
     {
         // player can see member of other team only if CONFIG_ALLOW_TWO_SIDE_WHO_LIST
-        if (target.GetTeamId() != team && !HasPermission(rbac::RBAC_PERM_TWO_SIDE_WHO_LIST))
+        if (target.GetTeamId() != team && !sWorld->getBoolConfig(CONFIG_ALLOW_TWO_SIDE_WHO_LIST))
             continue;
 
         // player can see MODERATOR, GAME MASTER, ADMINISTRATOR only if CONFIG_GM_IN_WHO_LIST
