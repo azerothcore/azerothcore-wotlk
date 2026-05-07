25595f8ea0ca37d239b8697bbc8488be885347b7
Steffen Schmidt <Steffen.Schmidt@bit-in.de>
2026-05-07 05:52:01 +0000

    fix_by_sniff

--- data/sql/updates/pending_db_world/rev_spell_kickin_nass_quest_completion.sql
+++ data/sql/updates/pending_db_world/rev_spell_kickin_nass_quest_completion.sql
@@ -1,7 +1,2 @@
 
-UPDATE `quest_template_addon` SET `SourceSpellID` = 51889 WHERE (`ID` = 12630);
-
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 28518;
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 28518) AND (`source_type` = 0) AND (`id` IN (3));
-
 UPDATE `spell_script_names` SET `ScriptName` = 'spell_kickin_nass_quest_completion' WHERE (`spell_id` = 51910);
