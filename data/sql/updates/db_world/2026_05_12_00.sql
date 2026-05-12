2127c6b136e96aeb165141cf3a1c3c7d0ca3e224
M4rchy-S <alex12345suslik@gmail.com>
2026-05-12 12:36:18 +0000

    Adding entities to Stork Peaks

---
+++ data/sql/updates/db_world/2026_05_12_00.sql
@@ -0,0 +1,17 @@
+DELETE FROM `creature` WHERE (`id1` = 29753) AND (`guid` IN (101018, 101049));
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `Comment`, `VerifiedBuild`) VALUES
+(101018, 29753, 0, 0, 571, 0, 0, 1, 1, 0, 6661.77, -1856.607, 1136, 2.14675, 300, 0, 0, 12600, 0, 0, 0, 0, 0, '', NULL, 0),
+(101049, 29753, 0, 0, 571, 0, 0, 1, 1, 0, 6714.574, -1857.95, 1170.914, 2.28, 300, 0, 0, 12600, 0, 0, 0, 0, 0, '', NULL, 0);
+
+
+DELETE FROM `creature` WHERE (`id1` = 29755) AND (`guid` IN (101589, 101599, 101626, 101638, 101686, 101722, 101724, 101725, 101727, 101733, 101734));
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `Comment`, `VerifiedBuild`) VALUES
+(101626, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6638.91, -1880.16, 1128, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101638, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6629.39, -1903, 1124.8, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101686, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6610.81, -1891.27, 1124, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101722, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6632.8, -1945.33, 1094, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101724, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6647.2, -1970.77, 1092.1, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101725, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6661.33, -1952.73, 1092.1, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101727, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6698.67, -1891.16, 1176.15, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101733, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6717.59, -1877.14, 1176.15, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0),
+(101734, 29755, 0, 0, 571, 0, 0, 1, 1, 0, 6734.12, -1900.9, 1173, 3.78115, 300, 5, 0, 3150, 0, 1, 0, 0, 0, '', NULL, 0);
