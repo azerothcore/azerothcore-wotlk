64ec871c8007e19bcf2f1a6aab98547006a29b8e
Vox <ndinosaur1213@Live.com>
2026-05-14 18:03:51 -0400

    correction### TITLE

--- data/sql/updates/pending_db_world/rev_1778719791748647500.sql
+++ data/sql/updates/pending_db_world/rev_1778719791748647500.sql
@@ -1,4 +1,2 @@
-/* Remove Dead Alliance Soldier from spawn in Icecrown, ID 31281, as it does not belong
-/*
-
-DELETE FROM `creature` WHERE `id1` = 31281;
+-- Remove Dead Alliance Soldier
+DELETE FROM `creature` WHERE `id1` = 31281 AND `guid` = 121653;
