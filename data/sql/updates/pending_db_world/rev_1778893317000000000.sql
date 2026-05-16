3492ef71a182e1118190d95429750b8aac59d308
HunterCML <chadlahman@yahoo.com>
2026-05-15 20:06:43 -0500

    fix(DB/Creature): remove stale Naxxanar teleporter quest condition

---
+++ data/sql/updates/pending_db_world/rev_1778893317000000000.sql
@@ -0,0 +1,2 @@
+--
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9417 AND `SourceEntry` = 0 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 9 AND `ConditionValue1` = 12019;
