580bc3c89aac981c96a7509627fd943fbc8135fa
sogla <sogladev@gmail.com>
2026-05-16 17:05:44 +0200

    fix(DB/PlayerShapeshiftModel): Night Elf shapeshift models

---
+++ data/sql/updates/pending_db_world/rev_1778943517143478573.sql
@@ -0,0 +1,19 @@
+-- Update Night Elf shapeshift forms
+DELETE FROM `player_shapeshift_model` WHERE (`RaceID` = 4 AND `CustomizationID` IN (3, 5) and `ShapeShiftID` IN (5, 8)) OR (`RaceID` = 4 AND `CustomizationID` = 5 and `ShapeShiftID` = 1) OR (`RaceID` = 4 AND `CustomizationID` = 7 AND `ShapeShiftID` IN (5, 8)) OR (`RaceID` = 4 AND `CustomizationID` = 6 AND `ShapeShiftID` = 1);
+INSERT INTO `player_shapeshift_model` (`ShapeshiftID`, `RaceID`, `CustomizationID`, `GenderID`, `ModelID`) VALUES
+-- light blue hair, bear
+(5, 4, 3, 2, 29415),
+(8, 4, 3, 2, 29415),
+-- blue hair, bear
+(5, 4, 5, 2, 29415),
+(8, 4, 5, 2, 29415),
+-- blue hair, cat
+(1, 4, 5, 2, 29406),
+-- purple hair, red bear
+(5, 4, 7, 2, 29417),
+(8, 4, 7, 2, 29417),
+-- darkblue hair, black cat
+(1, 4, 6, 2, 892);
+
+-- remove customization id 8
+DELETE FROM `player_shapeshift_model` WHERE `ShapeshiftID`=1 AND `RaceID`=4 AND `CustomizationID`=8 AND `GenderID`=2 AND `ModelID` = 29405;
