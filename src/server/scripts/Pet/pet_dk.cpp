b3505b7451fdf34cdcc4e35bd193de05adf79d5e
cameronjohnson17 <79820226+cameronjohnson17@users.noreply.github.com>
2026-05-17 11:32:22 -0400

    Update pet_dk.cpp

--- src/server/scripts/Pet/pet_dk.cpp
+++ src/server/scripts/Pet/pet_dk.cpp
@@ -223,8 +223,8 @@ struct npc_pet_dk_ebon_gargoyle : ScriptedAI
 
             if (_decisionTimer <= 0)
             {
-                _decisionTimer += 100;
-                if (_initialCastTimer >= 2000 && !me->HasUnitState(UNIT_STATE_CASTING | UNIT_STATE_LOST_CONTROL) && me->GetMotionMaster()->GetMotionSlotType(MOTION_SLOT_CONTROLLED) == NULL_MOTION_TYPE && rand_chance() > 20.0f)
+                _decisionTimer += 1;
+                if (_initialCastTimer >= 2000 && !me->HasUnitState(UNIT_STATE_CASTING | UNIT_STATE_LOST_CONTROL) && me->GetMotionMaster()->GetMotionSlotType(MOTION_SLOT_CONTROLLED) == NULL_MOTION_TYPE && rand_chance() > 10.0f)
                 {
                     if (me->HasSilenceAura() || me->IsSpellProhibited(SPELL_SCHOOL_MASK_NATURE))
                     {
