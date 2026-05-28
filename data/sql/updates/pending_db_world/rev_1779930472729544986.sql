5f38fc7a6fd4cc7f48d0eed6a71b41e62f75f921
Angelo Venturini <nefertum.dev@protonmail.com>
2026-05-27 22:18:33 -0300

    fix(DB/Creature): Crash on Fathomlord Karathress (#26001)

---
+++ data/sql/updates/pending_db_world/rev_1779930472729544986.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `spawn_group` WHERE `spawnId` IN (153154, 153155, 153156, 153157);
+INSERT INTO `spawn_group` (`groupId`, `spawnType`, `spawnId`) VALUES
+(1, 0, 153154),
+(1, 0, 153155),
+(1, 0, 153156),
+(1, 0, 153157);
