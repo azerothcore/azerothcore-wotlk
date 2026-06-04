8a7500f7fccc8be533164f6afe8b419cd983f1a7
Pagani Walter <paganiwalter@gmail.com>
2026-06-04 10:13:47 -0300

    Without SQL, use default value

--- data/sql/updates/pending_db_world/rev_1780530454211603800.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `creature_template_movement` WHERE `CreatureId` = 844;
-INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
-(844, NULL, NULL, NULL, NULL, NULL, NULL, 60000);
