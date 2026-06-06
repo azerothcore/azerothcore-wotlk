8c6fa1e68df56c90facfc79eee4709efbb1772ad
AlsoNotMehh <itsbrayanrodriguez@gmail.com>
2026-06-06 14:27:14 -0400

    style(DB): satisfy SQL insert safety check

--- data/sql/updates/pending_db_world/rev_1780701448105926000.sql
+++ data/sql/updates/pending_db_world/rev_1780701448105926000.sql
@@ -1,6 +1,5 @@
 -- Stoneclaw Totem pulses should not break stealth (Patch 3.2.2).
-DELETE FROM `spell_custom_attr` WHERE `spell_id` IN
-(5729, 6393, 6394, 6395, 10423, 10424, 25512, 58586, 58587, 58588);
+DELETE FROM `spell_custom_attr` WHERE `spell_id` IN (5729, 6393, 6394, 6395, 10423, 10424, 25512, 58586, 58587, 58588);
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
 (5729, 64),
 (6393, 64),
