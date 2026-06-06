eab102dd8f1de2aff6628f0be7fdd0b7536ce8a5
AlsoNotMehh <itsbrayanrodriguez@gmail.com>
2026-06-06 14:09:35 -0400

    Update data/sql/updates/pending_db_world/rev_1780701448105926000.sql
    Co-authored-by: sogladev <sogladev@gmail.com>

--- data/sql/updates/pending_db_world/rev_1780701448105926000.sql
+++ data/sql/updates/pending_db_world/rev_1780701448105926000.sql
@@ -1,7 +1,6 @@
 -- Stoneclaw Totem pulses should not break stealth (Patch 3.2.2).
 DELETE FROM `spell_custom_attr` WHERE `spell_id` IN
 (5729, 6393, 6394, 6395, 10423, 10424, 25512, 58586, 58587, 58588);
-
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
 (5729, 96),
 (6393, 96),
