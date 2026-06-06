fe23726bd99828b40b89d409eaf1d495f6385187
espkk <espkk@protonmail.ch>
2026-06-06 20:46:58 +0100

    .sql comment

--- data/sql/updates/pending_db_world/rev_1780409137151284500.sql
+++ data/sql/updates/pending_db_world/rev_1780409137151284500.sql
@@ -1,3 +1,4 @@
+-- Add bf diagnostics commands
 DELETE FROM `command` WHERE `name` IN ('bf diagnostics', 'bf diagnostics dump');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('bf diagnostics', 3, 'Syntax: .bf diagnostics #battleid [on|off]\r\nShows or toggles diagnostic tracing for the specified battlefield.\r\n#battleid: the battle ID (1 for Wintergrasp).'),
