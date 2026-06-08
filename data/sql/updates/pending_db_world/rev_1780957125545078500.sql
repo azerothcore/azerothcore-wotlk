d6153a3e42a60a0d4f1a0f1ac6de24a36c36c1dd
Ryan Turner <16946913+TheSCREWEDSoftware@users.noreply.github.com>
2026-06-08 23:25:53 +0100

    fix(DB/Creature): Restore ExpModifer Value and the correct no XP flag for "Risen Alliance Soldier" and "Icy Ghoul" (#26115)

---
+++ data/sql/updates/pending_db_world/rev_1780957125545078500.sql
@@ -0,0 +1,3 @@
+-- Reverts changes from https://github.com/azerothcore/azerothcore-wotlk/pull/26112
+-- Adds back the ExperienceModifier and Adds the correct flag "CREATURE_FLAG_EXTRA_NO_XP" for "Risen Alliance Soldier" and "Icy Ghoul"
+UPDATE `creature_template` SET `ExperienceModifier` = 1, `flags_extra` = `flags_extra` | 64 WHERE `entry` IN (31205, 31142);
