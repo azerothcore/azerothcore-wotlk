3d1c9453f7aa82d88514de31497d5137888e19af
sogla <sogladev@gmail.com>
2026-06-12 08:24:22 +0200

    remove duplicate and misplaced end escort text

--- data/sql/updates/pending_db_world/rev_1781018979570690447.sql
+++ data/sql/updates/pending_db_world/rev_1781018979570690447.sql
@@ -1,3 +1,5 @@
 --
+-- Remove "We made it! Thank you for getting me out of that hell hole. Tell Hemet to expect me!"
+DELETE FROM `creature_text` WHERE (`CreatureID` = 28787) AND `GroupID` = 0 AND `ID` = 0;
 -- Remove "Let's get the hell out of here"
 DELETE FROM `creature_text` WHERE (`CreatureID` = 28787) AND `GroupID` = 6 AND `ID` = 0;
