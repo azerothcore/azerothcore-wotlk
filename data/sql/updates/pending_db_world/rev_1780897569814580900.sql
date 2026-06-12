8262514a36ef72cf9aac371d395f602fccad463a
jakem009 <41561074+jakem009@users.noreply.github.com>
2026-06-13 06:03:05 +1000

    fix(DB/Loot): Add Design: Infused Twilight Opal to Anub'Arak heroic drop table (#26158)

---
+++ data/sql/updates/pending_db_world/rev_1780897569814580900.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature_loot_template` WHERE `Entry` = 31610 AND `Item` = 41796;
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(31610, 41796, 0, 100, 0, 1, 0, 1, 1, 'Anub\'arak (1) - Design: Infused Twilight Opal');
