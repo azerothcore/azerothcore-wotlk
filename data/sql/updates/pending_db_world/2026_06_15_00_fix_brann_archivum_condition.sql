50b7f18f4554d837ac4ffec0eb6a687d7e078ea7
Chinoske <guillermovera.tapia@gmail.com>
2026-06-15 21:03:43 -0400

    fix(DB): rename ConditionType to ConditionTypeOrReference in conditions INSERT

--- data/sql/updates/pending_db_world/2026_06_15_00_fix_brann_archivum_condition.sql
+++ data/sql/updates/pending_db_world/2026_06_15_00_fix_brann_archivum_condition.sql
@@ -5,5 +5,5 @@
 -- BOSS_ASSEMBLY_OF_IRON (4) in Ulduar, always returning 0. The correct type is
 -- ConditionValue3=2 (INSTANCE_INFO_BOSS_STATE) which calls GetBossState(4) == DONE(3).
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 22 AND `SourceGroup` = 13 AND `SourceEntry` = 33235;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionType`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (22, 13, 33235, 0, 0, 13, 1, 4, 3, 2, 0, 0, 0, '', 'Execute SAI only if Assembly of Iron Done');
