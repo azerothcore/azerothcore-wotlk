e0404acd42cc8d58ad1f9953d5ab6a57ac43b377
Chinoske <guillermovera.tapia@gmail.com>
2026-06-16 01:02:04 -0400

    fix(SQL): remove blank line between DELETE and INSERT (codestyle)

--- data/sql/updates/pending_db_world/2026_06_16_00_mini_tyrael_smart_scripts.sql
+++ data/sql/updates/pending_db_world/2026_06_16_00_mini_tyrael_smart_scripts.sql
@@ -16,7 +16,6 @@
 --    to chain from the unconditional TALK to the random action list.
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (29089, 2908900, 2908901, 2908902) AND `source_type` IN (0, 9);
-
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 -- On /dance emote received: cast Tyrael Dance spell (allows movement, 60s cooldown)
 (29089, 0, 0, 0, 22, 0, 100, 0, 34, 60000, 60000, 0, 0, 0, 11, 54398, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Mini Tyrael - On Receive Emote Dance: Cast Tyrael Dance'),
