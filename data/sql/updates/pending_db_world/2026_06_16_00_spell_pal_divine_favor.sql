1aca031b1600b165a1eac23258ec7bdd0e9b2c39
Chinoske <guillermovera.tapia@gmail.com>
2026-06-15 21:13:11 -0400

    fix(DB): add DELETE before INSERT in spell_script_names for codestyle

--- data/sql/updates/pending_db_world/2026_06_16_00_spell_pal_divine_favor.sql
+++ data/sql/updates/pending_db_world/2026_06_16_00_spell_pal_divine_favor.sql
@@ -1,3 +1,4 @@
 -- DB update 2026_06_11_02 -> 2026_06_16_00
+DELETE FROM `spell_script_names` WHERE `spell_id` = 20216 AND `ScriptName` = 'spell_pal_divine_favor';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (20216, 'spell_pal_divine_favor');
