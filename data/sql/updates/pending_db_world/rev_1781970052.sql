fa2893f44277b7d77bead53de99e15c065482755
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-06-20 19:31:25 +0200

    Update rev_1781970052.sql

--- data/sql/updates/pending_db_world/rev_1781970052.sql
+++ data/sql/updates/pending_db_world/rev_1781970052.sql
@@ -35,7 +35,7 @@ INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `positi
 (10054000, 9, 6015.04, -3792.97, 401.877, NULL, 'Heb Drakkar Striker 2'),
 (10054000, 10, 5975.41, -3758.67, 379.627, NULL, 'Heb Drakkar Striker 2');
 
--- Add Heb Drakkar Striker Specific GUID (the flying ones).
+-- Add Heb Drakkar Striker Specific GUID.
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0) AND (`entryorguid` IN (-100536, -100540));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (-100536, 0, 0, 1, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 60, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Heb\'Drakkar Striker - On Respawn - Set Disable Gravity On'),
