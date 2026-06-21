5aefc3f2d4f67edb6a86dd99bbc400486b8b0003
Djankaa <franjodankic96@gmail.com>
2026-06-21 23:34:23 +0200

    Updating the query for mage stormwind teleport

--- data/sql/updates/pending_db_world/fix_mage_stormwind_teleport.sql
+++ data/sql/updates/pending_db_world/fix_mage_stormwind_teleport.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `spell_target_position` SET `Orientation` = 2.22 WHERE `id` = 3561;
+UPDATE `spell_target_position` SET `PositionX` = -9003.46, `PositionY` = 870.031, `PositionZ` = 29.6206, `Orientation` = 2.28 WHERE `id` = 3561;
