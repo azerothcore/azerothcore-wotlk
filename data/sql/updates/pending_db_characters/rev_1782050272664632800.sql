9eea884171c714721213a79f341593d5e0323028
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2026-06-21 16:57:20 +0200

    Apply suggestion from @Kitzunu

--- data/sql/updates/pending_db_characters/rev_1782050272664632800.sql
+++ data/sql/updates/pending_db_characters/rev_1782050272664632800.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `mail_server_template`
-    ADD COLUMN `senderEntry` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `id` COMMENT 'Entry from creature_template. 0 for default "Customer Support" sender.';
+    ADD COLUMN `senderEntry` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Entry from creature_template. 0 for default "Customer Support" sender.' AFTER `id`;
