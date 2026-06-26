1a06bebc249594635b3cc5ae89eebc302d190543
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-06-26 22:29:28 +0200

    Update fix_ruul_escort.sql

--- data/sql/updates/pending_db_world/fix_ruul_escort.sql
+++ data/sql/updates/pending_db_world/fix_ruul_escort.sql
@@ -8,30 +8,30 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 --
 (12818, 0, 0, 1, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Ruul Snowhoof - On Respawn - Set Reactstate Passive'),
 (12818, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 3, 12819, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,             'Ruul Snowhoof - On Respawn - Morph To Creature Ruul Snowhoof Bear Form'),
-(12818, 0, 2, 3, 19, 0, 100, 0, 6482, 0, 0, 0, 0, 0, 64, 1, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0,            'Ruul Snowhoof - On Quest Taken - Store Targetlist'),
-(12818, 0, 3, 4, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                 'Ruul Snowhoof - On Quest Taken - Set Reactstate Aggressive'),
+--
+(12818, 0, 2, 3, 19, 0, 100, 0, 6482, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 14, 48166, 178147, 0, 0, 0, 0, 0, 0,    'Ruul Snowhoof - On Quest Taken - Activate Gameobject'),
+(12818, 0, 3, 4, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 64, 1, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0,               'Ruul Snowhoof - On Quest Taken - Store Targetlist'),
 (12818, 0, 4, 5, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                'Ruul Snowhoof - On Quest Taken - Set Event Phase 1'),
 (12818, 0, 5, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,                'Ruul Snowhoof - On Quest Taken - Set Run Off'),
 --
 (12818, 0, 6, 0, 1, 1, 100, 1, 100, 100, 0, 0, 0, 0, 53, 0, 12818, 0, 6482, 10000, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,  'Ruul Snowhoof - Out of Combat - Start Waypoint'),
 (12818, 0, 7, 0, 1, 1, 100, 1, 3100, 3100, 0, 0, 0, 0, 54, 3000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,        'Ruul Snowhoof - Out of Combat - Pause Waypoint'),
 (12818, 0, 8, 0, 1, 1, 100, 1, 3150, 3150, 0, 0, 0, 0, 1, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,         'Ruul Snowhoof - Out of Combat - Say Line 0'),
-(12818, 0, 9, 0, 1, 1, 100, 513, 5000, 5000, 0, 0, 0, 0, 3, 0, 29421, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,      'Ruul Snowhoof - Out of Combat - Morph To Model 29421'),
-(12818, 0, 10, 0, 1, 1, 100, 513, 5000, 5000, 0, 0, 0, 0, 3, 12819, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Ruul Snowhoof - Out of Combat - Morph To Creature Ruul Snowhoof Bear Form'),
+(12818, 0, 9, 0, 1, 1, 100, 513, 5000, 5000, 0, 0, 0, 0, 3, 12819, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,      'Ruul Snowhoof - Out of Combat - Morph To Creature Ruul Snowhoof Bear Form'),
 --
-(12818, 0, 11, 0, 40, 1, 100, 1, 4, 12818, 0, 0, 0, 0, 80, 1281801, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Ruul Snowhoof - On Waypoint Reached - Run Script'),
-(12818, 0, 12, 0, 40, 1, 100, 1, 7, 12818, 0, 0, 0, 0, 80, 1281802, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Ruul Snowhoof - On Waypoint Reached - Run Script'),
+(12818, 0, 10, 0, 40, 1, 100, 1, 4, 12818, 0, 0, 0, 0, 80, 1281801, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Ruul Snowhoof - On Waypoint Reached - Run Script 1281801'),
+(12818, 0, 11, 0, 40, 1, 100, 1, 7, 12818, 0, 0, 0, 0, 80, 1281802, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,     'Ruul Snowhoof - On Waypoint Reached - Run Script 1281802'),
 --
-(12818, 0, 13, 14, 58, 1, 100, 1, 16, 12818, 0, 0, 0, 0, 1, 1, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       'Ruul Snowhoof - On Waypoint Finished - Say Line 1'),
-(12818, 0, 14, 15, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,             'Ruul Snowhoof - On Waypoint Finished - Demorph'),
-(12818, 0, 15, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 15, 6482, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0,         'Ruul Snowhoof - On Waypoint Finished - Quest Credit'),
+(12818, 0, 12, 13, 58, 1, 100, 1, 16, 12818, 0, 0, 0, 0, 1, 1, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       'Ruul Snowhoof - On Waypoint Finished - Say Line 1'),
+(12818, 0, 13, 14, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,             'Ruul Snowhoof - On Waypoint Finished - Demorph'),
+(12818, 0, 14, 0, 61, 0, 100, 512, 0, 0, 0, 0, 0, 0, 15, 6482, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 0,         'Ruul Snowhoof - On Waypoint Finished - Quest Credit'),
 --
 (1281801, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 12, 3921, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3439.62, -616.591, 171.968, 4.30965,       'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Ursa'),
 (1281801, 9, 1, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 0, 12, 3922, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3439.62, -616.591, 171.968, 4.30965, 'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Totemic'),
-(1281801, 9, 2, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 0, 0, 12, 3926, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3439.62, -616.591, 171.968, 4.30965, 'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Pathfinder'),
+(1281801, 9, 2, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 0, 12, 3926, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3439.62, -616.591, 171.968, 4.30965, 'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Pathfinder'),
 (1281802, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 12, 3921, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3495.9, -526.57, 188.322, 4.62782,         'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Ursa'),
 (1281802, 9, 1, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 0, 12, 3922, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3495.9, -526.57, 188.322, 4.62782,   'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Totemic'),
-(1281802, 9, 2, 0, 0, 0, 100, 0, 8000, 8000, 0, 0, 0, 0, 12, 3926, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3495.9, -526.57, 188.322, 4.62782,   'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Pathfinder');
+(1281802, 9, 2, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 0, 0, 12, 3926, 7, 60000, 1, 1, 0, 8, 0, 0, 0, 0, 3495.9, -526.57, 188.322, 4.62782,   'Ruul Snowhoof - Actionlist - Summon Creature Thistlefur Pathfinder');
 
 DELETE FROM `waypoints` WHERE `entry` = 12818;
 INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `point_comment`) VALUES
