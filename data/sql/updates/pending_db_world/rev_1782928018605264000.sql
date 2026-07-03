19ceff0b925f8afd07591e8619f91133268e799a
Branel <emirpod02@gmail.com>
2026-07-03 07:51:23 +0200

    Delete data/sql/updates/pending_db_world/rev_1782928018605264000.sql

--- data/sql/updates/pending_db_world/rev_1782928018605264000.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `creature_template_model` WHERE (`CreatureID` = 26477) AND (`Idx` IN (0, 1, 2));
-INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
-(26477, 0, 14886, 1, 1, 0),
-(26477, 1, 23833, 1, 1, 0),
-(26477, 2, 23832, 1, 1, 0);
