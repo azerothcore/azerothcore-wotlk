26c97ffd673ef52d4b6e3078bc1c928daa7a7b93
AldebaraanMKII <gab.jesus76@yahoo.com>
2026-07-03 02:16:37 -0300

    fixed acore_string errors

--- data/sql/updates/pending_db_world/rev_1783009383945000000.sql
+++ data/sql/updates/pending_db_world/rev_1783009383945000000.sql
@@ -4,3 +4,5 @@ INSERT INTO `acore_string` (`entry`, `content_default`) VALUES
 (35456, 'Syntax: .titles current #title [$playername]\nSet title #title (id or shift-link) as current selected title for selected player. If title is not in known title list for player then it will be added to list.'),
 (35457, 'Syntax: .titles remove #title [$playername]\nRemove title #title (id or shift-link) from known titles list for selected player.'),
 (35458, 'Syntax: .titles set mask #mask [$playername]\nAllows user to use all titles from #mask.\n #mask=0 disables the title-choose-field.');
+
+UPDATE `command` SET `help` = NULL WHERE `name` IN ('titles add', 'titles current', 'titles remove', 'titles set mask');
