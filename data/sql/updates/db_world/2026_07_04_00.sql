2012005b9d5e18d7e575a2bb35c042b3d34dd3da
Moostigre <moostigre@proton.me>
2026-07-04 10:43:25 +0200

    fix(DB/Creature): move Sludge Beast out of terrain pocket

---
+++ data/sql/updates/db_world/2026_07_04_00.sql
@@ -0,0 +1,2 @@
+-- Move Sludge Beast out of the wooden plank collision pocket in Sludge Fen.
+UPDATE `creature` SET `position_x` = 1062.8604, `position_y` = -3125.564, `position_z` = 71.51024, `orientation` = 3.2642176 WHERE `guid` = 51809 AND `id` = 3295;
