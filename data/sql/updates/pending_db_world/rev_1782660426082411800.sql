1c06df1a9a27342df72b8c9b596aa3f694fb8dc5
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2026-07-04 10:32:55 +0200

    I think it is redundant, but here you go

--- data/sql/updates/pending_db_world/rev_1782660426082411800.sql
+++ data/sql/updates/pending_db_world/rev_1782660426082411800.sql
@@ -1,87 +1,101 @@
 DELETE FROM `spell_custom_attr` WHERE `spell_id` IN (6197, 11971, 12579, 14183, 15502, 17962, 27891, 28969, 29306, 29325, 30659, 32593, 32594, 34655, 35859, 38044, 38449, 40327, 40520, 43138, 45145, 45271, 45347, 45348, 45537, 49283, 49284, 49381, 49882, 50315, 50526, 52752, 53353, 54314, 54331, 54714, 55093, 55604, 55645, 56098, 58567, 58690, 58984, 59283, 59354, 59805, 61920, 62775, 63124, 63293, 63675, 63978, 64125, 64126, 64619, 64638, 65280, 65775, 66378, 66765, 66809, 67097, 67098, 67099, 67331, 67333, 67721, 67847, 67861, 67862, 67863, 68873, 70324, 71157, 72347, 72409, 72447, 72448, 72449, 72465, 72963, 72964, 72965, 72966, 74507);
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
-(6197, 16),
-(11971, 4194304),
-(12579, 4194304),
-(14183, 64),
-(15502, 4194304),
-(17962, 2097152),
-(27891, 4194304),
-(28969, 4194304),
-(29306, 4194304),
-(29325, 4194304),
-(30659, 4194304),
-(32593, 2097152),
-(32594, 2097152),
-(34655, 4194304),
-(35859, 4194304),
-(38044, 4194304),
-(38449, 4194304),
-(40327, 4194304),
-(40520, 4194304),
-(43138, 262144),
-(45145, 1048576),
-(45271, 4194304),
-(45347, 4194304),
-(45348, 4194304),
-(45537, 2048),
-(49283, 2097152),
-(49284, 2097152),
-(49381, 4194304),
-(49882, 32768),
-(50315, 128),
-(50526, 2097152),
-(52752, 2097152),
-(53353, 2097152),
-(54314, 4194304),
-(54331, 4194304),
-(54714, 4194304),
-(55093, 4194304),
-(55604, 4194304),
-(55645, 4194304),
-(56098, 4194304),
-(58567, 4194304),
-(58690, 2),
-(58984, 128),
-(59283, 2),
-(59354, 4194304),
-(59805, 4194304),
-(61920, 4194304),
-(62775, 32768),
-(63124, 131072),
-(63293, 4),
-(63675, 2097152),
-(63978, 4194304),
-(64125, 32768),
-(64126, 32768),
-(64619, 4),
-(64638, 4194304),
-(65280, 4194304),
-(65775, 4194304),
-(66378, 32768),
-(66765, 8),
-(66809, 8),
-(67097, 32768),
-(67098, 32768),
-(67099, 32768),
-(67331, 8),
-(67333, 8),
-(67721, 4194304),
-(67847, 4194304),
-(67861, 4194304),
-(67862, 4194304),
-(67863, 4194304),
-(68873, 4),
-(70324, 4),
-(71157, 4194304),
-(72347, 4096),
-(72409, 32768),
-(72447, 32768),
-(72448, 32768),
-(72449, 32768),
-(72465, 4194304),
-(72963, 4194304),
-(72964, 4194304),
-(72965, 4194304),
-(72966, 4194304),
-(74507, 4194304);
+-- SPELL_ATTR0_CU_CONE_BACK (0x2)
+(58690, 2), -- Cyanigosa, Tail Sweep
+(59283, 2), -- Cyanigosa, Tail Sweep
+-- SPELL_ATTR0_CU_CONE_LINE (0x4)
+(63293, 4), -- Mimiron - spinning damage
+(64619, 4), -- Ulduar, Mimiron, Emergency Fire Bot, Water Spray
+(68873, 4), -- Wailing Souls
+(70324, 4), -- Wailing Souls
+-- SPELL_ATTR0_CU_SHARE_DAMAGE (0x8)
+(66765, 8), -- Meteor Fists
+(66809, 8), -- Meteor Fists
+(67331, 8), -- Meteor Fists
+(67333, 8), -- Meteor Fists
+-- SPELL_ATTR0_CU_NO_INITIAL_THREAT (0x10)
+(6197, 16), -- Eagle Eye
+-- SPELL_ATTR0_CU_DONT_BREAK_STEALTH (0x40)
+(14183, 64), -- Premeditation
+-- SPELL_ATTR0_CU_NO_PVP_FLAG (0x80)
+(50315, 128), -- Disco Ball
+(58984, 128), -- Shadowmeld, issue #16626
+-- SPELL_ATTR0_CU_IGNORE_EVADE (0x800)
+(45537, 2048), -- Cosmetic - Lightning Beam Channel
+-- SPELL_ATTR0_CU_NEGATIVE_EFF0 (0x1000)
+(72347, 4096), -- Lock Players and Tap Chest (Gunship Battle)
+-- SPELL_ATTR0_CU_IGNORE_ARMOR (0x8000)
+(49882, 32768), -- Leviroth Self-Impale
+(62775, 32768), -- Ulduar: XT-002 Tympanic Tamparum
+(64125, 32768), -- Ulduar, Yogg-Saron, Squeeze
+(64126, 32768), -- Ulduar, Yogg-Saron, Squeeze
+(66378, 32768), -- Trial of the Crusader, Jaraxxus, Shivan Slash
+(67097, 32768), -- Trial of the Crusader, Jaraxxus, Shivan Slash
+(67098, 32768), -- Trial of the Crusader, Jaraxxus, Shivan Slash
+(67099, 32768), -- Trial of the Crusader, Jaraxxus, Shivan Slash
+(72409, 32768), -- Rune of Blood (Deathbringer Saurfang)
+(72447, 32768), -- Rune of Blood (Deathbringer Saurfang)
+(72448, 32768), -- Rune of Blood (Deathbringer Saurfang)
+(72449, 32768), -- Rune of Blood (Deathbringer Saurfang)
+-- SPELL_ATTR0_CU_REQ_CASTER_BEHIND_TARGET (0x20000)
+(63124, 131072), -- quest There's Something About the Squire (13654)
+-- SPELL_ATTR0_CU_ALLOW_INFLIGHT_TARGET (0x40000)
+(43138, 262144), -- North Fleet Reservist Kill Credit
+-- SPELL_ATTR0_CU_BINARY_SPELL (0x100000)
+(45145, 1048576), -- Snake Trap Effect
+-- SPELL_ATTR0_CU_NO_POSITIVE_TAKEN_BONUS (0x200000)
+(17962, 2097152), -- Conflagrate
+(32593, 2097152), -- Earth Shield aura
+(32594, 2097152), -- Earth Shield aura
+(49283, 2097152), -- Earth Shield aura
+(49284, 2097152), -- Earth Shield aura
+(50526, 2097152), -- Wandering Plague
+(52752, 2097152), -- Ancestral Awakening Heal
+(53353, 2097152), -- Chimera Shot - Serpent trigger
+(63675, 2097152), -- Improved Devouring Plague
+-- SPELL_ATTR0_CU_SINGLE_AURA_STACK (0x400000)
+(11971, 4194304), -- Sunder Armor
+(12579, 4194304), -- Player Winter's Chill
+(15502, 4194304), -- Sunder Armor
+(27891, 4194304), -- Naxxramas, Sludge Belcher, Acidic Sludge (10 normal)
+(28969, 4194304), -- Naxxramas, Crypt Guard, Acid Spit (10 normal)
+(29306, 4194304), -- Naxxramas (Gluth's Zombies): Infected Wound
+(29325, 4194304), -- Naxxramas, Stoneskin Gargoyle, Acid Volley (10 normal)
+(30659, 4194304), -- Hellfire Ramparts, Fel Infusion
+(34655, 4194304), -- Snake Trap, Deadly Poison
+(35859, 4194304), -- The Eye, Nether Vapor
+(38044, 4194304), -- Serpentshrine Cavern, Surge
+(38449, 4194304), -- Serpentshrine Cavern, Blessing of the Tides
+(40327, 4194304), -- Black Temple, Atrophy
+(40520, 4194304), -- Black Temple, Shade Soul Channel
+(45271, 4194304), -- Sunwell, Eredar Twins encounter, Dark Strike
+(45347, 4194304), -- Sunwell, Eredar Twins encounter, Dark Touched
+(45348, 4194304), -- Sunwell, Eredar Twins encounter, Flame Touched
+(49381, 4194304), -- Drak'tharon Keep, Consume
+(54314, 4194304), -- Azjol'Nerub Drain Power
+(54331, 4194304), -- Naxxramas, Sludge Belcher, Acidic Sludge (25 normal)
+(54714, 4194304), -- Naxxramas, Stoneskin Gargoyle, Acid Volley (25 normal)
+(55093, 4194304), -- Gundrak, Grip of Slad'ran
+(55604, 4194304), -- Naxxramas, Unrelenting Trainee, Death Plague (10 normal)
+(55645, 4194304), -- Naxxramas, Unrelenting Trainee, Death Plague (25 normal)
+(56098, 4194304), -- Naxxramas, Crypt Guard, Acid Spit (25 normal)
+(58567, 4194304), -- Player Sunder Armor
+(59354, 4194304), -- Azjol'Nerub Drain Power
+(59805, 4194304), -- Drak'tharon Keep, Consume
+(61920, 4194304), -- Ulduar (Spellbreaker): Supercharge
+(63978, 4194304), -- Ulduar (Rubble): Stone Nova
+(64638, 4194304), -- Ulduar, Winter Jormungar, Acidic Bite
+(65280, 4194304), -- Ulduar, Hodir, Singed
+(65775, 4194304), -- Anub'arak, Swarm Scarab, Acid-Drenched Mandibles (10 normal)
+(67721, 4194304), -- Anub'arak, Nerubian Burrower, Expose Weakness (normal)
+(67847, 4194304), -- Anub'arak, Nerubian Burrower, Expose Weakness (heroic)
+(67861, 4194304), -- Anub'arak, Swarm Scarab, Acid-Drenched Mandibles (25 normal)
+(67862, 4194304), -- Anub'arak, Swarm Scarab, Acid-Drenched Mandibles (10 heroic)
+(67863, 4194304), -- Anub'arak, Swarm Scarab, Acid-Drenched Mandibles (25 heroic)
+(71157, 4194304), -- Icecrown Citadel, Plagued Zombie, Infected Wound
+(72465, 4194304), -- Icecrown Citadel, Sindragosa, Respite for a Tormented Soul (weekly quest)
+(72963, 4194304), -- Icecrown Citadel, Rot Worm, Flesh Rot (10 normal)
+(72964, 4194304), -- Icecrown Citadel, Rot Worm, Flesh Rot (25 normal)
+(72965, 4194304), -- Icecrown Citadel, Rot Worm, Flesh Rot (10 heroic)
+(72966, 4194304), -- Icecrown Citadel, Rot Worm, Flesh Rot (25 heroic)
+(74507, 4194304); -- Ruby Sanctum, Siphoned Might
