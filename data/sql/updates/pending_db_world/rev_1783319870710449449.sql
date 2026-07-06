9065b38b7bf82412833bcae29c3e05c5d25c4546
zackchadwick <73787423+zackchadwick@users.noreply.github.com>
2026-07-06 10:42:02 -0400

    Update data/sql/updates/pending_db_world/rev_1783319870710449449.sql
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1783319870710449449.sql
+++ data/sql/updates/pending_db_world/rev_1783319870710449449.sql
@@ -3,4 +3,8 @@
 UPDATE `creature` SET `position_x` = -1720.64, `position_y` = 5637.84, `position_z` = 128.024, `orientation` = 2.60278 WHERE `guid` = 85406 AND `id` = 25195;
 
 DELETE FROM `creature_template_model` WHERE `CreatureID` = 25195;
-INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES (25195, 0, 18743, 1, 1, 51831);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(25195, 0, 18743, 1, 1, 51831),
+(25195, 1, 18742, 1, 0, 51831),
+(25195, 2, 18741, 1, 0, 51831),
+(25195, 3, 18740, 1, 0, 51831),
