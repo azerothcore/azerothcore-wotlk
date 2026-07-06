86087d384787165d6b71e6181a9de4557d806b69
Zack Chadwick <zack.chadwick@gmail.com>
2026-07-06 02:59:50 -0400

    fix(CORE/Creature): Update Marksman Bova location and model
    Resolves https://github.com/azerothcore/azerothcore-wotlk/issues/26493

---
+++ data/sql/updates/pending_db_world/rev_1783319870710449449.sql
@@ -0,0 +1,6 @@
+-- Move Marksman Bova to the correct position and model.
+
+UPDATE `creature` SET `position_x` = -1720.64, `position_y` = 5637.84, `position_z` = 128.024, `orientation` = 2.60278 WHERE `guid` = 85406 AND `id` = 25195;
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 25195;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES (25195, 0, 18743, 1, 1, 51831);
