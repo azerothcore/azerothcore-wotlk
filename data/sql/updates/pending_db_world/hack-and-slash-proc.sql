a54220b5f51cce8ab6c3f7b34a1cf2aef3133f62
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2026-07-12 19:45:23 -0400

    fix(DB/Spell): Only proc Hack and Slash with damaging attacks. (#26579)

---
+++ data/sql/updates/pending_db_world/hack-and-slash-proc.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId` = -13960;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
+(-13960, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0);
