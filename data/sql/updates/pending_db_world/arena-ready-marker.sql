1d892452a40d9047012fa6d4a5834a8500a46a20
heyitsbench <benjymansy123@gmail.com>
2026-07-11 20:44:35 -0400

    Drop database content.

---
+++ data/sql/updates/pending_db_world/arena-ready-marker.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject_template` WHERE `entry` = 301337;
+DELETE FROM `gameobject_template_addon` WHERE `entry` = 301337;
+DELETE FROM `gameobject_template_locale` WHERE `entry` = 301337;
