f44ca299f044ca232b666146c91ec588ff534bd3
Thierry PROST <thierry.prost@gmail.com>
2026-07-14 19:39:40 +0200

    chore(DB): wrap quest 21 frFR update statement (review)

--- data/sql/updates/pending_db_world/rev_1783885061161224.sql
+++ data/sql/updates/pending_db_world/rev_1783885061161224.sql
@@ -1,2 +1,4 @@
 -- Quest 21 (Skirmish at Echo Ridge): frFR objectives text says 8 Kobold Workers, the requirement is 12.
-UPDATE `quest_template_locale` SET `Objectives` = REPLACE(`Objectives`, 'Tuez 8 Travailleurs kobolds', 'Tuez 12 Travailleurs kobolds') WHERE `ID` = 21 AND `locale` = 'frFR';
+UPDATE `quest_template_locale` SET `Objectives` =
+    REPLACE(`Objectives`, 'Tuez 8 Travailleurs kobolds', 'Tuez 12 Travailleurs kobolds')
+WHERE `ID` = 21 AND `locale` = 'frFR';
