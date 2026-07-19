1dfd65c592dedea7c7e75b6a4346a2ad9ab6d973
Vox <ndinosaur1213@Live.com>
2026-07-19 10:04:18 -0400

    cleanup

--- data/sql/updates/pending_db_world/rev_1784403903526710200.sql
+++ data/sql/updates/pending_db_world/rev_1784403903526710200.sql
@@ -4,8 +4,8 @@
 -- Whitelist of creature entry pairs allowed to attack each other despite mutually-neutral factions.
 -- --------------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `creature_hostile_override` (
-  `AttackerEntry` int unsigned NOT NULL DEFAULT '0' COMMENT 'Entry of attacker',
-  `TargetEntry` int unsigned NOT NULL DEFAULT '0' COMMENT 'Entry is allowed to attack',
-  `Comment` varchar(255) NOT NULL DEFAULT '' COMMENT 'Comments',
-  PRIMARY KEY (`AttackerEntry`, `TargetEntry`) USING BTREE
+   `AttackerEntry` int unsigned NOT NULL DEFAULT '0' COMMENT 'Entry of attacker',
+   `TargetEntry` int unsigned NOT NULL DEFAULT '0' COMMENT 'Entry is allowed to attack',
+   `Comment` varchar(255) NOT NULL DEFAULT '' COMMENT 'Comments',
+   PRIMARY KEY (`AttackerEntry`, `TargetEntry`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Creature entry pairs allowed to attack despite mutually-neutral factions';
