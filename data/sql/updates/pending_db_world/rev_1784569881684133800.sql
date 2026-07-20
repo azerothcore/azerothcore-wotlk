967d67ebf9e06ee7445324d11d7b951099d33e39
Si1ker <61223313+Si1ker@users.noreply.github.com>
2026-07-20 11:52:55 -0600

    nature bomb

---
+++ data/sql/updates/pending_db_world/rev_1784569881684133800.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 34153 AND `CreatureDisplayID` = 169;
+UPDATE `creature_template_model` SET `Idx` = 0 WHERE `CreatureID` = 34153 AND `CreatureDisplayID` = 23258;
