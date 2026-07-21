ff6d0096465374582398a5c4a5f7a533f7a2b949
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-21 14:56:16 -0300

    Update rev_1767814923601389300.sql

--- data/sql/updates/pending_db_world/rev_1767814923601389300.sql
+++ data/sql/updates/pending_db_world/rev_1767814923601389300.sql
@@ -381,4 +381,4 @@ DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` BETWEEN 2
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (2545302, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 12, 25451, 6, 10000, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ith\'rix the Harvester - Actionlist - Summon Creature \'Nerub\'ar Sky Darkener\'');
 
-UPDATE `creature_template` SET `detection_range` = 30 WHERE (`entry` = 25451);
+UPDATE `creature_template` SET `detection_range` = 30 WHERE (`entry` IN (25451, 25244));
