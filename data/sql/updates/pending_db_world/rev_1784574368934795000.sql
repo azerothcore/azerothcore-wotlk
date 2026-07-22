b20406cd2cf3a476f006b47e3750aca06fea91da
Eddy Vega <61223313+Si1ker@users.noreply.github.com>
2026-07-22 13:13:35 -0600

    fix(DB/creature_template_model): Adjust Nature Bomb model (#26708)
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1784574368934795000.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature_template_model` WHERE `CreatureID` IN (34129, 34153);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(34129, 0, 169, 1, 0, 51831),
+(34129, 1, 23258, 1, 1, 51831),
+(34153, 0, 169, 1, 0, 51831),
+(34153, 1, 23258, 1, 1, 51831);
