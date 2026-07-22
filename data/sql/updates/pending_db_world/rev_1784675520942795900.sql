4ef959779876aee7cdee7c1e59c460a4b7f1acb6
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-21 21:15:29 -0300

    Update rev_1784675520942795900.sql

--- data/sql/updates/pending_db_world/rev_1784675520942795900.sql
+++ data/sql/updates/pending_db_world/rev_1784675520942795900.sql
@@ -7,7 +7,7 @@ DELETE FROM `smart_scripts` WHERE (`source_type` = 1 AND `entryorguid` = 177491)
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (177491, 1, 0, 0, 20, 0, 100, 0, 5902, 3000, 3000, 0, 0, 0, 107, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Termite Barrel - On Quest \'A Plague Upon Thee\' Finished - Summon Termites'),
 (177491, 1, 1, 0, 20, 0, 100, 0, 5904, 3000, 3000, 0, 0, 0, 107, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Termite Barrel - On Quest \'A Plague Upon Thee\' Finished - Summon Termites'),
-(177491, 1, 2, 0, 17, 0, 100, 0, 12120, 0, 0, 0, 0, 0, 46, 10, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Termite Barrel - On Summoned Unit - Move Forward 5 Yards');
+(177491, 1, 2, 0, 17, 0, 100, 0, 12120, 0, 0, 0, 0, 0, 46, 10, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Termite Barrel - On Summoned Unit - Move Forward 10 Yards');
 
 -- These had Target NONE before, so change to SELF
 UPDATE `smart_scripts` SET `target_type` = 1 WHERE `entryorguid` = 2737600 AND `source_type` = 9 AND `id` = 3;
