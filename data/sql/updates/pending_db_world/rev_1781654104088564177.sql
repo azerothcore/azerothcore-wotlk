32557fb3c771b8abec8fbfce55d3187a976980d0
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-22 14:19:25 -0300

    Refactor SQL conditions for Fel Creep
    Removed redundant DELETE statement and simplified INSERT syntax.

--- data/sql/updates/pending_db_world/rev_1781654104088564177.sql
+++ data/sql/updates/pending_db_world/rev_1781654104088564177.sql
@@ -1,21 +1,10 @@
 -- Fel Creep - requires Cenarion Beacon in inventory
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 4 AND `SourceEntry` = 11514;
-INSERT INTO `conditions`
-    (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`,
-    `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`,
-    `Comment`)
-VALUES
-(4, 13965, 11514, 2, 11511, 1,
-    'Sungrass - Fel Creep requires Cenarion Beacon'),
-(4, 13966, 11514, 2, 11511, 1,
-    'Gromsblood - Fel Creep requires Cenarion Beacon'),
-(4, 13967, 11514, 2, 11511, 1,
-    'Golden Sansam - Fel Creep requires Cenarion Beacon'),
-(4, 13968, 11514, 2, 11511, 1,
-    'Dreamfoil - Fel Creep requires Cenarion Beacon'),
-(4, 13969, 11514, 2, 11511, 1,
-    'Mountain Silversage - Fel Creep requires Cenarion Beacon'),
-(4, 13970, 11514, 2, 11511, 1,
-    'Arthas'' Tears - Fel Creep requires Cenarion Beacon'),
-(4, 13971, 11514, 2, 11511, 1,
-    'Plaguebloom - Fel Creep requires Cenarion Beacon');
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
+(4, 13965, 11514, 2, 11511, 1, 'Sungrass - Fel Creep requires Cenarion Beacon'),
+(4, 13966, 11514, 2, 11511, 1, 'Gromsblood - Fel Creep requires Cenarion Beacon'),
+(4, 13967, 11514, 2, 11511, 1, 'Golden Sansam - Fel Creep requires Cenarion Beacon'),
+(4, 13968, 11514, 2, 11511, 1, 'Dreamfoil - Fel Creep requires Cenarion Beacon'),
+(4, 13969, 11514, 2, 11511, 1, 'Mountain Silversage - Fel Creep requires Cenarion Beacon'),
+(4, 13970, 11514, 2, 11511, 1, 'Arthas\' Tears - Fel Creep requires Cenarion Beacon'),
+(4, 13971, 11514, 2, 11511, 1, 'Plaguebloom - Fel Creep requires Cenarion Beacon');
