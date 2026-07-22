db737344bb9029ed5416abb80e6f1af573005e58
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-22 13:14:20 -0300

    Update data/sql/updates/pending_db_world/rev_1784574368934795000.sql
    Co-authored-by: Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>

--- data/sql/updates/pending_db_world/rev_1784574368934795000.sql
+++ data/sql/updates/pending_db_world/rev_1784574368934795000.sql
@@ -1,3 +1,6 @@
---
-UPDATE `creature_template_model` SET `Probability` = 0 WHERE `CreatureID` = 34153 AND `Idx` = 0 AND `CreatureDisplayID` = 169;
-UPDATE `creature_template_model` SET `Probability` = 100 WHERE `CreatureID` = 34153 AND `Idx` = 1 AND `CreatureDisplayID` = 23258;
+DELETE FROM `creature_template_model` WHERE `CreatureID` IN (34129, 34153);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(34129, 0, 169, 1, 0, 51831),
+(34129, 1, 23258, 1, 1, 51831),
+(34153, 0, 169, 1, 0, 51831),
+(34153, 1, 23258, 1, 1, 51831);
