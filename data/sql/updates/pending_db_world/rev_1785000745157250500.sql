089f1171a0960d4326421f8f516534bc3fc8c6d7
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-25 14:34:35 -0300

    Create rev_1785000745157250500.sql

---
+++ data/sql/updates/pending_db_world/rev_1785000745157250500.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `quest_offer_reward` SET `Emote1` = 5, `Emote2` = 1, `Emote3` = 1, `EmoteDelay2` = 1000, `RewardText` = 'WHAT?!$B$B<The image before you distorts for a moment as the archmage attempts to regain his composure.>$B$BOf course, we should have guessed that this was what they were up to all along. Quickly, there\'s no time to waste.$B$BWyrmrest must be warned!' WHERE (`ID` = 12110);
