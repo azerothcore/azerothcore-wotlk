6feb1d3b1c0513835e383652bf925448ff079f62
sogladev <sogladev@gmail.com>
2026-07-25 13:22:04 +0200

    fix(Core/Creature): Target Dummy level

---
+++ data/sql/updates/pending_db_world/rev_1784980917228106168.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `minlevel` = 17, `maxlevel` = 17, `type_flags` = 0, `unit_flags` = 8 WHERE `entry` = 2673;
+UPDATE `creature_template` SET `minlevel` = 37, `maxlevel` = 37, `type_flags` = 0, `unit_flags` = 8 WHERE `entry` = 2674;
+UPDATE `creature_template` SET `minlevel` = 55, `maxlevel` = 55, `type_flags` = 0, `unit_flags` = 8 WHERE `entry` = 12426;
