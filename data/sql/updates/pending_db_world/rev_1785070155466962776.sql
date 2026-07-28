47038fd60b117c8c236d5aceb23b3b0cc5667b18
sogladev <sogladev@gmail.com>
2026-07-29 00:23:58 +0200

    fix(DB/Immunities): Embalming Slime (1) speed and immunity (#26854)
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1785070155466962776.sql
@@ -0,0 +1,7 @@
+--
+-- Invisibility and Stealth Detection
+UPDATE `creature_template_addon` SET `auras` = '18950' WHERE (`entry` IN (16024, 29355));
+-- 3.0 speed
+UPDATE `creature_template` SET `speed_run` = 0.42857 WHERE (`entry` = 29355);
+-- mech=0x3C8B3E12(DISORIENTED|FEAR|SLEEP|SNARE|STUN|FREEZE|KNOCKOUT|POLYMORPH|BANISH|SHACKLE|HORROR|DAZE|DISCOVERY|IMMUNE_SHIELD|SAPPED)
+UPDATE `creature_template` SET `CreatureImmunitiesId` = -393 WHERE (`entry` IN (16024, 29355));
