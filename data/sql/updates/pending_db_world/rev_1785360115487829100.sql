e791535666e9867f5a5a9b1610fcd9273460a202
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-29 20:29:17 -0300

    Update rev_1785360115487829100.sql

--- data/sql/updates/pending_db_world/rev_1785360115487829100.sql
+++ data/sql/updates/pending_db_world/rev_1785360115487829100.sql
@@ -49,34 +49,34 @@ INSERT INTO `creature_multispawn` (`spawnId`, `entry`) VALUES
 (@CGUID+5, 24063),
 (@CGUID+6, 24063);
 
-DELETE FROM `waypoint_data` WHERE `id` BETWEEN 240510 AND 240516;
+DELETE FROM `waypoint_data` WHERE `id` BETWEEN (@CGUID+0)*10 AND (@CGUID+6)*10;
 INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `move_type`) VALUES
 -- X: 733.43774 Y: -4872.8115 Z: 5.9952602
-(240510, 1, 732.7387, -4884.812, 3.0737715, NULL, 1),
-(240510, 2, 719.05145, -4894.5073, 6.2719183, NULL, 1),
-(240510, 3, 697.83594, -4907.74, 6.1251554, NULL, 1),
+((@CGUID+0)*10, 1, 732.7387, -4884.812, 3.0737715, NULL, 1),
+((@CGUID+0)*10, 2, 719.05145, -4894.5073, 6.2719183, NULL, 1),
+((@CGUID+0)*10, 3, 697.83594, -4907.74, 6.1251554, NULL, 1),
 -- X: 811.02466 Y: -4967.3896 Z: 1.8235762
-(240511, 1, 744.038, -4960.3228, 2.330449, NULL, 1),
-(240511, 2, 727.86505, -4985.579, 5.602709, NULL, 1),
-(240511, 3, 722.78345, -4988.4126, 6.404078, NULL, 1),
+((@CGUID+1)*10, 1, 744.038, -4960.3228, 2.330449, NULL, 1),
+((@CGUID+1)*10, 2, 727.86505, -4985.579, 5.602709, NULL, 1),
+((@CGUID+1)*10, 3, 722.78345, -4988.4126, 6.404078, NULL, 1),
 -- X: 798.0802 Y: -4867.249 Z: -0.5787722
-(240512, 1, 741.7717, -4904.015, 3.1975787, NULL, 1),
-(240512, 2, 734.9463, -4927.025, 6.25686, NULL, 1),
-(240512, 3, 719.4844, -4935.973, 6.7613916, NULL, 1),
+((@CGUID+2)*10, 1, 741.7717, -4904.015, 3.1975787, NULL, 1),
+((@CGUID+2)*10, 2, 734.9463, -4927.025, 6.25686, NULL, 1),
+((@CGUID+2)*10, 3, 719.4844, -4935.973, 6.7613916, NULL, 1),
 -- X: 780.9059 Y: -4882.871 Z: 0.5644784
-(240513, 1, 730.73535, -4893.994, 4.5011435, NULL, 1),
-(240513, 2, 704.7542, -4905.6694, 7.276061, NULL, 1),
-(240513, 3, 709.357, -4927.3623, 7.2387867, NULL, 1),
+((@CGUID+3)*10, 1, 730.73535, -4893.994, 4.5011435, NULL, 1),
+((@CGUID+3)*10, 2, 704.7542, -4905.6694, 7.276061, NULL, 1),
+((@CGUID+3)*10, 3, 709.357, -4927.3623, 7.2387867, NULL, 1),
 -- X: 783.75476 Y: -4949.7236 Z: 1.654979
-(240514, 1, 728.9895, -4940.7905, 6.076869, NULL, 1),
-(240514, 2, 715.9845, -4970.6724, 6.0854883, NULL, 1),
+((@CGUID+4)*10, 1, 728.9895, -4940.7905, 6.076869, NULL, 1),
+((@CGUID+4)*10, 2, 715.9845, -4970.6724, 6.0854883, NULL, 1),
 -- X: 794.9075 Y: -4874.745 Z: -0.62727714
-(240515, 1, 738.62933, -4915.262, 5.2849817, NULL, 1),
-(240515, 2, 731.65607, -4927.6084, 6.734372, NULL, 1),
-(240515, 3, 712.5723, -4926.47, 7.523102, NULL, 1),
+((@CGUID+5)*10, 1, 738.62933, -4915.262, 5.2849817, NULL, 1),
+((@CGUID+5)*10, 2, 731.65607, -4927.6084, 6.734372, NULL, 1),
+((@CGUID+5)*10, 3, 712.5723, -4926.47, 7.523102, NULL, 1),
 -- X: 803.7593 Y: -4924.593 Z: 0.1089665
-(240516, 1, 739.2899, -4937.828, 5.6794043, NULL, 1),
-(240516, 2, 719.42474, -4959.9834, 4.882431, NULL, 1);
+((@CGUID+6)*10, 1, 739.2899, -4937.828, 5.6794043, NULL, 1),
+((@CGUID+6)*10, 2, 719.42474, -4959.9834, 4.882431, NULL, 1);
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 24051);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
@@ -94,10 +94,10 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` BETWEEN -(@CGUID+6) AND -(@CGUID+0));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(-(@CGUID+0), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240510, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240510'),
-(-(@CGUID+1), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240511, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240511'),
-(-(@CGUID+2), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240512, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240512'),
-(-(@CGUID+3), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240513, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240513'),
-(-(@CGUID+4), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240514, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240514'),
-(-(@CGUID+5), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240515, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240515'),
-(-(@CGUID+6), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, 240516, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path 240515');
+(-(@CGUID+0), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+0)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path'),
+(-(@CGUID+1), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+1)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path'),
+(-(@CGUID+2), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+2)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path'),
+(-(@CGUID+3), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+3)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path'),
+(-(@CGUID+4), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+4)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path'),
+(-(@CGUID+5), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+5)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path'),
+(-(@CGUID+6), 0, 1000, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 0, 232, (@CGUID+6)*10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Dragonflayer Invader - On Respawn - Start Path');
