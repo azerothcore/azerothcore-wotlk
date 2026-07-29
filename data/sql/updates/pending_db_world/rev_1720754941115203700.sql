0f94f4ef63d095d4ea467c9bbce902aa641539c0
sogladev <sogladev@gmail.com>
2026-07-29 13:23:39 +0200

    fix(Scripts/ObsidianSanctum): Lava Strike

---
+++ data/sql/updates/pending_db_world/rev_1720754941115203700.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = 57572 AND `ScriptName` = 'spell_sartharion_lava_strike';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (57572, 'spell_sartharion_lava_strike');
