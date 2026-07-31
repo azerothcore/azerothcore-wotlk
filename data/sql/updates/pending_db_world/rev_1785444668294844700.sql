0c42af162ad99232b7a9f83daf5ab958339e3795
Gultask <100873791+Gultask@users.noreply.github.com>
2026-07-31 06:01:26 -0300

    fix(DB/Creature): Use correct model probabilities for Vrykul Harpoon Gun (#26881)

---
+++ data/sql/updates/pending_db_world/rev_1785444668294844700.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 24701);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(24701, 1, 17188, 1, 0, 51831),
+(24701, 0, 22522, 1, 1, 51831);
