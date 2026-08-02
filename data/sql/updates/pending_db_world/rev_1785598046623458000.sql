338b070bb2edd2d791e7e4b96fad2460d314cb9b
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-02 15:40:49 -0300

    leftover

--- data/sql/updates/pending_db_world/rev_1785598046623458000.sql
+++ data/sql/updates/pending_db_world/rev_1785598046623458000.sql
@@ -27,8 +27,6 @@ INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`,
 (24272, 0, 10702, 1, 0, 51831);
 
 -- Fight 1: Firjus the Soul Crusher
-DELETE FROM `creature` WHERE (`id` = 24213) AND (`guid` = 1552);
-
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 24213);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (24213, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Firjus the Soul Crusher - On Aggro - Say Line'),
