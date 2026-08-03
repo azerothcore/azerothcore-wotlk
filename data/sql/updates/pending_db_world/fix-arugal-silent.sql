5decdcef64ab0b73c23a915d5d0b334f8d199354
Ivan Usenka <vanek66650@gmail.com>
2026-08-03 10:16:20 +0300

    Update fix-arugal-silent.sql Codestyle fixes

--- data/sql/updates/pending_db_world/fix-arugal-silent.sql
+++ data/sql/updates/pending_db_world/fix-arugal-silent.sql
@@ -1,6 +1,6 @@
 -- Fix Archmage Arugal is silend during Shadowfang Keep progression
--- All voices IDs were taken from Wowhead 
-update `creature_text` set `Sound` = 5793 where `CreatureID` = 4275 and `BroadcastTextId` = 6115;
-update `creature_text` set `Sound` = 5791 where `CreatureID` = 4275 and `BroadcastTextId` = 1435;
-update `creature_text` set `Sound` = 5797 where `CreatureID` = 4275 and `BroadcastTextId` = 6535;
-update `creature_text` set `Sound` = 5795 where `CreatureID` = 4275 and `BroadcastTextId` = 6116;
+-- All voices IDs were taken from Wowhead
+UPDATE `creature_text` SET `Sound` = 5793 WHERE `CreatureID` = 4275 AND `BroadcastTextId` = 6115;
+UPDATE `creature_text` SET `Sound` = 5791 WHERE `CreatureID` = 4275 AND `BroadcastTextId` = 1435;
+UPDATE `creature_text` SET `Sound` = 5797 WHERE `CreatureID` = 4275 AND `BroadcastTextId` = 6535;
+UPDATE `creature_text` SET `Sound` = 5795 WHERE `CreatureID` = 4275 AND `BroadcastTextId` = 6116;
