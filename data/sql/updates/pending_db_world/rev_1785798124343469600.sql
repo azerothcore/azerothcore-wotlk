f5de4eb891659218a121c40709278ef800f099df
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-03 20:02:45 -0300

    Create rev_1785798124343469600.sql

---
+++ data/sql/updates/pending_db_world/rev_1785798124343469600.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 25350);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(25350, 1, 23175, 1, 0, 51831),
+(25350, 0, 23173, 1, 1, 51831);
