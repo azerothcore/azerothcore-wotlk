51fb7d6145c6e12e8f7485fc9c47ce27a6faffd4
Andrew <47818697+Nyeriah@users.noreply.github.com>
2026-08-03 08:00:14 -0300

    fix(Scripts/Ulduar): allow immunities to free players from Yogg-Saron's Constrictor Tentacle (#26935)
    Co-authored-by: horn <pankrac.ja@seznam.cz>

---
+++ data/sql/updates/pending_db_world/rev_1785720451234545500.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (64125, 64126) AND `ScriptName` = 'spell_yogg_saron_squeeze_aura';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(64125, 'spell_yogg_saron_squeeze_aura'),
+(64126, 'spell_yogg_saron_squeeze_aura');
