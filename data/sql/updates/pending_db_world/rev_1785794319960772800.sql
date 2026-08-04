e16adc4d3d08070841028258a5dd088671503242
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-03 21:10:14 -0300

    Update rev_1785794319960772800.sql

--- data/sql/updates/pending_db_world/rev_1785794319960772800.sql
+++ data/sql/updates/pending_db_world/rev_1785794319960772800.sql
@@ -14,7 +14,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 2623100);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(2623100, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 239, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Set AnimTier 3'),
+(2623100, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 239, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Set AnimTier 3'),
 (2623100, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 69, 1, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 4026.4883, 7297.1025, 640.27356, 0, 'Saragosa - Actionlist - Move To Position');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 9 AND `entryorguid` = 2623101);
@@ -24,7 +24,7 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (2623101, 9, 2, 0, 0, 0, 100, 0, 800, 800, 0, 0, 0, 0, 239, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Set AnimTier 0'),
 (2623101, 9, 3, 0, 0, 0, 100, 0, 1600, 1600, 0, 0, 0, 0, 11, 46802, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Cast \'Frost Breath\''),
 (2623101, 9, 4, 0, 0, 0, 100, 0, 3000, 3000, 0, 0, 0, 0, 5, 15, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Play Emote 15'),
-(2623101, 9, 5, 0, 0, 0, 100, 0, 1200, 1200, 0, 0, 0, 0, 227, 10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Set Scale to 30%'),
+(2623101, 9, 5, 0, 0, 0, 100, 0, 1200, 1200, 0, 0, 0, 0, 227, 10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Set Scale to 10%'),
 (2623101, 9, 7, 0, 0, 0, 100, 0, 3000, 3000, 0, 0, 0, 0, 12, 26232, 4, 60000, 0, 0, 0, 8, 0, 0, 0, 0, 4027.11, 7298.14, 636.055, 1.50098, 'Saragosa - Actionlist - Summon Creature \'Saragosa\''),
 (2623101, 9, 8, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Saragosa - Actionlist - Despawn Instant');
 
