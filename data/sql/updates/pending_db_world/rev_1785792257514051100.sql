2ab4611d2e138a49b8c8eeaefa73567e973a7eb1
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-04 13:08:49 -0300

    fix(DB/Creature): Save Beached Orca in Winterfin Retreat (#26949)

---
+++ data/sql/updates/pending_db_world/rev_1785792257514051100.sql
@@ -0,0 +1,23 @@
+--
+UPDATE `creature` SET
+`position_x`  = 4311.212890625,
+`position_y`  = 6124.88818359375,
+`position_z`  = -9.65260124206542968,
+`orientation` = 0.634859919548034667,
+`CreateObject`  = 2,
+`VerifiedBuild` = 68887 WHERE `id` = 25204 AND `guid` = 106831;
+UPDATE `creature` SET
+`position_x`  = 4242.046875,
+`position_y`  = 6111.78369140625,
+`position_z`  = -22.275308609008789,
+`orientation` = 6.013361454010009765,
+`CreateObject`  = 2,
+`VerifiedBuild` = 68887 WHERE `id` = 25204 AND `guid` = 106828;
+-- This one that was wrong btw
+UPDATE `creature` SET
+`position_x`  = 4352.39697265625,
+`position_y`  = 6166.98681640625,
+`position_z`  = -0.97076267004013061,
+`orientation` = 0.967668235301971435,
+`CreateObject`  = 2,
+`VerifiedBuild` = 68887 WHERE `id` = 25204 AND `guid` = 106829;
