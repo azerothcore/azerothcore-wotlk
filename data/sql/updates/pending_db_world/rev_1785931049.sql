4d628168da6ec9d72d844e9321701360485a19e0
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-08-05 14:27:25 +0200

    fix(DB/Spell): "Mission: Plague This!: Kill Credit" spell now works correctly when the player is mounted on the gryphon. (#26983)

---
+++ data/sql/updates/pending_db_world/rev_1785931049.sql
@@ -0,0 +1,5 @@
+
+-- Add SPELL_ATTR0_CU_ALLOW_INFLIGHT_TARGET
+DELETE FROM `spell_custom_attr` WHERE `spell_id` = 43419;
+INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
+(43419, 262144);
