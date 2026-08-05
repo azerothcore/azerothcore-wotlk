90d40058faa78cb1d6fe0661366e5afa1ad214c8
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-08-05 14:09:19 +0200

    Update rev_1785931049.sql

--- data/sql/updates/pending_db_world/rev_1785931049.sql
+++ data/sql/updates/pending_db_world/rev_1785931049.sql
@@ -2,4 +2,4 @@
 -- Add SPELL_ATTR0_CU_ALLOW_INFLIGHT_TARGET
 DELETE FROM `spell_custom_attr` WHERE `spell_id` = 43419;
 INSERT INTO `spell_custom_attr` (`spell_id`, `attributes`) VALUES
-(43419, 262144); 
+(43419, 262144);
