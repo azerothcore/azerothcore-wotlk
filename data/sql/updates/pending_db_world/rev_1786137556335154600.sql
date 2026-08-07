bd886acc08f3fc8590062eb1a8afcb9f703c5227
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-07 18:21:36 -0300

    init

---
+++ data/sql/updates/pending_db_world/rev_1786137556335154600.sql
@@ -0,0 +1,3 @@
+--
+-- Redundant, already handled in spell_area
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId` = 17) AND (`SourceGroup` = 0) AND (`SourceEntry` = 45614) AND (`SourceId` = 0) AND (`ElseGroup` = 0) AND (`ConditionTypeOrReference` = 23) AND (`ConditionTarget` = 0) AND (`ConditionValue1` = 4125) AND (`ConditionValue2` = 0) AND (`ConditionValue3` = 0);
