150e3788ddc283b96ec1b5d8bbab927af0791099
Andrew <47818697+Nyeriah@users.noreply.github.com>
2026-08-08 10:04:30 -0300

    fix(Scripts/Spells): implement Clear Demonic Circle, cast by General Vezax on engage (#27040)

---
+++ data/sql/updates/pending_db_world/rev_1786192707275526400.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gen_clear_demonic_circle';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(62037, 'spell_gen_clear_demonic_circle');
