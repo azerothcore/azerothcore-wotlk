8f43e9069c6b5920f87a8c4dead8ac1e73462432
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-09 11:13:00 -0300

    despawn ooc
    Updated smart_scripts for Negolash quest interactions.

--- data/sql/updates/pending_db_world/rev_1786220066682267000.sql
+++ data/sql/updates/pending_db_world/rev_1786220066682267000.sql
@@ -4,7 +4,7 @@
 -- Spawn position from vmangos quest_end_scripts (sql/migrations/20211001113141_world.sql).
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 2289 AND `source_type` = 1;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(2289, 1, 0, 0, 20, 0, 100, 0, 619, 0, 0, 0, 0, 0, 12, 1494, 1, 300000, 0, 0, 0, 8, 0, 0, 0, 0, -14598.6, 76.0563, -11.249, 0.925025, 'Ruined Lifeboat - On Quest ''Enticing Negolash'' Rewarded - Summon Negolash'),
+(2289, 1, 0, 0, 20, 0, 100, 0, 619, 0, 0, 0, 0, 0, 12, 1494, 4, 300000, 0, 0, 0, 8, 0, 0, 0, 0, -14598.6, 76.0563, -11.249, 0.925025, 'Ruined Lifeboat - On Quest ''Enticing Negolash'' Rewarded - Summon Negolash'),
 (2289, 1, 1, 0, 20, 0, 100, 0, 619, 0, 0, 0, 0, 0, 241, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ruined Lifeboat - On Quest ''Enticing Negolash'' Rewarded - Summon Gameobject Group 0');
 
 -- Negolash: yell once on summon (was: on out of combat, which fired again after evading), then walk to the boat
