132a9f76dd59c82651cf41113421e329ad35bd0c
Chinoske <guillermovera.tapia@gmail.com>
2026-08-08 23:26:08 -0400

    chore(DB): rewrite the lifeboat smart_scripts block instead of a partial update

--- data/sql/updates/pending_db_world/rev_1786239226000000000.sql
+++ data/sql/updates/pending_db_world/rev_1786239226000000000.sql
@@ -13,8 +13,10 @@ UPDATE `quest_template` SET
     `LogDescription`   = 'Bring 10 Barbecued Buzzard Wings and 5 Junglevine Wine to the Ruined Lifeboat.'
 WHERE `ID` = 619;
 
-UPDATE `smart_scripts` SET `target_x` = -14598.6, `target_y` = 76.0563, `target_z` = -11.249, `target_o` = 0.925025
-WHERE `entryorguid` = 2289 AND `source_type` = 1 AND `id` = 0 AND `event_type` = 20 AND `action_type` = 12;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 2289 AND `source_type` = 1;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(2289, 1, 0, 0, 20, 0, 100, 0, 619, 0, 0, 0, 0, 0, 12, 1494, 1, 0, 0, 0, 0, 8, 0, 0, 0, 0, -14598.6, 76.0563, -11.249, 0.925025, 'Ruined Lifeboat - On Quest Complete - Spawn Negolash'),
+(2289, 1, 1, 0, 20, 0, 100, 0, 619, 0, 0, 0, 0, 0, 241, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Ruined Lifeboat - On Quest Complete - Summon Gameobject Group 0');
 
 DELETE FROM `creature_text` WHERE `CreatureID` = 1494 AND `GroupID` = 1;
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `BroadcastTextId`, `comment`) VALUES
