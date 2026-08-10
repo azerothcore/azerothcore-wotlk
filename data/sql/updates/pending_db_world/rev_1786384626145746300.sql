5994c58d4d799859897814165e573da507fd12e4
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-10 15:34:43 -0300

    Update rev_1786384626145746300.sql

--- data/sql/updates/pending_db_world/rev_1786384626145746300.sql
+++ data/sql/updates/pending_db_world/rev_1786384626145746300.sql
@@ -1,2 +1,2 @@
 -- No spell has these as credit, so the flag is likely correct here
-UPDATE `creature_template` SET `flags_extra` = `flags_extra`|134217728 WHERE (`entry` IN (29978, 29984));
+UPDATE `creature_template` SET `flags_extra` = `flags_extra`|2097152 WHERE (`entry` IN (29978, 29984));
