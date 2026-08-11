a5156126ab5c7c378e456fd0d7c439e57891be1d
Jan125 <13677685+Jan125@users.noreply.github.com>
2026-08-11 09:26:27 +0200

    fix(DB/Command): Typo and clarification in help for account set gmlevel (#27050)

---
+++ data/sql/updates/pending_db_world/rev_1786218933123905765.sql
@@ -0,0 +1,2 @@
+DELETE FROM `command` WHERE `name` IN ('account set gmlevel');
+INSERT INTO `command` (`name`, `security`, `help`) VALUES ('account set gmlevel',3,'Syntax: .account set gmlevel [$account] #level #realmID\r\n\r\nSet the security level for targeted player (can\'t be used at self) or for an account name ($account) to a level of #level on the realm #realmID.\r\n\r\n#level may range from 0 to 3.\r\n\r\n#realmID may be -1 for all realms.');
