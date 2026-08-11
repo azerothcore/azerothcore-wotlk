748a068d263ff46c4ede4d5750f8f8f592db13e1
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-11 10:43:37 -0300

    Apply suggestion from @Gultask

--- data/sql/updates/pending_db_world/rev_1785886834436284800.sql
+++ data/sql/updates/pending_db_world/rev_1785886834436284800.sql
@@ -5,7 +5,7 @@
 -- cmangos classic-db/tbc-db. The short respawn lets every group member on the
 -- quest loot their own Heart, since the chest is consumable. The quest item in
 -- loot 11160 is already QuestRequired.
-DELETE FROM `gameobject` WHERE `guid` = 57;
+DELETE FROM `gameobject` WHERE `guid` = 57 AND `id` = 165554;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
 (57, 165554, 230, 0, 0, 1, 1, 802.907, -356.401, -48.9423, -0.785397, 0, 0, -0.382683, 0.92388, 10, 100, 1, '', 0, NULL);
 
