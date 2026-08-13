5d4846e0dafb6a7db8badabd436497f3abfa0423
Patrik Bukovský <144048412+lilliandris@users.noreply.github.com>
2026-08-13 20:33:11 +0200

    fix(DB/Quest): Remove incorrect requirement for Rifle the Bodies from Your Presence is Required (...) (#26204)
    Co-authored-by: sudlud <sudlud@users.noreply.github.com>
    Co-authored-by: Gultask <100873791+Gultask@users.noreply.github.com>

---
+++ data/sql/updates/pending_db_world/rev_1781483721171248043.sql
@@ -0,0 +1,3 @@
+-- Rifle the Bodies, Your Presence is Required - chain fix
+UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` = 11999;
+UPDATE `quest_template_addon` SET `BreadcrumbForQuestId` = 11999 WHERE (`ID` = 11996);
