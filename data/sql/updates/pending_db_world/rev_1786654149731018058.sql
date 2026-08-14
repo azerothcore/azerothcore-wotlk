9aec3bc03a4556d6cee8f785088b7ece388977d5
Helias <stefanoborzi32@gmail.com>
2026-08-14 22:12:50 +0200

    chore(DB): drop trailing blank line from the 2FA pending files

--- data/sql/updates/pending_db_world/rev_1786654149731018058.sql
+++ data/sql/updates/pending_db_world/rev_1786654149731018058.sql
@@ -1,3 +1,2 @@
 --
 UPDATE `command` SET `help` = 'Syntax: .account 2fa setup <token>\nSets up two-factor authentication for this account.\nRun .account 2fa setup 1 to get your 2FA key, then confirm with .account 2fa setup <token>.' WHERE `name` = 'account 2fa setup';
-
