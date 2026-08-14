cc8cdbcbe48382d4889cce85ad8dcfa3d89b5844
Stefano Borzì <stefanoborzi32@gmail.com>
2026-08-14 18:26:18 +0200

    Apply suggestion from @Helias

--- data/sql/updates/pending_db_world/rev_1786654149731018058.sql
+++ data/sql/updates/pending_db_world/rev_1786654149731018058.sql
@@ -1,2 +1,3 @@
 --
 UPDATE `command` SET `help` = 'Syntax: .account 2fa setup <token>\nSets up two-factor authentication for this account.\nRun .account 2fa setup 1 to get your 2FA key, then confirm with .account 2fa setup <token>.' WHERE `name` = 'account 2fa setup';
+
