34f97d5a70dfd6cf9046c3b4887382d236e612b0
Moostigre <moostigre@proton.me>
2026-08-18 13:09:24 +0200

    fix(DB/Quest): resolve player name in Totem of Vark RP

---
+++ data/sql/updates/pending_db_world/rev_1787051624546656976.sql
@@ -0,0 +1,4 @@
+-- Stillpine Ancestor Vark must address the player so that $n is resolved.
+UPDATE `smart_scripts`
+SET `target_type` = 7
+WHERE `entryorguid` = 1741000 AND `source_type` = 9 AND `id` = 4;
