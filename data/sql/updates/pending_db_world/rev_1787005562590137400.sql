fc0b94732676757a09d4e5379505fdb2842fb549
Gultask <100873791+Gultask@users.noreply.github.com>
2026-08-17 21:17:57 -0300

    Apply suggestion from @Gultask

--- data/sql/updates/pending_db_world/rev_1787005562590137400.sql
+++ data/sql/updates/pending_db_world/rev_1787005562590137400.sql
@@ -6,7 +6,7 @@ UPDATE `creature_template_movement` SET `Ground` = 0, `Flight` = 1 WHERE (`Creat
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 29457);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (29457, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 0, 11, 54495, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Plague Sprayer Trigger - On Just Summoned - Cast \'Plague Spray\''),
-(29457, 0, 1, 0, 60, 0, 100, 1, 3600, 3600, 0, 0, 0, 0, 210, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Plague Sprayer Trigger - On Update - Fall (No Repeat)');
+(29457, 0, 1, 0, 60, 0, 100, 1, 1200, 1200, 0, 0, 0, 0, 210, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Plague Sprayer Trigger - On Update - Fall (No Repeat)');
 
 DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 28274);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
