e32bfb7fac726a9584f08d88c4527cb2e13878c1
Moostigre <moostigre@proton.me>
2026-08-20 03:26:05 +0200

    fix(DB): correct Timmy the Cruel spawn position

--- data/sql/updates/pending_db_world/rev_1787188287042735114.sql
+++ data/sql/updates/pending_db_world/rev_1787188287042735114.sql
@@ -1,5 +1,7 @@
 --
 -- Timmy the Cruel
+UPDATE `creature` SET `position_x` = 3633.946, `position_y` = -3184.0342, `position_z` = 128.83388, `orientation` = 6.049384 WHERE `guid` = 247227;
+
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 10808 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (10808, 0, 0, 0, 0, 0, 100, 0, 3000, 8000, 9000, 14000, 0, 0, 11, 17470, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Timmy the Cruel - In Combat - Cast Ravenous Claw'),
