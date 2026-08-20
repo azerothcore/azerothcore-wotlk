cbe2812466e11e8ff1bb14f0f1deffeea1a016a6
moostigre <moostigre@proton.me>
2026-08-20 07:36:49 +0200

    one liner for CI alert fix

--- data/sql/updates/pending_db_world/rev_1787051624546656976.sql
+++ data/sql/updates/pending_db_world/rev_1787051624546656976.sql
@@ -1,4 +1,2 @@
 -- Stillpine Ancestor Vark must address the player so that $n is resolved.
-UPDATE `smart_scripts`
-SET `target_type` = 7
-WHERE `entryorguid` = 1741000 AND `source_type` = 9 AND `id` = 4;
+UPDATE `smart_scripts` SET `target_type` = 7 WHERE `entryorguid` = 1741000 AND `source_type` = 9 AND `id` = 4;
