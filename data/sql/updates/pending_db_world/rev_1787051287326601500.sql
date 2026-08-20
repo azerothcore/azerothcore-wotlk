2cd52dc7311c8f55478790aeec793a996b728645
moostigre <moostigre@proton.me>
2026-08-20 20:55:40 +0200

    fix(DB/Quest): show Recipe of the Kaldorei progress (#27213)

---
+++ data/sql/updates/pending_db_world/rev_1787051287326601500.sql
@@ -0,0 +1,4 @@
+-- Webwood Lurker should advertise Small Spider Leg for quest 4161, Recipe of the Kaldorei.
+DELETE FROM `creature_questitem` WHERE (`CreatureEntry` = 1998) AND (`Idx` = 1);
+INSERT INTO `creature_questitem` (`CreatureEntry`, `Idx`, `ItemId`, `VerifiedBuild`) VALUES
+(1998, 1, 5465, 0);
