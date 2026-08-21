cb7663dc5340979313e50c16c3950fd6b0086ef4
Ivan Usenka <vanek66650@gmail.com>
2026-08-21 20:56:28 +0300

    Update codestyle

--- data/sql/updates/pending_db_world/darkmaster-gandling-voice.sql
+++ data/sql/updates/pending_db_world/darkmaster-gandling-voice.sql
@@ -1,4 +1,3 @@
 -- Fix Darkmaster Gandling in Scholomance voice lines
 -- Now will pronounce "School is in session!" when spawning
-  
 UPDATE `creature_text` SET `Sound` = 27477 WHERE `CreatureID` = 1853 AND `BroadcastTextId` = 7145;
