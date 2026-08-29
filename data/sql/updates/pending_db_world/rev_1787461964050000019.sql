55fe02101e42d96d803b6c6499be2f7646dd5145
Moostigre <moostigre@proton.me>
2026-08-29 10:54:14 +0200

    fix(DB/Scholomance): Use guaranteed loot fallback

--- data/sql/updates/pending_db_world/rev_1787461964050000019.sql
+++ data/sql/updates/pending_db_world/rev_1787461964050000019.sql
@@ -1,2 +1,1 @@
-DELETE FROM `creature_loot_template` WHERE `Entry` = 10504 AND `Item` = 35031;
-INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (10504, 35031, 35031, 95, 0, 1, 1, 1, 1, 'Lord Alexei Barov - (ReferenceTable)');
+UPDATE `creature_loot_template` SET `Chance` = 0 WHERE `Entry` = 10504 AND `Item` = 35031;
