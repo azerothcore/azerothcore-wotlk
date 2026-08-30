45c20a578cf6bff4d648f955ba45534c994abdfd
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2026-08-30 17:44:30 -0300

    fix(DB/Creature): stamp VerifiedBuild on Ironwork Cannon model rows

--- data/sql/updates/pending_db_world/rev_1788122147238122800.sql
+++ data/sql/updates/pending_db_world/rev_1788122147238122800.sql
@@ -1,2 +1,3 @@
 -- Ironwork Cannon (33264): sniff shows only display id 25723 is used (other models have 0% probability)
-UPDATE `creature_template_model` SET `Probability` = 0 WHERE `CreatureID` = 33264 AND `Idx` IN (0, 1, 3);
+UPDATE `creature_template_model` SET `Probability` = 0, `VerifiedBuild` = 69497 WHERE `CreatureID` = 33264 AND `Idx` IN (0, 1, 3);
+UPDATE `creature_template_model` SET `VerifiedBuild` = 69497 WHERE `CreatureID` = 33264 AND `Idx` = 2;
