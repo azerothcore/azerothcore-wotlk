6dfc0068bb497cb7f6130c199850312e76d24747
Calmoran <47126936+Calmoran@users.noreply.github.com>
2026-08-30 20:36:28 -0500

    refactor(Core/WorldSession): move LoginQueryHolder declaration into the header

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -63,20 +63,6 @@
 #include "WorldSession.h"
 #include "WorldSessionMgr.h"
 
-class LoginQueryHolder : public CharacterDatabaseQueryHolder
-{
-private:
-    uint32 m_accountId;
-    ObjectGuid m_guid;
-public:
-    LoginQueryHolder(uint32 accountId, ObjectGuid guid)
-        : m_accountId(accountId), m_guid(guid) { }
-
-    ObjectGuid GetGuid() const { return m_guid; }
-    uint32 GetAccountId() const { return m_accountId; }
-    bool Initialize();
-};
-
 bool LoginQueryHolder::Initialize()
 {
     SetSize(MAX_PLAYER_LOGIN_QUERY);
