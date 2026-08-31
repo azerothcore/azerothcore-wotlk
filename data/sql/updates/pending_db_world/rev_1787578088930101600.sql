9a3ba2dbc2b457d6648083aa22ef8710faa2fed8
EricksOliveira <ericksoliveira258@gmail.com>
2026-08-31 15:48:36 +0000

    new fix

--- data/sql/updates/pending_db_world/rev_1787578088930101600.sql
+++ data/sql/updates/pending_db_world/rev_1787578088930101600.sql
@@ -3,41 +3,60 @@
 -- - Applies Defensive ReactState only to Mimiron Arena Bombots through negative GUID overrides
 -- - Adds formations for the corridor Bombots before the tram
 
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 34192;
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` |134217728 WHERE (`entry` IN (34192, 34216));
 
-UPDATE `creature` SET `spawntimesecs` = 30 WHERE `id` = 34192 AND `guid` BETWEEN 1975178 AND 1975197;
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE (`entry` IN (34192));
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 34192 AND `source_type` = 0;
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 34192);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (34192, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 - On Reset - Disable Auto Attack'),
 (34192, 0, 1, 2, 101, 0, 100, 0, 1, 1, 500, 0, 0, 0, 11, 63801, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 - On 1 or More Players in Range - Cast \'Bomb Bot\''),
-(34192, 0, 2, 3, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 37, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 - On Link - Kill Self'),
-(34192, 0, 3, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 - On Link - Despawn In 2000 ms');
+(34192, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 0, 37, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 - On 1 or More Players in Range - Kill Self');
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` BETWEEN -1975197 AND -1975178 AND `source_type` = 0;
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` IN (-1975178, -1975179, -1975180, -1975181, -1975182, -1975183, -1975184, -1975185, -1975186, -1975187, -1975188, -1975189, -1975190, -1975191, -1975192, -1975193, -1975194, -1975195, -1975196, -1975197));
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(-1975178, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975179, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975180, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975181, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975182, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975183, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975184, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975185, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975186, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975187, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975188, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975189, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975190, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975191, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975192, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975193, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975194, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975195, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975196, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive'),
-(-1975197, 0, 0, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set ReactState Defensive');
+(-1975178, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975178, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975179, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975179, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975180, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975180, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975181, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975181, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975182, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975182, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975183, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975183, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975184, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975184, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975185, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975185, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975186, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975186, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975187, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975187, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975188, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975188, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975189, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975189, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975190, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975190, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975191, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975191, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975192, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975192, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975193, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975193, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975194, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975194, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975195, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975195, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975196, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975196, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms'),
+(-1975197, 0, 1001, 0, 25, 0, 100, 0, 0, 0, 0, 0, 0, 0, 8, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Reset - Set Reactstate Defensive'),
+(-1975197, 0, 1002, 0, 6, 0, 100, 0, 0, 0, 0, 0, 0, 0, 41, 2000, 30, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Boomer XP-500 (Mimiron Arena) - On Just Died - Despawn In 2000 ms');
 
-DELETE FROM `creature_formations` WHERE `leaderGUID` IN (136535, 136536, 136534, 136575) AND `groupAI` = 3;
+DELETE FROM `creature_formations` WHERE (`LeaderGUID` IN (136535, 136536, 136534, 136575));
 INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
 (136535, 136535, 0, 0, 3, 0, 0),
 (136535, 136560, 0, 0, 3, 0, 0),
