d4cdb731a19f557139f5cc9351f4d15e5ff19030
AlsoNotMehh <itsbrayanrodriguez@gmail.com>
2026-09-01 04:31:14 -0400

    style(SQL): Indent VALUES rows in migration script

--- data/sql/updates/pending_db_world/rev_1788249089000000000.sql
+++ data/sql/updates/pending_db_world/rev_1788249089000000000.sql
@@ -5,5 +5,5 @@ DELETE FROM `spell_proc` WHERE `SpellId` = -14143;
 -- so both sub-spells benefit from the critical strike bonus before being consumed in AfterCast.
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_rog_remorseless_attacks' AND `spell_id` IN (14143, 14149);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(14143, 'spell_rog_remorseless_attacks'),
-(14149, 'spell_rog_remorseless_attacks');
+    (14143, 'spell_rog_remorseless_attacks'),
+    (14149, 'spell_rog_remorseless_attacks');
