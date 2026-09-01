0a47e9eb54718e1b2462f07d3c4851d192d34835
AlsoNotMehh <itsbrayanrodriguez@gmail.com>
2026-09-01 03:19:03 -0400

    fix(Scripts/Spells): Prevent Combustion proc stacks from getting stuck without Combustion active

--- src/server/scripts/Spells/spell_mage.cpp
+++ src/server/scripts/Spells/spell_mage.cpp
@@ -415,10 +415,18 @@ class spell_mage_glyph_of_eternal_water : public AuraScript
     {
         PrepareAuraScript(spell_mage_combustion_proc);
 
-    bool Validate(SpellInfo const* /*spellInfo*/) override
-    {
-        return ValidateSpellInfo({ SPELL_MAGE_COMBUSTION });
-    }
+        bool Validate(SpellInfo const* /*spellInfo*/) override
+        {
+            return ValidateSpellInfo({ SPELL_MAGE_COMBUSTION });
+        }
+
+        void OnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        {
+            if (!GetTarget()->HasAura(SPELL_MAGE_COMBUSTION))
+            {
+                GetAura()->Remove();
+            }
+        }
 
         void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
         {
@@ -427,7 +435,10 @@ class spell_mage_glyph_of_eternal_water : public AuraScript
 
         void Register() override
         {
-            AfterEffectRemove += AuraEffectRemoveFn(spell_mage_combustion_proc::OnRemove, EFFECT_0, SPELL_AURA_ADD_FLAT_MODIFIER, AURA_EFFECT_HANDLE_REAL);
+            AfterEffectApply += AuraEffectApplyFn(spell_mage_combustion_proc::OnApply, EFFECT_0,
+                SPELL_AURA_ADD_FLAT_MODIFIER, AURA_EFFECT_HANDLE_REAL);
+            AfterEffectRemove += AuraEffectRemoveFn(spell_mage_combustion_proc::OnRemove, EFFECT_0,
+                SPELL_AURA_ADD_FLAT_MODIFIER, AURA_EFFECT_HANDLE_REAL);
         }
     };
 
@@ -1065,7 +1076,10 @@ class spell_mage_combustion : public AuraScript
             Unit* actor = eventInfo.GetActor();
             actor->m_Events.AddEventAtOffset([actor]()
             {
-                actor->CastSpell(static_cast<Unit*>(nullptr), SPELL_MAGE_COMBUSTION_PROC, true);
+                if (actor->HasAura(SPELL_MAGE_COMBUSTION))
+                {
+                    actor->CastSpell(static_cast<Unit*>(nullptr), SPELL_MAGE_COMBUSTION_PROC, true);
+                }
             }, 1ms);
             return false;
         }
@@ -1073,9 +1087,16 @@ class spell_mage_combustion : public AuraScript
         return true;
     }
 
+    void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        GetTarget()->RemoveAurasDueToSpell(SPELL_MAGE_COMBUSTION_PROC);
+    }
+
     void Register() override
     {
         DoCheckProc += AuraCheckProcFn(spell_mage_combustion::CheckProc);
+        AfterEffectRemove += AuraEffectRemoveFn(spell_mage_combustion::OnRemove, EFFECT_0,
+            SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
     }
 };
 
