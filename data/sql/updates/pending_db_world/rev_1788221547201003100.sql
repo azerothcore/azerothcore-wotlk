5fe8ef7fc936420b798064559ce1cab2f1d7c32b
tomfranz2000-glitch <282922167+tomfranz2000-glitch@users.noreply.github.com>
2026-09-02 01:54:24 +0200

    chore(DB): Fix SQL continuation indentation

--- data/sql/updates/pending_db_world/rev_1788221547201003100.sql
+++ data/sql/updates/pending_db_world/rev_1788221547201003100.sql
@@ -3,12 +3,12 @@ UPDATE `quest_template_addon` SET `ExclusiveGroup` = 0 WHERE `ID` IN (6541, 6542
 
 DELETE FROM `conditions`
 WHERE `SourceTypeOrReferenceId` = 19 AND `SourceGroup` = 0 AND `SourceEntry` = 6541 AND `SourceId` = 0
-  AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 47 AND `ConditionTarget` = 0
-  AND `ConditionValue1` = 6542 AND `ConditionValue2` = 65 AND `ConditionValue3` = 0;
+    AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 47 AND `ConditionTarget` = 0
+    AND `ConditionValue1` = 6542 AND `ConditionValue2` = 65 AND `ConditionValue3` = 0;
 DELETE FROM `conditions`
 WHERE `SourceTypeOrReferenceId` = 19 AND `SourceGroup` = 0 AND `SourceEntry` = 6542 AND `SourceId` = 0
-  AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 47 AND `ConditionTarget` = 0
-  AND `ConditionValue1` = 6541 AND `ConditionValue2` = 65 AND `ConditionValue3` = 0;
+    AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 47 AND `ConditionTarget` = 0
+    AND `ConditionValue1` = 6541 AND `ConditionValue2` = 65 AND `ConditionValue3` = 0;
 /* DELETE safety: exact full condition primary keys removed above. */
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`,
 `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`,
