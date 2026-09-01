155d6c7e9990a4e703f2645db9c7474b90b145bf
AlsoNotMehh <itsbrayanrodriguez@gmail.com>
2026-09-01 03:52:13 -0400

    fix(Scripts/Spells): Fix Remorseless Attacks not being consumed by Mutilate

---
+++ data/sql/updates/pending_db_world/rev_1788249089000000000.sql
@@ -0,0 +1,9 @@
+-- Remove erroneous Relentless Strikes spell_proc override that was incorrectly assigned to Remorseless Attacks
+DELETE FROM spell_proc WHERE SpellId = -14143;
+
+-- Remorseless Attacks: Bind AuraScript to prevent charge consumption on Mutilate MH/OH,
+-- so both sub-spells benefit from the critical strike bonus before being consumed in AfterCast.
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_rog_remorseless_attacks';
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES
+(14143, 'spell_rog_remorseless_attacks'),
+(14149, 'spell_rog_remorseless_attacks');
