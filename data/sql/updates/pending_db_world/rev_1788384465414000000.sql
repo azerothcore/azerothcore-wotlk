1b6bb182d3ffa23d9c5bf9a233c9b55942495a8a
AlsoNotMehh <itsbrayanrodriguez@gmail.com>
2026-09-02 17:29:21 -0400

    fix(DB/Creature): remove excess 4th trash pack before Mimiron tram in Ulduar

---
+++ data/sql/updates/pending_db_world/rev_1788384465414000000.sql
@@ -0,0 +1,10 @@
+-- Ulduar: Remove excess 4th trash pack before Mimiron tram (removed in patch 3.3.0/3.3.2)
+DELETE FROM `creature` WHERE `guid` IN (
+    136570, 136571, 136572, 136573, 136574, 136575,
+    136578, 136579, 136580, 136581, 136591, 136592
+);
+DELETE FROM `creature_addon` WHERE `guid` IN (136591, 136592);
+DELETE FROM `creature_formations` WHERE `leaderGUID` = 136575 OR `memberGUID` IN (
+    136570, 136571, 136572, 136573, 136574, 136575,
+    136578, 136579, 136580, 136581, 136591, 136592
+);
