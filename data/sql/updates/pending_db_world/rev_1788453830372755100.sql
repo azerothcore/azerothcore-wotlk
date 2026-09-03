bcee93ab3694b0e474842370be0f0a2951128071
EricksOliveira <ericksoliveira258@gmail.com>
2026-09-03 16:49:31 +0000

    fix(DB/Quest): Gate Gluttonous Lurkers behind Precious Elemental Fluids (#27406)
    Gluttonous Lurkers (12527) had no prerequisite, so Apprentice Pestlepot offered it to
    anyone at level 74+. It should only open up once Precious Elemental Fluids (12510) is
    turned in.

---
+++ data/sql/updates/pending_db_world/rev_1788453830372755100.sql
@@ -0,0 +1,4 @@
+--
+-- Gluttonous Lurkers (12527) was offered to anyone at level 74+, but it should only open up
+-- once Precious Elemental Fluids (12510) is completed and turned in.
+UPDATE `quest_template_addon` SET `PrevQuestID` = 12510 WHERE `ID` = 12527;
