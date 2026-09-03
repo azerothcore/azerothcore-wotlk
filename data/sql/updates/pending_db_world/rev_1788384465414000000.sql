dd61e000a8e86d20b1b64496afa6969e645eb442
Gultask <100873791+Gultask@users.noreply.github.com>
2026-09-03 12:56:07 -0300

    Refine SQL to remove specific creature IDs

--- data/sql/updates/pending_db_world/rev_1788384465414000000.sql
+++ data/sql/updates/pending_db_world/rev_1788384465414000000.sql
@@ -1,5 +1,5 @@
 -- Ulduar: Remove excess 4th trash pack before Mimiron tram (removed in patch 3.3.0/3.3.2)
-DELETE FROM `creature` WHERE `guid` IN (
+DELETE FROM `creature` WHERE `id` IN (34191, 34192, 34193) AND `guid` IN (
     136570, 136571, 136572, 136573, 136574, 136575,
     136578, 136579, 136580, 136581, 136591, 136592
 );
