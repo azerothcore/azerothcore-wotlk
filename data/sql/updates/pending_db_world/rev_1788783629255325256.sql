eb8d905a0d0e95a784f4670734e5ebdddbbf982f
sogladev <sogladev@gmail.com>
2026-09-07 17:35:15 +0200

    fix(DB/Immunities): Update Algalon Dark Matter immunities (#27540)

---
+++ data/sql/updates/pending_db_world/rev_1788783629255325256.sql
@@ -0,0 +1,7 @@
+--
+SET @ID := -430;
+UPDATE `creature_template` SET `CreatureImmunitiesId` = @ID WHERE (`entry` IN (33089, 34221, 34097, 34222));
+
+DELETE FROM `creature_immunities` WHERE `ID`=@ID;
+INSERT INTO `creature_immunities` (`ID`, `SchoolMask`, `DispelTypeMask`, `MechanicsMask`, `Effects`, `Auras`, `ImmuneAoE`, `ImmuneChain`, `Comment`)
+VALUES(@ID, 0, 0, 0x4D96663E, '', '', 0, 0, 'mech=0x4D96663E(CHARM|DISORIENTED|DISARM|DISTRACT|FEAR|SILENCE|SLEEP|FREEZE|KNOCKOUT|POLYMORPH|BANISH|SHACKLE|TURN|HORROR|INTERRUPT|DAZE|SAPPED)');
