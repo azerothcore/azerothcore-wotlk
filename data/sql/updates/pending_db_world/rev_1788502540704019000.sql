66729598fc291da4c9f0bf0e064b03261cc7f008
Rocco Silipo <108557877+Rorschach91@users.noreply.github.com>
2026-09-08 18:44:53 +0200

    Update rev_1788502540704019000.sql

--- data/sql/updates/pending_db_world/rev_1788502540704019000.sql
+++ data/sql/updates/pending_db_world/rev_1788502540704019000.sql
@@ -1,4 +1,2 @@
--- Storm Tempered Keepers in Ulduar must assist both members of each pair and evade together.
-UPDATE `creature_formations` SET `groupAI` = 7 WHERE (`leaderGUID`, `memberGUID`) IN
-    ((136763, 136763), (136764, 136764), (136765, 136765), (136766, 136766),
-    (136763, 136783), (136764, 136784), (136765, 136785), (136766, 136786));
+-- Update groupAI.
+UPDATE `creature_formations` SET `groupAI` = 3 WHERE (`leaderGUID` IN (136763, 136764, 136765, 136766));
