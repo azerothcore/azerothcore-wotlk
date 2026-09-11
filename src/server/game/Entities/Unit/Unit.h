66a4cc072a3f169ab7ddc430ab6b92c2d297ebcc
ratkosrb <ratkomladic2@abv.bg>
2026-09-11 20:55:47 +0300

    Name all hitinfo flags seen in sniffs.

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -96,29 +96,31 @@ enum VictimState
 enum HitInfo
 {
     HITINFO_NORMALSWING         = 0x00000000,
-    HITINFO_UNK1                = 0x00000001,               // req correct packet structure
+    HITINFO_DEBUG               = 0x00000001,               // packet includes extra debug data such as chance to crit, miss, dodge, etc
     HITINFO_AFFECTS_VICTIM      = 0x00000002,
     HITINFO_OFFHAND             = 0x00000004,
-    HITINFO_UNK2                = 0x00000008,
+    HITINFO_UNK2                = 0x00000008,               // not seen in sniffs
     HITINFO_MISS                = 0x00000010,
     HITINFO_FULL_ABSORB         = 0x00000020,
     HITINFO_PARTIAL_ABSORB      = 0x00000040,
     HITINFO_FULL_RESIST         = 0x00000080,
     HITINFO_PARTIAL_RESIST      = 0x00000100,
     HITINFO_CRITICALHIT         = 0x00000200,               // critical hit
-    HITINFO_UNK10               = 0x00000400,
-    HITINFO_UNK11               = 0x00000800,
-    HITINFO_UNK12               = 0x00001000,
-    HITINFO_BLOCK               = 0x00002000,               // blocked damage
-    HITINFO_UNK14               = 0x00004000,               // set only if meleespellid is present//  no world text when victim is hit for 0 dmg(HideWorldTextForNoDamage?)
-    HITINFO_UNK15               = 0x00008000,               // player victim?// something related to blod sprut visual (BloodSpurtInBack?)
+    HITINFO_ROLLED_DODGE        = 0x00000400,               // victim is able to dodge and chance was rolled, application follows code flow in RollMeleeOutcomeAgainst
+    HITINFO_ROLLED_PARRY        = 0x00000800,               // victim is able to parry and chance was rolled, application follows code flow in RollMeleeOutcomeAgainst
+    HITINFO_ROLLED_BLOCK        = 0x00001000,               // victim is able to block and chance was rolled, application follows code flow in RollMeleeOutcomeAgainst
+    HITINFO_BLOCK               = 0x00002000,               // blocked damage, always implies HITINFO_ROLLED_BLOCK
+    HITINFO_NO_FLOATING_TEXT    = 0x00004000,               // set only if meleespellid is present, no world text when victim is hit for 0 dmg(HideWorldTextForNoDamage?)
+    HITINFO_BLOOD_SPURT         = 0x00008000,               // sprays extra blood, set on 100% of hits where damage (incl. absorb/resist/block)
+                                                            // is between 20% and 100% of victim max health, only seen with player victims
     HITINFO_GLANCING            = 0x00010000,
     HITINFO_CRUSHING            = 0x00020000,
-    HITINFO_NO_ANIMATION        = 0x00040000,
-    HITINFO_UNK19               = 0x00080000,
-    HITINFO_UNK20               = 0x00100000,
-    HITINFO_SWINGNOHITSOUND     = 0x00200000,               // unused?
-    HITINFO_UNK22               = 0x00400000,
+    HITINFO_NO_ANIMATION        = 0x00040000,               // always set if meleespellid is present
+    HITINFO_PVP                 = 0x00080000,               // set if and only if attacker and victim both have UNIT_FLAG_PLAYER_CONTROLLED
+    HITINFO_DUEL                = 0x00100000,               // always implies HITINFO_PVP, never seen in battlegrounds or arenas, often seen in sanctuary areas
+    HITINFO_SWINGNOHITSOUND     = 0x00200000,               // not seen in sniffs
+    HITINFO_CRITICAL_BLOCK      = 0x00400000,               // always implies HITINFO_BLOCK, victim is always a warrior (protection talent Critical Block),
+                                                            // per victim the highest blocked amount with this flag is exactly 2x the highest blocked amount without it
     HITINFO_RAGE_GAIN           = 0x00800000,
     HITINFO_FAKE_DAMAGE         = 0x01000000                // enables damage animation even if no damage done, set only if no damage
 };
