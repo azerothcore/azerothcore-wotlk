6d23bba07b4a8ec108a4b1dcc61d311a836e6826
sudlud <sudlud@users.noreply.github.com>
2026-09-12 21:59:51 +0200

    remove trailing comma in enum

--- src/server/scripts/Outland/CoilfangReservoir/SlavePens/the_slave_pens.h
+++ src/server/scripts/Outland/CoilfangReservoir/SlavePens/the_slave_pens.h
@@ -76,7 +76,7 @@ enum SPGameObjectIds
     GO_ICE_SPEAR                         = 188077,
     GO_ICE_STONE                         = 187882,
     GO_ICE_CHEST                         = 188192,
-    GO_ICE_CHEST_2                       = 187892,
+    GO_ICE_CHEST_2                       = 187892
 };
 
 template <class AI, class T>
