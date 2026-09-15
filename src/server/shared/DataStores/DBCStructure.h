c516248aab4e3c75576ba2d910323666df5db9d9
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-09-15 09:47:45 -0700

    clean up comments

--- src/server/shared/DataStores/DBCStructure.h
+++ src/server/shared/DataStores/DBCStructure.h
@@ -645,14 +645,14 @@ enum CharSectionType
 
 struct CharSectionsEntry
 {
-    //uint32 Id;
-    uint32 Race;
-    uint32 Gender;
-    uint32 GenType;
-    //char* TexturePath[3];
-    uint32 Flags;
-    uint32 Type;
-    uint32 Color;
+    //uint32 ID;                                            // 0
+    uint32 RaceID;                                          // 1
+    uint32 SexID;                                           // 2
+    uint32 BaseSection;                                     // 3, CharSectionType
+    //char const* TextureName[3];                           // 4-6
+    uint32 Flags;                                           // 7, CharSectionFlags
+    uint32 VariationIndex;                                  // 8
+    uint32 ColorIndex;                                      // 9
 };
 
 struct CharTitlesEntry
