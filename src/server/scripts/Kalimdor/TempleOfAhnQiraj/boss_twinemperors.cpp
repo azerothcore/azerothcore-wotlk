5e1a25692bd7f84d09dcd00ff3dfbedd66e02853
AnielloTortora_VitaDev <aniello.tortora@vitameals.com>
2026-09-17 09:19:47 +0200

    fix(Scripts/AQ40): Prevent Twin Emperors & C'Thun engagement without required bosses
    Enforce CheckRequiredBosses in Temple of Ahn'Qiraj (AQ40) instance script
    so that Twin Emperors require Princess Huhuran to be defeated, and C'Thun
    requires Twin Emperors to be defeated.
    
    Prevent players from bypassing Huhuran to engage Twin Emperors.
    
    Closes #13395

--- src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_twinemperors.cpp
+++ src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_twinemperors.cpp
@@ -231,6 +231,12 @@ struct boss_twinemperorsAI : public BossAI
 
     void JustEngagedWith(Unit* who) override
     {
+        if (!instance->CheckRequiredBosses(DATA_TWIN_EMPERORS, who->ToPlayer()))
+        {
+            EnterEvadeMode(EVADE_REASON_OTHER);
+            return;
+        }
+
         BossAI::JustEngagedWith(who);
 
         if (!_introDone)
