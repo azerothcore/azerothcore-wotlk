a991740f839797b9b2a7cf2e4ac800cbfd855cff
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-09-18 09:04:56 -0700

    Set new lfg failure enum

--- src/server/game/DungeonFinding/LFGQueue.cpp
+++ src/server/game/DungeonFinding/LFGQueue.cpp
@@ -413,7 +413,7 @@ namespace lfg
             return LFG_COMPATIBILITY_PENDING;
 
         if (!sScriptMgr->CanCreateLfgProposal(proposal.queues))
-            return LFG_INCOMPATIBLES_HAS_IGNORES;
+            return LFG_INCOMPATIBLES_REJECTED_BY_SCRIPT;
 
         // Create a new proposal
         proposal.cancelTime = GameTime::GetGameTime().count() + LFG_TIME_PROPOSAL;
