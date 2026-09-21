026e585e6bcd91058fc736d68df577b556327053
Gultask <100873791+Gultask@users.noreply.github.com>
2026-09-21 18:20:20 -0300

    Update data/sql/updates/pending_db_world/rev_1789570644571409200.sql

--- data/sql/updates/pending_db_world/rev_1789570644571409200.sql
+++ data/sql/updates/pending_db_world/rev_1789570644571409200.sql
@@ -1,4 +1,4 @@
 DELETE FROM `areatrigger_teleport` WHERE `ID` IN (5683, 5688);
 INSERT INTO `areatrigger_teleport` (`ID`, `Name`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
-(5683, 'Halls of Reflection (Entrance from Pit of Saron)', 668, 5239.01, 1932.64, 707.695, 0.800565),
-(5688, 'Pit of Saron (Entrance from Forge of Souls)', 658, 435.743, 212.413, 528.709, 6.25646);
+(5683, 'Halls of Reflection (Entrance from Pit of Saron)', 668, 5239.46, 1932.99, 707.695, 0.785398),
+(5688, 'Pit of Saron (Entrance from Forge of Souls)', 658, 432.568, 212.344, 528.709, 0);
