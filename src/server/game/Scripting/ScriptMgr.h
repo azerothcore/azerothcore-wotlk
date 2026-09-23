2d6742b267fe2e86200d223214b08cfff94c979a
Keleborn <22352763+Celandriel@users.noreply.github.com>
2026-09-23 09:45:32 -0700

    feat(Core/Scripting): add OnPlayerCanMarkAccountOffline player hook (#27599)
    Co-authored-by: UltraNix <ultranix@gmail.com>

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -335,6 +335,7 @@ public: /* PlayerScript */
     void OnPlayerLoadFromDB(Player* player);
     void OnPlayerBeforeLogout(Player* player);
     void OnPlayerLogout(Player* player);
+    bool OnPlayerCanMarkAccountOffline(ObjectGuid guid, uint32 accountId);
     void OnPlayerCreate(Player* player);
     void OnPlayerSave(Player* player);
     void OnPlayerDelete(ObjectGuid guid, uint32 accountId);
